3dfe60be1aafd4f06a9636fdb4ec4c0b4ef87529
Caroline Murphy <cmurphydesign@gmail.com>
2014-06-15 21:51:14 +0100

    Fixed missing images and code layout.

--- do-it-online.php
+++ do-it-online.php
@@ -12,140 +12,163 @@
 
 <?php include("inc/skip-links.php"); ?>
 
-<div> <a href="home.html" title="Small Business Service home"><img src="../../syntegra/images/global-business-link-logo.png" alt="Business Link" border="0" /></a> </div>
-<div>Information. Support. Compliance.</div>
-<span><a name="skiplogin"></a></span>
 <div>
-		<p> Your account </p>
-		<ul>
-		<li> <a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate"> Register </a> </li>
-		<li> <a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate"> Log in </a> </li>
+	<a href="home.html" title="Small Business Service home">
+		<img src="img/global-business-link-logo.png" alt="Business Link" border="0" />
+	</a>
+</div>
+
+<div>Information. Support. Compliance.</div>
+
+<span><a name="skiplogin"></a></span>
+
+<div>
+	<p>Your account</p>
+	<ul>
+		<li><a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate">Register</a></li>
+		<li><a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate">Log in</a></li>
 	</ul>
+</div>
+
+<div>
+	<div>
+		<p><a href="home.html">Home</a>&gt;Do it online</p>
 	</div>
-<div><img src="../../syntegra/images/px_trans.gif" alt="" width="745" height="1" /></div>
-<div >
+	
+	<span><a name="skipcont"></a></span>
+
+	<div>
 		<div>
-		<p><a href="home.html"  title="" >Home</a>&gt;Do it online</p>
-	</div>
-		<span><a name="skipcont"></a></span>
-		<div>
-		<div>
-				<h1>Do it online</h1>
-				Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies, as well as interactive tools to help you run your business.
-				<p>Most downloadable forms are in Adobe Acrobat&reg; portable document format (PDF) - <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=HelpWithPDFFiles"  >see our help page on using Adobe Acrobat&reg;</a>. </p>
-				<div>
+			<h1>Do it online</h1>
+			Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies, as well as interactive tools to help you run your business.
+			<p>Most downloadable forms are in Adobe Acrobat&reg; portable document format (PDF) - <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=HelpWithPDFFiles">see our help page on using Adobe Acrobat&reg;</a>.</p>
+			<div>
 				<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/searchDIOL">
+					<div>
 						<div>
-						<div>
-								<label for="searchby">Search do it online items</label>
-								<input type="text" name="expression" />
-								<input type="hidden" name="resultPage" value="1" />
-								<input name="go" value="go" src="../../syntegra/images/button_go_find_form.gif" alt="Go" type="image" />
-								<input name="topicId" value="1075193162" type="hidden" />
-							</div>
+							<label for="searchby">Search do it online items</label>
+							<input type="text" name="expression" />
+							<input type="hidden" name="resultPage" value="1" />
+							<input name="go" value="go" src="img/button_go_find_form.gif" alt="Go" type="image" />
+							<input name="topicId" value="1075193162" type="hidden" />
+						</div>
 					</div>
-					</form>
+				</form>
 			</div>
-				<div>
-				<p>Browse: <a href="showallDIOL21a5.html?topicId=1075193162"  title="" >by A-Z</a> | <a href="browsebydioldepartment21a5.html?topicId=1075193162"  title="" >by department</a></p>
+			<div>
+				<p>Browse: <a href="showallDIOL21a5.html?topicId=1075193162">by A-Z</a> | <a href="browsebydioldepartment21a5.html?topicId=1075193162">by department</a></p>
 			</div>
-			</div>
+		</div>
 		<div>
-				<div>
+			<div>
 				<h3>Most popular do it online items</h3>
 				<div>
-						<ul>
-						<li><a href="detailb063.html?itemId=1097427618&amp;type=PIP"  >Register online for business taxes</a></li>
-						<li><a href="detaila1a1.html?itemId=1097427746&amp;type=PIP"  >Use the Business Tax Dashboard</a></li>
-						<li><a href="detail8f1a.html?itemId=1082653972&amp;type=PIP" title="Access HMRC's PAYE Online for employers services" >PAYE Online for Employers</a></li>
-						<li><a href="detail8aaf.html?itemId=1083089667&amp;type=PIP" title="Access the Self Assessment Online for individuals service from HMRC" >Self Assessment Online</a></li>
-						<li><a href="detail9862.html?itemId=1082870261&amp;type=PIP" title="Access to HMRC VAT online and links to information on service availability and support" >Submit your VAT return online</a></li>
-						<li><a href="blukLicenceLanding.html" title="Find and apply for licences and permits for your business" >Apply for licences</a></li>
-						<li><a href="detailbbff.html?itemId=1083081245&amp;type=PIP" title="Access HMRC's Corporation Tax Online service" >Corporation Tax Online</a></li>
-						<li><a href="detail8eee.html?itemId=1083034263&amp;type=PIP" title="Access HMRC's CIS Online service" >CIS Online</a></li>
-						<li><a href="detail90ce.html?itemId=1081930073&amp;type=PIP" title="File most company information online (some services require payment)" >Companies House WebFiling</a></li>
-						<li><a href="detailb48e.html?itemId=1086687162&amp;type=PIP" title="Incorporate your company online as a private company, limited by shares, with model articles of association" >Incorporate your limited company online with Companies House</a></li>
-						<li><a href="detaila434.html?itemId=1084193797&amp;type=PIP" title="Access the Employer Direct online service from Jobcentre Plus" >Advertise a job using the Employer Direct online service</a></li>
-						<li><a href="detail4d0f.html?itemId=1086600605&amp;type=PIP" >Contracts Finder</a></li>
-						<li><a href="https://online.businesslink.gov.uk/bdotg/action/gwservicemgmt"  >Manage your services</a></li>
-						<li><a href="detail9017.html?itemId=1097066856&amp;type=PIP"  >UK Trade Tariff</a></li>
+					<ul>
+						<li><a href="detailb063.html?itemId=1097427618&amp;type=PIP">Register online for business taxes</a></li>
+						<li><a href="detaila1a1.html?itemId=1097427746&amp;type=PIP">Use the Business Tax Dashboard</a></li>
+						<li><a href="detail8f1a.html?itemId=1082653972&amp;type=PIP" title="Access HMRC's PAYE Online for employers services">PAYE Online for Employers</a></li>
+						<li><a href="detail8aaf.html?itemId=1083089667&amp;type=PIP" title="Access the Self Assessment Online for individuals service from HMRC">Self Assessment Online</a></li>
+						<li><a href="detail9862.html?itemId=1082870261&amp;type=PIP" title="Access to HMRC VAT online and links to information on service availability and support">Submit your VAT return online</a></li>
+						<li><a href="blukLicenceLanding.html" title="Find and apply for licences and permits for your business">Apply for licences</a></li>
+						<li><a href="detailbbff.html?itemId=1083081245&amp;type=PIP" title="Access HMRC's Corporation Tax Online service">Corporation Tax Online</a></li>
+						<li><a href="detail8eee.html?itemId=1083034263&amp;type=PIP" title="Access HMRC's CIS Online service">CIS Online</a></li>
+						<li><a href="detail90ce.html?itemId=1081930073&amp;type=PIP" title="File most company information online (some services require payment)">Companies House WebFiling</a></li>
+						<li><a href="detailb48e.html?itemId=1086687162&amp;type=PIP" title="Incorporate your company online as a private company, limited by shares, with model articles of association">Incorporate your limited company online with Companies House</a></li>
+						<li><a href="detaila434.html?itemId=1084193797&amp;type=PIP" title="Access the Employer Direct online service from Jobcentre Plus">Advertise a job using the Employer Direct online service</a></li>
+						<li><a href="detail4d0f.html?itemId=1086600605&amp;type=PIP">Contracts Finder</a></li>
+						<li><a href="https://online.businesslink.gov.uk/bdotg/action/gwservicemgmt">Manage your services</a></li>
+						<li><a href="detail9017.html?itemId=1097066856&amp;type=PIP">UK Trade Tariff</a></li>
 					</ul>
-					</div>
+				</div>
 			</div>
-				<div>
+			<div>
 				<h3>Business support tools</h3>
 				<div>
-						<ul>
-						<li><a href="detail29fc.html?itemId=1084515656&amp;type=ONEOFFPAGE" title="Links to interactive tools" >View our interactive tools directory</a></li>
-						<li><a href="http://online.businesslink.gov.uk/bdotg/action/bsf" title="Search our database of business grants, loans, expertise and advice that your business may be eligible for" >Use the business support finder</a></li>
+					<ul>
+						<li><a href="detail29fc.html?itemId=1084515656&amp;type=ONEOFFPAGE" title="Links to interactive tools">View our interactive tools directory</a></li>
+						<li><a href="http://online.businesslink.gov.uk/bdotg/action/bsf" title="Search our database of business grants, loans, expertise and advice that your business may be eligible for">Use the business support finder</a></li>
 					</ul>
-					</div>
+				</div>
 			</div>
-			</div>
+		</div>
 	</div>
-		<span><a name="skipfooter"></a></span>
+	
+	<span><a name="skipfooter"></a></span>
+
+	<div>
 		<div>
-		<div><img src="../../syntegra/images/footer_top3.gif" alt="" />
-				<div> <span><a href="https://online.businesslink.gov.uk/bdotg/action/userSupport?refPage=%2Fbdotg%2Faction%2Flayer%3FtopicId%3D1075193162" title="Help &amp; support" >Help &amp; support</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Copyright" title="&copy; Crown copyright 2011" >Copyright</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Disclaimer" title="Terms &amp; conditions" >Terms &amp; conditions</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Privacy" title="Privacy &amp; cookies" >Privacy &amp; cookies</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility" title="Accessibility" >Accessibility</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=HelpWithPDFFiles" title="PDF help" >PDF help</a> </span> </div>
+			<img src="img/footer_top3.gif" alt="" />
+			<div>
+				<span>
+					<a href="https://online.businesslink.gov.uk/bdotg/action/userSupport?refPage=%2Fbdotg%2Faction%2Flayer%3FtopicId%3D1075193162" title="Help &amp; support">Help &amp; support</a> | 
+					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Copyright" title="&copy; Crown copyright 2011">Copyright</a> | 
+					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Disclaimer" title="Terms &amp; conditions">Terms &amp; conditions</a> | 
+					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Privacy" title="Privacy &amp; cookies">Privacy &amp; cookies</a> | 
+					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility" title="Accessibility">Accessibility</a> | 
+					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=HelpWithPDFFiles" title="PDF help">PDF help</a>
+				</span>
 			</div>
+		</div>
 	</div>
-	</div>
+</div>
+
 <div>
-		<div> <span><a name="skiphelp"id="skiphelp"></a></span>
+	<div>
+		<span><a name="skiphelp"id="skiphelp"></a></span>
 		<div>
+			<div>
 				<div>
-				<div>
-						<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/redirectToSite">
+					<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/redirectToSite">
 						<fieldset>
-								<label for="localBusinessLink">Regional Support</label>
-								<select name="Site Id">
-								<option selected="selected" value="default">Select your location</option>
-								<optgroup label='Northern Ireland'>
-									<option value='1087659111'>nibusinessinfo.co.uk</option>
-									</optgroup>
-								<optgroup label='Scotland'>
-									<option value='1087659110'>Business Gateway</option>
-									</optgroup>
-								<optgroup label='Wales'>
-									<option value='1087659109'>Welsh Government</option>
-									</optgroup>
-							</select>
-								<input type="submit" value="Go" />
-							</fieldset>
+							<label for="localBusinessLink">Regional Support</label>
+							<select name="Site Id">
+							<option selected="selected" value="default">Select your location</option>
+							<optgroup label='Northern Ireland'>
+								<option value='1087659111'>nibusinessinfo.co.uk</option>
+								</optgroup>
+							<optgroup label='Scotland'>
+								<option value='1087659110'>Business Gateway</option>
+								</optgroup>
+							<optgroup label='Wales'>
+								<option value='1087659109'>Welsh Government</option>
+								</optgroup>
+						</select>
+							<input type="submit" value="Go" />
+						</fieldset>
 					</form>
-					</div>
+				</div>
 				<div>
-						<ul>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/fullsitemap" title="Site map" >Sitemap</a></li>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/help?page=HelpPage" title="Help" >Help</a></li>
-						<li><a href="detailddbd.html?itemId=1087351667&amp;page=AboutUs&amp;r.s=h&amp;type=CAMPAIGN" title="About us" >About us</a></li>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility" >Accessibility</a></li>
+					<ul>
+						<li><a href="http://www.businesslink.gov.uk/bdotg/action/fullsitemap" title="Site map">Sitemap</a></li>
+						<li><a href="http://www.businesslink.gov.uk/bdotg/action/help?page=HelpPage" title="Help">Help</a></li>
+						<li><a href="detailddbd.html?itemId=1087351667&amp;page=AboutUs&amp;r.s=h&amp;type=CAMPAIGN" title="About us">About us</a></li>
+						<li><a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility">Accessibility</a></li>
 					</ul>
-					</div>
+				</div>
 				<div>
-						<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/searchBasicMode">
+					<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/searchBasicMode">
 						<fieldset>
-								<input type="hidden" name="resultPage" value="1"/>
-								<label for="headersearchinput">Search</label>
-								<input type="text" name="expression" />
-								<input type="submit" value="Search" title="Start the search" />
-							</fieldset>
+							<input type="hidden" name="resultPage" value="1"/>
+							<label for="headersearchinput">Search</label>
+							<input type="text" name="expression" />
+							<input type="submit" value="Search" title="Start the search" />
+						</fieldset>
 					</form>
-					</div>
+				</div>
 			</div>
+			<div>
 				<div>
-				<div>
-						<ul>
-						<li><a href="https://online.businesslink.gov.uk/bdotg/action/myBusiness" title="" >My Business</a></li>
-						<li><a href="layer4866.html?topicId=1075193191" title="" >What's new?</a></li>
+					<ul>
+						<li><a href="https://online.businesslink.gov.uk/bdotg/action/myBusiness" title="">My Business</a></li>
+						<li><a href="layer4866.html?topicId=1075193191" title="">What's new?</a></li>
 						<li>Do it online</li>
-						<li><a href="layercdb4.html?topicId=1075193173" title="" >Directories</a></li>
+						<li><a href="layercdb4.html?topicId=1075193173" title="">Directories</a></li>
 					</ul>
-					</div>
+				</div>
 			</div>
-			</div>
+		</div>
 	</div>
-	</div>
+</div>
 
 <?php include("inc/category-links.php"); ?>
 
