98198984e07078582becf214929c61420b884b56
Caroline Murphy <cmurphydesign@gmail.com>
2014-06-15 21:34:05 +0100

    Fixed midsing images, external links and code layout.

--- apprenticeship-grants-now-available.php
+++ apprenticeship-grants-now-available.php
@@ -12,26 +12,32 @@
 
 <?php include("inc/skip-links.php"); ?>
 
-<div> <a href="home.html" title="Small Business Service home"><img src="../../syntegra/images/global-business-link-logo.png" alt="Business Link" border="0" /></a> </div>
-<div>Information. Support. Compliance.</div>
-<span><a name="skiplogin"></a></span>
 <div>
-		<p> Your account </p>
-		<ul>
-		<li> <a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate"> Register </a> </li>
-		<li> <a onclick="bLeaving=false;" href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate"> Log in </a> </li>
+	<a href="home.html" title="Small Business Service home"><img src="img/global-business-link-logo.png" alt="Business Link" border="0" /></a>
+</div>
+
+<div>Information. Support. Compliance.</div>
+
+<span><a name="skiplogin"></a></span>
+
+<div>
+	<p>Your account</p>
+	<ul>
+		<li><a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate">Register</a></li>
+		<li><a onclick="bLeaving=false;" href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate">Log in</a></li>
 	</ul>
+</div>
+
+<div>
+	<div>
+		<p><a href="home.html">Home</a>&gt;Get &pound;1,500 for recruiting an apprentice aged 16 to 24</p>
 	</div>
-<div><img src="../../syntegra/images/px_trans.gif" alt="" width="745" height="1" /></div>
-<div >
+
+	<span><a name="skipcont"></a></span>
 		<div>
-		<p><a href="home.html"  title="" >Home</a>&gt;Get &pound;1,500 for recruiting an apprentice aged 16 to 24 </p>
-	</div>
-		<span><a name="skipcont"></a></span>
-		<div>
-		<div>
-				<h1>Apprenticeship grants now available </h1>
-				<h2>Get &pound;1,500 for recruiting an apprentice aged 16 to 24 </h2>
+			<div>
+				<h1>Apprenticeship grants now available</h1>
+				<h2>Get &pound;1,500 for recruiting an apprentice aged 16 to 24</h2>
 				<p>If you're interested in recruiting your first apprentice, now's the ideal time.</p>
 				<p>The National Apprenticeship Service (NAS) is offering grants of &pound;1,500 to employers who take on a young person aged between 16 and 24. NAS will pay up to 100 per cent of their training costs as well.</p>
 				<p>The Apprenticeship Grant for Employers &ndash; known as <strong>AGE 16 to 24</strong> - will help you meet the initial costs of taking on your apprentice. It runs until March 2013 and is open to businesses in England that employ fewer than 250 people. The grant is offered on a 'first come first served' basis.</p>
@@ -39,94 +45,96 @@
 				<p>If your apprentice is aged between 16 and 18, NAS will pay their training costs in full. For apprentices aged between 19 and 24, NAS will pay 50 per cent of the training costs.</p>
 				<p>More support and assistance is available through the network of Apprenticeship training providers. They can help you advertise your job vacancy free of charge on the Apprenticeship vacancies website, and support you throughout the recruitment process. Your training provider will check that your business meets the grant criteria, and help you apply. They'll also claim the grant on your behalf from the NAS, and make the payment to you directly.</p>
 				<p><strong>How to find out more</strong></p>
-				<p>You can<a href="http://www.apprenticeships.org.uk/partners/policy/age-1624.aspx" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('http://www.apprenticeships.org.uk/partners/policy/age-1624.aspx', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="How to apply for the AGE 16 to 24 grant and eligibility information" >find out more about the AGE 16 to 24 grant on the Apprenticeships website<span> - Opens in a new window</span></a>. If you're ready to recruit an apprentice, talk to a NAS Apprenticeship Adviser on Tel 08000 150 600.</p>
-				<p>For more about the benefits of Apprenticeships and the government support that's available, see our page about<a href="http://www.improve.businesslink.gov.uk/people-and-assets/staff-managers-and-agents/how-apprenticeships-can-help-grow-your-business" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('http://www.improve.businesslink.gov.uk/people-and-assets/staff-managers-and-agents/how-apprenticeships-can-help-grow-your-business', 'bgExternalonlinebusinesslinkgovuk');p.focus();"  >how Apprenticeships can help grow your business<span> - Opens in a new window</span></a>on the Growth and Improvement Service website.</p>
-				<p><img src="../../syntegra/images/hr_325px_global.gif" alt="" align="middle" /></p>
+				<p>You can <a href="http://www.apprenticeships.org.uk/partners/policy/age-1624.aspx" title="How to apply for the AGE 16 to 24 grant and eligibility information" rel="external">find out more about the AGE 16 to 24 grant on the Apprenticeships website<span> - Opens in a new window</span></a>. If you're ready to recruit an apprentice, talk to a NAS Apprenticeship Adviser on Tel 08000 150 600.</p>
+				<p>For more about the benefits of Apprenticeships and the government support that's available, see our page about <a href="https://www.gov.uk/growing-your-business" rel="external">how Apprenticeships can help grow your business<span> - Opens in a new window</span></a> on the Growth and Improvement Service website.</p>
+				<p><img src="img/hr_325px_global.gif" alt="" align="middle" /></p>
 				<div>
-				<ul>
-						<script language="javascript" type="text/javascript">
-					<!--
-						document.write('<li><a href="#" onclick="Javascript:window.open(\'http://online.businesslink.gov.uk/bdotg/action/printpage?itemId=1097543702&amp;type=ONEOFFPAGE\', \'bgPrintable\', \'menubar=yes,width=630,height=510,resizable=yes,scrollbars=yes\')" title="Print page now  - Opens in a new window">Print page now <span> - Opens in a new window</span></a></li>');
-					//-->
-				</script>
-						<noscript>
-					<li><a href="http://online.businesslink.gov.uk/bdotg/action/printpage?itemId=1097543702&amp;type=ONEOFFPAGE" title="Print page now  - Opens in a new window" target="bgPrintable">Print page now <span> - Opens in a new window</span></a></li>
-					</noscript>
-						<li><a href="http://online.businesslink.gov.uk/bdotg/action/emailafriend?itemId=1097543702&amp;refPage=%2Fbdotg%2Faction%2Fdetail%3Fatom_id%3D0.PR391947%26itemId%3D1097543702%26pageId%3Dmynewbusiness%26type%3DONEOFFPAGE&amp;type=ONEOFFPAGE" title="Tell a friend about this page" >Tell a friend about this page</a></li>
-						<li><a href="https://online.businesslink.gov.uk/bdotg/action/subscribeAlert?alertType=ONEOFFPAGE&amp;itemId=1097543702&amp;refPage=%2Fbdotg%2Faction%2Fdetail%3Fatom_id%3D0.PR391947%26itemId%3D1097543702%26pageId%3Dmynewbusiness%26type%3DONEOFFPAGE&amp;type=ONEOFFPAGE" title="Tell a friend about this page" >Send me an email alert when this page changes</a></li>
+					<ul>
+						<li><a href="https://www.gov.uk/apprenticeships-guide" title="Print page now  - Opens in a new window" rel="external">Print page now <span> - Opens in a new window</span></a></li>
+						<li><a href="https://www.gov.uk/apprenticeships-guide" title="Tell a friend about this page" rel="external">Tell a friend about this page</a></li>
+						<li><a href="https://www.gov.uk/apprenticeships-guide" title="Tell a friend about this page" rel="external">Send me an email alert when this page changes</a></li>
 					</ul>
-			</div>
-			</div>
+				</div>
+		</div>
+	</div>
+		
+	<span><a name="skipfooter"></a></span>
+	
+	<div>
 		<div>
-				<div>
-				<div> </div>
-				<div> </div>
+			<img src="img/footer_top3.gif" alt="" />
+			<div>
+				<span>
+					<a href="https://online.businesslink.gov.uk/bdotg/action/userSupport?refPage=%2Fbdotg%2Faction%2Fdetail%3Fatom_id%3D0.PR391947%26itemId%3D1097543702%26pageId%3Dmynewbusiness%26type%3DONEOFFPAGE" title="Help &amp; support">Help &amp; support</a> | 
+					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Copyright" title="&copy; Crown copyright 2011">Copyright</a> | 
+					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Disclaimer" title="Terms &amp; conditions">Terms &amp; conditions</a> | 
+					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Privacy" title="Privacy &amp; cookies">Privacy &amp; cookies</a> | 
+					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility" title="Accessibility">Accessibility</a> | 
+					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=HelpWithPDFFiles" title="PDF help">PDF help</a>
+				</span>
 			</div>
-			</div>
+		</div>
 	</div>
-		<span><a name="skipfooter"></a></span>
-		<div>
-		<div><img src="../../syntegra/images/footer_top3.gif" alt="" />
-				<div> <span><a href="https://online.businesslink.gov.uk/bdotg/action/userSupport?refPage=%2Fbdotg%2Faction%2Fdetail%3Fatom_id%3D0.PR391947%26itemId%3D1097543702%26pageId%3Dmynewbusiness%26type%3DONEOFFPAGE" title="Help &amp; support" >Help &amp; support</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Copyright" title="&copy; Crown copyright 2011" >Copyright</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Disclaimer" title="Terms &amp; conditions" >Terms &amp; conditions</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Privacy" title="Privacy &amp; cookies" >Privacy &amp; cookies</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility" title="Accessibility" >Accessibility</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=HelpWithPDFFiles" title="PDF help" >PDF help</a> </span> </div>
-			</div>
-	</div>
-	</div>
+</div>
+
 <div>
-		<div> <span><a name="skiphelp"id="skiphelp"></a></span>
+	<div>
+		<span><a name="skiphelp"id="skiphelp"></a></span>
+
 		<div>
+			<div>
 				<div>
-				<div>
-						<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/redirectToSite">
+					<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/redirectToSite">
 						<fieldset>
-								<label for="localBusinessLink">Regional Support</label>
-								<select name="Site Id">
-								<option selected="selected" value="default">Select your location</option>
-								<optgroup label='Northern Ireland'>
-									<option value='1087659111'>nibusinessinfo.co.uk</option>
-									</optgroup>
-								<optgroup label='Scotland'>
-									<option value='1087659110'>Business Gateway</option>
-									</optgroup>
-								<optgroup label='Wales'>
-									<option value='1087659109'>Welsh Government</option>
-									</optgroup>
-							</select>
-								<input type="submit" value="Go" />
-							</fieldset>
+							<label for="localBusinessLink">Regional Support</label>
+							<select name="Site Id">
+							<option selected="selected" value="default">Select your location</option>
+							<optgroup label='Northern Ireland'>
+								<option value='1087659111'>nibusinessinfo.co.uk</option>
+								</optgroup>
+							<optgroup label='Scotland'>
+								<option value='1087659110'>Business Gateway</option>
+								</optgroup>
+							<optgroup label='Wales'>
+								<option value='1087659109'>Welsh Government</option>
+								</optgroup>
+						</select>
+							<input type="submit" value="Go" />
+						</fieldset>
 					</form>
-					</div>
+				</div>
 				<div>
-						<ul>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/fullsitemap" title="Site map" >Sitemap</a></li>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/help?page=HelpPage" title="Help" >Help</a></li>
-						<li><a href="detailcf34.html?itemId=1087351667&amp;page=AboutUs&amp;r.i=1097543702&amp;r.s=h&amp;r.t=ONEOFFPAGE&amp;type=CAMPAIGN" title="About us" >About us</a></li>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility" >Accessibility</a></li>
+					<ul>
+						<li><a href="http://www.businesslink.gov.uk/bdotg/action/fullsitemap" title="Site map">Sitemap</a></li>
+						<li><a href="http://www.businesslink.gov.uk/bdotg/action/help?page=HelpPage" title="Help">Help</a></li>
+						<li><a href="detailcf34.html?itemId=1087351667&amp;page=AboutUs&amp;r.i=1097543702&amp;r.s=h&amp;r.t=ONEOFFPAGE&amp;type=CAMPAIGN" title="About us">About us</a></li>
+						<li><a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility">Accessibility</a></li>
 					</ul>
-					</div>
+				</div>
 				<div>
-						<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/searchBasicMode">
+					<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/searchBasicMode">
 						<fieldset>
-								<input type="hidden" name="resultPage" value="1"/>
-								<label for="headersearchinput">Search</label>
-								<input type="text" name="expression" />
-								<input type="submit" value="Search" title="Start the search" />
-							</fieldset>
+							<input type="hidden" name="resultPage" value="1"/>
+							<label for="headersearchinput">Search</label>
+							<input type="text" name="expression" />
+							<input type="submit" value="Search" title="Start the search" />
+						</fieldset>
 					</form>
-					</div>
+				</div>
 			</div>
+			<div>
 				<div>
-				<div>
-						<ul>
-						<li><a href="https://online.businesslink.gov.uk/bdotg/action/myBusiness" title="" >My Business</a></li>
-						<li><a href="layer4866.html?topicId=1075193191" title="" >What's new?</a></li>
-						<li><a href="layer21a5.html?topicId=1075193162" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies" >Do it online</a></li>
-						<li><a href="layercdb4.html?topicId=1075193173" title="" >Directories</a></li>
+					<ul>
+						<li><a href="https://online.businesslink.gov.uk/bdotg/action/myBusiness" title="">My Business</a></li>
+						<li><a href="layer4866.html?topicId=1075193191" title="">What's new?</a></li>
+						<li><a href="layer21a5.html?topicId=1075193162" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies">Do it online</a></li>
+						<li><a href="layercdb4.html?topicId=1075193173" title="">Directories</a></li>
 					</ul>
-					</div>
+				</div>
 			</div>
-			</div>
+		</div>
 	</div>
-	</div>
+</div>
 
 <?php include("inc/category-links.php"); ?>
 	
