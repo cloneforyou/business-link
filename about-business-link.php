3649b7930655babf8ffd3a0cdcc43e68b75bd41a
Caroline Murphy <cmurphydesign@gmail.com>
2014-06-21 00:33:04 +0100

    Working on the about-business-link page

--- about-business-link.php
+++ about-business-link.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html class="no-js">
 <head>
-	<meta charset="utf-8">
-	
+	<meta charset="utf-8">	
 	<title>About Us | Business Link</title>
 	<meta name="author" content="Caroline Murphy">
 	<meta name="viewport" content="width=device-width">
@@ -10,36 +9,14 @@
 
 <body>
 
-<?php include("inc/skip-links.php"); ?>
+<?php include("inc/header.php"); ?>
 
-<div>
-	<div> <a href="#content"> <img width="1" height="1" alt="Skip to Content" src="../../syntegra/images/px_trans.gif"/> </a> </div>
-	<div><a href="home.html" title="Small Business Service home"><img border="0" title="Business Link" alt="Business Link" src="../../presentation/portal-businesslink/images/logo-sml.gif"/></a></div>
-	<div> <a href="http://www.businesslink.gov.uk/bdotg/action/fullsitemap">Site map</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/help?page=Help">Help</a> | <a href="detailddbd.html?itemId=1087351667&amp;page=AboutUs&amp;r.s=h&amp;type=CAMPAIGN" title="About us" >About us</a> </div>
-	<div>
-		<form action="http://www.businesslink.gov.uk/bdotg/action/searchBasicMode" method="get">
-			<fieldset>
-				<input type="hidden" value="1" name="resultPage"/>
-				<label for="headersearchinput">Search</label>
-				<input type="text" name="expression" />
-				<input type="submit" value="Search" title="Start the search" />
-			</fieldset>
-		</form>
-	</div>
-	<div> <strong>Your account</strong>
-		<ul>
-			<li> <a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate">Register</a> </li>
-			<li> <a href="https://online.businesslink.gov.uk/bdotg/action/login">Log in</a> </li>
-		</ul>
-	</div>
-	<img width="1" height="1" alt="" src="../../syntegra/images/px_trans.gif"/> </div>
 <div>
 	<div>
 		<table>
 			<tr>
-				<td><div>
-						<p><a href="home.html" title="Topic Home Page" >Home &gt;</a> About Business Link</p>
-					</div>
+				<td>
+					<?php include("inc/breadcrumbs.php"); ?>
 					<div>
 						<div>
 							<div>
@@ -50,7 +27,7 @@
 									<div>
 										<div>We have a range of different alerts to keep you up to date with important changes.</div>
 										<ul>
-											<li><a href="https://online.businesslink.gov.uk/bdotg/action/myBusinessTools?topicId=1082235004" title="From here you can sign up to, change or cancel all the email alerts available through the site." >Register for alerts</a></li>
+											<li><a href="#" title="From here you can sign up to, change or cancel all the email alerts available through the site.">Register for alerts</a></li>
 										</ul>
 									</div>
 								</div>
@@ -62,10 +39,10 @@
 									</div>
 									<div>
 										<ul>
-											<li><a href="../../Horizontal_Services_files/business_link_annual_review_0910.pdf" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('../../Horizontal_Services_files/business_link_annual_review_0910.pdf', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="Download the Business Link Annual Review 2009-10 (PDF, 1,001KB)" >2009-10 Annual Review<span> - Opens in a new window</span></a></li>
-											<li><a href="detail091e.html?itemId=1084757367&amp;type=ONEOFFPAGE"  >2011 Budget Summary</a></li>
-											<li><a href="../../Horizontal_Services_files/bigger_better_business.pdf" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('../../Horizontal_Services_files/bigger_better_business.pdf', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="A guide to helping small firms start, grow and prosper (PDF, 114KB)" >Bigger, Better Business<span> - Opens in a new window</span></a></li>
-											<li><a href="../../Horizontal_Services_files/local_growth_white_paper.pdf" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('../../Horizontal_Services_files/local_growth_white_paper.pdf', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="The Government's white paper on encouraging local growth (PDF, 1,940KB)" >Local Growth White Paper<span> - Opens in a new window</span></a></li>
+											<li><a href="files/business_link_annual_review_0910.pdf" title="Download the Business Link Annual Review 2009-10 (PDF, 1,001KB)" rel="external">2009-10 Annual Review<span> - Opens in a new window</span></a></li>
+											<li><a href="budget-2012.php">2011 Budget Summary</a></li>
+											<li><a href="files/bigger_better_business.pdf" title="A guide to helping small firms start, grow and prosper (PDF, 114KB)" rel="external">Bigger, Better Business<span> - Opens in a new window</span></a></li>
+											<li><a href="files/local_growth_white_paper.pdf" title="The Government's white paper on encouraging local growth (PDF, 1,940KB)" rel="external">Local Growth White Paper<span> - Opens in a new window</span></a></li>
 										</ul>
 									</div>
 								</div>
@@ -75,60 +52,29 @@
 									</div>
 									<div>
 										<ul>
-											<li><a href="https://online.businesslink.gov.uk/bdotg/action/userSupport"  >Get help</a></li>
-											<li><a href="http://www.businesslink.gov.uk/bdotg/action/feedback" title="Tell us what you think" >Feedback</a></li>
+											<li><a href="#">Get help</a></li>
+											<li><a href="#" title="Tell us what you think">Feedback</a></li>
 										</ul>
 									</div>
 								</div>
 							</div>
 							<div></div>
-							<div><script src="../../../widgets.twimg.com/j/2/widget.js"><!--
-//-->
-</script><script><!--
-new TWTR.Widget({
-  version: 2,
-  type: 'profile',
-  rpp: 4,
-  interval: 6000,
-  width: 210,
-  height: 300,
-  theme: {
-    shell: {
-      background: '#f2f2f2',
-      color: '#000000'
-    },
-    tweets: {
-      background: '#f2f2f2',
-      color: '#000000',
-      links: '#003399'
-    }
-  },
-  features: {
-    scrollbar: false,
-    loop: false,
-    live: false,
-    hashtags: true,
-    timestamp: true,
-    avatars: false,
-    'all'
-  }
-}).render().setUser('BusinessLinkGov').start();
-//-->
-</script></div>
+							<div></div>
 						</div>
 						<div>
 							<h1>About Business Link</h1>
-							<img alt="" src="../../Horizontal_Services_images/about_header2.jpg" /></div>
+							<img src="img/about_header2.jpg" alt="" />
+						</div>
 						<div>
 							<div>
 								<div>
 									<div>
 										<ul>
-											<li><a href="detail4701.html?itemId=1087351672&amp;type=CAMPAIGN"  >About Business Link</a></li>
-											<li><a href="detail4701.html?itemId=1087351672&amp;type=CAMPAIGN#overview" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('detail4701.html?itemId=1087351672&amp;type=CAMPAIGN#overview', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="An overview of the Business Link service" >Overview of the service<span> - Opens in a new window</span></a></li>
-											<li><a href="detail4701.html?itemId=1087351672&amp;type=CAMPAIGN#objectives" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('detail4701.html?itemId=1087351672&amp;type=CAMPAIGN#objectives', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="Find out about the Business Link objectives and governance" >Our governance<span> - Opens in a new window</span></a></li>
-											<li><a href="detail4701.html?itemId=1087351672&amp;type=CAMPAIGN#use" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('detail4701.html?itemId=1087351672&amp;type=CAMPAIGN#use', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="More information how how to use the businesslink.gov.uk website" >How to use the site<span> - Opens in a new window</span></a></li>
-											<li><a href="detail4701.html?itemId=1087351672&amp;type=CAMPAIGN#timeline" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('detail4701.html?itemId=1087351672&amp;type=CAMPAIGN#timeline', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="More information about the timeline of Business Link" >Timeline<span> - Opens in a new window</span></a></li>
+											<li><a href="#">About Business Link</a></li>
+											<li><a href="#overview" title="An overview of the Business Link service">Overview of the service<span> - Opens in a new window</span></a></li>
+											<li><a href="#objectives" title="Find out about the Business Link objectives and governance">Our governance<span> - Opens in a new window</span></a></li>
+											<li><a href="#use" title="More information how how to use the businesslink.gov.uk website">How to use the site<span> - Opens in a new window</span></a></li>
+											<li><a href="#timeline" title="More information about the timeline of Business Link">Timeline<span> - Opens in a new window</span></a></li>
 										</ul>
 									</div>
 								</div>
@@ -137,44 +83,44 @@ new TWTR.Widget({
 									<p>It contains essential information, support and services for you and your business &ndash; whether you work for a large organisation or are on your way to starting up.</p>
 									<p>Simple to use, up to date and practical, Business Link is the first place to go to find guidance on regulations and to access government services. It also has a number of useful online tools, calculators, and best practice case studies; and provides access to funding options, as well as wider support.</p>
 									<p><strong>Business link is changing</strong></p>
-									<p>From October GOV.UK will be the new home for government services and information. GOV.UK will replace businesslink.gov.uk and Directgov, making it simpler, clearer and faster to find what you need from government. <a href="detail39a9.html?itemId=1097621079&amp;type=ONEOFFPAGE">Find out how businesslink.gov.uk is changing</a>. </p>
+									<p>From October GOV.UK will be the new home for government services and information. GOV.UK will replace businesslink.gov.uk and Directgov, making it simpler, clearer and faster to find what you need from government. <a href="gov-uk-will-launch-in-october.php">Find out how businesslink.gov.uk is changing</a>. </p>
 								</div>
 							</div>
 						</div>
 						<div>
 							<div>
 								<h2>Business Link news</h2>
-								<img alt="" src="../../Horizontal_Services_images/about_news2.jpg" />
+								<img src="img/about_news2.jpg" alt="" />
 								<p>Business Link has launched two new services to help anyone looking to start up, improve and grow their business.</p>
-								<p><a href="https://online.businesslink.gov.uk/hub/action/render?pageId=mynewbusiness" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('https://online.businesslink.gov.uk/hub/action/render?pageId=mynewbusiness', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="Access the My New Business tool" >My New Business<span> - Opens in a new window</span></a>- provides people in the stages of starting their business with everything they need to know to get off the ground and plan for future success.</p>
-								<p><a href="http://www.improve.businesslink.gov.uk/" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('http://www.improve.businesslink.gov.uk/', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="Information and tools for people interested in growing or improving their business" >Growth and Improvement Service<span> - Opens in a new window</span></a>- provides straightforward, practical guidance for people who want to get more out of their business, such as generating more sales, increasing profits, and saving time and money.</p>
+								<p><a href="https://www.gov.uk/browse/business/setting-up" title="Access the My New Business tool" rel="external">My New Business<span> - Opens in a new window</span></a> - provides people in the stages of starting their business with everything they need to know to get off the ground and plan for future success.</p>
+								<p><a href="https://www.gov.uk/growing-your-business" title="Information and tools for people interested in growing or improving their business" rel="external">Growth and Improvement Service<span> - Opens in a new window</span></a> - provides straightforward, practical guidance for people who want to get more out of their business, such as generating more sales, increasing profits, and saving time and money.</p>
 								<p>Our January 2011 survey of site visitors revealed that 89% of respondents are likely to use this site again and 79% are likely to recommend it (based on 1099 site visitors).</p>
 							</div>
 							<div>
 								<h2>Useful links</h2>
-								<div><img alt="" src="../../Horizontal_Services_images/about_links2.jpg" /></div>
+								<div><img src="img/about_links2.jpg" alt="" /></div>
 								<div>
 									<ul>
-										<li><a href="layer4866.html?topicId=1075193191"  >What's new</a></li>
-										<li><a href="findContentBankf4d5.html?bank=CASE_STUDIES" title="Browse our library of case studies to find out how business owners and managers have tackled real-life challenges and opportunities" >Case studies</a></li>
-										<li><a href="detail29fc.html?itemId=1084515656&amp;type=ONEOFFPAGE" title="Links to interactive tools" >Interactive tools</a></li>
-										<li><a href="https://online.businesslink.gov.uk/bdotg/action/userSupportShowHelpTopic?itemId=1084970573" title="We encourage linking to our site" >Linking to us</a></li>
-										<li><a href="https://online.businesslink.gov.uk/bdotg/action/userSupport"  >Can't find what you're looking for?</a></li>
+										<li><a href="whats-new.php">What's new</a></li>
+										<li><a href="find-a-case-study.php" title="Browse our library of case studies to find out how business owners and managers have tackled real-life challenges and opportunities">Case studies</a></li>
+										<li><a href="tools-directory.php" title="Links to interactive tools">Interactive tools</a></li>
+										<li><a href="#" title="We encourage linking to our site">Linking to us</a></li>
+										<li><a href="#">Can't find what you're looking for?</a></li>
 									</ul>
 								</div>
 								<div>
 									<h2>Follow us on</h2>
-									<div><a href="http://www.facebook.com/pages/Business-Link/79996723930" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('http://www.facebook.com/pages/Business-Link/79996723930', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="View and follow Business Link Facebook page" ><img border="0" alt="Join us on Facebook" src="../../Horizontal_Services_images/about_facebook.gif" /><span> - Opens in a new window</span></a>
-										<p><a href="http://www.facebook.com/pages/Business-Link/79996723930" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('http://www.facebook.com/pages/Business-Link/79996723930', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="View and follow Business Link Facebook page" >Facebook<span> - Opens in a new window</span></a></p>
+									<div>
+										<a href="https://www.facebook.com/BusinessSupportGov" title="View and follow Business Link Facebook page" rel="external"><img border="0" alt="Join us on Facebook" src="img/about_facebook.gif" /> Facebook</a>
 									</div>
-									<div><a href="http://www.linkedin.com/company/businesslink.gov.uk" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('http://www.linkedin.com/company/businesslink.gov.uk', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="View and follow Business Link LinkedIn page" ><img border="0" alt="Join us on LinkedIn" src="../../Horizontal_Services_images/about_linkedin.gif" /><span> - Opens in a new window</span></a>
-										<p><a href="http://www.linkedin.com/company/businesslink.gov.uk" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('http://www.linkedin.com/company/businesslink.gov.uk', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="View and follow Business Link LinkedIn page" >LinkedIn<span> - Opens in a new window</span></a></p>
+									<div>
+										<a href="http://www.linkedin.com/company/businesslink.gov.uk" title="View and follow Business Link LinkedIn page" rel="external"><img border="0" alt="Join us on LinkedIn" src="img/about_linkedin.gif" /> LinkedIn</a>
 									</div>
-									<div><a href="http://twitter.com/BusinessLinkGov" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('http://twitter.com/BusinessLinkGov', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="View and follow Business Link on Twitter" ><img border="0" alt="Follow us on Twitter" src="../../Horizontal_Services_images/about_twitters.gif" /><span> - Opens in a new window</span></a>
-										<p><a href="http://twitter.com/BusinessLinkGov" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('http://twitter.com/BusinessLinkGov', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="View and follow Business Link on Twitter" >Twitter<span> - Opens in a new window</span></a></p>
+									<div>
+										<a href="https://twitter.com/BusinessLinkGov" title="View and follow Business Link on Twitter" rel="external"><img border="0" alt="Follow us on Twitter" src="img/about_twitters.gif" /> Twitter</a>
 									</div>
-									<div><a href="http://www.youtube.com/user/BusinessLinkGov" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('http://www.youtube.com/user/BusinessLinkGov', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="View videos on the Business Link YouTube channel" ><img border="0" alt="Follow us on YouTube" src="../../Horizontal_Services_images/about_youtubes.gif" /><span> - Opens in a new window</span></a>
-										<p><a href="http://www.youtube.com/user/BusinessLinkGov" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('http://www.youtube.com/user/BusinessLinkGov', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="View videos on the Business Link YouTube channel" >YouTube<span> - Opens in a new window</span></a></p>
+									<div>
+										<a href="http://www.youtube.com/user/BusinessLinkGov" title="View videos on the Business Link YouTube channel" rel="external"><img border="0" alt="Follow us on YouTube" src="img/about_youtubes.gif" /> YouTube</a>
 									</div>
 								</div>
 							</div>
@@ -198,15 +144,12 @@ new TWTR.Widget({
 								</div>
 							</div>
 						</div>
-					</div></td>
+					</div>
+				</td>
 				<td></td>
 			</tr>
 		</table>
 	</div>
 </div>
-<div> <a href="http://www.businesslink.gov.uk/bdotg/action/feedback?refPage=%2Fbdotg%2Faction%2Fdetail%3FitemId%3D1087351667%26type%3DCAMPAIGN" title="Help &amp; support" >Help &amp; support</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Copyright" title="&copy; Crown copyright 2011" >Copyright</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Disclaimer" title="View terms &amp; conditions information" >Terms &amp; conditions</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Privacy" title="View your rights as a user" >Privacy &amp; cookies</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility"  title="" >Accessibility</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=HelpWithPDFFiles"  title="" >PDF help</a> </div>
 
-<?php include("inc/scripts.php"); ?>
-
-</body>
-</html>
+<?php include("inc/footer.php"); ?>
--- apprenticeship-grants-now-available.php
+++ apprenticeship-grants-now-available.php
@@ -10,23 +10,7 @@
 
 <body>
 
-<?php include("inc/skip-links.php"); ?>
-
-<div>
-	<a href="home.html" title="Small Business Service home"><img src="img/global-business-link-logo.png" alt="Business Link" border="0" /></a>
-</div>
-
-<div>Information. Support. Compliance.</div>
-
-<span><a name="skiplogin"></a></span>
-
-<div>
-	<p>Your account</p>
-	<ul>
-		<li><a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate">Register</a></li>
-		<li><a onclick="bLeaving=false;" href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate">Log in</a></li>
-	</ul>
-</div>
+<?php include("inc/header.php"); ?>
 
 <div>
 	<div>
@@ -58,87 +42,4 @@
 		</div>
 	</div>
 		
-	<span><a name="skipfooter"></a></span>
-	
-	<div>
-		<div>
-			<img src="img/footer_top3.gif" alt="" />
-			<div>
-				<span>
-					<a href="https://online.businesslink.gov.uk/bdotg/action/userSupport?refPage=%2Fbdotg%2Faction%2Fdetail%3Fatom_id%3D0.PR391947%26itemId%3D1097543702%26pageId%3Dmynewbusiness%26type%3DONEOFFPAGE" title="Help &amp; support">Help &amp; support</a> | 
-					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Copyright" title="&copy; Crown copyright 2011">Copyright</a> | 
-					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Disclaimer" title="Terms &amp; conditions">Terms &amp; conditions</a> | 
-					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Privacy" title="Privacy &amp; cookies">Privacy &amp; cookies</a> | 
-					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility" title="Accessibility">Accessibility</a> | 
-					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=HelpWithPDFFiles" title="PDF help">PDF help</a>
-				</span>
-			</div>
-		</div>
-	</div>
-</div>
-
-<div>
-	<div>
-		<span><a name="skiphelp"id="skiphelp"></a></span>
-
-		<div>
-			<div>
-				<div>
-					<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/redirectToSite">
-						<fieldset>
-							<label for="localBusinessLink">Regional Support</label>
-							<select name="Site Id">
-							<option selected="selected" value="default">Select your location</option>
-							<optgroup label='Northern Ireland'>
-								<option value='1087659111'>nibusinessinfo.co.uk</option>
-								</optgroup>
-							<optgroup label='Scotland'>
-								<option value='1087659110'>Business Gateway</option>
-								</optgroup>
-							<optgroup label='Wales'>
-								<option value='1087659109'>Welsh Government</option>
-								</optgroup>
-						</select>
-							<input type="submit" value="Go" />
-						</fieldset>
-					</form>
-				</div>
-				<div>
-					<ul>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/fullsitemap" title="Site map">Sitemap</a></li>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/help?page=HelpPage" title="Help">Help</a></li>
-						<li><a href="detailcf34.html?itemId=1087351667&amp;page=AboutUs&amp;r.i=1097543702&amp;r.s=h&amp;r.t=ONEOFFPAGE&amp;type=CAMPAIGN" title="About us">About us</a></li>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility">Accessibility</a></li>
-					</ul>
-				</div>
-				<div>
-					<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/searchBasicMode">
-						<fieldset>
-							<input type="hidden" name="resultPage" value="1"/>
-							<label for="headersearchinput">Search</label>
-							<input type="text" name="expression" />
-							<input type="submit" value="Search" title="Start the search" />
-						</fieldset>
-					</form>
-				</div>
-			</div>
-			<div>
-				<div>
-					<ul>
-						<li><a href="https://online.businesslink.gov.uk/bdotg/action/myBusiness" title="">My Business</a></li>
-						<li><a href="layer4866.html?topicId=1075193191" title="">What's new?</a></li>
-						<li><a href="layer21a5.html?topicId=1075193162" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies">Do it online</a></li>
-						<li><a href="layercdb4.html?topicId=1075193173" title="">Directories</a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-
-<?php include("inc/category-links.php"); ?>
-	
-<?php include("inc/scripts.php"); ?>
-
-</body>
-</html>
+<?php include("inc/footer.php"); ?>
