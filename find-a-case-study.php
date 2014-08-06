a654164733f3af5110f8a3d8d774f73673ceddb0
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-06 12:10:55 +0100

    Formatted find-a-case-study and linked external pages, ready for formatting

---
+++ find-a-case-study.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<meta charset="utf-8">	
+	<title>Find A Case Study | Business Link</title>
+	<meta name="author" content="Caroline Murphy">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+
+<?php include("inc/header.php"); ?>
+
+<div> 
+	<!-- Page breadcrumb -->
+	<?php include("inc/breadcrumbs.php"); ?>
+	<span><a name="skipcont"></a></span>
+	<div>
+		<div>
+			<h1>Find a case study</h1>
+			<div>
+				<p>Browse our many case studies that show first hand how people have tackled real-life challenges and opportunities.</p>
+			</div>
+			<div>
+				<div>
+					<p>Browse by theme</p>
+				</div>
+				<div>
+					<ul>
+						<li>
+							<a href="browse-by-theme-starting-up.html">Starting up</a>
+						</li>
+						<li>
+							<a href="browse-by-theme-finance-and-grants.html">Finance and grants</a>
+						</li>
+						<li>
+							<a href="browse-by-theme-tax-payroll-and-company-information.html">Tax, payroll and company information</a>
+						</li>
+						<li>
+							<a href="browse-by-theme-employment-and-skills.html">Employment &amp; skills</a>
+						</li>
+						<li>
+							<a href="browse-by-theme-workplace-health-and-safety.html">Workplace health &amp; safety</a>
+						</li>
+						<li>
+							<a href="browse-by-theme-environment-and-efficiency.html">Environment &amp; efficiency</a>
+						</li>
+						<li>
+							<a href="browse-by-theme-premises-and-property.html">Premises and property</a>
+						</li>
+						<li>
+							<a href="browser-by-theme-create-innovate-and-protect.html">Create, innovate and protect</a>
+						</li>
+						<li>
+							<a href="browse-by-theme-it-and-ecommerce.html">IT &amp; e-commerce</a>
+						</li>
+						<li>
+							<a href="browse-by-theme-sales-and-marketing.html">Sales and marketing</a>
+						</li>
+						<li>
+							<a href="browse-by-theme-international-trade.html">International trade</a>
+						</li>
+						<li>
+							<a href="browse-by-theme-grow-your-business.html">Grow your business</a>
+						</li>
+						<li>
+							<a href="browse-by-theme-buy-or-sell-a-business.html">Buy or sell a business</a>
+						</li>
+						<li>
+							<a href="browse-by-theme-for-professional-advisers.html">For professional advisers</a>
+						</li>
+						<li>
+							<a href="browse-by-theme-your-business-sector.html">Your business sector</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+<?php include("inc/footer.php"); ?>
