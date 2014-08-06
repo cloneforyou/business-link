efdd236d8b43c378afbec915c267a2b20f0ad967
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-06 12:08:54 +0100

    Formatted budget-2012 page, changed to PHP and uploaded.

---
+++ budget-2012.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<meta charset="utf-8">	
+	<title>Budget 2012 | Business Link</title>
+	<meta name="author" content="Caroline Murphy">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+
+<?php include("inc/header.php"); ?>
+
+<div> 
+	
+	<!-- Page breadcrumb -->
+	<?php include("inc/breadcrumbs.php"); ?>
+	<span><a name="skipcont"></a></span>
+	<div>
+		<div>
+			<h1>Budget 2012</h1>
+			<h2>How will the Budget affect your business?</h2>
+			<p>Budget 2012 maintains the Government's strategy and sets out further action it will take in three areas: a stable economy; a fairer, more efficient and simpler tax system; and reforms to support growth.</p>
+			<p>You can find full details of the Budget announcements on the <a href="https://www.gov.uk/government/organisations/hm-treasury" rel="external">HM Treasury website</a>, <a href="http://www.hmrc.gov.uk/budget2012/index.htm" rel="external">HMRC website</a> and <a href="https://www.gov.uk/government/news/budget-2012--2" rel="external">BIS website</a>.</p>
+			<p><strong>Key announcements for business</strong></p>
+			<ul>
+				<li>The main rate of <strong>corporation tax</strong> will be reduced by an additional one per cent, so that the rate will reduce from 26 per cent to 24 per cent in April 2012, to 23 per cent in April 2013 and will come down to 22 per cent by April 2014.</li>
+				<li>Corporation tax reliefs will be introduced from April 2013 for the <strong>video games, animation and high-end television industries</strong>, subject to state aid approval and following consultation.</li>
+				<li>The top rate of <strong>income tax</strong> will be reduced from 50% to 45% in April 2013, and individual's income tax personal allowance will increase at the same time by a further &pound;1,100, taking it to &pound;9,205 in total.</li>
+				<li>A cap will be introduced on all unlimited <strong>income tax reliefs</strong>, of &pound;50,000 or 25% of income, whichever is higher.</li>
+				<li>Following the Office of Tax Simplification review of <strong>small business taxation</strong>, the Government will consult on introducing a voluntary cash basis for unincorporated businesses up to the VAT registration threshold; a simplified expenses system for business use of cars, motorcyles and home; and the possibility of a disincorporation relief.</li>
+				<li>From 1st April 2012, the VAT taxable turnover threshold, which determines whether a person must be registered for VAT, and the registration and deregistration threshold for relevant acquisitions from other EU Member States, will be increased from &pound;73,000 to &pound;77,000; and the taxable turnover threshold which determines whether a person may apply for deregistration will be increased from &pound;71,000 to &pound;75,000. The simplified reporting requirement (three line accounts) for the income tax Self Assessment return will continue to be aligned with the VAT registration threshold.</li>
+				<li><strong>Sunday trading laws</strong> will be relaxed during the Olympics and Paralympics, to allow retailers to make the most of the occasion.</li>
+				<li>The Government accepts the Low Pay Commission's recommendation for a below inflation increase in the <strong>National Minimum Wage</strong> to support employers and help protect jobs.</li>
+				<li>The <strong>Enterprise Management Incentive</strong> scheme, which helps SMEs recruit and retain talent, will provide additional support to help start-ups access the scheme and more than double the grant limit to &pound;250,000, subject to State Aid approval.</li>
+				<li>Selection of Belfast, Birmingham, Bradford, Bristol, Cardiff, Edinburgh, Leeds, London, Manchester and Newcastle has been confirmed to become <strong>broadband super-connected cities</strong>, as part of the &pound;100 million investment announced at 2011 Autumn Statement. &pound;50m will be used to fund a second wave of cities.</li>
+				<li>The Government will support the establishment of a new <strong>Pension Infrastructure Platform</strong> owned and run by UK pension funds, which will begin its initial &pound;2 billion investment in UK infrastructure by early 2013.</li>
+				<li>A <strong>strategy for gas generation</strong> will be published in the Autumn, recognising that gas-fired electricity generation will continue to play a major role in UK energy supplies for the next decade and beyond.</li>
+				<li>A package of tax measures will be introduced to secure billions of pounds of additional <strong>oil and gas investment</strong> in the UK Continental Shelf.</li>
+			</ul>
+			<p><img src="img/hr_325px_global.gif" alt="" /></p>
+			<div>
+				<ul>
+					<li><a href="#">Print page now</a></li>
+					<li><a href="#">Tell a friend about this page</a></li>
+					<li><a href="#">Send me an email alert when this page changes</a></li>
+				</ul>
+			</div>
+		</div>
+		<div>
+			<div>
+				<div>
+					<table border="0" cellpadding="0" cellspacing="0">
+						<tr>
+							<td><img src="img/Budgetoneoffpage.jpg" width="181" height="121" border="0" alt="" /></td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<?php include("inc/footer.php"); ?>
