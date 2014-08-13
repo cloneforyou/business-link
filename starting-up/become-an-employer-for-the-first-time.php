de96970cf8455d9bc176f62c292c4cb2dc09200d
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-13 17:35:06 +0100

    Completed page for "Become an employer for the first time".

---
+++ starting-up/become-an-employer-for-the-first-time.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Become An Employer For The First Time | Starting Up | Business Link</title>
+	<meta name="author" content="Caroline Murphy">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+
+	<!-- Page breadcrumb -->
+	<div id="breadcrumb">
+		<p><a href="home.html">Home</a> &gt; <a href="/starting-up.php">Starting Up</a> &gt; [title]</p>
+	</div>
+
+	<span><a name="skipcont" id="skipcont"></a></span>
+
+	<div>
+		<div>
+			<h1>Become an employer for the first time</h1>
+			<div>
+				<h2><a href="https://www.gov.uk/employing-staff" title="Employing staff for the first time" rel="external">Take on staff</a></h2>
+				<p>Find information on how to take on staff in your business</p>
+			</div>
+			<div>
+				<h2><a href="employment-and-skills_employment-checks_ensuring-your-workers-are-eligible-to-work-in-the-uk.php.html">Ensuring your workers are eligible to work in the UK</a></h2>
+				<p>Checking a worker's right to work in the UK and how to employ workers from outside the EEA and Switzerland</p>
+			</div>
+			<div>
+				<h2><a href="https://www.gov.uk/employment-contracts-and-conditions" title="Employment contracts" rel="external">Create a written statement of employment</a></h2>
+				<p>Use our interactive tool to create a document including everything you have to give your new employee in writing</p>
+			</div>
+			<div>
+				<h2> <a href="http://www.hmrc.gov.uk/payerti/index.htm" title="Operating PAYE in real time (RTI)" rel="external">Find out if you need to operate PAYE</a></h2>
+				<p>Make sure you need to operate PAYE before you register as an employer with HMRC</p>
+			</div>
+		</div>
+		<div>
+			<img src="../_img/Becoming_an_employer.jpg" width="146" height="146" border="0" alt="" />
+		</div>
+	</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
