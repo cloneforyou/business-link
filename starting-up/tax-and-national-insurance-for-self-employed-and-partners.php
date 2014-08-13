7581e8943b4baf9e22b2b4674f98811d0252d603
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-13 19:47:59 +0100

    Completed "Tax and National Insurance for self-employed or partners".

---
+++ starting-up/tax-and-national-insurance-for-self-employed-and-partners.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Tax And National Insurance For Self Employed And Partners | Starting Up | Business Link</title>
+	<meta name="author" content="Caroline Murphy">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<!-- Page breadcrumb -->
+<div id="breadcrumb">
+	<p><a href="home.html">Home</a> &gt; <a href="/starting-up.php">Starting Up</a> &gt; [title]</p>
+</div>
+
+<span><a name="skipcont" id="skipcont"></a></span>
+
+<div>
+	<div>
+		<h1>Tax and National Insurance for self-employed or partners</h1>
+		<div>
+			<h2><a href="https://www.gov.uk/browse/tax/income-tax" title="Income Tax" rel="external">Get started with Income Tax and National Insurance</a></h2>
+			<p>What is National Insurance and how does it apply to you if you work for yourself</p>
+		</div>
+		<div>
+			<h2><a href="https://www.gov.uk/self-assessment-tax-returns" title="Self Assessment tax returns" rel="external">Find out about Self Assessment and your tax return </a></h2>
+			<p>Tools and videos to help you fill in your Self Assessment tax return and understand what the deadlines and penalities are</p>
+		</div>
+		<div>
+			<h2><a href="http://www.hmrc.gov.uk/incometax/relief-self-emp.htm" title="Tax allowances and reliefs if you're self-employed" rel="external">Get to grips with expenses and records if you're self-employed</a></h2>
+			<p>What records you should keep as a self-employed person and some tools to help you understand what you can and can't claim as a business expense</p>
+		</div>
+	</div>
+	<div>
+		<img src="/_img/Income_corp.jpg" width="146" height="146" border="0" alt="" />
+	</div>
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
