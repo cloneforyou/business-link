cbf9ab8f409f11d23a375187fedb1ea44bcac916
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-13 15:43:02 +0100

    Starting up page and linked pages.

---
+++ do-it-online-incorporate-your-company-online.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<meta charset="utf-8">
+	
+	<title>Incorporate your company online | Business Link</title>
+	<meta name="author" content="Caroline Murphy">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+
+<!-- Starting Up > Business Structures -->
+
+<?php include("inc/header.php"); ?>
+
+<div> <a href="home.html" title="Small Business Service home"><img src="../../syntegra/images/global-business-link-logo.png" alt="Business Link" border="0" /></a> </div>
+<div>Information. Support. Compliance.</div>
+<span><a name="skiplogin"></a></span>
+<div>
+			<p> Your account </p>
+			<ul>
+		<li> <a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate"> Register </a> </li>
+		<li> <a onclick="bLeaving=false;" href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate"> Log in </a> </li>
+	</ul>
+		</div>
+<div><img src="../../syntegra/images/px_trans.gif" alt="" width="745" height="1" /></div>
+<div >
+			<div>
+		<p><a href="home.html"  title="" >Home</a>&gt;<a href="findDIOL.html"  title="" >Do it online</a>&gt;Incorporate your company online</p>
+	</div>
+			<span><a name="skipcont"></a></span>
+			<div>
+		<div>
+					<div>
+				<h1>Incorporate your company online</h1>
+				<p>This service enables you to incorporate your company online as a private company, limited by shares, with model articles of association.</p>
+				<p>For explanations of company types and how to incorporate, see our guide on <a href="layer4919.html?topicId=1073865730" title="Company, partnership or sole trader - find the right structure for your new business" >legal structures: the basics</a>.</p>
+				<p>To use the service, you will need to register with your email address and a password of your choice.</p>
+				<p>You will then need the following information to complete the form:</p>
+				<ol>
+							<li>Company name and address</li>
+							<li>Details of directors and secretary (optional)</li>
+							<li>Details of each shareholder and the value of their shares</li>
+							<li>Payment details -<a href="http://www.companieshouse.gov.uk/toolsToHelp/ourPrices.shtml" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('http://www.companieshouse.gov.uk/toolsToHelp/ourPrices.shtml', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="Current prices for Companies House services" >find out more about Companies House fees on the Companies House website<span> - Opens in a new window</span></a> </li>
+						</ol>
+				<p>Any data captured through online incorporation is stored with the Registrar of Companies and not businesslink.gov.uk and is subject to the <a href="http://www.companieshouse.gov.uk/legal/privacy.shtml" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('http://www.companieshouse.gov.uk/legal/privacy.shtml', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="Privacy Statement from Companies House" >Companies House privacy policy<span> - Opens in a new window</span></a>.</p>
+				<p>This service is delivered by Companies House in conjunction with businesslink.gov.uk.</p>
+				<div><script type="text/javascript" src="../../syntegra/js/skinAndLink.js"></script> 
+							<script>document.write('<a href="https://ewf.companieshouse.gov.uk/govlink?xgovk3w=bl1000&xgovf0p=|xgovs9k=coho|xgovr3h=winc|xgovc8h=1000|xgovk3w=bl1000|&xgovd2v=en&xgovj6d=452734a110d6d1a1d0893be4ee96cab3ffde1a4c" onclick="goToOGD(\'https://ewf.companieshouse.gov.uk/govlink?xgovk3w=bl1000&amp;xgovf0p=|xgovs9k=coho|xgovr3h=winc|xgovc8h=1000|xgovk3w=bl1000|&amp;xgovd2v=en&amp;xgovj6d=452734a110d6d1a1d0893be4ee96cab3ffde1a4c\', \'0\', \'null\', \'/bdotg/action/crossSell?itemId=1086686813&type=CROSSSELL\');">Incorporate with Companies House now</a>');</script>
+							<noscript>
+					<a href="http://online.businesslink.gov.uk/bdotg/action/lightweightTransaction?itemId=1086686891&amp;lrcOgdId=coho&amp;lrcOgdTxnId=winc&amp;type=LIGHTWEIGHT_TRANSACTION" >Incorporate with Companies House now</a>
+					</noscript>
+						</div>
+			</div>
+					<div>
+				<div>
+							<dl>
+						<dt>In partnership with:</dt>
+						<dd> <a href="http://www.companieshouse.gov.uk/" target="bgExternalwwwbusinesslinkgovuk" onclick="p=window.open('http://www.companieshouse.gov.uk/', 'bgExternalwwwbusinesslinkgovuk');p.focus();" title="Companies House - Opens in a new window"><img src="../../Taxes_and_Returns_images/ch_big_logo_new.jpg" width="115" height="81" border="0" alt="" />Companies House<span> - Opens in a new window</span></a> </dd>
+					</dl>
+						</div>
+			</div>
+				</div>
+	</div>
+
+<?php include("inc/footer.php"); ?>
---
+++ starting-up.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<meta charset="utf-8">	
+	<title>Starting Up | Business Link</title>
+	<meta name="author" content="Caroline Murphy">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+
+<?php include("inc/header.php"); ?>
+
+<div id="content" > 
+	
+	<?php include("inc/breadcrumbs.php"); ?>
+
+	<div id="layer11">
+		<div id="maincontent">
+			<h1>Starting up</h1>
+			<div id="theme_container">
+				<div id="theme_title">
+					<h2>My New Business</h2>
+				</div>
+				<div id="theme_left">
+					<p><img src="img/startingupabusiness.jpg" alt="" /> </p>
+				</div>
+				<div id="theme_right">
+					<h3>Start your business here</h3>
+					<ul id="start_up_list">
+						<li>Over 200 new video and learning resources</li>
+						<li>Create your own checklist from our recommended start-up tasks</li>
+						<li>Access government services online</li>
+					</ul>
+				</div>
+				<div id="newBusinessContent">
+					<a title="Go to My New Business" href="https://www.gov.uk/browse/business/setting-up" rel="external">Get started</a>
+				</div>
+			</div>
+			
+			<h2>Before you start</h2>
+			<ul>
+				<li><a href="starting-up-cosidering-starting-up.php">Considering starting up?</a></li>
+				<li><a href="starting-up-business-planning.php">Business planning</a></li>
+				<li><a href="starting-up-find-and-manage-the-money.php">Find and manage the money</a></li>
+			</ul>
+			<h2>Forming and naming your business</h2>
+			<ul>
+				<li><a href="starting-up-business-structures.php">Business structures</a></li>
+				<li><a href="starting-up-name-your-business.php">Name your business</a></li>
+				<li><a href="starting-up-buy-a-business.php">Buy a business</a></li>
+			</ul>
+			<h2>Your workplace</h2>
+			<ul>
+				<li><a href="starting-up-choose-and-set-up-your-workplace.php">Choose and set up your workplace</a></li>
+			</ul>
+			<h2>Tax, payroll &amp; company information</h2>
+			<ul>
+				<li><a href="https://www.gov.uk/browse/tax" rel="external">Get to grips with business taxes</a></li>
+				<li><a href="starting-up-tax-and-national-insurance-for-self-employed-and-partners.php">Tax and National Insurance for self-employed or partners</a></li>
+				<li><a href="starting-up-corporation-tax.php">Corporation Tax</a></li>
+				<li><a href="starting-up-vat.php">VAT</a></li>
+				<li><a href="starting-up-tax-and-payroll-for-employers.php">Tax and payroll for employers</a></li>
+			</ul>
+			<h2>Employing people</h2>
+			<ul>
+				<li><a href="starting-up-become-an-employer-for-the-first-time.php">Become an employer for the first time</a></li>
+			</ul>
+			<h2>Managing your business</h2>
+			<ul>
+				<li><a href="starting-up-sales-and-marketing.php">Sales and marketing</a></li>
+				<li><a href="starting-up-it-and-ecommerce-for-startups.php">IT and e-commerce for start ups</a></li>
+				<li><a href="starting-up-trade-with-other-countries.php">Trade with other countries</a></li>
+				<li><a href="starting-up-protect-your-business-ideas.php">Protect your business ideas</a></li>
+			</ul>
+			<h2>Help and support</h2>
+			<ul>
+				<li><a href="starting-up-further-support-for-your-business.php">Further support for your business</a></li>
+			</ul>
+			<h2>Go for growth</h2>
+			<ul>
+				<li><a href="starting-up-take-the-next-steps.php">Take the next steps</a></li>
+			</ul>
+			<p><a href="https://www.gov.uk/browse/business/setting-up" rel="external">Starting up - site map</a></p>
+		</div>
+
+		<div id="additionalcontent">
+			<div>
+				<h3>Spotlight</h3>
+				<div>
+					<div>
+						<h4><a href="find-the-business-in-you.php">The business in you</a></h4>
+						<p>Starting and growing a business - tips and tools from real-life success stories</p>
+					</div>
+					<div>
+						<a href="find-the-business-in-you.php" title="The 'Business in You' is a major new campaign to help people to start a new business or grow their existing one" >
+							<img src="img/Businessinyou_whatsnewpromo.jpg" width="76" height="76" border="0" alt="The "Business in You" is a major new campaign to help people to start a new business or grow their existing one" />
+						</a>
+					</div>
+				</div>
+				<div>
+					<div>
+						<h4><a href="do-it-online-register-online-for-business-taxes.php">HMRC registration service</a></h4>
+						<p>Need to register for business taxes? Use the new HMRC tax registration service</p>
+					</div>
+					<div>
+						<a href="do-it-online-register-online-for-business-taxes.php" title="Register online for business taxes">
+							<img src="img/Businessman.jpg" width="76" height="76" border="0" alt="Register online for business taxes" />
+						</a>
+					</div>
+				</div>
+				<div>
+					<div>
+						<h4><a href="https://www.gov.uk/business-selling-customers" rel="external">Understand your market</a></h4>
+						<p>How to find out about customers and competitors</p>
+					</div>
+					<div>
+						<a href="https://www.gov.uk/business-selling-customers" title="Researching your market and competition">
+							<img src="img/Understand_your_market.jpg" width="76" height="76" border="0" alt="Researching your market and competition" />
+						</a>
+					</div>
+				</div>
+				<div>
+					<div>
+						<h4><a href="https://www.gov.uk/business-finance-explained" rel="external">How much money do you need?</a></h4>
+						<p>Work out the cost of starting and running a new business</p>
+					</div>
+					<div>
+						<a href="https://www.gov.uk/business-finance-explained" title="Identify your start-up costs and manage your money. Find out how to forecast your cashflow and record your profit and loss">
+							<img src="img/How_much_money_do_you_need.jpg" width="76" height="76" border="0" alt="Identify your start-up costs and manage your money. Find out how to forecast your cashflow and record your profit and loss" />
+						</a>
+					</div>
+				</div>
+				<div>
+					<div>
+						<h4><a href="https://www.gov.uk/starting-up-a-business" rel="external">Be your own boss</a></h4>
+						<p>Decide if you've got what it takes</p>
+					</div>
+					<div>
+						<a href="https://www.gov.uk/starting-up-a-business" title="Learn about the qualities you need to be your own boss and how you can use your skills and experience to get your business up and running">
+							<img src="img/Be_your_own_boss.jpg" width="76" height="76" border="0" alt="Learn about the qualities you need to be your own boss and how you can use your skills and experience to get your business up and running" />
+						</a>
+					</div>
+				</div>
+				<div>
+					<div>
+						<h4><a href="https://www.gov.uk/business-legal-structures" rel="external">Your business options</a></h4>
+						<p>Understand the different business types</p>
+					</div>
+					<div>
+						<a href="https://www.gov.uk/business-legal-structures" title="Learn about the different types of business and legal structures to help you choose the right one for your new business">
+							<img src="img/Your_business_options.jpg" width="76" height="76" border="0" alt="Learn about the different types of business and legal structures to help you choose the right one for your new business" />
+						</a>
+					</div>
+				</div>
+				<div>
+					<div>
+						<h4><a href="do-it-online-register-online-for-business-taxes.php" rel="external">Your business type</a></h4>
+						<p>Choose the right business model and legal structure</p>
+					</div>
+					<div>
+						<a href="do-it-online-register-online-for-business-taxes.php" title="Tutorials, videos and tools to help you decide how to become self-employed - from sole trader to limited company, franchisee to social enterprise and more" >
+							<img src="img/Your_business_type.jpg" width="76" height="76" border="0" alt="Tutorials, videos and tools to help you decide how to become self-employed - from sole trader to limited company, franchisee to social enterprise and more" />
+						</a>
+					</div>
+				</div>
+				<div>
+					<div>
+						<h4><a href="https://www.gov.uk/write-business-plan" rel="external">Create your business plan</a></h4>
+						<p>All you need to know to write your plan</p>
+					</div>
+					<div>
+						<a href="https://www.gov.uk/write-business-plan" title="What you need to include in your business plan and how to create one step-by-step" rel="external">
+							<img src="img/Create_your_business_plan.jpg" width="76" height="76" border="0" alt="What you need to include in your business plan and how to create one step-by-step" />
+						</a>
+					</div>
+				</div>
+			</div>
+			<div>
+				<h3>Do it online</h3>
+				<ul>
+					<li><a href="https://www.gov.uk/get-information-about-a-company" rel="external">Search for an available company name and trade mark</a></li>
+					<li><a href="do-it-online-incorporate-your-company-online.php">Incorporate your company online</a></li>
+					<li><a href="do-it-online-apply-to-register-a-trademark.php">Apply to register a trade mark</a></li>
+				</ul>
+			</div>
+			<div>
+				<h3>Directories</h3>
+				<ul>
+					<li><a href="https://www.gov.uk/browse/business/setting-up" title="National and local events, seminars and workshops in your area to help you learn or network" rel="external">Starting up events near you</a></li>
+					<li>
+						<a href="browse-by-theme-starting-up.php">Starting up case studies</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+<?php include("inc/footer.php"); ?>
