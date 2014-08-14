34b055127d3aefd67beb318b2c79177726036c35
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-14 18:11:27 +0100

    Completed "Employer's Charter" page.

--- employers-charter.php
+++ employers-charter.php
@@ -10,34 +10,25 @@
 
 <body>
 
-<?php include("inc/skip-links.php"); ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
 
-<div> <a href="home.html" title="Small Business Service home"><img src="../../syntegra/images/global-business-link-logo.png" alt="Business Link" border="0" /></a> </div>
-<div>Information. Support. Compliance.</div>
-<span><a name="skiplogin"></a></span>
 <div>
-		<p> Your account </p>
-		<ul>
-		<li> <a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate"> Register </a> </li>
-		<li> <a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate"> Log in </a> </li>
-	</ul>
-	</div>
-<div><img src="../../syntegra/images/px_trans.gif" alt="" width="745" height="1" /></div>
-<div >
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+	<div>
 		<div>
-		<p><a href="home.html"  title="" >Home</a>&gt;Find out what you can and can't do when managing your staff</p>
-	</div>
-		<span><a name="skipcont"></a></span>
-		<div>
-		<div>
-				<h1>Employer's Charter</h1>
-				<h2>Find out what you can and can't do when managing your staff</h2>
-				<p>For employment law to be effective there needs to be a balance between protecting employees and enabling businesses to operate effectively in order to support economic growth.</p>
-				<p>Advice and guidance on employment law often focuses on the rights of employees and what employers have to do to comply with their responsibilities. This focus can leave some businesses feeling they have lots of responsibilities but no rights, the balance is all in the favour of the employee, and being afraid to take on and effectively manage their staff.</p>
-				<p>The <strong>Employer's Charter</strong> aims to dismiss some of the myths about what employers can and can't do in managing their workforce. It tells you what you reasonably can ask and know about your employees, and what action you can take if there are problems.</p>
-				<h3>The Employer's Charter</h3>
-				<p>As an employer -as long as you act fairly andreasonably -you can:</p>
-				<ul>
+			<h1>Employer's Charter</h1>
+			
+			<h2>Find out what you can and can't do when managing your staff</h2>
+			
+			<p>For employment law to be effective there needs to be a balance between protecting employees and enabling businesses to operate effectively in order to support economic growth.</p>
+			<p>Advice and guidance on employment law often focuses on the rights of employees and what employers have to do to comply with their responsibilities. This focus can leave some businesses feeling they have lots of responsibilities but no rights, the balance is all in the favour of the employee, and being afraid to take on and effectively manage their staff.</p>
+			<p>The <strong>Employer's Charter</strong> aims to dismiss some of the myths about what employers can and can't do in managing their workforce. It tells you what you reasonably can ask and know about your employees, and what action you can take if there are problems.</p>
+			<h3>The Employer's Charter</h3>
+			<p>As an employer - as long as you act fairly andreasonably -you can:</p>
+			
+			<ul>
 				<li>contact a woman on maternity leave and ask when she plans to return</li>
 				<li>reject an employee's request to work flexibly if you have a legitimate business reason</li>
 				<li>talk to your employees about their performance and how they can improve</li>
@@ -49,123 +40,50 @@
 				<li>seek an independent assessment of your employee's fitness for work</li>
 				<li>dismiss an employee if they have had a period of long-term absence or repeated short-term absences</li>
 			</ul>
-				<p>You can also ask an employee:</p>
-				<ul>
+			
+			<p>You can also ask an employee:</p>
+			
+			<ul>
 				<li>to take a pay cut or change some other terms and conditions, for example their normal working hours</li>
 				<li>whether they would be willing to opt out from the 48-hour limit in the Working Time Regulations</li>
 				<li>about their future career plans, including retirement</li>
 			</ul>
-				<p>And:</p>
-				<ul>
+			
+			<p>And:</p>
+			
+			<ul>
 				<li>during interview you can ask a job candidate questions that are relevant to the requirements of the job (but questions need to be fair and unbiased)</li>
 				<li>to settle a dispute, you can protect yourself against most claims from an employee by entering into a 'compromise agreement' (often in return for an agreed payment)</li>
 			</ul>
-				<p>This is intended to help employers understand what they can do in general. Of course, individual circumstances may vary and employers should act in accordance with their legal obligations.</p>
-				<p><a href="http://www.bis.gov.uk/assets/biscore/employment-matters/docs/e/11-680-employers-charter" target="bgExternalonlinebusinesslinkgovuk" title="Guidance on your rights and responsibilities as an employer" >Download the printer friendly Employer Charter from the Department for Business, Innovation and Skills website (PDF, 108K)<span> - Opens in a new window</span></a>.</p>
-				<p>If you are taking on an employee and want help on what you need to include in a contract see our guide on<a href="layer64ac.html?topicId=1073971494" title="Understanding the basic elements of a contract of employment as required by law" >employment contracts</a>.</p>
-				<p>Youcan also<a href="../../Employing_People_files/Written_Statement.pdf" target="bgExternalonlinebusinesslinkgovuk" title="An example of a written statement of employment particulars form" >download our template for a written statement of employment (PDF, 219K)<span> - Opens in a new window</span></a>.</p>
-				<p>For more information about employment law in practice, see our guidance on:</p>
-				<ul>
-				<li><a href="layer0b62.html?topicId=1073931239" title="Flexible working law and best practice" >Flexible working - the law and best practice</a></li>
-				<li><a href="layer5f6b.html?topicId=1080898273" title="The statutory rights to leave and pay when an employee has a baby" >Maternity leave and pay</a></li>
-				<li><a href="layer4a42.html?topicId=1084822795" title="Rules, calculations and best practice for giving people time off work" >Holiday and time off</a></li>
-				<li><a href="layer19db.html?topicId=1074019927" title="Legal obligations when making employees redundant" >Redundancy: the options</a></li>
-				<li><a href="layer1c63.html?topicId=1074200615" title="Understand the issues around dismissing staff and how to manage the process fairly and correctly" >Dismissal</a> </li>
+			
+			<p>This is intended to help employers understand what they can do in general. Of course, individual circumstances may vary and employers should act in accordance with their legal obligations.</p>
+			<p><a href="#">Download the printer friendly Employer Charter from the Department for Business, Innovation and Skills website (PDF, 108K)</a>.</p>
+			<p>If you are taking on an employee and want help on what you need to include in a contract see our guide on <a href="/employment-and-skills/staff-documents-and-employment-policies/the-employment-contract.php.html">employment contracts</a>.</p>
+			<p>You can also <a href="/_files/written_statement.pdf">download our template for a written statement of employment (PDF, 219K)</a>.</p>
+			<p>For more information about employment law in practice, see our guidance on:</p>
+			
+			<ul>
+				<li><a href="/employment-and-skills/working-time/flexible-working-the-law-and-best-practice.php.html">Flexible working - the law and best practice</a></li>
+				<li><a href="/employment-and-skills/maternity-paternity-and-adoption/maternity-leave-and-pay.php.html">Maternity leave and pay</a></li>
+				<li><a href="/employment-and-skills/holiday-and-other-leave.php.html">Holiday and time off</a></li>
+				<li><a href="/employment-and-skills/redundancy-restructures-and-change/redundancy-the-options.php.html">Redundancy: the options</a></li>
+				<li><a href="/employment-and-skills/dismissals-and-staff-leaving/dismissal.php.html">Dismissal</a> </li>
 				<li>Pay and pensions</li>
-				<li><a href="layer9415.html?topicId=1074014258" title="The rules about working hours and how best to manage them" >Hours, rest breaks and the working week</a></li>
-				<li><a href="layer2cd1.html?topicId=1073876974"  >Leaving and retirement</a><br />
-					</li>
+				<li><a href="/employment-and-skills/working-time/hours-rest-breaks-and-the-working-week.php.html">Hours, rest breaks and the working week</a></li>
+				<li><a href="/employment-and-skills/dismissals-and-staff-leaving.php.html">Leaving and retirement</a></li>
 			</ul>
-				<p><img src="../../syntegra/images/hr_325px_global.gif" alt="" align="middle" /></p>
-				<div>
+
+			<hr />
+			
+			<div>
 				<ul>
-						<script language="javascript" type="text/javascript">
-					<!--
-						document.write('<li><a href="#" title="Print page now  - Opens in a new window">Print page now <span> - Opens in a new window</span></a></li>');
-					//-->
-				</script>
-						<noscript>
-					<li><a href="http://online.businesslink.gov.uk/bdotg/action/printpage?itemId=1086685881&amp;type=ONEOFFPAGE" title="Print page now  - Opens in a new window" target="bgPrintable">Print page now <span> - Opens in a new window</span></a></li>
-					</noscript>
-						<li><a href="http://online.businesslink.gov.uk/bdotg/action/emailafriend?itemId=1086685881&amp;refPage=%2Fbdotg%2Faction%2Fdetail%3Fatom_id%3D0.PR388946%26itemId%3D1086685881%26type%3DONEOFFPAGE&amp;type=ONEOFFPAGE" title="Tell a friend about this page" >Tell a friend about this page</a></li>
-						<li><a href="https://online.businesslink.gov.uk/bdotg/action/subscribeAlert?alertType=ONEOFFPAGE&amp;itemId=1086685881&amp;refPage=%2Fbdotg%2Faction%2Fdetail%3Fatom_id%3D0.PR388946%26itemId%3D1086685881%26type%3DONEOFFPAGE&amp;type=ONEOFFPAGE" title="Tell a friend about this page" >Send me an email alert when this page changes</a></li>
-					</ul>
+					<li><a href="#">Print page now </a></li>
+					<li><a href="#">Tell a friend about this page</a></li>
+					<li><a href="#">Send me an email alert when this page changes</a></li>
+				</ul>
 			</div>
-			</div>
-		<div>
-				<div>
-				<div> </div>
-				<div> </div>
-			</div>
-			</div>
+		</div>
 	</div>
-		<span><a name="skipfooter"></a></span>
-		<div>
-		<div><img src="../../syntegra/images/footer_top3.gif" alt="" />
-				<div> <span><a href="https://online.businesslink.gov.uk/bdotg/action/userSupport?refPage=%2Fbdotg%2Faction%2Fdetail%3Fatom_id%3D0.PR388946%26itemId%3D1086685881%26type%3DONEOFFPAGE" title="Help &amp; support" >Help &amp; support</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Copyright" title="&copy; Crown copyright 2011" >Copyright</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Disclaimer" title="Terms &amp; conditions" >Terms &amp; conditions</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Privacy" title="Privacy &amp; cookies" >Privacy &amp; cookies</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility" title="Accessibility" >Accessibility</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=HelpWithPDFFiles" title="PDF help" >PDF help</a> </span> </div>
-			</div>
-	</div>
-	</div>
-<div>
-		<div> <span><a name="skiphelp"id="skiphelp"></a></span>
-		<div>
-				<div>
-				<div>
-						<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/redirectToSite">
-						<fieldset>
-								<label for="localBusinessLink">Regional Support</label>
-								<select name="Site Id">
-								<option selected="selected" value="default">Select your location</option>
-								<optgroup label='Northern Ireland'>
-									<option value='1087659111'>nibusinessinfo.co.uk</option>
-									</optgroup>
-								<optgroup label='Scotland'>
-									<option value='1087659110'>Business Gateway</option>
-									</optgroup>
-								<optgroup label='Wales'>
-									<option value='1087659109'>Welsh Government</option>
-									</optgroup>
-							</select>
-								<input type="submit" value="Go" />
-							</fieldset>
-					</form>
-					</div>
-				<div>
-						<ul>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/fullsitemap" title="Site map" >Sitemap</a></li>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/help?page=HelpPage" title="Help" >Help</a></li>
-						<li><a href="detail2d1f.html?itemId=1087351667&amp;page=AboutUs&amp;r.i=1086685881&amp;r.s=h&amp;r.t=ONEOFFPAGE&amp;type=CAMPAIGN" title="About us" >About us</a></li>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility" >Accessibility</a></li>
-					</ul>
-					</div>
-				<div>
-						<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/searchBasicMode">
-						<fieldset>
-								<input type="hidden" name="resultPage" value="1"/>
-								<label for="headersearchinput">Search</label>
-								<input type="text" name="expression" />
-								<input type="submit" value="Search" title="Start the search" />
-							</fieldset>
-					</form>
-					</div>
-			</div>
-				<div>
-				<div>
-						<ul>
-						<li><a href="https://online.businesslink.gov.uk/bdotg/action/myBusiness" title="" >My Business</a></li>
-						<li><a href="layer4866.html?topicId=1075193191" title="" >What's new?</a></li>
-						<li><a href="layer21a5.html?topicId=1075193162" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies" >Do it online</a></li>
-						<li><a href="layercdb4.html?topicId=1075193173" title="" >Directories</a></li>
-					</ul>
-					</div>
-			</div>
-			</div>
-	</div>
-	</div>
+</div>
 
-<?php include("inc/category-links.php"); ?>
-
-<?php include("inc/scripts.php"); ?>
-
-</body>
-</html>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
