dab39bf5ffed5c19f2c450234e949189d25cbebc
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-14 17:04:27 +0100

    Updated the "Find the business in you" page.

--- find-the-business-in-you.php
+++ find-the-business-in-you.php
@@ -10,130 +10,42 @@
 
 <body>
 
-<?php include("inc/skip-links.php"); ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
 
-<div> <a href="home.html" title="Small Business Service home"><img src="../../syntegra/images/global-business-link-logo.png" alt="Business Link" border="0" /></a> </div>
-<div>Information. Support. Compliance.</div>
-<span><a name="skiplogin"></a></span>
 <div>
-		<p> Your account </p>
-		<ul>
-		<li> <a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate"> Register </a> </li>
-		<li> <a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate"> Log in </a> </li>
-	</ul>
-	</div>
-<div><img src="../../syntegra/images/px_trans.gif" alt="" width="745" height="1" /></div>
-<div >
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+	<div>
 		<div>
-		<p><a href="home.html"  title="" >Home</a>&gt;The &quot;Business in You&quot; is a major new campaign to help people to start a new business or grow their existing one</p>
-	</div>
-		<span><a name="skipcont"></a></span>
-		<div>
-		<div>
-				<h1>Find the business in you</h1>
-				<h2>The &quot;Business in You&quot; is a major new campaign to help people to start a new business or grow their existing one</h2>
-				<p>Launched by the Prime Minister on 23 January, the campaign is a partnership between private enterprise and Government to highlight support for start-ups and growing businesses.</p>
-				<p>The campaign wants to encourage people to start a business, employ more staff, or start exporting to make 2012 the &quot;year of enterprise&quot;. It's predicted that the next generation of SMEs will create 70,000 jobs over the coming 12 months.</p>
-				<p>Everyone featured in the campaign is a real life small business owner whose firm is thriving despite the challenging economic times. They come from a wide range of backgrounds, but each has a real, inspiring story of how they turned their passion into their livelihood.</p>
-				<p>The campaign will also highlight top tools available from BusinessLink, StartUp Britain and a wide range of partners.</p>
-				<p>'Business in You' runs nationwide for the duration of the Olympic year 2012 which is a unique moment for the UK on the global stage. Each month will focus on a different theme will spotlight key business support products.</p>
-				<p><a href="http://businessinyou.helpfulclients.com/" target="bgExternalonlinebusinesslinkgovuk"  >Go to Business in You to find out about more about the campaign, read the case studies, or register for email alerts about the campaign<span> - Opens in a new window</span></a>.</p>
-				<p><img src="../../syntegra/images/hr_325px_global.gif" alt="" align="middle" /></p>
-				<div>
+			<h1>Find the business in you</h1>
+			<h2>The &quot;Business in You&quot; is a major new campaign to help people to start a new business or grow their existing one</h2>
+			<p>Launched by the Prime Minister on 23 January, the campaign is a partnership between private enterprise and Government to highlight support for start-ups and growing businesses.</p>
+			<p>The campaign wants to encourage people to start a business, employ more staff, or start exporting to make 2012 the &quot;year of enterprise&quot;. It's predicted that the next generation of SMEs will create 70,000 jobs over the coming 12 months.</p>
+			<p>Everyone featured in the campaign is a real life small business owner whose firm is thriving despite the challenging economic times. They come from a wide range of backgrounds, but each has a real, inspiring story of how they turned their passion into their livelihood.</p>
+			<p>The campaign will also highlight top tools available from BusinessLink, StartUp Britain and a wide range of partners.</p>
+			<p>'Business in You' runs nationwide for the duration of the Olympic year 2012 which is a unique moment for the UK on the global stage. Each month will focus on a different theme will spotlight key business support products.</p>
+			<p><a href="#">Go to Business in You to find out about more about the campaign, read the case studies, or register for email alerts about the campaign</a>.</p>
+			<p><img src="/_img/hr_325px_global.gif" alt="" align="middle" /></p>
+			<div>
 				<ul>
-						<script language="javascript" type="text/javascript">
-					<!--
-						document.write('<li><a href="#" title="Print page now  - Opens in a new window">Print page now <span> - Opens in a new window</span></a></li>');
-					//-->
-				</script>
-						<noscript>
-					<li><a href="http://online.businesslink.gov.uk/bdotg/action/printpage?itemId=1097414269&amp;type=ONEOFFPAGE" title="Print page now  - Opens in a new window" target="bgPrintable">Print page now <span> - Opens in a new window</span></a></li>
-					</noscript>
-						<li><a href="http://online.businesslink.gov.uk/bdotg/action/emailafriend?itemId=1097414269&amp;refPage=%2Fbdotg%2Faction%2Fdetail%3Fatom_id%3D0.PR384546%26itemId%3D1097414269%26type%3DONEOFFPAGE&amp;type=ONEOFFPAGE" title="Tell a friend about this page" >Tell a friend about this page</a></li>
-						<li><a href="https://online.businesslink.gov.uk/bdotg/action/subscribeAlert?alertType=ONEOFFPAGE&amp;itemId=1097414269&amp;refPage=%2Fbdotg%2Faction%2Fdetail%3Fatom_id%3D0.PR384546%26itemId%3D1097414269%26type%3DONEOFFPAGE&amp;type=ONEOFFPAGE" title="Tell a friend about this page" >Send me an email alert when this page changes</a></li>
-					</ul>
+					<li><a href="#">Print page now</a></li>
+					<li><a href="#">Tell a friend about this page</a></li>
+					<li><a href="#">Send me an email alert when this page changes</a></li>
+				</ul>
 			</div>
-			</div>
+		</div>
 		<div>
+			<div>
 				<div>
-				<div>
-						<table border="0" cellpadding="0" cellspacing="0">
+					<table border="0" cellpadding="0" cellspacing="0">
 						<tr>
-								<td><img src="../../syntegra/images/px_white.gif" width="181" height="5" alt="" /><img src="../../Horizontal_Services_images/Businessinyouoneoffpage.jpg" width="181" height="121" border="0" alt="" /><img src="../../syntegra/images/px_global.gif" width="181" height="5" alt="" /></td>
-							</tr>
+							<td><img src="/_img/Businessinyouoneoffpage.jpg" width="181" height="121" border="0" alt="" /></td>
+						</tr>
 					</table>
-					</div>
-				<div> </div>
+				</div>
 			</div>
-			</div>
+		</div>
 	</div>
-		<span><a name="skipfooter"></a></span>
-		<div>
-		<div><img src="../../syntegra/images/footer_top3.gif" alt="" />
-				<div> <span><a href="https://online.businesslink.gov.uk/bdotg/action/userSupport?refPage=%2Fbdotg%2Faction%2Fdetail%3Fatom_id%3D0.PR384546%26itemId%3D1097414269%26type%3DONEOFFPAGE" title="Help &amp; support" >Help &amp; support</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Copyright" title="&copy; Crown copyright 2011" >Copyright</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Disclaimer" title="Terms &amp; conditions" >Terms &amp; conditions</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Privacy" title="Privacy &amp; cookies" >Privacy &amp; cookies</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility" title="Accessibility" >Accessibility</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=HelpWithPDFFiles" title="PDF help" >PDF help</a> </span> </div>
-			</div>
-	</div>
-	</div>
-<div>
-		<div> <span><a name="skiphelp"id="skiphelp"></a></span>
-		<div>
-				<div>
-				<div>
-						<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/redirectToSite">
-						<fieldset>
-								<label for="localBusinessLink">Regional Support</label>
-								<select name="Site Id">
-								<option selected="selected" value="default">Select your location</option>
-								<optgroup label='Northern Ireland'>
-									<option value='1087659111'>nibusinessinfo.co.uk</option>
-									</optgroup>
-								<optgroup label='Scotland'>
-									<option value='1087659110'>Business Gateway</option>
-									</optgroup>
-								<optgroup label='Wales'>
-									<option value='1087659109'>Welsh Government</option>
-									</optgroup>
-							</select>
-								<input type="submit" value="Go" />
-							</fieldset>
-					</form>
-					</div>
-				<div>
-						<ul>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/fullsitemap" title="Site map" >Sitemap</a></li>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/help?page=HelpPage" title="Help" >Help</a></li>
-						<li><a href="detail63ca.html?itemId=1087351667&amp;page=AboutUs&amp;r.i=1097414269&amp;r.s=h&amp;r.t=ONEOFFPAGE&amp;type=CAMPAIGN" title="About us" >About us</a></li>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility" >Accessibility</a></li>
-					</ul>
-					</div>
-				<div>
-						<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/searchBasicMode">
-						<fieldset>
-								<input type="hidden" name="resultPage" value="1"/>
-								<label for="headersearchinput">Search</label>
-								<input type="text" name="expression" />
-								<input type="submit" value="Search" title="Start the search" />
-							</fieldset>
-					</form>
-					</div>
-			</div>
-				<div>
-				<div>
-						<ul>
-						<li><a href="https://online.businesslink.gov.uk/bdotg/action/myBusiness" title="" >My Business</a></li>
-						<li><a href="layer4866.html?topicId=1075193191" title="" >What's new?</a></li>
-						<li><a href="layer21a5.html?topicId=1075193162" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies" >Do it online</a></li>
-						<li><a href="layercdb4.html?topicId=1075193173" title="" >Directories</a></li>
-					</ul>
-					</div>
-			</div>
-			</div>
-	</div>
-	</div>
+</div>
 
-<?php include("inc/category-links.php"); ?>
-
-<?php include("inc/scripts.php"); ?>
-
-</body>
-</html>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
