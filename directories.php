7c9f1b131aa06010b18a860eecfa840113710b7e
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-14 17:22:50 +0100

    Completed "Directories" page. Removed stupid dotted hr image and made table full width.

--- directories.php
+++ directories.php
@@ -10,25 +10,7 @@
 
 <body>
 
-<?php include("inc/skip-links.php"); ?>
-
-<div>
-	<a href="home.html" title="Small Business Service home">
-		<img src="img/global-business-link-logo.png" alt="Business Link" border="0" />
-	</a>
-</div>
-
-<div>Information. Support. Compliance.</div>
-
-<span><a name="skiplogin" id="skiplogin"></a></span>
-
-<div>
-	<p>Your account</p>
-	<ul>
-		<li><a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate">Register</a></li>
-		<li><a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate">Log in</a></li>
-	</ul>
-</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
 
 <div>
 	<div>
@@ -39,182 +21,89 @@
 	
 	<div>
 		<h1>Directories</h1>
-		<table border="0" cellpadding="0" cellspacing="0" width="534">
+		<table border="0" cellpadding="0" cellspacing="0">
 			<tr>
 				<td>
-					<a href="http://www.events.businesslink.gov.uk/" target="bgExternalwwwbusinesslinkgovuk" title="Find events from a variety of organisations - Opens in a new window">
-						<img src="img/TL0011.jpg" width="76" height="76" border="0" alt="Find events from a variety of organisations" />
-						<span> - Opens in a new window</span>
+					<a href="https://www.gov.uk/events-finder" title="Search for business training and networking events" rel="external">
+						<img src="/_img/TL0011.jpg" width="76" height="76" border="0" alt="Find events from a variety of organisations" />
+						<h2>Find events</h2>
+						<p>Search or browse for business events throughout the UK</p>
 					</a>
-					<img src="img/tablanding_top.gif" alt="" width="185" height="10" />
-					<h2><a href="http://www.events.businesslink.gov.uk/" target="bgExternalwwwbusinesslinkgovuk" title="Find events from a variety of organisations - Opens in a new window">Find events<span> - Opens in a new window</span></a></h2>
-					<p>Search or browse for business events throughout the UK</p>
 				</td>
 				<td>
-					<a href="http://online.businesslink.gov.uk/bdotg/action/findcontact" title="Find contact details from our database of over 4,000 organisations">
-						<img src="img/TL0012.jpg" width="76" height="76" border="0" alt="Find contact details from our database of over 4,000 organisations" />
+					<a href="#">
+						<img src="/_img/TL0012.jpg" width="76" height="76" border="0" alt="Find contact details from our database of over 4,000 organisations" />
+						<h2>Find a contact</h2>
+						<p>Search our database of more than 4,000 business and government contacts</p>
 					</a>
-					<img src="img/tablanding_top.gif" alt="" width="185" height="10" />
-					<h2><a href="http://online.businesslink.gov.uk/bdotg/action/findcontact" title="Find contact details from our database of over 4,000 organisations">Find a contact</a></h2>
-					<p>Search our database of more than 4,000 business and government contacts</p>
 				</td>
 			</tr>
 			<tr>
 				<td>
-					<a href="https://online.businesslink.gov.uk/hub/action/render?pageId=learningdirectory" title="Start-up learning directory">
-						<img src="img/tutorial_02.jpg" width="76" height="76" border="0" alt="Start-up learning directory" />
+					<a href="https://www.gov.uk/browse/business/setting-up" title="Setting up" rel="external">
+						<img src="/_img/tutorial_02.jpg" width="76" height="76" border="0" alt="Start-up learning directory" />
+						<h2>Start-up learning directory</h2>
+						<p>Find over 200 videos and e-learning courses on a wide range of start-up topics</p>
 					</a>
-					<img src="img/tablanding_top.gif" alt="" width="185" height="10" />
-					<h2><a href="https://online.businesslink.gov.uk/hub/action/render?pageId=learningdirectory" title="Start-up learning directory">Start-up learning directory</a></h2>
-					<p>Find over 200 videos and e-learning courses on a wide range of start-up topics</p>
 				</td>
 				<td>
-					<a href="http://improve.businesslink.gov.uk/resources/business-support-finder" target="bgExternalwwwbusinesslinkgovuk" title="Search our business support finder for grants, loans, expertise and advice for which your business may be eligible - Opens in a new window">
-						<img src="img/support_finder_1.jpg" width="76" height="76" border="0" alt="Search our business support finder for grants, loans, expertise and advice for which your business may be eligible" />
-						<span> - Opens in a new window</span>
+					<a href="https://www.gov.uk/growing-your-business" title="Growing your business" rel="external">
+						<img src="/_img/support_finder_1.jpg" width="76" height="76" border="0" alt="Search our business support finder for grants, loans, expertise and advice for which your business may be eligible" />
+						<h2>Business support finder</h2>
+						<p>Search our business support finder for grants, loans, expertise and advice for which your business may be eligible</p>
 					</a>
-					<img src="img/tablanding_top.gif" alt="" width="185" height="10" />
-					<h2><a href="http://improve.businesslink.gov.uk/resources/business-support-finder" target="bgExternalwwwbusinesslinkgovuk" title="Search our business support finder for grants, loans, expertise and advice for which your business may be eligible - Opens in a new window">Business support finder<span> - Opens in a new window</span></a></h2>
-					<p>Search our business support finder for grants, loans, expertise and advice for which your business may be eligible</p>
 				</td>
 			</tr>
 			<tr>
 				<td>
-					<a href="http://online.businesslink.gov.uk/bdotg/action/glossaryLanding" title="Search our online dictionary of business terms">
-						<img src="img/glossary.jpg" width="76" height="76" border="0" alt="Search our online dictionary of business terms" />
+					<a href="#">
+						<img src="/_img/glossary.jpg" width="76" height="76" border="0" alt="Search our online dictionary of business terms" />
+						<h2>Glossary</h2>
+						<p>Find definitions of useful business terms in our glossary</p>
 					</a>
-					<img src="img/tablanding_top.gif" alt="" width="185" height="10" />
-					<h2><a href="http://online.businesslink.gov.uk/bdotg/action/glossaryLanding" title="Search our online dictionary of business terms">Glossary</a></h2>
-					<p>Find definitions of useful business terms in our glossary</p>
 				</td>
 				<td>
-					<a href="layer105b.html?topicId=1074400185" title="Search our Training Directory for details of courses currently available">
-						<img src="img/TL0010.jpg" width="76" height="76" border="0" alt="Search our Training Directory for details of courses currently available" />
+					<a href="/training-directory.php.html">
+						<img src="/_img/TL0010.jpg" width="76" height="76" border="0" alt="Search our Training Directory for details of courses currently available" />
+						<h2>Training Directory</h2>
+						<p>Find online training courses or search for workshops and seminars</p>
 					</a>
-					<img src="img/tablanding_top.gif" alt="" width="185" height="10" />
-					<h2><a href="layer105b.html?topicId=1074400185" title="Search our Training Directory for details of courses currently available">Training Directory</a></h2>
-					<p>Find online training courses or search for workshops and seminars</p>
 				</td>
 			</tr>
 			<tr>
 				<td>
-					<a href="findContentBankf4d5.html?bank=CASE_STUDIES" title="Browse our library of case studies to find out how business owners and managers have tackled real-life challenges and opportunities">
-						<img src="img/find_case_study.jpg" width="76" height="76" border="0" alt="Browse our library of case studies to find out how business owners and managers have tackled real-life challenges and opportunities" />
+					<a href="/find-a-case-study.php">
+						<img src="/_img/find_case_study.jpg" width="76" height="76" border="0" alt="Browse our library of case studies to find out how business owners and managers have tackled real-life challenges and opportunities" />
+						<h2>Find a case study</h2>
+						<p>Explore the many case studies on our website through an easy browse interface</p>
 					</a>
-					<img src="img/tablanding_top.gif" alt="" width="185" height="10" />
-					<h2><a href="findContentBankf4d5.html?bank=CASE_STUDIES" title="Browse our library of case studies to find out how business owners and managers have tackled real-life challenges and opportunities">Find a case study</a></h2>
-					<p>Explore the many case studies on our website through an easy browse interface</p>
 				</td>
 				<td>
-					<a href="http://improve.businesslink.gov.uk/resources/business-awards-finder" target="bgExternalwwwbusinesslinkgovuk" title="Search our Business Awards Finder to find business recognition awards you may be eligible to apply forbafSearch - Opens in a new window">
-						<img src="img/award_finder.jpg" width="76" height="76" border="0" alt="Search our Business Awards Finder to find business recognition awards you may be eligible to apply forbafSearch" />
-						<span> - Opens in a new window</span>
+					<a href="https://www.gov.uk/growing-your-business" title="Growing your business" rel="external">
+						<img src="/_img/award_finder.jpg" width="76" height="76" border="0" alt="Search our Business Awards Finder to find business recognition awards you may be eligible to apply forbafSearch" />
+						<h2>Business awards finder</h2>
+						<p>Search our database to find awards that your business may be eligible to enter</p>
 					</a>
-					<img src="img/tablanding_top.gif" alt="" width="185" height="10" />
-					<h2><a href="http://improve.businesslink.gov.uk/resources/business-awards-finder" target="bgExternalwwwbusinesslinkgovuk" title="Search our Business Awards Finder to find business recognition awards you may be eligible to apply forbafSearch - Opens in a new window">Business awards finder<span> - Opens in a new window</span></a></h2>
-					<p>Search our database to find awards that your business may be eligible to enter</p>
 				</td>
 			</tr>
 			<tr>
 				<td>
 					<a href="detail29fc.html?itemId=1084515656&amp;type=ONEOFFPAGE" title="Links to interactive tools">
-						<img src="img/tools_wn.jpg" width="76" height="76" border="0" alt="Links to interactive tools" />
+						<img src="/_img/tools_wn.jpg" width="76" height="76" border="0" alt="Links to interactive tools" />
+						<h2>Tools directory</h2>
+						<p>Interactive tools to help you run your business</p>
 					</a>
-					<img src="img/tablanding_top.gif" alt="" width="185" height="10" />
-					<h2><a href="detail29fc.html?itemId=1084515656&amp;type=ONEOFFPAGE" title="Links to interactive tools">Tools directory</a></h2>
-					<p>Interactive tools to help you run your business</p>
 				</td>
 				<td>
-					<a href="layer21a5.html?topicId=1075193162" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies">
-						<img src="img/TL0014.jpg" width="76" height="76" border="0" alt="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies" />
+					<a href="/tools-directory.php.html">
+						<img src="/_img/TL0014.jpg" width="76" height="76" border="0" alt="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies" />
+						<h2>Do it online</h2>
+						<p>Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies</p>
 					</a>
-					<img src="img/tablanding_top.gif" alt="" width="185" height="10" />
-					<h2><a href="layer21a5.html?topicId=1075193162" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies">Do it online</a></h2>
-					<p>Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies</p>
 				</td>
 			</tr>
 		</table>
 	</div>
-
-	<span><a name="skipfooter" id="skipfooter"></a></span>
-
-	<div>
-		<div><img src="img/footer_top3.gif" alt="" />
-			<div>
-				<span>
-					<a href="https://online.businesslink.gov.uk/bdotg/action/userSupport?refPage=%2Fbdotg%2Faction%2Flayer%3FtopicId%3D1075193173" title="Help &amp; support">Help &amp; support</a> | 
-					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Copyright" title="&copy; Crown copyright 2011">Copyright</a> | 
-					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Disclaimer" title="Terms &amp; conditions">Terms &amp; conditions</a> | 
-					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Privacy" title="Privacy &amp; cookies">Privacy &amp; cookies</a> | 
-					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility" title="Accessibility">Accessibility</a> | 
-					<a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=HelpWithPDFFiles" title="PDF help">PDF help</a>
-				</span>
-			</div>
-		</div>
-	</div>
 </div>
 
-<div>
-	<div>
-		<span><a name="skiphelp" id="skiphelp"></a></span>
-		<div>
-			<div>
-				<div>
-					<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/redirectToSite">
-						<fieldset>
-							<label for="localBusinessLink">Regional Support</label>
-							<select name="Site Id">
-								<option selected="selected" value="default">Select your location</option>
-								<optgroup label='Northern Ireland'>
-								<option value='1087659111'>nibusinessinfo.co.uk</option>
-								</optgroup>
-								<optgroup label='Scotland'>
-								<option value='1087659110'>Business Gateway</option>
-								</optgroup>
-								<optgroup label='Wales'>
-								<option value='1087659109'>Welsh Government</option>
-								</optgroup>
-							</select>
-							<input type="submit" value="Go" />
-						</fieldset>
-					</form>
-				</div>
-				<div>
-					<ul>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/fullsitemap" title="Site map">Site map</a></li>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/help?page=HelpPage" title="Help">Help</a></li>
-						<li><a href="detailddbd.html?itemId=1087351667&amp;page=AboutUs&amp;r.s=h&amp;type=CAMPAIGN" title="About us">About us</a></li>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility">Accessibility</a></li>
-					</ul>
-				</div>
-				<div>
-					<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/searchBasicMode">
-						<fieldset>
-							<input type="hidden" name="resultPage" value="1"/>
-							<label for="headersearchinput">Search</label>
-							<input type="text" name="expression" />
-							<input type="submit" value="Search" title="Start the search" />
-						</fieldset>
-					</form>
-				</div>
-			</div>
-			<div>
-				<div>
-					<ul>
-						<li><a href="https://online.businesslink.gov.uk/bdotg/action/myBusiness" title="">My Business</a></li>
-						<li><a href="layer4866.html?topicId=1075193191" title="">What's new?</a></li>
-						<li><a href="layer21a5.html?topicId=1075193162" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies">Do it online</a></li>
-						<li><span>Directories</span></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-
-<?php include("inc/category-links.php"); ?>
-
-<?php include("inc/scripts.php"); ?>
-
-</body>
-</html>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
