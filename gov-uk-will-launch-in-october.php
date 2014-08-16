5af557fff9fdbbd917fd5330fdcbffc9f94a7cfd
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-16 21:54:50 +0100

    Updated "GOV.UK will launch in October" page.

--- gov-uk-will-launch-in-october.php
+++ gov-uk-will-launch-in-october.php
@@ -21,24 +21,19 @@
 			<h1>businesslink.gov.uk is changing</h1>
 			<h2>GOV.UK will launch in October</h2>
 			<p>GOV.UK will be the new home for government services and information. GOV.UK will replace businesslink.gov.uk and Directgov, making it simpler, clearer and faster to find what you need from government. Content and services may look a little different to what you are used to, but you can still expect to find your key business information and transactions.</p>
-			<p>To get an idea of how GOV.UK will work, you can use and give feedback on a trial, or 'beta', version of the website now at <a href="https://www.gov.uk/" rel="external">www.gov.uk<span> - Opens in a new window</span></a>.</p>
+			<p>To get an idea of how GOV.UK will work, you can use and give feedback on a trial, or 'beta', version of the website now at <a href="https://www.gov.uk/" rel="external">www.gov.uk</a>.</p>
 			<p>Most people will not need to take any action &ndash; if you have bookmarked pages these will redirect automatically to an appropriate page on GOV.UK. If you are a registered user of businesslink.gov.uk, or use certain services such as Contracts Finder, you will receive information by email about the action you need to take.</p>
-			<p>If you have questions about GOV.UK please refer to <a href="http://www.direct.gov.uk/businesslink-faq" rel="external">www.direct.gov.uk/businesslink-faq<span> - Opens in a new window</span></a> </p>
-			<p><img src="img/hr_325px_global.gif" alt="" /></p>
+			<p>If you have questions about GOV.UK please refer to <a href="http://www.direct.gov.uk/businesslink-faq" title="About GOV.UK" rel="external">www.direct.gov.uk/businesslink-faq</a> </p>
+			<p><img src="/_img/hr_325px_global.gif" alt="" /></p>
 			<div>
 				<ul>
-					<li>
-						<a href="#" title="Print page now - Opens in a new window">Print page now <span>- Opens in a new window</span></a>
-					</li>
-					<li>
-						<a href="#" title="Tell a friend about this page" >Tell a friend about this page</a>
-					</li>
-					<li>
-						<a href="#" title="Tell a friend about this page" >Send me an email alert when this page changes</a>
-					</li>
+					<li><a href="#">Print page now</a></li>
+					<li><a href="#">Tell a friend about this page</a></li>
+					<li><a href="#">Send me an email alert when this page changes</a></li>
 				</ul>
 			</div>
 		</div>
 	</div>
+</div>
 	
 <?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
