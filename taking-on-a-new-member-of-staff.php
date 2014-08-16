f7e10791930310ef036d60679ea00d9fb12f699d
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-16 22:15:46 +0100

    Updated "Taking on a new member of staff?" page.

--- taking-on-a-new-member-of-staff.php
+++ taking-on-a-new-member-of-staff.php
@@ -10,129 +10,36 @@
 
 <body>
 
-<?php include("inc/skip-links.php"); ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
 
-<div> <a href="home.html" title="Small Business Service home"><img src="../../syntegra/images/global-business-link-logo.png" alt="Business Link" /></a> </div>
-<div>Information. Support. Compliance.</div>
-<span><a name="skiplogin"></a></span>
 <div>
-		<p> Your account </p>
-		<ul>
-		<li> <a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate"> Register </a> </li>
-		<li> <a onclick="bLeaving=false;" href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate"> Log in </a> </li>
-	</ul>
-	</div>
-<div><img src="../../syntegra/images/px_trans.gif" alt="" width="745" height="1" /></div>
-<div >
+	<div>
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
 		<div>
-		<p><a href="home.html"  title="" >Home</a>&gt;Use our new and improved interactive tools</p>
-	</div>
-		<span><a name="skipcont"></a></span>
-		<div>
-		<div>
+			<div>
 				<h1>Taking on a new member of staff?</h1>
 				<h2>Use our new and improved interactive tools</h2>
 				<p>We have launched three new tools to help you through the process of taking on a new member of staff.</p>
-				<p>You can find the new tools on <a href="layer9c2b.html?topicId=1081476661" title="Our interactive tool can help you comply with the law when employing new staff" >a single page within the Employment and skills section of the site</a>. They deal separately with:</p>
+				<p>You can find the new tools on <a href="/employment-and-skills/take-on-staff/help-with-recruitment.php.html">a single page within the Employment and skills section of the site</a>. They deal separately with:</p>
 				<ul>
-				<li><a href="http://online.businesslink.gov.uk/bdotg/action/logicToolStart?itemId=1087845833&amp;type=BLTTOOL" title="The steps you should take when looking to hire new staff" >recruiting staff</a></li>
-				<li><a href="http://online.businesslink.gov.uk/bdotg/action/logicToolStart?itemId=1087958695&amp;type=BLTTOOL" title="Making pre-employment checks tool" >making pre-employment checks and contracts of employment</a></li>
-				<li><a href="http://online.businesslink.gov.uk/bdotg/action/logicToolStart?itemId=1088043132&amp;type=BLTTOOL" title="Getting new workers started and contracts of employment" >getting new workers started</a></li>
+				<li><a href="https://www.gov.uk/employing-staff" title="Employing staff for the first time" rel="external">recruiting staff</a></li>
+				<li><a href="https://www.gov.uk/employers-checks-job-applicants" title="Checks employers can make on job applicants" rel="external">making pre-employment checks and contracts of employment</a></li>
+				<li><a href="https://www.gov.uk/employing-staff" title="Employing staff for the first time" rel="external">getting new workers started</a></li>
 			</ul>
 				<p>Previously, a single tool attempted to cover all three stages of this process. We listened to your feedback about this tool, which told us that it contained too many questions and took too long to work through.</p>
 				<p>You also told us that the process of taking on a new member of staff could be broken down into smaller, separate stages. You wanted to know your obligations at each stage separately, without having to sift through information about the other stages at the same time.</p>
 				<p>The three shorter tools that we have developed will provide just the information that you need, when you need it.</p>
-				<p>A related tool for when you're <a href="layer37d7.html?topicId=1086156452" title="Interactive tool to help an employer go through the steps of paying someone for the first time" >paying an employee for the first time</a> can also be found at the single page within the Employment and skills section of the site.</p>
-				<p><img src="../../syntegra/images/hr_325px_global.gif" alt="" align="middle" /></p>
+				<p>A related tool for when you're <a href="/tax-payroll-and-company-information/paye-for-employers/introduction-to-paye-for-employers/paying-an-employee-for-the-first-time.php.html">paying an employee for the first time</a> can also be found at the single page within the Employment and skills section of the site.</p>
+				<p><img src="/_img/hr_325px_global.gif" alt="" align="middle" /></p>
 				<div>
 				<ul>
-						<script language="javascript" type="text/javascript">
-					<!--
-						document.write('<li><a href="#" onclick="Javascript:window.open(\'http://online.businesslink.gov.uk/bdotg/action/printpage?itemId=1096974123&amp;type=ONEOFFPAGE\', \'bgPrintable\', \'menubar=yes,width=630,height=510,resizable=yes,scrollbars=yes\')" title="Print page now  - Opens in a new window">Print page now <span> - Opens in a new window</span></a></li>');
-					//-->
-				</script>
-						<noscript>
-					<li><a href="http://online.businesslink.gov.uk/bdotg/action/printpage?itemId=1096974123&amp;type=ONEOFFPAGE" title="Print page now  - Opens in a new window" target="bgPrintable">Print page now <span> - Opens in a new window</span></a></li>
-					</noscript>
-						<li><a href="http://online.businesslink.gov.uk/bdotg/action/emailafriend?itemId=1096974123&amp;refPage=%2Fbdotg%2Faction%2Fdetail%3Fatom_id%3D0.PR362945%26itemId%3D1096974123%26pageId%3Dmynewbusiness%26type%3DONEOFFPAGE&amp;type=ONEOFFPAGE" title="Tell a friend about this page" >Tell a friend about this page</a></li>
-						<li><a href="https://online.businesslink.gov.uk/bdotg/action/subscribeAlert?alertType=ONEOFFPAGE&amp;itemId=1096974123&amp;refPage=%2Fbdotg%2Faction%2Fdetail%3Fatom_id%3D0.PR362945%26itemId%3D1096974123%26pageId%3Dmynewbusiness%26type%3DONEOFFPAGE&amp;type=ONEOFFPAGE" title="Tell a friend about this page" >Send me an email alert when this page changes</a></li>
-					</ul>
+					<li><a href="#">Print page now</a></li>
+					<li><a href="#">Tell a friend about this page</a></li>
+					<li><a href="#">Send me an email alert when this page changes</a></li>
+				</ul>
 			</div>
-			</div>
-		<div>
-				<div>
-				<div> </div>
-				<div> </div>
-			</div>
-			</div>
+		</div>
 	</div>
-		<span><a name="skipfooter"></a></span>
-		<div>
-		<div><img src="../../syntegra/images/footer_top3.gif" alt="" />
-				<div> <span><a href="https://online.businesslink.gov.uk/bdotg/action/userSupport?refPage=%2Fbdotg%2Faction%2Fdetail%3Fatom_id%3D0.PR362945%26itemId%3D1096974123%26pageId%3Dmynewbusiness%26type%3DONEOFFPAGE" title="Help &amp; support" >Help &amp; support</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Copyright" title="&copy; Crown copyright 2011" >Copyright</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Disclaimer" title="Terms &amp; conditions" >Terms &amp; conditions</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Privacy" title="Privacy &amp; cookies" >Privacy &amp; cookies</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility" title="Accessibility" >Accessibility</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=HelpWithPDFFiles" title="PDF help" >PDF help</a> </span> </div>
-			</div>
-	</div>
-	</div>
-<div>
-		<div> <span><a name="skiphelp"id="skiphelp"></a></span>
-		<div>
-				<div>
-				<div>
-						<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/redirectToSite">
-						<fieldset>
-								<label for="localBusinessLink">Regional Support</label>
-								<select name="Site Id">
-								<option selected="selected" value="default">Select your location</option>
-								<optgroup label='Northern Ireland'>
-									<option value='1087659111'>nibusinessinfo.co.uk</option>
-									</optgroup>
-								<optgroup label='Scotland'>
-									<option value='1087659110'>Business Gateway</option>
-									</optgroup>
-								<optgroup label='Wales'>
-									<option value='1087659109'>Welsh Government</option>
-									</optgroup>
-							</select>
-								<input type="submit" value="Go" />
-							</fieldset>
-					</form>
-					</div>
-				<div>
-						<ul>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/fullsitemap" title="Site map" >Sitemap</a></li>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/help?page=HelpPage" title="Help" >Help</a></li>
-						<li><a href="detail8eb8.html?itemId=1087351667&amp;page=AboutUs&amp;r.i=1096974123&amp;r.s=h&amp;r.t=ONEOFFPAGE&amp;type=CAMPAIGN" title="About us" >About us</a></li>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility" >Accessibility</a></li>
-					</ul>
-					</div>
-				<div>
-						<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/searchBasicMode">
-						<fieldset>
-								<input type="hidden" name="resultPage" value="1"/>
-								<label for="headersearchinput">Search</label>
-								<input type="text" name="expression" />
-								<input type="submit" value="Search" title="Start the search" />
-							</fieldset>
-					</form>
-					</div>
-			</div>
-				<div>
-				<div>
-						<ul>
-						<li><a href="https://online.businesslink.gov.uk/bdotg/action/myBusiness" title="" >My Business</a></li>
-						<li><a href="layer4866.html?topicId=1075193191" title="" >What's new?</a></li>
-						<li><a href="layer21a5.html?topicId=1075193162" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies" >Do it online</a></li>
-						<li><a href="layercdb4.html?topicId=1075193173" title="" >Directories</a></li>
-					</ul>
-					</div>
-			</div>
-			</div>
-	</div>
-	</div>
+</div>
 
-<?php include("inc/category-links.php"); ?>
-
-<?php include("inc/scripts.php"); ?>
-
-</body>
-</html>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
