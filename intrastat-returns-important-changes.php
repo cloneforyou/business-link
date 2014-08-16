8795762fb1c11c8e026288cd2838c839518cd83e
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-16 21:58:33 +0100

    Updated "Submitting Intrastat declarations from 1 April 2012: a reminder" page.

--- intrastat-returns-important-changes.php
+++ intrastat-returns-important-changes.php
@@ -10,128 +10,35 @@
 
 <body>
 
-<?php include("inc/skip-links.php"); ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
 
-<div> <a href="home.html" title="Small Business Service home"><img src="../../syntegra/images/global-business-link-logo.png" alt="Business Link" /></a> </div>
-<div>Information. Support. Compliance.</div>
-<span><a name="skiplogin"></a></span>
 <div>
-		<p> Your account </p>
-		<ul>
-		<li> <a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate"> Register </a> </li>
-		<li> <a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate"> Log in </a> </li>
-	</ul>
-	</div>
-<div><img src="../../syntegra/images/px_trans.gif" alt="" width="745" height="1" /></div>
-<div >
+	<div>
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
 		<div>
-		<p><a href="home.html"  title="" >Home</a>&gt;Submitting Intrastat declarations from 1 April 2012: a reminder</p>
-	</div>
-		<span><a name="skipcont"></a></span>
-		<div>
-		<div>
+			<div>
 				<h1>Intrastat returns: important changes</h1>
 				<h2>Submitting Intrastat declarations from 1 April 2012: a reminder</h2>
 				<h3>Who needs to read this?</h3>
 				<p>All businesses that provide Intrastat returns to HMRC Trade Statistics.</p>
 				<h3>What will businesses need to do from 1 April 2012?</h3>
 				<ul>
-				<li>Submit your March 2012 return by <strong>21 April 2012</strong>. From then on, all Intrastat Supplementary Declarations must be made by the 21st day of the month.</li>
-				<li>Provide all Intrastat Supplementary Declarations using electronic methods, eg online form, Comma Separated Value (CSV) file format or Electronic Data Interchange (EDI).</li>
-			</ul>
+					<li>Submit your March 2012 return by <strong>21 April 2012</strong>. From then on, all Intrastat Supplementary Declarations must be made by the 21st day of the month.</li>
+					<li>Provide all Intrastat Supplementary Declarations using electronic methods, eg online form, Comma Separated Value (CSV) file format or Electronic Data Interchange (EDI).</li>
+				</ul>
 				<h3>Where can I get more information?</h3>
-				<p>You can<a href="https://www.uktradeinfo.com/Intrastat/Pages/Intrastat.aspx" target="bgExternalonlinebusinesslinkgovuk" title="How to get started with making Intrastat declarations online" >find out more about making Intrastat declarations on the uktradeinfo website.<span> - Opens in a new window</span></a></p>
+				<p>You can <a href="https://www.uktradeinfo.com/Intrastat/Pages/Intrastat.aspx" title="How to get started with making Intrastat declarations online" rel="external">find out more about making Intrastat declarations on the uktradeinfo website.</a></p>
 				<p>You can also contact the HMRC Customer Service Helpdesk on <a href="mailto:Intrastatonline@hmrc.gsi.gov.uk"><u>Intrastatonline@hmrc.gsi.gov.uk</u></a> quoting <strong>'Intrastat 2012'</strong> or telephone 01702 367248.</p>
-				<p><img src="../../syntegra/images/hr_325px_global.gif" alt="" align="middle" /></p>
+				<p><img src="/_img/hr_325px_global.gif" alt="" align="middle" /></p>
 				<div>
 				<ul>
-						<script language="javascript" type="text/javascript">
-					<!--
-						document.write('<li><a href="#" title="Print page now  - Opens in a new window">Print page now <span> - Opens in a new window</span></a></li>');
-					//-->
-				</script>
-						<noscript>
-					<li><a href="http://online.businesslink.gov.uk/bdotg/action/printpage?itemId=1096793108&amp;type=ONEOFFPAGE" title="Print page now  - Opens in a new window" target="bgPrintable">Print page now <span> - Opens in a new window</span></a></li>
-					</noscript>
-						<li><a href="http://online.businesslink.gov.uk/bdotg/action/emailafriend?itemId=1096793108&amp;refPage=%2Fbdotg%2Faction%2Fdetail%3Fatom_id%3D0.PR370744%26itemId%3D1096793108%26type%3DONEOFFPAGE&amp;type=ONEOFFPAGE" title="Tell a friend about this page" >Tell a friend about this page</a></li>
-						<li><a href="https://online.businesslink.gov.uk/bdotg/action/subscribeAlert?alertType=ONEOFFPAGE&amp;itemId=1096793108&amp;refPage=%2Fbdotg%2Faction%2Fdetail%3Fatom_id%3D0.PR370744%26itemId%3D1096793108%26type%3DONEOFFPAGE&amp;type=ONEOFFPAGE" title="Tell a friend about this page" >Send me an email alert when this page changes</a></li>
-					</ul>
+					<li><a href="#">Print page now</a></li>
+					<li><a href="#">Tell a friend about this page</a></li>
+					<li><a href="#">Send me an email alert when this page changes</a></li>
+				</ul>
 			</div>
-			</div>
-		<div>
-				<div>
-				<div> </div>
-				<div> </div>
-			</div>
-			</div>
+		</div>
 	</div>
-		<span><a name="skipfooter"></a></span>
-		<div>
-		<div><img src="../../syntegra/images/footer_top3.gif" alt="" />
-				<div> <span><a href="https://online.businesslink.gov.uk/bdotg/action/userSupport?refPage=%2Fbdotg%2Faction%2Fdetail%3Fatom_id%3D0.PR370744%26itemId%3D1096793108%26type%3DONEOFFPAGE" title="Help &amp; support" >Help &amp; support</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Copyright" title="&copy; Crown copyright 2011" >Copyright</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Disclaimer" title="Terms &amp; conditions" >Terms &amp; conditions</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Privacy" title="Privacy &amp; cookies" >Privacy &amp; cookies</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility" title="Accessibility" >Accessibility</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=HelpWithPDFFiles" title="PDF help" >PDF help</a> </span> </div>
-			</div>
-	</div>
-	</div>
-<div>
-		<div> <span><a name="skiphelp"id="skiphelp"></a></span>
-		<div>
-				<div>
-				<div>
-						<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/redirectToSite">
-						<fieldset>
-								<label for="localBusinessLink">Regional Support</label>
-								<select name="Site Id">
-								<option selected="selected" value="default">Select your location</option>
-								<optgroup label='Northern Ireland'>
-									<option value='1087659111'>nibusinessinfo.co.uk</option>
-									</optgroup>
-								<optgroup label='Scotland'>
-									<option value='1087659110'>Business Gateway</option>
-									</optgroup>
-								<optgroup label='Wales'>
-									<option value='1087659109'>Welsh Government</option>
-									</optgroup>
-							</select>
-								<input type="submit" value="Go" />
-							</fieldset>
-					</form>
-					</div>
-				<div>
-						<ul>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/fullsitemap" title="Site map" >Sitemap</a></li>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/help?page=HelpPage" title="Help" >Help</a></li>
-						<li><a href="detailca14.html?itemId=1087351667&amp;page=AboutUs&amp;r.i=1096793108&amp;r.s=h&amp;r.t=ONEOFFPAGE&amp;type=CAMPAIGN" title="About us" >About us</a></li>
-						<li><a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility" >Accessibility</a></li>
-					</ul>
-					</div>
-				<div>
-						<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/searchBasicMode">
-						<fieldset>
-								<input type="hidden" name="resultPage" value="1"/>
-								<label for="headersearchinput">Search</label>
-								<input type="text" name="expression" />
-								<input type="submit" value="Search" title="Start the search" />
-							</fieldset>
-					</form>
-					</div>
-			</div>
-				<div>
-				<div>
-						<ul>
-						<li><a href="https://online.businesslink.gov.uk/bdotg/action/myBusiness" title="" >My Business</a></li>
-						<li><a href="layer4866.html?topicId=1075193191" title="" >What's new?</a></li>
-						<li><a href="layer21a5.html?topicId=1075193162" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies" >Do it online</a></li>
-						<li><a href="layercdb4.html?topicId=1075193173" title="" >Directories</a></li>
-					</ul>
-					</div>
-			</div>
-			</div>
-	</div>
-	</div>
+</div>
 
-<?php include("inc/category-links.php"); ?>
-
-<?php include("inc/scripts.php"); ?>
-
-</body>
-</html>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
