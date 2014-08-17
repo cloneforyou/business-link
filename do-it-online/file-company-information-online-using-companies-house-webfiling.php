bbd6b35e22f87ad6dc514c9cfac8a5b5a13edea1
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-17 14:26:53 +0100

    Completed "File company information online using Companies House WebFiling" page.

---
+++ do-it-online/file-company-information-online-using-companies-house-webfiling.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>File company information online using Companies House WebFiling | Business Link</title>
+	<meta name="author" content="Caroline Murphy">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+	<p><a href="index.php">Home</a> &gt; <a href="/do-it-online.php">Do it online</a> &gt; File company information online using Companies House WebFiling</p>
+</div>
+
+<span><a name="skipcont" id="skipcont"></a></span>
+
+<div>
+	<div>
+		<div>
+			<h1>File company information online using Companies House WebFiling</h1>
+			<h3>Before you start</h3>
+			<p>WebFiling allows you to file most information online free of charge. <a href="http://www.companieshouse.gov.uk/toolsToHelp/ourPrices.shtml" title="Companies House prices" rel="external">Find out more about Companies House fees on the Companies House website</a>.</p>
+			<p>All major credit/debit cards and PayPal are accepted. MasterCard SecureCode and Verified by Visa are also used to improve online card payment security. <a href="https://ewf.companieshouse.gov.uk/help/en/stdwf/doclist06.html" title="Companies House WebFiling" rel="external">Find out which forms can be filed online and a list of filing exceptions</a>.</p>
+			<p>This service is delivered by Companies House in conjunction with businesslink.gov.uk. To start, you will need to complete the registration process as follows:</p>
+			<ol>
+				<li>provide a valid email address</li>
+				<li>wait for an email with our security code</li>
+				<li>apply for an authentication code for each company you intend to file information on to be posted to your registered address</li>
+			</ol>
+			<p>Each time you use WebFiling, log in and enter the company number and authentication code.</p>
+			<p>Any data captured through using WebFiling is stored with the Registrar of Companies and not businesslink.gov.uk and is subject to the Companies House privacy policy. <a href="http://www.companieshouse.gov.uk/legal/termsAndConditions.shtml" title="Terms and Conditions" rel="external">Read the Companies House privacy policy on the Companies House website</a>.</p>
+			<p><a href="https://ewf.companieshouse.gov.uk//seclogin?tc=1" title="Sign in to Companies House WebFiling" rel="external">Sign in to Companies House WebFiling</a></p>
+		</div>
+		<div>
+			<dl>
+				<dt>In partnership with:</dt>
+				<dd> <a href="http://www.companieshouse.gov.uk/" rel="external"><img src="/_img/ch_big_logo_new.jpg" width="115" height="81"  alt="" />Companies House</a></dd>
+			</dl>
+		</div>
+	</div>
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
