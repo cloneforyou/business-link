a0de8c50fdaf2305ed4eab0fbedbf80f276fd7e5
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-17 14:58:28 +0100

    Completed "Submit an idea to the Better Regulation Executive" page.

---
+++ do-it-online/submit-an-idea-to-the-better-regulation-executive.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<meta charset="utf-8">	
+	<title>Submit an idea to the Better Regulation Executive | Business Link</title>
+	<meta name="author" content="Caroline Murphy">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div id="content" > 
+	
+	<!-- Page breadcrumb -->
+	<div id="breadcrumb">
+		<p><a href="index.php">Home</a> &gt; <a href="/do-it-online.php">Do it online</a> &gt; Submit an idea to the Better Regulation Executive</p>
+	</div>
+	
+	<span><a name="skipcont" id="skipcont"></a></span>
+
+	<div id="allcontent">
+		<div>
+			<div id="maincontent">
+				<h1>Submit an idea to the Better Regulation Executive</h1>
+				<h3>Submit your idea</h3>
+				<p>This page links you to the 'submit your idea' tool provided by the Better Regulation Executive.</p>
+				<p>Businesses can have a direct influence on how the regulations that affect them are devised and delivered.<br />
+					All you have to do is submit your proposal outlining how a certain regulation can be improved. Suggestions can include cutting the amount of time you spend filling in forms, ideas on where regulations that overlap or contradict can be rationalised or regulations, which in your view, are unnecessary.</p>
+				</p>
+				<div id="startTrans">
+					<a href="https://www.gov.uk/government/policies/reducing-the-impact-of-regulation-on-business" title="Reducing the impact of regulation on business" rel="external">Submit an idea to the BRE</a>
+				</div>
+			</div>
+			<div id="additionalcontent">
+				<div id="developedWithBox">
+					<dl>
+						<dt>In partnership with:</dt>
+						<dd><a href="#"><img src="/_img/logo_primary_bis.gif" width="160" height="32" alt="" />Department for Business, Innovation &amp; Skills</a></dd>
+					</dl>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
