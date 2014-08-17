243fe5cdc677931435baf07c270b9d7d84f4d977
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-17 14:21:44 +0100

    Completed "Apply to register a trade mark" page.

---
+++ do-it-online/apply-to-register-a-trademark.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<meta charset="utf-8">	
+	<title>Apply to register a trade mark | Business Link</title>
+	<meta name="author" content="Caroline Murphy">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div id="content" > 
+	
+	<!-- Page breadcrumb -->
+	<div id="breadcrumb">
+		<p><a href="index.php">Home</a> &gt; <a href="/do-it-online.php">Do it online</a> &gt; Apply to register a trade mark</p>
+	</div>
+
+	<span><a name="skipcont" id="skipcont"></a></span>
+
+	<div id="allcontent">
+		<div>
+			<div id="maincontent">
+				<h1>Apply to register a trade mark</h1>
+				<p>Once you have chosen a trade mark you would like to use for your business, goods or services, you should apply to register it with the Intellectual Property Office. Remember that your trade mark may not be accepted if it describes your goods and services in any way. Once registered, your trade mark will be legally protected within the UK for a period of 10 years.</p>
+				<p>It is advisable to carry out a search for identical and similar trade marks before applying. You may apply to register a trade mark that is the same as, or similar to, an existing one, but the owner may oppose the new application.</p>
+				<p>Trade marks also need to be classified by the specific goods and services they will be applied to. A full list of goods and services, together with the appropriate class numbers, is available on the Intellectual Property Office website.</p>
+				<p>To apply to register your trade mark, complete the online application form on the Intellectual Property Office website by clicking the button below. Please note there is a fee to pay which will vary according to the filing option you choose. (All major credit/debit cards are accepted. Mastercard Secure Pay and Verified by Visa are used to improve online card payment security.)</p>
+				<p>While completing the online form, you will be able to save and return to it. Please note the service is unavailable between 00.15 and 03.15 every morning.</p>
+				<div id="startTrans">
+					<a href="https://www.ipo.gov.uk/tm3-online/CompleterTypeQuestion/Edit" title="Intellectual Property Office - TM3 On-line form" rel="external">Apply to register a trade mark</a>
+				</div>
+			</div>
+			<div id="additionalcontent">
+				<div id="developedWithBox">
+					<dl>
+						<dt>In partnership with:</dt>
+						<dd> <a href="http://www.ipo.gov.uk" rel="external"><img src="/_img/IPO_LOGO.gif" width="150" height="26" alt="" />Intellectual Property Office</a></dd>
+					</dl>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
