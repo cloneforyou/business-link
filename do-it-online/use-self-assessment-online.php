885674d2543cb10479fda09105b4d440f6389a32
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-17 15:21:35 +0100

    Completed "Use Self Assessment Online" page.

---
+++ do-it-online/use-self-assessment-online.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<meta charset="utf-8">	
+	<title>Use Self Assessment Online | Business Link</title>
+	<meta name="author" content="Caroline Murphy">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div id="content" > 
+	
+	<!-- Page breadcrumb -->
+	<div id="breadcrumb">
+		<p><a href="index.php">Home</a> &gt; <a href="/do-it-online.php">Do it online</a> &gt; Use Self Assessment Online</p>
+	</div>
+	
+	<span><a name="skipcont" id="skipcont"></a></span>
+
+	<div id="allcontent">
+		<div>
+			<div id="maincontent">
+				<h1>Use Self Assessment Online</h1>
+				<p>Self Assessment Online is provided by HM Revenue &amp; Customs (HMRC) in partnership with Business Link.</p>
+				<div id="startTrans">
+					<a href="#">Log in or register for Self Assessment Online</a>
+				</div>
+				<p>If you are a tax adviser or accountant please use <a href="/do-it-online/use-self-assessment-for-agents.php.html">Self Assessment for agents</a>.</p>
+				<h3>Service availability</h3>
+				<p><a href="http://customs.hmrc.gov.uk/channelsPortalWebApp/channelsPortalWebApp.portal?_nfpb=true&_pageLabel=pageOnlineServices_ShowContent&propertyType=document&id=HMCE_MIG_009921#P155_10104" title="Service availability" rel="external">Check the availability of HMRC's Online Services<span> - Opens in a new window</span></a>.</p>
+				<h3>Related guidance</h3>
+				<p>See our section on <a href="/tax-payroll-and-company-information/self-assessment.php.html">Self Assessment</a>.</p>
+				<h3>Disclaimer</h3>
+				<p>Please note that HMRC's terms and conditions will apply when using Self Assessment Online.</p>
+			</div>
+			<div id="additionalcontent">
+				<div id="developedWithBox">
+					<dl>
+						<dt>In partnership with:</dt>
+						<dd><a href="http://www.hmrc.gov.uk" rel="external"><img src="/_img/HMRC_logo.gif" width="134" height="35"  alt="" />HM Revenue &amp; Customs</a></dd>
+					</dl>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
