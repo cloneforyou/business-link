0ec5251b580a2dd6d430ea88bc37aea8c7cc6a72
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-17 14:32:09 +0100

    Completed "Incorporate your company online" page.

---
+++ do-it-online/incorporate-your-company-online.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Incorporate your company online | Business Link</title>
+	<meta name="author" content="Caroline Murphy">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+<p><a href="index.php">Home</a> &gt; <a href="/do-it-online.php">Do it online</a> &gt; Incorporate your company online</p>
+
+<a name="skipcont"></a>
+
+<div>
+	<h1>Incorporate your company online</h1>
+	<p>This service enables you to incorporate your company online as a private company, limited by shares, with model articles of association.</p>
+	<p>For explanations of company types and how to incorporate, see our guide on <a href="/starting-up/business-structures/legal-structures-the-basics.php.html">legal structures: the basics</a>.</p>
+	<p>To use the service, you will need to register with your email address and a password of your choice.</p>
+	<p>You will then need the following information to complete the form:</p>
+	<ol>
+		<li>Company name and address</li>
+		<li>Details of directors and secretary (optional)</li>
+		<li>Details of each shareholder and the value of their shares</li>
+		<li>Payment details -<a href="http://www.companieshouse.gov.uk/toolsToHelp/ourPrices.shtml" title="Companies House prices" rel="external">find out more about Companies House fees on the Companies House website</a></li>
+	</ol>
+	<p>Any data captured through online incorporation is stored with the Registrar of Companies and not businesslink.gov.uk and is subject to the <a href="http://www.companieshouse.gov.uk/legal/termsAndConditions.shtml" title="Terms and Conditions" rel="external">Companies House privacy policy</a>.</p>
+	<p>This service is delivered by Companies House in conjunction with businesslink.gov.uk.</p>
+	<p><a href="#">Incorporate with Companies House now</a></p>
+	<div>
+		<dl>
+			<dt>In partnership with:</dt>
+			<dd><a href="http://www.companieshouse.gov.uk/" rel="external"><img src="/_img/ch_big_logo_new.jpg" width="115" height="81" alt="" />Companies House</a></dd>
+		</dl>
+	</div>
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
