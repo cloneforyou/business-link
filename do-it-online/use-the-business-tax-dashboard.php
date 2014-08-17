43f50873d3ad91438d7e1cb85c0d5b5f80ebeb96
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-17 15:24:22 +0100

    Completed "Use the Business Tax Dashboard" page.

---
+++ do-it-online/use-the-business-tax-dashboard.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<meta charset="utf-8">	
+	<title>Use the Business Tax Dashboard | Business Link</title>
+	<meta name="author" content="Caroline Murphy">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div id="content" > 
+	
+	<!-- Page breadcrumb -->
+	<div id="breadcrumb">
+		<p><a href="index.php">Home</a> &gt; <a href="/do-it-online.php">Do it online</a> &gt; Use the Business Tax Dashboard</p>
+	</div>
+	<span><a name="skipcont" id="skipcont"></a></span>
+
+	<div id="allcontent">
+		<div>
+			<div id="maincontent">
+				<h1>Use the Business Tax Dashboard</h1>
+				<p>The Business Tax Dashboard is provided by HM Revenue &amp; Customs (HMRC) in partnership with Business Link.</p>
+				<p>You can use the Business Tax Dashboard create a single view of your payments and liabilities for these taxes:</p>
+				<ul>
+					<li>VAT</li>
+					<li>Corporation Tax</li>
+					<li>PAYE</li>
+					<li>Self Assessment</li>
+				</ul>
+				<div id="startTrans">
+					<a href="http://www.hmrc.gov.uk/online/dashboard.htm" title="Using a Business Tax Dashboard" rel="external">Set up a Business Tax Dashboard</a>
+				</div>
+				<h3>Service availability</h3>
+				<p><a href="http://customs.hmrc.gov.uk/channelsPortalWebApp/channelsPortalWebApp.portal?_nfpb=true&_pageLabel=pageOnlineServices_ShowContent&propertyType=document&id=HMCE_MIG_009921#P155_10104" title="Service availability" rel="external">Check the availability of HMRC's Online Services</a>.</p>
+				<h3>Disclaimer</h3>
+				<p>Please note that HMRC's terms and conditions will apply when using the Business Tax Dashboard.</p>
+			</div>
+			<div id="additionalcontent">
+				<div id="developedWithBox">
+					<dl>
+						<dt>In partnership with:</dt>
+						<dd><a href="http://www.hmrc.gov.uk" rel="external"><img src="/_img/HMRC_logo.gif" width="134" height="35"  alt="" />HM Revenue &amp; Customs</a></dd>
+					</dl>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
