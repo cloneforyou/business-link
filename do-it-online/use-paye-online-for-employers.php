5185aae755375ba4ab48440f738b79014a4665e4
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-17 15:17:26 +0100

    Completed "Use PAYE Online for employers" page.

---
+++ do-it-online/use-paye-online-for-employers.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Use PAYE Online for employers | Business Link</title>
+	<meta name="author" content="Caroline Murphy">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div  >
+	<p><a href="index.php">Home</a> &gt; <a href="/do-it-online.php">Do it online</a> &gt; Use PAYE Online for employers</p>
+
+	<a name="skipcont" id="skipcont"></a>
+	
+	<div>
+		<h1>Use PAYE Online for employers</h1>
+		<p>PAYE Online for employers is provided by HM Revenue &amp; Customs (HMRC) in partnership with Business Link.</p>
+		<p><a href="#">Log in or register for PAYE Online</a></p>
+		<p>If you are a tax adviser or accountant please <a href="/do-it-online/use-paye-for-agents.php.html">use PAYE Online for agents</a>.</p>
+		<h3>Service availability</h3>
+		<p><a href="http://customs.hmrc.gov.uk/channelsPortalWebApp/channelsPortalWebApp.portal?_nfpb=true&_pageLabel=pageOnlineServices_ShowContent&propertyType=document&id=HMCE_MIG_009921#P155_10104" title="Service availability" rel="external">Check the availability of HMRC's Online Services</a>.</p>
+		<h3>Related guidance</h3>
+		<p>See our section on <a href="/tax-payroll-and-company-information/paye-for-employers.php.html">PAYE for employers</a>.</p>
+		<h3>Disclaimer</h3>
+		<p>Please note that HMRC's terms and conditions will apply when using PAYE Online for employers.</p>
+		<div>
+			<dl>
+				<dt>In partnership with:</dt>
+				<dd><a href="http://www.hmrc.gov.uk" rel="external"><img src="/_img/HMRC_logo.gif" width="134" height="35" alt="" />HM Revenue &amp; Customs</a></dd>
+			</dl>
+		</div>
+	</div>
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
