248abc4f2a38ec453fa441968b809d7bd6a5e723
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-17 14:55:28 +0100

    Completed "Search for government opportunities on Contracts Finder" page.

---
+++ do-it-online/search-for-government-opportunities-on-contracts-finder.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<meta charset="utf-8">	
+	<title>Search for government opportunities on Contracts Finder | Business Link</title>
+	<meta name="author" content="Caroline Murphy">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div id="content" > 
+	
+	<!-- Page breadcrumb -->
+	<div id="breadcrumb">
+		<p><a href="index.php">Home</a> &gt; <a href="/do-it-online.php">Do it online</a> &gt; Search for government opportunities on Contracts Finder</p>
+	</div>
+	
+	<span><a name="skipcont" id="skipcont"></a></span>
+
+	<div id="allcontent">
+		<div>
+			<div id="maincontent">
+				<h1>Search for government opportunities on Contracts Finder</h1>
+				<p>Contracts Finder is a <strong>free</strong> new service from businesslink.gov.uk. You can use it to search for procurement opportunities with government, including <strong>lower-value contracts</strong> worth &pound;10,000 and above.</p>
+				<p>Government departments and their agencies are using Contracts Finder to publish live opportunities, as well as tender and contract documents for closed procurements. Many local authorities and other public sector bodies will join them in the future.</p>
+				<p>To find out what you can do with Contracts Finder, see our guide on how to <a href="/grow-your-business/suppliers-overview-of-tendering-for-contracts/find-government-opportunities-tenders-and-contracts-on-contracts-finder.php.html" title="How to use this free service to find government opportunities, tenders and contract documents " >find government opportunities, tenders and contracts on Contracts Finder</a>.</p>
+				<p>You can use Contracts Finder to search or browse for live opportunities, closed tender or contract documents, or both. Registered users can also sign up for <strong>email alerts</strong> when new documents are published.</p>
+				<div id="startTrans">
+					<a href="https://online.contractsfinder.businesslink.gov.uk/?xgovk3w=bl1000&xgovf0p=%7Cxgovs9k=biscf%7Cxgovr3h=cftx1%7Cxgovc8h=1000%7Cxgovk3w=bl1000%7C&xgovd2v=en&xgovj6d=0a80a437cd657c4ee8f025782ccc5264e9522f94" rel="external">Start using Contracts Finder</a>
+				</div>
+			</div>
+			<div id="additionalcontent">
+				<div id="developedWithBox">
+					<dl>
+						<dt>In partnership with:</dt>
+						<dd><a href="https://www.gov.uk/government/organisations/cabinet-office" rel="external"><img src="/_img/cabinet_office.gif" width="84" height="69" alt="Cabinet Office" />Cabinet Office</a></dd>
+					</dl>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
