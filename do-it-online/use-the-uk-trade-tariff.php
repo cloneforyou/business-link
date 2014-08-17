885d878282f624292bb9e270f3b5addde0ce3038
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-17 15:26:28 +0100

    Completed "Use the UK Trade Tariff" page.

---
+++ do-it-online/use-the-uk-trade-tariff.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<meta charset="utf-8">	
+	<title>Use the UK Trade Tariff | Business Link</title>
+	<meta name="author" content="Caroline Murphy">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div id="content" > 
+	
+	<!-- Page breadcrumb -->
+	<div id="breadcrumb">
+		<p><a href="index.php">Home</a> &gt; <a href="/do-it-online.php">Do it online</a> &gt; Use the UK Trade Tariff</p>
+	</div>
+	
+	<span><a name="skipcont" id="skipcont"></a></span>
+
+	<div id="allcontent">
+		<div>
+			<div id="maincontent">
+				<h1>Use the UK Trade Tariff</h1>
+				<p>This page links you to the UK Trade Tariff where you can:</p>
+				<ul>
+					<li>browse and search commodities allowing you to classify goods and quickly find detailed information such as duty rates, VAT and excise rates, preferential agreements, government controls and explanatory notes</li>
+					<li>register and bookmark your most used headings, chapters and commodity codes allowing you to find them easily the next time you come to the site</li>
+					<li>subscribe to the Tariff email alerts, saving you time by telling you whenever any information related to your Tariff bookmarks has been updated or changed</li>
+					<li>create downloadable PDFs, making management and storage of the information you need easier</li>
+					<li>find additional information such as guides, helplines and other contact details</li>
+				</ul>
+				<div id="startTrans">
+					<p><a href="https://www.gov.uk/trade-tariff" title="Trade Tariff" rel="external">Use the UK Trade Tariff</a></p>
+				</div>
+			</div>
+			<div id="additionalcontent">
+				<div id="developedWithBox">
+					<dl>
+						<dt>In partnership with:</dt>
+						<dd><a href="http://www.hmrc.gov.uk" rel="external"><img src="/_img/HMRC_logo.gif" width="134" height="35" alt="" />HM Revenue &amp; Customs</a></dd>
+					</dl>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
