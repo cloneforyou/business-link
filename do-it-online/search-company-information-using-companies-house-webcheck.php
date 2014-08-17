2c4b7b4468f13c202c64a1eb98e9b63b0a0a9504
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-17 14:40:00 +0100

    Completed "Search company information using Companies House WebCHeck" page.

---
+++ do-it-online/search-company-information-using-companies-house-webcheck.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Search company information using Companies House WebCHeck | Business Link</title>
+	<meta name="author" content="Caroline Murphy">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div id="content" >
+	<div id="breadcrumb">
+		<p><a href="index.php">Home</a> &gt; <a href="/do-it-online.php">Do it online</a> &gt; Search company information using Companies House WebCHeck</p>
+	</div>
+
+	<span><a name="skipcont" id="skipcont"></a></span>
+
+	<div id="allcontent">
+		<div>
+			<div id="maincontent">
+				<h1>Search company information using Companies House WebCHeck</h1>
+				<h3>Before you start</h3>
+				<p>WebCHeck allows you to search company details and view filing histories for all limited companies in the UK.</p>
+				<p>You can order documents and reports for a small fee or monitor a company free of charge, receiving email alerts of new documents filed by them at Companies.<a href="http://www.companieshouse.gov.uk/toolsToHelp/ourPrices.shtml" title="Companies House prices" rel="external">Find out more about Companies House fees on the Companies House website</a>.</p>
+				<p>Any data captured through using WebCHeck is stored with the Registrar of Companies and not businesslink.gov.uk and is subject to the Companies House privacy policy. <a href="http://www.companieshouse.gov.uk/legal/termsAndConditions.shtml" title="Terms and Conditions" rel="external">Read the Companies House privacy policy on the Companies House website</a>.</p>
+				<p>From the Companies House website, you can return to related information, tools and transactions on businesslink.gov.uk.</p>
+				<div id="startTrans">
+					<a href="http://wck2.companieshouse.gov.uk//wcframe?name=accessCompanyInfo" title="WebCHeck - Select and Access Company Information">Go to Companies House WebCHeck</a>
+				</div>
+			</div>
+			<div id="additionalcontent">
+				<div id="developedWithBox">
+					<dl>
+						<dt>In partnership with:</dt>
+						<dd><a href="http://www.companieshouse.gov.uk" rel="external"><img src="/_img/ch_big_logo_new.jpg" width="115" height="81" alt="" />Companies House</a></dd>
+					</dl>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
