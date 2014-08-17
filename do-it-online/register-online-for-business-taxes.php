85b845b2dd13e4f041d5bb1bb923e3e6f96f828b
Caroline Murphy <cmurphydesign@gmail.com>
2014-08-17 14:36:19 +0100

    Completed "Register online for business taxes" page.

---
+++ do-it-online/register-online-for-business-taxes.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<meta charset="utf-8">	
+	<title>Register online for business taxes | Business Link</title>
+	<meta name="author" content="Caroline Murphy">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div id="content" > 
+	
+	<!-- Page breadcrumb -->
+	<div id="breadcrumb">
+		<p><a href="index.php">Home</a> &gt; <a href="/do-it-online.php">Do it online</a> &gt; Register online for business taxes</p>
+	</div>
+	
+	<span><a name="skipcont" id="skipcont"></a></span>
+
+	<div id="allcontent">
+		<div>
+			<div id="maincontent">
+				<h1>Register online for business taxes</h1>
+				<p>HM Revenue and Customs (HMRC) in partnership with Business Link provide an online tax registration service. The service is aimed at small businesses who need to tell HMRC they are a new business, new employer, or new to VAT.</p>
+				<p>You can register for the key taxes that are relevant to your business and be enrolled for the HMRC Online Services at the same time:</p>
+				<ul>
+					<li>Corporation Tax</li>
+					<li>Self Assessment</li>
+					<li>PAYE for employers</li>
+					<li>VAT</li>
+				</ul>
+				<div id="startTrans">
+					<a href="#">Register for business taxes using the online tax registration service</a>
+				</div>
+				<p><strong>Important Note</strong> &ndash; If you have already told HMRC that you have started in business and have received your reference number, you do not need to do so again. If you have a Self Assessment or Corporation Tax Unique Taxpayer Reference (UTR), a VAT registration number or Employer reference you can sign up to use HMRC Online Services to deal with HMRC and file your tax return online.</p>
+				<p><a href="#">Sign up for HMRC Online Services</a></p>
+				<h3>Service availability</h3>
+				<p><a href="http://customs.hmrc.gov.uk/channelsPortalWebApp/channelsPortalWebApp.portal?_nfpb=true&_pageLabel=pageOnlineServices_ShowContent&propertyType=document&id=HMCE_MIG_009921#P155_10104" title="Service availability" rel="external">Check the availability of HMRC's Online Services</a>.</p>
+				<h3>Disclaimer</h3>
+				<p>Please note that HMRC's terms and conditions will apply when using the Online Tax Registration Service.</p>
+			</div>
+			<div id="additionalcontent">
+				<div id="developedWithBox">
+					<dl>
+						<dt>In partnership with:</dt>
+						<dd> <a href="http://www.hmrc.gov.uk" rel="external"><img src="/_img/HMRC_logo.gif" width="134" height="35" alt="" />HM Revenue &amp; Customs</a></dd>
+					</dl>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
