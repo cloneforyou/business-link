7e4dab478d8359b186daa4b01eaa06b995dca92e
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2015-12-26 23:26:15 +0000

    Completed "Buy or sell a business" page

---
+++ buy-or-sell-a-business.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Buy or sell a business | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div>
+		<h1>Buy or sell a business</h1>
+		<h2>Acquiring a business</h2>
+		<ul>
+			<li><a href="buy-or-sell-a-business/considering-buying-a-business.html">Considering buying a business</a></li>
+			<li><a href="buy-or-sell-a-business/buying.php.html">Buying</a></li>
+			<li><a href="buy-or-sell-a-business/franchising.php.html">Franchising</a></li>
+			<li><a href="buy-or-sell-a-business/mergers-and-aquisitions.html">Mergers and acquisitions</a></li>
+		</ul>
+		<h2>Selling your business</h2>
+		<ul>
+			<li><a href="buy-or-sell-a-business/considering-selling-a-business.html">Considering selling a business</a></li>
+			<li><a href="buy-or-sell-a-business/selling.html">Selling</a></li>
+			<li><a href="buy-or-sell-a-business/floating-on-the-stock-market.html">Floating on the stock market</a></li>
+			<li><a href="buy-or-sell-a-business/tax-considerations-when-selling.html">Tax considerations when selling</a></li>
+		</ul>
+		<h2>Exiting and transferring your business</h2>
+		<ul>
+			<li><a href="buy-or-sell-a-business/businesses-in-difficulty.html">Businesses in difficulty</a></li>
+			<li><a href="buy-or-sell-a-business/transferring-your-business.html">Transferring your business</a></li>
+			<li><a href="buy-or-sell-a-business/closing-a-business-checklist.html">Closing a business checklist</a></li>
+		</ul>
+		<p><a href="#">Buy or sell a business - site map</a></p>
+	</div>
+	<div>
+		<h3>Spotlight</h3>
+		<div>
+			<h4><a href="buy-or-sell-a-business/transferring-your-business/succession-planning-and-business-transfer.html" title="How effective succession planning can create the best outcome for you and your business">Future perfect</a></h4>
+			
+			<p>Explore your succession planning options</p>
+		</div>
+		<a href="buy-or-sell-a-business/transferring-your-business/succession-planning-and-business-transfer.html" title="How effective succession planning can create the best outcome for you and your business">
+			<img src="_img/Futureperfect.jpg" width="76" height="75" alt="How effective succession planning can create the best outcome for you and your business" />
+		</a>
+		<div>
+			<h4><a href="buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business.html" title="Setting a price, building a marketing proposition and identifying possible buyers for your business">Value and market your business</a></h4>
+			
+			<p>How to attract the right buyer for your business</p>
+		</div>
+		<a href="buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business.html" title="Setting a price, building a marketing proposition and identifying possible buyers for your business">
+			<img src="_img/Value.jpg" width="76" height="75" alt="Setting a price, building a marketing proposition and identifying possible buyers for your business" />
+		</a>
+	</div>
+	<h3>Case studies</h3>
+	<div>
+		<p>How I obtained the right information on a business before buying it</p>
+		
+		<ul>
+			<li><a href="buy-or-sell-a-business/considering-selling-a-business/here-is-how-i-obtained-the-right-information-about-a-business-before-buying-it.html" title="In this case study Paul Kennedy explains how he obtained the right information about a business before buying it">Here's how I obtained the right information about a business before buying it</a></li>
+		</ul>
+	</div>
+	<img src="_img/PaulKennedy.jpg" width="76" height="76" alt="" />
+	<h3>Do it online</h3>
+	<ul>
+		<li><a href="buy-or-sell-a-business/closing-a-business-checklist.html" title="This interactive tool tells you actions you need to take when exiting a business">Find out what you need to do when closing a business</a></li>
+		<li><a href="tax-payroll-and-company-information/selling-or-closing-your-business/check-tax-and-legal-issues-when-selling-a-business.html" title="Interactive tool to guide you through the main tax, VAT, employment and licensing implications when you sell your business">Check tax &amp; legal issues when selling a business</a></li>
+		<li><a href="do-it-online/search-company-information-using-companies-house-webcheck.php" title="Search company details and view filing histories for all limited companies in the UK">Search company information using the WebCHeck service at Companies House</a></li>
+	</ul>
+	<h3>What's popular</h3>
+	<ul>
+		<li><a href="buy-or-sell-a-business/buying/buy-an-existing-business.php" title="Advantages and disadvantages of buying an existing business including how to assess and value it">Buy an existing business</a></li>
+		<li><a href="tax-payroll-and-company-information/selling-or-closing-your-business/selling-or-closing-your-business-an-overview.html" title="Having an exit strategy may be the best way to ensure a pain-free sale or liquidation">Selling or closing your business - an overview</a></li>
+		<li><a href="buy-or-sell-a-business/considering-selling-a-business/understanding-contracts-when-buying-or-selling-a-business.html" title="Get expert advice to help you understand the many contracts involved in buying or selling a business">Understanding contracts when buying or selling a business</a></li>
+		<li><a href="employment-and-skills/redundancy-restructures-and-change/responsibilities-to-employees-if-you-buy-or-sell-a-business.html" title="TUPE regulations and your employees' rights if they are transferred into or out of your business">Responsibilities to employees if you buy or sell a business</a></li>
+	</ul>
+	<h3>Directories</h3>
+	<ul>
+		<li><a href="http://www.businesslink.gov.uk/bdotg/action/event?topicId=1073861225" title="National and local events, seminars and workshops in your area to help you learn or network">Buy or sell a business events near you</a></li>
+		<li><a href="contentBankByThemea3b1.html?bank=CASE_STUDIES&amp;theme=sellingabusiness&amp;topicId=1073861225">Buy or sell a business case studies</a></li>
+	</ul>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
