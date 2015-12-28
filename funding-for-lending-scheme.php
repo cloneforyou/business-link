45b8f13a868ec80a38a0ced95579ee2d527364c4
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2015-12-28 16:34:15 +0000

    Completed "Funding for Lending Scheme" page.

--- funding-for-lending-scheme.php
+++ funding-for-lending-scheme.php
@@ -17,19 +17,12 @@
 	<h2>Launched by the Bank of England and HM Treasury </h2>
 	<p>The Bank of England and HM Treasury launched the Funding for Lending Scheme (FLS) on 13 July.  It is designed to boost lending to households and businesses.  It works by allowing banks and building societies to borrow from the Bank of England for up to 4 years.  As security against that lending, banks will provide assets, such as business or mortgage loans, to the Bank of England.  Banks will be able to borrow during the 18 months from 1 August 2012 until 31 January 2014.</p>
 	<p>Banks will have strong incentives to boost lending, by lowering interest rates and increasing the availability of business loans and mortgages. The more that they lend, the more they can borrow from the Bank of England.  Banks that are increasing their lending will pay the lowest fee on their borrowing while those that reduce their lending they will pay a higher fee.  The Bank of England will publish data on the usage of the FLS along with lending data from the participants on a quarterly basis.</p>
-	<p><a href="http://www.bankofengland.co.uk/markets/Pages/FLS/default.aspx" target="bgExternalonlinebusinesslinkgovuk"   >Further information on the Funding for Lending Scheme is available on the Bank of England website - Opens in a new window</a>.</p>
-	<p><img src="_img/hr_325px_global.gif" alt=""  /></p>
+	<p><a href="http://www.bankofengland.co.uk/markets/Pages/FLS/default.aspx" target="_blank">Further information on the Funding for Lending Scheme is available on the Bank of England website - Opens in a new window</a>.</p>
+	<p><img src="_img/hr_325px_global.gif" alt="" /></p>
 	<ul>
-		<script language="javascript" type="text/javascript">
-					<!--
-						document.write('<li><a href="#"  title="Print page now  - Opens in a new window">Print page now <span> - Opens in a new window</span></a></li>');
-					//-->
-				</script>
-		<noscript>
-		<li><a href="http://online.businesslink.gov.uk/bdotg/action/printpage?itemId=1097595998&amp;type=ONEOFFPAGE" title="Print page now  - Opens in a new window" target="bgPrintable">Print page now - Opens in a new window</a></li>
-		</noscript>
-		<li><a href="http://online.businesslink.gov.uk/bdotg/action/emailafriend?itemId=1097595998&amp;refPage=%2Fbdotg%2Faction%2Fdetail%3FitemId%3D1097595998%26type%3DONEOFFPAGE&amp;type=ONEOFFPAGE" title="Tell a friend about this page">Tell a friend about this page</a></li>
-		<li><a href="https://online.businesslink.gov.uk/bdotg/action/subscribeAlert?alertType=ONEOFFPAGE&amp;itemId=1097595998&amp;refPage=%2Fbdotg%2Faction%2Fdetail%3FitemId%3D1097595998%26type%3DONEOFFPAGE&amp;type=ONEOFFPAGE" title="Tell a friend about this page">Send me an email alert when this page changes</a></li>
+		<li><a href="#">Print page now</a></li>
+		<li><a href="#">Tell a friend about this page</a></li>
+		<li><a href="#">Send me an email alert when this page changes</a></li>
 	</ul>
 </div>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
