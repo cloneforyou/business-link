b4b8aa534224f252931ce90525caae3f661df59c
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2015-12-28 17:03:35 +0000

    Completed "Green economy for business" page.

---
+++ green-economy-for-business.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>The government's new publication on the green economy  | Business Link</title>
+<meta name="author" content="Caroline Murphy">
+<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<h1>Green economy for business</h1>
+	<h2>The government's new publication on the green economy </h2>
+	<p>'<strong>Enabling the Transition to a Green Economy: government and business working together</strong>' is a new publication which sets out what the transition to a green economy means for businesses.</p>
+	<p>It is the government's response to requests from the private sector for greater clarity on what government means by a &quot;green economy&quot;, the policies being put in place to achieve this and how they come together.</p>
+	<p>The full suite of information available includes:</p>
+	<ul>
+		<li>the government's vision - <a href="_files/enabling_the_transition_to_a_green_economy.pdf" target="_blank">download the government's vision for Enabling the transition to a Green Economy (PDF, 582K) - Opens in a new window</a></li>
+		<li>a timeline for the key policies and investments - <a href="_files/green_economy_policy_timeline.pdf" target="_blank">download the policy timeline for Enabling the transition to a Green Economy (PDF, 292K) - Opens in a new window</a> or <a href="_files/green_economy_approximate_investments_timeline.pdf" target="_blank">download the approximate investments timeline for Enabling the transition to a Green Economy (PDF, 247K) - Opens in a new window</a></li>
+		<li>a document for SMEs on what the transition means for them - <a href="_files/the-move-to-a-green-economy-a-guide-for-small-business.pdf" target="_blank">download a guide for small businesses on Enabling the transition to a Green Economy (PDF, 278K) - Opens in a new window</a></li>
+		<li>3 case studies highlighting how the chemicals, food and drink and automotive sectors are responding to the issues of moving to a green economy - <a href="_files/enabling-the-transition-to-a-green-economy-government-and-business-working-together.pdf" target="_blank">download guidance on Enabling the transition to a Green Economy for the chemicals industry (PDF, 279K) - Opens in a new window</a>, <a href="_files/enabling_the_transition_to_a_green_economy__the__1.pdf" target="_blank">download guidance on Enabling the transition to a Green Economy for the food and drink industry (PDF, 255K) - Opens in a new window</a> or <a href="_files/enabling_the_transition_to_a_green_economy__the_tr-2.pdf" target="_blank">download guidance on Enabling the transition to a Green Economy for the automotive industry (PDF, 408K) - Opens in a new window</a></li>
+	</ul>
+	<p>You can also see our new guides on:</p>
+	<ul>
+		<li><a href="enabling-the-transition-to-a-green-economy-the-overarching-framework.php.html" title="An overview of how the UK will move towards a green economy">Enabling the Transition to a Green Economy - the overarching framework</a></li>
+		<li><a href="enabling-the-transition-to-a-green-economy-uk-climate-change-and-atmospheric-emissions-framework.php.html" title="An overview of the legislation that regulates UK atmospheric emissions and the targets that have been set">Enabling the Transition to a Green Economy - UK climate change and atmospheric emissions framework</a></li>
+		<li><a href="enabling-the-transition-to-a-green-economy-a-resource-smart-economy.php.html" title="How UK business can benefit from the efficient use of natural resources">Enabling the Transition to a Green Economy - a resource smart economy</a></li>
+		<li><a href="enabling-the-transition-to-a-green-economy-a-resource-smart-economy.html" title="How to unlock green growth through innovation, access to finance, skills and changes to UK infrastructure">Enabling the Transition to a Green Economy - unlocking green growth</a></li>
+		<li><a href="enabling-the-transition-to-a-green-economy-opportunities-for-businesses.html" title="The opportunities that a green economy will offer businesses">Enabling the Transition to a Green Economy - opportunities for businesses</a></li>
+		<li><a href="enabling-the-transition-to-a-green-economy-challenges-for-businesses.html" title="The challenges facing businesses as a result of climate change and other environmental policies">Enabling the Transition to a Green Economy - challenges for businesses</a></li>
+	</ul>
+	<p><img src="_img/hr_325px_global.gif" alt=""  /></p>
+	<ul>
+		<li><a href="#">Print page now</a></li>
+		<li><a href="#">Tell a friend about this page</a></li>
+		<li><a href="#">Send me an email alert when this page changes</a></li>
+	</ul>
+</div>
+<div>
+	<table  cellpadding="0" cellspacing="0">
+		<tr>
+			<td><img src="_img/globe_oneoff.jpg" width="181" height="121"  alt="" /></td>
+		</tr>
+	</table>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
