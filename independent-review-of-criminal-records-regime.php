1a7c6a701898cdf813f0a84b8b013a33d4bd8bba
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-01-01 20:11:05 +0000

    Completed "independent review of criminal records regime" page.

---
+++ independent-review-of-criminal-records-regime.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Second phase of recommendations published | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<h1>Independent review of criminal records regime</h1>
+	<h2>Second phase of recommendations published</h2>
+	<p>The second phase of an independent review of the criminal records regime has now been completed and submitted to Ministers. The review was carried out in two phases, and the second phase of the review has made further recommendations about how information is stored and shared.</p>
+	<p>Phase two of the review contains a number of recommendations for the administration of criminal records, including that more should be done to improve the sharing of criminal records information with other countries.</p>
+	<p>Phase one of the review was conducted in parallel with a review of the Vetting and Barring Scheme.</p>
+	<p><strong>What the review recommendations mean for employers</strong></p>
+	<p>There will be no immediate changes to the way employers check an employee or volunteer's criminal record, so it remains business as usual.</p>
+	<p>Some of the changes recommended by the review require legislation, and these provisions are in the Protection of Freedoms Bill which is currently going through Parliament.</p>
+	<p><strong>Further information on the Home Office website</strong></p>
+	<ul>
+		<li><a href="https://www.gov.uk/government/publications/criminal-records-regime-review-phase-one" target="_blank">Read the criminal records regime review - phase one report - Opens in a new window</a></li>
+		<li><a href="https://www.gov.uk/government/publications/criminal-records-regime-review-phase-two" target="_blank">Read the criminal records regime review - phase two report - Opens in a new window</a></li>
+		<li><a href="https://www.gov.uk/government/publications/independent-review-of-the-criminal-records-regime-government-response" target="_blank">Read the government's formal response to the criminal records review - Opens in a new window</a></li>
+		<li><a href="https://www.gov.uk/government/organisations/disclosure-and-barring-service/about" title="Information on the future of the Vetting and Barring Scheme" target="_blank">Find out about the Vetting and Barring Scheme review - Opens in a new window</a></li>
+	</ul>
+	<p><img src="_img/hr_325px_global.gif" alt="" /></p>
+	<ul>
+		<li><a href="#">Print page now - Opens in a new window</a></li>
+		<li><a href="#">Tell a friend about this page</a></li>
+		<li><a href="#">Send me an email alert when this page changes</a></li>
+	</ul>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
