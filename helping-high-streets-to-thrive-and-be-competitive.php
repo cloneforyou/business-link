c0760108ed7bbdb1e5fe7d28d33373b8df26857f
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-01-01 16:52:04 +0000

    Completed "Helping high streets to thrive and be competitive" page.

---
+++ helping-high-streets-to-thrive-and-be-competitive.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>The Portas Review | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div >
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<h1>Helping high streets to thrive and be competitive</h1>
+	<h2>The Portas Review</h2>
+	<p>In December 2011, the Government published &ldquo;The Portas Review: An independent Review into the future of our high streets&rdquo;. In that report Mary Portas set out the main issues facing high street and town centres, and set out her vision for how high streets can survive and thrive in a changing world.</p>
+	<p>As Mary makes clear, your local trading environment isn't just something that happens to you. <strong>You can play an active role</strong> in helping your high street or town centre to thrive and be competitive. Firstly, by ensuring that your own business is as effective and successful as possible. Secondly, <strong>you need to get involved.</strong> Your town needs everyone with an interest to work together to make sure that it's attractive and relevant to customers. And now that local authorities have more powers to change things, communities - including local businesses - have more say over what happens in their town. </p>
+	<p><strong>So what can you do?</strong></p>
+	<ul>
+		<li><a href="https://www.gov.uk/government/groups/bis-retail-sector-team#retail-and-high-streets" target="_blank">The Portas Review - Opens in a new window</a> talks about a lot of the issues you will need to think about, especially if you are an independent shopkeeper, and makes a number of useful suggestions on things you can do, working in partnership with other local businesses, the community, and the local authority.</li>
+		<li>The <a href="https://www.gov.uk/government/publications/high-streets-and-town-centres-a-healthcheck" target="_blank">Healthy High Street Guide - Opens in a new window</a> is a simple health check for your high street. It takes five minutes to do, but will help you see where your high street might be having problems. Most importantly, the second half of the guide gives a lot of basic advice on what you can do, how you can do it, and who you can help to make things happen. </li>
+		<li>Your local Chamber of Commerce is there to be a strong local voice for business, and influences the decisions made by your local authority that affect you. Think about joining - you can find more out from <a href="http://www.britishchambers.org.uk/" title="Contact details for your local chamber, which can help your business' international growth" target="_blank">British Chambers of Commerce - Opens in a new window</a>.</li>
+		<li>Business Improvement Districts (BIDs) can make a real difference to your local trading environment. Local businesses and the local authority can work together, pooling resources, to make immediate changes that make the area work better for you. Many BIDs were started because local traders decided they needed one.  Find out about BIDs, how they work and what they can do on the <a href="https://www.atcm.org/" target="_blank">UK bids website - Opens in a new window</a> or the <a href="http://www.britishbids.info/AboutBIDs/WhatisaBID.aspx" target="bgExternalonlinebusinesslinkgovuk"   >British Bids website - Opens in a new window</a>.</li>
+	</ul>
+	<p><img src="_img/hr_325px_global.gif" alt=""  /></p>
+	<ul>
+		<li><a href="#">Print page now - Opens in a new window</a></li>
+		<li><a href="#">Tell a friend about this page</a></li>
+		<li><a href="#">Send me an email alert when this page changes</a></li>
+	</ul>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
