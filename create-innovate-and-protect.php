61b168072596e1a5f8d93a2d6740d40a873827ea
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-01-01 16:36:29 +0000

    Completed "create, innovate and protect" page.

---
+++ create-innovate-and-protect.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Create, innovate and protect | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<h1>Create, innovate and protect</h1>
+	<ul>
+		<li><a href="create-innovate-and-protect/create-and-improve-products-and-services.php.html">Create and improve products and services</a>
+			<p>Information on how to develop new products, services and research business ideas</p>
+		</li>
+		<li><a href="create-innovate-and-protect/research-and-development.php.html">Research and development</a>
+			<p>The importance of research and development for the success of new products, services and ideas</p>
+		</li>
+		<li><a href="create-innovate-and-protect/use-innovation-in-your-business.php.html">Use innovation in your business</a>
+			<p>How innovation can improve both existing and new areas of your business</p>
+		</li>
+		<li><a href="create-innovate-and-protect/design-for-business-success.php.html">Design for business success</a>
+			<p>Discover how design can benefit your business, your products, services and branding</p>
+		</li>
+		<li><a href="create-innovate-and-protect/protecting-your-intellectual-property.php.html">Protecting your intellectual property</a>
+			<p>Learn about protecting your ideas, copyright, trade marks and exploiting the potential of your business</p>
+		</li>
+	</ul>
+	<p><a href="#">Create, innovate and protect - site map</a></p>
+</div>
+<div>
+	<div>
+		<h3>Spotlight</h3>
+		<div>
+			<div>
+				<h4><a href="do-it-online/use-the-ip-healthcheck-tool.php.html" title="Use the IP Healthcheck Tool">Have you had your IP Healthcheck?</a></h4>
+				
+				<p>Get a free online healthcheck to see how you could make the most of your IP</p>
+			</div>
+			<div>
+				<a href="do-it-online/use-the-ip-healthcheck-tool.php.html" title="Use the IP Healthcheck Tool">
+					<img src="_img/ip_healthcheack.jpg" width="76" height="76" alt="Use the IP Healthcheck Tool" />
+				</a>
+			</div>
+		</div>
+		<div>
+			<div>
+				<h4><a href="create-innovate-and-protect/use-innovation-in-your-business/increase-efficiency-to-maximise-your-profit.php.html" title="How to realise your businesses potential by improving efficiency and operations">How to make the most of what you've got</a></h4>
+				
+				<p>How to make the most of your business' existing potential and increase your efficiency</p>
+			</div>
+			<div>
+				<a href="create-innovate-and-protect/use-innovation-in-your-business/increase-efficiency-to-maximise-your-profit.php.html" title="How to realise your businesses potential by improving efficiency and operations">
+					<img src="_img/child.jpg" width="76" height="76" alt="How to realise your businesses potential by improving efficiency and operations" />
+				</a>
+			</div>
+		</div>
+		<div>
+			<div>
+				<h4><a href="create-innovate-and-protect/research-and-development/manage-your-research-design-and-development.php.html" title="How to anticipate and adapt to your customers' changing requirements">Manage your research, design and development</a></h4>
+				
+				<p>How to anticipate and adapt to your customers' changing requirements</p>
+			</div>
+			<div>
+				<a href="create-innovate-and-protect/research-and-development/manage-your-research-design-and-development.php.html" title="How to anticipate and adapt to your customers' changing requirements">
+					<img src="_img/randd_promo.jpg" width="76" height="76" alt="How to anticipate and adapt to your customers' changing requirements" />
+				</a>
+			</div>
+		</div>
+		<div>
+			<div>
+				<h4><a href="create-innovate-and-protect/research-and-development/product-life-cycle.php.html" title="How your product or service progresses through its life cycle and how to use this information to increase profits">The product lifecycle</a></h4>
+				
+				<p>How your product or service progresses through its life cycle and what it means for your business</p>
+			</div>
+			<div>
+				<a href="create-innovate-and-protect/research-and-development/product-life-cycle.php.html" title="How your product or service progresses through its life cycle and how to use this information to increase profits">
+					<img src="_img/promo_pdlc.jpg" width="76" height="76" alt="How your product or service progresses through its life cycle and how to use this information to increase profits" />
+				</a>
+			</div>
+		</div>
+		<div>
+			<div>
+				<h4><a href="grow-your-business/setting-a-vision-for-growth/the-queens-awards-for-enterprise.php.html" title="How applying for The Queen's Awards for Enterprise can improve your business and the benefits of winning the award">The Queen's Awards</a></h4>
+				
+				<p>Find out how to apply for a Queen's Award for outstanding achievement in innovation</p>
+			</div>
+			<div>
+				<a href="grow-your-business/setting-a-vision-for-growth/the-queens-awards-for-enterprise.php.html" title="How applying for The Queen's Awards for Enterprise can improve your business and the benefits of winning the award">
+					<img src="_img/award1_spotlight_size_1.jpg" width="76" height="76" alt="How applying for The Queen's Awards for Enterprise can improve your business and the benefits of winning the award" />
+				</a>
+			</div>
+		</div>
+		<div>
+			<div>
+				<h4><a href="https://www.gov.uk/growing-your-business" title="Information and tools for people interested in growing or improving their business - Opens in a new window" target="_blank">Growth and Improvement Service<span> - Opens in a new window</span></a></h4>
+				
+				<p>Access information and support to help your business work better</p>
+			</div>
+			<div>
+				<a href="https://www.gov.uk/growing-your-business" title="Information and tools for people interested in growing or improving their business - Opens in a new window" target="_blank">
+					<img src="_img/GIS_76x76.jpg" width="76" height="76" alt="Information and tools for people interested in growing or improving their business" />
+					<span> - Opens in a new window</span>
+				</a>
+			</div>
+		</div>
+		<div>
+			<div>
+				<h4><a href="create-innovate-and-protect/design-for-business-success/design-for-start-ups.php.html" title="How to use design when you are starting your business, developing new products or entering new markets">Design for start ups</a></h4>
+				
+				<p>How design can help you start up your business, develop new products or enter new markets</p>
+			</div>
+			<div>
+				<a href="create-innovate-and-protect/design-for-business-success/design-for-start-ups.php.html" title="How to use design when you are starting your business, developing new products or entering new markets">
+					<img src="_img/EY0015_promo.jpg" width="76" height="76" alt="How to use design when you are starting your business, developing new products or entering new markets" />
+				</a>
+			</div>
+		</div>
+		<div>
+			<div>
+				<h4><a href="create-innovate-and-protect/protecting-your-intellectual-property/patent-protection-and-your-business/intellectual-property-the-basics.php.html" title="Information on the different types of intellectual property and how to protect your rights and those of others">Intellectual property for start ups</a></h4>
+				
+				<p>Understand the different types of intellectual property, how to protect your rights, and those of others</p>
+			</div>
+			<div>
+				<a href="create-innovate-and-protect/protecting-your-intellectual-property/patent-protection-and-your-business/intellectual-property-the-basics.php.html" title="Information on the different types of intellectual property and how to protect your rights and those of others">
+					<img src="_img/ip_promo.jpg" width="76" height="76" alt="Information on the different types of intellectual property and how to protect your rights and those of others" />
+				</a>
+			</div>
+		</div>
+		<div>
+			<div>
+				<h4><a href="do-it-online/look-up-patents.php.html" title="Search the Intellectual Property Office online patents register">Search the patents directory with the Intellectual Property Office </a></h4>
+				
+				<p>Search the patents directory to ensure that your idea is unique before you apply for a patent</p>
+			</div>
+			<div>
+				<a href="do-it-online/look-up-patents.php.html" title="Search the Intellectual Property Office online patents register">
+					<img src="_img/search.jpg" width="76" height="76" alt="Search the Intellectual Property Office online patents register" />
+				</a>
+			</div>
+		</div>
+	</div>
+	<div>
+		<h3>Case studies</h3>
+		<div>
+			<div>
+				<p>How award winning ethical food company Tossed designed a successful brand<br />
+				</p>
+				
+				<ul>
+					<li><a href="create-innovate-and-protect/design-for-business-success/heres-how-I-designed-a-successful-brand-for-my-business_flash-video.php.html" title="In this video case study, Vincent McKevitt explains how he designed a successful brand for his business">Here's how I designed a successful brand for my business (Flash video)</a></li>
+				</ul>
+			</div>
+			<div><img src="_img/VincentMcKevitt_sml.jpg" width="76" height="76" alt="" /></div>
+		</div>
+	</div>
+	<div>
+		<h3>Do it online</h3>
+		<ul>
+			<li><a href="do-it-online/apply-to-register-a-trade-mark.php.html" title="Register a trade mark online with the Intellectual Property Office">Apply to register a trade mark</a></li>
+			<li><a href="do-it-online/look-up-trade-marks.php.html" title="Search for an existing trade mark online with the Intellectual Property Office">Look up an existing trade mark</a></li>
+			<li><a href="#">Sign up for email alerts</a></li>
+		</ul>
+	</div>
+	<div>
+		<h3>What's popular</h3>
+		<ul>
+			<li><a href="create-innovate-and-protect/protecting-your-intellectual-property/find-value-you-didnt-know-existed-in-your-business/find-out-how-to-protect-your-ideas.php" title="This interactive tool will outline the best ways for you to protect your intellectual property">Find out how to protect your ideas</a></li>
+			<li><a href="finance-and-grants/finance-options/grants-and-government-support/innovation-research-and-development-grants.php.html" title="Overview of grants and other support available for research and development, and innovation">Innovation, research and development grants</a></li>
+			<li><a href="create-innovate-and-protect/design-for-business-success/choose-and-work-with-a-designer.php.html" title="Hire the right designers and build a successful partnership - from initial selection to signing off final projects">Choose and work with a designer</a></li>
+			<li><a href="create-innovate-and-protect/research-and-development/work-with-uk-universities-and-colleges.php.html" title="Government-sponsored research and development programmes can help make your business more competitive">Work with UK universities and colleges</a></li>
+		</ul>
+	</div>
+	<div>
+		<h3>Useful links on other sites</h3>
+		<form method="post">
+			<label for="redirectParams">Find relevant information on other websites</label>
+			<select>
+				<option value="create-innovate-and-protect.php.html">Choose from this menu ...</option>
+				<option value="http://www.designingdemand.org.uk/">Designing Demand - design support programme</option>
+				<option value="http://www.mas.berr.gov.uk/">Manufacturing Advisory Service</option>
+				<option value="https://www.gov.uk/intellectual-property-an-overview/protect-your-intellectual-property">IP advice - protect your intellectual property</option>
+			</select>
+			<input type="image" value="Go" src="_img/button_go.gif" title="Go" />
+		</form>
+	</div>
+	<h3>Directories</h3>
+	<ul>
+		<li><a href="#" title="National and local events, seminars and workshops in your area to help you learn or network">Create, innovate and protect events near you</a></li>
+		<li><a href="find-a-case-study/browse-by-theme-find-a-case-study.php.html?bank=CASE_STUDIES&theme=growthandinnovation&topicId=1073858796">Create, innovate and protect case studies</a></li>
+	</ul>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
