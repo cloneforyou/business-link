445a4ca293cff1f3b261ae878113aafc8087c4c6
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-01-01 21:03:37 +0000

    Completed "interest rate swap misselling" page

---
+++ interest-rate-swap-misselling.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Advice for businesses worried about having been missold an interest rate swap | Business Link</title>
+<meta name="author" content="Caroline Murphy">
+<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>Interest rate Swap misselling </h1>
+		<h2>Advice for businesses worried about having been missold an interest rate swap</h2>
+		<p>In June 2012, the Financial Services Authority, the regulator responsible for governing banks and other financial institutions, announced it had found some misselling of &quot;Interest Rate Swap&quot; or &quot;Hedging&quot; products.</p>
+		<p>These products, also known as &quot;cap and collars&quot; are designed to reduce the risk of interest rate fluctuations for customers with loans. Interest rate hedging products can be appropriate when properly sold to businesses, in the right circumstances. But The FSA announced it found a number of cases of bad practice, including possible mis-selling, meaning that customers may be entitled to redress.</p>
+		<p>The FSA and the major banks have set up a review process, involving independent scrutiny, to make sure that each affected SME is dealt with fairly. This process will identify where misselling has taken place, and settle what redress businesses are owed.</p>
+		<p><strong>What should I do if I have bought one of these products?</strong></p>
+		<p>If you have bought a &quot;swap&quot; or &quot;cap and collar&quot; in the last 12 years your bank will write to you, setting out next steps. If you have a &quot;cap&quot; you can ask your bank to review your case.</p>
+		<p><strong>Where can I find out more information?</strong></p>
+		<p><a href="http://www.fsa.gov.uk/library/other_publications/interest-rate-swaps" target="_blank">For more information please visit the FSA's website. <span> - Opens in a new window</span></a> </p>
+		<p><img src="_img/hr_325px_global.gif" alt=""  /></p>
+		<div class="pageoptions">
+			<ul>
+				<li><a href="#">Print page now - Opens in a new window</a></li>
+				<li><a href="#">Tell a friend about this page</a></li>
+				<li><a href="#">Send me an email alert when this page changes</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
