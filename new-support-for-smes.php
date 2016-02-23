c38a731b5371b1d6c22f35eb9d64472db688fb70
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-23 21:49:43 +0000

    Completed "new support for smes" page

---
+++ new-support-for-smes.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Finance Fitness campaign for businesses | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>New support for SMEs</h1>
+		<h2>Finance Fitness campaign for businesses</h2>
+		<p>The government has launched a new initiative to encourage accountants, banks and representative bodies to offer support to the UK's small and medium sized enterprises (SMEs). The Finance Fitness campaign is designed to help businesses improve their finance fitness - to better manage cashflow and access finance more easily. Finance Fitness will also help SMEs find the information and advice they need and will support them in accessing finance so they are better able to meet economic challenges and are better positioned for growth.</p>
+		<p>Over the coming months, the campaign will focus on resources available to SMEs covering business finance topics including cash management, prompt payment, credit ratings, finding and securing the right finance and export finance.</p>
+		<p>As part of the campaign, there will be a series of free events in six UK cities run by private sector accounting software company Intuit. These events will provide practical advice to help improve the businesses' financial systems and also cover the basics of good recording keeping, cashflow management and other tasks such as invoicing and VAT.</p>
+		<p>In addition to this, the Association of Chartered Certified Accountants will be partnering with Barclays bank to launch a programme of free finance advice consisting of online resources, bank-tested sample business plans, research and a schedule of related events. More details of both these schemes will be announced shortly.</p>
+		<p>There are also some existing resources that complement the new Finance Fitness activities.</p>
+		<p>The Institute of Chartered Accountants in England and Wales (ICAEW) provides a Business Advice Service, which offers free advice to businesses in over 2,400 locations across the country. ICAEW accountants can offer guidance on topics from finding finance, dealing with regulation and export planning. <a href="http://www.businessadviceservice.com/" target="_blank">Find out about the Business Advice Service on the ICAEW website<span> - Opens in a new window</span></a>.</p>
+		<p>SMEs can also consult a range of guidance from the Institute of Credit Management. These guides cover topics including managing customers and suppliers, chasing payment and managing finances and provide straight-forward advice with simple checklists and top tips. <a href="http://www.cicm.com/" target="_blank">Read the guides to managing cashflow from the Credit Management Matters website<span> - Opens in a new window</span></a>.</p>
+		<p>Additional information is also available from Business Link's new <a href="https://www.gov.uk/growing-your-business" target="_blank">Growth and Improvement Service<span> - Opens in a new window</span></a>.</p>
+		<p><img src="_img/hr_325px_global.gif" alt="" align="middle" /></p>
+		<div>
+			<ul>
+				<li><a href="#">Print page now</a></li>
+				<li><a href="#">Tell a friend about this page</a></li>
+				<li><a href="#">Send me an email alert when this page changes</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
