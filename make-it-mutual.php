b10c0db9fc89d1dedad73c1ac7cfa40b7c480d36
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-23 21:44:37 +0000

    Completed "make it mutual" page

---
+++ make-it-mutual.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Government initiative to unleash the latent entrepreneurialism in public sector employees | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>Make It Mutual</h1>
+		<h2>Government initiative to unleash the latent entrepreneurialism in public sector employees</h2>
+		<p>The government is dedicated to increasing the ability of employees in the public sector to set up and run their own enterprises, called public service mutuals. These are organisations which have spun out of the public sector, continue to deliver public services (under contract) and have a high degree of employee ownership or engagement.</p>
+		<p>Launched by Francis Maude, the Minister for the Cabinet Office, in December 2011, the Mutuals Information Service and Support Programme are set up to encourage public servants to free themselves from tiresome bureaucracy and concentrate on what they're best at; delivering public services of the highest quality.</p>
+		<p>Giving staff ownership over their work can dramatically improve productivity and user satisfaction and deliver better public services. Public servants should have the opportunity to have a greater say in the way they run their services.</p>
+		<p>There is support available: the Mutuals Information Service website gives information on the process of becoming a mutual, case studies, high-level guidance and access to a resource library. A &pound;10 million Mutual Support Programme has been set up to support fledgling mutuals seeking to develop and grow.</p>
+		<p><a href="https://www.gov.uk/government/groups/mutuals-information-service" target="_blank">Visit the Mutual Information Service to find out more about the mutuals agenda and how the Support Programme could help you<span> - Opens in a new window</span></a>.</p>
+		<p><img src="_img/hr_325px_global.gif" alt="" align="middle" /></p>
+		<div>
+			<ul>
+				<li><a href="#">Print page now</a></li>
+				<li><a href="#">Tell a friend about this page</a></li>
+				<li><a href="#">Send me an email alert when this page changes</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
