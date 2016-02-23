87fc92ed3be112a442333c92b170416f3e52cb09
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-23 21:41:02 +0000

    Completed "businesses in difficulty" page.

---
+++ buy-or-sell-a-business/businesses-in-difficulty.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Businesses in difficulty | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div id="header"><img src="../../syntegra/images/px_trans.gif" alt="" width="745" height="1" /></div>
+<div id="content" > 
+	
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+	<div id="layer2">
+		<div id="maincontent">
+			<h1> Businesses in difficulty </h1>
+			<div>
+				<h2> <a href="layer7aaa.html?topicId=1077717513"  >Assess your business' performance</a></h2>
+				<p>Use our interactive tool to assess how well your business is currently performing</p>
+			</div>
+			<div>
+				<h2> <a href="layer861a.html?topicId=1074002362"  >Ensuring customers pay you on time</a></h2>
+				<p>Actions you can take if a payment you are due is late</p>
+			</div>
+			<div>
+				<h2> <a href="layer6fd4.html?topicId=1084616161"  >Insolvency: the basics</a></h2>
+				<p>What insolvency, bankruptcy and winding up are, how they can be avoided and what happens if you can't avoid them</p>
+			</div>
+			<div>
+				<h2> <a href="layere780.html?topicId=1074407360"  >Avoid insolvency</a></h2>
+				<p>Keep an eye on cashflow to help you avoid insolvency and do not allow debts to exceed assets</p>
+			</div>
+			<div>
+				<h2> <a href="layera024.html?topicId=1075430788"  >Individual insolvency: find out your options for dealing with debt</a></h2>
+				<p>An interactive tool to help you investigate alternatives to declaring yourself bankrupt</p>
+			</div>
+			<div>
+				<h2> <a href="layer2f3a.html?topicId=1084624281"  >Complain against an undischarged bankrupt or disqualified company director</a></h2>
+				<p>How to complain against a disqualified director, an undischarged bankrupt or a person subject to bankruptcy restrictions</p>
+			</div>
+			<div>
+				<h2> <a href="layerfff0.html?topicId=1085045289"  >Company and LLP strike-off, dissolution and restoration</a></h2>
+				<p>How to deal with voluntary and compulsory strike-off or restoration of a registered business</p>
+			</div>
+			<div>
+				<h2> <a href="layer0ad7.html?topicId=1087427482"  >How to turn your business around</a></h2>
+				<p>The areas of your business that you should assess in order to uncover any problems or potential problems</p>
+			</div>
+		</div>
+		<div id="additionalcontent"> 
+			
+			<!-- Top Image --> 
+			<img src="../../Topic_Structure_images/Business_in_difficulty.jpg" width="146" height="146" border="0" alt="" /> </div>
+		<!-- End additionalcontent--> 
+	</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
