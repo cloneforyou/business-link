16ea1963ae33f3c2ce0838ca01018650dc5c8502
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-23 21:45:03 +0000

    Completed "national business support services have changed" page

---
+++ national-business-support-services-have-changed.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>An overview of the services available to you | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>National business support services have changed</h1>
+		<h2>An overview of the services available to you</h2>
+		<p>In January 2011, the Government announced a series of changes to the way it offers business support.</p>
+		<p>The <strong>Business Link website</strong> is your first place to go to access essential information, support and services for your business. The website contains a wealth of information and guidance designed to help you whether you work within a large organisation or are looking to start up.</p>
+		<p>The website has been updated with new, easy-to-use services to help you start up, grow and improve your business. New features include:</p>
+		<ul>
+			<li>The Growth and Improvement Service, giving you access to the information you need to make your business work better; including tools to help you diagnose and solve business problems, an enhanced business support finder, tutorials and video case studies, information on events in your area and links to business discussions and other services. <a href="#">www.businesslink.gov.uk/improve</a>.</li>
+			<li>The comprehensive Start- Up Service offering support through the stages of setting up a business  with free access to over 200 videos, case studies,  tutorials and tools and a starting up task list to guide you through the process. <a href="#">www.businesslink.gov.uk/mynewbusiness</a>.</li>
+		</ul>
+		<h3>Mentorsme.co.uk</h3>
+		<p>Businesses who do well often have outside help. If you think you could benefit from talking to someone with a lot of business experience, you can find a business mentor at <a href="http://www.mentorsme.co.uk/" target="_blank">www.mentorsme.co.uk</a>.</p>
+		<ul>
+			<li>Mentorsme.co.uk will put you in touch with mentoring organisations across the UK who can help you find the kind of mentor that suits you.</li>
+			<li>Plus there's lots of advice on what to look for in a good mentor.</li>
+			<li>If you would like to be a mentor and share your wealth of experience with a less experienced business person, the mentoring portal can help you find an organisation to help.</li>
+		</ul>
+		<p><img src="_img/hr_325px_global.gif" alt="" align="middle" /></p>
+		<div>
+			<ul>
+				<li><a href="#">Print page now</a></li>
+				<li><a href="#">Tell a friend about this page</a></li>
+				<li><a href="#">Send me an email alert when this page changes</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
