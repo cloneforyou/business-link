dbabd10d495cb42c779dc84c47fd758e1845fee4
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-23 21:50:26 +0000

    Completed "open to export" page

---
+++ open-to-export.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Make the connections you need to take your business overseas | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>Open to Export</h1>
+		<h2>Make the connections you need to take your business overseas</h2>
+		<p>Many small and medium-sized UK companies are now looking at international markets in their search for new customers. If this isn't something you've already considered, now could be the time to start investigating.</p>
+		<p>To help, UK Trade &amp; Investment (UKTI) has teamed up with Yell to create<a href="http://opentoexport.com/" target="_blank">Open to Export<span> - Opens in a new window</span></a>&ndash; a dedicated website where businesses looking to export can access practical advice and have questions answered by the Open to Export community of users and contributors, including UK trade officers in Embassies and High Commissions.</p>
+		<p><strong>I already export<br />
+			</strong>Even if you already trade overseas, <a href="http://opentoexport.com/" target="_blank">Open to Export<span> - Opens in a new window</span></a>can help you consider other markets and allow you to make connections to the businesses and services that could help you expand further. And if you become a member of the Open to Export community, you will also be able to promote your business and build your reputation to other users and members of the site.</p>
+		<p>You'll be able to:</p>
+		<ul>
+			<li>Post articles and advice</li>
+			<li>Answer exporters' questions</li>
+			<li>Get updates on relevant questions</li>
+			<li>Engage with potential customers</li>
+		</ul>
+		<p><strong>Other benefits<br />
+			</strong>It's free to access and by just by visiting <a href="http://opentoexport.com/" target="_blank">Open to Export<span> - Opens in a new window</span></a>you will be able to see what exporting events are coming up, what overseas opportunities exist and gain access to a whole wealth of content posted by the community.</p>
+		<p>To ask a question, simply register as a user and you could have direct access to those with the experience to help. Our ratings system, means that you can provide feedback on the quality of the responses you receive.</p>
+		<p>For more information visit<a href="http://opentoexport.com/" target="_blank">OpentoExport.com<span> - Opens in a new window</span></a> </p>
+		<p><img src="_img/hr_325px_global.gif" alt="" align="middle" /></p>
+		<div>
+			<ul>
+				<li><a href="#">Print page now</a></li>
+				<li><a href="#">Tell a friend about this page</a></li>
+				<li><a href="#">Send me an email alert when this page changes</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
