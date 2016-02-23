f670edc35627d98d7d99d91dc1c39eacbef749a6
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-23 21:49:09 +0000

    Completed "new guidance on recruiting disabled people" page

---
+++ new-guidance-on-recruiting-disabled-people.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Disability Clearkit developed over three years with 220 employers | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>New guidance on recruiting disabled people</h1>
+		<h2>Disability Clearkit developed over three years with 220 employers</h2>
+		<p>A campaign promoting wider employer support for recruiting and retaining disabled workers is being supported by the Department for Work and Pensions (DWP).</p>
+		<p>The Disability <strong>Clearkit</strong> combines the latest thinking and employer case studies with helpful tips and 'must do' check lists, which help dispel the myths that prevent many employers from recruiting disabled people.</p>
+		<p>The Clearkit package is the result of three years of in-depth research with the DWP and 220 employers.</p>
+		<p><a href="http://www.clearkit.co.uk/clearkit/" target="_blank">Find out how to access free Clearkit information and advice on the Clear Company website<span> - Opens in a new window</span></a>.</p>
+		<h3>Further information</h3>
+		<ul>
+			<li><a href="employment-and-skills/equality-and-diversity/support-available-from-jobcentre-plus-to-employ-or-retain-someone-who-is-disabled.php" title="Help and advice from Jobcentre Plus when considering employing disabled people">Support if you employ someone who is disabled</a></li>
+			<li>Recruiting and employing disabled people</li>
+			<li><a href="premises-and-property/access-and-facilities/access-and-facilities-for-disabled-people.php" title="Information on what changes you can make to ensure your business is accessible to disabled people">Access and facilities for disabled people</a></li>
+		</ul>
+		<p><img src="_img/hr_325px_global.gif" alt="" /></p>
+		<div>
+			<ul>
+				<li><a href="#">Print page now</a></li>
+				<li><a href="#">Tell a friend about this page</a></li>
+				<li><a href="#">Send me an email alert when this page changes</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
