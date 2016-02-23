9a5e77d53bafd7cb1d2eb6085c4be1afba345939
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-23 21:44:19 +0000

    Completed "make business your business" page

---
+++ make-business-your-business.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Helping you to start and develop your business | Business Link</title>
+<title>Initiative to let unused government estate to business start ups | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+	<div>
+		<p><a href="home.html" title="" >Home</a> &gt; Helping you to start and develop your business</p>
+	</div>
+	<div>
+		<div>
+			<h1>Make Business Your Business </h1>
+			<h2>Helping you to start and develop your business</h2>
+			<p>Now is a great time to start a business, with more and more people doing it and a wide variety of support, advice and encouragement available. This report highlights some of the key developments affecting SME enterprise:</p>
+			<ul>
+				<li>The accessibility of new technology, in particular how the internet is changing the way that businesses market and trade;</li>
+				<li>The increasing support on offer through collaboration and relationships with big business, including mentoring;</li>
+				<li>The issues of finding and attracting finance; and,</li>
+				<li>Winning business from the public sector.</li>
+			</ul>
+			<p>For information on the abundance of support available to anyone thinking of starting a business, access the <a href="https://www.gov.uk/government/publications/starting-and-growing-a-home-business-advice-and-support" target="_blank">Guide<span> - Opens in a new window</span></a> for details of:</p>
+			<ul>
+				<li>StartUp Loans which have been established with &pound;10m of Government funding and offer mentoring, support and finance to young people looking to start a business.  StartUp Loans are available to 18-24 years olds. To find out more information, search online for 'Business in You'.</li>
+				<li>New tools like a Finance Finder, The Home Business Guide, 'taking on an employee' guidance, and an incubation search tool.</li>
+			</ul>
+			<p>Visit the <a href="https://www.gov.uk/government/organisations/department-for-business-innovation-skills" target="_blank">BIS website<span> - Opens in a new window</span></a> or search online for 'Business in You' to see the report, the guide, the home business guide and more details on StartUp Loans.</p>
+			<p><img src="_img/hr_325px_global.gif" alt="" align="middle" /></p>
+			<div>
+				<ul>
+					<li><a href="#">Print page now</a></li>
+					<li><a href="#">Tell a friend about this page</a></li>
+					<li><a href="#">Send me an email alert when this page changes</a></li>
+				</ul>
+			</div>
+		</div>
+		<div>
+			<div>
+				<div>
+					<p>The report by Lord Young, the Prime Minister's adviser on small business and enterprise, highlights the range of support for entrepreneurs and new businesses and includes help and advice on offer from the public and private sectors. </p>
+					<p>The <a href="https://www.gov.uk/government/publications/support-available-for-small-business-start-ups-analysis" target="_blank">report<span> - Opens in a new window</span></a> contains a short <a href="https://www.gov.uk/government/publications/starting-and-growing-a-home-business-advice-and-support" target="_blank">guide<span> - Opens in a new window</span></a> to help you turn your business idea into reality.  It will help you to start and develop your new business and gives details of the new StartUp Loans available to 18-24 year olds. It also includes a <a href="https://www.gov.uk/government/publications/the-home-business-guide" target="_blank">Home Business Guide<span> - Opens in a new window</span></a> to help you set up at home and explains alternative sources of finance such as 'crowdfinance' providers.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
