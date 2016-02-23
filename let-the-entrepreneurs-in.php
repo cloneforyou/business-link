846bb1a2900853b4f7cb65acd4378f7ab944a114
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-23 21:43:59 +0000

    Completed "let the entrepreneurs in" page

---
+++ let-the-entrepreneurs-in.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Initiative to let unused government estate to business start ups | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<div>
+	<div>
+		<h1>Let the Entrepreneurs in</h1>
+		<h2>Initiative to let unused government estate to business start ups</h2>
+		<p>Government is opening up its unused and underused office space to start-ups. To make this happen we need to recruit providers to manage the space and to provide a package of support to these new businesses.</p>
+		<p>31 May, Government announced details of space available in twenty buildings across England with the potential to be used by aspiring entrepreneurs to build and grow their businesses. We are pleased to announce today (2 July) that a further four spaces are now available.</p>
+		<p>To allow existing bidders the opportunity to bid for these spaces we are extending the closing date of this procurement to 23rd July.</p>
+		<p>We are inviting bids from organisations, with a background in managing business start-up space and a background in supporting new businesses, to manage these locations on behalf of Government.</p>
+		<p><strong>How to Bid</strong></p>
+		<p>Interested bidders should follow the links below to find more details about the tendering process and details of the properties.</p>
+		<p><strong>Information for Bidders</strong></p>
+		<p>If you have a Contracts Finder account, you can <a href="https://data.gov.uk/data/contracts-finder-archive/contract/530443/" target="_blank">submit a proposal to provide office space on Contracts Finder. <span> - Opens in a new window</span></a></p>
+		<p>If you do not have a Contracts Finder account, search for details on <a href="https://data.gov.uk/data/contracts-finder-archive/" target="_blank">Contracts Finder<span> - Opens in a new window</span></a> and insert the reference PR120046 in the 'find live opportunities' field.</p>
+		<p><strong>Timeline for Bidders</strong></p>
+		<ul>
+			<li>Procurement launched to find providers &ndash; 29th May.</li>
+			<li>Deadline for bids &ndash; 23rd July.</li>
+			<li>Tender evaluation - 24th July to 30th August.</li>
+			<li>Appointments from August</li>
+		</ul>
+		<p><img src="_img/hr_325px_global.gif" alt="" /></p>
+		<div>
+			<ul>
+				<li><a href="#">Print page now</a></li>
+				<li><a href="#">Tell a friend about this page</a></li>
+				<li><a href="#">Send me an email alert when this page changes</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
