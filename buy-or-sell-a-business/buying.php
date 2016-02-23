0431a3dcf8c11782485f17b7fdc379a06afc1670
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-23 21:57:53 +0000

    Completed "buy or sell a business > buying" page

---
+++ buy-or-sell-a-business/buying.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Buying | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div >
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>Buying</h1>
+		<div>
+			<h2><a href="buying/buy-an-existing-business.php">Buy an existing business</a></h2>
+			<p>Advantages and disadvantages of buying an existing business including how to assess and value it</p>
+		</div>
+		<div>
+			<h2><a href="../starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></h2>
+			<p>Company, partnership or sole trader - find the right structure for your new business</p>
+		</div>
+		<div>
+			<h2><a href="../starting-up/name-your-business/choose-the-right-name-for-your-business.php">Choose the right name for your business</a></h2>
+			<p>How to find the right name for your business and the rules and regulations involved in registering a name</p>
+		</div>
+		<div>
+			<h2><a href="considering-selling-a-business/understanding-contracts-when-buying-or-selling-a-business.php">Understanding contracts when buying or selling a business</a></h2>
+			<p>Get expert advice to help you understand the many contracts involved in buying or selling a business</p>
+		</div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
