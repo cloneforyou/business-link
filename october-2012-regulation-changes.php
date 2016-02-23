8cb10c8a66818a013c0cfdeb7199dd41f4bc3d85
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-23 21:50:05 +0000

    Completed "october 2012 regulation changes" page

---
+++ october-2012-regulation-changes.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Common commencement dates | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>October 2012 regulation changes</h1>
+		<h2>Common commencement dates</h2>
+		<p>Most new laws affecting businesses come into force on one of two common commencement dates (CCDs) each year - 6 April and 1 October. CCDs enable you to prepare your business for the introduction of new, amended or reduced requirements.</p>
+		<p>This page gives you a snapshot of some of the key regulations due to come into force in October 2012. It will be updated as more information becomes available.</p>
+		<hr />
+		<ul>
+			<li>Changing audit rules to give small enterprises and subsidiaries flexibility in how they submit their company reports.</li>
+			<li>Giving business the flexibility to determine the most appropriate set of accounting rules to use.</li>
+			<li>Publishing HM Treasury's improved implementation of the EU Third Money Laundering Directive, which will ensure that the UK is a hostile environment for money launderers and terrorist financing while reducing the burden of regulation on legitimate businesses.</li>
+			<li>Changing the Home Office's vetting and barring scheme. This will consist of scaling back criminal records disclosure and barring arrangements by making criminal record checks more portable and reducing the need for repeat checks.</li>
+		</ul>
+		<p><img src="_img/hr_325px_global.gif" alt="" align="middle" /></p>
+		<div>
+			<ul>
+				<li><a href="#">Print page now</a></li>
+				<li><a href="#">Tell a friend about this page</a></li>
+				<li><a href="#">Send me an email alert when this page changes</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
