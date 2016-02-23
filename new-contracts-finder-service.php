21baf3a5d1850dba1d19aa605582e582679ff8d3
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-23 21:47:49 +0000

    Completed "new contracts finder service" page

---
+++ new-contracts-finder-service.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Find public sector procurement tender and contract documents | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>New Contracts Finder service</h1>
+		<h2>Find public sector procurement tender and contract documents</h2>
+		<p><strong>The Prime Minister launched Contracts Finder on 11 February 2011. At the launch event at the Treasury in London, the PM said the Government was determined to make government procurement &ldquo;more welcoming&rdquo; for small and medium-sized enterprises. The Government's goal, he said, is to ensure that at least a 25 percent share of government contracts end up awarded to SMEs. You can <a href="https://www.gov.uk/government/news/pm-launches-contracts-finder" target="_blank">find details of the announcement on the Number10 website<span> - Opens in a new window</span></a>.</strong></p>
+		<p><a href="https://data.gov.uk/data/contracts-finder-archive/" target="_blank">Contracts Finder<span> - Opens in a new window</span></a> is a free new service for businesses, central government buyers and the public. It is now the main source of government procurement opportunities with a value greater than &pound;10,000. You can also use Contracts Finder to access and search for closed tender and contract documentation published by central government departments and agencies.</p>
+		<p>This service is one of several practical measures that government has taken to become more open and transparent, so that it can be held to account and deliver better value for money.</p>
+		<p>To find out what you can do with Contracts Finder, see our guide on how to <a href="grow-your-business/suppliers-overview-of-tendering-for-contracts/find-government-opportunities-tenders-and-contracts-on-contracts-finder.php" title="How to use this free service to find government opportunities, tenders and contract documents">find government opportunities, tenders and contracts on Contracts Finder</a>.</p>
+		<p>For more guidance on contract work and public sector procurement, you can see these guides in our Grow your business theme:</p>
+		<ul>
+			<li><a href="sales-and-marketing/selling/overview-on-selling-to-government.php" title="How you can find and compete for public sector contracts, and understand the procurement processes used">Overview of selling to government</a> </li>
+			<li><a href="sales-and-marketing/selling/tender-for-a-contract.php" title="How to prepare convincing tenders to win business from both private and public sector organisations">Tender for a contract</a> </li>
+			<li><a href="sales-and-marketing/pricing/price-lists-estimates-quotations-and-tenders.php" title="Find out how to draw up price lists, estimates and quotations and prices for tenders">Price lists, estimates, quotations and tenders</a> </li>
+			<li><a href="grow-your-business/suppliers-overview-of-tendering-for-contracts/tendering-for-public-sector-contracts-in-europe.php" title="The process of tendering for procurement contracts in the European Union">Tendering for public sector contracts in Europe</a> </li>
+		</ul>
+		<p><img src="_img/hr_325px_global.gif" alt="" /></p>
+		<div>
+			<ul>
+				<li><a href="#">Print page now</a></li>
+				<li><a href="#">Tell a friend about this page</a></li>
+				<li><a href="#">Send me an email alert when this page changes</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
