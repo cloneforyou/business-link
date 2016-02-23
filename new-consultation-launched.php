bdace43f60bb9c6ddf4ed55418ec192c562d1702
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-23 21:46:01 +0000

    Completed "new consultation launched" page

---
+++ new-consultation-launched.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Simpler Income Tax for Self employed and Small Partnerships | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>New consultation launched</h1>
+		<h2>Simpler Income Tax for Self employed and Small Partnerships</h2>
+		<p><strong>What's the Consultation about?</strong></p>
+		<p>The Government wants to make it easier for small businesses - that is, most self-employed sole traders and those in partnership with other individuals - to calculate their taxable income.</p>
+		<p>From April 2013, the Government proposes to introduce a voluntary cash basis for small businesses to calculate their income tax along with simplified arrangements for some business expenses.</p>
+		<p><strong>What are the proposals?</strong></p>
+		<p>The proposals are, that those who choose to use the new regime will be taxed on the basis of their receipts less allowable payments for expenses, rather than needing to spend their time doing accounting designed more for big business.</p>
+		<p>The Government is exploring proposals that small businesses with receipts of less than &pound;77,000 would be eligible to use the cash basis, and that they could continue to use it until their receipts rise to more than &pound;150,000 in any year.</p>
+		<p>Those businesses using the cash basis would use simplified expenses for business use of their cars or home and personal use of business premises. Businesses that do not opt to use the cash basis could however choose to use these simplified expenses.<br />
+			(The proposals do not include companies or property businesses.)</p>
+		<p><strong>How can I find out more?</strong></p>
+		<p>The consultation period runs until 22 June 2012, <a href="#">you can download the full consultation proposal Simpler Income Tax for the Simplest Small Businesses<span> - Opens in a new window</span></a> (PDF, 140K)</p>
+		<p>In particular Chapter 2 'Small Businesses - what the proposals mean for you' summarises the proposals for those running small businesses. <a href="_files/simpler-income-tax-for-the-simplest-small-businesses.pdf" target="_blank">You can download the shorter version of the consultation<span> - Opens in a new window</span></a>.</p>
+		<p><img src="_img/hr_325px_global.gif" alt="" /></p>
+		<div>
+			<ul>
+				<li><a href="#">Print page now</a></li>
+				<li><a href="#">Tell a friend about this page</a></li>
+				<li><a href="#">Send me an email alert when this page changes</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
