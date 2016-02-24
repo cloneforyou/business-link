cbd127bc69c8b3764790eb34d4cb2e4840b2d276
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-24 16:39:18 +0000

    Completed "buy or sell a business > Considering selling a business" page

---
+++ buy-or-sell-a-business/considering-selling-a-business.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Considering selling a business | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>Considering selling a business</h1>
+		<div>
+			<h2><a href="../tax-payroll-and-company-information/selling-or-closing-your-business/selling-or-closing-your-business-an-overview.php">Selling or closing your business - an overview</a></h2>
+			<p>Having an exit strategy may be the best way to ensure a pain-free sale or liquidation</p>
+		</div>
+		<div>
+			<h2><a href="considering-selling-a-business/preparing-to-sell-your-business.php">Preparing to sell your business</a></h2>
+			<p>Options for selling your business, making it attractive to purchasers and finding advisers</p>
+		</div>
+		<div>
+			<h2><a href="../tax-payroll-and-company-information/selling-or-closing-your-business/closing-a-business-checklist.php">Closing a business checklist</a></h2>
+			<p>This interactive tool tells you actions you need to take when exiting a business</p>
+		</div>
+		<div>
+			<h2><a href="considering-selling-a-business/value-and-market-your-business.php">Value and market your business</a></h2>
+			<p>Setting a price, building a marketing proposition and identifying possible buyers for your business</p>
+		</div>
+		<div>
+			<h2><a href="../grow-your-business/assessing-current-performance/identifying-protecting-and-transferring-your-assets.php">Identifying, protecting and transferring your assets</a></h2>
+			<p>Identifying, valuing, protecting and managing your business' tangible and intangible assets</p>
+		</div>
+		<div>
+			<h2><a href="considering-selling-a-business/understanding-contracts-when-buying-or-selling-a-business.php">Understanding contracts when buying or selling a business</a></h2>
+			<p>Get expert advice to help you understand the many contracts involved in buying or selling a business</p>
+		</div>
+	</div>
+	<div><img src="../_img/Selling_your_business.jpg" width="146" height="146" border="0" alt="" /> </div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
