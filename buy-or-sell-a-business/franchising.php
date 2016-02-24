97b8af06944b7c2857f264f3047d4469a2981923
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-24 16:30:17 +0000

    Completed "buy or sell a business > franchising" page

---
+++ buy-or-sell-a-business/franchising.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Franchising | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>Franchising</h1>
+		<div>
+			<h2><a href="franchising/buy-a-franchise.php">Buy a franchise</a></h2>
+			<p>What franchising is, how to find and assess franchise opportunities and how to buy a franchise</p>
+		</div>
+		<div>
+			<h2><a href="franchising/turn-your-business-into-a-franchise.php">Turn your business into a franchise</a></h2>
+			<p>Growing your business by working with independent franchisees</p>
+		</div>
+		<div>
+			<h2><a href="../grow-your-business/setting-a-vision-for-growth/how-to-grow-your-franchise.php">How to grow your franchise</a></h2>
+			<p>How to successfully expand a franchise business</p>
+		</div>
+	</div>
+	<div><img src="../_img/Obligations_to_employees.jpg" width="146" height="146" border="0" alt="" /></div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
