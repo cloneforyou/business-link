8f92a3eb3f41e0768bd72ed0b24ec0e82902d124
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-24 15:37:24 +0000

    Completed "buy or sell a business > considering buying a business" page

---
+++ buy-or-sell-a-business/considering-buying-a-business.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Considering buying a business | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1> Considering buying a business </h1>
+		<div>
+			<h2><a href="considering-selling-a-business/obtaining-information-on-a-business-you-are-thinking-of-buying.php">Obtaining information on a business you are thinking of buying</a></h2>
+			<p>How to obtain information on an existing business you are thinking of buying</p>
+		</div>
+		<div>
+			<h2><a href="considering-selling-a-business/understanding-contracts-when-buying-or-selling-a-business.php">Understanding contracts when buying or selling a business</a></h2>
+			<p>Get expert advice to help you understand the many contracts involved in buying or selling a business</p>
+		</div>
+		<div>
+			<h2><a href="businesses-in-difficulty/assess-your-business-performance.php">Assess your business' performance</a></h2>
+			<p>Use our interactive tool to assess how well your business is currently performing</p>
+		</div>
+		<div>
+			<h2><a href="considering-selling-a-business/consider-your-exit-strategy-when-starting-up.php">Consider your exit strategy when starting up</a></h2>
+			<p>Considering an exit strategy when you set up a business - from a family succession or trade sale to liquidation</p>
+		</div>
+	</div>
+	<div><img src="../_img/Buying_a_business.jpg" width="146" height="146" border="0" alt="" /></div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
