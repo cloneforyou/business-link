6983e690cc15139b672233c2b527213ba391976e
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-24 16:24:23 +0000

    Completed "buy or sell a business > mergers and acquisitions" page

---
+++ buy-or-sell-a-business/mergers-and-acquisitions.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Mergers and acquisitions | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>Mergers and acquisitions</h1>
+		<div>
+			<h2><a href="mergers-and-acquisitions/mergers.php">Mergers</a></h2>
+			<p>How anti-competitive mergers are investigated by the OFT and Competition Commission under the Enterprise Act 2002</p>
+		</div>
+		<div>
+			<h2><a href="../grow-your-business/financing-growth/mergers-and-acquisitions.php">Mergers and acquisitions</a></h2>
+			<p>Consider the pros and cons of joining forces with another company</p>
+		</div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
