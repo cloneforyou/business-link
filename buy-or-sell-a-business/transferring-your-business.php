564b2d50c198e5b5e70728ff2e6936678314df8b
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-24 16:03:27 +0000

    Completed "Transferring your business" page

---
+++ buy-or-sell-a-business/transferring-your-business.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Transferring your business | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>Transferring your business</h1>
+		<div>
+			<h2><a href="transferring-your-business/selling-or-passing-a-business-to-a-family-member.php">Selling or passing a business to a family member</a></h2>
+			<p>How objective succession planning can help you pass on your family business successfully and in a tax-efficient way</p>
+		</div>
+		<div>
+			<h2><a href="considering-selling-a-business/consider-your-exit-strategy-when-starting-up.php">Consider your exit strategy when starting up</a></h2>
+			<p>Considering an exit strategy when you set up a business - from a family succession or trade sale to liquidation</p>
+		</div>
+		<div>
+			<h2><a href="transferring-your-business/succession-planning-and-business-transfer.php">Succession planning and business transfer</a></h2>
+			<p>How effective succession planning can create the best outcome for you and your business</p>
+		</div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
