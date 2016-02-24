1f370654266a72a1b40d55d2e648b01ddd491611
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-24 17:17:51 +0000

    Completed "buy or sell a business > Tax considerations when selling" page

---
+++ buy-or-sell-a-business/tax-considerations-when-selling.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Tax considerations when selling | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>Tax considerations when selling</h1>
+		<ul>
+			<li><a href="../tax-payroll-and-company-information/selling-or-closing-your-business/selling-or-closing-your-business-an-overview.php">Selling or closing your business - an overview</a>
+				<p>Having an exit strategy may be the best way to ensure a pain-free sale or liquidation</p>
+			</li>
+			<li> <a href="../tax-payroll-and-company-information/selling-or-closing-your-business/check-tax-and-legal-issues-when-selling-a-business.php">Check tax &amp; legal issues when selling a business</a>
+				<p>Interactive tool to guide you through the main tax, VAT, employment and licensing implications when you sell your business</p>
+			</li>
+			<li> <a href="../tax-payroll-and-company-information/capital-gains-tax.php">Capital Gains Tax</a>
+				<p>Helping you to understand and work out your Capital Gains Tax</p>
+			</li>
+		</ul>
+	</div>
+	<div>
+		<div>
+			<h3>Spotlight</h3>
+			<div>
+				<div>
+					<h4><a href="considering-selling-a-business/value-and-market-your-business.php">Value and market your business</a></h4>
+					<p>Setting a price, building a marketing proposition and identifying possible buyers for your business</p>
+				</div>
+				<div><a href="considering-selling-a-business/value-and-market-your-business.php"><img src="../_img/value_promo.jpg" width="76" height="76" border="0" alt="Setting a price, building a marketing proposition and identifying possible buyers for your business" /></a></div>
+			</div>
+			<div>
+				<div>
+					<h4><a href="considering-selling-a-business/preparing-to-sell-your-business.php">Preparing to sell your business</a></h4>
+					<p>Options for selling your business, making it attractive to purchasers and finding advisers</p>
+				</div>
+				<div><a href="considering-selling-a-business/preparing-to-sell-your-business.php"><img src="../_img/preparing_to_sell_promo.jpg" width="76" height="76" border="0" alt="Options for selling your business, making it attractive to purchasers and finding advisers" /></a></div>
+			</div>
+		</div>
+		<div>
+			<h3>Case studies</h3>
+			<div>
+				<div>
+					<p>Find out how Bill Carey-Evans sold his IT consultancy</p>
+					<ul>
+						<li><a href="../tax-payroll-and-company-information/selling-or-closing-your-business/here-is-how-I-handled-the-sale-of-my-business.php">Here's how I handled the sale of my business</a></li>
+					</ul>
+				</div>
+				<div><img src="../_img/Bill_CareyEvans.jpg" width="76" height="76" border="0" alt="" /></div>
+			</div>
+		</div>
+		<div>
+			<h3>Do it online</h3>
+			<ul>
+				<li><a href="../tax-payroll-and-company-information/selling-or-closing-your-business/closing-a-business-checklist.php">Use our interactive tool to create a checklist of tasks for closing your business</a></li>
+			</ul>
+		</div>
+		<div>
+			<h3>What's popular</h3>
+			<ul>
+				<li><a href="../tax-payroll-and-company-information/vat/changing-or-cancelling-your-vat-registration/closing-or-selling-your-business-and-VAT-registration.php">Closing or selling your business and VAT registration</a></li>
+				<li><a href="../tax-payroll-and-company-information/corporation-tax/selling-or-closing-your-company-and-corporation-tax.php">Selling or closing your company and Corporation Tax</a></li>
+			</ul>
+		</div>
+		<div>
+			<h3>Directories</h3>
+			<ul>
+				<li><a href="http://www.businesslink.gov.uk/bdotg/action/event?topicId=1073861225" title="National and local events, seminars and workshops in your area to help you learn or network">Buy or sell a business events near you</a></li>
+				<li><a href="../find-a-case-study/browse-by-theme-buy-or-sell-a-business.php">Buy or sell a business case studies</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
