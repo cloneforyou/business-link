f160376081766d4ad5221cf49f9baa54d077a36a
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-24 16:09:48 +0000

    Completed "buy or sell a business > selling" page

---
+++ buy-or-sell-a-business/selling.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Selling | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>Selling</h1>
+		<div>
+			<h2><a href="selling/complete-the-sale-of-your-business.php">Complete the sale of your business</a></h2>
+			<p>Negotiating the sale of your business, assessing offers and understanding the legal and tax implications</p>
+		</div>
+		<div>
+			<h2><a href="../tax-payroll-and-company-information/selling-or-closing-your-business/closing-a-business-checklist.php">Closing a business checklist</a></h2>
+			<p>This interactive tool tells you actions you need to take when exiting a business</p>
+		</div>
+		<div>
+			<h2><a href="selling/how-to-achieve-an-employee-buyout.php">How to achieve an employee buyout</a></h2>
+			<p>The advantages of an employee buyout when you retire or sell your business and key issues to consider</p>
+		</div>
+		<div>
+			<h2><a href="../employment-and-skills/redundancy-restructures-and-change/responsibilities-to-employees-if-you-buy-or-sell-a-business.php">Responsibilities to employees if you buy or sell a business</a></h2>
+			<p>TUPE regulations and your employees' rights if they are transferred into or out of your business</p>
+		</div>
+		<div>
+			<h2><a href="../tax-payroll-and-company-information/selling-or-closing-your-business/company-and-llp-strike-off-dissolution-and-restoration.php">Company and LLP strike-off, dissolution and restoration</a></h2>
+			<p>How to deal with voluntary and compulsory strike-off or restoration of a registered business</p>
+		</div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
