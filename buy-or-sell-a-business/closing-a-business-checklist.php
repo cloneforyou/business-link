6b133539997b56b33bcaca16347b159f1251f8c0
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-02-25 16:04:54 +0000

    Completed "bur or sell a business > closing a business checklist" page

---
+++ buy-or-sell-a-business/closing-a-business-checklist.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Closing a business checklist | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>Closing a business checklist</h1>
+		<h2>Introduction</h2>
+		<span><img src="../_img/closing.gif" width="78" height="118" border="0" alt="" /></span>
+		<p>Every business owner will want to think carefully before exiting their business. You need to explore all the options and then plan your exit fully.</p>
+		<p>This interactive tool tells you what you need to do if you are closing, selling or transferring the business.</p>
+		<p>It also sets out the basic steps involved in stock market flotation and business mergers and explores how insolvency might affect your business exit.</p>
+		<p>The tool should be used as a guide only and is not a substitute for professional advice.</p>
+		<p><strong>Although the data you enter will be stored securely we recommend that you do not include bank account details and other sensitive information</strong></p>
+		<p><a href="#">Go straight to the first question</a></p>
+		<div>
+			<ul>
+				<li><a href="#" title="Business exit checklist">Next</a></li>
+			</ul>
+		</div>
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li>Introduction</li>
+			<li><a href="#">Business exit checklist</a></li>
+		</ul>
+		<p><img src="../_img/hr_325px_global.gif" alt="" /></p>
+		<div>
+			<ul>
+				<li><a href="#">Add guide to my print portfolio (print later as PDF)</a></li>
+				<li><a href="#">Print page now</a></li>
+				<li><a href="#">Tell a friend about this page</a></li>
+				<li><a href="#">Send me an email alert when this page changes</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
