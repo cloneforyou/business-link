95ec06597f2541f5f3d9bff06903e0279eaa22fb
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-03-07 10:43:08 +0000

    Completed "buy or sell a business > businesses in difficulty > Assess your business' performance" page

---
+++ buy-or-sell-a-business/businesses-in-difficulty/assess-your-business-performance.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Assess your business' performance | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>Assess your business' performance</h1>
+		<h2>Introduction</h2>
+		<span><img src="../../_img/assess_health.gif" width="78" height="118" border="0" alt="" /></span>
+		<p>This interactive tool helps you analyse key areas of your business' operations with the aim of improving performance. It considers finance, sales, administration and your relationships with customers, suppliers and staff.</p>
+		<p>The tool can help you work out how to overcome financial or operational problems that your business may be facing. It also explains how you might prepare your business for future growth.</p>
+		<p>The tool should take no longer than five minutes to complete. You do not have to input any sensitive or personal data.</p>
+		<p>The suggestions provided should be used as an indicator only and should be discussed with professional business advisers.</p>
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/logicToolStart?itemId=1077717552&type=BLTTOOL">Go straight to the first question</a>.</p>
+		<div>
+			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/logicToolStart?itemId=1077717552&type=BLTTOOL">Next</a></li>
+			</ul>
+		</div>
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li>Introduction</li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/logicToolStart?itemId=1077717552&type=BLTTOOL">Assess your business' performance</a></li>
+		</ul>
+		<p><img src="../../_img/hr_325px_global.gif" alt="" /></p>
+		<div>
+			<strong>Print options</strong> - <span><a href="#">What are my print options <img src="../../_img/whatisthis.gif" width="9" height="11" border="0" alt="" /><span> - Opens in a new window</span></a></span>
+			<ul>
+				<li><a href="#">Add guide to my print portfolio (print later as PDF)</a></li>
+			</ul>
+			<strong>Email options</strong> - <span><a href="#">What are my email options <img src="../../_img/whatisthis.gif" width="9" height="11" border="0" alt="" /><span> - Opens in a new window</span></a></span>
+			<ul>
+				<li><a href="#">Tell a friend about this page</a></li>
+				<li><a href="#">Send me an email alert when this guide changes</a></li>
+			</ul>
+		</div>
+	</div>
+	<div>
+		<div>
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.icaew.com/" target="_blank">Finance management guidance on the Institute of Chartered Accountants in England and Wales website<span> - Opens in a new window</span></a></li>
+				<li><a href="https://www.gov.uk/growing-your-business" target="_blank">Search our business support finder for grants, loans, expertise and advice for which your business may be eligible<span> - Opens in a new window</span></a></li>
+				<li><a href="../../finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance options for your business</a></li>
+			</ul>
+			</ul>
+			<img src="../../_img/px_sellingabusiness.gif" alt="" width="181" height="5" /></div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
