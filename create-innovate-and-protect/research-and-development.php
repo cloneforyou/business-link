c3e9d869d97a6b2b04c7fbe7275856aaa4fb88e0
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-03-07 12:13:36 +0000

    Completed "create, innovate and protect > Research and development" page

---
+++ create-innovate-and-protect/research-and-development.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Research and development | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1> Research and development </h1>
+		<div>
+			<h2><a href="../starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">Research and develop your business ideas, new products and services</a></h2>
+			<p>How to develop your idea into a viable business by investing in new products or services to meet customer needs</p>
+		</div>
+		<div>
+			<h2><a href="research-and-development/manage-your-research-design-and-development.php">Manage your research, design and development</a></h2>
+			<p>How to anticipate and adapt to your customers' changing requirements</p>
+		</div>
+		<div>
+			<h2><a href="research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d.php">Support networks and facilities for innovation and R&amp;D</a></h2>
+			<p>Enterprise organisations and groups that can help your business with innovation, research and development</p>
+		</div>
+		<div>
+			<h2><a href="research-and-development/work-with-uk-universities-and-colleges.php">Work with UK universities and colleges</a></h2>
+			<p>Government-sponsored research and development programmes can help make your business more competitive</p>
+		</div>
+		<div>
+			<h2><a href="../finance-and-grants/finance-options/grants-and-government-support/innovation-research-and-development-grants.php">Innovation, research and development grants</a></h2>
+			<p>Overview of grants and other support available for research and development, and innovation</p>
+		</div>
+		<div>
+			<h2><a href="../tax-payroll-and-company-information/corporation-tax/claiming-losses-allowances-and-reliefs/research-and-development-r-and-d-relief-for-corporation-tax.php">Research and Development (R&amp;D) Relief for Corporation Tax</a></h2>
+			<p>What is R&amp;D; who can claim; how to claim; when to claim; SME and Large Company Schemes explained</p>
+		</div>
+		<div>
+			<h2><a href="research-and-development/product-life-cycle.php">Product life cycle</a></h2>
+			<p>How your product or service progresses through its life cycle and how to use this information to increase profits</p>
+		</div>
+		<div>
+			<h2><a href="research-and-development/developing-computer-games-and-off-the-shelf-software.php">Developing computer games and off-the-shelf software</a></h2>
+			<p>An introduction to the software and games industries and best practice in starting a business</p>
+		</div>
+		<div>
+			<h2><a href="research-and-development/music-photography-visual-art-and-comedy.php">Music, photography, visual art and comedy</a></h2>
+			<p>Opportunities available to a business starting-up in the creative and performing arts sector</p>
+		</div>
+		<div>
+			<h2><a href="research-and-development/publishing-and-broadcasting.php">Publishing and broadcasting</a></h2>
+			<p>An introduction to working in the UK's publishing and broadcasting sectors</p>
+		</div>
+		<div>
+			<h2><a href="research-and-development/technology-strategy-board.php">Technology Strategy Board</a></h2>
+			<p>How the Technology Strategy Board can help your innovative business get your products closer to market</p>
+		</div>
+	</div>
+	<div>
+		<div>
+			<h3>Spotlight</h3>
+			<div>
+				<div>
+					<h4><a href="research-and-development/manage-your-research-design-and-development.php">Manage your research, design and development</a></h4>
+					<p>How to anticipate and adapt to your customers' changing requirements</p>
+				</div>
+				<div>
+					<a href="research-and-development/manage-your-research-design-and-development.php">
+						<img src="../../_img/randd_promo.jpg" width="76" height="76"  alt="How to anticipate and adapt to your customers' changing requirements" />
+					</a>
+				</div>
+			</div>
+		</div>
+		<div>
+			<h3>Case studies</h3>
+			<div>
+				<div>
+					<p>How Autoglass use research and development to ensure they stay competitive and relevant to their customers</p>
+					<ul>
+						<li><a href="research-and-development/manage-your-research-design-and-development/here-is-how-I-made-the-right-decisions-through-research_flash-video.php">Here's how I made the right decisions through research (Flash video)</a></li>
+					</ul>
+				</div>
+				<div><img src="../../_img/DrChrisDavies_sml.jpg" width="76" height="76"  alt="" /></div>
+			</div>
+		</div>
+		<div>
+			<h3>Directories</h3>
+			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://www.businesslink.gov.uk/bdotg/action/event?topicId=1073858796">Create, innovate and protect events near you</a></li>
+				<li><a href="../find-a-case-study/browse-by-theme-create-innovate-and-protect.php">Create, innovate and protect case studies</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
