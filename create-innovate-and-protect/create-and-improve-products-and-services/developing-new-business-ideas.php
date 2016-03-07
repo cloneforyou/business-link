bb57efd1e0c0e7312564e5ca62e4352bfc4c0cea
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-03-07 16:15:30 +0000

    Completed "create, innovate and protect > create and improve products and services > Developing new business ideas" page

---
+++ create-innovate-and-protect/create-and-improve-products-and-services/developing-new-business-ideas.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Developing new business ideas | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>Developing new business ideas</h1>
+		<h2>Introduction</h2>
+		<span><img src="../../_img/Developing_new_business_ideas.jpg" width="94" height="62" alt="" /></span>
+		<p>The successful development of your ideas is key to the success and growth of your business.</p>
+		<p>If you have a new business idea this tool can help you evaluate and manage its progress, taking into account the nature of your idea, your level of ability and the stage you have reached with your concept.</p>
+		<p>Note: This tool can give recommendations about the types of relevant intellectual property (IP) protection available to you. However, IP law is a complex area and you should consider seeking professional advice, for instance from a patent attorney or trade mark attorney, before deciding how to proceed.</p>
+		<p><strong><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/logicToolStart?itemId=1081776862&type=BLTTOOL">Go straight to the first question</a></strong></p>
+		<div>
+			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/logicToolStart?itemId=1081776862&type=BLTTOOL" title="Developing new business ideas">Next</a></li>
+			</ul>
+		</div>
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li>Introduction</li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/logicToolStart?itemId=1081776862&type=BLTTOOL">Developing new business ideas</a></li>
+		</ul>
+		<p><img src="../../_img/hr_325px_global.gif" alt="" /></p>
+		<div> <strong>Print options</strong> - <span><a href="#" title="What are my print options? - Opens in a new window" target="bgPopup" >What are my print options <img src="../../_img/whatisthis.gif" width="9" height="11"  alt="" /> - Opens in a new window</a></span>
+			<ul>
+				<li><a href="http://online.businesslink.gov.uk/bdotg/action/addGuide?refPage=%2Fbdotg%2Faction%2Flayer%3FtopicId%3D1081718116&amp;topicId=1081718116"  title="" >Add guide to my print portfolio (print later as PDF)</a></li>
+			</ul>
+			<strong>Email options</strong> - <span><a href="#" title="What are my email options? - Opens in a new window" target="bgPopup" >What are my email options <img src="../../_img/whatisthis.gif" width="9" height="11"  alt="" /> - Opens in a new window</a></span>
+			<ul>
+				<li><a href="#" title="Tell a friend about this page" >Tell a friend about this page</a></li>
+			</ul>
+		</div>
+	</div>
+	<div>
+		<div>
+			<div> </div>
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="../protecting-your-intellectual-property/find-value-you-didnt-know-existed-in-your-business/find-out-how-to-protect-your-ideas.php">Use our interactive tool to find out the best ways to protect your intellectual property</a></li>
+				<li><a href="../../grow-your-business/growth-through-product-and-service-development.php">Growth through product and service development</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20140603093549/http://www.ipo.gov.uk/design/d-applying/d-apply.htm" target="_blank">Find out how to apply to register a design on the Intellectual Property Office<span> - Opens in a new window</span></a></li>
+				<li><a href="http://www.britishdesigninnovation.org/" target="_blank">Search for a product designer on the BDI website<span> - Opens in a new window</span></a></li>
+			</ul>
+			</ul>
+			<img src="../../_img/px_growthandinnovation.gif" alt="" width="181" height="5" /></div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
