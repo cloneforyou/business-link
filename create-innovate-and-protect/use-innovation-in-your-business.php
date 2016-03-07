90aa6893bfc66b06140b94619e0d1e04788d5610
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-03-07 11:37:05 +0000

    Completed "create innovate and protect > Use innovation in your business" page

---
+++ create-innovate-and-protect/use-innovation-in-your-business.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Use innovation in your business | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>Use innovation in your business</h1>
+		<div>
+			<h2><a href="use-innovation-in-your-business/an-innovation-start-up.php">An innovation start-up</a></h2>
+			<p>How to set up a business to exploit an innovative business idea</p>
+		</div>
+		<div>
+			<h2><a href="use-innovation-in-your-business/use-innovation-to-start-or-grow-your-business.php">Use innovation to start or grow your business</a></h2>
+			<p>A definition of innovation and why it is important for both new and existing businesses</p>
+		</div>
+		<div>
+			<h2><a href="use-innovation-in-your-business/increase-efficiency-to-maximise-your-profit.php">Increase efficiency to maximise your profit</a></h2>
+			<p>How to realise your businesses potential by improving efficiency and operations</p>
+		</div>
+		<div>
+			<h2><a href="use-innovation-in-your-business/find-support-for-inventors.php">Find support for inventors</a></h2>
+			<p>The key sources of help and advice available for inventors and innovative businesses developing ideas</p>
+		</div>
+		<div>
+			<h2><a href="../sales-and-marketing/online-marketing/online-business-networking-and-social-networking.php">Online business networking and social networking</a></h2>
+			<p>How to use online business networking and social media to develop your business</p>
+		</div>
+		<div>
+			<h2><a href="../finance-and-grants/finance-options/grants-and-government-support/innovation-research-and-development-grants.php">Innovation, research and development grants</a></h2>
+			<p>Overview of grants and other support available for research and development, and innovation</p>
+		</div>
+		<div>
+			<h2><a href="use-innovation-in-your-business/manufacturing-innovation.php">Manufacturing innovation</a></h2>
+			<p>How new practices can improve business, what new technologies and thinking offer and where to go for further help</p>
+		</div>
+		<div>
+			<h2><a href="../sales-and-marketing/online-marketing/web-2-0-a-guide-for-business.html">Web 2.0: a guide for business</a></h2>
+			<p>How you can make the most of the changing internet to benefit your business</p>
+		</div>
+		<div>
+			<h2><a href="research-and-development/technology-strategy-board.php">Technology Strategy Board</a></h2>
+			<p>How the Technology Strategy Board can help your innovative business get your products closer to market</p>
+		</div>
+	</div>
+	<div>
+		<div>
+			<h3>Spotlight</h3>
+			<div>
+				<div>
+					<h4><a href="https://www.gov.uk/growing-your-business" target="_blank">Growth and Improvement Service<span> - Opens in a new window</span></a></h4>
+					<p>Access information and support to help your business work better</p>
+				</div>
+				<div>
+					<a href="https://www.gov.uk/growing-your-business" target="_blank">
+						<img src="../../_img/GIS_76x76.jpg" width="76" height="76"  alt="Information and tools for people interested in growing or improving their business" />
+					</a>
+				</div>
+			</div>
+		</div>
+		<div>
+			<h3>Case studies</h3>
+			<div>
+				<div>
+					<p>How design and communications agency The Team use knowledge management to improve business performance</p>
+					<ul>
+						<li><a href="create-and-improve-products-and-services/importance-of-knowledge-management-to-a-growing-business/here-is-how-I-used-knowledge-management-to-make-my-business-better_flash-video.php">Here's how I used knowledge management to make my business better (Flash video)</a></li>
+					</ul>
+				</div>
+				<div><img src="../../_img/PhilWhitehouse_sml.jpg" width="76" height="76"  alt="" /></div>
+			</div>
+		</div>
+		<div>
+			<h3>Directories</h3>
+			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://www.businesslink.gov.uk/bdotg/action/event?topicId=1073858796">Create, innovate and protect events near you</a></li>
+				<li><a href="../find-a-case-study/browse-by-theme-create-innovate-and-protect.php">Create, innovate and protect case studies</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
