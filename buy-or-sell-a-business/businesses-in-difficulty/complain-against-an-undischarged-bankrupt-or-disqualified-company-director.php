ca7e5c8ef58a6dece0671bd3fa5d49540834f32b
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-03-07 10:57:46 +0000

    Completed "buy or sell a business > businesses in difficulty > Complain against an undischarged bankrupt or disqualified company director" page

---
+++ buy-or-sell-a-business/businesses-in-difficulty/complain-against-an-undischarged-bankrupt-or-disqualified-company-director.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Complain against an undischarged bankrupt or disqualified company director | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>Complain against an undischarged bankrupt or disqualified company director</h1>
+		<h2>Introduction</h2>
+		<p>Complaints about disqualified directors, undischarged bankrupts and persons subject to bankruptcy restrictions are dealt with by The Insolvency Service, the government agency responsible for insolvency issues.</p>
+		</p>
+		<p>The Insolvency Service Enforcement Hotline allows people to report disqualified directors, undischarged bankrupts and persons subject to bankruptcy restrictions, if they are not complying with the restrictions imposed on them.</p>
+		<p>This guide explains the enforcement action that can be taken against these parties. It also provides information on how to complain about them.</p>
+		<div>
+			<ul>
+				<li><a href="complain-against-an-undischarged-bankrupt-or-disqualified-company-director/what-undischarged-bankrupts-and-persons-subject-to-bankruptcy-restrictions-are-not-allowed-to-do.php">Next</a></li>
+			</ul>
+		</div>
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li>Introduction</li>
+			<li><a href="complain-against-an-undischarged-bankrupt-or-disqualified-company-director/what-undischarged-bankrupts-and-persons-subject-to-bankruptcy-restrictions-are-not-allowed-to-do.php">What undischarged bankrupts and persons subject to bankruptcy restrictions are not allowed to do</a></li>
+			<li><a href="complain-against-an-undischarged-bankrupt-or-disqualified-company-director/how-to-check-if-a-director-has-been-disqualified-or-if-an-individual-is-an-undischarged-bankrupt-or-subject-to-bankruptcy-restrictions.php">How to check if a director has been disqualified or if an individual is an undischarged bankrupt or subject to bankruptcy restrictions</a></li>
+			<li><a href="complain-against-an-undischarged-bankrupt-or-disqualified-company-director/complaining-about-disqualified-directors-undischarged-bankrupts-and-persons-subject-to-bankruptcy-restrictions.html">Complaining about disqualified directors, undischarged bankrupts and persons subject to bankruptcy restrictions</a></li>
+		</ul>
+		<p><img src="../../_img/hr_325px_global.gif" alt="" /></p>
+		<div>
+			<strong>Print options</strong> - <span><a href="#">What are my print options <img src="../../_img/whatisthis.gif" width="9" height="11" border="0" alt="" /><span> - Opens in a new window</span></a></span>
+			<ul>
+				<li><a href="#">Add guide to my print portfolio (print later as PDF)</a></li>
+			</ul>
+			<strong>Email options</strong> - <span><a href="#">What are my email options <img src="../../_img/whatisthis.gif" width="9" height="11" border="0" alt="" /><span> - Opens in a new window</span></a></span>
+			<ul>
+				<li><a href="#">Tell a friend about this page</a></li>
+				<li><a href="#">Send me an email alert when this guide changes</a></li>
+			</ul>
+		</div>
+	</div>
+	<div>
+		<div>
+			<div>
+				<h3>Insolvency Service Enforcement Hotline</h3>
+				<p>0845 601 3546<br/>
+				</p>
+				<h3>Insolvency Service Enquiry Line</h3>
+				<p>0845 602 9848<br/>
+				</p>
+				<p><img src="../../_img/hr_140px_global.gif" alt="" /></p>
+			</div>
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="../../do-it-online/search-the-individual-insolvency-register.php">Search the Individual Insolvency Register with The Insolvency Service</a></li>
+				<li><a href="../../do-it-online/use-the-enforcement-hotline.php">Complete the Enforcement Hotline questionnaire with The Insolvency Service</a></li>
+			</ul>
+			</ul>
+			<img src="../../_img/px_sellingabusiness.gif" alt="" width="181" height="5" /></div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
