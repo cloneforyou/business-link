b2271a145886dfbf60228cd44b777d9c0e13a1f2
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-03-07 10:33:53 +0000

    Completed "buy or sell a business > businesses in difficulty > Individual insolvency: find out your options for dealing with debt" page

---
+++ buy-or-sell-a-business/businesses-in-difficulty/individual-insolvency-find-out-your-options-for-dealing-with-debt.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Individual insolvency: find out your options for dealing with debt | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>Individual insolvency: find out your options for dealing with debt</h1>
+		<h2>Introduction</h2>
+		<span><img src="../../_img/FB9120.gif" width="78" height="118" border="0" alt="" /></span>
+		<p>Bankruptcy is a very serious matter. If you become bankrupt you will have to give up any possessions of value you have. If you own a property it may have to be sold. Your personal details will be advertised in a local newspaper and the Official Receiver will investigate your conduct and financial affairs.</p>
+		<p>You may have to close any business you run and dismiss your employees. Bankruptcy also places certain restrictions on your life.</p>
+		<p>But you do not have to become bankrupt just because you are in debt.</p>
+		<p>This tool will help you understand what alternatives may be available to you.</p>
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/logicToolStart?itemId=1077722970&type=BLTTOOL" title="An interactive tool to help you investigate alternatives to declaring yourself bankrupt">Go straight to the first question</a>.</p>
+		<p>You can save your progress while using this tool. To do so, you will need to log in or register - then you can come back and check or change your responses at any time, and the tool will give you updated advice. Please ensure that before saving, you have answered all questions on the page you are working on.</p>
+		<div>
+			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/logicToolStart?itemId=1077722970&type=BLTTOOL" title="What other options do I have when considering bankruptcy?">Next</a></li>
+			</ul>
+		</div>
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li>Introduction</li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/logicToolStart?itemId=1077722970&type=BLTTOOL">What other options do I have when considering bankruptcy?</a></li>
+		</ul>
+		<p><img src="../../_img/hr_325px_global.gif" alt="" /></p>
+		<div> <strong>Print options</strong> - <span><a href="#">What are my print options <img src="../../_img/whatisthis.gif" width="9" height="11" border="0" alt="" /><span> - Opens in a new window</span></a></span>
+			<ul>
+				<li><a href="#">Add guide to my print portfolio (print later as PDF)</a></li>
+			</ul>
+			<strong>Email options</strong> - <span><a href="#">What are my email options <img src="../../_img/whatisthis.gif" width="9" height="11" border="0" alt="" /><span> - Opens in a new window</span></a></span>
+			<ul>
+				<li><a href="#">Tell a friend about this page</a></li>
+				<li><a href="#">Send me an email alert when this guide changes</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
