bc0fb4e71e4b160cba361eab9930572e8349bba1
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-03-07 16:30:43 +0000

    Completed "create, innovate and protect > create and improve products and services > Importance of knowledge management to a growing business" page

---
+++ create-innovate-and-protect/create-and-improve-products-and-services/importance-of-knowledge-management-to-a-growing-business.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Importance of knowledge management to a growing business | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>Importance of knowledge management to a growing business</h1>
+		<h2>Introduction</h2>
+		<span><img src="../../_img/Importance_of_knowledge_management_to_growing_a_bu.jpg" width="78" height="118"  alt="" /></span>
+		<p>All businesses have access to an extensive pool of knowledge - whether this is their understanding of customers' needs and the business environment or the skills and experience of staff.</p>
+		<p>The way a business gathers, shares and exploits this knowledge can be central to its ability to develop successfully. This doesn't just apply to huge multinational companies. Knowledge management can benefit everyone from a local newsagent to a manufacturing firm.</p>
+		<p>This guide explains the basic sources of knowledge available to your business, how you can best harness and exploit this information and how to create a knowledge strategy for your business.</p>
+		<div>
+			<ul>
+				<li><a href="importance-of-knowledge-management-to-a-growing-business/what-is-knowledge-in-a-business.php">Next</a></li>
+			</ul>
+		</div>
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li>Introduction</li>
+			<li><a href="importance-of-knowledge-management-to-a-growing-business/what-is-knowledge-in-a-business.php">What is knowledge in a business?</a></li>
+			<li><a href="importance-of-knowledge-management-to-a-growing-business/basic-sources-of-knowledge.php">Basic sources of knowledge</a></li>
+			<li><a href="importance-of-knowledge-management-to-a-growing-business/exploiting-your-knowledge.php">Exploiting your knowledge</a></li>
+			<li><a href="importance-of-knowledge-management-to-a-growing-business/make-knowledge-central-to-your-business.php">Make knowledge central to your business</a></li>
+			<li><a href="importance-of-knowledge-management-to-a-growing-business/sharing-knowledge-across-your-business.php">Sharing knowledge across your business</a></li>
+			<li><a href="importance-of-knowledge-management-to-a-growing-business/create-a-knowledge-strategy-for-your-business.php">Create a knowledge strategy for your business</a></li>
+			<li><a href="importance-of-knowledge-management-to-a-growing-business/using-information-technology-to-gain-and-manage-knowledge.php">Using information technology to gain and manage knowledge</a></li>
+			<li><a href="importance-of-knowledge-management-to-a-growing-business/here-is-how-I-used-knowledge-management-to-make-my-business-better_flash-video.php">Here's how I used knowledge management to make my business better (Flash video)</a></li>
+		</ul>
+		<p><img src="../../_img/hr_325px_global.gif" alt="" /></p>
+		<div> <strong>Print options</strong> - <span><a href="#" title="What are my print options? - Opens in a new window" target="bgPopup" >What are my print options <img src="../../_img/whatisthis.gif" width="9" height="11"  alt="" /> - Opens in a new window</a></span>
+			<ul>
+				<li><a href="#">Add guide to my print portfolio (print later as PDF)</a></li>
+			</ul>
+			<strong>Email options</strong> - <span><a href="#" title="What are my email options? - Opens in a new window" target="bgPopup" >What are my email options <img src="../../_img/whatisthis.gif" width="9" height="11"  alt="" /> - Opens in a new window</a></span>
+			<ul>
+				<li><a href="#" title="Tell a friend about this page" >Tell a friend about this page</a></li>
+			</ul>
+		</div>
+	</div>
+	<div>
+		<div>
+			<div> </div>
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://www.knowledgebusiness.com/knowledgebusiness/Templates/Home.aspx?siteId=1&menuItemId=25" target="_blank">Business knowledge news and resources on the KNOW website<span> - Opens in a new window</span></a></li>
+				<li><a href="http://www.trainingzone.co.uk/" target="_blank">Knowledge sharing information on the Training Zone website (registration required)<span> - Opens in a new window</span></a></li>
+			</ul>
+			</ul>
+			<img src="../../_img/px_growthandinnovation.gif" alt="" width="181" height="5" /></div>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
