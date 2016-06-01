a173723c9218b7997e0304e4f7b7552d408929ad
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-06-01 23:32:34 +0100

    'Intrastate returns important changes' changed the file extension, tidied up code.

--- intrastat-returns-important-changes.php
+++ intrastat-returns-important-changes.php
@@ -1,36 +1,33 @@
 <!DOCTYPE html>
-<html class="no-js">
+<html>
 <head>
-	<meta charset="utf-8">
-	
-	<title>Submitting Intrastat declarations from 1 April 2012: a reminder | Business Link</title>
-	<meta name="author" content="Caroline Murphy">
-	<meta name="viewport" content="width=device-width">
+<meta charset="utf-8">
+<title>Submitting Intrastat declarations from 1 April 2012: a reminder | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
 </head>
 
 <body>
-
 <?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
-
 <div>
+<div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
 	<div>
-		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
 		<div>
+			<h1>Intrastat returns: important changes</h1>
+			<h2>Submitting Intrastat declarations from 1 April 2012: a reminder</h2>
+			<h3>Who needs to read this?</h3>
+			<p>All businesses that provide Intrastat returns to HMRC Trade Statistics.</p>
+			<h3>What will businesses need to do from 1 April 2012?</h3>
+			<ul>
+				<li>Submit your March 2012 return by <strong>21 April 2012</strong>. From then on, all Intrastat Supplementary Declarations must be made by the 21st day of the month.</li>
+				<li>Provide all Intrastat Supplementary Declarations using electronic methods, eg online form, Comma Separated Value (CSV) file format or Electronic Data Interchange (EDI).</li>
+			</ul>
+			<h3>Where can I get more information?</h3>
+			<p>You can <a href="https://www.uktradeinfo.com/Intrastat/Pages/Intrastat.aspx" title="How to get started with making Intrastat declarations online" rel="external">find out more about making Intrastat declarations on the uktradeinfo website.</a></p>
+			<p>You can also contact the HMRC Customer Service Helpdesk on <a href="mailto:Intrastatonline@hmrc.gsi.gov.uk"><u>Intrastatonline@hmrc.gsi.gov.uk</u></a> quoting <strong>'Intrastat 2012'</strong> or telephone 01702 367248.</p>
+			<p><img src="/_img/hr_325px_global.gif" alt="" align="middle" /></p>
 			<div>
-				<h1>Intrastat returns: important changes</h1>
-				<h2>Submitting Intrastat declarations from 1 April 2012: a reminder</h2>
-				<h3>Who needs to read this?</h3>
-				<p>All businesses that provide Intrastat returns to HMRC Trade Statistics.</p>
-				<h3>What will businesses need to do from 1 April 2012?</h3>
-				<ul>
-					<li>Submit your March 2012 return by <strong>21 April 2012</strong>. From then on, all Intrastat Supplementary Declarations must be made by the 21st day of the month.</li>
-					<li>Provide all Intrastat Supplementary Declarations using electronic methods, eg online form, Comma Separated Value (CSV) file format or Electronic Data Interchange (EDI).</li>
-				</ul>
-				<h3>Where can I get more information?</h3>
-				<p>You can <a href="https://www.uktradeinfo.com/Intrastat/Pages/Intrastat.aspx" title="How to get started with making Intrastat declarations online" rel="external">find out more about making Intrastat declarations on the uktradeinfo website.</a></p>
-				<p>You can also contact the HMRC Customer Service Helpdesk on <a href="mailto:Intrastatonline@hmrc.gsi.gov.uk"><u>Intrastatonline@hmrc.gsi.gov.uk</u></a> quoting <strong>'Intrastat 2012'</strong> or telephone 01702 367248.</p>
-				<p><img src="/_img/hr_325px_global.gif" alt="" align="middle" /></p>
-				<div>
 				<ul>
 					<li><a href="#">Print page now</a></li>
 					<li><a href="#">Tell a friend about this page</a></li>
@@ -40,5 +37,4 @@
 		</div>
 	</div>
 </div>
-
 <?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
