e8e34080a843113a09e424c19c1267bdc6596775
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-06-09 16:21:40 +0100

    Updated Footer with HTML5 and tidied up the code

--- _inc/footer.php
+++ _inc/footer.php
@@ -1,91 +1,65 @@
-	<span><a name="skipfooter" id="skipfooter"></a></span> 
-	<!-- Page footer -->
-	<div>
-		<div id="footer"> 
-			<!-- "Developed With" section --> 
-			<!-- Rest of footer --> 
-			<img  src="/_img/footer_top3.gif" alt="" />
-			<div id="clickables">
-				<p>
-					<a href="https://www.gov.uk/help" title="Help &amp; support" rel="external">Help &amp; support</a> | 
-					<a href="https://www.gov.uk/help" title="&copy; Crown copyright 2011" rel="external">Copyright</a> | 
-					<a href="https://www.gov.uk/help" title="Terms &amp; conditions" rel="external">Terms &amp; conditions</a> | 
-					<a href="https://www.gov.uk/help" title="Privacy &amp; cookies" rel="external">Privacy &amp; cookies</a> | 
-					<a href="https://www.gov.uk/help" title="Accessibility" rel="external">Accessibility</a> | 
-					<a href="https://www.gov.uk/help" title="PDF help" rel="external">PDF help</a>
-				</p>
-			</div>
-		</div>
-	</div>
-</div>
-<div id="topnav">
-	<div id="showtopnavinner">
-		<span><a name="skiphelp"id="skiphelp"></a></span>
-		<div id="navgroup">
-			<div id="topgroup">
-				<div id="sitejump">
-					<form id="localForm" method="get" action="http://online.businesslink.gov.uk/bdotg/action/redirectToSite">
-						<fieldset>
-							<label for="localBusinessLink">Regional Support</label>
-							<select id="localBusinessLink" name="Site Id">
-								<option selected="selected" value="default">Select your location</option>
-								<optgroup label="Northern Ireland">
-									<option value='1087659111'>nibusinessinfo.co.uk</option>
-								</optgroup>
-								<optgroup label="Scotland">
-									<option value='1087659110'>Business Gateway</option>
-								</optgroup>
-								<optgroup label="Wales">
-									<option value='1087659109'>Welsh Government</option>
-								</optgroup>
-							</select>
-							<input type="submit" value="Go" />
-						</fieldset>
-					</form>
-				</div>
-				<!-- sitejump -->
-				<div id="quicklinks">
-					<ul>
-						<li><a href="#" title="Site map">Site map</a></li>
-						<li><a href="#" title="Help">Help</a></li>
-						<li><a href="about-business-link.php" title="About us">About us</a></li>
-						<li><a href="https://www.gov.uk/help" rel="external">Accessibility</a></li>
-					</ul>
-				</div>
-				<!-- quicklinks -->
-				<div id="sitesearch">
-					<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/searchBasicMode">
-						<fieldset>
-							<input type="hidden" name="resultPage" value="1"/>
-							<label for="headersearchinput">Search</label>
-							<input id="headersearchinput" type="text" name="expression" />
-							<input type="submit" value="Search" id="headersearchsubmit" title="Start the search" />
-						</fieldset>
-					</form>
-				</div>
-				<!-- sitesearch -->
-			</div>
-			<!-- topgroup -->
-			<div id="bottomgroup">
-				<div id="tabs">
-					<ul>
-						<li><a href="#">My Business</a></li>
-						<li><a href="whats-new.php">What's new?</a></li>
-						<li><a href="do-it-online.php" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies">Do it online</a></li>
-						<li><a href="directories.php">Directories</a></li>
-					</ul>
-				</div>
-				<!-- tabs --> 
-			</div>
-			<!-- bottomgroup --> 
-		</div>
-		<!-- navgroup --> 
-	</div>
-	<!-- topnavinner --> 
-</div>
-<!-- topnav --> 
+<a name="skipfooter" id="skipfooter"></a>
 
-<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+<footer>
+	<nav class="sub-links">
+		<p>
+			<a href="https://www.gov.uk/help" target="_blank">Help &amp; Support</a> | 
+			<a href="http://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/copyright-and-re-use/crown-copyright/" target="_blank">&copy; Crown Copyright</a> | 
+			<a href="https://www.gov.uk/help/terms-conditions" target="_blank">Terms &amp; Conditions</a> | 
+			<a href="https://www.gov.uk/help/privacy-policy" target="_blank">Privacy Policy</a> | 
+			<a href="https://www.gov.uk/help/accessibility" target="_blank">Accessibility</a> | 
+			<a href="https://www.gov.uk/help/cookies" target="_blank">Cookies</a>
+			<a href="#" title="Site map">Site map</a>
+			<a href="about-business-link.php">About us</a>
+		</p>
+	</nav><!-- end .sub-links -->
+
+	<a name="skiphelp"id="skiphelp"></a>
+
+	<!-- Regional Support dropdown -->
+	<form id="localForm" method="get" action="#">
+		<fieldset>
+			<label for="localBusinessLink">Regional Support</label>
+			<select id="localBusinessLink" name="Site Id">
+				<option selected="selected" value="default">Select your location</option>
+				<optgroup label="Northern Ireland">
+					<option value=''>nibusinessinfo.co.uk</option>
+				</optgroup>
+				<optgroup label="Scotland">
+					<option value=''>Business Gateway</option>
+				</optgroup>
+				<optgroup label="Wales">
+					<option value=''>Welsh Government</option>
+				</optgroup>
+			</select>
+			<input type="submit" value="Go" />
+		</fieldset>
+	</form>
+
+	<!-- Search form -->
+	<form method="get" action="#">
+		<fieldset>
+			<input type="hidden" name="resultPage" value="1"/>
+			<label for="headersearchinput">Search</label>
+			<input id="headersearchinput" type="text" name="expression" />
+			<input type="submit" value="Search" id="headersearchsubmit" title="Start the search" />
+		</fieldset>
+	</form>
+
+	<div id="bottomgroup">
+		<div id="tabs">
+			<ul>
+				<li><a href="#">My Business</a></li>
+				<li><a href="whats-new.php">What's new?</a></li>
+				<li><a href="do-it-online.php" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies">Do it online</a></li>
+				<li><a href="directories.php">Directories</a></li>
+			</ul>
+		</div>
+	</div>
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</footer>
 
 <?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/scripts.php'; ?>
 
