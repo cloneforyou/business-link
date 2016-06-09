1b472ac4ac3bdbc673f38b59bec95df62d76bd68
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-06-09 16:22:32 +0100

    Removed old JS and added CDN resource for latest version. Updated script code for console errors and removed old external links code.

--- _js/script.js
+++ _js/script.js
@@ -1,16 +1,31 @@
 // Avoid `console` errors in browsers that lack a console.
-(function(){var e;var d=function(){};var b=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeStamp","trace","warn"];var c=b.length;var a=(window.console=window.console||{});while(c--){e=b[c];if(!a[e]){a[e]=d}}}());
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
 
 // Place any jQuery/helper plugins in here.
 
 $(document).ready(function(){
 
 	
-	// external links
-	$("a[rel='external']").addClass("external").attr('title', function() { return this.title + ' (Opens in New Window)' });
-	$('body').on('click', "a[rel='external']", function() { window.open(this.href); return false; });
+
 
 });
-
-// Google Analytics
-//(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;e=o.createElement(i);r=o.getElementsByTagName(i)[0];e.src='//www.google-analytics.com/analytics.js';r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));ga('create','UA-XXXXX-X');ga('send','pageview');
