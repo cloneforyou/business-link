aeb0f9d9c6dcf313c2259dcc5400c4e3c0ea0abb
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-06-09 16:21:21 +0100

    Updated Header with HTML5 code and tidied up the layout with comments

--- _inc/header.php
+++ _inc/header.php
@@ -1,27 +1,35 @@
-<!-- skip links -->
-<div id="skip">
-	<ul>
-		<li><a href="#skipcont">Go to main content</a></li>
-		<li><a href="#skipleft">Go to main navigation</a></li>
-		<li><a href="#skiphelp">Go to site map, help, search</a></li>
-		<li><a href="#skiplogin">Go to register, log in</a></li>
-		<li><a href="#skipfooter">Go to footer navigation</a></li>
-	</ul>
-</div>
-<!-- end skip links -->
-
-<div id="logo">
-	<a href="home.html" title="Small Business Service home">
-		<img src="/_img/global-business-link-logo.png" alt="Business Link" />
-	</a>
-</div>
-<div id="strapline">Information. Support. Compliance.</div>
-
-<span><a name="skiplogin"></a></span>
-<div id="login">
-	<p>Your account</p>
-	<ul>
-		<li><a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate" id="hdr_register_now">Register</a></li>
-		<li><a href="https://online.businesslink.gov.uk/bdotg/action/BGAuthenticate">Log in</a></li>
-	</ul>
-</div>
+<header>
+	<?php /* 
+		skip links - also consider keyboard access, see also
+		https://www.w3.org/TR/UNDERSTANDING-WCAG20/navigation-mechanisms-skip.html
+		https://www.w3.org/TR/WCAG20-TECHS/G1.html
+	 */ ?>
+	<nav class="skip-links">
+		<h3>Skip content</h3>
+		<ul>
+			<li><a href="#skipcont">Go to main content</a></li>
+			<li><a href="#skipleft">Go to main navigation</a></li>
+			<li><a href="#skiphelp">Go to site map, help, search</a></li>
+			<li><a href="#skiplogin">Go to register, log in</a></li>
+			<li><a href="#skipfooter">Go to footer navigation</a></li>
+		</ul>
+	</nav><!-- end .skip-links -->
+	
+	<div class="branding">
+		<a href="index.php" title="Nusiness Link for Small Businesses">
+			<img src="/_img/global-business-link-logo.png" alt="Business Link logo" />
+		</a>
+		<p class="strapline">Information. Support. Compliance.</p>
+	</div><!-- end .branding -->
+	
+	<a name="skiplogin"></a>
+	
+	<div class="login">
+		<p>Your account</p>
+		<ul>
+			<li><a href="#">Register</a></li>
+			<li><a href="#">Log in</a></li>
+		</ul>
+	</div><!-- end .login -->
+	
+</header>
