af5871ce00aa1bf9c4dc03515b7748a185ae5452
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-06-09 16:20:57 +0100

    Updated Breadcrumbs include, to include Schema structure from Google

--- _inc/breadcrumbs.php
+++ _inc/breadcrumbs.php
@@ -1,6 +1,21 @@
-<!-- Page breadcrumb -->
-<div id="breadcrumb">
-	<p><a href="index.php">Home</a> &gt; [title]</p>
-</div>
+<?php /* 
+	Breadcrumbs - see the following documentation:
+	https://developers.google.com/search/docs/data-types/breadcrumbs#guidelines
+	http://schema.org/BreadcrumbList
+*/ ?>
+<ol itemscope itemtype="http://schema.org/BreadcrumbList" class="breadcrumbs">
+	<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+		<a itemprop="item" href="index.php" title="Business Link for Small Businesses">
+			<span itemprop="name">Home</span>
+		</a>
+		<meta itemprop="position" content="1" />
+	</li>
+	<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+		<a itemprop="item" href="#">
+			<span itemprop="name">Page Title</span>
+		</a>
+		<meta itemprop="position" content="2" />
+	</li>
+</ol>
 
-<span><a name="skipcont" id="skipcont"></a></span>
+<a name="skipcont" id="skipcont"></a>
