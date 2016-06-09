36aa2dfa074227eacc2dbe10f9333027a3b95aae
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-06-09 15:23:05 +0100

    Completed "buy or sell a business > considering selling a business > preparing to sell your business"

---
+++ buy-or-sell-a-business/considering-selling-a-business/preparing-to-sell-your-business.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Preparing to sell your business | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div>
+	<div>
+		<h1>Preparing to sell your business</h1>
+		<h2>Introduction</h2>
+		<span><img src="../../_img/FB1050.gif" width="78" height="118"  alt="" /></span>
+		<p>Selling a business is likely to be the largest and most important financial deal any business owner will ever make.</p>
+		<p>For many owners, selling the business they have spent years building up can be emotionally difficult. Unless you have sold a business before, you will have no experience to draw on and won't know what to expect.</p>
+		<p>This guide outlines the main options available and will help you decide what is best for you and your business. It also covers a few of the basic things which can be done to make your business attractive to potential buyers and has advice on how to find the right advisers.</p>
+		<div>
+			<ul>
+				<li><a href="preparing-to-sell-your-business/is-selling-my-business-the-right-option.php">Next</a></li>
+			</ul>
+		</div>
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li>Introduction</li>
+			<li><a href="preparing-to-sell-your-business/is-selling-my-business-the-right-option.php">Is selling my business the right option?</a></li>
+			<li><a href="preparing-to-sell-your-business/ways-to-sell-your-business.php">Ways to sell your business</a></li>
+			<li><a href="preparing-to-sell-your-business/is-a-sale-realistic.php">Is a sale realistic?</a></li>
+			<li><a href="preparing-to-sell-your-business/when-to-sell-your-business.php">When to sell your business</a></li>
+			<li><a href="preparing-to-sell-your-business/choosing-advisers-when-selling-your-business.php">Choosing advisers when selling your business</a></li>
+			<li><a href="preparing-to-sell-your-business/show-strong-financial-performance.php">Show strong financial performance</a></li>
+			<li><a href="preparing-to-sell-your-business/streamline-your-business-operations.php">Streamline your business operations</a></li>
+			<li><a href="preparing-to-sell-your-business/here-is-how-i-prepared-my-business-for-sale.php">Here's how I prepared my business for sale</a></li>
+		</ul>
+		<p><img src="../../_img/hr_325px_global.gif" alt="" /></p>
+		<div>
+			<strong>Print options</strong> - 
+			<span>
+				<a href="#" title="What are my print options? - Opens in a new window">
+					What are my print options <img src="../../_img/whatisthis.gif" width="9" height="11"  alt="" />
+					<span> - Opens in a new window</span>
+				</a>
+			</span>
+			<ul>
+				<li><a href="#">Add guide to my print portfolio (print later as PDF)</a></li>
+			</ul>
+			<strong>Email options</strong> - 
+			<span>
+				<a href="#" title="What are my email options? - Opens in a new window">
+					What are my email options <img src="../../_img/whatisthis.gif" width="9" height="11"  alt="" />
+					<span> - Opens in a new window</span>
+				</a>
+			</span>
+			<ul>
+				<li><a href="#" title="Tell a friend about this page">Tell a friend about this page</a></li>
+				<li><a href="#" title="Tell a friend about this page">Send me an email alert when this guide changes</a></li>
+			</ul>
+		</div>
+	</div>
+	<div>
+		<div>
+		<h3>Actions</h3>
+			<ul>
+				<li><a href="http://www.lucasweston.com/mistake.htm" target="_blank">Common mistakes in business sales explained on the Lucas &amp; Weston website<span> - Opens in a new window</span></a></li>
+			</ul>
+			<img src="../../_img/px_sellingabusiness.gif" alt="" width="1" height="1" /></div>
+	</div>
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
