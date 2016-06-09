7a0783915963961e9650304e6204815df9c2c374
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-06-09 12:37:25 +0100

    Completed "buy or sell a business > floating on the stock market > advantages and disadvantages of stock market floatation"

---
+++ buy-or-sell-a-business/floating-on-the-stock-market/advantages-and-disadvantages-of-stock-market-flotation.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Advantages and disadvantages of stock market flotation | Business Link</title>
+<meta content="Caroline Murphy">
+<meta content="width=device-width">
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div>
+	<div>
+		<h1>Floating on the stock market</h1>
+		<h2>Advantages and disadvantages of stock market flotation</h2>
+		<p>Even if your business is suited to flotation, it may not be the right choice for you. Being a public company can present a range of benefits to your business, but there are also issues that might require careful consideration.</p>
+		<p>The <strong>benefits</strong> of stock market flotation could include:</p>
+		<ul>
+			<li> giving access to new capital to develop the business</li>
+			<li> making it easier for you and other investors - including venture capitalists - to realise their investment</li>
+			<li> allowing you to offer employees extra incentives by granting share options - this can encourage and motivate your employees to work towards long-term goals</li>
+			<li> placing a value on your business</li>
+			<li> increasing your public profile, and providing reassurance to your customers and suppliers</li>
+			<li> allowing you to do business - eg acquisitions - by using quoted shares as currency</li>
+			<li> creating a market for the company's shares</li>
+		</ul>
+		<p>However, you should also consider the following potential <strong>problems</strong>:</p>
+		<ul>
+			<li> <strong>Market fluctuations</strong> - your business may become vulnerable to market fluctuations beyond your control - including market sentiment, economic conditions or developments in your sector.</li>
+			<li> <strong>Cost</strong> - the costs of flotation can be substantial and there are also ongoing costs of being a public company, such as higher professional fees.</li>
+			<li> <strong>Responsibilities to shareholders</strong> - in return for their capital, you will have to consider shareholders' interests when running the company - which may differ from your own objectives.</li>
+			<li><strong>The need for transparancy</strong> - public companies must comply with a wide range of additional regulatory requirements and meet accepted standards of corporate governance including transparancy, and needing to make announcements about new developments.</li>
+			<li> <strong>Demands on the management team</strong> - managers could be distracted from running the business during the flotation process and through needing to deal with investors afterwards.</li>
+			<li> <strong>Investor relations</strong> - to maximise the benefits of being a public company and attract further investor interest in shares, you will need to keep investors informed.</li>
+			<li> <strong>Employees may become demotivated</strong> - if shares are only offered to selected employees, there could be resentment. Shareholding employees could feel that there is little left to work for if they are sitting on valuable shares.</li>
+		</ul>
+		<p>You can <a href="http://www.londonstockexchange.com/companies-and-advisors/listing/float/float.htm" target="_blank">find out about the advantages of flotation on the London Stock Exchange website<span> - Opens in a new window</span></a>.</p>
+		<div>
+			<ul>
+				<li><a href="is-your-business-suitable-to-be-a-public-company.php" title="Is your business suitable to be a public company?" >Prev</a></li>
+				<li><a href="the-process-of-floating-on-a-stock-market.php" title="The process of floating on a stock market" >Next</a></li>
+			</ul>
+		</div>
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="../floating-on-the-stock-market.php" title="Floating on the stock market" >Introduction</a></li>
+			<li><a href="is-your-business-suitable-to-be-a-public-company.php"  >Is your business suitable to be a public company?</a></li>
+			<li>Advantages and disadvantages of stock market flotation</li>
+			<li><a href="the-process-of-floating-on-a-stock-market.php"  >The process of floating on a stock market</a></li>
+			<li><a href="appoint-your-advisers.php"  >Appoint your advisers</a></li>
+			<li><a href="../../finance-and-grants/finance-options/shares-and-equity-finance/here-is-how-we-prepared-our-business-for-flotation.php"  >Here's how we prepared our business for flotation</a></li>
+		</ul>
+		<p><img src="../../_img/hr_325px_global.gif" alt="" /></p>
+		<div>
+			<strong>Print options</strong> - 
+			<span>
+				<a href="#" title="What are my print options? - Opens in a new window">
+					What are my print options <img src="../../_img/whatisthis.gif" width="9" height="11"  alt="" />
+					<span> - Opens in a new window</span>
+				</a>
+			</span>
+			<ul>
+				<li><a href="#">Add guide to my print portfolio (print later as PDF)</a></li>
+			</ul>
+			<strong>Email options</strong> - 
+			<span>
+				<a href="#" title="What are my email options? - Opens in a new window">
+					What are my email options <img src="../../_img/whatisthis.gif" width="9" height="11"  alt="" />
+					<span> - Opens in a new window</span>
+				</a>
+			</span>
+			<ul>
+				<li><a href="#" title="Tell a friend about this page">Tell a friend about this page</a></li>
+				<li><a href="#" title="Tell a friend about this page">Send me an email alert when this guide changes</a></li>
+			</ul>
+		</div>		
+	</div>
+	<div>
+		<div>
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://www.londonstockexchange.com/companies-and-advisors/listing/float/float.htm" target="_blank">Flotation advantages and disadvantages explained on the London Stock Exchange website <span>- Opens in a new window</span></a></li>
+				<li><a href="../../finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance options for your business</a></li>
+			</ul>
+			</ul>
+			<img src="../../_img/px_sellingabusiness.gif" alt="" width="1" height="1" /></div>
+		<div>
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="../../finance-and-grants/finance-options/shares-and-equity-finance/equity-finance.php">Equity finance</a></li>
+				<li><a href="../../finance-and-grants/finance-options/shares-and-equity-finance/shares-and-shareholders.php">Shares and shareholders</a></li>
+				<li><a href="../considering-selling-a-business/value-and-market-your-business.php">Value and market your business</a></li>
+				<li><a href="../../grow-your-business/setting-a-vision-for-growth/assess-your-options-for-growth.html">Assess your options for growth</a></li>
+			</ul>
+			<img src="../../_img/px_sellingabusiness.gif" alt="" width="1" height="1" /></div>
+	</div>
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
