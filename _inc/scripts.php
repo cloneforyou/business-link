b5b9f982f1374d84c9ec56408279a569817b8e71
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-06-22 20:47:15 +0100

    Does included jQuery fix the 'Cannot read property 'id' of undefined' console error with foundation.core.js ?

--- _inc/scripts.php
+++ _inc/scripts.php
@@ -1,5 +1,7 @@
 <?php /* jQuery core on CDN. Time of update, v2.2.4, does not support IE<9 */ ?>
-<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+<?php /* ?><script src="https://code.jquery.com/jquery-2.2.4.min.js"></script><?php */ ?>
+
+<script src="<?php $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']); ?>/_vendor/js/jquery.min.js"></script>
 <?php /* Load What-Input files in footer */ ?>
 <script src="<?php $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']); ?>/_vendor/what-input/what-input.min.js"></script>
 <?php /* Adding Foundation scripts file in the footer */ ?>
