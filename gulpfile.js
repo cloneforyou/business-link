016b135ca40f384f1402f20eecabea8a0110438d
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-06-22 16:15:36 +0100

    Added image min. Updated the code to ensure the paths were correct. Outputting to assets from _assets/_vendor

--- gulpfile.js
+++ gulpfile.js
@@ -1,125 +1,137 @@
 // Grab our gulp packages
 var gulp  = require('gulp'),
-    gutil = require('gulp-util'),
-    sass = require('gulp-sass'),
-    cssnano = require('gulp-cssnano'),
-    autoprefixer = require('gulp-autoprefixer'),
-    sourcemaps = require('gulp-sourcemaps'),
-    jshint = require('gulp-jshint'),
-    stylish = require('jshint-stylish'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    rename = require('gulp-rename'),
-    plumber = require('gulp-plumber'),
-    bower = require('gulp-bower'),
-    babel = require('gulp-babel'),
-    browserSync = require('browser-sync').create();
+	gutil = require('gulp-util'),
+	imagemin = require('gulp-imagemin'), 
+	pngquant = require('imagemin-pngquant'), 
+	sass = require('gulp-sass'),
+	cssnano = require('gulp-cssnano'),
+	autoprefixer = require('gulp-autoprefixer'),
+	sourcemaps = require('gulp-sourcemaps'),
+	jshint = require('gulp-jshint'),
+	stylish = require('jshint-stylish'),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat'),
+	rename = require('gulp-rename'),
+	plumber = require('gulp-plumber'),
+	bower = require('gulp-bower'),
+	babel = require('gulp-babel'),
+	browserSync = require('browser-sync').create();
+
+// Added gulp-imagemin
+gulp.task('imagemin', function() {
+  gulp.src('./_assets/images/*')
+	.pipe(imagemin({
+			progressive: true,
+			use: [pngquant()]
+		}))
+	.pipe(gulp.dest('./assets/images/min/'))
+});
 
 // Compile Sass, Autoprefix and minify
 gulp.task('styles', function() {
-    return gulp.src('./_assets/scss/**/*.scss')
-        .pipe(plumber(function(error) {
-            gutil.log(gutil.colors.red(error.message));
-            this.emit('end');
-        }))
-        .pipe(sourcemaps.init()) // Start Sourcemaps
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('./_assets/css/'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(cssnano())
-        .pipe(sourcemaps.write('.')) // Creates sourcemaps for minified styles
-        .pipe(gulp.dest('./__assets/css/'))
+	return gulp.src('./_assets/scss/**/*.scss')
+		.pipe(plumber(function(error) {
+			gutil.log(gutil.colors.red(error.message));
+			this.emit('end');
+		}))
+		.pipe(sourcemaps.init()) // Start Sourcemaps
+		.pipe(sass())
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('./assets/css/'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(cssnano())
+		.pipe(sourcemaps.write('.')) // Creates sourcemaps for minified styles
+		.pipe(gulp.dest('./assets/css/'))
 });
-    
+	
 // JSHint, concat, and minify JavaScript
 gulp.task('site-js', function() {
   return gulp.src([	
-	  
-           // Grab your custom scripts
-  		  './__assets/js/scripts/*.js'
-  		  
+
+		// Grab your custom scripts
+		'./_assets/js/scripts/*.js'
+
   ])
-    .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('./__assets/js'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(sourcemaps.write('.')) // Creates sourcemap for minified JS
-    .pipe(gulp.dest('./__assets/js'))
+	.pipe(plumber())
+	.pipe(sourcemaps.init())
+	.pipe(jshint())
+	.pipe(jshint.reporter('jshint-stylish'))
+	.pipe(concat('scripts.js'))
+	.pipe(gulp.dest('./assets/js'))
+	.pipe(rename({suffix: '.min'}))
+	.pipe(uglify())
+	.pipe(sourcemaps.write('.')) // Creates sourcemap for minified JS
+	.pipe(gulp.dest('./assets/js'))
 });    
 
 // JSHint, concat, and minify Foundation JavaScript
 gulp.task('foundation-js', function() {
   return gulp.src([	
-  		  
-  		  // Foundation core - needed if you want to use any of the components below
-          './_vendor/foundation-sites/js/foundation.core.js',
-          './_vendor/foundation-sites/js/foundation.util.*.js',
-          
-          // Pick the components you need in your project
-          './_vendor/foundation-sites/js/foundation.abide.js',
-          './_vendor/foundation-sites/js/foundation.accordion.js',
-          './_vendor/foundation-sites/js/foundation.accordionMenu.js',
-          './_vendor/foundation-sites/js/foundation.drilldown.js',
-          './_vendor/foundation-sites/js/foundation.dropdown.js',
-          './_vendor/foundation-sites/js/foundation.dropdownMenu.js',
-          './_vendor/foundation-sites/js/foundation.equalizer.js',
-          './_vendor/foundation-sites/js/foundation.interchange.js',
-          './_vendor/foundation-sites/js/foundation.magellan.js',
-          './_vendor/foundation-sites/js/foundation.offcanvas.js',
-          './_vendor/foundation-sites/js/foundation.orbit.js',
-          './_vendor/foundation-sites/js/foundation.responsiveMenu.js',
-          './_vendor/foundation-sites/js/foundation.responsiveToggle.js',
-          './_vendor/foundation-sites/js/foundation.reveal.js',
-          './_vendor/foundation-sites/js/foundation.slider.js',
-          './_vendor/foundation-sites/js/foundation.sticky.js',
-          './_vendor/foundation-sites/js/foundation.tabs.js',
-          './_vendor/foundation-sites/js/foundation.toggler.js',
-          './_vendor/foundation-sites/js/foundation.tooltip.js',
+
+		// Foundation core - needed if you want to use any of the components below
+		'./vendor/foundation-sites/js/foundation.core.js',
+		'./vendor/foundation-sites/js/foundation.util.*.js',
+
+		// Pick the components you need in your project
+		// './vendor/foundation-sites/js/foundation.abide.js',
+		// './vendor/foundation-sites/js/foundation.accordion.js',
+		// './vendor/foundation-sites/js/foundation.accordionMenu.js',
+		// './vendor/foundation-sites/js/foundation.drilldown.js',
+		// './vendor/foundation-sites/js/foundation.dropdown.js',
+		// './vendor/foundation-sites/js/foundation.dropdownMenu.js',
+		// './vendor/foundation-sites/js/foundation.equalizer.js',
+		// './vendor/foundation-sites/js/foundation.interchange.js',
+		// './vendor/foundation-sites/js/foundation.magellan.js',
+		// './vendor/foundation-sites/js/foundation.offcanvas.js',
+		// './vendor/foundation-sites/js/foundation.orbit.js',
+		// './vendor/foundation-sites/js/foundation.responsiveMenu.js',
+		// './vendor/foundation-sites/js/foundation.responsiveToggle.js',
+		// './vendor/foundation-sites/js/foundation.reveal.js',
+		// './vendor/foundation-sites/js/foundation.slider.js',
+		// './vendor/foundation-sites/js/foundation.sticky.js',
+		// './vendor/foundation-sites/js/foundation.tabs.js',
+		// './vendor/foundation-sites/js/foundation.toggler.js',
+		// './vendor/foundation-sites/js/foundation.tooltip.js',
   ])
 	.pipe(babel({
 		presets: ['es2015'],
-	    compact: true
+		compact: true
 	}))
-    .pipe(sourcemaps.init())
-    .pipe(concat('foundation.js'))
-    .pipe(gulp.dest('./_assets/js'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(sourcemaps.write('.')) // Creates sourcemap for minified Foundation JS
-    .pipe(gulp.dest('./_assets/js'))
+	.pipe(sourcemaps.init())
+	.pipe(concat('foundation.js'))
+	.pipe(gulp.dest('./assets/js'))
+	.pipe(rename({suffix: '.min'}))
+	.pipe(uglify())
+	.pipe(sourcemaps.write('.')) // Creates sourcemap for minified Foundation JS
+	.pipe(gulp.dest('./assets/js'))
 }); 
 
-// Update Foundation with Bower and save to /_vendor
+// Update Foundation with Bower and save to /vendor
 gulp.task('bower', function() {
   return bower({ cmd: 'update'})
-    .pipe(gulp.dest('_vendor/'))
+	.pipe(gulp.dest('vendor/'))
 });  
 
 // Browser-Sync watch files and inject changes
 gulp.task('browsersync', function() {
-    // Watch files
-    var files = [
-    	'./_assets/css/*.css', 
-    	'./_assets/js/*.js',
-    	'**/*.php',
-    	'_assets/images/**/*.{png,jpg,gif,svg,webp}',
-    ];
+	// Watch files
+	var files = [
+		'./_assets/css/*.css', 
+		'./_assets/js/*.js',
+		'**/*.php',
+		'_assets/images/**/*.{png,jpg,gif,svg,webp}',
+	];
 
-    browserSync.init(files, {
-	    // Replace with URL of your local site
-	    proxy: "http://localhost/",
-    });
-    
-    gulp.watch('./_assets/scss/**/*.scss', ['styles']);
-    gulp.watch('./_assets/js/scripts/*.js', ['site-js']).on('change', browserSync.reload);
+	browserSync.init(files, {
+		// Replace with URL of your local site
+		proxy: "http://localhost/",
+	});
+	
+	gulp.watch('./_assets/scss/**/*.scss', ['styles']);
+	gulp.watch('./_assets/js/scripts/*.js', ['site-js']).on('change', browserSync.reload);
 
 });
 
@@ -139,5 +151,6 @@ gulp.task('watch', function() {
 
 // Run styles, site-js and foundation-js
 gulp.task('default', function() {
-  gulp.start('styles', 'site-js', 'foundation-js');
+  gulp.start('imagemin', 'styles', 'site-js', 'foundation-js');
 });
+
