ff051724129262df7078a87a806a2ce493cbc42d
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-06-22 17:07:19 +0100

    Added foundation divs to the HEADER include

--- _inc/header.php
+++ _inc/header.php
@@ -1,35 +1,41 @@
-<header>
-	<?php /* 
-		skip links - also consider keyboard access, see also
-		https://www.w3.org/TR/UNDERSTANDING-WCAG20/navigation-mechanisms-skip.html
-		https://www.w3.org/TR/WCAG20-TECHS/G1.html
-	 */ ?>
-	<nav class="skip-links">
-		<h3>Skip content</h3>
-		<ul>
-			<li><a href="#skipcont">Go to main content</a></li>
-			<li><a href="#skipleft">Go to main navigation</a></li>
-			<li><a href="#skiphelp">Go to site map, help, search</a></li>
-			<li><a href="#skiplogin">Go to register, log in</a></li>
-			<li><a href="#skipfooter">Go to footer navigation</a></li>
-		</ul>
-	</nav><!-- end .skip-links -->
-	
-	<div class="branding">
-		<a href="index.php" title="Nusiness Link for Small Businesses">
-			<img src="/_img/global-business-link-logo.png" alt="Business Link logo" />
-		</a>
-		<p class="strapline">Information. Support. Compliance.</p>
-	</div><!-- end .branding -->
-	
-	<a name="skiplogin"></a>
-	
-	<div class="login">
-		<p>Your account</p>
-		<ul>
-			<li><a href="#">Register</a></li>
-			<li><a href="#">Log in</a></li>
-		</ul>
-	</div><!-- end .login -->
-	
-</header>
+<div class="row">
+	<div class="large-12 columns">
+
+		<header>
+			<?php /* 
+				skip links - also consider keyboard access, see also
+				https://www.w3.org/TR/UNDERSTANDING-WCAG20/navigation-mechanisms-skip.html
+				https://www.w3.org/TR/WCAG20-TECHS/G1.html
+			 */ ?>
+			<nav class="skip-links">
+				<h3>Skip content</h3>
+				<ul>
+					<li><a href="#skipcont">Go to main content</a></li>
+					<li><a href="#skipleft">Go to main navigation</a></li>
+					<li><a href="#skiphelp">Go to site map, help, search</a></li>
+					<li><a href="#skiplogin">Go to register, log in</a></li>
+					<li><a href="#skipfooter">Go to footer navigation</a></li>
+				</ul>
+			</nav><!-- end .skip-links -->
+			
+			<div class="branding">
+				<a href="index.php" title="Nusiness Link for Small Businesses">
+					<img src="/_img/global-business-link-logo.png" alt="Business Link logo" />
+				</a>
+				<p class="strapline">Information. Support. Compliance.</p>
+			</div><!-- end .branding -->
+			
+			<a name="skiplogin"></a>
+			
+			<div class="login">
+				<p>Your account</p>
+				<ul>
+					<li><a href="#">Register</a></li>
+					<li><a href="#">Log in</a></li>
+				</ul>
+			</div><!-- end .login -->
+			
+		</header>
+
+	</div><!-- //large-12 columns -->
+</div><!-- //row -->
