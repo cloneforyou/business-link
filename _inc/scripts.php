c4489634a625f7e1c0e82ad72c8db0f3f5cc6991
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-06-27 16:37:10 +0100

    Changes version of jQuery to match dev.inspire, as that works fine (error still showing at this point)

--- _inc/scripts.php
+++ _inc/scripts.php
@@ -1,5 +1,5 @@
 <?php /* jQuery core on CDN. Time of update, v2.2.4, does not support IE<9 */ ?>
-<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 <?php /* Load What-Input files in footer */ ?>
 <script src="<?php $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']); ?>/_vendor/what-input/what-input.min.js"></script>
 <?php /* Adding Foundation scripts file in the footer */ ?>
