6ab3cc6567c6dd94d532a46420f007de0ee1cf23
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-06-27 17:20:25 +0100

    Added favicons, apple-touch-icon, win8-tile-icon. Added meta links for the files

--- _inc/meta.php
+++ _inc/meta.php
@@ -10,3 +10,12 @@
 <!-- Styles -->
 <?php /* reference : http://php.net/manual/en/function.dirname.php#114600 */ ?>
 <link href="<?php $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']); ?>/assets/css/style.min.css" rel="stylesheet">
+
+<link rel="icon" href="<?php $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']); ?>/favicon.png">
+<link href="<?php $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']); ?>/_img/apple-icon-touch.png" rel="apple-touch-icon" />
+<!--[if IE]>
+	<link rel="shortcut icon" href="<?php $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']); ?>/favicon.ico">
+<![endif]-->
+<meta name="msapplication-TileColor" content="#f01d4f">
+<meta name="msapplication-TileImage" content="<?php $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']); ?>/_img/win8-tile-icon.png">
+<meta name="theme-color" content="#121212">
