fbecf1c94f429366ee0fbd57aba237e43a234dc3
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-07-21 00:34:16 +0100

    Completed 'How to check if a director has been disqualified or if an individual is an undischarged bankrupt or subject to bankruptcy restrictions'.

---
+++ buy-or-sell-a-business/businesses-in-difficulty/complain-against-an-undischarged-bankrupt-or-disqualified-company-director/how-to-check-if-a-director-has-been-disqualified-or-if-an-individual-is-an-undischarged-bankrupt-or-subject-to-bankruptcy-restrictions.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>How to check if a director has been disqualified or if an individual is an undischarged bankrupt or subject to bankruptcy restrictions | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Complain against an undischarged bankrupt or disqualified company director</h1>
+		<h2>How to check if a director has been disqualified or if an individual is an undischarged bankrupt or subject to bankruptcy restrictions</h2>
+		<h3>Disqualified Directors Register</h3>
+		<p><strong>Before</strong> reporting any breach of disqualification to The Insolvency Service, you should check the Disqualified Directors Register to ensure the director has been disqualified.</p>
+		<p>The Disqualified Directors Register is kept by Companies House on behalf of the Secretary of State and lists all directors that are currently prohibited from involving themselves with a company. You can <a href="../../../do-it-online/search-company-information-using-companies-house-webcheck.php">find the Disqualified Directors Register on the Companies House website</a>.</p>
+		<h3>Individual Insolvency Register</h3>
+		<p><strong>Before</strong> reporting any breach of bankruptcy or bankruptcy restrictions to The Insolvency Service, you should check the Individual Insolvency Register to ensure the person is an undischarged bankrupt or is subject to bankruptcy restrictions.</p>
+		<p><a href="../../../do-it-online/search-the-individual-insolvency-register.php">Search the Individual Insolvency Register with The Insolvency Service</a>.</p>
+		<h3>Reporting disqualified directors, undischarged bankrupts and persons subject to bankruptcy restrictions</h3>
+		<p>If you suspect that an individual is acting in breach of a disqualification order, a disqualification undertaking or a bankruptcy order, you should complete the Insolvency Service Hotline questionnaire and send it to The Insolvency Service. The information in the completed questionnaire will help The Insolvency Service make enquiries into your allegations.</p>
+		<p>For guidance on what information you should provide and what The Insolvency Service will do after the complaint, see the page in this guide on <a href="complaining-about-disqualified-directors-undischarged-bankrupts-and-persons-subject-to-bankruptcy-restrictions.php">complaining about disqualified directors, undischarged bankrupts and persons subject to bankruptcy restrictions</a>.</p>
+		<ul>
+			<li><a href="what-undischarged-bankrupts-and-persons-subject-to-bankruptcy-restrictions-are-not-allowed-to-do.php">Prev</a></li>
+			<li><a href="complaining-about-disqualified-directors-undischarged-bankrupts-and-persons-subject-to-bankruptcy-restrictions.php">Next</a></li>
+		</ul>
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="../complain-against-an-undischarged-bankrupt-or-disqualified-company-director.php">Introduction</a></li>
+			<li><a href="what-undischarged-bankrupts-and-persons-subject-to-bankruptcy-restrictions-are-not-allowed-to-do.php">What undischarged bankrupts and persons subject to bankruptcy restrictions are not allowed to do</a></li>
+			<li>How to check if a director has been disqualified or if an individual is an undischarged bankrupt or subject to bankruptcy restrictions</li>
+			<li><a href="complaining-about-disqualified-directors-undischarged-bankrupts-and-persons-subject-to-bankruptcy-restrictions.php">Complaining about disqualified directors, undischarged bankrupts and persons subject to bankruptcy restrictions</a></li>
+		</ul>
+		<p><img src="../../../_img/hr_325px_global.gif" alt="" /></p>
+		<div id="additionalcontent">
+			<h3>Insolvency Service Enforcement Hotline</h3>
+			<p>0845 601 3546</p>
+			<h3>Insolvency Service Enquiry Line</h3>
+			<p>0845 602 9848</p>
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+			<p><img src="../../../_img/hr_140px_global.gif" alt="" /></p>
+		</div>
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="../../../do-it-online/search-company-information-using-companies-house-webcheck.php">Disqualified Directors Register on the Companies House website</a></li>
+			<li><a href="../../../do-it-online/search-the-individual-insolvency-register.php">Search the Individual Insolvency Register with The Insolvency Service</a></li>
+			<li><a href="../../../do-it-online/use-the-enforcement-hotline.php">Complete the Enforcement Hotline questionnaire with The Insolvency Service</a></li>
+			<li><a href="../../../do-it-online/insolvency-service-online-complaints.php">Make a complaint to The Insolvency Service</a></li>
+		</ul>
+		<img src="../../../_img/px_sellingabusiness.gif" alt="" width="1" height="1" /></div>
+	<div>
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="../../../finance-and-grants/insolvency-information-for-sole-traders-and-individuals/how-bankruptcy-affects-you-and-your-business/bankruptcy-what-happens-to-your-bank-account.php">Bankruptcy: what happens to your bank account</a></li>
+			<li><a href="../../../finance-and-grants/insolvency-information-for-limited-companies/director-disqualification.php">Director disqualification</a></li>
+			<li><a href="../../../finance-and-grants/how-to-complain-against-a-company-or-limited-liability-partnership.php">How to complain against a company or limited liability partnership</a></li>
+			<li><a href="../../../finance-and-grants/introduction-to-insolvency/insolvency-the-basics.php">Insolvency: the basics</a></li>
+		</ul>
+		<img src="../../../_img/px_sellingabusiness.gif" alt="" width="1" height="1" /></div>
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
