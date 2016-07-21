4f36d7efb821494ea2e9055e4b096324a03db68a
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-07-21 02:06:59 +0100

    Completed 'Is a sale realistic?'

---
+++ buy-or-sell-a-business/considering-selling-a-business/preparing-to-sell-your-business/is-a-sale-realistic.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Is a sale realistic? | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+		<h1>Preparing to sell your business</h1>
+		<h2>Is a sale realistic?</h2>
+		<p>You can only sell your business if someone is prepared to pay for it. If you can't identify strong reasons - that can be easily substantiated - why your business would make a good acquisition, it's likely to be difficult to find a buyer.</p>
+		<p>Ask yourself the following questions:</p>
+		<ul>
+			<li>Is the business <b>healthy</b>? A business in trouble is difficult to sell and potential buyers are likely to wait until they can get assets at a knockdown price - although sometimes financial distress can be a motivating factor to a buyer who can see a turnaround opportunity.</li>
+			<li> Are the <b>basics</b> in place to make the business attractive? Buyers like well-organised businesses with strong management. For more information, see the page in this guide on how to <a href="streamline-your-business-operations.php">streamline your business operations</a>.</li>
+			<li> Does the business have a <b>good financial record</b>? Buyers prefer a record of smoothly increasing profits with good growth potential. For more information, see the page in this guide on how to <a href="../preparing-to-sell-your-business.php">show strong financial performance</a>.</li>
+			<li> Can you identify <b>potential trade buyers</b> and a good reason why they should want to buy your business? Buying a business can be disruptive and expensive. Potential buyers may prefer to concentrate on their existing operations.</li>
+			<li> Are the <b>existing management team</b> interested in buying the business? You may find that they are the only potential buyer and that they only offer a modest price.</li>
+		</ul>
+		<p>It usually pays to start planning a sale well in advance. This gives you time to groom the business - fixing any issues which could dramatically affect its value and making it as attractive as possible to potential buyers.</p>
+		<p>You may also want to get a preliminary valuation before you offer it for sale. For more information read our guide on how to <a href="../value-and-market-your-business.php">value and market your business</a>.</p>
+		<ul>
+			<li><a href="ways-to-sell-your-business.php">Prev</a></li>
+			<li><a href="when-to-sell-your-business.php">Next</a></li>
+		</ul>
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="../preparing-to-sell-your-business.php">Introduction</a></li>
+			<li><a href="is-selling-my-business-the-right-option.php">Is selling my business the right option?</a></li>
+			<li><a href="ways-to-sell-your-business.php">Ways to sell your business</a></li>
+			<li>Is a sale realistic?</li>
+			<li><a href="when-to-sell-your-business.php">When to sell your business</a></li>
+			<li><a href="choosing-advisers-when-selling-your-business.php">Choosing advisers when selling your business</a></li>
+			<li><a href="../preparing-to-sell-your-business.php">Show strong financial performance</a></li>
+			<li><a href="streamline-your-business-operations.php">Streamline your business operations</a></li>
+			<li><a href="here-is-how-i-prepared-my-business-for-sale.php">Here's how I prepared my business for sale</a></li>
+		</ul>
+		<p><img src="../../../_img/hr_325px_global.gif" alt="" /></p>
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.lucasweston.com/top-ten-business-seller-mistakes.htm" target="_blank">Common mistakes in business sales explained on the Lucas &amp; Weston website<span> - Opens in a new window</span></a></li>
+			<li><a href="http://livingstonepartners.com/uk/" target="_blank">Business selling advice on the Livingstone website<span> - Opens in a new window</span></a></li>
+			<li><a href="#">Download succession planning guidance from the Deloitte website (PDF, 120K)</a></li>
+			<li><a href="../../../tax-payroll-and-company-information/selling-or-closing-your-business/check-tax-and-legal-issues-when-selling-a-business.php">Use our interactive tool to investigate the tax and legal issues when selling or closing your business</a></li>
+		</ul>
+		<img src="../../../_img/px_sellingabusiness.gif" alt="" width="181" height="5" /></div>
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="../consider-your-exit-strategy-when-starting-up.php">Consider your exit strategy when starting up</a></li>
+			<li><a href="../../transferring-your-business/selling-or-passing-a-business-to-a-family-member.php">Selling or passing a business to a family member</a></li>
+			<li><a href="../value-and-market-your-business.php">Value and market your business</a></li>
+			<li><a href="../../selling/complete-the-sale-of-your-business.php">Complete the sale of your business</a></li>
+			<li><a href="../../../tax-payroll-and-company-information/selling-or-closing-your-business/selling-or-closing-your-business-an-overview.php">Selling or closing your business - an overview</a></li>
+		</ul>
+		<img src="../../../_img/px_sellingabusiness.gif" alt="" width="181" height="5" /></div>
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
