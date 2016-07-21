3255419489a187bf198d137b33d9986352a1563b
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-07-21 01:52:46 +0100

    Completed 'Ways to sell your business'

---
+++ buy-or-sell-a-business/considering-selling-a-business/preparing-to-sell-your-business/ways-to-sell-your-business.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ways to sell your business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+		<h1>Preparing to sell your business</h1>
+		<h2>Ways to sell your business</h2>
+		<p>There are various ways you could sell your business, with the options available depending on factors like your business' type, size and sector. Most businesses are sold in a trade sale to another business - usually to one operating in the same or a related field.</p>
+		<p>Other options available to you could include:</p>
+		<ul>
+			<li>finding a private-equity buyer</li>
+			<li>a management/employee buyout - perhaps with the help of a venture capital firm or bank loan</li>
+			<li>attracting a private investor</li>
+		</ul>
+		<p>There are several different sale options - the one best for you will depend on your individual circumstances and the legal status of your business. The buyer will also have an opinion on deal structure and how they wish to make an acquisition, so you'll need to know what you want to achieve and how you would like to structure a sale early on. This will save time and money, and avoid unnecessary delays.</p>
+		<h3>Partial or full sale</h3>
+		<p>You may want to sell the entire business or keep a small stake in it. The buyer may prefer you to retain partial ownership and continue your involvement. This can give the business continuity and the buyer confidence that the business will do well.</p>
+		<h3>Sale of assets</h3>
+		<p>Instead of selling the business itself, you could sell assets like equipment, intellectual property or your customer list. This may be attractive to a buyer who doesn't want to take on liabilities and obligations.</p>
+		<p>For example, the buyer might not want to take on your employees. You will be left with whatever assets and liabilities are not included in the sale. In this case, tax and legal advice is an essential factor in deciding the most suitable deal structure.</p>
+		<h3>Immediate or phased payment</h3>
+		<p>You can ask for payment in full when the sale is completed, or you may be prepared to accept payment in instalments. The buyer may well prefer to pay in instalments. But you will be at risk, for example if the buyer cannot make future payments.</p>
+		<p>Some buyers will want to make a series of payments based on profits, in which case you may be contracted to stay with the business for a period of time. This is often known as an 'earn out'.</p>
+		<p>Your choices can affect whether buyers are interested and how much they are prepared to offer. They can also affect the tax treatment of the sale. <a href="../../../tax-payroll-and-company-information/selling-or-closing-your-business/check-tax-and-legal-issues-when-selling-a-business.php">Use our interactive tool to investigate the tax and legal issues when selling or closing your business</a>.</p>
+		<p>You can get guidance from an adviser. For help in selecting one, see the page in this guide on <a href="choosing-advisers-when-selling-your-business.php">choosing advisers when selling your business</a>.</p>
+		<ul>
+			<li><a href="is-selling-my-business-the-right-option.php">Prev</a></li>
+			<li><a href="is-a-sale-realistic.php">Next</a></li>
+		</ul>
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="../preparing-to-sell-your-business.php">Introduction</a></li>
+			<li><a href="is-selling-my-business-the-right-option.php">Is selling my business the right option?</a></li>
+			<li>Ways to sell your business</li>
+			<li><a href="is-a-sale-realistic.php">Is a sale realistic?</a></li>
+			<li><a href="when-to-sell-your-business.php">When to sell your business</a></li>
+			<li><a href="choosing-advisers-when-selling-your-business.php">Choosing advisers when selling your business</a></li>
+			<li><a href="../preparing-to-sell-your-business.php">Show strong financial performance</a></li>
+			<li><a href="streamline-your-business-operations.php">Streamline your business operations</a></li>
+			<li>Here's how I prepared my business for sale</li>
+		</ul>
+		<p><img src="../../../_img/hr_325px_global.gif" alt="" /></p>
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://livingstonepartners.com/uk/" target="_blank">Business selling advice on the Livingstone website<span> - Opens in a new window</span></a></li>
+			<li><a href="#">Download succession planning guidance from the Deloitte website (PDF, 120K)</a></li>
+			<li><a href="../../../tax-payroll-and-company-information/selling-or-closing-your-business/check-tax-and-legal-issues-when-selling-a-business.php">Use our interactive tool to investigate the tax and legal issues when selling or closing your business</a></li>
+		</ul>
+		<img src="../../../_img/px_sellingabusiness.gif" alt="" width="181" height="5" /></div>
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="../consider-your-exit-strategy-when-starting-up.php">Consider your exit strategy when starting up</a></li>
+			<li><a href="../../transferring-your-business/selling-or-passing-a-business-to-a-family-member.php">Selling or passing a business to a family member</a></li>
+			<li><a href="../../selling/how-to-achieve-an-employee-buyout.php">How to achieve an employee buyout</a></li>
+			<li><a href="../value-and-market-your-business.php">Value and market your business</a></li>
+			<li><a href="../../selling/complete-the-sale-of-your-business.php">Complete the sale of your business</a></li>
+		</ul>
+		<img src="../../../_img/px_sellingabusiness.gif" alt="" width="181" height="5" /></div>
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
