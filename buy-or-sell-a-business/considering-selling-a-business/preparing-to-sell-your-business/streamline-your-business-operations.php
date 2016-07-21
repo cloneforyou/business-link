4f79caf758d53a6b9e4810805098dc52ef68b5fd
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-07-21 01:59:07 +0100

    Completed 'Streamline your business operations'

---
+++ buy-or-sell-a-business/considering-selling-a-business/preparing-to-sell-your-business/streamline-your-business-operations.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Streamline your business operations | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+		<h1>Preparing to sell your business</h1>
+		<h2>Streamline your business operations</h2>
+		<p>The more confidence a buyer has in your business, the more attractive your business will become and the higher the price they are likely to offer. It's essential to set out a clearly defined strategy in your <b>business plan</b>.</p>
+		<p>You also need to show that you have got a <b>strong management team</b> in place. If your business is too dependent on your own skills, it might damage the price it can fetch - and could even make it impossible to sell. Appointing deputy or departmental managers can enhance a company's value by alleviating that risk. You may also want to encourage key employees to stay by considering appropriate incentive schemes.</p>
+		<p>Aim to <strong>reduce your dependence</strong> on too few customers or on one or two key suppliers. Show how your <b>customer base</b> is expanding and formalise any informal deals you have with customers and suppliers.</p>
+		<p>You should also:</p>
+		<ul>
+			<li> ensure you're complying with health and safety, employment and other legislation - consider asking your legal advisers to review the business</li>
+			<li> settle any legal disputes</li>
+			<li> make sure you have clear ownership of any intellectual property - see our guide on <a href="../../../create-innovate-and-protect/protecting-your-intellectual-property/find-value-you-didnt-know-existed-in-your-business/protecting-intellectual-property.php">protecting intellectual property</a></li>
+			<li> ensure property contracts are sorted out</li>
+			<li> put in place suitable management information systems</li>
+			<li> ensure your finances are in good order - see the page in this guide on how to <a href="../preparing-to-sell-your-business.php">show strong financial performance</a></li>
+		</ul>
+		<p>The sooner you start planning, the more effectively you can do all this. There is a strong case for setting out your exit strategy in your original business plan. This will prevent sudden or misguided decisions about leaving the business which could leave you financially worse off or make the business less attractive to potential buyers.</p>
+		<p>Throughout the sale process, continue to demonstrate that you will be flexible and co-operative. Show that you would also be willing to spend some time after the sale helping the buyer get acclimatised to the business. If you think it will help the sale, be prepared to work for the company for a fixed period after the sale is completed.</p>
+		<ul>
+			<li><a href="../preparing-to-sell-your-business.php">Prev</a></li>
+			<li><a href="here-is-how-i-prepared-my-business-for-sale.php">Next</a></li>
+		</ul>
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="../preparing-to-sell-your-business.php">Introduction</a></li>
+			<li><a href="is-selling-my-business-the-right-option.php">Is selling my business the right option?</a></li>
+			<li><a href="ways-to-sell-your-business.php">Ways to sell your business</a></li>
+			<li><a href="is-a-sale-realistic.php">Is a sale realistic?</a></li>
+			<li><a href="when-to-sell-your-business.php">When to sell your business</a></li>
+			<li><a href="choosing-advisers-when-selling-your-business.php">Choosing advisers when selling your business</a></li>
+			<li><a href="../preparing-to-sell-your-business.php">Show strong financial performance</a></li>
+			<li>Streamline your business operations</li>
+			<li><a href="here-is-how-i-prepared-my-business-for-sale.php">Here's how I prepared my business for sale</a></li>
+		</ul>
+		<p><img src="../../../_img/hr_325px_global.gif" alt="" /></p>
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="#">Download succession planning guidance from the Deloitte website (PDF, 120K)</a></li>
+			<li><a href="http://livingstonepartners.com/uk/" target="_blank">Business selling advice on the Livingstone website<span> - Opens in a new window</span></a></li>
+			<li><a href="../../../tax-payroll-and-company-information/selling-or-closing-your-business/check-tax-and-legal-issues-when-selling-a-business.php">Use our interactive tool to investigate the tax and legal issues when selling or closing your business</a></li>
+		</ul>
+		<img src="../../../_img/px_sellingabusiness.gif" alt="" width="181" height="5" /></div>
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="../../../create-innovate-and-protect/protecting-your-intellectual-property/find-value-you-didnt-know-existed-in-your-business/protecting-intellectual-property.php">Protecting intellectual property</a></li>
+			<li><a href="../consider-your-exit-strategy-when-starting-up.php">Consider your exit strategy when starting up</a></li>
+			<li><a href="../../selling/complete-the-sale-of-your-business.php">Complete the sale of your business</a></li>
+			<li><a href="../../../employment-and-skills/redundancy-restructures-and-change/responsibilities-to-employees-if-you-buy-or-sell-a-business.html">Responsibilities to employees if you buy or sell a business</a></li>
+			<li><a href="../value-and-market-your-business.php">Value and market your business</a></li>
+			<li><a href="../../../employment-and-skills/pay-and-pensions/implement-staff-incentive-schemes.php">Implement staff incentive schemes</a></li>
+		</ul>
+		<img src="../../../_img/px_sellingabusiness.gif" alt="" width="181" height="5" /></div>
+	
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
