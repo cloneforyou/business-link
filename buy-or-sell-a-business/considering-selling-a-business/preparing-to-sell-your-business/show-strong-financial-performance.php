b79c66bf2993492fdd9b67839798bbd941083366
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-07-21 02:01:28 +0100

    Completed "Show strong financial performance"

---
+++ buy-or-sell-a-business/considering-selling-a-business/preparing-to-sell-your-business/show-strong-financial-performance.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Show strong financial performance | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+		<h1>Preparing to sell your business</h1>
+		<h2>Show strong financial performance</h2>
+		<p>Planning well ahead helps you ensure that your business has a financial record that attracts buyers. A first step is to ensure that your finances are in good order. Although this should be the case at any time, planning to sell your business can push you to focus on this area.</p>
+		<p>One major area is <strong>control of working capital</strong>, through reducing stock levels and controlling creditors. There may also be opportunities to <strong>cut costs</strong>, such as renegotiating supply contracts and eliminating unnecessary perks. You can also sell underused equipment to <strong>reduce debt</strong>.</p>
+		<p>You will want to present your accounts as attractively as possible. Buyers usually prefer businesses that show increasing profits year on year. If possible, your financial performance should be reasonably stable throughout the year. You may be able to bring forward or delay purchases and sales to help with this. You may also want to change some of your accounting policies.</p>
+		<p>Good <b>sales forecasts</b> will help to increase prospective buyers' confidence in your business - but you must ensure they're realistic and can be supported with evidence. A full order book is a good sign.</p>
+		<p>It's important that buyers believe your accounts. For example, you should make realistic provisions for bad debts. Buyers and their advisers will usually see through any quick fixes you try to use to boost profits.</p>
+		<p>To maximise short-term profits you can reduce longer-term investment. For example, you might avoid expenses like advertising heavily or taking on new staff. But <strong>avoid excessive cost-cutting</strong> - you need to maintain spending in essential areas, otherwise the business suffers and so does the price buyers will be prepared to offer.</p>
+		<p>For advice on these and other options, consult your accountant and your corporate finance adviser.</p>
+		<ul>
+			<li><a href="choosing-advisers-when-selling-your-business.php">Prev</a></li>
+			<li><a href="streamline-your-business-operations.php">Next</a></li>
+		</ul>
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="../preparing-to-sell-your-business.php">Introduction</a></li>
+			<li><a href="is-selling-my-business-the-right-option.php">Is selling my business the right option?</a></li>
+			<li><a href="ways-to-sell-your-business.php">Ways to sell your business</a></li>
+			<li><a href="is-a-sale-realistic.php">Is a sale realistic?</a></li>
+			<li><a href="when-to-sell-your-business.php">When to sell your business</a></li>
+			<li><a href="choosing-advisers-when-selling-your-business.php">Choosing advisers when selling your business</a></li>
+			<li>Show strong financial performance</li>
+			<li><a href="streamline-your-business-operations.php">Streamline your business operations</a></li>
+			<li><a href="here-is-how-i-prepared-my-business-for-sale.php">Here's how I prepared my business for sale</a></li>
+		</ul>
+		<p><img src="../../../_img/hr_325px_global.gif" alt="" /></p>
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.lucasweston.com/top-ten-business-seller-mistakes.htm" target="_blank">Common mistakes in business sales explained on the Lucas &amp; Weston website<span> - Opens in a new window</span></a></li>
+			<li><a href="#">Download succession planning guidance from the Deloitte website (PDF, 120K)</a></li>
+			<li><a href="../../../tax-payroll-and-company-information/selling-or-closing-your-business/check-tax-and-legal-issues-when-selling-a-business.php">Use our interactive tool to investigate the tax and legal issues when selling or closing your business</a></li>
+		</ul>
+		<img src="../../../_img/px_sellingabusiness.gif" alt="" width="181" height="5" /></div>
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="../consider-your-exit-strategy-when-starting-up.php">Consider your exit strategy when starting up</a></li>
+			<li><a href="../../transferring-your-business/selling-or-passing-a-business-to-a-family-member.php">Selling or passing a business to a family member</a></li>
+			<li><a href="../value-and-market-your-business.php">Value and market your business</a></li>
+			<li><a href="../../selling/complete-the-sale-of-your-business.php">Complete the sale of your business</a></li>
+		</ul>
+		<img src="../../../_img/px_sellingabusiness.gif" alt="" width="181" height="5" /></div>
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
