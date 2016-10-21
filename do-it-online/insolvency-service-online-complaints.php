42e41a0810d6241b86f88412786ee920c006c0e1
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-10-21 21:31:46 +0100

    'Insolvency Service online complaints' page completed.

---
+++ do-it-online/insolvency-service-online-complaints.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Insolvency Service online complaints | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+    <h1>Insolvency Service online complaints</h1>
+    <p>The Insolvency Service is the government agency which administers and investigates the affairs of bankrupt individuals through its network of official receiver offices.</p>
+    <p>If you are dissatisfied with the service you have received from The Insolvency Service, you can register a complaint on line.</p>
+    <p>All staff at The Insolvency Service receive guidance on how to handle complaints.</p>
+    <p>For information, see our guide on how to <a href="/finance-and-grants/complain-against-an-insolvency-practitioner-or-the-insolvency-service.php">complain against an insolvency practitioner or The Insolvency Service</a>.</p>
+    <p>For general insolvency enquiries, you can call the Insolvency Service Enquiry Line on Tel 0845 602 9848. Alternatively, you can send an email to <a href="#">insolvency.enquiryline@insolvency.gsi.gov.uk</a>.</p>
+    <br />
+    <a href="https://www.gov.uk/government/organisations/insolvency-service/about/complaints-procedure" target="_blank" rel="noopener noreferrer">Make a complaint to The Insolvency Service</a>
+    
+    <p>
+        In partnership with:<br />
+        <a class="caption" href="#">
+            <img src="/_img/InsolvencyServiceLogo.jpg" width="132" height="53" border="0" alt="" />
+            The Insolvency Service
+        </a>
+    </p>
+
+    </div><!-- // .small-12 large-8 large-push-4 columns -->
+
+    <?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
