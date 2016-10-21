f70bb85a84c67b5a274543460b7bacd3e725e151
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-10-21 21:53:03 +0100

    'Implement staff incentive schemes' completed.

---
+++ employment-and-skills/pay-and-pensions/implement-staff-incentive-schemes.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Implement staff incentive schemes | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Implement staff incentive schemes</h1>
+		<h2>Introduction</h2>
+		<img src="/_img/EP0790.gif" width="78" height="118" border="0" alt="" />
+		<p>Introducing an <strong>effective system of incentives and perks</strong> can help you to recruit and retain valuable staff, reward performance and productivity. It can also help you get the best out of your employees.</p>
+		<p><strong>Incentives</strong> are rewards relating to certain goals. <strong>Perks</strong> are benefits on top of basic salary. Incentives and perks can be financial or non-financial. You can also have individual and group or team incentives.</p>
+		<p>This guide will help employers understand the benefits of offering incentives and perks whilst outlining different available options. It will also look at how you can decide what to offer and how to monitor the effectiveness of any selected scheme.</p>
+		<div>
+			<ul>
+				<li><a href="/employment-and-skills/pay-and-pensions/the-benefits-of-staff-incentive-schemes.php">Next</a></li>
+			</ul>
+		</div>
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li>Introduction</li>
+			<li><a href="/employment-and-skills/pay-and-pensions/the-benefits-of-staff-incentive-schemes.php">The benefits of staff incentive schemes</a></li>
+			<li><a href="/employment-and-skills/pay-and-pensions/incentive-schemes-and-the-options.php">Incentive schemes and the options</a></li>
+			<li><a href="/employment-and-skills/pay-and-pensions/perks-and-the-options.php">Perks and the options</a></li>
+			<li><a href="/employment-and-skills/pay-and-pensions/setting-up-an-incentive-scheme-for-employees.php">Setting up an incentive scheme for employees</a></li>
+			<li><a href="/employment-and-skills/pay-and-pensions/ensuring-the-incentive-scheme-is-successful.php">Ensuring the incentive scheme is successful</a></li>
+			<li><a href="/employment-and-skills/pay-and-pensions/here-is-how-a-staff-incentive-scheme-improved-our-business.php">Here's how a staff incentive scheme improved our business</a></li>
+		</ul>
+		<p><img src="/_img/hr_325px_global.gif" alt="" /></p>
+		
+		<p><strong>Print options</strong> - <span><a href="#">What are my print options <img src="/_img/whatisthis.gif" width="9" height="11" alt="" /></a></span></p>
+		<ul>
+			<li><a href="#">Print guide now</a></li>
+			<li><a href="#">Add guide to my print portfolio (print later as PDF)</a></li>
+		</ul>
+		<p><strong>Email options</strong> - <span><a href="#">What are my email options <img src="/_img/whatisthis.gif" width="9" height="11" alt="" /></a></span></p>
+		<ul>
+			<li><a href="#">Tell a friend about this page</a></li>
+			<li><a href="#">Send me an email alert when this guide changes</a></li>
+		</ul>
+
+		<h3>Acas Helpline</h3>
+		<p>08457 47 47 47</p>
+		<h3>HMRC Employer Helpline</h3>
+		<p>08457 143 143</p>
+		<h3>HMRC New Employer Helpline</h3>
+		<p>0845 60 70 143</p>
+		<p><img src="/_img/hr_140px_global.gif" alt="" /></p>
+	
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.acas.org.uk/index.aspx?articleid=2572" target="_blank" rel="noopener noreferrer">Incentive scheme guidance on the Acas website</a></li>
+			<li><a href="https://www.gov.uk/understanding-your-pay/performancerelated-pay" target="_blank" rel="noopener noreferrer">Performance-related pay guidance on the Directgov website</a></li>
+		</ul>
+		<img src="/_img/px_employingpeople.gif" alt="" width="181" height="5" />
+
+ 
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
