1e8b0964cc7824e61382c6c8d5c9a0d19a1f1ec1
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-10-21 16:57:12 +0100

    Cleaned code (sublime) added PHP extension and includes. Moved images. Starting to link up the other files associated.

---
+++ create-innovate-and-protect/protecting-your-intellectual-property/find-value-you-didnt-know-existed-in-your-business/protecting-intellectual-property.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Protecting intellectual property | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Protecting intellectual property</h1>
+		<h2>Introduction</h2>
+		<img src="/_img/GI0800.gif" width="78" height="118" border="0" alt="" />
+		<p>All businesses have intellectual property (IP), regardless of their size or sector. IP can mean a brand, invention, design or other kind of creation, and it can be legally owned.</p>
+		<p>Your IP is likely to be a valuable asset. It could include the name of your business, the products or services you make or provide, or the written or artistic material you create.</p>
+		<p>Securing and protecting it could be essential to your business' future success, so it's vital to understand your rights and how the law can help you.</p>
+		<p>This guide explains the importance of conducting an audit of your business' IP. It sets out the different kinds of legal protection available for IP available (including patents, trade marks, design rights, registered designs and copyright) and explains the range of things you can do to protect and manage your IP rights.</p>
+		
+		<ul>
+			<li><a href="/it-and-ecommerce/data-protection-and-legal-issues/protecting-intellectual-property.html" title="The importance of protecting intellectual property">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li>Introduction</li>
+			<li><a href="/it-and-ecommerce/data-protection-and-legal-issues/protecting-intellectual-property.html">The importance of protecting intellectual property</a></li>
+			<li><a href="detail0c0c-2.html?itemId=1074300708&amp;type=RESOURCES">Intellectual property management - conducting an audit</a></li>
+			<li><a href="detail3f3d.html?itemId=1074300727&amp;type=RESOURCES">Getting legal protection for your intellectual property</a></li>
+			<li><a href="detaile1c4.html?itemId=1075308805&amp;type=RESOURCES">Assistance with intellectual property protection</a></li>
+			<li><a href="detail0731.html?itemId=1074300742&amp;type=RESOURCES">Intellectual property rights and your employees</a></li>
+			<li><a href="detail6449.html?itemId=1074300758&amp;type=RESOURCES">Intellectual property and freelance contractors</a></li>
+			<li><a href="detail761f.html?itemId=1074300819&amp;type=RESOURCES">Prevent intellectual property infringement</a></li>
+			<li><a href="detailc375.html?itemId=1074300849&amp;type=RESOURCES">Protecting your business name and domain name</a></li>
+			<li><a href="detail1180.html?itemId=1075310362&amp;type=RESOURCES">Respect other people's intellectual property</a></li>
+		</ul>
+
+		<p><img src="/_img/hr_325px_global.gif" alt="" /></p>
+		<p><strong>Print options</strong> - <span><a href="#">What are my print options <img src="/_img/whatisthis.gif" width="9" height="11" alt="" /></a></span></p>
+		<ul>
+			<li><a href="#">Print guide now</a></li>
+			<li><a href="#">Add guide to my print portfolio (print later as PDF)</a></li>
+		</ul>
+		<p><strong>Email options</strong> - <span><a href="#">What are my email options <img src="/_img/whatisthis.gif" width="9" height="11" alt="" /></a></span></p>
+		<ul>
+			<li><a href="#">Tell a friend about this page</a></li>
+			<li><a href="#">Send me an email alert when this guide changes</a></li>
+		</ul>
+		
+		<h3>Intellectual Property Office Information Centre</h3>
+		<p>0300 300 2000</p>
+		<p><img src="/_img/hr_140px_global.gif" alt="" /></p>
+			
+		<!--Helplines-->
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="detaila6b0.html?itemId=1085931331&amp;type=PIP" title="Search the Intellectual Property Office online patents register">Search the patents directory with the Intellectual Property Office</a></li>
+			<li><a href="detaildb59.html?itemId=1085920811&amp;type=PIP" title="Register your patent with the Intellectual Property Office">Apply for a patent online with the Intellectual Property Office</a></li>
+			<li><a href="http://www.ipo.gov.uk/whyuse.htm" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('http://www.ipo.gov.uk/whyuse.htm', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="Find the right protection for your intellectual property">IP advice on the Intellectual Property Office website<span> - Opens in a new window</span></a></li>
+			<li><a href="detail4802.html?itemId=1087044741&amp;type=PIP">Take the IP Healthcheck with the Intellectual Property Office</a></li>
+			<li><a href="http://www.ipo.gov.uk/myip.pdf" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('http://www.ipo.gov.uk/myip.pdf', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="Leaflet providing an overview of patents, copyright, design and trade marks">Download guidance on IP from the Intellectual Property Office website (PDF, 1.01MB)<span> - Opens in a new window</span></a></li>
+			<li><a href="http://www.lawdonut.co.uk/law/disputes/intellectual-property-disputes/how-to-protect-your-intellectual-property" target="bgExternalonlinebusinesslinkgovuk" onclick="p=window.open('http://www.lawdonut.co.uk/law/disputes/intellectual-property-disputes/how-to-protect-your-intellectual-property', 'bgExternalonlinebusinesslinkgovuk');p.focus();" title="Protecting your intellectual property and dealing with intellectual property disputes">Intellectual property disputes guidance on the Law Donut website<span> - Opens in a new window</span></a></li>
+		</ul>
+		<img src="/_img/px_growthandinnovation.gif" alt="" width="181" height="5" />
+ 
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
