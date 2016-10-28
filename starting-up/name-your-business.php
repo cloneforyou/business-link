b4e5a43cafe46e92380ca1c34db04fb2a8b6209d
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-10-28 16:38:51 +0100

    Changing the links to PHP so they're ready for going live

--- starting-up/name-your-business.php
+++ starting-up/name-your-business.php
@@ -19,10 +19,10 @@
 		<h2><a href="https://www.gov.uk/starting-up-a-business" title="Start your own business" target="_blank" rel="noopener noreferrer">Name your business</a></h2>
 		<p>Case studies and tutorials to help you understand the legal dos and don'ts of naming your business</p>
 
-		<h2><a href="/name-your-business/choose-the-right-name-for-your-business.php.html">Choose the right name for your business</a></h2>
+		<h2><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php">Choose the right name for your business</a></h2>
 		<p>How to find the right name for your business and the rules and regulations involved in registering a name</p>
 
-		<h2><a href="/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php.html">Rules for naming your limited company or limited liability partnership (LLP)</a></h2>
+		<h2><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php">Rules for naming your limited company or limited liability partnership (LLP)</a></h2>
 		<p>Information on the regulations that affect the name of your limited company or limited liability partnership</p>
 
 		<h2><a href="https://www.gov.uk/get-information-about-a-company" title="Get information about a company" target="_blank" rel="noopener noreferrer">Search for an available company name or trade mark</a></h2>
