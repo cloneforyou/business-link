aece4d51c305175dee749071f7d0930d89d101ed
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-10-28 20:51:44 +0100

    Created Include for the email / print options section.

--- starting-up/considering-starting-up/starting-a-business-the-basics.php
+++ starting-up/considering-starting-up/starting-a-business-the-basics.php
@@ -39,15 +39,7 @@
 		
 		<img src="../../_img/hr_325px_global.gif" alt="" />
 
-		<p><strong>Print options</strong> - What are my print options <img src="../../_img/whatisthis.gif" width="9" height="11" border="0" alt="" /></a></p>
-		<ul>
-			<li><a href="#" title="">Add guide to my print portfolio (print later as PDF)</a></li>
-		</ul>
-		<p><strong>Email options</strong> - What are my email options <img src="../../_img/whatisthis.gif" width="9" height="11" border="0" alt="" /></a></p>
-		<ul>
-			<li><a href="#">Tell a friend about this page</a></li>
-			<li><a href="#">Send me an email alert when this guide changes</a></li>
-		</ul>
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
 
 		<h3>Business Link Helpline</h3>
 		<p>0845 600 9 006</p>
