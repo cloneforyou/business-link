a46d0fd0356c121fd82beedd9a3fb56f4a4178dc
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-01 15:26:02 +0000

    Completed 'Your business plan' page.

---
+++ starting-up/considering-starting-up/starting-a-business-the-basics/your-business-plan.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Your business plan | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business: the basics</h1>
+		<h2>Your business plan</h2>
+		<p>Planning and thinking about your business is a necessary process to undertake before, during and after you start. The headings in a business plan can be simply thought of as a checklist of questions you need to ask to reassure yourself that your venture will work. Writing the plan down is just as important because it helps to clarify your thinking and demonstrates your commitment to move forward. It also identifies where you intend to get to and how you intend to get there.</p>
+		<p>Other benefits include the chance for you to focus your mind on how you intend to run your business and to identify early on any areas or issues that you may have missed. You will also be able to review your progress against your forecast and make any necessary adjustments to get you back on track. Having a clearly presented document will also make it easier for any specialist you may use to provide support. In the case of raising funds from a bank or investor a business plan will be a necessary requirement.</p>
+		<p>Although the structure of business plans do vary, they will typically include an overview, a short description of the business idea and opportunity, what makes it different, who will be involved in the business, how you will provide your product or service, your marketing and sales strategy, and financial forecasts that will show the expected profitability.</p>
+		<p>The following material on this site can help:</p>
+		<ul>
+			<li><a href="https://www.gov.uk/write-business-plan"target="_blank" rel="noopener noreferrer">create your business plan</a></li>
+		</ul>
+		<p>The following guides will help you to put together a business plan:</p>
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php">prepare a business plan</a> - looks at the essential role that creating and using a business plan plays in starting a successful new business, and describes all the different components of a plan</li>
+			<li><a href="/finance-and-grants/prepare-to-raise-finance/how-to-attract-investment/use-your-business-plan-to-get-funding.php">use your business plan to get funding</a> - describes how to present your business plan to the right people in order to secure funding</li>
+		</ul>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/tax-and-legal-issues-for-start-ups.php" title="Tax and legal issues for start-ups">Prev</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics.php">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/what-to-do-before-you-start-your-business.php">What to do before you start your business</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/what-are-your-start-up-options.php">What are your start-up options?</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/finding-and-keeping-customers.php">Finding and keeping customers</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/finance-for-start-ups.php">Finance for start-ups</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/tax-and-legal-issues-for-start-ups.php">Tax and legal issues for start-ups</a></li>
+			<li>Your business plan</li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/write-business-plan"target="_blank" rel="noopener noreferrer">create your business plan</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
