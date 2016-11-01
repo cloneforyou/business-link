d8a630cf1946dee4d7474fe4c3704c06158c4327
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-01 14:33:00 +0000

    Completed 'Finding and keeping customers' page.

---
+++ starting-up/considering-starting-up/starting-a-business-the-basics/finding-and-keeping-customers.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Finding and keeping customers | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business: the basics</h1>
+		<h2>Finding and keeping customers</h2>
+
+
+		<p>When you start up your new business or become self employed, you will need to have a clear understanding of what an ideal or typical customer looks like. You will also require a way of identifying groups of customers with a similar profile.</p>
+		<p>Understanding target customers and their needs is a key aspect of your market research. Developing a marketing and sales plan is an important next step in planning how you will reach these customers through promotional activities while also enabling you to budget for the cost.</p>
+		<p>Once you have gained new customers it is vital that you build a long-term relationship with them so that you can enjoy their repeat business. It is significantly easier and cheaper to do business with existing profitable customers than to find new ones.</p>
+		<p>Retaining valuable customers also requires you to understand in detail the effectiveness of your customer service as seen from the customer's point of view. This customer feedback on your performance will help you to make informed decisions when making adjustments to your offer. It also increases the likelihood that your customers will talk about you positively to others and make referrals.</p>
+		<p>The following material on this site can help:</p>
+
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097102005&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">sell your product or service</a></li>
+			<li><a href="https://www.gov.uk/business-selling-customers" target="_blank" rel="noopener noreferrer">understand your market</a></li>
+			<li><a href="https://www.gov.uk/browse/business/sale-goods-services-data" target="_blank" rel="noopener noreferrer">get your pricing right</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097102044&type=HUBCHASSIS" title="Marketing your product or service to attract more customers" target="_blank" rel="noopener noreferrer">promote your business</a></li>
+		</ul>
+
+		<p>You can also look at the following guides:</p>
+
+		<ul>
+			<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs.php">know your customers' needs</a> - tells you what you need to know about your customers and how to use this information to sell more effectively</li>
+			<li><a href="/sales-and-marketing/keeping-your-customers/manage-your-customer-care.php">manage your customer care</a> explains how you can use customer contact, feedback and loyalty schemes to retain customers</li>
+			<li><a href="/sales-and-marketing/keeping-your-customers/retain-and-grow-your-customer-base.php">retain and grow your customer base</a> explains how to keep existing customers, grow your customer base and improve the effectiveness of your sales</li>
+		</ul>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/what-are-your-start-up-options.php" title="What are your start-up options?">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/finance-for-start-ups.php" title="Finance for start-ups">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics.php">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/what-to-do-before-you-start-your-business.php">What to do before you start your business</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/what-are-your-start-up-options.php">What are your start-up options?</a></li>
+			<li>Finding and keeping customers</li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/finance-for-start-ups.php">Finance for start-ups</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/tax-and-legal-issues-for-start-ups.php">Tax and legal issues for start-ups</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/your-business-plan.php">Your business plan</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097102005&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">sell your product or service</a></li>
+			<li><a href="https://www.gov.uk/business-selling-customers" target="_blank" rel="noopener noreferrer">understand your market</a></li>
+			<li><a href="https://www.gov.uk/browse/business/sale-goods-services-data" target="_blank" rel="noopener noreferrer">get your pricing right</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097102044&type=HUBCHASSIS" title="Marketing your product or service to attract more customers" target="_blank" rel="noopener noreferrer">promote your business</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5"  />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
