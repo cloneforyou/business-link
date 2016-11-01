4c621bc8477d91c80d46643a7e8ced776c913f05
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-01 14:02:02 +0000

    Completed 'What to do before you start your business' page.

---
+++ starting-up/considering-starting-up/starting-a-business-the-basics/what-to-do-before-you-start-your-business.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>What to do before you start your business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business: the basics</h1>
+		<h2>What to do before you start your business</h2>
+		<p>There are videos, e-learning tutorials and guides available on this site to help you before you start your new business. You will need to research and develop your basic business idea, and work out what you are going to call your business and what form it will take. You should also think carefully about the product or service that you want to sell, the audience you're selling to and what you have that makes you stand out from the crowd.</p>
+		<p>You must also think about how you are going to finance the start-up and what effect it will have on your personal finances while you wait for profits to show.</p>
+		<p>The following material will help you better understand what is required to start a business:</p>
+		<ul>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">decide if you have what it takes to be your own boss</a></li>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">develop your business idea</a></li>
+			<li><a href="https://www.gov.uk/business-finance-explained" target="_blank" rel="noopener noreferrer">think about the money</a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" target="_blank" rel="noopener noreferrer">understand the different business types</a></li>
+		</ul>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics.php" title="Starting a business: the basics">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/what-are-your-start-up-options.php" title="What are your start-up options?">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics.php" title="Starting a business: the basics">Introduction</a></li>
+			<li>What to do before you start your business</li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/what-are-your-start-up-options.php">What are your start-up options?</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/finding-and-keeping-customers.php">Finding and keeping customers</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/finance-for-start-ups.php">Finance for start-ups</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/tax-and-legal-issues-for-start-ups.php">Tax and legal issues for start-ups</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/your-business-plan.php">Your business plan</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">decide if you have what it takes to be your own boss</a></li>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">develop your business idea</a></li>
+			<li><a href="https://www.gov.uk/business-finance-explained" target="_blank" rel="noopener noreferrer">think about the money</a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" target="_blank" rel="noopener noreferrer">understand the different business types</a></li>
+		</ul>
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
