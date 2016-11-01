e43c009cc6ddb6ae20ad3a0becf63541f63cd6f2
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-01 13:23:53 +0000

    Completed 'Starting a business when economic conditions are tough' page.

---
+++ starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Starting a business when economic conditions are tough | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business when economic conditions are tough</h1>
+		<h2>Introduction</h2>
+		<img src="../../_img/starting_ec_downturn.gif" width="78" height="118" border="0" alt="" />
+		<p>When market and employment conditions are uncertain, starting your own business may be an option worth examining.</p>
+		<p>In an economic downturn, overall economic activity decreases. As a result, businesses could experience falling sales, cashflow problems, employment freezes or redundancies. However, in such a climate there are also opportunities to be exploited.</p>
+		<p>This guide looks at how, by choosing the right type of business and conducting it in the right way, your business could thrive when economic conditions are tough. It looks at the key factors that can help you successfully start up and run a business during a period of economic volatility, and shows you how a well-thought out business idea, proper sourcing of finance and good business planning can ensure that your business has the best chance of success.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/work-on-your-business-idea .php" title="Work on your business idea">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li>Introduction</li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/work-on-your-business-idea .php">Work on your business idea</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/your-business-structure-and-business-plan .php">Your business structure and business plan</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/access-to-sources-of-starting-up-finance .php">Access to sources of starting-up finance</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/develop-an-effective-survival-strategy .php">Develop an effective survival strategy</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/minimise-costs-and-maximise-efficiency .php">Minimise costs and maximise efficiency</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/sources-and-contacts-to-help-you-start-a-business .php">Sources and contacts to help you start a business</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/here-is-how-I-started-a-business-during-an-economic-downturn_flash-video .php">Here's how I started a business during an economic downturn (Flash video)</a></li>
+		</ul>
+		<img src="../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		<img src="../../_img/hr_140px_global.gif" alt="" />
+		<br />
+		<img src="../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
