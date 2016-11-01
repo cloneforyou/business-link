fd7b6145c8449b6e9bfaff69cb6830b9434e9d10
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-01 14:55:49 +0000

    Completed 'Tax and legal issues for start-ups' page.

---
+++ starting-up/considering-starting-up/starting-a-business-the-basics/tax-and-legal-issues-for-start-ups.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tax and legal issues for start-ups | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business: the basics</h1>
+		<h2>Tax and legal issues for start-ups</h2>
+		<p>Once you have decided to start a new business, you need to make sure that you have all of the legal formalities in place. This includes choosing the right legal structure to suit your particular circumstances and ambitions as well as registering with HM Revenue and Customs. Should you choose the limited company route you will also need to register with Companies House.</p>
+		<p>Depending on your business idea, you may need to seek specialist advice on Intellectual Property Rights (IPR) protection, whether this is to cover copyright, trade marking, design registration or patenting.</p>
+		<p>Keeping and maintaining accurate records as well as ensuring that you pay tax and National Insurance are essential requirements when you set up in business or go self employed.</p>
+		<p>The following material on this site can help:</p>
+		<ul>
+			<li><a href="https://www.gov.uk/business-legal-structures" target="_blank" rel="noopener noreferrer">choose the right business model and legal structure for your business</a></li>
+			<li><a href="https://www.gov.uk/browse/tax" target="_blank" rel="noopener noreferrer">get to grips with business taxes</a></li>
+			<li><a href="https://www.gov.uk/browse/business/intellectual-property" target="_blank" rel="noopener noreferrer">protect your business and premises</a></li>
+		</ul>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/finance-for-start-ups.php" title="Finance for start-ups">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/your-business-plan.php" title="Your business plan">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics.php">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/what-to-do-before-you-start-your-business.php">What to do before you start your business</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/what-are-your-start-up-options.php">What are your start-up options?</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/finding-and-keeping-customers.php">Finding and keeping customers</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/finance-for-start-ups.php">Finance for start-ups</a></li>
+			<li>Tax and legal issues for start-ups</li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/your-business-plan.php">Your business plan</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/do-it-online/incorporate-your-company-online.php">Incorporate your limited company online with Companies House</a></li>
+		</ul>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-legal-structures" target="_blank" rel="noopener noreferrer">choose the right business model and legal structure for your business</a></li>
+			<li><a href="https://www.gov.uk/browse/tax" target="_blank" rel="noopener noreferrer">get to grips with business taxes</a></li>
+			<li><a href="https://www.gov.uk/browse/business/intellectual-property" target="_blank" rel="noopener noreferrer">protect your business and premises</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5"  />  
+
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
