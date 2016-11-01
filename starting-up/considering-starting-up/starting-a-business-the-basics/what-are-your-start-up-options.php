a51a2f4630fda400f47a43757d14ed6cec4a48cf
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-01 14:17:58 +0000

    Completed 'What are your start-up options?' page.

---
+++ starting-up/considering-starting-up/starting-a-business-the-basics/what-are-your-start-up-options.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>What are your start-up options? | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+		
+		<h1>Starting a business: the basics</h1>
+		<h2>What are your start-up options?</h2>
+		<p>When considering starting a business, you should think about the different options available to you. You might want to start your business full-time or part-time, or even in your spare time. You could choose to work from home or separate premises, to buy an existing business or to invest in a franchise, depending on your experience, lifestyle and resources.</p>
+		<p>You should also think about assessing your own skills and where you might need some extra development or support - for example, looking after the business' finances.</p>
+		<p>The following material will help you to understand your options and choose which business is right for you:</p>
+		<ul>
+			<li><a href="https://www.gov.uk/business-legal-structures" target="_blank" rel="noopener noreferrer">choose the right business model and legal structure</a></li>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">name your business</a></li>
+		</ul>
+		<p>You can also look at the following guides:</p>
+		<ul>
+			<li><a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business.php">choose the right premises for your business</a> - explains how to choose the right premises and consider the options available to you as a start-up</li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php">starting a business from home</a> explains the benefits of working from your own home and the issues you might want to consider</li>
+			<li><a href="/buy-or-sell-a-business/franchising/buy-a-franchise.php">buy a franchise</a> explains what franchising is and why it might suit you more than starting your own business</li>
+			<li><a href="/buy-or-sell-a-business/buying/buy-an-existing-business.php">buy an existing business</a> looks at the issues around buying an established business, which may be quicker and easier than starting from scratch</li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them.php">common mistakes when starting up - and how to avoid them</a> - this guide will help you avoid the most frequent assumptions and mistakes made by new business owners</li>
+		</ul>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/what-to-do-before-you-start-your-business.php">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/finding-and-keeping-customers.php">Next</a></li>
+		</ul>
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics.php">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/what-to-do-before-you-start-your-business.php">What to do before you start your business</a></li>
+			<li>What are your start-up options?</li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/finding-and-keeping-customers.php">Finding and keeping customers</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/finance-for-start-ups.php">Finance for start-ups</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/tax-and-legal-issues-for-start-ups.php">Tax and legal issues for start-ups</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/your-business-plan.php">Your business plan</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-legal-structures" target="_blank" rel="noopener noreferrer">choose the right business model and legal structure</a></li>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">name your business</a></li>
+		</ul>
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+			
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
