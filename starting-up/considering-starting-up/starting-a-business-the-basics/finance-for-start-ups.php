adef5e59ac30b92665e05604539e6098f15d7a9e
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-01 14:45:19 +0000

    Completed 'Finance for start-ups' page.

---
+++ starting-up/considering-starting-up/starting-a-business-the-basics/finance-for-start-ups.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Finance for start-ups | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business: the basics</h1>
+		<h2>Finance for start-ups</h2>
+		<p>When you start up in business or self employment you will need money to get under way and to keep the business going until you get paid by your customers. You will also need to cover your personal expenses until the business can support you.</p>
+		<p>There are many different sources of funding for start-ups including personal savings, money from family and friends, bank loans, grants and equity funding from private investors. Creative thinking and planning your cashflow might save you money and make it less costly than you think to start your own business.</p>
+		<p>The following material on this site can help:</p>
+		<ul>
+			<li><a href="https://www.gov.uk/business-finance-explained" target="_blank" rel="noopener noreferrer">explore your finance options when starting up</a></li>
+			<li><a href="https://www.gov.uk/business-finance-explained" target="_blank" rel="noopener noreferrer">understand how much money you need to start up</a></li>
+		</ul>
+		<p>For more information, you can also see our guide on <a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">cashflow management: the basics</a>.</p>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/finding-and-keeping-customers.php" title="Finding and keeping customers">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/tax-and-legal-issues-for-start-ups.php" title="Tax and legal issues for start-ups">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics.php">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/what-to-do-before-you-start-your-business.php">What to do before you start your business</a></li>
+			<li>What are your start-up options?</li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/finding-and-keeping-customers.php">Finding and keeping customers</a></li>
+			<li>Finance for start-ups</li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/tax-and-legal-issues-for-start-ups.php">Tax and legal issues for start-ups</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-the-basics/your-business-plan.php">Your business plan</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-finance-explained" target="_blank" rel="noopener noreferrer">explore your finance options when starting up</a></li>
+			<li><a href="https://www.gov.uk/business-finance-explained" target="_blank" rel="noopener noreferrer">understand how much money you need to start up</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5"  />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
