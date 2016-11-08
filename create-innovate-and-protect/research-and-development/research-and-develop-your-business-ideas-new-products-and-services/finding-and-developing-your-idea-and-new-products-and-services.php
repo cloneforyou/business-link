00ddb90d7313a079f61a1fd5c061cff75fa3c9b9
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-08 14:13:09 +0000

    Completed 'Finding and developing your idea and new products and services' page.

---
+++ create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/finding-and-developing-your-idea-and-new-products-and-services.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Finding and developing your idea and new products and services | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Research and develop your business ideas, new products and services</h1>
+		<h2>Finding and developing your idea and new products and services</h2>
+		<p>A new idea is often the basis for starting up a business. Many entrepreneurs spot a gap in the market and start businesses that provide a product or service that fills it. Others come up with ways to improve an existing product.</p>
+		<h3>Coming up with a new idea</h3>
+		<p>If you want to start a business but don't yet have an idea to work with, there are many ways to go about identifying one. The following questions may help:</p>
+		<ul>
+			<li>Do you have any particular skills that could form the basis of a new business?</li>
+			<li>Are you aware of a gap in the market in the industry that you currently work in?</li>
+			<li>Do you have a hobby that could be turned into a business?</li>
+			<li>Has there ever been a time when you needed a particular service or product that nobody else provides? If you needed it, there is a good chance that other people will too.</li>
+			<li>Can you help solve any of the problems posted by government departments and public sector organisations on the Small Business Research Initiative (SBRI) website? SBRI is a government scheme supported by the Technology Strategy Board that awards funding to develop innovative ideas.</li>
+		</ul>
+
+		<p>See the page on <a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/the-small-business-research-initiative.php">the Small Business Research Initiative</a> in our guide on <a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d.php">support networks and facilities for innovation and R&amp;D</a>.</p>
+
+		<h3>Developing your idea</h3>
+		<p>Once you have got a business idea, take time to refine it. This will help you to decide whether it could be the foundation of a successful business.</p>
+		<p>There are various established methods of developing a business idea. You can:</p>
+		<ul>
+			<li>conduct <strong>market research</strong> to discover whether your idea fills a gap in the market - see our guide on <a href="/sales-and-marketing/marketing/market-research-and-market-reports.php">market research and market reports</a></li>
+			<li><strong>brainstorm</strong> your idea with friends, colleagues or staff - they can give different perspectives on the idea and may know if anyone else is doing the same thing</li>
+			<li>think about whether your idea can take advantage of an opportunity created by <strong>new technologies</strong>, eg by trading online</li>
+			<li>consider whether <strong>social trends</strong> will affect demand for your product, eg the increasing demand for organic food or concerns about global warming and carbon footprints</li>
+		</ul>
+
+		<h3>Developing new products and services</h3>
+		<p>There's a lot at stake when you are developing a new product or service. To minimise risks and allocate investment and resources wisely, you should consider a number of factors, including customer needs and design, whilst also clearly defining your plans. For more information, see the page on the <a href="/create-innovate-and-protect/research-and-development/product-life-cycle/development-stage-of-a-product-life-cycle.php">development stage of a product life cycle</a> in our guide to the <a href="/create-innovate-and-protect/research-and-development/product-life-cycle.php">product life cycle</a>.</p>
+
+		<h3>Support for businesses developing new ideas</h3>
+		<p>Designing Demand is a practical mentoring programme developed to help small to medium-sized businesses and technology start-ups use design to improve performance.</p>
+		<p>It helps businesses embed effective processes for the management of design and innovation and gives managers the skills to exploit design by spotting opportunities, briefing designers and running projects that deliver.</p>
+		<p>Designing Demand is part of a range of support offered to businesses in England through the government's Solutions for Business portfolio.</p>
+		<p><a href="http://www.designingdemand.org.uk/" target="_blank" rel="noopener noreferrer">Find out about Designing Demand on the Designing Demand website</a>.</p>
+		<p>Find out about the range of support for business included in the government's <a href="/solutions-for-business-portfolio.php">Solutions for Business portfolio</a>.</p> </p>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php" title="Research and develop your business ideas, new products and services">Prev</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/is-there-a-market-for-my-idea.php" title="Is there a market for my idea?">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">Introduction</a></li>
+			<li>Finding and developing your idea and new products and services</li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/is-there-a-market-for-my-idea.php">Is there a market for my idea?</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/plan-the-development-of-your-idea.php">Plan the development of your idea</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/financing-your-idea-and-cost-control.php">Financing your idea and cost control</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/creating-a-project-development-team.php">Creating a project development team</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/manage-a-development-project.php">Manage a development project</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/the-product-development-process.php">The product development process</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/pricing-your-proposed-service-or-product.php">Pricing your proposed service or product</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/sharing-your-ideas-with-others.php">Sharing your ideas with others</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/test-the-market.php">Test the market</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/checklist-developing-a-product-or-service.php">Checklist: developing a product or service</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-exploited-a-gap-in-the-market.php">Here's how I exploited a gap in the market</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-got-my-great-new-product-in-front-of-customers.php">Here's how I got my great new product in front of customers</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-developing-a-new-product-benefited-my-business.php">Here's how developing a new product benefited my business</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.designingdemand.org.uk/" target="_blank" rel="noopener noreferrer">Business support for innovative businesses on the Designing Demand website</a></li>
+			<li><a href="http://www.designcouncil.org.uk/resources/search/idea%2520generation?retain-filters=1" target="_blank" rel="noopener noreferrer">Idea generation and creativity advice on the Design Council website</a></li>
+			<li><a href="#">Business viability assessment tool on the Venture Navigator website (registration required)</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20140827133341/http://www.innovateuk.org/(X(1)S(k1ivh255c5252u55vlyescrf))/default.aspx?AspxAutoDetectCookieSupport=1" target="_blank" rel="noopener noreferrer">Innovation assessment tool on the Department for Business, Innovation &amp; Skills website</a></li>
+			<li><a href="http://www.designcouncil.org.uk/resources/search/product%2520design?retain-filters=1" target="_blank" rel="noopener noreferrer">Product design business directory on the Design Council website</a></li>
+			<li><a href="http://ethicsandtechnology.eu/?site_section=site-search&s=socially+responsible+innovation&submit=Search" target="_blank" rel="noopener noreferrer">Socially responsible innovation information on the 3TU.Centre for Ethics and Technology website</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20140827133341/https://www.innovateuk.org/-/sbri" target="_blank" rel="noopener noreferrer">SBRI opportunities on the Technology Strategy Board website</a></li>
+			<li><a href="/create-innovate-and-protect/create-and-improve-products-and-services/developing-new-business-ideas.php" title="Developing new business ideas">Use our interactive tool to evaluate your business idea</a></li>
+		</ul>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Develop your business idea</a></li>
+			<li><a href="/create-innovate-and-protect/design-for-business-success/user-centred-design.php">User-centred design</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports.php">Market research and market reports</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/work-with-uk-universities-and-colleges.php">Work with UK universities and colleges</a></li>
+			<li><a href="/sales-and-marketing/marketing/understand-your-competitors.php">Understand your competitors</a></li>
+			<li><a href="/create-innovate-and-protect/use-innovation-in-your-business/increase-efficiency-to-maximise-your-profit.php">Increase efficiency to maximise your profit</a></li>
+			<li><a href="/create-innovate-and-protect/design-for-business-success/designing-a-successful-brand.php">Designing a successful brand</a></li>
+			<li><a href="/create-innovate-and-protect/design-for-business-success/how-can-I-use-design-in-my-business.php">How can I use design in my business?</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/manage-your-research-design-and-development.php">Manage your research, design and development</a></li>
+		</ul>
+		<img src="../../../_img/px_growthandinnovation.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
