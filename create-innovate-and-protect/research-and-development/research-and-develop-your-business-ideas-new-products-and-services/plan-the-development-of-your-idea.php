df66aa66751218683df97509c2903f5331410109
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-08 15:45:54 +0000

    Completed 'Plan the development of your idea' page.

---
+++ create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/plan-the-development-of-your-idea.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Plan the development of your idea | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Research and develop your business ideas, new products and services</h1>
+		<h2>Plan the development of your idea</h2>
+		<p>Try to identify the key stages or <strong>checkpoints</strong> in the development of your idea. Each checkpoint gives you a chance to evaluate the progress of your product or service and decide whether you need to make any changes. It will also allow you to consider at the end of each stage whether to progress with the idea. Any idea that has no realistic chance of success should be dropped before too much time or money is invested.</p>
+		<p>Your checkpoints may include:</p>
+		<ul>
+			<li>
+			<strong>designing</strong> - turning your idea into a product or service that can actually be sold
+			</li>
+			<li>
+			<strong>prototyping</strong> - creating a useable example of your product or service, which can then be tested
+			</li>
+			<li>
+			<strong>protecting</strong> - applying for a patent if you have invented a product or type of technology
+			</li>
+			<li>
+			<strong>financing</strong> - raising the money you will need to get your business started
+			</li>
+			<li>
+			<strong>operations</strong> - setting up the structure of your business, eg finding a suitable location, hiring staff, etc
+			</li>
+			<li>
+			<strong>marketing</strong> - working out how you will sell your product or service
+			</li>
+		</ul>
+
+		<h3>Judge your progress</h3>
+		<p>If the goals of any of your checkpoints are not met, you need to analyse why this is the case. Ask yourself whether your objectives were unreasonable. If so, you may need to revise your objectives.</p>
+		<p>There are circumstances when you should reconsider your original idea.</p>
+		<p>These include:</p>
+		<ul>
+			<li>
+			developing a product or service that isn't commercially viable and won't produce a reasonable financial return
+			</li>
+			<li>
+			developing a product that is not technically viable, eg it cannot be manufactured or it doesn't meet performance requirements
+			</li>
+			<li>
+			someone else releasing a product or service which is very similar or identical to yours, especially if they are a large or well-established competitor
+			</li>
+		</ul>
+
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/is-there-a-market-for-my-idea.php" title="Is there a market for my idea?">Prev</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/financing-your-idea-and-cost-control.php" title="Financing your idea and cost control">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">Introduction</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/finding-and-developing-your-idea-and-new-products-and-services.php">Finding and developing your idea and new products and services</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/is-there-a-market-for-my-idea.php">Is there a market for my idea?</a></li>
+			<li>Plan the development of your idea</li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/financing-your-idea-and-cost-control.php">Financing your idea and cost control</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/creating-a-project-development-team.php">Creating a project development team</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/manage-a-development-project.php">Manage a development project</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/the-product-development-process.php">The product development process</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/pricing-your-proposed-service-or-product.php">Pricing your proposed service or product</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/sharing-your-ideas-with-others.php">Sharing your ideas with others</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/test-the-market.php">Test the market</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/checklist-developing-a-product-or-service.php">Checklist: developing a product or service</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-exploited-a-gap-in-the-market.php">Here's how I exploited a gap in the market</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-got-my-great-new-product-in-front-of-customers.php">Here's how I got my great new product in front of customers</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-developing-a-new-product-benefited-my-business.php">Here's how developing a new product benefited my business</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.designcouncil.org.uk/resources/search/idea%2520innovation?retain-filters=1" target="_blank" rel="noopener noreferrer">Idea innovation on the Design Council website</a></li>
+		</ul>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Develop your business idea</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/manage-your-research-design-and-development.php">Manage your research, design and development</a></li>
+		</ul>
+
+		<img src="../../../_img/px_growthandinnovation.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
