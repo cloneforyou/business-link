b86cb434e6afc360064aeda34bff359f55e5517f
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-08 16:09:43 +0000

    404 page created

---
+++ 404.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>404 Not Found | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		
+		<h1>Oh dear - 404 Not Found</h1>
+
+		<p>This section or page, is still under construction. Please click your Back button</p>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
