a0ededfa4300ead4a7b6a2fbe23edf6b0511cffc
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-08 15:03:12 +0000

    Completed 'Is there a market for my idea?' page.

---
+++ create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/is-there-a-market-for-my-idea.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Is there a market for my idea? | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Research and develop your business ideas, new products and services</h1>
+		<h2>Is there a market for my idea?</h2>
+		<p>There are certain criteria you can use to establish whether there is a market or demand for your product or service:</p>
+		<ul>
+			<li>Does it satisfy or create a market need?</li>
+			<li>Can you identify potential customers?</li>
+			<li>Will it outlive passing trends or capitalise on the trend before it dies away?</li>
+			<li>Is it unique, distinct or superior to those offered by competitors?</li>
+			<li>What competition will it face - direct or indirect, local, national or global?</li>
+			<li>Is the product safe for public use and does it comply with relevant regulations and legislation? Seek legal advice before proceeding.</li>
+			<li>Will the market want your product or service at a realistic price?</li>
+		</ul>
+
+		<p><strong>Market research</strong> can play an important role in answering many of these questions and increasing your chances of success. It is much better to invest time researching before you invest your money. See our guide on <a href="/sales-and-marketing/marketing/market-research-and-market-reports.php">market research and market reports</a>.</p>
+		<p>Remember that although the end user of your new product or service might be your most important customer, you may have to take the needs of other parties into account, such as retailers or distributors.</p>
+		<p>It is very important that you are as thorough in your market research as possible, as mistakes made at this stage of development could prove costly later on. Remember, the more information you have, the better you will be able to understand your potential customers, the marketplace and how your product fits in. For more information, see our guide on <a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them.php">common mistakes when starting up - and how to avoid them</a>.</p>
+
+		<h3>Your competition</h3>
+		<p>Not only must you meet your customers' needs, you should do so in a way that is better than the alternatives offered by your competitors.</p>
+		<p>Your new product or service should have a unique selling proposition - a feature or property that makes it stand out in the marketplace. Before entering the market you need to gather 'competitive intelligence' by determining:</p>
+		<ul>
+			<li>how customer needs are currently met</li>
+			<li>why customers would choose your product or service rather than your competitors', both now and in the future</li>
+			<li>what risks you are prepared to take to launch your product or service into this market</li>
+		</ul>
+		<p>For more information, see our guides on how to <a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs.php">know your customers' needs</a> and <a href="/sales-and-marketing/marketing/understand-your-competitors.php">understand your competitors</a>.</p>
+
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/finding-and-developing-your-idea-and-new-products-and-services.php" title="Finding and developing your idea and new products and services">Prev</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/plan-the-development-of-your-idea.php" title="Plan the development of your idea">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">Introduction</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/finding-and-developing-your-idea-and-new-products-and-services.php">Finding and developing your idea and new products and services</a></li>
+			<li>Is there a market for my idea?</li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/plan-the-development-of-your-idea.php">Plan the development of your idea</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/financing-your-idea-and-cost-control.php">Financing your idea and cost control</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/creating-a-project-development-team.php">Creating a project development team</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/manage-a-development-project.php">Manage a development project</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/the-product-development-process.php">The product development process</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/pricing-your-proposed-service-or-product.php">Pricing your proposed service or product</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/sharing-your-ideas-with-others.php">Sharing your ideas with others</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/test-the-market.php">Test the market</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/checklist-developing-a-product-or-service.php">Checklist: developing a product or service</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-exploited-a-gap-in-the-market.php">Here's how I exploited a gap in the market</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-got-my-great-new-product-in-front-of-customers.php">Here's how I got my great new product in front of customers</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-developing-a-new-product-benefited-my-business.php">Here's how developing a new product benefited my business</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>The Chartered Institute of Marketing General Enquiry Line</h3>
+		<p>01628 427 200</p>
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/do-it-online/look-up-patents.php">Search the patents directory with the Intellectual Property Office</a></li>
+			<li><a href="/do-it-online/apply-for-a-uk-patent.php">Apply for a patent online with the Intellectual Property Office</a></li>
+			<li><a href="https://www.mrs.org.uk/mrindustry/" target="_blank" rel="noopener noreferrer">Market research techniques on the Market Research Society website</a></li>
+			<li><a href="https://www.theresearchbuyersguide.com" target="_blank" rel="noopener noreferrer">Market research agency search on the Research Buyer's Guide website</a></li>
+		</ul>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-selling-customers" title="Your business and selling to customers" target="_blank" rel="noopener noreferrer">Understand your market</a></li>
+			<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs.php">Know your customers' needs</a></li>
+			<li><a href="/sales-and-marketing/marketing/understand-your-competitors.php">Understand your competitors</a></li>
+			<li><a href="/create-innovate-and-protect/design-for-business-success/user-centred-design.php">User-centred design</a></li>
+			<li><a href="/sales-and-marketing/pricing/price-your-product-or-service.php">Price your product or service</a></li>
+			<li><a href="/your-business-sector/farming/diversification-sales-and-marketing/ideas-and-funding-for-diversification.php">Ideas and funding for diversification</a></li>
+			<li><a href="/create-innovate-and-protect/design-for-business-success/how-can-I-use-design-in-my-business.php">How can I use design in my business?</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/manage-your-research-design-and-development.php">Manage your research, design and development</a></li>
+		</ul>
+
+		<img src="../../../_img/px_growthandinnovation.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
