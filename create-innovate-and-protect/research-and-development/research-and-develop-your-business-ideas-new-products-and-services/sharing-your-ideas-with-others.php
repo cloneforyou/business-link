f698ded14c8387f4d90d5abe0c48fe94acbe2e99
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-19 20:17:17 +0000

    Completed ‘Sharing your ideas with others’ page.

---
+++ create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/sharing-your-ideas-with-others.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sharing your ideas with others | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Research and develop your business ideas, new products and services</h1>
+		<h2>Sharing your ideas with others</h2>
+		<p>At some stage you will probably wish to discuss your idea with a third party. But once you put an idea into the public domain it can no longer be considered confidential or a trade secret. So you need to take steps to protect your intellectual property.</p>
+		<p>Before talking to third parties, it is a good idea to ask them to sign a non-disclosure or confidentiality agreement - to prevent them from sharing details of your idea with others. See our guide on <a href="/create-innovate-and-protect/protecting-your-intellectual-property/patent-protection-and-your-business/non-disclosure-agreements.php">non-disclosure agreements</a>.</p>
+		<h3>Intellectual property (IP)</h3>
+		<p>All businesses have IP. Your business' IP is what sets it apart from its competitors. For example, it could be your business name, logos, inventions, product designs or other creative work.</p>
+		<p>As your IP is likely to be a valuable asset, it's wise to help secure the future of your business by protecting it legally. For information on the different ways you can prevent others from stealing your IP, and advice on carrying out an IP audit, see our guide on <a href="/create-innovate-and-protect/protecting-your-intellectual-property/find-value-you-didnt-know-existed-in-your-business/protecting-intellectual-property.php">protecting intellectual property</a>.</p>
+
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/pricing-your-proposed-service-or-product.php" title="Pricing your proposed service or product">Prev</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/test-the-market.php" title="Test the market">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">Introduction</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/finding-and-developing-your-idea-and-new-products-and-services.php">Finding and developing your idea and new products and services</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/is-there-a-market-for-my-idea.php">Is there a market for my idea?</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/plan-the-development-of-your-idea.php">Plan the development of your idea</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/financing-your-idea-and-cost-control.php">Financing your idea and cost control</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/creating-a-project-development-team.php">Creating a project development team</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/manage-a-development-project.php">Manage a development project</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/the-product-development-process.php">The product development process</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/pricing-your-proposed-service-or-product.php">Pricing your proposed service or product</a></li>
+			<li>Sharing your ideas with others</li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/test-the-market.php">Test the market</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/checklist-developing-a-product-or-service.php">Checklist: developing a product or service</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-exploited-a-gap-in-the-market.php">Here's how I exploited a gap in the market</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-got-my-great-new-product-in-front-of-customers.php">Here's how I got my great new product in front of customers</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-developing-a-new-product-benefited-my-business.php">Here's how developing a new product benefited my business</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Intellectual Property Office Information Centre</h3>
+		<p>0300 300 2000</p>
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/do-it-online/look-up-patents.php">Search the patents directory with the Intellectual Property Office</a></li>
+			<li><a href="/do-it-online/apply-for-a-uk-patent.php">Apply for a patent online with the Intellectual Property Office</a></li>
+			<li><a href="https://www.gov.uk/government/organisations/intellectual-property-office" target="_blank" rel="noopener noreferrer">Trade mark, copyright, patent and design protection on the Intellectual Property Office website</a></li>
+			<li><a href="/do-it-online/apply-to-register-a-trade-mark.php">Apply to register your trade mark with the Intellectual Property Office</a></li>
+		</ul>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Develop your business idea</a></li>
+			<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/patent-protection-and-your-business/intellectual-property-the-basics.php">Intellectual property: the basics</a></li>
+		</ul>
+
+		<img src="../../../_img/px_growthandinnovation.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
