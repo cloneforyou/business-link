fa6f222e9fe663fef9937663275a622821b75f99
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-19 22:24:32 +0000

    Completed ‘Poor or inadequate market research’ page.

---
+++ starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-or-inadequate-market-research.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Poor or inadequate market research | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		
+		<h1>Common mistakes when starting up - and how to avoid them</h1>
+		<h2>Poor or inadequate market research</h2>
+		<p>Research and planning are vital to ensure that your business idea is viable and that your pricing is both competitive in your market place and provides an adequate return.</p>
+		<p>A common misconception is that entrepreneurs who have failed simply lacked sufficient funding or did not put the right team in place. However, many fail because they have not spent enough time researching their business idea and its viability in the market.</p>
+		<h3>Lack of in-depth market research</h3>
+		<p>Lack of proper market research is one of the key problems for new businesses. It's easy to get carried away with a business idea and set up a business without testing its viability.</p>
+		<p>Accurate market data will help prevent over-optimistic forecasts. For more information, see the page in this guide on <a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/setting-sights-too-high.php">setting sights too high</a> and our guide on <a href="/sales-and-marketing/marketing/market-research-and-market-reports.php">market research and market reports</a>.</p>
+		<p>It is also important to consider what your audience or customer needs are and to use market research to test them, and to then factor feedback into any products or services you are designing. For more information, see our guide on <a href="/create-innovate-and-protect/design-for-business-success/user-centred-design.php">user-centred design</a>.</p>
+		<h3>Keeping your business ideas to yourself</h3>
+		<p>Failing to share your business ideas with people you trust means that you will miss out on objective feedback.</p>
+		<p>Brainstorm with colleagues to give you valuable perspective. Note down any good ideas you get from brainstorming and use them when developing your business.</p>
+		<p>For more information, see our guide on how to <a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">research and develop your business ideas</a>.</p>
+		<p>Asking potential customers what they think of your plans or allowing them to examine a prototype can be invaluable. It can help you discover whether your product offers a solution to customers' problems or something new and unique that they would purchase. Positive feedback will give you the confidence to proceed and could help you attract investment funding. On the other hand, negative feedback will alert you to the need to rethink your plans and could help you avoid wasting time and money on a product that will not sell. See the page: <a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/test-the-market.php">test the market</a> in our guide on how to <a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">research and develop your business ideas</a>.</p>
+		<p>If you want to keep your ideas confidential, consider using a non-disclosure agreement, also known as a confidentiality agreement. This is a legal contract between you and another party not to disclose information you have shared for a specific purpose. See our guide on <a href="/create-innovate-and-protect/protecting-your-intellectual-property/patent-protection-and-your-business/non-disclosure-agreements.php">non-disclosure agreements</a>.</p>
+		<h3>Not knowing your clients or marketplace</h3>
+		<p>If you do not complete adequate research, you are in danger of selling to the wrong people or of not understanding your marketplace. To avoid this:</p>
+		<ul>
+			<li>use information, such as free government data or your own network of contacts</li>
+			<li>carry out field research to explore customers' profiles and discover buying trends</li>
+			<li>swap ideas with people in the same sector</li>
+		</ul>
+		<p>For more information, see our guides on <a href="/sales-and-marketing/marketing/market-research-and-market-reports.php">market research and market reports</a> and <a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs.php">know your customers' needs</a>.</p>
+		<p>Becoming a member of a Chamber of Commerce will give you access to services such as training, networking and savings on overheads. <a href="http://www.britishchambers.org.uk/find-your-chamber/" target="_blank" rel="noopener noreferrer">Find your local Chamber of Commerce on the British Chambers of Commerce website</a>.</p>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them.php" title="Common mistakes when starting up - and how to avoid them">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/weak-financial-planning.php" title="Weak financial planning">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-or-inadequate-market-research.php">Poor or inadequate market research</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/weak-financial-planning.php">Weak financial planning</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/setting-sights-too-high.php">Setting sights too high</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/taking-your-eye-off-the-competition.php">Taking your eye off the competition</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-supplier-and-customer-controls.php">Poor supplier and customer controls</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-stock-and-asset-management.php">Poor stock and asset management</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/hiring-the-wrong-people.php">Hiring the wrong people</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/here-is-how-I-started-a-successful-business_flash-video.php">Here's how I started a successful business (Flash video)</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097102079&amp;type=HUBCHASSIS" title="Researching your market and competition">Understand your market</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports.php" title="How to use market research to gain knowledge about your marketplace and your customers">Market research and market reports</a></li>
+			<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs.php" title="Why understanding your customers' needs is crucial to the success of your business">Know your customers' needs</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
