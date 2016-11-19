8587d6d731da899e29bf21500a39c4b5e9ea0acc
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-19 17:10:56 +0000

    Completed ‘Manage a development project’ page.

---
+++ create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/manage-a-development-project.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Manage a development project | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Research and develop your business ideas, new products and services</h1>
+		<h2>Manage a development project</h2>
+		<p>Project managers are essential to ensure the successful development of new products or services. The project manager is responsible for:</p>
+		<ul>
+			<li>controlling costs and allocating resources</li>
+			<li>drawing up the product or service's specification</li>
+			<li>co-ordinating the product development team - see the page in this guide on <a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/creating-a-project-development-team.php">creating a project development team</a></li>
+			<li>timetabling the development process</li>
+			<li>troubleshooting</li>
+		</ul>
+		<h3>Timetabling the development process</h3>
+		<p>Your project manager should draw up a critical path for the completion of key tasks and stages in the product development process. SMART (specific, measurable, agreed, realistic and time-bound) objectives should be introduced to help measure and monitor progress.</p>
+		<p>However, your plans should be flexible as well so that you can deal with any unknown event that occurs.  For example, you may have to deal with a change in the project's specifications or expected completion date.</p>
+		<p>For more information on managing your development project, see our guide on how to <a href="/create-innovate-and-protect/research-and-development/manage-your-research-design-and-development.php">manage your research, design and development</a>.</p>
+		<p>For more information on working with formal project management systems for the first time, see our guide on <a href="/grow-your-business/planning-how-to-grow/project-management-the-basics.php">project management - the basics</a>.</p>
+
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/creating-a-project-development-team.php" title="Creating a project development team">Prev</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/the-product-development-process.php" title="The product development process">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">Introduction</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/finding-and-developing-your-idea-and-new-products-and-services.php">Finding and developing your idea and new products and services</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/is-there-a-market-for-my-idea.php">Is there a market for my idea?</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/plan-the-development-of-your-idea.php">Plan the development of your idea</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/financing-your-idea-and-cost-control.php">Financing your idea and cost control</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/creating-a-project-development-team.php">Creating a project development team</a></li>
+			<li>Manage a development project</li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/the-product-development-process.php">The product development process</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/pricing-your-proposed-service-or-product.php">Pricing your proposed service or product</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/sharing-your-ideas-with-others.php">Sharing your ideas with others</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/test-the-market.php">Test the market</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/checklist-developing-a-product-or-service.php">Checklist: developing a product or service</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-exploited-a-gap-in-the-market.php">Here's how I exploited a gap in the market</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-got-my-great-new-product-in-front-of-customers.php">Here's how I got my great new product in front of customers</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-developing-a-new-product-benefited-my-business.php">Here's how developing a new product benefited my business</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<p><img src="../../../_img/hr_140px_global.gif" alt="" /></p>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.learndirect.com/store/business-workplace/managing-people/teamworking-leadership" target="_blank" rel="noopener noreferrer">Team building e-course on the learndirect website</a></li>
+			<li><a href="http://www.learndirect.com/store/catalogsearch/result/?q=%22time+management%22" target="_blank" rel="noopener noreferrer">Time management courses on the learndirect website</a></li>
+		</ul>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/patent-protection-and-your-business/managing-your-patents.php">Managing your patents</a></li>
+			<li><a href="/create-innovate-and-protect/design-for-business-success/protecting-and-handling-your-design.php">Protecting and handling your design</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports.php">Market research and market reports</a></li>
+			<li><a href="/sales-and-marketing/pricing/price-your-product-or-service.php">Price your product or service</a></li>
+			<li><a href="/sales-and-marketing/marketing/understand-your-competitors.php">Understand your competitors</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/manage-your-research-design-and-development.php">Manage your research, design and development</a></li>
+			<li><a href="/grow-your-business/planning-how-to-grow/project-management-the-basics.php">Project management - the basics</a></li>
+		</ul>
+
+		<img src="../../../_img/px_growthandinnovation.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
