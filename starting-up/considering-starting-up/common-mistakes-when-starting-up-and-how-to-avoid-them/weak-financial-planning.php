0cf88a51388a5b1f7a4de9344260c72f76f1e40a
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-19 22:44:16 +0000

    Completed ‘Weak financial planning’ page.

---
+++ starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/weak-financial-planning.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Weak financial planning | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Common mistakes when starting up - and how to avoid them</h1>
+		<h2>Weak financial planning</h2>
+		<p>Financial planning is extremely important for most new businesses. A lack of capital, lack of a contingency plan and reluctance to seek professional advice can all bring major problems.</p>
+		<h3>Lack of capital</h3>
+		<p>Having sufficient capital is essential for the survival and prosperity of your business, and is a primary indicator of your business' health.</p>
+		<p>It is important to create a high-quality <strong>business plan</strong> to attract and secure the right type and amount of funding that you need to make your business successful. A business plan can:</p>
+		<ul>
+			<li>be used as a tool to structure the financial side of your business and can be updated and changed as your business grows</li>
+			<li>keep your expectations grounded for what the business can deliver</li>
+		</ul>
+		<p>For more information, see our guide on how to <a href="/starting-up/business-planning/prepare-a-business-plan.php">prepare a business plan</a>.</p>
+		<h3>Lack of a contingency plan</h3>
+		<p>Without a contingency plan you can leave yourself exposed to the unexpected.</p>
+		<p>Situations beyond your control that may impact on your business and cashflow include interest rate rises, transport strikes and political instability. While your business can survive periods where there are no sales or profits, it cannot survive without cash. Building up cash reserves will ensure that you can trade effectively and develop your business.</p>
+		<p>For more information, see our guides on <a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">cashflow management: the basics</a> and <a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground.php">how do I survive until my business is off the ground?</a></p>
+		<h3>A reluctance to seek professional advice</h3>
+		<p>Failing to seek professional advice will make any financial troubles worse. Few new business owners can claim expertise in all areas of their business. Using an accountant or financial adviser can help you ensure you borrow and manage money cost-effectively.</p>
+		<p>For more information, see our guides on how to <a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">choose and work with an accountant</a>.</p>
+		<p>You can also <a href="http://www.apfa.net/APFA/Consumers/Find_a_financial_adviser/APFA/Consumers/Find_a_financial_adviser.aspx" target="_blank" rel="noopener noreferrer">find a local independent financial adviser on the Association of Independent Financial Advisers (AIFA) website</a>.</p>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-or-inadequate-market-research.php" title="Poor or inadequate market research">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/setting-sights-too-high.php" title="Setting sights too high">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them.php">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-or-inadequate-market-research.php">Poor or inadequate market research</a></li>
+			<li>Weak financial planning</li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/setting-sights-too-high.php">Setting sights too high</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/taking-your-eye-off-the-competition.php">Taking your eye off the competition</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-supplier-and-customer-controls.php">Poor supplier and customer controls</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-stock-and-asset-management.php">Poor stock and asset management</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/hiring-the-wrong-people.php">Hiring the wrong people</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/here-is-how-I-started-a-successful-business_flash-video.php">Here's how I started a successful business (Flash video)</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.apfa.net/APFA/Consumers/Find_a_financial_adviser/APFA/Consumers/Find_a_financial_adviser.aspx" target="_blank" rel="noopener noreferrer">Independent financial adviser details on the AIFA website</a></li>
+			<li><a href="http://www.cimaglobal.com/Documents/ImportedDocuments/cid_improving_cashflow_using_credit_mgm_Apr09.pdf.pdf" target="_blank" rel="noopener noreferrer">Download cashflow management advice from the Chartered Institute of Management Accountants website (PDF, 366K)</a></li>
+		</ul>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-finance-support-finder" target="_blank" rel="noopener noreferrer">Think about your financial planning</a></li>
+			<li><a href="https://www.gov.uk/business-finance-explained" target="_blank" rel="noopener noreferrer">Understand how much money you need to start up</a></li>
+			<li><a href="https://www.gov.uk/appoint-tax-agent" target="_blank" rel="noopener noreferrer">Decide if you need an accountant or an adviser </a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">Cashflow management: the basics</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/identify-potential-cashflow-problems.php">Identify potential cashflow problems</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
