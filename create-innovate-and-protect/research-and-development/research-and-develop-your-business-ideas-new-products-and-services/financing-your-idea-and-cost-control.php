736cad4bd7c1de43eaf93f2f3b1e7a7b984628e4
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-19 16:26:30 +0000

    Completed ‘Financing your idea and cost control’ page.

---
+++ create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/financing-your-idea-and-cost-control.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Financing your idea and cost control | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Research and develop your business ideas, new products and services</h1>
+		<h2>Financing your idea and cost control</h2>
+		<p>Securing adequate funding is one of the biggest obstacles many entrepreneurs face. Your funding needs may also change during the course of product development, as it may take longer or cost more than you first expected.</p>
+		<p>Bank loans and overdrafts are the most common ways to raise money for a new business. But there are plenty of alternative options too, including:</p>
+		<ul>
+			<li>cashing in shares or other investments you may hold</li>
+			<li>borrowing money from family or friends</li>
+			<li>remortgaging your property</li>
+			<li>non-bank finance - eg credit unions or peer-to-peer loans</li>
+			<li>government grants</li>
+			<li>investment from business angels or venture capitalists</li>
+		</ul>
+		<p>Remember to build into your financial forecasts a generous margin for contingencies and the unexpected. It's not worth investing money and then running out before your business has got off the ground.</p>
+		<p>For advice on working out how much money you will need, and the pros and cons of the different funding options, see our guide on how to <a href="/finance-and-grants/finance-options/choose-the-right-finance-options/choose-the-right-finance-when-starting-up.php">choose the right finance when starting up</a>.</p>
+		<p>It is important to plan any investment and control your costs carefully. You should:</p>
+		<ul>
+			<li>include future investment in products and services into your strategic business plan</li>
+			<li>plan exactly where this investment will be directed</li>
+			<li>justify the expenditure on every development project</li>
+			<li>manage your cost</li>
+		</ul>
+		<p>Before making investment decisions, consider how much your business stands to gain from the new product or service. Weigh this against any risks you face.</p>
+		<h3>Phasing new product development</h3>
+		<p>One way to minimise your risks is to phase investment in projects. By reviewing a project at the end of each stage of development, you can identify products or services that are unlikely to be successful. If the product or service fails to meet established criteria, you should consider cancelling the project. If it does meet them, you can allocate the resources to allow it to reach the next development stage.</p>
+		<h3>Cost control</h3>
+		<p>It's essential to keep a close eye on costs when you develop new products and services to avoid them spiralling out of control. You should:</p>
+		<ul>
+			<li>estimate development costs in advance</li>
+			<li>monitor spending throughout the development process</li>
+			<li>introduce phased investment</li>
+		</ul>
+		<p>There are two main ways to estimate costs:</p>
+		<ul>
+			<li>a top-down approach where you consider previous comparable projects and use them as a benchmark</li>
+			<li>a bottom-up approach where all team members agree the costs they expect to incur with one project manager, who will then estimate the total cost</li>
+		</ul>
+		<p>Remember that your costs could include staffing, materials, technology, product design, market research, prototyping and overhead costs.</p>
+		<p>For more information on working with formal project management systems for the first time, see our guide on <a href="/grow-your-business/planning-how-to-grow/project-management-the-basics.php">project management - the basics.</a></p>
+
+		<ul>
+			<li><a href="detailb319-2.html?itemId=1074400873&amp;type=RESOURCES" title="Plan the development of your idea">Prev</a></li>
+			<li><a href="detail73fe.html?itemId=1097303990&amp;type=RESOURCES" title="Creating a project development team">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">Introduction</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/finding-and-developing-your-idea-and-new-products-and-services.php">Finding and developing your idea and new products and services</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/is-there-a-market-for-my-idea.php">Is there a market for my idea?</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/plan-the-development-of-your-idea.php">Plan the development of your idea</a></li>
+			<li>Financing your idea and cost control</li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/creating-a-project-development-team.php">Creating a project development team</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/manage-a-development-project.php">Manage a development project</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/the-product-development-process.php">The product development process</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/pricing-your-proposed-service-or-product.php">Pricing your proposed service or product</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/sharing-your-ideas-with-others.php">Sharing your ideas with others</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/test-the-market.php">Test the market</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/checklist-developing-a-product-or-service.php">Checklist: developing a product or service</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-exploited-a-gap-in-the-market.php">Here's how I exploited a gap in the market</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-got-my-great-new-product-in-front-of-customers.php">Here's how I got my great new product in front of customers</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-developing-a-new-product-benefited-my-business.php">Here's how developing a new product benefited my business</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance options for your business</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20140827133341/https://sbri.innovateuk.org/" target="_blank" rel="noopener noreferrer">SBRI opportunities on the Technology Strategy Board website</a></li>
+			<li><a href="/create-innovate-and-protect/create-and-improve-products-and-services/developing-new-business-ideas.php">Use our interactive tool to evaluate your business idea</a></li>
+		</ul>
+
+		<h3>Also on this site</h3>
+
+		<ul>
+			<li><a href="https://www.gov.uk/business-finance-explained" target="_blank" rel="noopener noreferrer">Understand how much money you need to start up</a></li>
+			<li><a href="/finance-and-grants/prepare-to-raise-finance/how-to-attract-investment/use-your-business-plan-to-get-funding.php">Use your business plan to get funding</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d.php">Support networks and facilities for innovation and R&amp;D</a></li>
+			<li><a href="/create-innovate-and-protect/use-innovation-in-your-business/an-innovation-start-up.php">An innovation start-up</a></li>
+			<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/choose-the-right-finance-when-starting-up.php">Choose the right finance when starting up</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports.php">Market research and market reports</a></li>
+			<li><a href="/sales-and-marketing/pricing/price-your-product-or-service.php">Price your product or service</a></li>
+			<li><a href="/sales-and-marketing/marketing/understand-your-competitors.php">Understand your competitors</a></li>
+			<li><a href="/finance-and-grants/finance-options/grants-and-government-support/innovation-research-and-development-grants.php">Innovation, research and development grants</a></li>
+			<li><a href="ev/grow-your-business/planning-how-to-grow/project-management-the-basics.php">Project management - the basics</a></li>
+			<li><a href="/tax-payroll-and-company-information/corporation-tax/claiming-losses-allowances-and-reliefs/research-and-development-r-and-d-relief-for-corporation-tax.php">Research and Development (R&amp;D) Relief for Corporation Tax</a></li>
+			<li><a href="/create-innovate-and-protect/design-for-business-success/how-can-I-use-design-in-my-business.php">How can I use design in my business?</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/manage-your-research-design-and-development.php">Manage your research, design and development</a></li>
+		</ul>
+
+		<img src="../../../_img/px_growthandinnovation.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
