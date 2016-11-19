208a07c870298fe951dddf696aa50f81c8f14a25
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-19 20:00:15 +0000

    Completed ‘The product development process’ page.

---
+++ create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/the-product-development-process.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The product development process | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Research and develop your business ideas, new products and services</h1>
+		<h2>The product development process</h2>
+		<p>The process of developing new products or services can be divided into a number of key stages:</p>
+		<ul>
+			<li>Idea generation - to come up with innovative new ideas.</li>
+			<li>Idea distillation - to screen out any ideas not worth taking forward.</li>
+			<li>Concept definition - to consider specifications such as technical feasibility and market potential. If you're planning a new product, you should also consider the design process at this point - see our guide on <a href=" /create-innovate-and-protect/design-for-business-success/how-can-I-use-design-in-my-business.php">how can I use design in my business?</a> You can also <a href="http://www.designcouncil.org.uk/resources/search/%2522product%2520design%2522?retain-filters=1" target="_blank" rel="noopener noreferrer">find information on the design process on the Design Council website</a>.</li>
+			<li>Strategic analysis - to ensure your ideas fit into your business' strategic plans.</li>
+			<li>Concept development - to create a prototype product or pilot service.</li>
+			<li>Test marketing and finalising the concept - to modify the product or service according to customer, manufacturer and support organisations' feedback. This means deciding the best timing and process for piloting your new product or service.</li>
+			<li>Product launch - before setting a date for your launch, you should determine how you will sell, promote and support your product or service. It is important to get it right the first time, but remember that any decisions to delay your launch should be balanced against the danger that your competitors will beat you to market.</li>
+		</ul>
+		<p>You can <a href="#">see our diagram of the product development process</a>.</p>
+		<p>Some of these stages could overlap, but the presence of a staged product development process will help keep timing and costs under control. For more information on managing projects, see our guide on how to <a href="/create-innovate-and-protect/research-and-development/manage-your-research-design-and-development.php">manage your research, design and development</a>.</p>
+		<h3>The lifecycle of products and services</h3>
+		<p>All products and services have a lifecycle - this is the period that runs from the initial idea and development of a product to its withdrawal from the market and beyond. There are five key stages in the lifecycle of any product or service:</p>
+		<ul>
+			<li>Development</li>
+			<li>Introduction</li>
+			<li>Growth </li>
+			<li>Maturity</li>
+			<li>Decline</li>
+		</ul>
+		<p>Identifying where products or services are in their lifecycle is central to your profitability. For more information, see our guide on the <a href="/create-innovate-and-protect/research-and-development/product-life-cycle.php">product life cycle</a>.</p>
+
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/manage-a-development-project.php" title="Manage a development project">Prev</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/pricing-your-proposed-service-or-product.php" title="Pricing your proposed service or product">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">Introduction</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/finding-and-developing-your-idea-and-new-products-and-services.php">Finding and developing your idea and new products and services</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/is-there-a-market-for-my-idea.php">Is there a market for my idea?</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/plan-the-development-of-your-idea.php">Plan the development of your idea</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/financing-your-idea-and-cost-control.php">Financing your idea and cost control</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/creating-a-project-development-team.php">Creating a project development team</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/manage-a-development-project.php">Manage a development project</a></li>
+			<li>The product development process</li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/pricing-your-proposed-service-or-product.php">Pricing your proposed service or product</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/sharing-your-ideas-with-others.php">Sharing your ideas with others</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/test-the-market.php">Test the market</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/checklist-developing-a-product-or-service.php">Checklist: developing a product or service</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-exploited-a-gap-in-the-market.php">Here's how I exploited a gap in the market</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-got-my-great-new-product-in-front-of-customers.php">Here's how I got my great new product in front of customers</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-developing-a-new-product-benefited-my-business.php">Here's how developing a new product benefited my business</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/do-it-online/look-up-patents.php">Search the patents directory with the Intellectual Property Office</a></li>
+			<li><a href="/do-it-online/apply-for-a-uk-patent.php">Apply for a patent online with the Intellectual Property Office</a></li>
+			<li><a href="http://www.productmanager.co.uk/innovation-management.html" target="_blank" rel="noopener noreferrer">Innovation management information on the Innovation Process Management website</a></li>
+			<li><a href="http://www.designcouncil.org.uk/search/site/%22product%20design%22?search_only=1" target="_blank" rel="noopener noreferrer">Product design advice on the Design Council website</a></li>
+		</ul>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle.php">Product life cycle</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports.php">Market research and market reports</a></li>
+			<li><a href="/sales-and-marketing/pricing/price-your-product-or-service.php">Price your product or service</a></li>
+			<li><a href="/sales-and-marketing/marketing/understand-your-competitors.php">Understand your competitors</a></li>
+			<li><a href=" /create-innovate-and-protect/design-for-business-success/how-can-I-use-design-in-my-business.php">How can I use design in my business?</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/manage-your-research-design-and-development.php">Manage your research, design and development</a></li>
+		</ul>
+
+		<img src="../../../_img/px_growthandinnovation.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
