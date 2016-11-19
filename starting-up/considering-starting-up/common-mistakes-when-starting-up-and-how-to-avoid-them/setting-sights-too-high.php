d74fb2cc04674aacc87f512d36e348f1bf5a0f13
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-19 22:52:14 +0000

    Completed ‘Setting sights too high’ page.

---
+++ starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/setting-sights-too-high.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Setting sights too high | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Common mistakes when starting up - and how to avoid them</h1>
+		<h2>Setting sights too high</h2>
+		<p>It is important to make realistic forecasts about your business' potential. During the start-up phase, it can be easy to make over-optimistic forecasts, however there can be serious consequences for your business if your projections are not realistic.</p>
+		<h3>Over-optimistic forecasts about market size</h3>
+		<p>Inaccurate forecasting of market size is a common mistake when starting up.</p>
+		<p>Cash levels can be quickly depleted if you recruit too many people, buy unnecessary equipment or spend too much on business premises. Effective cashflow and income forecasting can help you avoid this.</p>
+		<p>Inaccurate forecasting is often linked to poor market research, so it is essential to get your research right. For more information, see our guides on <a href="/sales-and-marketing/marketing/market-research-and-market-reports.php">market research and market reports</a> and <a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">cashflow management: the basics</a>.</p>
+		<h3>Focusing on sales volume or size not profit</h3>
+		<p>A common mistake for new businesses is to focus too much on growing the sales volume or size rather than profit.</p>
+		<p>Overtrading can occur during the rapid expansion of a new business when it takes on more orders than can be supported by its working capital or net current assets. This can have serious repercussions.</p>
+		<p>For more information, see our guides on how to <a href="/sales-and-marketing/selling/forecast-and-plan-your-sales.php">forecast and plan your sales</a> and <a href="/finance-and-grants/financial-planning-and-accounts/avoid-the-problems-of-overtrading.php">avoid the problems of overtrading</a>.</p>
+		<h3>Diversifying too soon</h3>
+		<p>There may be a temptation for you to tap into a new market or geographical area, but keeping a clear focus on your core business is crucial. Diversifying too quickly can actually increase your business risks during the vulnerable start-up stage.</p>
+		<h3>Poor planning</h3>
+		<p>Poor planning will increase your chances of making business mistakes and will reduce the probability of achieving your goals.</p>
+		<p>Drawing up a high-quality and realistic business plan is essential. A business plan will help to secure external funding, pre-empt problems and measure how well your business is doing.</p>
+		<p>Writing a marketing plan will also ensure that you take into account your target customers, your marketing objectives and will help you set goals to address these.</p>
+		<p>For more information, see our guides on how to <a href="/starting-up/business-planning/prepare-a-business-plan.php">prepare a business plan</a> and <a href="/sales-and-marketing/marketing/write-a-marketing-plan.php">write a marketing plan</a>.</p>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/weak-financial-planning.php" title="Weak financial planning">Prev</a></li>
+			<li><a href="detailf94f.html?itemId=1075219871&amp;type=RESOURCES" title="Taking your eye off the competition">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them.php">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-or-inadequate-market-research.php">Poor or inadequate market research</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/weak-financial-planning.php">Weak financial planning</a></li>
+			<li>Setting sights too high</li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/taking-your-eye-off-the-competition.php">Taking your eye off the competition</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-supplier-and-customer-controls.php">Poor supplier and customer controls</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-stock-and-asset-management.php">Poor stock and asset management</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/hiring-the-wrong-people.php">Hiring the wrong people</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/here-is-how-I-started-a-successful-business_flash-video.php">Here's how I started a successful business (Flash video)</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-selling-customers" target="_blank" rel="noopener noreferrer">Understand your market</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">Cashflow management: the basics</a></li>
+			<li><a href="/sales-and-marketing/selling/forecast-and-plan-your-sales.php">Forecast and plan your sales</a></li>
+			<li><a href="/sales-and-marketing/marketing/write-a-marketing-plan.php">Write a marketing plan</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
