b12cf3c66c8474bf44d270f72ec305e6b6c0f1c4
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-19 20:52:27 +0000

    Completed ‘Here's how I got my great new product in front of customers’ page.

---
+++ create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-got-my-great-new-product-in-front-of-customers.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Here's how I got my great new product in front of customers | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Here's how I got my great new product in front of customers</h1>
+		<h2>Here's how I got my great new product in front of customers</h2>
+		<p>Sam Tate co-founded Sprue Aegis plc in 1998 when he and his business partner came up with an innovative smoke detector. The FireAngel fits between a light fitting and the bulb, taking its energy from the mains. FireAngel charges up its rechargeable battery when the light is on - and runs off the battery when the light is off.</p>
+		<p>The company hasn't looked back since convincing one high street retailer to sell the detector. It's now stocked in around 6,000 outlets.</p>
+		<p><strong>What I did</strong></p>
+		<h3>Research the target consumer</h3>
+		<p>"We spent months digging out market research reports, talking to the Fire Brigade and the Office of the Deputy Prime Minister - the government department responsible for fire safety (now the responsibility of Communities and Local Government).</p>
+		<p>"Once we confirmed the FireAngel was a valuable idea, I went out on to the street to see if there was a market for it. We basically just stopped people, showed them the prototype and asked a set list of questions. We also looked closely at our competitors' products and who their market was.</p>
+		<p>"We initially worked out a sales strategy involving the Internet, direct marketing and advertising. However, we soon knew on a cost-per-sale basis this wouldn't be feasible - we would have sold less and had to charge twice as much. We then realised an ideal way to reach consumers who wanted a quick solution was through supermarkets."</p>
+		<h3>Get the right price and sales channel</h3>
+		<p>"Most smoke detectors retail at between &pound;5 and &pound;10. We knew we could charge a premium because fitting the alarm is as simple as changing a bulb. It doesn't need a battery and it lasts for up to ten years.</p>
+		<p>"We set the price at around &pound;20 but the retailer obviously takes a margin. Remember - it's a lot harder to put the price up than drop it down, so don't go in too cheaply."</p>
+		<h3>Nail the sales pitch early</h3>
+		<p>"It's really important to be able to get your message across in 30 seconds flat. If you get the chance to speak to a buyer, you need to be able to sock it to them - they're extremely busy people. It's definitely down to trial and error so I would recommend rehearsing your sales pitch in front of anyone who will listen, and not delivering it for the first time to the top buyer at a potentially major client.</p>
+		<p>"It took three years to get our product to market. A lot of this time was spent on design and safety testing, but it took 18 months of talking to one well-known high street retailer before they agreed to stock the alarm. Once you're in there with one retail chain, it gets much easier to approach others. But you definitely have to be persistent to succeed."</p>
+		<p><strong>What I'd do differently</strong></p>
+		<h3>Don't take 'no' for an answer - right from the start</h3>
+		<p>"It's easy to say, but be prepared for knockbacks. We got them - and would have started to get demoralised by them without a total belief in our product.</p>
+		<p>"After a short time we realised that persistence was the key to making it all happen. Buyers are busy people and you've got to make yourself stand out from the crowd. For example, we waited in the reception of one high street retailer's head office until the buyer agreed to see us. Sometimes you have to go to that level."</p>
+		<p><a href="/find-a-case-study.php">Read more case studies that describe first hand how people tackle real-life challenges and opportunities</a>.</p>
+
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-exploited-a-gap-in-the-market.php" title="Here's how I exploited a gap in the market">Prev</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-developing-a-new-product-benefited-my-business.php" title="Here's how developing a new product benefited my business">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">Introduction</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/finding-and-developing-your-idea-and-new-products-and-services.php">Finding and developing your idea and new products and services</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/is-there-a-market-for-my-idea.php">Is there a market for my idea?</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/plan-the-development-of-your-idea.php">Plan the development of your idea</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/financing-your-idea-and-cost-control.php">Financing your idea and cost control</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/creating-a-project-development-team.php">Creating a project development team</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/manage-a-development-project.php">Manage a development project</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/the-product-development-process.php">The product development process</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/pricing-your-proposed-service-or-product.php">Pricing your proposed service or product</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/sharing-your-ideas-with-others.php">Sharing your ideas with others</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/test-the-market.php">Test the market</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/checklist-developing-a-product-or-service.php">Checklist: developing a product or service</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-exploited-a-gap-in-the-market.php">Here's how I exploited a gap in the market</a></li>
+			<li>Here's how I got my great new product in front of customers</li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-developing-a-new-product-benefited-my-business.php">Here's how developing a new product benefited my business</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<div class="media-object stack-for-small">
+			<div class="media-object-section">
+				<div class="thumbnail">
+					<img src="../../../_img/CS0046.jpg" width="181" height="121" border="0" alt="Sam Tate, Sprue Aegis plc" />
+				</div>
+			</div>
+			<div class="media-object-section">
+				<h3>
+					Sam Tate<br />
+					<a href="http://fireangel.co.uk" target="_blank" rel="noopener noreferrer" title="The website for Sprue Aegis plc, subject of one of our case studies">FireAngel Limited</a><br />
+					Sam's top tips:
+				</h3>
+				<ul>
+					<li>"Remember that getting your first customer will give you a sales record, making it easier for others to follow."</li>
+					<li>"Provide as much hard data as you can about your pricing and the benefits your product will bring to the buyer."</li>
+					<li>"Eliminate all the unknowns: make sure you understand your market, product, customers, users and brand."</li>
+				</ul>
+			</div>
+		</div>
+
+		<p><a href="/find-a-case-study.php"><strong>Find a case study</strong></a></p>
+
+		<img src="../../../_img/px_growthandinnovation.gif" width="181" height="5" alt="" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
