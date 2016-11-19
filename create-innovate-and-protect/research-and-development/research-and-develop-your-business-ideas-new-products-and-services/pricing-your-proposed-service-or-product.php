00bc70a70240196c401c95ab3058c81e9f12d61d
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-19 20:10:21 +0000

    Completed ‘Pricing your proposed service or product’ page.

---
+++ create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/pricing-your-proposed-service-or-product.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pricing your proposed service or product | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Research and develop your business ideas, new products and services</h1>
+		<h2>Pricing your proposed service or product</h2>
+		<p>Establishing a pricing strategy for a new product or service is an important part of the development process. You should consider pricing the moment you decide to take an idea forward as it will determine how much you can afford to invest in the project.</p>
+		<p>You will need to take the following factors into account:</p>
+		<ul>
+			<li>The benefits - or value - to the customer of your product or service compared with what the competition has to offer - will customers be prepared to pay the price you want to charge?</li>
+			<li>Whether or not you're first to market - is your product or service innovative or are you following a market trend?</li>
+			<li>The selling channels you want to use as this will affect your promotional spend and distribution costs.</li>
+			<li>How quickly you want to establish your product or service.</li>
+			<li>The expected lifecycle of your product or service.</li>
+			<li>How much you will need to charge to cover your costs.</li>
+		</ul>
+		<p>Strategic pricing can be used to drive sales and regulate demand. See our guide on how to <a href="/sales-and-marketing/pricing/price-your-product-or-service.php">price your product or service.</a></p>
+
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/the-product-development-process.php" title="The product development process">Prev</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/sharing-your-ideas-with-others.php" title="Sharing your ideas with others">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">Introduction</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/finding-and-developing-your-idea-and-new-products-and-services.php">Finding and developing your idea and new products and services</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/is-there-a-market-for-my-idea.php">Is there a market for my idea?</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/plan-the-development-of-your-idea.php">Plan the development of your idea</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/financing-your-idea-and-cost-control.php">Financing your idea and cost control</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/creating-a-project-development-team.php">Creating a project development team</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/manage-a-development-project.php">Manage a development project</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/the-product-development-process.php">The product development process</a></li>
+			<li>Pricing your proposed service or product</li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/sharing-your-ideas-with-others.php">Sharing your ideas with others</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/test-the-market.php">Test the market</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/checklist-developing-a-product-or-service.php">Checklist: developing a product or service</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-exploited-a-gap-in-the-market.php">Here's how I exploited a gap in the market</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-got-my-great-new-product-in-front-of-customers.php">Here's how I got my great new product in front of customers</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-developing-a-new-product-benefited-my-business.php">Here's how developing a new product benefited my business</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="l/sales-and-marketing/pricing/price-your-product-or-service.php">Price your product or service</a></li>
+			<li><a href="/sales-and-marketing/marketing/understand-your-competitors.php">Understand your competitors</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports.php">Market research and market reports</a></li>
+			<li><a href="/create-innovate-and-protect/design-for-business-success/how-can-I-use-design-in-my-business.php">How can I use design in my business?</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/manage-your-research-design-and-development.php">Manage your research, design and development</a></li>
+		</ul>
+
+		<img src="../../../_img/px_growthandinnovation.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
