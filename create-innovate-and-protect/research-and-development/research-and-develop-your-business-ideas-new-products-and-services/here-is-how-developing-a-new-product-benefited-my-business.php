980ef3ead403b5eb60dd4bde7126e5309c3efa76
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-11-19 21:47:28 +0000

    Completed ‘Here's how developing a new product benefited my business’ page. Note the use of Vimeo where the .flv file has been uploaded. Couldn’t get the XML captions to convert to a format that would upload to Vimeo. However if you sit and calculate the seconds to mins I’m sure it’ll work.

---
+++ create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-developing-a-new-product-benefited-my-business.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Here's how developing a new product benefited my business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Here's how developing a new product benefited my business</h1>
+		<h2>Here's how developing a new product benefited my business</h2>
+		<p>WOW Toys was formed in 1997 by toy maker Nadim Ednan-Laperouse and is based in Fulham, London. The company designs and supplies multi functional toys that don't need batteries for children aged 0-5 years. They currently sell to over 400 retail outlets in the UK and export their products to 36 countries around the world.</p>
+		<p>Here Nadim talks about the value of new product development, and how it has benefited his business.</p>
+
+		<div class="flex-video widescreen vimeo">
+			<iframe src="http://player.vimeo.com/video/192278297?badge=0&byline=0&portrait=0&title=0" width="720" height="540" allowfullscreen style="border:0"></iframe>
+		</div>
+		
+		<ul>
+			<li><a href="../../../_flash/eyi_new_product_transcript.txt">Read transcript</a></li>
+			<li><a title="Play the audio only version of this video" href="../../../_flash/eyi_new_product_audio.mp3">Listen to audio only (mp3, 13MB)</a></li>
+			<li><a href="/video-and-e-learning-help.php">Help</a></li>
+		</ul>
+
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-developing-a-new-product-benefited-my-business.php" title="Here's how I got my great new product in front of customers">Prev</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">Introduction</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/finding-and-developing-your-idea-and-new-products-and-services.php">Finding and developing your idea and new products and services</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/is-there-a-market-for-my-idea.php">Is there a market for my idea?</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/plan-the-development-of-your-idea.php">Plan the development of your idea</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/financing-your-idea-and-cost-control.php">Financing your idea and cost control</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/creating-a-project-development-team.php">Creating a project development team</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/manage-a-development-project.php">Manage a development project</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/the-product-development-process.php">The product development process</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/pricing-your-proposed-service-or-product.php">Pricing your proposed service or product</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/sharing-your-ideas-with-others.php">Sharing your ideas with others</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/test-the-market.php">Test the market</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/checklist-developing-a-product-or-service.php">Checklist: developing a product or service</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-exploited-a-gap-in-the-market.php">Here's how I exploited a gap in the market</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/research-and-develop-your-business-ideas-new-products-and-services/here-is-how-I-got-my-great-new-product-in-front-of-customers.php">Here's how I got my great new product in front of customers</a></li>
+			<li>Here's how developing a new product benefited my business</li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<div class="media-object stack-for-small">
+			<div class="media-object-section">
+				<div class="thumbnail">
+					<img src="../../../_img/Copy_of_wow.jpg" width="181" height="121" border="0" alt="" />
+				</div>
+			</div>
+			<div class="media-object-section">
+				<h3>
+					Nadim Ednan-Laperouse<br />
+					<a href="http://www.wowtoys.com/" target="_blank" rel="noopener noreferrer">WOW Toys</a><br />
+					Nadim's top tips:
+				</h3>
+				<ul>
+					<li>"Never be afraid of the seemingly unknown. Dream the future and then reach out to grasp it with both hands!"</li>
+					<li>"You must have a plan and a clear timeline of the events/decisions that you need to make from beginning to end."</li>
+					<li>"New product development demands financing and not only is it necessary to have the cash, but also to understand the expected return on investment."</li>
+				</ul>
+			</div>
+		</div>
+
+		<p><a href="/find-a-case-study.php"><strong>Find a case study</strong></a></p>
+		
+		<img src="../../../_img/px_growthandinnovation.gif" width="181" height="5" alt="" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
