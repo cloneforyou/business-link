2f04ff6c93ca3ad915df69dab6ead075d867e588
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 18:37:28 +0000

    Completed ‘Forecast your personal financial needs’ page.

---
+++ starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/forecast-your-personal-financial-needs.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Forecast your personal financial needs | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>How do I survive until my business is off the ground?</h1>
+		<h2>Forecast your personal financial needs</h2>
+		<p>When starting your business you will need to make a realistic forecast of your personal financial needs. A <strong>personal budget</strong> is a plan detailing the personal living expenses you will need to fund from the business or other sources. It should set limits to the amount you plan to spend each month on items like rent, food and housekeeping.</p>
+		<p>Tracking your personal spending can help you find out how much money you will need to take from the business. You can <a href="../../../_files/personal-budget-spreadsheet.xls" target="_blank" rel="noopener noreferrer">download our personal budget spreadsheet (XLS, 25K)</a> and adapt it to your individual needs.</p>
+		<p>You can now work out how much money you will need each month. If you multiply the monthly figure by 12, and make adjustments to cover one-off spending such as holidays or car tax, you will know how much you need to live on during your first year of trading.</p>
+		<p>It is important to be realistic. You may need to find other funds, borrow money or ask HM Revenue &amp; Customs or Jobcentre Plus whether you qualify for tax credits or benefits. Financial advisers usually say that the equivalent of three months money should be held on deposit for a rainy day.</p>
+		<p>Some expenses, such as your rent or mortgage, are likely to be fixed, whilst your spending on other items may change from month to month. You need to keep a close eye on the areas where savings can be made - such as leisure or travel. The first year in business is vital, so you may have to accept that a financial sacrifice of some sort is required to keep on trading.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground.php" title="How do I survive until my business is off the ground?">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/how-much-money-will-your-business-make.php" title="How much money will your business make?">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground.php" title="How do I survive until my business is off the ground?">Introduction</a></li>
+			<li>Forecast your personal financial needs</li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/how-much-money-will-your-business-make.php">How much money will your business make?</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/make-savings.php">Make savings</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/other-sources-of-income.php">Other sources of income</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/prepare-a-financial-plan.php">Prepare a financial plan</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/find-support.php">Find support</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/claim-tax-credits" target="_blank" rel="noopener noreferrer">Tax credit information on the Directgov website</a></li>
+			<li><a href="../../../_files/personal-budget-spreadsheet.xls" target="_blank" rel="noopener noreferrer">Download our personal budget spreadsheet template (XLS, 25K)</a></li>
+		</ul>
+				
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-finance-explained" title="Discover your options for self-financing and look at the pros and cons of using your own money to get started" target="_blank" rel="noopener noreferrer">Use your own money to start up</a></li>
+			<li><a href="https://www.gov.uk/business-finance-explained" title="Identify your start-up costs and manage your money. Find out how to forecast your cashflow and record your profit and loss" target="_blank" rel="noopener noreferrer">Understand how much money you need to start up</a></li>
+			<li><a href="https://www.gov.uk/business-finance-explained" title="Learn about the various sources of finance available to start-ups and which could be most appropriate for your business" target="_blank" rel="noopener noreferrer">Explore your finance options when starting up</a></li>
+			<li><a href="https://www.gov.uk/avoid-business-cashflow-problems" title="Understand whether you can meet your financial commitments and how to cope when things don't go to plan" target="_blank" rel="noopener noreferrer">Meet your financial commitments</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting.php" title="The importance of managing a budget as part of your business planning and how to create one">Business budgeting</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5"  />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
