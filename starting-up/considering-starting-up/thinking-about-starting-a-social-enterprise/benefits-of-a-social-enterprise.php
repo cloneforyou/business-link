8d8cd44da4d559b47a802454114bd5fdc3c262e0
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 23:20:05 +0000

    Completed ‘Benefits of a social enterprise’ page.

---
+++ starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/benefits-of-a-social-enterprise.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Benefits of a social enterprise | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Thinking about starting a social enterprise?</h1>
+		<h2>Benefits of a social enterprise</h2>
+		<p>Social enterprises need to generate revenue for sustainability but they also have equally important social and/or environmental aims. The requirement to manage this 'multiple bottom line' - financial, social and environmental - can result in unique challenges. However, the ability to bring about positive change to people and communities can be enormously satisfying and provide a means of making a living.</p>
+		<p>One of the benefits of running a social enterprise can be in providing employment for local people. This may include people who have traditionally found it hard to enter the labour market. A social enterprise can be the ideal environment for such people to develop their potential - either as an employee or heading up their own operation. The types of individual who can benefit from running or working in a social enterprise include:</p>
+		<ul>
+			<li>the long-term unemployed</li>
+			<li>people looking for a career change</li>
+			<li>people with learning disabilities</li>
+			<li>disabled people</li>
+			<li>people with mental health issues</li>
+			<li>ex-offenders</li>
+			<li>minority ethnic groups</li>
+			<li>women</li>
+			<li>young people - many schools now have pupil-run social enterprises</li>
+		</ul>
+		<p>Some groups of people may find it difficult to work within the limitations of a traditional working environment. Social firms are an example of social enterprise businesses that have been set up to remove these barriers to employment and offer a more flexible approach to work. <a href="http://www.socialfirmsuk.co.uk/" target="_blank" rel="noopener noreferrer">Find information on employing disadvantaged people in social enterprises on the Social Firms UK website</a>.</p>
+		<p>If you set up a social enterprise in your local community, you are likely to witness the social benefits first hand. For example, you could run a social enterprise that matches up people in need of flexible employment, such as parents of young children, with elderly or vulnerable people in need of care in the same neighbourhood.</p>
+		<h3>Marketing social enterprises</h3>
+		<p>Social enterprises need to be competitive in any environment in which they operate. Attracting customers is vital to success. Highlighting the social and/or environmental credentials of your service/product, could give you the edge over your competitors.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/why-set-up-a-social-enterprise.php" title="Why set up a social enterprise?">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/social-enterprise-skills.php" title="Social enterprise skills">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise.php" title="Thinking about starting a social enterprise?">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/what-is-a-social-enterprise.php">What is a social enterprise?</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/why-set-up-a-social-enterprise.php">Why set up a social enterprise?</a></li>
+			<li>Benefits of a social enterprise</li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/social-enterprise-skills.php">Social enterprise skills</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/challenges-facing-social-enterprises.php">Challenges facing social enterprises</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/business-and-marketing-plans.php">Business and marketing plans</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/financing-your-social-enterprise.php">Financing your social enterprise</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/social-enterprises-and-the-public-sector.php">Social enterprises and the public sector</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/checklist-starting-up-a-social-enterprise.php">Checklist: starting up a social enterprise</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-being-a-social-enterprise-works-for-our-business.php">Here's how being a social enterprise works for our business</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-I-started-a-successful-social-enterprise.php">Here's how I started a successful social enterprise</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/78927/building-stronger-civil-society.pdf" target="_blank" rel="noopener noreferrer">Download 'Big Society' strategy information from the Cabinet Office website (PDF, 182K)</a></li>
+			<li><a href="http://www.socialfirmsuk.co.uk/" target="_blank" rel="noopener noreferrer">Disadvantaged persons employment in social enterprises on the Social Firms UK website</a></li>
+			<li><a href="http://www.socialenterprise.org.uk/about/about-social-enterprise" target="_blank" rel="noopener noreferrer">Social enterprise information on the Social Enterprise UK website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php">Set up a social enterprise</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/corporate-social-responsibility.php">Corporate social responsibility</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/overseas-supplier-relationships/ethical-trading.php">Ethical trading</a></li>
+			<li><a href="/employment-and-skills/different-types-of-worker/employing-family-members-and-voluntary-staff.php">Employing family members and voluntary staff</a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" title="Learn about the different types of business and legal structures to help you choose the right one for your new business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" title="Tutorials, videos and tools to help you decide how to become self-employed - from sole trader to limited company, franchisee to social enterprise and more" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+			<li><a href="https://www.gov.uk/set-up-business" title="Case studies and tutorials to help you understand the legal dos and don'ts of naming your business" target="_blank" rel="noopener noreferrer">Name your business</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
