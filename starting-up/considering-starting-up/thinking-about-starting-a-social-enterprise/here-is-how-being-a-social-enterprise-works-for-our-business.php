198ee85dada44624c691b910b2fdb7dfce84080e
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 23:49:22 +0000

    Completed ‘Here's how being a social enterprise works for our business’ page.

---
+++ starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-being-a-social-enterprise-works-for-our-business.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Here's how being a social enterprise works for our business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Here's how being a social enterprise works for our business</h1>
+		<h2>Here's how being a social enterprise works for our business</h2>
+		<p>Swamp Circus is the longest-running contemporary circus in the UK, promoting the art of circus performance through highly original entertainment, training, education and community regeneration projects. Founded in Sheffield in 1986, the business has always had strong charitable and environmental links. Becoming a charitable trust (Swamp Circus Trust) in 1993 helped the company to take its community work to new levels. Development Officer Tim Woolliscroft explains.</p>
+		<h3>What I did</h3>
+		<h3>Get plenty of advice</h3>
+		<p>"Swamp Circus Trust uses circus skills in a variety of projects that benefit individuals and communities in the South Yorkshire area. That could be anything from confidence-building workshops in schools, to live shows that tackle community themes. The business also has a strong commercial side, providing high quality circus-based entertainment and team-building events to the corporate market. The two sides of the business co-existed for several years when we decided we wanted to do more.</p>
+		<p>"We got advice from several sources, including Business Link, about how we could set up a more appropriate legal structure. The main criteria was that we wanted to be able to pursue more funding opportunities, such as grants - many of which are easier to apply for and get if you have charitable status. That, and the fact that we were already engaged in charitable work, led us to incorporate as a charitable trust, which we did with the help of legal advisers."</p>
+		<h3>Operate as a business</h3>
+		<p>"While a social enterprise has built-in differences compared with a traditional company, it still needs to operate as a business. The process of becoming a charitable trust helped us to put business structures in place. For example, we had to write business plans, plus a company constitution outlining our guiding principles and goals. We also had to appoint trustees, which is a valuable exercise because it makes you think about where the skills gaps are in running your company, then gives you the opportunity to fill them.</p>
+		<p>"Preparing a business plan also made us focus on the sources of finance available. Setting up as a charitable trust can limit the options for traditional bank finance, partly because the legal structure means that trustees may be personally liable for debts, which can make them more cautious about loans. So we've had to be flexible and creative, exploring as many financing options as possible and adapting ourselves as we go.</p>
+		<p>We now operate using a mixture of grants plus the income we generate from our commercial clients. Both have increased over the years as a result of careful planning. We have also developed strong partnerships with a number of organisations in our area, which is invaluable for exchanging funding ideas."</p>
+		<h3>Reinvest wisely</h3>
+		<p>"Swamp Circus Trust is a non-profit company, which means that once we've covered our core costs, any profit is reinvested straight back into the business. We've found from experience that being non-profit helps with funding opportunities.</p>
+		<p>"It would be tempting to pour all our profits directly into the community projects we run and into starting new ones, since we always have fresh ideas waiting in the wings! However, we are careful to invest in the business infrastructure as well, things like premises, equipment and marketing. Without covering these costs we cannot develop new projects so it's a false economy to ignore that side of things."</p>
+		<h3>What I'd do differently</h3>
+		<h3>Use charitable status for marketing purposes</h3>
+		<p>"When we set up as a charitable trust, we didn't make much of a fanfare about it and in retrospect we probably should have done. It's only recently that we've started actively publicising our status and using it more creatively as a marketing tool. Our clients do cite our community work as an incentive to employ us, but we're careful not to make that the only reason. We want to be judged on the quality of our service as well."</p>
+		<h3>Employ people who share the passion</h3>
+		<p>"As a social enterprise grows, you sometimes need to take on people with commercial experience, but it's ultimately frustrating if they don't also share the original vision and sense of excitement. It takes a certain type of person to make a success of a social enterprise and after one or two early mistakes, we're a lot more choosy these days about who we employ."</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/checklist-starting-up-a-social-enterprise.php" title="Checklist: starting up a social enterprise">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-I-started-a-successful-social-enterprise.php" title="Here's how I started a successful social enterprise">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise.php" title="Thinking about starting a social enterprise?">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/what-is-a-social-enterprise.php">What is a social enterprise?</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/why-set-up-a-social-enterprise.php">Why set up a social enterprise?</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/benefits-of-a-social-enterprise.php">Benefits of a social enterprise</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/social-enterprise-skills.php">Social enterprise skills</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/challenges-facing-social-enterprises.php">Challenges facing social enterprises</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/business-and-marketing-plans.php">Business and marketing plans</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/financing-your-social-enterprise.php">Financing your social enterprise</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/social-enterprises-and-the-public-sector.php">Social enterprises and the public sector</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/checklist-starting-up-a-social-enterprise.php">Checklist: starting up a social enterprise</a></li>
+			<li>Here's how being a social enterprise works for our business</li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-I-started-a-successful-social-enterprise.php">Here's how I started a successful social enterprise</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<div class="media-object stack-for-small">
+			<div class="media-object-section">
+				<div class="thumbnail">
+					<img src="../../../_img/socialenterpriseCS.jpg" width="181" height="121" border="0" alt="Tim Woolliscroft, Swamp Circus Trust" />
+				</div>
+			</div>
+			<div class="media-object-section">
+				<h3>
+					Tim Woolliscroft<br />
+					<a href="http://www.swamptrust.org.uk/" target="_blank" rel="noopener noreferrer">Swamp Circus Trust</a><br />
+					Tim's top tips:
+				</h3>
+				<ul>
+					<li>"Be flexible, adaptable and creative in your approach to generating income."</li>
+					<li>"Choose trustees with care - they need to provide advice and stability, but equally you don't want people who are going to put up barriers."</li>
+					<li>"Maintain rigorous professional standards so that your clients have confidence in your abilities, over and above the 'warm feeling' they get from employing a social enterprise."</li>
+				</ul>
+			</div>
+		</div>
+
+		<p><strong><a href="/find-a-case-study.php" title="">Find a case study</a></strong></p>
+
+		<img src="../../../_img/px_startingabusiness.gif" width="181" height="5" alt="" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
