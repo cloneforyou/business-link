a9cf28509d861f0056e275410eb0a57cafe43403
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 20:06:51 +0000

    Completed ‘Find support’ page.

---
+++ starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/find-support.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Find support | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>How do I survive until my business is off the ground?</h1>
+		<h2>Find support</h2>
+		<p>Most businesses need assistance at some stage in their development. This is especially true of businesses in the early days.</p>
+		There are a number of sources of help, including:
+		<ul>
+			<li>local Business Links</li>
+			<li>start-up schemes run by local enterprise agencies and others</li>
+			<li>schemes for young starters such as Shell LiveWIRE and Prince's Trust</li>
+			<li>schemes for starters aged 50 plus, eg PRIME </li>
+			<li>schemes for women starters including Women in Business, and everywoman</li>
+			<li>schemes for starters from ethnic minority communities including the African Caribbean Business Network and the Asian Business Network (ABDN)</li>
+			<li>financial advisers</li>
+			<li>accountants</li>
+		</ul>
+		<p>Bear in mind that you will almost certainly have to pay for the specialist knowledge and advice from financial advisers and accountants. You can <a href="https://www.unbiased.co.uk/" target="_blank" rel="noopener noreferrer">search for an independent financial adviser on the Independent Financial Adviser Promotion website</a>.</p>
+		<p>It is a good idea to have an accountant for your business. Although having an accountant is an additional expense, it is a good investment. For more information, see our guide on how to <a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">choose and manage an accountant</a>.</p>
+		<p>The Prince's Trust is an initiative to help young people to succeed in business. The Trust offers advice, opportunities and financial assistance. <a href="https://www.princes-trust.org.uk/" target="_blank" rel="noopener noreferrer">Find out how the Trust can help you on the Prince's Trust website</a>.</p>
+		<p>Another body that helps new businesses is Shell LiveWIRE. This organisation aims to encourage young people aged between 18 and 30 to start up their own business. <a href="http://www.shell-livewire.org/" target="_blank" rel="noopener noreferrer">Find out what help you can get on the Shell LiveWIRE website</a>.</p>
+		<p>
+			<p>If you're receiving <strong>Jobseeker's Allowance</strong> and want to get back into employment by starting your own business, <a href="https://www.gov.uk/new-enterprise-allowance" target="_blank" rel="noopener noreferrer">find out how New Enterprise Allowance can help you on the Directgov website </a>. You can also ask your adviser about the self-employed support package provided by Business Link through Jobcentre Plus. This package is aimed at people who have been unemployed and claiming Jobseeker's Allowance for six months or longer.</p>
+		</p>
+		<p>You may also be entitled to Working Tax Credits. These are payments to top up the earnings of working people on low incomes, including the self-employed. You can find out about Working Tax Credits by calling the HMRC Tax Credits Helpline on Tel 0845 300 3900.</p>
+		<p>If you find that your business is unable to pay the taxes it owes to HMRC, contact the HMRC Business Payment Support Service. Their team will review your circumstances and discuss temporary options tailored to your business needs, such as arranging for you to make payments over a longer period. You can contact the HMRC Business Payment Support Service Helpline on Tel 0845 302 1435.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/prepare-a-financial-plan.php" title="Prepare a financial plan">Prev</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground.php" title="How do I survive until my business is off the ground?">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/forecast-your-personal-financial-needs.php">Forecast your personal financial needs</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/how-much-money-will-your-business-make.php">How much money will your business make?</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/make-savings.php">Make savings</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/other-sources-of-income.php">Other sources of income</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/prepare-a-financial-plan.php">Prepare a financial plan</a></li>
+			<li>Find support</li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Prince's Trust Helpline</h3>
+		<p>0800 842 842<br/></p>
+		<h3>HMRC Newly Self-Employed Helpline</h3>
+		<p>0845 915 4515<br/></p>
+		<h3>HMRC Tax Credits Helpline</h3>
+		<p>0845 300 3900<br/></p>
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101805&type=HUBCHASSIS" title="Watch our video showing how other people used business mentoring when starting up and find links to business mentor sites" target="_blank" rel="noopener noreferrer">Find out about business mentoring</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101974&type=HUBCHASSIS" title="Watch our video on how to get the most from business networking and find networking events near you" target="_blank" rel="noopener noreferrer">Get tips on how to network</a></li>
+			<li><a href="https://www.gov.uk/business-finance-support-finder" title="Watch our case study on how a start up business found the right help and support and get tips on the best way to use outside help" target="_blank" rel="noopener noreferrer">Find the right support for your new business</a></li>
+			<li><a href="/sales-and-marketing/online-marketing/online-business-networking-and-social-networking.php">Online business networking and social networking</a></li>
+			<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up.php">Extra help and support available for your start up</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5"  />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
