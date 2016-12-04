9823ae780e8826632223efb751659b4afff9997a
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 16:22:12 +0000

    Completed ‘Poor stock and asset management’ page.

---
+++ starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-stock-and-asset-management.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Poor stock and asset management | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Common mistakes when starting up - and how to avoid them</h1>
+		<h2>Poor stock and asset management</h2>
+		<p>Poor stock control and over-investment in fixed assets can mean your capital is tied up unnecessarily.</p>
+		<h3>Poor stock control</h3>
+		<p>Efficient stock control (inventory) will mean you have the right amount of stock in the right place at the right time. It ensures that capital is not tied up unnecessarily, and protects production when there are problems with the supply chain.</p>
+		<p>You need to put systems in place to keep close track of stock levels and values. Taking control will allow you to free up cash, while also having the right amount of stock on hand.</p>
+		<p>There are a number of ways you can approach stock control. You can:</p>
+		<ul>
+			<li>re-order when stock reaches a minimum level</li>
+			<li>carry out regular reviews of stock</li>
+			<li>use <strong>just in time</strong> (JIT) delivery to avoid excessive stock build up</li>
+		</ul>
+		<p>See our guide on <a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory.php">stock control and inventory</a>.</p>
+		<h3>Over-investing in fixed assets</h3>
+		<p>In the early years of your new business, you need to limit drawing on your cash reserves unnecessarily. Over-investment in fixed assets, such as office furniture or computer equipment, can be a problem. Acquiring fixed assets outright gives you ownership straightaway, but you have to pay for the full cost upfront, which drains cash.</p>
+		<p>The alternatives include:</p>
+		<ul>
+			<li><strong>Leasing assets</strong> - at least while your business finds its feet. This allows you to spread payments in regular instalments over a fixed period, thus freeing up more cash. You may be able to upgrade equipment without having to buy more up-to-date models.</li>
+			<li><strong>Hire purchase</strong> - you own the asset at the end of the payment process. This is not the case with leasing.</li>
+			<li><strong>Buying second hand</strong> - for office furniture, fittings, etc. You can find second hand furniture in a number of places. For example, check your local press and local auctions. Green Works is a registered charity that recycles and sells on used office furniture. <a href="#">Find second hand furniture on the Green Works website</a>.</li>
+		</ul>
+		<p>See our guide on how to <a href="/finance-and-grants/manage-your-cashflow/decide-whether-to-lease-or-buy-assets.php">decide whether to lease or buy assets</a>.</p>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them.php">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-or-inadequate-market-research.php">Poor or inadequate market research</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/weak-financial-planning.php">Weak financial planning</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/setting-sights-too-high.php">Setting sights too high</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/taking-your-eye-off-the-competition.php">Taking your eye off the competition</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-supplier-and-customer-controls.php">Poor supplier and customer controls</a></li>
+			<li>Poor stock and asset management</li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/hiring-the-wrong-people.php">Hiring the wrong people</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/here-is-how-I-started-a-successful-business_flash-video.php">Here's how I started a successful business (Flash video)</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory.php">Stock control and inventory</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/decide-whether-to-lease-or-buy-assets.php">Decide whether to lease or buy assets</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
