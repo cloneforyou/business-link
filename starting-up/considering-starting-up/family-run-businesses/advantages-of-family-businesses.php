fb22ff265eb0785afc31cf12d81c268096ff6bed
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 21:19:44 +0000

    Completed ‘Advantages of family businesses’ page.

---
+++ starting-up/considering-starting-up/family-run-businesses/advantages-of-family-businesses.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Advantages of family businesses | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Family-run businesses</h1>
+		<h2>Advantages of family businesses</h2>
+		<p>If you start or join a family business you're likely to benefit from a range of advantages which you often don't find in other enterprises. These include:</p>
+		<ul>
+			<li><strong>Common values</strong> - you and your family are likely to share the same ethos and beliefs on how things should be done. This will give you an extra sense of purpose and pride - and a competitive edge for your business.</li>
+			<li><strong>Strong commitment</strong> - building a lasting family enterprise means you're more likely to put in the extra hours and effort needed to make it a success. Your family is more likely to understand that you need to take a more flexible approach to your working hours.</li>
+			<li><strong>Loyalty</strong> - strong personal bonds mean you and family members are likely to stick together in hard times and show the determination needed for business success.</li>
+			<li><strong>Stability</strong> - knowing you're building for future generations encourages the long-term thinking needed for growth and success - though it can also produce a potentially damaging inability to react to change.</li>
+			<li><strong>Decreased costs</strong> - family members may be more willing to make financial sacrifices for the sake of the business. For example, accepting lower pay than they would get elsewhere to help the business in the longer term, or deferring wages during a cashflow crisis. You may also find you don't need employers' liability insurance if you only employ close family members.</li>
+		</ul>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses.php" title="Family-run businesses">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/key-things-to-consider-when-setting-up-a-family-business.php" title="Key things to consider when setting up a family business">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses.php" title="Family-run businesses">Introduction</a></li>
+			<li>Advantages of family businesses</li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/key-things-to-consider-when-setting-up-a-family-business.php">Key things to consider when setting up a family business</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/communication-and-family-businesses.php">Communication and family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/managing-conflict-in-family-businesses.php">Managing conflict in family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/pay-and-benefits-for-family-members.php">Pay and benefits for family members</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/draw-up-a-family-business-constitution.php">Draw up a family-business constitution</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/succession-planning-in-family-businesses.php">Succession planning in family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/here-is-how-having-a-family-member-in-the-business-made-a-difference.php">Here's how having a family member in the business made a difference</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="hse_employers-liability-compulsory-insurance-act-1969.pdf" target="_blank" rel="noopener noreferrer">Download a guide to employers' liability insurance from the Health &amp; Safety Executive website (PDF, 192K)</a></li>
+			<li><a href="http://www.ifb.org.uk/" target="_blank" rel="noopener noreferrer">Family business resources on the Institute for Family Business website</a></li>
+			<li><a href="#">Download a guide on family businesses from the BDO Stoy Hayward website (PDF, 50K)</a></li>
+			<li><a href="#">Download a family-business rulebook from the BDO Stoy Hayward website (PDF, 55K)</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/finance-and-grants/finance-options/borrowing/financing-from-friends-and-family.php">Financing from friends and family</a></li>
+			<li><a href="/employment-and-skills/different-types-of-worker/employing-family-members-and-voluntary-staff.php">Employing family members and voluntary staff</a></li>
+			<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php">Starting a business from home </a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
