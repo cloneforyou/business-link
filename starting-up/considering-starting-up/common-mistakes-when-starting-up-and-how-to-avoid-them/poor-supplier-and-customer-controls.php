a035826fd67efa9241eb86dbd16c154dae7ad71d
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 16:08:56 +0000

    Completed ‘Poor supplier and customer controls’ page.

---
+++ starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-supplier-and-customer-controls.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Poor supplier and customer controls | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Common mistakes when starting up - and how to avoid them</h1>
+		<h2>Poor supplier and customer controls</h2>
+		<p>Common mistakes for new businesses include setting up unsatisfactory credit arrangements and not taking due care when choosing suppliers. Choose carefully as your business' profitability and reputation could be at stake.</p>
+		<h3>Setting up poor supplier contracts</h3>
+		<p>Finding a reliable and competitively priced supplier can be vital to the success of your business. This is because you rely on your suppliers to provide you with the goods and services your business needs to operate. And getting the best deals can have a significant effect on your business' profits.</p>
+		<p>When selecting your suppliers, price is an obvious concern. However, other factors such as value for money, quality, reliability and service must also be taken into consideration.</p>
+		<p>Establish exactly what you are looking for in a supplier. Carry out a credit check to ensure that the supplier can deliver what you need and is not about to fold. Once you have identified your chosen supplier, you can then discuss terms and conditions and draw up a formal contract.</p>
+		<p>For more information, see our guides on how to <a href="/grow-your-business/growth-through-strategic-sourcing/manage-your-suppliers.php">manage your suppliers</a> and <a href="/grow-your-business/growth-through-strategic-sourcing/negotiate-the-right-deal-with-suppliers.php">negotiate the right deal with suppliers</a>, and our guide on <a href="/grow-your-business/growth-through-strategic-sourcing/choosing-the-right-suppliers.php">choosing the right suppliers</a>.</p>
+		<h3>Setting up poor credit arrangements</h3>
+		<p>If you are dealing with a potential new customer, it can be tempting to offer credit without carrying out checks. But this can leave your business exposed to delayed or non-payment. You may find that you cannot pay your suppliers or bank on time. In turn, they may withdraw their supplies or funds, putting your business at risk.</p>
+		<p>To avoid potential problems with customer payments, you may want to:</p>
+		<ul>
+			<li>carry out credit checks on new and existing customers</li>
+			<li>check bank references, trade references and online credit-ratings, from a credit-reference agency</li>
+			<li>ensure that your customer is aware of your credit terms (eg they must pay within 30 days) and that the payment terms for your debtors are longer than the terms you offer your customers</li>
+			<li>motivate customers to make early payments by offering discounts</li>
+			<li>investigate legally enforceable ways of encouraging prompt payment</li>
+		</ul>
+		<p>See our guide on <a href="/finance-and-grants/debt-recovery/ensuring-customers-pay-you-on-time.php">managing late payment</a>.</p>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them.php">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-or-inadequate-market-research.php">Poor or inadequate market research</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/weak-financial-planning.php">Weak financial planning</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/setting-sights-too-high.php">Setting sights too high</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/taking-your-eye-off-the-competition.php">Taking your eye off the competition</a></li>
+			<li>Poor supplier and customer controls</li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-stock-and-asset-management.php">Poor stock and asset management</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/hiring-the-wrong-people.php">Hiring the wrong people</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/here-is-how-I-started-a-successful-business_flash-video.php">Here's how I started a successful business (Flash video)</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/grow-your-business/growth-through-strategic-sourcing/choosing-the-right-suppliers.php">Choosing the right suppliers</a></li>
+			<li><a href="/grow-your-business/growth-through-strategic-sourcing/negotiate-the-right-deal-with-suppliers.php">Negotiate the right deal with suppliers</a></li>
+			<li><a href="/finance-and-grants/debt-recovery/ensuring-customers-pay-you-on-time.php">Ensuring customers pay you on time</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
