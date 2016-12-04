27cfef5369e3dac73b6e11cf8c8fcb1abc76e145
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 18:49:22 +0000

    Completed ‘How much money will your business make?’ page.

---
+++ starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/how-much-money-will-your-business-make.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>How much money will your business make? | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		
+		<h1>How do I survive until my business is off the ground?</h1>
+		<h2 >How much money will your business make?</h2>
+		<p>You need to identify how much money your business is likely to bring in over the coming year and then how much profit you hope to make.</p>
+		<p>You can do this by:</p>
+		<ul>
+			<li>estimating your total income from sales</li>
+			<li>estimating your expenses</li>
+			<li>working out a figure for salaries and dividends, including tax</li>
+			<li>working out the difference between your financial requirements and the amount you are prepared to take out of the business</li>
+		</ul>
+		<p>This will leave you with the amount you potentially need to find from other sources. For advice on what you will need to take into account and plan for, see our guide on how to <a href="/starting-up/business-planning/prepare-a-business-plan.php">prepare a business plan</a>.</p>
+		<h3>Profit and cashflow</h3>
+		<p>It may not be easy to calculate exactly how much your business will make in its first year, so it is important to concentrate on managing cashflow rather than profit.</p>
+		<p>Profit is the difference between the total amount your business earns and the costs it must pay out over the trading period - usually a year. Even a nominally profitable business can find itself in trouble if it runs out of cash.</p>
+		<p>Cashflow is the balance of all the money flowing into, and out of, your business. It covers actual payments of money, as opposed to what is owed by your debtors or to your creditors. Cash pays the bills and allows trading to continue. The need for cash is even greater if your business is growing and extending credit to more customers.</p>
+		<p>The main outflows of cash are:</p>
+		<ul>
+			<li>wages and salaries</li>
+			<li>overheads such as rent and rates</li>
+			<li>capital spending on plant and equipment</li>
+			<li>working capital such as stock and raw materials</li>
+		</ul>
+		<p>If you sell on credit, your cash inflow is delayed until you are actually paid so effective credit control is important. A business that buys on credit and is paid in cash, such as a retailer, is at a great advantage in cashflow terms. Businesses which make sales over the internet can also be cash positive.</p>
+		<p>Many businesses rely on bank overdrafts and quickly reach their borrowing limits. It is therefore important to think carefully about your cashflow and reduce the need to rely on an overdraft. See our guide on <a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php" title="How to manage your cashflow and avoid cash problems in your business">cashflow management: the basics</a>.</p>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/forecast-your-personal-financial-needs.php" title="Forecast your personal financial needs">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/make-savings.php" title="Make savings">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground.php" title="How do I survive until my business is off the ground?">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/forecast-your-personal-financial-needs.php">Forecast your personal financial needs</a></li>
+			<li>How much money will your business make?</li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/make-savings.php">Make savings</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/other-sources-of-income.php">Other sources of income</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/prepare-a-financial-plan.php">Prepare a financial plan</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/find-support.php">Find support</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="../../../_files/cash-flow-projection-worksheet.xls" target="_blank" rel="noopener noreferrer">Download our sample cashflow projection spreadsheet (XLS, 79K)</a></li>
+			<li><a href="../../../_files/cima_improving-cash-flow-using-credit-management_the-outline-case.pdf" target="_blank" rel="noopener noreferrer">Download a practical guide to cashflow management from the Chartered Institute of Management Accountants (PDF, 366K)</a></li>
+			<li><a href="http://www.startupdonut.co.uk/startup/financing-a-business/cash-flow" target="_blank" rel="noopener noreferrer">Cashflow guidance on the Start up Donut website</a></li>
+			<li><a href="http://www.learndirect.com/store/understanding-accounts" target="_blank" rel="noopener noreferrer">Understanding Accounts course on the learndirect business website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-finance-explained" title="Identify your start-up costs and manage your money. Find out how to forecast your cashflow and record your profit and loss" target="_blank" rel="noopener noreferrer">Understand how much money you need to start up</a></li>
+			<li><a href="https://www.gov.uk/business-finance-explained" title="Learn about the various sources of finance available to start-ups and which could be most appropriate for your business" target="_blank" rel="noopener noreferrer">Explore your finance options when starting up</a></li>
+			<li><a href="https://www.gov.uk/avoid-business-cashflow-problems" title="Understand whether you can meet your financial commitments and how to cope when things don't go to plan" target="_blank" rel="noopener noreferrer">Meet your financial commitments</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">Cashflow management: the basics</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting.php">Business budgeting</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" /> 
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
