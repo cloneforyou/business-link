e4e8cbbc227ed87146b56f250829638c1929825f
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 16:33:31 +0000

    Completed ‘Hiring the wrong people’ page.

---
+++ starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/hiring-the-wrong-people.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hiring the wrong people | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Common mistakes when starting up - and how to avoid them</h1>
+		<h2>Hiring the wrong people</h2>
+		<p>A large part of your new business' success will be determined by the <strong>quality of the people you recruit</strong>. Taking on people will always mean some form of investment for your business and requires careful consideration. Taking this investment seriously can make it more valuable and improve your chances of success.</p>
+		<p>Ensuring that you hire high-calibre people with the right mix of skills is not an easy process but one that will pay dividends.</p>
+		<p>How you go about employing new people will depend on your business needs, eg whether the work is constant, how long it will last and the number of hours available.</p>
+		<p>You need to explore all the options available to you. These include:</p>
+		<ul>
+			<li>recruiting permanent staff on a full or part-time basis</li>
+			<li>fixed-term contract employee</li>
+			<li>temporary staff</li>
+			<li>freelancers</li>
+			<li>consultants</li>
+			<li>contractors</li>
+		</ul>
+		<p>Employing relatives and friends may appear an easy solution to staffing issues, but they may not have the right mix of skills that you need. It can also be more difficult to bring a period of employment to a close when a personal relationship exists.</p>
+		<p>For more information, see our guide on <a href="/employment-and-skills/take-on-staff/taking-on-staff-the-options.php">taking on staff - the options</a>.</p>
+		<h3>Failing to delegate</h3>
+		<p>Being your own boss may be a key motivator to setting up your own business. However, delegating the right task to the right person is important for both you and your business. Failing to delegate could mean you take on too much and increase your stress levels.</p>
+		<p>A good way to tackle delegation is to identify a few key tasks of your own that are very valuable to the business and hand over the rest to your team. For more information, see our guide on <a href="/employment-and-skills/skills-and-development/skills-and-training-for-directors-and-owners.php">skills and training for directors and owners</a>.</p>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-stock-and-asset-management.php" title="Poor stock and asset management">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/here-is-how-I-started-a-successful-business_flash-video.php" title="Here's how I started a successful business (Flash video)">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them.php">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-or-inadequate-market-research.php">Poor or inadequate market research</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/weak-financial-planning.php">Weak financial planning</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/setting-sights-too-high.php">Setting sights too high</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/taking-your-eye-off-the-competition.php">Taking your eye off the competition</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-supplier-and-customer-controls.php">Poor supplier and customer controls</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-stock-and-asset-management.php">Poor stock and asset management</a></li>
+			<li>Hiring the wrong people</li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/here-is-how-I-started-a-successful-business_flash-video.php">Here's how I started a successful business (Flash video)</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="../../../_files/acas_recruiting-staff.pdf" target="_blank" rel="noopener noreferrer">Download recruitment and induction guidance (PDF, 1.04MB)</a></li>
+			<li><a href="/employment-and-skills/take-on-staff/help-with-recruitment.php">Use our interactive tools to help you take on a new employee</a></li>
+		</ul>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/employing-staff" title="Find information on how to take on staff in your business" target="_blank" rel="noopener noreferrer">Take on staff</a></li>
+			<li><a href="/employment-and-skills/take-on-staff/taking-on-staff-the-options.php">Taking on staff - the options</a></li>
+			<li><a href="/employment-and-skills/skills-and-development/skills-and-training-for-directors-and-owners.php">Skills and training for directors and owners</a></li>
+			<li><a href="/employment-and-skills/advertise-a-job/advertising-a-job-and-interviewing-candidates.php">Advertising a job and interviewing candidates</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
