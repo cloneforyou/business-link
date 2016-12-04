b18676ae3384eb7367f9550f6d079c2daa976412
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 18:17:49 +0000

    Completed ‘Here's how I decided I was ready to start up my business’ page.

---
+++ starting-up/considering-starting-up/starting-a-part-time-business/here-is-how-I-decided-I-was-ready-to-start-up-my-business.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Here's how I decided I was ready to start up my business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Here's how I decided I was ready to start up my business</h1>
+		<h2>Here's how I decided I was ready to start up my business</h2>
+		<p>Paul Kruzycki discovered an entrepreneurial streak while running a not-for-profit charity convention and he decided to set up his own venture. When he did, a cautious approach to the launch of his real-ale mail-order company, Ales By Mail, proved important in giving the business a sound start.</p>
+		<p><strong>What I did</strong></p>
+		<h3>Research the business proposition fully</h3>
+		<p>"I continued working full time as a building surveyor while I developed my business plan, built contacts and got my idea to a stage where I felt I could get it off the ground. I wanted to prove fully to myself that the business could work.</p>
+		<p>"I also knew that if I wanted to get finance to help me start up I would need to answer some tough questions such as who are your customers, where will you advertise and so on.</p>
+		<p>"I spent a lot of time talking to people in the industry and finding information on websites - seeing how other people were doing things, at what level and how differently."</p>
+		<h3>Take the opportunity to work part-time to provide a financial buffer</h3>
+		<p>"My research showed that Ales by Mail couldn't pay me a salary at first so I decided I needed to save at least six months' living expenses. Then an opportunity to work as a property consultant came up.</p>
+		<p>"I now do that two days a week and spend two days on Ales by Mail. On the fifth and sixth days I work on whatever needs doing most urgently.</p>
+		<p>"I'm building the business on a tight budget and am going for slow growth. My consultancy work allows me to take this approach."</p>
+		<h3>Consider domestic circumstances</h3>
+		<p>"I spent 18 months preparing to start the business. There were things that needed to be resolved before I gave up my job. For example, I needed to wait until my house had been refurbished so that I could work from home.</p>
+		<p>"I've had a lot of support from my wife. Knowing I can go to my family after a bad day and receive that support helps me remember that the decision I made was the right one."</p>
+		<p><strong>What I'd do differently</strong></p>
+		<h3>Think about the impact of routine paperwork</h3>
+		<p>"Before starting up I'd find out more about the organisational aspects of running a business. I underestimated the amount of time needed to deal with tax matters, chase up clients and do all the other bits of paperwork."</p>
+		<p><a href="/find-a-case-study.php">Read more case studies that describe first hand how people tackle real-life challenges and opportunities</a>.</p>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/help-and-support-for-part-time-businesses.php" title="Help and support for part-time businesses">Prev</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business.php" title="Starting a part-time business">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/what-to-consider-when-starting-your-part-time-business.php">What to consider when starting your part-time business</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/tax-and-legal-implications-of-a-part-time-business.php">Tax and legal implications of a part-time business</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/advantages-and-disadvantages-of-starting-a-part-time-business.php">Advantages and disadvantages of starting a part-time business</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/help-and-support-for-part-time-businesses.php">Help and support for part-time businesses</a></li>
+			<li>Here's how I decided I was ready to start up my business</li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<div class="media-object stack-for-small">
+			<div class="media-object-section">
+				<div class="thumbnail">
+					<img src="../../../_img/PaulKruzycki.jpg" width="181" height="121" border="0" alt="Paul Kruzycki, Ales By Mail Ltd" />
+				</div>
+			</div>
+			<div class="media-object-section">
+				<h3>
+					Paul Kruzycki<br />
+					<a href="http://www.alesbymail.co.uk/" target="_blank" rel="noopener noreferrer">Ales by Mail Ltd</a><br />
+					Paul's top tips:
+				</h3>
+				<ul>
+					<li>"Really understand what you are giving up by leaving a job such as paid holiday, company car and so on."</li>
+					<li>"Find information wherever you can, especially on the internet."</li>
+					<li>"Be organised and methodical in your paperwork."</li>
+				</ul>
+			</div>
+		</div>
+
+		<h4>More case studies</h4>
+		<ul>
+		<li><a href="/it-and-ecommerce/introduction-to-it/here-is-how-my-first-it-system-helped-me-set-up-my-business.php">Here's how my first IT system helped me set up my business</a></li>
+		<li><a href="/starting-up/name-your-business/here-is-how-I-chose-the-name-for-my-business.html">Here's how I chose the name for my business</a></li>
+		<li><a href="/tax-payroll-and-company-information/tax-reliefs-and-allowances/tax-advantages-for-those-starting-up-in-business/here-is-how-the-tax-authorities-helped-me-start-my-business.html">Here's how the tax authorities helped me start my business</a></li>
+		</ul>
+		
+		<p><a href="/find-a-case-study.php"><b>Find a case study</b></a></p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
