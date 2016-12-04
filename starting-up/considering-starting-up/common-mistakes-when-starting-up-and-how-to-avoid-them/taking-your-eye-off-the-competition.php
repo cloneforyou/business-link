ccd19d5164ea884a1e30dbd8420690b593ebe9e7
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 15:57:59 +0000

    Completed ‘Taking your eye off the competition’ page.

---
+++ starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/taking-your-eye-off-the-competition.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Taking your eye off the competition | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		
+		<h1>Common mistakes when starting up - and how to avoid them</h1>
+		<h2>Taking your eye off the competition</h2>
+		<p>During the busy start-up phase it can be easy to forget to set aside enough time to monitor the competition. However, it's essential that you are ready to respond to competitors in your market place and to new developments.</p>
+		<h3>Failing to actively monitor the competition</h3>
+		<p>Failing to monitor your rivals will stop you from seeing what competition or threats to your business exist in your market place.</p>
+		<p>Competition is not just another business that might take money away from you. It can be another product or service that's being developed which you ought to be selling or looking to license before somebody else takes it up.</p>
+		<p>You can get clues to the existence of competitors from:</p>
+		<ul>
+			<li>advertising</li>
+			<li>press reports</li>
+			<li>exhibitions and trade fairs</li>
+			<li>questionnaires</li>
+			<li>searching on the web for similar products or services</li>
+			<li>approaches reported by your customers</li>
+			<li>flyers and marketing literature that have been sent to you - this is quite common if you're on a bought-in marketing list</li>
+			<li>planning applications and building work in progress</li>
+		</ul>
+		<p>For more information, see our guide on how to <a href="/sales-and-marketing/marketing/understand-your-competitors.php">understand your competitors</a>.</p>
+		<h3>Failing to act on competitors' information</h3>
+		<p>Failing to use information gathered about your competitors will weaken your position in the market.</p>
+		<p>Feed any useful information into your marketing plan. Your marketing plan and research will help you to set realistic targets and deadlines, and allocate appropriate resources. You can then decide to focus on building relationships with your existing clients or attract new customers. Your marketing can then be turned into sales by deciding on your sales methods.</p>
+		<p>For more information, see our guide on how to <a href="/sales-and-marketing/marketing/write-a-marketing-plan.php">write a marketing plan</a>.</p>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them.php">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-or-inadequate-market-research.php">Poor or inadequate market research</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/weak-financial-planning.php">Weak financial planning</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/setting-sights-too-high.php">Setting sights too high</a></li>
+			<li>Taking your eye off the competition</li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-supplier-and-customer-controls.php">Poor supplier and customer controls</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-stock-and-asset-management.php">Poor stock and asset management</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/hiring-the-wrong-people.php">Hiring the wrong people</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/here-is-how-I-started-a-successful-business_flash-video.php">Here's how I started a successful business (Flash video)</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="../../../_files/how-to-get-to-grips-with-your-competitors.pdf" target="_blank" rel="noopener noreferrer">Download competitor information gathering advice from the Chartered Institute of Marketing website (PDF, 159K)</a></li>
+		</ul>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-selling-customers" target="_blank" rel="noopener noreferrer">Understand your market</a></li>
+			<li><a href="/sales-and-marketing/marketing/understand-your-competitors.php">Understand your competitors</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
