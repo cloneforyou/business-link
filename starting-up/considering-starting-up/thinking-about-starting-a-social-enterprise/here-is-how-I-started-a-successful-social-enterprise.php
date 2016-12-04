66e3ab72b834b2062140c7edbc392827039073ae
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 23:54:09 +0000

    Completed ‘Here's how I started a successful social enterprise’ page.

---
+++ starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-I-started-a-successful-social-enterprise.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Here's how I started a successful social enterprise | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Here's how I started a successful social enterprise</h1>
+		<h2>Here's how I started a successful social enterprise</h2>
+		<p>Psychologist Ian Glasscock set up Community Interest Company (CIC) Games for Life in 2009, after learning about new technology which allows people with attention deficit hyperactivity disorder (ADHD) to be treated using a thought-operated computer system. The Herts-based social enterprise has since successfully used the pioneering system to help children suffering from the behavioural disorder to learn to control their symptoms.</p>
+		<h3>What I did</h3>
+		<h4>Research the idea</h4>
+		<p>"I've had problems with attention difficulties myself which was part of my motivation to set up the business. I completed a master's degree in the cognitive neuroscience of attention and then spent some time in the US, where I found out about NASA technology which monitors levels of concentration in the brain using sensors linked into a computer game.</p>
+		<p>"That was really when I decided that there was a business idea here - that we could retrain our brains with these mind-controlled computer games. The way the system works is that when someone is in a focused state of attention their brainwaves feedback wirelessly to interact with the computer and the games. So it teaches people to concentrate, because they have to in order to play the game. I established the business with my wife in 2009 after setting up an exclusive agreement with a US manufacturer to use the technology in the UK market.</p>
+		<p>"We approached the local university and launched a pilot study project with some Herts County Council schools. At the end of the pilot the children involved had a reduction in their core ADHD symptoms. Instead of taking medication or going through a therapy process, they teach themselves how to pay attention."</p>
+		<h4>Choose the right form of social enterprise</h4>
+		<p>"Before setting up, I did some research and came across the social enterprise. There seemed to be a couple of different formats, including the CIC. I took the view that I didn't want to just be in it for the money. But don't get me wrong, I do need to earn enough to support my basic needs.</p>
+		<p>"Initially we set up the business with just a laptop at home, but we've now moved to a bigger office at the business technology centre in Stevenage. I've done all of this on a shoestring. We are now working with local authorities and a leading children's charity, as well as providing systems to schools and parents for home use."</p>
+		<h4>Make the most of support</h4>
+		<p>"There is support and advice available for social enterprises, so make the most of it. I received a lot of help from an expert adviser on social enterprises at Business Link. He has helped us keep our head above the water in the tough economic climate.</p>
+		<p>"We've received two Business Link social enterprise grants, each worth &pound;1,500. The money helped us fund our website marketing - an area we knew nothing about. We've also just taken on an apprentice for 18 months using funding from Herts County Council. We took her on so it would free me up to develop the business. The future is bright for us and we're now looking at other health and education related product innovation."</p>
+		<h3>What I'd do differently</h3>
+		<p>"In hindsight, I should have had more of a business focus from the start, rather than just wanting to do something meaningful. A social enterprise needs to stand on its own two feet just like any other business.</p>
+		<p>"I should also have applied for more funding from the start. Even if you don't get it, you can get useful feedback and advice."</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-being-a-social-enterprise-works-for-our-business.php" title="Here's how being a social enterprise works for our business">Prev</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise.php" title="Thinking about starting a social enterprise?">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/what-is-a-social-enterprise.php">What is a social enterprise?</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/why-set-up-a-social-enterprise.php">Why set up a social enterprise?</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/benefits-of-a-social-enterprise.php">Benefits of a social enterprise</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/social-enterprise-skills.php">Social enterprise skills</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/challenges-facing-social-enterprises.php">Challenges facing social enterprises</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/business-and-marketing-plans.php">Business and marketing plans</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/financing-your-social-enterprise.php">Financing your social enterprise</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/social-enterprises-and-the-public-sector.php">Social enterprises and the public sector</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/checklist-starting-up-a-social-enterprise.php">Checklist: starting up a social enterprise</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-being-a-social-enterprise-works-for-our-business.php">Here's how being a social enterprise works for our business</a></li>
+			<li>Here's how I started a successful social enterprise</li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<div class="media-object stack-for-small">
+			<div class="media-object-section">
+				<div class="thumbnail">
+					<img src="../../../_img/PlayAttention_cs.jpg" width="181" height="121" border="0" alt="Ian Glasscock, Games for Life" />
+				</div>
+			</div>
+			<div class="media-object-section">
+				<h3>
+					Ian Glasscock<br />
+					<a href="http://www.gamesforlife.co.uk/" target="_blank" rel="noopener noreferrer">Games for Life</a><br />
+					Ian's top tips:
+				</h3>
+				<ul>
+					<li>"If you set out and just want to make money don't form a social enterprise, set up a limited company. But by setting up a social enterprise you can still earn a good living and do something meaningful."</li>
+					<li>"Establish if there is going to be enough cash to keep your business going. If there is and you believe in your idea, then it's worth jumping in."</li>
+					<li>"Don't be reliant on getting financial handouts. You need to make the business work and if you get any financial support that's a bonus."</li>
+				</ul>
+			</div>
+		</div>
+
+		<p><strong><a href="/find-a-case-study.php">Find a case study</a></strong></p>
+
+		<img src="../../../_img/px_startingabusiness.gif" width="181" height="5" alt="" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
