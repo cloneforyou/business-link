6475c4e07790ec5439df1382592493c2cf8fbe5b
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 21:45:58 +0000

    Completed ‘Draw up a family-business constitution’ page.

---
+++ starting-up/considering-starting-up/family-run-businesses/draw-up-a-family-business-constitution.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Draw up a family-business constitution | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Family-run businesses</h1>
+		<h2>Draw up a family-business constitution</h2>
+		<p>One way to successfully manage conflict in a family business is to have a family-business constitution. When well drawn-up, such a document can even <strong>prevent conflict</strong> occurring in the first place.</p>
+		<p>A family-business constitution - sometimes known as a family charter - is partly a statement of general principles. It outlines your business' core values and vision, and your family's commitment to them.</p>
+		<p>Importantly, it is also a <strong>practical guide</strong> for running the business and a framework you can use to deal with family business issues that have the potential to cause disputes.</p>
+		<p>The process of drawing up a family constitution should be <strong>collaborative</strong>, involving everybody with a stake in the business. The document should be regularly reviewed.</p>
+		<h3>Structure your constitution</h3>
+		<p>A typical family-business constitution might include the following sections:</p>
+		<ul>
+			<li>business goals, vision and values</li>
+			<li>leadership</li>
+			<li>management structure</li>
+			<li>entry principles for family members</li>
+			<li>succession and exit policies</li>
+			<li>rights, responsibilities and obligations of family appointments</li>
+			<li>rights, responsibilities and obligations of family members not working in the business</li>
+			<li>appointment and rights of non-family board members, management and employees</li>
+			<li>training, remuneration and appraisal of employees - both family and non-family</li>
+			<li>developing the next generation</li>
+			<li>involvement of non-executive directors and other outsiders</li>
+			<li>communication channels</li>
+			<li>dispute-resolution procedures</li>
+		</ul>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/pay-and-benefits-for-family-members.php" title="Pay and benefits for family members">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/succession-planning-in-family-businesses.php" title="Succession planning in family businesses">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses.php" title="Family-run businesses">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/advantages-of-family-businesses.php">Advantages of family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/key-things-to-consider-when-setting-up-a-family-business.php">Key things to consider when setting up a family business</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/communication-and-family-businesses.php">Communication and family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/managing-conflict-in-family-businesses.php">Managing conflict in family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/pay-and-benefits-for-family-members.php">Pay and benefits for family members</a></li>
+			<li>Draw up a family-business constitution</li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/succession-planning-in-family-businesses.php">Succession planning in family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/here-is-how-having-a-family-member-in-the-business-made-a-difference.php">Here's how having a family member in the business made a difference</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.ifb.org.uk/" target="_blank" rel="noopener noreferrer">Business development advice on the Institute for Family Business website</a></li>
+			<li><a href="http://www.rileycom.co.uk/cms/" target="_blank" rel="noopener noreferrer">Family constitution development advice on the Riley Chartered Accountants website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php">Prepare a business plan</a></li>
+			<li><a href="/grow-your-business/assessing-current-performance/measure-performance-and-set-targets.php">Measure performance and set targets</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php">Starting a business from home </a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
