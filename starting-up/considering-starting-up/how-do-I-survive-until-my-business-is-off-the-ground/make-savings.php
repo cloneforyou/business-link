d2e3a7e5fb9d5dd1fa6c306ec32b517e3fee2399
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 18:54:38 +0000

    Completed ‘Make savings’ page.

---
+++ starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/make-savings.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Make savings | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>How do I survive until my business is off the ground?</h1>
+		<h2>Make savings</h2>
+		<p>There are ways you can save money on goods and services, both at home and in your business.</p>
+		<h3>Making personal savings</h3>
+		<p>You may be able to reduce the amount of money you pay out each month, simply by shopping around for services and loans. If you already have personal debts, for example on credit cards or personal loans, seek advice from an independent adviser or your bank as it may be possible to reduce your outgoings this way as well.</p>
+		<p>There are many ways you can save money on essential goods and services. Many <strong>utility companies</strong> offer attractive deals when you change to a new supplier. Look carefully at exactly what is being offered - you may find, for example, that you can make savings if you receive both your gas and electricity from a single supplier.</p>
+		<p>You can also try to reduce your everyday expenses. For example, you could sell your car and buy one that is cheaper to run or use public transport. You could also choose not to update personal equipment such as your laptop or mobile phone until it is absolutely necessary.</p>
+		<h3>Making savings in your business</h3>
+		<p>You could implement simple cost control systems across your whole business to identify scope for savings. You could cut unnecessary or excessive costs, for example, by not heating your premises at night or finding low price suppliers for goods or services. Adopting 'green' practices such as switching off computers when not in use can also save money as well as reducing your carbon footprint. Consider leasing goods or buying them second hand. Consider whether you can save money by running your business from home. See our guide on how to <a href="/premises-and-property/choosing-the-right-premises/use-your-home-as-a-workplace.php" title="Tax implications, financial and legal issues, security and setting up your work area if you plan to work from home">use your home as a workplace</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/how-much-money-will-your-business-make.php" title="How much money will your business make?">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/other-sources-of-income.php" title="Other sources of income">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground.php" title="How do I survive until my business is off the ground?">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/forecast-your-personal-financial-needs.php">Forecast your personal financial needs</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/how-much-money-will-your-business-make.php">How much money will your business make?</a></li>
+			<li>Make savings</li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/other-sources-of-income.php">Other sources of income</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/prepare-a-financial-plan.php">Prepare a financial plan</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/find-support.php">Find support</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.startupdonut.co.uk/startup/financing-a-business/start-up-costs" target="_blank" rel="noopener noreferrer">Start up finance resources on the Start up Donut website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-finance-explained" title="Identify your start-up costs and manage your money. Find out how to forecast your cashflow and record your profit and loss" target="_blank" rel="noopener noreferrer">Understand how much money you need to start up</a></li>
+			<li><a href="https://www.gov.uk/avoid-business-cashflow-problems" title="Understand whether you can meet your financial commitments and how to cope when things don't go to plan" target="_blank" rel="noopener noreferrer">Meet your financial commitments</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting.php">Business budgeting</a></li>
+			<li><a href="/tax-payroll-and-company-information/tax-reliefs-and-allowances/tax-advantages-for-those-starting-up-in-business.php" title="The tax allowances and reliefs available to new businesses">Tax advantages for those starting up in business</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5"  />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
