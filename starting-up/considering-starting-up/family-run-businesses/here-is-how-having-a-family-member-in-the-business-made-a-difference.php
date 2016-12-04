5d366cd9baacef9a00987a80efdaf79ca5778b0b
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 22:06:41 +0000

    Completed ‘Here's how having a family member in the business made a difference’ page.

---
+++ starting-up/considering-starting-up/family-run-businesses/here-is-how-having-a-family-member-in-the-business-made-a-difference.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Here's how having a family member in the business made a difference | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Here's how having a family member in the business made a difference</h1>
+		<h2>Here's how having a family member in the business made a difference</h2>
+		<p>Peter Henman was 15 when he joined his brother as the fourth generation in his family's Bedfordshire-based construction and funeral businesses. He worked his way up from teaboy to chairman and now both he and the businesses are reaping the benefits of the fifth generation to join the companies. His daughter Vicky Trumper is a director and company secretary of both Neville Construction Group and Neville Funeral Service.</p>
+		<p><strong>What we did</strong></p>
+		<h3>Brought in a new era of confidence and long-term planning</h3>
+		<p>"Vicky has brought in a new period of confidence for the future. In most companies you can do three- or five-year plans but when I came into the business we talked about ten or even 15 years ahead. Now we can go back to doing that.</p>
+		<p>"For example, on the funeral side of the business we've invested further and advanced it greatly since Vicky came in. With Vicky's arrival, shareholders could see we had the ability to make longer-term investment plans that she would bring to fruition and were happy the investment was made. Without the confidence in the future that Vicky provides, the decisions may not have been so easy to make."</p>
+		<h3>Brought in experience from outside the business</h3>
+		<p>"Vicky's entrance into the business has brought a lot of people-focused skills she gained from her previous positions in the National Health Service. The health service is a people business like the construction and funeral businesses and her skills gained in another environment have helped us in sharpening our people policies.</p>
+		<p>"Now the ownership and leadership of the company is spread between the older and experienced family members and the enthusiastic younger generation, giving balance and stability and a mixture of ideas gained from different environments. It also gives confidence to the staff for the long-term future of the business."</p>
+		<h3>Allowed me to take a sideways step</h3>
+		<p>"It's almost inconceivable that a chairman of a family firm could ever walk out and shut the door on the business completely, but personally I am in favour of moving sideways. I've got the confidence to do that with Vicky there.</p>
+		<p>"The first generation of our company was still signing cheques at 90, the next still coming into work when he was 80 and the third died when still working at 75. Vicky's arrival means that I may still be able to contribute for as long as I like, but with the reassurance that the core of the business is being well run by the next generation."</p>
+		<p><strong>What could have happened without Vicky</strong></p>
+		<h3>Fallen behind with technological developments</h3>
+		<p>"Vicky did a business administration course and she did her project work on IT related to the business. That was particularly useful because she's from a generation at home with IT. Because of my age we were behind with this technology before she joined but now it's spread throughout the business. She'll perhaps bring the business closer to the world of the paperless office and instant communication than I ever could have."</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/succession-planning-in-family-businesses.php" title="Succession planning in family businesses">Prev</a></li>
+		
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses.php" title="Family-run businesses">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/advantages-of-family-businesses.php">Advantages of family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/key-things-to-consider-when-setting-up-a-family-business.php">Key things to consider when setting up a family business</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/communication-and-family-businesses.php">Communication and family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/managing-conflict-in-family-businesses.php">Managing conflict in family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/pay-and-benefits-for-family-members.php">Pay and benefits for family members</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/draw-up-a-family-business-constitution.php">Draw up a family-business constitution</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/succession-planning-in-family-businesses.php">Succession planning in family businesses</a></li>
+			<li>Here's how having a family member in the business made a difference</li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<div class="media-object stack-for-small">
+			<div class="media-object-section">
+				<div class="thumbnail">
+					<img src="../../../_img/CS0039.jpg" width="181" height="121" border="0" alt="Peter Henman/Vicki Trumper, Neville Funeral Services" />
+				</div>
+			</div>
+			<div class="media-object-section">
+				<h3>
+					Peter Henman and Vicki Trumper<br />
+					<a href="http://www.neville-funeral-service.co.uk/" target="_blank" rel="noopener noreferrer">Neville Funeral Services</a><br />
+					Peter's top tips:
+				</h3>
+				<ul>
+					<li>"Use the arrival of a family member into the business as an opportunity to make longer-term plans."</li>
+					<li>"Make best use of any skills that your family member has gained in different environments."</li>
+					<li>"Show confidence in the enthusiasm of the younger generation so they can make a difference to the business."</li>
+				</ul>
+			</div>
+		</div>
+
+		<h4>More case studies</h4>
+		<ul>
+			<li><a href="/buy-or-sell-a-business/transferring-your-business/selling-or-passing-a-business-to-a-family-member/here-is-how-i-am-transferring-my-business-to-my-son.php">Here's how I'm transferring my business to my son</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/here-is-how-having-an-up-to-date-business-plan-helped-my-business.php">Here's how having an up-to-date business plan helped my business</a></li>
+			<li><a href="/employment-and-skills/different-types-of-worker/employing-family-members-and-voluntary-staff/here-is-how-our-business-overcame-the-pitfalls-of-employing-family-members.php">Here's how our business overcame the pitfalls of employing family members</a></li>
+		</ul>
+		
+		<p><strong><a href="/find-a-case-study.php">Find a case study</a></strong></p>
+
+		<img src="../../../_img/px_startingabusiness.gif" width="181" height="5" alt="" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
