93de55b7636611ea1ebd183ca1efda3a0d2626ca
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 23:42:31 +0000

    Completed ‘Social enterprises and the public sector’ page.

---
+++ starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/social-enterprises-and-the-public-sector.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Social enterprises and the public sector | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		
+		<h1>Thinking about starting a social enterprise?</h1>
+		<h2>Social enterprises and the public sector</h2>
+		<p>As well as opportunities in the commercial world, social enterprises are increasingly seeking to diversify income streams, with some deciding to operate solely or partly within the public sector. Working with the government to deliver public services can provide a consistency of demand for your services and be extremely rewarding. However, it is sensible to ensure that your entire income stream is not reliant on a single source.</p>
+		<p>Many social enterprises are attracted into public service delivery because they see a need to inject innovative approaches and fresh thinking into traditional ways of conducting business in this area.</p>
+		<p>There are a wide variety of opportunities in the delivery of public services, where social enterprises work in partnership with government, including:</p>
+		<ul>
+			<li>the health and social care sector</li>
+			<li>fostering and adoption</li>
+			<li>transport services</li>
+			<li>refuse collection/recycling</li>
+			<li>sports and leisure services</li>
+		</ul>
+		<h3>Barriers facing social enterprises competing for public service contracts</h3>
+		<p>There are certain issues you may face when trying to compete with commercial operators for public service contracts. These can include financial and structural barriers and a general lack of understanding of the social enterprise business model among financiers and commissioners.</p>
+		<h3>Public sector opportunities for social enterprises</h3>
+		<p>A good example of how social enterprises are successfully contributing to the delivery of public services is in health and social care. <a href="http://webarchive.nationalarchives.gov.uk/20130107105354/http://www.dh.gov.uk/en/Publicationsandstatistics/Publications/PublicationsPolicyAndGuidance/DH_072928" title="Provides a gateway to setting up and expanding your operation within health and social care services provision" target="_blank" rel="noopener noreferrer">Read a resource pack for social enterprise providers on the Department of Health (DoH) website</a>.</p>
+		<p>Local authorities may also offer opportunities, for example, in the provision of domestic help for elderly and disabled people.</p>
+		<p>The provision of immigration advice or services may constitute additional opportunities for social enterprises and other business entities. However, this area is regulated and prospective advisers need to demonstrate that they are fit and competent to take on such roles. You can <a href="https://www.gov.uk/government/publications/how-to-become-a-regulated-immigration-adviser" target="_blank" rel="noopener noreferrer">find out more about the regulatory scheme and how to become an immigration adviser on the Office of the Immigration Services Commissioner website</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/financing-your-social-enterprise.php" title="Financing your social enterprise">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/checklist-starting-up-a-social-enterprise.php" title="Checklist: starting up a social enterprise">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise.php" title="Thinking about starting a social enterprise?">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/what-is-a-social-enterprise.php">What is a social enterprise?</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/why-set-up-a-social-enterprise.php">Why set up a social enterprise?</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/benefits-of-a-social-enterprise.php">Benefits of a social enterprise</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/social-enterprise-skills.php">Social enterprise skills</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/challenges-facing-social-enterprises.php">Challenges facing social enterprises</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/business-and-marketing-plans.php">Business and marketing plans</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/financing-your-social-enterprise.php">Financing your social enterprise</a></li>
+			<li>Social enterprises and the public sector</li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/checklist-starting-up-a-social-enterprise.php">Checklist: starting up a social enterprise</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-being-a-social-enterprise-works-for-our-business.php">Here's how being a social enterprise works for our business</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-I-started-a-successful-social-enterprise.php">Here's how I started a successful social enterprise</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/78927/building-stronger-civil-society.pdf" target="_blank" rel="noopener noreferrer">Download 'Big Society' strategy information from the Cabinet Office website (PDF, 182K)</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20130107105354/http://www.dh.gov.uk/en/Publicationsandstatistics/Publications/PublicationsPolicyAndGuidance/DH_072928" title="Provides a gateway to setting up and expanding your operation within health and social care services provision" target="_blank" rel="noopener noreferrer">Social enterprise provider resource pack on the DoH website</a></li>
+			<li><a href="https://www.networks.nhs.uk/nhs-networks/social-enterprise-network/about-us" title="Provides a forum to establish contacts in health and care provision within the NHS" target="_blank" rel="noopener noreferrer">Social enterprise network information on the NHS Networks website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/selling/overview-on-selling-to-government.php">Overview on selling to government</a></li>
+			<li><a href="/sales-and-marketing/selling/tender-for-a-contract.php">Tender for a contract</a></li>
+			<li><a href="/your-business-sector/property-management/residential-property-standards-licences-and-useful-schemes/becoming-a-social-landlord.php">Becoming a social landlord</a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" title="Learn about the different types of business and legal structures to help you choose the right one for your new business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" title="Tutorials, videos and tools to help you decide how to become self-employed - from sole trader to limited company, franchisee to social enterprise and more" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure</a></li>
+			<li><a href="https://www.gov.uk/set-up-business" title="Case studies and tutorials to help you understand the legal dos and don'ts of naming your business" target="_blank" rel="noopener noreferrer">Name your business</a></li>
+			<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php">Set up a social enterprise</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
