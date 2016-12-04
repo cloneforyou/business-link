88f54fb8f40c0e96ef02dba36de4c2aa559eb5a8
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 19:58:12 +0000

    Completed ‘Prepare a financial plan’ page.

---
+++ starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/prepare-a-financial-plan.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prepare a financial plan | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>How do I survive until my business is off the ground?</h1>
+		<h2>Prepare a financial plan</h2>
+		<p>Once you have gathered together all your key financial information such as estimates, overheads and expenses you can produce a financial plan.</p>
+		<p>The first step is to draw up a <strong>budget</strong> - a plan for spending and saving your money.</p>
+		<p>You should:</p>
+		<ul>
+			<li>prepare budgets showing the level of sales and profits you expect to achieve, and the costs involved in doing so</li>
+			<li>estimate your total sales - see our guide on how to <a href="/sales-and-marketing/selling/forecast-and-plan-your-sales.php">forecast and plan your sales</a></li>
+			<li>prepare monthly or weekly cashflow forecasts (which should be regularly updated), looking ahead one year - overheads such as rent can be accurately predicted</li>
+			<li>make sure you will have enough money on the day to cover each payment</li>
+		</ul>
+		<p><a href="../../../_files/cashflow_profit-and-loss_sales-forecast.xls" target="_blank" rel="noopener noreferrer">Download a spreadsheet containing cashflow forecast, profit and loss forecast and sales forecast templates (XLS, 44K)</a>.</p>
+		<p>Once you have drawn up your budget, it is important to stick to it so you don't risk overspending or running out of money for essentials. The key to budgeting is maintaining simple but good records. You will need to keep track of where your money comes from and where it goes.</p>
+		<p>For more information on preparing a financial plan read our guide on <a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting.php">budgeting and business planning</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/other-sources-of-income.php" title="Other sources of income">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/find-support.php" title="Find support">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground.php" title="How do I survive until my business is off the ground?">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/forecast-your-personal-financial-needs.php">Forecast your personal financial needs</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/how-much-money-will-your-business-make.php">How much money will your business make?</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/make-savings.php">Make savings</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/other-sources-of-income.php">Other sources of income</a></li>
+			<li>Prepare a financial plan</li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/find-support.php">Find support</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="../../../_files/cashflow_profit-and-loss_sales-forecast.xls" target="_blank" rel="noopener noreferrer">Download a spreadsheet containing cashflow forecast, profit and loss forecast and sales forecast templates (XLS, 44K) </a></li>
+			<li><a href="http://www.learndirect.com/store/business-workplace/finance-accounts/understanding-accounts" target="_blank" rel="noopener noreferrer">Understanding Accounts course on the learndirect business website </a></li>
+			<li><a href="http://www.startupdonut.co.uk/startup/financing-a-business/cash-flow/budgeting" target="_blank" rel="noopener noreferrer">Budgeting guidance on the Start up Donut website </a></li>
+		</ul>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-finance-support-finder" title="How to forecast your finances and raise finance when starting up" target="_blank" rel="noopener noreferrer">Think about your financial planning</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">Cashflow management: the basics</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting.php">Business budgeting</a></li>
+			<li><a href="/sales-and-marketing/selling/forecast-and-plan-your-sales.php">Forecast and plan your sales</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5"  />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
