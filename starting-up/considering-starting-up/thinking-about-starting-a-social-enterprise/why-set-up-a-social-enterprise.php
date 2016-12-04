acc98a7ec76b7dc67bba90fa025da520ffa91223
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 23:11:09 +0000

    Completed ‘Why set up a social enterprise?’ page.

---
+++ starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/why-set-up-a-social-enterprise.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Why set up a social enterprise? | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Thinking about starting a social enterprise?</h1>
+		<h2>Why set up a social enterprise?</h2>
+		<p>If you are thinking about setting up a business but you want to achieve more than just financial returns, then the social enterprise model may be the most appropriate available.</p>
+		<p>Social enterprises have led the way in using commercial business principles to promote the ethical trading of goods and services and achieve social and/or environmental aims. Although such organisations have traditionally been associated with the delivery of public services, many operate very successfully across a wide variety of commercial sectors.</p>
+		<p>The commercial sectors in which social enterprises operate include furniture restoration, the supply of bottled water, chocolate and coffee production, the hospitality industry, graphic design, call centres and consultancy firms and many more.</p>
+		<h3>Examples of social enterprises</h3>
+		<p>Social enterprises are often formed to address a particular social or environmental need, such as in the provision of community transport for schoolchildren, the elderly and the disabled or in recycling services. For example, 'Women like Us' is a social enterprise that supports women to get into part-time, flexible employment.</a>
+		</p>
+		<p>Many social enterprises address a variety of needs at once, such as the Eden Project in Cornwall which educates visitors on global environmental issues while also regenerating and bringing tourism to the area.</p>
+		<p>You may have identified a gap in local service provision or a failure of the commercial market to address a more widespread community problem.</p>
+		<p>Consumers are increasingly demanding ethically-sourced and produced goods, whilst the reform of public services has led to a growing desire for greater choice, eg in the provision of health and social care. Central Surrey Health (CSH) is a social enterprise providing community nursing and therapy services in Surrey. CSH began life within the local primary care trust but is now an independent organisation that is co-owned by its employees. It exists to serve the local community which started life within a primary care trust, and now operates as a separate nursing and therapy body operating alongside the NHS.</p>
+		<p>Social enterprises are well-placed to address these demands, using commercially viable business models to innovate and effect long-term social and environmental change. See the page in this guide on <a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/social-enterprises-and-the-public-sector.php">social enterprises and the public sector</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/what-is-a-social-enterprise.php" title="What is a social enterprise?">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/benefits-of-a-social-enterprise.php" title="Benefits of a social enterprise">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise.php" title="Thinking about starting a social enterprise?">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/what-is-a-social-enterprise.php">What is a social enterprise?</a></li>
+			<li>Why set up a social enterprise?</li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/benefits-of-a-social-enterprise.php">Benefits of a social enterprise</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/social-enterprise-skills.php">Social enterprise skills</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/challenges-facing-social-enterprises.php">Challenges facing social enterprises</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/business-and-marketing-plans.php">Business and marketing plans</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/financing-your-social-enterprise.php">Financing your social enterprise</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/social-enterprises-and-the-public-sector.php">Social enterprises and the public sector</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/checklist-starting-up-a-social-enterprise.php">Checklist: starting up a social enterprise</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-being-a-social-enterprise-works-for-our-business.php">Here's how being a social enterprise works for our business</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-I-started-a-successful-social-enterprise.php">Here's how I started a successful social enterprise</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/78927/building-stronger-civil-society.pdf" target="_blank" rel="noopener noreferrer">Download 'Big Society' strategy information from the Cabinet Office website (PDF, 182K)</a></li>
+			<li><a href="http://www.socialenterprise.org.uk/about/about-social-enterprise" target="_blank" rel="noopener noreferrer">Social enterprise information on the Social Enterprise UK website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/selling/overview-on-selling-to-government.php">Overview on selling to government</a></li>
+			<li><a href="/environment-and-efficiency/the-business-case-for-environmental-improvements/benefits-of-improving-your-environmental-performance.php">Benefits of improving your environmental performance</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/overseas-supplier-relationships/ethical-trading.php">Ethical trading</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/corporate-social-responsibility.php">Corporate social responsibility</a></li>
+			<li><a href="/your-business-sector/property-management/residential-property-standards-licences-and-useful-schemes/becoming-a-social-landlord.php">Becoming a social landlord</a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" title="Learn about the different types of business and legal structures to help you choose the right one for your new business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" title="Tutorials, videos and tools to help you decide how to become self-employed - from sole trader to limited company, franchisee to social enterprise and more" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+			<li><a href="https://www.gov.uk/set-up-business" title="Case studies and tutorials to help you understand the legal dos and don'ts of naming your business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php">Set up a social enterprise</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
