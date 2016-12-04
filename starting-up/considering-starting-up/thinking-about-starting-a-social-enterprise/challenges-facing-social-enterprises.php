735c88b9364243dce4f8d39d7a12e6f7aead91db
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 23:22:36 +0000

    Completed ‘Challenges facing social enterprises’ page.

---
+++ starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/challenges-facing-social-enterprises.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Challenges facing social enterprises | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Thinking about starting a social enterprise?</h1>
+		<h2>Challenges facing social enterprises</h2>
+		<p>Starting a social enterprise can be both exciting and rewarding, but it is also full of challenges. Social enterprises have to compete in the commercial market and face the same challenges and risks as all businesses.</p>
+		<p>For your social enterprise to be successful, you need to work to a 'double or triple bottom line' - social and/or environmental and financial - and in competitive markets. This can be a challenge when competing against traditional businesses working to a purely financial bottom line.</p>
+		<p>You will need to use your entrepreneurial drive to achieve social and/or environmental and financial aims without relying on grants to succeed. However, your independence will help to avoid excessive bureaucracy and allow you to change and innovate more quickly.</p>
+		<p>If you are successful, you could do well financially and have an interesting and fulfilling business career.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/social-enterprise-skills.php" title="Social enterprise skills">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/business-and-marketing-plans.php" title="Business and marketing plans">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise.php" title="Thinking about starting a social enterprise?">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/what-is-a-social-enterprise.php">What is a social enterprise?</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/why-set-up-a-social-enterprise.php">Why set up a social enterprise?</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/benefits-of-a-social-enterprise.php">Benefits of a social enterprise</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/social-enterprise-skills.php">Social enterprise skills</a></li>
+			<li>Challenges facing social enterprises</li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/business-and-marketing-plans.php">Business and marketing plans</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/financing-your-social-enterprise.php">Financing your social enterprise</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/social-enterprises-and-the-public-sector.php">Social enterprises and the public sector</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/checklist-starting-up-a-social-enterprise.php">Checklist: starting up a social enterprise</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-being-a-social-enterprise-works-for-our-business.php">Here's how being a social enterprise works for our business</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-I-started-a-successful-social-enterprise.php">Here's how I started a successful social enterprise</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/78927/building-stronger-civil-society.pdf" target="_blank" rel="noopener noreferrer">Download 'Big Society' strategy information from the Cabinet Office website (PDF, 182K)</a></li>
+			<li><a href="http://www.socialenterprise.org.uk/about/about-social-enterprise" target="_blank" rel="noopener noreferrer">Social enterprise information on the Social Enterprise UK website</a></li>
+		</ul>
+				
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-legal-structures" title="Learn about the different types of business and legal structures to help you choose the right one for your new business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" title="Tutorials, videos and tools to help you decide how to become self-employed - from sole trader to limited company, franchisee to social enterprise and more"target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+			<li><a href="https://www.gov.uk/set-up-business" title="Case studies and tutorials to help you understand the legal dos and don'ts of naming your business"target="_blank" rel="noopener noreferrer">Name your business</a></li>
+			<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php">Set up a social enterprise</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
