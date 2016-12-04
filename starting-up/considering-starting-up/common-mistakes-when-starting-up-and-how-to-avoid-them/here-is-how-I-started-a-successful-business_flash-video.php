42c4055d5182fcc2ac03c2607a9e580e3d539d55
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 17:02:41 +0000

    Completed ‘Here's how I started a successful business (Flash video)’ page.

---
+++ starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/here-is-how-I-started-a-successful-business_flash-video.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Here's how I started a successful business (Flash video) | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Here's how I started a successful business (Flash video)</h1>
+		<h2>Here's how I started a successful business (Flash video)</h2>
+		<p>King of Shaves is a toiletries company started by entrepreneur Will King in 1993, which now has its products stocked in approximately 33,000 stores worldwide.</p>
+		<p>Here, founder and CEO Will King talks about how he created King of Shaves and how he secured funding to grow the business.</p>
+
+		<?php /* ?>
+		<div class="flex-video widescreen vimeo">
+			<iframe src="http://player.vimeo.com/video/192278297?badge=0&byline=0&portrait=0&title=0" width="720" height="540" allowfullscreen style="border:0"></iframe>
+		</div>
+		<?php */ ?>
+		
+		<ul>
+			<li>
+				<a href="#">Read transcript</a>
+			</li>
+			<li>
+				<a title="Play the audio only version of this video" href="../../../_flash/king-of-shaves.mp3">Listen to audio only (mp3, 3.5MB)</a>
+			</li>
+			<li><a href="/video-and-e-learning-help.php">Help</a></li>
+		</ul>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/hiring-the-wrong-people.php" title="Hiring the wrong people">Prev</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them.php">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-or-inadequate-market-research.php">Poor or inadequate market research</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/weak-financial-planning.php">Weak financial planning</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/setting-sights-too-high.php">Setting sights too high</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/taking-your-eye-off-the-competition.php">Taking your eye off the competition</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-supplier-and-customer-controls.php">Poor supplier and customer controls</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/poor-stock-and-asset-management.php">Poor stock and asset management</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them/hiring-the-wrong-people.php">Hiring the wrong people</a></li>
+			<li>Here's how I started a successful business (Flash video)</li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<div class="media-object stack-for-small">
+			<div class="media-object-section">
+				<div class="thumbnail">
+					<img src="../../../_img/WillKing.jpg" width="181" height="121" border="0" alt="" />
+				</div>
+			</div>
+			<div class="media-object-section">
+				<h3>
+					Will King<br />
+					<a href="http://www.shave.com" target="_blank" rel="noopener noreferrer">King of Shaves</a><br />
+					Will's top tips:
+				</h3>
+				<ul>
+					<li>"Believe in the satisfaction of success - the belief you're going to get there."</li>
+					<li>"Rely on passion and persistence - put your skin in the game."</li>
+					<li>"Have an attitude of action."</li>
+				</ul>
+			</div>
+		</div>
+
+		<p><a href="findContentBankf4d5.html?bank=CASE_STUDIES"  title=""><b>Find a case study</b></a></p>
+
+		<img src="../../../_img/px_startingabusiness.gif" width="181" height="5" alt="" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
