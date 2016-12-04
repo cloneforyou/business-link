2b730d23fd41e338008f19b956b81f178d6b60a9
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 18:00:15 +0000

    Completed ‘Advantages and disadvantages of starting a part-time business’ page.

---
+++ starting-up/considering-starting-up/starting-a-part-time-business/advantages-and-disadvantages-of-starting-a-part-time-business.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Advantages and disadvantages of starting a part-time business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a part-time business</h1>
+		<h2>Advantages and disadvantages of starting a part-time business</h2>
+		<p>There are advantages and disadvantages to running a business on a part-time basis.</p>
+		<p>Advantages include:</p>
+		<ul>
+			<li>you are able to stay in paid work and have the security of a regular income until the business is up and running</li>
+			<li>you can use skills you have learned working for an employer in starting and building your own business</li>
+			<li>if you have obligations such as caring for dependents, it is a way of having an income</li>
+			<li>you can test whether there is a market for your products or services without a major financial commitment</li>
+			<li>if you have a hobby that you would spend time on outside work anyway, you may be able to make money from your interests</li>
+		</ul>
+		<p>However, disadvantages include:</p>
+		<ul>
+			<li>finding the time to run a business can be difficult and it may take up more time than you have available</li>
+			<li>you may find it hard to concentrate on paid work if the business has a problem</li>
+			<li>you may have to put in long hours, which can cause stress</li>
+			<li>it might take a long time for the new business to develop and become viable</li>
+			<li>if you stay in work, you'll have to pay tax on both sets of income, which may mean the business is not financially viable</li>
+		</ul>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/tax-and-legal-implications-of-a-part-time-business.php" title="Tax and legal implications of a part-time business">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/help-and-support-for-part-time-businesses.php" title="Help and support for part-time businesses">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business.php" title="Starting a part-time business">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/what-to-consider-when-starting-your-part-time-business.php">What to consider when starting your part-time business</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/tax-and-legal-implications-of-a-part-time-business.php">Tax and legal implications of a part-time business</a></li>
+			<li>Advantages and disadvantages of starting a part-time business</li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/help-and-support-for-part-time-businesses.php">Help and support for part-time businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/here-is-how-I-decided-I-was-ready-to-start-up-my-business.php">Here's how I decided I was ready to start up my business</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<h3>Also on this site</h3>
+		<ul>
+		<li><a href="https://www.gov.uk/set-up-business" title="Discover what it means to be your own boss and how to take the first steps in getting your business off the ground" target="_blank" rel="noopener noreferrer">Find out whether you can work for yourself</a></li>
+		<li><a href="https://www.gov.uk/business-legal-structures" title="Learn about the different types of business and legal structures to help you choose the right one for your new business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
