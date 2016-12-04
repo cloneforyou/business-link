3add7ba1f1c8122a7fffdf55b62253b57726b093
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 20:54:07 +0000

    Completed ‘Finance and grants’ page.

---
+++ finance-and-grants.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Finance and grants | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-6 large-push-4 columns">
+	
+			
+			<h1>Finance and grants</h1>
+			<h2>Raising finance</h2>
+			<ul>
+				<li><a href="/finance-and-grants/finance-options.php">Finance options</a></li>
+				<li><a href="/finance-and-grants/prepare-to-raise-finance.php">Prepare to raise finance</a></li>
+			</ul>
+			<h2>Managing finance</h2>
+			<ul>
+				<li><a href="/finance-and-grants/financial-planning-and-accounts.php">Financial planning and accounts</a></li>
+				<li><a href="/finance-and-grants/manage-your-cashflow.php">Manage your cashflow</a></li>
+				<li><a href="/finance-and-grants/improve-your-cashflow-and-business-performance.php">Improve your cashflow and business performance</a></li>
+				<li><a href="/finance-and-grants/managing-a-business-when-economic-conditions-are-tough.php">Managing a business when economic conditions are tough</a></li>
+				<li><a href="/finance-and-grants/managing-suppliers-and-payments.php">Managing suppliers and payments</a></li>
+				<li><a href="/finance-and-grants/debt-recovery.php">Debt recovery</a></li>
+				<li><a href="/finance-and-grants/business-banking.php">Business banking</a></li>
+				<li><a href="/finance-and-grants/insurance.php">Insurance</a></li>
+				<li><a href="/finance-and-grants/expert-financial-advice.php">Expert financial advice</a></li>
+			</ul>
+			<h2>Financial difficulty</h2>
+			<ul>
+				<li><a href="/finance-and-grants/introduction-to-insolvency.php">Introduction to insolvency</a></li>
+				<li><a href="/finance-and-grants/insolvency-information-for-limited-companies.php">Insolvency information for limited companies</a></li>
+				<li><a href="/finance-and-grants/insolvency-information-for-partnerships.php">Insolvency information for partnerships</a></li>
+				<li><a href="/finance-and-grants/insolvency-information-for-sole-traders-and-individuals.php">Insolvency information for sole traders and individuals</a></li>
+				<li><a href="/finance-and-grants/complain-against-an-insolvency-practitioner-or-the-insolvency-service.php">Complain against an insolvency practitioner or The Insolvency Service</a></li>
+				<li><a href="/finance-and-grants/how-to-complain-against-a-company-or-limited-liability-partnership.php">How to complain against a company or limited liability partnership</a></li>
+				<li><a href="/finance-and-grants/recover-assets.php">Recover assets</a></li>
+			</ul>
+			<p><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/sitemap?topicId=1073858790" target="_blank" rel="noopener noreferrer">Finance and grants - site map</a></p>
+
+			<h3>Spotlight</h3>
+			<h4><a href="https://www.gov.uk/growing-your-business" title="Information and tools for people interested in growing or improving their business" target="_blank" rel="noopener noreferrer">Growth and Improvement Service</a></h4>
+			
+			<img alt="" src="../../_img/hr_165px_black.gif" />
+			
+			<p>Access information and support to help your business work better</p>
+			
+			<a href="https://www.gov.uk/growing-your-business" target="_blank" rel="noopener noreferrer">
+				<img src="../../_img/GIS_76x76.jpg" width="76" height="76"  alt="Information and tools for people interested in growing or improving their business" />
+			</a>
+			
+			<h4><a href="#" title="Video guidance from Business Link on the Business Link YouTube channel">Watch videos about financing your business</a></h4>
+			
+			<img alt="" src="../../_img/hr_165px_black.gif"/>
+			
+			<p>Get practical advice from business advisers</p>
+			
+			<a href="#" title="Video guidance from Business Link on the Business Link YouTube channel">
+				<img src="../../_img/Video3Pitch2.jpg" width="76" height="76"  alt="Video guidance from Business Link on the Business Link YouTube channel" />
+			</a>
+			
+			<h3>Case studies</h3>
+			<p>View case studies for finance and grants</p>
+			
+			<img alt="" src="../../_img/hr_165px_black.gif"/>
+
+			<ul>
+				<li><a href="/find-a-case-study.php">Find a case study</a></li>
+			</ul>
+			
+			<img src="../../_img/face.jpg" width="76" height="76"  alt="" />
+			
+			<h3>Do it online</h3>
+			<ul>
+				<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance options for your business</a></li>
+				<li><a href="/finance-and-grants/debt-recovery/recover-your-debts.php.html">Find out how to recover your debts</a></li>
+				<li><a href="/buy-or-sell-a-business/businesses-in-difficulty/assess-your-business-performance.php">Assess your business' performance</a></li>
+				<li><a href="https://www.gov.uk/growing-your-business" target="_blank" rel="noopener noreferrer">Search our business support finder for grants, loans, expertise and advice for which your business may be eligible</a></li>
+			</ul>
+
+			<h3>What's popular</h3>
+			<ul>
+				<li><a href="/finance-and-grants/manage-your-cashflow/credit-checking-your-customers-and-setting-credit-limits.php">Credit checking your customers and setting credit limits</a></li>
+				<li><a href="https://www.gov.uk/growing-your-business" target="_blank" rel="noopener noreferrer">Business awards finder</a></li>
+				<li><a href="/finance-and-grants/managing-a-business-when-economic-conditions-are-tough.php" title="Run your business more effectively when economic conditions are tough or unpredictable">Managing a business when economic conditions are tough</a></li>
+				<li><a href="/finance-and-grants/manage-your-cashflow/invoicing-and-payment-terms.php">Invoicing and payment terms</a></li>
+				<li><a href="/finance-and-grants/manage-your-cashflow/identify-potential-cashflow-problems.php">Identify potential cashflow problems</a></li>
+				<li><a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">Cashflow management: the basics</a></li>
+			</ul>
+
+			<h3>Useful links on other sites</h3>
+			<ul>
+				<li><a href="http://www.cicm.com/" target="_blank" rel="noopener noreferrer">Credit Management Matters</a></li>
+			</ul>
+			
+			<h3>Directories</h3>
+			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://www.businesslink.gov.uk/bdotg/action/event?topicId=1073858790" title="National and local events, seminars and workshops in your area to help you learn or network" target="_blank" rel="noopener noreferrer">Finance and grants events near you</a></li>
+				<li><a href="/find-a-case-study/browse-by-theme-finance-and-grants.php">Finance and grants case studies</a></li>
+			</ul>
+
+
+	</div><!-- // .small-12 .large-6 .columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
