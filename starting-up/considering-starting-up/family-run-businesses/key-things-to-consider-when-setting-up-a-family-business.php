ae8b7f18a80d306b303f3fd7a2365977e74e6068
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 21:26:04 +0000

    Completed ‘Key things to consider when setting up a family business’ page.

---
+++ starting-up/considering-starting-up/family-run-businesses/key-things-to-consider-when-setting-up-a-family-business.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Key things to consider when setting up a family business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Family-run businesses</h1>
+		<h2>Key things to consider when setting up a family business</h2>
+		<p>Setting up a family business can be an exciting challenge. Before you go ahead it's important to consider how you'll deal with a number of issues that commonly confront such ventures. Think about how:</p>
+		<ul>
+			<li>the business' <strong>shares</strong> will be allocated between family members and if there will be non-family shareholders</li>
+			<li>to ensure <strong>business decisions</strong> are taken for business reasons, rather than personal ones</li>
+			<li>the <strong>roles</strong> and <strong>responsibilities</strong> will differ between family shareholders who are active in the business, those who aren't, and outside shareholders</li>
+			<li>to <strong>reward</strong> family members, whether it will be different to remuneration for non-family members and what problems this could cause</li>
+			<li>you will cope with <strong>conflict</strong> when it arises</li>
+			<li>to communicate with your family when you are also their boss</li>
+			<li>to avoid resentment when deciding who will <strong>succeed</strong> you</li>
+			<li>to ensure that the family's finances aren't entirely dependent on the business</li>
+		</ul>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/advantages-of-family-businesses.php" title="Advantages of family businesses">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/communication-and-family-businesses.php" title="Communication and family businesses">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses.php" title="Family-run businesses">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/advantages-of-family-businesses.php">Advantages of family businesses</a></li>
+			<li>Key things to consider when setting up a family business</li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/communication-and-family-businesses.php">Communication and family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/managing-conflict-in-family-businesses.php">Managing conflict in family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/pay-and-benefits-for-family-members.php">Pay and benefits for family members</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/draw-up-a-family-business-constitution.php">Draw up a family-business constitution</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/succession-planning-in-family-businesses.php">Succession planning in family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/here-is-how-having-a-family-member-in-the-business-made-a-difference.php">Here's how having a family member in the business made a difference</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="#">Download a guide on family businesses from the BDO Stoy Hayward website (PDF, 50K)</a></li>
+			<li><a href="http://www.ifb.org.uk/" target="_blank" rel="noopener noreferrer">Family business resources on the Institute for Family Business website</a></li>
+			<li><a href="#">Download a family-business rulebook from the BDO Stoy Hayward website (PDF, 55K)</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/employment-and-skills/different-types-of-worker/employing-family-members-and-voluntary-staff.php">Employing family members and voluntary staff</a></li>
+			<li><a href="/finance-and-grants/finance-options/borrowing/financing-from-friends-and-family.php">Financing from friends and family</a></li>
+			<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php">Prepare a business plan</a></li>
+			<li><a href="/buy-or-sell-a-business/transferring-your-business/selling-or-passing-a-business-to-a-family-member.php">Selling or passing a business to a family member</a></li>
+			<li><a href="/employment-and-skills/pay-and-pensions/set-the-right-pay-rates.php">Set the right pay rates</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php">Starting a business from home </a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
