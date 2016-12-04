fdaf7116e0430fbdb515e46133075a2fc483ec7d
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 17:53:36 +0000

    Completed ‘What to consider when starting your part-time business’ page.

---
+++ starting-up/considering-starting-up/starting-a-part-time-business/what-to-consider-when-starting-your-part-time-business.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>What to consider when starting your part-time business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a part-time business</h1>
+		<h2>What to consider when starting your part-time business</h2>
+		<p>You should consider a number of issues when you start your own business, even if it is only on a part-time basis.</p>
+		<h3>Are you ready to start up?</h3>
+		<p>There are lots of reasons to consider starting a business but you need to be sure that you are ready. You'll need to assess your own skills and where you might need some extra development or support - for example, looking after the business' finances. You should also think carefully about the product or service that you want to sell, the audience you're selling to and what you have that makes you stand out from the crowd. You'll also have to be ready to face the challenges and pressures on yourself and your finances.</p>
+		<h3>What legal structure?</h3>
+		<p>You will need a formal legal structure for your business. This can take the form of anything from a sole trader to a limited company. When you have decided the structure, you will need to register with HM Revenue &amp; Customs and (if it is a limited company) with Companies House.</p>
+		<h3>What about premises?</h3>
+		<p>You may have decided to run your business from home but you need to be aware of the implications of this as there are tax, health, safety and security issues. You might find that separating your work area from your living area will maximise efficiency. For more information, see our guide on <a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php">starting a business from home</a>.</p>
+		<p>If you decide not to work from home, read our guide on how to <a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business.php">choose the right premises for your business</a>.</p>
+		<h3>Money matters</h3>
+		<p>Your business will start out small and you will still need to fund both the set-up and the development of your business to help it grow. Creative thinking and planning your finances might save you money and make it less costly than you think to start your own business. You will find help in our interactive guide on how to <a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">identify the right finance options for your business</a>.</p>
+		<h3>Professional advice</h3>
+		<p>Depending on the scale of your business, you may need professional advice or assistance, which can range from accountancy and bookkeeping through to marketing and IT support. You'll find more information in our section of guides on <a href="/starting-up/further-support-for-your-business.php">further support for your business</a>.</p>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business.php" title="Starting a part-time business">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/tax-and-legal-implications-of-a-part-time-business.php" title="Tax and legal implications of a part-time business">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business.php" title="Starting a part-time business">Introduction</a></li>
+			<li>What to consider when starting your part-time business</li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/tax-and-legal-implications-of-a-part-time-business.php">Tax and legal implications of a part-time business</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/advantages-and-disadvantages-of-starting-a-part-time-business.php">Advantages and disadvantages of starting a part-time business</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/help-and-support-for-part-time-businesses.php">Help and support for part-time businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/here-is-how-I-decided-I-was-ready-to-start-up-my-business.php">Here's how I decided I was ready to start up my business</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php">Starting a business from home</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business.php">Choose the right premises for your business</a></li>
+			<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Identify the right finance options for your business</a></li>
+			<li><a href="https://www.gov.uk/set-up-business" title="Discover what it means to be your own boss and how to take the first steps in getting your business off the ground" target="_blank" rel="noopener noreferrer">Find out whether you can work for yourself</a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" title="Learn about the different types of business and legal structures to help you choose the right one for your new business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
