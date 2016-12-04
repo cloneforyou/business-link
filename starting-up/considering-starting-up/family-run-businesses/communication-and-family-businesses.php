02c5fb3b1c768a3429806812fb274b6c1e6072e4
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 21:36:08 +0000

    Completed ‘Communication and family businesses’ page.

---
+++ starting-up/considering-starting-up/family-run-businesses/communication-and-family-businesses.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Communication and family businesses | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Family-run businesses</h1>
+		<h2>Communication and family businesses</h2>
+		<p>Many misunderstandings and potential areas for dispute in family businesses can be avoided if you ensure good communication channels are in place.</p>
+		<p>The risks are that:</p>
+		<ul>
+			<li>family members assume they know what other family members feel or want</li>
+			<li>personal ties inhibit honest opinions being expressed</li>
+			<li>the head of the family may automatically assume control of the business even if they don't have the best business skills</li>
+			<li>one family member ends up dominating the business</li>
+			<li>family-member shareholders not active in the business fail to understand the objectives of those who are active, and vice versa</li>
+			<li>personal resentments become business resentments, and vice versa</li>
+			<li>non-family board or management members feel excluded</li>
+		</ul>
+		<p>To avoid these pitfalls, you should foster an atmosphere in which open discussion and communication is welcomed and concerns can be voiced without blame being cast.</p>
+		<p>There are a number of practical things you can do. You might:</p>
+		<ul>
+			<li>remove personal issues from business discussions by holding all meetings in a work rather than home environment</li>
+			<li>create mechanisms for providing constructive feedback - this can help prevent staff, particularly non-family employees, from feeling demotivated and uninvolved</li>
+			<li>arrange occasional away days to discuss the business' strategy and direction</li>
+			<li>appoint a non-executive director to the board, or establish an advisory board to provide an impartial viewpoint and help prevent emotions from clouding business decisions</li>
+			<li>establish a family constitution creating policies that will guide the family's relationship with the business</li>
+		</ul>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/key-things-to-consider-when-setting-up-a-family-business.php" title="Key things to consider when setting up a family business">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/managing-conflict-in-family-businesses.php" title="Managing conflict in family businesses">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses.php" title="Family-run businesses">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/advantages-of-family-businesses.php">Advantages of family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/key-things-to-consider-when-setting-up-a-family-business.php">Key things to consider when setting up a family business</a></li>
+			<li>Communication and family businesses</li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/managing-conflict-in-family-businesses.php">Managing conflict in family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/pay-and-benefits-for-family-members.php">Pay and benefits for family members</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/draw-up-a-family-business-constitution.php">Draw up a family-business constitution</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/succession-planning-in-family-businesses.php">Succession planning in family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/here-is-how-having-a-family-member-in-the-business-made-a-difference.php">Here's how having a family member in the business made a difference</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="#">Download a guide on family businesses from the BDO Stoy Hayward website (PDF, 50K)</a></li>
+			<li><a href="http://www.ifb.org.uk/" target="_blank" rel="noopener noreferrer">Family business resources on the Institute for Family Business website</a></li>
+			<li><a href="#">Download a family-business rulebook from the BDO Stoy Hayward website (PDF, 55K)</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/employment-and-skills/different-types-of-worker/employing-family-members-and-voluntary-staff.php">Employing family members and voluntary staff</a></li>
+			<li><a href="/employment-and-skills/staff-documents-and-employment-policies/set-up-employment-policies-for-your-business.php">Set up employment policies for your business</a></li>
+			<li><a href="/employment-and-skills/redundancy-restructures-and-change/inform-and-consult-your-employees.php">Inform and consult your employees</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php">Starting a business from home</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
