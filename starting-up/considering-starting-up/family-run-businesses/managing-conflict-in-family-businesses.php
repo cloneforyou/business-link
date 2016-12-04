38ea4fb0fe3f7e43fb6461bd76233b716c33e04d
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 21:37:10 +0000

    Completed ‘Managing conflict in family businesses’ page.

---
+++ starting-up/considering-starting-up/family-run-businesses/managing-conflict-in-family-businesses.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Managing conflict in family businesses | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Family-run businesses</h1>
+		<h2>Managing conflict in family businesses</h2>
+		<p>The potential for conflict in family businesses can be greater than for other businesses - typically due to a clash between commercial and emotional concerns.</p>
+		<p>However, conflict can be seen as a <strong>challenge</strong> - or even as a positive driver for change. For example, a disagreement between family members on the strategic direction of your business may result in a much-needed rethinking of your business plan and a new agreed vision for the business.</p>
+		<p>Such outcomes, though, are only possible if techniques for avoiding, managing and resolving disputes have already been instituted.</p>
+		<h3>Ways to avoid conflict</h3>
+		<p>Think about how people in your family business communicate with each other. Are emotional issues kept separate from business discussions? Are mechanisms in place to allow all employees - not just family members - to contribute their views? Or does one person tend to dominate?</p>
+		<p>The best way of avoiding conflict is to prevent misunderstandings from happening in the first place. Drawing up a family constitution can help you achieve this.</p>
+		<p>Plan how you'll deal with conflict and disagreements and set this out in the family business constitution, see our guide on <a href="/employment-and-skills/problems-at-work/managing-conflict.php">managing conflict</a>.</p>
+		<p>Holding a <strong>meeting</strong> of the business' management may be appropriate for addressing relatively minor disputes.</p>
+		<p>For more serious matters you may want to involve an independent <strong>third party</strong> - many family businesses benefit from having a non-executive director or business adviser - to act as an impartial mediator.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/communication-and-family-businesses.php" title="Communication and family businesses">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/pay-and-benefits-for-family-members.php" title="Pay and benefits for family members">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses.php" title="Family-run businesses">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/advantages-of-family-businesses.php">Advantages of family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/key-things-to-consider-when-setting-up-a-family-business.php">Key things to consider when setting up a family business</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/communication-and-family-businesses.php">Communication and family businesses</a></li>
+			<li>Managing conflict in family businesses</li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/pay-and-benefits-for-family-members.php">Pay and benefits for family members</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/draw-up-a-family-business-constitution.php">Draw up a family-business constitution</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/succession-planning-in-family-businesses.php">Succession planning in family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/here-is-how-having-a-family-member-in-the-business-made-a-difference.php">Here's how having a family member in the business made a difference</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.rileycom.co.uk/cms/" target="_blank" rel="noopener noreferrer">Conflict management advice on the Riley Chartered Accountants website</a></li>
+			<li><a href="http://www.ifb.org.uk/" target="_blank" rel="noopener noreferrer">Family business resources on the Institute for Family Business website</a></li>
+			<li><a href="#">Download a guide on family businesses from the BDO Stoy Hayward website (PDF, 50K)</a></li>
+		</ul>
+		
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/employment-and-skills/different-types-of-worker/employing-family-members-and-voluntary-staff.php">Employing family members and voluntary staff</a></li>
+			<li><a href="/employment-and-skills/staff-documents-and-employment-policies/set-up-employment-policies-for-your-business.php">Set up employment policies for your business</a></li>
+			<li><a href="/employment-and-skills/problems-at-work/disciplinary-procedures-hearings-and-appeals.php">Disciplinary procedures, hearings and appeals</a></li>
+			<li><a href="/employment-and-skills/redundancy-restructures-and-change/inform-and-consult-your-employees.php">Inform and consult your employees</a></li>
+			<li><a href="/employment-and-skills/problems-at-work/managing-conflict.php">Managing conflict</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php">Starting a business from home </a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
