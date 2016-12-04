e2cc7e2680fce0b3c26e2c929c3df9681d958aa1
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 21:57:25 +0000

    Completed ‘Succession planning in family businesses’ page.

---
+++ starting-up/considering-starting-up/family-run-businesses/succession-planning-in-family-businesses.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Succession planning in family businesses | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Family-run businesses</h1>
+		<h2>Succession planning in family businesses</h2>
+		<p>How best to pass on your business to the next generation will be one of the biggest challenges you face. You need to make the right decisions for you, your family and your business - balancing the needs of all three.</p>
+		<p>It can be made easier if you plan the succession process early - ideally when you set up the business.</p>
+		<p>Your <strong>succession plan</strong> should include:</p>
+		<ul>
+			<li>your key goals for the succession process</li>
+			<li>a timetable of the transition stages, from identifying a successor to the staged and then full transfer of responsibilities</li>
+			<li>contingency plans in case the unforeseen happens, such as your intended successor declining the role</li>
+		</ul>
+		<p>Questions you should ask yourself include the following:</p>
+		<ul>
+			<li>Does my intended successor have the right <strong>skills</strong> and <strong>abilities</strong>?</li>
+			<li>Does my intended successor actually want to take over?</li>
+			<li>Is my plan <strong>fair</strong> to all family members?</li>
+			<li>Does it minimise the potential for conflict?</li>
+			<li>Will family succession be <strong>tax-efficient</strong>? <a href="/tax-payroll-and-company-information/selling-or-closing-your-business/check-tax-and-legal-issues-when-selling-a-business.php">Use our interactive tool to investigate the tax and legal issues when selling or closing your business</a>.</li>
+			<li>Is family succession the best option or would an <strong>alternative exit strategy</strong> - such as a trade sale or management buy-out - be a better option?</li>
+		</ul>
+		<p>For further information, see our guides on <a href="/buy-or-sell-a-business/transferring-your-business/selling-or-passing-a-business-to-a-family-member.php">selling or passing a business to a family member</a> and how to <a href="/buy-or-sell-a-business/considering-buying-a-business/consider-your-exit-strategy-when-starting-up.php">consider your exit strategy when starting up</a>.</p>
+		<p>You can also <a href="http://www.accaglobal.com/vn/en/member/member/accounting-business/2016/10/practice/prac-family.html" target="_blank" rel="noopener noreferrer">find out more about passing family businesses from one generation to the next</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/draw-up-a-family-business-constitution.php" title="Draw up a family-business constitution">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/here-is-how-having-a-family-member-in-the-business-made-a-difference.php" title="Here's how having a family member in the business made a difference">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses.php" title="Family-run businesses">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/advantages-of-family-businesses.php">Advantages of family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/key-things-to-consider-when-setting-up-a-family-business.php">Key things to consider when setting up a family business</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/communication-and-family-businesses.php">Communication and family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/managing-conflict-in-family-businesses.php">Managing conflict in family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/pay-and-benefits-for-family-members.php">Pay and benefits for family members</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/draw-up-a-family-business-constitution.php">Draw up a family-business constitution</a></li>
+			<li>Succession planning in family businesses</li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/here-is-how-having-a-family-member-in-the-business-made-a-difference.php">Here's how having a family member in the business made a difference</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.accaglobal.com/vn/en/member/member/accounting-business/2016/10/practice/prac-family.html" target="_blank" rel="noopener noreferrer">Best practices for passing family businesses from one generation to the next</a></li>
+			<li><a href="http://www.ukfbs.co.uk/" target="_blank" rel="noopener noreferrer">Practical advice for family enterprises on the FBS Global website</a></li>
+			<li><a href="http://www.ifb.org.uk/" target="_blank" rel="noopener noreferrer">Family business resources on the Institute for Family Business website</a></li>
+			<li><a href="/tax-payroll-and-company-information/selling-or-closing-your-business/check-tax-and-legal-issues-when-selling-a-business.php">Use our interactive tool to investigate the tax and legal issues when selling or closing your business</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/buy-or-sell-a-business/transferring-your-business/selling-or-passing-a-business-to-a-family-member.php">Selling or passing a business to a family member</a></li>
+			<li><a href="/buy-or-sell-a-business/transferring-your-business/succession-planning-and-business-transfer.php">Succession planning and business transfer</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-buying-a-business/consider-your-exit-strategy-when-starting-up.php">Consider your exit strategy when starting up</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php">Prepare a business plan</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php">Starting a business from home</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
