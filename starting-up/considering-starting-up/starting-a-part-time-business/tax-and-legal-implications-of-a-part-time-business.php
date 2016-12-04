ae1520dd365b8aaaa477ebe91fe3f79bcbdc278b
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 17:58:05 +0000

    Completed ‘Tax and legal implications of a part-time business’ page.

---
+++ starting-up/considering-starting-up/starting-a-part-time-business/tax-and-legal-implications-of-a-part-time-business.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tax and legal implications of a part-time business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a part-time business</h1>
+		<h2>Tax and legal implications of a part-time business</h2>
+		<p>When you set up a part-time business you need to inform HM Revenue &amp; Customs (HMRC) and if it is a limited company you'll need to register it as well.</p>
+		<p>You also have an obligation to maintain accurate financial records - see our guide on how to <a href="/tax-payroll-and-company-information/set-up-a-basic-record-keeping-system.php">set up a basic record-keeping system</a>.</p>
+		<p>These requirements are the same whether you are starting a full or part-time business but there are other issues to consider which are specific to part-time businesses.</p>
+		<h3>Continuing in paid work?</h3>
+		<p>If you continue in paid work when you are not running your business, you have to decide whether to tell your employer. You should look at your contract of employment, as this may require you to tell your employer if you have another source of income. In addition, if your new venture is likely to compete with your employer, you have a conflict of interest which could cause problems.</p>
+		<p>You must be very careful about separating your personal business activities from those of your employer. Taking phone calls, sending emails, and writing letters that relate to your business may constitute a breach of contract, unless you have permission to do so. You should also not make use of your employer's supplies, materials or intellectual property. For example, taking copies of proprietary software to use in your business is a criminal offence.</p>
+		<p>If you discuss your plans with your employer, they could become a source of encouragement or work, especially if you are a valued employee.</p>
+		<h3>Just running the business?</h3>
+		<p>If you don't have other paid employment - eg you might be a parent or carer with family responsibilities - you still need to tell HMRC and, if appropriate, register with Companies House. If you were previously unwaged and had been receiving benefits, you will need to check whether your new venture affects that income, and report it to HMRC if necessary.</p>
+		<p>If you are working from home, you need to take into account insurance, health and safety issues. You can read about this in our guide on <a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php">starting a business from home</a>.</p>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/what-to-consider-when-starting-your-part-time-business.php" title="What to consider when starting your part-time business">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/advantages-and-disadvantages-of-starting-a-part-time-business.php" title="Advantages and disadvantages of starting a part-time business">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business.php" title="Starting a part-time business">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/what-to-consider-when-starting-your-part-time-business.php">What to consider when starting your part-time business</a></li>
+			<li>Tax and legal implications of a part-time business</li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/advantages-and-disadvantages-of-starting-a-part-time-business.php">Advantages and disadvantages of starting a part-time business</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/help-and-support-for-part-time-businesses.php">Help and support for part-time businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/here-is-how-I-decided-I-was-ready-to-start-up-my-business.php">Here's how I decided I was ready to start up my business</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/set-up-a-basic-record-keeping-system.php">Set up a basic record-keeping system</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php">Starting a business from home</a></li>
+			<li><a href="https://www.gov.uk/set-up-business" title="Discover what it means to be your own boss and how to take the first steps in getting your business off the ground" target="_blank" rel="noopener noreferrer">Find out whether you can work for yourself</a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" title="Learn about the different types of business and legal structures to help you choose the right one for your new business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
