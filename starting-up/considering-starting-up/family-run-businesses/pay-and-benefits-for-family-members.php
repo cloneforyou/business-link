1f8ef21159b38c2f605078c1450a2e8092862f78
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 21:42:29 +0000

    Completed ‘Pay and benefits for family members’ page.

---
+++ starting-up/considering-starting-up/family-run-businesses/pay-and-benefits-for-family-members.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pay and benefits for family members | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Family-run businesses</h1>
+		<h2>Pay and benefits for family members</h2>
+		<p>Remuneration needn't be a thorny issue. The goal is to have a <strong>remuneration strategy</strong> which is consistent, fair and open.</p>
+		<p>Resentment and conflict tend to occur when these three attributes are missing - for example, if family staff members are paid more than non-family employees without good reason.</p>
+		<p>Family members who hold shares but who aren't active in the business may also question the remuneration of those who are.</p>
+		<h3>Develop a remuneration strategy</h3>
+		<ul>
+			<li>An individual's pay should be based on their value rather than their personal need. Look at what the market rate is for the job.</li>
+			<li>Family members shouldn't be lured into the business with inflated salaries. Likewise, they shouldn't need to endure unreasonably small salaries to prove their loyalty.</li>
+			<li>Benefits, bonuses and incentives should be based on set criteria.</li>
+			<li>Unreasonably high salaries and phantom jobs shouldn't be used to transfer tax-deductible wealth to family members.</li>
+			<li>Post-retirement remuneration plans should be agreed before they come into play.</li>
+			<li>Non-family employees doing the same work as family members should receive the same remuneration.</li>
+		</ul>
+		<p>It's important that your remuneration policy is seen to be <strong>fair and objective</strong>. Write it down, be open about it and review it regularly.</p>
+		<p>Read about pay and pay structures in our guide on how to <a href="/employment-and-skills/pay-and-pensions/set-the-right-pay-rates.php">set the right pay rates</a>.</p>
+		<p>Advice from an outsider - an HR consultant, for example - can be invaluable in avoiding remuneration disputes.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/managing-conflict-in-family-businesses.php" title="Managing conflict in family businesses">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/draw-up-a-family-business-constitution.php" title="Draw up a family-business constitution">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses.php" title="Family-run businesses">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/advantages-of-family-businesses.php">Advantages of family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/key-things-to-consider-when-setting-up-a-family-business.php">Key things to consider when setting up a family business</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/communication-and-family-businesses.php">Communication and family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/managing-conflict-in-family-businesses.php">Managing conflict in family businesses</a></li>
+			<li>Pay and benefits for family members</li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/draw-up-a-family-business-constitution.php">Draw up a family-business constitution</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/succession-planning-in-family-businesses.php">Succession planning in family businesses</a></li>
+			<li><a href="/starting-up/considering-starting-up/family-run-businesses/here-is-how-having-a-family-member-in-the-business-made-a-difference.php">Here's how having a family member in the business made a difference</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="#">Download a family-business rulebook from the BDO Stoy Hayward website (PDF, 55K)</a></li>
+			<li><a href="http://www.ifb.org.uk/" target="_blank" rel="noopener noreferrer">Family business resources on the Institute for Family Business website</a></li>
+			<li><a href="#">Download a guide on family businesses from the BDO Stoy Hayward website (PDF, 50K)</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/employment-and-skills/pay-and-pensions/set-the-right-pay-rates.php">Set the right pay rates</a></li>
+			<li><a href="/employment-and-skills/different-types-of-worker/employing-family-members-and-voluntary-staff.php">Employing family members and voluntary staff</a></li>
+			<li><a href="/employment-and-skills/pay-and-pensions/implement-staff-incentive-schemes.php">Implement staff incentive schemes</a></li>
+			<li><a href="/employment-and-skills/pay-and-pensions/pay-an-overview-of-obligations.php">Pay - an overview of obligations</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php">Starting a business from home </a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
