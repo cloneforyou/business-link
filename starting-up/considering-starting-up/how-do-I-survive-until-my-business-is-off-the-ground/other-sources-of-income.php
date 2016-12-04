76d1ef244ace7416387011a4888ad0efd84c5bba
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 19:51:11 +0000

    Completed ‘Other sources of income’ page.

---
+++ starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/other-sources-of-income.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Other sources of income | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		
+		<h1>How do I survive until my business is off the ground?</h1>
+		<h2>Other sources of income</h2>
+		<p>You will almost certainly have to find other ways to finance your needs during the early days of your business.</p>
+		<p>There are a number of options open to you. You could:</p>
+		<ul>
+			<li>Use savings - make sure that you have an emergency rainy-day fund which should add up to three months spending.</li>
+			<li>Start up a business while retaining your existing employment - using your spare time to run your business.</li>
+			<li>Release equity from an existing asset - for example, trading in your car for a cheaper one.</li>
+			<li>Sell unwanted assets to create income - many people have things that they do not use or want that can be sold at auctions, online or private sales.</li>
+			<li>Get a loan from your family and friends. In most cases people who borrow from family or friends do not pay as much interest on such loans. However, be aware of ill feeling that may be caused if you are unable to repay on time. See our guide on <a href="/finance-and-grants/finance-options/borrowing/financing-from-friends-and-family.php">financing from friends and family</a>.</li>
+			<li>Seek finance from a low-cost fund managed by a local enterprise agency or community organisation.</li>
+			<li>Borrow against future income by selling debts owed to you to a third party - see our guide on <a href="/finance-and-grants/manage-your-cashflow/factoring-and-invoice-discounting-the-basics.php">factoring and invoice discounting: the basics</a>.</li>
+			<li>Get an overdraft or loan from your bank. Remember that the overdraft or loan will have to be repaid, and the interest rate may be high. See our guide on <a href="/finance-and-grants/finance-options/borrowing/bank-finance.php">bank finance</a>.</li>
+			<li>Use leasing or hire purchase to fund fixed assets such as vehicles or equipment.</li>
+			<li>Seek investment from external sources in return for a share in your business. See our guide on <a href="/finance-and-grants/finance-options/shares-and-equity-finance/equity-finance.php">equity finance</a>.</li>
+			<li>Take on a second or part-time job - this will provide a useful source of income but it is important that it does not distract you from your priority of running your business.</li>
+		</ul>
+		<p><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance options for your business</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/make-savings.php" title="Make savings">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/prepare-a-financial-plan.php" title="Prepare a financial plan">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground.php" title="How do I survive until my business is off the ground?">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/forecast-your-personal-financial-needs.php">Forecast your personal financial needs</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/how-much-money-will-your-business-make.php">How much money will your business make?</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/make-savings.php">Make savings</a></li>
+			<li>Other sources of income</li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/prepare-a-financial-plan.php">Prepare a financial plan</a></li>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground/find-support.php">Find support</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.startupdonut.co.uk/startup/financing-a-business/start-up-funding" target="_blank" rel="noopener noreferrer">Finance options explained on the Start up Donut website</a></li>
+			<li><a href="https://www.gov.uk/growing-your-business" target="_blank" rel="noopener noreferrer">Search our business support finder for grants, loans, expertise and advice for which your business may be eligible </a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-finance-explained" title="Discover your options for self-financing and look at the pros and cons of using your own money to get started" target="_blank" rel="noopener noreferrer">Use your own money to start up</a></li>
+			<li><a href="https://www.gov.uk/business-finance-explained" title="Explore outside investment and 'non-bank' options for raising finance for your business" target="_blank" rel="noopener noreferrer">Raise finance from outside investors or other sources to get started</a></li>
+			<li><a href="https://www.gov.uk/business-finance-explained" title="Understand the advantages and disadvantages of raising finance from family and friends to start your business" target="_blank" rel="noopener noreferrer">Raise finance from family and friends</a></li>
+			<li><a href="https://www.gov.uk/business-finance-explained" title="Learn about the various sources of finance available to start-ups and which could be most appropriate for your business" target="_blank" rel="noopener noreferrer">Explore your finance options when starting up</a></li>
+			<li><a href="https://www.gov.uk/business-finance-explained" title="Learn about the basics of business banking, using bank finance to get started and important questions to ask your bank when starting up" target="_blank" rel="noopener noreferrer">Understand the basics of business banking</a></li>
+			<li><a href="/finance-and-grants/finance-options/borrowing/bank-finance.php">Bank finance</a></li>
+			<li><a href="/finance-and-grants/finance-options/borrowing/financing-from-friends-and-family.php">Financing from friends and family</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/factoring-and-invoice-discounting-the-basics.php">Factoring and invoice discounting: the basics</a></li>
+			<li><a href="/finance-and-grants/finance-options/shares-and-equity-finance/equity-finance.php">Equity finance</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5"  />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
