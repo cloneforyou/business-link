f44b311fed2718fd0ee28e229a18ebdc724ecd07
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 18:07:47 +0000

    Completed ‘Help and support for part-time businesses’ page.

---
+++ starting-up/considering-starting-up/starting-a-part-time-business/help-and-support-for-part-time-businesses.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Help and support for part-time businesses | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a part-time business</h1>
+		<h2>Help and support for part-time businesses</h2>
+		<p>A wide range of help is available to you if you're starting a business - through government agencies or business mentoring. You can get help from a variety of sources online, while other forms of support can be obtained from events such as networking meetings.</p>
+		<p>Business Link is the official government website for businesses of all sizes and provides free business advice and support. You can access information, guidance and support for your business needs and your online dealings with government.</p>
+		<p>Find out how the Business Link service can help your business in our section <a href="/about-business-link.php">about us</a>.</p>
+		<p>Alternative support from the government may be available in your local area, including grants, funding, advice, guidance, information and training. Find out more in our guide on <a href="/finance-and-grants/finance-options/grants-and-government-support/government-support-for-businesses.php">government support for businesses</a>.</p>
+		<p><strong>Mentoring</strong> can help you develop business skills. Read more in our guide to the <a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring.php">benefits of business mentoring</a>. You can also benefit from online mentoring from one of the many mentoring organisations such as horsesmouth - <a href="http://www.horsesmouth.co.uk/publisha.content/spotlight/taxonomy.publisha" target="_blank" rel="noopener noreferrer">find out how mentoring could help your business on the horsesmouth website</a>.</p>
+		<p>Your local <strong>Chamber of Commerce</strong> offers support and advice and may organise networking meetings for businesses of a similar size and in similar business sectors. You can <a href="http://www.britishchambers.org.uk/find-your-chamber/" target="_blank" rel="noopener noreferrer">find contact details for your local Chamber of Commerce on the British Chambers of Commerce website</a>.</p>
+		<p>You might find it helpful to join the trade association for your business sector, where you will be able to interact with peer businesses for advice and information. <a href="http://www.taforum.org/Members" target="_blank" rel="noopener noreferrer">Find contact details for trade associations on the Trade Association Forum website</a>.</p>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/advantages-and-disadvantages-of-starting-a-part-time-business.php" title="Advantages and disadvantages of starting a part-time business">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/here-is-how-I-decided-I-was-ready-to-start-up-my-business.php" title="Here's how I decided I was ready to start up my business">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business.php" title="Starting a part-time business">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/what-to-consider-when-starting-your-part-time-business.php">What to consider when starting your part-time business</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/tax-and-legal-implications-of-a-part-time-business.php">Tax and legal implications of a part-time business</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/advantages-and-disadvantages-of-starting-a-part-time-business.php">Advantages and disadvantages of starting a part-time business</a></li>
+			<li>Help and support for part-time businesses</li>
+		<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business/here-is-how-I-decided-I-was-ready-to-start-up-my-business.php">Here's how I decided I was ready to start up my business</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.horsesmouth.co.uk/publisha.content/spotlight/taxonomy.publisha" target="_blank" rel="noopener noreferrer">Online business mentoring on the horsesmouth website</a></li>
+			<li><a href="http://www.britishchambers.org.uk/find-your-chamber/" target="_blank" rel="noopener noreferrer">Chambers directory on the British Chambers of Commerce website</a></li>
+			<li><a href="http://www.taforum.org/Members" target="_blank" rel="noopener noreferrer">Trade association directory on the Trade Association Forum website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/finance-and-grants/finance-options/grants-and-government-support/government-support-for-businesses.php">Government support for businesses</a></li>
+			<li><a href="https://www.gov.uk/set-up-business" title="Discover what it means to be your own boss and how to take the first steps in getting your business off the ground" target="_blank" rel="noopener noreferrer">Find out whether you can work for yourself</a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" title="Learn about the different types of business and legal structures to help you choose the right one for your new business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
