1371bca76b39e29e25c8a34714742afd22d26819
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-04 23:34:39 +0000

    Completed ‘Financing your social enterprise’ page.

---
+++ starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/financing-your-social-enterprise.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Financing your social enterprise | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Thinking about starting a social enterprise?</h1>
+		<h2>Financing your social enterprise</h2>
+		<p>As with any business, one of the main issues you are likely to face will be finding sources of finance to help convert your idea into a viable business operation, in order to meet your social and environmental objectives.</p>
+		<p>When you start to look for sources of investment finance, it is advisable to write a business plan so that you can formally present your case to potential investors or loan providers. See our guide on how to <a href="/finance-and-grants/prepare-to-raise-finance/how-to-attract-investment/use-your-business-plan-to-get-funding.php">use your business plan to get funding</a>.</p>
+		<p>The number of investors seeking both social and financial returns on their money is growing. Charitable trusts and foundations are among the financial institutions that lend to or invest in social enterprises.</p>
+		<h3>Sources of finance for social enterprises</h3>
+		<p>It is worth investigating traditional finance providers, including banks and building societies, some of which are social enterprises themselves. You could also seek donations from family and friends or the local community.</p>
+		<p>Another option is to approach a Community Development Finance Institution (CDFI). CDFIs have been created specifically to provide investment in businesses that operate in disadvantaged communities. <a href="http://responsiblefinance.org.uk/funding-innovation/" target="_blank" rel="noopener noreferrer">Find information on CDFIs and sources of finance on the Community Development Finance Association (CDFA) website</a>.</p>
+		<p>Government-backed funding includes the Adventure Capital Fund (ACF), which helps ambitious social enterprises to become financially sustainable through a mixture of loans and grants and the provision of expert mentoring and support services. <a href="#">Find out if you are eligible to apply for funding on the ACF website</a>.</p>
+		<p>Futurebuilders England may offer support if you are looking to set up your social enterprise to provide public services. <a href="http://www.futurebuilders-england.org.uk/are-you-eligible/" target="_blank" rel="noopener noreferrer">Check to see if you are eligible to apply for support on the Futurebuilders England website</a>.</p>
+		<p>You can also approach the Bridges Community Development Venture Fund (BCDVF), which has attracted matched funding from the Department for Business, Innovation &amp; Skills. <a href="http://bridgesventures.com/our-funds/" target="_blank" rel="noopener noreferrer">Read about the social criteria for investment for the BCDVF on the Bridges Ventures website</a>.</p>
+		<p>In addition, third sector organisations can <a href="http://www.fundingcentral.org.uk/default.aspx" target="_blank" rel="noopener noreferrer">search for grants and loan financing on the Funding Central website</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/business-and-marketing-plans.php" title="Business and marketing plans">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/social-enterprises-and-the-public-sector.php" title="Social enterprises and the public sector">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise.php" title="Thinking about starting a social enterprise?">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/what-is-a-social-enterprise.php">What is a social enterprise?</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/why-set-up-a-social-enterprise.php">Why set up a social enterprise?</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/benefits-of-a-social-enterprise.php">Benefits of a social enterprise</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/social-enterprise-skills.php">Social enterprise skills</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/challenges-facing-social-enterprises.php">Challenges facing social enterprises</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/business-and-marketing-plans.php">Business and marketing plans</a></li>
+			<li>Financing your social enterprise</li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/social-enterprises-and-the-public-sector.php">Social enterprises and the public sector</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/checklist-starting-up-a-social-enterprise.php">Checklist: starting up a social enterprise</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-being-a-social-enterprise-works-for-our-business.php">Here's how being a social enterprise works for our business</a></li>
+			<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-I-started-a-successful-social-enterprise.php">Here's how I started a successful social enterprise</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://responsiblefinance.org.uk/funding-innovation/" target="_blank" rel="noopener noreferrer">CDFI information on the CDFA website</a></li>
+			<li><a href="http://bridgesventures.com/our-funds/" target="_blank" rel="noopener noreferrer">Social criteria for BCDVF funding on the Bridges Ventures website</a></li>
+			<li><a href="http://www.fundingcentral.org.uk/default.aspx" target="_blank" rel="noopener noreferrer">Civil Society financing support search on the Funding Central website - Opens in a new window</a></li>
+			<li><a href="http://www.governmentfunding.org.uk/Default.aspx" target="_blank" rel="noopener noreferrer">Government funding sources on the governmentfunding.org.uk website</a></li>
+			<li><a href="https://www.gov.uk/growing-your-business" target="_blank" rel="noopener noreferrer">Search our business support finder for grants, loans, expertise and advice for which your business may be eligible</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/finance-and-grants/prepare-to-raise-finance/how-to-attract-investment/use-your-business-plan-to-get-funding.php">Use your business plan to get funding</a></li>
+			<li><a href="/finance-and-grants/finance-options/borrowing/bank-finance.php">Bank finance</a></li>
+			<li><a href="/finance-and-grants/finance-options/borrowing/financing-from-friends-and-family.php">Financing from friends and family</a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" title="Learn about the different types of business and legal structures to help you choose the right one for your new business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" title="Tutorials, videos and tools to help you decide how to become self-employed - from sole trader to limited company, franchisee to social enterprise and more" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+			<li><a href="https://www.gov.uk/set-up-business" title="Case studies and tutorials to help you understand the legal dos and don'ts of naming your business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php">Set up a social enterprise</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
