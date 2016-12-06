c9cc8dcdb5b21e7a985a3802fe263a25f808ab88
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-06 16:44:18 +0000

    Completed ‘Access to sources of starting-up finance’ page.

---
+++ starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/access-to-sources-of-starting-up-finance.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Access to sources of starting-up finance | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business when economic conditions are tough</h1>
+		<h2>Access to sources of starting-up finance</h2>
+		<p>If you need to raise money to start up your business, there are various options, including:</p>
+		<p>
+			<ul>
+				<li><strong>Using personal money</strong> - such as savings, redundancy payments or a pension windfall.</li>
+				<li><strong>Approaching a bank or business investor for a business loan</strong> - this may be more difficult during a credit crunch. Any lender will want to review a detailed business plan before loaning any funds. See our guide on how to <a href="/finance-and-grants/prepare-to-raise-finance/how-to-attract-investment/use-your-business-plan-to-get-funding.php">use your business plan to get funding</a>.</li>
+				<li><strong>Applying for a grant</strong> - there are many business grant schemes nationwide, geared to different industry sectors and project scales. You should be aware that applying for a grant may take some time. <a href="https://www.gov.uk/growing-your-business" target="_blank" rel="noopener noreferrer">Search our business support finder for grants, loans, expertise and advice for which your business may be eligible</a>.</li>
+			</ul>
+		</p>
+		<p>Essential to raising finance is convincing investors or grant bodies that your business is sound. This means showing that your idea is worth investing in and that you have what it takes to run your own business successfully.</p>
+		<h3>Top five tips on getting finance</h3>
+		<ul>
+			<li>Make sure you have a realistic business proposal. Have you calculated the money needed to start up?</li>
+			<li>Think about and plan your ongoing budget</li>
+			<li>Start to plan your finances by thinking about costs, living expenses and sales targets</li>
+			<li>Make time to do your market research. It helps to show you are serious about your business</li>
+			<li>Be prepared for probing questions</li>
+		</ul>
+		<h3>Loans and grants</h3>
+		<p>Banks are not the only option when it comes to business loans. You could also explore the following:</p>
+		<ul>
+			<li><strong>Angel investors</strong>, or business angels, usually provide funds in exchange for a small stake. They typically invest between &pound;10,000 and &pound;750,000 and are often just as valuable for their contacts, insight and experience as for the investment they can offer.</li>
+			<li><strong>Venture capitalists</strong> may demand a large percentage of your business in exchange for investment. Ensure you have a clear understanding of their expectations.</li>
+		</ul>
+		<p>Find out more about angel investors and venture capitalists in our guide on <a href="/finance-and-grants/finance-options/shares-and-equity-finance/equity-finance.php">equity finance</a>.</p>
+		<p>The <strong>Enterprise Finance Guarantee</strong> aims to help smaller credit-worthy companies that are unable to obtain a conventional loan because of current tight lending conditions. <a href="https://www.gov.uk/government/policies/business-enterprise?keywords=%22finance+guarantee%22&public_timestamp%5Bfrom%5D=&public_timestamp%5Bto%5D=" target="_blank" rel="noopener noreferrer">Find out more about the Enterprise Finance Guarantee on the Department for Business, Innovation &amp; Skills (BIS) website</a>.</p>
+		<p>See our guide on <a href="/finance-and-grants/finance-options/borrowing/bank-finance.php">bank finance</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/your-business-structure-and-business-plan.php" title="Your business structure and business plan">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/develop-an-effective-survival-strategy.php" title="Develop an effective survival strategy">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough.php" title="Starting a business when economic conditions are tough">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/work-on-your-business-idea.php">Work on your business idea</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/your-business-structure-and-business-plan.php">Your business structure and business plan</a></li>
+			<li>Access to sources of starting-up finance</li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/develop-an-effective-survival-strategy.php">Develop an effective survival strategy</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/minimise-costs-and-maximise-efficiency.php">Minimise costs and maximise efficiency</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/sources-and-contacts-to-help-you-start-a-business.php">Sources and contacts to help you start a business</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/here-is-how-I-started-a-business-during-an-economic-downturn_flash-video.php">Here's how I started a business during an economic downturn (Flash video)</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.bis.gov.uk/policies/enterprise-and-business-support/access-to-finance/enterprise-finance-guarantee" target="_blank" rel="noopener noreferrer">Enterprise Finance Guarantee information on the BIS website </a></li>
+			<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance options for your business</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-finance-explained" target="_blank" rel="noopener noreferrer">Think about the money</a></li>
+			<li><a href="https://www.gov.uk/business-finance-explained" target="_blank" rel="noopener noreferrer">Understand how much money you need to start up</a></li>
+			<li><a href="https://www.gov.uk/business-finance-explained" target="_blank" rel="noopener noreferrer">Explore your finance options when starting up</a></li>
+			<li><a href="/finance-and-grants/prepare-to-raise-finance/how-to-attract-investment/use-your-business-plan-to-get-funding.php">Use your business plan to get funding</a></li>
+			<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/choose-the-right-finance-when-starting-up.php">Choose the right finance when starting up</a></li>
+			<li><a href="/finance-and-grants/finance-options/borrowing/bank-finance.php">Bank finance</a></li>
+			<li><a href="/finance-and-grants/finance-options/grants-and-government-support/government-support-for-businesses.php">Government support for businesses</a></li>
+			<li><a href="/finance-and-grants/finance-options/grants-and-government-support/grants-the-basics.php">Grants: the basics</a></li>
+			<li><a href="/finance-and-grants/finance-options/shares-and-equity-finance/equity-finance.php">Equity finance</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
