2b05564fff8154786adddf9e75a7a11cb19e2b65
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-06 16:49:35 +0000

    Completed ‘Minimise costs and maximise efficiency’ page.

---
+++ starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/minimise-costs-and-maximise-efficiency.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Minimise costs and maximise efficiency | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business when economic conditions are tough</h1>
+		<h2>Minimise costs and maximise efficiency</h2>
+		<p>When starting up a business at any time, it is important to ensure that your business is lean and efficient, but this is particularly the case when wider economic conditions are difficult.</p>
+		<h3>Minimising costs</h3>
+		<p>Even this early on in your business, you may be able to reduce your business costs, just by considering whether:</p>
+		<ul>
+			<li>any element of your business product or service can be removed while still allowing the product or service to meet its requirements</li>
+			<li>there are cost-effective alternatives to high-cost elements</li>
+		</ul>
+		<p>You may be able to make savings on your <strong>fixed costs</strong> (often called overheads), which you pay for regardless of how much you produce or sell. They include rent, rates and wages.</p>
+		<p>You may be able to make savings on your <strong>variable</strong> costs, which are linked to how much you produce or sell. Variable costs include materials, packaging, overtime and transport costs.</p>
+		<h3>Choosing and managing your suppliers</h3>
+		<p>Different suppliers can vary greatly in terms of value for money, reliability and quality. Be prepared to shop around until you find the best supplier to meet your specific needs. See our guide on <a href="/grow-your-business/growth-through-strategic-sourcing/choosing-the-right-suppliers.php">choosing the right suppliers</a>.</p>
+		<p>To minimise your costs, relationships with suppliers should be managed effectively and their performance assessed regularly. Become a valuable customer by being reliable in placing orders and paying on time. Consider drawing up a contract or service level agreement to ensure that you receive good service from them. Read our guide on how to <a href="/grow-your-business/growth-through-strategic-sourcing/manage-your-suppliers.php">manage your suppliers</a>.</p>
+		<p>Your bank is also a supplier so try to minimise your banking costs and make sure you get the cheapest form of credit available. See our guide on <a href="/finance-and-grants/business-banking/how-to-choose-and-manage-a-business-bank-account.php">how to choose and manage a business bank account</a>.</p>
+		<p>Maintain a good relationship with your bank or other lender. Always try to be personally involved in dealings between your business and the lender.</p>
+		<h3>The personal approach</h3>
+		<p>Take a personal approach to every aspect of your business start-up, to create a good rapport with the suppliers and customers who will be instrumental in helping your business succeed.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/develop-an-effective-survival-strategy.php" title="Develop an effective survival strategy">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/sources-and-contacts-to-help-you-start-a-business.php" title="Sources and contacts to help you start a business">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough.php" title="Starting a business when economic conditions are tough">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/work-on-your-business-idea.php">Work on your business idea</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/your-business-structure-and-business-plan.php">Your business structure and business plan</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/access-to-sources-of-starting-up-finance.php">Access to sources of starting-up finance</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/develop-an-effective-survival-strategy.php">Develop an effective survival strategy</a></li>
+			<li>Minimise costs and maximise efficiency</li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/sources-and-contacts-to-help-you-start-a-business.php">Sources and contacts to help you start a business</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/here-is-how-I-started-a-business-during-an-economic-downturn_flash-video.php">Here's how I started a business during an economic downturn (Flash video)</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/grow-your-business/growth-through-strategic-sourcing/choosing-the-right-suppliers.php">Choosing the right suppliers</a></li>
+			<li><a href="/grow-your-business/growth-through-strategic-sourcing/negotiate-the-right-deal-with-suppliers.php">Negotiate the right deal with suppliers</a></li>
+			<li><a href="/grow-your-business/growth-through-strategic-sourcing/manage-your-suppliers.php">Manage your suppliers</a></li>
+			<li><a href="/finance-and-grants/business-banking/how-to-choose-and-manage-a-business-bank-account.php">How to choose and manage a business bank account</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
