8996505e7800b83af99aa442b029d4520fa041f2
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-06 17:01:37 +0000

    Completed ‘Sources and contacts to help you start a business’ page.

---
+++ starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/sources-and-contacts-to-help-you-start-a-business.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sources and contacts to help you start a business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business when economic conditions are tough</h1>
+		<h2>Sources and contacts to help you start a business</h2>
+		<p>There is a wide range of government support available to businesses, not only through grants and other funding but also through numerous advisory, guidance, information and other services, including training. See our guide on <a href="/finance-and-grants/finance-options/grants-and-government-support/government-support-for-businesses.php">government support for businesses</a>.</p>
+		<p>Grants and loans are available to some businesses. <a href="https://www.gov.uk/growing-your-business" target="_blank" rel="noopener noreferrer">Search our business support finder for grants, loans, expertise and advice for which your business may be eligible</a>.</p>
+		<p>There are different organisations which specialise in offering advice and/or financial help to different types of entrepreneur, for example:</p>
+		<p>If you are over the age of 50, Prime offers you free information, advice and training to help you start up a business. You can <a href="#">find practical advice on starting a business on the Prime website</a>.</p>
+		<p>If you are starting up a co-operative, employee-owned business or social enterprise, Industrial Common Ownership Finance (ICOF) can offer loans of between &pound;5,000 and &pound;50,000. You can <a href="https://coopfinance.coop/borrow/" target="_blank" rel="noopener noreferrer">find out if you are eligible for a loan from ICOF from the ICOF website</a>.</p>
+		<p>If you are disabled and starting a business, the Access to Work scheme provides grants to employers towards any extra employment costs resulting from an employee having a disability. <a href="https://www.gov.uk/access-to-work" target="_blank" rel="noopener noreferrer">Find out about Access to Work on the Jobcentre Plus website</a>.</p>
+		<p>If you are starting up a minority ethnic business - the Asian Business Development Network (ABDN) is a network of entrepreneurs from across minority ethnic communities. It aims to encourage ethnic minority entrepreneurship. You can <a href="https://www.nabauk.org/abdn/" target="_blank" rel="noopener noreferrer">find support for starting up a minority ethnic business from the ABDN</a>.</p>
+		<p>If you are aged between 16 and 30 - Shell LiveWIRE provides advice and practical support in starting up your business. <a href="http://www.shell-livewire.org/" target="_blank" rel="noopener noreferrer">Find guidance on starting up a business on the Shell LiveWIRE website</a>.</p>
+		<p></p>
+		<p>If you are aged between 18 and 30 and unemployed - the Prince's Trust could offer a loan of up to &pound;5,000. It also offers practical support, such as personal development courses, work experience, skills and qualifications. <a href="https://www.princes-trust.org.uk/" target="_blank" rel="noopener noreferrer">Find out how to apply for a Prince's Trust loan on the Prince's Trust website</a>.</p>
+		<p>If you are a woman - Prowess is an association of organisations that specifically support women in business. <a href="http://www.prowess.org.uk/directory" target="_blank" rel="noopener noreferrer">Find support for women in business on the Prowess website</a>.</p>
+		<p>If you need to improve your business skills before or after starting a business, you can apply for a Professional and Career Development Loan. <a href="https://www.gov.uk/career-development-loans/overview" target="_blank" rel="noopener noreferrer">Find out about Professional and Career Development Loans on the Directgov website</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/minimise-costs-and-maximise-efficiency.php" title="Minimise costs and maximise efficiency">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/here-is-how-I-started-a-business-during-an-economic-downturn_flash-video.php" title="Here's how I started a business during an economic downturn (Flash video)">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough.php" title="Starting a business when economic conditions are tough">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/work-on-your-business-idea.php">Work on your business idea</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/your-business-structure-and-business-plan.php">Your business structure and business plan</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/access-to-sources-of-starting-up-finance.php">Access to sources of starting-up finance</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/develop-an-effective-survival-strategy.php">Develop an effective survival strategy</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/minimise-costs-and-maximise-efficiency.php">Minimise costs and maximise efficiency</a></li>
+			<li>Sources and contacts to help you start a business</li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/here-is-how-I-started-a-business-during-an-economic-downturn_flash-video.php">Here's how I started a business during an economic downturn (Flash video)</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101805&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Find out about business mentoring</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101974&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Get tips on how to network</a></li>
+			<li><a href="https://www.gov.uk/business-finance-support-finder" target="_blank" rel="noopener noreferrer">Find the right support for your new business</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
