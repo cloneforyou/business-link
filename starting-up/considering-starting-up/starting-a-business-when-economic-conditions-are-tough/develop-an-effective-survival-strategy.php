a9fc5e3f37029d52387cd8bbe9dca702dc418558
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-06 16:44:35 +0000

    Completed ‘Develop an effective survival strategy’ page.

---
+++ starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/develop-an-effective-survival-strategy.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Develop an effective survival strategy | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business when economic conditions are tough</h1>
+		<h2>Develop an effective survival strategy</h2>
+		<p>It's never too early to identify risks and learn how to address and manage them.</p>
+		<h3>Risk management</h3>
+		<p>It is important to identify the key risks your business may face and develop strategies to eliminate or reduce them. This will make your business more flexible and more able to withstand volatile market conditions.</p>
+		<p>Common risks include:</p>
+		<ul>
+			<li>losing customers and failing to attract new ones</li>
+			<li>increased competition</li>
+			<li>poor cashflow</li>
+			<li>failure to anticipate problems/inability to adapt to changing market environment</li>
+		</ul>
+		<p>For more information, see our guide on <a href="/grow-your-business/managing-risk.php">managing risk</a>.</p>
+		<h3>Customers and competitors</h3>
+		<p>You must have a clear strategy for identifying and looking after key customers and for developing relationships with them. You should try to plan for worst-case scenarios, such as losing a major customer, and expand your customer base as quickly as possible to reduce the impact such a loss would have on your business.</p>
+		<p>You also need to consider potential opportunities that could arise, for example, if one of your competitors ceases trading.</p>
+		<p>Read our guides on how to <a href="/sales-and-marketing/keeping-your-customers/manage-your-customer-care.php">manage your customer care</a> and <a href="/sales-and-marketing/keeping-your-customers/retain-and-grow-your-customer-base.php">retain and grow your customer base</a>.</p>
+		<h3>Keep your cashflow healthy</h3>
+		<p>Cashflow is the balance of money entering and leaving a business. It is important to anticipate cashflow problems as early as possible, so that solutions can be found before the problem becomes too serious.</p>
+		<p>At all times you should know how much money your business has in the bank, how much it owes and how much it is owed. If you regularly update your financial records and develop forecasts showing likely sales, profit and loss, you should be able to identify when additional funds might be required.</p>
+		<p><a href="http://www.cicm.com/resources/cashflow-guides/" target="_blank" rel="noopener noreferrer">Find out about good cashflow management on the Department for Business, Innovation and Skills (BIS) website</a>.</p>
+		<p>If you anticipate serious cashflow or funding problems in the early stages of your business, seek advice from your accountant or your bank.</p>
+		<p>For more advice, read our guides: <a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">cashflow management: the basics</a> and <a href="/finance-and-grants/manage-your-cashflow/identify-potential-cashflow-problems.php">identify potential cashflow problems</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/access-to-sources-of-starting-up-finance.php" title="Access to sources of starting-up finance">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/minimise-costs-and-maximise-efficiency.php" title="Minimise costs and maximise efficiency">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough.php" title="Starting a business when economic conditions are tough">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/work-on-your-business-idea.php">Work on your business idea</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/your-business-structure-and-business-plan.php">Your business structure and business plan</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/access-to-sources-of-starting-up-finance.php">Access to sources of starting-up finance</a></li>
+			<li>Develop an effective survival strategy</li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/minimise-costs-and-maximise-efficiency.php">Minimise costs and maximise efficiency</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/sources-and-contacts-to-help-you-start-a-business.php">Sources and contacts to help you start a business</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/here-is-how-I-started-a-business-during-an-economic-downturn_flash-video.php">Here's how I started a business during an economic downturn (Flash video)</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.cicm.com/resources/cashflow-guides/" target="_blank" rel="noopener noreferrer">Cashflow management information on the BIS website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-finance-explained" target="_blank" rel="noopener noreferrer">Explore your finance options when starting up</a></li>
+			<li><a href="https://www.gov.uk/avoid-business-cashflow-problems" target="_blank" rel="noopener noreferrer">Meet your financial commitments</a></li>
+			<li><a href="/grow-your-business/managing-risk.php">Managing risk</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">Cashflow management: the basics</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/identify-potential-cashflow-problems.php">Identify potential cashflow problems</a></li>
+			<li><a href="/sales-and-marketing/keeping-your-customers/retain-and-grow-your-customer-base.php">Retain and grow your customer base</a></li>
+			<li><a href="/sales-and-marketing/marketing/marketing-on-a-tight-budget.php">Marketing on a tight budget</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
