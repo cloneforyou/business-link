fce8a5b977a2112434a9a9a31fe7d8438df914f1
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-06 16:21:03 +0000

    Completed ‘Your business structure and business plan’ page.

---
+++ starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/your-business-structure-and-business-plan.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Your business structure and business plan | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business when economic conditions are tough</h1>
+		<h2>Your business structure and business plan</h2>
+		<p>You need to decide what legal structure will best suit your business. Carefully consider the advantages and disadvantages of each option because the structure will affect:</p>
+		<ul>
+			<li>the tax and National Insurance you have to pay</li>
+			<li>the records and accounts you have to keep</li>
+			<li>the way management decisions are made</li>
+		</ul>
+		<p>Some of the options are:</p>
+		<ul>
+			<li>Sole trader - The simplest way to run a business. Accounts are straightforward and profits belong to you, however you are also personally liable for any debts.</li>
+			<li>Limited liability company - Including private limited companies. You must comply with strict regulations, but can raise money through shareholders. The company's finances are separate from the personal finances of their owners.</li>
+			<li>Partnership - Two or more people share the responsibilities of running the business, and take equal share of both profits and debts.</li>
+			<li>Limited liability partnership - Similar to a partnership but liability is limited to what each partner has invested and any personal guarantees they have given.</li>
+		</ul>
+		<p>Other company formats include franchises, social enterprises and co-operatives.</p>
+		<p>For guidance on deciding which structure would best suit your business, see our videos and e-learning resources to <a href="https://www.gov.uk/business-legal-structures" target="_blank" rel="noopener noreferrer">choose the right business model and legal structure</a>.</p>
+		<h3>Creating a business plan</h3>
+		<p>A business plan describes the business' objectives, its strategies, the market it is in and its financial forecasts.</p>
+		<p>Going through the process of writing the plan should focus your mind on everything from where the business will be based to where you are going to find your first customer.</p>
+		<p>It is also a blueprint for the future development of the business that potential investors will want to read before they lend to you.</p>
+		<p>Your business plan should include:</p>
+		<ul>
+			<li>an executive summary</li>
+			<li>a description of the business</li>
+			<li>a marketing and sales strategy - who you think your customers are going to be and how you are going to reach them</li>
+			<li>information on your management team and personnel</li>
+			<li>information on your operations, including your business premises, your IT and management information systems</li>
+			<li>financial forecasts - how much you need to start the business and where you are going to get this money from, how much you think the business is going to make and what salary you plan to take, etc</li>
+		</ul>
+		<p>See our videos and e-learning resources to <a href="https://www.gov.uk/write-business-plan" target="_blank" rel="noopener noreferrer">create your business plan</a>.</p>
+		<p>After the business plan, you should consider writing a marketing plan, which sets out how you will target and interest potential customers in your product or service. See our guide on <a href="/sales-and-marketing/marketing/write-a-marketing-plan.php" title="How to identify your objectives and write a plan that will help your marketing generate sales">how to write a marketing plan</a>.</p>
+		<p>Business support organisations can help with setting out your marketing and business plans and you may also be able to attend start-up events or courses which cover writing a marketing plan as part of their content.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/work-on-your-business-idea.php" title="Work on your business idea">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/access-to-sources-of-starting-up-finance.php" title="Access to sources of starting-up finance">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough.php" title="Starting a business when economic conditions are tough">Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/work-on-your-business-idea.php">Work on your business idea</a></li>
+			<li>Your business structure and business plan</li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/access-to-sources-of-starting-up-finance.php">Access to sources of starting-up finance</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/develop-an-effective-survival-strategy.php">Develop an effective survival strategy</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/minimise-costs-and-maximise-efficiency.php">Minimise costs and maximise efficiency</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/sources-and-contacts-to-help-you-start-a-business.php">Sources and contacts to help you start a business</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/here-is-how-I-started-a-business-during-an-economic-downturn_flash-video.php">Here's how I started a business during an economic downturn (Flash video)</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-legal-structures" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+			<li><a href="https://www.gov.uk/business-finance-support-finder" target="_blank" rel="noopener noreferrer">Understand the importance of business planning</a></li>
+			<li><a href="https://www.gov.uk/write-business-plan" target="_blank" rel="noopener noreferrer">Create your business plan</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php">Prepare a business plan</a></li>
+			<li><a href="/sales-and-marketing/marketing/write-a-marketing-plan.php">Write a marketing plan</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
