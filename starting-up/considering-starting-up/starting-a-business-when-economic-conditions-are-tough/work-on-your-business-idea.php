a8fe218e95afd22d5ef8b9e2e575b20b6595e255
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-06 16:15:15 +0000

    Completed ‘Work on your business idea’ page.

---
+++ starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/work-on-your-business-idea.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Work on your business idea | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business when economic conditions are tough</h1>
+		<h2>Work on your business idea</h2>
+		<p>A good business idea is essential to setting up a successful business. You can use what you know about the volatile financial climate to help you develop a targeted and specific business idea.</p>
+		<h3>Your business idea</h3>
+		<p>If you don't already have an idea, consider which of your skills could be used as a base for starting a business. See our videos and e-learning resources to <a href="https://www.gov.uk/set-up-business" title="Find out about researching your chosen market and how to generate or develop a new business idea" target="_blank" rel="noopener noreferrer">develop your business idea</a>.</p>
+		<p>Some types of business are more likely to succeed in a volatile financial climate than others. If you want to start up a business but you haven't decided which area you'd like to work in, find out what sorts of business are thriving, and what about them makes them 'recession-resistant'. You could also look into:</p>
+		<ul>
+			<li>the 'essentials' markets - eg food and house maintenance</li>
+			<li>the affordable luxuries market - in an economic downturn, the market aimed at people aged over 50 may still be growing, as they may have equity in their property and savings</li>
+			<li>existing businesses - not only is buying a business which already has established revenue a safer option, but you might get a bargain in difficult economic times</li>
+			<li>franchises - an established franchise stands a good chance of surviving when economic conditions are tough as they have experience and time-honed, streamlined systems and processes</li>
+		</ul>
+		<p>Once you have a business idea, you should research whether there is a market for your product or service and formulate a business plan. If you need start-up capital, lenders will usually request a business plan.</p>
+		<h3>Test your business idea</h3>
+		<p>You can use the economic downturn as an opportunity to define what you and your business want to achieve - and how best to set about achieving this. Ensure that the customer is central to your business idea. Consider whether they would really buy your product or service, particularly in the context of the current market.</p>
+		<p>Difficult market conditions are often caused by very specific issues, such as an unavailability of loan finance. Diagnosing these issues and seeing to what extent they affect you will allow you to rethink the nature and scale of, as well as the market for, your business.</p>
+		<p>It is worth looking at the business pages of newspapers for information about the issues involved in an economic downturn and their effect on businesses. Understanding these issues will help you develop strategies for coping with them.</p>
+		<p>Once you have tweaked your business idea for current market conditions, it needs to be implemented through a full business plan and a decision on the legal structure of your business.</p>
+		
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough.php" title="Starting a business when economic conditions are tough">Prev</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/your-business-structure-and-business-plan.php" title="Your business structure and business plan">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough.php" title="Starting a business when economic conditions are tough">Introduction</a></li>
+			<li>Work on your business idea</li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/your-business-structure-and-business-plan.php">Your business structure and business plan</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/access-to-sources-of-starting-up-finance.php">Access to sources of starting-up finance</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/develop-an-effective-survival-strategy.php">Develop an effective survival strategy</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/minimise-costs-and-maximise-efficiency.php">Minimise costs and maximise efficiency</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/sources-and-contacts-to-help-you-start-a-business.php">Sources and contacts to help you start a business</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/here-is-how-I-started-a-business-during-an-economic-downturn_flash-video.php">Here's how I started a business during an economic downturn (Flash video)</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.bl.uk/business-and-ip-centre" target="_blank" rel="noopener noreferrer">Idea development information on the British Library Business &amp; IP Centre website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/set-up-business" title="Find out about researching your chosen market and how to generate or develop a new business idea" target="_blank" rel="noopener noreferrer">Develop your business idea</a></li>
+			<li><a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">Research and develop your business ideas, new products and services</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
