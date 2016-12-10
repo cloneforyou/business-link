3a5ada73b029f5fa07b24c948f117bb7ddc43370
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-10 18:05:14 +0000

    Completed ‘Your markets and competitors’ page.

---
+++ starting-up/business-planning/prepare-a-business-plan/your-markets-and-competitors.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Your markets and competitors | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Prepare a business plan</h1>
+		<h2>Your markets and competitors</h2>
+		<p>In this section you should define your market, your position in it and outline who your competitors are. In order to do this you should refer to any market research you have carried out. You need to demonstrate that you're fully aware of the marketplace you're planning to operate in and that you understand any important trends and drivers.</p>
+		<p>You should also be able to show that your business will be able to attract customers in a growing market despite the competition.</p>
+		<h3>Key areas to cover include:</h3>
+		<ul>
+			<li><strong>your market</strong> - its size, historical data about its development and key current issues</li>
+			<li><strong>your target customer base</strong> - who they are and how you know they will be interested in your products or services</li>
+			<li><strong>your competitors</strong> - who they are, how they work and the share of the market they hold</li>
+			<li><strong>the future</strong> - anticipated changes in the market and how you expect your business and your competitors to react to them</li>
+		</ul>
+		<p>For further information, see our guides on <a href="/sales-and-marketing/marketing/market-research-and-market-reports.php">market research and market reports</a> and how to <a href="/sales-and-marketing/marketing/understand-your-competitors.php">understand your competitors</a>.</p>
+		<p>It is important to know your competitors' strengths and weaknesses as compared to your own - and it is good practice to do a competitor analysis of each one. Remember that the market is not static - your customers' needs and your competitors can change. So, as well as showing the competitor analyses you have undertaken, you should also demonstrate that you have considered and drawn up contingency plans to cover alternative scenarios.</p>
+		<p><a href="../../../_files/how-to-get-to-grips-with-your-competitors.pdf" target="_blank" rel="noopener noreferrer">Download a guide on getting to grips with your competitors from the Chartered Institute of Marketing website (PDF, 159K)</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-business-its-products-and-services.php" title="Your business, its products and services">Prev</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/marketing-and-sales.php" title="Marketing and sales">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php" title="Prepare a business plan">Introduction</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-audience-for-your-business-plan.php">The audience for your business plan</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/what-a-business-plan-should-include.php">What a business plan should include</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-executive-summary.php">The executive summary</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-business-its-products-and-services.php">Your business, its products and services</a></li>
+			<li>Your markets and competitors</li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/marketing-and-sales.php">Marketing and sales</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-teams-skills.php">Your team's skills</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-operations.php">Your operations</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/financial-forecasts.php">Financial forecasts</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/presenting-your-business-plan.php">Presenting your business plan</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/using-the-business-plan-template.php">Using the business plan template</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/here-is-how-having-an-up-to-date-business-plan-helped-my-business.php">Here's how having an up-to-date business plan helped my business</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		
+		<p>0845 600 9 006</p>
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="../../../_files/how-to-get-to-grips-with-your-competitors.pdf" target="_blank" rel="noopener noreferrer">Download competitor information gathering advice from the Chartered Institute of Marketing website (PDF, 159K)</a></li>
+			<li><a href="../../_files/business-plan-template.doc" target="_blank" rel="noopener noreferrer">Download our business plan template (DOC, 458K)</a></li>
+			<li><a href="../../_files/business-plan-template.odt" target="_blank" rel="noopener noreferrer">Download our business plan template in ODT format (ODT, 33K)</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports.php">Market research and market reports</a></li>
+			<li><a href="/sales-and-marketing/marketing/understand-your-competitors.php">Understand your competitors</a></li>
+			<li><a href="/sales-and-marketing/marketing/write-a-marketing-plan.php">Write a marketing plan</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them.php">Common mistakes when starting up - and how to avoid them</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/overseas-supplier-relationships/entering-overseas-markets.php">Entering overseas markets</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough.php">Starting a business when economic conditions are tough</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
