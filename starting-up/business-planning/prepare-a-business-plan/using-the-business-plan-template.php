79b2dc3dc7885afd18e2b6dd4aa460162ff80a2c
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-10 18:37:44 +0000

    Completed ‘Using the business plan template’ page.

---
+++ starting-up/business-planning/prepare-a-business-plan/using-the-business-plan-template.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Using the business plan template | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Prepare a business plan</h1>
+		<h2>Using the business plan template</h2>
+		<p>The business plan document is an interactive form designed to simplify the process of creating your business plan.</p>
+		<p>The document is a Microsoft Word template and is also available to download as an OpenDocument file in ODT format.</p>
+		<p>Bullet points have been used in the document to help you structure your thoughts into clear and concise messages. The fields will expand as you type into them, so there is plenty of space for everything you need to say.</p>
+		<p>The template should be read in conjunction with the guide 'Prepare a business plan'. It is recommended that you print off both the guide and the template and read both before completing the template.</p>
+		<p>The template asks a number of detailed questions about the proposed business - its markets, costs, etc. They may demand research prior to completing it. The time spend is an investment which will pay dividends when you get the business started as well as helping to convince potential finance providers that you are in control of the business.</p>
+		<p>Here are the step-by-step instructions for using the business plan template:</p>
+		<ol>
+			<li>To start work on your business plan, download our business plan template below to create a new business plan document for you to complete.</li>
+			<li>Save the document - give it a memorable name and store it in a suitable and secure location. Remember to also save the document regularly as you work on it.</li>
+			<li>The template uses protected forms. The cursor will only point to and allow changes to be made in the 'field' boxes. You cannot make changes to any other part of the document.</li>
+			<li>The best way to move through the document is to use the cursor keys. To navigate using the mouse pointer, simply click into each field. The best place to click will normally be near the left margin of the field. If the cursor jumps to the cell below, you can use the cursor keys to navigate back to the field you want to complete.</li>
+			<li>You can change the headings of any of the sections to suit your needs. If you do this, the name will also need to be changed within the contents page at the start of the document.</li>
+			<li>This document has a number of tables that are used to simplify the calculations you'll need to do when you start a business. To start entering data into a table, double-click on the table. This will activate it and allow you to click into individual cells. Financial and descriptive information can then be entered in the cells provided. Each table contains an automated formula that will do the maths on your behalf, calculating the summaries and totals you need to understand your business model.</li>
+			<li>Once you have completed all the information, click anywhere out of the table to return to the rest of the document. If you have accidentally moved around the Excel document, you may see that the wrong bit of the table is now showing on the screen and will therefore be printed. To avoid this, always make sure that the appropriate bit of the table can be seen before clicking out.</li>
+		</ol>
+		<p>You can close or print the document at any time in the normal way. To re-open it, open Microsoft Word, click file then open and navigate to its location in the normal way.</p>
+		<p>This template is intended for business planning purposes only. If the financial tables are to be used for any other purpose other than cashflow management, then we strongly recommend that you consult an accountant or tax advisor.</p>
+		<p>Please note that this business plan template may not necessarily follow the same format as one a bank may ask you to complete, nevertheless much of the content required will be similar.</p>
+		<p><a href="../../_files/business-plan-template.doc" target="_blank" rel="noopener noreferrer">Download our business plan template (DOC, 874K)</a>.</p>
+		<p><a href="../../_files/business-plan-template.odt" target="_blank" rel="noopener noreferrer">Download our business plan template in ODT format (ODT, 108K)</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/presenting-your-business-plan.php" title="Presenting your business plan">Prev</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/here-is-how-having-an-up-to-date-business-plan-helped-my-business.php" title="Here's how having an up-to-date business plan helped my business">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php" title="Prepare a business plan">Introduction</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-audience-for-your-business-plan.php">The audience for your business plan</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/what-a-business-plan-should-include.php">What a business plan should include</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-executive-summary.php">The executive summary</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-business-its-products-and-services.php">Your business, its products and services</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-markets-and-competitors.php">Your markets and competitors</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/marketing-and-sales.php">Marketing and sales</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-teams-skills.php">Your team's skills</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-operations.php">Your operations</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/financial-forecasts.php">Financial forecasts</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/presenting-your-business-plan.php">Presenting your business plan</a></li>
+			<li>Using the business plan template</li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/here-is-how-having-an-up-to-date-business-plan-helped-my-business.php">Here's how having an up-to-date business plan helped my business</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="../../_files/business-plan-template.doc" target="_blank" rel="noopener noreferrer">Download our business plan template (DOC, 458K)</a></li>
+			<li><a href="../../_files/business-plan-template.odt" target="_blank" rel="noopener noreferrer">Download our business plan template in ODT format (ODT, 33K)</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-finance-support-finder" title="How completing a business plan can help you improve your business' chance of success" target="_blank" rel="noopener noreferrer">Understand the importance of business planning</a></li>
+			<li><a href="https://www.gov.uk/write-business-plan" title="What you need to include in your business plan and how to create one step-by-step" target="_blank" rel="noopener noreferrer">Create your business plan</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
