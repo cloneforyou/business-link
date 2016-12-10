3e51dfaaab9fd5efc6da5de8a5b8e5ccd2185d96
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-10 16:23:11 +0000

    Completed ‘Here's how I started a business during an economic downturn (Flash video)’ page.

---
+++ starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/here-is-how-I-started-a-business-during-an-economic-downturn_flash-video.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Here's how I started a business during an economic downturn (Flash video) | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Here's how I started a business during an economic downturn (Flash video)</h1>
+		<h2>Here's how I started a business during an economic downturn (Flash video)</h2>
+		<p>Siggi sold his first collection of hats to Harvey Nichols in 1982 and was soon supplying boutiques and department stores with hats on a wholesale basis. In 2000, he opened a shop in West London to develop the retail side of his business. Siggi Hats has remained a small but flexible business - Siggi sees flexibility as key to survival during an economic downturn.</p>
+
+		<!-- Start of Brightcove Player -->
+
+		<!--
+		By use of this code snippet, I agree to the Brightcove Publisher T and C 
+		found at https://accounts.brightcove.com/en/terms-and-conditions/. 
+		-->
+
+		<script language="JavaScript" type="text/javascript" src="https://sadmin.brightcove.com/js/BrightcoveExperiences.js"></script>
+
+		<object id="myExperience_carousel" class="BrightcoveExperience">
+		  <param name="bgcolor" value="#FFFFFF" />
+		  <param name="width" value="700" />
+		  <param name="height" value="360" />
+		  <param name="playerID" value="1275380667001" />
+		  <param name="playerKey" value="AQ~~,AAABAlA0wUk~,LXBzxm1_r0Kf3j3Ls5uMOnpvYHDDvqaz" />
+		  <param name="isVid" value="true" />
+		  <param name="isUI" value="true" />
+		  <param name="dynamicStreaming" value="true" />
+		  
+		</object>
+
+		<!-- 
+		This script tag will cause the Brightcove Players defined above it to be created as soon
+		as the line is read by the browser. If you wish to have the player instantiated only after
+		the rest of the HTML is processed and the page load is complete, remove the line.
+		-->
+		<script type="text/javascript">brightcove.createExperiences();</script>
+
+		<!-- End of Brightcove Player -->
+
+		<ul>
+			<li>
+				<a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/openpopup?itemId=1081930044&amp;type=ONEOFFPAGE" target="_blank" rel="noopener noreferrer">Read transcript</a>
+			</li>
+			<li>
+				<a title="Play the audio only version of this video" href="../../../_files/start_up_in_economic_downturn.mp3" target="_blank" rel="noopener noreferrer">Listen to audio only (MP3, 9MB)</a>
+			</li>
+			<li>
+				<a href="/video-and-e-learning-help.php">Help</a>
+			</li>
+		</ul>
+
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/sources-and-contacts-to-help-you-start-a-business.php" title="Sources and contacts to help you start a business">Prev</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough.php" title="Starting a business when economic conditions are tough" >Introduction</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/work-on-your-business-idea.php">Work on your business idea</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/your-business-structure-and-business-plan.php">Your business structure and business plan</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/access-to-sources-of-starting-up-finance.php">Access to sources of starting-up finance</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/develop-an-effective-survival-strategy.php">Develop an effective survival strategy</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/minimise-costs-and-maximise-efficiency.php">Minimise costs and maximise efficiency</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough/sources-and-contacts-to-help-you-start-a-business.php">Sources and contacts to help you start a business</a></li>
+			<li>Here's how I started a business during an economic downturn (Flash video)</li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<div class="media-object stack-for-small">
+			<div class="media-object-section">
+				<div class="thumbnail">
+					<img src="../../../_img/siggi.jpg" width="181" height="121" border="0" alt="" />
+				</div>
+			</div>
+			<div class="media-object-section">
+				<h3>
+					Siggi Hesbacher<br />
+					<a href="http://www.siggihats.co.uk" target="_blank" rel="noopener noreferrer">Siggi Hats</a><br />
+					Siggi's top tips:
+				</h3>
+				<ul>
+					<li>"Work out your business plan and budgets carefully - you need to be sure to get through the starting up period."</li>
+					<li>"Be certain about your target market. Do research as to who your customers are going to be and find out who your competitors are. See what you can do differently and better."</li>
+					<li>"Have confidence in your product or services."</li>
+				</ul>
+			</div>
+		</div>
+		
+		<p><strong><a href="/find-a-case-study.php" title="">Find a case study</a></strong></p>
+
+		<img src="../../../_img/px_startingabusiness.gif" width="181" height="5" alt="" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
