eceee86848bdfdbcb95c9efbc509550a5e6263c4
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-10 22:12:06 +0000

    Completed ‘Workplace health & safety’ page.

---
+++ workplace-health-and-safety.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Workplace health &amp; safety | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+			
+			<h1>Workplace health &amp; safety</h1>
+			<ul>
+				<li><a href="http://shop.bsigroup.com/BusinessLinks/BusinessLinkLP/" target="_blank" rel="noopener noreferrer">Standards explained on the British Standards Institution website</a></li>
+			</ul>
+			
+			<h2>Health and safety made simple</h2>
+			<ul>
+				<li><a href="/workplace-health-and-safety/what-you-need-to-do-about-health-and-safety.php">What you need to do about health and safety</a></li>
+				<li><a href="/workplace-health-and-safety/write-a-health-and-safety-policy-for-your-business.php">Write a health and safety policy for your business</a></li>
+				<li><a href="/workplace-health-and-safety/decide-who-will-help-with-your-health-and-safety-duties.php">Decide who will help with your health and safety duties</a></li>
+				<li><a href="/workplace-health-and-safety/managing-the-risks-in-your-business.php">Managing the risks in your business</a></li>
+				<li><a href="/workplace-health-and-safety/consult-your-employees-on-health-and-safety.php">Consult your employees on health and safety</a></li>
+				<li><a href="/workplace-health-and-safety/provide-training-and-information.php">Provide training and information</a></li>
+				<li><a href="/workplace-health-and-safety/what-workplace-facilities-do-you-need-to-provide.php">What workplace facilities do you need to provide?</a></li>
+				<li><a href="/workplace-health-and-safety/first-aid-accidents-and-ill-health.php">First aid, accidents and ill health</a></li>
+				<li><a href="/workplace-health-and-safety/getting-insurance.php">Getting insurance</a></li>
+			</ul>
+			
+			<h2>Specialised health and safety information</h2>
+			<ul>
+				<li><a href="/workplace-health-and-safety/protecting-your-business.php">Protecting your business</a></li>
+				<li><a href="/workplace-health-and-safety/managing-the-welfare-of-people.php">Managing the welfare of people</a></li>
+				<li><a href="/workplace-health-and-safety/safer-ways-of-working.php">Safer ways of working</a></li>
+			</ul>
+			<p><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/sitemap?topicId=1073858799" target="_blank" rel="noopener noreferrer">Workplace health &amp; safety - site map</a></p>
+		
+			<h3>Spotlight</h3>
+			
+				<h4><a href="/workplace-health-and-safety/managing-the-risks-in-your-business.php">Managing the risks in your business</a></h4>
+				<hr />
+				<p>The key steps that all businesses should follow when carrying out a health and safety risk assessment</p>
+			
+			<a href="/workplace-health-and-safety/managing-the-risks-in-your-business.php" title="The key steps that all businesses should follow when carrying out a health and safety risk assessment">
+				<img src="../../../_img/PR0497.jpg" width="76" height="76" alt="The key steps that all businesses should follow when carrying out a health and safety risk assessment" />
+			</a>
+
+			<h3>Do it online</h3>
+			<ul>
+				<li><a href="https://web.archive.org/web/20120111054629/https://online.businesslink.gov.uk/bdotg/action/ruAlertManage" target="_blank" rel="noopener noreferrer">Sign up for email alerts about new and changing regulations in this theme</a></li>
+				<li><a href="https://www.gov.uk/licence-finder" target="_blank" rel="noopener noreferrer">Use our interactive tool to find licences and trade bodies for your business</a></li>
+				<li><a href="/finance-and-grants/insurance/get-the-right-insurance-for-your-business.php">Get the right insurance for your business</a></li>
+			</ul>
+
+			<h3>What's popular</h3>
+			<ul>
+				<li><a href="/workplace-health-and-safety/write-a-health-and-safety-policy-for-your-business.php">Write a health and safety policy for your business</a></li>
+				<li><a href="/workplace-health-and-safety/decide-who-will-help-with-your-health-and-safety-duties.php">Decide who will help with your health and safety duties</a></li>
+				<li><a href="/workplace-health-and-safety/what-workplace-facilities-do-you-need-to-provide.php">What workplace facilities do you need to provide?</a></li>
+				<li><a href="/workplace-health-and-safety/first-aid-accidents-and-ill-health.php">First aid, accidents and ill health</a></li>
+			</ul>
+
+			<h3>Directories</h3>
+			<ul>
+				<li><a href="https://web.archive.org/web/20071005054244/http://www.businesslink.gov.uk/bdotg/action/event?topicId=1073858799"  target="_blank" rel="noopener noreferrer">Workplace health &amp; safety events near you</a></li>
+				<li><a href="/find-a-case-study/browse-by-theme-workplace-health-and-safety.php">Workplace health &amp; safety case studies</a></li>
+			</ul>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
