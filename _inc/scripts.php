b13bbd8573e80e42aa80b5b6eaeb7ed17e55f9a6
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-10 17:01:23 +0000

    Updated jQuery CDN version - seems to have fixed issue of it not loading all the time

--- _inc/scripts.php
+++ _inc/scripts.php
@@ -1,6 +1,7 @@
-<?php /* jQuery core on CDN. Time of update, v2.2.4, does not support IE<9 */ ?>
-<script async src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script async src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+
 <?php /* Load What-Input files in footer */ ?>
+<script defer src="<?php $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']); ?>/_vendor/what-input/what-input.min.js"></script>
 <script defer src="<?php $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']); ?>/_vendor/what-input/what-input.min.js"></script>
 <?php /* Adding Foundation scripts file in the footer */ ?>
 <script defer src="<?php $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']); ?>/assets/js/foundation.min.js"></script>
