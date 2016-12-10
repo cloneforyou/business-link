0ae1ff031f40df6bdfb6de5230a1cd9ff2f483b7
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-10 17:48:49 +0000

    Completed ‘The executive summary’ page.

---
+++ starting-up/business-planning/prepare-a-business-plan/the-executive-summary.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>The executive summary | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Prepare a business plan</h1>
+		<h2>The executive summary</h2>
+		<p>The executive summary is often the most important part of your business plan. Positioned at the front of the document, it is the first part to be read. However, as a summary it makes sense to write it last.</p>
+		<p>It may be the only part that will be read. Faced with a large pile of funding requests, venture capitalists and banks have been known to separate business plans into 'worth considering' and 'discard' piles based on this section alone.</p>
+		<h3>What is it?</h3>
+		<p>The executive summary is a synopsis of the <strong>key points</strong> of your entire plan. It should include highlights from each section of the rest of the document - from the key features of the business opportunity through to the elements of the financial forecasts.</p>
+		<p>Its purpose is to explain the basics of your business in a way that both informs and interests the reader. If, after reading the executive summary, an investor or manager understands what the business is about and is keen to know more, it has done its job.</p>
+		<p>It should be concise - no longer than two pages at most - and interesting. It's advisable to write this section of your plan after you have completed the rest.</p>
+		<h3>What is it not?</h3>
+		<ul>
+			<li>A brief description of the business and its products. It's a synopsis of the entire plan.</li>
+			<li>An extended table of contents. This makes for very dull reading. You should ensure it shows the highlights of the plan, rather than restating the details the plan contains.</li>
+			<li>Hype. While the executive summary should excite the reader enough to read the entire plan, an experienced investor or business person will recognise hype and this will undermine the plan's credibility.</li>
+		</ul>
+		<p><a href="../../_files/business-plan-template.doc" target="_blank" rel="noopener noreferrer">Download our business plan template (DOC, 458K)</a>.</p>
+		<p><a href="../../_files/business-plan-template.odt" target="_blank" rel="noopener noreferrer">Download our business plan template in ODT format (ODT, 33K)</a>.</p>
+		<p>See our videos and e-learning resources to <a href="https://www.gov.uk/write-business-plan" title="What you need to include in your business plan and how to create one step-by-step" target="_blank" rel="noopener noreferrer">create your business plan</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/what-a-business-plan-should-include.php" title="What a business plan should include">Prev</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-business-its-products-and-services.php" title="Your business, its products and services">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php" title="Prepare a business plan">Introduction</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-audience-for-your-business-plan.php">The audience for your business plan</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/what-a-business-plan-should-include.php">What a business plan should include</a></li>
+			<li>The executive summary</li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-business-its-products-and-services.php">Your business, its products and services</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-markets-and-competitors.php">Your markets and competitors</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/marketing-and-sales.php">Marketing and sales</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-teams-skills.php">Your team's skills</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-operations.php">Your operations</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/financial-forecasts.php">Financial forecasts</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/presenting-your-business-plan.php">Presenting your business plan</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/using-the-business-plan-template.php">Using the business plan template</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/here-is-how-having-an-up-to-date-business-plan-helped-my-business.php">Here's how having an up-to-date business plan helped my business</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="../../_files/business-plan-template.doc" target="_blank" rel="noopener noreferrer">Download our business plan template (DOC, 458K)</a></li>
+			<li><a href="../../_files/business-plan-template.odt" target="_blank" rel="noopener noreferrer">Download our business plan template in ODT format (ODT, 33K)</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-finance-support-finder" title="How completing a business plan can help you improve your business' chance of success" target="_blank" rel="noopener noreferrer">Understand the importance of business planning</a></li>
+			<li><a href="https://www.gov.uk/write-business-plan" title="What you need to include in your business plan and how to create one step-by-step" target="_blank" rel="noopener noreferrer">Create your business plan</a></li>
+			<li><a href="/finance-and-grants/prepare-to-raise-finance/how-to-attract-investment/use-your-business-plan-to-get-funding.php">Use your business plan to get funding</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/prepare-a-business-plan-for-growth.php">Prepare a business plan for growth</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them.php">Common mistakes when starting up - and how to avoid them</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough.php">Starting a business when economic conditions are tough</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
