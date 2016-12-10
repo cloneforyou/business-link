e55408aa8f6b6c092a7a60da49cd29f1e95f7460
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-10 18:14:20 +0000

    Completed ‘Your team's skills’ page.

---
+++ starting-up/business-planning/prepare-a-business-plan/your-teams-skills.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Your team's skills | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Prepare a business plan</h1>
+		<h2>Your team's skills</h2>
+		<p>Your business plan needs to set out your own background and skills and the structure and key skills of both your management team and your staff. It should identify the strengths in your team and your plans to deal with any obvious weaknesses.</p>
+		<h3>The management team</h3>
+		<p>If you're looking for external funding, your management team can be a decisive factor. Explain who is involved, their role and how it fits into the organisation. Include a CV or paragraph on each individual, outlining their background, relevant experience and qualifications. Include any advisers you might have such as accountants or lawyers.</p>
+		<p>If you're looking to satisfy your bank manager or other investors, you need to demonstrate that your management team has the right balance of skills, drive and experience to enable your business to succeed. Key skills include sales, marketing and financial management as well as production, operational and market experience.</p>
+		<p>Your investors will also want to be convinced that you and your team are fully committed. Therefore it's a good idea to set out how much time and money each person will contribute - or has already contributed - to the business and the salaries and benefits you plan to draw. You can find more practical tips in our guide on how to <a href="/finance-and-grants/prepare-to-raise-finance/how-to-attract-investment/use-your-business-plan-to-get-funding.php">use your business plan to get funding</a>.</p>
+		<h3>Your people</h3>
+		<p>Give details of your workforce in terms of total numbers and by department. Spell out what work you plan to do internally and if you plan to outsource any work. Other useful figures might be sales or profit per employee, average salaries, employee retention rates and productivity.</p>
+		<p>Your plan should also outline any recruitment or training plans, including timescales and costs.</p>
+		<p>It's vital to be realistic about the commitment and motivation of your people and spell out any plans to improve or maintain staff morale.</p>
+		<p>See our videos and e-learning resources to <a href="https://www.gov.uk/write-business-plan" title="What you need to include in your business plan and how to create one step-by-step" target="_blank" rel="noopener noreferrer">create your business plan</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/marketing-and-sales.php" title="Marketing and sales">Prev</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-operations.php" title="Your operations">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php" title="Prepare a business plan">Introduction</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-audience-for-your-business-plan.php">The audience for your business plan</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/what-a-business-plan-should-include.php">What a business plan should include</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-executive-summary.php">The executive summary</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-business-its-products-and-services.php">Your business, its products and services</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-markets-and-competitors.php">Your markets and competitors</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/marketing-and-sales.php">Marketing and sales</a></li>
+			<li>Your team's skills</li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-operations.php">Your operations</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/financial-forecasts.php">Financial forecasts</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/presenting-your-business-plan.php">Presenting your business plan</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/using-the-business-plan-template.php">Using the business plan template</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/here-is-how-having-an-up-to-date-business-plan-helped-my-business.php">Here's how having an up-to-date business plan helped my business</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="../../_files/business-plan-template.doc" target="_blank" rel="noopener noreferrer">Download our business plan template (DOC, 458K)</a></li>
+			<li><a href="../../_files/business-plan-template.odt" target="_blank" rel="noopener noreferrer">Download our business plan template in ODT format (ODT, 33K)</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-finance-support-finder" title="How completing a business plan can help you improve your business' chance of success" target="_blank" rel="noopener noreferrer">Understand the importance of business planning</a></li>
+			<li><a href="https://www.gov.uk/write-business-plan" title="What you need to include in your business plan and how to create one step-by-step" target="_blank" rel="noopener noreferrer">Create your business plan</a></li>
+			<li><a href="/finance-and-grants/prepare-to-raise-finance/how-to-attract-investment/use-your-business-plan-to-get-funding.php">Use your business plan to get funding</a></li>
+			<li><a href="/sales-and-marketing/marketing/create-your-marketing-strategy.php">Create your marketing strategy</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
