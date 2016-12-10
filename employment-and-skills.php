b1e8cdc79c7ff9d3d63ef83a7088e4089adf007c
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-10 21:38:31 +0000

    Completed ‘Employment & skills’ page.

---
+++ employment-and-skills.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Employment &amp; skills | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+		
+		<h1>Employment &amp; skills</h1>
+		<h2>Recruitment</h2>
+		<ul>
+			<li><a href="/employment-and-skills/advertise-a-job.php">Advertise a job</a></li>
+			<li><a href="/employment-and-skills/take-on-staff.php">Take on staff</a></li>
+			<li><a href="/employment-and-skills/different-types-of-worker.php">Different types of worker</a></li>
+			<li><a href="/employment-and-skills/employment-checks.php">Employment checks</a></li>
+			<li><a href="/employment-and-skills/staff-documents-and-employment-policies.php">Staff documents and employment policies</a></li>
+		</ul>
+		
+		<h2>Pay staff</h2>
+		<ul>
+			<li><a href="/employment-and-skills/pay-and-pensions.php">Pay and pensions</a></li>
+			<li><a href="/employment-and-skills/national-minimum-wage.php">National minimum wage</a></li>
+		</ul>
+		
+		<h2>Manage people</h2>
+		<ul>
+			<li><a href="/employment-and-skills/holiday-and-other-leave.php">Holiday and other leave</a></li>
+			<li><a href="/employment-and-skills/working-time.php">Working time</a></li>
+			<li><a href="/employment-and-skills/equality-and-diversity.php">Equality and diversity</a></li>
+			<li><a href="/employment-and-skills/maternity-paternity-and-adoption.php">Maternity, paternity and adoption</a></li>
+			<li><a href="/employment-and-skills/employee-engagement.php">Employee engagement</a></li>
+			<li><a href="/employment-and-skills/staff-motivation-and-performance.php">Staff motivation and performance</a></li>
+			<li><a href="/employment-and-skills/trade-unions.php">Trade unions</a></li>
+			<li><a href="/employment-and-skills/redundancy-restructures-and-change.php">Redundancy, restructures and change</a></li>
+		</ul>
+		
+		<h2>Develop staff</h2>
+		<ul>
+			<li><a href="/employment-and-skills/skills-and-development.php">Skills and development</a></li>
+		</ul>
+		
+		<h2>Resolve conflict and staff leaving</h2>
+		<ul>
+			<li><a href="/employment-and-skills/problems-at-work.php">Problems at work</a></li>
+			<li><a href="/employment-and-skills/dismissals-and-staff-leaving.php">Dismissals and staff leaving</a></li>
+		</ul>
+
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/sitemap?topicId=1073858787" target="_blank" rel="noopener noreferrer">Employment &amp; skills - site map</a></p>
+		
+		<h3>Spotlight</h3>
+		<h4><a href="/advertise-a-job-vacancy-with-jobcentre-plus.php">Jobcentre Plus: advertise a vacancy</a></h4>
+		
+		<hr />
+
+		<p>Create, view and update your vacancies online for free</p>
+		<a href="/advertise-a-job-vacancy-with-jobcentre-plus.php">
+			<img src="../../_img/mousejobs_76x76.jpg" width="76" height="76" alt="Post your job vacancies for free on Jobcentre Plus using Employer Direct online" />
+		</a>
+
+		<h4><a href="/employment-and-skills/holiday-and-other-leave/calculate-your-employees-holiday-entitlement.php">Calculate your employees' holiday entitlement</a></h4>
+
+		<hr />
+
+		<p>Use our quick tool to find out how much leave an employee is entitled to</p>
+		<a href="/employment-and-skills/holiday-and-other-leave/calculate-your-employees-holiday-entitlement.php">
+			<img src="../../_img/PR0293.jpg" width="76" height="76" alt="Use our interactive tool to find out how much annual leave your employees are due" />
+		</a>
+
+		<h4><a href="/employment-and-skills/staff-documents-and-employment-policies/how-investors-in-people-can-help-your-business.php">Investors in People</a></h4>
+
+		<hr />
+		
+		<p>Find out how to continuously improve your management practices</p>
+		<a href="/employment-and-skills/staff-documents-and-employment-policies/how-investors-in-people-can-help-your-business.php">
+			<img src="../../_img/award1.jpg" width="76" height="76" alt="How Investors in People can help your business, including using the framework in your business and business advice" />
+		</a>
+
+		<h4><a href="/employers-charter.php">Updated Employer's Charter</a></h4>
+		
+		<hr />
+		
+		<p>Find out what you can and can't do when managing your staff</p>
+		<a href="/employers-charter.php">
+			<img src="../../_img/business_benefits.jpg" width="75" height="75" alt="Find out what you can and can't do when managing your staff" />
+		</a>
+
+		<h3>Case studies</h3>
+		<p>Find out how Knowsley Safari Park trains staff in customer care skills</p>
+		
+		<hr />
+		
+		<ul>
+			<li><a href="/employment-and-skills/skills-and-development/training-your-staff/here-is-how-I-set-up-in-house-training-and-tailored-it-to-our-needs.php">Here's how I set up in-house training and tailored it to our needs</a></li>
+		</ul>
+		<img src="../../_img/SeanLawler_promo.jpg" width="76" height="76" alt="Sean Lawler" />
+		
+		<h3>Do it online</h3>
+		<ul>
+			<li><a href="/do-it-online/use-paye-online-for-employers.php">Log in or register for PAYE online with HMRC</a></li>
+			<li><a href="/employment-and-skills/redundancy-restructures-and-change/calculate-the-statutory-redundancy-pay-due-to-your-employee.php">Use our interactive tool to calculate the statutory redundancy pay due to your employee</a></li>
+			<li><a href="https://www.gov.uk/employment-contracts-and-conditions" title="Use our interactive tool to create a document including everything you have to give your new employee in writing" target="_blank" rel="noopener noreferrer">Create a written statement of employment</a></li>
+			<li><a href="/employment-and-skills/national-minimum-wage/national-minimum-wage-entitlement-rate-and-arrears-checker.php">Check your employees' entitlement to the national minimum wage </a></li>
+		</ul>
+		
+		<h3>What's popular</h3>
+		<ul>
+			<li><a href="/employment-and-skills/skills-and-development/advice-and-funding-for-training.php">Advice and funding for training</a></li>
+			<li><a href="/employment-and-skills/maternity-paternity-and-adoption/additional-paternity-leave-entitlement.php">Additional paternity leave entitlement</a></li>
+			<li><a href="/employment-and-skills/working-time/hours-rest-breaks-and-the-working-week.php">Hours, rest breaks and the working week</a></li>
+		</ul>
+		
+		<h3>Useful links on other sites</h3>
+		<form method="post">
+			<select>
+				<option value="/employment-and-skills.php">Choose from this menu ...</option>
+				<option value="https://www.gov.uk/calculate-statutory-sick-pay" target="_blank" rel="noopener noreferrer">HMRC statutory sick pay calculator</option>
+				<option value="https://www.gov.uk/maternity-paternity-calculator" target="_blank" rel="noopener noreferrer">HMRC statutory maternity pay calculator</option>
+				<option value="https://www.gov.uk/maternity-paternity-calculator" target="_blank" rel="noopener noreferrer">HMRC statutory paternity pay calculator for births</option>
+				<option value="https://www.gov.uk/guidance/employment-status-indicator" target="_blank" rel="noopener noreferrer">HMRC employment status indicator</option>
+				<option value="https://web.archive.org/web/20130101105312/http://www.investorsinpeople.co.uk/MediaResearch/Tools/Pages/Register.aspx" target="_blank" rel="noopener noreferrer">Investors in People self-check tool</option>
+			</select>
+			<input type="image" value="Go" src="../../../_img/button_go.gif" title="Go" />
+		</form>
+		
+		<h3>Directories</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://www.businesslink.gov.uk/bdotg/action/event?topicId=1073858787" title="National and local events, seminars and workshops in your area to help you learn or network" target="_blank" rel="noopener noreferrer">Employment &amp; skills events near you</a></li>
+			<li><a href="/find-a-case-study/browse-by-theme-employment-and-skills.php">Employment &amp; skills case studies</a></li>
+		</ul>
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
