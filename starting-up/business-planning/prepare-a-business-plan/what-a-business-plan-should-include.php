0201a27c6eab448c5a936b0d6f24aa2ef1d9e8fa
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-10 17:45:43 +0000

    Completed ‘What a business plan should include’ page.

---
+++ starting-up/business-planning/prepare-a-business-plan/what-a-business-plan-should-include.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>What a business plan should include | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Prepare a business plan</h1>
+		<h2>What a business plan should include</h2>
+		<p>Your business plan should provide details of how you are going to develop your business, when you are going to do it, who's going to play a part and how you will manage the finances.</p>
+		<p>Clarity on these issues is particularly important if you're looking for finance or investment. The process of building your plan will also focus your mind on how your new business will need to operate to give it the best chance of success.</p>
+		<p>Your plan should include:</p>
+		<ul>
+			<li><strong>An executive summary</strong> - this is an overview of the business you want to start. It's vital. Many lenders and investors make judgments about your business based on this section of the plan alone. See the page in this guide on <a href="/starting-up/business-planning/prepare-a-business-plan/the-executive-summary.php" title="The initial and potentially most important part of your plan, outlining the highlights of your business idea">the executive summary</a>.</li>
+			<li><strong>A short description of the business opportunity</strong> - who you are, what you plan to sell or offer, why and to whom. See the page in this guide on <a href="/starting-up/business-planning/prepare-a-business-plan/your-business-its-products-and-services.php" title="Outlining what your company does, what you can offer, the market needs you address and why your idea is viable">your business, its products and services</a>.</li>
+			<li><strong>Your marketing and sales strategy</strong> - why you think people will buy what you want to sell and how you plan to sell to them. See the pages in this guide on <a href="/starting-up/business-planning/prepare-a-business-plan/your-markets-and-competitors.php" title="Define your market, identify your target customers and main competitors and look to the future to anticipate change">your markets and competitors</a> and <a href="/starting-up/business-planning/prepare-a-business-plan/marketing-and-sales.php" title="Sectors covering price, customers, your sales channels, distribution methods, promotional strategy and sales plan">marketing and sales</a>.</li>
+			<li><strong>Your management team and personnel</strong> - your credentials and the people you plan to recruit to work with you. See the page in this guide on <a href="/starting-up/business-planning/prepare-a-business-plan/your-teams-skills.php" title="An analysis of your workforce - the structure, key skills of your team, training and recruitment plans">your team's skills</a>.</li>
+			<li><strong>Your operations</strong> - your premises, production facilities, your management information systems and IT. See the page in this guide on <a href="/starting-up/business-planning/prepare-a-business-plan/your-operations.php" title="Information to include about how you will source, procure, create and deliver the goods or services you will be selling">your operations</a>.</li>
+			<li><strong>Financial forecasts</strong> - this section translates everything you have said in the previous sections into numbers. See the page in this guide on <a href="/starting-up/business-planning/prepare-a-business-plan/financial-forecasts.php" title="Putting your business into numbers with sales, cashflow and profit and loss forecasts and reviewing possible risks">financial forecasts</a>.</li>
+		</ul>
+		<p><a href="../../_files/business-plan-template.doc" target="_blank" rel="noopener noreferrer">Download our business plan template (DOC, 458K)</a>.</p>
+		<p><a href="../../_files/business-plan-template.odt" target="_blank" rel="noopener noreferrer">Download our business plan template in ODT format (ODT, 33K)</a>.</p>
+		<p>See our videos and e-learning resources to <a href="https://www.gov.uk/write-business-plan" title="What you need to include in your business plan and how to create one step-by-step" target="_blank" rel="noopener noreferrer">create your business plan</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-audience-for-your-business-plan.php" title="The audience for your business plan">Prev</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-executive-summary.php" title="The executive summary">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php" title="Prepare a business plan">Introduction</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-audience-for-your-business-plan.php">The audience for your business plan</a></li>
+			<li>What a business plan should include</li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-executive-summary.php">The executive summary</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-business-its-products-and-services.php">Your business, its products and services</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-markets-and-competitors.php">Your markets and competitors</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/marketing-and-sales.php">Marketing and sales</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-teams-skills.php">Your team's skills</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-operations.php">Your operations</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/financial-forecasts.php">Financial forecasts</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/presenting-your-business-plan.php">Presenting your business plan</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/using-the-business-plan-template.php">Using the business plan template</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/here-is-how-having-an-up-to-date-business-plan-helped-my-business.php">Here's how having an up-to-date business plan helped my business</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="../../_files/business-plan-template.doc" target="_blank" rel="noopener noreferrer">Download our business plan template (DOC, 458K)</a></li>
+			<li><a href="../../_files/business-plan-template.odt" target="_blank" rel="noopener noreferrer">Download our business plan template in ODT format (ODT, 33K)</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-finance-support-finder" title="How completing a business plan can help you improve your business' chance of success" target="_blank" rel="noopener noreferrer">Understand the importance of business planning</a></li>
+			<li><a href="https://www.gov.uk/write-business-plan" title="What you need to include in your business plan and how to create one step-by-step" target="_blank" rel="noopener noreferrer">Create your business plan</a></li>
+			<li><a href="/finance-and-grants/prepare-to-raise-finance/how-to-attract-investment/use-your-business-plan-to-get-funding.php">Use your business plan to get funding</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/prepare-a-business-plan-for-growth.php">Prepare a business plan for growth</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them.php">Common mistakes when starting up - and how to avoid them</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough.php">Starting a business when economic conditions are tough</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
