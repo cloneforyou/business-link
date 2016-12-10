136ac61d0c9651d0ade00af3dde9d5f44b6960ea
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-10 18:35:54 +0000

    Completed ‘Presenting your business plan’ page.

---
+++ starting-up/business-planning/prepare-a-business-plan/presenting-your-business-plan.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Presenting your business plan | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Prepare a business plan</h1>
+		<h2>Presenting your business plan</h2>
+		<p>To make sure your business plan has maximum impact, there are a number of points to observe.</p>
+		<p>Keep the plan <strong>short</strong> - it's more likely to be read if it's a manageable length. Think about the <strong>presentation</strong> and keep it professional - even if you only intend to use the plan in-house. Remember, a well-presented plan will reinforce the positive impression you want to create of your business.</p>
+		<h3>Tips for presenting your plan</h3>
+		<ul>
+			<li>Include a cover or binding and a contents page with page and section numbering.</li>
+			<li>Start with the executive summary.</li>
+			<li>Ensure it's legible - make sure the type is ten point or above.</li>
+			<li>You may want to email it, so ensure you use email-friendly formatting.</li>
+			<li>Even if it's for internal use only, write the plan as if it's intended for an external audience.</li>
+			<li>Edit the plan carefully - get at least two people to read it and check that it makes sense.</li>
+			<li>Show the plan to expert advisers - such as your accountant - and ask for feedback. Redraft sections they say are difficult to understand.</li>
+			<li>Avoid jargon and put detailed information - such as market research data or balance sheets - in an appendix at the back.</li>
+			<li>You may have detailed plans for specific areas of your business, such as a sales plan or a staff training plan, but it is best not to include these, though it is good practice to mention that they exist.</li>
+		</ul>
+		<p>While it is sensible to seek advice from external advisers, it is not a good idea to get them to write the plan for you. Investors and lenders need to have confidence that you personally understand your business plan and are committed to the vision for the business.</p>
+		<p>Make sure your plan is <strong>realistic</strong>. Once you have prepared your plan, use it. If you update it regularly, it will help you keep track of your business' development. See our guides on <a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting.php">budgeting and business planning</a> and how to <a href="/grow-your-business/setting-a-vision-for-growth/prepare-a-business-plan-for-growth.php">prepare a business plan for growth</a>.</p>
+		<p>See our videos and e-learning resources to <a href="https://www.gov.uk/write-business-plan" title="What you need to include in your business plan and how to create one step-by-step" target="_blank" rel="noopener noreferrer">create your business plan</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/financial-forecasts.php" title="Financial forecasts">Prev</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/using-the-business-plan-template.php" title="Using the business plan template">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php" title="Prepare a business plan">Introduction</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-audience-for-your-business-plan.php">The audience for your business plan</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/what-a-business-plan-should-include.php">What a business plan should include</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-executive-summary.php">The executive summary</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-business-its-products-and-services.php">Your business, its products and services</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-markets-and-competitors.php">Your markets and competitors</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/marketing-and-sales.php">Marketing and sales</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-teams-skills.php">Your team's skills</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-operations.php">Your operations</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/financial-forecasts.php">Financial forecasts</a></li>
+			<li>Presenting your business plan</li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/using-the-business-plan-template.php">Using the business plan template</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/here-is-how-having-an-up-to-date-business-plan-helped-my-business.php">Here's how having an up-to-date business plan helped my business</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="../../_files/business-plan-template.doc" target="_blank" rel="noopener noreferrer">Download our business plan template (DOC, 458K)</a></li>
+			<li><a href="../../_files/business-plan-template.odt" target="_blank" rel="noopener noreferrer">Download our business plan template in ODT format (ODT, 33K)</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-finance-support-finder" title="How to forecast your finances and raise finance when starting up" target="_blank" rel="noopener noreferrer">Think about your financial planning</a></li>
+			<li><a href="/sales-and-marketing/online-marketing/online-business-networking-and-social-networking.php">Online business networking and social networking</a></li>
+			<li><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them.php">Common mistakes when starting up - and how to avoid them</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
