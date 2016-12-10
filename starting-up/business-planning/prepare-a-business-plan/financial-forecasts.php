74e0aefbbefb41b44970056e967c036459f6771d
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-10 18:33:08 +0000

    Completed ‘Financial forecasts’ page.

---
+++ starting-up/business-planning/prepare-a-business-plan/financial-forecasts.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Financial forecasts | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+
+		<h1>Prepare a business plan</h1>
+		<h2>Financial forecasts</h2>
+		<p>As part of your plan you will need to provide a set of financial projections which translate what you have said about your business into numbers.</p>
+		<p>You will need to look carefully at:</p>
+		<ul>
+			<li>how much capital you need if you are seeking external funding</li>
+			<li>the security you can offer lenders</li>
+			<li>how you plan to repay any borrowings</li>
+			<li>sources of revenue and income</li>
+		</ul>
+		<p>You may also want to include your personal finances as part of the plan at this stage.</p>
+		<h3>Financial planning</h3>
+		<p>Your forecasts should run for the next three (or even five) years and their level of sophistication should reflect the sophistication of your business. However, the first 12 months' forecasts should have the most detail associated with them.</p>
+		<p>Include the assumptions behind your projection with your figures, both in terms of costs and revenues so investors can clearly see the thinking behind the numbers.</p>
+		<h3>What your forecasts should include</h3>
+		<p><strong>Sales forecast</strong> - the amount of money you expect to raise from sales. See our guide on how to <a href="/sales-and-marketing/selling/forecast-and-plan-your-sales.php">forecast and plan your sales</a>.</p>
+		<p><strong>Cashflow statements</strong> - your cash balance and monthly cashflow patterns for at least the first 12 to 18 months. The aim is to show that your business will have enough working capital to survive so make sure you have considered the key factors such as the timing of sales and salaries. See our guide on <a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">cashflow management: the basics</a>.</p>
+		<p><strong>Profit and loss forecast</strong> - a statement of the trading position of the business: the level of profit you expect to make, given your projected sales and the costs of providing goods and services and your overheads.</p>
+		<p>Your forecasts should cover a range of scenarios. New businesses often forecast over-optimistic sales and most external readers will take this into account. It is sensible to include subsidiary forecasts based on sales being significantly slower than you are actually predicting, with one for sales starting three months later than expected, and another forecasting a 20 per cent lower level of sales.</p>
+		<p>You can <a href="../../../_files/cashflow_profit-and-loss_sales-forecast.xls" target="_blank" rel="noopener noreferrer">download our sample profit and loss forecast template (XLS, 50K)</a>.</p>
+		<h3>Risk analysis</h3>
+		<p>Alongside your financial forecasts it is good practice to show that you have reviewed the risks your business could be faced with, and that you have looked at contingencies and insurance to cover these. Risks can include:</p>
+		<ul>
+			<li>competitor action</li>
+			<li>commercial issues - sales, prices, deliveries</li>
+			<li>operations - IT, technology or production failure</li>
+			<li>staff - skills, availability and costs</li>
+			<li>acts of God - fire or flood</li>
+		</ul>
+		<p>See our guide on <a href="/grow-your-business/managing-risk.php">managing risk</a>.</p>
+		<p>See our videos and e-learning resources to <a href="https://www.gov.uk/business-finance-support-finder" title="How to forecast your finances and raise finance when starting up" target="_blank" rel="noopener noreferrer">think about your financial planning</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-operations.php" title="Your operations">Prev</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/presenting-your-business-plan.php" title="Presenting your business plan">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php" title="Prepare a business plan">Introduction</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-audience-for-your-business-plan.php">The audience for your business plan</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/what-a-business-plan-should-include.php">What a business plan should include</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-executive-summary.php">The executive summary</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-business-its-products-and-services.php">Your business, its products and services</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-markets-and-competitors.php">Your markets and competitors</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/marketing-and-sales.php">Marketing and sales</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-teams-skills.php">Your team's skills</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-operations.php">Your operations</a></li>
+			<li>Financial forecasts</li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/presenting-your-business-plan.php">Presenting your business plan</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/using-the-business-plan-template.php">Using the business plan template</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/here-is-how-having-an-up-to-date-business-plan-helped-my-business.php">Here's how having an up-to-date business plan helped my business</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="../../../_files/cashflow_profit-and-loss_sales-forecast.xls" target="_blank" rel="noopener noreferrer">Download our sample profit and loss forecast template (XLS, 50K)</a></li>
+			<li><a href="../../_files/business-plan-template.doc" target="_blank" rel="noopener noreferrer">Download our business plan template (DOC, 458K)</a></li>
+			<li><a href="../../_files/business-plan-template.odt" target="_blank" rel="noopener noreferrer">Download our business plan template in ODT format (ODT, 33K)</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground.php">How do I survive until my business is off the ground?</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">Cashflow management: the basics</a></li>
+			<li><a href="/sales-and-marketing/selling/forecast-and-plan-your-sales.php">Forecast and plan your sales</a></li>
+			<li><a href="/finance-and-grants/prepare-to-raise-finance/how-to-attract-investment/use-your-business-plan-to-get-funding.php">Use your business plan to get funding</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
