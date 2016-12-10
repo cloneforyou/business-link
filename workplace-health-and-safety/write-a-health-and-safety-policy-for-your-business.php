92bef4140251ea31bf4fbcac2b07496d3e83c370
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-10 23:40:01 +0000

    Completed ‘Write a health and safety policy for your business’ page.

---
+++ workplace-health-and-safety/write-a-health-and-safety-policy-for-your-business.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Write a health and safety policy for your business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Write a health and safety policy for your business</h1>
+		<h2>Introduction</h2>
+		<img src="../../_img/HS2220.gif" width="94" height="62" border="0" alt="" />
+		<p>Describing how you will manage health and safety in your business will let your staff and others know about your commitment to health and safety. This will be your health and safety policy. It should clearly say who does what, when and how.</p>
+		<p>If you have five or more employees, you must have a written policy.</p>
+		<p>The policy does not need to be complicated or time-consuming. To help you, the Health and Safety Executive (HSE) have created a template that you can download and complete. The template also includes a section for your risk assessment so that you can record everything in one document.</p>
+		<p>HSE also provide an example health and safety policy to give you an idea of what to include when writing your own.</p>
+		<p>A policy will only be effective if you and your staff follow it and review it regularly.</p>
+		
+		<ul>
+			<li><a href="/workplace-health-and-safety/write-a-health-and-safety-policy-for-your-business/what-should-be-in-your-health-and-safety-policy.php" title="What should be in your health and safety policy?">Next</a></li>
+		</ul>
+		
+		<h3>Write a health and safety policy for your business</h3>
+		<ul>
+			<li>Introduction</li>
+			<li><a href="/workplace-health-and-safety/write-a-health-and-safety-policy-for-your-business/what-should-be-in-your-health-and-safety-policy.php">What should be in your health and safety policy?</a></li>
+			<li><a href="/workplace-health-and-safety/write-a-health-and-safety-policy-for-your-business/the-statement-of-general-policy.php">The statement of general policy</a></li>
+			<li><a href="/workplace-health-and-safety/write-a-health-and-safety-policy-for-your-business/the-responsibilities-section-of-your-health-and-safety-policy.php">The responsibilities section of your health and safety policy</a></li>
+			<li><a href="/workplace-health-and-safety/write-a-health-and-safety-policy-for-your-business/the-arrangements-section-of-your-health-and-safety-policy.php">The arrangements section of your health and safety policy</a></li>
+			<li><a href="/workplace-health-and-safety/write-a-health-and-safety-policy-for-your-business/implementing-your-health-and-safety-policy.php">Implementing your health and safety policy</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="../../../_files/risk-assessment-and-policy-template.doc" target="_blank" rel="noopener noreferrer">Download a health and safety policy template from the HSE website (DOC, 110K)</a></li>
+			<li><a href="../../../_files/health-and-safety-policy-example.doc" target="_blank" rel="noopener noreferrer">Download an example health and safety policy from the HSE website (DOC, 215K)</a></li>
+		</ul>
+		
+		<img src="../../../_img/px_healthandenvironment.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
