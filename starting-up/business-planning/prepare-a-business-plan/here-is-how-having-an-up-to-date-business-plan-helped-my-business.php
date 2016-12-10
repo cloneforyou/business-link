d2ea6af2aac59ad72af6e1710f92d6b4c896f959
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-10 18:42:45 +0000

    Completed ‘Here's how having an up-to-date business plan helped my business’ page.

---
+++ starting-up/business-planning/prepare-a-business-plan/here-is-how-having-an-up-to-date-business-plan-helped-my-business.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Here's how having an up-to-date business plan helped my business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Here's how having an up-to-date business plan helped my business</h1>
+		<h2>Here's how having an up-to-date business plan helped my business</h2>
+		<p>Darren Jones launched his care business, AKC Home Support Services, in 1991 with his wife Sharron. Although writing their business plan was one of the first things the couple did, Darren admits he originally saw it as a bit of a chore. Now, he takes a different view, believing it has helped the business stay on track and true to its goals.</p>
+		<p><strong>What I did</strong></p>
+		<h3>Write the plan</h3>
+		<p>"When we started the firm I knew we needed a business plan but saw it more as a document for everyone else than something to help us. If I started another business tomorrow I would write one much more willingly as it brings a number of benefits - from helping you secure finance to keeping you focused on your goals.</p>
+		<p>"We got help from our local enterprise centre, looked at examples from other businesses and a template from the bank. We mixed and matched bits from these sources because not everything applied to us. For example, because we were going into a new market we couldn't write about our competitors but needed a lot of information about the market for care services."</p>
+		<h3>Consult the plan</h3>
+		<p>"We used our business plan to set out the financial and strategic goals we wanted to achieve in the short and long-term. We review it annually now unless there's a significant shift in our market and then we use it to immediately re-evaluate our goals.</p>
+		<p>"Our business plan has also helped us to avoid expanding too quickly. Early on, we were offered work in another county. This seemed great but when we looked at our business plan - and particularly our cashflow forecasts - we realised it was important to establish a firm base in one county before taking on work in another otherwise we would overstretch ourselves."</p>
+		<h3>Use the plan</h3>
+		<p>"We purchased a residential unit four years ago and our business plan definitely helped us demonstrate why the bank should lend us the money. Without it being put down on paper I don't think it would have sounded like a very viable suggestion.</p>
+		<p>"The home added a different dimension to the business in which we had no trading record so the bank lent us the money according to our past performance. We could also show that we would offset some of the cost by using part of the new building as office space.</p>
+		<p>"Our plan also helped us to get support from Shell LiveWire - the organisation that assists 16-30 year olds to start and develop businesses - as you must have a business plan to enter its competitions. We were awarded prizes twice - not only bringing in extra money but publicity too."</p>
+		<p><strong>What I'd do differently</strong></p>
+		<h3>Work on the plan's presentation</h3>
+		<p>"I would have tried to get more assistance and perhaps made the document look a bit more professional. It's your way of gaining support for your business and is the one thing that your bank manager will remember apart from how you were dressed."</p>
+		<h3>Get as much help and advice as possible</h3>
+		<p>"Show the plan to an independent third party - such as friends or family who have run their own businesses - who will be able to point out if anything is missing. It's much better to make mistakes on a practice run than when it really matters."</p>
+		<p><a href="/find-a-case-study.php">Read more case studies that describe first hand how people tackle real-life challenges and opportunities</a>. </p>
+		
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/using-the-business-plan-template.php" title="Using the business plan template">Prev</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php" title="Prepare a business plan">Introduction</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-audience-for-your-business-plan.php">The audience for your business plan</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/what-a-business-plan-should-include.php">What a business plan should include</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-executive-summary.php">The executive summary</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-business-its-products-and-services.php">Your business, its products and services</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-markets-and-competitors.php">Your markets and competitors</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/marketing-and-sales.php">Marketing and sales</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-teams-skills.php">Your team's skills</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-operations.php">Your operations</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/financial-forecasts.php">Financial forecasts</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/presenting-your-business-plan.php">Presenting your business plan</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/using-the-business-plan-template.php">Using the business plan template</a></li>
+			<li>Here's how having an up-to-date business plan helped my business</li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<div class="media-object stack-for-small">
+			<div class="media-object-section">
+				<div class="thumbnail">
+					<img src="../../../_img/CS0038.jpg" width="181" height="121" border="0" alt="Darren Jones, AKC Home Support Services" />
+				</div>
+			</div>
+			<div class="media-object-section">
+				<h3>
+					Darren Jones<br />
+					AKC Home Support Services<br />
+					Darren's top tips:
+				</h3>
+				<ul>
+					<li>"Make sure your business plan reflects your personal vision - don't just put things in because you think it's what someone else wants to read."</li>
+					<li>"Be objective. It is important to weigh up the pros and cons."</li>
+					<li>"Get assistance whenever you can."</li>
+				</ul>
+			</div>
+		</div>
+
+		<h4>More case studies</h4>
+		<p><a href="/find-a-case-study.php"><b>Find a case study</b></a></p>
+
+		<img src="../../../_img/px_startingabusiness.gif" width="181" height="5" alt="" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
