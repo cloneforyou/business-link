674512d69e48389a891246b3130a8900623fe604
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-10 18:19:14 +0000

    Completed ‘Your operations’ page.

---
+++ starting-up/business-planning/prepare-a-business-plan/your-operations.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Your operations | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Prepare a business plan</h1>
+		<h2>Your operations</h2>
+		<p>Your business plan also needs to outline your operational capabilities and any planned improvements. There are certain areas you should focus on.</p>
+		<h3>Location</h3>
+		<ul>
+			<li>Do you have any business property?</li>
+			<li>What are your long-term commitments to the property?</li>
+			<li>Do you own or rent it?</li>
+			<li>What are the advantages and disadvantages of your current location?</li>
+		</ul>
+		<h3>Producing your goods and services</h3>
+		<ul>
+			<li>Do you need your own production facilities or would it be cheaper to outsource any manufacturing processes?</li>
+			<li>If you do have your own facilities, how modern are they?</li>
+			<li>What is the capacity compared with existing and forecasted demand?</li>
+			<li>Will any investment be needed?</li>
+			<li>Who will be your suppliers?</li>
+		</ul>
+		<h3>Management-information systems</h3>
+		<ul>
+			<li>Have you got established procedures for stock control, management accounts and quality control?</li>
+			<li>Can they cope with any proposed expansion?</li>
+		</ul>
+		<p>For more information, see our guides on <a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory.php">stock control and inventory</a> and <a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php">financial and management accounts: the basics</a>.</p>
+		<h3>Information technology</h3>
+		<ul>
+			<li>IT is a key factor in most businesses, so include your strengths and weaknesses in this area.</li>
+			<li>Outline the reliability and the planned development of your systems.</li>
+		</ul>
+		<p>For more information, see our guide providing <a href="/it-and-ecommerce/introduction-to-it/an-overview-of-it-and-e-commerce.php">an overview of IT and e-commerce</a>.</p>
+		<p>See our videos and e-learning resources to <a href="https://www.gov.uk/write-business-plan" title="What you need to include in your business plan and how to create one step-by-step" target="_blank" rel="noopener noreferrer">create your business plan</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-teams-skills.php" title="Your team's skills">Prev</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/financial-forecasts.php" title="Financial forecasts">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php" title="Prepare a business plan">Introduction</a></li>
+			<li>The audience for your business plan</li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/what-a-business-plan-should-include.php">What a business plan should include</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-executive-summary.php">The executive summary</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-business-its-products-and-services.php">Your business, its products and services</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-markets-and-competitors.php">Your markets and competitors</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/marketing-and-sales.php">Marketing and sales</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-teams-skills.php">Your team's skills</a></li>
+			<li>Your operations</li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/financial-forecasts.php">Financial forecasts</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/presenting-your-business-plan.php">Presenting your business plan</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/using-the-business-plan-template.php">Using the business plan template</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/here-is-how-having-an-up-to-date-business-plan-helped-my-business.php">Here's how having an up-to-date business plan helped my business</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="../../_files/business-plan-template.doc" target="_blank" rel="noopener noreferrer">Download our business plan template (DOC, 458K)</a></li>
+			<li><a href="../../_files/business-plan-template.odt" target="_blank" rel="noopener noreferrer">Download our business plan template in ODT format (ODT, 33K)</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-finance-support-finder" title="How completing a business plan can help you improve your business' chance of success" target="_blank" rel="noopener noreferrer">Understand the importance of business planning</a></li>
+			<li><a href="https://www.gov.uk/write-business-plan" title="What you need to include in your business plan and how to create one step-by-step" target="_blank" rel="noopener noreferrer">Create your business plan</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory.php">Stock control and inventory</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php">Financial and management accounts: the basics</a></li>
+			<li><a href="/it-and-ecommerce/introduction-to-it/an-overview-of-it-and-e-commerce.php">An overview of IT and e-commerce</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
