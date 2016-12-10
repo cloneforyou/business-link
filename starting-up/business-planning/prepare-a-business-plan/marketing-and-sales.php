ba1954d6ac76c4ed035b7235280a2f2102e644db
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-10 18:12:53 +0000

    Completed ‘Marketing and sales’ page.

---
+++ starting-up/business-planning/prepare-a-business-plan/marketing-and-sales.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Marketing and sales | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Prepare a business plan</h1>
+		<h2>Marketing and sales</h2>
+		<p>This section should describe the specific activities you intend to use to promote and sell your products and services. It's often the weak link in business plans so it's worth spending time on it to make sure it's both realistic and achievable.</p>
+		<p>A strong sales and marketing section means you have a clear idea of how you will get your products and services to market.</p>
+		<p>Your plan will need to provide answers to these questions:</p>
+		<ul>
+			<li>How do you plan to <strong>position</strong> your product or service in the market place? For further information, see our guide on how to <a href="/sales-and-marketing/marketing/create-your-marketing-strategy.php">create your marketing strategy</a>.</li>
+			<li>Who are your <strong>customers</strong>? Include details of customers who have shown an interest in your product or service and explain how you plan to go about attracting new customers. See our guide: <a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs.php">know your customers' needs</a>.</li>
+			<li>What is your <strong>pricing</strong> policy? How much will you charge for different customer segments, quantities, etc? See our guide on how to <a href="/sales-and-marketing/pricing/price-your-product-or-service.php">price your product or service</a>.</li>
+			<li>How will you <strong>promote</strong> your product or service? Identify your sales process methods, eg direct marketing, advertising, PR, email, e-sales, social marketing. See our guide on sales &amp; marketing: the basics.</li>
+			<li>How will you <strong>reach</strong> your customers? What channels will you use? Which partners will be needed in your distribution channels? See our guide on how to <a href="/sales-and-marketing/keeping-your-customers/reach-your-customers-effectively.php">reach your customers effectively</a>.</li>
+			<li>How will you do your <strong>selling</strong>? Do you have a sales plan? Have you considered which sales method will be the most effective and most appropriate for your market, such as selling by phone, over the internet, face-to-face or through retail outlets? Are your proposed sales methods consistent with your marketing plan? And do you have the right skills to secure the sales you need? See our guide on <a href="/sales-and-marketing/selling/the-sales-process.php">the sales process</a>.</li>
+		</ul>
+		<p>See our videos and e-learning resources to <a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101953&amp;type=HUBCHASSIS" title="How business planning can help you develop your sales and marketing strategy" target="_blank" rel="noopener noreferrer">develop your sales and marketing strategy</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-markets-and-competitors.php" title="Your markets and competitors">Prev</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-teams-skills.php" title="Your team's skills">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php" title="Prepare a business plan">Introduction</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-audience-for-your-business-plan.php">The audience for your business plan</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/what-a-business-plan-should-include.php">What a business plan should include</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-executive-summary.php">The executive summary</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-business-its-products-and-services.php">Your business, its products and services</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-markets-and-competitors.php">Your markets and competitors</a></li>
+			<li>Marketing and sales</li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-teams-skills.php">Your team's skills</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-operations.php">Your operations</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/financial-forecasts.php">Financial forecasts</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/presenting-your-business-plan.php">Presenting your business plan</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/using-the-business-plan-template.php">Using the business plan template</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/here-is-how-having-an-up-to-date-business-plan-helped-my-business.php">Here's how having an up-to-date business plan helped my business</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/advertising-branding-and-design/choosing-the-right-advertising-media.php">Use our interactive tool to investigate what will be the best advertising media for your business</a></li>
+			<li><a href="../../_files/business-plan-template.doc" target="_blank" rel="noopener noreferrer">Download our business plan template (DOC, 458K)</a></li>
+			<li><a href="../../_files/business-plan-template.odt" target="_blank" rel="noopener noreferrer">Download our business plan template in ODT format (ODT, 33K)</a></li>
+		</ul>
+				
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101953&amp;type=HUBCHASSIS" title="How business planning can help you develop your sales and marketing strategy" target="_blank" rel="noopener noreferrer">Develop your sales and marketing strategy</a></li>
+			<li><a href="https://www.gov.uk/write-business-plan" title="What you need to include in your business plan and how to create one step-by-step" target="_blank" rel="noopener noreferrer">Create your business plan</a></li>
+			<li><a href="/sales-and-marketing/marketing/create-your-marketing-strategy.php">Create your marketing strategy</a></li>
+			<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs.php">Know your customers' needs</a></li>
+			<li><a href="/sales-and-marketing/pricing/price-your-product-or-service.php">Price your product or service</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
