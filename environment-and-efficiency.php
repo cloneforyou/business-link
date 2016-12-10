5f2861ec921d1cbffc64fabd317812341e350145
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-10 23:12:05 +0000

    Completed ‘Environment & efficiency’ page.

---
+++ environment-and-efficiency.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Environment &amp; efficiency | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+			
+		<h1>Environment &amp; efficiency</h1>
+		<h2>Environmental compliance and responsibility</h2>
+		<ul>
+			<li><a href="/environment-and-efficiency/your-environmental-responsibilities-an-overview.php">Your environmental responsibilities - an overview</a></li>
+			<li><a href="/environment-and-efficiency/permits-and-licences.php">Permits and licences</a></li>
+			<li><a href="/environment-and-efficiency/waste-legislation-and-responsibility.php">Waste legislation and responsibility</a></li>
+			<li><a href="/environment-and-efficiency/managing-hazardous-substances.php">Managing hazardous substances</a></li>
+			<li><a href="/environment-and-efficiency/preventing-and-managing-pollution.php">Preventing and managing pollution</a></li>
+		</ul>
+		<h2>Reduce, reuse and recycle</h2>
+		<ul>
+			<li><a href="/environment-and-efficiency/reducing-and-recycling-waste.php">Reducing and recycling waste</a></li>
+		</ul>
+		<h2>Save money and resources</h2>
+		<ul>
+			<li><a href="/environment-and-efficiency/making-your-business-more-resource-efficient.php">Making your business more resource efficient</a></li>
+			<li><a href="/environment-and-efficiency/cut-energy-costs.php">Cut energy costs</a></li>
+			<li><a href="/environment-and-efficiency/cut-water-costs.php">Cut water costs</a></li>
+			<li><a href="/environment-and-efficiency/tax-breaks-finance-and-incentives.php">Tax breaks, finance and incentives</a></li>
+		</ul>
+		<h2>Reducing carbon emissions</h2>
+		<ul>
+			<li><a href="/environment-and-efficiency/carbon-reduction.php">Carbon reduction</a></li>
+			<li><a href="/environment-and-efficiency/adapt-to-climate-change.php">Adapt to climate change</a></li>
+			<li><a href="/environment-and-efficiency/transport-and-fuel.php">Transport and fuel</a></li>
+		</ul>
+		<h2>Improving your environmental performance</h2>
+		<ul>
+			<li><a href="/environment-and-efficiency/measuring-and-reporting-environmental-performance.php">Measuring and reporting environmental performance</a></li>
+			<li><a href="/environment-and-efficiency/using-an-environmental-management-system-ems.php">Using an environmental management system (EMS)</a></li>
+			<li><a href="/environment-and-efficiency/making-your-business-more-sustainable.php">Making your business more sustainable</a></li>
+			<li><a href="/environment-and-efficiency/sources-of-environmental-advice.php">Sources of environmental advice</a></li>
+		</ul>
+		<h2>Green business strategy and marketing</h2>
+		<ul>
+			<li><a href="/environment-and-efficiency/the-business-case-for-environmental-improvements.php">The business case for environmental improvements</a></li>
+			<li><a href="/environment-and-efficiency/marketing-and-pr.php">Marketing and PR</a></li>
+		</ul>
+		<h2>Goods and services</h2>
+		<ul>
+			<li><a href="/environment-and-efficiency/product-and-packaging-design.php">Product and packaging design</a></li>
+			<li><a href="/environment-and-efficiency/your-supply-chain.php">Your supply chain</a></li>
+			<li><a href="/environment-and-efficiency/selling-and-marketing.php">Selling and marketing</a></li>
+		</ul>
+		<h2>Environmental guidance by business sector</h2>
+		<ul>
+			<li><a href="/environment-and-efficiency/chemical-industry.php">Chemical industry</a></li>
+			<li><a href="/environment-and-efficiency/construction-and-building-trades.php">Construction and building trades</a></li>
+			<li><a href="/environment-and-efficiency/electrical-and-electronic-equipment-manufacturing.php">Electrical and electronic equipment manufacturing</a></li>
+			<li><a href="/environment-and-efficiency/engineering-and-metalworking.php">Engineering and metalworking</a></li>
+			<li><a href="/environment-and-efficiency/food-and-drink-production.php">Food and drink production</a></li>
+			<li><a href="/environment-and-efficiency/furniture-manufacturing.php">Furniture manufacturing</a></li>
+			<li><a href="/environment-and-efficiency/metal-production-and-processing.php">Metal production and processing</a></li>
+			<li><a href="/environment-and-efficiency/offices-retail-and-hospitality.php">Offices, retail and hospitality</a></li>
+			<li><a href="/environment-and-efficiency/paper-and-cardboard-manufacturing.php">Paper and cardboard manufacturing</a></li>
+			<li><a href="/environment-and-efficiency/printing-industry.php">Printing industry</a></li>
+			<li><a href="/environment-and-efficiency/waste-and-recycling-industry.php">Waste and recycling industry</a></li>
+		</ul>
+
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/sitemap?topicId=1079068363" target="_blank" rel="noopener noreferrer">Environment &amp; efficiency - site map</a></p>
+		
+		<h3>Spotlight</h3>
+		<h4><a href="/climate-change-adaptation-survey.php">Climate change adaptation survey</a></h4>
+		
+		<hr />
+
+		<p>Find out about changes to business support on climate change adaptation</p>
+		<a href="/climate-change-adaptation-survey.php" title="From 1 October 2011, the Environment Agency is taking over responsibility for providing the government????s support to businesses in England on adapting to the effects of climate change">
+			<img src="../../_img/Climate_adaptation_image_for_EE_landing_page.jpg" width="76" height="76" alt="From 1 October 2011, the Environment Agency is taking over responsibility for providing the government????s support to businesses in England on adapting to the effects of climate change" />
+		</a>
+
+		<h3>Do it online</h3>
+		<ul>
+			<li><a href="/environment-and-efficiency/measuring-and-reporting-environmental-performance/assess-your-environmental-compliance.php">Assess your environmental compliance</a></li>
+			<li><a href="/environment-and-efficiency/the-business-case-for-environmental-improvements/identify-where-you-can-save-money-by-going-green.php">Identify where you can save money by going green</a></li>
+			<li><a href="https://web.archive.org/web/20120111054629/https://online.businesslink.gov.uk/bdotg/action/ruAlertManage" target="_blank" rel="noopener noreferrer">Sign up for email alerts about new and changing regulations</a></li>
+			<li><a href="http://shop.bsigroup.com/BusinessLinks/BusinessLinkLP/" target="_blank" rel="noopener noreferrer">View business standards</a></li>
+		</ul>
+
+		<h3>What's popular</h3>
+		<ul>
+			<li><a href="/environment-and-efficiency/waste-legislation-and-responsibility/duty-of-care-your-waste-responsibilities.php">Duty of care - your waste responsibilities</a></li>
+			<li><a href="/environment-and-efficiency/the-business-case-for-environmental-improvements/benefits-of-improving-your-environmental-performance.php">Benefits of improving your environmental performance</a></li>
+			<li><a href="/environment-and-efficiency/using-an-environmental-management-system-ems/environmental-management-systems-ems-the-basics.php">Environmental management systems (EMS) - the basics</a></li>
+			<li><a href="/environment-and-efficiency/permits-and-licences/environmental-permits-and-licences-an-overview.php">Environmental permits and licences - an overview</a></li>
+		</ul>
+
+		<h3>Directories</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://www.businesslink.gov.uk/bdotg/action/event?topicId=1079068363" target="_blank" rel="noopener noreferrer">Environment &amp; efficiency events near you</a></li>
+			<li><a href="/find-a-case-study/browse-by-theme-environment-and-efficiency.php">Environment &amp; efficiency case studies</a></li>
+		</ul>
+
+
+	</div><!-- // .small-12 .large-6 .columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
