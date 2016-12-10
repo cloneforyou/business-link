e211af184c27ec425abd05a1847ca9b2189a4d0b
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-10 17:53:53 +0000

    Completed ‘Your business, its products and services’ page.

---
+++ starting-up/business-planning/prepare-a-business-plan/your-business-its-products-and-services.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Your business, its products and services | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Prepare a business plan</h1>
+		<h2>Your business, its products and services</h2>
+		<p>You must be able to clearly describe what your business does, whether you are writing the business plan for your own purposes or if you want other people to provide funds through investments or loans.</p>
+		<p>This part of the plan sets out your <strong>vision</strong> for your new business and includes who you are, what you do, what you have to offer and the market you want to address.</p>
+		<p>Start with an overview of your business:</p>
+		<ul>
+			<li>when you started or intend to start trading and the progress and investment you have made to date</li>
+			<li>the type of business and the sector it is in</li>
+			<li>any relevant history - for example, if you acquired the business, who owned it originally and what they achieved with it</li>
+			<li>the current legal structure</li>
+			<li>your vision for the future</li>
+		</ul>
+		<p>Then describe your products or services as simply as possible, defining:</p>
+		<ul>
+			<li>what makes it different</li>
+			<li>what benefits it offers</li>
+			<li>why customers would buy it from you instead of your competitors</li>
+			<li>how you plan to develop your products or services</li>
+			<li>whether you hold any patents, trade marks or design registration</li>
+			<li>the key features and success factors of your industry or sector</li>
+		</ul>
+		<p>Remember that the person reading the plan may not understand your business and its products, services or processes as well as you do, so try to avoid jargon. It's a good idea to get someone who isn't involved in the business - a friend or family member perhaps - to read this section of your plan and make sure they can understand it.</p>
+		<p>See our videos and e-learning resources to <a href="https://www.gov.uk/write-business-plan" title="What you need to include in your business plan and how to create one step-by-step" target="_blank" rel="noopener noreferrer">create your business plan</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-executive-summary.php" title="The executive summary">Prev</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-markets-and-competitors.php" title="Your markets and competitors">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php" title="Prepare a business plan">Introduction</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-audience-for-your-business-plan.php">The audience for your business plan</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/what-a-business-plan-should-include.php">What a business plan should include</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/the-executive-summary.php">The executive summary</a></li>
+			<li>Your business, its products and services</li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-markets-and-competitors.php">Your markets and competitors</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/marketing-and-sales.php">Marketing and sales</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-teams-skills.php">Your team's skills</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/your-operations.php">Your operations</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/financial-forecasts.php">Financial forecasts</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/presenting-your-business-plan.php">Presenting your business plan</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/using-the-business-plan-template.php">Using the business plan template</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/here-is-how-having-an-up-to-date-business-plan-helped-my-business.php">Here's how having an up-to-date business plan helped my business</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="../../_files/business-plan-template.doc" target="_blank" rel="noopener noreferrer">Download our business plan template (DOC, 458K)</a></li>
+			<li><a href="../../_files/business-plan-template.odt" target="_blank" rel="noopener noreferrer">Download our business plan template in ODT format (ODT, 33K)</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/business-finance-support-finder" title="How completing a business plan can help you improve your business' chance of success" target="_blank" rel="noopener noreferrer">Understand the importance of business planning</a></li>
+			<li><a href="https://www.gov.uk/write-business-plan" title="What you need to include in your business plan and how to create one step-by-step" target="_blank" rel="noopener noreferrer">Create your business plan</a></li>
+			<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs.php">Know your customers' needs</a></li>
+			<li><a href="/sales-and-marketing/pricing/price-your-product-or-service.php">Price your product or service</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
