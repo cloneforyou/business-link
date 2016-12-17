0b30fe7106ea3846dacf4d9dfe89e0288c0e522c
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 23:30:54 +0000

    Completed ‘Changes for 2011-12’ page.

---
+++ statutory-payment-rates.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Changes for 2011-12 | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Statutory payment rates</h1>
+		<h2>Changes for 2011-12</h2>
+		<p>Employers may have to pay employees Statutory Sick Pay (SSP), Statutory Maternity Pay (SMP), Ordinary Statutory Paternity Pay (OSPP), Additional Statutory Paternity Pay (ASPP) or Statutory Adoption Pay (SAP).</p>
+		<p>The following statutory payment rates have been announced for 2011-12. There is also a change to the additional amount small employers get back.</p>
+		
+		<p><strong>Statutory payment rates</strong></p>
+		<table>
+			<tbody>
+				<tr>
+					<th valign="top"><strong>Statutory payment type</strong></th>
+					<th valign="top"><strong>Pay rate for 2011-12</strong></th>
+				</tr>
+				<tr>
+					<th valign="top">SSP</th>
+					<td valign="top">&pound;81.60 per week</td>
+				</tr>
+				<tr>
+					<th valign="top">SMP</th>
+					<td valign="top">Six weeks at 90% of the employee's average weekly earnings followed by a further 33 weeks at &pound;128.73 per week or 90% of average weekly earnings - whichever is lower.</td>
+				</tr>
+				<tr>
+					<th valign="top">OSPP</th>
+					<td valign="top">One or two weeks' at &pound;128.73 per week or 90% of average weekly earnings - whichever is lower. If the employee opts to take two weeks' leave, they must be taken together.</td>
+				</tr>
+				<tr>
+					<th valign="top">ASPP</th>
+					<td valign="top">Paid during the mother or adopter's SMP, Maternity Allowance (MA) or SAP pay period, if the mother or adopter has returned to work, at &pound;128.73 per week or 90% of average weekly earnings - whichever is lower.</td>
+				</tr>
+				<tr>
+					<th valign="top">SAP</th>
+					<td valign="top">
+						<p>39 weeks at &pound;128.73 per week or 90% of weekly earnings - whichever is lower.</p>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<p>For employees earning &pound;102 or more per week in 2011-12, the above rate of SSP applies from 6 April 2011, and for SMP, OSPP, ASPP and SAP the above rates apply for complete pay weeks commencing on or after 3 April 2011 (the first Sunday in April).</p>
+		<p>The threshold for recovery of SSP under the percentage threshold scheme remains unchanged from last year at 13 per cent.</p>
+		
+		<h3>Change in the additional payment made to small employers</h3>
+		<p>From 6 April 2011, employers who <strong>qualify</strong> for the small employers rate of reimbursement, can recover 100 per cent of the SMP, OSPP, ASPP and SAP paid to their employees, plus an additional amount of 3 per cent (previously 4.5 per cent) in compensation for the employers share of National Insurance Contributions (NICs) due on these payments. This means that small employers can claim back a total of 103 per cent of the SMP, OSPP, ASPP and SAP they pay out.</p>
+		<p>Employers who <strong>do not qualify</strong> for the small employers' rate can recover 92 per cent of the SMP, OSPP, ASPP and SAP paid to their employees.</p>
+		
+		<h3>What is the additional amount paid to small employers?</h3>
+		<p>Small employers can get back all of the SMP, OSPP, ASPP and SAP they pay out plus an additional amount in compensation for the employer's NICs due to be paid on those payments. This is often known as the small employers' compensation rate.</p>
+		<p>A small employer is an employer who has paid total gross Class 1 NICs below an annual threshold in a qualifying tax year. For the 2011-12 tax year, the annual threshold is &pound;45,000 or less - this has not changed from the 2010-11 tax year.</p>
+		
+		<h3>Why has the rate of the additional amount changed?</h3>
+		<p>The additional amount is calculated using a formula laid down in legislation based on the employers' share of NICs paid and is reviewed each year to see if it needs to be adjusted to reflect the shift in the ratio of SMP, OSPP, ASPP and SAP paid out and the employers' NICs paid on these payments.</p>
+		<p>The employer's NICs threshold for the 2011-12 tax year rose from &pound;110 to &pound;136 per week. This means that from 6 April 2011, NICs are not payable on the &pound;128.73 weekly standard rate of any relevant statutory payment, so employers will be paying less NICs on statutory payments.</p>
+		<p>The change in the additional amount - from 4.5 per cent to 3 per cent - reflects this reduction on employers NICs payments.</p>
+		<p>For more information about calculating and recovering statutory payments, see our section: <a href="/tax-payroll-and-company-information/paye-for-employers/maternity-paternity-adoption-sickness.php">maternity, paternity, adoption, sickness</a>.</p>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<img src="../../_img/key_decisions_large.jpg" width="181" height="121" border="0" alt="" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
