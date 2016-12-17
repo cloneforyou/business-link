350475c727cb2938413081b3efdea1a5fa5817fd
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 23:32:44 +0000

    Completed ‘Supporting long-term unemployed and sick people back into work’ page.

---
+++ the-work-programme.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Supporting long-term unemployed and sick people back into work | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>The Work Programme</h1>
+		<h2>Supporting long-term unemployed and sick people back into work</h2>
+		<p>The Work Programme is the biggest single payment by results employment programme ever introduced. It is delivered by private and voluntary organisations called providers. They are paid to help long-term unemployed people, claiming Jobseeker's Allowance or sickness benefits, into work.</p>
+		<p>The Work Programme providers are free to innovate and design support that is tailored to an individual's needs. They will work closely with employers in their area to understand and support the local labour market. Your local provider could help you with your recruitment needs and find the right person for the job.</p>
+		<p><a href="https://www.gov.uk/government/publications/work-programme-dwp-provider-guidance" target="_blank" rel="noopener noreferrer">Find information about the Work Programme and contact details for your local providers on the DWP website</a>.</p>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
