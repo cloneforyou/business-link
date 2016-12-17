6bfee8c9c1eb17e185b8f63cce32a4d0f31af286
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 21:13:04 +0000

    Completed ‘Stock control systems - keeping track manually’ page.

---
+++ starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-manually.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Stock control systems - keeping track manually | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Stock control and inventory</h1>
+		<h2>Stock control systems - keeping track manually</h2>
+		<p>Stocktaking involves making an <strong>inventory</strong>, or list, of stock, and noting its location and value. It's often an annual exercise - a kind of audit to work out the value of the stock as part of the accounting process.</p>
+		<p><strong>Codes</strong>, including barcodes, can make the whole process much easier but it can still be quite time consuming. Checking stock more frequently - a rolling stocktake - avoids a massive annual exercise, but demands constant attention throughout the year. Radio Frequency Identification (RFID) tagging using hand-held readers can offer a simple and efficient way to maintain a continuous check on inventory. See the page in this guide on <a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/using-rfid-for-inventory-control-stock-security-and-quality-management.php">using RFID for inventory control, stock security and quality management</a>.</p>
+		<p>Any stock control system must enable you to:</p>
+		<ul>
+			<li>track stock levels</li>
+			<li>make orders</li>
+			<li>issue stock</li>
+		</ul>
+
+		<p>The simplest manual system is the <strong>stock book</strong>, which suits small businesses with few stock items. It enables you to keep a log of stock received and stock issued.</p>
+		<p>It can be used alongside a simple <strong>reorder system</strong>. For example, the two-bin system works by having two containers of stock items. When one is empty, it's time to start using the second bin and order more stock to fill up the empty one.</p>
+		<p><strong>Stock cards</strong> are used for more complex systems. Each type of stock has an associated card, with information such as:</p>
+		<ul>
+			<li>description</li>
+			<li>value</li>
+			<li>location</li>
+			<li>reorder levels, quantities and lead times (if this method is used)</li>
+			<li>supplier details</li>
+			<li>information about past stock history</li>
+		</ul>
+
+		<p>More sophisticated manual systems incorporate <strong>coding</strong> to classify items. Codes might indicate the value of the stock, its location and which batch it is from, which is useful for quality control.</p>
+		
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-methods.php" title="Stock control methods">Prev</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-using-computer-software.php" title="Stock control systems - keeping track using computer software">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory.php" title="Stock control and inventory">Introduction</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/types-of-stock.php">Types of stock</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/how-much-stock-should-you-keep.php">How much stock should you keep?</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-methods.php">Stock control methods</a></li>
+			<li>Stock control systems - keeping track manually</li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-using-computer-software.php">Stock control systems - keeping track using computer software</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/using-rfid-for-inventory-control-stock-security-and-quality-management.php">Using RFID for inventory control, stock security and quality management</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-security.php">Stock security</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/control-the-quality-of-your-stock.php">Control the quality of your stock</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-administration.php">Stock control administration</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/here-is-how-I-control-my-stock.php">Here's how I control my stock</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.cips.org/en-au/training/training-courses1/inventory-management/" target="_blank" rel="noopener noreferrer">Inventory management course on the Chartered Institute of Purchasing and Supply website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/workplace-health-and-safety/safer-ways-of-working/storing-goods-and-materials.php">Storing goods and materials</a></li>
+			<li><a href="/tax-payroll-and-company-information/set-up-a-basic-record-keeping-system.php">Set up a basic record-keeping system</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/avoid-the-problems-of-overtrading.php">Avoid the problems of overtrading</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
