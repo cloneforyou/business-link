78f6a57c5e1d38b7736b30740892c5e92a0f56e7
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 21:22:10 +0000

    Completed ‘Stock control systems - keeping track using computer software’ page.

---
+++ starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-using-computer-software.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Stock control systems - keeping track using computer software | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Stock control and inventory</h1>
+		<h2>Stock control systems - keeping track using computer software</h2>
+		<p>Computerised stock control systems run on similar principles to manual ones, but are more flexible and information is easier to retrieve. You can quickly get a stock valuation or find out how well a particular item of stock is moving.</p>
+		<p>A computerised system is a good option for businesses dealing with many different types of stock. Other useful features include:</p>
+		<ul>
+			<li>Stock and pricing data integrating with accounting and invoicing systems. All the systems draw on the same set of data, so you only have to input the data once. <strong>Sales Order Processing</strong> and <strong>Purchase Order Processing</strong> can be integrated in the system so that stock balances and statistics are automatically updated as orders are processed.</li>
+			<li>Automatic stock monitoring, triggering orders when the re-order level is reached.</li>
+			<li>Automatic batch control if you produce goods in batches.</li>
+			<li>Identifying the cheapest and fastest suppliers.</li>
+			<li>Bar coding systems which speed up processing and recording. The software will print and read bar codes from your computer.</li>
+			<li>Radio Frequency Identification (RFID) which enables individual products or components to be tracked throughout the supply chain. See the page in this guide on <a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/using-rfid-for-inventory-control-stock-security-and-quality-management.php">using RFID for inventory control, stock security and quality management</a>.</li>
+		</ul>
+
+		<p>The system will only be as good as the data put into it. Run a thorough <strong>stocktake</strong> before it goes 'live' to ensure accurate figures. It's a good idea to run the previous system alongside the new one for a while, giving you a back-up and enabling you to check the new system and sort out any problems.</p>
+		
+		<h3>Choose a system</h3>
+		<p>There are many software systems available. Talk to others in your line of business about the software they use, or contact your trade association for advice.</p>
+		<p>Make a checklist of your requirements. For example, your needs might include:</p>
+		<ul>
+			<li>multiple prices for items</li>
+			<li>prices in different currencies</li>
+			<li>automatic updating, selecting groups of items to update, single-item updating</li>
+			<li>using more than one warehouse</li>
+			<li>ability to adapt to your changing needs</li>
+			<li>quality control and batch tracking</li>
+			<li>integration with other packages</li>
+			<li>multiple users at the same time</li>
+		</ul>
+
+		<p>Avoid choosing software that's too complicated for your needs as it will be a waste of time and money.</p>
+		
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-manually.php" title="Stock control systems - keeping track manually">Prev</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/using-rfid-for-inventory-control-stock-security-and-quality-management.php" title="Using RFID for inventory control, stock security and quality management">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory.php" title="Stock control and inventory">Introduction</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/types-of-stock.php">Types of stock</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/how-much-stock-should-you-keep.php">How much stock should you keep?</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-methods.php">Stock control methods</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-manually.php">Stock control systems - keeping track manually</a></li>
+			<li>Stock control systems - keeping track using computer software</li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/using-rfid-for-inventory-control-stock-security-and-quality-management.php">Using RFID for inventory control, stock security and quality management</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-security.php">Stock security</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/control-the-quality-of-your-stock.php">Control the quality of your stock</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-administration.php">Stock control administration</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/here-is-how-I-control-my-stock.php">Here's how I control my stock</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.apics.org/search?keyword=%22scor%22#keyword=%22scor%22&it=resources" target="_blank" rel="noopener noreferrer">Supply Chain Operations Reference model on the Supply Chain Council website</a></li>
+			<li><a href="https://www.gs1uk.org/get-a-barcode" target="_blank" rel="noopener noreferrer">Bar codes and unique numbers information on the GS1 UK website</a></li>
+			<li><a href="/it-and-ecommerce/introduction-to-it/setting-up-a-basic-it-system.php">Use our interactive tool to find out which computer equipment you should buy for your business</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/software-and-business-applications/supply-chain-software.php">Supply chain software</a></li>
+			<li><a href="/it-and-ecommerce/introduction-to-it/an-overview-of-it-and-e-commerce.php">An overview of IT and e-commerce</a></li>
+			<li><a href="/tax-payroll-and-company-information/set-up-a-basic-record-keeping-system.php">Set up a basic record-keeping system</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/avoid-the-problems-of-overtrading.php">Avoid the problems of overtrading</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
