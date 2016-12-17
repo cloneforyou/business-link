eeb0760ef80069fa2e97b434692b8b5cbed29b62
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 23:55:27 +0000

    Completed ‘SMEs are changing the way they do business’ page.

---
+++ web-fuelled-business.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SMEs are changing the way they do business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Web Fuelled Business</h1>
+		<h2>SMEs are changing the way they do business</h2>
+		<p>Greater and more effective use of the web and associated technologies can help small business owners and managers realise growth and efficiency. Small and medium-sized enterprises (SMEs) need to adopt a strategy that incorporates strong web-based approaches in their business model that will better place them to meet the global competitive challenge, increase competitive advantage and thus enhance profitability and job creation.</p>
+		<p>Adopting a web-based approach is more than just selling online. It's about such things as driving marketing through social media, increasing website exposure through search engine optimisation and procuring and enhancing supply chains through business-to-business websites. <strong>Web Fuelled Business</strong> provides tools helping small firms achieve global and local reach and increased productivity.
+			<br />
+			<br /> Doug Richards (a former Dragon from Dragon's Den), with support from the Department for Business Innovation and Skills (BIS), has created a series of free videos which will teach you how to fully leverage the Internet.
+			<br />
+			<br /> You will learn how to create an identity across the web, build a brand through social media and search engine marketing. You will also learn how to find customers through pay per click and viral other techniques, how to sell online effectively and build a website that has an impact on your customers.
+			<br />
+			<br /> You can now view these videos <strong>free of charge</strong>:</p>
+		
+		<p><strong>Class 1 Starting up: 10 essential questions<br />
+		</strong>Learn how to define your business proposition, understand the customer promise, articulate the promise concisely and set the stage for building the business proposition.
+		<br />Watch <a href="#">What do we do that people need or want?</a></p>
+		
+		<p><strong>Class 2 Guide to Ecommerce</strong><br />
+		In this class we review the five pillars of successful e-commerce sites: discovery, selection, price, availability, and delivery.
+		<br />Watch <a href="#">The five pillars of success</a>.</p>
+		
+		<p><strong>Class 3 Emarketing: how to be relevant</strong><br />
+		In this class we will teach you about Search Engine Optimisation (SEO) and how online marketing can transform the way in which you advertise your business, products and services.
+		<br />Watch <a href="#">Emarketing: how to be relevant</a>.</p>
+		
+		<p><strong>Class 4 Getting started with social media</strong><br />
+		Learn why and how social media matters to your business and the basics of developing a social media strategy that will help you achieve your business goals.
+		<br />Watch <a href="#">Getting started with social media</a>.</p>
+
+		<p><em>Editor note: http://schoolforstartups.co.uk/portfolio/web-fuelled-business/</em></p>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
