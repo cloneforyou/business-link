dfbe72c2276433c798a6de992abf25a147edaf05
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 23:47:46 +0000

    Completed ‘DWP launch new job posting and matching service’ page.

---
+++ universal-jobmatch.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DWP launch new job posting and matching service | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Universal Jobmatch</h1>
+		<h2>DWP launch new job posting and matching service</h2>
+		<p>The Department for Work and Pensions (DWP) will launch Universal Jobmatch in Autumn 2012, their new job posting and matching service.</p>
+		<p>This new streamlined service will allow employers to post and manage their vacancies on-line, to make it quicker and easier for them to find suitable jobseekers.</p>
+		<p>Universal Jobmatch is an important part of DWP's digitalisation agenda to make more of its services available on-line. The new service will replace Employer Direct and Employer Direct Online.</p>
+		<p>Please see the attached <a href="https://www.gov.uk/advertise-job" target="_blank" rel="noopener noreferrer">Frequently Asked Questions</a> for further detailed information.</p>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
