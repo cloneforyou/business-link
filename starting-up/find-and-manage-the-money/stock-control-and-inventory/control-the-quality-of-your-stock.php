a49909c50999d2808b26a1aac7a11d0970019260
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 21:42:22 +0000

    Completed ‘Control the quality of your stock’ page.

---
+++ starting-up/find-and-manage-the-money/stock-control-and-inventory/control-the-quality-of-your-stock.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Control the quality of your stock | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Stock control and inventory</h1>
+		<h2>Control the quality of your stock</h2>
+		<p>Quality control is a vital aspect of stock control - especially as it may affect the safety of customers or the quality of the finished product.</p>
+		<p>Efficient stock control should incorporate <strong>stock tracking</strong> and <strong>batch tracking</strong>. This means being able to trace a particular item backwards or forwards from source to finished product, and identifying the other items in the batch.</p>
+		<p>Goods should be checked systematically for quality, faults identified and the affected batch weeded out. This will allow you to raise any problems with your supplier and at the same time demonstrate the safety and quality of your product.</p>
+		<p>With a good computerised stock control system, this kind of tracking is relatively straightforward. Manual stock control methods can also use <strong>codes</strong> to systematise tracking and make it easier to trace particular batches.</p>
+		<p>Radio Frequency Identification (RFID) can be used to store information about a product or component's manufacturing date, to ensure that it is sold or processed in time. The system can also be used to trace faulty products quickly and efficiently. See the page in this guide on <a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/using-rfid-for-inventory-control-stock-security-and-quality-management.php">using RFID for inventory control, stock security and quality management</a>.</p>
+		<p>The British Standards Institution (BSI) has a scheme to certify businesses that have achieved a certain standard of quality management. Achieving the standard is one way of showing customers and regulators that you take quality control seriously.</p>
+		<p>The details vary according to the particular business sector. For some industries, it is a legal requirement to be certified - <a href="https://www.bsigroup.com/en-GB/iso-9001-quality-management/" target="_blank" rel="noopener noreferrer">view the guide to quality control on the BSI website</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-security.php" title="Stock security">Prev</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-administration.php" title="Stock control administration">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory.php" title="Stock control and inventory">Introduction</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/types-of-stock.php">Types of stock</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/how-much-stock-should-you-keep.php">How much stock should you keep?</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-methods.php">Stock control methods</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-manually.php">Stock control systems - keeping track manually</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-using-computer-software.php">Stock control systems - keeping track using computer software</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/using-rfid-for-inventory-control-stock-security-and-quality-management.php">Using RFID for inventory control, stock security and quality management</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-security.php">Stock security</a></li>
+			<li>Control the quality of your stock</li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-administration.php">Stock control administration</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/here-is-how-I-control-my-stock.php">Here's how I control my stock</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>BSI Standards and Publications</h3>
+		<p>020 8996 9001</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.bsigroup.com/en-GB/iso-9001-quality-management/" target="_blank" rel="noopener noreferrer">Quality control guidance on the BSI website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/quality-management-standards.php">Quality management standards</a></li>
+			<li><a href="/tax-payroll-and-company-information/set-up-a-basic-record-keeping-system.php">Set up a basic record-keeping system</a></li>
+			<li><a href="/grow-your-business/growth-through-strategic-sourcing/negotiate-the-right-deal-with-suppliers.php">Negotiate the right deal with suppliers</a></li>
+			<li><a href="/grow-your-business/growth-through-strategic-sourcing/manage-your-suppliers.php">Manage your suppliers</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
