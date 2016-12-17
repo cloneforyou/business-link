0e75f56965014d75853f4da4c371fe6c00166fa5
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 23:38:48 +0000

    Completed ‘Training Directory’ page.

---
+++ training-directory.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Training Directory | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Training Directory</h1>
+		<p>Welcome to our training directory. This directory provides you with ways to access business related courses and events designed to meet your needs and preferred method of learning.</p>
+		<table>
+			<tr>
+				<td>
+					<a href="#">
+						<img src="../../_img/TD_events.jpg" width="170" height="80" border="0" alt="Find events from a variety of organisations" />
+					</a>
+				</td>
+				<td>
+					<p><strong><a href="#">Find a workshop, event or seminar</a></p></strong>
+						<p>Learn more about running your business and network with other business managers and owners</p>
+				</td>
+			</tr>
+		</table>
+		<p>
+			<h3>Government-funded further education skills providers</h3>
+			<p>The National Careers Service (formerly Next Step) provides information, advice and guidance to help you make decisions on learning, training and work opportunities. The service offers confidential, helpful and impartial advice, supported by qualified careers advisers.</p>
+			<p>The service is freely available for people living in England. Links to organisations offering targeted advice for people living in Scotland, Northern Ireland or Wales are provided on the National Careers Service website.</p>
+			<p><a href="https://nationalcareersservice.direct.gov.uk/course-directory/home" target="_blank" rel="noopener noreferrer">Find training opportunities on the National Careers Service website</a>.</p>
+		</p>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
