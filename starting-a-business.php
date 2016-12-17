16e28f63ab42e8bb2c165eb58dbcfe117ed815f1
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 23:26:43 +0000

    Completed ‘Essential resources’ page.

---
+++ starting-a-business.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Essential resources | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business</h1>
+		<h2>Essential resources </h2>
+		<p>This page contains links to essential resources including useful guides, tools and case studies designed to help you start your business.</p>
+		<h3>Starting a Business - The No Nonsense Guide</h3>
+		<p>The Starting a Business - The No Nonsense Guide, produced in partnership with government departments, is designed in six easy-to-use modules so that you can select the ones that are right for you.</p>
+		
+		<p><strong>Module 1: Overview</strong> - provides a summary of the basic steps you will need to take in order to set up your own business. <a href="../../_files/Starting_up__Module_1.pdf" target="_blank" rel="noopener noreferrer">Download Starting a Business - The No Nonsense Guide. Module 1: Overview (PDF, 2.24 MB)</a>.</p>
+		
+		<p><strong>Module 2: Is starting a business right for you?</strong> - provides information and activities to help you decide whether running your own business will suit you. <a href="../../_files/Starting_up__Module_2.pdf" target="_blank" rel="noopener noreferrer">Download Starting a Business - The No Nonsense Guide. Module 2: Is starting a business right for you? (PDF, 1.31 MB)</a>.</p>
+		
+		<p><strong>Module 3: Planning your business</strong> - provides information on planning the practical and legal aspects of your business. <a href="../../_files/Starting_up__Module_3.pdf" target="_blank" rel="noopener noreferrer">Download Starting a Business - The No Nonsense Guide. Module 3: Planning your business (PDF, 1.15 MB)</a>.</p>
+		
+		<p><strong>Module 4: Finding and keeping customers</strong> - covers researching the market, getting your business offer right and successfully selling to customers. <a href="../../_files/Starting_up__Module_4.pdf" target="_blank" rel="noopener noreferrer">Download Starting a Business - The No Nonsense Guide. Module 4: Finding and keeping customers (PDF, 1.15 MB)</a>.</p>
+		
+		<p><strong>Module 5: Managing money</strong> - includes information on the key skills needed to organise your business finances and how to apply for funding. <a href="../../_files/Starting_up__Module_5.pdf" target="_blank" rel="noopener noreferrer">Download Starting a Business - The No Nonsense Guide. Module 5: Managing money (PDF, 1.82 MB)</a>.</p>
+		
+		<p><strong>Module 6: Your business plan</strong> - collates the information that you wrote in the worksheets in previous modules into an essential or comprehensive business plan. <a href="../../_files/Starting_up__Module_6.pdf" target="_blank" rel="noopener noreferrer">Download Starting a Business - The No Nonsense Guide. Module 6: Your business plan (PDF, 817K)</a>.</p>
+		
+		<p><a href="../../_files/business-plan-template.doc" target="_blank" rel="noopener noreferrer">Download our business plan template (DOC, 458K)</a>.</p>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<img src="../../_img/meeting.jpg" width="181" height="121" border="0" alt="" />
+		
+		<h3>Further resources</h3>
+		<ul>
+			<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Identify the right finance options for your business</a></li>
+			<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Find out what records you should be keeping</a></li>
+			<li><a href="/finance-and-grants/insurance/get-the-right-insurance-for-your-business.php">Get the right insurance for your business</a></li>
+			<li><a href="../../_files/business-plan-template.doc" target="_blank" rel="noopener noreferrer">Download our Business Plan template (DOC, 458K)</a></li>
+		</ul>
+		<h3>Regulations and compliance</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/licence-finder" target="_blank" rel="noopener noreferrer">Your Licence and Compliance Requirements</a> </li>
+		</ul>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
