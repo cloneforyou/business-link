776b0a97b8003923a3e43f2a275742c8c7a3b28c
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 21:38:36 +0000

    Completed ‘Stock security’ page.

---
+++ starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-security.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Stock security | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Stock control and inventory</h1>
+		<h2>Stock security</h2>
+		<p>Keeping stock secure depends on knowing what you have, where it is located and how much it is worth - so good records are essential. Stock that is portable, does not feature the business' logo, or is easy to sell on, is at particular risk.</p>
+		<h3>Thieves and shoplifters</h3>
+		<p>A thief coming in from outside is an obvious threat. Check the security around your premises to keep the risk to a minimum. In a shop, thieves may steal in groups - some providing a distraction while others take goods. Teach your staff to be alert and to recognise behaviour like this. Set up a clear policy and make sure staff are trained in dealing with thieves.</p>
+		<p>Offering to help a customer if you are suspicious will often prevent a theft. Avoid using confrontational words like 'steal' if you do have to approach a suspected thief, and avoid getting into a dangerous situation.</p>
+		<h3>Protect your stock</h3>
+		<ul>
+			<li>Identify and mark expensive portable equipment (such as computers). If possible, fit valuable stock with security tags - such as Radio Frequency Identification tags - which will sound an alarm if they are moved.</li>
+			<li>Don't leave equipment hanging around after delivery. Put it away in a secure place, record it and clear up packaging. It is a good idea to dispose of packaging securely - leaving boxes in view could be an advertisement to thieves.</li>
+			<li>Carry out regular stocktakes.</li>
+			<li>Put CCTV in car parks and other key locations.</li>
+		</ul>
+
+		<p><a href="http://www.met.police.uk/business/index.htm" target="_blank" rel="noopener noreferrer">Read crime prevention advice for businesses on the Metropolitan Police website</a>.</p>
+		
+		<h3>Theft by staff</h3>
+		<p>Theft by employees can sometimes be a problem. To prevent this:</p>
+		<ul>
+			<li>Train staff about your security systems and your disciplinary policies and procedures. Training about the cost of stock theft will help, as many people aren't aware of the implications for company turnover and job security.</li>
+			<li>Set up procedures to prevent theft. Staff with financial responsibilities should not be in charge of stock records.</li>
+			<li>Restrict access to warehouses, stockrooms and stationery cupboards.</li>
+			<li>Regularly change staff controlling stock to avoid collusion or bad practice.</li>
+		</ul>
+
+		<p>Find out more in our guide on <a href="/premises-and-property/protecting-your-premises/security-and-crime-prevention.php">security and crime prevention</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/using-rfid-for-inventory-control-stock-security-and-quality-management.php" title="Using RFID for inventory control, stock security and quality management">Prev</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/control-the-quality-of-your-stock.php" title="Control the quality of your stock">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory.php" title="Stock control and inventory">Introduction</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/types-of-stock.php">Types of stock</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/how-much-stock-should-you-keep.php">How much stock should you keep?</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-methods.php">Stock control methods</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-manually.php">Stock control systems - keeping track manually</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-using-computer-software.php">Stock control systems - keeping track using computer software</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/using-rfid-for-inventory-control-stock-security-and-quality-management.php">Using RFID for inventory control, stock security and quality management</a></li>
+			<li>Stock security</li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/control-the-quality-of-your-stock.php">Control the quality of your stock</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-administration.php">Stock control administration</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/here-is-how-I-control-my-stock.php">Here's how I control my stock</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>BSI Standards and Publications</h3>
+		<p>020 8996 9001</p>
+		<h3>BSIA Helpline</h3>
+		<p>0845 389 3889</p>
+
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.met.police.uk/business/index.htm" target="_blank" rel="noopener noreferrer">Crime prevention guidance on the Metropolitan Police Service website</a></li>
+			<li><a href="https://web-beta.archive.org/web/20120305003349/http://www.bsia.co.uk/MK10IQ45986_p;MK101Y26750_gp;LY8M9N53879" target="_blank" rel="noopener noreferrer">Security tips on the British Security Industry Association website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/premises-and-property/protecting-your-premises/security-and-crime-prevention.php">Security and crime prevention</a></li>
+			<li><a href="/employment-and-skills/staff-documents-and-employment-policies/monitoring-and-security-of-staff.php">Monitoring and security of staff</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
