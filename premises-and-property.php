32129b1f467adf5e5d63b94e62eddcf566c695ca
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 23:01:16 +0000

    Completed ‘Premises and property’ page.

---
+++ premises-and-property.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Premises and property | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+	
+		<h1>Premises and property</h1>
+		<h2>Business premises</h2>
+		<ul>
+			<li><a href="/premises-and-property/choosing-the-right-premises.php">Choosing the right premises</a></li>
+			<li><a href="/premises-and-property/tax-breaks-and-finance-for-your-business.php">Tax breaks and finance for your business</a></li>
+			<li><a href="/premises-and-property/protecting-your-premises.php">Protecting your premises</a></li>
+		</ul>
+
+		<h2>Adapting and improving your premises</h2>
+		<ul>
+			<li><a href="/premises-and-property/planning-permission-and-building-regulations.php">Planning permission and building regulations</a></li>
+			<li><a href="/premises-and-property/access-and-facilities.php">Access and facilities</a></li>
+			<li><a href="/premises-and-property/improving-energy-performance.php">Improving energy performance</a></li>
+		</ul>
+		<p><a href="#">Premises and property - site map</a></p>
+		
+		<h3>Spotlight</h3>
+		<h4><a href="/tax-payroll-and-company-information/business-rates/business-rates-relief.php">Business rates relief</a></h4>
+		
+		<hr />
+
+		<p>Information on schemes and reliefs which may allow your business to pay lower rates</p>
+		<a href="/tax-payroll-and-company-information/business-rates/business-rates-relief.php">
+			<img src="../../_img/business_rates_relief.jpg" width="76" height="76" alt="Information on schemes and reliefs which may allow your business to pay lower rates" />
+		</a>
+		
+		<h3>Do it online</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/ruLanding" target="_blank" rel="noopener noreferrer">Sign up for email alerts about new and changing regulations in this theme</a></li>
+			<li><a href="https://www.gov.uk/licence-finder" target="_blank" rel="noopener noreferrer">Use our interactive tool to find licences and trade bodies for your business</a></li>
+			<li><a href="/finance-and-grants/insurance/get-the-right-insurance-for-your-business.php">Use our interactive tool to work out which forms of insurance you should have for your business</a></li>
+		</ul>
+		
+		<h3>What's popular</h3>
+		<ul>
+			<li><a href="/workplace-health-and-safety/protecting-your-business/fire-safety-and-risk-assessment.php">Fire safety and risk assessment</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business.php">Choose the right premises for your business</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/commercial-property-responsibilities-of-business-tenants.php">Commercial property: responsibilities of business tenants</a></li>
+			<li><a href="/your-business-sector/property-management/residential-landlords-the-basics/disabled-facilities-grant-a-guide-for-residential-landlords.php">Disabled facilities grant: a guide for residential landlords</a></li>
+		</ul>
+		
+		<h3>Directories</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://www.businesslink.gov.uk/bdotg/action/event?topicId=1086951342" target="_blank" rel="noopener noreferrer">Premises and property events near you</a></li>
+			<li><a href="/find-a-case-study/browse-by-theme-premises-and-property.html">Premises and property case studies</a></li>
+		</ul>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
