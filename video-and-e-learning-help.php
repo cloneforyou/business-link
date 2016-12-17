91a91066d75d0050f14493844544028364b193d1
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 22:05:17 +0000

    Completed ‘Video and e-learning Help’ page.

---
+++ video-and-e-learning-help.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Video and e-learning Help | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Video and e-learning Help</h1>
+		
+		<ul>
+			<li><a href="#Q1">Flash video and e-learning</a></li>
+			<li><a href="#Q2">My video or e-learning does not start!</a></li>
+			<li><a href="#Q3">What are 'plug-ins'?</a></li>
+			<li><a href="#Q4">Getting plug-ins</a></li>
+			<li><a href="#Q5">Why do I need plug-ins?</a></li>
+			<li><a href="#Q6">Keyboard shortcuts</a></li>
+			<li><a href="#Q7">Transcripts</a></li>
+			<li><a href="#Q8">What is the difference between captions and subtitles?</a></li>
+			<li><a href="#Q9">Audio version</a></li>
+			<li><a href="#Q10">Text-only version (e-learning)</a></li>
+		</ul>
+		
+		<h3><a name="Q1">Flash video and e-learning</a></h3>
+		<p>All of the video and e-learning on our site is delivered through Adobe Flash, and this means you need to have installed the Adobe Flash player. The player is a plug-in for your web browser.</p>
+		<p>Generally, you will be offered the option to download this plug-in as soon as your system detects that you need it.</p>
+		<p>If this has not happened, you can <a href="http://www.adobe.com/products/flashplayer.html" target="_blank" rel="noopener noreferrer">find the free Flash plug-in for download from the Adobe website</a>.</p>
+		<p><a href="#menu">Back to top</a></p>
+
+		<h3><a name="Q2">My video or e-learning does not start!</a></h3>
+		<p>If you do not have the Flash plug-in installed, you may receive an error message. If this occurs, follow the on-screen instructions to download and install the plug-in from Macromedia.</p>
+		<p><a href="#menu">Back to top</a></p>
+
+		<h3><a name="Q3">What are 'plug-ins'?</a></h3>
+		<p>Plug-ins are small bits of software that let you view a document, interact with an image, listen to music or watch a video.</p>
+		<p>If you did not have the right plug-in on your computer, you would not be able to listen to your favourite song or see animations and video on screen.</p>
+		<p>So, if you want to play a game that uses Shockwave and you don't have that program on your computer, you'll be prompted to install it.</p>
+		<p><a href="#menu">Back to top</a></p>
+
+		<h3><a name="Q4">Getting plug-ins</a></h3>
+		<p>They are called plug-ins because they 'plug in' to your browser the first time you download them onto your computer, generally installing automatically.</p>
+		<p>When you go to a site that needs a plug-in your browser will automatically know and ask you whether you want to download the required program.</p>
+		<p>After you have installed a plug-in, it will start whenever it is required - that is, whenever you visit a website designed to be viewed using that particular program.</p>
+		<p>Typically, plug-ins add multimedia functions which enable you to receive information in more powerful ways than the normal text and image - such as animation, sound or video.</p>
+		<p><a href="#menu">Back to top</a></p>
+
+		<h3><a name="Q5">Why do I need plug-ins?</a></h3>
+		<p>Plug-ins are necessary because it would simply not be practical to include this type of functionality in the main browser types. There are hundreds of different plug-ins for different purposes, so it would be impossible for a basic browser package to include them all and remain up-to-date.</p>
+		<p>Plus, it makes more sense to keep the basic browser as small as possible to make it easier to install extra functionality and let individual users decide which specific plug-ins they require.</p>
+		<p><a href="#menu">Back to top</a></p>
+
+		<h3><a name="Q6">Using keyboard shortcuts to operate the video player and e-learning courses</a></h3>
+		<p>As well as using the mouse, you can control our videos and e-learning via your computer keyboard:</p>
+		<ul>
+			<li>use the tab key to cycle through the player options in order</li>
+			<li>to select an option whilst using the tab key, use the enter key</li>
+			<li>use the up and down arrow keys to increase or decrease the volume of sound in videos</li>
+			<li>use the left and right arrow keys to move forwards or backwards through the video programme</li>
+			<li>use the spacebar to switch between play and pause</li>
+			<li>use the escape key to exit from full screen video mode</li>
+		</ul>
+		<p><a href="#menu">Back to top</a></p>
+
+		<h3><a name="Q7">Transcripts</a></h3>
+		<p>Each video is accompanied by a text-only transcript. This is an accurate and concise rendering of what is both being said and being depicted on the screen. This includes any ambient or off-screen audio that affects the experience of the video, any 'colour' commentary if applicable (visual puns, droll expression, etc), and is descriptive of the scene if there is no dialogue, or if the visuals inform the narrative (eg a new person enters the room).</p>
+		<p>For all video content in the My New Business area of our site, select the 'Switch to accessible version' button on the video player. Then select 'Download transcript' to open the text-only transcript.</p>
+		<p><a href="#menu">Back to top</a></p>
+
+		<h3><a name="Q8">What is the difference between captions and subtitles?</a></h3>
+		<p>The differences between the two are simple, but very specific:</p>
+		<ul>
+			<li>captions are primarily for the hearing impaired</li>
+			<li>subtitles are written for hearing viewers in another language</li>
+		</ul>
+		<p>Our videos have captions, and we offer subtitles when and if they are available.</p>
+		<p><a href="#menu">Back to top</a></p>
+
+		<h3><a name="Q9">Audio version</a></h3>
+		<p>We also offer a downloadable, audio-only version of each video in MP3 format. This allows users to listen to the programme offline and also offers another option for the visually impaired to enjoy our content.</p>
+		<p>You may access the audio-only version from the 'Listen to audio only' link below the video. Where there is no link to the audio-only version, you should follow the instructions below.</p>
+		<p>For all video content in the My New Business area of our site, select the 'Switch to accessible version' button on the video player. Then select 'Download audio version' and save the MP3 file to your computer.</p>
+		<p><a href="#menu">Back to top</a></p>
+
+		<h3><a name="Q10">Text-only version (e-learning)</a></h3>
+		<p>We offer a text-only version of all My New Business e-learning courses. Text-only versions are optimised for assistive technologies, including screen readers and magnifiers.</p>
+		<p>To access text-only versions of e-learning content, click the 'Text-only version' links on Task pages and in the Start-up learning directory. The text-only version will open in a new window.</p>
+		<p><a href="#menu">Back to top</a></p>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<p><img src="../../_img/generic_help.jpg" width="181" height="121" border="0" alt="" /></p>
+
+		<ul>
+			<li><a href="http://www.adobe.com/products/flashplayer.html" target="_blank" rel="noopener noreferrer">Find the free Flash plug-in for download&nbsp;from the Adobe website</a></li>
+			<li><a href="https://www.w3.org/WAI/" target="_blank" rel="noopener noreferrer">Find more information on web accessibility on the World Wide Web Consortium website</a></li>
+		</ul>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
