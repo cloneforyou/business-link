ef7096fbb88299427d136196c2d0e3bd9db57c44
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 21:05:32 +0000

    Completed page ‘How much stock should you keep?’.

---
+++ starting-up/find-and-manage-the-money/stock-control-and-inventory/how-much-stock-should-you-keep.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>How much stock should you keep? | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Stock control and inventory</h1>
+		<h2>How much stock should you keep?</h2>
+		<p>Deciding how much stock to keep depends on the size and nature of your business, and the type of stock involved. If you are short of space you may be able to buy stock in bulk and then pay a fee to your supplier to store it, calling it off as and when needed.</p>
+		
+		<p><strong>Keeping little or no stock and negotiating with suppliers to deliver stock as you need it</strong></p>
+		<table>
+			<tbody>
+				<tr>
+					<th>Advantages</th>
+					<th>Disadvantages</th>
+				</tr>
+				<tr>
+					<th valign="top">Efficient and flexible - you only have what you need, when you need it</th>
+					<td valign="top">Meeting stock needs can become complicated and expensive</td>
+				</tr>
+				<tr>
+					<th valign="top">Lower storage costs</th>
+					<td valign="top">You might run out of stock if there's a hitch in the system</td>
+				</tr>
+				<tr>
+					<th valign="top">You can keep up to date and develop new products without wasting stock</th>
+					<td valign="top">You are dependent on the efficiency of your suppliers</td>
+				</tr>
+			</tbody>
+		</table>
+		
+		<p>This might suit your business if it's in a fast-moving environment where products develop rapidly, the stock is expensive to buy and store, the items are perishable or replenishing stock is quick and easy.</p>
+		
+		<p><strong>Keeping lots of stock</strong></p>
+		<table>
+			<tbody>
+				<tr>
+					<th><strong>Advantages</strong></th>
+					<th><strong>Disadvantages</strong></th>
+				</tr>
+				<tr>
+					<th valign="top">Easy to manage</th>
+					<td valign="top">Higher storage and insurance costs</td>
+				</tr>
+				<tr>
+					<th valign="top">Low management costs</th>
+					<td valign="top">Certain goods might perish</td>
+				</tr>
+				<tr>
+					<th valign="top">You never run out</th>
+					<td valign="top">Stock may become obsolete before it is used</td>
+				</tr>
+				<tr>
+					<th valign="top">Buying in bulk may be cheaper</th>
+					<td valign="top">Your capital is tied up</td>
+				</tr>
+			</tbody>
+		</table>
+		
+		<p>This might suit your business if sales are difficult to predict (and it is hard to pin down how much stock you need and when), you can store plenty of stock cheaply, the components or materials you buy are unlikely to go through rapid developments or they take a long time to re-order.</p>
+		
+		<h3>Stock levels depending on type of stock</h3>
+		<p>There are four main types of stock:</p>
+		<p><strong>Raw materials and components</strong></p>
+		<p>Ask yourself some key questions to help decide how much stock you should keep:</p>
+		<ul>
+			<li>How reliable is the supply and are alternative sources available?</li>
+			<li>Are the components produced or delivered in batches?</li>
+			<li>Can you predict demand?</li>
+			<li>Is the price steady?</li>
+			<li>Are there discounts if you buy in bulk?</li>
+		</ul>
+
+		<p><strong>Work in progress - stocks of unfinished goods</strong></p>
+		<p>Keeping stocks of unfinished goods can be a useful way to protect production if there are problems down the line with other supplies.</p>
+		<p><strong>Finished goods ready for sale</strong></p>
+		<p>You might keep stocks of finished goods when:</p>
+		<ul>
+			<li>demand is certain</li>
+			<li>goods are produced in batches</li>
+			<li>you are completing a large order</li>
+		</ul>
+		
+		<p><strong>Consumables</strong></p>
+		<p>The type of consumables you will use will depend on your type of business, but could include fuel and stationery. How much stock you keep will depend on factors such as:</p>
+		<ul>
+			<li>reliability of supply</li>
+			<li>expectations of price rises</li>
+			<li>how steady demand is</li>
+			<li>discounts for buying in bulk</li>
+		</ul>
+
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/types-of-stock.php" title="Types of stock">Prev</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-methods.php" title="Stock control methods">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory.php" title="Stock control and inventory">Introduction</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/types-of-stock.php">Types of stock</a></li>
+			<li>How much stock should you keep?</li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-methods.php">Stock control methods</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-manually.php">Stock control systems - keeping track manually</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-using-computer-software.php">Stock control systems - keeping track using computer software</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/using-rfid-for-inventory-control-stock-security-and-quality-management.php">Using RFID for inventory control, stock security and quality management</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-security.php">Stock security</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/control-the-quality-of-your-stock.php">Control the quality of your stock</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-administration.php">Stock control administration</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/here-is-how-I-control-my-stock.php">Here's how I control my stock</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.cips.org/en-au/training/training-courses1/inventory-management/" target="_blank" rel="noopener noreferrer">Inventory management course on the Chartered Institute of Purchasing and Supply website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/grow-your-business/growth-through-strategic-sourcing/manage-your-suppliers.php">Manage your suppliers</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/avoid-the-problems-of-overtrading.php">Avoid the problems of overtrading</a></li>
+			<li><a href="/workplace-health-and-safety/safer-ways-of-working/storing-goods-and-materials.php">Storing goods and materials</a></li>
+			<li><a href="/grow-your-business/growth-through-strategic-sourcing/negotiate-the-right-deal-with-suppliers.php">Negotiate the right deal with suppliers</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
