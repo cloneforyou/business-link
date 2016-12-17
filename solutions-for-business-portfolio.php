f2bc3c4330dd1ea91e25aa23f722bd8030131800
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 22:39:04 +0000

    Completed ‘Solutions for Business Portfolio’ page.

---
+++ solutions-for-business-portfolio.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Solutions for Business is a package of publicly funded business support products and services designed to help businesses start, grow and succeed | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Solutions for Business portfolio</h1>
+		<h2>Solutions for Business is a package of publicly funded business support products and services designed to help businesses start, grow and succeed</h2>
+		<p>Solutions for Business (SfB) products are designed to equip businesses with the necessary knowledge and skills needed to tackle issues they might be facing.</p>
+		<p>A new SfB portfolio was announced on 17 March 2011. The transition to the streamlined SfB portfolio will start to take place from April 2011 onwards. During this period a number of products from the old portfolio will continue to be available through Business Link. The list below represents products that are currently available and will be updated as the transition to the new portfolio takes place.</p>
+		<p>For further information about the changes to the portfolio please <a href="https://web-beta.archive.org/web/20120416033252/http://www.bis.gov.uk/policies/enterprise-and-business-support/solutions-for-business-simplified-business-support" target="_blank" rel="noopener noreferrer">find information about the updated SfB portfolio on the Department for Business, Innovation and Skills website</a>.</p>
+		<p>The products are:</p>
+		
+		<h3>Starting up</h3>
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://improve.businesslink.gov.uk/resources/business-finance-finder" target="_blank" rel="noopener noreferrer">Starting a business</a>
+			<br /> Intensive Start Up Support
+			<br /> Starting a High Growth Business
+		</p>
+		
+		<h3>Finance &amp; grants</h3>
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://improve.businesslink.gov.uk/resources/business-finance-finder" target="_blank" rel="noopener noreferrer">Understanding Finance for Business</a>
+			<br />
+			<a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://improve.businesslink.gov.uk/resources/business-finance-finder" target="_blank" rel="noopener noreferrer">Finance for business</a>
+			<br />
+			<a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://improve.businesslink.gov.uk/resources/business-finance-finder" target="_blank" rel="noopener noreferrer">Grant for Business Investment</a>
+			<br />
+			<a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://improve.businesslink.gov.uk/resources/business-finance-finder" target="_blank" rel="noopener noreferrer">Small Loans for Business</a>
+			<br />
+			<a href="https://www.gov.uk/growing-your-business" target="_blank" rel="noopener noreferrer">Enterprise Finance Guarantee</a>
+		</p>
+		
+		<h3>Employing people</h3>
+		<p><a href="/employment-and-skills/advertise-a-job/advertise-a-job-and-jobcentre-plus-services.php">Jobcentre Plus</a></p>
+		
+		<h3>Grow your business</h3>
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://improve.businesslink.gov.uk/resources/business-finance-finder" target="_blank" rel="noopener noreferrer">Coaching for high growth</a>
+			<br />
+			<a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://improve.businesslink.gov.uk/resources" target="_blank" rel="noopener noreferrer">Manufacturing Advisory Service</a>
+		</p>
+		
+		<h3>Exploit your ideas</h3>
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://improve.businesslink.gov.uk/resources" target="_blank" rel="noopener noreferrer">Collaborative Research &amp; Development</a>
+			<br />
+			<a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://improve.businesslink.gov.uk/resources/business-finance-finder" target="_blank" rel="noopener noreferrer">Innovation advice and guidance</a>
+			<br />
+			<a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://improve.businesslink.gov.uk/resources" target="_blank" rel="noopener noreferrer">Networking for innovation</a>
+			<br />
+			<a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://improve.businesslink.gov.uk/resources" target="_blank" rel="noopener noreferrer">Knowledge Transfer Partnerships</a>
+		</p>
+		
+		<h3>IT &amp; e-commerce</h3>
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://improve.businesslink.gov.uk/resources/business-finance-finder" target="_blank" rel="noopener noreferrer">Transformational ICT</a></p>
+
+		<h3>Environment &amp; efficiency</h3>
+		<p><a href="/find-out-how-to-cut-costs-increase-profitability-and-reduce-the-environmental-impacts-of-your-business.php">Improving your resource efficiency</a>
+			<br />
+			<a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://improve.businesslink.gov.uk/resources/business-finance-finder">Rural Development Programme for England</a>
+		</p>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<img src="../../_img/bssp_logo_one_off.jpg" width="181" height="121" border="0" alt="" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
