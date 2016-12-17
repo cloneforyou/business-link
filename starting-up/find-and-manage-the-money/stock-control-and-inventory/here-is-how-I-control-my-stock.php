e8b49eaad98fd962e00b2761f245731e44da66a2
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 21:53:36 +0000

    Completed ‘Here's how I control my stock’ page.

---
+++ starting-up/find-and-manage-the-money/stock-control-and-inventory/here-is-how-I-control-my-stock.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Here's how I control my stock | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Here's how I control my stock</h1>
+		<p>Andrea Jones is managing director of Liversedge-based Systems (Telecoms) Limited, a business specialising in the next-day delivery of refurbished telecommunications equipment. </p>
+		<h2>What I did</h2>
+		<h3>Invest in a computerised system</h3>
+		<p>"We bought our computer system with its accounting software when we were turning over &pound;500,000 a year. It cost a lot of money but I got it on a five-year lease and I only paid &pound;50 a week. I couldn't have got anybody to do the stock work for &pound;50 a week.</p>
+		<p>"I chose this system because I wanted something that integrated all my accounting functions - my stock control, my buying &hellip; basically, to have everything under one roof, as it were. And, importantly, I wanted barcoding. I did a lot of phoning around software companies before making my choice.</p>
+		<p>"As an item comes in it gets barcoded and then it's logged on to the system under a purchase order with the serial number, stock code and details of the product. The product then goes down to our test room for refurbishing.</p>
+		<p>"The system can tell you whether any item has been tested or not and exactly where it's located in the warehouse."</p>
+		<h3>Hold enough equipment to fulfil customer orders</h3>
+		<p>"For the refurbished products side of my business there's no such thing as having too much stock. We can't ask people to wait a week if they want something so I buy anything that comes up for sale if it's cheap enough and I know it will move. I wouldn't buy stuff if there was no demand for it but that doesn't happen. I know my market very well.</p>
+		<p>"We sell new installation products too and we re-stock that on a demand-led basis. We set minimum and maximum stock levels on the computerised accounting system and when the stock hits the minimum level the screen lights up telling me to reorder. But I prefer to print a report on stock levels every Friday. That tells me what we're low on and I always top up to the maximum level."</p>
+		<h3>Reorder consumables only when necessary</h3>
+		<p>"We use lots of paper, lever arch files, pens and ink cartridges. Everything is kept on bookshelves and I can see instantly what's in and what we're running low on. I do a stationery order every couple of weeks or so - though I try to time it with when there are special offers in the stationery catalogue I use."</p>
+		<h2>What I'd do differently</h2>
+		<h3>Install a computer connection from work to my home earlier than I did</h3>
+		<p>"When I get home I can click on two buttons and I'm basically sitting in my office. This means I can keep an eye on my stock at all times and I do a lot of bidding for products from home, for example on eBay. I can also complete purchase orders from home."</p>
+		<p><a href="/find-a-case-study.php">Read more case studies that describe first hand how people tackle real-life challenges and opportunities</a></p>
+		
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-administration.php" title="Stock control administration">Prev</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory.php" title="Stock control and inventory">Introduction</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/types-of-stock.php">Types of stock</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/how-much-stock-should-you-keep.php">How much stock should you keep?</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-methods.php">Stock control methods</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-manually.php">Stock control systems - keeping track manually</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-using-computer-software.php">Stock control systems - keeping track using computer software</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/using-rfid-for-inventory-control-stock-security-and-quality-management.php">Using RFID for inventory control, stock security and quality management</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-security.php">Stock security</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/control-the-quality-of-your-stock.php">Control the quality of your stock</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-administration.php">Stock control administration</a></li>
+			<li>Here's how I control my stock</li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<div class="media-object stack-for-small">
+			<div class="media-object-section">
+				<div class="thumbnail">
+					<img src="../../../_img/AndreaJones.jpg" width="181" height="121" border="0" alt="Andrea Jones, Systems (Telecoms) Limited" />
+				</div>
+			</div>
+			<div class="media-object-section">
+				<h3>
+					Andrea Jones<br />
+					<a href="https://www.systemstl.co.uk/" target="_blank" rel="noopener noreferrer" title="Refurbished Telecommunication Equipment">Systems (Telecoms) Limited</a><br />
+					Andrea's top tips:
+				</h3>
+				<ul>
+					<li>"Keep plenty of stock so you don't disappoint customers."</li>
+					<li>"Know exactly where different stock items are located."</li>
+					<li>"Use barcoding as a way to track items through processes."</li>
+				</ul>
+			</div>
+		</div>
+
+		<h4>More case studies</h4>
+		<ul>
+			<li><a href="/it-and-ecommerce/introduction-to-it/computer-software-the-basics/here-is-how-investing-in-computer-hardware-benefited-my-business.php">Here's how investing in computer hardware benefited my business</a></li>
+			<li><a href="/it-and-ecommerce/software-and-business-applications/accounting-software/here-is-how-accounting-software-brought-efficiency-savings-to-my-business.php">Here's how accounting software brought efficiency savings to my business</a></li>
+		</ul>
+		
+		<p><a href="/find-a-case-study.php"><strong>Find a case study</strong></a></p>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
