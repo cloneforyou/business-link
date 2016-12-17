d661fe390e3fa78c8ec7199dc73d392fecfd4d07
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 23:17:40 +0000

    Completed ‘Download a free guide that tells you what you need to know and do’ page.

---
+++ running-or-starting-a-business-from-home.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Download a free guide that tells you what you need to know and do | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Running or starting a business from home</h1>
+		<h2>Download a free guide that tells you what you need to know and do</h2>
+		<p>Becoming your own boss and running a business from home can make sense for a lot of reasons. There's no office rent to pay, no need to commute, and the opportunity to build a business around your family life. And thanks to new technology and working practices, it's now possible to run many different types of business from home.</p>
+		<p>So could this be an option for you? To help you decide, the Department for Business, Innovation and Skills (BIS) has published a free guide written by Emma Jones - a business expert, author, and founder of small business support company Enterprise Nation.</p>
+		<p>The guide offers your straightforward, practical advice on the various aspects of running and growing a business from home, including:</p>
+		<ul>
+			<li>making sure your idea will work as a home-based business - and having a plan or making it happen</li>
+			<li>the practicalities of running a home-based business, including household and company admin</li>
+			<li>tips on creating an effective working environment in your home</li>
+			<li>how to create the right impression with business contacts &ndash; eg hiring meeting facilities and having a company website</li>
+			<li>working on the move - using mobile technology to run your business and stay organised</li>
+			<li>growing the business without outgrowing the home &ndash; including outsourcing and work hubs</li>
+		</ul>
+		
+		<p><strong>Actions</strong></p>
+		<ul>
+			<li>For more advice on running a home-based business and tips on how to make it succeed, see our guidance on how to <a href="https://www.gov.uk/browse/business/premises-rates" target="_blank" rel="noopener noreferrer">set up your business at home</a>.</li>
+			<li><a href="https://www.gov.uk/government/publications/the-home-business-guide" target="_blank" rel="noopener noreferrer">Download The Home Business Guide from the BIS website</a> (PDF, 780K)</li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
