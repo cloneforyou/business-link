6fd86b923f378633eb92a1c66cce3741483545ee
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 23:14:58 +0000

    Completed ‘Red Tape challenge now focusing on Company &amp; Commercial Law’ page.

---
+++ public-have-their-say-on-business-regulations.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Red Tape challenge now focusing on Company &amp; Commercial Law | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		
+		<h1>Public have their say on business regulations</h1>
+		<h2>Red Tape challenge now focusing on Company &amp; Commercial Law</h2>
+		<p>The <strong>Red Tape Challenge</strong> (RTC) website gives the public a chance to have their say on regulations that affect their everyday lives; whether it's to speak up for well designed rules that are there to protect or to challenge badly designed regulations that are an unnecessary burden.</p>
+		<p>The current challenge is <strong>Company &amp; Commercial Law.</strong></p>
+		<p>Every few weeks a new set of regulations, organised around themes, will open on the website for anyone to comment on. Once a theme has closed to the public, Ministers will have three months to explain why a regulation is still required, or it will be scrapped.</p>
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20150522175321/http://www.redtapechallenge.cabinetoffice.gov.uk/home/index/" target="_blank" rel="noopener noreferrer">Have your say on Company &amp; Commercial Law on the RTC website</a>.</p>
+		
+		<p><strong>Further resources</strong></p>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20121212135622/http://www.bis.gov.uk/red-tape-challenge" target="_blank" rel="noopener noreferrer">For Social Media News Releases and multi-media content visit the BIS website</a></li>
+			<li><strong><a href="/october-2012-regulation-changes.php">Guidance on the October 2011 regulation changes and how they affect your business</a></strong></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
