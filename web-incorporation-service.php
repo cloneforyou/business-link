5f5240012b5a865cfa3b875615eab254f772d154
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 23:58:30 +0000

    Completed ‘New online incorporation service from Companies House’ page.

---
+++ web-incorporation-service.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>New online incorporation service from Companies House | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Web Incorporation Service</h1>
+		<h2>New online incorporation service from Companies House</h2>
+		<p>From 6 April 2011, you can use the new <a href="/do-it-online/incorporate-your-company-online.php">web incorporation service</a> - a Companies House service available on Business Link - for incorporating a private company, limited by shares, with model articles of association.</p>
+		<h3>Easy to use and straightforward</h3>
+		<p>To <a href="/do-it-online/incorporate-your-company-online.php">use the service</a>, you will need to register with your email address and a password of your choice.</p>
+		<p>You will then need the following information to complete the form:</p>
+		<ul>
+			<li>company name and address</li>
+			<li>details of directors and secretary (optional)</li>
+			<li>details of each shareholder and the value of their shares</li>
+			<li>payment details (there is a fee of &pound;18 for this service)</li>
+		</ul>
+		<p>When using this simple, new service, you will find guidance throughout the form to help you answer the questions. You can also save your answers and return to complete the application later. Please note that the company name will not be reserved.</p>
+		<p>A summary of the information you entered is shown at the end of each section and on completion of the form. This enables you to review and amend any information before finally submitting the application form.</p>
+		<p>An incomplete application will be deleted from the system three months from the date it was first registered. If you request another application, then the data from the original application will be lost.</p>
+		<p><a href="/do-it-online/incorporate-your-company-online.php">Incorporate your private limited company online</a> or see our guide on how to <a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public.php.html">set up and register a limited company (private or public)</a>.</p>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
