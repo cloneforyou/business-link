9539b86058362035b0b00fbcbe5088012958a1d9
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 21:45:29 +0000

    Completed ‘Stock control administration’ page.

---
+++ starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-administration.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Stock control administration | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Stock control and inventory</h1>
+		<h2>Stock control administration</h2>
+		<p>There are many administrative tasks associated with stock control. Depending on the size and complexity of your business, they may be done as part of an administrator's duties, or by a dedicated <strong>stock controller</strong>.</p>
+		<p>For security reasons, it's good practice to have different staff responsible for finance than those responsible for stock.</p>
+		<p>Typical paperwork to be processed includes:</p>
+		<ul>
+			<li>delivery and supplier notes for incoming goods</li>
+			<li>purchase orders, receipts and credit notes</li>
+			<li>returns notes</li>
+			<li>requisitions and issue notes for outgoing goods</li>
+		</ul>
+
+		<p>Stock can tie up a large slice of your business capital, so accurate information about stock levels and values is essential for your company's accounting.</p>
+		<p>Figures should be checked systematically, either through a regular audit of stock - <strong>stocktaking</strong> - or an ongoing programme of checking stock - <strong>rolling stocktake</strong>.</p>
+		<p>If the figures don't add up, you need to investigate as there could be stock security problems or a failure in the system.</p>
+		<h3>Health and safety</h3>
+		<p>Health and safety aspects of stock control are related to the nature of the stock itself. Issues such as where and how items are stored, how they are moved and who moves them might be significant - depending on what they are.</p>
+		<p>You might have hazardous materials on your premises, goods that deteriorate with time or items that are very heavy or awkward to move.</p>
+		<p>Find more information in our guide on <a href="/workplace-health-and-safety/safer-ways-of-working/storing-goods-and-materials.php">storing goods and materials</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/control-the-quality-of-your-stock.php" title="Control the quality of your stock">Prev</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/here-is-how-I-control-my-stock.php" title="Here's how I control my stock">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory.php" title="Stock control and inventory">Introduction</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/types-of-stock.php">Types of stock</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/how-much-stock-should-you-keep.php">How much stock should you keep?</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-methods.php">Stock control methods</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-manually.php">Stock control systems - keeping track manually</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-using-computer-software.php">Stock control systems - keeping track using computer software</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/using-rfid-for-inventory-control-stock-security-and-quality-management.php">Using RFID for inventory control, stock security and quality management</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-security.php">Stock security</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/control-the-quality-of-your-stock.php">Control the quality of your stock</a></li>
+			<li>Stock control administration</li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/here-is-how-I-control-my-stock.php">Here's how I control my stock</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.hse.gov.uk/pubns/manlinde.htm" target="_blank" rel="noopener noreferrer">Manual handling resources on the Health &amp; Safety Executive (HSE) website</a></li>
+			<li><a href="http://www.hse.gov.uk/chemicals/manufacture.htm" target="_blank" rel="noopener noreferrer">Chemicals storage guidance on the HSE website</a></li>
+			<li><a href="https://www.food.gov.uk/business-industry/" target="_blank" rel="noopener noreferrer">Food safety information for food industry businesses on the Food standards Agency website</a></li>
+			<li><a href="https://www.cips.org/en-au/training/training-courses1/inventory-management/" target="_blank" rel="noopener noreferrer">Inventory management course on the Chartered Institute of Purchasing and Supply website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/workplace-health-and-safety/safer-ways-of-working/storing-goods-and-materials.php">Storing goods and materials</a></li>
+			<li><a href="/tax-payroll-and-company-information/set-up-a-basic-record-keeping-system.php">Set up a basic record-keeping system</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php">Financial and management accounts: the basics</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
