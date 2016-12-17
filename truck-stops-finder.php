88cbf02644153ed9d0227a68c1a8a3e96d54ec6c
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 23:45:56 +0000

    Completed ‘Truck stops finder’ page.

---
+++ truck-stops-finder.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Truck stops finder | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Truck stops finder</h1>
+		<p>Truck stops are vital rest areas for lorry drivers. This service will help you find places where you can take a break, and shows the facilities available at each stop. For more information, see our guide on how to <a href="/your-business-sector/transport-and-logistics/business-travel-and-journey-planning/find-truck-stops-online.php">find truck stops in England</a>. If you manage a truck stop shown here and want to update your details, contact the Highways Agency Information Line on Tel 0300 123 5000.</p>
+		<ul>
+			<li><a href="/your-business-sector/transport-and-logistics.php">Transport and logistics homepage</a></li>
+		</ul>
+		<p>Developed with:</p>
+		<hr />
+		<img src="../../_img/ha_logo_148x35px.gif" width="148" height="35" border="0" alt="Highways Agency logo" />
+		<p>[http://www.highways.gov.uk/publications/truckstops-in-England]</p>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
