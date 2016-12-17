f39000e216e9a00e64dbf2d90a87e616ab0b7469
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 21:29:49 +0000

    Completed ‘Using RFID for inventory control, stock security and quality management’ page.

---
+++ starting-up/find-and-manage-the-money/stock-control-and-inventory/using-rfid-for-inventory-control-stock-security-and-quality-management.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Using RFID for inventory control, stock security and quality management | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Stock control and inventory</h1>
+		<h2>Using RFID for inventory control, stock security and quality management</h2>
+		<p>Radio Frequency Identification (RFID) allows a business to identify individual products and components, and to track them throughout the supply chain from production to point-of-sale.</p>
+		<p>RFID is a technology that uses radio waves for communication between a tag (data carrier or RF transponder) and a reading device. The tag usually consists of a microchip attached to an antenna. The reader (or interrogator) is usually capable of reading data from and writing data to the tag.</p>
+		<p>An RFID tag is a tiny microchip, plus a small aerial, which can contain a range of digital information about a particular item. Tags are encapsulated in plastic, paper or similar material, and fixed to the product or its packaging, to a pallet or container, or even to a van or delivery truck.</p>
+		<p>The tag is interrogated by an RFID reader which transmits and receives radio signals to and from the tag. Readers can range in size from a hand-held device to a 'portal' through which several tagged devices can be passed at once, eg on a pallet. The information that the reader collects is collated and processed using special computer software. Readers can be placed at different positions within a factory or warehouse to show when goods are moved, providing continuous inventory control.</p>
+		<p>Using RFID tagging for stock control offers several advantages over other methods such as barcodes:</p>
+		<ul>
+			<li>tags can be read remotely, often at a distance of several metres</li>
+			<li>several tags can be read at once, enabling an entire pallet-load of products to be checked simultaneously</li>
+			<li>tags can be given unique identification codes, so that individual products can be tracked</li>
+			<li>certain types of tag can be overwritten, enabling information about items to be updated, eg when they are moved from one part of a factory to another</li>
+		</ul>
+
+		<p>RFID tagging can be used:</p>
+		<ul>
+			<li>to prevent over-stocking or under-stocking a product or component</li>
+			<li>for stock security, by positioning tag-readers at points of high risk, such as exits, and causing them to trigger alarms</li>
+			<li>for quality control, particularly if you make or stock items with a limited shelf life</li>
+		</ul>
+
+		<p>The costs associated with RFID tagging have fallen over recent years, and continue to do so, to bring the process within the reach of more and more businesses. The benefits of more efficient stock control and improved security make it particularly attractive to retailers, wholesalers or distributors who stock a wide range of items, and to manufacturers who produce volume runs of products for different customers.</p>
+		<p>You can <a href="http://www.rfidc.com/docs/introductiontorfid.htm" target="_blank" rel="noopener noreferrer">find out about RFID on the RFID Centre website</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-using-computer-software.php" title="Stock control systems - keeping track using computer software">Prev</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-security.php" title="Stock security">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory.php" title="Stock control and inventory">Introduction</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/types-of-stock.php">Types of stock</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/how-much-stock-should-you-keep.php">How much stock should you keep?</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-methods.php">Stock control methods</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-manually.php">Stock control systems - keeping track manually</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-using-computer-software.php">Stock control systems - keeping track using computer software</a></li>
+			<li>Using RFID for inventory control, stock security and quality management</li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-security.php">Stock security</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/control-the-quality-of-your-stock.php">Control the quality of your stock</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-administration.php">Stock control administration</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/here-is-how-I-control-my-stock.php">Here's how I control my stock</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.rfidc.com/docs/introductiontorfid.htm" target="_blank" rel="noopener noreferrer">RFID information on the RFID Centre website</a></li>
+			<li><a href="../../../_files/dti_how-rfid-can-improve-retail-and-supply-chain-efficiency.pdf" target="_blank" rel="noopener noreferrer">Download RFID guidance from the Department for Business, Innovation &amp; Skills website (PDF, 228K)</a></li>
+			<li><a href="http://www.aimglobal.org/page/technologies" target="_blank" rel="noopener noreferrer">RFID frequently asked questions on the Association for Automatic Identification and Mobility website</a></li>
+			<li><a href="/it-and-ecommerce/introduction-to-it/setting-up-a-basic-it-system.php">Use our interactive tool to find out which computer equipment you should buy for your business</a></li>
+		</ul>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/software-and-business-applications/supply-chain-software.php">Supply chain software</a></li>
+			<li><a href="/it-and-ecommerce/introduction-to-it/an-overview-of-it-and-e-commerce.php">An overview of IT and e-commerce</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
