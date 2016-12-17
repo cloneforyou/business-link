52cfd8bab18bd195b2c6f096b4d94c0445f58d36
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 20:59:02 +0000

    Completed page ‘Types of stock’.

---
+++ starting-up/find-and-manage-the-money/stock-control-and-inventory/types-of-stock.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Types of stock | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Stock control and inventory</h1>
+		<h2>Types of stock</h2>
+		<p>Everything you use to make your products, provide your services and to run your business is part of your stock.</p>
+		<p>There are four main types of stock:</p>
+		<ul>
+			<li>raw materials and components - ready to use in production</li>
+			<li>work in progress - stocks of unfinished goods in production</li>
+			<li>finished goods ready for sale</li>
+			<li>consumables - for example, fuel and stationery</li>
+		</ul>
+		<p>The type of stock can influence how much you should keep - see the page in this guide on <a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/how-much-stock-should-you-keep.php">how much stock should you keep?</a></p>
+		<h3>Stock value</h3>
+		<p>You can categorise stock further, according to its value. For example, you could put items into low, medium and high value categories. If your stock levels are limited by capital, this will help you to plan expenditure on new and replacement stock.</p>
+		<p>You may choose to concentrate resources on the areas of greatest value. However, low-cost items can be crucial to your production process and should not be overlooked.</p>
+		
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory.php" title="Stock control and inventory">Prev</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/how-much-stock-should-you-keep.php" title="How much stock should you keep?">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory.php" title="Stock control and inventory">Introduction</a></li>
+			<li>Types of stock</li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/how-much-stock-should-you-keep.php">How much stock should you keep?</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-methods.php">Stock control methods</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-manually.php">Stock control systems - keeping track manually</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-using-computer-software.php">Stock control systems - keeping track using computer software</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/using-rfid-for-inventory-control-stock-security-and-quality-management.php">Using RFID for inventory control, stock security and quality management</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-security.php">Stock security</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/control-the-quality-of-your-stock.php">Control the quality of your stock</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-administration.php">Stock control administration</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/here-is-how-I-control-my-stock.php">Here's how I control my stock</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/workplace-health-and-safety/safer-ways-of-working/storing-goods-and-materials.php">Storing goods and materials</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">Cashflow management: the basics</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting.php">Business budgeting</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
