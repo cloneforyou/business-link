881d675623e81bc450241b5c27a99509fea7e5eb
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 21:09:35 +0000

    Completed ‘Stock control methods’ page.

---
+++ starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-methods.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Stock control methods | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Stock control and inventory</h1>
+		<h2>Stock control methods</h2>
+		<p>There are several methods for controlling stock, all designed to provide an efficient system for deciding what, when and how much to order.</p>
+		<p>You may opt for one method or a mixture of two or more if you have various types of stock. For further information, see the page in this guide on <a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/types-of-stock.php">types of stock</a>.</p>
+		<ul>
+			<li><b>Minimum stock level</b> - you identify a minimum stock level, and re-order when stock reaches that level. This is known as the Re-order Level.</li>
+			<li><b>Stock review</b> - you have regular reviews of stock. At every review you place an order to return stocks to a predetermined level.</li>
+		</ul>
+
+		<p><strong>Just In Time (JIT)</strong> - this aims to reduce costs by cutting stock to a minimum - see our guide on how to <a href="/finance-and-grants/financial-planning-and-accounts/avoid-the-problems-of-overtrading.php">avoid the problems of overtrading</a>. Items are delivered when they are needed and used immediately. There is a risk of running out of stock, so you need to be confident that your suppliers can deliver on demand.</p>
+		<p>These methods can be used alongside other processes to refine the stock control system. For example:</p>
+		<p><b>Re-order lead time</b> - allows for the time between placing an order and receiving it.</p>
+		<p><b>Economic Order Quantity</b> (EOQ) - a standard formula used to arrive at a balance between holding too much or too little stock. It's quite a complex calculation, so you may find it easier to use stock control software. <a href="http://www.inventoryops.com/economic_order_quantity.htm" target="_blank" rel="noopener noreferrer">Find out about the EOQ formula on the InventoryOps website</a>.</p>
+		<p><b>Batch control</b> - managing the production of goods in batches. You need to make sure that you have the right number of components to cover your needs until the next batch.</p>
+		<p>If your needs are predictable, you may order a <b>fixed quantity</b> of stock every time you place an order, or order at a <b>fixed interval</b> - say every week or month. In effect, you're placing a standing order, so you need to keep the quantities and prices under review.</p>
+		<p><b>First in, first out</b> - a system to ensure that perishable stock is used efficiently so that it doesn't deteriorate. Stock is identified by date received and moves on through each stage of production in strict order.</p>
+		
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/how-much-stock-should-you-keep.php" title="How much stock should you keep?">Prev</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-manually.php" title="Stock control systems - keeping track manually">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory.php" title="Stock control and inventory">Introduction</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/types-of-stock.php">Types of stock</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/how-much-stock-should-you-keep.php">How much stock should you keep?</a></li>
+			<li>Stock control methods</li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-manually.php">Stock control systems - keeping track manually</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-systems-keeping-track-using-computer-software.php">Stock control systems - keeping track using computer software</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/using-rfid-for-inventory-control-stock-security-and-quality-management.php">Using RFID for inventory control, stock security and quality management</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-security.php">Stock security</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/control-the-quality-of-your-stock.php">Control the quality of your stock</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/stock-control-administration.php">Stock control administration</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/here-is-how-I-control-my-stock.php">Here's how I control my stock</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.inventoryops.com/economic_order_quantity.htm" target="_blank" rel="noopener noreferrer">EOQ formula information on the InventoryOps website</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20140320153140/http://www.mas.berr.gov.uk/resources/resource-library/manufacturing-processes" target="_blank" rel="noopener noreferrer">JIT production information on the MAS website (registration required)</a></li>
+			<li><a href="https://www.cips.org/en-au/training/training-courses1/inventory-management/" target="_blank" rel="noopener noreferrer">Inventory management course on the Chartered Institute of Purchasing and Supply website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/avoid-the-problems-of-overtrading.php">Avoid the problems of overtrading</a></li>
+			<li><a href="/workplace-health-and-safety/safer-ways-of-working/storing-goods-and-materials.php">Storing goods and materials</a></li>
+			<li><a href="/tax-payroll-and-company-information/set-up-a-basic-record-keeping-system.php">Set up a basic record-keeping system</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
