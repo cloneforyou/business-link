441f516150acca1bd4b487882dd69440562a15ef
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-17 22:22:17 +0000

    Completed ‘Changes to business support on climate change adaptation’ page.

---
+++ climate-change-adaptation-survey.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Changes to business support on climate change adaptation | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Climate change adaptation survey</h1>
+		<h2>Changes to business support on climate change adaptation</h2>
+		<p>From 1 October 2011, the Environment Agency is taking over responsibility for providing the government's support to businesses in England on adapting to the effects of climate change. They take over this work from the UK Climate Impacts Programme.</p>
+		<p>The Environment Agency is currently carrying out an online survey to find out what information and support businesses need to help them adapt to climate change.</p>
+		<p>You can <a href="https://www.gov.uk/government/organisations/environment-agency/about/research" target="_blank" rel="noopener noreferrer">find the survey and information about the adapting to climate change programme on the Environment Agency website</a>.</p>
+		<p>You can also <a href="/environment-and-efficiency/adapt-to-climate-change/identify-where-you-can-save-money-by-preparing-for-climate-change-impacts.php">use our interactive tool to assess financial risks and opportunities from the effects of climate change</a> or read our guide on <a href="l/environment-and-efficiency/adapt-to-climate-change/adapt-your-business-to-the-effects-of-climate-change.php">how to adapt your business to the effects of climate change</a>.</p>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
