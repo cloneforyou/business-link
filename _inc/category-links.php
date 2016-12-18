378ad9a7511b5b20f9ee39680bbaf761aa645d53
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-18 14:02:06 +0000

    Completed ‘For professional advisers’ page.

--- _inc/category-links.php
+++ _inc/category-links.php
@@ -721,11 +721,11 @@
 						</ul>
 					</li>
 					<li data-accordion-item>
-						<a href="../for-professional-advisers.php">For professional advisers</a>
+						<a href="/for-professional-advisers.php">For professional advisers</a>
 						<ul>
-							<li>
-								<a href="../for-professional-advisers.php">~~</a>
-							</li>
+							<li><a href="/for-professional-advisers.php" title="For professional advisers">Introduction</a></li>
+							<li><a href="/for-professional-advisers/for-accountants-and-tax-advisers.php">For accountants and tax advisers</a></li>
+							<li><a href="/for-professional-advisers/for-freight-forwarders.php">For freight forwarders</a></li>
 						</ul>
 					</li>
 					<li data-accordion-item>
---
+++ for-professional-advisers.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>For professional advisers | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+
+		<h1>For professional advisers</h1>
+		<h2>Interested in using the services of a professional adviser?</h2>
+		<p>This page offers advice for those acting as professional advisers (from accountants and tax advisers to freight forwarders). If you are interested in using the services of a professional adviser, see our page on <a href="/how-to-engage-a-professional-adviser.php">how to engage a professional adviser</a>.</p>
+		<h3>Detailed information for professional advisers</h3>
+		<p>Guides, tools and online forms for professional advisers.</p>
+		<ul>
+			<li><a href="/for-professional-advisers/for-accountants-and-tax-advisers.php">For accountants and tax advisers</a></li>
+			<li><a href="/for-professional-advisers/for-freight-forwarders.php">For freight forwarders</a></li>
+		</ul>
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/sitemap?topicId=1081986989" target="_blank" rel="noopener noreferrer">For professional advisers - site map</a></p>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
