904d62b9451364e3291b926347a9ed36e3e51a22
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-18 14:33:59 +0000

    Completed ‘Grow your business’ page.

---
+++ grow-your-business.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Grow your business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+		<h1>Grow your business</h1>
+		<h2>Growth and Improvement Service</h2>
+		
+		<img src="../../_img/Theme_lp_180x127.jpg" />
+
+		<p><strong>Access information to help your business work better</strong></p>
+		<ul>
+			<li>Find videos, tools and case studies to help take your business forward</li>
+			<li>Search the events finder for business related training</li>
+			<li>Access government support online</li>
+		</ul>
+		<p><a href="https://www.gov.uk/growing-your-business" target="_blank" rel="noopener noreferrer">Get Started</a></p>
+		<ul>
+			<li><a href="http://shop.bsigroup.com/BusinessLinks/BusinessLinkLP/" target="_blank" rel="noopener noreferrer">Standards explained on the British Standards Institution website</a></li>
+		</ul>
+		
+		<h2>Getting started</h2>
+		<ul>
+			<li><a href="/grow-your-business/assessing-current-performance.php">Assessing current performance</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth.php">Setting a vision for growth</a></li>
+		</ul>
+		
+		<h2>Finance and logistics</h2>
+		<ul>
+			<li><a href="/grow-your-business/planning-how-to-grow.php">Planning how to grow</a></li>
+			<li><a href="/grow-your-business/financing-growth.php">Financing growth</a></li>
+		</ul>
+		
+		<h2>Strategies for growth</h2>
+		<ul>
+			<li><a href="/grow-your-business/growth-through-product-and-service-development.php">Growth through product and service development</a></li>
+			<li><a href="/grow-your-business/growth-through-sales.php">Growth through sales</a></li>
+			<li><a href="/grow-your-business/growth-through-technology.php">Growth through technology</a></li>
+			<li><a href="/grow-your-business/growth-through-strategic-sourcing.php">Growth through strategic sourcing</a></li>
+			<li><a href="/grow-your-business/growth-through-strategic-partnering.php">Growth through strategic partnering</a></li>
+		</ul>
+		
+		<h2>Tendering for contracts</h2>
+		<ul>
+			<li><a href="/sales-and-marketing/suppliers-overview-of-tendering-for-contracts.php">Suppliers: overview of tendering for contracts</a></li>
+			<li><a href="/sales-and-marketing/buyers-publish-tender-documents.php">Buyers: publish tender documents</a></li>
+		</ul>
+		
+		<h2>Growing internationally</h2>
+		<ul>
+			<li><a href="/grow-your-business/growth-through-international-trade.php">Growth through international trade</a></li>
+			<li><a href="/grow-your-business/growing-your-business-into-europe.php">Growing your business into Europe</a></li>
+		</ul>
+		
+		<h2>Managing growth</h2>
+		<ul>
+			<li><a href="/grow-your-business/leading-staff-through-growth.php">Leading staff through growth</a></li>
+			<li><a href="/grow-your-business/managing-risk.php">Managing risk</a></li>
+		</ul>
+		
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/sitemap?topicId=1074404796" target="_blank" rel="noopener noreferrer">Grow your business - site map</a></p>
+		
+		<h3>Spotlight</h3>
+		<h4><a href="https://data.gov.uk/data/contracts-finder-archive/" target="_blank" rel="noopener noreferrer">Government tenders on Contracts Finder</a></h4>
+
+		<hr />
+
+		<p>Search for opportunities to supply goods and services, and set up email alerts</p>
+		<a href="https://data.gov.uk/data/contracts-finder-archive/" target="_blank" rel="noopener noreferrer">
+			<img src="../../_img/PR0037.jpg" width="76" height="76" alt="Search for government opportunities" />
+		</a>
+		
+		<h3>Case studies</h3>
+		<p>Real life business growth</p>
+		<hr />
+		<ul>
+			<li><a href="/find-a-case-study.php">View case studies for growing your business</a></li>
+		</ul>
+
+		<img src="../../_img/gyb_case_study_image.jpg" width="76" height="76" alt="" />
+
+		<h3>Do it online</h3>
+		<ul>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/create-an-action-plan-to-successfully-grow-your-business.php">Create an action plan to successfully grow your business</a></li>
+			<li><a href="/sales-and-marketing/keeping-your-customers/discover-who-your-most-valuable-customers-are.php">Use our interactive tool to find out who your most valuable customers are</a></li>
+			<li><a href="/buy-or-sell-a-business/businesses-in-difficulty/assess-your-business-performance.php">Assess your business' performance</a></li>
+			<li><a href="/do-it-online/incorporate-your-company-online.php">Incorporate your limited company online with Companies House</a></li>
+		</ul>
+
+		<h3>What's popular</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/overseas-supplier-relationships/entering-overseas-markets.php">Entering overseas markets</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/managing-risks-of-importing-and-exporting/manage-the-risks-of-exporting.php">Manage the risks of exporting</a></li>
+			<li><a href="/create-innovate-and-protect/design-for-business-success/use-design-to-improve-your-export-potential.php">Use design to improve your export potential</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/prepare-a-business-plan-for-growth.php">Prepare a business plan for growth</a></li>
+		</ul>
+
+		<h3>Directories</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://www.businesslink.gov.uk/bdotg/action/event?topicId=1074404796" target="_blank" rel="noopener noreferrer">Grow your business events near you</a></li>
+			<li><a href="/find-a-case-study/browse-by-theme-grow-your-business.php">Grow your business case studies</a></li>
+		</ul>
+
+	
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
