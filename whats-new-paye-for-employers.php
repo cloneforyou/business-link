4d2ac1ec4610a0e074bd98d638d3fad307fec6c0
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-18 00:04:09 +0000

    Completed ‘HMRC's weekly employer update’ page.

---
+++ whats-new-paye-for-employers.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HMRC's weekly employer update | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>What's new - PAYE for employers</h1>
+		<h2>HMRC's weekly employer update</h2>
+		<p>HM Revenue &amp; Customs (HMRC) produces a short, informal weekly update, issued by email to employer representatives, on current issues you tell HMRC about.</p>
+		<p>These weekly email updates are reproduced on businesslink.gov.uk for the benefit of all employers. You may also find the information helps you answer questions your employees may have.</p>
+		<p>HMRC is pleased to receive any feedback on these articles. To give feedback on these updates you can <a href="http://webarchive.nationalarchives.gov.uk/20120823131012/https://online.businesslink.gov.uk/bdotg/action/usersupport" target="_blank" rel="noopener noreferrer">use our online feedback form to send your comments, views and ideas</a>.</p>
+		<hr>
+		<ul>
+			<li><a href="/hmrcs-weekly-employer-update_31-08-12.php">Read HMRC update 130 (31 August 2012)</a></li>
+			<li><a href="/hmrcs-weekly-employer-update_07-09-12.php">Read HMRC update 131 (7 September 2012)</a></li>
+			<li><a href="/hmrcs-weekly-employer-update_14-09-12.php">Read HMRC update 132 (14 September 2012)</a> </li>
+			<li><a href="/hmrcs-weekly-employer-update_21-09-12.php">Read HMRC update 133 (21 September 2012)</a></li>
+		</ul>
+		<ul></ul>
+		<h3>News for employers from HMRC</h3>
+		<p>You can <a href="https://www.gov.uk/topic/business-tax/paye/latest" target="_blank" rel="noopener noreferrer">find general news and HMRC messages on the HMRC website</a>.</p>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
