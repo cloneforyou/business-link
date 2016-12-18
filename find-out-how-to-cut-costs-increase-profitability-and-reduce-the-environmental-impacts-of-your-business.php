25c6da02630c547a05edc17a44928b04c38469c8
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-18 00:19:52 +0000

    Completed ‘Improving Your Resource Efficiency’ page.

---
+++ find-out-how-to-cut-costs-increase-profitability-and-reduce-the-environmental-impacts-of-your-business.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Find out how to cut costs, increase profitability and reduce the environmental impacts of your business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Improving Your Resource Efficiency</h1>
+		<h2>Find out how to cut costs, increase profitability and reduce the environmental impacts of your business</h2>
+		<p>Improving Your Resource Efficiency is one of the business support products under the government's Solutions for Business portfolio.</p>
+		<p>Improving Your Resource Efficiency provides both simple and more specialist forms of support, advice and guidance on how to save money by using raw materials, energy and water more efficiently.</p>
+		
+		<h3>Why should you get involved?</h3>
+		<p>Improving your resource efficiency leads to a number of benefits including:</p>
+		<ul>
+			<li>cutting your business' costs</li>
+			<li>improving your business' reputation with customers, investors, the public, employees and other stakeholders</li>
+			<li>reducing the impact your business has on the environment</li>
+		</ul>
+		
+		<h3>Are you eligible for help?</h3>
+		<p>Yes. Most of the support, especially at an initial level, is available to all businesses. Much of it can be implemented at no or low cost.</p>
+		
+		<h3>Where should you start?</h3>
+		<p>Read our <a href="/resource-efficiency-general-tips.php">top ten tips for reducing waste and saving energy</a>.</p>
+		<p>For more information, see our guide on the <a href="/environment-and-efficiency/the-business-case-for-environmental-improvements/benefits-of-improving-your-environmental-performance.php">benefits of improving your environmental performance</a>. You can also <a href="/environment-and-efficiency/the-business-case-for-environmental-improvements/identify-where-you-can-save-money-by-going-green.php">use our interactive tool to identify where you can save money by going green</a>.</p>
+		
+		<h3>Where can you go for more specialist help?</h3>
+		<p>The Carbon Trust provides advice and practical support to help your business cut its energy bills and carbon emissions:</p>
+		<ul>
+			<li><a href="https://www.carbontrust.com/resources/guides/" target="_blank" rel="noopener noreferrer">find advice for your business sector on the Carbon Trust website</a></li>
+			<li><a href="https://www.carbontrust.com/client-services/" target="_blank" rel="noopener noreferrer">read about further practical support on the Carbon Trust website</a></li>
+			<li><a href="https://www.carbontrust.com/client-services/technology/implementation-and-finance/" target="_blank" rel="noopener noreferrer">read about finance for energy saving projects on the Carbon Trust website</a></li>
+		</ul>
+		
+		<p>WRAP (Waste &amp; Resources Action Programme) is the lead body for resource efficiency. WRAP advises businesses on environmental issues, such as:</p>
+		<ul>
+			<li>efficient use of raw materials and resources</li>
+			<li>waste reduction at source</li>
+			<li>supply chain efficiency</li>
+			<li>eco-design</li>
+			<li>recycling</li>
+		</ul>
+		
+		<p>The business sectors that WRAP works with include:</p>
+		<ul>
+			<li>construction - <a href="http://www.wrap.org.uk/category/sector/construction" target="_blank" rel="noopener noreferrer">read about using resources more efficiently and reducing waste in the construction sector on the WRAP website</a></li>
+			<li>manufacturing - <a href="http://www.wrap.org.uk/content/manufacturers" target="_blank" rel="noopener noreferrer">read about using recycled materials in the manufacturing sector on the WRAP website</a></li>
+			<li>retail - <a href="http://www.wrap.org.uk/content/wrap-and-retail-supply-chain" target="_blank" rel="noopener noreferrer">read about reducing food and packaging waste in the retail sector on the WRAP website</a></li>
+			<li>recycling</li>
+			<li>compost producers - <a href="http://www.wrap.org.uk/category/role/farmer" target="_blank" rel="noopener noreferrer">find information about composting on the WRAP website</a></li>
+		</ul>
+		
+		<p>National Industrial Symbiosis Programme (NISP) operates a network that brings together all types of businesses, identifying mutually profitable links so that previously unused resources (materials, energy and expertise) from one organisation are recovered, reprocessed and reused by others:</p>
+		<ul>
+			<li><a href="https://web-beta.archive.org/web/20120206074435/http://www.nisp.org.uk/case_study_index.aspx" target="_blank" rel="noopener noreferrer">find out how other businesses have increased their profits through NISP on the NISP website</a></li>
+			<li><a href="https://web-beta.archive.org/web/20120714011923/http://www.nisp.org.uk/how_to_join.aspx" target="_blank" rel="noopener noreferrer">join NISP to get instant access to business opportunities, workshops and events on the NISP website</a></li>
+		</ul>
+		
+		<h3>Where can you get more information on different types of support?</h3>
+		<p>Read our guide on <a href="/environment-and-efficiency/sources-of-environmental-advice/where-to-get-advice-on-environmental-issues.php">where to get advice on environmental issues</a>.</p>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<img src="../../_img/bssp_logo_one_off.jpg" width="181" height="121" border="0" alt="" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
