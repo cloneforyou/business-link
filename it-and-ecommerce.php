7d760cfe0d1a9faf0605b2cdb3ef5ab5f22c1977
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-19 16:52:55 +0000

    Completed ‘IT & e-commerce’ page.

---
+++ it-and-ecommerce.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>IT &amp; e-commerce | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+		
+		<h1>IT &amp; e-commerce</h1>
+		<ul>
+			<li>
+				<a href="/it-and-ecommerce/introduction-to-it.php">Introduction to IT</a>
+				<p>Introducing IT to your business, basic information and advice on the decisions you need to consider</p>
+			</li>
+			<li>
+				<a href="/it-and-ecommerce/suppliers.php">Suppliers</a>
+				<p>Information to help you make the right IT choices and choosing and managing IT suppliers and providers</p>
+			</li>
+			<li>
+				<a href="/it-and-ecommerce/software-and-business-applications.php">Software and business applications</a>
+				<p>Find information on how different software and applications can benefit your business</p>
+			</li>
+			<li>
+				<a href="/it-and-ecommerce/communications.php">Communications</a>
+				<p>Information on how to ensure you have the correct communications in place for your business</p>
+			</li>
+			<li>
+				<a href="/it-and-ecommerce/create-and-manage-a-website.php">Create and manage a website</a>
+				<p>Advice on how to develop a website for your business</p>
+			</li>
+			<li>
+				<a href="/it-and-ecommerce/e-commerce.php">E-commerce</a>
+				<p>How e-commerce can benefit your business and information on the mistakes to avoid</p>
+			</li>
+			<li>
+				<a href="/it-and-ecommerce/data-protection-and-legal-issues.php">Data protection and legal issues</a>
+				<p>Information on how data protection can affect your business and the legal issues to be aware of</p>
+			</li>
+			<li>
+				<a href="/it-and-ecommerce/it-security-and-risks.php">IT security and risks</a>
+				<p>How to ensure that you have the correct IT security and best practice</p>
+			</li>
+			<li>
+				<a href="/it-and-ecommerce/employees-and-it.php">Employees and IT</a>
+				<p>Guidance on IT support and policy for your staff</p>
+			</li>
+		</ul>
+
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/sitemap?topicId=1073861197" target="_blank" rel="noopener noreferrer">IT &amp; e-commerce - site map</a></p>
+		
+		<h3>Spotlight</h3>
+		<h4><a href="/it-and-ecommerce/software-and-business-applications/cloud-computing.php">Working 'in the cloud'</a></h4>
+		
+		<p>Find out how cloud computing could help your business become more efficient</p>
+		
+		<a href="/it-and-ecommerce/software-and-business-applications/cloud-computing.php">
+			<img src="../../_img/cloud_comp_small.jpg" width="76" height="76" alt="How businesses can use cloud computing to cut IT costs and innovate more quickly" />
+		</a>
+
+		<hr />
+		
+		<h4><a href="/it-and-ecommerce/it-security-and-risks/incident-management.php">Managing an IT incident</a></h4>
+		
+		<p>Guidance on how you can introduce an incident management policy into your business</p>
+		
+		<a href="/it-and-ecommerce/it-security-and-risks/incident-management.php">
+			<img src="../../_img/incident_mgmt_small.jpg" width="76" height="76" alt="What Incident Management is and how it can help your business" />
+		</a>
+
+		<hr />
+		
+		<h3>Case studies</h3>
+		<p>How people tackle real-life challenges and opportunities</p>
+		
+		<ul>
+			<li><a href="/find-a-case-study.php">View case studies for IT &amp; e-commerce</a></li>
+		</ul>
+		
+		<img src="../../_img/case_study.jpg" width="76" height="76" alt="" />
+
+		<hr />
+		
+		<h3>Do it online</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/create-and-manage-a-website/get-the-right-website-for-your-business.php">Use our interactive tool to investigate what kind of website is best for your business</a></li>
+			<li><a href="/it-and-ecommerce/data-protection-and-legal-issues/notifying-the-information-commissioner.php">Use our interactive tool to find out whether you need to notify the Information Commissioner about the data your business holds</a></li>
+		</ul>
+
+		<h3>What's popular</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/data-protection-and-legal-issues/sample-it-policies-disclaimers-and-notices.php">Sample IT policies, disclaimers and notices</a></li>
+			<li><a href="/it-and-ecommerce/software-and-business-applications/customer-relationship-management.php">Customer relationship management</a></li>
+			<li><a href="/it-and-ecommerce/communications/mobile-technology.php">Mobile technology</a></li>
+		</ul>
+		
+		<h3>Directories</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://www.businesslink.gov.uk/bdotg/action/event?topicId=1073861197" target="_blank" rel="noopener noreferrer">IT &amp; e-commerce events near you</a></li>
+			<li><a href="/find-a-case-study/browse-by-theme-it-and-ecommerce.php">IT &amp; e-commerce case studies</a></li>
+		</ul>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
