63c6c12a398d0c6085042a9cbf66c15ee946d3a0
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-19 16:24:30 +0000

    Completed ‘International trade’ page.

---
+++ international-trade.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>International trade | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+
+		<h1>International trade</h1>
+		<h2>Getting started</h2>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics.php">Importing and exporting basics</a></li>
+			<li><a href="/international-trade/doing-business-in-the-eu.php">Doing business in the EU</a></li>
+			<li><a href="/international-trade/trading-with-countries-outside-the-eu.php">Trading with countries outside the EU</a></li>
+			<li><a href="/international-trade/authorised-economic-operators-aeos.php">Authorised Economic Operators (AEOs)</a></li>
+		</ul>
+		<h2>Sector overviews and regulations</h2>
+		<ul>
+			<li><a href="/international-trade/service-industries.php">Service industries</a></li>
+			<li><a href="/international-trade/manufactured-goods.php">Manufactured goods</a></li>
+			<li><a href="/international-trade/food-and-agriculture.php">Food and agriculture</a></li>
+			<li><a href="/international-trade/natural-resources-and-chemicals.php">Natural resources and chemicals</a></li>
+		</ul>
+		<h2>Customs procedures and systems</h2>
+		<ul>
+			<li><a href="/international-trade/import-and-export-procedures.php">Import and export procedures</a></li>
+			<li><a href="/international-trade/classifying-your-goods.php">Classifying your goods</a></li>
+			<li><a href="/international-trade/customs-it-systems.php">Customs IT systems</a></li>
+		</ul>
+		<h2>Export and import licences</h2>
+		<ul>
+			<li><a href="/international-trade/export-control-organisation.php">Export Control Organisation</a></li>
+			<li><a href="/international-trade/do-you-need-an-export-or-import-licence.php">Do you need an export or import licence?</a></li>
+			<li><a href="/international-trade/importing-controlled-goods.php">Importing controlled goods</a></li>
+		</ul>
+		<h2>Moving your goods</h2>
+		<ul>
+			<li><a href="/international-trade/preparing-your-goods-for-transport.php">Preparing your goods for transport</a></li>
+			<li><a href="/international-trade/transport-options-for-moving-your-goods.php">Transport options for moving your goods</a></li>
+			<li><a href="/international-trade/taking-lorries-abroad.php">Taking lorries abroad</a></li>
+			<li><a href="/international-trade/transporting-dangerous-goods.php">Transporting dangerous goods</a></li>
+			<li><a href="/international-trade/freight-forwarding.php">Freight forwarding</a></li>
+		</ul>
+		<h2>Tax in international trade</h2>
+		<ul>
+			<li><a href="/international-trade/tax-obligations.php">Tax obligations</a></li>
+			<li><a href="/international-trade/excise-and-vat.php">Excise and VAT</a></li>
+		</ul>
+
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/sitemap?topicId=1079717544" target="_blank" rel="noopener noreferrer">International trade - site map</a></p>
+		
+		<h3>Spotlight</h3>
+		<h4><a href="/international-trade/export-control-organisation/how-to-determine-if-your-goods-need-an-export-licence.php">How to determine if your goods need an export licence</a></h4>
+		
+		<hr />
+
+		<p>Find out how to establish the Control List or End-Use status of your goods for export control purposes</p>
+		
+		<a href="/international-trade/export-control-organisation/how-to-determine-if-your-goods-need-an-export-licence.php">
+			<img src="../../_img/bigstockphoto_Customs_Control_Inspecting_At__36446.jpg" width="76" height="76" alt="Guidance to help you understand if your goods require an export licence issued by the Export Control Organisation including advice on self-rating, Control List Classification Advice Service, End User Advice Service and understanding the Control List number provided on an issued licence." />
+		</a>
+		
+		<h4><a href="/international-trade/customs-it-systems/how-to-use-the-uk-trade-tariff.php">How to use the UK Trade Tariff</a></h4>
+
+		<hr />
+
+		<p>Find out how to use the online UK Trade Tariff to find commodity codes, measures and duty and customs reliefs</p>
+		<a href="/international-trade/customs-it-systems/how-to-use-the-uk-trade-tariff.php">
+			<img src="../../_img/euros.JPG" width="76" height="76" alt="Introducing how to use the online UK Trade Tariff to find commodity codes, measures and duty and customs reliefs" />
+		</a>
+		
+		<h3>Do it online</h3>
+		<ul>
+			<li><a href="/do-it-online/use-the-uk-trade-tariff.php">Find commodity codes and other measures applying to imports and exports by accessing our free online UK Trade Tariff</a></li>
+			<li><a href="https://www.gov.uk/growing-your-business" target="_blank" rel="noopener noreferrer">Export for growth tool on the Growth and Improvement Service</a></li>
+			<li><a href="/international-trade/tax-obligations/understand-your-tax-obligations-when-importing-or-exporting.php">Understand your tax obligations when importing or exporting</a></li>
+			<li><a href="/international-trade/doing-business-in-the-eu/getting-ready-to-export-to-individual-countries.php">Use our interactive tool to identify export opportunities in your target country</a></li>
+		</ul>
+
+		<h3>What's popular</h3>
+		<ul>
+			<li><a href="/international-trade/do-you-need-an-export-or-import-licence.php">Do you need an export or import licence?</a></li>
+			<li><a href="/international-trade/trading-with-countries-outside-the-eu/importing-your-goods-from-outside-the-european-union.php">Importing your goods from outside the European Union</a></li>
+			<li><a href="/international-trade/export-control-organisation/sanctions-embargoes-and-restrictions.php">Sanctions, embargoes and restrictions</a></li>
+		</ul>
+
+		<h3>Useful links on other sites</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/organisations/department-for-international-trade" target="_blank" rel="noopener noreferrer">International trade team search on the UK Trade &amp; Investment website</a></li>
+			<li><a href="https://www.gov.uk/government/collections/rates-and-allowances-hm-revenue-and-customs#excise-duties" target="_blank" rel="noopener noreferrer">Excise rates from HMRC</a></li>
+			<li><a href="https://www.gov.uk/government/collections/exchange-rates-for-customs-and-vat" target="_blank" rel="noopener noreferrer">Exchange rates from HMRC</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20080910135103/ecochecker.co.uk/goodschecker/" target="_blank" rel="noopener noreferrer">Export restriction checker on the BIS Goods Checker website (registration required)</a></li>
+		</ul>
+
+		<h3>Directories</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://www.businesslink.gov.uk/bdotg/action/event?topicId=1079717544" target="_blank" rel="noopener noreferrer">International trade events near you</a></li>
+			<li><a href="/find-a-case-study/browse-by-theme-international-trade.php">International trade case studies</a></li>
+		</ul>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
