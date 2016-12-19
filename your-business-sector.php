28799fdd8445c012f78a84abf6e19a8b11e33936
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-19 22:14:02 +0000

    Completed ‘Your business sector’ page.

---
+++ your-business-sector.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Your business sector | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+
+		<h1>Your business sector</h1>
+		<h2>Detailed information for these business sectors</h2>
+		<p>Guides, tools and online forms related to different business sectors.</p>
+		
+		<h3>Your licence and compliance requirements</h3>
+		<p>Licences and compliance guidance for your business.</p>
+		<p><strong>Describe your business</strong></p>
+		
+		<form method="get" action="http://online.businesslink.gov.uk/bdotg/action/sectorsSIMCommitPage" styleclass="searchform">
+			<p>
+				<label for="expressionNav">Enter a word or words, eg <strong>plumber</strong> or <strong>bookshop with cafe</strong></label>
+			</p>
+			<input type="hidden" value="5" />
+			<input type="hidden" value="941" />
+			<input type="hidden" value="1" />
+			<input type="hidden" value="true" />
+			<input type="hidden" value="1081597476" />
+			<input type="text" />
+			<input type="image" value="Go" src="../../../_img/button_goblue.gif" alt="Go" />
+			<ul>
+				<li><a href="http://online.businesslink.gov.uk/bdotg/action/sectorsSIMLandingPage" title="">Or browse business categories</a></li>
+			</ul>
+		</form>
+
+		<a href="/your-business-sector/transport-and-logistics.php">
+			<img src="../../_img/lorry.jpg" width="76" height="76" alt="Lorry" />
+		</a>
+		<h3><a href="/your-business-sector/transport-and-logistics.php">Transport and logistics</a></h3>
+		<p>Online services and guides to help you operate safely and within the law</p>
+		
+		<a href="/your-business-sector/farming.php">
+			<img src="../../_img/corn.jpg" width="76" height="76" alt="Corn" />
+		</a>
+		<h3><a href="/your-business-sector/farming.php">Farming</a></h3>
+		<p>Guidance on livestock, crops, protecting environments and managing your farm business</p>
+		
+		<a href="/your-business-sector/marine-maritime-and-freshwater.php">
+			<img src="../../_img/boat_spotlight.jpg" width="76" height="76" alt="" />
+		</a>
+		<h3><a href="/your-business-sector/marine-maritime-and-freshwater.php">Marine, maritime and freshwater</a></h3>
+		<p>Information for businesses involved in marine, maritime or aquaculture activities</p>
+		
+		<a href="/your-business-sector/food-and-drink.php">
+			<img src="../../_img/food_sector.jpg" width="76" height="76" alt="" />
+		</a>
+		<h3><a href="/your-business-sector/food-and-drink.php">Food and drink</a></h3>
+		<p>Get advice and guidance while keeping up-to-date with the latest industry regulations</p>
+		
+		<a href="/your-business-sector/manufacturing.php">
+			<img src="../../_img/l_manufacturing_3.jpg" width="76" height="76" alt="" />
+		</a>
+		<h3><a href="/your-business-sector/manufacturing.php">Manufacturing</a></h3>
+		<p>Support and guidance for people working within the manufacturing industry</p>
+		
+		<a href="/your-business-sector/property-management.php">
+			<img src="../../_img/PM_sector.jpg" width="76" height="76" alt="" />
+		</a>
+		<h3><a href="/your-business-sector/property-management.php">Property management</a></h3>
+		<p>Guidance for residential and commercial landlords and their agents</p>
+		
+		<a href="/your-business-sector/private-security-and-surveillance.php">
+			<img src="../../_img/l_security_surveillance.jpg" width="76" height="76" alt="" />
+		</a>
+		<h3><a href="/your-business-sector/private-security-and-surveillance.php">Private security and surveillance</a></h3>
+		<p>Information on licensing and complying with the law in the security industry</p>
+		
+		<a href="/your-business-sector/construction.php">
+			<img src="../../_img/construction.jpg" width="76" height="76" alt="" />
+		</a>
+		<h3><a href="/your-business-sector/construction.php">Construction</a></h3>
+		<p>Industry guidance covering regulations, tendering, and advice on improving your environmental impact</p>
+		
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/sitemap?topicId=1081597476" target="_blank" rel="noopener noreferrer">Your business sector - site map</a></p>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
