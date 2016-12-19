7a372f0d361d62d66840d63914ec44d1f70e2e38
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-19 21:19:05 +0000

    Completed ‘Sales and marketing’ page.

--- _inc/category-links.php
+++ _inc/category-links.php
@@ -409,6 +409,9 @@
 						<ul>
 							<li>
 								<a href="/premises-and-property.php" title="Premises and property">Introduction</a>
+							</li>
+							<li>
+								<a href="/premises-and-property.php" title="Premises and property">Expand</a>
 								<ul>
 									<li>
 										<a href="/premises-and-property/choosing-the-right-premises.php">Choosing the right premises</a>
@@ -505,6 +508,15 @@
 						<a href="/sales-and-marketing.php">Sales and marketing</a>
 						<ul>
 							<li><a href="/sales-and-marketing.php" title="Sales and marketing">Introduction</a></li>
+							<li><a href="/sales-and-marketing/marketing.php">Marketing</a></li>
+							<li><a href="/sales-and-marketing/online-marketing.php">Online marketing</a></li>
+							<li><a href="/sales-and-marketing/advertising-branding-and-design.php">Advertising, branding and design</a></li>
+							<li><a href="/sales-and-marketing/selling.php">Selling</a></li>
+							<li><a href="/sales-and-marketing/online-selling.php">Online selling</a></li>
+							<li><a href="/sales-and-marketing/selling-and-the-law.php">Selling and the law</a></li>
+							<li><a href="/sales-and-marketing/selling-overseas.php">Selling overseas</a></li>
+							<li><a href="/sales-and-marketing/pricing.php">Pricing</a></li>
+							<li><a href="/sales-and-marketing/keeping-your-customers.php">Keeping your customers</a></li>
 							<li><a href="/sales-and-marketing/suppliers-overview-of-tendering-for-contracts.php">Suppliers: overview of tendering for contracts</a></li>
 							<li><a href="/sales-and-marketing/buyers-publish-tender-documents.php">Buyers: publish tender documents</a></li>
 						</ul>
---
+++ sales-and-marketing.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sales and marketing | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">	
+
+		<h1>Sales and marketing</h1>
+		<h2>Marketing your business</h2>
+		<ul>
+			<li><a href="/sales-and-marketing/marketing.php">Marketing</a></li>
+			<li><a href="/sales-and-marketing/online-marketing.php">Online marketing</a></li>
+			<li><a href="/sales-and-marketing/advertising-branding-and-design.php">Advertising, branding and design</a></li>
+		</ul>
+		<h2>Maximise your sales</h2>
+		<ul>
+			<li><a href="/sales-and-marketing/selling.php">Selling</a></li>
+			<li><a href="/sales-and-marketing/online-selling.php">Online selling</a></li>
+			<li><a href="/sales-and-marketing/selling-and-the-law.php">Selling and the law</a></li>
+			<li><a href="/sales-and-marketing/selling-overseas.php">Selling overseas</a></li>
+			<li><a href="/sales-and-marketing/pricing.php">Pricing</a></li>
+			<li><a href="/sales-and-marketing/keeping-your-customers.php">Keeping your customers</a></li>
+		</ul>
+		<h2>Tendering for contracts</h2>
+		<ul>
+			<li><a href="/sales-and-marketing/suppliers-overview-of-tendering-for-contracts.php">Suppliers: overview of tendering for contracts</a></li>
+			<li><a href="/sales-and-marketing/buyers-publish-tender-documents.php">Buyers: publish tender documents</a></li>
+		</ul>
+		
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/sitemap?topicId=1073861169" target="_blank" rel="noopener noreferrer">Sales and marketing - site map</a></p>
+		
+		<h3>Spotlight</h3>
+		<h4><a href="/sales-and-marketing/selling/networking-for-success.php">Networking for success</a></h4>
+		
+		<p>Chief Executive of skincare brand Mamo Mio, Sian Sutherland explains her methods to help build business relationships</p>
+		
+		<a href="/sales-and-marketing/selling/networking-for-success.php">
+			<img src="../../_img/sian.jpg" width="76" height="76" alt="A video case-study showing how networking can help your business" />
+		</a>
+
+		<hr />
+
+		<h4><a href="/sales-and-marketing/selling-and-the-law/approved-codes-and-self-regulation.php">Approved codes and self regulation</a></h4>
+
+		<p>An overview of the Office of Fair Trading Consumer Code Approval Scheme and policy on self-regulation, and how you can benefit</p>
+		
+		<a href="/sales-and-marketing/selling-and-the-law/approved-codes-and-self-regulation.php">
+			<img src="../../_img/G_sign_forward.gif" width="76" height="76" alt="An overview of consumer code of practice schemes and policy on self-regulation, and how you can benefit" />
+		</a>
+
+		<hr />
+		
+		<h3>Case studies</h3>
+		<p>How people tackle real-life challenges and opportunities</p>
+		
+		<ul>
+			<li><a href="/find-a-case-study.php">Find a case study</a></li>
+		</ul>
+		
+		<img src="../../_img/TP_men_discussing.jpg" width="76" height="76" alt="" />
+		
+		<hr />
+
+		<h3>Do it online</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/selling/identify-potential-sales-channels.php">Use our interactive tool to identify the sales channels most likely to benefit your business</a></li>
+			<li><a href="/sales-and-marketing/advertising-branding-and-design/choosing-the-right-advertising-media.php">Use our interactive tool to find out the best advertising media for your business</a></li>
+			<li><a href="/sales-and-marketing/keeping-your-customers/discover-who-your-most-valuable-customers-are.php">Use our interactive tool to find out who your most valuable customers are</a></li>
+		</ul>
+		
+		<h3>What's popular</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/keeping-your-customers/retain-and-grow-your-customer-base.php">Retain and grow your customer base</a></li>
+			<li><a href="/sales-and-marketing/pricing/competing-fairly.php">Competing fairly</a></li>
+		</ul>
+		
+		<h3>Directories</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://www.businesslink.gov.uk/bdotg/action/event?topicId=1073861169" target="_blank" rel="noopener noreferrer">Sales and marketing events near you</a></li>
+			<li><a href="/find-a-case-study/browse-by-theme-sales-and-marketing.php">Sales and marketing case studies</a></li>
+		</ul>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
