193ac53153d4662b99cacf7eafd57ce07fc2f061
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-22 15:18:37 +0000

    Completed ‘HMRC Weekly Update - 21 September 2012’ page.

---
+++ hmrcs-weekly-employer-update_21-09-12.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>21 September 2012 | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>HMRC Weekly Update</h1>
+		<h2>21 September 2012</h2>
+		<p>Dear Customers and Stakeholders,</p>
+		<p>We do not have a further update for you this week &ndash; so, although a 'nil' report, this email is being sent to reassure you that our commitment to a weekly update has not been overlooked!</p>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
