5254839d1cd5e27599570369ff0cf2b10f8d2b2e
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-22 17:06:26 +0000

    Completed ‘Resource efficiency - Retail’ page. ~ awaiting Vimeo video

---
+++ resource-efficiency-retail-tips.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Resource efficiency - Retail | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<div class="flex-video widescreen vimeo">
+			<iframe src="http://player.vimeo.com/video/192278297?badge=0&byline=0&portrait=0&title=0" width="720" height="540" allowfullscreen style="border:0"></iframe>
+		</div>
+
+		<hr />
+
+		<script type="text/javascript" src="../../../businesslink.edgeboss.net/download/businesslink/flash_player/swfobject.js">
+		<!--
+
+		//-->
+		</script>
+		<a href="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash">Adobe Flash Player</a> is required to view this content.
+		<script type="text/javascript">
+		<!--
+		var flashvars = {
+			height: "167",
+			width: "740"
+		};
+		var params = {
+			wmode: "transparent"
+		};
+		var attributes = {};
+		swfobject.embedSWF("http://businesslink.edgeboss.net/download/businesslink/flash/campaigns/resource_efficiency/retail.swf", "previewvid", "740", "167", "9.0.0", "http://businesslink.edgeboss.net/download/businesslink/flash_player/expressinstall.swf", flashvars, params, attributes);
+
+		//-->
+		</script>
+
+		<hr />
+
+		<ul>
+			<li><a href="/resource-efficiency-general-tips.php">General tips</a></li>
+			<li><a href="/resource-efficiency-hospitality-and-catering-tips.php">Hospitality &amp; Catering</a></li>
+			<li>Retail</li>
+			<li><a href="/resource-efficiency-manufacturing-tips.php">Manufacturing</a></li>
+		</ul>
+		
+		<h2>Retail tips</h2>
+		<p>Here are some case studies showing how your business can save money and increase efficiency</p>
+		
+		<h3>1. Staff awareness</h3>
+		<img height="140" alt="Staff awareness" src="../../_img/re_retail_1.jpg" width="250">
+		<p>Buttermarket shopping centre in Ipswich began a lighting awareness campaign to ensure that all staff were aware of potential savings, such as making good use of natural light and switching off lighting in unoccupied areas. Within just one year, the centre has implemented over half of the suggested actions including fitting lighting controls and time switches to vending machines. It was estimated that if all prioritised measures were implemented, the centre would make an annual saving of more than 150 tonnes of carbon dioxide and cost savings of &pound;19,366.</p>
+		<p><a href="https://web-beta.archive.org/web/20110110121037/http://www.carbontrust.co.uk/cut-carbon-reduce-costs/products-services/carbon-surveys/default/Pages/buttermarket.aspx" target="_blank" rel="noopener noreferrer">Read the full case study</a> </p>
+		
+		<h3>2. Working with suppliers</h3>
+		<img height="140" alt="Working with suppliers" src="../../_img/re_retail_2.jpg" width="250">
+		<p>Boots UK Ltd, realised cost and environmental benefits throughout their supply chain by implementing resource efficiency projects. Benefits include:</p>
+		<ul>
+			<li>transport and disposal cost savings of &pound;393,000 in the first 18 months following the introduction of a new display unit</li>
+			<li>20 tonnes of virgin material and &pound;2,100 saved by the introduction of plastic bottles with a 30% recycled content</li>
+			<li>Supplier savings of over &pound;20,500 in transport costs over three years by installing warehouse baling machines</li>
+		</ul>
+		<p><a href="../../_files/Envirowise_Boots_case_study.pdf" target="_blank" rel="noopener noreferrer">Download the full case study (PDF, 394KB)</a></p>
+		
+		<h3>3. Improve waste management</h3>
+		<img height="145" alt="Improve waste management" src="../../_img/re_retail_3.jpg" width="250">
+		<p>Chelmsford's High Chelmer Shopping Centre is the town's largest shopping centre, and consists of over 80 retail outlets. Following an awareness campaign aimed at increasing effort to reduce waste generation and increase recycling, a number of opportunities were spotted. These included improving the segregation and disposal of cardboard and segregating and baling polythene which led to tonnes of material being diverted to landfill. As a result, the financial savings made from improved recycling initiatives have provided means to support other currently non-financially self sufficient environmental issues.</p>
+		<p><a href="../../_files/Envirowise_Highchelmer_case_study.pdf" target="_blank" rel="noopener noreferrer">Download the full case study (PDF, 331KB)</a></p>
+		
+		<h3>Further links:</h3>
+		<ul>
+			<li><a href="https://www.carbontrust.com/resources/guides/sector-based-advice/retail-and-distribution/" target="_blank" rel="noopener noreferrer">More information from the Carbon Trust</a></li>
+			<li><a href="http://www.wrap.org.uk/category/sector/retail" target="_blank" rel="noopener noreferrer">More information from Waste &amp; Resources Action Programme (WRAP)</a></li>
+			<li><a href="/new-business-through-action-on-carbon.php">Learn about the benefits of cutting your carbon emissions</a></li>
+		</ul>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
