f946e4c64474d3de109434ecf368417f1e6950f0
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-22 15:15:23 +0000

    Completed ‘HMRC Weekly Update - 7 September 2012’ page.

---
+++ hmrcs-weekly-employer-update_07-09-12.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>7 September 2012 | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+			
+		<h1>HMRC Weekly Update</h1>
+		<h2>7 September 2012</h2>
+		<p>This is our one hundred and thirty first weekly update for you and, if appropriate, is for you to use with your respective membership and contacts. When sharing these updates with colleagues and networks, I would be very grateful if you would stress the context in which these are sent; namely as informal highlights as a result of listening to employer feedback and working in partnership with you.</p>
+		
+		<p><strong>Updated PDV Application</strong></p>
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20140109143644/http://www.hmrc.gov.uk/paye/tools/PDV/index.htm" target="_blank" rel="noopener noreferrer">PAYE Desktop Viewer (PDV) - required update for all users</a></p>
+		<p>HM Revenue and Customs has updated the PDV application. All PDV users should download and install the new version of the PDV now to ensure that they can view the latest versions of these notices.</p>
+		
+		<p><strong>Regulation 67CA</strong></p>
+		<p><a href="../_files/direction-under-regulation-67ca-of-the-income-tax_pay-as-you-earn_regulations-2003.pdf" target="_blank" rel="noopener noreferrer">Direction under regulation 67CA of the Income Tax (Pay As You Earn) Regulations 2003 (PDF 20K)</a></p>
+		<p>This direction gives employers and banks technical details about how to fulfill their obligations under regulation 67CA and comes into effect from 1 September 2012.</p>
+		
+		<p><strong>VAT</strong></p>
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20140109143644/http://www.hmrc.gov.uk/briefs/vat/brief2712.htm" target="_blank" rel="noopener noreferrer">Revenue &amp; Customs Brief 27/12 VAT: Addressing borderline anomalies - guidance</a></p>
+		<p>This Brief announces publication of guidance (in the form of 7 information sheets) on the changes to address VAT borderline anomalies announced at Budget 2012.</p>
+		<p><a href="https://www.gov.uk/reclaim-vat/cars" target="_blank" rel="noopener noreferrer">What is a car, commercial vehicle or motor home for VAT</a></p>
+		<p>Updated table adding 3 new models.</p>
+		<p><a href="../_files/vat-notes_03-12.pdf" target="_blank" rel="noopener noreferrer">VAT Notes 03/12</a></p>
+		<p>HM Revenue &amp; Customs (HMRC) publish VAT Notes quarterly. They contain a summary of all recent changes to the VAT rules and announce future changes.</p>
+		<p><a href="vat_426.pdf" target="_blank" rel="noopener noreferrer">Form VAT 426</a></p>
+		<p>The form has been redesigned to make it easier to complete, as well as updating National Insolvency Unit address for sending completed form.</p>
+		
+		<p><strong>QROPS</strong></p>
+		<p><a href="https://www.gov.uk/government/publications/list-of-qualifying-recognised-overseas-pension-schemes-qrops" target="_blank" rel="noopener noreferrer">Qualifying Recognised Overseas Pension Schemes (QROPS) (PDF 768K)</a></p>
+		<p>HM Revenue &amp; Customs has published an update to the QROPS list.</p>
+		
+		<p><strong>Employee Benefit Trust - update</strong></p>
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20140109143644/http://www.hmrc.gov.uk/news/ebt-news0812.htm" target="_blank" rel="noopener noreferrer">Employee Benefit Trust, (EBT) Settlement Opportunity - update</a></p>
+		<p>This update provides an overview of developments since April 2011 and a range of Frequently Asked Questions (FAQ) to help explain to customers more fully how their liabilities will be calculated.</p>
+		
+		<p><strong>Patent Box and Controlled Foreign Company (CFC) legislation</strong></p>
+		<p><a href="https://www.gov.uk/guidance/corporation-tax-the-patent-box" target="_blank" rel="noopener noreferrer">Patent Box roadshows</a></p>
+		<p>Roadshows of interest to accountancy firms representing smaller businesses that may be able to benefit from the Patent Box.</p>
+		
+		<p><strong>HMRC Compliance checks</strong></p>
+		<p><a href="https://www.gov.uk/government/collections/hm-revenue-and-customs-leaflets-factsheets-and-booklets#compliance-checks-factsheets-general-information" target="_blank" rel="noopener noreferrer">Compliance checks factsheets</a></p>
+		<p>HM Revenue &amp; Customs has updated the factsheets to make it clearer how and why they carry out certain activities during a compliance check.</p>
+		
+		<p><strong>Confirmation of your National Insurance number</strong></p>
+		<p><a href="https://www.gov.uk/government/publications/national-insurance-get-your-national-insurance-number-in-writing-ca5403" target="_blank" rel="noopener noreferrer">Emailing HM Revenue &amp; Customs for written confirmation of your National Insurance number</a></p>
+		<p>HMRC has introduced an electronic version of form CA5403 so you can now email to ask for written confirmation of your National Insurance number.</p>
+		
+		<p><strong>Guidance on completion of Form C1454</strong></p>
+		<p><a href="guidance-on-the-completion-of-approved-exporter-application-form-c1454.pdf" target="_blank" rel="noopener noreferrer">Customs Information Paper (12) 42 (PDF 42K)</a></p>
+		<p>Specific Guidance on the completion of Form C1454 (Approved Exporter application).</p>
+		
+		<p><strong>New HMRC Tracker advising when to expect a reply to an enquiry</strong></p>
+		<p><a href="http://www.hmrc.gov.uk/tools/progress-tool/index.htm" target="_blank" rel="noopener noreferrer">Where's my reply</a></p>
+		<p>Agents, employees and those paying tax on company pensions through Pay As You Earn can use a new tracker to find out when to expect to receive a reply to their enquiry from HM Revenue &amp; Customs.</p>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
