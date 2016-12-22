302af1b1859c7d4370f3a80f981828e1299f72dd
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-22 15:20:50 +0000

    Completed ‘HMRC's weekly employer update - 31 August 2012’ page.

---
+++ hmrcs-weekly-employer-update_31-08-12.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>31 August 2012 | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>HMRC's weekly employer update</h1>
+		<h2>31 August 2012</h2>
+		<p>Dear Customers and Stakeholders,</p>
+		<p>This is our one hundred and thirtieth weekly update for you and, if appropriate, is for you to use with your respective membership and contacts. When sharing these updates with colleagues and networks, I would be very grateful if you would stress the context in which these are sent; namely as informal highlights as a result of listening to employer feedback and working in partnership with you.</p>
+		
+		<p><strong>Shares &amp; Securities Bulletin</strong></p>
+		<p><a href="../_files/employment-related-shares-and-securities-bulletin_08-12.pdf" target="_blank" rel="noopener noreferrer">Employment-Related Shares &amp; Securities Bulletin (PDF, 42K)</a> </p>
+		<p>The third Employment-Related Shares &amp; Securities Bulletin providing information and updates on developments relating to employment-related securities, including the tax-advantaged employee share schemes has been published.</p>
+		
+		<p><strong>Fuel Rates updated</strong></p>
+		<p><a href="https://www.gov.uk/government/publications/advisory-fuel-rates" target="_blank" rel="noopener noreferrer">Advisory Fuel Rates updated</a> </p>
+		<p>HM Revenue &amp; Customs has published advisory fuel rates to operate from 1 September 2012.</p>
+		
+		<p><strong>Tax relief models</strong></p>
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20140109143644/http://www.hmrc.gov.uk/news/news290812.htm" target="_blank" rel="noopener noreferrer">Pay day by pay day tax relief models and dispensations</a> </p>
+		<p>HMRC has published a further statement on pay day by pay day tax relief models and dispensations.</p>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
