6719ef7c6a414fe7caa42f49c56b0cc21a955e5f
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-22 15:23:49 +0000

    Completed ‘How to engage a professional adviser’ page.

---
+++ how-to-engage-a-professional-adviser.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>How to engage a professional adviser | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+			
+		<h1>How to engage a professional adviser</h1>
+		<p>If you are interested in using the services of an accountant or freight forwarder, select one of the guides below to find out about what they can do for your business.</p>
+		<h3>Accountants</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a> </li>
+		</ul>
+		<h3>Freight Forwarders</h3>
+		<ul>
+			<li><a href="/international-trade/freight-forwarding/using-brokers-and-forwarders.php">Using brokers and forwarders</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
