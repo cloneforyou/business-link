12ba420aced156be7cd111ecffdc22f57cd8ff84
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-22 16:48:11 +0000

    Completed ‘Resource efficiency - Hospitality and Catering’ page. ~ Awaiting vimeo conversion to insert.

---
+++ resource-efficiency-hospitality-and-catering-tips.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Resource efficiency - Hospitality and Catering | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<div class="flex-video widescreen vimeo">
+			<iframe src="http://player.vimeo.com/video/192278297?badge=0&byline=0&portrait=0&title=0" width="720" height="540" allowfullscreen style="border:0"></iframe>
+		</div>
+
+		<hr />
+
+		<script type="text/javascript" src="../../../businesslink.edgeboss.net/download/businesslink/flash_player/swfobject.js">
+		<!--
+
+		//-->
+		</script>
+		<a href="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash">Adobe Flash Player</a> is required to view this content.
+		<script type="text/javascript">
+		<!--
+
+		var flashvars = {
+			height: "167",
+			width: "740"
+		};
+		var params = {
+			wmode: "transparent"
+		};
+		var attributes = {};
+		swfobject.embedSWF("http://businesslink.edgeboss.net/download/businesslink/flash/campaigns/resource_efficiency/hospitality_and_catering.swf", "previewvid", "740", "167", "9.0.0", "http://businesslink.edgeboss.net/download/businesslink/flash_player/expressinstall.swf", flashvars, params, attributes);
+
+		//-->
+		</script>
+
+		<hr />
+
+		<ul>
+			<li><a href="/resource-efficiency-general-tips.php">General tips</a></li>
+			<li><a href="/resource-efficiency-hospitality-and-catering-tips.php">Hospitality &amp; Catering</a></li>
+			<li><a href="/resource-efficiency-retail-tips.php">Retail</a></li>
+			<li><a href="/resource-efficiency-manufacturing-tips.php">Manufacturing</a></li>
+		</ul>
+		
+		<h2>Hospitality and Catering tips</h2>
+		<p>Here are some case studies showing how your business can save money and increase efficiency</p>
+		
+		<h3>1. Reduce, reuse, and recycle where possible</h3>
+		<img height="140" alt="Reduce, reuse, and recycle where possible" src="../../_img/re_hospitality_1.jpg" width="250">
+		<p>Independent family run hotel Strattons in Norfolk considered a wide range of ways in which they could achieve cost savings and environmental benefits. Their commitment to eliminate, reduce, re-use and recycle as much waste as possible led to a total cost savings of over &pound;10,000 a year.</p>
+		<ul>
+			<li>The hotel refillable toiletry dispensers saved nearly &pound;2,000 a year in purchase costs and water saving initiatives reduces dependency on mains water</li>
+			<li>Good housekeeping and laundry policy saves &pound;4,050 a year, and over 98% of all waste is re-used or recycled</li>
+		</ul>
+		<p><a href="../../_files/Envirowise_Strattons_case_study.pdf" target="_blank" rel="noopener noreferrer">Read the full case study (PDF, 2694KB)</a></p>
+		<p><a href="/environment-and-efficiency/offices-retail-and-hospitality/use-resources-efficiently-in-your-hospitality-business/here-is-how-our-hospitality-business-cut-costs-by-using-resources-efficiently_flash-video.php">Watch the video case study</a></p>
+		
+		<h3>2. Cut energy costs</h3>
+		<img height="140" alt="Cut energy costs" src="../../_img/re_hospitality_2.jpg" width="250">
+		<p>Surrey's Rhodes Foods has operated a chain of "Superfish" restaurants and takeaways for the last 30 years. Their out-of-date fryers were contributing to high energy costs, and so they applied for an interest-free Carbon trust loan of &pound;21,000 to invest in seven new models. As a result, these will save the company around &pound;4,000 a year, so the loan should be paid back within five years, after which savings go straight to the company.</p>
+		<p><a href="#">Read the full case study</a> </p>
+		
+		<h3>3. Look into recycling food waste with local businesses</h3>
+		<img height="140" alt="Look into recycling food waste" src="../../_img/re_hospitality_3.jpg" width="250">
+		<p>The Best Western Premier Mount Pleasant Hotel in Doncaster used to send its catering waste to pig farms as feed, but since the ban on this practice in 2003, the hotel was left with little option but to send their food waste to landfill. Through working with local businesses the hotel trialled an alternative way of getting rid of food waste using Grott boxes.</p>
+		<p><a href="#">Read the full case study (PDF, 1150KB)</a> </p>
+		
+		<h3>Further links:</h3>
+		<ul>
+			<li><a href="https://www.carbontrust.com/resources/guides/sector-based-advice/hotels-and-the-hospitality-industry/" target="_blank" rel="noopener noreferrer">More information from the Carbon Trust</a> </li>
+			<li><a href="http://www.wrap.org.uk/" target="_blank" rel="noopener noreferrer">More information from Waste &amp; Resources Action Programme (WRAP)</a> </li>
+			<li><a href="#">More tips on hospitality from Envirowise (PDF, 220KB)</a></li>
+			<li><a href="#">More tips on food preparation from Envirowise (PDF, 225KB)</a></li>
+			<li><a href="#">More tips on guest rooms from Envirowise (PDF, 219KB)</a></li>
+			<li><a href="/new-business-through-action-on-carbon.php">Learn about the benefits of cutting your carbon emissions</a></li>
+		</ul>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
