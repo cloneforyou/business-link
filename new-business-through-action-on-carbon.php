7af26afc31b6dfa58b063e07375e9460e60a84a2
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-22 16:30:25 +0000

    Completed ‘New Business through action on Carbon’ page.

---
+++ new-business-through-action-on-carbon.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>New Business through action on Carbon | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Act on Co2penhagen</h1>
+		<h2>New Business through action on Carbon</h2>
+		<p>In July, the UK Government published the Low Carbon Transition Plan - setting out how the UK will move to a low carbon economy. In December more than 180 countries will join together in Copenhagen to agree a new international deal that will aim put the world onto a growth path that will avoid dangerous climate change.</p>
+		<p>The deal reached in Copenhagen will have important implication for businesses all around the world, of all sizes in all industries, driving the global transition to a prosperous low carbon future.</p>
+		<p>For UK companies, the global requirement to reduce emissions will add to the business case to use less energy and water, and produce less waste. See our guide on <a href="/environment-and-efficiency/carbon-reduction/cutting-your-carbon-emissions.php">cutting your carbon emissions</a>.</p>
+		<p>Reducing your carbon emissions makes good business sense, as the cost savings that can be achieved through reducing energy use go straight to the bottom line. See our guide on how to <a href="/environment-and-efficiency/cut-energy-costs/save-money-by-using-energy-more-efficiently.php">save money by using energy more efficiently</a>.</p>
+		<p>In some cases, action will be voluntary, relying on a good common-sense approach to cost control, but in some key areas it will be through legislation, such as the Waste Electrical and Electronic Equipment Directive.</p>
+		<p>For larger energy users there are regulatory measures to incentivise CO2 reductions, such as the new CRC Energy Efficiency Scheme (formerly known as the Carbon Reduction Commitment), which will enter into force from April 2010, and Climate Change Agreements.</p>
+		<p>But Copenhagen is about more than just efficiency. An ambitious deal will also offer massive opportunities for growth in a new low carbon economy. Businesses of all sizes can benefit from growing market opportunities in:</p>
+		<ul>
+			<li>developing, financing or fitting renewable technology - see our guide on how to <a href="/environment-and-efficiency/carbon-reduction/generate-your-own-renewable-energy.php">generate your own renewable energy</a></li>
+			<li>offering consultancy services to companies needing to meet efficiency goals</li>
+		</ul>
+		<p>Find further environmental guidance, tools and case studies in the <a href="/environment-and-efficiency.php">environment &amp; efficiency</a> area of our website.</p>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<img src="../../_img/act_on_copenhagen_181.jpg" width="181" height="121" border="0" alt="Act on Co2penhagen" />
+
+		<ul>
+			<li><a href="#">Act on Co2penhagen information on the Department of Energy and Climate Change website</a></li>
+			<li><a href="../../_files/business_success_low_carbon_economy_co2.pdf" target="_blank" rel="noopener noreferrer">Download a guide to business success in a low carbon economy (PDF, 428KB)</a></li>
+			<li><a href="/resource-efficiency-general-tips.php">Top ten tips to help your business save money and increase efficiency</a>&nbsp;</li>
+			<li><a href="https://www.gov.uk/guidance/measuring-and-reporting-environmental-impacts-guidance-for-businesses" target="_blank" rel="noopener noreferrer">Greenhouse gas measuring and reporting guidance on the Department for Environment Food and Rural Affairs website</a></li>
+			<li><a href="https://www.carbontrust.com/resources/faqs/services/loans/" target="_blank" rel="noopener noreferrer">Energy saving equipment loans information on the Carbon Trust website</a></li>
+			<li><a href="http://www.bitc.org.uk/programmes/princes-mayday-network" target="_blank" rel="noopener noreferrer">May Day Journey information on the Business in the Community website</a></li>
+			<li><a href="#">Find out how reducing your carbon output can benefit your business on the Department for Business Innovation and Skills website</a></li>
+		</ul>
+
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
