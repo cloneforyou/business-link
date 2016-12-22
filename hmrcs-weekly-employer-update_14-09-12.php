6beae9166cc39dc014f728918495117d1773116f
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-22 15:17:47 +0000

    Completed ‘HMRC Weekly Update - 14 September 2012’ page.

---
+++ hmrcs-weekly-employer-update_14-09-12.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>14 September 2012 | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>HMRC Weekly Update</h1>
+		<h2>14 September 2012</h2>
+		<p>Dear Customers and Stakeholders,</p>
+		<p>This is our one hundred and thirty second weekly update for you and, if appropriate, is for you to use with your respective membership and contacts.  When sharing these updates with colleagues and networks, I would be very grateful if you would stress the context in which these are sent; namely as informal highlights as a result of listening to employer feedback and working in partnership with you.</p>
+		
+		<h3>Real Time Information</h3>
+		<p>The pilot continues to go well and is on track. No significant issues have emerged and employers have given us positive feedback, saying how easy reporting in real time has been.</p>
+		<p>At 9 September, 1,431 PAYE schemes covering 1.91 million individuals had submitted information in real time. This means we have exceeded our target of having up to 1,300 schemes reporting PAYE in real time by end of September.</p>
+		<p>We have a wide range of employer types and sizes in the pilot, ranging from one to over one million individuals, and including users of commercial software, payroll services and HMRC's Basic PAYE Tools.</p>
+		<p>External confidence is growing with more software developers and large payroll bureaux asking to come on board or extend their existing involvement &ndash; more evidence that the pilot is going well.</p>
+		
+		<h3>National Minimum Wage</h3>
+		<p>The Department for Business, Innovation and Skills has announced that the rates of national minimum wage applicable to pay reference periods starting on or after 1 October 2012 will be as follows:</p>
+		<ul>
+		<li>the main adult rate (for workers 21 and over) will increase by 11p to &pound;6.19 an hour (currently &pound;6.08 an hour)</li>
+		<li>the rate for 18-20 year olds will remain at &pound;4.98 an hour</li>
+		<li>the rate for 16-17 year olds will remain at &pound;3.68 an hour</li>
+		<li>the rate for apprentices will increase by 5p to &pound;2.65 an hour (currently &pound;2.60 an hour).</li>
+		</ul>
+		<p>From the same date, the daily accommodation offset rate will increase from the current &pound;4.73 to &pound;4.82.</p>
+		
+		<h3>Employee queries where operation of 'week 53 payment' results in an underpayment of tax</h3>
+		<p>In some tax years, the way dates fall mean that employers will end up making one more payment than usual to their employees towards the end of the tax year. If employees are paid weekly this would be a 53rd weekly payment, if paid fortnightly it would be a 27th fortnightly payment and if paid four-weekly it would be a 14th four-weekly payment. These extra payments are often referred to as 'Week 53 payments'. <a href="http://webarchive.nationalarchives.gov.uk/20140109143644/http://www.hmrc.gov.uk/paye/payroll/paydays/week-53.htm" target="_blank" rel="noopener noreferrer">Find information on week 53 payments on the HMRC website</a>.</p>
+		<p>This may lead to an underpayment of PAYE as an individual's personal allowance for the year is worked out on the basis of 52 weekly, 26 fortnightly and 13 four-weekly paydays.</p>
+		<p>Employees who have been notified by HMRC that they have underpaid and query this with their employer should be advised to read the notes enclosed with the calculation. Alternatively, <a href="https://www.gov.uk/tax-overpayments-and-underpayments" target="_blank" rel="noopener noreferrer">find information on checking your P800 tax calculation on the HMRC website</a>.
+		</p>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
