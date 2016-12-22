0060bba8bed4bbcc09d51e4e5883ec92071f27df
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-22 16:12:02 +0000

    Completed ‘Resource efficiency - General tips’ page. ~Awaiting vimeo conversion to insert.

---
+++ resource-efficiency-general-tips.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Resource efficiency - General tips | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<div class="flex-video widescreen vimeo">
+			<iframe src="http://player.vimeo.com/video/192278297?badge=0&byline=0&portrait=0&title=0" width="720" height="540" allowfullscreen style="border:0"></iframe>
+		</div>
+
+		<hr />
+		<script type="text/javascript" src="../../../businesslink.edgeboss.net/download/businesslink/flash_player/swfobject.js">
+		</script>
+		<script type="text/javascript">
+		var flashvars = {
+			height: "167",
+			width: "740"
+		};
+		var params = {
+			wmode: "transparent"
+		};
+		var attributes = {};
+		swfobject.embedSWF("http://businesslink.edgeboss.net/download/businesslink/flash/campaigns/resource_efficiency/General_Tips.swf", "previewvid", "740", "167", "9.0.0", "http://businesslink.edgeboss.net/download/businesslink/flash_player/expressinstall.swf", flashvars, params, attributes);
+
+		</script>
+
+		<hr />
+
+		<ul>
+			<li><a href="#generaltips">General tips</a></li>
+			<li><a href="/resource-efficiency-hospitality-and-catering-tips.php">Hospitality &amp; Catering</a></li>
+			<li><a href="/resource-efficiency-retail-tips.php">Retail</a></li>
+			<li><a href="/resource-efficiency-manufacturing-tips.php">Manufacturing</a></li>
+		</ul>
+		<h2><a name="generaltips">General tips</a></h2>
+		<p>Top ten tips to help your business save money and increase efficiency.</p>
+		
+		<h3>1. Turn off office equipment when you're not using it</h3>
+		<p>Overnight, at weekends and during bank holidays. Savings on just a single computer and monitor could be up to &pound;35 a year.</p>
+		<p><a href="/environment-and-efficiency/cut-energy-costs/save-money-by-using-energy-more-efficiently/create-an-energy-efficient-work-culture.php">More information on using office equipment efficiently</a></p>
+		
+		<h3>2. Cut lighting costs by as much as 15%</h3>
+		<p>Turning off lights in empty rooms and replacing bulbs with energy saving ones.</p>
+		<p><a href="https://www.carbontrust.com/resources/guides/energy-efficiency/lighting/" target="_blank" rel="noopener noreferrer">More information on using lighting efficiently</a></p>
+		
+		<h3>3. A constantly dripping tap can cost your business about &pound;400 per year</h3>
+		<p>Make sure you turn off taps fully and fix any drips.</p>
+		<p><a href="/environment-and-efficiency/cut-water-costs/use-water-efficiently-the-basics.php">More information on using water efficiently</a></p>
+		
+		<h3>4. Save 4% of your turnover with waste management techniques</h3>
+		<p>Such as partnering with other businesses to recycle waste or selling it to be used as raw materials.</p>
+		<p><a href="/environment-and-efficiency/reducing-and-recycling-waste/save-money-by-reducing-reusing-and-recycling-waste/reducing-waste.php">More information on reducing waste</a></p>
+		
+		<h3>5. Save 8% on your company's heating bill</h3>
+		<p>Turn the thermostat down one degree.</p>
+		<p><a href="https://www.carbontrust.com/resources/guides/energy-efficiency/heating-ventilation-and-air-conditioning-hvac/" target="_blank" rel="noopener noreferrer">More information on using heating efficiently</a></p>
+		
+		<h3>6. Ask your suppliers about take-back schemes for unused products</h3>
+		<p>You may be able to get your money back, or at least a portion of the cost.</p>
+		<p><a href="/environment-and-efficiency/your-supply-chain/reduce-the-environmental-impact-of-your-supply-chain.php">More information on making your supply chain greener</a></p>
+		
+		<h3>7. Save up to 1 month's worth of fuel</h3>
+		<p>Over a year through smarter driving such as reducing load and good maintenance.</p>
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20121015120517/http://direct.gov.uk/en/environmentandgreenerliving/thewiderenvironment/index.htm" target="_blank" rel="noopener noreferrer">More information on eco-driving</a></p>
+		
+		<h3>8. Invest in efficient equipment to cut your energy and water costs</h3>
+		<p>Government support includes interest-free energy efficiency loans delivered through the Carbon Trust.</p>
+		<p><a href="https://www.carbontrust.com/resources/faqs/services/loans/" target="_blank" rel="noopener noreferrer">More information on Energy Efficiency Loans</a></p>
+		
+		<h3>9. To refill an inkjet cartridge costs about half the price of a new one</h3>
+		<p>Use email where possible, set your PC to print double-sided and refill printer, fax and photocopier cartridges.</p>
+		<p><a href="/environment-and-efficiency/reducing-and-recycling-waste/save-money-by-reducing-reusing-and-recycling-waste/reusing-waste.php">More information on reducing office waste</a></p>
+		
+		<h3>10. Always publicise your commitment to reducing your environmental impact</h3>
+		<p>More than a third of consumers would favour recyclable products or those with minimal packaging.</p>
+		<p><a href="/environment-and-efficiency/selling-and-marketing/market-your-environmental-credentials.php">More information on how to market your environmental credentials</a></p>
+		
+		<h3>Useful Links:</h3>
+		<ul>
+			<li><a href="https://www.carbontrust.com/home/" target="_blank" rel="noopener noreferrer">Cut energy use with help from Carbon Trust</a></li>
+			<li><a href="https://web-beta.archive.org/web/20120106022627/http://www.nisp.org.uk:80/default.aspx" target="_blank" rel="noopener noreferrer">Share resources with other organisations with support from the National Industrial Symbiosis Programme (NISP)</a></li>
+			<li><a href="http://www.wrap.org.uk/" target="_blank" rel="noopener noreferrer">Recycle your waste with support from the Waste &amp; Resources Action Programme (WRAP)</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20121015120517/http://direct.gov.uk/en/environmentandgreenerliving/thewiderenvironment/index.htm" target="_blank" rel="noopener noreferrer">Practical water efficiency tips from the Act on C02 website</a></li>
+			<li><a href="/new-business-through-action-on-carbon.php">Learn about the benefits of cutting your carbon emissions</a></li>
+			<li><a href="/environment-and-efficiency.php">Practical advice on environment and efficiency for your business</a></li>
+		</ul>
+		<p>For impartial advice and support to help your business succeed, call Business Link on <strong>0845 600 9 006</strong></p>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
