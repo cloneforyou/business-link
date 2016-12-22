dee1c6a5b570c61f0c13a9cb96d50a1a907c3df4
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-22 15:28:42 +0000

    Completed ‘You may be able to save up to £50,000. Regional employer NICs holiday for new businesses’ page.

---
+++ you-may-be-able-to-save-up-to-50k.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Regional employer NICs holiday for new businesses | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+			
+		<h1>You may be able to save up to &pound;50,000</h1>
+		<h2>Regional employer NICs holiday for new businesses</h2>
+		<p>The Regional Employer National Insurance contributions (NICs) holiday is available to new businesses for a limited period and subject to meeting certain conditions. New businesses may be able to qualify for a reduction of up to &pound;5,000 of the employer NICs that would normally be due - for each of the first ten employees they take on. This means a potential aid to business of up to &pound;50,000.</p>
+		<p>This page contains everything you need to help you decide if you are eligible to apply and what you should do next.</p>
+		
+		<h3>Guidance available to support employers</h3>
+		<ul>
+		<li><a href="http://webarchive.nationalarchives.gov.uk/20140109143644/http://www.hmrc.gov.uk/paye/intro/nics-holiday/eligibility.htm" target="_blank" rel="noopener noreferrer">Find out who can apply for an employer NICs holiday on the HMRC website</a>. Read this guide first to help you decide if you are eligible for this scheme - includes a link to the application form.</li>
+		<li><a href="http://webarchive.nationalarchives.gov.uk/20140109143644/http://www.hmrc.gov.uk/paye/intro/nics-holiday/calc.htm" target="_blank" rel="noopener noreferrer">Find out how to calculate and record an employer NICs holiday on the HMRC website</a>. Once your application has been accepted, you can begin to calculate the employer NICs that you can withhold and keep all the records you need to - includes a link to the recording tool.</li>
+		<li><a href="http://webarchive.nationalarchives.gov.uk/20140109143644/http://www.hmrc.gov.uk/paye/intro/nics-holiday/business-changes.htm" target="_blank" rel="noopener noreferrer">Read about employer NICs holiday change of circumstances on the HMRC website</a>. If you have started to take advantage of the NICs holiday and your circumstances change, you need to check that you still qualify.</li>
+		<li><a href="http://webarchive.nationalarchives.gov.uk/20140109143644/http://hmrc.gov.uk/payerti/bau-redirect.htm" target="_blank" rel="noopener noreferrer">Read about completing the NICs holiday End of Year Return on the HMRC website</a>. If you have successfully registered for the NICs holiday, read this guidance to help you complete the NICs holiday End of Year Return.</li>
+		</ul>
+		
+		<h3>Tools to help you</h3>
+		<p>The tools available in this section are also available from the appropriate guide available to support employers. They are listed here as quick links for those revisiting this page and wishing to access the tools directly.</p>
+		<ul>
+		<li><strong>Region checker</strong> - One of the key things you need to decide before you apply for the NICs holiday is whether your business is located in a qualifying region. You can find a list of the local authority areas which are in qualifying regions and a postcode tool to help you check which local authority area your business is located in on the HMRC website. <a href="https://www.gov.uk/guidance/regional-employer-nics-holiday-making-a-retrospective-claim" target="_blank" rel="noopener noreferrer">Check your business is in a qualifying region on the HMRC website</a>.
+		<br />
+		</li>
+		<li><strong>Online recording tool</strong> - Once your application has been accepted, you'll need to calculate and record the employer NICs that you are eligible to withhold under this scheme. The Employer NICs Holiday Recording Tool on the HMRC website can help you to do this. <a href="https://www.gov.uk/government/publications/paye-regional-employer-national-insurance-contributions-holiday-e89" target="_blank" rel="noopener noreferrer">Download the Employer NICs Holiday Recording Tool from the HMRC website (PDF, 111K)</a>.</li>
+		</ul>
+		
+		<h3>Technical and policy documents for accountants and payroll advisers</h3>
+		<p>If you're an employer and want to know more about these subjects, <a href="http://webarchive.nationalarchives.gov.uk/20140109143644/http://www.hmrc.gov.uk/paye/intro/nics-holiday/eligibility.htm" target="_blank" rel="noopener noreferrer">find out who can apply for employer NICs holidays on the HMRC website</a>. This will direct you to the specialist guides where necessary.</p>
+		<p>In addition to the guidance available from this page there are more detailed documents in the HMRC Library. These cover the more unusual situations such as businesses HMRC may not consider to be new and some anomalies for business who already receive support in the form of State Aid.</p>
+		<p>Payroll advisers and professional representatives may find this guidance useful to supplement the basic guides. You can <a href="https://www.gov.uk/guidance/regional-employer-nics-holiday-making-a-retrospective-claim" target="_blank" rel="noopener noreferrer">browse the HMRC Library on the HMRC website</a>.</p>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
