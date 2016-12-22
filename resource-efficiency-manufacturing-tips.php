87b4e4a4789606afcdc138a2ced1fb05a045fb45
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-22 16:59:53 +0000

    Completed ‘Resource efficiency - Manufacturing’ page. ~ Awaiting vimeo video

---
+++ resource-efficiency-manufacturing-tips.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Resource efficiency - Manufacturing | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<div class="flex-video widescreen vimeo">
+			<iframe src="http://player.vimeo.com/video/192278297?badge=0&byline=0&portrait=0&title=0" width="720" height="540" allowfullscreen style="border:0"></iframe>
+		</div>
+
+		<hr />
+
+		<script type="text/javascript" src="../../../businesslink.edgeboss.net/download/businesslink/flash_player/swfobject.js"></script>
+		<a href="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash">Adobe Flash Player</a> is required to view this content.
+		<script type="text/javascript">
+		var flashvars = {
+			height: "167",
+			width: "740"
+		};
+		var params = {
+			wmode: "transparent"
+		};
+		var attributes = {};
+		swfobject.embedSWF("http://businesslink.edgeboss.net/download/businesslink/flash/campaigns/resource_efficiency/manufacturing.swf", "previewvid", "740", "167", "9.0.0", "http://businesslink.edgeboss.net/download/businesslink/flash_player/expressinstall.swf", flashvars, params, attributes);
+		</script>
+
+		<hr />
+
+		<ul>
+			<li><a href="/resource-efficiency-general-tips.php">General tips</a></li>
+			<li><a href="/resource-efficiency-hospitality-and-catering-tips.php">Hospitality &amp; Catering</a></li>
+			<li><a href="/resource-efficiency-retail-tips.php">Retail</a></li>
+			<li>Manufacturing</li>
+		</ul>
+
+		<h2>Manufacturing tips</h2>
+		<p>Here are some case studies showing how your business can save money and increase efficiency</p>
+		
+		<h3>1. Involve your staff in finding a wide range of ways to become more environmentally friendly</h3>
+		<img height="140" alt="Involve your staff in becoming environmentally friendly" src="../../_img/re_manufacturing_1.jpg" width="250">
+		<p>Yeo Valley, a dairy product manufacturer, achieved cost savings and environmental benefits by introducing a number of resource efficiency measures such as waste minimisation, water minimisation and packaging. The benefits achieved were:</p>
+		<ul>
+			<li>433,600 road miles saved per year, worth &#163;715,440</li>
+			<li>477 tonnes of waste diverted from landfill from one site in 2007, saving over &#163;36,000</li>
+			<li>7,500 m3 of water saved in 2007; and a significant reduction in annual carbon emissions</li>
+		</ul>
+		<p><a href="../../_files/Envirowise_Yeovalley_case_study.pdf" target="_blank" rel="noopener noreferrer">Download the full case study (PDF, 2269KB)</a></p>
+		<p><a href="/environment-and-efficiency/food-and-drink-production/use-water-efficiently-in-your-food-and-drink-business/here-is-how-our-food-processing-business-cut-costs-by-using-water-efficiently_flash-video.php">Watch the video case study</a></p>
+		
+		<h3>2. Look for alternative uses for your waste materials instead of sending to landfill</h3>
+		<img height="140" alt="Look for alternatives to landfill" src="../../_img/re_manufacturing_2.jpg" width="250">
+		<p>James Robinson Fibres is one of the oldest textile companies still in existence in the UK. Following a detailed analysis of the carbon footprint, the company made a number of changes to their working practices including finding a way to reuse significant amounts of the carpet and textile waste. As a result:</p>
+		<ul>
+			<li>Additional product sales opportunities of &#163;250,000 and 50 tonnes of CO2 saving were identified</li>
+		</ul>
+		<p><a href="../../_files/James_Robinson_case_study.pdf" target="_blank" rel="noopener noreferrer">Download the full case study (PDF, 276KB)</a></p>
+		
+		<h3>3. Review waste management options to establish more cost-effective ways to deal with waste from manufacturing processes</h3>
+		<img height="140" alt="Establish cost-effective ways to deal with waste" src="../../_img/re_manufacturing_3.jpg" width="250">
+		<p>Snacks producer CTO Holdings Ltd was reviewing its waste management options to establish more cost-effective ways to deal with plastic waste arising from their manufacturing processes. Through sourcing a local firm willing to buy their plastics waste directly from CTO, they made cost savings of &#163;2,500 per year (&#163;10,000 expected long term) as the waste was diverted from landfill. They generated &#163;11,000 sales, diverted 55 tonnes of waste from landfill per year and avoided the use of 110 tonnes of virgin materials per year.</p>
+		<p><a href="#">Download the full case study (PDF, 132KB)</a></p>
+		
+		<h3>Further Links:</h3>
+		<ul>
+			<li><a href="https://www.carbontrust.com/resources/guides/sector-based-advice/sector-specific-publications/" target="_blank" rel="noopener noreferrer">More information from the Carbon Trust</a></li>
+			<li><a href="https://web-beta.archive.org/web/20120206064216/http://www.nisp.org.uk/about_us.aspx" target="_blank" rel="noopener noreferrer">More information from the National Symbiosis Programme (NISP)</a></li>
+			<li><a href="/new-business-through-action-on-carbon.php">Learn about the benefits&nbsp;of cutting your carbon emissions</a></li>
+		</ul>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
