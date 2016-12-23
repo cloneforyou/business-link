3710e348e901305a3343ae1d47e8a18d427e3b9c
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-23 13:02:59 +0000

    Completed ‘Your growth options’ page.

---
+++ starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/your-growth-options.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Your growth options | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business from home </h1>
+		<h2>Your growth options</h2>
+		<p>When it comes to growing your business, one popular option is to focus on what you do best and outsource the rest.</p>
+		<p>If you are thinking of employing people, you need to determine their employment status, for example whether you will treat them as employees, contractors or sub-contractors, or simply 'volunteer' family and friends.</p>
+		<p>See our guide on <a href="/employment-and-skills/take-on-staff/taking-on-staff-the-options.php">taking on staff - the options</a>.</p>
+		<p>If you employ someone, you will have extra responsibilities relating to health and safety.</p>
+		<p>See our guide on <a href="/workplace-health-and-safety/what-you-need-to-do-about-health-and-safety.php">what you need to do about health and safety</a>.</p>
+		<h3>Outsourcing</h3>
+		<p>You can grow your business without increasing headcount or expanding your premises, by outsourcing, for example:</p>
+		<ul>
+			<li>accounts</li>
+			<li>administration</li>
+			<li>telemarketing - for generating leads</li>
+			<li>PR</li>
+			<li>design and copywriting</li>
+		</ul>
+		<p>For more information, see our guide on <a href="/grow-your-business/growth-through-strategic-sourcing/outsourcing.php">outsourcing</a>.</p>
+		<h3>Meeting spaces</h3>
+		<p>As your business grows, you may find that your home is no longer suitable for all your business needs. Remember that you can rent managed workspace and meeting facilities for occasional use through business centres. These can be found in central locations in many towns across the UK.</p>
+		
+		<ul>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/managing-your-home-based-business.php" title="Managing your home-based business">Prev</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php" title="Starting a business from home ">Introduction</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/is-starting-a-home-based-business-right-for-you.php">Is starting a home-based business right for you?</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/popular-home-business-ideas.php">Popular home business ideas</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/start-up-costs-and-financing-your-home-based-business.php">Start-up costs and financing your home-based business</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/record-keeping.php">Record keeping</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/it-and-home-business.php">IT and home business</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/your-work-environment.php">Your work environment</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/managing-your-home-based-business.php">Managing your home-based business</a></li>
+			<li>Your growth options</li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/create-an-action-plan-to-successfully-grow-your-business.php">Use our interactive tool to identify the areas your business will need to work on to grow successfully</a></li>
+			<li><a href="/employment-and-skills/take-on-staff/help-with-recruitment.php">Use our interactive tools to help you take on a new employee</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/assess-your-options-for-growth.php">Assess your options for growth</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/prepare-a-business-plan-for-growth.php">Prepare a business plan for growth</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
