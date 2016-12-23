dc7d80b75b57e434ea837e819fcb1640a762dc86
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-23 12:52:48 +0000

    Completed ‘Your work environment’ page.

---
+++ starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/your-work-environment.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Your work environment | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business from home </h1>
+		<h2>Your work environment</h2>
+		<p>It is important to create dedicated work space - it allows you to work without distraction and close the door on work at the end of the day.</p>
+		<h3>Workspace priorities</h3>
+		<p>The area that you use for work should ideally be clearly marked as such. A spare room with a lockable door or an outbuilding are popular choices enabling home business owners to:</p>
+		<ul>
+			<li>deal with clients in a professional manner</li>
+			<li>resist demands from other members of the household</li>
+			<li>keep work equipment separate from home equipment and therefore protected</li>
+		</ul>
+		<p>Allocating part of your house as a workplace can have tax and insurance implications. For more information, see the page on <a href="/premises-and-property/choosing-the-right-premises/use-your-home-as-a-workplace/tax-considerations-of-working-from-home.php">tax considerations of working from home</a> in our guide on how to <a href="/premises-and-property/choosing-the-right-premises/use-your-home-as-a-workplace.php">use your home as a workplace</a>.</p>
+		<p>You can create a space to call your own by:</p>
+		<ul>
+			<li>using office equipment in a general area of the house and putting it away when not in use</li>
+			<li>housing your workstation in a cupboard, wardrobe or under-stair space with lockable shutters or doors</li>
+		</ul>
+		<h3>Work equipment and workstation setup</h3>
+		<p>When setting up your work area:</p>
+		<ul>
+			<li>your chair and desk should be fully adjustable and suited for computer use</li>
+			<li>your desk should be big enough for your computer, keyboard and monitor</li>
+			<li>use storage to keep office clutter free</li>
+			<li>when sitting at your desk, keep your feet flat on the floor and your back straight - the top of your monitor should be at eye level</li>
+		</ul>
+		<p>For more information, see the page on <a href="/premises-and-property/choosing-the-right-premises/use-your-home-as-a-workplace/work-equipment-and-workstation-setup.php">work equipment and workstation setup</a> in our guide on how to <a href="/premises-and-property/choosing-the-right-premises/use-your-home-as-a-workplace.php">use your home as a workplace</a>.</p>
+		<p>You must carry out a health and safety risk assessment on your work space. For more information, see our guides on how to <a href="/workplace-health-and-safety/managing-the-risks-in-your-business.php">manage the risks in your business</a> and <a href="/workplace-health-and-safety/what-workplace-facilities-do-you-need-to-provide.php">what workplace facilities do you need to provide?</a></p>
+		<h3>Home business and planning permission</h3>
+		<p>If you are making substantial changes to your home to accommodate your business you should contact your local authority regarding planning permission. For more information, see the page on <a href="/premises-and-property/choosing-the-right-premises/use-your-home-as-a-workplace/building-regulations-and-planning-permission-for-home-businesses.php">building regulations and planning permission for home businesses</a> in our guide on how to <a href="/premises-and-property/choosing-the-right-premises/use-your-home-as-a-workplace.php">use your home as a workplace</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/it-and-home-business.php" title="IT and home business">Prev</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/managing-your-home-based-business.php" title="Managing your home-based business">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php" title="Starting a business from home ">Introduction</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/is-starting-a-home-based-business-right-for-you.php">Is starting a home-based business right for you?</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/popular-home-business-ideas.php">Popular home business ideas</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/start-up-costs-and-financing-your-home-based-business.php">Start-up costs and financing your home-based business</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/record-keeping.php">Record keeping</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/it-and-home-business.php">IT and home business</a></li>
+			<li>Your work environment</li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/managing-your-home-based-business.php">Managing your home-based business</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/your-growth-options.php">Your growth options</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.planningportal.co.uk/info/200130/common_projects/56/working_from_home" target="_blank" rel="noopener noreferrer">Working from home guidance on the Planning Portal website </a></li>
+			<li><a href="../../../_files/hse_homeworkers-guidance_for-employers-on-health-and-safety.pdf" target="_blank" rel="noopener noreferrer">Download homeworking guidance from the Health and Safety Executive website (PDF, 247K)</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/premises-and-property/choosing-the-right-premises/use-your-home-as-a-workplace.php">Use your home as a workplace</a></li>
+			<li><a href="/workplace-health-and-safety/what-workplace-facilities-do-you-need-to-provide.php">What workplace facilities do you need to provide?</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
