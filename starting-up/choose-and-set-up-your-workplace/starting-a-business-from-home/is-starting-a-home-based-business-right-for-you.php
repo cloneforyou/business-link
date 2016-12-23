96c01e2d16bc95515966d85ac468ac73187573f5
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-23 12:19:47 +0000

    Completed ‘Is starting a home-based business right for you?’ page.

---
+++ starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/is-starting-a-home-based-business-right-for-you.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Is starting a home-based business right for you? | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business from home </h1>
+		<h2>Is starting a home-based business right for you?</h2>
+		<p>Becoming a home business owner is an option if you don't actually need to run your business from rented or owned business premises.</p>
+		<p>It's a common choice for people who just need a small office, or who spend most of their time working on premises owned by their clients.</p>
+		<p>However, working from home may not be an option if it significantly changes the use of your home, or affects your local area, for example if you have lots of visitors. If you rent your home, your licence or lease may include restrictions on using it for business purposes.</p>
+		<h3>Advantages and disadvantages of home-based business</h3>
+		<p>The major <strong>advantages</strong> are that you:</p>
+		<ul>
+			<li>do not have a sizeable expenditure on office rental or office purchase</li>
+			<li>save time and money by cutting your commute</li>
+			<li>can be flexible around the hours you work</li>
+		</ul>
+		<p>The <strong>disadvantages</strong> are:</p>
+		<ul>
+			<li>it may prove difficult to keep work and home-life separate, and there may be domestic distractions and interruptions</li>
+			<li>a lack of contact with other people and businesses</li>
+			<li>you might end up working long hours</li>
+			<li>your mortgage, home insurance and tax situation may be affected - see our guide on how to <a href="/premises-and-property/choosing-the-right-premises/use-your-home-as-a-workplace.php">use your home as a workplace</a></li>
+		</ul>
+		<h3>Addressing the disadvantages</h3>
+		<p>It is important to keep your home and work life separate, especially if you have a family. There are simple steps you can take to make this separation - see the page in this guide on <a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/your-work-environment.php" title="Work equipment and workspace requirements, planning permission and questions to ask yourself">your work environment</a>.</p>
+		<p>If you are feeling isolated from other people, Business Link and other organisations can provide you with details of networking events. You can also get involved in online networking - see our guide on <a href="/sales-and-marketing/online-marketing/online-business-networking-and-social-networking.php" title="How to use online business networking and social media to develop your business">online networking</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php" title="Starting a business from home ">Prev</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/popular-home-business-ideas.php" title="Popular home business ideas">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php" title="Starting a business from home">Introduction</a></li>
+			<li>Is starting a home-based business right for you?</li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/popular-home-business-ideas.php">Popular home business ideas</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/start-up-costs-and-financing-your-home-based-business.php">Start-up costs and financing your home-based business</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/record-keeping.php">Record keeping</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/it-and-home-business.php">IT and home business</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/your-work-environment.php">Your work environment</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/managing-your-home-based-business.php">Managing your home-based business</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/your-growth-options.php">Your growth options</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.enterprisenation.com/" target="_blank" rel="noopener noreferrer">Networking community for home-based businesses on the Enterprise Nation website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/employment-and-skills/skills-and-development/learning-through-networking-with-others.php">Learning through networking with others</a></li>
+			<li><a href="/sales-and-marketing/online-marketing/online-business-networking-and-social-networking.php">Online business networking and social networking</a></li>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business.php">Starting a part-time business</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
