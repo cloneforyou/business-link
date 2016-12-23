da385fa235d5a13a5e6a2e4d316fb6396a04f942
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-23 12:31:39 +0000

    Completed ‘Start-up costs and financing your home-based business’ page.

---
+++ starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/start-up-costs-and-financing-your-home-based-business.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Start-up costs and financing your home-based business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business from home </h1>
+		<h2>Start-up costs and financing your home-based business</h2>
+		<p>If you are looking for some extra funds, there are various options to consider.</p>
+		<p>For more information, see our guides on how to <a href="/finance-and-grants/finance-options/choose-the-right-finance-options/choose-the-right-finance-when-starting-up.php">choose the right finance when starting up</a> and <a href="/finance-and-grants/prepare-to-raise-finance/how-to-attract-investment/use-your-business-plan-to-get-funding.php">use your business plan to get funding</a>.</p>
+		<p>You can also <a href="https://www.gov.uk/growing-your-business" target="_blank" rel="noopener noreferrer">search our business support finder for grants, loans, expertise and advice for which your business may be eligible</a>.</p>
+		<h3>Initial costs</h3>
+		<p>You will need to buy, lease or rent the equipment for your home office and any materials you need for the service you are supplying. Find out more in our guide <a href="/finance-and-grants/manage-your-cashflow/decide-whether-to-lease-or-buy-assets.php">decide whether to lease or buy assets</a>.</p>
+		<p>Finding and keeping in touch with customers is a major issue, so investing in a good computer with relevant software and a broadband connection will be a priority in most cases.</p>
+		<p>The most common costs to consider when starting out are:</p>
+		<ul>
+			<li>a computer or laptop</li>
+			<li>broadband access</li>
+			<li>mobile phone</li>
+			<li>office desk or chair</li>
+			<li>business cards</li>
+			<li>stock - if you're supplying products</li>
+		</ul>
+		<p><a href="/it-and-ecommerce/introduction-to-it/setting-up-a-basic-it-system.php">Use our interactive tool to find out which computer equipment you should buy for your business</a>.</p>
+		<p>When you are factoring software packages into your start up costs, take a look at the much cheaper or even free options - open-source software such as OpenOffice, Zoho, Google Docs and StarOffice.</p>
+		<p>For more information, see our guides on <a href="/it-and-ecommerce/introduction-to-it/computer-software-the-basics.php">computer software: the basics</a> and <a href="/it-and-ecommerce/software-and-business-applications/open-source-software.php">open source software</a> and the page in this guide on <a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/it-and-home-business.php">IT and home business</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/popular-home-business-ideas.php" title="Popular home business ideas">Prev</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/record-keeping.php" title="Record keeping">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php" title="Starting a business from home ">Introduction</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/is-starting-a-home-based-business-right-for-you.php">Is starting a home-based business right for you?</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/popular-home-business-ideas.php">Popular home business ideas</a></li>
+			<li>Start-up costs and financing your home-based business</li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/record-keeping.php">Record keeping</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/it-and-home-business.php">IT and home business</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/your-work-environment.php">Your work environment</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/managing-your-home-based-business.php">Managing your home-based business</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/your-growth-options.php">Your growth options</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/growing-your-business" target="_blank" rel="noopener noreferrer">Search our business support finder for grants, loans, expertise and advice for which your business may be eligible</a></li>
+			<li><a href="/it-and-ecommerce/introduction-to-it/setting-up-a-basic-it-system.php">Use our interactive tool to find out which computer equipment you should buy for your business</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/choose-the-right-finance-when-starting-up.php">Choose the right finance when starting up</a></li>
+			<li><a href="/finance-and-grants/prepare-to-raise-finance/how-to-attract-investment/use-your-business-plan-to-get-funding.php">Use your business plan to get funding</a></li>
+			<li><a href="/tax-payroll-and-company-information/business-rates/business-rates-an-overview.php">Business rates - an overview</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
