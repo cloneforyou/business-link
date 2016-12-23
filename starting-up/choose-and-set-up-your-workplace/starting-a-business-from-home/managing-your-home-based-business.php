05a4ddf555922e07f1abff9f190717fc653f7127
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-23 12:57:55 +0000

    Completed ‘Managing your home-based business’ page.

---
+++ starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/managing-your-home-based-business.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Managing your home-based business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business from home </h1>
+		<h2>Managing your home-based business</h2>
+		<p>Managing your business properly will improve your chances of success and prepare you to grow your business.</p>
+		<h3>Developing relationships</h3>
+		<p>The more you communicate with customers, suppliers and other entrepreneurs, the more business opportunities you will identify.</p>
+		<p>For example, developing relationships with suppliers and other businesses might enable you to bid for public procurement contracts - something that would be impossible on your own. See our guides on <a href="/grow-your-business/financing-growth/joint-ventures-and-partnering.php">joint ventures and partnering</a> and an <a href="/sales-and-marketing/selling/overview-on-selling-to-government.php">overview of selling to government</a>.</p>
+		<p>IT is the key to many aspects of business development - see our guide on how to <a href="/it-and-ecommerce/introduction-to-it/plan-your-it.php">plan your IT</a>.</p>
+		<p>Stay in touch with fellow entrepreneurs through networking, speaking at events, contacting local journalists and entering awards and competitions.</p>
+		<p>Consider using customer contact, feedback and loyalty schemes to retain customers and increase your sales. See our guide on how to <a href="/sales-and-marketing/keeping-your-customers/manage-your-customer-care.php">manage your customer care</a>.</p>
+		<h3>Cashflow</h3>
+		<p>Cashflow is the lifeblood of every business. You must do all you can to ensure that money owed is coming into the business. You can ensure prompt payment by issuing invoices on time and having a system in place to alert you to non-payment. See our guide on <a href="/finance-and-grants/manage-your-cashflow/credit-checking-your-customers-and-setting-credit-limits.php">getting paid on time</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/your-work-environment.php" title="Your work environment">Prev</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/your-growth-options.php" title="Your growth options">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php" title="Starting a business from home ">Introduction</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/is-starting-a-home-based-business-right-for-you.php">Is starting a home-based business right for you?</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/popular-home-business-ideas.php">Popular home business ideas</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/start-up-costs-and-financing-your-home-based-business.php">Start-up costs and financing your home-based business</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/record-keeping.php">Record keeping</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/it-and-home-business.php">IT and home business</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/your-work-environment.php">Your work environment</a></li>
+			<li>Managing your home-based business</li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/your-growth-options.php">Your growth options</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/grow-your-business/financing-growth/joint-ventures-and-partnering.php">Joint ventures and partnering</a></li>
+			<li><a href="/sales-and-marketing/keeping-your-customers/manage-your-customer-care.php">Manage your customer care</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/credit-checking-your-customers-and-setting-credit-limits.php">Credit checking your customers and setting credit limits</a></li>
+			<li><a href="/it-and-ecommerce/introduction-to-it/plan-your-it.php">Plan your IT</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
