28a4613b6392ad4e574d03d5f7f161e7b2b89447
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-23 12:45:04 +0000

    Completed ‘IT and home business’ page.

---
+++ starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/it-and-home-business.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>IT and home business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business from home </h1>
+		<h2>IT and home business</h2>
+		<p>The home business owner depends on technology - to find and keep customers and to keep in touch with suppliers, partners and contacts.</p>
+		<p>Email and the internet offer simple and effective methods of customer care and management, while IT can help you with almost every aspect of your business.</p>
+		<p>For more information, see our guide on the <a href="/it-and-ecommerce/introduction-to-it/benefits-of-email-and-the-internet.php">benefits of email and the internet</a> and <a href="/it-and-ecommerce/introduction-to-it/setting-up-a-basic-it-system.php">use our interactive tool to find out which computer equipment you should buy for your business</a>.</p>
+		<h3>The benefits of a website</h3>
+		<p>Websites have become a must-have marketing tool for most businesses. Websites are good for:</p>
+		<ul>
+			<li>attracting sales</li>
+			<li>branding your product or service</li>
+			<li>keeping in touch with customers through email updates</li>
+		</ul>
+		<p>You might want to start by using an 'off the shelf' template site. These can come with in-built payment systems for selling online and Search Engine Optimisation (SEO) for pushing your site higher up in online search result listings.</p>
+		<p>For more information, <a href="/it-and-ecommerce/create-and-manage-a-website/get-the-right-website-for-your-business.php">use our interactive tool to investigate what kind of website is best for your business</a> and read our guide on <a href="/sales-and-marketing/online-marketing/search-engine-optimisation.php">search engine optimisation</a>.</p>
+		<p>If you are thinking of selling products online, remember that you can also do this through existing auction websites.</p>
+		<h3>Networking</h3>
+		<p>You can use social media like Twitter and LinkedIn to keep in touch with other business owners. This will alert you to business development opportunities and help overcome any feelings of isolation you experience as a home business owner. For more information, read our guides on <a href="/sales-and-marketing/online-marketing/online-business-networking-and-social-networking.php">online networking</a> and <a href="/sales-and-marketing/online-marketing/web-2-0-a-guide-for-business.php">web 2.0: a guide for business</a>.</p>
+		<p>You can also use email and the internet to network, posting comments and articles in relevant forums on websites like horsesmouth and Enterprise Nation. You can also blog on relevant topics on your own website or write articles and syndicate them to other sites your customers are likely to visit.</p>
+		<h3>Working away from home</h3>
+		<p>IT doesn't just allow you to build a professional office at home - it enables you to work on the move. Recent technology such as WiFi, USB dongles, smart phones and Skype means that you can work wherever you want. For more information, see our guide on <a href="/it-and-ecommerce/communications/mobile-technology.php">mobile technology</a>.</p>
+		<p><strong>Cloud computing</strong> provides businesses with a way of managing data and software requirements over the internet - 'in the cloud'. This makes your business information accessible from any computer or mobile device with an internet connection and web browser. Cloud computing can provide a cost effective and flexible solution to your IT requirements. For more information see our guide on <a href="/it-and-ecommerce/software-and-business-applications/cloud-computing.php">cloud computing</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/record-keeping.php" title="Record keeping">Prev</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/your-work-environment.php" title="Your work environment">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php" title="Starting a business from home ">Introduction</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/is-starting-a-home-based-business-right-for-you.php">Is starting a home-based business right for you?</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/popular-home-business-ideas.php">Popular home business ideas</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/start-up-costs-and-financing-your-home-based-business.php">Start-up costs and financing your home-based business</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/record-keeping.php">Record keeping</a></li>
+			<li>IT and home business</li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/your-work-environment.php">Your work environment</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/managing-your-home-based-business.php">Managing your home-based business</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/your-growth-options.php">Your growth options</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.enterprisenation.com/" target="_blank" rel="noopener noreferrer">Home working advice on the Enterprise Nation website</a></li>
+			<li><a href="http://www.horsesmouth.co.uk/publisha.content/work/topics.publisha?topic=Starting%20a%20business" target="_blank" rel="noopener noreferrer">Online business mentoring on the horsesmouth website</a></li>
+			<li><a href="/it-and-ecommerce/introduction-to-it/setting-up-a-basic-it-system.php">Use our interactive tool to find out which computer equipment you should buy for your business</a></li>
+			<li><a href="/it-and-ecommerce/create-and-manage-a-website/get-the-right-website-for-your-business.php">Use our interactive tool to investigate what kind of website is best for your business</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/starting-up/considering-starting-up/starting-a-part-time-business.php">Starting a part-time business</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
