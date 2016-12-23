a83c87e0fcf075d87dfb77c46438bf0565bfe258
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-23 12:24:53 +0000

    Completed ‘Popular home business ideas’ page.

---
+++ starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/popular-home-business-ideas.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Popular home business ideas | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Starting a business from home</h1>
+		<h2>Popular home business ideas</h2>
+		<p>If you do not yet have a home business idea, ask yourself three questions:</p>
+		<p><strong>Is there a gap in the market?</strong> Have you tried to buy something that you just can't find? In which case, others may be looking for that product too.</p>
+		<p><strong>What is my passion/skill/hobby?</strong> Can you find a way of making a living from it?</p>
+		<p><strong>Can I do something better?</strong> Have you seen someone offer a product or service that you think you can offer better yourself?</p>
+		<h3>Popular home business ideas</h3>
+		<p>Consider opportunities in your local area for services such as child care, tutoring, translation, editorial services, dog walking and alteration services.</p>
+		<p>Other common home-based businesses include:</p>
+		<ul>
+			<li>consultants</li>
+			<li>website designers</li>
+			<li>arts and craft makers</li>
+			<li>publishers</li>
+			<li>caterers</li>
+			<li>virtual assistants who provide professional assistance to clients from a home office</li>
+		</ul>
+		<p>You could also consider investing in a franchise. The benefits of being a franchisee are that you are your own boss but also have the benefit of working with a central team. There are a growing number of home-based franchise opportunities that you can explore.</p>
+		<p>If you need more help developing your home business idea, see our guide <a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">research and develop your business ideas</a>.</p>
+		<h3>Working 'five to nine'</h3>
+		<p>You may want to consider starting a business whilst in full or part-time employment, using the evenings and weekends to build your business. This means that you will have money coming in whilst your business finds its feet. However, you might find it difficult to manage the extra hours and the extra work.</p>
+		<h3>Your business plan</h3>
+		<p>Once you have worked out a business idea, you need to create a written business plan. For more information, see our videos and e-learning resources on how to <a href="https://www.gov.uk/write-business-plan" target="_blank" rel="noopener noreferrer">create your business plan</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/is-starting-a-home-based-business-right-for-you.php" title="Is starting a home-based business right for you?">Prev</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/start-up-costs-and-financing-your-home-based-business.php" title="Start-up costs and financing your home-based business">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php" title="Starting a business from home ">Introduction</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/is-starting-a-home-based-business-right-for-you.php">Is starting a home-based business right for you?</a></li>
+			<li>Popular home business ideas</li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/start-up-costs-and-financing-your-home-based-business.php">Start-up costs and financing your home-based business</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/record-keeping.php">Record keeping</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/it-and-home-business.php">IT and home business</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/your-work-environment.php">Your work environment</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/managing-your-home-based-business.php">Managing your home-based business</a></li>
+			<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home/your-growth-options.php">Your growth options</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.bl.uk/business-and-ip-centre" target="_blank" rel="noopener noreferrer">Business development resources on the British Library Business &amp; IP Centre website</a></li>
+			<li><a href="http://www.businessballs.com/freebusinessplansandmarketingtemplates.htm" target="_blank" rel="noopener noreferrer">Business planning resources on the Businessballs website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php">Prepare a business plan</a></li>
+			<li><a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">Research and develop your business ideas, new products and services</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/buy-a-franchise.php">Buy a franchise</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
