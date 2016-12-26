811f017e58aa0346abc18af76adf4e230d680b9c
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-26 22:09:19 +0000

    Made all links in top-bar(s) uppercase. Added title tags and tab index.

--- _inc/header.php
+++ _inc/header.php
@@ -9,21 +9,21 @@
 		
 		<ul class="vertical menu">
 			<li><a href="">Home</a></li>
-			<li><a href="/starting-up.php">Starting up</a></li>
-			<li><a href="/finance-and-grants.php">Finance and grants</a></li>
-			<li><a href="/tax-payroll-and-company-information.php">Tax, payroll and company information</a></li>
-			<li><a href="/employment-and-skills.php">Employment &amp; skills</a></li>
-			<li><a href="/workplace-health-and-safety.php">Workplace health &amp; safety</a></li>
-			<li><a href="/environment-and-efficiency.php">Environment &amp; efficiency</a></li>
-			<li><a href="/premises-and-property.php">Premises and property</a></li>
-			<li><a href="/create-innovate-and-protect.php">Create, innovate and protect</a></li>
-			<li><a href="/it-and-ecommerce.php">IT &amp; e-commerce</a></li>
-			<li><a href="/sales-and-marketing.php">Sales and marketing</a></li>
-			<li><a href="/international-trade.php">International trade</a></li>
-			<li><a href="/grow-your-business.php">Grow your business</a></li>
-			<li><a href="/buy-or-sell-a-business.php">Buy or sell a business</a></li>
-			<li><a href="/for-professional-advisers.php">For professional advisers</a></li>
-			<li><a href="/your-business-sector.php">Your business sector</a></li>
+			<li><a href="/starting-up.php">Starting Up</a></li>
+			<li><a href="/finance-and-grants.php">Finance &amp; Grants</a></li>
+			<li><a href="/tax-payroll-and-company-information.php">Tax, Payroll &amp; Company Information</a></li>
+			<li><a href="/employment-and-skills.php">Employment &amp; Skills</a></li>
+			<li><a href="/workplace-health-and-safety.php">Workplace Health &amp; Safety</a></li>
+			<li><a href="/environment-and-efficiency.php">Environment &amp; Efficiency</a></li>
+			<li><a href="/premises-and-property.php">Premises &amp; Property</a></li>
+			<li><a href="/create-innovate-and-protect.php">Create, Innovate &amp; Protect</a></li>
+			<li><a href="/it-and-ecommerce.php">IT &amp; e-Commerce</a></li>
+			<li><a href="/sales-and-marketing.php">Sales &amp; Marketing</a></li>
+			<li><a href="/international-trade.php">International Trade</a></li>
+			<li><a href="/grow-your-business.php">Grow Your Business</a></li>
+			<li><a href="/buy-or-sell-a-business.php">Buy or Sell a Business</a></li>
+			<li><a href="/for-professional-advisers.php">For Professional Advisers</a></li>
+			<li><a href="/your-business-sector.php">Your Business Sector</a></li>
 		</ul>
 
 	</div><!-- //off-canvas position-left -->
@@ -58,21 +58,69 @@
 				<div class="top-bar-right show-for-medium">
 					
 					<ul class="menu align-right">
-						<li><a href="/environment-and-efficiency.php">Environmental</a></li>
-						<li><a href="/workplace-health-and-safety.php">H &amp; S</a></li>
-						<li><a href="/employment-and-skills.php">Employment</a></li>
-						<li><a href="/tax-payroll-and-company-information.php">Taxes</a></li>
-						<li><a href="/finance-and-grants.php">Financial</a></li>
-						<li><a href="/starting-up.php">Starting Up</a></li>
+						<li>
+							<a href="/environment-and-efficiency.php" tabindex="6" title="Environment &amp; Efficiency">
+								<span data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" title="Environment &amp; Efficiency">Environmental</span>
+							</a>
+						</li>
+						<li>
+							<a href="/workplace-health-and-safety.php" tabindex="5" title="Health &amp; Safety">
+								<span data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" title="Health &amp; Safety">H &amp; S</span>
+							</a>
+						</li>
+						<li>
+							<a href="/employment-and-skills.php" tabindex="4" title="Employment &amp; Skills">
+								<span data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" title="Employment &amp; Skills">Employment</span>
+							</a>
+						</li>
+						<li>
+							<a href="/tax-payroll-and-company-information.php" tabindex="3" title="Tax, Payroll &amp; Company Information">
+								<span data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" title="Tax, Payroll &amp; Company Information">Taxes</span>
+							</a>
+						</li>
+						<li>
+							<a href="/finance-and-grants.php" tabindex="2" title="Finance and Grants">
+								<span data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" title="Finance and Grants">Financial</span>
+							</a>
+						</li>
+						<li><a href="/starting-up.php" tabindex="1">Starting Up</a></li>
 					</ul>
 					<ul class="menu align-right">
-						<li><a href="/buy-or-sell-a-business.php">Buy or Sell</a></li>
-						<li><a href="/grow-your-business.php">Grow</a></li>
-						<li><a href="/international-trade.php">International</a></li>
-						<li><a href="/sales-and-marketing.php">Sales</a></li>
-						<li><a href="/it-and-ecommerce.php">IT</a></li>
-						<li><a href="/create-innovate-and-protect.php">Creative</a></li>
-						<li><a href="/premises-and-property.php">Premises</a></li>
+						<li>
+							<a href="/buy-or-sell-a-business.php" tabindex="13" title="Buy or Sell a Business">
+								<span data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" title="Buy or Sell a Business">Buy or Sell</span>
+							</a>
+						</li>
+						<li>
+							<a href="/grow-your-business.php" tabindex="12" title="Grow Your Business">
+								<span data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" title="Grow Your Business">Grow</span>
+							</a>
+						</li>
+						<li>
+							<a href="/international-trade.php" tabindex="11" title="International Trade">
+								<span data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" title="International Trade">International</span>
+							</a>
+						</li>
+						<li>
+							<a href="/sales-and-marketing.php" tabindex="10" title="Sales &amp; Marketing">
+								<span data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" title="Sales &amp; Marketing">Sales</span>
+							</a>
+						</li>
+						<li>
+							<a href="/it-and-ecommerce.php" tabindex="9" title="IT &amp; e-Commerce">
+								<span data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" title="IT &amp; e-Commerce">IT</span>
+							</a>
+						</li>
+						<li>
+							<a href="/create-innovate-and-protect.php" tabindex="8" title="Creative, Innovate &amp; Protect">
+								<span data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" title="Creative, Innovate &amp; Protect">Creative</span>
+							</a>
+						</li>
+						<li>
+							<a href="/premises-and-property.php" tabindex="7" title="Premises &amp; Property">
+								<span data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" title="Premises &amp; Property">Premises</span>
+							</a>
+						</li>
 					</ul>
 
 				</div><!-- //top-bar-right show-for-medium -->
