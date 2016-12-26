95317787533db2557a877e29008714a18ae4156c
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-26 21:47:52 +0000

    Added trailing slash to internal links. Removed broken linked content, or irrelevant.

--- index.php
+++ index.php
@@ -11,18 +11,24 @@
 
 <?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
 
-<div class="row">
+
 	<div class="small-12 large-8 large-push-4 columns">
 
 		<h1>Welcome to Venture Labs</h1>
-		<p>Venture Labs is government's online resource for businesses. It is now enhanced with new, easy-to-use tools to help you start up, improve and grow your business.</p>
-		<p>For more information visit <a href="about-business-link.php">about us</a>.</p>
+		
+		<p>Venture Labs is an online resource for businesses. It is now enhanced with new, easy-to-use tools to help you start up, improve and grow your business.</p>
+		
+		<p>For more information visit <a href="/about-business-link.php">about us</a>.</p>
+		
 		<p>You can also find information and support specific to businesses in <a href="http://www.business.scotland.gov.uk/" title="Government-funded support services for businesses in Scotland" target="_blank" rel="noopener noreferrer">Scotland</a>, <a href="http://business.wales.gov.uk/" title="Government-funded support services for businesses in Wales" target="_blank" rel="noopener noreferrer">Wales</a> and <a href="http://www.nibusinessinfo.co.uk/" title="Government-funded support services for businesses in Northern Ireland" target="_blank" rel="noopener noreferrer">Northern Ireland</a>.</p>
+		
+		<?php /* ?>
 		<a class="caption" href="http://twitter.com/BusinessLinkGov" title="Keep your business on track with our latest news and updates" target="_blank" rel="noopener noreferrer">
 			<img src="/_img/shp_twitter.jpg" width="166" height="56" alt="Keep your business on track with our latest news and updates" />
 			Follow us on Twitter
 		</a>
-		
+		<?php */ ?>
+
 		<hr />
 		
 		<p>Keep your business on track with our latest news and updates</p>
@@ -35,7 +41,7 @@
 		
 		<h3>Find out what records you should be keeping</h3>
 		<p>
-			<a href="find-out-what-records-you-should-be-keeping.php" title="Use our interactive tool to find out the records you must keep as a business and assess how well you are keeping them">
+			<a href="/find-out-what-records-you-should-be-keeping.php" title="Use our interactive tool to find out the records you must keep as a business and assess how well you are keeping them">
 				<img src="/_img/web_chk_home.jpg" width="166" height="56" alt="Use our interactive tool to find out the records you must keep as a business and assess how well you are keeping them" />
 				<br />Use our interactive tool to find out the records you must keep as a business
 			</a>
@@ -43,14 +49,14 @@
 		
 		<h3>Intrastat returns: important changes</h3>
 		<p>
-			<a href="intrastat-returns-important-changes.php" title="Submitting Intrastat declarations from 1 April 2012: a reminder">
+			<a href="/intrastat-returns-important-changes.php" title="Submitting Intrastat declarations from 1 April 2012: a reminder">
 				<img src="/_img/HP_hands_on_keyboard.jpg" width="166" height="56" alt="Submitting Intrastat declarations from 1 April 2012: a reminder" />
 			<br />Businesses providing Intrastat returns to HMRC need to act on this information</a>
 		</p>
 
 		<h3>Updated Employer's Charter</h3>
 		<p>
-			<a href="employers-charter.php" title="Find out what you can and can't do when managing your staff">
+			<a href="/employers-charter.php" title="Find out what you can and can't do when managing your staff">
 				<img src="/_img/support.jpg" width="166" height="56" alt="Find out what you can and can't do when managing your staff" />
 			<br />Find out what you can and can't do when managing your staff
 			</a>
@@ -60,13 +66,15 @@
 		<p>
 			<a href="http://improve.businesslink.gov.uk/what-service-can-do-you" title="GIS service" target="_blank" rel="noopener noreferrer">
 				<img src="/_img/helplineblsmallhomepage.jpg" width="166" height="56" alt="GIS service" />
+			<?php /* ?>
 			<br />Unable to find the information you are looking for?</a>
 			<br />
 			Contact the <strong>Venture Labs Helpline</strong> on: <a href="tel:08456009006">0845 600 9006</a>, open 9am - 6pm Monday to Friday.
+		<?php */ ?>
 		</p>
 
 		<h3>The business in you</h3>
-		<a href="find-the-business-in-you.php" title="The 'Business in You' is a major new campaign to help people to start a new business or grow their existing one">
+		<a href="/find-the-business-in-you.php" title="The 'Business in You' is a major new campaign to help people to start a new business or grow their existing one">
 			<img src="/_img/Business_in_you_smaller_homepage_promo.jpg" width="166" height="56" alt="The "business in you" is a major new campaign to help people to start a new business or grow their existing one" />
 		</a>
 		<p>Starting and growing a business - tips and tools from real-life success stories</p>
@@ -76,7 +84,7 @@
 		<div class="row small-up-1 medium-up-2">
 			<div class="column">
 				<p>
-					<a href="gov-uk-will-launch-in-october.php" title="GOV.UK will launch in October">
+					<a href="/gov-uk-will-launch-in-october.php" title="GOV.UK will launch in October">
 						Businesslink.gov.uk is changing
 						<img src="/_img/gov-uk-will-launch-in-october.jpg" alt="" />
 					</a>
@@ -92,6 +100,7 @@
 					<br />Access information to help your business work better
 				</p>
 			</div>
+			<?php /* ?>
 			<div class="column">
 				<p>
 					<a href="#">
@@ -101,6 +110,8 @@
 					<br />Take note of our advice regarding fake emails
 				</p>
 			</div>
+			<?php */ ?>
+			<?php /* ?>
 			<div class="column">
 				<p>
 					<a href="#" title="A report and short guide by Lord Young, highlighting the evolving nature of enterprise and business and showcasing the opportunities and support available to you.">
@@ -110,16 +121,20 @@
 					<br />Helping you to start and develop your business
 				</p>
 			</div>
+			<?php */ ?>
 			<div class="column">
 				<p>
 					<a href="https://www.gov.uk/growing-your-business" title="GIS service">
 						Have a business idea, looking to start-up or grow your business?
 						<img src="/_img/icon-helpline.jpg" alt="" />
 					</a>
+					<?php /* ?>
 					<br />Unable to find the information you are looking for?<br />
 					Contact the <strong>Venture Labs Helpline</strong> on: <a href="tel:08456009006">0845 600 9006</a>, open 9am - 6pm Monday to Friday.
+					<?php */ ?>
 				</p>
 			</div>
+			<?php /* ?>
 			<div class="column">
 				<p>
 					<a href="#">
@@ -129,6 +144,7 @@
 					<br />Find public sector procurement tender and contract documents
 				</p>
 			</div>
+			<?php */ ?>
 			<div class="column">
 				<p>
 					<a href="https://www.gov.uk/browse/business/setting-up" title="My New Business" target="_blank" rel="noopener noreferrer">
@@ -142,7 +158,7 @@
 			</div>
 			<div class="column">
 				<p>
-					<a href="our-use-of-cookies.php" title="How cookies are used on this site">
+					<a href="/our-use-of-cookies.php" title="How cookies are used on this site">
 						Our use of cookies
 						<img src="/_img/handonkeyboard_large.jpg" alt="Cookies" />
 					</a>
@@ -151,7 +167,7 @@
 			</div>
 			<div class="column">
 				<p>
-					<a href="apprenticeship-grants-now-available.php" title="Get �1,500 for recruiting an apprentice aged 16 to 24">
+					<a href="/apprenticeship-grants-now-available.php" title="Get �1,500 for recruiting an apprentice aged 16 to 24">
 						Apprenticeship grants now available
 						<img src="/_img/apprenticeships_302x239.JPG" alt="Apprenticeships" />
 					</a>
@@ -160,7 +176,7 @@
 			</div>
 			<div class="column">
 				<p>
-					<a href="regulation-licences-and-compliance.php" title="Find and apply for licences and permits for your business">
+					<a href="/regulation-licences-and-compliance.php" title="Find and apply for licences and permits for your business">
 						Apply for licences and permits online
 						<img src="/_img/online_licences_2.jpg" alt="" />
 					</a>
@@ -169,7 +185,7 @@
 			</div>
 			<div class="column">
 				<p>
-					<a href="incorporate-your-company-online.php" title="Incorporate your company online as a private company, limited by shares, with model articles of association">
+					<a href="/incorporate-your-company-online.php" title="Incorporate your company online as a private company, limited by shares, with model articles of association">
 						Incorporate your limited company online with Companies House
 						<img src="/_img/hp_promo_302x239_comp2.jpg" alt="" />
 					</a>
@@ -178,7 +194,7 @@
 			</div>
 			<div class="column">
 				<p>
-					<a href="taking-on-a-new-member-of-staff.php" title="We have launched three new tools to help you through the process of taking on a new member of staff">
+					<a href="/taking-on-a-new-member-of-staff.php" title="We have launched three new tools to help you through the process of taking on a new member of staff">
 						Taking on a new member of staff?
 						<img src="/_img/hp_promo_302x239_comp1.jpg" alt="" />
 					</a>
@@ -218,33 +234,33 @@
 			</div>
 			<div class="column"></div>
 		</div>
-		<p><a href="whats-new.php">View all what's new items</a></p>
+		<p><a href="/whats-new.php">View all what's new items</a></p>
 		
 
-		<h2><a href="do-it-online.php" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies">Do it online</a></h2>
+		<h2><a href="/do-it-online.php" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies">Do it online</a></h2>
 		<ul>
 			<li>
-				<a href="search-company-information-using-companies-house-webcheck.php" title="Search company details and view filing histories for all limited companies in the UK">Find company details with WebCHeck</a>
+				<a href="/search-company-information-using-companies-house-webcheck.php" title="Search company details and view filing histories for all limited companies in the UK">Find company details with WebCHeck</a>
 			</li>
 			<li>
-				<a href="file-company-information-online-using-companies-house-webfiling.php" title="File most company information online (some services require payment)">File company details with WebFiling</a>
+				<a href="/file-company-information-online-using-companies-house-webfiling.php" title="File most company information online (some services require payment)">File company details with WebFiling</a>
 			</li>
 			<li>
 				<a href="/do-it-online/using-vat-online.php" title="Access to HMRC VAT online and links to information on service availability and support">Submit your VAT Return online</a>
 			</li>
 			<li>
-				<a href="incorporate-your-company-online.php" title="Incorporate your company online as a private company, limited by shares, with model articles of association">Incorporate your limited company online with Companies House</a>
+				<a href="/incorporate-your-company-online.php" title="Incorporate your company online as a private company, limited by shares, with model articles of association">Incorporate your limited company online with Companies House</a>
 			</li>
 			<li>
 				<a href="/do-it-online/use-paye-online-for-employers.php" title="Access HMRC's PAYE Online for employers services">Log in or register for PAYE Online</a>
 			</li>
 			<li>
-				<a href="regulation-licences-and-compliance.php" title="Find and apply for licences and permits for your business">Apply for licences</a>
+				<a href="/regulation-licences-and-compliance.php" title="Find and apply for licences and permits for your business">Apply for licences</a>
 			</li>
 		</ul>
-		<p><a href="do-it-online.php" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies">View all services</a></p>
+		<p><a href="/do-it-online.php" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies">View all services</a></p>
 
-		<!--
+		<?php /* ?>
 		<h2><a href="#">My Business</a></h2>
 		<p>
 			<a href="#" title="The more you tell us about your business, the more time we can save you. We recommend that you keep your profile up to date.">
@@ -256,12 +272,12 @@
 		</p>
 		<p><a href="#">Go to My Business</a></p>
 		<p><a href="#">Log in</a></p>
-		-->
+		<?php */ ?>
 
 	</div><!-- // .small-12 large-8 large-push-4 columns -->
 
 	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
 
-</div><!-- // .row -->
+
 
 <?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
