92491081d07c0f0aa7eecdc01989f925e0179848
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-26 20:28:45 +0000

    Added wrappers for off-canvas, demo links as moving top-level menu items into new layout.

--- _inc/header.php
+++ _inc/header.php
@@ -1,43 +1,57 @@
-<div class="row header-row">
-	<div class="small-12 columns">
+<div class="off-canvas-wrapper">
 
-		<header>
-			<?php /* 
-				skip links - also consider keyboard access, see also
-				https://www.w3.org/TR/UNDERSTANDING-WCAG20/navigation-mechanisms-skip.html
-				https://www.w3.org/TR/WCAG20-TECHS/G1.html
-			 */ ?>
-			<nav class="skip-links">
-				<ul class="menu">
-					<li><a class="show-on-focus" href="#skipcont">Go to main content</a></li>
-					<li><a class="show-on-focus" href="#skipleft">Go to main navigation</a></li>
-					<li><a class="show-on-focus" href="#skiphelp">Go to site map, help, search</a></li>
-					<?php /* ?>
-					<li><a class="show-on-focus" href="#skiplogin">Go to register, log in</a></li>
-					<?php */ ?>
-					<li><a class="show-on-focus" href="#skipfooter">Go to footer navigation</a></li>
-				</ul>
-			</nav><!-- end .skip-links -->
-			
-			<div class="branding clearfix">
-				<a class="top-logo" href="/index.php" title="VentureLabs for Small Businesses">
-					<img src="/_img/venturelabs_logo_680.png" alt="VentureLabs logo" />
-				</a>
-			</div><!-- end .branding -->
-			
-			<?php /* ?>
-			<a name="skiplogin"></a>
-			
-			<div class="login clearfix">
-				<ul class="menu align-right">
-					<li><a href="#">Register</a></li>
-					<li><a href="#">Log in</a></li>
-					<!-- <li>Your Account</li> -->
-				</ul>
-			</div><!-- end .login -->
-			<?php */ ?>
-			
+	<div class="off-canvas position-left" id="off-canvas" data-off-canvas>
+		
+		<ul class="vertical menu">
+			<li><a href="">Foundation</a></li>
+			<li><a href="">Dot</a></li>
+			<li><a href="">ZURB</a></li>
+			<li><a href="">Com</a></li>
+			<li><a href="">Slash</a></li>
+			<li><a href="">Sites</a></li>
+		</ul>
+
+	</div><!-- //off-canvas position-left -->
+
+	<div class="off-canvas-content" data-off-canvas-content>
+
+		<header class="header" role="banner">
+
+			<!-- By default, this menu will use off-canvas for small
+				 and a topbar for medium-up -->
+
+			<div class="top-bar" id="top-bar-menu">
+				
+				<div class="top-bar-left float-left">
+					
+					<a class="top-logo" href="/index.php" title="VentureLabs for Small Businesses">
+						<img src="/_img/venturelabs_logo_680.png" alt="VentureLabs logo" />
+					</a>
+
+				</div><!-- //top-bar-left float-left -->
+
+				<div class="top-bar-right show-for-medium">
+					
+					<ul class="menu">
+						<li><a href="">Foundation</a></li>
+						<li><a href="">Dot</a></li>
+						<li><a href="">ZURB</a></li>
+						<li><a href="">Com</a></li>
+						<li><a href="">Slash</a></li>
+						<li><a href="">Sites</a></li>
+					</ul>
+
+				</div><!-- //top-bar-right show-for-medium -->
+
+				<div class="top-bar-right float-right show-for-small-only">
+					
+					<ul class="menu">
+						<li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
+						<li><a data-toggle="off-canvas">Menu</a></li>
+					</ul>
+
+				</div>
+			</div><!-- //top-bar-right float-right show-for-small-only -->
+
+
 		</header>
-
-	</div><!-- // .small-12 .columns -->
-</div><!-- // .row -->
