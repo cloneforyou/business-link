0cdc0cc181353e61d797051bc1bd00cacb8a4318
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-26 21:55:21 +0000

    Re-worked the footer layout to simplify.

--- _inc/footer.php
+++ _inc/footer.php
@@ -4,31 +4,23 @@
 				<a name="skipfooter" id="skipfooter"></a>
 
 				<footer>
-					<nav class="sub-links">
-						<p>
-							<a href="https://www.gov.uk/help" target="_blank">Help &amp; Support</a> | 
-							<a href="http://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/copyright-and-re-use/crown-copyright/" target="_blank">&copy; Crown Copyright</a> | 
-							<a href="https://www.gov.uk/help/terms-conditions" target="_blank">Terms &amp; Conditions</a> | 
-							<a href="https://www.gov.uk/help/privacy-policy" target="_blank">Privacy Policy</a> | 
-							<a href="https://www.gov.uk/help/accessibility" target="_blank">Accessibility</a> | 
-							<a href="https://www.gov.uk/help/cookies" target="_blank">Cookies</a> |
-							<a href="/about-business-link.php">About us</a>
-						</p>
-					</nav><!-- end .sub-links -->
+					<ul class="menu">
+						<li><a href="https://www.gov.uk/help" target="_blank">Help &amp; Support</a></li>
+						<li><a href="http://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/copyright-and-re-use/crown-copyright/" target="_blank">&copy; Crown Copyright</a></li>
+						<li><a href="https://www.gov.uk/help/terms-conditions" target="_blank">Terms &amp; Conditions</a></li>
+						<li><a href="https://www.gov.uk/help/privacy-policy" target="_blank">Privacy Policy</a></li>
+						<li><a href="https://www.gov.uk/help/accessibility" target="_blank">Accessibility</a></li>
+						<li><a href="https://www.gov.uk/help/cookies" target="_blank">Cookies</a></li>
+						<li><a href="/about-business-link.php">About us</a></li>
+					</ul>
 
-					<a name="skiphelp"id="skiphelp"></a>
+					<ul class="menu">
+						<li><a href="/whats-new.php">What's new?</a></li>
+						<li><a href="/do-it-online.php" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies">Do it online</a></li>
+						<li><a href="/directories.php">Directories</a></li>
+					</ul>
 
-					<div id="bottomgroup">
-						<div id="tabs">
-							<ul class="menu">
-								<li><a href="/whats-new.php">What's new?</a></li>
-								<li><a href="/do-it-online.php" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies">Do it online</a></li>
-								<li><a href="/directories.php">Directories</a></li>
-							</ul>
-							<p><small><a href="http://www.blueocto.co.uk" title="Web Design and Development Newcastle" target="_blank" rel="noopener noreferrer">Reincarnated and maintained by Blueocto Ltd</a> &nbsp;|&nbsp; Please bear with me, this website is in <span class="alert label">ALPHA</span></small></p>
-						</div>
-					</div>
-
+					<p><small><a href="http://www.blueocto.co.uk" title="Web Design and Development Newcastle" target="_blank" rel="noopener noreferrer">Reincarnated and maintained by Blueocto Ltd</a> &nbsp;|&nbsp; Please bear with me, this website is in <span class="alert label">ALPHA</span></small></p>
 				</footer>
 
 			</div><!-- // .small-12 .columns -->
