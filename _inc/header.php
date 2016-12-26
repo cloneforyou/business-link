58dc19ae9aa9ab30fc488bcaa2b116586ed1427b
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-26 21:26:51 +0000

    Added close button to off-canvas menu. Added links to both nav so they pick up the colours.

--- _inc/header.php
+++ _inc/header.php
@@ -1,14 +1,29 @@
 <div class="off-canvas-wrapper">
 
 	<div class="off-canvas position-left" id="off-canvas" data-off-canvas>
+
+		<!-- Close button -->
+		<button class="close-button" aria-label="Close menu" type="button" data-close>
+			<span aria-hidden="true">&times;</span>
+		</button>
 		
 		<ul class="vertical menu">
-			<li><a href="">Foundation</a></li>
-			<li><a href="">Dot</a></li>
-			<li><a href="">ZURB</a></li>
-			<li><a href="">Com</a></li>
-			<li><a href="">Slash</a></li>
-			<li><a href="">Sites</a></li>
+			<li><a href="">Home</a></li>
+			<li><a href="/starting-up.php">Starting up</a></li>
+			<li><a href="/finance-and-grants.php">Finance and grants</a></li>
+			<li><a href="/tax-payroll-and-company-information.php">Tax, payroll and company information</a></li>
+			<li><a href="/employment-and-skills.php">Employment &amp; skills</a></li>
+			<li><a href="/workplace-health-and-safety.php">Workplace health &amp; safety</a></li>
+			<li><a href="/environment-and-efficiency.php">Environment &amp; efficiency</a></li>
+			<li><a href="/premises-and-property.php">Premises and property</a></li>
+			<li><a href="/create-innovate-and-protect.php">Create, innovate and protect</a></li>
+			<li><a href="/it-and-ecommerce.php">IT &amp; e-commerce</a></li>
+			<li><a href="/sales-and-marketing.php">Sales and marketing</a></li>
+			<li><a href="/international-trade.php">International trade</a></li>
+			<li><a href="/grow-your-business.php">Grow your business</a></li>
+			<li><a href="/buy-or-sell-a-business.php">Buy or sell a business</a></li>
+			<li><a href="/for-professional-advisers.php">For professional advisers</a></li>
+			<li><a href="/your-business-sector.php">Your business sector</a></li>
 		</ul>
 
 	</div><!-- //off-canvas position-left -->
@@ -32,20 +47,39 @@
 
 				<div class="top-bar-right show-for-medium">
 					
-					<ul class="menu">
-						<li><a href="">Foundation</a></li>
-						<li><a href="">Dot</a></li>
-						<li><a href="">ZURB</a></li>
-						<li><a href="">Com</a></li>
-						<li><a href="">Slash</a></li>
-						<li><a href="">Sites</a></li>
+					<ul class="menu quick-links">
+						<li><a href="/index.php">Home</a></li>
+						<li><a href="/for-professional-advisers.php">For Professional Advisers</a></li>
+						<li><a href="/your-business-sector.php">Your Business Sector</a></li>
 					</ul>
 
 				</div><!-- //top-bar-right show-for-medium -->
 
-				<div class="top-bar-right float-right show-for-small-only">
+				<div class="top-bar-right show-for-medium">
 					
-					<ul class="menu">
+					<ul class="menu align-right">
+						<li><a href="/environment-and-efficiency.php">Environmental</a></li>
+						<li><a href="/workplace-health-and-safety.php">H &amp; S</a></li>
+						<li><a href="/employment-and-skills.php">Employment</a></li>
+						<li><a href="/tax-payroll-and-company-information.php">Taxes</a></li>
+						<li><a href="/finance-and-grants.php">Financial</a></li>
+						<li><a href="/starting-up.php">Starting Up</a></li>
+					</ul>
+					<ul class="menu align-right">
+						<li><a href="/buy-or-sell-a-business.php">Buy or Sell</a></li>
+						<li><a href="/grow-your-business.php">Grow</a></li>
+						<li><a href="/international-trade.php">International</a></li>
+						<li><a href="/sales-and-marketing.php">Sales</a></li>
+						<li><a href="/it-and-ecommerce.php">IT</a></li>
+						<li><a href="/create-innovate-and-protect.php">Creative</a></li>
+						<li><a href="/premises-and-property.php">Premises</a></li>
+					</ul>
+
+				</div><!-- //top-bar-right show-for-medium -->
+
+				<div class="top-bar-left float-left show-for-small-only">
+					
+					<ul class="menu mobile-menu">
 						<li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
 						<li><a data-toggle="off-canvas">Menu</a></li>
 					</ul>
