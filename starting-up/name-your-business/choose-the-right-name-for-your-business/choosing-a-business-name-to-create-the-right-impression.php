195f4c8570d89d0e00083c00b3c17736a2097d37
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-31 21:50:44 +0000

    Completed ‘Choosing a business name to create the right impression’ page.

---
+++ starting-up/name-your-business/choose-the-right-name-for-your-business/choosing-a-business-name-to-create-the-right-impression.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Choosing a business name to create the right impression | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Choose the right name for your business</h1>
+		<h2>Choosing a business name to create the right impression</h2>
+		<p>When generating ideas about a business name, you may initially focus on personal preference. However, an objective approach will enable you to consider the customer first.</p>
+		<p>Your business name will be the cornerstone of your brand. It should work well wherever you use it - on the phone, in your logo, signage, stationery, advertisements, website, email and any other media you plan to use to reach the market. See our guide on <a href="/sales-and-marketing/advertising-branding-and-design/branding-the-basics.php">branding: the basics</a>.</p>
+		<h3>Points to help you decide on a name for your business</h3>
+		<p>When choosing a name for your business, you need to ask:</p>
+		<ul>
+			<li>Do you want the name to reflect what your business does - moving, cleaning, building? Or would something more abstract be suitable?</li>
+			<li>Would it be a good idea to include your own name?</li>
+			<li>Do you want a traditional-sounding name, conveying durability and old-fashioned values, or a modern name, suggesting a fresh, innovative approach?</li>
+			<li>Think about the future - avoid words or phrases that are likely to date quickly.</li>
+			<li>If you're likely to be trading overseas, check that the name doesn't mean anything inappropriate in the relevant languages.</li>
+			<li>Think about callers and customers - avoid very long names, strange wordings and unusual spelling. If you're planning to advertise in directories such as the Yellow Pages, think about using a name that appears near the beginning of the listings for your type of business.</li>
+			<li>If you're focusing on the local market for your product or service, think about using the name of the city or town in the business name.</li>
+			<li>Keep your trading name creative, but your corporate name bland. This will give you the flexibility to develop other brands and trading names in the future.</li>
+		</ul>
+		<p>Note that there are rules that could affect your choice of business name. For more information on business names, see our guide on <a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php">rules for naming your limited company or limited liability partnership (LLP)</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php" title="Choose the right name for your business">Prev</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/names-for-limited-companies-and-llps.php" title="Names for limited companies and LLPs">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php" title="Choose the right name for your business">Introduction</a></li>
+			<li>Choosing a business name to create the right impression</li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/names-for-limited-companies-and-llps.php">Names for limited companies and LLPs</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/names-for-sole-traders-partnerships-and-limited-partnerships.php">Names for sole traders, partnerships and limited partnerships</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/use-of-sensitive-words-and-expressions-in-business-names.php">Use of sensitive words and expressions in business names</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/displaying-and-disclosing-your-business-company-or-limited-liability-partnership-name.php">Displaying and disclosing your business, company or limited liability partnership name</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/getting-your-business-name-on-the-internet.php">Getting your business name on the internet</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/here-is-how-i-chose-the-name-for-my-business.php">Here's how I chose the name for my business</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Companies House Contact Centre</h3>
+		<p>0303 1234 500</p>
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/publications/incorporation-and-names#ch2" target="_blank" rel="noopener noreferrer">Company name guidance from Companies House </a></li>
+			<li><a href="http://www.start.biz/business_names/" target="_blank" rel="noopener noreferrer">Choosing a business name advice on the National Business Register website</a></li>
+			<li><a href="http://www.nominet.uk/domains/about-domain-names/" target="_blank" rel="noopener noreferrer">Choosing your domain name guidance from Nominet</a></li>
+			<li><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Solicitor directory from the Law Society</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php">Rules for naming your limited company or limited liability partnership (LLP)</a></li>
+			<li><a href="/for-professional-advisers/for-accountants-and-tax-advisers/helping-your-client-set-up-or-dissolve-a-limited-company/get-started-with-company-incorporation-for-agents.php">Get started with company incorporation for agents</a></li>
+			<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+			<li><a href="/finance-and-grants/expert-financial-advice/choose-and-work-with-a-solicitor.php">Choose and work with a solicitor</a></li>
+			<li><a href="/sales-and-marketing/advertising-branding-and-design/branding-the-basics.php">Branding: the basics</a></li>
+			<li><a href="/create-innovate-and-protect/use-innovation-in-your-business/an-innovation-start-up.php">An innovation start-up</a></li>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
