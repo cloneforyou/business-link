285cd4f92713ea3ad59c44cc9d592c8713235d67
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-31 22:24:26 +0000

    Completed ‘Displaying and disclosing your business, company or limited liability partnership name’ page.

---
+++ starting-up/name-your-business/choose-the-right-name-for-your-business/displaying-and-disclosing-your-business-company-or-limited-liability-partnership-name.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Displaying and disclosing your business, company or limited liability partnership name | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Choose the right name for your business</h1>
+		<h2>Displaying and disclosing your business, company or limited liability partnership name</h2>
+		<p>Every business must display its business name - and other details - to inform customers and suppliers who they are dealing with. You should not print your stationery until you're certain your proposed name is acceptable.</p>
+		<p>Limited companies and limited liability partnerships (LLPs) must wait until registration is complete and a Certificate of Incorporation has been issued. This shows the company's registered name and number - see the page in this guide on <a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/names-for-limited-companies-and-llps.php">names for limited companies and LLPs</a>.</p>
+		<p>A sole trader or partnership must obtain prior approval to use a sensitive word in their proposed business name. See the page in this guide on <a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/names-for-sole-traders-partnerships-and-limited-partnerships.php">names for sole traders, partnerships and limited partnerships</a>.</p>
+		<h3>Displaying a limited company or LLP name</h3>
+		<p>You must display a sign with your company or LLP name:</p>
+		<ul>
+			<li>in characters that can be easily read</li>
+			<li>in a place where visitors can easily and clearly see it at any time and not just during business hours</li>
+			<li>continuously</li>
+		</ul>
+		<p>You must also include your company's or LLP's registered name on all hard copy and electronic business correspondence and documents including:</p>
+		<ul>
+			<li>letters, notices and other official publications</li>
+			<li>emails</li>
+			<li>bills of exchange, promissory notes, endorsements and order forms</li>
+			<li>cheques signed by or on behalf of the company</li>
+			<li>orders for money, goods or services signed by or on behalf of the company</li>
+			<li>bills of parcels, invoices and other demands for payment, receipts and letters of credit</li>
+			<li>your website - you do not need to include the company name on every page but it must be displayed so it can be easily read</li>
+		</ul>
+		<h3>Information you must display</h3>
+		<p>On all business letters, order forms and websites, you must display:</p>
+		<ul>
+			<li>the place of registration</li>
+			<li>the registered number</li>
+			<li>the address of registered office</li>
+			<li>the fact that it is a limited company or LLP</li>
+			<li>the amount of paid up share capital, if you have chosen to display your share capital</li>
+			<li>information if the company or LLP is being wound up</li>
+		</ul>
+		<p>You do not have to state directors' names on business letters unless you want to do so. However, if you do decide to include directors' names, then you must state all the directors' names.</p>
+		<p>If you are an LLP with more than 20 members, you don't need to display the members' names. However, you must keep a list of members at your principal place of business and state that the list is available for inspection.</p>
+		<h3>Displaying a sole trader or partnership business name</h3>
+		<p>If you are a sole trader or partnership, your business name, your own name, or the partners' names and business address must be clearly displayed in most cases:</p>
+		<ul>
+			<li>wherever you run your business and deal with customers or suppliers</li>
+			<li>on all business letters, orders, payments, invoices, receipts and other business documents</li>
+		</ul>
+		<h3>Displaying a name online</h3>
+		<p>If your business has a website, you must display:</p>
+		<ul>
+			<li>general information about your business - including business name, address, email address, VAT registration number (if applicable)</li>
+			<li>details of any relevant professional body that you belong to or any authorisation scheme to which your service is subject</li>
+		</ul>
+
+		<ul>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/use-of-sensitive-words-and-expressions-in-business-names.php" title="Use of sensitive words and expressions in business names">Prev</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/getting-your-business-name-on-the-internet.php" title="Getting your business name on the internet">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php" title="Choose the right name for your business">Introduction</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/choosing-a-business-name-to-create-the-right-impression.php">Choosing a business name to create the right impression</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/names-for-limited-companies-and-llps.php">Names for limited companies and LLPs</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/names-for-sole-traders-partnerships-and-limited-partnerships.php">Names for sole traders, partnerships and limited partnerships</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/use-of-sensitive-words-and-expressions-in-business-names.php">Use of sensitive words and expressions in business names</a></li>
+			<li>Displaying and disclosing your business, company or limited liability partnership name</li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/getting-your-business-name-on-the-internet.php">Getting your business name on the internet</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/here-is-how-i-chose-the-name-for-my-business.php">Here's how I chose the name for my business</a></li>
+		</ul>
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Companies House Contact Centre</h3>
+		<p>0303 1234 500</p>
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/get-information-about-a-company" target="_blank" rel="noopener noreferrer">Use our interactive tool to search for an available company name and trade mark</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+			<li><a href="/it-and-ecommerce/e-commerce/e-commerce-and-the-law.php">E-commerce and the law</a></li>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
