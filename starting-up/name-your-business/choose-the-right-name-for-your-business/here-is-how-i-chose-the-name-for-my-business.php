d46901d952522f1a935c210f077c491af1d8a724
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-31 22:43:41 +0000

    Completed ‘Here's how I chose the name for my business’ page.

---
+++ starting-up/name-your-business/choose-the-right-name-for-your-business/here-is-how-i-chose-the-name-for-my-business.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Here's how I chose the name for my business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Here's how I chose the name for my business</h1>
+		<h2>Here's how I chose the name for my business</h2>
+		<p>Managing director Nick Jenkins founded Moonpig, a London-based company selling personalised greeting cards over the internet.</p>
+		<p><strong>What I did</strong></p>
+		<h3>Consider how a name could work as a memorable brand</h3>
+		<p>"I wanted business to spread by word-of-mouth so it was important my company's name was both catchy and memorable. I decided it had to be only two syllables so that people wouldn't forget it, and phonetic so that they wouldn't misspell it. Moonpig is fun and cheeky. Most of our cards are humour based and Moonpig encapsulates that.</p>
+		<p>"The name also had to be easily represented in graphical format. Once you've seen our logo you never forget it. I commissioned a cartoonist to design the moonpig. The whole branding exercise cost &pound;200 and three days' work, but it has been far more effective than we could ever have expected."</p>
+		<h3>Ensure the name is available on the internet</h3>
+		<p>"Control of a domain name was very important for us. To check what was available, I sat on Nominet for four days plugging in variations of names. I originally came up with lots of different names - red-dog, green-carrot, that sort of thing - but they were all already taken.</p>
+		<p>"Moonpig was actually my nickname at school. It was available as both a '.com' and a '.co.uk' which was also an important consideration.</p>
+		<p>"Moonpig is a completely unique word. The only references to it on Google are links to our website."</p>
+		<h3>Trade mark the name</h3>
+		<p>"Having the '.com' and '.co.uk' gave the name Moonpig some protection against copiers but we thought people might also try other permutations of Moonpig to capitalise on our success. It wasn't that expensive to go for trade marking and it gave the name an extra layer of protection.</p>
+		<p>"I employed a trade mark attorney and Moonpig is now a trade mark in the UK and the US.</p>
+		<p>"It's such a small insurance premium. In a customer-facing brand I think it's critical as the brand name is the thing that customers really recognise."</p>
+		<p><strong>What I'd do differently</strong></p>
+		<h3>Start out with a general, bland registered-company name</h3>
+		<p>"Although we continue to trade under the name Moonpig, we changed our name at Companies House to Altergraphics Limited in 2001, when we received venture-capital investment. The new investors seemed reluctant to write out a cheque with the word Moonpig in it.</p>
+		<p>"It might have been easier if we'd started out with Altergraphics as our company name anyway. Moonpig pigeonholes us as funky and humorous which is great as a trading name for our current website, but having Altergraphics as a corporate name gives us the flexibility to develop other brands under different trading names in the future."</p>
+		<p><a href="/find-a-case-study.php">Read more case studies that describe first hand how people tackle real-life challenges and opportunities</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/getting-your-business-name-on-the-internet.php" title="Getting your business name on the internet">Prev</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php" title="Choose the right name for your business">Introduction</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/choosing-a-business-name-to-create-the-right-impression.php">Choosing a business name to create the right impression</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/names-for-limited-companies-and-llps.php">Names for limited companies and LLPs</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/names-for-sole-traders-partnerships-and-limited-partnerships.php">Names for sole traders, partnerships and limited partnerships</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/use-of-sensitive-words-and-expressions-in-business-names.php">Use of sensitive words and expressions in business names</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/displaying-and-disclosing-your-business-company-or-limited-liability-partnership-name.php">Displaying and disclosing your business, company or limited liability partnership name</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/getting-your-business-name-on-the-internet.php">Getting your business name on the internet</a></li>
+			<li>Here's how I chose the name for my business</li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<div class="media-object stack-for-small">
+			<div class="media-object-section">
+				<div class="thumbnail">
+					<img src="../../../_img/moonpig.01a.jpg" width="181" height="121" border="0" alt="Nick Jenkins, Moonpig Cards" />
+				</div>
+			</div>
+			<div class="media-object-section">
+				<h3>
+					Nick Jenkins<br />
+					<a href="https://www.moonpig.com/uk/" target="_blank" rel="noopener noreferrer">Moonpig Cards</a><br />
+					Nick's top tips:
+				</h3>
+				<ul>
+					<li>"Keep your name short and simple if you're a consumer-facing company."</li>
+						<li>"Don't box yourself in. The real thought should go into your trading name. Your corporate name should be bland and give you room for manoeuvre into other activities."</li>
+						<li>"Make sure you secure the domain name for your trading name. Even if you're not selling over the internet, people will be reassured to see a professional-looking website if they are checking out the solidity of the business."</li>
+				</ul>
+			</div>
+		</div>
+
+		<h4>More case studies</h4>
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan/here-is-how-having-an-up-to-date-business-plan-helped-my-business.php">Here's how having an up-to-date business plan helped my business</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory/here-is-how-I-control-my-stock.php">Here's how I control my stock</a></li>
+			<li><a href="/sales-and-marketing/advertising-branding-and-design/best-practice-in-web-design/here-is-how-a-well-designed-website-has-boosted-my-sales.php">Here's how a well-designed website has boosted my sales </a></li>
+		</ul>
+		
+		<p><strong><a href="/find-a-case-study.php">Find a case study</a></strong></p>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
