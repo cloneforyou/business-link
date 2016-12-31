cbc394c148e37df1c6b3ab34461309461efde996
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-31 22:34:39 +0000

    Completed ‘Getting your business name on the internet’ page.

---
+++ starting-up/name-your-business/choose-the-right-name-for-your-business/getting-your-business-name-on-the-internet.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Getting your business name on the internet | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Choose the right name for your business</h1>
+		<h2>Getting your business name on the internet</h2>
+		<p>Even if you are not intending to create a website for your business immediately, you'll probably be using email and want to have a presence on the web at some point in the future. This could be a single screen advertising your business and giving contact details, or it could be a site that allows customers to browse through products, place orders and make payments online.</p>
+		<h3>Choosing a domain name</h3>
+		<p>The website address - for example, my-new-business.co.uk - is known as a <b>domain name</b>. For most businesses based in the UK, a name ending with .co.uk is suitable. Your <b>email address</b> will normally include this name - for example, enquiries@my-new-business.co.uk.</p>
+		<p>Businesses and individuals that meet certain criteria can apply for the .eu domain extension - for example, www.my-new-business.eu.</p>
+		<p>If your business is active in other European Union countries, the .eu domain name can help you market your company as a pan-European business. <a href="https://eurid.eu/en/register-a-eu-domain/" target="_blank" rel="noopener noreferrer">Find out how to register .eu domain names on the European Registry of Internet Domain Names (EURid) website</a>.</p>
+		<p>To reserve a domain name for your business, you need to register it through an agent, who will charge a small annual fee. You should do this as soon as possible - even if you're not going to use your domain name straight away.</p>
+		<p>You cannot have a domain name that is the same name as a company registered with Companies House.</p>
+		<h3>Registering your domain name</h3>
+		<ul>
+			<li>
+				<strong>Decide on a suitable domain name for your website</strong> - if you are unable to use one that exactly matches your business name. You can use numbers as well as letters. Hyphens can be used to separate words - but you can't include spaces, full stops or other punctuation. It's a good idea to have a few alternative names in case your first choice has already been taken.
+			</li>
+			<li>
+				<b>Check whether the name is available -</b> the official registry for UK domain names is Nominet. <a href="http://www.nominet.uk/" target="_blank" rel="noopener noreferrer">Check whether your chosen domain name is available on the Nominet website</a>.
+			</li>
+			<li>
+				<strong>Register the name</strong> - this is a simple process which you can carry out online with any registration agent. There are hundreds of registration agents to choose from - a good starting point is Nominet. <a href="https://www.theukdomain.uk/advice/choosing-a-registrar/" target="_blank" rel="noopener noreferrer">Find out about choosing a registration agent on the Nominet website</a>.
+			</li>
+		</ul>
+
+		<ul>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/displaying-and-disclosing-your-business-company-or-limited-liability-partnership-name.php" title="Displaying and disclosing your business, company or limited liability partnership name">Prev</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/here-is-how-i-chose-the-name-for-my-business.php" title="Here's how I chose the name for my business">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php" title="Choose the right name for your business">Introduction</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/choosing-a-business-name-to-create-the-right-impression.php">Choosing a business name to create the right impression</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/names-for-limited-companies-and-llps.php">Names for limited companies and LLPs</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/names-for-sole-traders-partnerships-and-limited-partnerships.php">Names for sole traders, partnerships and limited partnerships</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/use-of-sensitive-words-and-expressions-in-business-names.php">Use of sensitive words and expressions in business names</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/displaying-and-disclosing-your-business-company-or-limited-liability-partnership-name.php">Displaying and disclosing your business, company or limited liability partnership name</a></li>
+			<li>Getting your business name on the internet</li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/here-is-how-i-chose-the-name-for-my-business.php">Here's how I chose the name for my business</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Nominet Enquiry Line</h3>
+		<p>01865 33 22 11</p>
+		<h3>Companies House Contact Centre</h3>
+		<p>0303 1234 500</p>
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.nominet.uk/" target="_blank" rel="noopener noreferrer">Domain name checker from Nominet</a></li>
+			<li><a href="https://www.gov.uk/get-information-about-a-company" target="_blank" rel="noopener noreferrer">Use our interactive tool to search for an available company name and trade mark</a></li>
+			<li><a href="http://list.eurid.eu/registrars/ListRegistrars.htm?lang=en" target="_blank" rel="noopener noreferrer">Approved agents for .eu domain names from EURid</a></li>
+			<li><a href="/it-and-ecommerce/create-and-manage-a-website/get-the-right-website-for-your-business.php">Use our interactive tool to investigate what kind of website is best for your business</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/e-commerce/planning-for-e-commerce.php">Planning for e-commerce</a></li>
+			<li><a href="/it-and-ecommerce/create-and-manage-a-website/website-hosting-options.php">Website hosting options</a></li>
+			<li><a href="/it-and-ecommerce/suppliers/choose-and-manage-your-it-supplier.php">Choose and manage your IT supplier</a></li>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
