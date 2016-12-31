115908a64689622cde35450aad22bbfc76a59a3b
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-31 23:05:13 +0000

    Completed ‘Objections to company names’ page.

---
+++ starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp/objections-to-company-names.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Objections to company names | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Rules for naming your limited company or limited liability partnership (LLP)</h1>
+		<h2>Objections to company names</h2>
+		<p>Once the name of your company or LLP has been registered, there is usually no need to change it. However, there are some exceptions. You may need to change your company or LLP name if:</p>
+		<ul>
+			<li>there is an objection, upheld by the Secretary of State, that the name is 'too like' another business' name</li>
+			<li>within five years of registration it is found that misleading information was given at the time of registration</li>
+			<li>within five years of registration any conditions attached to the registration - eg the provision of support documentation for a sensitive name - have not been fulfilled</li>
+			<li>someone objects that the company or LLP name is the same as one in which they have goodwill and that the reason for registration was 'opportunistic'</li>
+			<li>the name is misleading and as a result may cause harm to the public</li>
+		</ul>
+		<h3>What is 'too like'?</h3>
+		<p>If a company name is found to be very similar to that of an existing company name - eg if the names differ in a minor way - they are likely to be 'too like'. Most names that are 'too like' could cause some confusion, such as the names A &amp; B Consultants Limited and A &amp; B Consulting Limited.</p>
+		<p>If names differ by one or more words, they will usually be acceptable, unless the names have very distinctive elements in common, and differ only by words such as 'services' or 'trading'.</p>
+		<h3>Objecting to a name</h3>
+		<p>If you wish to object to a name, write to the registrar where the company to which you object is registered:</p>
+		<p>For England and Wales:
+			<br /> The Secretary of State
+			<br /> Registration Customer Support
+			<br /> Companies House
+			<br /> Crown Way
+			<br /> Cardiff
+			<br /> CF14 3UZ</p>
+		<p>For Scotland:
+			<br /> The Secretary of State
+			<br /> New Companies
+			<br /> Companies House
+			<br /> 4th Floor
+			<br /> Edinburgh Quay 2
+			<br /> 139 Fountainbridge
+			<br /> Edinburgh
+			<br /> EH3 9FF</p>
+		<p>For Northern Ireland:
+			<br /> The Registrar of Companies
+			<br /> Companies House
+			<br /> Second Floor
+			<br /> The Linenhall
+			<br /> 32-38 Linenhall Street
+			<br /> Belfast
+			<br /> BT2 8BG</p>
+		<p>Directions to change a company name must be issued within 12 months of registration or change of name. If the registrar upholds the objection, the company or LLP must change its name within 12 weeks.</p>
+		<h3>Opportunistic registrations</h3>
+		<p>If a company has registered a name with the main objective of getting money or other consideration from an individual or another company, they can be ordered to change the name. <a href="https://www.gov.uk/government/organisations/company-names-tribunal" target="_blank" rel="noopener noreferrer">Find guidance on opportunistic registrations on the Intellectual Property Office (IPO) website</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp/choosing-a-name-for-a-company-or-llp.php" title="Choosing a name for a company or LLP">Prev</a></li>
+			<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp/prescribed-and-sensitive-words.php" title="Prescribed and sensitive words">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php" title="Rules for naming your limited company or limited liability partnership (LLP)">Introduction</a></li>
+			<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp/choosing-a-name-for-a-company-or-llp.php">Choosing a name for a company or LLP</a></li>
+			<li>Objections to company names</li>
+			<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp/prescribed-and-sensitive-words.php">Prescribed and sensitive words</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Companies House Contact Centre</h3>
+		<p>0303 1234 500</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/organisations/company-names-tribunal" target="_blank" rel="noopener noreferrer">Opportunistic registrations information on the IPO website</a></li>
+			<li><a href="https://www.gov.uk/topic/intellectual-property/trade-marks" target="_blank" rel="noopener noreferrer">Trade marks information on the IPO website</a></li>
+			<li><a href="https://www.gov.uk/get-information-about-a-company" target="_blank" rel="noopener noreferrer">Use our interactive tool to search for an available company name and trade mark</a></li>
+			<li><a href="https://www.gov.uk/topic/intellectual-property/trade-marks" target="_blank" rel="noopener noreferrer">Trade marks information on the Intellectual Property Office website</a></li>
+			<li><a href="https://www.citma.org.uk/trade_marks/about_trade_marks" target="_blank" rel="noopener noreferrer">Trade marks guidance on the Institute of Trade Mark Attorneys website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php">Choose the right name for your business</a></li>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business</a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
