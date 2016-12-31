8dee4582a78a156865e59d81af2d85c46ecd60fd
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-31 22:57:37 +0000

    Completed ‘Choosing a name for a company or LLP’ page.

---
+++ starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp/choosing-a-name-for-a-company-or-llp.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Choosing a name for a company or LLP | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Rules for naming your limited company or limited liability partnership (LLP)</h1>
+		<h2>Choosing a name for a company or LLP</h2>
+		<p>There are several rules to consider when choosing a name for your company or LLP.</p>
+		<p>You will need to check that it is not the 'same as' that of an existing company or LLP. A 'same as' name may not be identical to an existing company name, but if it is very similar it could cause confusion. For example, if a company called 'Smith Transport Services Limited' already exists, you could not register 'Smith Transport Services UK Limited', unless the two companies are part of the same group and other conditions are satisfied.</p>
+		<p>To check whether a company or LLP name is already registered, <a href="https://www.gov.uk/get-information-about-a-company" target="_blank" rel="noopener noreferrer">use our interactive tool to search for an available company name and trade mark</a>.</p>
+		<h3>Required words</h3>
+		<p>Limited companies and LLPs are required to include certain words in their names:</p>
+		<ul>
+			<li>private limited company names must end with 'Limited or 'Ltd'</li>
+			<li>private unlimited company names may end with 'unlimited' but are not required to do so</li>
+			<li>public limited company names must end with 'public limited company' or 'plc', in capital or lower-case with or without full stops</li>
+			<li>LLP names must end with 'limited liability partnership' or 'LLP'</li>
+		</ul>
+		<p>The names of limited companies registered in Wales may end with the Welsh equivalent of 'Limited', 'public limited company' or their abbreviations.</p>
+		<h3>When will Companies House refuse to register a company name?</h3>
+		<p>Companies House will not register a name if:</p>
+		<ul>
+			<li>its use would constitute an offence or is offensive</li>
+			<li>it implies a connection with central or local government, or with some public authorities</li>
+			<li>it includes a 'sensitive' word or expression - see the page in this guide on <a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp/prescribed-and-sensitive-words.php">prescribed and sensitive words</a></li>
+			<li>it includes characters, signs, symbols and punctuation which are not permitted</li>
+			<li>it is identical to, or the same as, another name appearing on the index of company names</li>
+			<li>it uses any of the name endings 'limited', 'ltd', 'unlimited', 'public limited company' or 'plc' in a way that misleads or causes confusion</li>
+		</ul>
+
+		<ul>
+			<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php" title="Rules for naming your limited company or limited liability partnership (LLP)">Prev</a></li>
+			<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp/objections-to-company-names.php" title="Objections to company names">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php" title="Rules for naming your limited company or limited liability partnership (LLP)">Introduction</a></li>
+			<li>Choosing a name for a company or LLP</li>
+			<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp/objections-to-company-names.php">Objections to company names</a></li>
+			<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp/prescribed-and-sensitive-words.php">Prescribed and sensitive words</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Companies House Contact Centre</h3>
+		<p>0303 1234 500</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/do-it-online/incorporate-your-company-online.php">Incorporate your limited company online with Companies House</a></li>
+			<li><a href="https://www.gov.uk/get-information-about-a-company" target="_blank" rel="noopener noreferrer">Use our interactive tool to search for an available company name and trade mark</a></li>
+			<li><a href="https://www.gov.uk/topic/intellectual-property/trade-marks" target="_blank" rel="noopener noreferrer">Trade marks information on the Intellectual Property Office website</a></li>
+			<li><a href="https://www.citma.org.uk/trade_marks/about_trade_marks" target="_blank" rel="noopener noreferrer">Trade marks guidance on the Institute of Trade Mark Attorneys website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
