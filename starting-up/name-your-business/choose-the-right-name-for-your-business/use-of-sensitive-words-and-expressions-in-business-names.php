f11fa40d03567797f1b36a36671bd122079e7c71
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-31 22:19:48 +0000

    Completed ‘Use of sensitive words and expressions in business names’ page.

---
+++ starting-up/name-your-business/choose-the-right-name-for-your-business/use-of-sensitive-words-and-expressions-in-business-names.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Use of sensitive words and expressions in business names | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Choose the right name for your business</h1>
+		<h2>Use of sensitive words and expressions in business names</h2>
+		<p>There are some words and expressions that you can't use in a business name unless you have official permission. These are words that might give a false impression about your business. They are known as <b>sensitive</b> words.</p>
+		<p>The rules about sensitive words apply to all types of businesses and fall into five main groups:</p>
+		<p><strong>Sensitive words</strong></p>
+		
+		<table>
+			<tbody>
+				<tr>
+					<th scope="col"><strong>Type of expression</strong></th>
+					<th scope="col"><strong>Examples</strong></th>
+				</tr>
+				<tr>
+					<th>Words that suggest your business is of national importance or status</th>
+					<td>British, National, International, European</td>
+				</tr>
+				<tr>
+					<th>Words that suggest a special status</th>
+					<td>Association, Authority, Chartered, Council, Institute, Society</td>
+				</tr>
+				<tr>
+					<th>Words that suggest a particular function</th>
+					<td>Charity, Insurance, Register, Trust</td>
+				</tr>
+				<tr>
+					<th>Words that suggest a specialised activity</th>
+					<td>Health Centre</td>
+				</tr>
+				<tr>
+					<th>Words that suggest connections with government or royalty</th>
+					<td>Parliament, Government, Royal, Queen, Prince</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<h3>Getting further help</h3>
+		<p>These guidelines provide a basic summary of sensitive words. You can find further details on company names in our guide on the <a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php">rules for naming your limited company or limited liability partnership (LLP)</a>.</p>
+		<p><a href="https://www.gov.uk/government/publications/incorporation-and-names#appA" target="_blank" rel="noopener noreferrer">Find a list of prescribed sensitive words on the Companies House website</a>.</p>
+		<ul>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/names-for-sole-traders-partnerships-and-limited-partnerships.php" title="Names for sole traders, partnerships and limited partnerships">Prev</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/displaying-and-disclosing-your-business-company-or-limited-liability-partnership-name.php" title="Displaying and disclosing your business, company or limited liability partnership name">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php" title="Choose the right name for your business">Introduction</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/choosing-a-business-name-to-create-the-right-impression.php">Choosing a business name to create the right impression</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/names-for-limited-companies-and-llps.php">Names for limited companies and LLPs</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/names-for-sole-traders-partnerships-and-limited-partnerships.php">Names for sole traders, partnerships and limited partnerships</a></li>
+			<li>Use of sensitive words and expressions in business names</li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/displaying-and-disclosing-your-business-company-or-limited-liability-partnership-name.php">Displaying and disclosing your business, company or limited liability partnership name</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/getting-your-business-name-on-the-internet.php">Getting your business name on the internet</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/here-is-how-i-chose-the-name-for-my-business.php">Here's how I chose the name for my business</a></li>
+		</ul>
+
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Companies House Contact Centre</h3>
+		<p>0303 1234 500</p>
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/publications/incorporation-and-names#appA" target="_blank" rel="noopener noreferrer">List of sensitive words from Companies House</a></li>
+			<li><a href="https://www.gov.uk/get-information-about-a-company" target="_blank" rel="noopener noreferrer">Use our interactive tool to search for an available company name and trade mark</a></li>
+		</ul>
+		
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php">Rules for naming your limited company or limited liability partnership (LLP)</a></li>
+			<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
