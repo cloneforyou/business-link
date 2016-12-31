3f2572e1e67823fbadfddc01fc3635949b76f844
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-31 22:03:19 +0000

    Completed ‘Names for sole traders, partnerships and limited partnerships’ page.

---
+++ starting-up/name-your-business/choose-the-right-name-for-your-business/names-for-sole-traders-partnerships-and-limited-partnerships.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Names for sole traders, partnerships and limited partnerships | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+
+		<h1>Choose the right name for your business</h1>
+		<h2>Names for sole traders, partnerships and limited partnerships</h2>
+		<p>People operating as sole traders or in general partnerships can trade under their own names, or choose a different business name.</p>
+		<h3>Sole trader, partnership and limited partnership names - the rules</h3>
+		<p>If you decide to use a business name, it must not:</p>
+		<ul>
+			<li>be offensive</li>
+			<li>include the terms public limited company (plc), limited (ltd), limited liability partnership (LLP) or their Welsh equivalents</li>
+			<li>contain prescribed or sensitive words and expressions, unless you have obtained permission to use them - see the page in this guide on the <a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/use-of-sensitive-words-and-expressions-in-business-names.php">use of sensitive words and expressions in business names</a></li>
+		</ul>
+		<p>If you register a limited partnership you must include either 'Limited Partnership' or 'LP' (or their Welsh equivalent if registered you are in Wales) at the end of your business name.</p>
+		<p>For more information, see our guide on <a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php">rules for naming your limited company or limited liability partnership (LLP)</a>. Alternatively, you can contact the Companies House Contact Centre on Tel 0845 604 88 88 for a copy.</p>
+		<h3>Is anyone else using your proposed business name?</h3>
+		<p>Before using your chosen name, check that it isn't already being used.</p>
+		<p>If a sole trader at the other end of the country is using it, there may not be a problem. However, if another local business, company or national firm is using it, you should choose a different name.</p>
+		<ul>
+			<li>Check local phone books, business directories and the internet.</li>
+			<li>Make sure that your proposed name - or something similar - hasn't been registered by a company.</li>
+			<li>Make sure that the name isn't too similar to a word or expression that has been registered as a trade mark.</li>
+			<li><a href="https://www.gov.uk/get-information-about-a-company" target="_blank" rel="noopener noreferrer">Use our interactive tool to search for an available company name and trade mark</a>.</li>
+		</ul>
+		<p>If you're in any doubt about your business name, get expert advice from your local Business Link.</p>
+		
+		<ul>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/names-for-limited-companies-and-llps.php" title="Names for limited companies and LLPs">Prev</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/use-of-sensitive-words-and-expressions-in-business-names.php" title="Use of sensitive words and expressions in business names">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php" title="Choose the right name for your business">Introduction</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/choosing-a-business-name-to-create-the-right-impression.php">Choosing a business name to create the right impression</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/names-for-limited-companies-and-llps.php">Names for limited companies and LLPs</a></li>
+			<li>Names for sole traders, partnerships and limited partnerships</li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/use-of-sensitive-words-and-expressions-in-business-names.php">Use of sensitive words and expressions in business names</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/displaying-and-disclosing-your-business-company-or-limited-liability-partnership-name.php">Displaying and disclosing your business, company or limited liability partnership name</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/getting-your-business-name-on-the-internet.php">Getting your business name on the internet</a></li>
+			<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business/here-is-how-i-chose-the-name-for-my-business.php">Here's how I chose the name for my business</a></li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Companies House Contact Centre</h3>
+		<p>0303 1234 500</p>
+		<h3>Business Link Helpline</h3>
+		<p>0845 600 9 006</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/get-information-about-a-company" target="_blank" rel="noopener noreferrer">Use our interactive tool to search for an available company name and trade mark</a></li>
+			<li><a href="http://www.nominet.uk/domains/about-domain-names/" target="_blank" rel="noopener noreferrer">Choosing your domain name guidance from Nominet</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php">Rules for naming your limited company or limited liability partnership (LLP)</a></li>
+			<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+			<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership.php">Set up and register a partnership</a></li>
+			<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed.php">Set up and register as self-employed</a></li>
+			<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp.php">Set up and register a limited liability partnership (LLP)</a></li>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
