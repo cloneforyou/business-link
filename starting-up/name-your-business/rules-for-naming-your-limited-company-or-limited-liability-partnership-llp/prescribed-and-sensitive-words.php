bde672a8b0a6820e137521cf916394d519909071
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2016-12-31 23:06:20 +0000

    Completed ‘Prescribed and sensitive words’ page.

---
+++ starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp/prescribed-and-sensitive-words.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Prescribed and sensitive words | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Rules for naming your limited company or limited liability partnership (LLP)</h1>
+		<h2>Prescribed and sensitive words</h2>
+		<p>Some words require the consent of a relevant body or must meet certain requirements before they can be used in a company or LLP name. A copy of the consent must be sent to Companies House with the application to register your company or LLP.</p>
+		<p><a href="https://www.gov.uk/government/organisations/company-names-tribunal" target="_blank" rel="noopener noreferrer">Find a list of prescribed and sensitive words and the bodies whose consent you may need to obtain on the Companies House website</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp/objections-to-company-names.php" title="Objections to company names">Prev</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php" title="Rules for naming your limited company or limited liability partnership (LLP)">Introduction</a></li>
+			<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp/choosing-a-name-for-a-company-or-llp.php">Choosing a name for a company or LLP</a></li>
+			<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp/objections-to-company-names.php">Objections to company names</a></li>
+			<li>Prescribed and sensitive words</li>
+		</ul>
+		
+		<img src="../../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Companies House Contact Centre</h3>
+		<p>0303 1234 500</p>
+
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/organisations/company-names-tribunal" target="_blank" rel="noopener noreferrer">Prescribed and sensitive words and the bodies whose consent you may need to obtain listed on the Companies House website</a></li>
+			<li><a href="https://www.gov.uk/get-information-about-a-company" target="_blank" rel="noopener noreferrer">Use our interactive tool to search for an available company name and trade mark</a></li>
+			<li><a href="https://www.gov.uk/topic/intellectual-property/trade-marks" target="_blank" rel="noopener noreferrer">Trade marks information on the Intellectual Property Office website</a></li>
+			<li><a href="https://www.citma.org.uk/trade_marks/about_trade_marks" target="_blank" rel="noopener noreferrer">Trade marks guidance on the Institute of Trade Mark Attorneys website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business</a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+			<li><a href="https://www.gov.uk/business-legal-structures" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
