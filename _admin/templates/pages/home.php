6ba65b75c5b2e3afc909b5259412ce5e99ba3396
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2017-01-10 21:24:01 +0000

    Removed index - this is now in the Perch CMS as Home - and used the Master Page ‘home’

--- _admin/templates/pages/home.php
+++ _admin/templates/pages/home.php
@@ -3,17 +3,259 @@
 	perch_layout('global/header', [
 		'body-class' => 'home',
 	]);
+?>
 
-	// An editable content region
-	perch_content('Main heading');
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
 
-    // Main navigation
-    perch_pages_navigation([
-    	'levels'=>1
-    ]);
+		<h1><?php perch_content('Main heading'); ?></h1>
 
-    // An editable content region
-    perch_content('Intro');
+		<?php perch_content('Intro'); ?>
+		
+		<hr />
+		
+		<p>Keep your business on track with our latest news and updates</p>
+		
+		<h3>UK welcomes business</h3>
+		<a href="http://www.ukwelcomes.businesslink.gov.uk/" title="Learn about the opportunities that the UK could offer to your business" target="_blank" rel="noopener noreferrer">
+			<img src="/_img/PSCPromoimage.jpg" width="166" height="56" alt="Learn about the opportunities that the UK could offer to your business" /></a>
+		
+		<p>Are you based outside the UK? Do you want to explore opportunities or expand your business in the UK? If so, then UK welcomes business is for you&hellip;</p>
+		
+		<h3>Find out what records you should be keeping</h3>
+		<p>
+			<a href="find-out-what-records-you-should-be-keeping.php" title="Use our interactive tool to find out the records you must keep as a business and assess how well you are keeping them">
+				<img src="/_img/web_chk_home.jpg" width="166" height="56" alt="Use our interactive tool to find out the records you must keep as a business and assess how well you are keeping them" />
+				<br />Use our interactive tool to find out the records you must keep as a business
+			</a>
+		</p>
+		
+		<h3>Intrastat returns: important changes</h3>
+		<p>
+			<a href="intrastat-returns-important-changes.php" title="Submitting Intrastat declarations from 1 April 2012: a reminder">
+				<img src="/_img/HP_hands_on_keyboard.jpg" width="166" height="56" alt="Submitting Intrastat declarations from 1 April 2012: a reminder" />
+			<br />Businesses providing Intrastat returns to HMRC need to act on this information</a>
+		</p>
 
- 	// Include the footer. You can find this in tempates/layouts/global
-    perch_layout('global/footer');
+		<h3>Updated Employer's Charter</h3>
+		<p>
+			<a href="employers-charter.php" title="Find out what you can and can't do when managing your staff">
+				<img src="/_img/support.jpg" width="166" height="56" alt="Find out what you can and can't do when managing your staff" />
+			<br />Find out what you can and can't do when managing your staff
+			</a>
+		</p>
+
+		<h3>Have a business idea, looking to start-up or grow your business?</h3>
+		<p>
+			<a href="http://improve.businesslink.gov.uk/what-service-can-do-you" title="GIS service" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/helplineblsmallhomepage.jpg" width="166" height="56" alt="GIS service" />
+			<br />Unable to find the information you are looking for?</a>
+			<br />
+			Contact the <strong>Business Link Helpline</strong> on: <a href="tel:08456009006">0845 600 9006</a>, open 9am - 6pm Monday to Friday.
+		</p>
+
+		<h3>The business in you</h3>
+		<a href="find-the-business-in-you.php" title="The 'Business in You' is a major new campaign to help people to start a new business or grow their existing one">
+			<img src="/_img/Business_in_you_smaller_homepage_promo.jpg" width="166" height="56" alt="The "business in you" is a major new campaign to help people to start a new business or grow their existing one" />
+		</a>
+		<p>Starting and growing a business - tips and tools from real-life success stories</p>
+
+		<hr />
+
+		<div class="row small-up-1 medium-up-2">
+			<div class="column">
+				<p>
+					<a href="gov-uk-will-launch-in-october.php" title="GOV.UK will launch in October">
+						Businesslink.gov.uk is changing
+						<img src="/_img/gov-uk-will-launch-in-october.jpg" alt="" />
+					</a>
+					<br />GOV.UK October launch
+				</p>
+			</div>
+			<div class="column">
+				<p>
+					<a href="https://www.gov.uk/growing-your-business" title="Information and tools for people interested in growing or improving their business">
+						Growth and Improvement Service
+						<img src="/_img/growing-your-business.jpg" alt="" />
+					</a>
+					<br />Access information to help your business work better
+				</p>
+			</div>
+			<div class="column">
+				<p>
+					<a href="#">
+						Look out for fake emails from legitimate organisations
+						<img src="/_img/spam-emails.jpg" alt="" />
+					</a>
+					<br />Take note of our advice regarding fake emails
+				</p>
+			</div>
+			<div class="column">
+				<p>
+					<a href="#" title="A report and short guide by Lord Young, highlighting the evolving nature of enterprise and business and showcasing the opportunities and support available to you.">
+						Make Business Your Business
+						<img src="/_img/make-business-your-business.jpg" alt="Make Business Your Business" />
+					</a>
+					<br />Helping you to start and develop your business
+				</p>
+			</div>
+			<div class="column">
+				<p>
+					<a href="https://www.gov.uk/growing-your-business" title="GIS service">
+						Have a business idea, looking to start-up or grow your business?
+						<img src="/_img/icon-helpline.jpg" alt="" />
+					</a>
+					<br />Unable to find the information you are looking for?<br />
+					Contact the <strong>Business Link Helpline</strong> on: <a href="tel:08456009006">0845 600 9006</a>, open 9am - 6pm Monday to Friday.
+				</p>
+			</div>
+			<div class="column">
+				<p>
+					<a href="#">
+						Contracts Finder for suppliers and buyers
+						<img src="/_img/bhp_contract_finder.jpg" alt="Contract Finder" />
+					</a>
+					<br />Find public sector procurement tender and contract documents
+				</p>
+			</div>
+			<div class="column">
+				<p>
+					<a href="https://www.gov.uk/browse/business/setting-up" title="My New Business" target="_blank" rel="noopener noreferrer">
+						Start-up service
+					</a>
+					<a href="https://www.gov.uk/browse/business/setting-up" title="My New Business">
+						<img src="/_img/MNB_302x239.jpg" alt="" />
+					</a>
+					<br />Get your business off the ground
+				</p>
+			</div>
+			<div class="column">
+				<p>
+					<a href="our-use-of-cookies.php" title="How cookies are used on this site">
+						Our use of cookies
+						<img src="/_img/handonkeyboard_large.jpg" alt="Cookies" />
+					</a>
+					<br />How cookies are used on Business Link
+				</p>
+			</div>
+			<div class="column">
+				<p>
+					<a href="apprenticeship-grants-now-available.php" title="Get �1,500 for recruiting an apprentice aged 16 to 24">
+						Apprenticeship grants now available
+						<img src="/_img/apprenticeships_302x239.JPG" alt="Apprenticeships" />
+					</a>
+					<br />Get &pound;1,500 when you recruit an apprentice aged 16 to 24
+				</p>
+			</div>
+			<div class="column">
+				<p>
+					<a href="regulation-licences-and-compliance.php" title="Find and apply for licences and permits for your business">
+						Apply for licences and permits online
+						<img src="/_img/online_licences_2.jpg" alt="" />
+					</a>
+					<br />You can now apply online for business licences and permits from around 90 per cent of authorities in the UK
+				</p>
+			</div>
+			<div class="column">
+				<p>
+					<a href="incorporate-your-company-online.php" title="Incorporate your company online as a private company, limited by shares, with model articles of association">
+						Incorporate your limited company online with Companies House
+						<img src="/_img/hp_promo_302x239_comp2.jpg" alt="" />
+					</a>
+					<br />Use the web incorporation service tool to incorporate your limited company online with Companies House
+				</p>
+			</div>
+			<div class="column">
+				<p>
+					<a href="taking-on-a-new-member-of-staff.php" title="We have launched three new tools to help you through the process of taking on a new member of staff">
+						Taking on a new member of staff?
+						<img src="/_img/hp_promo_302x239_comp1.jpg" alt="" />
+					</a>
+					<br />Use our new and improved interactive tools
+				</p>
+			</div>
+			<div class="column">
+				<p>
+					<a href="http://www.events.businesslink.gov.uk/" title="Search for relevant business events near you" target="_blank" rel="noopener noreferrer">
+						Events Finder
+					</a>
+					<a href="http://www.events.businesslink.gov.uk/" title="Search for relevant business events near you">
+						<img src="/_img/EMS_302x239.jpg" alt="" />
+					</a>
+					<br />Find and book business related events near you
+				</p>
+			</div>
+			<div class="column">
+				<p>
+					<a href="http://improve.businesslink.gov.uk/resources/business-support-finder" title="Search our business support finder for grants, loans, expertise and advice for which your business may be eligible">
+						Business Support Finder
+						<img src="/_img/bsf_promo.jpg"  alt="" />
+					</a>
+					<br />Find sources of help to start, run or grow your business
+				</p>
+			</div>
+			<div class="column">
+				<p>
+					<a href="https://www.gov.uk/growing-your-business" title="Taking on an employee" target="_blank" rel="noopener noreferrer">
+						Taking on an employee tool launched
+					</a>
+					<a href="https://www.gov.uk/growing-your-business" title="Taking on an employee">
+						<img src="/_img/takingonanemployee_home.jpg" alt="Taking on an employee tool" />
+					</a>
+					<br />Tool outlines the five steps an employer must take when employing someone
+				</p>
+			</div>
+			<div class="column"></div>
+		</div>
+		<p><a href="whats-new.php">View all what's new items</a></p>
+		
+
+		<h2><a href="do-it-online.php" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies">Do it online</a></h2>
+		<ul>
+			<li>
+				<a href="search-company-information-using-companies-house-webcheck.php" title="Search company details and view filing histories for all limited companies in the UK">Find company details with WebCHeck</a>
+			</li>
+			<li>
+				<a href="file-company-information-online-using-companies-house-webfiling.php" title="File most company information online (some services require payment)">File company details with WebFiling</a>
+			</li>
+			<li>
+				<a href="/do-it-online/using-vat-online.php" title="Access to HMRC VAT online and links to information on service availability and support">Submit your VAT Return online</a>
+			</li>
+			<li>
+				<a href="incorporate-your-company-online.php" title="Incorporate your company online as a private company, limited by shares, with model articles of association">Incorporate your limited company online with Companies House</a>
+			</li>
+			<li>
+				<a href="/do-it-online/use-paye-online-for-employers.php" title="Access HMRC's PAYE Online for employers services">Log in or register for PAYE Online</a>
+			</li>
+			<li>
+				<a href="regulation-licences-and-compliance.php" title="Find and apply for licences and permits for your business">Apply for licences</a>
+			</li>
+		</ul>
+		<p><a href="do-it-online.php" title="Search or browse our directory to find online and downloadable forms to use when dealing with government departments and agencies">View all services</a></p>
+
+		<!--
+		<h2><a href="#">My Business</a></h2>
+		<p>
+			<a href="#" title="The more you tell us about your business, the more time we can save you. We recommend that you keep your profile up to date.">
+			<strong>Register for My Business now</strong>
+			</a><br /><br />
+			Access interactive tools.<br />
+			Receive important updates.<br />
+			Save relevant information.
+		</p>
+		<p><a href="#">Go to My Business</a></p>
+		<p><a href="#">Log in</a></p>
+		-->
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php
+		perch_pages_navigation(array(
+			'levels'    => 0, 
+			'template'	=> 'accordionnav.html'
+		));
+	?>
+
+</div><!-- // .row -->
+
+<?php perch_layout('global/footer'); ?>
