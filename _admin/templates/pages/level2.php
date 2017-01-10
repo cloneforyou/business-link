a3d2c1e104055384d4644bb9cb7067f1a73f1b18
Caroline Hagan <carolinehagan@Carolines-iMac.local>
2017-01-10 22:20:47 +0000

    removed ‘considering starting up’ content into the CMS, added the Perch regions and defined their type.

--- _admin/templates/pages/level2.php
+++ _admin/templates/pages/level2.php
@@ -8,57 +8,25 @@
 <div class="row">
 	<div class="small-12 large-8 large-push-4 columns">
 
-		<h1>Considering starting up?</h1>
+		<h1>
+			<?php 
+				perch_content_create('Heading One', array(
+					'template' => 'text.html'
+				)); 
+				perch_content('Heading One'); 
+			?>
+		</h1>
 
-		<h2><a href="https://www.gov.uk/starting-up-a-business" target="_blank" rel="noopener noreferrer">Find out whether you can work for yourself</a></h2>
-		<p>Discover what it means to be your own boss and how to take the first steps in getting your business off the ground</p>
-
-		<h2><a href="https://www.gov.uk/starting-up-a-business" target="_blank" rel="noopener noreferrer">Decide if you've got what it takes to be your own boss</a></h2>
-		<p>Learn about the qualities you need to be your own boss and how you can use your skills and experience to get your business up and running</p>
-
-		<h2><a href="https://www.gov.uk/starting-up-a-business" target="_blank" rel="noopener noreferrer">Develop your business idea</a></h2>
-		<p>Find out about researching your chosen market and how to generate or develop a new business idea</p>
-
-		<h2><a href="/starting-up/considering-starting-up/starting-a-business-the-basics.php">Starting a business: the basics</a></h2>
-		<p>What to do when starting a business, including advice on business plans, financial and legal requirements</p>
-
-		<h2><a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">Research and develop your business ideas, new products and services</a></h2>
-		<p>How to develop your idea into a viable business by investing in new products or services to meet customer needs</p>
-
-		<h2><a href="/starting-up/considering-starting-up/common-mistakes-when-starting-up-and-how-to-avoid-them.php">Common mistakes when starting up - and how to avoid them</a></h2>
-		<p>Learn about and avoid the most frequent mistakes made by new business owners</p>
-
-		<h2><a href="/starting-up/considering-starting-up/starting-a-part-time-business.php">Starting a part-time business</a></h2>
-		<p>Information on starting a part-time business</p>
-
-		<h2><a href="/starting-up/considering-starting-up/how-do-I-survive-until-my-business-is-off-the-ground.php">How do I survive until my business is off the ground?</a></h2>
-		<p>How to keep yourself afloat during the early months of your new business</p>
-
-		<h2><a href="/starting-up/considering-starting-up/family-run-businesses.php">Family-run businesses</a></h2>
-		<p>Understand the particular benefits and issues that affect family-run businesses</p>
-
-		<h2><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise.php">Thinking about starting a social enterprise?</a></h2>
-		<p>Why set up a social enterprise and the benefits of doing so</p>
-
-		<h2><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough.php">Starting a business when economic conditions are tough</a></h2>
-		<p>Assess the opportunities for starting a business during an economic downturn</p>
-
-		<h2><a href="https://www.gov.uk/business-finance-explained" target="_blank" rel="noopener noreferrer">Think about the money</a></h2>
-		<p>Understand your finance options when starting up and how to manage your money in the first year of trading</p>
-
-		<h3>Directories</h3>
-		<ul>
-			<li><a href="https://www.gov.uk/browse/business/setting-up" title="Setting up" target="_blank" rel="noopener noreferrer">Starting up events near you</a></li>
-			<li><a href="/find-a-case-study/browse-by-theme-starting-up.php">Starting up case studies</a></li>
-		</ul>
+		<?php 
+			perch_content_create('Main Content', array(
+				'template' => 'code_block.html'
+			)); 
+			perch_content('Main Content'); 
+		?>
 
 	</div><!-- // .small-12 large-8 large-push-4 columns -->
 
-	<?php
-		perch_pages_navigation(array(
-			'template'	=> 'accordionnav.html'
-		));
-	?>
+	<?php perch_layout('global/navigation'); ?>
 
 </div><!-- // .row -->
 
