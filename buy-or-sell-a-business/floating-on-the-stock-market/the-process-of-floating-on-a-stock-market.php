6d1b6803b9e5de67f013b1faa71da74e515ea11d
Caroline Hagan <info@blueocto.co.uk>
2017-02-03 17:08:29 +0000

    Completed, Buy or sell a business > Floating on the stock market > The process of floating on a stock market.

---
+++ buy-or-sell-a-business/floating-on-the-stock-market/the-process-of-floating-on-a-stock-market.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The process of floating on a stock market | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+		<h1>Floating on the stock market</h1>
+		<h2>The process of floating on a stock market</h2>
+		<p>When seeking to join a stock market, your business will be subject to legal and financial due diligence to help attract investor interest and to fulfil the entry requirements and documentation of the relevant market.</p>
+		<p><a href="../../_files/london-stock-exchange_guide-to-capital-markets.pdf" target="_blank" rel="noopener noreferrer">Download information on the AIM and Main Market admission criteria from the London Stock Exchange website (PDF, 1.68MB)</a>.</p>
+		<p>Your business also needs to have the right legal structure. The legal structure of a sole trader or a partnership is not suitable for a public market listing and as such a change to the company's legal structure would be necessary.</p>
+		<p>Public companies have different obligations to private companies under the Companies Act 2006. For example, private companies are no longer required to hold an annual general meeting (AGM) though they may opt to do so or be required to hold one if sufficient shareholders demand one. However, all public companies and private companies with traded shares must hold an AGM. See our guide on <a href="/tax-payroll-and-company-information/company-administration-an-introduction/company-administration-the-basics.php">company administration: the basics</a>.</p>
+		<p>You will also have to make the following key information available:</p>
+		<ul>
+			<li>who the directors are and what service contracts they have with the company</li>
+			<li>who the major shareholders are and details of the new and existing shares being offered for sale</li>
+			<li>information on the company's key contracts</li>
+			<li>the memorandum and articles of association</li>
+		</ul>
+		<p>Typically, the admission process may take between three and six months to complete, but it could take as long as a year to prepare the business and conduct the due diligence. To help with this process, you will need to appoint a set of advisers with relevant experience in helping businesses like yours seek capital via the public markets.</p>
+		<p><a href="http://www.londonstockexchange.com/companies-and-advisors/main-market/advisors/advisors.htm" target="_blank" rel="noopener noreferrer">Find information on how advisers can help you float your company on the stock exchange on the London Stock Exchange website</a>.</p>
+
+		<ul>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market/advantages-and-disadvantages-of-stock-market-flotation.php" title="Advantages and disadvantages of stock market flotation">Prev</a></li>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market/appoint-your-advisers.php" title="Appoint your advisers">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market.php" title="Floating on the stock market">Introduction</a></li>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market/is-your-business-suitable-to-be-a-public-company.php">Is your business suitable to be a public company?</a></li>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market/advantages-and-disadvantages-of-stock-market-flotation.php">Advantages and disadvantages of stock market flotation</a></li>
+			<li>The process of floating on a stock market</li>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market/appoint-your-advisers.php">Appoint your advisers</a></li>
+			<li><a href="/finance-and-grants/finance-options/shares-and-equity-finance/here-is-how-we-prepared-our-business-for-flotation.php">Here's how we prepared our business for flotation</a></li>
+		</ul>
+
+		<img src="../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="../.../_files/london-stock-exchange_aim-rules-for-companies_02-10.pdf" target="_blank" rel="noopener noreferrer">Download AIM rules from the London Stock Exchange website (PDF, 922K)</a></li>
+			<li><a href="http://www.londonstockexchange.com/traders-and-brokers/rules-regulations/rules-regulations.htm" target="_blank" rel="noopener noreferrer">Main Market rules on the London Stock Exchange website</a></li>
+			<li><a href="../../_files/baker-tilly_european-guide-to-aim-2008.pdf" target="_blank" rel="noopener noreferrer">Download AIM guidance from the Baker Tilly website (PDF, 144K)</a></li>
+			<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance options for your business</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/company-administration-an-introduction/company-administration-the-basics.php">Company administration: the basics</a></li>
+			<li><a href="/finance-and-grants/finance-options/shares-and-equity-finance/equity-finance.php">Equity finance</a></li>
+			<li><a href="/finance-and-grants/finance-options/shares-and-equity-finance/shares-and-shareholders.php">Shares and shareholders</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business.php">Value and market your business</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/assess-your-options-for-growth.php">Assess your options for growth</a></li>
+		</ul>
+
+		<img src="../../_img/px_sellingabusiness.gif" alt="" width="181" height="5" />
+		
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
