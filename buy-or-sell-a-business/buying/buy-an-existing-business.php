f00c0055460b72341a35e48f2092e8712c7cf2a5
Caroline Hagan <info@blueocto.co.uk>
2017-02-03 21:31:54 +0000

    Completed “buy-or-sell-a-business > buying > buy-an-existing-business”

---
+++ buy-or-sell-a-business/buying/buy-an-existing-business.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Buy an existing business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+		<h1>Buy an existing business</h1>
+		
+		<h2>Introduction</h2>
+		<span><img src="/_img/SU0220.gif" width="94" height="62" alt="""" /></span>
+		<p>If you're thinking about running your own business, buying a company that's already established may be quicker and easier than starting from scratch.</p>
+		<p>However, you will need to put time and effort into finding the business that's right for you. Also, the costs involved in buying an existing business can be substantial and should not be underestimated.</p>
+		<p>This guide takes you through the steps of buying an existing business, including how to assess and value a business, your obligations to any existing staff and where you can get professional help.</p>
+		<ul>
+			<li><a href="/starting-up/buy-a-business/buy-an-existing-business.php">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li>Introduction</li>
+			<li><a href="/starting-up/buy-a-business/buy-an-existing-business/advantages-and-disadvantages-of-buying-an-existing-business.php">Advantages and disadvantages of buying an existing business</a></li>
+			<li><a href="/starting-up/buy-a-business/buy-an-existing-business/deciding-on-the-right-type-of-business-to-buy.php">Deciding on the right type of business to buy</a></li>
+			<li><a href="/starting-up/buy-a-business/buy-an-existing-business/where-to-look-for-a-business-to-buy.php">Where to look for a business to buy</a></li>
+			<li><a href="/starting-up/buy-a-business/buy-an-existing-business/how-to-value-a-business.php">How to value a business</a></li>
+			<li><a href="/starting-up/buy-a-business/buy-an-existing-business/make-sure-a-business-is-worth-buying-due-diligence.php">Make sure a business is worth buying: due diligence</a></li>
+			<li><a href="/starting-up/buy-a-business/buy-an-existing-business.php">Step-by-step: how to buy a business</a></li>
+			<li><a href="/starting-up/buy-a-business/buy-an-existing-business/looking-after-existing-employees.php">Looking after existing employees</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Companies House Contact Centre</h3>
+		<p>0303 1234 500</p>
+		
+		<hr />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance options for your business</a></li>
+		</ul>
+
+		<img src="/_img/px_sellingabusiness.gif" alt="" width="181" height="5" />
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
