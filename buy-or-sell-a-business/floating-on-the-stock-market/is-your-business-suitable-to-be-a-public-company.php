19b4f8460c67a168eb6a2a83d0dd8861c300e399
Caroline Hagan <info@blueocto.co.uk>
2017-02-03 17:01:22 +0000

    Completed Buy or sell a business > Floating on the stock market > Is your business suitable to be a public company?

---
+++ buy-or-sell-a-business/floating-on-the-stock-market/is-your-business-suitable-to-be-a-public-company.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Is your business suitable to be a public company? | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+		<h1>Floating on the stock market</h1>
+		<h2>Is your business suitable to be a public company?</h2>
+		<p>Before you even consider floating your business, you need to determine whether it is suitable and ready for life on a public market.</p>
+		<p>Investors will only be interested in buying shares in companies that have <strong>secure earning streams</strong> and <b>strong growth prospects</b>. They will look for a good rate of return on any investment but will require a higher rate of return from an unproven, smaller business than from a large established company to compensate for the greater risks involved.</p>
+		<p>It is harder to guarantee a successful investment in companies new to the market. Smaller companies are more likely to suffer should market or financial conditions change - making investment in them risky.</p>
+		<p>You will need to consider whether your business can deliver that rate of return. You should ask yourself whether:</p>
+		<ul>
+			<li>your business has a strong record of delivering profits and growth</li>
+			<li>your sector is attractive to investors</li>
+			<li>your business plan sets out how you will deliver strong growth in earnings in the future</li>
+			<li>your management team is up to the task of delivering the performance required</li>
+		</ul>
+		<p>If you feel your business is not at the right stage of development or will not be able to meet investor expectations of growth, you may want to consider other financing options.</p>
+		<p><a href="http://www.londonstockexchange.com/companies-and-advisors/listing/float/float.htm" target="_blank" rel="noopener noreferrer">Find out whether your business is suited to flotation on the London Stock Exchange website</a>.</p>
+
+		<ul>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market.php" title="Floating on the stock market">Prev</a></li>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market/advantages-and-disadvantages-of-stock-market-flotation.php" title="Advantages and disadvantages of stock market flotation">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market.php" title="Floating on the stock market">Introduction</a></li>
+			<li>Is your business suitable to be a public company?</li>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market/advantages-and-disadvantages-of-stock-market-flotation.php">Advantages and disadvantages of stock market flotation</a></li>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market/the-process-of-floating-on-a-stock-market.php">The process of floating on a stock market</a></li>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market/appoint-your-advisers.php">Appoint your advisers</a></li>
+			<li><a href="/finance-and-grants/finance-options/shares-and-equity-finance/here-is-how-we-prepared-our-business-for-flotation.php">Here's how we prepared our business for flotation</a></li>
+		</ul>
+		
+		<img src="../../_img/hr_325px_global.gif" alt="" />
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.londonstockexchange.com/companies-and-advisors/listing/float/float.htm" target="_blank" rel="noopener noreferrer">Companies suited to flotation explained on the London Stock Exchange website</a></li>
+			<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance options for your business</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/consider-your-exit-strategy-when-starting-up.php">Consider your exit strategy when starting up</a></li>
+			<li><a href="/finance-and-grants/finance-options/shares-and-equity-finance/equity-finance.php">Equity finance</a></li>
+			<li><a href="/finance-and-grants/finance-options/shares-and-equity-finance/shares-and-shareholders.php">Shares and shareholders</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business.php">Value and market your business</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/assess-your-options-for-growth.php">Assess your options for growth</a></li>
+		</ul>
+		
+		<img src="../../_img/px_sellingabusiness.gif" alt="" width="181" height="5" />
+	
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
