a12b32976fd2ba787e85434e7a10b4171e8dc31c
Caroline Hagan <info@blueocto.co.uk>
2017-02-03 16:55:40 +0000

    Completed Buy or sell a business > Floating on the stock market > Appoint your advisers

---
+++ buy-or-sell-a-business/floating-on-the-stock-market/appoint-your-advisers.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Appoint your advisers | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+		<h1>Floating on the stock market</h1>
+		<h2>Appoint your advisers</h2>
+		<p>Getting the right advisers is key to a successful flotation. They are the experts who will guide you through the demanding legal, regulatory, financial and marketing processes.</p>
+		<p>A corporate finance adviser will be the central adviser to your company during the admission process and throughout your company's life on a public market. A range of other advisers will also play integral roles in supporting you through the admission process and thereafter. These usually include a broker, a reporting accountant, and legal, public relations and investor relations companies.</p>
+		<p>Bad advice, or using an adviser with a poor reputation, could seriously affect your business' reputation, ability to attract investors and float successfully. Some prefer to choose companies who have worked together previously as teams.</p>
+		<p>Advisers' professional fees, which can run into six-figure sums depending on the size of the flotation, will be the main cost in floating your business.</p>
+		<h3>Adviser roles</h3>
+		<p>The corporate adviser will be responsible for guiding your company through the application and admission process. They work closely with the company and its lawyers to compile the necessary admission documents and make the formal application to the stock exchange.</p>
+		<p>Most companies seeking a flotation use a <strong>corporate adviser</strong> to guide them through the process. The corporate adviser makes the application to join the stock market on behalf of the company. They are responsible for ensuring all the information in the application is complete and dealing with any requests for additional information.</p>
+		<p>Companies floating on AIM must appoint a nominated adviser (nomad) who is approved by the London Stock Exchange to act in such a capacity. <a href="http://www.londonstockexchange.com/companies-and-advisors/aim/advisers/advisers.htm" target="_blank" rel="noopener noreferrer">Find information on advisers for AIM on the London Stock Exchange website</a>.</p>
+		<p>If you're floating on PLUS, your adviser will need to be a member of PLUS.</p>
+		<p>You'll also need a <strong>stockbroker</strong> to generate interest in your business in the investment community.</p>
+		<p>Your <strong>corporate lawyer</strong> will be responsible for the due diligence process and for verifying statements in the prospectus and other documents.</p>
+		<p>An <strong>accountant</strong> will be needed to conduct the financial due diligence and prepare any financial statements needed for the admission documents. You will also need to appoint the services of a corporate broker to seek investor interest in your business - the key reason for floating on a stock market.</p>
+		<p>For large-scale floats it's also worth considering the services of a financial public relations company to reach wider audiences of investors.</p>
+
+		<ul>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market/the-process-of-floating-on-a-stock-market.php" title="The process of floating on a stock market">Prev</a></li>
+			<li><a href="/finance-and-grants/finance-options/shares-and-equity-finance/here-is-how-we-prepared-our-business-for-flotation.php" title="Here's how we prepared our business for flotation">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market.php" title="Floating on the stock market">Introduction</a></li>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market/is-your-business-suitable-to-be-a-public-company.php">Is your business suitable to be a public company?</a></li>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market/advantages-and-disadvantages-of-stock-market-flotation.php">Advantages and disadvantages of stock market flotation</a></li>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market/the-process-of-floating-on-a-stock-market.php"  >The process of floating on a stock market</a></li>
+			<li>Appoint your advisers</li>
+			<li><a href="/finance-and-grants/finance-options/shares-and-equity-finance/here-is-how-we-prepared-our-business-for-flotation.php">Here's how we prepared our business for flotation</a></li>
+		</ul>
+
+		<img src="../../_img/hr_325px_global.gif" alt="" />
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="../../_files/london-stock-exchange_practical-guide-to-listing.pdf" target="_blank" rel="noopener noreferrer">Download guidance on the floatation process from the London Stock exchange website (PDF, 1.97MB)</a></li>
+			<li><a href="#">PLUS flotation advisers on the PLUS website</a></li>
+			<li><a href="http://www.londonstockexchange.com/companies-and-advisors/aim/advisers/advisers.htm" target="_blank" rel="noopener noreferrer">AIM adviser selection advice on the London Stock Exchange website</a></li>
+			<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance options for your business</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+			<li><a href="/finance-and-grants/expert-financial-advice/choose-and-work-with-a-solicitor.php">Choose and work with a solicitor</a></li>
+			<li><a href="/finance-and-grants/finance-options/shares-and-equity-finance/equity-finance.php">Equity finance</a></li>
+			<li><a href="/finance-and-grants/finance-options/shares-and-equity-finance/shares-and-shareholders.php">Shares and shareholders</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business.php">Value and market your business</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/assess-your-options-for-growth.php">Assess your options for growth</a></li>
+		</ul>
+		
+		<img src="../../_img/px_sellingabusiness.gif" alt="" width="181" height="5" />
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
