43d338eb4c50271ff4d516276b44db4f837ba284
Caroline Hagan <info@blueocto.co.uk>
2017-02-03 21:53:07 +0000

    Completed “buy-or-sell-a-business > franchising > buy-a-franchise”

---
+++ buy-or-sell-a-business/franchising/buy-a-franchise.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Buy a franchise | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Buy a franchise</h1>
+		
+		<h2>Introduction</h2>
+		<img src="../../_img/SU0830.gif" width="94" height="62" alt="" />
+		<p>Taking on a franchise is an option worth considering for anyone who wants to <b>run a business</b> but doesn't have a specific business idea or prefers the security provided by an established concept.</p>
+		<p>The right franchise can give you a head start. Instead of setting up a business from scratch, you <strong>use a proven business idea</strong>. Typically, you trade under the brand name of the business offering you the franchise, and they give you help and support.</p>
+		<p>Successful franchises have a much lower failure rate than completely new businesses. However, you will still need to work hard to make the franchise a success and you may have to sacrifice some of your own business ideas to fit in with the franchisor's terms.</p>
+		<p>This guide will help you decide whether franchising is for you. It shows how you can find the right franchise, and highlights the key issues you need to consider.</p>
+		
+		<ul>
+			<li><a href="/starting-up/buy-a-business/buy-a-franchise/what-is-franchising.php" title="What is franchising?">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li>Introduction</li>
+			<li><a href="/starting-up/buy-a-business/buy-a-franchise/what-is-franchising.php">What is franchising?</a></li>
+			<li><a href="/starting-up/buy-a-business/buy-a-franchise/advantages-and-disadvantages-of-franchising.php">Advantages and disadvantages of franchising</a></li>
+			<li><a href="/starting-up/buy-a-business/buy-a-franchise/should-I-buy-a-franchise.php">Should I buy a franchise?</a></li>
+			<li><a href="/starting-up/buy-a-business/buy-a-franchise/find-out-about-possible-franchises.php">Find out about possible franchises</a></li>
+			<li><a href="/starting-up/buy-a-business/buy-a-franchise/assess-a-franchise-opportunity.php">Assess a franchise opportunity</a></li>
+			<li><a href="/starting-up/buy-a-business/buy-a-franchise/the-costs-of-a-franchise.php">The costs of a franchise</a></li>
+			<li><a href="/starting-up/buy-a-business/buy-a-franchise/how-to-purchase-a-franchise.php">How to purchase a franchise</a></li>
+			<li><a href="/starting-up/buy-a-business/buy-a-franchise/tips-on-franchise-agreements.php">Tips on franchise agreements</a></li>
+			<li><a href="/starting-up/buy-a-business/buy-a-franchise/heres-how-I-selected-and-bought-my-franchise.php">Here's how I selected and bought my franchise</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.thebfa.org" target="_blank" rel="noopener noreferrer">Franchising explained on the British Franchise Association (BFA) website</a></li>
+			<li><a href="http://www.whichfranchise.com/resources.cfm" target="_blank" rel="noopener noreferrer">Franchising information on the whichfranchise.com website</a></li>
+			<li><a href="http://www.thebfa.org/join-a-franchise/how-to-choose-a-franchise" target="_blank" rel="noopener noreferrer">Choosing the right franchise guidance on the BFA website</a></li>
+			<li><a href="http://www.theukfranchisedirectory.net" target="_blank" rel="noopener noreferrer">UK franchise opportunities on the UK Franchise Directory website</a></li>
+		</ul>
+
+		<img src="/_img/px_sellingabusiness.gif" alt="" width="181" height="5" />
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
