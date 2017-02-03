bfc64c2d200e1d8cae6598d8d72fa259c8868791
Caroline Hagan <info@blueocto.co.uk>
2017-02-03 20:28:48 +0000

    Completed “buy-or-sell-a-business > considering-selling-a-business > consider-your-exit-strategy-when-starting-up”

--- buy-or-sell-a-business/considering-selling-a-business/consider-your-exit-strategy-when-starting-up.php
+++ buy-or-sell-a-business/considering-selling-a-business/consider-your-exit-strategy-when-starting-up.php
@@ -15,50 +15,40 @@
 	
 		<h1>Consider your exit strategy when starting up</h1>
 		<h2>Introduction</h2>
-		<span><img src="../../Starting_a_Business_images/SU0360.gif" width="78" height="118"  alt="" /></span>
+		<img src="../../_img/SU0360.gif" width="78" height="118"  alt="" />
 		<p>When you're setting up your business it's essential to think about how you'll ultimately end your involvement with it.</p>
-		<p>A well thought-out exit strategy can help you to maximise the value you get from your business, successfully market your business to potential buyers or investors&nbsp;and ensure you end your involvement with as little disruption to the business as possible.</p>
-		<p>Regardless of whether your exit occurs&nbsp;to a planned&nbsp;schedule or you are forced to make a move for&nbsp;unexpected reasons, the decisions you make when setting up can affect how easy it is for you to eventually exit your business.</p>
+		<p>A well thought-out exit strategy can help you to maximise the value you get from your business, successfully market your business to potential buyers or investors and ensure you end your involvement with as little disruption to the business as possible.</p>
+		<p>Regardless of whether your exit occurs to a planned schedule or you are forced to make a move for unexpected reasons, the decisions you make when setting up can affect how easy it is for you to eventually exit your business.</p>
 		<p>This guide provides an overview of how some decisions can affect your ability to exit the business successfully, and shows you how to prepare and manage your business to maximise its value. It also covers the different exit options available and outlines their advantages and disadvantages.</p>
 
 		<ul>
-			<li><a href="detail446e.html?itemId=1073792644&amp;type=RESOURCES" title="Why you need an exit strategy" >Next</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/consider-your-exit-strategy-when-starting-up/why-you-need-an-exit-strategy.php" title="Why you need an exit strategy">Next</a></li>
 		</ul>
 
 		<h3>Subjects covered in this guide</h3>
 		<ul>
 			<li>Introduction</li>
-			<li><a href="detail446e.html?itemId=1073792644&amp;type=RESOURCES"  >Why you need an exit strategy</a></li>
-			<li><a href="detail9395.html?itemId=1073792645&amp;type=RESOURCES"  >What do you want from your business?</a></li>
-			<li><a href="detaile7aa.html?itemId=1073792646&amp;type=RESOURCES"  >Decisions that could affect your eventual exit</a></li>
-			<li><a href="detail630a.html?itemId=1073792647&amp;type=RESOURCES"  >Exit option: family succession</a></li>
-			<li><a href="detail8cf1.html?itemId=1073792648&amp;type=RESOURCES"  >Exit option: selling your business</a></li>
-			<li><a href="detail9e14.html?itemId=1074480582&amp;type=RESOURCES"  >Exit option: float your business</a></li>
-			<li><a href="detaild675-2.html?itemId=1074480630&amp;type=RESOURCES"  >Exit option: close your business</a></li>
-			<li><a href="detail58c6.html?itemId=1075191784&amp;type=RESOURCES"  >The exit process</a></li>
-			<li><a href="detailf6b0.html?itemId=1075378170&amp;type=CASE STUDIES"  >Here's how I planned an exit strategy</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/consider-your-exit-strategy-when-starting-up/why-you-need-an-exit-strategy.php">Why you need an exit strategy</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/consider-your-exit-strategy-when-starting-up/what-do-you-want-from-your-business.php">What do you want from your business?</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/consider-your-exit-strategy-when-starting-up/decisions-that-could-affect-your-eventual-exit.php">Decisions that could affect your eventual exit</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/consider-your-exit-strategy-when-starting-up/exit-option-family-succession.php">Exit option: family succession</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/consider-your-exit-strategy-when-starting-up/exit-option-selling-your-business.php">Exit option: selling your business</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/consider-your-exit-strategy-when-starting-up/exit-option-float-your-business.php">Exit option: float your business</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/consider-your-exit-strategy-when-starting-up/exit-option-close-your-business.php">Exit option: close your business</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/consider-your-exit-strategy-when-starting-up/the-exit-process.php">The exit process</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/consider-your-exit-strategy-when-starting-up/here-is-how-I-planned-an-exit-strategy.php">Here's how I planned an exit strategy</a></li>
 		</ul>
-		<p><img src="../../syntegra/images/hr_325px_global.gif" alt="" /></p>
-		<strong>Print options</strong> - <span><a href="http://online.businesslink.gov.uk/bdotg/action/openpopup?flag=p&amp;itemId=1075331869&amp;r.i=1073792216&amp;r.t=RESOURCES&amp;type=ONEOFFPAGE" title="What are my print options? - Opens in a new window" target="bgPopup" >What are my print options <img src="../../Horizontal_Services_images/whatisthis.gif" width="9" height="11"  alt="" /><span> - Opens in a new window</span></a></span>
-		<ul>
-		<noscript>
-			<li><a href="http://online.businesslink.gov.uk/bdotg/action/printguide?topicId=1074039237" title="Print guide now  - Opens in a new window" target="bgPrintable">Print guide now <span> - Opens in a new window</span></a></li>
-		</noscript>
-			<li><a href="http://online.businesslink.gov.uk/bdotg/action/addGuide?refPage=%2Fbdotg%2Faction%2Flayer%3FtopicId%3D1074039237&amp;topicId=1074039237"  title="" >Add guide to my print portfolio (print later as PDF)</a></li>
-		</ul>
-		<strong>Email options</strong> - <span><a href="http://online.businesslink.gov.uk/bdotg/action/openpopup?flag=e&amp;itemId=1075331868&amp;r.i=1073792216&amp;r.t=RESOURCES&amp;type=ONEOFFPAGE" title="What are my email options? - Opens in a new window" target="bgPopup" >What are my email options <img src="../../Horizontal_Services_images/whatisthis.gif" width="9" height="11"  alt="" /><span> - Opens in a new window</span></a></span>
-		<ul>
-			<li><a href="http://online.businesslink.gov.uk/bdotg/action/emailafriend?refPage=%2Fbdotg%2Faction%2Flayer%3FtopicId%3D1074039237&amp;topicId=1074039237" title="Tell a friend about this page" >Tell a friend about this page</a></li>
-			<li><a href="https://online.businesslink.gov.uk/bdotg/action/subscribeAlert?alertType=TOPIC&amp;refPage=%2Fbdotg%2Faction%2Flayer%3FtopicId%3D1074039237&amp;topicId=1074039237" title="Send me an email alert when this guide changes" >Send me an email alert when this guide changes</a></li>
-		</ul>
+				
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
 
 		<h3>Actions</h3>
 		<ul>
-			<li><a href="http://www.icaew.co.uk/enterprise/db/pdf/cf5exits.pdf" target="bgExternalonlinebusinesslinkgovuk"  title="Short pdf guide to planning your exit from your business from the ICAEW" >Download exit planning guidance from the Institute of Chartered Accountants in England and Wales website (PDF, 156K)<span> - Opens in a new window</span></a></li>
-			<li><a href="http://www.lawdonut.co.uk/law/exit-strategies/selling-your-business/valuing-your-business" target="bgExternalonlinebusinesslinkgovuk"  title="Practical advice on the various methods of valuing your business" >Exit strategy guidance on the Law Donut website<span> - Opens in a new window</span></a></li>
-			<li><a href="layera816.html?topicId=1073877791" title="Interactive tool to guide you through the main tax, VAT, employment and licensing implications when you sell your business" >Use our interactive tool to investigate the tax and legal issues when selling or closing your business</a></li>
+			<li><a href="../../_files/icaew_planning-your-exit-from-your-business.pdf" target="_blank" rel="noopener noreferrer">Download exit planning guidance from the Institute of Chartered Accountants in England and Wales website (PDF, 156K)</a></li>
+			<li><a href="http://www.startupdonut.co.uk/blog/11/07/what%E2%80%99s-your-exit-strategy" target="_blank" rel="noopener noreferrer">Exit strategy guidance on the Law Donut website</a></li>
+			<li><a href="/tax-payroll-and-company-information/selling-or-closing-your-business/check-tax-and-legal-issues-when-selling-a-business.php">Use our interactive tool to investigate the tax and legal issues when selling or closing your business</a></li>
 		</ul>
-		<img src="../../syntegra/images/px_sellingabusiness.gif" alt="" width="181" height="5" />
+		
+		<img src="../../_img/px_sellingabusiness.gif" alt="" width="181" height="5" />
 
 	</div><!-- // .small-12 large-8 large-push-4 columns -->
 
