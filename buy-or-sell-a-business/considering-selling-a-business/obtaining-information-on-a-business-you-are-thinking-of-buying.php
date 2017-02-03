174b63aeedd1ca02ff43525e8c1fdea4dee13edf
Caroline Hagan <info@blueocto.co.uk>
2017-02-03 20:48:37 +0000

    Completed “buy-or-sell-a-business > considering-selling-a-business > obtaining-information-on-a-business-you-are-thinking-of-buying”

--- buy-or-sell-a-business/considering-selling-a-business/obtaining-information-on-a-business-you-are-thinking-of-buying.php
+++ buy-or-sell-a-business/considering-selling-a-business/obtaining-information-on-a-business-you-are-thinking-of-buying.php
@@ -16,39 +16,28 @@
 		<h1>Obtaining information on a business you are thinking of buying</h1>
 		
 		<h2>Introduction</h2>
-		<span><img src="../../Starting_a_Business_images/info_on_business.gif" width="94" height="62"  alt="" /></span>
+		<span><img src="../../_img/info_on_business.gif" width="94" height="62"  alt="" /></span>
 		<p>Buying an established business is a good option for some aspiring entrepreneurs. Much of the groundwork for success may have already been done by the existing owners.</p>
-		<p>Before making an offer there are some important things to investigate. Initially you&nbsp;may only have access to the business' sales memorandum. Sellers will usually ask you to sign a confidentiality undertaking or non-disclosure agreement before you can access sensitive or detailed information. They will usually also ask for details about you (via a CV) and evidence of your ability to fund a purchase.</p>
+		<p>Before making an offer there are some important things to investigate. Initially you may only have access to the business' sales memorandum. Sellers will usually ask you to sign a confidentiality undertaking or non-disclosure agreement before you can access sensitive or detailed information. They will usually also ask for details about you (via a CV) and evidence of your ability to fund a purchase.</p>
 		<p>After an offer is made and accepted, due diligence is carried out. This is where the buyer looks at the business in detail, including its finances, its employees, outstanding litigation, major contracts, IT and other technology.</p>
 		<p>It may sound complex, but professionals such as accountants, solicitors, chartered surveyors, business transfer agents, business brokers and corporate financiers can help. You can also do some of the research yourself.</p>
 
 		<ul>
-			<li><a href="detail2113.html?itemId=1081380310&amp;type=RESOURCES" title="Assess a company's assets" >Next</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/obtaining-information-on-a-business-you-are-thinking-of-buying/assess-a-companys-assets.php" title="Assess a company's assets">Next</a></li>
 		</ul>
 
 		<h3>Subjects covered in this guide</h3>
 		<ul>
 			<li>Introduction</li>
-			<li><a href="detail2113.html?itemId=1081380310&amp;type=RESOURCES"  >Assess a company's assets</a></li>
-			<li><a href="detaild17b.html?itemId=1081380144&amp;type=RESOURCES"  >Legal and financial checks</a></li>
-			<li><a href="detail4054.html?itemId=1081380259&amp;type=RESOURCES"  >Employment, IT and environment checks</a></li>
-			<li><a href="detail1162.html?itemId=1081380887&amp;type=RESOURCES"  >Operating within the law</a></li>
-			<li><a href="detailaf5a.html?itemId=1081380927&amp;type=RESOURCES"  >Where to get help and advice</a></li>
-			<li><a href="detaile81c.html?itemId=1081894811&amp;type=CASE STUDIES"  >Here's how I obtained the right information about a business before buying it</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/obtaining-information-on-a-business-you-are-thinking-of-buying/assess-a-companys-assets.php">Assess a company's assets</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/obtaining-information-on-a-business-you-are-thinking-of-buying/legal-and-financial-checks.php">Legal and financial checks</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/obtaining-information-on-a-business-you-are-thinking-of-buying/employment-it-and-environment-checks.php">Employment, IT and environment checks</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/obtaining-information-on-a-business-you-are-thinking-of-buying/operating-within-the-law.php">Operating within the law</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/obtaining-information-on-a-business-you-are-thinking-of-buying/where-to-get-help-and-advice.php">Where to get help and advice</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/here-is-how-i-obtained-the-right-information-about-a-business-before-buying-it.php">Here's how I obtained the right information about a business before buying it</a></li>
 		</ul>
-		<p><img src="../../syntegra/images/hr_325px_global.gif" alt="" /></p>
-		<strong>Print options</strong> - <span><a href="http://online.businesslink.gov.uk/bdotg/action/openpopup?flag=p&amp;itemId=1075331869&amp;r.i=1079422479&amp;r.t=RESOURCES&amp;type=ONEOFFPAGE" title="What are my print options? - Opens in a new window" target="bgPopup" >What are my print options <img src="../../Horizontal_Services_images/whatisthis.gif" width="9" height="11"  alt="" /><span> - Opens in a new window</span></a></span>
-		<ul>
-		<noscript>
-			<li><a href="http://online.businesslink.gov.uk/bdotg/action/printguide?topicId=1081372417" title="Print guide now  - Opens in a new window" target="bgPrintable">Print guide now <span> - Opens in a new window</span></a></li>
-		</noscript>
-			<li><a href="http://online.businesslink.gov.uk/bdotg/action/addGuide?refPage=%2Fbdotg%2Faction%2Flayer%3FtopicId%3D1081372417&amp;topicId=1081372417"  title="" >Add guide to my print portfolio (print later as PDF)</a></li>
-		</ul>
-		<strong>Email options</strong> - <span><a href="http://online.businesslink.gov.uk/bdotg/action/openpopup?flag=e&amp;itemId=1075331868&amp;r.i=1079422479&amp;r.t=RESOURCES&amp;type=ONEOFFPAGE" title="What are my email options? - Opens in a new window" target="bgPopup" >What are my email options <img src="../../Horizontal_Services_images/whatisthis.gif" width="9" height="11"  alt="" /><span> - Opens in a new window</span></a></span>
-		<ul>
-			<li><a href="http://online.businesslink.gov.uk/bdotg/action/emailafriend?refPage=%2Fbdotg%2Faction%2Flayer%3FtopicId%3D1081372417&amp;topicId=1081372417" title="Tell a friend about this page" >Tell a friend about this page</a></li>
-			<li><a href="https://online.businesslink.gov.uk/bdotg/action/subscribeAlert?alertType=TOPIC&amp;refPage=%2Fbdotg%2Faction%2Flayer%3FtopicId%3D1081372417&amp;topicId=1081372417" title="Send me an email alert when this guide changes" >Send me an email alert when this guide changes</a></li>
-		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
 
 		<h3>Companies House Contact Centre - International</h3>
 		<p>0303 1234 500<br/>
@@ -65,15 +54,15 @@
 		<h3>Intellectual Property Office Information Centre</h3>
 		<p>0300 300 2000<br/>
 		</p>
-		<p><img src="../../syntegra/images/hr_140px_global.gif" alt="" /></p>
 
 		<h3>Actions</h3>
 		<ul>
-			<li><a href="http://www.businessesforsale.com/" target="bgExternalonlinebusinesslinkgovuk"  title="Searchable database of businesses for sale" >Businesses for sale on the BusinessesForSale website<span> - Opens in a new window</span></a></li>
-			<li><a href="http://www.daltonsbusiness.com/buy/business-for-sale-listing" target="bgExternalonlinebusinesslinkgovuk"  title="Lists of businesses and franchises currently available for sale in the UK and abroad" >Valuation advice and businesses for sale on the Daltons Business website<span> - Opens in a new window</span></a></li>
-			<li><a href="http://www.companieshouse.gov.uk/toolsToHelp/chdDirectInfo.shtml" target="bgExternalonlinebusinesslinkgovuk"  title="Companies House information on over 1.8 million registered companies and their directors" >Detailed company information on the Companies House website<span> - Opens in a new window</span></a></li>
+			<li><a href="http://www.businessesforsale.com/" target="_blank" rel="noopener noreferrer">Businesses for sale on the BusinessesForSale website</a></li>
+			<li><a href="http://www.daltonsbusiness.com/buy/business-for-sale-listing" target="_blank" rel="noopener noreferrer">Valuation advice and businesses for sale on the Daltons Business website</a></li>
+			<li><a href="https://www.gov.uk/government/organisations/companies-house/about/about-our-services#companies-house-direct-chd" target="_blank" rel="noopener noreferrer">Detailed company information on the Companies House website</a></li>
 		</ul>
-		<img src="../../syntegra/images/px_sellingabusiness.gif" alt="" width="181" height="5" />
+		
+		<img src="../../_img/px_sellingabusiness.gif" alt="" width="181" height="5" />
 
 	</div><!-- // .small-12 large-8 large-push-4 columns -->
 
