62ebafcf15dc7a00067b19cdeddc1e290e52af3c
Caroline Hagan <info@blueocto.co.uk>
2017-02-05 17:30:15 +0000

    Completed “starting-up > business-structures > legal-structures-the-basics > european-economic-interest-groupings”

---
+++ starting-up/business-structures/legal-structures-the-basics/european-economic-interest-groupings.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>European Economic Interest Groupings | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+
+			<h1>Legal structures: the basics</h1>
+			<h2>European Economic Interest Groupings</h2>
+			<p>A European Economic Interest Grouping (EEIG) is an association of businesses and/or individuals from different European Union (EU) countries who need to operate together across national borders.</p>
+			<p>Its aim is to facilitate or develop the economic activities of its members but is a separate entity from their own businesses.</p>
+			<p>An EEIG may be set up in any one of the Member States, and operate in any part of the EU. It can also enter into arrangements with organisations outside the EU, although these organisations cannot themselves become members of an EEIG.</p>
+			<p>An EEIG must be registered in the EU Member State where it has its 'official address'. The official address must be where either:</p>
+			<ul>
+				<li>the EEIG has its central administration</li>
+				<li>one of its members has its central administration or - in the case of an individual who is a member - their principal activity, provided that the grouping carries on an activity there</li>
+			</ul>
+			<p>You must register with Companies House if:</p>
+			<ul>
+				<li>it's an new EEIG and its central administration is going to be in the UK</li>
+				<li>you want to transfer an existing EEIG's central administration to the UK</li>
+				<li>you want to set up an establishment for an existing EEIG in the UK - even if its central administration will remain elsewhere in the EU</li>
+			</ul>
+			<p>Registration with Companies House is via paper application only.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/setting-up-specific-types-of-limited-company.php" title="Setting up specific types of limited company">Prev</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/uk-establishments-of-overseas-companies.php" title="UK establishments of overseas companies">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php" title="Legal structures: the basics">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/becoming-a-sole-trader.php">Becoming a sole trader</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/partnerships.php">Partnerships</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/starting-a-private-company.php">Starting a private company</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/starting-a-public-limited-company.php">Starting a public limited company</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/setting-up-specific-types-of-limited-company.php">Setting up specific types of limited company</a></li>
+				<li>European Economic Interest Groupings</li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/uk-establishments-of-overseas-companies.php">UK establishments of overseas companies</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/buying-a-franchise.php">Buying a franchise</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+			
+			<img src="../../../_img/hr_140px_global.gif" alt="" />
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/publications/european-economic-interest-groupings-eeigs" target="_blank" rel="noopener noreferrer">EEIG guidance from Companies House</a></li>
+				<li><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Solicitor search with the Law Society</a></li>
+				<li><a href="https://www.icaew.com/" target="_blank" rel="noopener noreferrer">Accountant search from the Institute of Chartered Accountants in England and Wales</a></li>
+				<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+			
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
