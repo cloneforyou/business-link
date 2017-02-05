4fdda9ee01937090a34022f9b04e1e69a3ced32d
Caroline Hagan <info@blueocto.co.uk>
2017-02-05 20:51:10 +0000

    Completed “starting-up > business-structures > set-up-a-social-enterprise > charitable-incorporated-organisations”

---
+++ starting-up/business-structures/set-up-a-social-enterprise/charitable-incorporated-organisations.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Charitable incorporated organisations | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+
+			<h1>Set up a social enterprise</h1>
+			<h2>Charitable incorporated organisations</h2>
+			<p>The new legal structure known as the charitable incorporated organisation (CIO) will be available to new organisations and existing charities that wish to convert into the CIO form.</p>
+			<h3>Converting to a CIO </h3>
+			<p>Under certain conditions, the following organisations may be able to apply for conversion to a CIO through a special conversion process:</p>
+			<ul>
+				<li>an existing company which is registered as a charity</li>
+				<li>a community interest company</li>
+			</ul>
+			<p>In future a conversion process may be established for charities which are regulated industrial and provident societies.</p>
+			<p>Other forms of charity will be able to convert into a CIO by setting up a new CIO, transferring the charity's assets to it, and then winding up the old charity.</p>
+			<p>Exempt charities and companies or regulated societies with a share capital - where any of the shares are not fully paid up - will not be able to apply for conversion to a CIO. <a href="https://www.gov.uk/topic/running-charity" target="_blank" rel="noopener noreferrer">Read about exempt charities on the Charity Commission website</a>. </p>
+			
+			<h3>Special features of CIOs </h3>
+			<p>CIOs will differ from other charity forms in the following ways:</p>
+			<ul>
+				<li>Although CIOs will not use company terminology - directors will be called charity trustees - they will be more like company forms than other types of charity forms.</li>
+				<li>As CIOs are always incorporated, they will be separate legal entities and their members will have either no liability or limited liability.</li>
+				<li>CIOs - like Community Interest Companies and community benefit societies - will have their assets locked in for their charitable purposes. They will not be able to distribute profits or assets to their members. See the pages in this guide on <a href="/starting-up/business-structures/set-up-a-social-enterprise.php">Community Interest Companies</a> and <a href="/starting-up/business-structures/set-up-a-social-enterprise/community-benefit-societies.php">community benefit societies</a>.</li>
+				<li>Unlike some of the other forms used by charities, CIOs will only register with and report to the Charity Commission, not to Companies House or the Financial Services Authority.</li>
+				<li>A choice of formats and administration will be available to suit organisations of all sizes, with or without a membership structure.</li>
+			</ul>
+
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php" title="Community Interest Companies">Prev</a></li>
+				<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-being-a-social-enterprise-works-for-our-business.php" title="Here's how being a social enterprise works for our business">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php" title="Set up a social enterprise">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/unincorporated-associations.php">Unincorporated associations</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/trusts.php">Trusts</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/limited-companies-with-a-social-purpose.php">Limited companies with a social purpose</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/community-benefit-societies.php">Community benefit societies</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/social-enterprises-as-registered-charities.php">Social enterprises as registered charities</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php">Community Interest Companies</a></li>
+				<li>Charitable incorporated organisations</li>
+				<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-being-a-social-enterprise-works-for-our-business.php">Here's how being a social enterprise works for our business</a></li>
+			</ul>
+
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="../../../_files/LegalStructures.doc" target="_blank" rel="noopener noreferrer">Download our at-a-glance guide to legal structures for social enterprises (DOC, 44K)</a></li>
+			</ul>
+						
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+				<li><a href="/tax-payroll-and-company-information/changes-to-your-business/change-your-business-structure.php">Change your business structure</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
