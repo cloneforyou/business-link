ae16edeb014a64e6d6287141b8ec130f068e1822
Caroline Hagan <info@blueocto.co.uk>
2017-02-05 20:58:43 +0000

    Completed “starting-up > business-structures > set-up-a-social-enterprise > trusts”

---
+++ starting-up/business-structures/set-up-a-social-enterprise/trusts.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Trusts | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+
+			<h1>Set up a social enterprise</h1>
+			<h2>Trusts</h2>
+			<p>A trust is a legal device which governs how assets given by an individual or organisation are to be used. Many organisations, such as those involved in education, healthcare, providing advice and conservation, can be structured as trusts.</p>
+			
+			<h3>How trusts work</h3>
+			<p>Trusts are unincorporated bodies and do not distribute their profits. They're managed by trustees. Trustees do not benefit from the trust, but act on behalf of the community for whose benefit the trust is set up. Trusts make their own governing rules and have no legal identity of their own.</p>
+			<p>Trusts have a <strong>trust deed</strong> which protects their social objectives. This covers the terms under which an individual or organisation is given assets. It also lists the intended beneficiaries and the conditions under which the trust's assets may be used.</p>
+			<p>As trusts can hold property and other assets for the community, they can act as sister bodies to unincorporated associations. See the page in this guide on <a href="/starting-up/business-structures/set-up-a-social-enterprise/unincorporated-associations.php">unincorporated associations</a>.</p>
+			<p>Trusts are relatively straightforward and cost-effective to set up. As they have no separate legal identity, the trustees are personally liable for the trust's liabilities. You should seek legal advice on whether you need to register the trust's name with Companies House.</p>
+			
+			<h3>Asset locks</h3>
+			<p>Trusts may write an asset lock into their rules to <strong>secure assets for their intended community</strong>. Some other social enterprises can also do this. See the pages in this guide on <a href="/starting-up/business-structures/set-up-a-social-enterprise.php">Community Interest Companies</a>, <a href="/starting-up/business-structures/set-up-a-social-enterprise/community-benefit-societies.php">community benefit societies</a> and <a href="/starting-up/business-structures/set-up-a-social-enterprise/charitable-incorporated-organisations.php">charitable incorporated organisations</a>.</p>
+			
+			<h3>Charitable trusts</h3>
+			<p>If a trust's aims are exclusively charitable and those aims are for the public benefit, the trust should (in most cases) apply to the Charity Commission to be registered as a charity. <a href="https://www.gov.uk/topic/running-charity/setting-up" target="_blank" rel="noopener noreferrer">Read about registering as a charity on the Charity Commission website</a>.</p>
+			
+			<h3>Development trusts</h3>
+			<p>Development trusts are community owned and led organisations, and do not distribute any profits. They provide a hub of trade or service with the aim of regenerating their local community - eg to develop or manage property, restore buildings or improve the environment.</p>
+			<p>There is no standard legal form for a development trust. Most register as a company limited by guarantee and in a few cases as an industrial and provident society. Many register as charities. For the legal and financial issues, see the pages in this guide on <a href="/starting-up/business-structures/set-up-a-social-enterprise/limited-companies-with-a-social-purpose.php">limited companies with a social purpose</a> and on <a href="/starting-up/business-structures/set-up-a-social-enterprise/community-benefit-societies.php">community benefit societies</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/unincorporated-associations.php" title="Unincorporated associations">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/limited-companies-with-a-social-purpose.php" title="Limited companies with a social purpose">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php" title="Set up a social enterprise">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/unincorporated-associations.php">Unincorporated associations</a></li>
+				<li>Trusts</li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/limited-companies-with-a-social-purpose.php">Limited companies with a social purpose</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/community-benefit-societies.php">Community benefit societies</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/social-enterprises-as-registered-charities.php">Social enterprises as registered charities</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php">Community Interest Companies</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/charitable-incorporated-organisations.php">Charitable incorporated organisations</a></li>
+				<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-being-a-social-enterprise-works-for-our-business.php">Here's how being a social enterprise works for our business</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Development Trust Association</h3>
+			<p>0845 458 8336</p>
+			
+			<img src="../../../_img/hr_140px_global.gif" alt="" />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="../../../_files/LegalStructures.doc" target="_blank" rel="noopener noreferrer">Download our at-a-glance guide to legal structures for social enterprises (DOC, 43K)</a></li>
+				<li><a href="https://www.gov.uk/topic/running-charity/setting-up" target="_blank" rel="noopener noreferrer">Charity registration guidance from the Charity Commission</a></li>
+				<li><a href="https://www.ncvo.org.uk/practical-support/information/governance" target="_blank" rel="noopener noreferrer">Good governance and leadership advice from the National Council for Voluntary Organisations</a></li>
+				<li><a href="http://locality.org.uk/our-members/federation-of-northumberland-development-trusts/" target="_blank" rel="noopener noreferrer">Development trusts guidance from Development Trusts Association</a></li>
+				<li><a href="https://www.the-sse.org/about-school-for-social-entrepreneurs/" target="_blank" rel="noopener noreferrer">Social enterprises information from the Social Enterprise Coalition</a></li>
+				<li><a href="https://www.the-sse.org/our-courses/" target="_blank" rel="noopener noreferrer">Training programmes for social enterprises from the School for Social Entrepreneurs</a></li>
+			</ul>
+
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/choose-the-right-finance-when-starting-up.php">Choose the right finance when starting up</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
