f5b9b6c20cb84c53f2e38734bb085a2d80ea6a95
Caroline Hagan <info@blueocto.co.uk>
2017-02-05 20:19:38 +0000

    Completed “starting-up > business-structures > legal-structures-the-basics > uk-establishments-of-overseas-companies”

---
+++ starting-up/business-structures/legal-structures-the-basics/uk-establishments-of-overseas-companies.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>UK establishments of overseas companies | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Legal structures: the basics</h1>
+			<h2>UK establishments of overseas companies</h2>
+			<p>If you are an overseas company, you must - within one month of opening a UK establishment - register with Companies House. Registration is via paper application only.</p>
+			<p>If this is going to be your first UK establishment, you must also send Companies House:</p>
+			<ul>
+				<li>a certified copy of your company's constitutional documents, eg charter, statute, memorandum and articles of association, with a certified translation in English if the original is in a language other than English</li>
+				<li>a copy of your company's latest set of accounts, again with a certified translation in English if the original is in a language other than English</li>
+			</ul>
+			<p>You only have to submit the accounts if either:</p>
+			<ul>
+				<li>the accounts must be filed under the law of country in which your company is incorporated - the 'parent law'</li>
+				<li>your company is incorporated in an European Economic Area state and the parent law requires you to prepare and disclose accounts but does not require such accounts to be audited or delivered</li>
+			</ul>
+			<p>If this is going to be an additional UK establishment, you don't have to deliver the constitutional documents. You may instead state in the registration document that you have delivered them in respect of another UK establishment - but you must give the registered number of that establishment.</p>
+			<p>You must notify Companies House of any changes to the original registration information. You must deliver the forms notifying the changes to Companies House within 21 days of the change.</p>
+			<p>If there is a change in the parent state that affects your company, you must deliver the notifying form to Companies House within 21 days of the date on which notice of the change could have been received by post in the UK.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/european-economic-interest-groupings.php" title="European Economic Interest Groupings">Prev</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/buying-a-franchise.php" title="Buying a franchise">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php" title="Legal structures: the basics">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/becoming-a-sole-trader.php">Becoming a sole trader</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/partnerships.php">Partnerships</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/starting-a-private-company.php">Starting a private company</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/setting-up-specific-types-of-limited-company.php">Starting a public limited company</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/setting-up-specific-types-of-limited-company.php">Setting up specific types of limited company</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/european-economic-interest-groupings.php">European Economic Interest Groupings</a></li>
+				<li>UK establishments of overseas companies</li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/buying-a-franchise.php">Buying a franchise</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+			
+			<img src="../../../_img/hr_140px_global.gif" alt="" />
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/publications/overseas-companies-in-the-uk-registration-filing-and-disclosure-obligations" target="_blank" rel="noopener noreferrer">Overseas companies guidance from Companies House</a></li>
+				<li><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Solicitor search with the Law Society</a></li>
+				<li><a href="https://www.icaew.com/" target="_blank" rel="noopener noreferrer">Accountant search from the Institute of Chartered Accountants in England and Wales</a></li>
+				<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a></li>
+			</ul>
+						
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php">Choose the right name for your business</a></li>
+				<li><a href="/tax-payroll-and-company-information/companies-house-annual-returns-and-accounts.php">Companies House annual returns and accounts</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+			
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
