49a538738ccd03ba0b54a7f147df7326559739f5
Caroline Hagan <info@blueocto.co.uk>
2017-02-05 21:23:54 +0000

    Completed “starting-up > business-structures > set-up-a-social-enterprise > social-enterprises-as-registered-charities”

---
+++ starting-up/business-structures/set-up-a-social-enterprise/social-enterprises-as-registered-charities.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Social enterprises as registered charities | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+
+			<h1>Set up a social enterprise</h1>
+			<h2>Social enterprises as registered charities</h2>
+			<p>Many social enterprises have charitable status. It is only possible to gain this status if the purposes of your organisation are exclusively charitable and are for the public benefit. Charitable purposes include advancing education or religion, and relieving financial hardship. Over many years, a host of other charitable purposes that benefit the community have been recognised as charitable by the courts or the Charity Commission.</p>
+			<h3>Organisational restrictions on charitable social enterprises</h3>
+			<ul>
+				<li>A charitable social enterprise must have exclusively charitable purposes, and those purposes must be for the public benefit.</li>
+				<li>The directors or trustees are responsible for administration and management and generally must not be paid for this work. However, charities can pay trustees (including the director) for providing goods and non-trustee or employee services to the charity. A range of safeguards are in place to prevent conflicts of interest or abuse.</li>
+				<li>Any profits or surpluses made by the organisation must be invested back into it and used to support its charitable purposes. Any profit or surplus must not be paid out to members of the charity.</li>
+				<li>Assets must always be used for the charitable purposes of the organisation.</li>
+			</ul>
+			
+			<p><a href="https://www.gov.uk/topic/running-charity" target="_blank" rel="noopener noreferrer">Read about trustee expenses and payments on the Charity Commission website</a>.</p>
+			<p><a href="https://www.gov.uk/topic/running-charity/setting-up" target="_blank" rel="noopener noreferrer">Read a guide to registering as a charity on the Charity Commission website</a>.</p>
+			<p><a href="https://www.gov.uk/government/publications/how-we-ensure-charities-meet-their-legal-requirements" target="_blank" rel="noopener noreferrer">Read about the Charities Act 2006 on the Charity Commission website</a>.</p>
+			
+			<h3>Incorporated charities</h3>
+			<p>Most charities that seek the benefits of incorporation currently incorporate as a company limited by guarantee. A new incorporated legal form designed exclusively for charities is being developed, called the <strong>charitable incorporated organisation (CIO)</strong>. See the page in this guide on <a href="/starting-up/business-structures/set-up-a-social-enterprise/charitable-incorporated-organisations.php">charitable incorporated organisations</a>.</p>
+			<p><a href="https://www.gov.uk/topic/running-charity/setting-up" target="_blank" rel="noopener noreferrer">Read about incorporating a charity on the Charity Commission website</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/community-benefit-societies.php" title="Community benefit societies">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php" title="Community Interest Companies">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php" title="Set up a social enterprise">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/unincorporated-associations.php">Unincorporated associations</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/trusts.php">Trusts</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/limited-companies-with-a-social-purpose.php">Limited companies with a social purpose</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/community-benefit-societies.php">Community benefit societies</a></li>
+				<li>Social enterprises as registered charities</li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php">Community Interest Companies</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/charitable-incorporated-organisations.php">Charitable incorporated organisations</a></li>
+				<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-being-a-social-enterprise-works-for-our-business.php">Here's how being a social enterprise works for our business</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Charity Commission Contact Centre General Enquiry Line</h3>
+			<p>0845 3000 218</p>
+			
+			<img src="../../../_img/hr_140px_global.gif" alt="" />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/topic/running-charity/setting-up" target="_blank" rel="noopener noreferrer">Charity registration documents from the Charity Commission</a></li>
+				<li><a href="https://www.gov.uk/topic/running-charity/setting-up" target="_blank" rel="noopener noreferrer">Charity registration guidance from the Charity Commission</a></li>
+				<li><a href="https://www.gov.uk/government/publications/how-we-ensure-charities-meet-their-legal-requirements" target="_blank" rel="noopener noreferrer">Charities Act 2006 information from the Charity Commission</a></li>
+				<li><a href="https://www.the-sse.org/about-school-for-social-entrepreneurs/" target="_blank" rel="noopener noreferrer">Social enterprises information from the Social Enterprise Coalition</a></li>
+			</ul>
+						
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/choose-the-right-finance-when-starting-up.php">Choose the right finance when starting up</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+				<li><a href="/finance-and-grants/expert-financial-advice/choose-and-work-with-a-solicitor.php">Choose and work with a solicitor</a></li>
+				<li><a href="/tax-payroll-and-company-information/changes-to-your-business/change-your-business-structure.php">Change your business structure</a></li>
+				<li><a href="/tax-payroll-and-company-information/companies-house-annual-returns-and-accounts/companies-house-annual-return.php">Companies House annual return</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
