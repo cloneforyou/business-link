1361a5dac1157919a07e39280deb32d7fd61e9cd
Caroline Hagan <info@blueocto.co.uk>
2017-02-05 20:51:42 +0000

    Completed “starting-up > business-structures > set-up-a-social-enterprise > unincorporated-associations”

---
+++ starting-up/business-structures/set-up-a-social-enterprise/unincorporated-associations.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Unincorporated associations | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+
+			<h1>Set up a social enterprise</h1>
+			<h2>Unincorporated associations</h2>
+			<p>The unincorporated association form is usually chosen when a number of individuals agree or 'contract' to come together for a common purpose - which may be of a social nature.</p>
+			
+			<h3>How unincorporated associations operate</h3>
+			<p>Unincorporated associations are relatively straightforward to run and cost nothing to set up. They make their own rules for running the organisation and set these down in a democratic constitution. A management committee is elected to run the organisation on behalf of the members (if it has any).</p>
+			<p>Unincorporated associations do not need to register with or be regulated by either Companies House or the Financial Services Authority. They enjoy greater freedom of operation than a company. For example, they don't have to submit annual returns.</p>
+			<p>If an unincorporated association's objects are exclusively charitable<strong> </strong>and those objects are for the public benefit, the association should apply to the Charity Commission to be registered as a charity. All charities must follow the requirements of charity law, and most registered charities must also submit annual returns to the Charity Commission. <a href="https://www.gov.uk/topic/running-charity/setting-up" target="_blank" rel="noopener noreferrer">Read frequently asked questions on registering a charity on the Charity Commission website</a>.</p>
+			<p>Unincorporated associations may also have trading or business objectives or carry on commercial activities.</p>
+			<p>Although an unincorporated association cannot own property, it may be able to set up a trust to legally hold property and assets for the community they are intended to benefit. See the page in this guide on <a href="/starting-up/business-structures/set-up-a-social-enterprise/trusts.php">trusts</a>.</p>
+			
+			<h3>Unincorporated associations and personal risk</h3>
+			<p>Unincorporated associations have <strong>no separate legal identity</strong>. This means that their members will have to sign loans and contracts as individuals and carry the risk of personal liability.</p>
+			<p>This form is unlikely to offer a long-term solution if you intend to sign contracts or expand the enterprise. You should consider incorporation if you intend to:</p>
+			<ul>
+				<li>take on employees</li>
+				<li>raise finance, apply for grants or open bank accounts</li>
+				<li>issue shares</li>
+				<li>enter into large contracts</li>
+				<li>take on a lease or buy freehold property</li>
+			</ul>
+			<p>This should help you to gain access to a wide range of financing sources that will not put your personal assets at risk.</p>
+			<p><a href="https://www.ncvo.org.uk/practical-support/information/governance" target="_blank" rel="noopener noreferrer">Read about good governance and leadership on the National Council for Voluntary Organisations (NCVO) website</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php" title="Set up a social enterprise">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/trusts.php" title="Trusts">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php" title="Set up a social enterprise">Introduction</a></li>
+				<li>Unincorporated associations</li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/trusts.php">Trusts</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/limited-companies-with-a-social-purpose.php">Limited companies with a social purpose</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/community-benefit-societies.php">Community benefit societies</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/social-enterprises-as-registered-charities.php">Social enterprises as registered charities</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php">Community Interest Companies</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/charitable-incorporated-organisations.php">Charitable incorporated organisations</a></li>
+				<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-being-a-social-enterprise-works-for-our-business.php">Here's how being a social enterprise works for our business</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="../../../_files/LegalStructures.doc" target="_blank" rel="noopener noreferrer">Download our at-a-glance guide to legal structures for social enterprises (DOC, 44K)</a></li>
+				<li><a href="https://www.ncvo.org.uk/practical-support/information/governance" target="_blank" rel="noopener noreferrer">Good governance and leadership advice from NCVO</a></li>
+				<li><a href="https://www.gov.uk/topic/running-charity" target="_blank" rel="noopener noreferrer">Charitable objects guidance from the Charity Commission</a></li>
+				<li><a href="https://www.gov.uk/hmrc-internal-manuals/company-taxation-manual/ctm41305" target="_blank" rel="noopener noreferrer">Legal definition of unincorporated association from HM Revenue &amp; Customs</a></li>
+				<li><a href="http://responsiblefinance.org.uk/funding-and-development/funding-for-your-cdfi/" target="_blank" rel="noopener noreferrer">Funding information from the Community Development Finance Association</a></li>
+			</ul>
+
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/corporation-tax/unincorporated-organisations-and-corporation-tax.php">Unincorporated organisations and Corporation Tax</a></li>
+				<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/choose-the-right-finance-when-starting-up.php">Choose the right finance when starting up</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+				<li><a href="/tax-payroll-and-company-information/changes-to-your-business/change-your-business-structure.php" title="You may want to switch from a partnership to a limited company, or the other way round - find out the implications">Change your business structure</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+			<table height="100%" border="0" width="100%">
+				<tbody>
+					<tr>
+						<td>
+							<a href="http://www.berr.gov.uk/" target="bgExternalwwwbusinesslinkgovuk" onclick="p=window.open('http://www.berr.gov.uk/', 'bgExternalwwwbusinesslinkgovuk');p.focus();" title="Department for Business, Innovation & Skills - Opens in a new window"><img src="../../Taxes_and_Returns_images/logo_primary_bis.gif" width="160" height="32" border="0" alt="" />
+								<br/>Department for Business, Innovation &amp; Skills</a>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<a href="https://online.businesslink.gov.uk/bdotg/action/userSupport?refPage=%2Fbdotg%2Faction%2Fdetail%3FitemId%3D1077475675%26type%3DRESOURCES" title="Help &amp; support">Help &amp; support</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Copyright" title="&copy; Crown copyright 2011">Copyright</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Disclaimer" title="Terms &amp; conditions">Terms &amp; conditions</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Privacy" title="Privacy & cookies">Privacy & cookies</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility" title="Accessibility">Accessibility</a> | <a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=HelpWithPDFFiles" title="PDF help">PDF help</a>
+			<ul>
+				<li><a href="http://www.businesslink.gov.uk/bdotg/action/fullsitemap" title="Site map">Site map</a></li>
+				<li><a href="http://www.businesslink.gov.uk/bdotg/action/help?page=HelpPage" title="Help">Help</a></li>
+				<li><a href="detail01b3.html?itemId=1087351667&amp;page=AboutUs&amp;r.i=1077475675&amp;r.s=h&amp;r.t=RESOURCES&amp;type=CAMPAIGN" title="About us">About us</a></li>
+				<li><a href="http://www.businesslink.gov.uk/bdotg/action/staticpage?page=Accessibility">Accessibility</a></li>
+			</ul>
+			<table border="0" cellpadding="0" cellspacing="0" width="100%">
+				<tr>
+					<td rowspan="14"><img src="../../_img/px_white.gif" alt="" width="5" height="6" /></td>
+					<td colspan="2">
+						<p>
+							<a href="layer068f.html?topicId=1073858805" title="From first thoughts to a firm footing">Starting up</a>
+						</p>
+						<p><img src="../../_img/leftnav_sub_hr.gif" alt="" /></p>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2">
+						<p><a href="layer1e2d.html?topicId=1085161962">Business structures</a>
+						</p>
+						<p><img src="../../_img/leftnav_sub_hr.gif" alt="" /></p>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<p><strong>Set up a social enterprise</strong></p>
+					</td>
+				</tr>
+				<tr>
+					<td width="10"> </td>
+					<td>
+						<p><img src="../../_img/leftnav_sub_sub_hr.gif" alt="" /></p>
+					</td>
+				</tr>
+				<tr>
+					<td width="10"> </td>
+					<td>
+						<p><a href="/starting-up/business-structures/set-up-a-social-enterprise.php" title="The various types of social enterprise and how you can set one up, with links to forms and further information">Introduction</a>
+						</p>
+					</td>
+				</tr>
+				<tr>
+					<td><img src="../../_img/px_startingabusiness.gif" alt="Current section" width="5" height="5" /></td>
+					<td>
+						<p><strong>Unincorporated associations</strong></p>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<p><a href="/starting-up/business-structures/set-up-a-social-enterprise/trusts.php">Trusts</a>
+						</p>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<p><a href="/starting-up/business-structures/set-up-a-social-enterprise/limited-companies-with-a-social-purpose.php">Limited companies with a social purpose</a>
+						</p>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<p><a href="/starting-up/business-structures/set-up-a-social-enterprise/community-benefit-societies.php">Community benefit societies</a>
+						</p>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<p><a href="/starting-up/business-structures/set-up-a-social-enterprise/social-enterprises-as-registered-charities.php">Social enterprises as registered charities</a>
+						</p>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<p><a href="/starting-up/business-structures/set-up-a-social-enterprise.php">Community Interest Companies</a>
+						</p>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<p><a href="/starting-up/business-structures/set-up-a-social-enterprise/charitable-incorporated-organisations.php">Charitable incorporated organisations</a>
+						</p>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<p><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-being-a-social-enterprise-works-for-our-business.php">Here's how being a social enterprise works for our business</a>
+						</p>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2"></td>
+				</tr>
+			</table>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
