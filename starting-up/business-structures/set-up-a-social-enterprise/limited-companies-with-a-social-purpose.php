55b731f9e69b4a16716b9848c87797a2a7aacd8b
Caroline Hagan <info@blueocto.co.uk>
2017-02-05 21:06:22 +0000

    Completed “starting-up > business-structures > set-up-a-social-enterprise > limited-companies-with-a-social-purpose”

---
+++ starting-up/business-structures/set-up-a-social-enterprise/limited-companies-with-a-social-purpose.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Limited companies with a social purpose | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+
+			<h1>Set up a social enterprise</h1>
+			<h2>Limited companies with a social purpose</h2>
+			<p>The limited company is an organisational structure which gives limited liability to its members. Some social enterprises take on the form of a limited company. This is a more accountable form than, for example, an unincorporated association.</p>
+			<p>Limited companies may have an 'objects' clause that sets out the company's aims or purposes. Although these objects can be commercial, if your business is <strong>a social enterprise</strong>, they must also relate to social and/or environmental objectives such as regenerating an area or providing employment and/or training for people disadvantaged in the labour market. Social enterprises that are registered charities must only have objects that the law defines as charitable, such as relieving financial hardship or promoting education.</p>
+			
+			<h3>Types of limited company</h3>
+			<p>There are two incorporated forms to choose between when setting up a social enterprise as a limited company:</p>
+			<ul>
+				<li>
+					<strong>Company limited by shares (CLS)</strong> - shareholders each hold shares in the company. Their liability is limited to the amount unpaid on shares they hold. A public limited company (plc) differs from a CLS in that its shares can be sold to the general public.
+				</li>
+				<li>
+					<strong>Company limited by guarantee (CLG)</strong> - each of the members gives a guarantee for a certain sum that will be put towards the company's finances if the company is wound up. A CLG cannot raise finance by issuing shares, nor pay dividends to its members.
+				</li>
+			</ul>
+			<p>The issues are complex and you should seek professional advice. See our guide on how to <a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public.php">set up and register a limited company (private or public)</a>.</p>
+			
+			<h3>Limited company registration and costs</h3>
+			<p>All limited companies - including Community Interest Companies (CICs) - must register (incorporate) and file annual returns at Companies House.
+				If you are in England, Scotland or Wales, you can register your limited company with Companies House using our online company incorporation service. Limited companies must also submit a set of memoranda and articles of association. A standard incorporation certificate costs &pound;20.</p>
+			<p>There is a fee for incorporating your company with Companies House. The fee varies depending on:</p>
+			<ul>
+				<li>whether you submit the necessary documentation as hard copies or electronically</li>
+				<li>whether or not you want to incorporate on the same day that you submit the documentation</li>
+			</ul>
+			<p><a href="https://www.gov.uk/government/organisations/companies-house/about/about-our-services#companies-house-fees" target="_blank" rel="noopener noreferrer">Find a full list of Companies House fees on the Companies House website</a>.</p>
+			
+			<p>Limited companies, with the exception of CICs, can also be set up as charities if the organisation has exclusively charitable objects and is for the public benefit, and should (in most cases) apply to the Charity Commission to be registered as a charity. However, it is very rare for a CLS to be a charity. See the pages in this guide on <a href="/starting-up/business-structures/set-up-a-social-enterprise/social-enterprises-as-registered-charities.php">social enterprises as registered charities</a> and on <a href="/starting-up/business-structures/set-up-a-social-enterprise.php">Community Interest Companies</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/trusts.php" title="Trusts">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/community-benefit-societies.php" title="Community benefit societies">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php" title="Set up a social enterprise">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/unincorporated-associations.php">Unincorporated associations</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/trusts.php">Trusts</a></li>
+				<li>Limited companies with a social purpose</li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/community-benefit-societies.php">Community benefit societies</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/social-enterprises-as-registered-charities.php">Social enterprises as registered charities</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php">Community Interest Companies</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/charitable-incorporated-organisations.php">Charitable incorporated organisations</a></li>
+				<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-being-a-social-enterprise-works-for-our-business.php">Here's how being a social enterprise works for our business</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="../../../_files/LegalStructures.doc" target="_blank" rel="noopener noreferrer">Download our at-a-glance guide to legal structures for social enterprises (DOC, 44K)</a></li>
+				<li><a href="https://www.ncvo.org.uk/practical-support/information/governance" target="_blank" rel="noopener noreferrer">Good governance and leadership advice from the National Council for Voluntary Organisations</a></li>
+				<li><a href="https://www.gov.uk/government/publications/incorporation-and-names" target="_blank" rel="noopener noreferrer">Company formation requirements guidance from Companies House</a></li>
+				<li><a href="https://www.the-sse.org/about-school-for-social-entrepreneurs/" target="_blank" rel="noopener noreferrer">Social enterprises information from the Social Enterprise Coalition</a></li>
+				<li><a href="https://www.the-sse.org/our-courses/" target="_blank" rel="noopener noreferrer">Training programmes for social enterprises from the School for Social Entrepreneurs</a></li>
+				<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance options for your business</a></li>
+				<li><a href="/do-it-online/incorporate-your-company-online.php">Incorporate your limited company online with Companies House</a></li>
+			</ul>
+						
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public.php">Set up and register a limited company (private or public)</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+				<li><a href="/finance-and-grants/finance-options/grants-and-government-support/grants-the-basics.php">Grants: the basics</a></li>
+				<li><a href="/tax-payroll-and-company-information/changes-to-your-business/change-your-business-structure.php">Change your business structure</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+			
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
