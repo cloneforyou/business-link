f6bf46e98bda0f975268e9bbb5109cdda9f3ce87
Caroline Hagan <info@blueocto.co.uk>
2017-02-05 17:20:20 +0000

    Completed “starting-up > business-structures > legal-structures-the-basics > starting-a-public-limited-company”

---
+++ starting-up/business-structures/legal-structures-the-basics/starting-a-public-limited-company.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Starting a public limited company | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+
+			<h1>Legal structures: the basics</h1>
+			<h2>Starting a public limited company</h2>
+			<p>Public limited companies (PLCs) exist in their own right. This means the company's finances are separate from the personal finances of their members.</p>
+			
+			<h3>Set-up</h3>
+			<p>PLCs must:</p>
+			<ul>
+				<li>Have at least two shareholders.</li>
+				<li>Have issued shares to the public to a value of at least &pound;50,000 or the prescribed equivalent in euros before it can trade.</li>
+				<li>Be registered (incorporated) at Companies House - you can register either via a paper application form or electronically using a third party with access to the necessary software, eg an incorporation agent, software provider or solicitor.</li>
+				<li>Have at least two directors - at least one must be an individual. Each director who is an individual must be at least 16 years of age.</li>
+				<li>Have a qualified company secretary.</li>
+			</ul>
+			<p>You can <a href="/do-it-online/incorporate-your-company-online.php">incorporate your limited company online with Companies House</a>. The web incorporation service is used for incorporating a private company, limited by shares, with model articles of association.</p>
+			
+			<h3>Management and raising finance</h3>
+			<p>Businesses that are PLCs are the only type of business that can raise money by selling shares to the general public. Shareholders can be individuals or other companies.</p>
+			<p>The shares may or may not be traded on the stock exchange.</p>
+			<p>Finance can also be raised through loans and retained profits.</p>
+			<p>Note that directors may be asked to give personal guarantees of loans to the company.</p>
+			<p>A board of directors usually makes the management decisions.</p>
+			
+			<h3>Records and accounts</h3>
+			<p>PLCs must:</p>
+			<ul>
+				<li>send an annual return to Companies House</li>
+				<li>file accounts with Companies House once a year - the accounts must be audited unless the company is exempt</li>
+			</ul>
+			<p>You can send your annual return and other documents to Companies House electronically using its WebFiling service. For more information, see the page on <a href="/tax-payroll-and-company-information/company-administration-an-introduction/submitting-documents-to-companies-house/submitting-documents-to-companies-house-using-webfiling.php">submitting documents to Companies House using WebFiling</a> in our guide on <a href="/tax-payroll-and-company-information/company-administration-an-introduction/submitting-documents-to-companies-house.php">submitting documents to Companies House</a>.</p>
+			<p>Directors must notify Companies House of changes in the structure and management of the business.</p>
+			
+			<h3>Profits</h3>
+			<p>Profits are usually distributed to shareholders in the form of dividends, apart from profits retained in the business as working capital.</p>
+			
+			<h3>Tax and National Insurance</h3>
+			<p>If a PLC has any taxable income or profits, it must tell HM Revenue &amp; Customs (HMRC) that it exists and is liable to <strong>Corporation Tax</strong>. It must then pay any Corporation Tax that's due and submit a Company Tax Return to HMRC. A PLC must also comply with HMRC's requirements for PAYE for employers, VAT, the Construction Industry Scheme etc. For more information, see our section on <a href="/tax-payroll-and-company-information.php">tax, payroll and company information</a>.</p>
+			<p>Company directors are an office holder of the company and therefore regarded as an employed earner for the purposes of paying National Insurance contributions (NICs). As such, company directors must pay both income tax and Class 1 NICs on their director's earnings.</p>
+			<p>However, while regular employees' Class 1 NICs are calculated on their monthly or weekly earnings separately, directors' NICs are calculated on an annual cumulative basis.</p>
+			<p>For more information, see the page on <a href="/tax-payroll-and-company-information/paye-for-employers/monthly-weekly-payroll-and-form-filling/calculating-nics-deductions-paper-methods/calculating-directors-national-insurance-contributions.php">calculating directors' National Insurance contributions</a> in our guide on <a href="/tax-payroll-and-company-information/paye-for-employers/monthly-weekly-payroll-and-form-filling/calculating-nics-deductions-paper-methods.php">calculating NICs deductions (paper methods)</a>.</p>
+			<p>Company directors must complete a Self Assessment tax return each year. You will need to give details of the income from your directorship on the employment pages. If you do not usually fill in a tax return you must register for Self Assessment.  See our guide on <a href="/tax-payroll-and-company-information/self-assessment/your-tax-return-the-basics/registering-for-self-assessment.php">registering for Self Assessment</a>. </p>
+			
+			<h3>Liability</h3>
+			<p>The liability of each member is limited to the amount unpaid on their shares.</p>
+			<p>Members are not responsible for the company's debts unless they have given guarantees - eg when taking out a bank loan.</p>
+
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/starting-a-private-company.php" title="Starting a private company">Prev</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/setting-up-specific-types-of-limited-company.php" title="Setting up specific types of limited company">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php" title="Legal structures: the basics">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/becoming-a-sole-trader.php">Becoming a sole trader</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/partnerships.php">Partnerships</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/starting-a-private-company.php">Starting a private company</a></li>
+				<li>Starting a public limited company</li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/setting-up-specific-types-of-limited-company.php">Setting up specific types of limited company</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/european-economic-interest-groupings.php">European Economic Interest Groupings</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/uk-establishments-of-overseas-companies.php">UK establishments of overseas companies</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/buying-a-franchise.php">Buying a franchise</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+
+			<img src="../../../_img/hr_140px_global.gif" alt="" />
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/collections/companies-house-guidance-for-limited-companies-partnerships-and-other-company-types" target="_blank" rel="noopener noreferrer">Company guidance from Companies House</a></li>
+				<li><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Solicitor search with the Law Society</a></li>
+				<li><a href="https://www.icaew.com/" target="_blank" rel="noopener noreferrer">Accountant search from the Institute of Chartered Accountants in England and Wales</a></li>
+				<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a></li>
+			</ul>
+						
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public.php">Set up and register a limited company (private or public)</a></li>
+				<li><a href="/tax-payroll-and-company-information/companies-house-annual-returns-and-accounts.php">Companies House annual returns and accounts</a></li>
+				<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php">Choose the right name for your business</a></li>
+				<li><a href="/tax-payroll-and-company-information/corporation-tax.php">Corporation Tax</a></li>
+				<li><a href="/tax-payroll-and-company-information/changes-to-your-business/change-your-business-structure.php">Change your business structure</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+			
+		
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
