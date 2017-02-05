144c0d74e33d86c75a2a3db1398c691e5ff99e1a
Caroline Hagan <info@blueocto.co.uk>
2017-02-05 17:27:22 +0000

    Completed “starting-up > business-structures > legal-structures-the-basics > setting-up-specific-types-of-limited-company”

---
+++ starting-up/business-structures/legal-structures-the-basics/setting-up-specific-types-of-limited-company.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Setting up specific types of limited company | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+
+			<h1>Legal structures: the basics</h1>
+			<h2>Setting up specific types of limited company</h2>
+			<p>Instead of setting up a 'standard' type of public or private company, you could consider setting up a:</p>
+			<ul>
+				<li>Community Interest Company</li>
+				<li>Right-to-manage company</li>
+				<li>Societas Europaea or European company</li>
+			</ul>
+			<p>These types of company are subject to company law in the same way as standard public or private companies - see the pages in this guide on <a href="/starting-up/business-structures/legal-structures-the-basics/starting-a-private-company.php">starting a private company</a> and <a href="/starting-up/business-structures/legal-structures-the-basics/setting-up-specific-types-of-limited-company.php">starting a public limited company</a>.</p>
+			<p>However, they must meet additional criteria in order to qualify as the company type concerned.</p>
+			<p>Note also that you can register one of these types of company via a paper application form only - ie you cannot register electronically.</p>
+			
+			<h3>Community Interest Company (CIC)</h3>
+			<p>A CIC is a type of limited company designed for people who want to run a business for the benefit of the community - ie not purely for the benefit of the company's members and shareholders.</p>
+			<p>A CIC may be a public or private company limited by:</p>
+			<ul>
+				<li>share capital</li>
+				<li>guarantee with or without share capital</li>
+			</ul>
+			<p>A company cannot become a CIC if it is:</p>
+			<ul>
+				<li>is a charity</li>
+				<li>intends to be a political party, a political campaigning organisation, or a subsidiary of either</li>
+			</ul>
+			<p>If you intend to set up a CIC, you must register it with Companies House. You can register either via a paper application form or electronically using a third party with access to the necessary software - eg an incorporation agent, software provider or solicitor.</p>
+			<p>However, it's the CIC Regulator that decides whether or not your company is eligible to become a CIC.</p>
+			<p>To be accepted, you must pass a 'community interest test' and 'asset lock'. These ensure that the:</p>
+			<ul>
+				<li>CIC is established for community purposes</li>
+				<li>assets and profits are dedicated to these purposes</li>
+			</ul>
+			<p>CICs are often seen as a type of social enterprises and may take the form of:</p>
+			<ul>
+				<li>leisure centres</li>
+				<li>housing associations</li>
+				<li>worker-owned co-operatives</li>
+				<li>community development trusts</li>
+			</ul>
+			<p>See our guide on how to <a href="/starting-up/business-structures/set-up-a-social-enterprise.php">set up a social enterprise</a>.</p>
+			
+			<h3>Right-to-manage (RTM) company</h3>
+			<p>A RTM company is run by leaseholders who have taken on the landlord's management functions, eg repairs and maintenance of the premises.</p>
+			<p>For a company to be a RTM company in relation to those premises:</p>
+			<ul>
+				<li>it must be a private company limited by guarantee</li>
+				<li>its articles of association must state that its object, or one of its objects, is gaining the right to manage those premises</li>
+			</ul>
+			
+			<h3>Societas Europaea (SE)</h3>
+			<p>An SE is a trans-European company that may be created on registration in any European Economic Area state.</p>
+			<p>If you wish to create an SE in the UK, you must register it with Companies House.</p>
+			<p>An SE can be formed:</p>
+			<ul>
+				<li>by merger</li>
+				<li>as a subsidiary</li>
+				<li>as a holding company</li>
+				<li>by a PLC transforming itself into an SE</li>
+			</ul>
+			<p>If registered in the UK, an SE:</p>
+			<ul>
+				<li>is a public limited company (PLC) formed in accordance with the Companies Act 2006 and related legislation</li>
+				<li>must have both its registered office and head office in the UK - although they don't have to be at the same address</li>
+			</ul>
+			<p>An SE must have share capital and shareholders whose liability is limited in a similar manner to that of a PLC.</p>
+			<p>An SE may denominate its share capital in any currency provided that at least &pound;50,000 is denominated in sterling or &euro;57,100 is denominated in euros.</p>
+			<p>It must also have a minimum amount of subscribed share capital equivalent to &euro;120,000.</p>
+			<p>As a PLC, an SE registered in the UK must submit an annual return to, and file company accounts with, Companies House.</p>
+
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/setting-up-specific-types-of-limited-company.php" title="Starting a public limited company">Prev</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/european-economic-interest-groupings.php" title="European Economic Interest Groupings">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php" title="Legal structures: the basics">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/becoming-a-sole-trader.php">Becoming a sole trader</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/partnerships.php">Partnerships</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/starting-a-public-limited-company.php">Starting a private company</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/starting-a-private-company.php">Starting a public limited company</a></li>
+				<li>Setting up specific types of limited company</li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/european-economic-interest-groupings.php">European Economic Interest Groupings</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/uk-establishments-of-overseas-companies.php">UK establishments of overseas companies</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/buying-a-franchise.php">Buying a franchise</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+
+			<img src="../../../_img/hr_140px_global.gif" alt="" />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/publications/incorporation-and-names#ch7" target="_blank" rel="noopener noreferrer">CIC guidance from Companies House </a></li>
+				<li><a href="https://www.gov.uk/government/organisations/office-of-the-regulator-of-community-interest-companies" target="_blank" rel="noopener noreferrer">CIC information from the CIC Regulator</a></li>
+				<li><a href="https://www.gov.uk/government/publications/incorporation-and-names#ch8" target="_blank" rel="noopener noreferrer">RTM company guidance from Companies House</a></li>
+				<li><a href="https://www.gov.uk/government/publications/european-companies-in-the-uk-registration-and-administration" target="_blank" rel="noopener noreferrer">SE guidance from Companies House </a></li>
+				<li><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Solicitor search with the Law Society</a></li>
+				<li><a href="https://www.icaew.com/" target="_blank" rel="noopener noreferrer">Accountant search from the Institute of Chartered Accountants in England and Wales</a></li>
+				<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a></li>
+			</ul>
+						
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public.php">Set up and register a limited company (private or public)</a></li>
+				<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php">Choose the right name for your business</a></li>
+				<li><a href="/tax-payroll-and-company-information/companies-house-annual-returns-and-accounts.php">Companies House annual returns and accounts</a></li>
+				<li><a href="/tax-payroll-and-company-information/corporation-tax.php">Corporation Tax</a></li>
+				<li><a href="/tax-payroll-and-company-information/changes-to-your-business/change-your-business-structure.php">Change your business structure</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
