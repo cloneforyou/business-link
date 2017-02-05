decbe58fbe867cf6e83440839d506c7bdc2ad488
Caroline Hagan <info@blueocto.co.uk>
2017-02-05 21:18:39 +0000

    Completed “starting-up > business-structures > set-up-a-social-enterprise > community-benefit-societies”

---
+++ starting-up/business-structures/set-up-a-social-enterprise/community-benefit-societies.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Community benefit societies | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+
+			<h1>Set up a social enterprise</h1>
+			<h2>Community benefit societies</h2>
+			<p>Community benefit societies (BenComs) are incorporated industrial and provident societies (IPS) that conduct business for the benefit of their community. Profits are not distributed among members, or external shareholders, but returned to the community. For example, a nursery school might use this form to let staff take part in decision-making.</p>
+			
+			<h3>How BenComs operate</h3>
+			<p>As IPS, some key characteristics of BenComs are as follows:</p>
+			<ul>
+				<li>
+					They are set up with social objectives to conduct a business or trade.
+				</li>
+				<li>
+					They are run and managed by their members.
+				</li>
+				<li>
+					They must submit annual accounts.
+				</li>
+				<li>
+					They can raise funds by issuing shares to the public.
+				</li>
+				<li>
+					They can be established as charities, providing they have exclusively charitable objects that are for the public benefit, allowing them to raise capital through public grants and charitable trusts. If approved, they're known as exempt charities - reporting to the Financial Services Authority (FSA), not the Charity Commission. <a href="https://www.gov.uk/topic/running-charity" target="_blank" rel="noopener noreferrer">Read about exempt charities on the Charity Commission website</a>.
+				</li>
+			</ul>
+			
+			<p>BenComs are not to be confused with another form of IPS - co-operatives. Co-operatives operate for the mutual benefit of their members and may or may not be a social enterprise, depending on their activities and how they distribute their profits. Co-operatives cannot be established as charities. <a href="http://www.fsa.gov.uk/Pages/Doing/small_firms/MSR/Societies/index.shtml" target="_blank" rel="noopener noreferrer">Read information on IPS on the FSA website</a>. BenComs and co-operatives are both regulated by the FSA. <a href="../../../_files/fsa_mutual-societies-application-form.pdf" target="_blank" rel="noopener noreferrer">Download information on BenComs and their registration from the FSA website (PDF, 98K)</a>.</p>
+			
+			<h3>BenCom registration and costs</h3>
+			<p>To register as a BenCom, you must demonstrate your social objectives and your reasons for registering as a society, rather than a company.</p>
+			<p>It can cost between &pound;40 and &pound;950 to register a BenCom with the FSA - payable each year. The fee depends on the BenCom's assets and whether it registers under self-written rules or FSA-approved rules. <a href="../../../_files/fsa_mutual-societies-application-fees.pdf" target="_blank" rel="noopener noreferrer">Download information on application fees from the FSA website (PDF, 23K)</a>.</p>
+			<p>You should seek legal advice, particularly if creating your own rules.</p>
+			
+			<h3>Adopting or opting out of the BenCom structure</h3>
+			<p>A registered company may, by special resolution and under certain circumstances, convert into a registered society. Conversely, members can vote to change the objectives of an IPS and convert it into a company.</p>
+			
+			<h3>Asset locks</h3>
+			<p>Charitable BenComs must have an asset lock. Non-charitable BenComs can apply an asset lock, which protects their assets for the future benefit of the community. BenComs that do so may only convert to a Community Interest Company (CIC). See the page in this guide on <a href="/starting-up/business-structures/set-up-a-social-enterprise.php">Community Interest Companies</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/limited-companies-with-a-social-purpose.php" title="Limited companies with a social purpose">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/social-enterprises-as-registered-charities.php" title="Social enterprises as registered charities">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php" title="Set up a social enterprise">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/unincorporated-associations.php">Unincorporated associations</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/trusts.php">Trusts</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/limited-companies-with-a-social-purpose.php">Limited companies with a social purpose</a></li>
+				<li>Community benefit societies</li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/social-enterprises-as-registered-charities.php">Social enterprises as registered charities</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php">Community Interest Companies</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/charitable-incorporated-organisations.php">Charitable incorporated organisations</a></li>
+				<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-being-a-social-enterprise-works-for-our-business.php">Here's how being a social enterprise works for our business</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="../../../_files/fsa_mutual-societies-application-form.pdf" target="_blank" rel="noopener noreferrer">Download information on BenComs and their registration from FSA (PDF, 98K)</a></li>
+				<li><a href="../../../_files/fsa_mutual-societies-application-fees.pdf" target="_blank" rel="noopener noreferrer">Download information on application fees from FSA (PDF, 23K)</a></li>
+				<li><a href="../../../_files/fsa_converting-a-company-into-an-ips.doc" target="_blank" rel="noopener noreferrer">Download guidance and an application form for converting a company into an IPS from FSA (DOC, 1.73MB)</a></li>
+				<li><a href="https://www.gov.uk/topic/running-charity" target="_blank" rel="noopener noreferrer">Exempt charities guidance from the Charity Commission</a></li>
+				<li><a href="https://www.the-sse.org/about-school-for-social-entrepreneurs/" target="_blank" rel="noopener noreferrer">Social enterprises information from the Social Enterprise Coalition</a></li>
+				<li><a href="https://www.gov.uk/government/collections/cic-regulator-forms" target="_blank" rel="noopener noreferrer">CIC information from Community Interest Companies</a></li>
+			</ul>
+
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/environment-and-efficiency/selling-and-marketing/market-your-environmental-credentials.php">Market your environmental credentials</a></li>
+				<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/choose-the-right-finance-when-starting-up.php">Choose the right finance when starting up</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+				<li><a href="/finance-and-grants/expert-financial-advice/choose-and-work-with-a-solicitor.php">Choose and work with a solicitor</a></li>
+				<li><a href="/tax-payroll-and-company-information/changes-to-your-business/change-your-business-structure.php">Change your business structure</a></li>
+				<li><a href="/tax-payroll-and-company-information/companies-house-annual-returns-and-accounts/companies-house-annual-return.php">Companies House annual return</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
