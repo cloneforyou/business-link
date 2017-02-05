5ecb6d9b3c6b51e60c33ab63bba958861c95e3d4
Caroline Hagan <info@blueocto.co.uk>
2017-02-05 21:39:56 +0000

    Completed “starting-up > business-structures > set-up-and-register-a-limited-company-private-or-public > registration-documents-and-forms”

---
+++ starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/registration-documents-and-forms.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Registration documents and forms | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+
+			<h1>Set up and register a limited company (private or public)</h1>
+			<h2>Registration documents and forms</h2>
+			<p>To set up as a limited company in the UK, you - or the agent acting for you - will need to send the following documents to Companies House:</p>
+			<ul>
+				<li>
+					<strong>application form IN01 to register a company</strong> which includes the company's registered office, type of articles, the names and addresses of its directors, statement of capital and initial shareholdings
+				</li>
+				<li>
+					<strong>memorandum of association</strong> giving the names of each subscriber and authentication that they have agreed to become members of that company
+				</li>
+				<li><strong>articles of association</strong> (unless the company adopts model articles in their entirety), describing how the company will be run, shareholders' rights, any restricted objects and details of the directors' powers</li>
+				<li><strong>additional information</strong> if your application includes a prescribed or sensitive word or expression</li>
+			</ul>
+			<p><a href="https://www.gov.uk/guidance/model-articles-of-association-for-limited-companies" target="_blank" rel="noopener noreferrer">Find the model articles of association on the Companies House website</a>.</p>
+			<p>You can <a href="https://www.gov.uk/government/publications/register-a-private-or-public-company-in01" target="_blank" rel="noopener noreferrer">download form IN01 from the Companies House website (PDF, 1.03MB)</a>.</p>
+			
+			<h3>What is the memorandum of association?</h3>
+			<p>The memorandum of association of a company incorporated under the Companies Act 2006 confirms the subscribers' intention to form and become members of a company. In the case of a company limited by shares, the memorandum will also provide evidence of the members' agreement to take at least one share each in the company.</p>
+			<p>Information on capital and shareholdings is no longer part of the memorandum and is contained in form IN01 as either a 'statement of capital and shareholdings' or, for those companies limited by guarantee, a 'statement of guarantee'.</p>
+			<p>Once the company has been incorporated, the memorandum no longer affects the operation of the company and cannot be amended. It becomes, to a large extent, a historical document.</p>
+			<p><a href="https://www.gov.uk/government/publications/give-notice-of-subscribers-company-with-share-capital" target="_blank" rel="noopener noreferrer">Download a template memorandum from the Companies House website (PDF, 228K)</a>.</p>
+			
+			<h3>What are the articles of association?</h3>
+			<p>A company's articles of association is an internal rulebook. Every company formed under company law will have articles of association - commonly referred to simply as the company's 'articles'.</p>
+			<p>The articles are chosen by the members and form a contract between the company and its members. They help ensure the company runs as smoothly and efficiently as possible and set out how the company makes decisions and includes various matters connected with the shares. Every company is required to have articles and they are legally binding on the company and all of its members.</p>
+			<p>The articles cannot contain rules that do not comply with the Companies Act 2006 or make the company operate outside the law. Provided members observe this general principle they can choose which rules go into their company's articles, although they may find it convenient to use model articles. If the members decide to draw up their own rules, they should consider taking legal or other professional advice before having the articles drawn up for their company.</p>
+			<p>On incorporation, a company can adopt model articles, model articles with amendments or bespoke articles. Model articles are available for private companies limited by shares, private companies limited by guarantee and public companies.</p>
+			<p><a href="https://www.gov.uk/guidance/model-articles-of-association-for-limited-companies" target="_blank" rel="noopener noreferrer">Find the model articles of association on the Companies House website</a>.</p>
+			<p>When you complete form IN01, you need to indicate if the proposed company is adopting:</p>
+			<ul>
+				<li>model articles in their entirety - the model articles should not be sent with the IN01</li>
+				<li>model articles with amendments - a copy of the amended articles must be sent with the IN01</li>
+				<li>bespoke model articles - a copy of the articles must be sent with the IN01</li>
+			</ul>
+			<p>If you do not indicate which articles you are adopting, the model articles appropriate to your company type will be automatically applied.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public.php" title="Set up and register a limited company (private or public)">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/the-companys-officers.php" title="The company's officers">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public.php" title="Set up and register a limited company (private or public)">Introduction</a></li>
+				<li>Registration documents and forms</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/the-companys-officers.php">The company's officers</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/types-of-limited-company.php">Types of limited company</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/requirements-for-public-limited-companies.php">Requirements for public limited companies</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/where-to-register-your-company-and-get-help.php">Where to register your company and get help</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/tax-matters-of-a-limited-company.php">Tax matters of a limited company</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/checklist-setting-up-and-registering-a-limited-company-private-or-public.php">Checklist: setting up and registering a limited company (private or public)</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/right-to-manage-companies-and-commonhold-associations.php">Right to manage companies and commonhold associations</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+			<h3>TSO Customer Services</h3>
+			<p>0870 600 5522</p>
+			
+			<img src="../../../_img/hr_140px_global.gif" alt="" />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/publications/register-a-private-or-public-company-in01" target="_blank" rel="noopener noreferrer">Download form IN01 from Companies House (PDF, 1.03MB)</a></li>
+				<li><a href="/do-it-online/incorporate-your-company-online.php">Incorporate your limited company online with Companies House</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php">Rules for naming your limited company or limited liability partnership (LLP)</a></li>
+				<li><a href="/tax-payroll-and-company-information/changes-to-your-business/change-your-business-structure.php">Change your business structure</a></li>
+				<li><a href="/tax-payroll-and-company-information/company-administration-an-introduction/company-directors-responsibilities.php">Company directors' responsibilities</a></li>
+				<li><a href="/tax-payroll-and-company-information/company-administration-an-introduction/companies-house-deadlines-and-late-filing-penalties.php">Companies House deadlines and late filing penalties</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
