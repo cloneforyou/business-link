5bd73a36b8272789f4d53538ad7f3b4f7cc2b03d
Caroline Hagan <info@blueocto.co.uk>
2017-02-05 20:29:17 +0000

    Completed “starting-up > business-structures > legal-structures-the-basics > buying-a-franchise”

---
+++ starting-up/business-structures/legal-structures-the-basics/buying-a-franchise.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Buying a franchise | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+
+			<h1>Legal structures: the basics</h1>
+			<h2>Buying a franchise</h2>
+			<p>A franchise is not a legal structure but is a business model that can operate under one of the legal structures, ie as a sole trader, or type of partnership or limited company - see the pages in this guide on:</p>
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/becoming-a-sole-trader.php">becoming a sole trader</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/partnerships.php">going into partnerships</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/starting-a-private-company.php">starting a private company</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/setting-up-specific-types-of-limited-company.php">starting a public limited company</a></li>
+			</ul>
+			<p>Buying a franchise is a way of taking advantage of the success of an established business. As the '<strong>franchisee</strong>', you buy a licence to use the name, products and services of the 'franchisor' business. This licence normally covers a particular geographical area and runs for a limited time, after which it should be renewable as long as you meet the terms of the franchise agreement.</p>
+			<p>Franchise agreements usually set out how the franchised business should be run, although they may allow some flexibility. Franchisors usually provide management help and training to franchisees.</p>
+			<p>The way you pay for the franchise may be through an initial fee, ongoing management fees, a percentage of your turnover, purchases of goods from the franchisor, or a combination of these.</p>
+			<p>You often pay a percentage of your turnover to the franchisor, which reduces your overall profits.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/uk-establishments-of-overseas-companies.php" title="UK establishments of overseas companies">Prev</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php" title="Legal structures: the basics">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/becoming-a-sole-trader.php">Becoming a sole trader</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/partnerships.php">Partnerships</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/starting-a-private-company.php">Starting a private company</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/setting-up-specific-types-of-limited-company.php">Starting a public limited company</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/setting-up-specific-types-of-limited-company.php">Setting up specific types of limited company</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/european-economic-interest-groupings.php">European Economic Interest Groupings</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/uk-establishments-of-overseas-companies.php">UK establishments of overseas companies</a></li>
+				<li>Buying a franchise</li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>BFA Enquiry Line</h3>
+			<p>01235 820 470</p>
+			
+			<img src="../../../_img/hr_140px_global.gif" alt="" />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://www.thebfa.org/" target="_blank" rel="noopener noreferrer">Franchising information from the British Franchise Association</a></li>
+				<li><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Solicitor directory from the Law Society</a></li>
+				<li><a href="https://www.icaew.com/" target="_blank" rel="noopener noreferrer">Accountant listing from the Institute of Chartered Accountants in England and Wales</a></li>
+				<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php" title="Use our interactive tool to find out the records you must keep as a business and assess how well you are keeping them">Use our interactive tool to find out the records you must keep as a business</a></li>
+			</ul>
+						
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/changes-to-your-business/change-your-business-structure.php">Change your business structure</a></li>
+				<li><a href="/buy-or-sell-a-business/buying/buy-an-existing-business.php">Buy an existing business</a></li>
+				<li><a href="/buy-or-sell-a-business/franchising/buy-a-franchise.php">Buy a franchise</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+			
+
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
