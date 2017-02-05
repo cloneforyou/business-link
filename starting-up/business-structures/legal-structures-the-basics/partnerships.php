fed12f93b0fc38f80e10c1f803358a19420b7396
Caroline Hagan <info@blueocto.co.uk>
2017-02-05 16:55:33 +0000

    Completed “starting-up > business-structures > legal-structures-the-basics/partnerships”

---
+++ starting-up/business-structures/legal-structures-the-basics/partnerships.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Partnerships | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+
+			<h1>Legal structures: the basics</h1>
+			<h2>Partnerships</h2>
+			<p>There are three types of partnership:</p>
+			<ul>
+				<li>'ordinary' partnerships</li>
+				<li>limited partnerships</li>
+				<li>limited liability partnerships (LLPs)</li>
+			</ul>
+			
+			<h3>Common features of all types of partnership</h3>
+			<p>All three types of partnership have the following features in common:</p>
+			<ul>
+				<li>two or more persons &ndash; ie the partners - share the risks, costs and responsibilities of being in business</li>
+				<li>a partner can be an individual or another business, eg a limited company or another partnership</li>
+				<li>the profits and gains of the partnership are shared among the partners, unless the partnership agreement states otherwise</li>
+				<li>each partner is personally responsible for paying tax on their share of the profits and gains, and for their National Insurance contributions</li>
+				<li>each partner must register for Self Assessment with HM Revenue &amp; Customs (HMRC) and complete an annual tax return</li>
+				<li>a nominated partner must also send HMRC a partnership return</li>
+				<li>partners raise money for the business out of their own assets and/or with loans</li>
+				<li>the partners themselves usually manage the business, although they can delegate certain responsibilities to employees</li>
+				<li>it's possible to have 'sleeping' partners who contribute money to the business but are not involved in running it from day to day</li>
+				<li>the partnership must keep records showing business income and expenses</li>
+			</ul>
+			<p>It's a good idea to draw up a written agreement between the partners. For further advice, consult an accountant or solicitor.</p>
+			
+			<h3>'Ordinary' partnerships</h3>
+			<p>An 'ordinary' partnership has no legal existence distinct from the partners themselves. If one of the partners resigns, dies or goes bankrupt, the partnership must be dissolved - although the business can still continue.</p>
+			<p>A partnership is a relatively <b>simple and flexible</b> way for two or more people to own and run a business together.</p>
+			<p>Ordinary partnerships also have to be registered with HMRC for tax purposes. The nominated partner does this by registering the partnership for Self Assessment.</p>
+			<p>If the partnership has debts, the partners are jointly liable for any amounts owed and so are equally responsible for paying off the whole debt.</p>
+			<p>Creditors can claim a partner's personal assets to pay off any debts - even those debts caused by other partners.</p>
+			<p>If a partner leaves the partnership, the remaining partners may be liable for the entire debt of the partnership.</p>
+			<p>Therefore, partners do not enjoy any protection if the business fails.</p>
+			<p>For more information on going into partnership, see our guide on how to <a href="/starting-up/business-structures/set-up-and-register-a-partnership.php">set up and register a partnership</a>.</p>
+			<p>For more information on Self Assessment, see our guide on <a href="/tax-payroll-and-company-information/self-assessment/tax-returns-if-you-are-self-employed-or-in-a-partnership/tax-returns-for-partners-and-partnerships.php">tax returns for partners and partnerships</a>.</p>
+			
+			<h3>Limited partnerships</h3>
+			<p>A limited partnership is made up of a mixture of ordinary partners and limited partners.</p>
+			<p>Limited partnerships must register with Companies House but don't generally have to make an annual return or file accounts. When they receive the registration Companies House inform HMRC that the limited partnership has been set up. HMRC will set up the partnership's tax records so there is no need to register with them.</p>
+			<p>Ordinary partners are jointly liable for any debts owed by the partnership and so are equally responsible for paying off the whole debt.</p>
+			<p>A limited partner's liability is limited to the amount of money they have invested in the business and to any personal guarantees they have given to raise finance.</p>
+			<p>For more information on starting a limited partnership, see our guide on how to <a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership.php">set up and register a limited partnership</a>.</p>
+			
+			<h3>Limited liability partnerships (LLPs)</h3>
+			<p>LLPs must have at least two <strong>designated members</strong> - the law places extra responsibilities on them.</p>
+			<p>If for any reason the number of designated members falls to one, every member is deemed to be a designated member.</p>
+			<p>LLPs must:</p>
+			<ul>
+				<li>register with Companies House</li>
+				<li>send Companies House an annual return</li>
+				<li>file accounts with Companies House</li>
+			</ul>
+			<p>When they receive the registration Companies House inform HMRC that the LLP has been set up. HMRC will set up the LLP's tax records so there is no need to register with them.</p>
+			<p>A partner's liability is limited to the amount of money they have invested in the business and to any personal guarantees they have given to raise finance.</p>
+			<p>This means that members have some protection if the business runs into trouble.</p>
+			<p>For more information on starting an LLP, see our guide on how to <a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp.php">set up and register a limited liability partnership (LLP)</a>.</p>
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/becoming-a-sole-trader.php" title="Becoming a sole trader">Prev</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/starting-a-private-company.php" title="Starting a private company">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php" title="Legal structures: the basics">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/becoming-a-sole-trader.php">Becoming a sole trader</a></li>
+				<li>Partnerships</li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/starting-a-private-company.php">Starting a private company</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/setting-up-specific-types-of-limited-company.php">Starting a public limited company</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/setting-up-specific-types-of-limited-company.php">Setting up specific types of limited company</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/european-economic-interest-groupings.php">European Economic Interest Groupings</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/uk-establishments-of-overseas-companies.php">UK establishments of overseas companies</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics/buying-a-franchise.php">Buying a franchise</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/publications/limited-partnership-act" target="_blank" rel="noopener noreferrer">Limited partnership registration guidance from Companies House</a></li>
+				<li><a href="https://www.gov.uk/government/collections/companies-house-guidance-for-limited-companies-partnerships-and-other-company-types" target="_blank" rel="noopener noreferrer">LLP registration guidance from Companies House</a></li>
+				<li><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Solicitor directory from the Law Society</a></li>
+				<li><a href="https://www.icaew.com/" target="_blank" rel="noopener noreferrer">Accountant listing from the Institute of Chartered Accountants in England and Wales</a></li>
+				<li><a href="http://www.lawdonut.co.uk/business/starting-up" target="_blank" rel="noopener noreferrer">Business formation and legal form resources from Law Donut</a></li>
+				<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a></li>
+			</ul>
+						
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership.php">Set up and register a partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership.php">Set up and register a limited partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp.php">Set up and register a limited liability partnership (LLP)</a></li>
+				<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php">Choose the right name for your business</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
