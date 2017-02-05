dc424176f92ec50d3555c76439e95cbfa2cde9ac
Caroline Hagan <info@blueocto.co.uk>
2017-02-05 16:43:42 +0000

    Completed “starting-up > business-structures > legal-structures-the-basics > becoming-a-sole-trader”

---
+++ starting-up/business-structures/legal-structures-the-basics/becoming-a-sole-trader.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Becoming a sole trader | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+		
+		<h1>Legal structures: the basics</h1>
+		<h2>Becoming a sole trader</h2>
+		<p>Being a sole trader is the simplest way to run a business - it does not involve paying any registration fees, keeping records and accounts is straightforward, and you get to keep all the profits. However, you are personally liable for any debts that your business runs up, which make this a risky option for businesses that need a lot of investment.</p>
+		<h3>Management and raising finance</h3>
+		<p>You make all the decisions on how to manage your business.</p>
+		<p>You raise money for the business out of your own assets and/or with loans from banks or other lenders.</p>
+		<h3>Records and accounts</h3>
+		<p>You must keep records showing your business income and expenses.</p>
+		<h3>Profits</h3>
+		<p>Any profits go to you.</p>
+		<h3>Tax and National Insurance</h3>
+		<p>As you are self-employed:</p>
+		<ul>
+			<li>
+				your profits are taxed as income
+			</li>
+			<li>
+				you pay fixed-rate Class 2 National Insurance contributions (NICs) regardless of any profits you make
+			</li>
+			<li>
+				you pay Class 4 NICs on any profits
+			</li>
+			<li>
+				you need to register for Self Assessment and complete a tax return each year
+			</li>
+		</ul>
+		<p>See our guide on <a href="/tax-payroll-and-company-information/self-assessment/your-tax-return-the-basics/registering-for-self-assessment.php">registering for Self Assessment</a>.</p>
+		<h3>Liability</h3>
+		<p>As a sole trader, you are personally responsible for any debts run up by your business. This means your home or other assets may be at risk if your business runs into trouble.</p>
+		<p>For more information on starting your own business, see our guide on how to <a href="/starting-up/business-structures/set-up-and-register-as-self-employed.php" title="How to meet your legal and tax responsibilities">set up and register as self employed</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/business-structures/legal-structures-the-basics.php" title="Legal structures: the basics">Prev</a></li>
+			<li><a href="/starting-up/business-structures/legal-structures-the-basics/partnerships.php" title="Partnerships">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/business-structures/legal-structures-the-basics.php" title="Legal structures: the basics">Introduction</a></li>
+			<li>Becoming a sole trader</li>
+			<li><a href="/starting-up/business-structures/legal-structures-the-basics/partnerships.php">Partnerships</a></li>
+			<li><a href="/starting-up/business-structures/legal-structures-the-basics/starting-a-private-company.php">Starting a private company</a></li>
+			<li><a href="/starting-up/business-structures/legal-structures-the-basics/starting-a-public-limited-company.php">Starting a public limited company</a></li>
+			<li><a href="/starting-up/business-structures/legal-structures-the-basics/setting-up-specific-types-of-limited-company.php">Setting up specific types of limited company</a></li>
+			<li><a href="/starting-up/business-structures/legal-structures-the-basics/european-economic-interest-groupings.php">European Economic Interest Groupings</a></li>
+			<li><a href="/starting-up/business-structures/legal-structures-the-basics/uk-establishments-of-overseas-companies.php">UK establishments of overseas companies</a></li>
+			<li><a href="/starting-up/business-structures/legal-structures-the-basics/buying-a-franchise.php">Buying a franchise</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Solicitor directory from the Law Society</a></li>
+			<li><a href="https://www.icaew.com/" target="_blank" rel="noopener noreferrer">Accountant listing from the Institute of Chartered Accountants in England and Wales</a></li>
+			<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php" title="Use our interactive tool to find out the records you must keep as a business and assess how well you are keeping them">Use our interactive tool to find out the records you must keep as a business</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed.php">Set up and register as self-employed</a></li>
+			<li><a href="/tax-payroll-and-company-information/self-assessment/your-tax-return-the-basics/registering-for-self-assessment.php">Registering for Self Assessment</a></li>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
