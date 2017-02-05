40d84d696dd2d151cce64165a631112645a064f0
Caroline Hagan <info@blueocto.co.uk>
2017-02-05 20:26:32 +0000

    Replaced gif with fontawesome, deleted image.

--- _inc/print-email.php
+++ _inc/print-email.php
@@ -1,9 +1,9 @@
 <hr />
-<p><strong>Print options</strong> - What are my print options <img src="../../_img/whatisthis.gif" width="9" height="11" border="0" alt="" /></a></p>
+<p><strong>Print options</strong> - What are my print options <i class="fa fa-question-circle-o" aria-hidden="true"></i></a></p>
 <ul>
 	<li><a href="#" title="">Add guide to my print portfolio (print later as PDF)</a></li>
 </ul>
-<p><strong>Email options</strong> - What are my email options <img src="../../_img/whatisthis.gif" width="9" height="11" border="0" alt="" /></a></p>
+<p><strong>Email options</strong> - What are my email options <i class="fa fa-question-circle-o" aria-hidden="true"></i></a></p>
 <ul>
 	<li><a href="#">Tell a friend about this page</a></li>
 	<li><a href="#">Send me an email alert when this guide changes</a></li>
