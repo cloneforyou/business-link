e65d0f68102ce5bc528d25ff9bf7f2ae17306dd9
Caroline Hagan <info@blueocto.co.uk>
2017-02-25 21:20:06 +0000

    Completed “starting-up > business-structures > set-up-and-register-a-limited-company-private-or-public > tax-matters-of-a-limited-company” page

---
+++ starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/tax-matters-of-a-limited-company.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Tax matters of a limited company | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Set up and register a limited company (private or public)</h1>
+			<h2>Tax matters of a limited company</h2>
+			<p>Limited companies have to pay Corporation Tax on their income and profits. They also need to operate a PAYE (Pay As You Earn) system to collect and pay Income Tax and National Insurance contributions from their employees - including company directors.</p>
+			<p>The company itself must work out how much Corporation Tax it needs to pay, using a self assessment system. To avoid paying penalties, it's important to understand how this system works and when your company needs to make returns. Your accountant or auditor will be able to advise you.</p>
+			<p>You can also <a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/keydates"target="_blank" rel="noopener noreferrer">get regular reminders of important tax dates with our Tax deadline email alerts</a>.</p>
+			
+			<h3>What do I need to do about taxes when I set up a company?</h3>
+			<p>When you register a new company, Companies House will pass on the details to HM Revenue &amp; Customs (HMRC). Additionally, you must tell HMRC if your company is active for Corporation Tax purposes.</p>
+			<p>For more information see our guide on <a href="/tax-payroll-and-company-information/corporation-tax/corporation-tax-for-new-companies-and-organisations/starting-a-company-or-organisation-and-corporation-tax.php">starting a company or organisation and Corporation Tax</a>.</p>
+			<p>If you have any questions about tax for companies, you should contact the HMRC Employer Helpline on Tel 08457 143 143. For new businesses contact the HMRC New Employer Helpline on Tel 0845 60 70 143.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/where-to-register-your-company-and-get-help.php" title="Where to register your company and get help">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/checklist-setting-up-and-registering-a-limited-company-private-or-public.php" title="Checklist: setting up and registering a limited company (private or public)">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public.php" title="Set up and register a limited company (private or public)">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/registration-documents-and-forms.php">Registration documents and forms</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/the-companys-officers.php">The company's officers</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/types-of-limited-company.php">Types of limited company</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/requirements-for-public-limited-companies.php">Requirements for public limited companies</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/where-to-register-your-company-and-get-help.php">Where to register your company and get help</a></li>
+				<li>Tax matters of a limited company</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/checklist-setting-up-and-registering-a-limited-company-private-or-public.php">Checklist: setting up and registering a limited company (private or public)</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/right-to-manage-companies-and-commonhold-associations.php">Right to manage companies and commonhold associations</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>HMRC Employer Helpline</h3>
+			<p>08457 143 143</p>
+
+			<h3>HMRC New Employer Helpline</h3>
+			<p>0845 60 70 143</p>
+
+			<hr />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/publications/corporation-tax-company-tax-return-guide-ct600-guide-2008-version-2" target="_blank" rel="noopener noreferrer">Corporation Tax information from HMRC</a></li>
+				<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/keydates" target="_blank" rel="noopener noreferrer">Get regular reminders of important tax dates with our Tax deadline email alerts</a></li>
+			</ul>
+						
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/corporation-tax/introduction-to-corporation-tax.php">Introduction to Corporation Tax</a></li>
+				<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+				<li><a href="/tax-payroll-and-company-information/paye-for-employers/introduction-to-paye-for-employers/paye-for-employers-the-basics.php">PAYE for employers: the basics</a></li>
+				<li><a href="/tax-payroll-and-company-information/expenses-and-benefits-for-employers/expenses-and-benefits-record-keeping.php">Expenses and benefits record keeping</a></li>
+				<li><a href="/tax-payroll-and-company-information/selling-or-closing-your-business/company-and-llp-strike-off-dissolution-and-restoration.php">Company and LLP strike-off, dissolution and restoration</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
