4d46a8433a306ceccb3fbfb7fb368f5c9fc7af34
Caroline Hagan <info@blueocto.co.uk>
2017-02-25 21:08:11 +0000

    Completed “starting-up > business-structures > set-up-and-register-a-limited-company-private-or-public > where-to-register-your-company-and-get-help” page.

---
+++ starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/where-to-register-your-company-and-get-help.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Where to register your company and get help | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Set up and register a limited company (private or public)</h1>
+			<h2>Where to register your company and get help</h2>
+			<p>The registration process for a limited company can be handled directly with Companies House or you can use the services of a company formation agent, accountant or solicitor to do it for you. A third option is to <a href="/do-it-online/incorporate-your-company-online.php">use the online web incorporation service with Companies House</a>. This allows a company limited by shares, with model articles in their entirety with a non-sensitive name to be incorporated online.</p>
+			<p><a href="https://www.gov.uk/guidance/model-articles-of-association-for-limited-companies" target="_blank" rel="noopener noreferrer">Find the model articles of association on the Companies House website</a>.</p>
+			<h3>How much will registration cost?</h3>
+			<p>The cost of registering a company online is considerably lower than on paper. <a href="https://www.gov.uk/government/organisations/companies-house/about/about-our-services#companies-house-fees" target="_blank" rel="noopener noreferrer">Find out more about Companies House fees on the Companies House website</a>.</p>
+			<p>The <strong>quickest</strong> way to incorporate a company is to file the application electronically. To find software filers, who provide this as a chargeable service, <a href="https://www.gov.uk/government/publications/formation-and-company-secretarial-agents" target="_blank" rel="noopener noreferrer">see the list of online filers on the Companies House website</a>.</p>
+			
+			<h3>Where can I get help?</h3>
+			<p>Companies House can advise you on the basics of the registration process, but they cannot give you detailed advice on how to draw up the necessary documentation. It is a good idea to seek <b>professional advice</b>. A company formation agent, solicitor or accountant can offer advice or carry out the process for you for a fee. Again, look in your local telephone directory or search the web.</p>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/requirements-for-public-limited-companies.php" title="Requirements for public limited companies">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/tax-matters-of-a-limited-company.php" title="Tax matters of a limited company">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public.php" title="Set up and register a limited company (private or public)">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/registration-documents-and-forms.php">Registration documents and forms</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/the-companys-officers.php">The company's officers</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/types-of-limited-company.php">Types of limited company</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/requirements-for-public-limited-companies.php">Requirements for public limited companies</a></li>
+				<li>Where to register your company and get help</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/tax-matters-of-a-limited-company.php">Tax matters of a limited company</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/checklist-setting-up-and-registering-a-limited-company-private-or-public.php">Checklist: setting up and registering a limited company (private or public)</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/right-to-manage-companies-and-commonhold-associations.php">Right to manage companies and commonhold associations</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+			
+			<hr />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Solicitor directory search from the Law Society</a></li>
+				<li><a href="http://web.archive.org/web/20120319152052/http://www.icaewfirms.co.uk/business/index.php"target="_blank" rel="noopener noreferrer">Chartered accountants search facility from the Institute of Chartered Accountants in England and Wales</a></li>
+				<li><a href="https://www.yell.com/" target="_blank" rel="noopener noreferrer">Sources of professional help from Yell.com</a></li>
+				<li><a href="/do-it-online/incorporate-your-company-online.php">Incorporate your limited company online with Companies House</a></li>
+			</ul>
+						
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php">Choose the right name for your business</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php">Set up a social enterprise</a></li>
+				<li><a href="/for-professional-advisers/for-accountants-and-tax-advisers/helping-your-client-set-up-or-dissolve-a-limited-company/get-started-with-company-incorporation-for-agents.php">Get started with company incorporation for agents</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
