02f553300a96822c93080462147a8a6035b8cc74
Caroline Hagan <info@blueocto.co.uk>
2017-02-25 20:58:50 +0000

    Completed “starting-up > business-structures > set-up-and-register-a-limited-company-private-or-public > requirements-for-public-limited-companies” page.

---
+++ starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/requirements-for-public-limited-companies.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Requirements for public limited companies | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Set up and register a limited company (private or public)</h1>
+			<h2>Requirements for public limited companies</h2>
+			<p>A public limited company (plc) must have:</p>
+			<ul>
+				<li>at least two directors - who may also be members of the company</li>
+				<li>have at least one director who is an individual</li>
+				<li>all individual directors must be aged 16 or over</li>
+				<li>at least one qualified secretary</li>
+			</ul>
+			
+			<p>The secretary must:</p>
+			<ul>
+				<li>have held the office of secretary of a plc for at least three of the five years before their appointment</li>
+				<li>be a barrister, advocate or solicitor called or admitted in any part of the UK</li>
+				<li>by virtue of their previous experience or membership of another body, appear to the directors to be capable of discharging the functions of secretary</li>
+				<li>be a member of the Institute of Chartered Accountants in England and Wales, Scotland or Ireland, the Institute of Chartered Secretaries and Administrators, the Association of Chartered Certified Accountants, the Chartered Institute of Management or the Chartered Institute of Public Finance and Accountancy</li>
+			</ul>
+			
+			<h3>When can a plc start business?</h3>
+			<p>A company incorporated as a plc cannot conduct business or exercise borrowing powers unless it has obtained a trading certificate from Companies House, confirming that it has the minimum allotted share capital. To obtain a trading certificate for a plc, you must file form SH50. You can <a href="https://www.gov.uk/government/publications/apply-for-trading-certificate-for-a-public-company-sh50" target="_blank" rel="noopener noreferrer">download form SH50 from the Companies House website (PDF, 717K)</a>.</p>
+			<p>It is an offence to trade without a trading certificate and the directors are liable, on conviction, to a fine. For information on share capital and trading certificates, see our guide on <a href="/tax-payroll-and-company-information/company-administration-an-introduction/reporting-changes-to-companies-house/change-of-address-directors-and-more-what-to-report-to-companies-house.php">change of address, directors and more - what to report to Companies House</a>.</p>
+			<p>Different rules apply if you intend to re-register from a private company limited by shares or a private unlimited company to a plc. You can <a href="https://www.gov.uk/government/publications/life-of-a-company-event-driven-filings#ch8" target="_blank" rel="noopener noreferrer">read about re-registration on the Companies House website</a>.</p>
+			<p>Please note the name of a public company must end with 'Public Limited Company', or 'plc' or, if it is registered in Wales, the Welsh equivalent.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/types-of-limited-company.php" title="Types of limited company">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/where-to-register-your-company-and-get-help.php" title="Where to register your company and get help">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public.php" title="Set up and register a limited company (private or public)">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/registration-documents-and-forms.php">Registration documents and forms</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/the-companys-officers.php">The company's officers</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/types-of-limited-company.php">Types of limited company</a></li>
+				<li>Requirements for public limited companies</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/where-to-register-your-company-and-get-help.php">Where to register your company and get help</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/tax-matters-of-a-limited-company.php">Tax matters of a limited company</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/checklist-setting-up-and-registering-a-limited-company-private-or-public.php">Checklist: setting up and registering a limited company (private or public)</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/right-to-manage-companies-and-commonhold-associations.php">Right to manage companies and commonhold associations</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/publications/apply-for-trading-certificate-for-a-public-company-sh50" target="_blank" rel="noopener noreferrer">Download form SH50 from Companies House (PDF, 717K)</a></li>
+				<li><a href="https://www.gov.uk/government/publications/incorporation-and-names#ch6" target="_blank" rel="noopener noreferrer">Requirements for plcs from Companies House</a></li>
+				<li><a href="https://www.gov.uk/government/publications/life-of-a-company-event-driven-filings#ch8" target="_blank" rel="noopener noreferrer">Re-registration guidance from Companies House</a></li>
+			</ul>
+						
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/company-administration-an-introduction/company-secretaries-responsibilities.php">Company secretaries' responsibilities</a></li>
+				<li><a href="/tax-payroll-and-company-information/company-administration-an-introduction/reporting-changes-to-companies-house/change-of-address-directors-and-more-what-to-report-to-companies-house.php">Change of address, directors and more - what to report to Companies House</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
