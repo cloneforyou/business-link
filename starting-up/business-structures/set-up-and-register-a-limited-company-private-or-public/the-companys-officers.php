78bf264b9fe33e79f3bd6ebbd3b8483f0f24c178
Caroline Hagan <info@blueocto.co.uk>
2017-02-25 20:50:37 +0000

    Completed “starting-up > business-structures > set-up-and-register-a-limited-company-private-or-public > the-companys-officers” page

---
+++ starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/the-companys-officers.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>The company's officers | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Set up and register a limited company (private or public)</h1>
+			<h2>The company's officers</h2>
+			<p>The officers of your company are the people formally appointed to run it - the company directors and if it has or requires one, the company secretary. Among their other duties the directors have a responsibility to prepare and deliver documents - such as the accounts and annual return to the registrar. They also have a duty to notify changes to, for example, its registered office or directors.</p>
+			<p>If officers resign or new ones are appointed, or if their personal details change, the Registrar of Companies must be informed straight away. You can <a href="https://www.gov.uk/topic/company-registration-filing/forms" target="_blank" rel="noopener noreferrer">find downloadable company forms on the Companies House website</a>.</p>
+			<p>You can find out more about company directors and secretaries in our guides on <a href="/tax-payroll-and-company-information/company-administration-an-introduction/company-directors-responsibilities.php">company directors' responsibilities</a> and <a href="/tax-payroll-and-company-information/company-administration-an-introduction/company-secretaries-responsibilities.php">company secretaries' responsibilities</a>.</p>
+			
+			<h3>How many officers does a company need to have?</h3>
+			<p>The number of officers required depends on the type of company:</p>
+			<ul>
+				<li>
+					<strong>Private companies</strong> must have at least one director, one of whom must be an individual person (as opposed to another company). A private company is not required to have a secretary but can choose to appoint one if they wish. A sole director may also be the company secretary.
+				</li>
+				<li>
+					<strong>Public limited companies</strong> must have at least two directors, one of whom must be an individual person and a company secretary. The company secretary must be formally qualified.
+				</li>
+			</ul>
+
+			<h3>Restrictions on company directors</h3>
+			<p>It is up to the members to appoint the people they believe will run the company well on their behalf. The only restrictions that prevent anyone becoming a director are:</p>
+			<ul>
+				<li>they must not have been disqualified from acting as a company director - unless the court has given them permission to act for a particular company</li>
+				<li>they must not be an undischarged bankrupt - unless they have been given permission by the court to act for a particular company</li>
+				<li>they must not be under the age of 16</li>
+			</ul>
+			
+			<h3>Can one person form a company?</h3>
+			<p>An individual can form a private company but the sole director must be an individual. A public company must be formed by at least two persons one of which must be an individual.</p>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/registration-documents-and-forms.php" title="Registration documents and forms">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/types-of-limited-company.php" title="Types of limited company">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public.php" title="Set up and register a limited company (private or public)">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/registration-documents-and-forms.php">Registration documents and forms</a></li>
+				<li>The company's officers</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/types-of-limited-company.php">Types of limited company</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/requirements-for-public-limited-companies.php">Requirements for public limited companies</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/where-to-register-your-company-and-get-help.php">Where to register your company and get help</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/tax-matters-of-a-limited-company.php">Tax matters of a limited company</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/checklist-setting-up-and-registering-a-limited-company-private-or-public.php">Checklist: setting up and registering a limited company (private or public)</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/right-to-manage-companies-and-commonhold-associations.php">Right to manage companies and commonhold associations</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+
+			<hr />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/publications/incorporation-and-names#ch2" target="_blank" rel="noopener noreferrer">Guidance for company directors and secretaries from Companies House</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20090609003228/http://www.berr.gov.uk/whatwedo/businesslaw/co-act-2006/index.html" target="_blank" rel="noopener noreferrer">Companies Act 2006 information from the Department for Business, Innovation &amp; Skills</a></li>
+			</ul>
+						
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/company-administration-an-introduction/company-directors-responsibilities.php">Company directors' responsibilities</a></li>
+				<li><a href="/tax-payroll-and-company-information/company-administration-an-introduction/company-secretaries-responsibilities.php">Company secretaries' responsibilities</a></li>
+				<li><a href="/tax-payroll-and-company-information/company-administration-an-introduction/companies-house-deadlines-and-late-filing-penalties.php">Companies House deadlines and late filing penalties</a></li>
+				<li><a href="/tax-payroll-and-company-information/company-administration-an-introduction/reporting-changes-to-companies-house/change-of-address-directors-and-more-what-to-report-to-companies-house.php">Change of address, directors and more - what to report to Companies House</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
