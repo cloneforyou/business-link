d13df05d9d5fb08a7a9088dfc3364cbf8e5f85b6
Caroline Hagan <info@blueocto.co.uk>
2017-02-25 20:21:21 +0000

    Completed “starting-up > business-structures > set-up-a-social-enterprise > community-interest-companies” page.

---
+++ starting-up/business-structures/set-up-a-social-enterprise/community-interest-companies.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Community Interest Companies | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Set up a social enterprise</h1>
+			<h2>Community Interest Companies</h2>
+			<p>Community Interest Companies (CICs) are limited companies that exist to provide benefits to a community, or a specific section of a community. The CIC has the flexibility of the familiar company form, and access to a range of financing options, so may be appropriate for those working for a social purpose.</p>
+			<p>Its key features include an asset lock and a community interest statement.</p>
+			<h3>Setting up a CIC</h3>
+			<p>Registering as a CIC is a single process.  When you register you choose to be a company limited by share or a company limited by guarantee. CICs must comply with the CIC Regulations and Company Law. See the page in this guide on <a href="/starting-up/business-structures/set-up-a-social-enterprise/limited-companies-with-a-social-purpose.php">limited companies with a social purpose</a>.</p>
+			<p>When registering your company with Companies House, you will need to provide additional documents, including a <strong>community interest statement</strong> describing your social purpose. The CIC Regulator will approve your application if your statement passes the <strong>community interest test</strong> - ie the business activities you intend to undertake will be carried out for the benefit of the community or a section of it, or that the CIC's purpose is in the community's or wider public's interest.</p>
+			<p><a href="https://www.gov.uk/government/collections/cic-regulator-forms" target="_blank" rel="noopener noreferrer">Read guidance on how to form a CIC on the Community Interest Companies website</a>.</p>
+			<p>CICs shouldn't be confused with charities. CICs cannot have charitable status but a charity can set up a CIC subsidiary company. This means they do not get the tax benefits of a charity, but in return they do not have the strict reporting requirements of a charity.</p>
+			<p>See the page in this guide on <a href="/starting-up/business-structures/set-up-a-social-enterprise/social-enterprises-as-registered-charities.php">social enterprises as registered charities</a>.</p>
+			
+			<h3>How CICs operate</h3>
+			<p>CICs have to follow specific rules, including the following:</p>
+			<ul>
+				<li>CICs must have an <strong>asset lock</strong>. This means that the company cannot generally transfer its profits or assets for less than their full market value except as permitted by regulation. It will also protect any remaining assets for the community if you dissolve the CIC.</li>
+				<li>If you set up your CIC as a company limited by shares, you'll have the option of <strong>issuing shares</strong> that pay a capped dividend to investors. The cap is set by the CIC Regulator to protect the asset lock.</li>
+				<li>Together with your annual accounts, you must present an <strong>annual community interest company report</strong> for public record. The report must show what the CIC has done during the year to pursue its pre-specified community interest and involve the individuals or groups with a particular interest in the CIC.</li>
+				<li>Certain voting rights changed on 1 October 2009. A CIC's chairperson no longer has the right to have a second or casting vote at a board meeting when the votes are equally divided. An alternate director can no longer - in the absence of their appointer - have a separate vote on behalf of their appointer as well as their own vote.</li>
+			</ul>
+			<p><a href="https://www.gov.uk/government/publications/incorporation-and-names#ch7" target="_blank" rel="noopener noreferrer">Find details of what fees you will need to pay to register as a CIC on the Companies House website</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/social-enterprises-as-registered-charities.php" title="Social enterprises as registered charities">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/charitable-incorporated-organisations.php" title="Charitable incorporated organisations">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php" title="Set up a social enterprise">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/unincorporated-associations.php">Unincorporated associations</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/trusts.php">Trusts</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/limited-companies-with-a-social-purpose.php">Limited companies with a social purpose</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/community-benefit-societies.php">Community benefit societies</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/social-enterprises-as-registered-charities.php">Social enterprises as registered charities</a></li>
+				<li>Community Interest Companies</li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise/charitable-incorporated-organisations.php">Charitable incorporated organisations</a></li>
+				<li><a href="/starting-up/considering-starting-up/thinking-about-starting-a-social-enterprise/here-is-how-being-a-social-enterprise-works-for-our-business.php">Here's how being a social enterprise works for our business</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/publications/incorporation-and-names#ch7" target="_blank" rel="noopener noreferrer">CIC fees information from Companies House </a></li>
+				<li><a href="https://www.gov.uk/government/collections/cic-regulator-forms" target="_blank" rel="noopener noreferrer">CIC formation guidance from Community Interest Companies</a></li>
+				<li><a href="https://www.gov.uk/government/publications/community-interest-companies-business-activities/cic-business-activities-forms-and-step-by-step-guidelines" target="_blank" rel="noopener noreferrer">CIC registration forms from Community Interest Companies</a></li>
+				<li><a href="../../../_files/LegalStructures.doc" target="_blank" rel="noopener noreferrer">Download our at-a-glance guide to legal structures for social enterprises (DOC, 43K)</a></li>
+				<li><a href="https://www.ncvo.org.uk/practical-support/information/governance" target="_blank" rel="noopener noreferrer">Good governance and leadership advice from the National Council for Voluntary Organisations</a></li>
+				<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance options for your business</a></li>
+				<li><a href="/do-it-online/incorporate-your-company-online.php">Incorporate your limited company online with Companies House</a></li>
+			</ul>
+						
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php" title="Company, partnership or sole trader - find the right structure for your new business">Legal structures: the basics</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public.php" title="The legal steps you need to take for company formation, with links to the relevant registration forms">Set up and register a limited company (private or public)</a></li>
+				<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/choose-the-right-finance-when-starting-up.php" title="The different finance options to consider when starting a new business">Choose the right finance when starting up</a></li>
+				<li><a href="/tax-payroll-and-company-information/changes-to-your-business/change-your-business-structure.php" title="You may want to switch from a partnership to a limited company, or the other way round - find out the implications">Change your business structure</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
