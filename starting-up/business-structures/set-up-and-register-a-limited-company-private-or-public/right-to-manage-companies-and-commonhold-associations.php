43dc886018de21570ffa56a092f73e85ebbe1c72
Caroline Hagan <info@blueocto.co.uk>
2017-02-25 21:30:50 +0000

    Completed “starting-up > business-structures > set-up-and-register-a-limited-company-private-or-public > right-to-manage-companies-and-commonhold-associations” page

---
+++ starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/right-to-manage-companies-and-commonhold-associations.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Right to manage companies and commonhold associations | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Set up and register a limited company (private or public)</h1>
+			<h2>Right to manage companies and commonhold associations</h2>
+			<p>If you are a right to manage (RTM) company or commonhold association then there are certain rules you need to follow for incorporation.</p>
+			<h3>RTM companies</h3>
+			<p>RTM companies were introduced under the Commonhold and Leasehold Reform Act 2002. This means that leaseholders can transfer the landlord's management functions relating to, for example, repairs and maintenance, to an RTM company registered by them.</p>
+			<p>The company must be limited by guarantee and its memorandum and articles must comply with the RTM Companies (Model Articles) (England) Regulations 2009.</p>
+			<p>To incorporate an RTM you need to complete application form IN01 to register a company. You can <a href="https://www.gov.uk/government/publications/register-a-private-or-public-company-in01" target="_blank" rel="noopener noreferrer">download form IN01 from the Companies House website (PDF, 1.03MB)</a>.</p>
+			<p>You may choose any name available so long as it ends with 'RTM Company Limited' or the Welsh equivalent, and it complies with rules on company names. You can <a href="../../../_files/clg_right-to-manage-rtm-companies-under-the-commonhold-and-leasehold-reform-act-2002.pdf" target="_blank" rel="noopener noreferrer">download RTM companies information from the Communities and Local Government website (PDF, 81K)</a>.</p>
+			<p>RTM companies do not exist in Scotland or Northern Ireland.</p>
+			
+			<h3>Commonhold associations</h3>
+			<p>Commonhold associations were introduced under the Commonhold and Leasehold Reform Act 2002. Commonhold is a form of land ownership available in England and Wales. The company must be limited by guarantee and its memorandum and articles must comply with the Commonhold (Amendment) Regulations 2009. You can <a href="../../../_files/dca_commonhold-non-statutory-guidance-on-the-commonhold-regulations-2004.pdf" target="_blank" rel="noopener noreferrer">download Commonhold Regulations information from the Department for Constitutional Affairs (DCA) website (PDF, 214K)</a>.</p>
+			<p>Commonhold associations:</p>
+			<ul>
+				<li>are an alternative to long leasehold ownership of flats and other interdependent properties</li>
+				<li>combine freehold ownership of a single property or unit in a larger development with membership of a limited company that owns and manages the common parts of the development - for example a block of flats where each flat is a unit and all the other parts, such as the hallway are commonhold</li>
+			</ul>
+
+			<p>To incorporate a commonhold association you need to complete application form IN01 to register a company. You can <a href="https://www.gov.uk/government/publications/register-a-private-or-public-company-in01" target="_blank" rel="noopener noreferrer">download form IN01 from the Companies House website (PDF, 1.03MB)</a>.</p>
+			<p>You may choose any name available as long as it ends with 'Commonhold Association Limited' or the Welsh equivalent and complies with rules on company names.</p>
+			<p>Commonhold associations do not exist in Scotland or Northern Ireland.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/checklist-setting-up-and-registering-a-limited-company-private-or-public.php" title="Checklist: setting up and registering a limited company (private or public)">Prev</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public.php" title="Set up and register a limited company (private or public)">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/registration-documents-and-forms.php">Registration documents and forms</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/the-companys-officers.php">The company's officers</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/types-of-limited-company.php">Types of limited company</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/requirements-for-public-limited-companies.php">Requirements for public limited companies</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/where-to-register-your-company-and-get-help.php">Where to register your company and get help</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/tax-matters-of-a-limited-company.php">Tax matters of a limited company</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/checklist-setting-up-and-registering-a-limited-company-private-or-public.php">Checklist: setting up and registering a limited company (private or public)</a></li>
+				<li>Right to manage companies and commonhold associations</li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="../../../_files/clg_right-to-manage-rtm-companies-under-the-commonhold-and-leasehold-reform-act-2002.pdf"target="_blank" rel="noopener noreferrer">Download RTM companies information from the Communities and Local Government website (PDF, 81K)</a></li>
+				<li><a href="https://www.gov.uk/government/publications/register-a-private-or-public-company-in01" target="_blank" rel="noopener noreferrer">Download form IN01 from Companies House (PDF, 1.03MB)</a></li>
+				<li><a href="../../../_files/dca_commonhold-non-statutory- guidance-on-the-commonhold-regulations-2004.pdf"target="_blank" rel="noopener noreferrer">Download commonhold association incorporation advice from the DCA (PDF, 214K)</a></li>
+			</ul>
+						
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php">Rules for naming your limited company or limited liability partnership (LLP)</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
