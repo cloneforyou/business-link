75d05300a6a72b7f4eccb3fbdd83a843382d973f
Caroline Hagan <info@blueocto.co.uk>
2017-02-25 20:52:37 +0000

    Completed “starting-up > business-structures > set-up-and-register-a-limited-company-private-or-public > types-of-limited-company” page

---
+++ starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/types-of-limited-company.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Types of limited company | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register a limited company (private or public)</h1>
+			<h2>Types of limited company</h2>
+			<p>There are four main types of company:</p>
+			<ul>
+				<li><strong>private</strong> company limited by shares</li>
+				<li><strong>private</strong> company limited by guarantee</li>
+				<li><strong>private</strong> unlimited company</li>
+				<li><strong>public</strong> limited company <strong>(plc)</strong></li>
+			</ul>
+			
+			<h3>Private company limited by shares</h3>
+			<p>Most private limited companies are owned by their shareholders and are <strong>limited by shares</strong>. This means that the liability of each member is limited to the amount unpaid on shares held by them.</p>
+			
+			<h3>Private company limited by guarantee</h3>
+			<p>Companies limited by guarantee do not have shares, and its members are guarantors rather than shareholders. The members' liability is limited to the amount they have agreed to contribute to the company's assets if it is wound up. This structure is often used by charities, Right to Manage, commonhold companies and social enterprises to limit the personal liability of their directors and trustees. See our guide on how to <a href="/starting-up/business-structures/set-up-a-social-enterprise.php">set up a social enterprise</a>.</p>
+			
+			<h3>Private unlimited company</h3>
+			<p>This type of company may or may not have a share capital but there is no limit to the members' liability. There are relatively few unlimited companies.</p>
+			
+			<h3>Public Limited Companies</h3>
+			<p>This type of company has a share capital and limits the liability of each member to the amount unpaid on their shares. PLCs:</p>
+			<ul>
+				<li>can raise money by selling shares on the stock market</li>
+				<li>
+					must have share capital of at least &pound;50,000 or the prescribed equivalent in euros
+				</li>
+				<li>
+					must have at least two directors and a qualified company secretary
+				</li>
+			</ul>
+
+			<p>See the page in this guide on <a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/requirements-for-public-limited-companies.php">requirements for public limited companies</a>.</p>
+			<p>A private company limited by shares can become a plc, but it will need to re-register in order to do this. For more information on changing from a private limited company to a plc, see our guide on <a href="/tax-payroll-and-company-information/company-administration-an-introduction/reporting-changes-to-companies-house/change-of-address-directors-and-more-what-to-report-to-companies-house.php">change of address, directors and more - what to report to Companies House</a>.</p>
+			
+			<h3>Naming your company</h3>
+			<p>You cannot choose a name that is the same as an existing company and you should avoid a name:</p>
+			<ul>
+				<li>
+					that people may find offensive
+				</li>
+				<li>
+					that is 'too like' an existing company name unless it is part of the same group
+				</li>
+			</ul>
+			
+			<p>You will need to obtain special approval for a name that:</p>
+			<ul>
+				<li>Suggests a connection to a government department, a devolved administration, a local authority or specified public body.</li>
+				<li>Includes any sensitive words and expressions that require approval from Companies House. For example, you are not allowed to choose a name which includes words that are potentially misleading, such as 'international' if you are a UK-only business.</li>
+			</ul>
+			
+			<p>See our guide on how to <a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php">choose the right name for your business</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/the-companys-officers.php" title="The company's officers">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/requirements-for-public-limited-companies.php" title="Requirements for public limited companies">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public.php" title="Set up and register a limited company (private or public)">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/registration-documents-and-forms.php">Registration documents and forms</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/the-companys-officers.php">The company's officers</a></li>
+				<li>Types of limited company</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/requirements-for-public-limited-companies.php">Requirements for public limited companies</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/where-to-register-your-company-and-get-help.php">Where to register your company and get help</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/tax-matters-of-a-limited-company.php">Tax matters of a limited company</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/checklist-setting-up-and-registering-a-limited-company-private-or-public.php">Checklist: setting up and registering a limited company (private or public)</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/right-to-manage-companies-and-commonhold-associations.php">Right to manage companies and commonhold associations</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+
+			<hr />
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/publications/incorporation-and-names#ch1"target="_blank" rel="noopener noreferrer">Company formation guidance from Companies House</a></li>
+				<li><a href="https://www.gov.uk/get-information-about-a-company" target="_blank" rel="noopener noreferrer">Use our interactive tool to search for an available company name and trade mark</a></li>
+				<li><a href="/do-it-online/incorporate-your-company-online.php">Incorporate your limited company online with Companies House</a></li>
+			</ul>
+						
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+				<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php">Choose the right name for your business</a></li>
+				<li><a href="/starting-up/business-structures/set-up-a-social-enterprise.php">Set up a social enterprise</a></li>
+				<li><a href="/tax-payroll-and-company-information/changes-to-your-business/change-your-business-structure.php">Change your business structure</a></li>
+				<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php">Rules for naming your limited company or limited liability partnership (LLP)</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
