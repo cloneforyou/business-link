2cf3ec96b1376f1d6d078f2ef773c893c62098c7
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 16:38:35 +0100

    Completed “starting-up > business-structures > set-up-and-register-as-self-employed > Checklist: setting up as self-employed” page.

---
+++ starting-up/business-structures/set-up-and-register-as-self-employed/checklist-setting-up-as-self-employed.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Checklist: setting up as self-employed | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register as self-employed</h1>
+			<h2>Checklist: setting up as self-employed</h2>
+			<p>To set yourself up as self-employed there are several things you must do. Make sure that you:</p>
+			<ul>
+				<li>register straight away for Self Assessment with HM Revenue &amp; Customs (HMRC) so they can set up your tax and National Insurance records</li>
+				<li>obtain any permits and planning permission that you may need from your local authority</li>
+				<li>contact your local authority to find out whether you need to pay business rates</li>
+				<li>contact HMRC to register for VAT if you expect to have turnover of more than &pound;73,000 a year</li>
+				<li>register with HMRC for PAYE (Pay As You Earn) if you employ staff</li>
+				<li>register with HMRC if you are a contractor or subcontractor in the construction industry</li>
+				<li>set up a financial record-keeping system</li>
+				<li>put your name on all your business stationery, including letters, invoices, receipts and cheques</li>
+			</ul>
+
+			<p>It's worth remembering, though, that this is just a start. As you continue in business, you may well have other legal and tax issues to bear in mind. <a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a>.</p>
+			<p>For more information on National Insurance contributions, <a href="/tax-payroll-and-company-information/national-insurance/identify-the-national-insurance-contributions-you-must-pay.php">use our interactive tool to identify the National Insurance contributions you must pay</a>.</p>
+			<p>The Citizens Advice Bureau provides additional information on a wide range of self-employment topics and gives a list of other organisations to contact for specialist advice. <a href="https://www.citizensadvice.org.uk/work/self-employed-or-looking-for-work/self-employment-checklist/" target="_blank" rel="noopener noreferrer">Find sources of advice on self-employment issues on the Advice Guide website</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/checking-your-self-employment-status.php" title="Checking your self-employment status">Prev</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed.php" title="Set up and register as self-employed">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/setting-up-as-self-employed.php">Setting up as self-employed</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/registering-for-tax-and-national-insurance.php">Registering for tax and National Insurance</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/arranging-to-make-national-insurance-contributions.php">Arranging to make National Insurance contributions</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/checking-your-self-employment-status.php">Checking your self-employment status</a></li>
+				<li>Checklist: setting up as self-employed</li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<dl>
+				<dt></dt>
+				<dd>
+					 <a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer"><img src="../../../_img/HMRC_logo.gif" width="134" height="35" border="0" alt="" />HM Revenue &amp; Customs</a>
+				</dd>
+			</dl>
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/set-up-a-basic-record-keeping-system.php">Set up a basic record-keeping system</a></li>
+				<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business.php">Set up a simple profit and loss account for your business</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
