dbfbb1ef0e0355d3e356a42db9b96a0c8af9967d
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 18:41:25 +0100

    Completed “starting-up > business-structures > Set up and register a limited partnership > How to register a limited partnership” page.

---
+++ starting-up/business-structures/set-up-and-register-a-limited-partnership/how-to-register-a-limited-partnership.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>How to register a limited partnership | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register a limited partnership</h1>
+			<h2>How to register a limited partnership</h2>
+			<p>You must register a limited partnership in accordance with the Limited Partnership Act 1907.</p>
+			<p>To register, you need to complete a limited partnership registration form - LP5 - have it signed by all the partners of the business, and return it to the Registrar of Companies. Once form LP5 has been accepted and registered, the limited partnership will officially come into existence.</p>
+			<p>You can <a href="https://www.gov.uk/government/publications/apply-for-a-registration-of-a-limited-partnership-lp5" target="_blank" rel="noopener noreferrer">download limited partnership registration form LP5 from the Companies House website (PDF, 244K)</a>.</p>
+			<p>You must include all possible information on the limited partnership registration form, specifically the:</p>
+			<ul>
+				<li>firm's name and appropriate ending - for example 'Limited Partnership' or 'LP'</li>
+				<li>nature of the business</li>
+				<li>business address</li>
+				<li>full names of each partner - with separate lists for general and limited partners</li>
+				<li>length of the partnership - if applicable</li>
+				<li>statement that the partnership is limited and a description of each limited partner</li>
+				<li>amount that each limited partner is contributing - and the form that the contribution is to take</li>
+			</ul>
+
+			<p>If all this information - the particulars - is included correctly, the Registrar of Companies will issue a certificate of registration.</p>
+			<p>The standard fee for registering a limited partnership is &pound;20 - or &pound;100 for a same-day registration.</p>
+			<p>Overseas limited partnerships cannot be registered as the business address must be an area of the UK where the principal place of business is situated.</p>
+			
+			<h3>Changing limited partnership details</h3>
+			<p>You can change any details about your limited partnership by completing and returning form LP6 to Companies House. You must notify the Registrar of Companies within seven days of the change being made.</p>
+			<p><a href="https://www.gov.uk/government/publications/specify-the-nature-of-a-change-in-the-limited-partnership-lp6" target="_blank" rel="noopener noreferrer">Download limited partnership form LP6 from the Companies House website (PDF, 324K)</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership/tax-matters-of-a-limited-partnership.php" title="Definition of a limited partnership and partners">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership/tax-matters-of-a-limited-partnership.php" title="Tax matters of a limited partnership">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership.php" title="Set up and register a limited partnership">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership/tax-matters-of-a-limited-partnership.php">Definition of a limited partnership and partners</a></li>
+				<li>How to register a limited partnership</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership/tax-matters-of-a-limited-partnership.php">Tax matters of a limited partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership/dissolving-a-limited-partnership.php">Dissolving a limited partnership</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<dl>
+				<dt></dt>
+				<dd>
+					<a href="https://www.gov.uk/government/organisations/companies-house" target="_blank" rel="noopener noreferrer"><img src="../../../_img/ch_logo_new_3.jpg" width="130" height="35" border="0" alt="" />Companies House</a>
+				</dd>
+			</dl>
+			
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+
+			<hr />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/publications/apply-for-a-registration-of-a-limited-partnership-lp5" target="_blank" rel="noopener noreferrer">Download limited partnership registration form LP5 from Companies House (PDF, 244K)</a></li>
+				<li><a href="https://www.gov.uk/government/publications/specify-the-nature-of-a-change-in-the-limited-partnership-lp6" target="_blank" rel="noopener noreferrer">Download limited partnership form LP6 from Companies House (PDF, 324K)</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php" title="Company, partnership or sole trader - find the right structure for your new business">Legal structures: the basics</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
