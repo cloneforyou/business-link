13871dab2b01f957372a4048f8d96f545c632799
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 17:05:41 +0100

    Completed “starting-up > business-structures > Set up and register a partnership > Checklist: setting up a partnership” page.

---
+++ starting-up/business-structures/set-up-and-register-a-partnership/checklist-setting-up-a-partnership.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Checklist: setting up a partnership | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register a partnership</h1>
+			<h2>Checklist: setting up a partnership</h2>
+			<p>In order to set up business as a partnership there are certain things you need to do - some must be done as a group and others as individual partners. You should:</p>
+			<ul>
+				<li>Display all the partners' names at all your business premises together with the address to which official documents should be sent.</li>
+				<li>Display all the partners' names on your business website and stationery, including letters, invoices, receipts and cheques along with your principal place of business. If the partnership has more than 20 partners you need only display your principal place of business.</li>
+				<li>Register the partnership, and each partner, for Self Assessment with HM Revenue &amp; Customs (HMRC).</li>
+				<li>Contact HMRC to register your partnership for VAT if you expect a turnover of more than &pound;73,000 a year.</li>
+				<li>Register with HMRC for PAYE (Pay As You Earn) if the partnership employs staff.</li>
+				<li>Register with HMRC for the Construction Industry Scheme if the partnership is a contractor or sub-contractor.</li>
+			</ul>
+
+			<p>It's worth remembering that this is just a start. As an ongoing business, your partnership will have many other legal and tax obligations to bear in mind. <a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/tax-matters-of-a-partnership.php" title="Tax matters of a partnership">Prev</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership.php" title="Set up and register a partnership">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/the-officers-of-a-partnership.php">The officers of a partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/types-of-partner.php">Types of partner</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/setting-up-a-deed-of-partnership.php">Setting up a deed of partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/naming-your-partnership.php">Naming your partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/tax-matters-of-a-partnership.php">Tax matters of a partnership</a></li>
+				<li>Checklist: setting up a partnership</li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
