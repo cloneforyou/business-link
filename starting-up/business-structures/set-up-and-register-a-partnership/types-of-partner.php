388b4ca0c536efe73fd85563f6aaa76fc76eb14a
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 16:51:00 +0100

    Completed “starting-up > business-structures > Set up and register a partnership > Types of partner” page.

---
+++ starting-up/business-structures/set-up-and-register-a-partnership/types-of-partner.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Types of partner | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register a partnership</h1>
+			<h2>Types of partner</h2>
+			<p>There are three main types of partner, each of which has different rights and responsibilities.</p>
+			<p><strong>General partners</strong> invest in the business, take part in running it and share in its profits. Each general partner is fully liable for any debts that the partnership may have. This means that they could lose more than their initial investment in the business if it runs into trouble, and that their personal assets could be at risk. Every ordinary and limited partnership must have at least one general partner.</p>
+			<p><strong>Limited partners</strong> are not permitted to participate in the day-to-day running of the business. Their debt is limited to the amount of their initial investment.</p>
+			<p><strong>Sleeping partners</strong> invest money in the business and share in its profits, but do not take part in running it. Like general partners, they are fully liable for the partnership's debts.</p>
+			<h3>Limited partnerships and limited liability partnerships</h3>
+			<p>For information about setting up limited partnerships and limited liability partnerships, see our guides on how to <a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership.php">set up and register a limited partnership</a> and <a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp.php">set up and register a limited liability partnership (LLP)</a>.</p>
+			<h3>Companies and limited liability partnerships (LLPs) as members of partnerships</h3>
+			<p>As well as individuals, other legal entities - such as companies and LLPs - can also be members of partnerships. They have the same rights and responsibilities within the partnership as other partners. See the page in this guide on <a href="/starting-up/business-structures/set-up-and-register-a-partnership/tax-matters-of-a-partnership.php">tax matters of a partnership</a>.</p>
+			<p>Partnerships whose members are all companies have to prepare 'partnership accounts' and send them to Companies House each year. Members of such partnerships must attach a copy of these documents to their own company accounts when they submit them to Companies House.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/the-officers-of-a-partnership.php" title="The officers of a partnership">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/setting-up-a-deed-of-partnership.php" title="Setting up a deed of partnership">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership.php" title="Set up and register a partnership">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/the-officers-of-a-partnership.php">The officers of a partnership</a></li>
+				<li>Types of partner</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/setting-up-a-deed-of-partnership.php">Setting up a deed of partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/naming-your-partnership.php">Naming your partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/tax-matters-of-a-partnership.php">Tax matters of a partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/checklist-setting-up-a-partnership.php">Checklist: setting up a partnership</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership.php">Set up and register a limited partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp.php">Set up and register a limited liability partnership (LLP)</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
