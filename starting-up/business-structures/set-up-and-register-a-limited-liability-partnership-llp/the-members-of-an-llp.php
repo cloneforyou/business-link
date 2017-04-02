69347868f05f19adc4e19b47108b3e6d459ff732
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 19:00:35 +0100

    Completed “starting-up > business-structures > Set up and register a limited liability partnership (LLP) > The members of an LLP” page.

---
+++ starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/the-members-of-an-llp.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>The members of an LLP | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register a limited liability partnership (LLP)</h1>
+			<h2>The members of an LLP</h2>
+			<p>Any group of two or more people who want to set up a profit-making business together can form a limited liability partnership (LLP), unless one of them has a disqualification order (disqualified to act as a company director or LLP member) in place. It is also possible for companies, and other LLPs, to be members of an LLP.</p>
+			
+			<h3>How many members should an LLP have?</h3>
+			<p>An LLP must have at least two members, and at least two must be 'designated' members. If there are fewer than two designated members then every member is deemed to be a designated member.</p>
+			
+			<h3>The rights and responsibilities of LLP members</h3>
+			<p>The members of an LLP normally share in both the responsibilities of running the business and the profits that it makes. Exactly how their rights and responsibilities are defined and divided depends on the LLP's partnership agreement or 'deed of partnership'. Designated members have some extra responsibilities on top of those of ordinary members.</p>
+			
+			<h3>The responsibilities of designated members</h3>
+			<p>Designated members have to ensure that the LLP meets its legal obligations by:</p>
+			<ul>
+				<li>registering the LLP at Companies House</li>
+				<li>appointing an auditor - if one is needed</li>
+				<li>preparing and signing the accounts on behalf of the members</li>
+				<li>delivering the accounts to Companies House</li>
+				<li>notifying Companies House of any membership changes, or of a change to the registered office address or name of the LLP</li>
+				<li>preparing, signing and delivering the annual return to Companies House</li>
+				<li>acting on behalf of the LLP if it is wound up and dissolved</li>
+			</ul>
+			<p>Designated members are legally accountable if they fail to carry out their duties properly.</p>
+			
+			<h3>Changes to members' details or status</h3>
+			<p>It is important that you notify Companies House whenever any of the members' details change. For more information on what you need to file, see our guide on <a href="/tax-payroll-and-company-information/changes-to-your-business/changes-within-a-partnership.php">changes within a partnership</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp.php" title="Set up and register a limited liability partnership (LLP)">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/where-to-register-your-llp-and-get-help.php" title="Where to register your LLP and get help">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp.php" title="Set up and register a limited liability partnership (LLP)">Introduction</a></li>
+				<li>The members of an LLP</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/where-to-register-your-llp-and-get-help.php">Where to register your LLP and get help</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/deed-of-partnership-of-an-llp.php">Deed of partnership of an LLP</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/naming-your-llp.php">Naming your LLP</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/tax-matters-of-an-llp.php">Tax matters of an LLP</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/checklist-setting-up-and-registering-an-llp.php">Checklist: setting up and registering an LLP</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+			
+			<hr />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/publications/limited-liability-partnership-incorporation-and-names" target="_blank" rel="noopener noreferrer">LLP membership requirements from Companies House</a></li>
+				<li><a href="https://www.gov.uk/government/publications/limited-liability-partnership-incorporation-and-names#ch5" target="_blank" rel="noopener noreferrer">Changing an LLP's name guidance from Companies House</a></li>
+				<li><a href="https://www.gov.uk/government/publications/life-of-a-limited-liability-partnership" target="_blank" rel="noopener noreferrer">LLP filing requirements from Companies House</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/changes-to-your-business/changes-within-a-partnership.php">Changes within a partnership</a></li>
+				<li><a href="/tax-payroll-and-company-information/companies-house-annual-returns-and-accounts/choose-and-manage-auditors.php">Choose and manage auditors</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
