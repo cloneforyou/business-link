a27b35630d0b8ccca12a7c6fb9b6fb5fc2d070c2
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 16:56:41 +0100

    Completed “starting-up > business-structures > Set up and register a partnership > Setting up a deed of partnership” page.

---
+++ starting-up/business-structures/set-up-and-register-a-partnership/setting-up-a-deed-of-partnership.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Setting up a deed of partnership | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register a partnership</h1>
+			<h2>Setting up a deed of partnership</h2>
+			<p>A deed of partnership is a legally binding agreement between partners who are in business together. It describes how the partnership will be run and the rights and duties of the partners themselves.</p>
+			<p>It's not necessary to have a deed in order to set up a partnership, but it's a good idea, as it can help to prevent misunderstandings and disputes. It may be a good idea to enlist a solicitor to ensure that each partner understands their responsibilities before the deed is finalised.</p>
+			<p>As well as giving basic information about the partnership, such as its business name and the names of the partners, the type of business and business address, the deed will usually set out:</p>
+			<ul>
+				<li>the amount of capital that each partner is to contribute to the business</li>
+				<li>the way in which partners will share profits or losses, and whether any of the partners should be paid a salary</li>
+				<li>working arrangements, such as how much time each partner should contribute to the business, who does what management tasks and what type of decisions need collective agreement between the partners </li>
+				<li>
+					changes to the partnership, such as how new partners can be appointed and what happens if a partner dies or wishes to leave
+				</li>
+			</ul>
+
+			<p>If the partnership does not have a deed, it will be governed by the terms of the Partnership Act 1890. This does not offer solutions to many of the problems that can arise and may not suit the way that you and your partners want to work together. <a href="http://webarchive.nationalarchives.gov.uk/20140109143644/http://www.hmrc.gov.uk/manuals/bimmanual/bim72501.htm" target="_blank" rel="noopener noreferrer">Read about the Partnership Act 1890 on the HM Revenue &amp; Customs (HMRC) website</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/types-of-partner.php" title="Types of partner">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/naming-your-partnership.php" title="Naming your partnership">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership.php" title="Set up and register a partnership">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/the-officers-of-a-partnership.php">The officers of a partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/types-of-partner.php">Types of partner</a></li>
+				<li>Setting up a deed of partnership</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/naming-your-partnership.php">Naming your partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/tax-matters-of-a-partnership.php">Tax matters of a partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/checklist-setting-up-a-partnership.php">Checklist: setting up a partnership</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20140109143644/http://www.hmrc.gov.uk/manuals/bimmanual/bim72501.htm" target="_blank" rel="noopener noreferrer">Partnership Act 1890 guidance from HMRC</a></li>
+				<li><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Solicitor search from the Law Society</a></li>
+				<li><a href="https://www.cedr.com/" target="_blank" rel="noopener noreferrer">Dispute resolution advice and resources from the Centre for Effective Dispute Resolution</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/finance-and-grants/expert-financial-advice/choose-and-work-with-a-solicitor.php">Choose and work with a solicitor</a></li>
+				<li><a href="/tax-payroll-and-company-information/changes-to-your-business/changes-within-a-partnership.php">Changes within a partnership</a></li>
+				<li><a href="/do-it-online.php">Do it online</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership.php">Set up and register a limited partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp.php">Set up and register a limited liability partnership (LLP)</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
