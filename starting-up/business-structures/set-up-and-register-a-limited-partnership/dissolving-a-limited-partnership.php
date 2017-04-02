a0322b0847cf33110e0b1b88d7ef6984c9f8c9fd
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 18:46:41 +0100

    Completed “starting-up > business-structures > Set up and register a limited partnership > Dissolving a limited partnership” page.

---
+++ starting-up/business-structures/set-up-and-register-a-limited-partnership/dissolving-a-limited-partnership.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Dissolving a limited partnership | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register a limited partnership</h1>
+			<h2>Dissolving a limited partnership</h2>
+			<p>You can dissolve your limited partnership for any number of reasons - for example financial reasons, business purposes or personal disagreements. For limited partnerships, the dissolution must be handled by the general partners unless the court decides otherwise.</p>
+			<p>A limited partnership cannot be dissolved through any of the following circumstances:</p>
+			<ul>
+				<li>a limited partner giving notice - unless there is a previous agreement between the partners</li>
+				<li>a limited partner offering his share as security for a debt - unless there is a previous agreement between the partners</li>
+				<li>the death or bankruptcy of a limited partner</li>
+				<li>a limited partner is considered a 'person of unsound mind' - unless their share cannot be determined</li>
+			</ul>
+			
+			<h3>Notifying Companies House</h3>
+			<p>The general partners are responsible for filing Forms LP5 and LP6 at Companies House even if accountants or other professionals have helped in their preparation.</p>
+			<p>The Limited Partnerships Act 1907 provides for the levying of penalties for failing to send the required forms to the Registrar.</p>
+			<p>There is no specific requirement for you to notify Companies House when dissolving a limited partnership. However, if you do notify Companies House of the dissolution of your limited partnership by filling in a form LP6, it will be accepted in good faith but the name will still remain on the index of live company names.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership/tax-matters-of-a-limited-partnership.php" title="Tax matters of a limited partnership">Prev</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership.php" title="Set up and register a limited partnership">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership/tax-matters-of-a-limited-partnership.php">Definition of a limited partnership and partners</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership/how-to-register-a-limited-partnership.php">How to register a limited partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership/tax-matters-of-a-limited-partnership.php">Tax matters of a limited partnership</a></li>
+				<li>Dissolving a limited partnership</li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<dl>
+				<dt></dt>
+				<dd>
+					<a href="https://www.gov.uk/government/organisations/companies-house" target="_blank" rel="noopener noreferrer"><img src="../../../_img/ch_logo_new_3.jpg" width="130" height="35" border="0" alt="" />Companies House</a>
+				</dd>
+			</dl>
+			
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
