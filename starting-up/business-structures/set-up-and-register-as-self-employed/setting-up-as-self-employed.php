68bd1dc52d287c210f908f65f8016e47b8b6dfd5
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 15:35:40 +0100

    Completed “starting-up > business-structures > set-up-and-register-as-self-employed > setting-up-as-self-employed” page.

---
+++ starting-up/business-structures/set-up-and-register-as-self-employed/setting-up-as-self-employed.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Setting up as self-employed | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+
+			<h1>Set up and register as self-employed</h1>
+			<h2>Setting up as self-employed</h2>
+			<p>Anyone can set up in business as a sole trader, although for certain types of work you may need a licence or permission from your local authority. Restaurants, childminders, cab drivers and street traders, for example, all need to have a <strong>local authority licence</strong>. Your qualifications and business premises may be inspected beforehand to ensure you comply with regulations.</p>
+			<h3>Working from home</h3>
+			<p>If you run a business from home, you may have to pay <strong>business rates</strong> for the part of your home that you use for your business. This mainly depends on whether the business area of your home is also used for domestic purposes. If you simply work on a computer in a bedroom, for example, you will probably not have to pay business rates. See our guide: <a href="/tax-payroll-and-company-information/business-rates.php">business rates - an overview</a>.</p>
+			<p>You may need to get planning permission to set up a business at home. For example, if your business could cause a nuisance to your neighbours, or if you intend to do any building work to adapt your home. If in doubt, contact the planning department of your local authority. <a href="https://www.planningportal.co.uk/info/200125/do_you_need_permission" target="_blank" rel="noopener noreferrer">Find planning permission guidance on the Planning Portal website</a>.</p>
+			<p>Bear in mind that there may also be restrictions in the deeds of your property or, if you rent, in the rental or lease agreement.</p>
+			<h3>Choosing a name for your business</h3>
+			<p>You can trade under your <strong>own name</strong>, eg M.Y.Self, or use another business name, eg Peerless Promotions.</p>
+			<p>If you decide to use a business name, you must make sure that your business <strong>stationery</strong> displays your name as well as the trading name of the business for example, M.Y.Self, trading as Peerless Promotions.</p>
+			<p>Your trading name should not be the same as, or too similar to, that of a business which already exists. Be careful that it does not contain words that people might find <strong>offensive or misleading</strong>. See our guide on how to <a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php" title="How to find the right name for your business and the rules and regulations involved in registering a name">choose the right name for your business</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed.php" title="Set up and register as self-employed">Prev</a></li>
+				<li><a href="detail4897.html?itemId=1083844795&amp;type=RESOURCES" title="Registering for tax and National Insurance">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed.php" title="Set up and register as self-employed">Introduction</a></li>
+				<li>Setting up as self-employed</li>
+				<li><a href="detail4897.html?itemId=1083844795&amp;type=RESOURCES">Registering for tax and National Insurance</a></li>
+				<li><a href="detail9aa4.html?itemId=1073789949&amp;type=RESOURCES">Arranging to make National Insurance contributions</a></li>
+				<li><a href="detail3b1c.html?itemId=1073789948&amp;type=RESOURCES">Checking your self-employment status</a></li>
+				<li><a href="detail9ef1.html?itemId=1073789950&amp;type=RESOURCES">Checklist: setting up as self-employed</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<dl>
+				<dt></dt>
+				<dd>
+					<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer"><img src="../../../_img/HMRC_logo.gif" width="134" height="35" border="0" alt="" />HM Revenue &amp; Customs</a>
+				</dd>
+			</dl>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.planningportal.co.uk/info/200125/do_you_need_permission" target="_blank" rel="noopener noreferrer">Planning permission guidance from the Planning Portal</a></li>
+				<li><a href="https://www.gov.uk/correct-your-business-rates" target="_blank" rel="noopener noreferrer">Working from home guidance from VOA</a></li>
+				<li><a href="https://www.citizensadvice.org.uk/work/self-employed-or-looking-for-work/self-employment-checklist/" target="_blank" rel="noopener noreferrer">Sources of advice on self-employment issues from Advice Guide</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20140109143644/http://hmrc.gov.uk/startingup/help-support.htm" target="_blank" rel="noopener noreferrer">Local HM Revenue &amp; Customs Business Support Team contact details from HMRC</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php" title="How to find the right name for your business and the rules and regulations involved in registering a name">Choose the right name for your business</a></li>
+				<li><a href="/tax-payroll-and-company-information/business-rates.php" title="What business rates are used for and the meanings of key words and phrases">Business rates - an overview</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
