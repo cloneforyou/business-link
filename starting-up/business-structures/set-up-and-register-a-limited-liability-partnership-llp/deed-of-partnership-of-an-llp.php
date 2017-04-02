1b035245a01c38272976e77774ef6dbb4adf8405
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 19:14:57 +0100

    Completed “starting-up > business-structures > Set up and register a limited liability partnership (LLP) > Deed of partnership of an LLP” page.

---
+++ starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/deed-of-partnership-of-an-llp.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Deed of partnership of an LLP | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register a limited liability partnership (LLP)</h1>
+			<h2>Deed of partnership of an LLP</h2>
+			<p>A deed of partnership (or 'partnership agreement') is a legally binding agreement between the partners who are in business together. It describes how the partnership will be run and the rights and duties of the members themselves. The deed of partnership is usually drawn up by a solicitor, who will consult with the partners about exactly what should be in it.</p>
+			<p>It's not absolutely necessary to have a deed in order to set up a limited liability partnership, but it's a good idea as it can help to prevent misunderstandings and disputes between members.</p>
+			
+			<h3>What does the deed of partnership cover?</h3>
+			<p>As well as giving basic information about the partnership, such as its business name and the names of the partners, the type of business and business address, the deed will usually set out:</p>
+			<ul>
+				<li>the amount of capital that each partner is to contribute to the business</li>
+				<li>the way in which profits or losses are shared between partners, and whether any of the partners should be paid a salary</li>
+				<li>working arrangements, such as how much time each partner should contribute to the business, who does what management tasks and what type of decisions need collective agreement between the partners</li>
+				<li>
+					changes to the partnership, such as how new partners can be appointed and what happens if a partner dies or wishes to leave
+				</li>
+			</ul>
+			
+			<p>If members do not have a deed, they will be governed by the terms of the Limited Liability Partnerships Act 2000 (LLP Act 2000), which does not offer solutions to many of the problems that can arise and may not suit the way that you and your members want to work together. <a href="https://www.gov.uk/hmrc-internal-manuals/inheritance-tax-manual/ihtm25094" target="_blank" rel="noopener noreferrer">Read about the LLP Act 2000 on the HM Revenue &amp; Customs (HMRC) website</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/where-to-register-your-llp-and-get-help.php" title="Where to register your LLP and get help">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/naming-your-llp.php" title="Naming your LLP">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp.php" title="Set up and register a limited liability partnership (LLP)">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/the-members-of-an-llp.php">The members of an LLP</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/where-to-register-your-llp-and-get-help.php">Where to register your LLP and get help</a></li>
+				<li>Deed of partnership of an LLP</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/naming-your-llp.php">Naming your LLP</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/tax-matters-of-an-llp.php">Tax matters of an LLP</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/checklist-setting-up-and-registering-an-llp.php">Checklist: setting up and registering an LLP</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+
+			<hr />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/hmrc-internal-manuals/inheritance-tax-manual/ihtm25094" target="_blank" rel="noopener noreferrer">LLP Act 2000 guidance from HMRC</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20140109143644/http://www.hmrc.gov.uk/manuals/bimmanual/bim72501.htm" target="_blank" rel="noopener noreferrer">Partnership Act 1890 guidance from HMRC </a></li>
+				<li><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Solicitor search from the Law Society</a></li>
+				<li><a href="https://web-beta.archive.org/web/20130420094152/http://www.accaglobal.com/en/discover/find-accountant.html" target="_blank" rel="noopener noreferrer">Accountant search from the Association of Chartered Certified Accountants</a></li>
+				<li><a href="../../../_files/compact-law_sample-partnership-agreement.pdf" target="_blank" rel="noopener noreferrer">Download a sample partnership agreement from the Compact Law website (PDF, 74K)</a></li>
+				<li><a href="https://www.cedr.com/" target="_blank" rel="noopener noreferrer">Dispute resolution advice and resources from the Centre for Effective Dispute Resolution</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/finance-and-grants/expert-financial-advice/choose-and-work-with-a-solicitor.php">Choose and work with a solicitor</a></li>
+				<li><a href="/tax-payroll-and-company-information/changes-to-your-business/changes-within-a-partnership.php">Changes within a partnership</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
