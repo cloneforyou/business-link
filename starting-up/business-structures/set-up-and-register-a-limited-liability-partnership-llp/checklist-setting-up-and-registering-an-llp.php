0f5626765af7af9e6a2773f8acf2bd11df648185
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 19:22:17 +0100

    Completed “starting-up > business-structures > Set up and register a limited liability partnership (LLP) > Checklist: setting up and registering an LLP” page.

---
+++ starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/checklist-setting-up-and-registering-an-llp.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Checklist: setting up and registering an LLP | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register a limited liability partnership (LLP)</h1>
+			<h2>Checklist: setting up and registering an LLP</h2>
+			<p>As well as registering your limited liability partnership (LLP) with Companies House, there are several other things that you need to do to put it on a proper legal footing and maintain its status. Make sure you:</p>
+			<ul>
+				<li>send Companies House a completed and signed incorporation document - form LL IN01 - together with the appropriate filing fee</li>
+				<li>display your LLP's name on the outside of all its offices or other places of business</li>
+				<li>display your LLP's name on all its business stationery, including letters, invoices, receipts and cheques</li>
+				<li>show your LLP's place of registration, registered number and registered office address on all its business letters, order forms and electronic business communications</li>
+				<li>check that you have received a Certificate of Incorporation from Companies House</li>
+				<li>register with HM Revenue &amp; Customs (HMRC) for VAT, PAYE or CIS if appropriate</li>
+				<li>make sure all partners are registered with HMRC for Self Assessment</li>
+				<li>send a Self Assessment tax return for the LLP to HMRC</li>
+				<li>send a set of accounts to Companies House every year</li>
+				<li>send Companies House an annual return - form LL AR01 - in order to keep your LLP's records up to date</li>
+				<li>inform Companies House of any changes to your LLP's membership, the personal details of its members or the address of its registered office</li>
+			</ul>
+
+			<p>If the partnership has a website, it will need to show the partnership's name, registered office and geographic addresses, registered number, VAT number and the place of registration. It must also include an email address.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/tax-matters-of-an-llp.php" title="Tax matters of an LLP">Prev</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp.php" title="Set up and register a limited liability partnership (LLP)">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/the-members-of-an-llp.php">The members of an LLP</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/where-to-register-your-llp-and-get-help.php">Where to register your LLP and get help</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/deed-of-partnership-of-an-llp.php">Deed of partnership of an LLP</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/naming-your-llp.php">Naming your LLP</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/tax-matters-of-an-llp.php">Tax matters of an LLP</a></li>
+				<li>Checklist: setting up and registering an LLP</li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+			
+			<hr />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/publications/register-a-limited-liability-partnership-ll-in01" target="_blank" rel="noopener noreferrer">Download an application form for incorporation of an LLP from the Companies House website (PDF, 866K)</a></li>
+				<li><a href="https://www.gov.uk/government/publications/limited-liability-partnership-incorporation-and-names#ch7" target="_blank" rel="noopener noreferrer">Disclose requirements for LLPs from Companies House</a></li>
+				<li><a href="https://www.gov.uk/government/publications/file-your-limited-liability-partnership-annual-return-ll-ar01" target="_blank" rel="noopener noreferrer">Download the LLP annual return form from Companies House (PDF, 827K)</a></li>
+				<li><a href="https://www.gov.uk/government/publications/life-of-a-limited-liability-partnership" target="_blank" rel="noopener noreferrer">LLP filing requirements from Companies House</a></li>
+				<li><a href="http://online.businesslink.gov.uk/bdotg/action/cntc" title="Use our interactive tool to search for an available company name and trade mark">Use our interactive tool to search for an available company name and trade mark</a></li>
+				<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+				<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php">Rules for naming your limited company or limited liability partnership (LLP)</a></li>
+				<li><a href="layer12e0.html?topicId=1085038569">Change of address, directors and more - what to report to Companies House</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public.php">Set up and register a limited company (private or public)</a></li>
+				<li><a href="/do-it-online.php">Do it online</a></li>
+				<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
