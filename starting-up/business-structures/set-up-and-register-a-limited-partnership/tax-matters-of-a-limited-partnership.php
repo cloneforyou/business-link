11235fb224094920ed93b2bd8bf9cbff08a1b110
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 18:45:35 +0100

    Completed “starting-up > business-structures > Set up and register a limited partnership > Tax matters of a limited partnership” page.

---
+++ starting-up/business-structures/set-up-and-register-a-limited-partnership/tax-matters-of-a-limited-partnership.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Tax matters of a limited partnership | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register a limited partnership</h1>
+			<h2>Tax matters of a limited partnership</h2>
+			<p>Once you've registered the limited partnership with Companies House, using form LP5, they will notify HM Revenue and Customs (HMRC), so you don't need to do this. HMRC will then set up the right tax records for the partnership. Each partner in the limited partnership must register with HMRC separately to get their own tax records set up.</p>
+			<p><a href="https://www.gov.uk/government/publications/self-assessment-register-a-partnership-for-self-assessment-sa400" target="_blank" rel="noopener noreferrer">Find the form you need to register as a partner on the HMRC website</a>.</p>
+			<p>Generally, limited partnerships are treated the same as general partnerships for tax purposes. Like general partnerships, profits are shared amongst the members of a limited partnership. Individual members - not the limited partnership - pay tax on income or gains. Unlike limited companies, limited partnerships are not liable for Corporation Tax. The nominated partner will need to complete a Self Assessment tax return for the partnership every year. Each member of the partnership will need to show their share of the profits on their own tax return.</p>
+			<p>For more on tax see the page on <a href="/starting-up/business-structures/set-up-and-register-a-partnership/tax-matters-of-a-partnership.php">tax matters of a partnership</a> in our guide on how to <a href="/starting-up/business-structures/set-up-and-register-a-partnership.php">set up and register a partnership</a>. This guide includes information on VAT, PAYE for employers and the Construction Industry Scheme.</p>
+			<p>For information about Self Assessment see our guide: <a href="/tax-payroll-and-company-information/self-assessment/your-tax-return-the-basics/introduction-to-self-assessment.php">introduction to Self Assessment</a>.  </p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership/how-to-register-a-limited-partnership.php" title="How to register a limited partnership">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership/dissolving-a-limited-partnership.php" title="Dissolving a limited partnership">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership.php" title="Set up and register a limited partnership">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership/tax-matters-of-a-limited-partnership.php">Definition of a limited partnership and partners</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership/how-to-register-a-limited-partnership.php">How to register a limited partnership</a></li>
+				<li>Tax matters of a limited partnership</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership/dissolving-a-limited-partnership.php">Dissolving a limited partnership</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<dl>
+				<dt></dt>
+				<dd>
+					<a href="https://www.gov.uk/government/organisations/companies-house" target="_blank" rel="noopener noreferrer"><img src="../../../_img/ch_logo_new_3.jpg" width="130" height="35" border="0" alt="" />Companies House</a>
+				</dd>
+			</dl>
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+
+			<hr />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/publications/self-assessment-register-a-partnership-for-self-assessment-sa400" target="_blank" rel="noopener noreferrer">Partner registration form from HMRC</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership.php">Set up and register a partnership</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
