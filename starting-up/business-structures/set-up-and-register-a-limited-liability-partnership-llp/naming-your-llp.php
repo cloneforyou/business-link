46072daf7da9faeeb231722decb6089b8d201e6c
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 19:18:00 +0100

    Completed “starting-up > business-structures > Set up and register a limited liability partnership (LLP) > Naming your LLP” page.

---
+++ starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/naming-your-llp.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Naming your LLP | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register a limited liability partnership (LLP)</h1>
+			<h2>Naming your LLP</h2>
+			<p>A limited liability partnership (LLP) can trade under the names of the partners, or some of the partners. You might choose to use an established business name of this kind if you are converting your ordinary partnership to an LLP. Alternatively you can use another business name, for example H &amp; S LLP.</p>
+			<p>The trading name should not be the same as, or too similar to, that of any business that already exists, and it should not contain words that people might find offensive or misleading. The name must end with 'Limited Liability Partnership' or 'LLP'.</p>
+			<p>You will need to obtain permission from the relevant professional bodies to use professional words, such as architect or solicitor, in your business name.</p>
+			<p>You will also need to obtain permission to use words connected with public authorities, such as health, education, etc.</p>
+			<p>You must make sure that your business website and stationery - such as letters and invoices - display the trading name, the fact that it is an LLP, the place of registration, the registration number, and the address of the registered office.</p>
+			<p><a href="https://www.gov.uk/government/publications/limited-liability-partnership-incorporation-and-names#ch7" target="_blank" rel="noopener noreferrer">Find out how and where you must disclose the name of your LLP on the Companies House website</a>.</p>
+			<p>For more information, see our guides on how to <a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php">choose the right name for your business</a> and <a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php">rules for naming your limited company or limited liability partnership (LLP)</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/deed-of-partnership-of-an-llp.php" title="Deed of partnership of an LLP">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/tax-matters-of-an-llp.php" title="Tax matters of an LLP">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp.php" title="Set up and register a limited liability partnership (LLP)">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/the-members-of-an-llp.php">The members of an LLP</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/where-to-register-your-llp-and-get-help.php">Where to register your LLP and get help</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/deed-of-partnership-of-an-llp.php">Deed of partnership of an LLP</a></li>
+				<li>Naming your LLP</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/tax-matters-of-an-llp.php">Tax matters of an LLP</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/checklist-setting-up-and-registering-an-llp.php">Checklist: setting up and registering an LLP</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+
+			<hr />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/publications/limited-liability-partnership-incorporation-and-names#ch7" target="_blank" rel="noopener noreferrer">Disclose requirements for LLPs from Companies House </a></li>
+				<li><a href="https://www.gov.uk/get-information-about-a-company" target="_blank" rel="noopener noreferrer">Use our interactive tool to search for an available company name and trade mark</a></li>
+				<li><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Solicitor search from the Law Society</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php">Rules for naming your limited company or limited liability partnership (LLP)</a></li>
+				<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php">Choose the right name for your business</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
