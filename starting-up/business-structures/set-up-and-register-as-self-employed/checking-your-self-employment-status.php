4c3ea18e14eedf4ec9e5fe53fb214f317de4d85b
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 16:32:49 +0100

    Completed “starting-up > business-structures > set-up-and-register-as-self-employed > Checking your self-employment status” page.

---
+++ starting-up/business-structures/set-up-and-register-as-self-employed/checking-your-self-employment-status.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Checking your self-employment status | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register as self-employed</h1>
+			<h2>Checking your self-employment status</h2>
+			<p>If your work comes from more than one source it is possible to be <strong>employed</strong> in one or more jobs - with your employer deducting tax and National Insurance - and <strong>self-employed</strong> in others.</p>
+			<p>If you're not sure whether your work counts as self-employment, ask yourself these questions:</p>
+			<ul>
+				<li>Do you have the final say in how the business is run?</li>
+				<li>Do you risk your own money in the business?</li>
+				<li>Are you responsible for meeting the losses as well as taking the profits?</li>
+				<li>Do you provide the main items of equipment you need to do your job, not just the small tools many employees provide for themselves?</li>
+				<li>Are you free to hire other people on your own terms to do the work you have taken on? Do you pay them out of your own pocket?</li>
+				<li>
+					Do you have to correct unsatisfactory work in your own time and at your own expense?
+				</li>
+			</ul>
+			
+			<p>If you answer 'yes' to most of these questions then you are probably self-employed already. To find out how to tell HM Revenue &amp; Customs (HMRC) about your self employment, see the page in this guide on <a href="/starting-up/business-structures/set-up-and-register-as-self-employed/registering-for-tax-and-national-insurance.php">registering for tax and National Insurance</a>.</p>
+			<p>The latest you can register with HMRC is 5 October after the tax year for which you need to submit a Self Assessment tax return - for example 5 October 2011 for the 2010-11 tax year. If you register late, you may be charged a penalty.</p>
+			<p>To better understand your employment status, <a href="https://www.gov.uk/guidance/check-employment-status-for-tax" target="_blank" rel="noopener noreferrer">use the employment status indicator on the HMRC website</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/arranging-to-make-national-insurance-contributions.php" title="Arranging to make National Insurance contributions">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/checklist-setting-up-as-self-employed.php" title="Checklist: setting up as self-employed">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed.php" title="Set up and register as self-employed">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/setting-up-as-self-employed.php">Setting up as self-employed</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/registering-for-tax-and-national-insurance.php">Registering for tax and National Insurance</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/arranging-to-make-national-insurance-contributions.php">Arranging to make National Insurance contributions</a></li>
+				<li>Checking your self-employment status</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/checklist-setting-up-as-self-employed.php">Checklist: setting up as self-employed</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<dl>
+				<dt></dt>
+				<dd>
+					 <a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer"><img src="../../../_img/HMRC_logo.gif" width="134" height="35" border="0" alt="" />HM Revenue &amp; Customs</a>
+				</dd>
+			</dl>
+			
+			<h3>HMRC Newly Self-Employed Helpline</h3>
+			<p>0845 915 4515</p>
+
+			<hr />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="../../../_files/employed-or-self-employed-for-tax-and-national-insurance-contributions.pdf" title="A guide to employment status for tax National Insurance contributions" target="_blank" rel="noopener noreferrer">Download employment status for tax and National Insurance contributions guidance from HMRC (PDF, 60K)</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/employment-and-skills/take-on-staff/employment-status.php">Employment status</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
