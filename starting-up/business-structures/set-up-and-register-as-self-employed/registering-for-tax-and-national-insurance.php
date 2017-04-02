e612f35fa6bd5bd4f11f41f8a69017cd8d2d1fec
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 16:06:05 +0100

    Completed “starting-up > business-structures > set-up-and-register-as-self-employed > Registering for tax and National Insurance” page.

---
+++ starting-up/business-structures/set-up-and-register-as-self-employed/registering-for-tax-and-national-insurance.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Registering for tax and National Insurance | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register as self-employed</h1>
+			<h2>Registering for tax and National Insurance</h2>
+			<p>You can let HM Revenue &amp; Customs (HMRC) know about your new self-employment by registering online for business taxes. HMRC will need to know information about yourself and your business to set up tax records for:</p>
+			<ul>
+				<li>Self Assessment</li>
+				<li>National Insurance</li>
+				<li>PAYE if you have people working for you</li>
+			</ul>
+
+			<p>A Self Assessment Online account will be set up for you automatically at the same time.</p>
+			<p>Once you've registered, HMRC will send you a Unique Taxpayer Reference number and you'll then complete a Self Assessment tax return each year.</p>
+			<p>Even if you already complete a Self Assessment tax return, you can still complete a Self Assessment registration form to tell HMRC about your self-employment.</p>
+			<p>It's best if you register as soon as you become self-employed.  You must register at the latest by 5 October after the end of the tax year for which you need a tax return. The tax year runs from 6 April one year to 5 April the next. So if, for example, you have tax to pay on your business profits for the 2010-11 tax year, you need to let HMRC know by 5 October 2011.</p>
+			<p><a href="/do-it-online/register-online-for-business-taxes.php">Register your business for HMRC taxes</a> </p>
+			<p>If you register late you may have to pay a penalty.</p>
+			<h3>Registering for the Construction Industry Scheme</h3>
+			<p>If you are a contractor and/or a sub-contractor in the construction industry, you must also register with the Construction Industry Scheme.</p>
+			<p>For more information, see our guide: <a href="/tax-payroll-and-company-information/construction-industry-scheme/what-is-the-construction-industry-scheme.php">what is the Construction Industry Scheme?</a> </p>
+			
+			<h3>Registering for VAT</h3>
+			<p>If you expect your business to have turnover of more than &pound;73,000, you will need to charge VAT to your customers and pass this on to HMRC. <a href="/starting-up/vat/when-to-register-for-VAT.php">Use our interactive tool to find out if you need to register for VAT</a>.</p>
+			<h3>Registering as an employer</h3>
+			<p>If you employ other people in your business you need to collect and pay income tax and NICs, which will mean operating a PAYE (Pay As You Earn) system. See our guide on <a href="/tax-payroll-and-company-information/paye-for-employers/introduction-to-paye-for-employers/paye-for-employers-the-basics.php">PAYE for employers: the basics</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/setting-up-as-self-employed.php" title="Setting up as self-employed">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/arranging-to-make-national-insurance-contributions.php" title="Arranging to make National Insurance contributions">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed.php" title="Set up and register as self-employed">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/setting-up-as-self-employed.php">Setting up as self-employed</a></li>
+				<li>Registering for tax and National Insurance</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/arranging-to-make-national-insurance-contributions.php">Arranging to make National Insurance contributions</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/checking-your-self-employment-status.php">Checking your self-employment status</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/checklist-setting-up-as-self-employed.php">Checklist: setting up as self-employed</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<dl>
+				<dt></dt>
+				<dd>
+					 <a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer"><img src="../../../_img/HMRC_logo.gif" width="134" height="35" border="0" alt="" />HM Revenue &amp; Customs</a>
+				</dd>
+			</dl>
+
+			<h3>HMRC Newly Self-Employed Helpline</h3>
+			<p>0845 915 4515</p>
+			</ul>
+
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/self-assessment/your-tax-return-the-basics/registering-for-self-assessment.php">Registering for Self Assessment</a></li>
+				<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat.php">When to register for UK VAT </a></li>
+				<li><a href="/tax-payroll-and-company-information/paye-for-employers/introduction-to-paye-for-employers/how-to-register-as-a-employer.php">How to register as an employer</a></li>
+				<li><a href="/tax-payroll-and-company-information/construction-industry-scheme/what-is-the-construction-industry-scheme.php">What is the Construction Industry Scheme?</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
