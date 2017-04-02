08907d565f0295ac7decd53772317b2ab11d0f7f
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 16:48:45 +0100

    Completed “starting-up > business-structures > Set up and register a partnership > The officers of a partnership” page.

---
+++ starting-up/business-structures/set-up-and-register-a-partnership/the-officers-of-a-partnership.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>The officers of a partnership | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register a partnership</h1>
+			<h2>The officers of a partnership</h2>
+			<p>Any group of people who want to set up a profit-making business together can form a partnership. However, if a member of a partnership is under the age of 18, they cannot be legally bound by the terms of the partnership agreement.</p>
+			<p>It is also possible for companies and limited liability partnerships to be members of a partnership.</p>
+			
+			<h3>How many officers can a partnership have?</h3>
+			<p>Forming a partnership allows two or more people to set up in business together, sharing profits, managing burdens and risks.</p>
+			
+			<h3>The rights and responsibilities of partners</h3>
+			<p>Partners normally share in both the responsibilities of running the business and the profits or losses that it makes. However, their precise rights and responsibilities will depend on:</p>
+			<ul>
+				<li>what type of partner they are</li>
+				<li>any partnership agreement or 'deed of partnership' that they have entered into</li>
+			</ul>
+
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership.php" title="Set up and register a partnership">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/types-of-partner.php" title="Types of partner">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership.php" title="Set up and register a partnership">Introduction</a></li>
+				<li>The officers of a partnership</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/types-of-partner.php">Types of partner</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/setting-up-a-deed-of-partnership.php">Setting up a deed of partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/naming-your-partnership.php">Naming your partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/tax-matters-of-a-partnership.php">Tax matters of a partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/checklist-setting-up-a-partnership.php">Checklist: setting up a partnership</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+			
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
