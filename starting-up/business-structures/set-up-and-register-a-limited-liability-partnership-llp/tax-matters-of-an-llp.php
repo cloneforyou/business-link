959a68eed32373754b5c460a9024374457673733
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 19:19:54 +0100

    Completed “starting-up > business-structures > Set up and register a limited liability partnership (LLP) > Tax matters of an LLP” page.

---
+++ starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/tax-matters-of-an-llp.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Tax matters of an LLP | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register a limited liability partnership (LLP)</h1>
+			<h2>Tax matters of an LLP</h2>
+			<p>Once you've registered the LLP with Companies House, using form LL IN01, they will notify HM Revenue and Customs (HMRC), so you don't need to do this. HMRC will then set up the right tax records for the partnership.</p>
+			<p>Each partner in the LLP must also register separately with HMRC for get their own tax records set up. Find out more about how to register partners in our guide on <a href="/tax-payroll-and-company-information/self-assessment/your-tax-return-the-basics/registering-for-self-assessment.php">registering for Self Assessment</a>.</p>
+			<p>On the whole, LLPs are treated the same as general partnerships for tax purposes. Like general partnerships, profits are shared among the members of a limited partnership. Individual members - not the limited partnership - pay tax on income or gains. Unlike limited companies, LLPs are not generally liable for Corporation Tax. The nominated partner will need to complete a Self Assessment tax return for the partnership every year. Members of the LLP will need to also complete a Self Assessment tax return and show their share of the profits on it.</p>
+			<p>For more on tax see the page on <a href="/starting-up/business-structures/set-up-and-register-a-partnership/tax-matters-of-a-partnership.php">tax matters of a partnership</a> in our guide on how to <a href="/starting-up/business-structures/set-up-and-register-a-partnership.php">set up and register a partnership</a>. This guide includes information on VAT, PAYE for employers and the Construction Industry Scheme.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/naming-your-llp.php" title="Naming your LLP">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/checklist-setting-up-and-registering-an-llp.php" title="Checklist: setting up and registering an LLP">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp.php" title="Set up and register a limited liability partnership (LLP)">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/the-members-of-an-llp.php">The members of an LLP</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/where-to-register-your-llp-and-get-help.php">Where to register your LLP and get help</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/deed-of-partnership-of-an-llp.php">Deed of partnership of an LLP</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/naming-your-llp.php">Naming your LLP</a></li>
+				<li>Tax matters of an LLP</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/checklist-setting-up-and-registering-an-llp.php">Checklist: setting up and registering an LLP</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/publications/self-assessment-register-a-partnership-for-self-assessment-sa400" target="_blank" rel="noopener noreferrer">Partner registration form from HMRC</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/self-assessment/tax-returns-if-you-are-self-employed-or-in-a-partnership/tax-returns-for-partners-and-partnerships.php">Tax returns for partners and partnerships</a></li>
+				<li><a href="/tax-payroll-and-company-information/paye-for-employers/introduction-to-paye-for-employers/paye-for-employers-the-basics.php">PAYE for employers: the basics</a></li>
+				<li><a href="/tax-payroll-and-company-information/self-assessment/your-tax-return-the-basics/registering-for-self-assessment.php">Registering for Self Assessment</a></li>
+				<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat.php">When to register for UK VAT </a></li>
+				<li><a href="/tax-payroll-and-company-information/paye-for-employers/introduction-to-paye-for-employers/how-to-register-as-a-employer.php">How to register as an employer</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
