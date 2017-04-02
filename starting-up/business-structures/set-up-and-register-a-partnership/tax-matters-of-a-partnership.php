d14f1a686a95a30f6d670a25ff5ae0e8a443c007
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 17:02:58 +0100

    Completed “starting-up > business-structures > Set up and register a partnership > Tax matters of a partnership” page.

---
+++ starting-up/business-structures/set-up-and-register-a-partnership/tax-matters-of-a-partnership.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Tax matters of a partnership | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register a partnership</h1>
+			<h2>Tax matters of a partnership</h2>
+			<h3>Income Tax and Capital Gains Tax</h3>
+			<p>The profits and gains of the partnership are shared among the partners. Each is personally responsible for paying tax on their share.</p>
+			<p>The nominated partner must register the partnership for business taxes with HM Revenue &amp; Customs (HMRC). They can do this online.</p>
+			<p>Each partner must also register themselves for Self Assessment and National Insurance. The partnership and each partner will each then receive their own Unique Taxpayer Reference for Self Assessment. HMRC will then set up the right tax and National Insurance records for both the partnership and partners.</p>
+			<p><a href="https://www.gov.uk/log-in-file-self-assessment-tax-return" target="_blank" rel="noopener noreferrer">Register online for Self Assessment and National Insurance Contributions on the HMRC website</a> </p>
+			<p>Find out more about how to register partnerships and partners in our guide on <a href="/tax-payroll-and-company-information/self-assessment/your-tax-return-the-basics/registering-for-self-assessment.php">registering for Self Assessment</a>.</p>
+			<p>Every year each individual partner will need to complete a Self Assessment tax return to show the profits they get from the partnership.</p>
+			<p>The partnership should appoint one of its officers - the nominated partner - to fill in the Self Assessment Partnership Tax Return and send it to HMRC. This includes a Partnership Statement, which shows how profits or losses have been divided among the partners. The nominated partner should also ensure that all other officers are given copies of the Partnership Statement to help them complete their own tax returns. Although the nominated officer has responsibility for the Partnership Tax Return, all the partners are jointly liable for any penalties resulting from it being submitted late or incorrectly.</p>
+			<p>One of the easiest ways to send a tax return is online. See our guide on <a href="/tax-payroll-and-company-information/self-assessment/tax-returns-if-you-are-self-employed-or-in-a-partnership/completing-your-tax-return-partners-and-partnerships.php">completing your tax return (partners and partnerships)</a>.</p>
+
+			<h3>National Insurance contributions</h3>
+			<p>As well as being responsible for their tax, individual partners are also responsible for paying their own National Insurance contributions (NICs). Partners normally have to pay Class 2 National Insurance contributions. If their annual profits are over a certain amount they also pay Class 4 contributions. For more information, see our guide on <a href="/tax-payroll-and-company-information/national-insurance/self-employed-tax-and-national-insurance.php">self-employed tax and National Insurance</a>.</p>
+			
+			<h3>Corporation Tax</h3>
+			<p>If partners are companies, they must pay Corporation Tax on their profits from the partnership, and should record the relevant figures on their Corporation Tax return.  For more see our guide <a href="/tax-payroll-and-company-information/corporation-tax/introduction-to-corporation-tax.php">introduction to Corporation Tax</a>.</p>
+			
+			<h3>VAT</h3>
+			<p>If the partnership has - or expects to have - turnover of more than &pound;73,000, it will need to charge VAT to its customers and pass this on to HMRC. <a href="/starting-up/vat/when-to-register-for-VAT.php">Use our interactive tool to find out if you need to register for VAT</a>.</p>
+			
+			<h3>PAYE (Pay As You Earn) for employers</h3>
+			<p>Partnerships with employees will need to collect and pay income tax and NICs, which will mean operating a PAYE (Pay As You Earn) system. See our guide on <a href="/tax-payroll-and-company-information/paye-for-employers/introduction-to-paye-for-employers/paye-for-employers-the-basics.php">PAYE for employers: the basics</a>.</p>
+			
+			<h3>Construction Industry Scheme</h3>
+			<p>If the partnership is a contractor and/or a sub-contractor in the construction industry, you must also register with the Construction Industry Scheme.</p>
+			<p>For more information, see our guide: <a href="/tax-payroll-and-company-information/construction-industry-scheme/what-is-the-construction-industry-scheme.php">what is the Construction Industry Scheme?</a></p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/naming-your-partnership.php" title="Naming your partnership">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/checklist-setting-up-a-partnership.php" title="Checklist: setting up a partnership">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership.php" title="Set up and register a partnership">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/the-officers-of-a-partnership.php">The officers of a partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/types-of-partner.php">Types of partner</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/setting-up-a-deed-of-partnership.php">Setting up a deed of partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/naming-your-partnership.php">Naming your partnership</a></li>
+				<li>Tax matters of a partnership</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/checklist-setting-up-a-partnership.php">Checklist: setting up a partnership</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>HMRC Newly Self-Employed Helpline</h3>
+			<p>0845 915 4515</p>
+			<h3>HMRC New Employer Helpline</h3>
+			<p>0845 60 70 143</p>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/self-assessment/your-tax-return-the-basics/registering-for-self-assessment.php">Registering for Self Assessment</a></li>
+				<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat.php">When to register for UK VAT </a></li>
+				<li><a href="/tax-payroll-and-company-information/paye-for-employers/introduction-to-paye-for-employers/how-to-register-as-a-employer.php">How to register as an employer</a></li>
+				<li><a href="/tax-payroll-and-company-information/self-assessment/tax-returns-if-you-are-self-employed-or-in-a-partnership/tax-returns-for-partners-and-partnerships.php">Tax returns for partners and partnerships</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
