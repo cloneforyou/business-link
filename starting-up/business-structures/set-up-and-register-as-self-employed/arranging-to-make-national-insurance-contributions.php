e6a16ee3d0dcc5ae4f6df548a07eb80e247a629c
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 16:23:08 +0100

    Completed “starting-up > business-structures > set-up-and-register-as-self-employed > Arranging to make National Insurance contributions” page.

---
+++ starting-up/business-structures/set-up-and-register-as-self-employed/arranging-to-make-national-insurance-contributions.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Arranging to make National Insurance contributions | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register as self-employed</h1>
+			<h2>Arranging to make National Insurance contributions</h2>
+			<p>As well as registering with HM Revenue &amp; Customs (HMRC) so that they can set up records for your tax and National Insurance contribuions (NICs) you'll need to arrange to pay Class 2 NICs. For the tax year 2011-12 the Class 2 NICs rate is &pound;2.50 a week.</p>
+			<p>The most convenient way to pay Class 2 NICs is via Direct Debit. Paying by Direct Debit has many advantages:</p>
+			<ul>
+				<li>it's easy to set up - just fill in the application form and send it to HMRC - you will need a suitable account at a bank or building society</li>
+				<li>it runs automatically - once set up, you don't have to remember to make payments</li>
+				<li>you can choose to pay monthly or six monthly</li>
+				<li>it helps your budgeting - for people in business, good cash flow and budgeting are essential and paying by Direct Debit means that your payments are always made in arrears of your liability</li>
+				<li>it gives you peace of mind - payments are made on time, every time and there is no risk of losing benefit because you forgot to pay</li>
+			</ul>
+
+			<p><a href="https://www.gov.uk/government/publications/national-insurance-application-to-pay-self-employed-national-insurance-contributions-by-direct-debit-ca5601" target="_blank" rel="noopener noreferrer">Find an application form to pay Class 2 NICs by Direct Debit on the HMRC website</a>.</p>
+			<p>If you are unable to pay by Direct Debit, HMRC will issue you with bill reminders in April and October showing the amount due by 31 July and 31 January respectively.</p>
+			
+			<h3>Applying to be exempt from paying Class 2 NICs</h3>
+			<p>If you expect your income from your business to be low for the tax year you may not have to pay Class 2 NICs. <strike>To find out more please download form CF10 Self-employed people with small earnings.</strike></p>
+			<p><strike><a href="http://webarchive.nationalarchives.gov.uk/20140603092408/http://search2.hmrc.gov.uk/kb5/hmrc/forms/view.page?record=ykpjabzqcc8&formid=433" target="_blank" rel="noopener noreferrer">Find form CF10 for self-employed people with small earnings on the HMRC website</a>.</strike></p>
+			<p>To read more detail on both Class 2 and Class 4 National Insurance if you're self-employed, see our guide on <a href="/tax-payroll-and-company-information/national-insurance/self-employed-tax-and-national-insurance.php">self-employed tax and National Insurance</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/registering-for-tax-and-national-insurance.php" title="Registering for tax and National Insurance">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/checking-your-self-employment-status.php" title="Checking your self-employment status">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed.php" title="Set up and register as self-employed">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/setting-up-as-self-employed.php">Setting up as self-employed</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/registering-for-tax-and-national-insurance.php">Registering for tax and National Insurance</a></li>
+				<li>Arranging to make National Insurance contributions</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/checking-your-self-employment-status.php">Checking your self-employment status</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-as-self-employed/checklist-setting-up-as-self-employed.php">Checklist: setting up as self-employed</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<dl>
+				<dt></dt>
+				<dd>
+					 <a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer"><img src="../../../_img/HMRC_logo.gif" width="134" height="35" border="0" alt="" />HM Revenue &amp; Customs</a>
+				</dd>
+			</dl>
+
+			<h3>HMRC New Employer Helpline</h3>
+			<p>0845 60 70 143</p>
+			
+			<hr />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/national-insurance/identify-the-national-insurance-contributions-you-must-pay.php">Use our interactive tool to identify the National Insurance contributions you must pay</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/national-insurance/national-insurance-an-introduction.php">National Insurance: an introduction</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+			
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
