00da5345e55183ace8c6eb367dc50dde992a62c6
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 19:09:20 +0100

    Completed “starting-up > business-structures > Set up and register a limited liability partnership (LLP) > Where to register your LLP and get help” page.

---
+++ starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/where-to-register-your-llp-and-get-help.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Where to register your LLP and get help | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register a limited liability partnership (LLP)</h1>
+			<h2>Where to register your LLP and get help</h2>
+			<p>Companies House can advise you on the registration process for a limited liability partnership (LLP), but cannot give you detailed advice on drawing up the necessary documentation.</p>
+			<p>You can register your LLP yourself by completing application form LL IN01 and sending it along with the fee to Companies House. Fees payable are outlined below in the section: 'The cost of registration' below.</p>
+			<p><a href="https://www.gov.uk/government/publications/register-a-limited-liability-partnership-ll-in01" target="_blank" rel="noopener noreferrer">Download form LL IN01 from the Companies House website (PDF, 866K)</a>.</p>
+			<p>Although you can register your LLP yourself, it is a good idea to seek professional advice. A company formation agent, solicitor or accountant can carry out the process for you, for a fee, as well as offer advice. A solicitor will also be able to help you draw up your deed of partnership. To find your nearest company formation agent, solicitor or accountant, look in your local telephone directory or search online. Many incorporation agents and software providers are now able to offer their customers a web-based electronic service which is an easier and quicker way for you to register your LLP (this is chargeable).</p>
+			<p><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Search for a qualified solicitor on the Law Society website</a>.</p>
+			<p><a href="https://web-beta.archive.org/web/20130420094152/http://www.accaglobal.com/en/discover/find-accountant.html" target="_blank" rel="noopener noreferrer">Search for a chartered accountant on the Association of Chartered Certified Accountants (ACCA) website</a>.</p>
+			<p>You may not be able to use the LLP name you want if that name is the same as that of another LLP or company on the registrar's index of company names held at Companies House.</p>
+			<p>There is an exception to this if an existing LLP or company is in the same group as your LLP and consents to the use of your proposed LLP name. See our guide on <a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php">rules for naming your limited company or limited liability partnership (LLP)</a>.</p>
+			
+			<p>You can <a href="/do-it-online/search-company-information-using-companies-house-webcheck.php">check the registrar's index of company names with the WebCHeck service at Companies House</a>.</p>
+			
+			<p><a href="https://www.gov.uk/government/organisations/companies-house/about/access-and-opening#Cardiff" target="_blank" rel="noopener noreferrer">Find contact details to register an LLP based in England or Wales on the Companies House website</a>.</p>
+			
+			<p><a href="https://www.gov.uk/government/organisations/companies-house/about/access-and-opening#Edinburgh" target="_blank" rel="noopener noreferrer">Find contact details to register an LLP based in Scotland using details on the Companies House website</a>.</p>
+			
+			<p><a href="https://www.gov.uk/government/organisations/companies-house/about/access-and-opening#NI" target="_blank" rel="noopener noreferrer">Find contact details to register an LLP based in Northern Ireland using details on the Companies House website</a>.</p>
+			
+			<h3>The cost of registration</h3>
+			<p><a href="https://www.gov.uk/government/organisations/companies-house/about/about-our-services#companies-house-fees" target="_blank" rel="noopener noreferrer">Find out more about Companies House fees on the Companies House website</a>.</p>
+			<p>Companies House aims to process standard paper documents within five days of receipt.</p>
+			<p>To get an idea of the cost of using company formation agents or others who can handle the registration process for you, search online or find them in your local telephone directory.</p>
+			<h3>Further help</h3>
+			<p>Companies House will guide you through the registration process, or you can ask a solicitor or company formation agent to handle this for you. It is a good idea to seek the help of a solicitor to draw up a deed of partnership.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/the-members-of-an-llp.php" title="The members of an LLP">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/deed-of-partnership-of-an-llp.php" title="Deed of partnership of an LLP">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp.php" title="Set up and register a limited liability partnership (LLP)">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/the-members-of-an-llp.php">The members of an LLP</a></li>
+				<li>Where to register your LLP and get help</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/deed-of-partnership-of-an-llp.php">Deed of partnership of an LLP</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/naming-your-llp.php">Naming your LLP</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/tax-matters-of-an-llp.php">Tax matters of an LLP</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-liability-partnership-llp/checklist-setting-up-and-registering-an-llp.php">Checklist: setting up and registering an LLP</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+			
+			<hr />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/publications/limited-liability-partnership-incorporation-and-names" target="_blank" rel="noopener noreferrer">LLP incorporation advice from Companies House</a></li>
+				<li><a href="https://www.gov.uk/government/publications/limited-liability-partnership-incorporation-and-names#ch5" target="_blank" rel="noopener noreferrer">Changing an LLP's name guidance from Companies House</a></li>
+				<li><a href="https://www.gov.uk/government/publications/life-of-a-limited-liability-partnership" target="_blank" rel="noopener noreferrer">LLP filing requirements from Companies House</a></li>
+				<li><a href="/do-it-online/search-company-information-using-companies-house-webcheck.php">Search company information using the WebCHeck service at Companies House</a></li>
+				<li><a href="https://www.gov.uk/government/publications/register-a-limited-liability-partnership-ll-in01" target="_blank" rel="noopener noreferrer">Download form LL IN01 from Companies House (PDF, 866K)</a></li>
+				<li><a href="https://www.gov.uk/government/organisations/companies-house/about/about-our-services#companies-house-fees" target="_blank" rel="noopener noreferrer">Fees for LLP administrative tasks from Companies House</a></li>
+				<li><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Solicitor search from the Law Society</a></li>
+				<li><a href="https://web-beta.archive.org/web/20130420094152/http://www.accaglobal.com/en/discover/find-accountant.html" target="_blank" rel="noopener noreferrer">Accountant search from ACCA</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/finance-and-grants/expert-financial-advice/choose-and-work-with-a-solicitor.php">Choose and work with a solicitor</a></li>
+				<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+				<li><a href="/starting-up/name-your-business/rules-for-naming-your-limited-company-or-limited-liability-partnership-llp.php">Rules for naming your limited company or limited liability partnership (LLP)</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership.php">Set up and register a limited partnership</a></li>
+				<li><a href="layerfff0.html?topicId=1085045289">Company and LLP strike-off, dissolution and restoration</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
