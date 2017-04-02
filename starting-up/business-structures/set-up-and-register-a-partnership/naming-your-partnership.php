11b228a8ebc9b373d0ccf5cc7db6cf69f680ce63
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 16:57:52 +0100

    Completed “starting-up > business-structures > Set up and register a partnership > Naming your partnership” page.

---
+++ starting-up/business-structures/set-up-and-register-a-partnership/naming-your-partnership.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Naming your partnership | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register a partnership</h1>
+			<h2>Naming your partnership</h2>
+			<p>A partnership can trade under the names of the partners, 'Wright, Brown and Ali LP', for example, or it can use another business name - such as 'Fantastic Design Solutions Limited Partnership'.</p>
+			<p>If your trading name does not include the partners' names, you must still make sure that your business website and stationery - such as letters and invoices - display all of their names as well as the trading name - for example, 'Wright, Brown and Ali, trading as Fantastic Design Solutions Limited Partnership'.</p>
+			<p>If there are more than 20 partners then the business website and stationery do not have to list them, but they must show the address of the partnership's principal place of business.</p>
+			<p>The trading name should not be the same as, or too similar, to that of any existing business, and it should not contain words that people might find offensive or misleading.</p>
+			<p>For more information see our guide on how to <a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php">choose the right name for your business</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/setting-up-a-deed-of-partnership.php" title="Setting up a deed of partnership">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/tax-matters-of-a-partnership.php" title="Tax matters of a partnership">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership.php" title="Set up and register a partnership">Introduction</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/the-officers-of-a-partnership.php">The officers of a partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/types-of-partner.php">Types of partner</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/setting-up-a-deed-of-partnership.php">Setting up a deed of partnership</a></li>
+				<li>Naming your partnership</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/tax-matters-of-a-partnership.php">Tax matters of a partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership/checklist-setting-up-a-partnership.php">Checklist: setting up a partnership</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/name-your-business/choose-the-right-name-for-your-business.php">Choose the right name for your business</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
