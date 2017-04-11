797105391a378f2cc84d02c81e2f1ef5d43a2702
Caroline Hagan <info@blueocto.co.uk>
2017-04-11 16:59:24 +0100

    Completed “starting-up > buy-a-business > buy-a-franchise > find-out-about-possible-franchises” page.

---
+++ starting-up/buy-a-business/buy-a-franchise/find-out-about-possible-franchises.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Find out about possible franchises | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Buy a franchise</h1>
+			<h2>Find out about possible franchises</h2>
+			<p>You can find out about possible franchise opportunities from a range of sources.</p>
+			<p>A useful starting point is the British Franchise Association (BFA) website. As well as offering guidance and seminars on franchising, it provides contact information on all BFA members' franchise opportunities - both new franchises and existing franchises for resale.</p>
+			<p><a href="http://www.thebfa.org/members" target="_blank" rel="noopener noreferrer">Find members who may be offering new franchises on the BFA website</a>.</p>
+			<p>Franchises are advertised and written about in various national <strong>newspapers</strong> and in trade publications such as:</p>
+			<ul>
+				<li>Daltons Weekly - <a href="https://www.daltonsbusiness.com/" target="_blank" rel="noopener noreferrer">find out about Daltons Weekly on the Daltons website</a> </li>
+				<li>Franchise World - <a href="http://www.franchiseworld.co.uk/" target="_blank" rel="noopener noreferrer">read franchise opportunities, news and views on the Franchise World website</a> </li>
+				<li>Business Franchise - <a href="http://www.businessfranchise.com/" target="_blank" rel="noopener noreferrer">search for franchise opportunities on the Business Franchise website</a> </li>
+				<li>The Franchise Magazine - <a href="http://www.thefranchisemagazine.net/" target="_blank" rel="noopener noreferrer">find business franchise opportunities on The Francise Magazine website</a></li>
+			</ul>
+			<p>Websites such as <strong>whichfranchise.com</strong> and <strong>Franchiseinfo</strong> can be a useful source of information on franchises. You can find other listings using a <strong>search engine</strong> and searching for terms like'franchise', 'franchise opportunity' or 'franchise directory'.</p>
+			<p>Attending a franchise <strong>exhibition</strong> can also be a good way of finding out what's on offer. You can <a href="https://www.franchiseinfo.co.uk/exhibitions-across-the-uk" target="_blank" rel="noopener noreferrer">find details of some major franchise exhibitions on the Franchiseinfo website</a>.</p>
+			<p>But tread carefully. Advertised franchise opportunities - particularly multi-level marketing schemes - can be untried, dishonest or even illegal. Assess the franchise opportunity carefully and check if the business offering the franchise is a member of the BFA.</p>
+			
+			<ul>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/should-I-buy-a-franchise.php" title="Should I buy a franchise?">Prev</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/assess-a-franchise-opportunity.php" title="Assess a franchise opportunity">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/buy-or-sell-a-business/franchising/buy-a-franchise.php" title="Buy a franchise">Introduction</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/what-is-franchising.php">What is franchising?</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/advantages-and-disadvantages-of-franchising.php">Advantages and disadvantages of franchising</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/should-I-buy-a-franchise.php">Should I buy a franchise?</a></li>
+				<li>Find out about possible franchises</li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/assess-a-franchise-opportunity.php">Assess a franchise opportunity</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/the-costs-of-a-franchise.php">The costs of a franchise</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/how-to-purchase-a-franchise.php">How to purchase a franchise</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/tips-on-franchise-agreements.php">Tips on franchise agreements</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/heres-how-I-selected-and-bought-my-franchise.php">Here's how I selected and bought my franchise</a></li>
+			</ul>
+
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://www.thebfa.org/members" target="_blank" rel="noopener noreferrer">Members offering new franchise opportunities listed on the BFA website</a></li>
+				<li><a href="http://www.whichfranchise.com/franchiseSearch/index.cfm?Search=yes" target="_blank" rel="noopener noreferrer">Resales and other BFA approved franchise opportunities on the whichfranchise.com website</a></li>
+				<li><a href="http://www.thebfa.org/join-a-franchise/how-to-choose-a-franchise" title="Information on how to choose the right franchise for you" target="_blank" rel="noopener noreferrer">Choosing the right franchise guidance on the BFA website</a></li>
+				<li><a href="https://www.franchiseinfo.co.uk/exhibitions-across-the-uk" target="_blank" rel="noopener noreferrer">Franchise exhibitions information on the Franchiseinfo website</a></li>
+				<li><a href="http://www.whichfranchise.com/" target="_blank" rel="noopener noreferrer">Franchising in your area information on the whichfranchise.com website</a></li>
+				<li><a href="http://www.theukfranchisedirectory.net/" title="Franchise opportunities in the UK" target="_blank" rel="noopener noreferrer">UK franchise opportunities on the UK Franchise Directory website</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/set-up-business" title="Learn about the different types of business and legal structures to help you choose the right one for your new business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" title="Tutorials, videos and tools to help you decide how to become self-employed - from sole trader to limited company, franchisee to social enterprise and more" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" title="Case studies and tutorials to help you understand the legal dos and don'ts of naming your business" target="_blank" rel="noopener noreferrer">Name your business</a></li>
+				<li><a href="/buy-or-sell-a-business/buying/buy-an-existing-business.php">Buy an existing business</a></li>
+				<li><a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">Research and develop your business ideas, new products and services</a></li>
+				<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports.php">Market research and market reports</a></li>
+				<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs.php">Know your customers' needs</a></li>
+				<li><a href="/sales-and-marketing/selling/forecast-and-plan-your-sales.php">Forecast and plan your sales</a></li>
+				<li><a href="/sales-and-marketing/marketing/understand-your-competitors.php">Understand your competitors</a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+			<hr />
+			
+			<p>Developed with:<br />
+				<a href="http://www.whichfranchise.com/" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/whichfranchise_logo_sm.gif" width="105" height="35" alt="" />whichfranchise.com
+				</a>
+			</p>
+		
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
