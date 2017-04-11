277202fd7774c877665e2879b78dbef977c454e8
Caroline Hagan <info@blueocto.co.uk>
2017-04-11 21:14:22 +0100

    Completed “starting-up > buy-a-business > buy-a-franchise > tips-on-franchise-agreements” page.

---
+++ starting-up/buy-a-business/buy-a-franchise/tips-on-franchise-agreements.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Tips on franchise agreements | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Buy a franchise</h1>
+			<h2>Tips on franchise agreements</h2>
+			<p>The franchise agreement is crucial. Don't sign any agreement, or pay any fees or deposit, until you have taken <strong>legal advice</strong> from an experienced franchise solicitor accredited by the British Franchise Association. Get a specimen contract for them to review.</p>
+			
+			<h3>Areas covered by a typical agreement</h3>
+			<ul>
+				<li><strong>Term -</strong>how long does the franchise last? Will you have the option to renew it, and on what terms?</li>
+				<li><strong>Territory -</strong> what area does your franchise cover? Do you have exclusive rights to sell within it?</li>
+				<li><strong>Fees -</strong> what initial fee will you pay? Whatpercentage of sales revenuewill you pay? Will you pay a regular management fee - and if so, what does it cover? Will you have to pay other costs? How are the costs worked out?</li>
+				<li><strong>Support -</strong>how much help will you get starting the business? What continuing support will you get?</li>
+				<li><strong>Restrictions -</strong>what restrictions are there on what you're allowed to do and how you must run the business?</li>
+				<li><strong>Exit -</strong>what happens if you can't continue in the business for some reason - perhaps due to ill health? What happens if you want to sell your franchise?</li>
+			</ul>
+
+			<ul>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/how-to-purchase-a-franchise.php" title="How to purchase a franchise">Prev</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/heres-how-I-selected-and-bought-my-franchise.php" title="Here's how I selected and bought my franchise">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/buy-or-sell-a-business/franchising/buy-a-franchise.php" title="Buy a franchise">Introduction</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/what-is-franchising.php">What is franchising?</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/advantages-and-disadvantages-of-franchising.php">Advantages and disadvantages of franchising</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/should-I-buy-a-franchise.php">Should I buy a franchise?</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/find-out-about-possible-franchises.php">Find out about possible franchises</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/assess-a-franchise-opportunity.php">Assess a franchise opportunity</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/the-costs-of-a-franchise.php">The costs of a franchise</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/how-to-purchase-a-franchise.php">How to purchase a franchise</a></li>
+				<li>Tips on franchise agreements</li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/heres-how-I-selected-and-bought-my-franchise.php">Here's how I selected and bought my franchise</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.franchiseinfo.co.uk/advice/legal-advice" title="Legal considerations for anyone thinking of becoming a franchisee" target="_blank" rel="noopener noreferrer">Legal pitfalls of franchising explained on the Franchiseinfo website</a></li>
+				<li><a href="http://www.whichfranchise.com/legalAdvisers/indepthAdvice.cfm" target="_blank" rel="noopener noreferrer">Franchise agreements explained on the whichfranchise.com website</a></li>
+				<li><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Solicitor search tool on the Law Society website</a></li>
+				<li><a href="../../../_files/berr_employment-rights-on-the-transfer-of-an-undertaking_mar-2007.pdf" target="_blank" rel="noopener noreferrer">Download the Transfer of Undertakings (Protection of Employment) regulations guidance from the Department for Business, Innovation &amp; Skills website (PDF, 188K)</a></li>
+				<li><a href="http://www.theukfranchisedirectory.net/" title="Franchise opportunities in the UK" target="_blank" rel="noopener noreferrer">UK franchise opportunities on the UK Franchise Directory website</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/set-up-business" title="Learn about the different types of business and legal structures to help you choose the right one for your new business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" title="Tutorials, videos and tools to help you decide how to become self-employed - from sole trader to limited company, franchisee to social enterprise and more" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" title="Case studies and tutorials to help you understand the legal dos and don'ts of naming your business" target="_blank" rel="noopener noreferrer">Name your business</a></li>
+				<li><a href="/grow-your-business/setting-a-vision-for-growth/how-to-grow-your-franchise.php">How to grow your franchise</a></li>
+				<li><a href="/buy-or-sell-a-business/buying/buy-an-existing-business.php">Buy an existing business</a></li>
+				<li><a href="/finance-and-grants/expert-financial-advice/choose-and-work-with-a-solicitor.php">Choose and work with a solicitor</a></li>
+				<li><a href="/employment-and-skills/redundancy-restructures-and-change/responsibilities-to-employees-if-you-buy-or-sell-a-business.php">Responsibilities to employees if you buy or sell a business</a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+			
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
