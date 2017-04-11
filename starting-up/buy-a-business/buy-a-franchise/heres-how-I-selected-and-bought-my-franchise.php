82cd5386f17b638244a9462ad6a0034c58c05c7e
Caroline Hagan <info@blueocto.co.uk>
2017-04-11 21:22:34 +0100

    Completed “starting-up > buy-a-business > buy-a-franchise > here’s-how-I-selected-and-bought-my-franchise” page.

---
+++ starting-up/buy-a-business/buy-a-franchise/heres-how-I-selected-and-bought-my-franchise.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Here's how I selected and bought my franchise | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Here's how I selected and bought my franchise</h1>
+			<h2>Here's how I selected and bought my franchise</h2>
+			<p>Entrepreneur and trained optometrist Stephen Halpin always intended to run his own business. In his chosen market - the high street optical services sector - a franchise seemed like a good way to get a head start. After considering the options, Stephen bought one of the first Boots Opticians franchises - in Northwich, Cheshire.</p>
+			<p><strong>What I did</strong></p>
+			
+			<h3>Select the franchise</h3>
+			<p>"The retail optical services market is highly competitive and starting an outlet in your own name is a risky affair. A franchise made sense, because it reduces some - not all - of the risks, offers a familiar brand name to build on and provides support with marketing and other aspects. There are several franchisors out there and I considered them all. I decided to go for a Boots Opticians franchise.</p>
+			<p>"Boots' reputation with UK consumers is excellent and brand awareness is almost universal. Also, because I was applying for one of the first Boots Opticians franchises, there was more scope to get involved in developing the operational systems. This was important to me- I found that longer-established franchisors had a less flexible attitude."</p>
+			
+			<h3>Work out the figures</h3>
+			<p>"With the help of an accountant, I put together a detailed business plan. The store I wanted to take on had been trading as a Boots Opticians for several years, so it had financial records to assess. Other factors I considered were the store location, local competitors and current operational practices. Without a clear idea of how much the business could make and how much cash I needed, it would have been impossible to tell if the franchise agreement on offer was worthwhile."</p>
+			
+			<h3>Check the agreement</h3>
+			<p>"The business plan also formed the basis of the presentation I gave to Boots' management. This was a key part of the process of being accepted for the franchise. The presentation and plan also gave me the information I needed to negotiate a contract. Following the presentation we had a number of conversations about the principles of any agreement.</p>
+			<p>"Once I had been accepted for the franchise, Boots drew up a franchise agreement setting out terms, conditions and fees. I got advice from specialist franchise solicitors before signing. One of my key objectives was to ensure that the agreement benefited both parties-that I made money and so did Boots.</p>
+			<p>"Starting from this position, I didn't accept the initial agreement and was able to change a few things. I wanted an incentive to grow the business, rather than just keep it ticking over, and Boots recognised that. One very useful point we negotiated was a deferment of payment on the upfront licence fee, which is one of the biggest franchise costs. I paid a portion on signing, the rest a few months later. It meant I had to borrow less in the early days."</p>
+			<p><strong>What I'd do differently</strong></p>
+			
+			<h3>Get even more advice</h3>
+			<p>"I made use of professional advisers and also had help from Boots, who paid for a course covering tax planning, regulatory compliance and so on. Even so, I wish I'd had more advice, especially with regard to employment law and the impact of VAT on the business."</p>
+			
+			<h3>Understand TUPE better</h3>
+			<p>"TUPE stands for the Transfer of Undertakings (Protection of Employment) Regulations. It's a piece of legislation that's concerned with the transfer of staff from one employer to another. As I was taking on existing staff with the franchise, a better understanding of TUPE would have been helpful during negotiations."</p>
+			<p><a href="/find-a-case-study.php">Read more case studies that describe first hand how people tackle real-life challenges and opportunities</a></p>
+			
+			<ul>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/tips-on-franchise-agreements.php" title="Tips on franchise agreements">Prev</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/buy-or-sell-a-business/franchising/buy-a-franchise.php" title="Buy a franchise">Introduction</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/what-is-franchising.php">What is franchising?</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/advantages-and-disadvantages-of-franchising.php">Advantages and disadvantages of franchising</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/should-I-buy-a-franchise.php">Should I buy a franchise?</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/find-out-about-possible-franchises.php">Find out about possible franchises</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/assess-a-franchise-opportunity.php">Assess a franchise opportunity</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/the-costs-of-a-franchise.php">The costs of a franchise</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/how-to-purchase-a-franchise.php">How to purchase a franchise</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/tips-on-franchise-agreements.php">Tips on franchise agreements</a></li>
+				<li>Here's how I selected and bought my franchise</li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<div class="media-object stack-for-small">
+				<div class="media-object-section">
+					<div class="thumbnail">
+						<img src="../../../_img/CS0011.jpg" width="181" height="121" border="0" alt="Stephen Halpin, Boots Opticians Franchise" />
+					</div>
+				</div>
+				<div class="media-object-section">
+					<h3>
+						Stephen Halpin<br />
+						Boots Opticians Franchise<br />
+						Stephen's top tips:
+					</h3>
+					<ul>
+						<li>"Don't assume a franchise is an easy, risk-free option - you'll be running your own business with all the responsibility and hard work that entails."</li>
+						<li>"Use professional advisers and make sure they're franchise specialists."</li>
+						<li>"Negotiate your agreement and don't think you have to accept the first set of terms put in front of you."</li>
+					</ul>
+				</div>
+			</div>
+
+			<hr />
+
+			<h4>More case studies</h4>
+			<p><a href="/starting-up/business-planning/prepare-a-business-plan/here-is-how-having-an-up-to-date-business-plan-helped-my-business.php">Here's how having an up-to-date business plan helped my business</a></p>
+			<p><a href="/find-a-case-study.php"><strong>Find a case study</strong></a></p>
+
+			<hr />
+			
+			<p>Developed with:<br />
+				<a href="http://www.whichfranchise.com/" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/whichfranchise_logo_sm.gif" width="105" height="35" alt="" />whichfranchise.com
+				</a>
+			</p>
+
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
