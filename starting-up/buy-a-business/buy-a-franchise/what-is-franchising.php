a70bc2da759399385b5a7ddd9a97f2d8c18731f2
Caroline Hagan <info@blueocto.co.uk>
2017-04-11 16:28:27 +0100

    Completed “starting-up > buy-a-business > buy-a-franchise > what-is-franchising” page.

---
+++ starting-up/buy-a-business/buy-a-franchise/what-is-franchising.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>What is franchising? | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Buy a franchise</h1>
+			<h2>What is franchising?</h2>
+			<p>The term 'franchising' can describe some very different business arrangements. It is important to understand exactly what you're being offered.</p>
+			<h3>Business format franchise</h3>
+			<p>This is the most common form of franchising. A true business format franchise occurs when the owner of a business (the franchisor) grants a licence to another person or business (the franchisee) to use their business idea - often in a specific geographical area.</p>
+			<p>The franchisee sells the franchisor's product or services, trades under the franchisor's trade mark or trade name, and benefits from the franchisor's help and support.</p>
+			<p>In return, the franchisee usually pays an initial fee to the franchisor and then a percentage of the sales revenue.</p>
+			<p>The franchisee owns the outlet they run. But the franchisor keeps control over how products are marketed and sold and how their business idea is used.</p>
+			<p>Well-known businesses that offer franchises of this kind include Prontaprint, Dyno-Rod and McDonald's.</p>
+			
+			<h3>Other types of arrangement</h3>
+			<p>Different types of sales relationships are also sometimes referred to as franchises. For example:</p>
+			<ul>
+				<li><strong>Distributorship and dealership</strong> - you sell the product but don't usually trade under the franchise name. You have more freedom over how you run the business.</li>
+				<li><strong>Agency</strong> - you sell goods or services on behalf of the supplier.</li>
+				<li><strong>Licensee</strong> - you have a licence giving you the right to make and sell the licensor's product. There are usually no extra restrictions on how you run your business.</li>
+			</ul>
+			
+			<h3>Multi-level marketing</h3>
+			<p>Some businesses offer franchises that are really multi-level marketing schemes. This is where self-employed distributors sell goods on a manufacturer's behalf. You get commission on any sales you make, and also on sales made by other distributors you recruit.</p>
+			<p>Be aware that some multi-level marketing schemes may be dishonest or illegal. <a href="../../../_files/dti_the_trading-schemes-guide_june-2005.pdf" title="Guidance on trading schemes from BIS" target="_blank" rel="noopener noreferrer">Download the Trading Schemes Guide from the Department for Business, Innovation &amp; Skills (BIS) website (PDF, 66K)</a>.</p>
+			
+			<ul>
+				<li><a href="/buy-or-sell-a-business/franchising/buy-a-franchise.php" title="Buy a franchise">Prev</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/advantages-and-disadvantages-of-franchising.php" title="Advantages and disadvantages of franchising">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/buy-or-sell-a-business/franchising/buy-a-franchise.php" title="Buy a franchise">Introduction</a></li>
+				<li>What is franchising?</li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/advantages-and-disadvantages-of-franchising.php">Advantages and disadvantages of franchising</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/should-I-buy-a-franchise.php">Should I buy a franchise?</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/find-out-about-possible-franchises.php">Find out about possible franchises</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/assess-a-franchise-opportunity.php">Assess a franchise opportunity</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/the-costs-of-a-franchise.php">The costs of a franchise</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/how-to-purchase-a-franchise.php">How to purchase a franchise</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/tips-on-franchise-agreements.php">Tips on franchise agreements</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/heres-how-I-selected-and-bought-my-franchise.php">Here's how I selected and bought my franchise</a></li>
+			</ul>
+
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://www.thebfa.org/about-franchising" title="Find out about licensing a franchise which entitles the franchisee to trade under the trade name of the franchisor" target="_blank" rel="noopener noreferrer">Franchising explained on the British Franchise Association (BFA) website</a></li>
+				<li><a href="http://www.thebfa.org/franchise-seminars" title="Find out about franchise seminars including where and when they are being held" target="_blank" rel="noopener noreferrer">Franchising seminars listed on the BFA website</a></li>
+				<li><a href="../../../_files/dti_the_trading-schemes-guide_june-2005.pdf" title="Guidance on trading schemes from BIS" target="_blank" rel="noopener noreferrer">Download the Trading Schemes Guide from the BIS website (PDF, 66K)</a></li>
+				<li><a href="http://www.theukfranchisedirectory.net/" title="Franchise opportunities in the UK" target="_blank" rel="noopener noreferrer">UK franchise opportunities on the UK Franchise Directory website</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/set-up-business" title="Learn about the different types of business and legal structures to help you choose the right one for your new business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" title="Tutorials, videos and tools to help you decide how to become self-employed - from sole trader to limited company, franchisee to social enterprise and more" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" title="Case studies and tutorials to help you understand the legal dos and don'ts of naming your business" target="_blank" rel="noopener noreferrer">Name your business</a></li>
+				<li><a href="/buy-or-sell-a-business/buying/buy-an-existing-business.php">Buy an existing business</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+				<li><a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">Research and develop your business ideas, new products and services</a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+	</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
