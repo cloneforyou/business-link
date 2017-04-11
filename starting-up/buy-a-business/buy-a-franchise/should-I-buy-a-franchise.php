0512608a425bda940e4fc03cb6f9fe7654965d2f
Caroline Hagan <info@blueocto.co.uk>
2017-04-11 16:50:37 +0100

    Completed “starting-up > buy-a-business > buy-a-franchise > should-I-buy-a-franchise” page.

---
+++ starting-up/buy-a-business/buy-a-franchise/should-I-buy-a-franchise.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Should I buy a franchise? | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Buy a franchise</h1>
+			<h2>Should I buy a franchise?</h2>
+			<p>As with any new business venture, you need to consider carefully whether you have got the right skills and attitude to run a successful franchise. You need to consider how much you enjoy managing people, selling to the public or working alone - as these are common requirements of different types of franchise. This page will help you decide whether franchising is right for you and which type of franchise plays best to your strengths.</p>
+			
+			<h3>Assess yourself</h3>
+			<ul>
+				<li>You must be prepared to <strong>sell</strong> and you will need entrepreneurial flair. A franchise gives you a business blueprint - but it won't automatically give you customers.</li>
+				<li>You'll need to <strong>work hard</strong>, probably for long hours. Do you have the necessary dedication?</li>
+				<li>Running your own business can be <strong>stressful</strong>. Think how you react to pressure.</li>
+				<li>You may be starting up in business because you want to be <strong>your own boss</strong>. If so, would you be happy with the restrictions imposed by a franchise arrangement?</li>
+				<li>On the other hand, you may want to limit <strong>your risk</strong>. You might be more comfortable with a franchise than starting a new business from scratch.</li>
+			</ul>
+			
+			<h3>The right franchise for you</h3>
+			<ul>
+				<li>Do you like office work? Or would you prefer a business that involves physical labour or using a particular skill?</li>
+				<li>Are you happy working on your own? Or would you be good at recruiting, training and managing employees?</li>
+				<li>Do you like dealing with members of the public? Or would you prefer a franchise where you sell to business customers?</li>
+				<li>Are you weak in particular business skills such as finance? Can you find a franchise that offers the support you need in those areas?</li>
+			</ul>
+
+			<p><a href="http://www.whichfranchise.com/diagnostic_tool3.cfm" title="A quick survey to help identify if franchising is right for you and which type of franchise might suit you best" target="_blank" rel="noopener noreferrer">Take a test to find out if franchising is for you on the whichfranchise.com website</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/advantages-and-disadvantages-of-franchising.php" title="Advantages and disadvantages of franchising">Prev</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/find-out-about-possible-franchises.php" title="Find out about possible franchises">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/buy-or-sell-a-business/franchising/buy-a-franchise.php" title="Buy a franchise">Introduction</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/what-is-franchising.php">What is franchising?</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/advantages-and-disadvantages-of-franchising.php">Advantages and disadvantages of franchising</a></li>
+				<li>Should I buy a franchise?</li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/find-out-about-possible-franchises.php">Find out about possible franchises</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/assess-a-franchise-opportunity.php">Assess a franchise opportunity</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/the-costs-of-a-franchise.php">The costs of a franchise</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/how-to-purchase-a-franchise.php">How to purchase a franchise</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/tips-on-franchise-agreements.php">Tips on franchise agreements</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/heres-how-I-selected-and-bought-my-franchise.php">Here's how I selected and bought my franchise</a></li>
+			</ul>
+
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://www.whichfranchise.com/resources.cfm" title="Explains the legal stages you need to go through in order to own a franchise" target="_blank" rel="noopener noreferrer">Franchising suitability guidance on the whichfranchise.com website</a></li>
+				<li><a href="http://www.whichfranchise.com/diagnostic_tool3.cfm" title="A quick survey to help identify if franchising is right for you and which type of franchise might suit you best" target="_blank" rel="noopener noreferrer">Franchising suitability test on the whichfranchise.com website</a></li>
+				<li><a href="http://www.theukfranchisedirectory.net/" title="Franchise opportunities in the UK" target="_blank" rel="noopener noreferrer">UK franchise opportunities on the UK Franchise Directory website</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/set-up-business" title="Learn about the different types of business and legal structures to help you choose the right one for your new business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" title="Tutorials, videos and tools to help you decide how to become self-employed - from sole trader to limited company, franchisee to social enterprise and more" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" title="Case studies and tutorials to help you understand the legal dos and don'ts of naming your business" target="_blank" rel="noopener noreferrer">Name your business</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+				<li><a href="/buy-or-sell-a-business/buying/buy-an-existing-business.php">Buy an existing business</a></li>
+				<li><a href="/starting-up/business-planning/prepare-a-business-plan.php">Prepare a business plan</a></li>
+				<li><a href="/sales-and-marketing/marketing/write-a-marketing-plan.php">Write a marketing plan</a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+			<hr />
+			
+			<p>Developed with:<br />
+				<a href="http://www.whichfranchise.com/" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/whichfranchise_logo_sm.gif" width="105" height="35" alt="" />whichfranchise.com
+				</a>
+			</p>
+
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
