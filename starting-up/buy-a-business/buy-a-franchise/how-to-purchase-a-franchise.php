9201104e844fd819a7089509c5a5fa2b89a750a3
Caroline Hagan <info@blueocto.co.uk>
2017-04-11 21:10:17 +0100

    Completed “starting-up > buy-a-business > buy-a-franchise > how-to-purchase-a-franchise” page.

---
+++ starting-up/buy-a-business/buy-a-franchise/how-to-purchase-a-franchise.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>How to purchase a franchise | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Buy a franchise</h1>
+			<h2>How to purchase a franchise</h2>
+			<p>There are a number of key things you need to consider when planning to buy a franchise. It is worth thinking about the following:</p>
+			<ul>
+				<li>Assess yourself to see what kind of franchise, if any, will suit you.</li>
+				<li>Find out what franchises are available and draw up a shortlist.</li>
+				<li>Assess franchise opportunities carefully, ask questions and talk to other franchisees.</li>
+				<li>When you find a business, investigate its financial prospects. Base this on thorough research of performance figures. Include an analysis of three years' accounts - if they have been trading for that period - and management figures.</li>
+				<li>If you'll need to raise finance, ask your bank if it will consider a loan for the type of franchise you're considering.</li>
+				<li>Do your own market research into the business and the competitors in your area.</li>
+				<li>Draw up a business plan.</li>
+				<li>Check the franchise agreement and get professional advice.</li>
+			</ul>
+
+			<p>However, it is advisable to make sure you don't:</p>
+			<ul>
+				<li>take up the first opportunity before investigating alternatives</li>
+				<li>allow yourself to be hurried into making a decision</li>
+				<li>pay any non-refundable deposit</li>
+				<li>commit yourself before you're completely sure</li>
+				<li>assume a business will work in your area just because it works elsewhere</li>
+				<li>rely on the forecasts provided by the business selling you the franchise</li>
+				<li>sign any agreement without legal advice</li>
+			</ul>
+
+			<ul>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/the-costs-of-a-franchise.php" title="The costs of a franchise">Prev</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/tips-on-franchise-agreements.php" title="Tips on franchise agreements">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/buy-or-sell-a-business/franchising/buy-a-franchise.php" title="Buy a franchise">Introduction</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/what-is-franchising.php">What is franchising?</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/advantages-and-disadvantages-of-franchising.php">Advantages and disadvantages of franchising</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/should-I-buy-a-franchise.php">Should I buy a franchise?</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/find-out-about-possible-franchises.php">Find out about possible franchises</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/assess-a-franchise-opportunity.php">Assess a franchise opportunity</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/the-costs-of-a-franchise.php">The costs of a franchise</a></li>
+				<li>How to purchase a franchise</li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/tips-on-franchise-agreements.php">Tips on franchise agreements</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/heres-how-I-selected-and-bought-my-franchise.php">Here's how I selected and bought my franchise</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://www.whichfranchise.com/resources.cfm" target="_blank" rel="noopener noreferrer">Buying a franchise guidance on the whichfranchise.com website</a></li>
+				<li><a href="/do-it-online/search-company-information-using-companies-house-webcheck.php">Search company information using the WebCHeck service at Companies House</a></li>
+				<li><a href="http://www.theukfranchisedirectory.net/" title="Franchise opportunities in the UK" target="_blank" rel="noopener noreferrer">UK franchise opportunities on the UK Franchise Directory website</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/set-up-business" title="Learn about the different types of business and legal structures to help you choose the right one for your new business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" title="Tutorials, videos and tools to help you decide how to become self-employed - from sole trader to limited company, franchisee to social enterprise and more" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" title="Case studies and tutorials to help you understand the legal dos and don'ts of naming your business" target="_blank" rel="noopener noreferrer">Name your business</a></li>
+				<li><a href="/buy-or-sell-a-business/buying/buy-an-existing-business.php">Buy an existing business</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+				<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports.php">Market research and market reports</a></li>
+				<li><a href="/starting-up/business-planning/prepare-a-business-plan.php">Prepare a business plan</a></li>
+				<li><a href="/finance-and-grants/expert-financial-advice/choose-and-work-with-a-solicitor.php">Choose and work with a solicitor</a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+			
+			<p>Developed with:<br />
+				<a href="http://www.whichfranchise.com/" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/whichfranchise_logo_sm.gif" width="105" height="35" alt="" />whichfranchise.com
+				</a>
+			</p>
+
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
