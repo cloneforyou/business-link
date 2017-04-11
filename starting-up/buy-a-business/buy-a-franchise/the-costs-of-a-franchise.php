e2191f26ccb4810f45334628167b453100f45a9b
Caroline Hagan <info@blueocto.co.uk>
2017-04-11 21:08:06 +0100

    Completed “starting-up > buy-a-business > buy-a-franchise > the-costs-of-a-franchise” page.

---
+++ starting-up/buy-a-business/buy-a-franchise/the-costs-of-a-franchise.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>The costs of a franchise | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Buy a franchise</h1>
+			<h2>The costs of a franchise</h2>
+			<p>When calculating the likely cost of a franchise, you need to take both initial and ongoing fees into account.</p>
+			
+			<h3>Initial costs</h3>
+			<p>The franchisor - the business that sells you the franchise - usually charges an <strong>up-front fee</strong>. If the franchisor relies mainly on taking a percentage of your sales revenue, rather than on a high initial fee, it is usually a good indication that they have confidence in the value of their product or service.</p>
+			<p>Your largest initial costs are usually your <strong>investment</strong> in:</p>
+			<ul>
+				<li>premises</li>
+				<li>equipment</li>
+				<li>initial stock</li>
+			</ul>
+			
+			<p>You will need to establish <strong>a business entity</strong>. Although a franchisee holds a contractual agreement with the franchisor, each franchisee is an independent business - and it is this business entity that will enter into the franchise agreement. Your chosen business structure could be a limited company, partnership or sole trader - each of which will involve different costs - or your franchisor might have specific requirements. See our guide on <a href="/starting-up/business-structures/legal-structures-the-basics.php">legal structures: the basics</a>.</p>
+			
+			<h3>Continuing costs</h3>
+			<p>You usually pay a percentage of the sales revenue to the franchisor by way of a <strong>management service fee</strong>. Alternatively, you may pay a fixed management fee of some kind.</p>
+			<p>Under the terms of the franchise agreement, you may have to buy <strong>stock</strong> from the franchisor. Check what they charge. They may mark up the prices - or they may be able to offer them to you at a discount because of theirbuying power.</p>
+			<p>You also have to pay the usual business costs - for example, rent for premises, utility bills or the costs of any employees you take on. Again, check if the things that you pay for through the franchisor have a realistic cost.</p>
+			<p>Check too if the agreement includes additional charges. For example, you may be required to pay for <strong>training</strong>, or to contribute to the cost of national <strong>advertising campaigns</strong>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/assess-a-franchise-opportunity.php" title="Assess a franchise opportunity">Prev</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/how-to-purchase-a-franchise.php" title="How to purchase a franchise">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/buy-or-sell-a-business/franchising/buy-a-franchise.php" title="Buy a franchise">Introduction</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/what-is-franchising.php">What is franchising?</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/advantages-and-disadvantages-of-franchising.php">Advantages and disadvantages of franchising</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/should-I-buy-a-franchise.php">Should I buy a franchise?</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/find-out-about-possible-franchises.php">Find out about possible franchises</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/assess-a-franchise-opportunity.php">Assess a franchise opportunity</a></li>
+				<li>The costs of a franchise</li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/how-to-purchase-a-franchise.php">How to purchase a franchise</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/tips-on-franchise-agreements.php">Tips on franchise agreements</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-a-franchise/heres-how-I-selected-and-bought-my-franchise.php">Here's how I selected and bought my franchise</a></li>
+			</ul>
+
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://www.whichfranchise.com/feature_template.cfm?FeatureID=18" target="_blank" rel="noopener noreferrer">Franchise fees information and guidance on the whichfranchise.com website</a></li>
+				<li><a href="http://www.thebfa.org/join-a-franchise/how-to-choose-a-franchise" target="_blank" rel="noopener noreferrer">Choosing the right franchise guidance on the BFA website</a></li>
+				<li><a href="http://www.theukfranchisedirectory.net/" title="Franchise opportunities in the UK" target="_blank" rel="noopener noreferrer">UK franchise opportunities on the UK Franchise Directory website</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/set-up-business" title="Learn about the different types of business and legal structures to help you choose the right one for your new business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" title="Tutorials, videos and tools to help you decide how to become self-employed - from sole trader to limited company, franchisee to social enterprise and more" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" title="Case studies and tutorials to help you understand the legal dos and don'ts of naming your business" target="_blank" rel="noopener noreferrer">Name your business</a></li>
+				<li><a href="/buy-or-sell-a-business/buying/buy-an-existing-business.php">Buy an existing business</a></li>
+				<li><a href="/starting-up/business-planning/prepare-a-business-plan.php">Prepare a business plan</a></li>
+				<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting.php">Business budgeting</a></li>
+				<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/choose-the-right-finance-when-starting-up.php">Choose the right finance when starting up</a></li>
+				<li><a href="/finance-and-grants/prepare-to-raise-finance/how-to-attract-investment/use-your-business-plan-to-get-funding.php">Use your business plan to get funding</a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+			
+			<p>Developed with:<br />
+				<a href="http://www.whichfranchise.com/" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/whichfranchise_logo_sm.gif" width="105" height="35" alt="" />whichfranchise.com
+				</a>
+			</p>
+
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
