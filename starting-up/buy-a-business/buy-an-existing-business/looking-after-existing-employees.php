a5c5d3068e88cf3165b0da73c6492b041d800234
Caroline Hagan <info@blueocto.co.uk>
2017-04-14 22:14:46 +0100

    Completed “starting-up > buy-a-business > buy-an-existing-business > Looking after existing employees” page.

---
+++ starting-up/buy-a-business/buy-an-existing-business/looking-after-existing-employees.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Looking after existing employees | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Buy an existing business</h1>
+			<h2>Looking after existing employees</h2>
+			<p>There are regulations that govern what happens to employees when someone new takes over a business.</p>
+			<p>These apply to all employees when a business is transferred as a going concern, meaning employees automatically start working for the new owner under the same terms and conditions. For more information, see our guide on <a href="/employment-and-skills/redundancy-restructures-and-change/responsibilities-to-employees-if-you-buy-or-sell-a-business.php">responsibilities to employees if you buy or sell a business</a>.</p>
+			
+			<h3>Employment tribunal awards</h3>
+			<p>When you buy an existing business, you might decide you need to employ fewer staff. But be careful about making any changes, as an employee might take a case to an employment tribunal for unfair dismissal or unfair selection for redundancy. It's best to consult a solicitor before making any such changes. For more information see our guides on <a href="/employment-and-skills/dismissals-and-staff-leaving/dismissal.php">dismissal</a> and <a href="/employment-and-skills/redundancy-restructures-and-change/redundancy-the-options.php">redundancy: the options</a>.</p>
+			
+			<h3>Inform and consult employees</h3>
+			<p>If you do want to discuss reducing employee numbers or reorganising staff, it's a good idea to do this once you have completed the due diligence period, but before you take over the business. As the new employer you should inform and consult all employees - including employee representatives - who may be affected. For more information see our guide on how to <a href="/employment-and-skills/redundancy-restructures-and-change/inform-and-consult-your-employees.php">inform and consult your employees</a>.</p>
+			<h3>Pensions</h3>
+			<p>As their new employer, you do not have to take over rights and obligations relating to employees' occupational pension schemes put in place by the previous employer. However, if you don't provide comparable pensions arrangements, you could theoretically face a claim for unfair dismissal.</p>
+			
+			<ul>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/step-by-step-how-to-buy-a-business.php" title="Step-by-step: how to buy a business">Prev</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/buy-or-sell-a-business/buying/buy-an-existing-business.php" title="Buy an existing business">Introduction</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/advantages-and-disadvantages-of-buying-an-existing-business.php">Advantages and disadvantages of buying an existing business</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/deciding-on-the-right-type-of-business-to-buy.php">Deciding on the right type of business to buy</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/where-to-look-for-a-business-to-buy.php">Where to look for a business to buy</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/how-to-value-a-business.php">How to value a business</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/make-sure-a-business-is-worth-buying-due-diligence.php">Make sure a business is worth buying: due diligence</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/step-by-step-how-to-buy-a-business.php">Step-by-step: how to buy a business</a></li>
+				<li>Looking after existing employees</li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>HMRC New Employer Helpline</h3>
+			<p>0845 60 70 143</p>
+			
+			<h3>Lawyers for your Business</h3>
+			<p>020 7405 9075</p>
+			
+			<h3>Acas Helpline</h3>
+			<p>08457 47 47 47</p>
+			
+			<hr />
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance options for your business</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/employment-and-skills/redundancy-restructures-and-change/responsibilities-to-employees-if-you-buy-or-sell-a-business.php">Responsibilities to employees if you buy or sell a business</a></li>
+				<li><a href="/employment-and-skills/dismissals-and-staff-leaving/dismissal.php">Dismissal</a></li>
+				<li><a href="/employment-and-skills/redundancy-restructures-and-change/redundancy-the-options.php">Redundancy: the options</a></li>
+				<li><a href="/employment-and-skills/redundancy-restructures-and-change/inform-and-consult-your-employees.php">Inform and consult your employees</a></li>
+				<li><a href="/employment-and-skills/trade-unions/recognising-and-derecognising-a-trade-union.php">Recognising and derecognising a trade union</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
