db0427712a681c86ebe0884a17f709bd0236423f
Caroline Hagan <info@blueocto.co.uk>
2017-04-14 21:56:12 +0100

    Completed “starting-up > buy-a-business > buy-an-existing-business > Make sure a business is worth buying: due diligence” page.

---
+++ starting-up/buy-a-business/buy-an-existing-business/make-sure-a-business-is-worth-buying-due-diligence.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Make sure a business is worth buying: due diligence | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Buy an existing business</h1>
+			<h2>Make sure a business is worth buying: due diligence</h2>
+			<p>Having done your research, you should verify the information you have been given about your prospective new business. Once an offer has been made and accepted a period of time is allowed for you to access its books and records. This is known as due diligence. It should give you a realistic picture of how the business is performing now, and how it is likely to perform in the future. It should also highlight any issues or problems which might need warranting or guaranteeing.</p>
+			<p>There are traditionally three types of due diligence you should do. You might need different advisors for each:</p>
+			<ul>
+				<li><strong>legal due diligence</strong> - as part of a sales and purchase contract, the lawyers can check that the business has legal title to sell, ownership of all the assets and that regulatory and litigation issues are fully addressed</li>
+				<li><strong>financial due diligence</strong> - checking the numbers and making sure there are no black holes or hidden financial issues</li>
+				<li><strong>commercial due diligence</strong> - finding out the business' place in the marketplace, checking competitors and the regulatory environment</li>
+			</ul>
+
+			<h3>When to begin due diligence</h3>
+			<p>Don't start due diligence until you have agreed a price and terms with the seller. They may agree to take the business off the market during your investigation. This is known as an exclusivity period - and the seller will often ask for a down payment to secure it.</p>
+			<p>The investigation period is negotiable - but most small businesses need at least three to four weeks.</p>
+			
+			<h3>Where to get help</h3>
+			<p>Ideally you should get accountants and solicitors to help you identify risk areas but, if it is registered with Companies House, you can also obtain copies of the company accounts, the annual return and the other key documents filed by your target business using the Companies House WebCHeck service. The documents can be downloaded from the Companies House website, some at a small fee, helping you assess the value of the business and its assets.</p>
+			<p><a href="/do-it-online/search-company-information-using-companies-house-webcheck.php">Find detailed financial information about companies using the Companies House WebCHeck service</a>.</p>
+			<p>Due diligence is about much more than the finances of a business. You need to come out of this period knowing exactly what you are getting into, what needs to be fixed, what it will cost to fix, and if you are the right person to take on this business.</p>
+			<p>Key areas to cover are:</p>
+			<ul>
+				<li>employment terms and conditions</li>
+				<li>outstanding litigation</li>
+				<li>major contracts and orders</li>
+				<li>IT systems and other technology</li>
+				<li>environmental issues</li>
+				<li>commercial management including customer service, research and development, and marketing</li>
+			</ul>
+
+			<h3>Information sources</h3>
+			<p>Dig as deeply as you can and use whatever documents are available. For instance, if you're looking at employee records, you could check out:</p>
+			<ul>
+				<li>payroll records</li>
+				<li>staff files</li>
+				<li>copies of pension and profit-sharing plans, plus financial statements, if relevant</li>
+				<li>employment contracts</li>
+				<li>the staff manual</li>
+				<li>union contracts, if relevant</li>
+			</ul>
+
+			<p>You may also need information from external sources such as the landlord, tax office or bank.</p>
+			
+			<ul>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/how-to-value-a-business.php" title="How to value a business">Prev</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/step-by-step-how-to-buy-a-business.php" title="Step-by-step: how to buy a business">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/buy-or-sell-a-business/buying/buy-an-existing-business.php" title="Buy an existing business">Introduction</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/advantages-and-disadvantages-of-buying-an-existing-business.php">Advantages and disadvantages of buying an existing business</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/deciding-on-the-right-type-of-business-to-buy.php">Deciding on the right type of business to buy</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/where-to-look-for-a-business-to-buy.php">Where to look for a business to buy</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/how-to-value-a-business.php">How to value a business</a></li>
+				<li>Make sure a business is worth buying: due diligence</li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/step-by-step-how-to-buy-a-business.php">Step-by-step: how to buy a business</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/looking-after-existing-employees.php">Looking after existing employees</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+
+			<h3>Lawyers for your Business</h3>
+			<p>020 7405 9075</p>
+
+			<hr />
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="/do-it-online/search-company-information-using-companies-house-webcheck.php">Search company information using the WebCHeck service at Companies House</a></li>
+				<li><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Solicitor directory on the Law Society website</a></li>
+				<li><a href="http://www.startupdonut.co.uk/set-up-a-business/buying-a-business/buying-a-business" target="_blank" rel="noopener noreferrer">Buying a business factsheet on the Start up Donut website</a></li>
+				<li><a href="http://www.accaglobal.com/gb/en/member/find-an-accountant.html?isocountry=GB" target="_blank" rel="noopener noreferrer">Certified accountants listed on the Association of Chartered Certified Accountants website</a></li>
+				<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance options for your business</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+				<li><a href="/finance-and-grants/expert-financial-advice/choose-and-work-with-a-solicitor.php">Choose and work with a solicitor</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
