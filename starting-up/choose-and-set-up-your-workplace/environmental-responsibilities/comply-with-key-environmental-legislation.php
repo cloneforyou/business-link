612222201af2ebaf01be3519b2a51f6371c15ba5
Caroline Hagan <info@blueocto.co.uk>
2017-04-14 23:13:34 +0100

    Completed “starting-up > choose-and-set-up-your-workplace > environmental-responsibilities > Comply with key environmental legislation” page

--- starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/comply-with-key-environmental-legislation.php
+++ starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/comply-with-key-environmental-legislation.php
@@ -17,17 +17,17 @@
 			<h2>Comply with key environmental legislation</h2>
 			<p>There is a range of environmental rules your business may have to follow:</p>
 			<ul>
-				<li>Comply with legislation regarding emissions into the air. See our guide on <a href="layerc5bf.html?topicId=1080460523" title="Environmental guidance on how to avoid causing air pollution from your business">preventing air pollution</a>.</li>
-				<li>Store <strong>waste</strong> safely and securely, make sure it is treated appropriately, ensure it is collected by an authorised organisation (such as your local authority or a licensed private waste contractor) and complete a waste transfer note or consignment note when waste is handed over. Read our guide on <a href="layer6ca8.html?topicId=1079431917" title="An overview of the different laws you must comply with for different types of waste ">duty of care - your waste responsibilities</a>.</li>
-				<li>Ensure you do not cause a <strong>statutory nuisance</strong> which could affect someone's health or annoy your neighbours. This covers things like producing noise, smoke, fumes, gases, dust, odour, light pollution or accumulating rubbish. Read our guide on <a href="layer502e.html?topicId=1086041496" title="Types of nuisance, how to avoid causing them, and the duties of local authorities and the Environment Agency">noise, odour and other nuisances</a>.</li>
-				<li>Get permission from your water company before you allow <strong>trade effluent</strong> such as waste chemicals, detergents, cooling or cleaning water to enter the sewerage system. Read our guide on <a href="layerf43a.html?topicId=1086676178" title="An overview of trade effluent (liquid waste) and how its discharge is controlled and regulated">discharging trade effluent</a>.</li>
-				<li>Register with the Environment Agency, Northern Ireland Environment Agency or Scottish Environment Protection Agency, or join an approved compliance scheme if you handle more than 50 tonnes of packaging and have a turnover of more than &pound;2 million. You must then provide evidence that you're recovering and recycling a set amount of packaging waste. Read our guide on <a href="layer6760.html?topicId=1086901227" title="How to comply with packaging regulations and how to reduce costs and the environmental impact of packaging">packaging and packaging waste - your environmental responsibilities</a>.</li>
-				<li>Make sure that you comply with restrictions on the storage and use of <strong>hazardous substances</strong>. Ensure that any <strong>hazardous waste</strong> your business produces is correctly classified and described, and is either disposed of or recovered at an appropriately authorised facility. Read our guides on <a href="layer071b.html?topicId=1080525388" title="Guidance on safely using, storing, transporting or disposing of hazardous substances">hazardous substances and the environment - the basics</a> and <a href="layer9ae9.html?topicId=1080466581" title="How to manage hazardous waste and comply with legal requirements">managing your hazardous waste</a>.</li>
+				<li>Comply with legislation regarding emissions into the air. See our guide on <a href="/environment-and-efficiency/preventing-and-managing-pollution/preventing-air-pollution.php">preventing air pollution</a>.</li>
+				<li>Store <strong>waste</strong> safely and securely, make sure it is treated appropriately, ensure it is collected by an authorised organisation (such as your local authority or a licensed private waste contractor) and complete a waste transfer note or consignment note when waste is handed over. Read our guide on <a href="/environment-and-efficiency/waste-legislation-and-responsibility/duty-of-care-your-waste-responsibilities.php">duty of care - your waste responsibilities</a>.</li>
+				<li>Ensure you do not cause a <strong>statutory nuisance</strong> which could affect someone's health or annoy your neighbours. This covers things like producing noise, smoke, fumes, gases, dust, odour, light pollution or accumulating rubbish. Read our guide on <a href="/environment-and-efficiency/preventing-and-managing-pollution/noise-odour-and-other-nuisances.php">noise, odour and other nuisances</a>.</li>
+				<li>Get permission from your water company before you allow <strong>trade effluent</strong> such as waste chemicals, detergents, cooling or cleaning water to enter the sewerage system. Read our guide on <a href="/environment-and-efficiency/waste-legislation-and-responsibility/discharging-trade-effluent.php">discharging trade effluent</a>.</li>
+				<li>Register with the Environment Agency, Northern Ireland Environment Agency or Scottish Environment Protection Agency, or join an approved compliance scheme if you handle more than 50 tonnes of packaging and have a turnover of more than &pound;2 million. You must then provide evidence that you're recovering and recycling a set amount of packaging waste. Read our guide on <a href="/environment-and-efficiency/product-and-packaging-design/packaging-and-packaging-waste-your-environmental-responsibilities.php">packaging and packaging waste - your environmental responsibilities</a>.</li>
+				<li>Make sure that you comply with restrictions on the storage and use of <strong>hazardous substances</strong>. Ensure that any <strong>hazardous waste</strong> your business produces is correctly classified and described, and is either disposed of or recovered at an appropriately authorised facility. Read our guides on <a href="/environment-and-efficiency/managing-hazardous-substances/hazardous-substances-and-the-environment-the-basics.php">hazardous substances and the environment - the basics</a> and <a href="/environment-and-efficiency/waste-legislation-and-responsibility/managing-your-hazardous-waste.php">managing your hazardous waste</a>.</li>
 				<li>Notify the relevant enforcing authority and take steps to prevent the damage if your business activities pose an imminent threat to the environment. If your business activities cause actual environmental damage, you must take remedial action to repair the damage. See the page in this guide on how to <a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/prevent-and-remedy-pollution-incidents.php" title="Your responsibilities for reporting and preventing major environmental incidents">prevent and remedy pollution incidents</a>.</li>
 			</ul>
 			
-			<p>There are also specific environmental rules covering a wide range of businesses - from construction and electronics to textiles and chemicals manufacture. You can <a href="http://www.netregs.org.uk/business_sectors.aspx" target="bgExternalonlinebusinesslinkgovuk" title="How your business sector can comply with the law and reduce the risk of harm to the environment">check what environmental legislation applies to your business on the NetRegs website</a>.</p>
-			<p>Read our guide on <a href="layer6760.html?topicId=1086901227" title="How to comply with packaging regulations and how to reduce costs and the environmental impact of packaging">packaging and packaging waste - your environmental responsibilities</a>.</p>
+			<p>There are also specific environmental rules covering a wide range of businesses - from construction and electronics to textiles and chemicals manufacture. You can <a href="http://www.netregs.org.uk/business-sectors/" target="_blank" rel="noopener noreferrer">check what environmental legislation applies to your business on the NetRegs website</a>.</p>
+			<p>Read our guide on <a href="/environment-and-efficiency/product-and-packaging-design/packaging-and-packaging-waste-your-environmental-responsibilities.php">packaging and packaging waste - your environmental responsibilities</a>.</p>
 			
 			<ul>
 				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities.php" title="Environmental responsibilities">Prev</a></li>
@@ -56,33 +56,34 @@
 			
 			<h3>Actions</h3>
 			<ul>
-				<li><a href="http://www.netregs.org.uk/business_sectors.aspx" target="bgExternalonlinebusinesslinkgovuk" title="How your business sector can comply with the law and reduce the risk of harm to the environment">Environmental legislation explained for your industry sector on the NetRegs website</a></li>
-				<li><a href="http://www.wrap.org.uk/" target="bgExternalonlinebusinesslinkgovuk" title="Waste reduction advice">Waste reduction advice on the Envirowise website</a></li>
-				<li><a href="layer3ac4-2.html?topicId=1079438165" title="These interactive tools will help you find out what you need to do to comply with environmental legislation">Use our interactive tool to assess your environmental compliance</a></li>
+				<li><a href="http://www.netregs.org.uk/business-sectors/" target="_blank" rel="noopener noreferrer">Environmental legislation explained for your industry sector on the NetRegs website</a></li>
+				<li><a href="http://www.wrap.org.uk/" target="_blank" rel="noopener noreferrer">Waste reduction advice on the Envirowise website</a></li>
+				<li><a href="/environment-and-efficiency/measuring-and-reporting-environmental-performance/assess-your-environmental-compliance.php">Use our interactive tool to assess your environmental compliance</a></li>
 			</ul>
 			
 			<h3>Also on this site</h3>
 			<ul>
-				<li><a href="layerf48f.html?topicId=1080480296" title="How to comply with the law by getting the right environmental permits and other licences">Environmental permits and licences - an overview</a></li>
-				<li><a href="layer071b.html?topicId=1080525388" title="Guidance on safely using, storing, transporting or disposing of hazardous substances">Hazardous substances and the environment - the basics</a></li>
-				<li><a href="layerc5bf.html?topicId=1080460523" title="Environmental guidance on how to avoid causing air pollution from your business">Preventing air pollution</a></li>
-				<li><a href="layer502e.html?topicId=1086041496" title="Types of nuisance, how to avoid causing them, and the duties of local authorities and the Environment Agency">Noise, odour and other nuisances</a></li>
-				<li><a href="layerf43a.html?topicId=1086676178" title="An overview of trade effluent (liquid waste) and how its discharge is controlled and regulated">Discharging trade effluent</a></li>
-				<li><a href="layer9ae9.html?topicId=1080466581" title="How to manage hazardous waste and comply with legal requirements">Managing your hazardous waste</a></li>
-				<li><a href="layer6ca8.html?topicId=1079431917" title="An overview of the different laws you must comply with for different types of waste ">Duty of care - your waste responsibilities</a></li>
+				<li><a href="/environment-and-efficiency/permits-and-licences/environmental-permits-and-licences-an-overview.php">Environmental permits and licences - an overview</a></li>
+				<li><a href="/environment-and-efficiency/managing-hazardous-substances/hazardous-substances-and-the-environment-the-basics.php" title="Guidance on safely using, storing, transporting or disposing of hazardous substances">Hazardous substances and the environment - the basics</a></li>
+				<li><a href="/environment-and-efficiency/preventing-and-managing-pollution/preventing-air-pollution.php">Preventing air pollution</a></li>
+				<li><a href="/environment-and-efficiency/preventing-and-managing-pollution/noise-odour-and-other-nuisances.php">Noise, odour and other nuisances</a></li>
+				<li><a href="/environment-and-efficiency/waste-legislation-and-responsibility/discharging-trade-effluent.php">Discharging trade effluent</a></li>
+				<li><a href="/environment-and-efficiency/waste-legislation-and-responsibility/managing-your-hazardous-waste.php">Managing your hazardous waste</a></li>
+				<li><a href="/environment-and-efficiency/waste-legislation-and-responsibility/duty-of-care-your-waste-responsibilities.php">Duty of care - your waste responsibilities</a></li>
 			</ul>
 
 			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
 			
-			<p>Developed with:</p>
-			<img src="../../../_img/footer_top2.gif" alt="" />
-			<a href="http://www.defra.gov.uk/index.htm" target="bgExternalwwwbusinesslinkgovuk" title="Department for Environment Food and Rural Affairs - Opens in a new window"><img src="../../Regulation_Update_images/Defra_black_and_white_sml.jpg" width="84" height="65" alt="" />
-			<br/>Department for Environment Food and Rural Affairs</a>
-			<a href="http://www.environment-agency.gov.uk/" target="bgExternalwwwbusinesslinkgovuk" title="Environment Agency - Opens in a new window"><img src="../../Health_and_Environment_images/environment_agency_2006.gif" width="115" height="31" alt="" />
-			<br/>Environment Agency</a>
-
-			<img src="../../../_img/footer_top3.gif" alt="" />
-			
+			<p>
+				Developed with:<br />
+				<a href="http://webarchive.nationalarchives.gov.uk/20130822084033/http://defra.gov.uk/" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/Defra_black_and_white_sml.jpg" width="84" height="65" alt="" /> Department for Environment Food and Rural Affairs
+				</a>
+				<br />
+				<a href="https://www.gov.uk/government/organisations/environment-agency" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/environment_agency_2006.gif" width="115" height="31" alt="" /> Environment Agency
+				</a>
+			</p>
 				
 		</div>
 		<!-- // .small-12 large-8 large-push-4 columns -->
