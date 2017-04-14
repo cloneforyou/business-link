92dca0b5d0399f9a528b5bcbd5b237950c784441
Caroline Hagan <info@blueocto.co.uk>
2017-04-15 00:12:07 +0100

    Completed “starting-up > choose-and-set-up-your-workplace > environmental-responsibilities > The business benefits of good environmental and sustainable practices” page

--- starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/the-business-benefits-of-good-environmental-and-sustainable-practices.php
+++ starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/the-business-benefits-of-good-environmental-and-sustainable-practices.php
@@ -12,91 +12,84 @@
 	
 	<div class="row">
 		<div class="small-12 large-8 large-push-4 columns">
-        <h1>Environmental responsibilities</h1>
-        <h2>The business benefits of good environmental and sustainable practices</h2>
-        <p>Complying with legislation and going further to adopt environmental and sustainable practices can benefit your business. For example:</p>
-        <ul>
-            <li>Reducing your energy use can save you money. See our guide on how to <a href="layercbab.html?topicId=1079432186" title="Reduce costs and increase your competitiveness by managing how your business uses electricity, gas and other fuels">save money by using energy more efficiently</a>.</li>
-            <li>Reducing your use of raw materials can cut your costs, minimise your waste and lower your waste disposal bills. See our guide on how to <a href="layer33e4.html?topicId=1081267120" title="Your options for reducing and dealing with business waste and the benefits of choosing more sustainable practices">save money by reducing, reusing and recycling waste</a>.</li>
-            <li>Reducing your water use can cut your water bills and also lower your waste water disposal costs. See our guide on how to <a href="layer1bd4.html?topicId=1080452940" title="How to use water efficiently, the benefits of saving water and your responsibilities if you take or store water">use water efficiently - the basics</a>.</li>
-            <li>Recycling equipment and materials could make you money and save on disposal costs - you may be able to sell old equipment or parts to subsidise the cost of buying new ones. Read our guide on how to <a href="layer33e4.html?topicId=1081267120" title="Your options for reducing and dealing with business waste and the benefits of choosing more sustainable practices">save money by reducing, reusing and recycling waste</a>.</li>
-            <li>Taking steps to prevent environmental damage will reduce your risk of incurring costs and of having to carry out potentially expensive remedial work in the event of an environmental incident. You can <a href="http://www.defra.gov.uk/environment/quality/environmental-liability/" target="bgExternalonlinebusinesslinkgovuk" title="Introduction to environmental liability legislation and downloadable guidance">read information and guidance on environmental liability on the Department for Environment, Food and Rural Affairs (Defra) website</a>.</li>
-        </ul>
-        <p>Your business could also enjoy other benefits. Many government organisations and large companies will only work with businesses who take their environmental responsibilities seriously. You may also find it easier to attract investment from banks and other sources of finance.</p>
-        <p><a href="layer3ac4-2.html?topicId=1079438165" title="These interactive tools will help you find out what you need to do to comply with environmental legislation">Use our interactive tool to assess your environmental compliance</a>. You can also <a href="http://online.businesslink.gov.uk/bdotg/action/logicToolStart?itemId=1079446499&amp;type=BLTTOOL" title="Find out how to save money by going green">use our interactive tool to identify where you can save money by going green</a>.</p>
-        <p><strong>Sustainable development</strong> can create opportunities for your business. As well as making financial savings, you can enhance your reputation and brand value by fostering customer loyalty and motivating staff. You can also use sustainable development as an opportunity to encourage innovation, increase investment and open new markets.</p>
-        <p>Read our guide about the <a href="layer2570.html?topicId=1079416602" title="How a more environmentally friendly approach can protect your business, save money and improve its reputation">benefits of improving your environmental performance</a>.</p>
-        <ul>
-            <li><a href="detail8b34.html?itemId=1084010499&amp;type=RESOURCES" title="Prevent and remedy pollution incidents">Prev</a></li>
-        </ul>
-        <h3>Subjects covered in this guide</h3>
-        <ul>
-            <li><a href="layer8cd1.html?topicId=1079433025" title="Environmental responsibilities">Introduction</a></li>
-            <li><a href="detail6eb3.html?itemId=1079437508&amp;type=RESOURCES">Comply with key environmental legislation</a></li>
-            <li><a href="detail4195.html?itemId=1079437773&amp;type=RESOURCES">Does my business need a permit or licence?</a></li>
-            <li><a href="detail3813.html?itemId=1079437899&amp;type=RESOURCES">Hazardous substances covered by environmental legislation</a></li>
-            <li><a href="detailc2ce.html?itemId=1084012286&amp;type=RESOURCES">Conservation and biodiversity issues for businesses</a></li>
-            <li><a href="detailb9c6.html?itemId=1079438098&amp;type=RESOURCES">Environmental issues in specific business sectors</a></li>
-            <li><a href="detailf04a.html?itemId=1079437753&amp;type=RESOURCES">Who enforces environmental legislation?</a></li>
-            <li><a href="detail8b34.html?itemId=1084010499&amp;type=RESOURCES">Prevent and remedy pollution incidents</a></li>
-            <li>The business benefits of good environmental and sustainable practices</li>
-        </ul>
-        <p><img src="../../syntegra/images/hr_325px_global.gif" alt="" /></p>
-        
-        <?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
-        
-        <h3>Envirowise Advice Line</h3>
-        <p>0800 585 794
-            <br/>
-        </p>
-        <h3>Carbon Trust Advice Line</h3>
-        <p>0800 085 2005
-            <br/>
-        </p>
-        <h3>Environment Agency Helpline</h3>
-        <p>03708 506 506
-            <br/>
-        </p>
-        <p><img src="../../syntegra/images/hr_140px_global.gif" alt="" /></p>
-        <!--Helplines-->
-        <h3>Actions</h3>
-        <ul>
-            <li><a href="http://www.wrap.org.uk/" target="bgExternalonlinebusinesslinkgovuk" title="Information and advice from Envirowise about cross-sector initiatives">Waste management advice on the Envirowise website</a></li>
-            <li><a href="http://www.carbontrust.com/" target="bgExternalonlinebusinesslinkgovuk" title="Information on how to reduce costs by improving the energy efficiency of your business">Carbon emission reduction guidance on the Carbon Trust website</a></li>
-            <li><a href="layer3ac4-2.html?topicId=1079438165" title="These interactive tools will help you find out what you need to do to comply with environmental legislation">Use our interactive tool to assess your environmental compliance</a></li>
-        </ul>
-        </ul>
-        <img src="../../syntegra/images/px_startingabusiness.gif" alt="" width="181" height="5" />
-        <h3>Also on this site</h3>
-        <ul>
-            <li><a href="layer2570.html?topicId=1079416602" title="How a more environmentally friendly approach can protect your business, save money and improve its reputation">Benefits of improving your environmental performance</a></li>
-            <li><a href="layer2859.html?topicId=1079388461" title="How considering environmental, social and financial aspects of your business can lead to a more sustainable approach">Make your business more sustainable</a></li>
-            <li><a href="layer6ca8.html?topicId=1079431917" title="An overview of the different laws you must comply with for different types of waste ">Duty of care - your waste responsibilities</a></li>
-            <li><a href="layerbe2a.html?topicId=1079422683" title="Set up an environmental management system to boost efficiency, monitor progress and meet regulatory obligations">Environmental management systems (EMS) - the basics</a></li>
-            <li><a href="layer33e4.html?topicId=1081267120" title="Your options for reducing and dealing with business waste and the benefits of choosing more sustainable practices">Save money by reducing, reusing and recycling waste</a></li>
-            <li><a href="layer0040.html?topicId=1086057566" title="How green activities can benefit your farm business and the environment">Business innovation for agriculture</a></li>
-        </ul><img src="../../syntegra/images/px_startingabusiness.gif" alt="" width="181" height="5" />
-        <a>&nbsp;</a>
-        <!-- Page footer -->
-        <!-- "Developed With" section -->
-        Developed with:
-        <img src="../../syntegra/images/footer_top2.gif" alt="" />
-        <table height="100%" width="100%">
-            <tbody>
-                <tr>
-                    <td>
-                        <a href="http://www.defra.gov.uk/index.htm" target="bgExternalwwwbusinesslinkgovuk" title="Department for Environment Food and Rural Affairs - Opens in a new window"><img src="../../Regulation_Update_images/Defra_black_and_white_sml.jpg" width="84" height="65" alt="" />
-                            <br/>Department for Environment Food and Rural Affairs</a>
-                    </td>
-                    <td>
-                        <a href="http://www.environment-agency.gov.uk/" target="bgExternalwwwbusinesslinkgovuk" title="Environment Agency - Opens in a new window"><img src="../../Health_and_Environment_images/environment_agency_2006.gif" width="115" height="31" alt="" />
-                            <br/>Environment Agency</a>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+			
+			<h1>Environmental responsibilities</h1>
+			<h2>The business benefits of good environmental and sustainable practices</h2>
+			<p>Complying with legislation and going further to adopt environmental and sustainable practices can benefit your business. For example:</p>
+			<ul>
+				<li>Reducing your energy use can save you money. See our guide on how to <a href="/environment-and-efficiency/cut-energy-costs/save-money-by-using-energy-more-efficiently.php">save money by using energy more efficiently</a>.</li>
+				<li>Reducing your use of raw materials can cut your costs, minimise your waste and lower your waste disposal bills. See our guide on how to <a href="/environment-and-efficiency/reducing-and-recycling-waste/save-money-by-reducing-reusing-and-recycling-waste.php">save money by reducing, reusing and recycling waste</a>.</li>
+				<li>Reducing your water use can cut your water bills and also lower your waste water disposal costs. See our guide on how to <a href="/environment-and-efficiency/cut-water-costs/use-water-efficiently-the-basics.php">use water efficiently - the basics</a>.</li>
+				<li>Recycling equipment and materials could make you money and save on disposal costs - you may be able to sell old equipment or parts to subsidise the cost of buying new ones. Read our guide on how to <a href="/environment-and-efficiency/reducing-and-recycling-waste/save-money-by-reducing-reusing-and-recycling-waste.php">save money by reducing, reusing and recycling waste</a>.</li>
+				<li>Taking steps to prevent environmental damage will reduce your risk of incurring costs and of having to carry out potentially expensive remedial work in the event of an environmental incident. You can <a href="http://webarchive.nationalarchives.gov.uk/20130822084033/http://www.defra.gov.uk/environment/quality/environmental-liability/" target="bgExternalonlinebusinesslinkgovuk" target="_blank" rel="noopener noreferrer">read information and guidance on environmental liability on the Department for Environment, Food and Rural Affairs (Defra) website</a>.</li>
+			</ul>
 
-</div>
+			<p>Your business could also enjoy other benefits. Many government organisations and large companies will only work with businesses who take their environmental responsibilities seriously. You may also find it easier to attract investment from banks and other sources of finance.</p>
+			<p><a href="/environment-and-efficiency/measuring-and-reporting-environmental-performance/assess-your-environmental-compliance.php">Use our interactive tool to assess your environmental compliance</a>. You can also <a href="http://online.businesslink.gov.uk/bdotg/action/logicToolStart?itemId=1079446499&amp;type=BLTTOOL" target="_blank" rel="noopener noreferrer">use our interactive tool to identify where you can save money by going green</a>.</p>
+			<p><strong>Sustainable development</strong> can create opportunities for your business. As well as making financial savings, you can enhance your reputation and brand value by fostering customer loyalty and motivating staff. You can also use sustainable development as an opportunity to encourage innovation, increase investment and open new markets.</p>
+			<p>Read our guide about the <a href="/environment-and-efficiency/the-business-case-for-environmental-improvements/benefits-of-improving-your-environmental-performance.php">benefits of improving your environmental performance</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/prevent-and-remedy-pollution-incidents.php" title="Prevent and remedy pollution incidents">Prev</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities.php" title="Environmental responsibilities">Introduction</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/comply-with-key-environmental-legislation.php">Comply with key environmental legislation</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/does-my-business-need-a-permit-or-licence.php">Does my business need a permit or licence?</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/hazardous-substances-covered-by-environmental-legislation.php">Hazardous substances covered by environmental legislation</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/conservation-and-biodiversity-issues-for-businesses.php">Conservation and biodiversity issues for businesses</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/environmental-issues-in-specific-business-sectors.php">Environmental issues in specific business sectors</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/who-enforces-environmental-legislation.php">Who enforces environmental legislation?</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/prevent-and-remedy-pollution-incidents.php">Prevent and remedy pollution incidents</a></li>
+				<li>The business benefits of good environmental and sustainable practices</li>
+			</ul>
+
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Envirowise Advice Line</h3>
+			<p>0800 585 794</p>
+
+			<h3>Carbon Trust Advice Line</h3>
+			<p>0800 085 2005</p>
+
+			<h3>Environment Agency Helpline</h3>
+			<p>03708 506 506</p>
+			
+			<hr />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://www.wrap.org.uk/" target="_blank" rel="noopener noreferrer">Waste management advice on the Envirowise website</a></li>
+				<li><a href="https://www.carbontrust.com/home/" target="_blank" rel="noopener noreferrer">Carbon emission reduction guidance on the Carbon Trust website</a></li>
+				<li><a href="/environment-and-efficiency/measuring-and-reporting-environmental-performance/assess-your-environmental-compliance.php">Use our interactive tool to assess your environmental compliance</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/environment-and-efficiency/the-business-case-for-environmental-improvements/benefits-of-improving-your-environmental-performance.php">Benefits of improving your environmental performance</a></li>
+				<li><a href="/environment-and-efficiency/make-your-business-more-sustainable.php">Make your business more sustainable</a></li>
+				<li><a href="/environment-and-efficiency/waste-legislation-and-responsibility/duty-of-care-your-waste-responsibilities.php">Duty of care - your waste responsibilities</a></li>
+				<li><a href="/environment-and-efficiency/using-an-environmental-management-system-ems/using-an-environmental-management-system-ems-the-basics.php">Environmental management systems (EMS) - the basics</a></li>
+				<li><a href="/environment-and-efficiency/reducing-and-recycling-waste/save-money-by-reducing-reusing-and-recycling-waste.php">Save money by reducing, reusing and recycling waste</a></li>
+				<li><a href="/your-business-sector/farming/farm-business-planning/business-innovation-for-agriculture.php">Business innovation for agriculture</a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+			<p>
+				Developed with:
+				<br />
+				<a href="http://webarchive.nationalarchives.gov.uk/20130822084033/http://defra.gov.uk/" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/Defra_black_and_white_sml.jpg" width="84" height="65" alt="" /> Department for Environment Food and Rural Affairs
+				</a>
+				<br />
+				<a href="https://www.gov.uk/government/organisations/environment-agency" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/environment_agency_2006.gif" width="115" height="31" alt="" /> Environment Agency
+				</a>
+			</p>
+		</div>
 		<!-- // .small-12 large-8 large-push-4 columns -->
 		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
 	</div>
