66b1255c9cc4a264bdeb1a050cd3324fa7612852
Caroline Hagan <info@blueocto.co.uk>
2017-04-14 23:56:25 +0100

    Completed “starting-up > choose-and-set-up-your-workplace > environmental-responsibilities > Who enforces environmental legislation?” page

--- starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/who-enforces-environmental-legislation.php
+++ starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/who-enforces-environmental-legislation.php
@@ -12,89 +12,85 @@
 	
 	<div class="row">
 		<div class="small-12 large-8 large-push-4 columns">
-        <h1>Environmental responsibilities</h1>
-        <h2>Who enforces environmental legislation?</h2>
-        <p>There are three main environmental regulators in the UK:</p>
-        <ul>
-            <li>the Environment Agency in England and Wales</li>
-            <li>the Northern Ireland Environment Agency</li>
-            <li>the Scottish Environment Protection Agency</li>
-        </ul>
-        <p>The environmental regulators have produced the NetRegs website, which provides free environmental guidance. You can <a href="http://www.environment-agency.gov.uk/netregs/" target="bgExternalonlinebusinesslinkgovuk" title="Find out how to manage the different ways your business activities could affect the environment">find guidance on environmental legislation and good practice on the NetRegs website</a>.</p>
-        <p>You have the right to appeal against environmental regulators' decisions.</p>
-        <p>Your local authority is also responsible for regulating some environmental issues such as:</p>
-        <ul>
-            <li>air quality</li>
-            <li>statutory nuisance problems such as air pollution, noise, odour and light pollution</li>
-            <li>land contamination</li>
-            <li>environmental health</li>
-        </ul>
-        <p>See our guide on <a href="layer837b.html?topicId=1074439576" title="Local council services that could affect your business - business rates, trading standards, parking and planning">local authority services: an overview</a>.</p>
-        <p>Your local authority can also help you achieve compliance with various aspects of environmental legislation. <a href="http://online.businesslink.gov.uk/bdotg/action/findcontactbrowse?topicId=1074537118" title="All county, unitary, metropolitan, district and borough councils in England, Scotland, Wales and Northern Ireland">Find your local authority using our Contacts Directory</a>.</p>
-        <ul>
-            <li><a href="detailb9c6.html?itemId=1079438098&amp;type=RESOURCES" title="Environmental issues in specific business sectors">Prev</a></li>
-            <li><a href="detail8b34.html?itemId=1084010499&amp;type=RESOURCES" title="Prevent and remedy pollution incidents">Next</a></li>
-        </ul>
-        <h3>Subjects covered in this guide</h3>
-        <ul>
-            <li><a href="layer8cd1.html?topicId=1079433025" title="Environmental responsibilities">Introduction</a></li>
-            <li><a href="detail6eb3.html?itemId=1079437508&amp;type=RESOURCES">Comply with key environmental legislation</a></li>
-            <li><a href="detail4195.html?itemId=1079437773&amp;type=RESOURCES">Does my business need a permit or licence?</a></li>
-            <li><a href="detail3813.html?itemId=1079437899&amp;type=RESOURCES">Hazardous substances covered by environmental legislation</a></li>
-            <li><a href="detailc2ce.html?itemId=1084012286&amp;type=RESOURCES">Conservation and biodiversity issues for businesses</a></li>
-            <li><a href="detailb9c6.html?itemId=1079438098&amp;type=RESOURCES">Environmental issues in specific business sectors</a></li>
-            <li>Who enforces environmental legislation?</li>
-            <li><a href="detail8b34.html?itemId=1084010499&amp;type=RESOURCES">Prevent and remedy pollution incidents</a></li>
-            <li><a href="detail8573.html?itemId=1079437613&amp;type=RESOURCES">The business benefits of good environmental and sustainable practices</a></li>
-        </ul>
-        <p><img src="../../syntegra/images/hr_325px_global.gif" alt="" /></p>
-        
-        <?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
-        
-        <h3>Environment Agency Helpline</h3>
-        <p>03708 506 506
-            <br/>
-        </p>
-        <p><img src="../../syntegra/images/hr_140px_global.gif" alt="" /></p>
-        <!--Helplines-->
-        <h3>Actions</h3>
-        <ul>
-            <li><a href="http://www.environment-agency.gov.uk/netregs/" target="bgExternalonlinebusinesslinkgovuk" title="Find out how to manage the different ways your business activities could affect the environment">Environmental legislation and good practice guidance on the NetRegs website</a></li>
-            <li><a href="http://www.environment-agency.gov.uk/business/regulation/default.aspx" target="bgExternalonlinebusinesslinkgovuk" title="Understanding regulations that control pollution and environmental damage">Regulation of businesses in England and Wales information on the Environment Agency website</a></li>
-            <li><a href="http://online.businesslink.gov.uk/bdotg/action/findcontactbrowse?topicId=1074537118" title="All county, unitary, metropolitan, district and borough councils in England, Scotland, Wales and Northern Ireland">Find your local authority using our Contacts Directory</a></li>
-            <li><a href="layer3ac4-2.html?topicId=1079438165" title="These interactive tools will help you find out what you need to do to comply with environmental legislation">Use our interactive tool to assess your environmental compliance</a></li>
-        </ul>
-        </ul>
-        <img src="../../syntegra/images/px_startingabusiness.gif" alt="" width="181" height="5" />
-        <h3>Also on this site</h3>
-        <ul>
-            <li><a href="layerf48f.html?topicId=1080480296" title="How to comply with the law by getting the right environmental permits and other licences">Environmental permits and licences - an overview</a></li>
-            <li><a href="layer071b.html?topicId=1080525388" title="Guidance on safely using, storing, transporting or disposing of hazardous substances">Hazardous substances and the environment - the basics</a></li>
-            <li><a href="layer6ca8.html?topicId=1079431917" title="An overview of the different laws you must comply with for different types of waste ">Duty of care - your waste responsibilities</a></li>
-            <li><a href="layer2570.html?topicId=1079416602" title="How a more environmentally friendly approach can protect your business, save money and improve its reputation">Benefits of improving your environmental performance</a></li>
-            <li><a href="layer837b.html?topicId=1074439576" title="Local council services that could affect your business - business rates, trading standards, parking and planning">Local authority services: an overview</a></li>
-        </ul><img src="../../syntegra/images/px_startingabusiness.gif" alt="" width="181" height="5" />
-        <a>&nbsp;</a>
-        <!-- Page footer -->
-        <!-- "Developed With" section -->
-        Developed with:
-        <img src="../../syntegra/images/footer_top2.gif" alt="" />
-        <table height="100%" width="100%">
-            <tbody>
-                <tr>
-                    <td>
-                        <a href="http://www.defra.gov.uk/index.htm" target="bgExternalwwwbusinesslinkgovuk" title="Department for Environment Food and Rural Affairs - Opens in a new window"><img src="../../Regulation_Update_images/Defra_black_and_white_sml.jpg" width="84" height="65" alt="" />
-                            <br/>Department for Environment Food and Rural Affairs</a>
-                    </td>
-                    <td>
-                        <a href="http://www.environment-agency.gov.uk/" target="bgExternalwwwbusinesslinkgovuk" title="Environment Agency - Opens in a new window"><img src="../../Health_and_Environment_images/environment_agency_2006.gif" width="115" height="31" alt="" />
-                            <br/>Environment Agency</a>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+			
+			<h1>Environmental responsibilities</h1>
+			<h2>Who enforces environmental legislation?</h2>
+			<p>There are three main environmental regulators in the UK:</p>
+			<ul>
+				<li>the Environment Agency in England and Wales</li>
+				<li>the Northern Ireland Environment Agency</li>
+				<li>the Scottish Environment Protection Agency</li>
+			</ul>
 
-</div>
+			<p>The environmental regulators have produced the NetRegs website, which provides free environmental guidance. You can <a href="http://webarchive.nationalarchives.gov.uk/20140328084622/http://www.environment-agency.gov.uk/netregs/" target="_blank" rel="noopener noreferrer">find guidance on environmental legislation and good practice on the NetRegs website</a>.</p>
+			<p>You have the right to appeal against environmental regulators' decisions.</p>
+			<p>Your local authority is also responsible for regulating some environmental issues such as:</p>
+			<ul>
+				<li>air quality</li>
+				<li>statutory nuisance problems such as air pollution, noise, odour and light pollution</li>
+				<li>land contamination</li>
+				<li>environmental health</li>
+			</ul>
+
+			<p>See our guide on <a href="/environment-and-efficiency/sources-of-environmental-advice/local-authority-services-an-overview.php">local authority services: an overview</a>.</p>
+			<p>Your local authority can also help you achieve compliance with various aspects of environmental legislation. <a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/findcontactbrowse?topicId=1074537118" target="_blank" rel="noopener noreferrer">Find your local authority using our Contacts Directory</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/environmental-issues-in-specific-business-sectors.php" title="Environmental issues in specific business sectors">Prev</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/prevent-and-remedy-pollution-incidents.php" title="Prevent and remedy pollution incidents">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities.php" title="Environmental responsibilities">Introduction</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/comply-with-key-environmental-legislation.php">Comply with key environmental legislation</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/does-my-business-need-a-permit-or-licence.php">Does my business need a permit or licence?</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/hazardous-substances-covered-by-environmental-legislation.php">Hazardous substances covered by environmental legislation</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/conservation-and-biodiversity-issues-for-businesses.php">Conservation and biodiversity issues for businesses</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/environmental-issues-in-specific-business-sectors.php">Environmental issues in specific business sectors</a></li>
+				<li>Who enforces environmental legislation?</li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/prevent-and-remedy-pollution-incidents.php">Prevent and remedy pollution incidents</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/the-business-benefits-of-good-environmental-and-sustainable-practices.php">The business benefits of good environmental and sustainable practices</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Environment Agency Helpline</h3>
+			<p>03708 506 506</p>
+			
+			<hr />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20140328084622/http://www.environment-agency.gov.uk/netregs/" target="_blank" rel="noopener noreferrer">Environmental legislation and good practice guidance on the NetRegs website</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20140328084622/http://www.environment-agency.gov.uk/business/regulation/default.aspx" target="_blank" rel="noopener noreferrer">Regulation of businesses in England and Wales information on the Environment Agency website</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120823131012/http://online.businesslink.gov.uk/bdotg/action/findcontactbrowse?topicId=1074537118" target="_blank" rel="noopener noreferrer">Find your local authority using our Contacts Directory</a></li>
+				<li><a href="/environment-and-efficiency/measuring-and-reporting-environmental-performance/assess-your-environmental-compliance.php">Use our interactive tool to assess your environmental compliance</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/environment-and-efficiency/permits-and-licences/environmental-permits-and-licences-an-overview.php">Environmental permits and licences - an overview</a></li>
+				<li><a href="/environment-and-efficiency/managing-hazardous-substances/hazardous-substances-and-the-environment-the-basics.php">Hazardous substances and the environment - the basics</a></li>
+				<li><a href="/environment-and-efficiency/waste-legislation-and-responsibility/duty-of-care-your-waste-responsibilities.php">Duty of care - your waste responsibilities</a></li>
+				<li><a href="/environment-and-efficiency/the-business-case-for-environmental-improvements/benefits-of-improving-your-environmental-performance.php">Benefits of improving your environmental performance</a></li>
+				<li><a href="/environment-and-efficiency/sources-of-environmental-advice/local-authority-services-an-overview.php">Local authority services: an overview</a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+			
+			<p>
+				Developed with:
+				<br />
+				<a href="http://webarchive.nationalarchives.gov.uk/20130822084033/http://defra.gov.uk/" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/Defra_black_and_white_sml.jpg" width="84" height="65" alt="" /> Department for Environment Food and Rural Affairs
+				</a>
+				<br />
+				<a href="https://www.gov.uk/government/organisations/environment-agency" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/environment_agency_2006.gif" width="115" height="31" alt="" /> Environment Agency
+				</a>
+			</p>
+		</div>
 		<!-- // .small-12 large-8 large-push-4 columns -->
 		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
 	</div>
