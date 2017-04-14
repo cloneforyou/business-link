98af8f972be13a8b708f00faea9463505d92ce27
Caroline Hagan <info@blueocto.co.uk>
2017-04-14 21:49:49 +0100

    Completed “starting-up > buy-a-business > buy-an-existing-business > How to value a business” page.

---
+++ starting-up/buy-a-business/buy-an-existing-business/how-to-value-a-business.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>How to value a business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Buy an existing business</h1>
+			<h2>How to value a business</h2>
+			<p>Valuing a business can be one of the most worrying parts of buying an existing business.</p>
+			<p>There are several valuation methods you can use. For specific advice on valuation methods see our guide on how to <a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business.php">value and market your business</a>. Your accountant may be able to help you value the business, but a business transfer agent, business broker or corporate financier will be best qualified to provide valuation advice.</p>
+			<h3>A healthy business</h3>
+			<p>To get a general idea of how healthy the business is, look at:</p>
+			<ul>
+				<li>the history of the business</li>
+				<li>its current performance - sales, turnover, profit</li>
+				<li>future projections or a business plan</li>
+				<li>its financial situation - cashflow, debts, expenses, assets</li>
+				<li>why the business is being sold</li>
+				<li>any outstanding or major litigation the business is involved in</li>
+				<li>any regulatory changes which might have an impact on the business</li>
+			</ul>
+			<p>As part of your investigations, talk to the vendor and, if possible, the business' existing customers and suppliers. The vendor must be comfortable with you doing this and you must be sensitive to their position. Customer and suppliers may be able to give you information that affects your valuation, as well as information about market conditions affecting the business. Such research can also be done on the internet or at your local reference library.</p>
+			<p>For example, if the vendor is being forced to sell due to decreasing profits, your valuation might be lower.</p>
+			<h3>Intangible assets</h3>
+			<p>The most difficult part is valuing the intangible assets. These are usually difficult to measure and could include:</p>
+			<ul>
+				<li>the company's reputation</li>
+				<li>the relationship with suppliers</li>
+				<li>the value of goodwill</li>
+				<li>the value of licences</li>
+				<li>patents or intellectual property</li>
+			</ul>
+			<p>You should consider how the value of these assets could be affected if you decide to buy the business.</p>
+			<h3>Other considerations</h3>
+			<p>The list below details other factors that will affect the value:</p>
+			<ul>
+				<li>stock</li>
+				<li>location</li>
+				<li>assets</li>
+				<li>products</li>
+				<li>debtors</li>
+				<li>creditors</li>
+				<li>suppliers</li>
+				<li>employees</li>
+				<li>premises</li>
+				<li>competition</li>
+				<li>benchmarking - what other businesses in the sector have sold for</li>
+				<li>who else in the sector is for sale or on the market</li>
+				<li>the economic climate - will any new government legislation have an impact on the business</li>
+			</ul>
+			<p>Once you have considered all these factors you can then decide how much you want to offer, or whether you want to buy it at all.</p>
+			<p>If you do decide to make an offer, and agree a price with the seller, a period of time is allowed for you to verify that all of the information you have been told is accurate. This is known as <strong>due diligence</strong>. See the page in this guide on how to <a href="/starting-up/buy-a-business/buy-an-existing-business/make-sure-a-business-is-worth-buying-due-diligence.php">make sure a business is worth buying: due diligence</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/where-to-look-for-a-business-to-buy.php" title="Where to look for a business to buy">Prev</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/make-sure-a-business-is-worth-buying-due-diligence.php" title="Make sure a business is worth buying: due diligence">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/buy-or-sell-a-business/buying/buy-an-existing-business.php" title="Buy an existing business">Introduction</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/advantages-and-disadvantages-of-buying-an-existing-business.php">Advantages and disadvantages of buying an existing business</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/deciding-on-the-right-type-of-business-to-buy.php">Deciding on the right type of business to buy</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/where-to-look-for-a-business-to-buy.php">Where to look for a business to buy</a></li>
+				<li>How to value a business</li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/make-sure-a-business-is-worth-buying-due-diligence.php">Make sure a business is worth buying: due diligence</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/step-by-step-how-to-buy-a-business.php">Step-by-step: how to buy a business</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/looking-after-existing-employees.php">Looking after existing employees</a></li>
+			</ul>
+
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://www.accaglobal.com/gb/en/member/find-an-accountant.html?isocountry=GB" target="_blank" rel="noopener noreferrer">Certified accountants listed on the Association of Chartered Certified Accountants website</a></li>
+				<li><a href="http://www.startinbusiness.co.uk/buyingbiz.htm#4" target="_blank" rel="noopener noreferrer">Business valuation advice on the Start in Business website</a></li>
+				<li><a href="/do-it-online/search-company-information-using-companies-house-webcheck.php">Search company information using the WebCHeck service at Companies House</a></li>
+				<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance options for your business</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business.php">Value and market your business</a></li>
+				<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php">Legal structures: the basics</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
