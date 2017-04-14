6e35d5410387dc36c313bb2c6f9e1cf210f442c3
Caroline Hagan <info@blueocto.co.uk>
2017-04-14 21:34:22 +0100

    Completed “starting-up > buy-a-business > buy-an-existing-business > deciding-on-the-right-type-of-business-to-buy” page.

---
+++ starting-up/buy-a-business/buy-an-existing-business/deciding-on-the-right-type-of-business-to-buy.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Deciding on the right type of business to buy | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Buy an existing business</h1>
+			<h2>Deciding on the right type of business to buy</h2>
+			<p>Ideally any business you buy needs to fit your own skills, lifestyle and aspirations. Before you start looking, think about what you can bring to a business and what you'd like to get back.</p>
+			<p>List what is important to you. Look at your motivations and what you ultimately want to achieve. It is useful to consider:</p>
+			<ul type="disc">
+				<li>Your <strong>abilities</strong> - can you achieve what you want to achieve?</li>
+				<li>Your <strong>capital</strong> - how much money do you have to invest?</li>
+				<li>Your <strong>expectations</strong> in terms of earning - what level of profit do you need to be looking for to accommodate your needs?</li>
+				<li>Your <strong>commitment</strong> - are you prepared for all the hard work and money that you will need to put into the business to get it to succeed?</li>
+				<li>Your <strong>strengths</strong> - what kind of business opportunity will give you the chance to put your skills and experience to good use?</li>
+				<li>The <strong>business sector</strong> you're interested in - learn as much as you can about your chosen industry so you can compare different businesses. It's important to take the time to talk to people already in similar businesses. The internet and your local library will also be good sources of information. Find out how to comply with all the regulations and licences that apply to <a href="/your-business-sector.php">Your business sector</a>.</li>
+				<li><strong>Location</strong> - don't restrict your search to your local area. Some businesses can be easily relocated.</li>
+			</ul>
+
+			<ul>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/advantages-and-disadvantages-of-buying-an-existing-business.php" title="Advantages and disadvantages of buying an existing business">Prev</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/where-to-look-for-a-business-to-buy.php" title="Where to look for a business to buy">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/buy-or-sell-a-business/buying/buy-an-existing-business.php" title="Buy an existing business">Introduction</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/advantages-and-disadvantages-of-buying-an-existing-business.php">Advantages and disadvantages of buying an existing business</a></li>
+				<li>Deciding on the right type of business to buy</li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/where-to-look-for-a-business-to-buy.php">Where to look for a business to buy</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/how-to-value-a-business.php">How to value a business</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/make-sure-a-business-is-worth-buying-due-diligence.php">Make sure a business is worth buying: due diligence</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/step-by-step-how-to-buy-a-business.php">Step-by-step: how to buy a business</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/looking-after-existing-employees.php">Looking after existing employees</a></li>
+			</ul>
+
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="/your-business-sector.php">Use our interactive tool to find regulations, licences, standards and trade bodies for your business</a></li>
+				<li><a href="http://www.startupdonut.co.uk/set-up-a-business/buying-a-business" target="_blank" rel="noopener noreferrer">Buying a business advice on the Start up Donut website</a></li>
+				<li><a href="/do-it-online/search-company-information-using-companies-house-webcheck.php">Search company information using the WebCHeck service with Companies House</a></li>
+				<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance options for your business</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/buy-or-sell-a-business/franchising/buy-a-franchise.php">Buy a franchise</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+			
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
