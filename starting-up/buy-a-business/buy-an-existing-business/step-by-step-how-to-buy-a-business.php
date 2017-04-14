503d8415bb57a03a98c13d4c0156fa3325a638a1
Caroline Hagan <info@blueocto.co.uk>
2017-04-14 22:05:19 +0100

    Completed “starting-up > buy-a-business > buy-an-existing-business > Step-by-step: how to buy a business” page.

---
+++ starting-up/buy-a-business/buy-an-existing-business/step-by-step-how-to-buy-a-business.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Step-by-step: how to buy a business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Buy an existing business</h1>
+			<h2>Step-by-step: how to buy a business</h2>
+			<p>An organised approach will help you find and acquire the right business.</p>
+			
+			<h3>1. Get professional advice</h3>
+			<p>Professional help is invaluable as you go through the negotiation, valuation and purchase process. You can find details of how to find professional help in our guides on how to <a href="/finance-and-grants/expert-financial-advice/choose-and-work-with-a-solicitor.php">choose and work with a solicitor</a> and how to <a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">choose and work with an accountant</a>.</p>
+			<p>You can also <a href="http://www.lucasweston.com/how-to-choose-a-business-broker.htm" target="_blank" rel="noopener noreferrer">find clear advice on buying a business through a business broker on the Lucas &amp; Weston website</a>.</p>
+			
+			<h3>2. Research</h3>
+			<p>Research the sector you're interested in, including the best time to buy, and shortlist two or three businesses.</p>
+			
+			<h3>3. Initial viewing and valuation</h3>
+			<p>Be discreet - the owner may not want staff to know they are selling, but be thorough and record key findings.</p>
+			
+			<h3>4. Arrange finance</h3>
+			<p>Lenders generally require:</p>
+			<ul>
+				<li>details of the business/sales particulars</li>
+				<li>accounts for the last three years</li>
+				<li>financial projections - if no accounts are available</li>
+				<li>details of your personal assets and liabilities</li>
+			</ul>
+			
+			<p>There are several possible sources of finance you could consider. For specific advice, see our guides on <a href="/finance-and-grants/finance-options/borrowing/bank-finance.php">bank finance</a>, <a href="/finance-and-grants/finance-options/borrowing/financing-from-friends-and-family.php">financing from friends and family</a> and <a href="/finance-and-grants/finance-options/shares-and-equity-finance/equity-finance.php">equity finance</a>.</p>
+			
+			<p><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance for your business</a>.</p>
+			
+			<h3>5. Make a formal offer</h3>
+			<p>If you make your initial offer by phone, follow this up in writing. Head your letter <strong>subject to contract</strong> and include this phrase in all written communication.</p>
+			
+			<h3>6. Negotiation</h3>
+			<p>Before completing the sale, it may be worth trying to negotiate an overlap period so you have time to become familiar with the business before taking over.</p>
+			<p>You and your solicitor need to verify the information you have based your offer on. See the page in this guide on how to <a href="/starting-up/buy-a-business/buy-an-existing-business/make-sure-a-business-is-worth-buying-due-diligence.php">make sure a business is worth buying: due diligence</a>.</p>
+			<p>If you're buying premises, you may want to arrange an independent survey and valuation, even if a lender is also carrying out their own survey and valuation at your expense. You can <a href="https://www.ricsfirms.com/" target="_blank" rel="noopener noreferrer">find a surveryor who specialises in commercial property on the Royal Institution of Chartered Surveyors (RICS) website</a>.</p>
+			
+			<h3>7. Completion</h3>
+			<p>Even after you reach an agreement on the price and terms of sale, the deal could still fall through. You have to meet certain conditions of sale to complete, including:</p>
+			<ul>
+				<li>verification of financial statements</li>
+				<li>transfer of leases</li>
+				<li>transfer of contracts/licences</li>
+				<li>transfer of finance</li>
+				<li>transfer of existing or new VAT registration</li>
+			</ul>
+			
+			<ul>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/make-sure-a-business-is-worth-buying-due-diligence.php" title="Make sure a business is worth buying: due diligence">Prev</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/looking-after-existing-employees.php" title="Looking after existing employees">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/buy-or-sell-a-business/buying/buy-an-existing-business.php" title="Buy an existing business">Introduction</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/advantages-and-disadvantages-of-buying-an-existing-business.php">Advantages and disadvantages of buying an existing business</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/deciding-on-the-right-type-of-business-to-buy.php">Deciding on the right type of business to buy</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/where-to-look-for-a-business-to-buy.php">Where to look for a business to buy</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/how-to-value-a-business.php">How to value a business</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/make-sure-a-business-is-worth-buying-due-diligence.php">Make sure a business is worth buying: due diligence</a></li>
+				<li>Step-by-step: how to buy a business</li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/looking-after-existing-employees.php">Looking after existing employees</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+			
+			<h3>Lawyers for your Business</h3>
+			<p>020 7405 9075</p>
+			
+			<hr />
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://www.businessesforsale.com/" target="_blank" rel="noopener noreferrer">Businesses for sale on the BusinessesForSale website</a></li>
+				<li><a href="https://www.daltonsbusiness.com/buy/business-for-sale-listing" target="_blank" rel="noopener noreferrer">Businesses for sale database on the Daltons Business website</a></li>
+				<li><a href="http://www.accaglobal.com/gb/en/member/find-an-accountant.html?isocountry=GB" target="_blank" rel="noopener noreferrer">Certified accountants listed on the Association of Chartered Certified Accountants website</a></li>
+				<li><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Solicitor directory on the Law Society website</a></li>
+				<li><a href="https://www.ricsfirms.com/" target="_blank" rel="noopener noreferrer">Commercial property surveyor directory on the RICS website</a></li>
+				<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance options for your business</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/finance-and-grants/expert-financial-advice/choose-and-work-with-a-solicitor.php">Choose and work with a solicitor</a></li>
+				<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+				<li><a href="/finance-and-grants/finance-options/borrowing/bank-finance.php">Bank finance</a></li>
+				<li><a href="/finance-and-grants/finance-options/borrowing/financing-from-friends-and-family.php">Financing from friends and family</a></li>
+				<li><a href="/finance-and-grants/finance-options/shares-and-equity-finance/equity-finance.php">Equity finance</a></li>
+				<li><a href="layer2852.html?topicId=1073958998">Use your business plan to get funding</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+			
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
