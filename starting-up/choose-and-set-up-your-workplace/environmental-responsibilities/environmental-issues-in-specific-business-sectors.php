e31883f9086dfc3ba706b4e73cc5ef8474aeee76
Caroline Hagan <info@blueocto.co.uk>
2017-04-14 23:52:26 +0100

    Completed “starting-up > choose-and-set-up-your-workplace > environmental-responsibilities > Environmental issues in specific business sectors” page

--- starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/environmental-issues-in-specific-business-sectors.php
+++ starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/environmental-issues-in-specific-business-sectors.php
@@ -12,75 +12,69 @@
 	
 	<div class="row">
 		<div class="small-12 large-8 large-push-4 columns">
-        <h1>Environmental responsibilities</h1>
-        <h2>Environmental issues in specific business sectors</h2>
-        <p>Different types of businesses face their own particular environmental issues. For example, factory processes may use large amounts of energy or involve hazardous substances, while office-based practices are more concerned with reducing energy and water waste. Transport businesses may use large amounts of fuel and create significant vehicle emissions.</p>
-        <p>Whatever sector you are in, it's a good idea to seek advice on the major issues. <a href="http://www.environment-agency.gov.uk/netregs/" target="bgExternalonlinebusinesslinkgovuk" title="Environmental guidance for small and medium-sized businesses">Find guidance on environmental legislation and good practice for your business on the NetRegs website</a>.</p>
-        <p>A good first step is to conduct an environmental review of your business. This will help you to identify what you need to do to comply with the law and how you could minimise waste, emissions, energy usage and your impact on the environment. See our guide on how to <a href="layerfcc5.html?topicId=1082899305" title="How reviewing your business' activities and processes can help you to reduce your environmental impact">carry out an environment review of your business</a>. You can also use our interactive tool to assess your environmental compliance.</p>
-        <p>If you would like more specialist advice, you may need to engage an environmental consultant. In certain business sectors, such as quarrying, engineering and chemicals, consultants are regularly used. An environmental consultant can give specific advice about your environmental responsibilities, including insurance and business development plans. See our guide on <a href="layer959f.html?topicId=1079422061" title="How environmental consultants can provide help and advice to your business on environmental issues">using an environmental consultant</a>.</p>
-        <ul>
-            <li><a href="detailc2ce.html?itemId=1084012286&amp;type=RESOURCES" title="Conservation and biodiversity issues for businesses">Prev</a></li>
-            <li><a href="detailf04a.html?itemId=1079437753&amp;type=RESOURCES" title="Who enforces environmental legislation?">Next</a></li>
-        </ul>
-        <h3>Subjects covered in this guide</h3>
-        <ul>
-            <li><a href="layer8cd1.html?topicId=1079433025" title="Environmental responsibilities">Introduction</a></li>
-            <li><a href="detail6eb3.html?itemId=1079437508&amp;type=RESOURCES">Comply with key environmental legislation</a></li>
-            <li><a href="detail4195.html?itemId=1079437773&amp;type=RESOURCES">Does my business need a permit or licence?</a></li>
-            <li><a href="detail3813.html?itemId=1079437899&amp;type=RESOURCES">Hazardous substances covered by environmental legislation</a></li>
-            <li><a href="detailc2ce.html?itemId=1084012286&amp;type=RESOURCES">Conservation and biodiversity issues for businesses</a></li>
-            <li>Environmental issues in specific business sectors</li>
-            <li><a href="detailf04a.html?itemId=1079437753&amp;type=RESOURCES">Who enforces environmental legislation?</a></li>
-            <li><a href="detail8b34.html?itemId=1084010499&amp;type=RESOURCES">Prevent and remedy pollution incidents</a></li>
-            <li><a href="detail8573.html?itemId=1079437613&amp;type=RESOURCES">The business benefits of good environmental and sustainable practices</a></li>
-        </ul>
-        <p><img src="../../syntegra/images/hr_325px_global.gif" alt="" /></p>
-        
-        <?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
-        
-        <h3>Environment Agency Helpline</h3>
-        <p>03708 506 506
-            <br/>
-        </p>
-        <p><img src="../../syntegra/images/hr_140px_global.gif" alt="" /></p>
-        <!--Helplines-->
-        <h3>Actions</h3>
-        <ul>
-            <li><a href="http://www.netregs.org.uk/business_sectors.aspx" target="bgExternalonlinebusinesslinkgovuk" title="How your business sector can comply with the law and reduce the risk of harm to the environment">Environmental guidance for your business sector on the NetRegs website</a></li>
-            <li><a href="layer3ac4-2.html?topicId=1079438165" title="These interactive tools will help you find out what you need to do to comply with environmental legislation">Use our interactive tool to assess your environmental compliance</a></li>
-        </ul>
-        </ul>
-        <img src="../../syntegra/images/px_startingabusiness.gif" alt="" width="181" height="5" />
-        <h3>Also on this site</h3>
-        <ul>
-            <li><a href="layerfcc5.html?topicId=1082899305" title="How reviewing your business' activities and processes can help you to reduce your environmental impact">Carry out an environmental review of your business</a></li>
-            <li><a href="layerf48f.html?topicId=1080480296" title="How to comply with the law by getting the right environmental permits and other licences">Environmental permits and licences - an overview</a></li>
-            <li><a href="layer9ca4-2.html?topicId=1082900339" title="Complying with restrictions on the use of certain hazardous substances in electrical and electronic equipment">Restriction of the use of certain hazardous substances in electrical and electronic equipment (RoHS)</a></li>
-            <li><a href="layer071b.html?topicId=1080525388" title="Guidance on safely using, storing, transporting or disposing of hazardous substances">Hazardous substances and the environment - the basics</a></li>
-            <li><a href="layer6ca8.html?topicId=1079431917" title="An overview of the different laws you must comply with for different types of waste ">Duty of care - your waste responsibilities</a></li>
-            <li><a href="layer959f.html?topicId=1079422061" title="How environmental consultants can provide help and advice to your business on environmental issues">Using an environmental consultant</a></li>
-        </ul><img src="../../syntegra/images/px_startingabusiness.gif" alt="" width="181" height="5" />
-        <a>&nbsp;</a>
-        <!-- Page footer -->
-        <!-- "Developed With" section -->
-        Developed with:
-        <img src="../../syntegra/images/footer_top2.gif" alt="" />
-        <table height="100%" width="100%">
-            <tbody>
-                <tr>
-                    <td>
-                        <a href="http://www.defra.gov.uk/index.htm" target="bgExternalwwwbusinesslinkgovuk" title="Department for Environment Food and Rural Affairs - Opens in a new window"><img src="../../Regulation_Update_images/Defra_black_and_white_sml.jpg" width="84" height="65" alt="" />
-                            <br/>Department for Environment Food and Rural Affairs</a>
-                    </td>
-                    <td>
-                        <a href="http://www.environment-agency.gov.uk/" target="bgExternalwwwbusinesslinkgovuk" title="Environment Agency - Opens in a new window"><img src="../../Health_and_Environment_images/environment_agency_2006.gif" width="115" height="31" alt="" />
-                            <br/>Environment Agency</a>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+			
+			<h1>Environmental responsibilities</h1>
+			<h2>Environmental issues in specific business sectors</h2>
+			<p>Different types of businesses face their own particular environmental issues. For example, factory processes may use large amounts of energy or involve hazardous substances, while office-based practices are more concerned with reducing energy and water waste. Transport businesses may use large amounts of fuel and create significant vehicle emissions.</p>
+			<p>Whatever sector you are in, it's a good idea to seek advice on the major issues. <a href="http://webarchive.nationalarchives.gov.uk/20140328084622/http://www.environment-agency.gov.uk/netregs/" target="_blank" rel="noopener noreferrer">Find guidance on environmental legislation and good practice for your business on the NetRegs website</a>.</p>
+			<p>A good first step is to conduct an environmental review of your business. This will help you to identify what you need to do to comply with the law and how you could minimise waste, emissions, energy usage and your impact on the environment. See our guide on how to <a href="/environment-and-efficiency/measuring-and-reporting-environmental-performance/carry-out-an-environmental-review-of-your-business.php">carry out an environment review of your business</a>. You can also use our interactive tool to assess your environmental compliance.</p>
+			<p>If you would like more specialist advice, you may need to engage an environmental consultant. In certain business sectors, such as quarrying, engineering and chemicals, consultants are regularly used. An environmental consultant can give specific advice about your environmental responsibilities, including insurance and business development plans. See our guide on <a href="/environment-and-efficiency/sources-of-environmental-advice/using-an-environmental-consultant.php">using an environmental consultant</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/conservation-and-biodiversity-issues-for-businesses.php" title="Conservation and biodiversity issues for businesses">Prev</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/who-enforces-environmental-legislation.php" title="Who enforces environmental legislation?">Next</a></li>
+			</ul>
 
-</div>
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities.php" title="Environmental responsibilities">Introduction</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/comply-with-key-environmental-legislation.php">Comply with key environmental legislation</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/does-my-business-need-a-permit-or-licence.php">Does my business need a permit or licence?</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/hazardous-substances-covered-by-environmental-legislation.php">Hazardous substances covered by environmental legislation</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/conservation-and-biodiversity-issues-for-businesses.php">Conservation and biodiversity issues for businesses</a></li>
+				<li>Environmental issues in specific business sectors</li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/who-enforces-environmental-legislation.php">Who enforces environmental legislation?</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/prevent-and-remedy-pollution-incidents.php">Prevent and remedy pollution incidents</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/the-business-benefits-of-good-environmental-and-sustainable-practices.php">The business benefits of good environmental and sustainable practices</a></li>
+			</ul>
+
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Environment Agency Helpline</h3>
+			<p>03708 506 506</p>
+			
+			<hr />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://www.netregs.org.uk/business-sectors/" target="_blank" rel="noopener noreferrer">Environmental guidance for your business sector on the NetRegs website</a></li>
+				<li><a href="/environment-and-efficiency/measuring-and-reporting-environmental-performance/assess-your-environmental-compliance.php">Use our interactive tool to assess your environmental compliance</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/environment-and-efficiency/measuring-and-reporting-environmental-performance/carry-out-an-environmental-review-of-your-business.php">Carry out an environmental review of your business</a></li>
+				<li><a href="/environment-and-efficiency/permits-and-licences/environmental-permits-and-licences-an-overview.php">Environmental permits and licences - an overview</a></li>
+				<li><a href="/environment-and-efficiency/electrical-and-electronic-equipment-manufacturing/restriction-of-the-use-of-certain-hazardous-substances-in-electrical-and-electronic-equipment-rohs.php">Restriction of the use of certain hazardous substances in electrical and electronic equipment (RoHS)</a></li>
+				<li><a href="/environment-and-efficiency/managing-hazardous-substances/hazardous-substances-and-the-environment-the-basics.php">Hazardous substances and the environment - the basics</a></li>
+				<li><a href="/environment-and-efficiency/waste-legislation-and-responsibility/duty-of-care-your-waste-responsibilities.php">Duty of care - your waste responsibilities</a></li>
+				<li><a href="/environment-and-efficiency/sources-of-environmental-advice/using-an-environmental-consultant.php">Using an environmental consultant</a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+			
+			<p>
+				Developed with:
+				<br />
+				<a href="http://webarchive.nationalarchives.gov.uk/20130822084033/http://defra.gov.uk/" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/Defra_black_and_white_sml.jpg" width="84" height="65" alt="" /> Department for Environment Food and Rural Affairs
+				</a>
+				<br />
+				<a href="https://www.gov.uk/government/organisations/environment-agency" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/environment_agency_2006.gif" width="115" height="31" alt="" /> Environment Agency
+				</a>
+			</p>
+		</div>
 		<!-- // .small-12 large-8 large-push-4 columns -->
 		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
 	</div>
