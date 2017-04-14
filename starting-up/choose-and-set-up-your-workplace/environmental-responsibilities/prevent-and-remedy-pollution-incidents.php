9b3df53b1303e3b350ee1fa79ae55f940534fd2e
Caroline Hagan <info@blueocto.co.uk>
2017-04-15 00:00:30 +0100

    Completed “starting-up > choose-and-set-up-your-workplace > environmental-responsibilities > Prevent and remedy pollution incidents” page

--- starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/prevent-and-remedy-pollution-incidents.php
+++ starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/prevent-and-remedy-pollution-incidents.php
@@ -12,99 +12,90 @@
 	
 	<div class="row">
 		<div class="small-12 large-8 large-push-4 columns">
-        <h1>Environmental responsibilities</h1>
-        <h2>Prevent and remedy pollution incidents</h2>
-        <p>Every year there are thousands of cases of damage to the environment. The <strong>Environmental Damage Regulations</strong> relate to the most serious cases, covering:</p>
-        <ul>
-            <li>damage to species and habitats</li>
-            <li>damage to water</li>
-            <li>risks to human health from contamination of land</li>
-        </ul>
-        <p>The regulations apply to both actual cases of damage and threats of imminent damage. If you are responsible - ie you are the 'operator' of the activity that causes or threatens the damage - you must take immediate action to prevent or remedy this.</p>
-        <p>See our guide on <a href="layer46a9.html?topicId=1086885096" title="Avoid causing environmental damage from pollution incidents or firefighting, and writing a pollution incident response plan">pollution incidents and environmental damage - an overview</a>.</p>
-        <p>
-            <h3>Reporting an incident</h3>
-            <p>You should call the Environment Agency Incident Hotline on Tel 0800 80 70 60 to report incidents such as:</p>
-            <ul>
-                <li>damage or danger to the natural environment</li>
-                <li>pollution to water or land</li>
-                <li>poaching or illegal fishing</li>
-                <li>fish in distress or dead fish</li>
-                <li>watercourse blocked by a vehicle or fallen tree causing risk of flooding</li>
-                <li>illegal dumping of hazardous waste or large amounts of industrial waste</li>
-                <li>incidents at Environment Agency-regulated waste sites</li>
-                <li>illegal abstraction from watercourses</li>
-                <li>unusual drops in river flow</li>
-                <li>collapsed or badly damaged river or canal banks</li>
-            </ul>
-            <p>You should report incidents such as gas leaks, burst water mains, or domestic odours, noise, waste or pest nuisances to your local authority or utility company.</p>
-        </p>
-        <ul>
-            <li><a href="detailf04a.html?itemId=1079437753&amp;type=RESOURCES" title="Who enforces environmental legislation?">Prev</a></li>
-            <li><a href="detail8573.html?itemId=1079437613&amp;type=RESOURCES" title="The business benefits of good environmental and sustainable practices">Next</a></li>
-        </ul>
-        <h3>Subjects covered in this guide</h3>
-        <ul>
-            <li><a href="layer8cd1.html?topicId=1079433025" title="Environmental responsibilities">Introduction</a></li>
-            <li><a href="detail6eb3.html?itemId=1079437508&amp;type=RESOURCES">Comply with key environmental legislation</a></li>
-            <li><a href="detail4195.html?itemId=1079437773&amp;type=RESOURCES">Does my business need a permit or licence?</a></li>
-            <li><a href="detail3813.html?itemId=1079437899&amp;type=RESOURCES">Hazardous substances covered by environmental legislation</a></li>
-            <li><a href="detailc2ce.html?itemId=1084012286&amp;type=RESOURCES">Conservation and biodiversity issues for businesses</a></li>
-            <li><a href="detailb9c6.html?itemId=1079438098&amp;type=RESOURCES">Environmental issues in specific business sectors</a></li>
-            <li><a href="detailf04a.html?itemId=1079437753&amp;type=RESOURCES">Who enforces environmental legislation?</a></li>
-            <li>Prevent and remedy pollution incidents</li>
-            <li><a href="detail8573.html?itemId=1079437613&amp;type=RESOURCES">The business benefits of good environmental and sustainable practices</a></li>
-        </ul>
-        <p><img src="../../syntegra/images/hr_325px_global.gif" alt="" /></p>
-        
-        <?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
-        
-        <h3>Environment Agency Incident Hotline</h3>
-        <p>0800 80 70 60
-            <br/>
-        </p>
-        <h3>Environment Agency Helpline</h3>
-        <p>03708 506 506
-            <br/>
-        </p>
-        <p><img src="../../syntegra/images/hr_140px_global.gif" alt="" /></p>
-        <!--Helplines-->
-        <h3>Actions</h3>
-        <ul>
-            <li><a href="http://www.defra.gov.uk/environment/quality/environmental-liability/" target="bgExternalonlinebusinesslinkgovuk" title="Introduction to environmental liability legislation and downloadable guidance">Environmental Damage (Prevention and Remediation) Regulations guidance on the Defra website</a></li>
-            <li><a href="layer3ac4-2.html?topicId=1079438165" title="These interactive tools will help you find out what you need to do to comply with environmental legislation">Use our interactive tool to assess your environmental compliance</a></li>
-        </ul>
-        </ul>
-        <img src="../../syntegra/images/px_startingabusiness.gif" alt="" width="181" height="5" />
-        <h3>Also on this site</h3>
-        <ul>
-            <li><a href="layer46a9.html?topicId=1086885096" title="Avoid causing environmental damage from pollution incidents or firefighting, and writing a pollution incident response plan">Pollution incidents and environmental damage - an overview</a></li>
-            <li><a href="layerf48f.html?topicId=1080480296" title="How to comply with the law by getting the right environmental permits and other licences">Environmental permits and licences - an overview</a></li>
-            <li><a href="layer071b.html?topicId=1080525388" title="Guidance on safely using, storing, transporting or disposing of hazardous substances">Hazardous substances and the environment - the basics</a></li>
-            <li><a href="layer8e7b.html?topicId=1080374134" title="How to comply with water pollution legislation and avoid being prosecuted for causing a pollution incident">Preventing water pollution</a></li>
-            <li><a href="layer6ca8.html?topicId=1079431917" title="An overview of the different laws you must comply with for different types of waste ">Duty of care - your waste responsibilities</a></li>
-        </ul><img src="../../syntegra/images/px_startingabusiness.gif" alt="" width="181" height="5" />
-        <a>&nbsp;</a>
-        <!-- Page footer -->
-        <!-- "Developed With" section -->
-        Developed with:
-        <img src="../../syntegra/images/footer_top2.gif" alt="" />
-        <table height="100%" width="100%">
-            <tbody>
-                <tr>
-                    <td>
-                        <a href="http://www.defra.gov.uk/index.htm" target="bgExternalwwwbusinesslinkgovuk" title="Department for Environment Food and Rural Affairs - Opens in a new window"><img src="../../Regulation_Update_images/Defra_black_and_white_sml.jpg" width="84" height="65" alt="" />
-                            <br/>Department for Environment Food and Rural Affairs</a>
-                    </td>
-                    <td>
-                        <a href="http://www.environment-agency.gov.uk/" target="bgExternalwwwbusinesslinkgovuk" title="Environment Agency - Opens in a new window"><img src="../../Health_and_Environment_images/environment_agency_2006.gif" width="115" height="31" alt="" />
-                            <br/>Environment Agency</a>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+			
+			<h1>Environmental responsibilities</h1>
+			<h2>Prevent and remedy pollution incidents</h2>
+			<p>Every year there are thousands of cases of damage to the environment. The <strong>Environmental Damage Regulations</strong> relate to the most serious cases, covering:</p>
+			<ul>
+				<li>damage to species and habitats</li>
+				<li>damage to water</li>
+				<li>risks to human health from contamination of land</li>
+			</ul>
+			<p>The regulations apply to both actual cases of damage and threats of imminent damage. If you are responsible - ie you are the 'operator' of the activity that causes or threatens the damage - you must take immediate action to prevent or remedy this.</p>
+			<p>See our guide on <a href="/environment-and-efficiency/preventing-and-managing-pollution/pollution-incidents-and-environmental-damage-an-overview.php">pollution incidents and environmental damage - an overview</a>.</p>
+			<h3>Reporting an incident</h3>
+			<p>You should call the Environment Agency Incident Hotline on Tel 0800 80 70 60 to report incidents such as:</p>
+			<ul>
+				<li>damage or danger to the natural environment</li>
+				<li>pollution to water or land</li>
+				<li>poaching or illegal fishing</li>
+				<li>fish in distress or dead fish</li>
+				<li>watercourse blocked by a vehicle or fallen tree causing risk of flooding</li>
+				<li>illegal dumping of hazardous waste or large amounts of industrial waste</li>
+				<li>incidents at Environment Agency-regulated waste sites</li>
+				<li>illegal abstraction from watercourses</li>
+				<li>unusual drops in river flow</li>
+				<li>collapsed or badly damaged river or canal banks</li>
+			</ul>
+			<p>You should report incidents such as gas leaks, burst water mains, or domestic odours, noise, waste or pest nuisances to your local authority or utility company.</p>
+			
+			<ul>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/who-enforces-environmental-legislation.php" title="Who enforces environmental legislation?">Prev</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/the-business-benefits-of-good-environmental-and-sustainable-practices.php" title="The business benefits of good environmental and sustainable practices">Next</a></li>
+			</ul>
 
-</div>
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities.php" title="Environmental responsibilities">Introduction</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/comply-with-key-environmental-legislation.php">Comply with key environmental legislation</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/does-my-business-need-a-permit-or-licence.php">Does my business need a permit or licence?</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/hazardous-substances-covered-by-environmental-legislation.php">Hazardous substances covered by environmental legislation</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/conservation-and-biodiversity-issues-for-businesses.php">Conservation and biodiversity issues for businesses</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/environmental-issues-in-specific-business-sectors.php">Environmental issues in specific business sectors</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/who-enforces-environmental-legislation.php">Who enforces environmental legislation?</a></li>
+				<li>Prevent and remedy pollution incidents</li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/the-business-benefits-of-good-environmental-and-sustainable-practices.php">The business benefits of good environmental and sustainable practices</a></li>
+			</ul>
+
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Environment Agency Incident Hotline</h3>
+			<p>0800 80 70 60</p>
+			
+			<h3>Environment Agency Helpline</h3>
+			<p>03708 506 506</p>
+			
+			<hr />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20130822084033/http://www.defra.gov.uk/environment/quality/environmental-liability/" target="bgExternalonlinebusinesslinkgovuk" target="_blank" rel="noopener noreferrer">Environmental Damage (Prevention and Remediation) Regulations guidance on the Defra website</a></li>
+				<li><a href="/environment-and-efficiency/measuring-and-reporting-environmental-performance/assess-your-environmental-compliance.php">Use our interactive tool to assess your environmental compliance</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/environment-and-efficiency/preventing-and-managing-pollution/pollution-incidents-and-environmental-damage-an-overview.php">Pollution incidents and environmental damage - an overview</a></li>
+				<li><a href="/environment-and-efficiency/permits-and-licences/environmental-permits-and-licences-an-overview.php">Environmental permits and licences - an overview</a></li>
+				<li><a href="/environment-and-efficiency/managing-hazardous-substances/hazardous-substances-and-the-environment-the-basics.php">Hazardous substances and the environment - the basics</a></li>
+				<li><a href="/environment-and-efficiency/preventing-and-managing-pollution/preventing-water-pollution.php" title="How to comply with water pollution legislation and avoid being prosecuted for causing a pollution incident">Preventing water pollution</a></li>
+				<li><a href="/environment-and-efficiency/waste-legislation-and-responsibility/duty-of-care-your-waste-responsibilities.php">Duty of care - your waste responsibilities</a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+			
+			<p>
+				Developed with:
+				<br />
+				<a href="http://webarchive.nationalarchives.gov.uk/20130822084033/http://defra.gov.uk/" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/Defra_black_and_white_sml.jpg" width="84" height="65" alt="" /> Department for Environment Food and Rural Affairs
+				</a>
+				<br />
+				<a href="https://www.gov.uk/government/organisations/environment-agency" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/environment_agency_2006.gif" width="115" height="31" alt="" /> Environment Agency
+				</a>
+			</p>
+		</div>
 		<!-- // .small-12 large-8 large-push-4 columns -->
 		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
 	</div>
