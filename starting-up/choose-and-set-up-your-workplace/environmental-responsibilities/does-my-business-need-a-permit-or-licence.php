b0906227c10c6af85d23893d9c906acdae763b5e
Caroline Hagan <info@blueocto.co.uk>
2017-04-14 23:23:28 +0100

    Completed “starting-up > choose-and-set-up-your-workplace > environmental-responsibilities > Does my business need a permit or licence?” page

--- starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/does-my-business-need-a-permit-or-licence.php
+++ starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/does-my-business-need-a-permit-or-licence.php
@@ -12,81 +12,76 @@
 	
 	<div class="row">
 		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Environmental responsibilities</h1>
+			<h2>Does my business need a permit or licence?</h2>
+			<p>You may require a permit or licence if your business poses certain types of environmental risk. For example if your business:</p>
+			<ul>
+				<li><strong>collects or transports waste</strong> on behalf of others, or transports your own construction or demolition waste, you must register as a waste carrier with your environmental regulator - this is the Environment Agency in England and Wales, the Northern Ireland Environment Agency (NIEA) or the Scottish Environment Protection Agency (SEPA)</li>
+				<li>carries out <strong>waste activities</strong> such as treating, storing or disposing of other people's waste, you may need an environmental permit or an exemption from your environmental regulator</li>
+				<li>carries out <strong>industrial, waste or intensive agricultural activities</strong>, or uses hazardous substances such as solvents, you may need an environmental permit or a pollution prevention and control permit from your local authority or environmental regulator</li>
+				<li><strong>allows anything other than</strong> clean, uncontaminated <strong>water</strong> from your premises into watercourses or surface water drains, you must have a discharge consent from your environmental regulator</li>
+				<li>discharges <strong>trade effluent</strong> into the sewerage system, you must get consent from your water company or water authority</li>
+				<li>takes water from surface water or groundwater, ie not from the mains supply, you may need an abstraction licence from your environmental regulator</li>
+			</ul>
 
-        <h1>Environmental responsibilities</h1>
-        <h2>Does my business need a permit or licence?</h2>
-        <p>You may require a permit or licence if your business poses certain types of environmental risk. For example if your business:</p>
-        <ul>
-            <li><strong>collects or transports waste</strong> on behalf of others, or transports your own construction or demolition waste, you must register as a waste carrier with your environmental regulator - this is the Environment Agency in England and Wales,&nbsp;the Northern Ireland Environment Agency (NIEA)&nbsp;or&nbsp;the Scottish Environment Protection Agency (SEPA)</li>
-            <li>carries out <strong>waste activities</strong> such as treating, storing or disposing of other people's waste, you may need an&nbsp;environmental permit or an exemption from your environmental regulator</li>
-            <li>carries out <strong>industrial, waste or intensive agricultural activities</strong>, or uses hazardous substances such as solvents, you may need an environmental permit or a pollution prevention and control permit from your local authority or environmental regulator</li>
-            <li><strong>allows anything other than</strong> clean, uncontaminated <strong>water</strong> from your premises into watercourses or surface water drains, you must have a discharge consent from your environmental regulator</li>
-            <li>discharges <strong>trade effluent</strong> into the sewerage system, you must get consent from your water company or water authority</li>
-            <li>takes water from surface water or groundwater, ie not from the mains supply, you may need an abstraction licence from your environmental regulator</li>
-        </ul>
-        <p><strong>This list is not comprehensive</strong>. For more information on registering as a waste carrier, see our guide on <a href="layerf48f.html?topicId=1080480296" title="How to comply with the law by getting the right environmental permits and other licences">environmental permits and licences - an overview</a>.&nbsp;You can also <a href="layer3ac4-2.html?topicId=1079438165" title="These interactive tools will help you find out what you need to do to comply with environmental legislation">use our interactive tool to assess your environmental compliance</a>.&nbsp;</p>
-        <p>You can contact the Environment Agency Helpline on Tel 03708 506 506.</p>
-        <p>Businesses in Northern Ireland should contact the NIEA Helpline on Tel 0845 302 0008.</p>
-        <p>Businesses in Scotland should contact SEPA on Tel 01786 457 700.</p>
-        <ul>
-            <li><a href="detail6eb3.html?itemId=1079437508&amp;type=RESOURCES" title="Comply with key environmental legislation">Prev</a></li>
-            <li><a href="detail3813.html?itemId=1079437899&amp;type=RESOURCES" title="Hazardous substances covered by environmental legislation">Next</a></li>
-        </ul>
-        <h3>Subjects covered in this guide</h3>
-        <ul>
-            <li><a href="layer8cd1.html?topicId=1079433025" title="Environmental responsibilities">Introduction</a></li>
-            <li><a href="detail6eb3.html?itemId=1079437508&amp;type=RESOURCES">Comply with key environmental legislation</a></li>
-            <li>Does my business need a permit or licence?</li>
-            <li><a href="detail3813.html?itemId=1079437899&amp;type=RESOURCES">Hazardous substances covered by environmental legislation</a></li>
-            <li><a href="detailc2ce.html?itemId=1084012286&amp;type=RESOURCES">Conservation and biodiversity issues for businesses</a></li>
-            <li><a href="detailb9c6.html?itemId=1079438098&amp;type=RESOURCES">Environmental issues in specific business sectors</a></li>
-            <li><a href="detailf04a.html?itemId=1079437753&amp;type=RESOURCES">Who enforces environmental legislation?</a></li>
-            <li><a href="detail8b34.html?itemId=1084010499&amp;type=RESOURCES">Prevent and remedy pollution incidents</a></li>
-            <li><a href="detail8573.html?itemId=1079437613&amp;type=RESOURCES">The business benefits of good environmental and sustainable practices</a></li>
-        </ul>
-        <p><img src="../../syntegra/images/hr_325px_global.gif" alt="" /></p>
-        
-        <?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
-        
-        <h3>Environment Agency Helpline</h3>
-        <p>03708 506 506
-            <br/>
-        </p>
-        <p><img src="../../syntegra/images/hr_140px_global.gif" alt="" /></p>
-        <!--Helplines-->
-        <h3>Actions</h3>
-        <ul>
-            <li><a href="layer3ac4-2.html?topicId=1079438165" title="These interactive tools will help you find out what you need to do to comply with environmental legislation">Use our interactive tool to assess your environmental compliance</a></li>
-        </ul>
-        </ul>
-        <img src="../../syntegra/images/px_startingabusiness.gif" alt="" width="181" height="5" />
-        <h3>Also on this site</h3>
-        <ul>
-            <li><a href="layerf48f.html?topicId=1080480296" title="How to comply with the law by getting the right environmental permits and other licences">Environmental permits and licences - an overview</a></li>
-            <li><a href="layer071b.html?topicId=1080525388" title="Guidance on safely using, storing, transporting or disposing of hazardous substances">Hazardous substances and the environment - the basics</a></li>
-            <li><a href="layer6ca8.html?topicId=1079431917" title="An overview of the different laws you must comply with for different types of waste ">Duty of care - your waste responsibilities</a></li>
-            <li><a href="layer2570.html?topicId=1079416602" title="How a more environmentally friendly approach can protect your business, save money and improve its reputation">Benefits of improving your environmental performance</a></li>
-        </ul><img src="../../syntegra/images/px_startingabusiness.gif" alt="" width="181" height="5" />
-        <a>&nbsp;</a>
-        <!-- Page footer -->
-        <!-- "Developed With" section -->
-        Developed with:
-        <img src="../../syntegra/images/footer_top2.gif" alt="" />
-        <table height="100%" width="100%">
-            <tbody>
-                <tr>
-                    <td>
-                        <a href="http://www.defra.gov.uk/index.htm" target="bgExternalwwwbusinesslinkgovuk" title="Department for Environment Food and Rural Affairs - Opens in a new window"><img src="../../Regulation_Update_images/Defra_black_and_white_sml.jpg" width="84" height="65" alt="" />
-                            <br/>Department for Environment Food and Rural Affairs</a>
-                    </td>
-                    <td>
-                        <a href="http://www.environment-agency.gov.uk/" target="bgExternalwwwbusinesslinkgovuk" title="Environment Agency - Opens in a new window"><img src="../../Health_and_Environment_images/environment_agency_2006.gif" width="115" height="31" alt="" />
-                            <br/>Environment Agency</a>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-        
+			<p><strong>This list is not comprehensive</strong>. For more information on registering as a waste carrier, see our guide on <a href="/environment-and-efficiency/permits-and-licences/environmental-permits-and-licences-an-overview.php">environmental permits and licences - an overview</a>. You can also <a href="/environment-and-efficiency/measuring-and-reporting-environmental-performance/assess-your-environmental-compliance.php">use our interactive tool to assess your environmental compliance</a>. </p>
+			<p>You can contact the Environment Agency Helpline on Tel 03708 506 506.</p>
+			<p>Businesses in Northern Ireland should contact the NIEA Helpline on Tel 0845 302 0008.</p>
+			<p>Businesses in Scotland should contact SEPA on Tel 01786 457 700.</p>
+			
+			<ul>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/comply-with-key-environmental-legislation.php" title="Comply with key environmental legislation">Prev</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/hazardous-substances-covered-by-environmental-legislation.php" title="Hazardous substances covered by environmental legislation">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities.php" title="Environmental responsibilities">Introduction</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/comply-with-key-environmental-legislation.php">Comply with key environmental legislation</a></li>
+				<li>Does my business need a permit or licence?</li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/hazardous-substances-covered-by-environmental-legislation.php">Hazardous substances covered by environmental legislation</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/conservation-and-biodiversity-issues-for-businesses.php">Conservation and biodiversity issues for businesses</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/environmental-issues-in-specific-business-sectors.php">Environmental issues in specific business sectors</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/who-enforces-environmental-legislation.php">Who enforces environmental legislation?</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/prevent-and-remedy-pollution-incidents.php">Prevent and remedy pollution incidents</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/environmental-responsibilities/the-business-benefits-of-good-environmental-and-sustainable-practices.php">The business benefits of good environmental and sustainable practices</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Environment Agency Helpline</h3>
+			<p>03708 506 506</p>
+			
+			<p><img src="../../../_img/hr_140px_global.gif" alt="" /></p>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="/environment-and-efficiency/measuring-and-reporting-environmental-performance/assess-your-environmental-compliance.php">Use our interactive tool to assess your environmental compliance</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/environment-and-efficiency/permits-and-licences/environmental-permits-and-licences-an-overview.php">Environmental permits and licences - an overview</a></li>
+				<li><a href="/environment-and-efficiency/managing-hazardous-substances/hazardous-substances-and-the-environment-the-basics.php">Hazardous substances and the environment - the basics</a></li>
+				<li><a href="/environment-and-efficiency/waste-legislation-and-responsibility/duty-of-care-your-waste-responsibilities.php">Duty of care - your waste responsibilities</a></li>
+				<li><a href="/environment-and-efficiency/the-business-case-for-environmental-improvements/benefits-of-improving-your-environmental-performance.php">Benefits of improving your environmental performance</a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+			
+			<p>
+				Developed with:
+				<br />
+				<a href="http://webarchive.nationalarchives.gov.uk/20130822084033/http://defra.gov.uk/" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/Defra_black_and_white_sml.jpg" width="84" height="65" alt="" /> Department for Environment Food and Rural Affairs
+				</a>
+				<br />
+				<a href="https://www.gov.uk/government/organisations/environment-agency" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/environment_agency_2006.gif" width="115" height="31" alt="" /> Environment Agency
+				</a>
+			</p>
+			
 		</div>
 		<!-- // .small-12 large-8 large-push-4 columns -->
 		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
