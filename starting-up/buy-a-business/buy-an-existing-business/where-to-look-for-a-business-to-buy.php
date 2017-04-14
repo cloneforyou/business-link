a0ca1fe1d91393833d809cff0776767340d7cbfc
Caroline Hagan <info@blueocto.co.uk>
2017-04-14 21:43:24 +0100

    Completed “starting-up > buy-a-business > buy-an-existing-business > Where to look for a business to buy” page.

---
+++ starting-up/buy-a-business/buy-an-existing-business/where-to-look-for-a-business-to-buy.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Where to look for a business to buy | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Buy an existing business</h1>
+			<h2>Where to look for a business to buy</h2>
+			<p>Many national and local <strong>newspapers</strong> carry adverts for businesses and business premises for sale.</p>
+			<p>Depending on what sector you're interested in, you could look in <strong>trade journals</strong>, eg Retail Newsagent, British Baker, The Publican and The Grocer. <a href="http://www.taforum.org/Members" target="_blank" rel="noopener noreferrer" title="Search for a trade association by name, activity or initial letter">Find your trade association on the Trade Association website</a>.</p>
+			<p>You could put in your own <strong>advertisement</strong>, saying what you are looking for. You can get contact details for most newspapers, magazines and trade journals from press directories available at your local library.</p>
+			<p>Some <strong>magazines</strong> - many of them with their own websites - specialise in buying and selling, like Exchange and Mart, Loot and Daltons Business. These sources tend to be specific to certain kinds of businesses.</p>
+			<p>Business brokers, transfer agents and corporate financiers all maintain lists of businesses for sale.</p>
+			<p>Prepare a CV and info sheet about yourself and what you are looking for, your skillset and your ability to fund. Once prepared, circulate this to agents so that they can assist in matching a business with your requirements. This also shows the agent and their client that you are efficient, organised and serious.</p>
+			<p>On the internet you will find several listings websites where you can search for businesses or place 'business wants' adverts - sometimes for free. You can search for businesses for sale on websites such as Daltons Business and Businesses for Sale. <a href="http://www.businessesforsale.com/" target="_blank" rel="noopener noreferrer">Find businesses for sale on the BusinessesForSale website</a>. <a href="https://www.daltonsbusiness.com/buy/business-for-sale-listing" target="_blank" rel="noopener noreferrer">Find businesses sale listings on the Daltons Business website</a>.</p>
+			<p>Other websites offer databases of <strong>business transfer agents and business brokers</strong>. These act in a similar way to estate agents - they are experts in valuing, marketing and selling businesses. They can help you find the right business and can also put you in touch with possible sources of finance.</p>
+			<p>When looking for a transfer agent ensure they are members of a trade organisation like the International Business Brokers Association (IBBA) or Institute of Commercial &amp; Business Agents (ICBA). Members of such organisations are subject to a code of conduct and membership demonstrates that they possess the necessary professional skills, knowledge and experience. <a href="https://www.ibba.org/" target="_blank" rel="noopener noreferrer">Find a member on the IBBA website</a>. <a href="http://www.naea.co.uk/commercial/" target="_blank" rel="noopener noreferrer">Find a member on the NAEA website</a>.</p>
+			<p>If the business you want isn't available, do some research and don't be afraid to make an approach to a business owner. However, an unsolicited approach may take the vendor off guard. You should also be respectful of the vendor's need to safeguard confidentiality. A business broker, transfer agent or corporate financier will be able to help you find a business that may not be obviously for sale.</p>
+			<p>Finally, don't forget <strong>word of mouth</strong>. Ask around among trade contacts, business associates, and at exhibitions and conferences.</p>
+			
+			<ul>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/deciding-on-the-right-type-of-business-to-buy.php" title="Deciding on the right type of business to buy">Prev</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/how-to-value-a-business.php" title="How to value a business">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/buy-or-sell-a-business/buying/buy-an-existing-business.php" title="Buy an existing business">Introduction</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/advantages-and-disadvantages-of-buying-an-existing-business.php">Advantages and disadvantages of buying an existing business</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/deciding-on-the-right-type-of-business-to-buy.php">Deciding on the right type of business to buy</a></li>
+				<li>Where to look for a business to buy</li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/how-to-value-a-business.php">How to value a business</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/make-sure-a-business-is-worth-buying-due-diligence.php">Make sure a business is worth buying: due diligence</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/step-by-step-how-to-buy-a-business.php">Step-by-step: how to buy a business</a></li>
+				<li><a href="/starting-up/buy-a-business/buy-an-existing-business/looking-after-existing-employees.php">Looking after existing employees</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://www.businessesforsale.com/" target="_blank" rel="noopener noreferrer" title="Searchable database of businesses for sale">Businesses for sale on the BusinessesForSale website</a></li>
+				<li><a href="http://www.daltonsbusiness.com/buy/business-for-sale-listing" target="_blank" rel="noopener noreferrer" title="Lists of businesses and franchises currently available for sale in the UK and abroad">Businesses sale listings on the Daltons Business website </a></li>
+				<li><a href="http://www.lucasweston.com/buy-a-business-advice.htm" target="_blank" rel="noopener noreferrer" title="Information on buying a business through a business broker">Advice for buying a business through a business broker on the Lucas &amp; Weston website</a></li>
+				<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance options for your business</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
