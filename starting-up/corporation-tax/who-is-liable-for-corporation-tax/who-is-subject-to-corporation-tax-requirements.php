b5bddd935713fa19c1311bc1c5a80d8c41400583
Caroline Hagan <info@blueocto.co.uk>
2017-05-13 15:28:17 +0100

    Completed “starting-up > corporation-tax > who-is-liable-for-corporation-tax > Who is subject to Corporation Tax requirements?” page.

---
+++ starting-up/corporation-tax/who-is-liable-for-corporation-tax/who-is-subject-to-corporation-tax-requirements.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Who is subject to Corporation Tax requirements? | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Who is liable for Corporation Tax</h1>
+		<h2>Who is subject to Corporation Tax requirements?</h2>
+		<p>The following limited companies and unincorporated organisations are subject to Corporation Tax requirements:</p>
+		<ul>
+			<li>limited companies incorporated in the UK</li>
+			<li>foreign-based companies with a permanent place of business in the UK</li>
+			<li>members' clubs, such as social clubs, sports clubs and holiday clubs</li>
+			<li>societies, such as friendly societies and provident societies</li>
+			<li>associations, such as housing associations and trade associations</li>
+			<li>co-operatives</li>
+			<li>other unincorporated associations</li>
+			<li>groups of individuals carrying on a business that is not a partnership</li>
+			<li>charities, or companies that are subsidiaries of - or wholly owned by - a charity</li>
+			<li>NHS foundation trusts if they are carrying out significant commercial activities that are not part of core health care delivery - such as running a commercial laundry</li>
+		</ul>
+		<p>The taxable profits or surpluses of these businesses and organisations are subject to Corporation Tax requirements.</p>
+		<p>A company or organisation subject to Corporation Tax requirements is known to HM Revenue &amp; Customs for Corporation Tax purposes as being 'within the charge to Corporation Tax', 'chargeable to tax' or in 'the charge to tax'.</p>
+		<p><a href="/tax-payroll-and-company-information/corporation-tax/deadlines-and-requirements-for-corporation-tax.php" title="When to tell HM Revenue &amp; Customs a company or organisation is liable, active or dormant, pay Corporation Tax and file a Company Tax Return ">Deadlines and requirements for Corporation Tax</a></p>
+
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/corporation-tax/corporation-tax-for-new-companies-and-organisations/who-is-liable-for-corporation-tax.php" title="Who is liable for Corporation Tax">Prev</a></li>
+			<li><a href="/starting-up/corporation-tax/who-is-liable-for-corporation-tax/who-is-not-subject-to-corporation-tax-requirements.php" title="Who is not subject to Corporation Tax requirements?">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/corporation-tax/corporation-tax-for-new-companies-and-organisations/who-is-liable-for-corporation-tax.php" title="Who is liable for Corporation Tax">Introduction</a></li>
+			<li>Who is subject to Corporation Tax requirements?</li>
+			<li><a href="/starting-up/corporation-tax/who-is-liable-for-corporation-tax/who-is-not-subject-to-corporation-tax-requirements.php">Who is not subject to Corporation Tax requirements?</a></li>
+			<li><a href="/starting-up/corporation-tax/who-is-liable-for-corporation-tax/what-activities-are-exempt-from-corporation-tax.php">What activities are exempt from Corporation Tax?</a></li>
+			<li><a href="/starting-up/corporation-tax/who-is-liable-for-corporation-tax/what-you-must-do-if-your-company-or-organisation-is-liable-for-corporation-tax.php">What you must do if your company or organisation is liable for Corporation Tax</a></li>
+		</ul>
+
+	   <?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" title="HM Revenue &amp; Customs" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" />HM Revenue &amp; Customs
+			</a>
+		</p>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/corporation-tax/corporation-tax-for-new-companies-and-organisations/corporation-tax-glossary.php">Corporation Tax glossary</a></li>
+			<li><a href="/tax-payroll-and-company-information/corporation-tax/introduction-to-corporation-tax.php">Introduction to Corporation Tax</a></li>
+			<li><a href="/tax-payroll-and-company-information/corporation-tax/corporation-tax-for-new-companies-and-organisations/starting-a-company-or-organisation-and-corporation-tax.php">Starting a company or organisation and Corporation Tax</a></li>
+			<li><a href="/tax-payroll-and-company-information/corporation-tax/trading-and-non-trading-for-corporation-tax-explained.php">Trading and non-trading for Corporation Tax explained</a></li>
+			<li><a href="https://www.gov.uk/browse/tax" target="_blank" rel="noopener noreferrer">Get started with Corporation Tax</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
