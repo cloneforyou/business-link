64dfb342601052d7a9e035b10bdb63ea6d4cb1c1
Caroline Hagan <info@blueocto.co.uk>
2017-05-13 15:54:20 +0100

    Completed “starting-up > corporation-tax > who-is-liable-for-corporation-tax > What you must do if your company or organisation is liable for Corporation Tax” page.

---
+++ starting-up/corporation-tax/who-is-liable-for-corporation-tax/what-you-must-do-if-your-company-or-organisation-is-liable-for-corporation-tax.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>What you must do if your company or organisation is liable for Corporation Tax | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Who is liable for Corporation Tax</h1>
+		<h2>What you must do if your company or organisation is liable for Corporation Tax</h2>
+		<p>If your company or organisation is liable for Corporation Tax, it is subject to Corporation Tax deadlines and requirements. This means you have to do more than just pay the correct amount of Corporation Tax each year.</p>
+		<p>You must also tell HM Revenue &amp; Customs (HMRC) you're liable for Corporation Tax and file a Company Tax Return on time.</p>
+		<p>There are different requirements depending on whether or not your company or organisation is active - eg carrying on business, trading or receiving income - or dormant - eg not active, not carrying on business or not trading.</p>
+		
+		<h3>What active companies or organisations must do</h3>
+		<p>If your company or organisation is liable for Corporation Tax, and is active - eg trading or receiving income - you must:</p>
+		<ul>
+			<li>tell HMRC that they are active</li>
+			<li>pay Corporation Tax on any profits that are taxable within nine months and one day&nbsp;of the end of your Corporation Tax accounting period - usually the end of your financial year</li>
+			<li>file a Company Tax Return within 12 months of the end of your company or organisation's Corporation Tax 'accounting period' - usually the end of your financial year</li>
+		</ul>
+		<p>If your company or organisation is liable for Corporation Tax, but is dormant or is not active, you must still submit a Company Tax Return within 12 months of the end of your Corporation Tax accounting period (usually the end of your company or organisation's financial year) if HMRC sends your company or organisation a 'Notice to deliver a Company Tax Return'. You don't need to file a return if HMRC does not send you this notice.</p>
+		
+		<p>Find out how to tell HMRC your company or organisation is active in our guide on <a href="/tax-payroll-and-company-information/corporation-tax/corporation-tax-for-new-companies-and-organisations/starting-a-company-or-organisation-and-corporation-tax.php">starting a company or organisation and Corporation Tax</a>.</p>
+		
+		<p><a href="/tax-payroll-and-company-information/corporation-tax/trading-and-non-trading-for-corporation-tax-explained.php">Trading and non-trading for Corporation Tax explained</a></p>
+		
+		<p><a href="/tax-payroll-and-company-information/corporation-tax/deadlines-and-requirements-for-corporation-tax.php">Deadlines and requirements for Corporation Tax</a></p>
+		
+		<h3>Submitting your Company Tax Return and how to pay</h3>
+		<p>Virtually all companies and organisations must submit their Company Tax Returns <strong>online</strong>. You must also pay any Corporation Tax that's due <strong>electronically</strong>.</p>
+		<p><strong>Additionally</strong> your tax computations and, with very few exceptions, the accounts that form part of your Company Tax Return, must be submitted in Inline eXtensible Business Reporting Language (iXBRL) format.</p>
+		<p>
+			<a href="/tax-payroll-and-company-information/corporation-tax/file-returns-and-manage-your-corporation-tax-account/switching-from-paper-to-online-company-tax-returns-and-electronic-payment.php">Switching from paper to online Company Tax Returns and electronic payment</a>
+		</p>
+		<p>
+			<a href="/tax-payroll-and-company-information/corporation-tax/file-returns-and-manage-your-corporation-tax-account/corporation-tax-online-setting-up-and-managing-your-online-account.php">Corporation Tax Online: setting up and managing your online account</a>
+		</p>
+		<p>
+			<a href="/tax-payroll-and-company-information/paying-hmrc/how-to-pay-corporation-tax.php">How to pay Corporation Tax</a>
+		</p>
+		<p>
+			<a href="https://www.gov.uk/file-your-company-accounts-and-tax-return" target="_blank" rel="noopener noreferrer">
+				<s>See a demonstration of filing returns and managing your Corporation Tax account online on the HMRC website</s><br />
+				File your accounts and Company Tax Return
+			</a>
+		</p>
+		
+		<ul>
+			<li><a href="/starting-up/corporation-tax/who-is-liable-for-corporation-tax/what-activities-are-exempt-from-corporation-tax.php" title="What activities are exempt from Corporation Tax?">Prev</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/corporation-tax/corporation-tax-for-new-companies-and-organisations/who-is-liable-for-corporation-tax.php" title="Who is liable for Corporation Tax">Introduction</a></li>
+			<li><a href="/starting-up/corporation-tax/who-is-liable-for-corporation-tax/who-is-subject-to-corporation-tax-requirements.php">Who is subject to Corporation Tax requirements?</a></li>
+			<li><a href="/starting-up/corporation-tax/who-is-liable-for-corporation-tax/who-is-not-subject-to-corporation-tax-requirements.php">Who is not subject to Corporation Tax requirements?</a></li>
+			<li><a href="/starting-up/corporation-tax/who-is-liable-for-corporation-tax/what-activities-are-exempt-from-corporation-tax.php">What activities are exempt from Corporation Tax?</a></li>
+			<li>What you must do if your company or organisation is liable for Corporation Tax</li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" title="HM Revenue &amp; Customs" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" />HM Revenue &amp; Customs
+			</a>
+		</p>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/corporation-tax/trading-and-non-trading-for-corporation-tax-explained.php">Trading and non-trading for Corporation Tax explained</a></li>
+			<li><a href="/tax-payroll-and-company-information/corporation-tax/deadlines-and-requirements-for-corporation-tax.php">Deadlines and requirements for Corporation Tax</a></li>
+			<li><a href="/tax-payroll-and-company-information/corporation-tax/corporation-tax-for-new-companies-and-organisations/corporation-tax-glossary.php">Corporation Tax glossary</a></li>
+			<li><a href="/tax-payroll-and-company-information/corporation-tax/introduction-to-corporation-tax.php">Introduction to Corporation Tax</a></li>
+			<li><a href="/tax-payroll-and-company-information/corporation-tax/corporation-tax-for-new-companies-and-organisations/starting-a-company-or-organisation-and-corporation-tax.php">Starting a company or organisation and Corporation Tax </a></li>
+			<li><a href="https://www.gov.uk/browse/tax" target="_blank" rel="noopener noreferrer">Get started with Corporation Tax</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
