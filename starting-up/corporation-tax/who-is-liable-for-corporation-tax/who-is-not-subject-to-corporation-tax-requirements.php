06a29833d1f3e526a43bd8ad9aa8e863bf727edb
Caroline Hagan <info@blueocto.co.uk>
2017-05-13 15:33:38 +0100

    Completed “starting-up > corporation-tax > who-is-liable-for-corporation-tax > Who is not subject to Corporation Tax requirements?” page.

---
+++ starting-up/corporation-tax/who-is-liable-for-corporation-tax/who-is-not-subject-to-corporation-tax-requirements.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Who is not subject to Corporation Tax requirements? | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Who is liable for Corporation Tax</h1>
+		<h2>Who is not subject to Corporation Tax requirements?</h2>
+		<p>Businesses and organisations that are not subject to Corporation Tax requirements include:</p>
+		<ul>
+			<li>sole traders - one-person businesses that are not operating through a limited company</li>
+			<li>traditional partnerships</li>
+			<li>limited liability partnerships</li>
+			<li>local authorities</li>
+			<li>local authority associations</li>
+			<li>investment clubs</li>
+			<li>allotment and garden societies</li>
+			<li>health service bodies</li>
+			<li>London Organising Committee of the Olympic Games Ltd and the International Olympic Committee</li>
+		</ul>
+		<p>If your business or organisation is not subject to Corporation Tax, you don't need to meet Corporation Tax deadlines and requirements.</p>
+		<p>This doesn't mean your business is exempt from all taxes as you may have to complete a Self Assessment return.</p>
+		<p><a href="/tax-payroll-and-company-information/self-assessment/tax-returns-if-you-are-self-employed-or-in-a-partnership.php">Tax returns if you're self-employed or in a partnership</a></p>
+		
+		<ul>
+			<li><a href="/starting-up/corporation-tax/who-is-liable-for-corporation-tax/who-is-subject-to-corporation-tax-requirements.php" title="Who is subject to Corporation Tax requirements?">Prev</a></li>
+			<li><a href="/starting-up/corporation-tax/who-is-liable-for-corporation-tax/what-activities-are-exempt-from-corporation-tax.php" title="What activities are exempt from Corporation Tax?">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/corporation-tax/corporation-tax-for-new-companies-and-organisations/who-is-liable-for-corporation-tax.php" title="Who is liable for Corporation Tax">Introduction</a></li>
+			<li><a href="/starting-up/corporation-tax/who-is-liable-for-corporation-tax/who-is-subject-to-corporation-tax-requirements.php">Who is subject to Corporation Tax requirements?</a></li>
+			<li>Who is not subject to Corporation Tax requirements?</li>
+			<li><a href="/starting-up/corporation-tax/who-is-liable-for-corporation-tax/what-activities-are-exempt-from-corporation-tax.php">What activities are exempt from Corporation Tax?</a></li>
+			<li><a href="/starting-up/corporation-tax/who-is-liable-for-corporation-tax/what-you-must-do-if-your-company-or-organisation-is-liable-for-corporation-tax.php">What you must do if your company or organisation is liable for Corporation Tax</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+	   
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" title="HM Revenue &amp; Customs" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" />HM Revenue &amp; Customs
+			</a>
+		</p>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/browse/tax" title="What is Corporation Tax, who does it apply to, how to manage it online and other tax responsiblities if you're a company director">Get started with Corporation Tax</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
