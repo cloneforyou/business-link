628ebc986699f6d9fbeb4e9420ee6195af962bbd
Caroline Hagan <info@blueocto.co.uk>
2017-05-13 16:58:26 +0100

    Completed “finance-and-grants > financial-planning-and-accounts > Financial and management accounts: the basics” page.

--- _inc/nav--finance-and-grants.php
+++ _inc/nav--finance-and-grants.php
@@ -4,5 +4,22 @@
 		<li>
 			<a href="/finance-and-grants.php" title="Finance and grants">Introduction</a>
 		</li>
+		<li>
+			<a href="/finance-and-grants/financial-planning-and-accounts.php">Financial planning and accounts</a>
+			<ul>
+				<li>
+					<a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php">Financial and management accounts: the basics</a>
+					<ul>
+						<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php" title="Financial and management accounts: the basics">Introduction</a></li>
+						<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/financial-accounts.php">Financial accounts</a></li>
+						<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/filing-financial-accounts.php">Filing financial accounts</a></li>
+						<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/management-accounts.php">Management accounts</a></li>
+						<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/uses-of-management-accounting.php">Uses of management accounting</a></li>
+						<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/the-importance-of-maintaining-accurate-accounts.php">The importance of maintaining accurate accounts</a></li>
+						<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/false-accounting.php">False accounting</a></li>
+					</ul>
+				</li>
+			</ul>
+		</li>
 	</ul>
 </li>
---
+++ finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Financial and management accounts: the basics | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Financial and management accounts: the basics</h1>
+		<h2>Introduction</h2>
+		<img src="../../_img/SU0320.gif" width="94" height="62" alt="" />
+		<p>The accounting needs of your business will vary according to its size, type and sector. As the business owner, it's your responsibility to make sure your business keeps accurate records and accounts.</p>
+		<p>There are two types of accounting information - financial accounts and management accounts.</p>
+		<p>Financial accounts describe the performance of your business and have to be filed at Companies House. Management accounts are aimed at helping you to plan your business and make decisions about key areas such as sales, margins and stock.</p>
+		<p>This guide explains the basics of both types of accounts and what they should include. It outlines your financial accounting obligations and details how management accounting can help you run your business more effectively.</p>
+		
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/financial-accounts.php" title="Financial accounts">Next</a></li>
+		</ul>
+		<h3>Subjects covered in this guide</h3>
+		
+		<ul>
+			<li>Introduction</li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/financial-accounts.php">Financial accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/filing-financial-accounts.php">Filing financial accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/management-accounts.php">Management accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/uses-of-management-accounting.php">Uses of management accounting</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/the-importance-of-maintaining-accurate-accounts.php">The importance of maintaining accurate accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/false-accounting.php">False accounting</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Companies House Contact Centre</h3>
+		<p>0303 1234 500</p>
+
+		<p><img src="../../../_img/hr_140px_global.gif" alt="" /></p>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/publications/life-of-a-company-annual-requirements" target="_blank" rel="noopener noreferrer">Preparing and filing accounts guidance on the Companies House website</a></li>
+			<li><a href="http://www.learndirect.com/store/" target="_blank" rel="noopener noreferrer">Financial environment online course on the learndirect business website</a></li>
+		</ul>
+		
+		<p>Developed with:<br />
+			<a href="http://www.icaew.com/" target="bgExternalwwwbusinesslinkgovuk" title="Institute of Chartered Accountants in England and Wales - Opens in a new window">
+				<img src="../../_img/icaew_solus_logo_19x35px.JPG" width="19" height="35" alt="" /> Institute of Chartered Accountants in England and Wales
+			</a>
+		</p>
+		
+		<img src="../../../_img/px_finance.gif" alt="" width="181" height="5" />
+
+		<table cellpadding="0" cellspacing="0" width="100%">
+				<tr>
+					<td rowspan="12"><img src="../../../_img/px_white.gif" alt="" width="5" height="6" /></td>
+					<td colspan="2">
+						<p>
+							<a href="layere2de.html?topicId=1073858790" title="Find and manage money for your business">Finance and grants</a>
+						</p>
+						<p><img src="../../../_img/leftnav_sub_hr.gif" alt="" /></p>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2">
+						<p><a href="layer69e1.html?topicId=1073858944">Financial planning and accounts</a>
+						</p>
+						<p><img src="../../../_img/leftnav_sub_hr.gif" alt="" /></p>
+					</td>
+				</tr>
+				<tr>
+					<td>&nbsp;</td>
+					<td>
+						<p><strong>Financial and management accounts: the basics</strong></p>
+					</td>
+				</tr>
+				<tr>
+					<td width="10">&nbsp;</td>
+					<td>
+						<p><img src="../../../_img/leftnav_sub_sub_hr.gif" alt="" /></p>
+					</td>
+				</tr>
+				<tr>
+					<td width="10"><img src="../../../_img/px_finance.gif" alt="Current section" width="5" height="5" /></td>
+					<td>
+						<p><strong>Introduction</strong></p>
+					</td>
+				</tr>
+				<tr>
+					<td>&nbsp;</td>
+					<td>
+						<p>
+						</p>
+					</td>
+				</tr>
+				<tr>
+					<td>&nbsp;</td>
+					<td>
+						<p>
+						</p>
+					</td>
+				</tr>
+				<tr>
+					<td>&nbsp;</td>
+					<td>
+						<p>
+						</p>
+					</td>
+				</tr>
+				<tr>
+					<td>&nbsp;</td>
+					<td>
+						<p>
+						</p>
+					</td>
+				</tr>
+				<tr>
+					<td>&nbsp;</td>
+					<td>
+						<p>
+						</p>
+					</td>
+				</tr>
+				<tr>
+					<td>&nbsp;</td>
+					<td>
+						<p>
+						</p>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2">
+						<p>&nbsp;</p>
+					</td>
+				</tr>
+			</table>
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
