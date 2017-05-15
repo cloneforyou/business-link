e7d7dd671739ae05572156e8f1eb0d811592d8b1
Caroline Hagan <info@blueocto.co.uk>
2017-05-15 16:30:55 +0100

    Completed “finance-and-grants > financial-planning-and-accounts > financial-and-management-accounts-the-basics > The importance of maintaining accurate accounts” page.

---
+++ finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/the-importance-of-maintaining-accurate-accounts.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The importance of maintaining accurate accounts | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Financial and management accounts: the basics</h1>
+		<h2>The importance of maintaining accurate accounts</h2>
+		<p>It's important that your accounts are accurate and up to date so you can draw up 'true and fair' annual accounts. Your accounts should be backed up with full and detailed records of all business income and expenditure, such as receipts, invoices and purchase orders, payments in and out, etc. See the page on <a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/records-required-for-producing-a-profit-and-loss-account-or-completing-a-tax-return.php">records required for producing a profit and loss account or completing a tax return</a> in our guide on how to <a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business.php">set up a simple profit and loss account for your business</a>.</p>
+		
+		<h3>Why you should keep records and documents</h3>
+		<p>Following careful record keeping procedures can also help you with tax returns and prevent fraud or theft. Using a good record keeping system will keep you up to date and help you to:</p>
+		
+		<ul>
+			<li>track expenses, debts and creditors</li>
+			<li>apply for additional funding - eg a bank loan or overdraft facility</li>
+			<li>save time and accountancy costs</li>
+			<li>pay tax, accurately and on time, avoiding penalties</li>
+			<li>apply for and receive the correct amount of benefits or credits</li>
+		</ul>
+
+		<p>If you are starting a new business it is essential that you get a proper record keeping system in place immediately.</p>
+		<p>You can use various storage methods to keep records - such as a computer, hard disc drive or CD - as long as they:</p>
+		<ul>
+			<li>show all information contained within a document</li>
+			<li>allow information to be presented in a readable format</li>
+		</ul>
+
+		<p>You should try to keep all original documents, and must keep any which show that tax has been deducted - eg your end of year certificate for PAYE (form P60).</p>
+		<p>For more information, see our guide <a href="/tax-payroll-and-company-information/set-up-a-basic-record-keeping-system.php">set up a basic record-keeping system</a>.</p>
+		<p>Detailed and up-to-date records will help you comply with tax legislation, deal with mistakes and avoid penalties. You can be penalised for:</p>
+		<ul>
+			<li>not keeping adequate records</li>
+			<li>failing to keep records for required periods of time</li>
+			<li>inaccurate tax returns</li>
+		</ul>
+
+		<p><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the business records you must keep.</a></p>
+		
+		<h3>Analytical accounting tools</h3>
+		<p>Analysing your financial accounts enables you to compare your performance against previous years and with its competitors.</p>
+		<p>Ratios enable you to quickly compare relative values - eg two items on the balance sheet.</p>
+		<p>Ratio analysis can also be applied to non-financial data. For ease of reference, ratios are often split into the following areas of common control:</p>
+		<ul>
+			<li>liquidity ratios - these are used to measure solvency and short-term survival prospects</li>
+			<li>capital structure ratios - these measure the adequacy of owners' funding in relation to long-term debt</li>
+			<li>activity and efficiency ratios - these measure the operating efficiency of the business in non-financial terms</li>
+			<li>profitability ratios - these measure overall profitability and how well the business is using its assets and covering overhead costs</li>
+		</ul>
+
+		<p>You can <a href="https://web.archive.org/web/20111102190949/http://www.lloydstsbbusiness.com/support/businessguides/key_accounting_ratios.asp" target="_blank" rel="noopener noreferrer">find guidance on key accounting ratios and ratio analyses on the Lloyds TSB website</a> or see our guide on <a href="/finance-and-grants/financial-planning-and-accounts/balance-sheets-the-basics.php">balance sheets: the basics</a>.</p>
+		
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/uses-of-management-accounting.php" title="Uses of management accounting">Prev</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/false-accounting.php" title="False accounting">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php" title="Financial and management accounts: the basics">Introduction</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/financial-accounts.php">Financial accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/filing-financial-accounts.php">Filing financial accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/management-accounts.php">Management accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/uses-of-management-accounting.php">Uses of management accounting</a></li>
+			<li>The importance of maintaining accurate accounts</li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/false-accounting.php">False accounting</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Companies House Contact Centre</h3>
+		<p>0303 1234 500</p>
+
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.learndirect.com/store/" target="_blank" rel="noopener noreferrer">Financial management courses on the learndirect business website</a></li>
+			<li><a href="http://www.learndirect.com/store/" target="_blank" rel="noopener noreferrer">Financial environment online course on the learndirect business website</a></li>
+			<li><a href="../../../_files/hmrc_keeping-records-for-business-what-you-need-to-know.pdf" target="_blank" rel="noopener noreferrer">Download record keeping guidance for business from the HM Revenue &amp; Customs website (PDF, 336K)</a></li>
+			<li><a href="https://www.gov.uk/government/publications/life-of-a-company-annual-requirements" target="_blank" rel="noopener noreferrer">Accounts requirements guidance on the Companies House website</a></li>
+			<li><a href="https://web.archive.org/web/20110828140300/http://www.bized.co.uk/compfact/ratios/index.htm" target="_blank" rel="noopener noreferrer">Ratio analysis on the Biz/ed website</a></li>
+			<li><a href="https://web.archive.org/web/20111102190949/http://www.lloydstsbbusiness.com/support/businessguides/key_accounting_ratios.asp" target="_blank" rel="noopener noreferrer">Key accounting ratio guidance on the Lloyds TSB website</a></li>
+		</ul>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business.php">Set up a simple profit and loss account for your business</a></li>
+			<li><a href="/it-and-ecommerce/software-and-business-applications/accounting-software.php">Accounting software</a></li>
+			<li><a href="/tax-payroll-and-company-information/set-up-a-basic-record-keeping-system.php">Set up a basic record-keeping system</a></li>
+			<li><a href="/tax-payroll-and-company-information/self-assessment/tax-returns-if-you-are-self-employed-or-in-a-partnership/record-keeping-self-employed.php">Record keeping (self-employed)</a></li>
+			<li><a href="/tax-payroll-and-company-information/self-assessment/tax-returns-if-you-are-self-employed-or-in-a-partnership/record-keeping-partners-and-partnerships.php">Record keeping (partners and partnerships)</a></li>
+			<li><a href="/tax-payroll-and-company-information/self-assessment/your-tax-return-the-basics/record-keeping-individuals-and-directors.php">Record keeping (individuals and directors)</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/balance-sheets-the-basics.php">Balance sheets: the basics</a></li>
+			<li><a href="/buy-or-sell-a-business/buying/buy-an-existing-business.php">Buy an existing business</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting.php">Business budgeting</a></li>
+			<li><a href="https://www.gov.uk/business-finance-support" target="_blank" rel="noopener noreferrer">Think about your financial planning</a></li>
+			<li><a href="https://www.gov.uk/keeping-your-pay-tax-records" target="_blank" rel="noopener noreferrer">Understand how to set up a record-keeping system</a></li>
+		</ul>
+
+		<img src="../../../_img/px_finance.gif" alt="" width="181" height="5" />
+		
+		<p>Developed with:<br />
+			<a href="http://www.icaew.com/" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/icaew_solus_logo_19x35px.JPG" width="19" height="35" alt="" /> Institute of Chartered Accountants in England and Wales
+			</a>
+		</p>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
