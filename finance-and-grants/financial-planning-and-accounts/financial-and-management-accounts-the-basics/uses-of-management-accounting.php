f85e597fc4a183f4e3d97896edf9fa5287276439
Caroline Hagan <info@blueocto.co.uk>
2017-05-15 16:15:46 +0100

    Completed “finance-and-grants > financial-planning-and-accounts > financial-and-management-accounts-the-basics > Uses of management accounting” page.

---
+++ finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/uses-of-management-accounting.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Uses of management accounting | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Financial and management accounts: the basics</h1>
+		<h2>Uses of management accounting</h2>
+		<p>Management accounts will enable you to:</p>
+		<ul>
+			<li>compare your accounts with original budgets or forecasts</li>
+			<li>manage your resources better</li>
+			<li>identify trends in your business</li>
+			<li>highlight variations in your income or spending which may require attention</li>
+		</ul>
+
+		<p>They should be used for the following:</p>
+		<p><strong>Record keeping</strong></p>
+		<ul>
+			<li>recording business transactions</li>
+			<li>measuring results of financial changes</li>
+			<li>projecting financial effects of future transactions</li>
+			<li>preparing internal reports in a user-friendly format</li>
+		</ul>
+
+		<p><strong>Planning and control</strong></p>
+		<ul>
+			<li>collecting cash</li>
+			<li>controlling stocks</li>
+			<li>controlling expenses</li>
+			<li>co-ordination and monitoring of strategy/performance</li>
+			<li>monitoring gross margins</li>
+		</ul>
+
+		<p><strong>Decision making</strong></p>
+		<ul>
+			<li>using cost information for pricing, capital investment and marketing</li>
+			<li>evaluating market and product profitability</li>
+			<li>evaluating the financial effect of strategies and plans</li>
+		</ul>
+
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/management-accounts.php" title="Management accounts">Prev</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/the-importance-of-maintaining-accurate-accounts.php" title="The importance of maintaining accurate accounts">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php" title="Financial and management accounts: the basics">Introduction</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/financial-accounts.php">Financial accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/filing-financial-accounts.php">Filing financial accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/management-accounts.php">Management accounts</a></li>
+			<li>Uses of management accounting</li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/the-importance-of-maintaining-accurate-accounts.php">The importance of maintaining accurate accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/false-accounting.php">False accounting</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.learndirect.com/store/" target="_blank" rel="noopener noreferrer">Financial management online courses on the learndirect business website</a></li>
+			<li><a href="http://www.learndirect.com/store/" target="_blank" rel="noopener noreferrer">Financial environment online course on the learndirect business website</a></li>
+		</ul>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting.php">Business budgeting</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+			<li><a href="https://www.gov.uk/business-finance-support" target="_blank" rel="noopener noreferrer">Think about your financial planning</a></li>
+			<li><a href="https://www.gov.uk/keeping-your-pay-tax-records" target="_blank" rel="noopener noreferrer">Understand how to set up a record-keeping system</a></li>
+		</ul>
+
+		<img src="../../../_img/px_finance.gif" alt="" width="181" height="5" />
+		
+		<p>Developed with:<br />
+			<a href="http://www.icaew.com/" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/icaew_solus_logo_19x35px.JPG" width="19" height="35" alt="" /> Institute of Chartered Accountants in England and Wales
+			</a>
+		</p>
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
