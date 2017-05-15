3ae1062a191b7ad2a7847fdb1c3ac7baec3ff79a
Caroline Hagan <info@blueocto.co.uk>
2017-05-15 16:03:12 +0100

    Completed “finance-and-grants > financial-planning-and-accounts > financial-and-management-accounts-the-basics > Financial accounts” page.

---
+++ finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/financial-accounts.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Financial accounts | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Financial and management accounts: the basics</h1>
+		<h2>Financial accounts</h2>
+		<p>Financial accounts are a historical record of your <strong>business' performance</strong> over a past period - usually one year - for the benefit of external users such as shareholders, employees, suppliers, bankers and authorities.</p>
+		<p>Financial accounts normally include the following elements.</p>
+		
+		<h3>Profit and loss account</h3>
+		<p>This measures your business' performance over a given period of time, usually one year.</p>
+		<p>It compares the income of your business against the cost of goods or services and expenses incurred in earning that revenue. See our guide on how to <a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business.php">set up a simple profit and loss account for your business</a>.</p>
+		
+		<h3>Balance sheet</h3>
+		<p>This is a snapshot of your business' assets (what you own or are owed) and your liabilities (what you owe) on a particular day - eg the last day of your financial year. See our guide on <a href="/finance-and-grants/financial-planning-and-accounts/balance-sheets-the-basics.php">balance sheets: the basics</a>.</p>
+		
+		<h3>Cashflow statement</h3>
+		<p>This shows how your business has generated and disposed of cash and liquid funds during the period under review. A cashflow statement is different from a cashflow forecast, which is used to predict the expected rises and falls in cashflow over the coming year. See our guide on <a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">cashflow management: the basics</a>.</p>
+		
+		<h3>Statement of recognised gains and losses</h3>
+		<p>This records all gains and losses since the previous set of accounts. For example, changes caused by currency fluctuations, property revaluation, profits earned by associates and joint ventures not included in the normal accounts.</p>
+		
+		<h3>Unincorporated businesses</h3>
+		<p>Unincorporated businesses such as sole traders and partnerships are required by HM Revenue &amp; Customs (HMRC) to maintain proper books and records to support annual income tax returns. These must be kept for a minimum of six years. See our guide on how to <a href="/tax-payroll-and-company-information/set-up-a-basic-record-keeping-system.php">set up a basic record-keeping system</a>.</p>
+		<p>For more information on what to include in a company's annual accounts, see our guide on <a href="/tax-payroll-and-company-information/companies-house-annual-returns-and-accounts/filing-accounts-with-companies-house-dates-and-checklist.php">filing accounts with Companies House: dates and checklist</a>.</p>
+		
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php" title="Financial and management accounts: the basics">Prev</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/filing-financial-accounts.php" title="Filing financial accounts">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php" title="Financial and management accounts: the basics">Introduction</a></li>
+			<li>Financial accounts</li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/filing-financial-accounts.php">Filing financial accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/management-accounts.php">Management accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/uses-of-management-accounting.php">Uses of management accounting</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/the-importance-of-maintaining-accurate-accounts.php">The importance of maintaining accurate accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/false-accounting.php">False accounting</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Companies House Contact Centre</h3>
+		<p>0303 1234 500</p>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/publications/life-of-a-company-annual-requirements" target="_blank" rel="noopener noreferrer">Accounts requirements guidance on the Companies House website</a></li>
+			<li><a href="https://www.gov.uk/government/collections/hm-revenue-and-customs-leaflets-factsheets-and-booklets" target="_blank" rel="noopener noreferrer">Tax and National Insurance contribution booklets on the HMRC website</a></li>
+			<li><a href="http://www.learndirect.com/store/" target="_blank" rel="noopener noreferrer">Financial environment online course on the learndirect business website</a></li>
+		</ul>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/companies-house-annual-returns-and-accounts/filing-accounts-with-companies-house-dates-and-checklist.php">Filing accounts with Companies House: dates and checklist</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business.php">Set up a simple profit and loss account for your business</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/balance-sheets-the-basics.php">Balance sheets: the basics</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">Cashflow management: the basics</a></li>
+			<li><a href="/tax-payroll-and-company-information/set-up-a-basic-record-keeping-system.php">Set up a basic record-keeping system</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting.php">Business budgeting</a></li>
+			<li><a href="https://www.gov.uk/business-finance-support" target="_blank" rel="noopener noreferrer">Think about your financial planning</a></li>
+			<li><a href="https://www.gov.uk/keeping-your-pay-tax-records" target="_blank" rel="noopener noreferrer">Understand how to set up a record-keeping system</a></li>
+		</ul>
+
+		<img src="../../../_img/px_finance.gif" alt="" width="181" height="5" />
+		
+		<p>Developed with:<br />
+			<a href="http://www.icaew.com/" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/icaew_solus_logo_19x35px.JPG" width="19" height="35" alt="" /> Institute of Chartered Accountants in England and Wales
+			</a>
+		</p>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
