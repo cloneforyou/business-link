021da48502879c7f65c6ef5c7c4aea88dea0f559
Caroline Hagan <info@blueocto.co.uk>
2017-05-15 16:39:50 +0100

    Completed “finance-and-grants > financial-planning-and-accounts > financial-and-management-accounts-the-basics > False accounting” page.

---
+++ finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/false-accounting.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>False accounting | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Financial and management accounts: the basics</h1>
+		<h2>False accounting</h2>
+		<p>False accounting is fraudulent and usually occurs when a business or employee:</p>
+		<ul>
+			<li>deliberately records false financial information - eg to hide losses or appear more successful</li>
+			<li>changes, defaces or destroys records</li>
+			<li>exaggerates the business' assets or understates its liabilities</li>
+		</ul>
+
+		<p>In this way, an employee who adds only a few pounds to an expenses claim could constitute false accounting, even though it might not seriously affect your finances. But more serious fraud could mean that your business suffers major financial losses, or that it has been trading while insolvent.</p>
+		
+		<h3>What to do if you think an employee has been falsifying accounts</h3>
+		<p>You should immediately report false accounting to the National Fraud Reporting Centre (NFRC), who may then pass it to the police to investigate. Your business can also take action to recover any losses if an employee was involved.</p>
+		<p>You can <a href="http://www.actionfraud.org.uk/" target="_blank" rel="noopener noreferrer">find information on fraud, scams and identity theft on the Action Fraud website</a> or contact the Action Fraud Helpline on Tel 0300 123 2040.</p>
+		<p>To work out how much your business has lost and how the fraud occurred, you might need to use an accountant or auditor - possibly from outside the business. But you must still remember to report the fraud immediately.</p>
+		<p>You may suspend an employee while the investigations are carried out, but only if their contract of employment allows it. For further guidance on this, see the page on <a href="/employment-and-skills/problems-at-work/disciplinary-procedures-hearings-and-appeals/investigating-disciplinary-issues.php">investigating disciplinary issues</a> in our guide on <a href="/employment-and-skills/problems-at-work/disciplinary-procedures-hearings-and-appeals.php">disciplinary procedures, hearings and appeals</a>.</p>
+		
+		<h3>Reduce the risk of false accounting</h3>
+		<p>Common sense and sound business practices will help you to protect your business against the risks of theft and fraud. For example, you should:</p>
+		<ul>
+			<li>maintain thorough recruitment procedures and carry out pre-employment checks such as checking references - see our guide on <a href="/employment-and-skills/employment-checks/pre-employment-checks.php">pre-employment checks</a></li>
+			<li>put in place a whistle-blowing policy and try to encourage a culture of fraud awareness across the business</li>
+			<li>have a 'zero tolerance' approach to employee theft and fraud and clearly state this in employees' terms and conditions or your discipline and grievance policy - see the page on <a href="/employment-and-skills/staff-documents-and-employment-policies/set-up-employment-policies-for-your-business/policies-on-discipline-grievance-bullying-and-harassment.php">policies on discipline, grievance, bullying and harassment</a> in our guide on <a href="/employment-and-skills/staff-documents-and-employment-policies/set-up-employment-policies-for-your-business.php">how to set up employment policies for your business</a> </li>
+			<li>restrict access to financial information and divide duties so that no single person is responsible for all accounts and have more than one person authorise payments</li>
+			<li>check bank statements and other accounts - look into any unusual transactions or discrepancies, and audit processes and procedures regularly</li>
+			<li>commission a registered auditor, who would usually be a qualified accountant, to conduct an external audit to examine the business' financial report to check that they show a true and fair view of the business' financial performance and its assets and liabilities</li>
+			<li>undertake an assurance report - similar to an external audit but with more limited scope and objectives - possibly conducted by an external accountant, to review the entire business' accounts, but with specific aspects checked where necessary</li>
+		</ul>
+
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/the-importance-of-maintaining-accurate-accounts.php" title="The importance of maintaining accurate accounts">Prev</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php" title="Financial and management accounts: the basics">Introduction</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/financial-accounts.php">Financial accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/filing-financial-accounts.php">Filing financial accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/management-accounts.php">Management accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/uses-of-management-accounting.php">Uses of management accounting</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/the-importance-of-maintaining-accurate-accounts.php">The importance of maintaining accurate accounts</a></li>
+			<li>False accounting</li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.actionfraud.org.uk/" target="_blank" rel="noopener noreferrer">Fraud, scams and identity theft information on the Action Fraud website</a></li>
+			<li><a href="http://www.learndirect.com/store/" target="_blank" rel="noopener noreferrer">Financial management courses on the learndirect business website</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111206102847/http://online.businesslink.gov.uk/bdotg/action/stmtEmpLanding?r.i=1084729750&r.l1=1073858790&r.l2=1073858944&r.l3=1073933591&r.t=RESOURCES&topicId=1075225309" target="_blank" rel="noopener noreferrer">Use our interactive tool to create a written statement of employment</a></li>
+			<li><a href="http://www.learndirect.com/store/" target="_blank" rel="noopener noreferrer">Financial environment online course on the learndirect business website</a></li>
+			<li><a href="http://www.icaew.com/en/technical/audit-and-assurance/assurance" target="_blank" rel="noopener noreferrer">Assurance services information from the Institute of Chartered Accountants in England and Wales on the ICAEW website</a></li>
+		</ul>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business.php" title="The financial records you need to keep in order to report your profit or loss">Set up a simple profit and loss account for your business</a></li>
+			<li><a href="/employment-and-skills/problems-at-work/disciplinary-procedures-hearings-and-appeals.php" title="How to take fair and prompt disciplinary action against an employee">Disciplinary procedures, hearings and appeals</a></li>
+			<li><a href="/employment-and-skills/employment-checks/pre-employment-checks.php" title="The pre-employment checks that you are required to make and those that are advisable to make before offering a job">Pre-employment checks</a></li>
+			<li><a href="/employment-and-skills/staff-documents-and-employment-policies/set-up-employment-policies-for-your-business.php" title="Setting out your business' rules and procedures to benefit your staff and satisfy legal requirements">Set up employment policies for your business</a></li>
+			<li><a href="/it-and-ecommerce/software-and-business-applications/accounting-software.php" title="Selecting and implementing a financial records package to help your business">Accounting software</a></li>
+			<li><a href="/tax-payroll-and-company-information/set-up-a-basic-record-keeping-system.php" title="What you need to record and for how long, with step-by-step guidance on setting up your own system">Set up a basic record-keeping system</a></li>
+			<li><a href="https://www.gov.uk/business-finance-support" target="_blank" rel="noopener noreferrer">Think about your financial planning</a></li>
+			<li><a href="https://www.gov.uk/keeping-your-pay-tax-records" target="_blank" rel="noopener noreferrer">Understand how to set up a record-keeping system</a></li>
+		</ul>
+
+		<img src="../../../_img/px_finance.gif" alt="" width="181" height="5" />
+		
+		<p>Developed with:<br />
+			<a href="http://www.icaew.com/" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/icaew_solus_logo_19x35px.JPG" width="19" height="35" alt="" /> Institute of Chartered Accountants in England and Wales
+			</a>
+		</p>
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
