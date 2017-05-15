f40c2c94b6db1cb2856c36236a2749c58844d55f
Caroline Hagan <info@blueocto.co.uk>
2017-05-15 16:14:19 +0100

    Completed “finance-and-grants > financial-planning-and-accounts > financial-and-management-accounts-the-basics > Management accounts” page.

---
+++ finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/management-accounts.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Management accounts | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Financial and management accounts: the basics</h1>
+		<h2>Management accounts</h2>
+		<p>Management accounts can help you make timely and meaningful management decisions about your business.</p>
+		<p>Different businesses will have different management accounting needs, depending on the business areas that are important to them. These can include:</p>
+		<ul>
+			<li>the sales process - such as pricing, distribution and debtors</li>
+			<li>the purchasing process - such as stock records and creditors</li>
+			<li>a fixed asset register - details of all fixed assets, including identification numbers, cost and date of purchase, etc</li>
+			<li>employee records</li>
+		</ul>
+
+		<p>There is no legal requirement to prepare management accounts, but it is hard to run a business effectively without them. Most companies produce them regularly - eg monthly or quarterly.</p>
+		<p>Management accounts analyse recent <strong>historical</strong> performance and usually include <strong>forward-looking</strong> elements such as sales, cashflow and profit forecasts. The analysis is usually performed against forecasts and budgets that have been produced at the start of the year. See our guide on <a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting.php">budgeting and business planning</a>.</p>
+		<p>The information in management accounts is usually broken down so that the performance of different elements of the business can be measured. For example if a business has more than one sales outlet, there might be a separate report for each. There may also be a report produced to show how well a particular product has done across different outlets.</p>
+		<p>For businesses selling more than one product, it is advisable to provide a financial breakdown for each product category. This will allow you to ensure that profitable products are not subsidising those that are selling poorly, unless you intentionally promote loss leaders to attract further custom.</p>
+		
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/filing-financial-accounts.php" title="Filing financial accounts">Prev</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/uses-of-management-accounting.php" title="Uses of management accounting">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php" title="Financial and management accounts: the basics">Introduction</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/financial-accounts.php">Financial accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/filing-financial-accounts.php">Filing financial accounts</a></li>
+			<li>Management accounts</li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/uses-of-management-accounting.php">Uses of management accounting</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/the-importance-of-maintaining-accurate-accounts.php">The importance of maintaining accurate accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/false-accounting.php">False accounting</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/publications/life-of-a-company-annual-requirements" target="_blank" rel="noopener noreferrer">Accounts requirements guidance on the Companies House website</a></li>
+			<li><a href="http://www.learndirect.com/store/" target="_blank" rel="noopener noreferrer">Financial management online courses on the learndirect business website</a></li>
+			<li><a href="http://www.learndirect.com/store/" target="_blank" rel="noopener noreferrer">Financial environment online course on the learndirect business website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">Cashflow management: the basics</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting.php">Business budgeting</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+			<li><a href="/tax-payroll-and-company-information/set-up-a-basic-record-keeping-system.php">Set up a basic record-keeping system</a></li>
+			<li><a href="https://www.gov.uk/business-finance-support" target="_blank" rel="noopener noreferrer">Think about your financial planning</a></li>
+			<li><a href="https://www.gov.uk/keeping-your-pay-tax-records" target="_blank" rel="noopener noreferrer">Understand how to set up a record-keeping system</a></li>
+		</ul>
+
+		<img src="../../../_img/px_finance.gif" alt="" width="181" height="5" />
+		
+		<p>Developed with:<br />
+			<a href="http://www.icaew.com/" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/icaew_solus_logo_19x35px.JPG" width="19" height="35" alt="" /> Institute of Chartered Accountants in England and Wales
+			</a>
+		</p>
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
