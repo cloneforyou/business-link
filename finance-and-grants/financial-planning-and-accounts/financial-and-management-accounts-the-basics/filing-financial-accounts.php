1cc945175d2f09d6e780d606382859ba7fbc9185
Caroline Hagan <info@blueocto.co.uk>
2017-05-15 16:12:18 +0100

    Completed “finance-and-grants > financial-planning-and-accounts > financial-and-management-accounts-the-basics > Filing financial accounts” page.

---
+++ finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/filing-financial-accounts.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Filing financial accounts | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+
+		<h1>Financial and management accounts: the basics</h1>
+		<h2>Filing financial accounts</h2>
+		<p>Limited companies are <strong>obliged by law</strong> to prepare a set of financial accounts each year and to file a copy with Companies House.</p>
+		<p>For accounting periods starting on or after 6 April 2008:</p>
+		<ul>
+			<li>private companies must file accounts within 21 months of the business' formation, and within nine months of the end of each financial year thereafter</li>
+			<li>public companies must file accounts within 18 months of the business' formation, and within six months of the end of each financial year thereafter</li>
+		</ul>
+
+		<p><a href="https://www.gov.uk/government/publications/life-of-a-company-annual-requirements" target="_blank" rel="noopener noreferrer">Read guidance on preparing and filing accounts on the Companies House website</a>.</p>
+		<p>There are statutory penalties for late or incorrect filing, for which the directors are liable. <a href="https://www.gov.uk/government/publications/late-filing-penalties" target="_blank" rel="noopener noreferrer">Find information about late filing penalties on the Companies House website</a>.</p>
+		<p>Small and medium-sized companies are allowed to file accounts with certain information omitted. For more information, see our guide on <a href="/tax-payroll-and-company-information/companies-house-annual-returns-and-accounts/accounting-and-audit-exemptions-for-small-companies.php">accounting and auditing exemptions for small companies</a>.</p>
+		
+		<h3>Small companies</h3>
+		<p>A small company is one which meets at least <strong>two</strong> of the following criteria:</p>
+		<ul>
+			<li>annual turnover must be &pound;6.5 million or less</li>
+			<li>the balance sheet total - fixed and current assets - must be &pound;3.26 million or less</li>
+			<li>the average number of employees must not exceed 50</li>
+		</ul>
+		<p>Small companies may deliver a copy of the full accounts to Companies House or choose to deliver abbreviated accounts which include the following:</p>
+		<ul>
+			<li>abbreviated balance sheet and notes</li>
+			<li>special auditors' report - unless the company has a small company audit exemption</li>
+		</ul>
+		
+		<h3>Medium-sized companies</h3>
+		<p>A medium-sized company must meet at least <strong>two</strong> of the following criteria:</p>
+		<ul>
+			<li>annual turnover must not exceed &pound;25.9 million (&pound;22.8 million or less for accounting reference periods before 6 April 2008)</li>
+			<li>the balance sheet total must not exceed &pound;12.9 million (&pound;11.4 million on or before 6 April 2008)</li>
+			<li>the average number of employees must not exceed 250</li>
+		</ul>
+		<p>Medium-sized companies may deliver a copy of the full accounts to Companies House or choose to deliver abbreviated accounts which include the following:</p>
+		<ul>
+			<li>full balance sheet</li>
+			<li>abbreviated profit and loss account</li>
+			<li>special auditors' report</li>
+			<li>directors' report</li>
+			<li>notes to the accounts</li>
+		</ul>
+
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/financial-accounts.php" title="Financial accounts">Prev</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/management-accounts.php" title="Management accounts">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php" title="Financial and management accounts: the basics">Introduction</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/financial-accounts.php">Financial accounts</a></li>
+			<li>Filing financial accounts</li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/management-accounts.php">Management accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/uses-of-management-accounting.php">Uses of management accounting</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/the-importance-of-maintaining-accurate-accounts.php">The importance of maintaining accurate accounts</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/false-accounting.php">False accounting</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Companies House Contact Centre</h3>
+		<p>0303 1234 500</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/publications/life-of-a-company-annual-requirements" target="_blank" rel="noopener noreferrer">Preparing and filing accounts guidance on the Companies House website</a></li>
+			<li><a href="https://www.gov.uk/government/publications/late-filing-penalties" target="_blank" rel="noopener noreferrer">Late filing penalties information on the Companies House website</a></li>
+			<li><a href="/do-it-online/file-company-information-online-using-companies-house-webfiling.php">Use WebFiling with Companies House</a></li>
+			<li><a href="http://www.learndirect.com/store/" target="_blank" rel="noopener noreferrer">Financial environment online course on the learndirect business website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business.php">Set up a simple profit and loss account for your business</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/balance-sheets-the-basics.php">Balance sheets: the basics</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">Cashflow management: the basics</a></li>
+			<li><a href="/tax-payroll-and-company-information/companies-house-annual-returns-and-accounts/accounting-and-audit-exemptions-for-small-companies.php">Accounting and audit exemptions for small companies</a></li>
+			<li><a href="/tax-payroll-and-company-information/companies-house-annual-returns-and-accounts/filing-accounts-with-companies-house-dates-and-checklist.php">Filing accounts with Companies House: dates and checklist</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+			<li><a href="/tax-payroll-and-company-information/company-administration-an-introduction/company-administration-the-basics.php">Company administration: the basics</a></li>
+			<li><a href="https://www.gov.uk/business-finance-support" target="_blank" rel="noopener noreferrer">Think about your financial planning</a></li>
+			<li><a href="https://www.gov.uk/keeping-your-pay-tax-records" target="_blank" rel="noopener noreferrer">Understand how to set up a record-keeping system</a></li>
+		</ul>
+		<img src="../../../_img/px_finance.gif" alt="" width="181" height="5" />
+		
+
+		<p>Developed with:<br />
+			<a href="http://www.icaew.com/" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/icaew_solus_logo_19x35px.JPG" width="19" height="35" alt="" /> Institute of Chartered Accountants in England and Wales
+			</a>
+		</p>
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
