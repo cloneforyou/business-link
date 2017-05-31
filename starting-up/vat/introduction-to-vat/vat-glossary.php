449264c063b34bfc490dc789bdc1483121165d58
Caroline Hagan <info@blueocto.co.uk>
2017-05-31 21:44:59 +0100

    Completed “starting-up > vat > introduction-to-vat > VAT glossary” page.

---
+++ starting-up/vat/introduction-to-vat/vat-glossary.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>VAT glossary | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Introduction to VAT</h1>
+		<h2>VAT glossary</h2>
+		<p>These are some plain English definitions of common VAT terms that HM Revenue &amp; Customs (HMRC) uses:</p>
+		<ul>
+			<li><strong>accounting period:</strong> see tax period</li>
+			<li><strong>acquisitions:</strong> goods brought into the UK from other EU countries - (goods brought into the UK from outside of the EU are known as Imports)</li>
+			<li><strong>corporate body:</strong> an incorporated body such as a limited company, limited liability partnership, friendly, industrial or provident society</li>
+			<li><strong>distance sales:</strong> where a business in one EU country sells and ships goods directly to consumers in another EU country, eg internet or mail-order sales</li>
+			<li><strong>exports:</strong> goods sent to a non-EU country</li>
+			<li><strong>despatches:</strong> goods sent to another EU country</li>
+			<li><strong>imports:</strong> goods brought into the EU from another country</li>
+			<li><strong>input tax:</strong> the VAT you pay on your purchases</li>
+			<li><strong>output tax:</strong> the VAT you charge on your sales</li>
+			<li><strong>place of supply:</strong> the country in which a supply of goods or services must be accounted for VAT purposes</li>
+			<li><strong>self-billing:</strong> your customer issues your VAT invoice and sends a copy to you with their payment</li>
+			<li><strong>supply:</strong> selling or otherwise providing goods or services, including barter and some free provision</li>
+			<li><strong>supply of goods:</strong> when exclusive ownership of goods passes from one person to another</li>
+			<li><strong>taxable person:</strong> any business entity that buys or sells goods or services and is required to be registered for VAT - this includes individuals, partnerships, companies, clubs, associations and charities</li>
+			<li><strong>taxable supplies:</strong> all goods and services sold or otherwise supplied by a taxable person which are liable to VAT at the standard, reduced or zero rate</li>
+			<li><strong>taxable turnover:</strong> the total value - excluding VAT - of the taxable supplies you make in the UK (excludes capital items like buildings, equipment, vehicles or exempt supplies)</li>
+			<li><strong>tax period:</strong> the period of time covered by your VAT return, usually quarterly</li>
+			<li><strong>tax point:</strong> the date when VAT has to be accounted for - for goods, this is usually when you send the goods to a customer or when they take them away, for services, this is usually when the service is performed</li>
+			<li><strong>time of supply:</strong> see tax point</li>
+		</ul>
+
+		<ul>
+			<li><a href="/starting-up/vat/introduction-to-vat/find-out-more-about-vat.php" title="Find out more about VAT">Prev</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/identify-which-vat-scheme-your-business-is-eligible-to-use/here-is-how-I-got-to-grips-with-vat.php" title="Here's how I got to grips with VAT">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/introduction-to-vat.php" title="Introduction to VAT">Introduction</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/what-is-vat.php">What is VAT?</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/who-charges-vat-and-what-vat-is-charged-on.php">Who charges VAT and what VAT is charged on</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/rates-of-vat.php">Rates of VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/items-not-covered-by-vat.php">Items not covered by VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/the-difference-between-exempt-and-zero-rated.php">The difference between exempt and zero-rated</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/find-out-more-about-vat.php">Find out more about VAT</a></li>
+			<li>VAT glossary</li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/identify-which-vat-scheme-your-business-is-eligible-to-use/here-is-how-I-got-to-grips-with-vat.php">Here's how I got to grips with VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/more-useful-links.php">More Useful Links</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" /> 
+				HM Revenue &amp; Customs
+			</a>
+		</p>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/publications/vat-notice-700-the-vat-guide#P1956_159320" target="_blank" rel="noopener noreferrer" title="Detailed information on tax periods">Tax periods information - VAT Notice 700 from HMRC</a></li>
+			<li><a href="https://www.gov.uk/government/publications/vat-notice-700-the-vat-guide" target="_blank" rel="noopener noreferrer" title="Notice 700 - guidance on VAT rules and processes, help with problems and links to more information">A guide to VAT - VAT Notice 700 from HMRC</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/reclaiming-vat-on-your-purchases/what-you-can-and-cannot-reclaim-vat-on.php">What you can and cannot reclaim VAT on</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
