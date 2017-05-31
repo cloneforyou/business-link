bc05762d480ed86c93f744450be296674242db95
Caroline Hagan <info@blueocto.co.uk>
2017-05-31 21:16:04 +0100

    Completed “starting-up > vat > introduction-to-vat > Who charges VAT and what VAT is charged on” page.

---
+++ starting-up/vat/introduction-to-vat/who-charges-vat-and-what-vat-is-charged-on.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Who charges VAT and what VAT is charged on | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Introduction to VAT</h1>
+		<h2>Who charges VAT and what VAT is charged on</h2>
+		<p>VAT-registered businesses add VAT to the sale price of most goods and services they provide.</p>
+		
+		<h3>When you must register for VAT</h3>
+		<p>If you're a business and the goods or services you provide count as what's known as 'taxable supplies' (see 'What is VAT charged on?' below) you'll have to register for VAT if either:</p>
+		<ul>
+			<li>your turnover for the previous 12 months has gone over a specific limit - called the 'VAT threshold' (currently &pound;77,000)</li>
+			<li>you think your turnover will soon go over this limit</li>
+		</ul>
+		
+		<p>You can choose to register for VAT if you want, even if you don't have to.</p>
+		<p><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/how-to-register-for-vat.php">How and when to register for VAT</a></p>
+		
+		<h3>What is VAT charged on?</h3>
+		<p>If you're VAT-registered, you'll have to charge VAT on any goods and services that you provide in the UK that are VAT taxable. You charge VAT on the full sale price, even if you accept goods in part exchange or through barter instead of money. You can find more information about goods and services on which you have to charge VAT in the guide below.</p>
+		<p><a href="/tax-payroll-and-company-information/vat/charging-vat-on-your-sales-and-other-supplies/goods-and-services-where-you-have-to-charge-vat.php">Goods and services where you have to charge VAT</a></p>
+		
+		<h3>How VAT is charged and accounted for</h3>
+		<p>If you're VAT-registered, the VAT you add to the sale price of your goods or services is called your 'output tax'. The VAT you pay when you buy goods and services for your business is called your 'input tax'.</p>
+		
+		<h3>Filling in your VAT Return</h3>
+		<p>If you're VAT-registered you'll have to submit a VAT Return at regular intervals - usually quarterly - and send it to HM Revenue &amp; Customs (HMRC). The return shows:</p>
+		<ul>
+			<li>the VAT you have charged on your sales to your customers in the period - known as output tax</li>
+			<li>the VAT you have paid on your purchases - known as input tax</li>
+		</ul>
+		
+		<p>If the amount of output tax is more than the input tax, then you send the difference to HMRC with your return.</p>
+		<p>If the input tax is more than your output tax, you claim the difference back from HMRC.</p>
+		<p>There are special schemes that some businesses can use to help them work out and pay their VAT.</p>
+		<p>You can read about VAT Returns, payments and repayments, VAT accounting schemes and VAT online services in our guides below.</p>
+		<p><a href="/tax-payroll-and-company-information/vat.php">Managing your VAT</a></p>
+		<p><a href="/tax-payroll-and-company-information/vat/how-to-submit-your-vat-return-online.php">How to submit your VAT Return online</a></p>
+		<p><a href="/tax-payroll-and-company-information/vat/accounting-schemes-to-simplify-your-vat/vat-accounting-schemes-the-basics.php">VAT accounting schemes: the basics</a></p>
+		
+		<ul>
+			<li><a href="/starting-up/vat/introduction-to-vat/what-is-vat.php" title="What is VAT?">Prev</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/rates-of-vat.php" title="Rates of VAT">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/introduction-to-vat.php" title="Introduction to VAT">Introduction</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/what-is-vat.php">What is VAT?</a></li>
+			<li>Who charges VAT and what VAT is charged on</li>
+			<li><a href="/starting-up/vat/introduction-to-vat/rates-of-vat.php">Rates of VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/items-not-covered-by-vat.php">Items not covered by VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/the-difference-between-exempt-and-zero-rated.php">The difference between exempt and zero-rated</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/find-out-more-about-vat.php">Find out more about VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/vat-glossary.php">VAT glossary</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/identify-which-vat-scheme-your-business-is-eligible-to-use/here-is-how-I-got-to-grips-with-vat.php">Here's how I got to grips with VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/more-useful-links.php">More Useful Links</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" /> 
+				HM Revenue &amp; Customs
+			</a>
+		</p>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/publications/vat-notice-700-the-vat-guide" target="_blank" rel="noopener noreferrer" title="Notice 700 - guidance on VAT rules and processes, help with problems and links to more information">A guide to VAT - VAT Notice 700 from HMRC</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/charging-vat-on-your-sales-and-other-supplies/goods-and-services-where-you-have-to-charge-vat.php">Goods and services where you have to charge VAT</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-returns-and-accounts/how-to-complete-your-vat-return-box-by-box.php">How to complete your VAT Return box by box</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/accounting-schemes-to-simplify-your-vat/vat-accounting-schemes-the-basics.php">VAT accounting schemes: the basics</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
