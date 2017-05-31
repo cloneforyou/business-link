9cdae3c32061cdae65608adb7a9e56da32acc653
Caroline Hagan <info@blueocto.co.uk>
2017-05-31 21:31:49 +0100

    Completed “starting-up > vat > introduction-to-vat > The difference between exempt and zero-rated” page.

---
+++ starting-up/vat/introduction-to-vat/the-difference-between-exempt-and-zero-rated.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>The difference between exempt and zero-rated | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Introduction to VAT</h1>
+		<h2>The difference between exempt and zero-rated</h2>
+		<p>If you sell zero-rated goods or services, they count as taxable supplies, but you don't add any VAT to your selling price because the VAT rate is 0 per cent.</p>
+		<p>If you sell goods or services that are exempt, you don't charge any VAT and they're not taxable supplies. This means that you won't normally be able to reclaim any of the VAT on your expenses.</p>
+		<p>Generally, you can't register for VAT or reclaim the VAT on your purchases if you sell <strong>only</strong> exempt goods or services. If you sell <strong>some</strong> exempt goods or services you may not be able to reclaim the VAT on all of your purchases.</p>
+		<p>If you buy and sell only - or mainly - zero-rated goods or services you can apply to HM Revenue &amp; Customs to be exempt from registering for VAT. This could make sense if you pay little or no VAT on your purchases. You can find out how to apply for exemption from registration, get details of exempt and partly exempt goods and services and check the rates of VAT and how they apply to different goods and services in our guides below.</p>
+		<p><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat.php">When to register for UK VAT</a></p>
+		<p><a href="/tax-payroll-and-company-information/vat/reclaiming-vat-on-your-purchases/exempt-and-partially-exempt-businesses-for-vat-purposes.php">Exempt and partially-exempt businesses for VAT purposes</a></p>
+		<p><a href="/tax-payroll-and-company-information/vat/vat-rates-thresholds-fuel%20scales-and-exchange-rates/vat-rates-explained-standard-reduced-zero-exempt.php">VAT rates explained: standard, reduced, zero, exempt</a></p>
+		<p><a href="/tax-payroll-and-company-information/vat/vat-rates-thresholds-fuel%20scales-and-exchange-rates/rates-of-vat-on-different-goods-and-services.php">Rates of VAT on different goods and services</a></p>
+		
+		<ul>
+			<li><a href="/starting-up/vat/introduction-to-vat/items-not-covered-by-vat.php" title="Items not covered by VAT">Prev</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/find-out-more-about-vat.php" title="Find out more about VAT">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/introduction-to-vat.php" title="Introduction to VAT">Introduction</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/what-is-vat.php">What is VAT?</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/who-charges-vat-and-what-vat-is-charged-on.php">Who charges VAT and what VAT is charged on</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/rates-of-vat.php">Rates of VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/items-not-covered-by-vat.php">Items not covered by VAT</a></li>
+			<li>The difference between exempt and zero-rated</li>
+			<li><a href="/starting-up/vat/introduction-to-vat/find-out-more-about-vat.php">Find out more about VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/vat-glossary.php">VAT glossary</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/identify-which-vat-scheme-your-business-is-eligible-to-use/here-is-how-I-got-to-grips-with-vat.php">Here's how I got to grips with VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/more-useful-links.php">More Useful Links</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" /> 
+				HM Revenue &amp; Customs
+			</a>
+		</p>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-rates-thresholds-fuel%20scales-and-exchange-rates/vat-rates-explained-standard-reduced-zero-exempt.php">VAT rates explained: standard, reduced, zero, exempt</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat.php">When to register for UK VAT </a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/reclaiming-vat-on-your-purchases/exempt-and-partially-exempt-businesses-for-vat-purposes.php">Exempt and partially-exempt businesses for VAT purposes</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-rates-thresholds-fuel%20scales-and-exchange-rates/rates-of-vat-on-different-goods-and-services.php">Rates of VAT on different goods and services</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
