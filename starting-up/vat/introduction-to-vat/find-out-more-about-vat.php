eb05d30d2eede136a2a0c25d0c9cccebd163e522
Caroline Hagan <info@blueocto.co.uk>
2017-05-31 21:42:05 +0100

    Completed “starting-up > vat > introduction-to-vat > Find out more about VAT” page.

---
+++ starting-up/vat/introduction-to-vat/find-out-more-about-vat.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Find out more about VAT | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Introduction to VAT</h1>
+		<h2>Find out more about VAT</h2>
+		<p>There is a range of general guidance on VAT in this section of the website including:</p>
+		<ul>
+			<li>how and when to register for VAT</li>
+			<li>VAT thresholds, fuel scale charges and exchange rates</li>
+			<li>VAT rates on different goods and services</li>
+			<li>online VAT Returns</li>
+			<li>accounting schemes</li>
+			<li>payments and refunds</li>
+			<li>links to downloadable forms, guidance and notices</li>
+		</ul>
+
+		<h3>VAT Helpline</h3>
+		<p>If you cannot find the answer to your question on the HM Revenue &amp; Customs (HMRC) website, the easiest way is to ring the VAT Helpline where you can get most of your VAT questions answered.</p>
+		<p>Before you ring, make sure you have your VAT registration number and postcode to hand. If you're not VAT-registered, you'll need your postcode - it's necessary so HMRC can keep a record of your call.</p>
+		<p>It's not always possible to answer your question straightaway over the telephone because the helpline adviser may need to research the answer. If this happens, they'll take your details and call you back as soon as possible. The helpline adviser will tell you how long it will take.</p>
+		<p>In some cases, HMRC might need you to write in, especially if they need to look at your paperwork. The helpline adviser will tell you if this needs to happen and where you need to write to.</p>
+		<p><a href="https://www.gov.uk/government/organisations/hm-revenue-customs/contact/vat-enquiries" title="Phone the VAT Helpline, or send your VAT question by email or post" target="_blank" rel="noopener noreferrer">Find contact details for the VAT Helpline on the HMRC website</a></p>
+		<h3>Business Advice Open Days</h3>
+		<p>Business Advice Open Days are popular events designed especially for small and medium-sized businesses and they take place in different locations around the UK. It's not just HMRC at these events - there are other government departments as well, like the Department for Work and Pensions, the Health &amp; Safety Executive, the Intellectual Property Office and Business Link.</p>
+		<p>It's an opportunity to talk to experts and get all your questions answered in one place on the same day. You can:</p>
+		<ul>
+			<li>book a one-to-one session with a VAT adviser</li>
+			<li>attend seminars about making VAT easier</li>
+			<li>learn how to do tax online</li>
+			<li>get help with marketing, funding and business planning</li>
+			<li>get tips on how to make your business grow</li>
+		</ul>
+
+		<p><a href="/starting-up/further-support-for-your-business/business-advice-open-days.php">Business Advice Open Days</a></p>
+		
+		<ul>
+			<li><a href="/starting-up/vat/introduction-to-vat/the-difference-between-exempt-and-zero-rated.php" title="The difference between exempt and zero-rated">Prev</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/vat-glossary.php" title="VAT glossary">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/introduction-to-vat.php" title="Introduction to VAT">Introduction</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/what-is-vat.php">What is VAT?</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/who-charges-vat-and-what-vat-is-charged-on.php">Who charges VAT and what VAT is charged on</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/rates-of-vat.php">Rates of VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/items-not-covered-by-vat.php">Items not covered by VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/the-difference-between-exempt-and-zero-rated.php">The difference between exempt and zero-rated</a></li>
+			<li>Find out more about VAT</li>
+			<li><a href="/starting-up/vat/introduction-to-vat/vat-glossary.php">VAT glossary</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/identify-which-vat-scheme-your-business-is-eligible-to-use/here-is-how-I-got-to-grips-with-vat.php">Here's how I got to grips with VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/more-useful-links.php">More Useful Links</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" /> 
+				HM Revenue &amp; Customs
+			</a>
+		</p>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/topic/business-tax/vat" target="_blank" rel="noopener noreferrer">Guidance on VAT for consumers</a></li>
+			<li><a href="https://www.gov.uk/government/publications/vat-notice-700-the-vat-guide" target="_blank" rel="noopener noreferrer" title="Notice 700 - guidance on VAT rules and processes, help with problems and links to more information">A guide to VAT - VAT Notice 700 from HMRC</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days.php">Business Advice Open Days</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat.php">When to register for UK VAT </a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/accounting-schemes-to-simplify-your-vat/vat-accounting-schemes-the-basics.php">VAT accounting schemes: the basics</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-returns-and-accounts/accounts-and-records-for-your-vat.php">Accounts and records for your VAT</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-rates-thresholds-fuel%20scales-and-exchange-rates/rates-of-vat-on-different-goods-and-services.php">Rates of VAT on different goods and services</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-returns-and-accounts.php">VAT Returns and accounts</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-problems-mistakes-adjustments-visits-and-questions.php">VAT problems, mistakes, adjustments, visits and questions</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/changing-or-cancelling-your-vat-registration.php">Changing or cancelling your VAT registration</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/international-trade-international-visits-and-vat.php">International trade, international visits and VAT</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/charities-and-other-not-for-profit-organisations-and-vat.php">Charities and other not-for-profit organisations and VAT</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
