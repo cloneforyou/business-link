8ba3cb8af96dc79c33e61aa76e776811ad3e6b75
Caroline Hagan <info@blueocto.co.uk>
2017-05-31 20:57:04 +0100

    Completed “starting-up > vat > introduction-to-vat > What is VAT?” page.

--- _inc/nav--starting-up.php
+++ _inc/nav--starting-up.php
@@ -506,19 +506,18 @@
 			<ul>
 				<li><a href="/starting-up/vat.php" title="VAT">Introduction</a></li>
 				<li><a href="/tax-payroll-and-company-information/vat/introduction-to-vat.php">Introduction to VAT</a>
-			<!--
-			|	|	|
-			|	|	├── Introduction
-			|	|	├── What is VAT?
-			|	|	├── Who charges VAT and what VAT is charged on
-			|	|	├── Rates of VAT
-			|	|	├── Items not covered by VAT
-			|	|	├── The difference between exempt and zero-rated
-			|	|	├── Find out more about VAT
-			|	|	├── VAT glossary
-			|	|	├── Here's how I got to grips with VAT
-			|	|	└── More Useful Links
-			//-->
+					<ul>
+						<li><a href="/tax-payroll-and-company-information/vat/introduction-to-vat.php" title="Introduction to VAT">Introduction</a></li>
+						<li><a href="/starting-up/vat/introduction-to-vat/what-is-vat.php">What is VAT?</a></li>
+						<li><a href="/starting-up/vat/introduction-to-vat/who-charges-vat-and-what-vat-is-charged-on.php">Who charges VAT and what VAT is charged on</a></li>
+						<li><a href="/starting-up/vat/introduction-to-vat/rates-of-vat.php">Rates of VAT</a></li>
+						<li><a href="/starting-up/vat/introduction-to-vat/items-not-covered-by-vat.php">Items not covered by VAT</a></li>
+						<li><a href="/starting-up/vat/introduction-to-vat/the-difference-between-exempt-and-zero-rated.php">The difference between exempt and zero-rated</a></li>
+						<li><a href="/starting-up/vat/introduction-to-vat/find-out-more-about-vat.php">Find out more about VAT</a></li>
+						<li><a href="/starting-up/vat/introduction-to-vat/vat-glossary.php">VAT glossary</a></li>
+						<li><a href="/finance-and-grants/financial-planning-and-accounts/identify-which-vat-scheme-your-business-is-eligible-to-use/here-is-how-I-got-to-grips-with-vat.php">Here's how I got to grips with VAT</a></li>
+						<li><a href="/starting-up/vat/introduction-to-vat/more-useful-links.php">More Useful Links</a></li>
+					</ul>
 				</li>
 				<li><a href="https://www.gov.uk/vat-businesses" target="_blank" rel="noopener noreferrer">Get to grips with VAT and your business</a></li>
 				<!--//
---
+++ starting-up/vat/introduction-to-vat/what-is-vat.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>What is VAT? | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Introduction to VAT</h1>
+		<h2>What is VAT?</h2>
+		<p>VAT is a tax that's charged on most business transactions in the UK. Businesses add VAT to the price they charge when they provide goods and services to:</p>
+		<ul>
+			<li>business customers - for example, a clothing manufacturer adds VAT to the prices they charge a clothes shop</li>
+			<li>non-business customers - members of the public or 'consumers' - for example, a hairdressing salon includes VAT in the prices they charge members of the public</li>
+		</ul>
+
+		<p>If you're a VAT-registered business, in most cases you:</p>
+		<ul>
+			<li>charge VAT on the goods and services you provide</li>
+			<li>reclaim the VAT you pay when you buy goods and services for your business</li>
+		</ul>
+
+		<p>If you are not VAT-registered then you cannot reclaim the VAT you pay when you purchase goods and services.</p>
+		
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/introduction-to-vat.php" title="Introduction to VAT">Prev</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/who-charges-vat-and-what-vat-is-charged-on.php" title="Who charges VAT and what VAT is charged on">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/introduction-to-vat.php" title="Introduction to VAT">Introduction</a></li>
+			<li>What is VAT?</li>
+			<li><a href="/starting-up/vat/introduction-to-vat/who-charges-vat-and-what-vat-is-charged-on.php">Who charges VAT and what VAT is charged on</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/rates-of-vat.php">Rates of VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/items-not-covered-by-vat.php">Items not covered by VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/the-difference-between-exempt-and-zero-rated.php">The difference between exempt and zero-rated</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/find-out-more-about-vat.php">Find out more about VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/vat-glossary.php">VAT glossary</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/identify-which-vat-scheme-your-business-is-eligible-to-use/here-is-how-I-got-to-grips-with-vat.php">Here's how I got to grips with VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/more-useful-links.php">More Useful Links</a></li>
+		</ul>
+
+		
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<p>
+			Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" />
+				HM Revenue &amp; Customs
+			</a>
+		</p>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/publications/vat-notice-700-the-vat-guide" target="_blank" rel="noopener noreferrer" title="Notice 700 - guidance on VAT rules and processes, help with problems and links to more information">A guide to VAT - VAT Notice 700 from HM Revenue &amp; Customs</a></li>
+		</ul>
+		
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
