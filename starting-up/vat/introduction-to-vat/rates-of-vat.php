1e0ed38e250c7b5c942894e191a5333d9b186215
Caroline Hagan <info@blueocto.co.uk>
2017-05-31 21:25:31 +0100

    Completed “starting-up > vat > introduction-to-vat > Rates of VAT” page.

---
+++ starting-up/vat/introduction-to-vat/rates-of-vat.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Rates of VAT | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Introduction to VAT</h1>
+		<h2>Rates of VAT</h2>
+		<p>There are different VAT rates, depending on the goods or services that are being provided. Currently there are three rates:</p>
+		<ul>
+			<li>standard rate - 20 per cent</li>
+			<li>reduced rate - 5 per cent</li>
+			<li>zero rate - 0 per cent</li>
+		</ul>
+
+		<p>The standard rate of VAT is the default rate - this is the rate that's charged on most goods and services in the UK unless they're specifically identified as being reduced or zero-rated.</p>
+		
+		<h3>Examples of reduced-rated items</h3>
+		<p>These are some examples of goods and services that may be reduced-rated, depending on the product itself and the circumstances of the sale:</p>
+		<ul>
+			<li>domestic fuel and power</li>
+			<li>installing energy-saving materials</li>
+			<li>sanitary hygiene products</li>
+			<li>children's car seats</li>
+		</ul>
+
+		<p>This isn't a complete list of reduced-rated items and services.</p>
+		
+		<h3>Examples of zero-rated items</h3>
+		<p>These are examples of goods and services that may be zero-rated, depending on the product itself and the circumstances of the sale:</p>
+		<ul>
+			<li>food - but not meals in restaurants or hot takeaways</li>
+			<li>books and newspapers</li>
+			<li>children's clothes and shoes</li>
+			<li>public transport</li>
+		</ul>
+
+		<p>This isn't a full list of zero-rated items.</p>
+		<p>You can read an explanation of different VAT rates and find out what rates apply to different goods and services in our related guides below.</p>
+		<p><a href="/tax-payroll-and-company-information/vat/vat-rates-thresholds-fuel%20scales-and-exchange-rates/vat-rates-explained-standard-reduced-zero-exempt.php">VAT rates explained: standard, reduced, zero, exempt</a></p>
+		<p><a href="/tax-payroll-and-company-information/vat/charging-vat-on-your-sales-and-other-supplies/goods-and-services-where-you-have-to-charge-vat.php">Goods and services where you have to charge VAT</a></p>
+		
+		<ul>
+			<li><a href="/starting-up/vat/introduction-to-vat/who-charges-vat-and-what-vat-is-charged-on.php" title="Who charges VAT and what VAT is charged on">Prev</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/items-not-covered-by-vat.php" title="Items not covered by VAT">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/introduction-to-vat.php" title="Introduction to VAT">Introduction</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/what-is-vat.php">What is VAT?</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/who-charges-vat-and-what-vat-is-charged-on.php">Who charges VAT and what VAT is charged on</a></li>
+			<li>Rates of VAT</li>
+			<li><a href="/starting-up/vat/introduction-to-vat/items-not-covered-by-vat.php">Items not covered by VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/the-difference-between-exempt-and-zero-rated.php">The difference between exempt and zero-rated</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/find-out-more-about-vat.php">Find out more about VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/vat-glossary.php">VAT glossary</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/identify-which-vat-scheme-your-business-is-eligible-to-use/here-is-how-I-got-to-grips-with-vat.php">Here's how I got to grips with VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/more-useful-links.php">More Useful Links</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" /> 
+				HM Revenue &amp; Customs
+			</a>
+		</p>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/publications/vat-notice-70130-education-and-vocational-training" target="_blank" rel="noopener noreferrer" title="Notice 701/30 - VAT on education and vocational training">VAT education exemptions - VAT Notice 701/30 from HM Revenue &amp; Customs (HMRC)</a></li>
+			<li><a href="https://www.gov.uk/government/publications/vat-notice-742a-opting-to-tax-land-and-buildings" target="_blank" rel="noopener noreferrer" title="Opting to tax land and buildings">Opting to tax land and buildings - VAT notice 742A from HMRC</a></li>
+			<li><a href="https://www.gov.uk/government/publications/vat-notice-700-the-vat-guide" target="_blank" rel="noopener noreferrer" title="Notice 700 - guidance on VAT rules and processes, help with problems and links to more information">Guide to VAT - VAT Notice 700 from HMRC</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat.php">When to register for UK VAT </a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-rates-thresholds-fuel%20scales-and-exchange-rates/vat-rates-explained-standard-reduced-zero-exempt.php">VAT rates explained: standard, reduced, zero, exempt</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/charging-vat-on-your-sales-and-other-supplies/goods-and-services-where-you-have-to-charge-vat.php">Goods and services where you have to charge VAT</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/reclaiming-vat-on-your-purchases/exempt-and-partially-exempt-businesses-for-vat-purposes.php">Exempt and partially-exempt businesses for VAT purposes</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-rates-thresholds-fuel%20scales-and-exchange-rates/rates-of-vat-on-different-goods-and-services.php">Rates of VAT on different goods and services</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/charities-and-other-not-for-profit-organisations-and-vat/how-vat-applies-to-fundraising-events.php">How VAT applies to fundraising events</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
