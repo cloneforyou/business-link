c02d74d7cdfa977a236abcb3aa9976afcf3e4013
Caroline Hagan <info@blueocto.co.uk>
2017-05-31 21:30:01 +0100

    Completed “starting-up > vat > introduction-to-vat > Items not covered by VAT” page.

---
+++ starting-up/vat/introduction-to-vat/items-not-covered-by-vat.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Items not covered by VAT | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Introduction to VAT</h1>
+		<h2>Items not covered by VAT</h2>
+		<p>There are some items that aren't covered by VAT. These items are either:</p>
+		<ul>
+			<li>exempt</li>
+			<li>outside the scope of VAT</li>
+		</ul>
+
+		<h3>Exempt items</h3>
+		<p>Some items are exempt from VAT because the law says they mustn't have any VAT charged on them. Items that are exempt include the following:</p>
+		<ul>
+			<li>insurance</li>
+			<li>providing credit</li>
+			<li>education and training, if certain conditions are met</li>
+			<li>fundraising events by charities, if certain conditions are met</li>
+			<li>membership subscriptions, if certain conditions are met</li>
+			<li>most services provided by doctors and dentists</li>
+		</ul>
+
+		<p>Selling, leasing and letting of commercial land and buildings are also exempt from VAT.</p>
+		<p>But you can choose - or 'opt' - to give up the right to the exemption and to charge VAT at the standard rate instead. This allows you to reclaim input tax when otherwise you wouldn't be able to. For more information, see the page in this guide on <a href="/starting-up/vat/introduction-to-vat/the-difference-between-exempt-and-zero-rated.php">the difference between exempt and zero-rated</a>.</p>
+		<p><a href="https://www.gov.uk/government/publications/vat-notice-70130-education-and-vocational-training" target="_blank" rel="noopener noreferrer" title="Notice 701/30 - VAT on education and vocational training">Find out about VAT exemptions for education in VAT Notice 701/30 on the HM Revenue &amp; Customs (HMRC) website</a></p>
+		<p><a href="/tax-payroll-and-company-information/vat/charities-and-other-not-for-profit-organisations-and-vat/how-vat-applies-to-fundraising-events.php">How VAT applies to fundraising events</a>&nbsp;</p>
+		<p><a href="https://www.gov.uk/government/publications/vat-notice-742a-opting-to-tax-land-and-buildings" target="_blank" rel="noopener noreferrer" title="Opting to tax land and buildings">Find out about opting to tax land and buildings in VAT Notice 742A on the HMRC website</a></p>
+		
+		<h3>Outside the scope of VAT</h3>
+		<p>There are some things that aren't in the UK VAT system at all - they're outside the scope of VAT. They are not taxable supplies and no VAT is charged on them. Items that are outside the scope of VAT include:</p>
+		<ul>
+			<li>non-business activities like a hobby - for example, you might sell some stamps from your collection</li>
+			<li>fees that are fixed by law - known as 'statutory fees' - for example the congestion charge or vehicle MOT tests</li>
+		</ul>
+		
+		<ul>
+			<li><a href="/starting-up/vat/introduction-to-vat/rates-of-vat.php" title="Rates of VAT">Prev</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/the-difference-between-exempt-and-zero-rated.php" title="The difference between exempt and zero-rated">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/introduction-to-vat.php" title="Introduction to VAT">Introduction</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/what-is-vat.php">What is VAT?</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/who-charges-vat-and-what-vat-is-charged-on.php">Who charges VAT and what VAT is charged on</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/rates-of-vat.php">Rates of VAT</a></li>
+			<li>Items not covered by VAT</li>
+			<li><a href="/starting-up/vat/introduction-to-vat/the-difference-between-exempt-and-zero-rated.php">The difference between exempt and zero-rated</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/find-out-more-about-vat.php">Find out more about VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/vat-glossary.php">VAT glossary</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/identify-which-vat-scheme-your-business-is-eligible-to-use/here-is-how-I-got-to-grips-with-vat.php">Here's how I got to grips with VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/more-useful-links.php">More Useful Links</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" /> 
+				HM Revenue &amp; Customs
+			</a>
+		</p>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/publications/vat-notice-70130-education-and-vocational-training" target="_blank" rel="noopener noreferrer" title="Notice 701/30 - VAT on education and vocational training">VAT exemptions for education - VAT Notice 701/30 from HMRC</a></li>
+			<li><a href="https://www.gov.uk/government/publications/vat-notice-742a-opting-to-tax-land-and-buildings" target="_blank" rel="noopener noreferrer" title="Opting to tax land and buildings">Tax on land and buildings - VAT Notice 742A from HMRC</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/charities-and-other-not-for-profit-organisations-and-vat/how-vat-applies-to-fundraising-events.php">How VAT applies to fundraising events</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
