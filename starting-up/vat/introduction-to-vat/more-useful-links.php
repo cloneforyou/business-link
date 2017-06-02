8c4470d3e1601ffb480918d1ca9c9d69891c416b
Caroline Hagan <info@blueocto.co.uk>
2017-06-02 09:08:49 +0100

    Completed “starting-up > vat > introduction-to-vat > More Useful Links” page.

---
+++ starting-up/vat/introduction-to-vat/more-useful-links.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>More Useful Links | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Introduction to VAT</h1>
+		<h2>More Useful Links</h2>
+		<p><a href="../../../_files/hmrc_vat-fs1-what-you-need-to-know-about-vat.pdf" title="Find factsheet TH FS4 'VAT - what you need to know' from the HMRC website" target="_blank" rel="noopener noreferrer">Find factsheet TH FS4 'VAT - what you need to know' on the HM Revenue &amp; Customs (HMRC) website</a></p>
+		<p><a href="/tax-payroll-and-company-information/vat/vat-returns-and-accounts/when-transactions-take-place-for-vat-purposes.php">When transactions take place for VAT purposes</a></p>
+		<p><a href="#" title="The basics of VAT, when you must charge VAT and when you can reclaim it">Use our interactive e-learning tool to find out more about getting the grips with VAT and your business</a></p>
+		
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/identify-which-vat-scheme-your-business-is-eligible-to-use/here-is-how-I-got-to-grips-with-vat.php" title="Here's how I got to grips with VAT">Prev</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/introduction-to-vat.php" title="Introduction to VAT">Introduction</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/what-is-vat.php">What is VAT?</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/who-charges-vat-and-what-vat-is-charged-on.php">Who charges VAT and what VAT is charged on</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/rates-of-vat.php">Rates of VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/items-not-covered-by-vat.php">Items not covered by VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/the-difference-between-exempt-and-zero-rated.php">The difference between exempt and zero-rated</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/find-out-more-about-vat.php">Find out more about VAT</a></li>
+			<li><a href="/starting-up/vat/introduction-to-vat/vat-glossary.php">VAT glossary</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/identify-which-vat-scheme-your-business-is-eligible-to-use/here-is-how-I-got-to-grips-with-vat.php">Here's how I got to grips with VAT</a></li>
+			<li>More Useful Links</li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" /> 
+				HM Revenue &amp; Customs
+			</a>
+		</p>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="../../../_files/hmrc_vat-fs1-what-you-need-to-know-about-vat.pdf" title="Find factsheet TH FS4 'VAT - what you need to know' from the HMRC website" target="_blank" rel="noopener noreferrer">TH FS4 'VAT - what you need to know' from HMRC</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-returns-and-accounts/when-transactions-take-place-for-vat-purposes.php">When transactions take place for VAT purposes</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
