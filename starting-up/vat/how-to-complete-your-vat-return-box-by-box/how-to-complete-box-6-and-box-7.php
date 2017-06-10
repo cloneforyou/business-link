a589fd52d46ea57a7490eec968188debb9bae769
Caroline Hagan <info@blueocto.co.uk>
2017-06-10 16:30:46 +0100

    Completed ”starting-up > vat > how-to-complete-your-vat-return-box-by-box > How to complete Box 6 and Box 7” page.

---
+++ starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-box-6-and-box-7.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>How to complete Box 6 and Box 7 | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>How to complete your VAT Return box by box</h1>
+		<h2>How to complete Box 6 and Box 7</h2>
+		<h3>Box 6: your total sales excluding VAT</h3>
+		<p>Enter the total figure for your sales (excluding VAT) for the period, that is, the sales on which you charged the VAT you put in Box 1. Additionally, you should include;</p>
+		<ul>
+			<li>any zero-rated and exempt sales or other supplies you made</li>
+			<li>any amount you put in Box 8</li>
+			<li>goods or services you have <strong>supplied</strong> that are subject to the reverse charge</li>
+			<li>gold or services that you have <strong>purchased</strong> that are subject to the reverse charge</li>
+			<li>exports outside the EU</li>
+		</ul>
+
+		<p>Take off the net amount of any credit notes you issued or debit notes you received.</p>
+		<p>Don't include:</p>
+		<ul>
+			<li>loans, dividends and gifts of money</li>
+			<li>insurance claims</li>
+		</ul>
+		
+		<p><a href="/tax-payroll-and-company-information/vat/vat-rates-thresholds-fuel-scales-and-exchange-rates/vat-rates-explained-standard-reduced-zero-exempt.php">VAT rates explained: standard, reduced, zero, exempt</a></p>
+		
+		<h3>Reverse Charge Sales List</h3>
+		<p>If you make reverse charge sales - sales to which a reverse charge is applied - you must notify HM Revenue &amp; Customs (HMRC) and send in regular Reverse Charge Sales Lists (RCSL).</p>
+		<p><a href="/tax-payroll-and-company-information/vat/international-trade-international-visits-and-vat/reverse-charge-sales-list-rcsl.php">Reverse Charge Sales List (RCSL)</a></p>
+		
+		<h3>Box 7: your total purchases excluding VAT</h3>
+		<p>Enter the total figure for your purchases (excluding VAT) for the period, including:</p>
+		<ul>
+			<li>the purchases on which you paid the VAT you put in Box 4</li>
+			<li>anything you bought that the reverse charge procedure applies to</li>
+			<li>any amount you put in Box 9</li>
+		</ul>
+
+		<p>Don't include:</p>
+		<ul>
+			<li>expenses like salaries and taxes</li>
+			<li>anything outside the scope of VAT like vehicle licences, MOT certificates and local authority rates</li>
+		</ul>
+
+		<ul>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-box-1-to-box-5.php" title="How to complete Box 1 to Box 5">Prev</a></li>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-box-8-and-box-9.php" title="How to complete Box 8 and Box 9">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-returns-and-accounts/how-to-complete-your-vat-return-box-by-box.php" title="How to complete your VAT Return box by box">Introduction</a></li>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-your-vat-return.php">How to complete your VAT Return</a></li>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-box-1-to-box-5.php">How to complete Box 1 to Box 5</a></li>
+			<li>How to complete Box 6 and Box 7</li>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-box-8-and-box-9.php">How to complete Box 8 and Box 9</a></li>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/vat-rate-changes.php">VAT rate changes</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/identify-which-vat-scheme-your-business-is-eligible-to-use/here-is-how-I-got-to-grips-with-vat.php">Here's how I got to grips with VAT</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" /> 
+				HM Revenue &amp; Customs
+			</a>
+		</p>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.tax.service.gov.uk/gg/sign-in?continue=https%3A%2F%2Fonline.hmrc.gov.uk%2Fhome" target="_blank" rel="noopener noreferrer">Log in to the RCSL online service</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111206102847/http://www.hmrc.gov.uk/demo/organisation/register-and-enrol-VAT/" target="_blank" rel="noopener noreferrer">Online demonstrator from HMRC</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-to-sign-up-to-use-vat-online-services.php">How to sign up to use VAT online services</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-rates-thresholds-fuel-scales-and-exchange-rates/vat-rates-explained-standard-reduced-zero-exempt.php">VAT rates explained: standard, reduced, zero, exempt</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
