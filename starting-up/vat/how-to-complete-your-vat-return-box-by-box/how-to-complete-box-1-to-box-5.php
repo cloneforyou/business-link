651235960b1943e02b56eb84c53c360630859e46
Caroline Hagan <info@blueocto.co.uk>
2017-06-10 16:24:28 +0100

    Completed ”starting-up > vat > how-to-complete-your-vat-return-box-by-box > How to complete Box 1 to Box 5” page.

---
+++ starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-box-1-to-box-5.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>How to complete Box 1 to Box 5 | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>How to complete your VAT Return box by box</h1>
+		<h2>How to complete Box 1 to Box 5</h2>
+		<h3>Box 1: VAT due on sales and other outputs</h3>
+		<p>This is the total amount of VAT you charged on sales to customers. You must also include the VAT you need to pay to HM Revenue &amp; Customs (HMRC) for certain other supplies that you have made, such as:</p>
+		<ul>
+			<li>sales to staff - for example, canteen meals or via vending machines</li>
+			<li>sales of business assets, such as commercial vehicles or machinery</li>
+			<li>hiring or loaning of goods to someone else</li>
+			<li>commission received from selling something on behalf of someone else</li>
+			<li>goods or services, such as products or computer software, that you or your staff take out of the business for personal use</li>
+			<li>gifts or samples you gave to someone that cost you more than &pound;50</li>
+			<li>anything you bartered, exchanged or part-exchanged</li>
+			<li>any goods you <strong>bought</strong> that the reverse charge procedure applies to</li>
+			<li>fuel scale charges</li>
+		</ul>
+		<p>Take away from this total any credit notes you have issued or debit notes you have received and enter the result in Box 1.</p>
+		
+		<h3>Box 2: VAT due from you (but not paid) on acquisitions from other European Union (EU) countries</h3>
+		<p>You need to work out the VAT due - but not yet paid by you - on goods that you buy from other EU countries, and any services directly related to those goods (such as delivery charges). Put the figure in Box 2. You may be able to reclaim this amount, and if so remember to include this figure in your total in Box 4.</p>
+		
+		<h3>Box 3: total VAT due</h3>
+		<p>This is the total of box 1 and box 2 added together. It is the amount of VAT that is due to HMRC.</p>
+		<p>When you complete your return online, this figure is worked out automatically for you, based on the information you put in boxes 1 and 2. You must not enter the total VAT due figure into box 3 or any other box on your online return.</p>
+		
+		<h3>Box 4: VAT reclaimable on your purchases</h3>
+		<p>This is the VAT you have been charged on your purchases for use in your business. You should also include:</p>
+		<ul>
+			<li>VAT you paid on imports from countries outside the EU</li>
+			<li>VAT you paid on goods you removed from a bonded warehouse or free zone</li>
+			<li>VAT due (but not paid) on goods from other EU countries and services directly related to those goods (such as delivery charges) - this is the figure you put in Box 2</li>
+		</ul>
+		
+		<p>Deduct from this total:</p>
+		<ul>
+			<li>the VAT on any credit notes you have received</li>
+			<li>the VAT on any supplier invoices over six months old that you haven't paid and that you are obliged to account for as a 'bad debt'</li>
+		</ul>
+		
+		<p>The result is the total VAT you can claim back from HMRC.</p>
+		<p><a href="https://www.gov.uk/government/publications/vat-notice-70018-relief-from-vat-on-bad-debts" title="Read abiut relief from VAT on bad debts in VAT notice 700/18 on the HMRC website" target="_blank" rel="noopener noreferrer">Read about relief from VAT on bad debts in VAT Notice 700/18 on the HMRC website</a></p>
+		
+		<h3>Box 5: VAT payable or reclaimable</h3>
+		<p>When you complete your return online, the figure for this box is worked out automatically for you based on the information in boxes 3 and 4. If the amount in box 3 is more than the figure in box 4, you pay the amount in box 5 to HMRC. If the amount in box 3 is less than the figure in box 4, you reclaim the amount in box 5 from HMRC. If the amount in box 5 is zero, you have no VAT to pay or reclaim, but you must still submit your return.</p>
+		<p><a href="/tax-payroll-and-company-information/vat/charging-vat-on-your-sales-and-other-supplies/goods-and-services-where-you-have-to-charge-vat.php">Goods and services where you have to charge VAT</a></p>
+
+		<p><a href="https://www.gov.uk/government/publications/vat-notice-735-vat-reverse-charge-on-specified-goods-and-services" title="The latest VAT reverse charge information for supplies of mobile phones and computer chips" target="_blank" rel="noopener noreferrer">Find out about the reverse charge procedure in VAT Notice 735 on the HMRC website</a></p>
+
+		<ul>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-your-vat-return.php" title="How to complete your VAT Return">Prev</a></li>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-box-6-and-box-7.php" title="How to complete Box 6 and Box 7">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-returns-and-accounts/how-to-complete-your-vat-return-box-by-box.php" title="How to complete your VAT Return box by box">Introduction</a></li>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-your-vat-return.php">How to complete your VAT Return</a></li>
+			<li>How to complete Box 1 to Box 5</li>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-box-6-and-box-7.php">How to complete Box 6 and Box 7</a></li>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-box-8-and-box-9.php">How to complete Box 8 and Box 9</a></li>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/vat-rate-changes.php">VAT rate changes</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/identify-which-vat-scheme-your-business-is-eligible-to-use/here-is-how-I-got-to-grips-with-vat.php">Here's how I got to grips with VAT</a></li>
+		</ul>
+		
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" /> 
+				HM Revenue &amp; Customs
+			</a>
+		</p>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111206102847/http://www.hmrc.gov.uk/demo/organisation/register-and-enrol-VAT/" target="_blank" rel="noopener noreferrer">Online demonstrator from HMRC</a></li>
+		</ul>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-to-sign-up-to-use-vat-online-services.php">How to sign up to use VAT online services</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/charging-vat-on-your-sales-and-other-supplies/goods-and-services-where-you-have-to-charge-vat.php">Goods and services where you have to charge VAT</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/reclaiming-vat-on-your-purchases/what-you-can-and-cannot-reclaim-vat-on.php">What you can and cannot reclaim VAT on</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-returns-and-accounts.php">VAT Returns and accounts</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
