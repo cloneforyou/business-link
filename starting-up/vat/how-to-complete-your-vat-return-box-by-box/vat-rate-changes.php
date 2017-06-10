2084709b19d06ff072fe54ad991bbd4050da3723
Caroline Hagan <info@blueocto.co.uk>
2017-06-10 16:47:46 +0100

    Completed ”starting-up > vat > how-to-complete-your-vat-return-box-by-box > VAT rate changes” page.

---
+++ starting-up/vat/how-to-complete-your-vat-return-box-by-box/vat-rate-changes.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>VAT rate changes | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>How to complete your VAT Return box by box</h1>
+		<h2>VAT rate changes</h2>
+		<p>If the VAT rate changes, you complete your VAT Return in exactly the same way. If the rate changes during your return period, you calculate a total for each rate and just add them together to get the figures that go in each box.</p>
+		<p><a href="/tax-payroll-and-company-information/vat/vat-rates-thresholds-fuel-scales-and-exchange-rates/increase-in-the-standard-rate-of-vat-to-20-per-cent.php">Increase in the standard of rate of VAT to 20 per cent</a> </p>
+		<p><a href="/tax-payroll-and-company-information/vat/vat-rates-thresholds-fuel-scales-and-exchange-rates/accounting-for-vat-when-the-standard-rate-of-vat-returned-to-17-and-half-per-cent.php">Accounting for VAT when the standard rate of VAT returned to 17.5 per cent</a></p>
+
+		<ul>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-box-8-and-box-9.php" title="How to complete Box 8 and Box 9">Prev</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/identify-which-vat-scheme-your-business-is-eligible-to-use/here-is-how-I-got-to-grips-with-vat.php" title="Here's how I got to grips with VAT">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-returns-and-accounts/how-to-complete-your-vat-return-box-by-box.php" title="How to complete your VAT Return box by box">Introduction</a></li>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-your-vat-return.php">How to complete your VAT Return</a></li>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-box-1-to-box-5.php">How to complete Box 1 to Box 5</a></li>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-box-6-and-box-7.php">How to complete Box 6 and Box 7</a></li>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-box-8-and-box-9.php">How to complete Box 8 and Box 9</a></li>
+			<li>VAT rate changes</li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/identify-which-vat-scheme-your-business-is-eligible-to-use/here-is-how-I-got-to-grips-with-vat.php">Here's how I got to grips with VAT</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" /> 
+				HM Revenue &amp; Customs
+			</a>
+		</p>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111206102847/http://www.hmrc.gov.uk/demo/organisation/register-and-enrol-VAT/" target="_blank" rel="noopener noreferrer">Online demonstrator from HMRC</a></li>
+		</ul>
+		
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-rates-thresholds-fuel-scales-and-exchange-rates/vat-rates-explained-standard-reduced-zero-exempt.php">VAT rates explained: standard, reduced, zero, exempt</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
