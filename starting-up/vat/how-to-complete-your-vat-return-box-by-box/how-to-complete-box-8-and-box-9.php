3952f5cf09adc9c1a875576951fddc6dcc1486c4
Caroline Hagan <info@blueocto.co.uk>
2017-06-10 16:43:44 +0100

    Completed ”starting-up > vat > how-to-complete-your-vat-return-box-by-box > How to complete Box 8 and Box 9” page.

---
+++ starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-box-8-and-box-9.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>How to complete Box 8 and Box 9 | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>How to complete your VAT Return box by box</h1>
+		<h2>How to complete Box 8 and Box 9</h2>
+		<p>You only need to fill in Box 8 and Box 9 if you have supplied goods to or acquired goods from another European Union (EU) country.</p>
+		
+		<h3>Box 8: the total value of goods you supplied to other EU countries</h3>
+		<p>Put in the total value of goods you supplied to another EU country and services related to those goods (such as delivery charges). Please note that services related to those goods should only be included if they form part of the overall invoice total. If these, or any other services, have been invoiced separately they should not be included. You should do this for all goods that were physically removed from the UK to another EU country, even if there was no actual sale, or if the sale was invoiced to a person or organisation outside the EU.</p>
+		<p>Remember to also include this amount in your Box 6 total.</p>
+		<p>You may also have to complete an EC Sales List for supplies to VAT-registered customers in other EU countries.</p>
+		<p>You should not include in box 8 the value of goods supplied to customers (including private individuals) who are not registered for VAT where you have not exceeded the 'distance selling' threshold. Each EU country can set its own distance selling threshold which at present must be roughly the equivalent of either &euro;35,000 or &euro;100,000.</p>
+		<p><a href="/tax-payroll-and-company-information/vat/international-trade-international-visits-and-vat/ec-sales-lists-completing-and-submitting-online-or-on-paper/reporting-your-eu-sales-using-the-ec-sales-list-esl.php">Reporting your EU sales using the EC Sales List (ESL)</a></p>
+		<p>Find out about distance selling in the page on <a href="/tax-payroll-and-company-information/vat/international-trade-international-visits-and-vat/exports-supplies-of-goods-and-services-overseas-charging-vat/vat-on-sales-to-someone-who-is-not-vat-registered-in-another-eu-country.php">VAT on sales to someone who isn't VAT registered in another EU country</a> in our guide on <a href="/tax-payroll-and-company-information/vat/international-trade-international-visits-and-vat/exports-supplies-of-goods-and-services-overseas-charging-vat/exports-dispatches-supplying-goods-abroad-charging-vat.php">exports, dispatches, supplying goods abroad: charging VAT</a>.</p>
+		<p><a href="http://ec.europa.eu/taxation_customs/frequently-asked-questions/taxation_en" title="Find out about distance selling of goods on the European Commission website" target="_blank" rel="noopener noreferrer">Find the distance selling thresholds for each EU country on the European Commission website</a></p>
+		
+		<h3>Box 9: the total value of goods you acquired from other EU countries</h3>
+		<p>Enter the total value of goods you received from VAT-registered suppliers in another EU country, and services related to those goods (such as delivery charges). Please note that services related to those goods should only be included if they form part of the overall invoice total. If these, or any other services, have been invoiced separately they should not be included. You should do this for all goods that were physically shipped to you from another EU country, even if there was no actual purchase, or if the goods were invoiced to a person or organisation outside the EU.</p>
+		<p>Remember to also include this amount in your Box 7 total.</p>
+		
+		<ul>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-box-6-and-box-7.php" title="How to complete Box 6 and Box 7">Prev</a></li>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/vat-rate-changes.php" title="VAT rate changes">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-returns-and-accounts/how-to-complete-your-vat-return-box-by-box.php" title="How to complete your VAT Return box by box">Introduction</a></li>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-your-vat-return.php">How to complete your VAT Return</a></li>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-box-1-to-box-5.php">How to complete Box 1 to Box 5</a></li>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/how-to-complete-box-6-and-box-7.php">How to complete Box 6 and Box 7</a></li>
+			<li>How to complete Box 8 and Box 9</li>
+			<li><a href="/starting-up/vat/how-to-complete-your-vat-return-box-by-box/vat-rate-changes.php">VAT rate changes</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/identify-which-vat-scheme-your-business-is-eligible-to-use/here-is-how-I-got-to-grips-with-vat.php">Here's how I got to grips with VAT</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" /> 
+				HM Revenue &amp; Customs
+			</a>
+		</p>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111206102847/http://www.hmrc.gov.uk/demo/organisation/register-and-enrol-VAT/" target="_blank" rel="noopener noreferrer">Online demonstrator from HMRC</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-to-sign-up-to-use-vat-online-services.php">How to sign up to use VAT online services</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
