79e38474fa701b1ea632c26a5b6d289953bf69be
Caroline Hagan <info@blueocto.co.uk>
2017-06-10 15:38:27 +0100

    Added content I’d already completed previously.

--- _inc/nav--starting-up.php
+++ _inc/nav--starting-up.php
@@ -205,14 +205,12 @@
 				<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php">Financial and management accounts: the basics</a>
 					<ul>
 						<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php" title="Financial and management accounts: the basics">Introduction</a></li>
-						<!--
-						<li>Financial accounts</li>
-						<li>Filing financial accounts</li>
-						<li>Management accounts</li>
-						<li>Uses of management accounting</li>
-						<li>The importance of maintaining accurate accounts</li>
-						<li>False accounting</li>
-						// -->
+						<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/financial-accounts.php">Financial accounts</a></li>
+						<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/filing-financial-accounts.php">Filing financial accounts</a></li>
+						<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/management-accounts.php">Management accounts</a></li>
+						<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/uses-of-management-accounting.php">Uses of management accounting</a></li>
+						<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/the-importance-of-maintaining-accurate-accounts.php">The importance of maintaining accurate accounts</a></li>
+						<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics/false-accounting.php">False accounting</a></li>
 					</ul>
 				</li>
 				<li>
