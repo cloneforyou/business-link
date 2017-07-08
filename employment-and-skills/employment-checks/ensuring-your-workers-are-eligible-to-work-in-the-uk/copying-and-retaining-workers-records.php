402f6b0d8043b5e0194b2cee9940a51f9b44d249
Caroline Hagan <info@blueocto.co.uk>
2017-07-08 16:17:27 +0100

    Completed page "employment-and-skills > employment-checks > Ensuring your workers are eligible to work in the UK > Copying and retaining workers' records".

---
+++ employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/copying-and-retaining-workers-records.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Copying and retaining workers' records | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Ensuring your workers are eligible to work in the UK</h1>
+		<h2>Copying and retaining workers' records</h2>
+		<p>When a prospective worker gives you their relevant documents to prove that they have the right to work in the UK, you must keep a record of these documents. You must do this by either photocopying them or scanning and saving them electronically on a non-rewritable disk - eg CD-R or DVD-R.</p>
+		<p>With passports and travel documents, you must copy:</p>
+		<ul>
+			<li>the front cover</li>
+			<li>the date of expiry</li>
+			<li>any photographs and signature</li>
+			<li>all of the pages giving the prospective worker's personal details, including nationality</li>
+			<li>pages containing a UK government stamp or endorsement which allows the prospective worker to do the work you are offering</li>
+		</ul>
+		
+		<p>You should copy other documents in their entirety.</p>
+		<p>Keep copies of the documents until two years after the worker's employment ends. Having a record of every copied document will help you establish a statutory excuse if the UK Border Agency (UKBA) detects anyone working illegally for you.</p>
+		<p>If you lose any records, the UKBA may look at your normal recruitment procedures when considering if you have established a statutory excuse, eg if you have a consistent practice of copying documents for each worker.</p>
+		<p>You should not retain a person's original documents, except for the purpose of copying them. The only exceptions are:</p>
+		<ul>
+			<li>when an individual gives you a P45 as part of a combination of documents, you should retain part 2 of the document for three years to comply with HM Revenue &amp; Customs rules</li>
+			<li>when you employ someone for 24 hours or less and it is not practicable to obtain a copy of the documents, you can retain the document(s) while they work for you but you must have facilities for keeping them safe</li>
+		</ul>
+		
+		<p>You must not keep a potential worker's original documents for more than a day. If you deliberately keep, or retain without consent, a person's passport or other original documents belonging to them, you may be guilty of an offence.</p>
+		<ul>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/checking-a-prospective-workers-entitlement-to-work-in-the-uk.php" title="Checking a prospective worker's entitlement to work in the UK">Prev</a></li>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/compliance-checks-and-penalties-for-immigration-offences.php" title="Compliance checks and penalties for immigration offences">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk.php" title="Ensuring your workers are eligible to work in the UK">Introduction</a></li>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/checking-a-prospective-workers-entitlement-to-work-in-the-uk.php">Checking a prospective worker's entitlement to work in the UK</a></li>
+			<li>Copying and retaining workers' records</li>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/compliance-checks-and-penalties-for-immigration-offences.php">Compliance checks and penalties for immigration offences</a></li>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/here-is-how-migrant-workers-contribute-to-our-workforce.php">Here's how migrant workers contribute to our workforce</a></li>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/here-is-how-we-check-the-entitlement-of-our-staff-to-work-in-the-uk.php">Here's how we check the entitlement of our staff to work in the UK</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>UKBA Sponsorship and Employers' Helpline</h3>
+		<p>0300 123 4699</p>
+
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/employment-and-skills/employment-checks/check-the-eligibility-of-your-workers.php">Use our interactive tool to check if your workers are eligible to work in the UK</a></li>
+			<li><a href="https://www.gov.uk/check-job-applicant-right-to-work" title="Information for employers to ensure you only employ legitimate workers and the penalties for employing those not entitled to work in the UK" target="_blank" rel="noopener noreferrer">Illegal working prevention information on the UKBA website</a></li>
+			<li><a href="../../../_files/home-office_anti-discrimination-code_2008.pdf" title="How to prevent illegal working, including checking documents in List A or B" target="_blank" rel="noopener noreferrer">Download the code of practice on preventing unlawful discrimination when checking the right to work in the UK from the UKBA website (PDF, 199K)</a></li>
+			<li><a href="/employment-and-skills/take-on-staff/help-with-recruitment.php">Use our interactive tools to help you take on a new employee</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/employment-and-skills/equality-and-diversity/prevent-discrimination-and-value-diversity.php">Prevent discrimination and value diversity</a></li>
+			<li><a href="/employment-and-skills/staff-documents-and-employment-policies/the-employment-contract.php">The employment contract</a></li>
+			<li><a href="/employment-and-skills/staff-documents-and-employment-policies/keep-the-right-staff-records.php">Keep the right staff records</a></li>
+			<li><a href="/employment-and-skills/advertise-a-job/advertising-a-job-and-interviewing-candidates.php">Advertising a job and interviewing candidates</a></li>
+			<li><a href="/your-business-sector/farming/starting-up/starting-up-in-farming/employing-farm-workers.php" title="Regulations and sources of advice for agricultural sector employers when starting up a farm business">Employing farm workers</a></li>
+			<li><a href="/your-business-sector/farming/managing-farm-employees/gangmasters-licensing.php">Gangmasters licensing</a></li>
+			<li><a href="https://www.gov.uk/employing-staff" title="Find information on how to take on staff in your business" target="_blank" rel="noopener noreferrer">Take on staff</a><!-- http://webarchive.nationalarchives.gov.uk/20111208124450/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097102011&r.i=1073791948&r.l1=1073858787&r.l2=1073858914&r.l3=1084607697&r.l4=1073981874&r.t=RESOURCES&type=HUBCHASSIS --></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/uk-visas-and-immigration" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/logo_uk_border_agency.gif" width="55" height="35" alt="" /> 
+				UK Border Agency
+			</a>
+		</p>
+
+		<img src="../../../_img/px_employingpeople.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
