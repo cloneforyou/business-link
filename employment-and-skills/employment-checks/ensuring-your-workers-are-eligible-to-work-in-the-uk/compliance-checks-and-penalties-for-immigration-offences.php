f142c52818845642397316e7b2f0a7da8421c59a
Caroline Hagan <info@blueocto.co.uk>
2017-07-08 16:22:13 +0100

    Completed page "employment-and-skills > employment-checks > Ensuring your workers are eligible to work in the UK > Compliance checks and penalties for immigration offences".

---
+++ employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/compliance-checks-and-penalties-for-immigration-offences.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Compliance checks and penalties for immigration offences | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Ensuring your workers are eligible to work in the UK</h1>
+		<h2>Compliance checks and penalties for immigration offences</h2>
+		<p>Police and immigration officers have certain powers to enter business premises where they believe an immigration offence is being committed.</p>
+		<p>For the more serious immigration offences, an employer may be fined and/or prosecuted.</p>
+		
+		<h3>Compliance checks of sponsor businesses</h3>
+		<p>If you are applying to become a licensed sponsor under the points-based system, the UK Border Agency (UKBA) may carry out checks before making a decision on your sponsorship application. You may also be visited by one of their visiting officers as part of this process.</p>
+		<p>The UKBA may also conduct checks and/or visits after you have been granted a sponsor licence.</p>
+		<p>Such checks/visits are carried out to make sure that the information you have given on the sponsorship application is accurate and that you are fulfilling all of the duties required of you as a licensed sponsor.</p>
+		<p>For more information, <a href="https://www.gov.uk/government/collections/sponsorship-information-for-employers-and-educators" title="Guidance for businesses wanting to employ a migrant under Tiers 2 to 5 of the points-based system" target="_blank" rel="noopener noreferrer">read guidance on sponsor applications on the UKBA website</a>.</p>
+		<p>See our guide on <a href="/employment-and-skills/different-types-of-worker/employing-migrants-an-overview.php">employing migrants - an overview</a>.</p>
+		
+		<h3>Compliance checks for immigration offences</h3>
+		<p>The police and immigration officers have the power to arrest and remove an individual who:</p>
+		<ul>
+			<li>has entered the UK unlawfully</li>
+			<li>no longer has the right to be in the UK</li>
+		</ul>
+		
+		<p>The police and immigration officers also have the power to:</p>
+		<ul>
+			<li>enter premises, including business premises, to arrest such individuals</li>
+			<li>search, and in some cases seize, personnel records</li>
+		</ul>
+		
+		<p>However, this only applies when the individual is working and the police and immigration officers reasonably believe that the individual and/or their employer have committed an immigration offence.</p>
+		<p><a href="https://www.gov.uk/government/publications/chapters-16-to-18-powers-and-prosecutions" title="Guidance and information on officers dealing with the enforcement of immigration matters" target="_blank" rel="noopener noreferrer">Find guidance on immigration officers' powers when seeking to enter business premises on the UKBA website</a>.</p>
+		<p>You can also <a href="https://www.gov.uk/government/organisations/uk-visas-and-immigration/about/complaints-procedure" title="Information on how to complain or comment on the service of the UK Border Agency" target="_blank" rel="noopener noreferrer">read about how to complain about the conduct of an immigration officer on the UKBA website</a>.</p>
+		
+		<h3>Penalties for immigration offences</h3>
+		<p>If they are found to have committed an immigration offence, employers may face any or a combination of the sanctions below. They may:</p>
+		<ul>
+			<li>receive a written warning or a fine (known as a 'civil penalty') for employing an illegal worker</li>
+			<li>be prosecuted and fined for employing an unregistered A8 national prior to 1 April 2011 or an unauthorised A2 national with no permission to work in the UK</li>
+			<li>be prosecuted for facilitation/trafficking</li>
+			<li>be prosecuted for the procuring/use of fraudulent identity documents</li>
+			<li>be prosecuted for knowingly employing an illegal migrant worker</li>
+			<li>be disbarred as a company director/officer as a result of prosecution</li>
+		</ul>
+		
+		<p>For more information on A8 and A2 nationals, see our guide on <a href="/employment-and-skills/different-types-of-worker/employing-migrants-an-overview.php">employing migrants - an overview</a>.</p>
+		<p>If the employer is a sponsor under the points-based system, they may also:</p>
+		<ul>
+			<li>be downgraded on the points-based system sponsorship register</li>
+			<li>have their sponsor licence cancelled and be removed from the points-based system sponsorship register</li>
+		</ul>
+		
+		<ul>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/copying-and-retaining-workers-records.php" title="Copying and retaining workers' records">Prev</a></li>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/here-is-how-migrant-workers-contribute-to-our-workforce.php" title="Here's how migrant workers contribute to our workforce">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk.php" title="Ensuring your workers are eligible to work in the UK">Introduction</a></li>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/checking-a-prospective-workers-entitlement-to-work-in-the-uk.php">Checking a prospective worker's entitlement to work in the UK</a></li>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/copying-and-retaining-workers-records.php">Copying and retaining workers' records</a></li>
+			<li>Compliance checks and penalties for immigration offences</li>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/here-is-how-migrant-workers-contribute-to-our-workforce.php">Here's how migrant workers contribute to our workforce</a></li>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/here-is-how-we-check-the-entitlement-of-our-staff-to-work-in-the-uk.php">Here's how we check the entitlement of our staff to work in the UK</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>UKBA Sponsorship and Employers' Helpline</h3>
+		<p>0300 123 4699</p>
+
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/collections/sponsorship-information-for-employers-and-educators" title="Guidance for businesses wanting to employ a migrant under Tiers 2 to 5 of the points-based system" target="_blank" rel="noopener noreferrer">Sponsor application guidance on the UKBA website</a></li>
+			<li><a href="https://www.gov.uk/government/publications/chapters-16-to-18-powers-and-prosecutions" title="Guidance and information on officers dealing with the enforcement of immigration matters" target="_blank" rel="noopener noreferrer">Immigration officer enforcement information on the UK Border Agency website</a></li>
+			<li><a href="https://www.gov.uk/government/organisations/uk-visas-and-immigration/about/complaints-procedure" title="Information on how to complain or comment on the service of the UK Border Agency" target="_blank" rel="noopener noreferrer">Immigration officer complaint guidance on the UK Border Agency website</a></li>
+			<li><a href="/employment-and-skills/employment-checks/check-the-eligibility-of-your-workers.php">Use our interactive tool to check if your workers are eligible to work in the UK</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/your-business-sector/farming/managing-farm-employees/gangmasters-licensing.php">Gangmasters licensing</a></li>
+			<li><a href="https://www.gov.uk/employing-staff" title="Find information on how to take on staff in your business" target="_blank" rel="noopener noreferrer">Take on staff</a><!-- http://webarchive.nationalarchives.gov.uk/20111208124450/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097102011&r.i=1073791948&r.l1=1073858787&r.l2=1073858914&r.l3=1084607697&r.l4=1073981874&r.t=RESOURCES&type=HUBCHASSIS --></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/uk-visas-and-immigration" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/logo_uk_border_agency.gif" width="55" height="35" alt="" /> 
+				UK Border Agency
+			</a>
+		</p>
+
+		<img src="../../../_img/px_employingpeople.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
