80a13d1772c7023ded2b52fefc27c03278a53815
Caroline Hagan <info@blueocto.co.uk>
2017-07-13 15:56:57 +0100

    Completed page "employment-and-skills > employment-checks > Ensuring your workers are eligible to work in the UK > Here's how migrant workers contribute to our workforce".

---
+++ employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/here-is-how-migrant-workers-contribute-to-our-workforce.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Here's how migrant workers contribute to our workforce | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Here's how migrant workers contribute to our workforce</h1>
+		<!-- <h2>Here's how migrant workers contribute to our workforce</h2> -->
+		<p>Based in Norfolk, Bernard Matthews Ltd is the largest and most successful turkey producer in Europe. As the company grew and expanded into new product areas, the need for labour increased dramatically, but labour shortages in the local area posed a real problem. Development services manager Steve Grant explains how recruiting migrant workers to bridge the gap has since evolved into an on-going employment diversity programme that has brought many business benefits.</p>
+		
+		<h2>What I did</h2>
+		<h3>Assess the business' needs</h3>
+		<p>"Companies of any size can face recruitment problems. In our case, rapid growth during the 1980s, plus the labour-intensive nature of the business, meant that we needed to recruit large numbers of workers.</p>
+		<p>"However, that was easier said than done - East Anglia has a mostly rural population spread over a very wide area and we also faced intense competition for labour from agricultural businesses in the area. Plus we had the added challenge of seasonality, meaning that we needed a large number of temporary workers to cope with the surge in demand around Christmas.</p>
+		<p>"We tried several solutions, including setting up our own bus service to bring in workers from all over the region, but we were still experiencing shortages. One obvious solution was to bring in workers from overseas, so despite some initial reservations, a decision was taken to explore that option."</p>
+		
+		<h3>Introduce proper processes</h3>
+		<p>"We went down the agency route to get us started. While that works fine for many businesses, we soon found that we wanted greater continuity among our migrant workforce.</p>
+		<p>"We therefore started researching ways to do the recruiting in-house. We consulted existing members of the workforce, contacted recruitment consultants overseas and gathered information from other businesses. We also worked closely with our local Jobcentre Plus, who provided excellent advice and contacts.</p>
+		<p>"There was a lot of trial and error involved, but we now have a streamlined recruitment process that starts before our migrant workers even set foot in the UK. We provide them with recruitment information and interviews in their own language, then help them to set up bank accounts and arrange accommodation before they arrive.</p>
+		<p>"On arrival we arrange inductions and language training with assistance from Learndirect, as well as providing a welcome pack incorporating practical information about local facilities like schools and transport, plus contact details of any local clubs or groups from their country of origin. We also have dedicated members of staff who liaise with the workers on a regular basis."</p>
+		
+		<h3>Maximise the benefits</h3>
+		<p>"When we started, I would never have dreamt how far the process would take us. Almost a third of our UK workforce is now made up of migrant workers, mostly Portuguese, but overall comprising nationals from over 30 different countries.</p>
+		<p>"Through careful selection and best-practice employment policies, our migrant workers consistently provide high standards of work, plus we have developed relationships with many of them meaning they come back to us year in, year out, when we need help over our busiest periods. This reduces our training costs, and reassures us that our temporary workers are already familiar with company policies and procedures.</p>
+		<p>"Employing migrant workers has also been beneficial for our company profile - for example we have been recognised by the Department for Business, Innovation &amp; Skills as a best-practice employer, and we also won a Business in the Community Award in recognition of the work we do to help the workers integrate into the local community."</p>
+		
+		<h2>What I'd do differently</h2>
+		<h3>Make the decision sooner</h3>
+		<p>"Looking back, we should have made the decision to employ migrant workers as soon as we started experiencing labour shortages. We were perhaps a bit too cautious to start with."</p>
+		<h3>Understand cultural differences better</h3>
+		<p>"Over the years, we've built up quite a deep understanding of the cultural differences between various countries and nationalities, but at the outset we didn't have that insight. More research would have helped us avoid a few culture clashes in the early days."</p>
+		<ul>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/compliance-checks-and-penalties-for-immigration-offences.php" title="Compliance checks and penalties for immigration offences">Prev</a></li>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/here-is-how-we-check-the-entitlement-of-our-staff-to-work-in-the-uk.php" title="Here's how we check the entitlement of our staff to work in the UK">Next</a></li>
+		</ul>
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk.php" title="Ensuring your workers are eligible to work in the UK">Introduction</a></li>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/checking-a-prospective-workers-entitlement-to-work-in-the-uk.php">Checking a prospective worker's entitlement to work in the UK</a></li>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/copying-and-retaining-workers-records.php">Copying and retaining workers' records</a></li>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/compliance-checks-and-penalties-for-immigration-offences.php">Compliance checks and penalties for immigration offences</a></li>
+			<li>Here's how migrant workers contribute to our workforce</li>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/here-is-how-we-check-the-entitlement-of-our-staff-to-work-in-the-uk.php">Here's how we check the entitlement of our staff to work in the UK</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<div class="media-object stack-for-small">
+			<div class="media-object-section">
+				<div class="thumbnail">
+					<img src="../../../_img/bm04d.jpg" width="181" height="121" alt="Steve Grant, Bernard Matthews Ltd" />
+				</div>
+			</div>
+			<div class="media-object-section">
+				<h3>
+					Steve Grant<br />
+					<a href="http://www.bernardmatthews.com/" title="A case study about Bernard Matthews">Bernard Matthews Ltd</a><br />
+					Steve's top tips:
+				</h3>
+				<ul>
+					<li>"Recognise the need to support the workers - you'll end up with a far more productive, loyal and motivated workforce than if you leave them to fend for themselves."</li>
+					<li>"Make your local Jobcentre Plus the first port of call for help and advice."</li>
+					<li>"Get the proper processes in place before you start recruiting - it's not as complicated as you think and will save time later."</li>
+				</ul>
+			</div>
+		</div>
+
+		<p><a href="/find-a-case-study.php">Find a case study</a></p>
+		
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/uk-visas-and-immigration" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/logo_uk_border_agency.gif" width="55" height="35" alt="" /> 
+				UK Border Agency
+			</a>
+		</p>
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
