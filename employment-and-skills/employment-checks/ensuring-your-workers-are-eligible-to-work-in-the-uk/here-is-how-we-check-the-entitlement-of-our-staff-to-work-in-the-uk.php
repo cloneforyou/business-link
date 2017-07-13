6c04a32c8ccf66c1b00d0c170bbe67b8259ca818
Caroline Hagan <info@blueocto.co.uk>
2017-07-13 16:00:42 +0100

    Completed page "employment-and-skills > employment-checks > Ensuring your workers are eligible to work in the UK > Here's how we check the entitlement of our staff to work in the UK".

---
+++ employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/here-is-how-we-check-the-entitlement-of-our-staff-to-work-in-the-uk.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Here's how we check the entitlement of our staff to work in the UK | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Here's how we check the entitlement of our staff to work in the UK</h1>
+		
+		<p>Founded in 1991, D&amp;D London Ltd is one of the UK's most dynamic restaurant groups, owning and managing some of London's most popular restaurants, such as Quaglino's and Bluebird. More than three quarters of the company's employees are non-UK nationals, a choice that reflects the company's policy to employ an ethnically and culturally diverse workforce. Here, human resources manager Holly Lowes-Bond explains the processes the company has developed to ensure employees are eligible to work in the UK.</p>
+		
+		<h2>What I did</h2>
+		<h3>Understand responsibilities</h3>
+		<p>"We regularly receive and encourage job applications from non-UK nationals, partly because it's a cultural tradition in the restaurant industry, but also because our mission to employ the best staff means we cast our recruitment net as wide as possible. However, D&amp;D London has a zero tolerance policy on illegal working, so it's vital to have appropriate procedures in place.</p>
+		<p>"As an employer, you have to be completely clear about your responsibilities. Our policy and guidelines have evolved over several years, but in principle we follow Home Office guidance to ensure that we comply with the law. The Home Office UK Border Agency website provides plenty of information and advice to help you to get started."</p>
+		
+		<h3>Be methodical</h3>
+		<p>"Our checking process begins at the point of application. We require every applicant to fill in and sign our application form, acknowledging that they have answered all questions honestly.</p>
+		<p>"When an employee attends an interview, we work through a comprehensive checklist we've developed. The first thing we ask for is visual ID, usually a passport, and any work permits held. We will occasionally conduct an interview without seeing the documents - for example when someone walks in off the street - but we always insist on the documents being produced before the employee starts work.</p>
+		<p>"We also satisfy ourselves as far as possible that the documents are genuine and belong to the holder. For example, we use a UV light - similar to those used for checking banknotes - to scan passports. We also use a website, documentchecker.com, which provides clear visuals and instructions on what to look for in passports and permits from various countries. Other measures include checking expiry dates and signatures, checking that the photo matches the person, checking that the date of birth on documents is consistent with the person's appearance and checking that the plastic seal around the photo doesn't appear to have been tampered with.</p>
+		<p>"Once the interviewer has completed the checklist and made a decision to employ, they sign the checklist to acknowledge that they are satisfied with the person's eligibility to work."</p>
+		
+		<h3>Keep records</h3>
+		<p>"We make a copy of all the relevant pages of each document for our files. We also make a note of whether an employee needs to join the Home Office Worker Registration Scheme and we ask the employee to sign a document stating that they understand their requirement to register and the possible consequences of failing to do so. It's important to provide clear explanations before they sign.</p>
+		<p>"We also make use of the reminders in Outlook to note key dates, such as visa expiration, so that we can remind employees of any action they need to take in order to remain employed by us. It's helpful for them and helps us avoid disruptions.</p>
+		<p>"We conduct a basic document check every six months and a full ID audit once a year, when employees are required to bring in their ID, which managers check against our records. It's a useful discipline for keeping records up to date, and gives us an opportunity to review procedures and incorporate key lessons."</p>
+		
+		<h2>What I'd do differently</h2>
+		<h3>Don't delegate too much responsibility</h3>
+		<p>"While it's important to make all staff aware of employment policies, in the early days we were sometimes guilty of delegating too far down the line. These days we make sure that ultimate responsibility remains with the general manager of each business, even though other staff may contribute to the checking process."</p>
+		
+		<h3>Introduce checklists sooner</h3>
+		<p>"We've found that using a checklist is an invaluable tool, ensuring that nothing gets overlooked during the employment process. With the benefit of hindsight, we should have introduced them right from the start."</p>
+		
+		<ul>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/here-is-how-migrant-workers-contribute-to-our-workforce.php" title="Here's how migrant workers contribute to our workforce">Prev</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk.php" title="Ensuring your workers are eligible to work in the UK">Introduction</a></li>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/checking-a-prospective-workers-entitlement-to-work-in-the-uk.php">Checking a prospective worker's entitlement to work in the UK</a></li>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/copying-and-retaining-workers-records.php">Copying and retaining workers' records</a></li>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/compliance-checks-and-penalties-for-immigration-offences.php">Compliance checks and penalties for immigration offences</a></li>
+			<li><a href="/employment-and-skills/employment-checks/ensuring-your-workers-are-eligible-to-work-in-the-uk/here-is-how-migrant-workers-contribute-to-our-workforce.php">Here's how migrant workers contribute to our workforce</a></li>
+			<li>Here's how we check the entitlement of our staff to work in the UK</li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<div class="media-object stack-for-small">
+			<div class="media-object-section">
+				<div class="thumbnail">
+					<img src="../../../_img/DD3d.jpg" width="181" height="121" alt="Holly Lowes-Bond, D&D London Ltd" />
+				</div>
+			</div>
+			<div class="media-object-section">
+				<h3>
+					Holly Lowes-Bond<br />
+					<a href="http://www.danddlondon.com/" title="A case study for a London restaurant">D&amp;D London Ltd</a><br />
+					Holly's top tips:
+				</h3>
+				<ul>
+					<li>"Make use of the Home Office UK Border Agency Employers' Helpline - it's free and will point you in the right direction."</li>
+					<li>"Make communication with staff as clear and simple as possible and provide training if necessary."</li>
+					<li>"Apply the same procedures to all employees, not just to non-UK nationals. It's good practice and can help to quash any suggestions of discriminatory treatment."</li>
+				</ul>
+			</div>
+		</div>
+
+
+		<p><a href="/find-a-case-study.php">Find a case study</a></p>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/uk-visas-and-immigration" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/logo_uk_border_agency.gif" width="55" height="35" alt="" /> 
+				UK Border Agency
+			</a>
+		</p>
+
+ 
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
