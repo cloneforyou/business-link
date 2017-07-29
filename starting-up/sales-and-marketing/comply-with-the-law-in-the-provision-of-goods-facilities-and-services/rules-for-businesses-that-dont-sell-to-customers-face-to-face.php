bb0f313313ee8ec5b0c4ad784679c0db372d20fa
Caroline Hagan <info@blueocto.co.uk>
2017-07-29 12:46:58 +0100

    Completed sales-and-marketing > selling-and-the-law > comply-with-the-law-in-the-provision-of-goods-facilities-and-services > "Rules for businesses that don't sell to customers face to face" page. Including global link updates.

---
+++ starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/rules-for-businesses-that-dont-sell-to-customers-face-to-face.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Rules for businesses that don't sell to customers face to face | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Comply with the law in the provision of goods, facilities and services</h1>
+		
+		<h2>Rules for businesses that don't sell to customers face to face</h2>
+		<p>If you sell to consumers - as opposed to other traders - without seeing them face-to-face, there are additional distance selling rules that you must stick to.</p>
+		<p>When communicating with your customers, you must clearly inform them about:</p>
+		<ul>
+			<li>your business name</li>
+			<li>its place of registration, registered number and registered office address</li>
+			<li>your postal address (when advance payment is required)</li>
+			<li>the goods or services (including a description)</li>
+			<li>prices (including taxes)</li>
+			<li>payment arrangements</li>
+			<li>arrangements for delivery for goods or performance in the case of services</li>
+			<li>the right to cancel during the 'cooling-off' period</li>
+			<li>who is responsible for postage/delivery of returned goods</li>
+			<li>how long prices are valid</li>
+			<li>minimum duration of contracts</li>
+		</ul>
+		
+		<p>Once an order has been placed you must send confirmation to the customer by post, email or fax. You must also make it clear if you intend to substitute unavailable goods with similar items.</p>
+		<p>You must provide key information, such as pricing and the right to cancel, before the order is placed. You must also confirm all the required information in writing, at the latest when the goods are delivered or before the service you are providing comes to an end.</p>
+		<p>The customer can cancel the agreement up to seven days after the goods are delivered or the service contract is agreed. Special rules apply if the written information has not been provided, or if the customer agrees to the service starting before this cooling-off period is over.</p>
+		<p>Delivery of the goods or the beginning of the performance of a service must be within 30 days from the day after the consumer sent the order to you, unless otherwise agreed. The customer must be informed if you are unable to meet this deadline - they are not obliged to agree to a revised date and can cancel the contract. Any money paid must be refunded within 30 days of cancellation of the contract.</p>
+		<p>Customers who enter contracts lasting more than a year must be given details of how the agreement can be terminated.</p>
+		<p>See our guide on <a href="/sales-and-marketing/online-marketing/distance-selling-and-online-trading.php" title="Information on the rules applying to businesses that sell goods or services to consumers at a distance or online">distance selling and online trading</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/getting-paid-on-time.php" title="Getting paid on time">Prev</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-to-customers-in-their-home-or-workplace.php" title="Special rules for businesses that sell to customers in their home or workplace">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/selling-and-the-law/comply-with-the-law-in-the-provision-of-goods-facilities-and-services.php" title="Comply with the law in the provision of goods, facilities and services">Introduction</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/supplying-satisfactory-and-safe-goods-and-services.php">Supplying satisfactory and safe goods and services</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/how-equality-law-applies-to-the-provision-of-goods-facilities-and-services.php">How equality law applies to the provision of goods, facilities and services</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/customers-and-data-protection.php">Customers and data protection</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/getting-paid-on-time.php">Getting paid on time</a></li>
+			<li>Rules for businesses that don't sell to customers face to face</li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-to-customers-in-their-home-or-workplace.php">Special rules for businesses that sell to customers in their home or workplace</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-electronic-and-electrical-goods.php">Special rules for businesses that sell electronic and electrical goods</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/the-proof-of-age-standards-scheme.php">The Proof of Age Standards Scheme</a></li>
+		</ul>
+		
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.bis.gov.uk/policies/consumer-issues/buying-and-selling/distance-selling" target="_blank" rel="noopener noreferrer" title="Regulations covering sales to consumers without a face-to-face meeting and online sales to businesses or consumers">Distance selling and e-commerce rules on the Department for Business, Innovation &amp; Skills website</a></li>
+			<li><a href="http://online.businesslink.gov.uk/bdotg/action/licenceSIMLandingPage" title="Use our licence tool to find out which licences and permits could apply to your business" target="_blank" rel="noopener noreferrer">Use our licence and permit checklist to find out which licences and permits could apply to your business</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/online-marketing/distance-selling-and-online-trading.php">Distance selling and online trading</a></li>
+			<li><a href="/sales-and-marketing/selling-and-the-law/buyers-terms-and-conditions-and-unfair-contract-terms.php">Buyers' terms and conditions and unfair contract terms</a></li>
+			<li><a href="/sales-and-marketing/selling-and-the-law/approved-codes-and-self-regulation.php">Approved codes and self regulation</a></li>
+			<li><a href="/sales-and-marketing/pricing/competing-fairly.php">Competing fairly</a></li>
+		</ul>
+		
+		<p>
+			Developed with:<br />
+			<a href="http://webarchive.nationalarchives.gov.uk/20130403014324/http://oft.gov.uk/" target="_blank" rel="noopener noreferrer" title="Office of Fair Trading - Opens in a new window">
+				<img src="../../../_img/OFT_unagreed.gif" width="130" height="32"  alt="" /> Office of Fair Trading
+			</a>
+			<br />
+			<a href="https://www.businesscompanion.info/" target="_blank" rel="noopener noreferrer" title="Trading Standards - Opens in a new window">
+				<img src="../../../_img/TS_unagreed.gif" width="24" height="32"  alt="" /> Trading Standards
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+		
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
