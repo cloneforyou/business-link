307920dfa04acc9af52ae27ec58e5adcd3eaace5
Caroline Hagan <info@blueocto.co.uk>
2017-07-29 12:10:00 +0100

    Completed sales-and-marketing > selling-and-the-law > comply-with-the-law-in-the-provision-of-goods-facilities-and-services > "Supplying satisfactory and safe goods and services" page.

---
+++ starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/supplying-satisfactory-and-safe-goods-and-services.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Supplying satisfactory and safe goods and services | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Comply with the law in the provision of goods, facilities and services</h1>
+		
+		<h2>Supplying satisfactory and safe goods and services</h2>
+		<p>The Sale of Goods Act and the Supply of Goods and Services Act make it a legal requirement for every business to ensure that the products or services it sells meet minimum standards.</p>
+		<p><b>All goods</b> you sell must:</p>
+		<ul>
+			<li>correspond with any description given - verbally, in writing or in an illustration</li>
+			<li>be of satisfactory quality</li>
+			<li>be fit for their purpose</li>
+		</ul>
+
+		<p><b>All services</b> you supply must be carried out:</p>
+		<ul>
+			<li>with reasonable care and skill</li>
+			<li>within a reasonable time and for a reasonable charge, unless these matters have previously been agreed with the customer</li>
+		</ul>
+
+		<p>Service providers must also provide specific information such as their business name and address and details of the service to be provided. See our guide on <a href="/sales-and-marketing/selling-and-the-law/customer-protection.php" title="An introduction to the rights customers have when buying goods or services from you">customer protection</a>.</p>
+		<p>Customers who are unhappy with goods may be able to claim compensation, while in other circumstances they may be able to ask for repair or replacement of the goods.</p>
+		<p>If any services you provide don't meet the requirements specified above you will be in breach of contract and customers may well be entitled to compensation.</p>
+		<p>Your responsibility to your customers can last for a maximum of six years, dependent on the nature of the goods or services (five years from discovery of the problem in Scotland).</p>
+		<p>For more information, see our guide on <a href="/sales-and-marketing/selling-and-the-law/the-sale-of-goods-act.php" title="Keep on the right side of the law when selling to your customers">the Sale of Goods Act</a>.</p>
+		<p>The Consumer Protection from Unfair Trading Regulations give consumers the right not to be misled by the actions or omissions of traders. They also protect consumers from aggressive or high-pressure selling techniques.</p>
+		<p>Business customers are protected by rules that prohibit misleading advertising.</p>
+		<p><a href="../../../_files/berr_the-consumer-protection-from-unfair-trading-regulations-a-basic-guide-for-business_mar-2008.pdf" title="Practical advice on how the Consumer Protection from Unfair Trading Regulations apply to your business" target="_blank" rel="noopener noreferrer">Download a basic guide to the Consumer Protection from Unfair Trading Regulations from the Department for Business, Innovation &amp; Skills (BIS) website (PDF, 574K)</a>.</p>
+		<p>If anyone incurs damage to their private property, is injured or killed as a result of using goods you have supplied you can be sued under product liability law. See our guide on <a href="/workplace-health-and-safety/managing-the-welfare-of-people/product-liability.php" title="Your legal responsibility to make sure your products are sufficiently safe">product liability</a>.</p>
+		<p>Your local Trading Standards service monitors and enforces product safety and day-to-day fair-trading practices. <a href="http://webarchive.nationalarchives.gov.uk/20111006195104/https://www.businesscompanion.info/advice/advice-business.cfm" target="_blank" rel="noopener noreferrer">Find your local Trading Standards office on the Trading Standards Institute (TSI) website</a>.</p>
+		<ul>
+			<li><a href="/sales-and-marketing/selling-and-the-law/comply-with-the-law-in-the-provision-of-goods-facilities-and-services.php" title="Comply with the law in the provision of goods, facilities and services">Prev</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/how-equality-law-applies-to-the-provision-of-goods-facilities-and-services.php" title="How equality law applies to the provision of goods, facilities and services">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/selling-and-the-law/comply-with-the-law-in-the-provision-of-goods-facilities-and-services.php" title="Comply with the law in the provision of goods, facilities and services">Introduction</a></li>
+			<li>Supplying satisfactory and safe goods and services</li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/how-equality-law-applies-to-the-provision-of-goods-facilities-and-services.php">How equality law applies to the provision of goods, facilities and services</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/customers-and-data-protection.php">Customers and data protection</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/getting-paid-on-time.php">Getting paid on time</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/rules-for-businesses-that-dont-sell-to-customers-face-to-face.php">Rules for businesses that don't sell to customers face to face</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-to-customers-in-their-home-or-workplace.php">Special rules for businesses that sell to customers in their home or workplace</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-electronic-and-electrical-goods.php">Special rules for businesses that sell electronic and electrical goods</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/the-proof-of-age-standards-scheme.php">The Proof of Age Standards Scheme</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/organisations/office-of-fair-trading" title="Your customers' legal rights when they buy goods from you" target="_blank" rel="noopener noreferrer">The Sale of Goods Act on the Office of Fair Trading website</a></li>
+			<li><a href="https://www.gov.uk/government/topics/consumer-rights-and-issues" title="How to comply and avoid unfair commercial practices" target="_blank" rel="noopener noreferrer">Unfair commercial practices advice on the BIS website</a></li>
+			<li><a href="../../../_files/berr_the-consumer-protection-from-unfair-trading-regulations-a-basic-guide-for-business_mar-2008.pdf" title="Practical advice on how the Consumer Protection from Unfair Trading Regulations apply to your business" target="_blank" rel="noopener noreferrer">Download Consumer Protection from Unfair Trading Regulations guidance from the BIS website (PDF, 574K)</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111006195104/https://www.businesscompanion.info/advice/advice-business.cfm" target="_blank" rel="noopener noreferrer">Business guidance and Trading Standards office search on the TSI website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/selling-and-the-law/the-sale-of-goods-act.php">The Sale of Goods Act</a></li>
+			<li><a href="/sales-and-marketing/selling-and-the-law/returns-and-refunds-warranties-and-complaints.php">Returns and refunds, warranties and complaints</a></li>
+			<li><a href="/sales-and-marketing/selling-and-the-law/approved-codes-and-self-regulation.php">Approved codes and self regulation</a></li>
+			<li><a href="/sales-and-marketing/selling-and-the-law/buyers-terms-and-conditions-and-unfair-contract-terms.php">Buyers' terms and conditions and unfair contract terms</a></li>
+			<li><a href="/sales-and-marketing/selling-and-the-law/customer-protection.php">Customer protection</a></li>
+			<li><a href="/sales-and-marketing/selling-overseas/ce-marking.php">CE marking</a></li>
+			<li><a href="/sales-and-marketing/pricing/competing-fairly.php">Competing fairly</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+		<p>
+			Developed with:<br />
+			<a href="http://webarchive.nationalarchives.gov.uk/20130403014324/http://oft.gov.uk/" target="_blank" rel="noopener noreferrer" title="Office of Fair Trading - Opens in a new window">
+				<img src="../../../_img/OFT_unagreed.gif" width="130" height="32"  alt="" /> Office of Fair Trading
+			</a>
+			<br />
+			<a href="https://www.businesscompanion.info/" target="_blank" rel="noopener noreferrer" title="Trading Standards - Opens in a new window">
+				<img src="../../../_img/TS_unagreed.gif" width="24" height="32"  alt="" /> Trading Standards
+			</a>
+		</p>
+		
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
