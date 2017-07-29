34c35b353730f35be7df8c18a6ddc368b7d22c7b
Caroline Hagan <info@blueocto.co.uk>
2017-07-29 13:12:02 +0100

    Completed sales-and-marketing > selling-and-the-law > comply-with-the-law-in-the-provision-of-goods-facilities-and-services > "The Proof of Age Standards Scheme" page. Including global link updates.

---
+++ starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/the-proof-of-age-standards-scheme.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>The Proof of Age Standards Scheme | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+		<h1>Comply with the law in the provision of goods, facilities and services</h1>
+		
+		<h2>The Proof of Age Standards Scheme</h2>
+		<p>The Proof of Age Standards Scheme (PASS) is the UK's national scheme for proof-of-age cards.</p>
+		<p>PASS brings in a common standard and accreditation process to help protect you and your employees when making age-related sales.</p>
+		<p>The PASS hologram indicates that the card issuer has passed the rigorous Trading Standards audit process and that you can rely on the card's authenticity.</p>
+		
+		<h3>How PASS makes life easier for retailers</h3>
+		<p>If you are a retailer, PASS has several important benefits for you and your employees. It can help to:</p>
+		<ul>
+			<li>protect your business from receiving fines for selling age-restricted goods</li>
+			<li>protect you from the loss of your premises licence</li>
+			<li>clarify which cards are genuine</li>
+			<li>give a single recognisable logo you can trust - the PASS hologram is a registered trademark, which means that to forge the hologram is a criminal offence</li>
+			<li>make it easier to check the identity of the carrier against the photo and date of birth on the card with the PASS hologram</li>
+		</ul>
+		
+		<p>Almost two million young people hold proof-of-age cards bearing the PASS hologram, and numbers are increasing all the time.</p>
+		<p>The following cards are all part of PASS and are available nationally:</p>
+		<ul>
+			<li>CitizenCard</li>
+			<li>VALIDATE UK</li>
+			<li>Young Scot</li>
+			<li>Proof GB</li>
+		</ul>
+		
+		<p>There are a number of local schemes which produce PASS-accredited cards. <a href="http://www.pass-scheme.org.uk/" title="PASS card issuers, including national and regional schemes" target="_blank" rel="noopener noreferrer">Find a list of local and national schemes issuing PASS-accredited cards on the PASS website</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-electronic-and-electrical-goods.php" title="Special rules for businesses that sell electronic and electrical goods">Prev</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/selling-and-the-law/comply-with-the-law-in-the-provision-of-goods-facilities-and-services.php" title="Comply with the law in the provision of goods, facilities and services">Introduction</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/supplying-satisfactory-and-safe-goods-and-services.php">Supplying satisfactory and safe goods and services</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/how-equality-law-applies-to-the-provision-of-goods-facilities-and-services.php">How equality law applies to the provision of goods, facilities and services</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/customers-and-data-protection.php">Customers and data protection</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/getting-paid-on-time.php">Getting paid on time</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/rules-for-businesses-that-dont-sell-to-customers-face-to-face.php">Rules for businesses that don't sell to customers face to face</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-to-customers-in-their-home-or-workplace.php">Special rules for businesses that sell to customers in their home or workplace</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-electronic-and-electrical-goods.php">Special rules for businesses that sell electronic and electrical goods</a></li>
+			<li>The Proof of Age Standards Scheme</li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.pass-scheme.org.uk/governance/" title="Information on the PASS scheme for retailers" target="_blank" rel="noopener noreferrer">Retailers and the PASS scheme information on the PASS website</a></li>
+			<li><a href="https://www.businesscompanion.info/en/quick-guides/underage-sales" title="Products covered by age restrictions" target="_blank" rel="noopener noreferrer">Age-restricted products on the Trading Standards Institute website</a></li>
+		</ul>
+		
+		<p>
+			Developed with:<br />
+			<a href="http://webarchive.nationalarchives.gov.uk/20130403014324/http://oft.gov.uk/" target="_blank" rel="noopener noreferrer" title="Office of Fair Trading - Opens in a new window">
+				<img src="../../../_img/OFT_unagreed.gif" width="130" height="32"  alt="" /> Office of Fair Trading
+			</a>
+			<br />
+			<a href="https://www.businesscompanion.info/" target="_blank" rel="noopener noreferrer" title="Trading Standards - Opens in a new window">
+				<img src="../../../_img/TS_unagreed.gif" width="24" height="32"  alt="" /> Trading Standards
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+		
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
