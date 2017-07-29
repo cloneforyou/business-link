6088891ada48b14548bd199747a238ee9473e90d
Caroline Hagan <info@blueocto.co.uk>
2017-07-29 13:04:00 +0100

    Completed sales-and-marketing > selling-and-the-law > comply-with-the-law-in-the-provision-of-goods-facilities-and-services > "Special rules for businesses that sell electronic and electrical goods" page. Including global link updates.

---
+++ starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-electronic-and-electrical-goods.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Special rules for businesses that sell electronic and electrical goods | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Comply with the law in the provision of goods, facilities and services</h1>
+		
+		<h2>Special rules for businesses that sell electronic and electrical goods</h2>
+		<p>The Waste Electrical and Electronic Equipment (WEEE) Regulations 2006 place responsibilities on users, producers and distributors of electrical and electronic equipment. Under the WEEE regulations you are a distributor if you sell electrical and electronic equipment to consumers.</p>
+		<p>You'll need to comply with the WEEE Regulations if you sell:</p>
+		<ul>
+			<li>household appliances</li>
+			<li>IT, telecommunications or audio-visual equipment</li>
+			<li>lighting equipment</li>
+			<li>electrical and electronic tools</li>
+			<li>electronic and battery-operated toys</li>
+		</ul>
+
+		<p>This list is not exhaustive. For further information, see the page on <a href="/environment-and-efficiency/waste-legislation-and-responsibility/waste-electrical-and-electronic-equipment-weee/equipment-covered-by-the-weee-regulations.php">equipment covered by the WEEE Regulations</a> in our guide on <a href="/environment-and-efficiency/waste-legislation-and-responsibility/waste-electrical-and-electronic-equipment-weee.php">waste electrical and electronic equipment (WEEE)</a>.</p>
+		<p>To fulfil your obligations under the WEEE Regulations you must:</p>
+		<ul>
+			<li>provide information to consumers on the environmental impacts of electrical and electronic equipment and the reasons for separating WEEE from other waste</li>
+			<li>explain the meaning of the crossed-out wheeled bin symbol</li>
+			<li>explain how consumers can safely dispose of WEEE free of charge</li>
+			<li>display information on the benefits of take-back schemes</li>
+			<li>establish an in-store take-back scheme or join the distributor take-back scheme to enable consumers to dispose of their WEEE</li>
+		</ul>
+
+		<p>You must keep records of this information for four years.</p>
+		<p>If you decide to set up an in-store take-back scheme, you are required to take back the old item from your customer when you sell them an equivalent new item. You must keep records of the amounts and category of items that you receive and keep these records for four years. You must then arrange the removal of the WEEE you collect through a licensed waste carrier or producer compliance scheme.</p>
+		<p>Alternatively you can join the distributor take-back scheme. This scheme works through a network of centres where consumers can dispose of their WEEE free of charge. You must inform customers about how and where they can do this.</p>
+		<p>See the page on <a href="/environment-and-efficiency/waste-legislation-and-responsibility/waste-electrical-and-electronic-equipment-weee/obligations-of-electrical-and-electronic-equipment-distributors.php">obligations of electrical and electronic equipment distributors</a> in our guide on <a href="/environment-and-efficiency/waste-legislation-and-responsibility/waste-electrical-and-electronic-equipment-weee.php">waste electrical and electronic equipment (WEEE)</a>.</p>
+		<p>The distributor take-back scheme is operated by Valpak Retail WEEE Services.</p>
+		
+		<ul>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-to-customers-in-their-home-or-workplace.php" title="Special rules for businesses that sell to customers in their home or workplace">Prev</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/the-proof-of-age-standards-scheme.php" title="The Proof of Age Standards Scheme">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/selling-and-the-law/comply-with-the-law-in-the-provision-of-goods-facilities-and-services.php" title="Comply with the law in the provision of goods, facilities and services">Introduction</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/supplying-satisfactory-and-safe-goods-and-services.php">Supplying satisfactory and safe goods and services</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/how-equality-law-applies-to-the-provision-of-goods-facilities-and-services.php">How equality law applies to the provision of goods, facilities and services</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/customers-and-data-protection.php">Customers and data protection</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/getting-paid-on-time.php">Getting paid on time</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/rules-for-businesses-that-dont-sell-to-customers-face-to-face.php">Rules for businesses that don't sell to customers face to face</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-to-customers-in-their-home-or-workplace.php">Special rules for businesses that sell to customers in their home or workplace</a></li>
+			<li>Special rules for businesses that sell electronic and electrical goods</li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/the-proof-of-age-standards-scheme.php">The Proof of Age Standards Scheme</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Environment Agency Helpline</h3>
+		<p>03708 506 506</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/guidance/waste-electrical-and-electronic-equipment-weee-reuse-and-treat" title="The WEEE regulations and distributors of electrical and electronic equipment" target="_blank" rel="noopener noreferrer">The WEEE regulations<!-- on the Envirowise website --></a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/environment-and-efficiency/reducing-and-recycling-waste/waste-reviews-policies-and-action-plans.php">Waste reviews, policies and action plans</a></li>
+			<li><a href="/environment-and-efficiency/waste-legislation-and-responsibility/duty-of-care-your-waste-responsibilities.php">Duty of care - your waste responsibilities</a></li>
+			<li><a href="/environment-and-efficiency/waste-legislation-and-responsibility/waste-electrical-and-electronic-equipment-weee.php">Waste electrical and electronic equipment (WEEE)</a></li>
+		</ul>
+
+		<p>
+			Developed with:<br />
+			<a href="http://webarchive.nationalarchives.gov.uk/20130403014324/http://oft.gov.uk/" target="_blank" rel="noopener noreferrer" title="Office of Fair Trading - Opens in a new window">
+				<img src="../../../_img/OFT_unagreed.gif" width="130" height="32"  alt="" /> Office of Fair Trading
+			</a>
+			<br />
+			<a href="https://www.businesscompanion.info/" target="_blank" rel="noopener noreferrer" title="Trading Standards - Opens in a new window">
+				<img src="../../../_img/TS_unagreed.gif" width="24" height="32"  alt="" /> Trading Standards
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+	   
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
