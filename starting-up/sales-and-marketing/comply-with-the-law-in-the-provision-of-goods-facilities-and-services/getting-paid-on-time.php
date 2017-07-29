8973276bd3428f35815b02dcaf80fc6019fe2d3a
Caroline Hagan <info@blueocto.co.uk>
2017-07-29 12:42:41 +0100

    Completed sales-and-marketing > selling-and-the-law > comply-with-the-law-in-the-provision-of-goods-facilities-and-services > "Getting paid on time" page. Including global link updates.

---
+++ starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/getting-paid-on-time.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Getting paid on time | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Comply with the law in the provision of goods, facilities and services</h1>
+		
+		<h2>Getting paid on time</h2>
+		<p>Late payment of bills can be frustrating and can cause serious cashflow problems for businesses of any size.</p>
+		<p>The law aims to discourage late payment by allowing businesses to charge each other interest on overdue invoices under the Late Payment of Commercial Debts (Interest) Act 1998.</p>
+		<p>Unless your terms and conditions state otherwise, an invoice becomes due within 30 days of the invoice date. If it is not paid within this time you are entitled to charge interest.</p>
+		<p>It is not compulsory to charge interest but if you do it should be calculated at the Bank of England base rate plus 8 per cent. You can <a href="http://www.bankofengland.co.uk/" target="_blank" rel="noopener noreferrer" title="Find out the Bank of England base rate">find the current base rate on the Bank of England website</a>.</p>
+		<p>To calculate how much interest you are owed, <a href="/finance-and-grants/debt-recovery/calculate-the-interest-due-on-an-unpaid-debt.php">use our interactive tool to calculate statutory or contractual interest you may be able to charge on an unpaid debt</a>.</p>
+		<p>The law also allows you to claim <b>compensation</b> for late payment at the following rates:</p>
+		<ul>
+			<li>&pound;40 for debts up to &pound;999</li>
+			<li>&pound;70 for debts between &pound;1,000 and &pound;9,999.99</li>
+			<li>&pound;100 for debts more than &pound;10,000</li>
+		</ul>
+		
+		<p>Some firms choose not to charge interest because they are concerned it may alienate important customers. Developing strong credit management procedures can help avoid this conflict of interests.</p>
+		<p>And remember - your purchase invoices may also attract interest if you fail to pay them on time.</p>
+		<p>
+			<p><a href="../../../_files/berr_a-users-guide-to-late-payment-legislation_apr-2002.pdf" target="_blank" rel="noopener noreferrer">Download the user's guide to late payment legislation from the Department for Business, Innovation &amp; Skills (BIS) website (PDF, 390K)</a>.</p>
+		</p>
+		
+		<ul>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/customers-and-data-protection.php" title="Customers and data protection">Prev</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/rules-for-businesses-that-dont-sell-to-customers-face-to-face.php" title="Rules for businesses that don't sell to customers face to face">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/selling-and-the-law/comply-with-the-law-in-the-provision-of-goods-facilities-and-services.php" title="Comply with the law in the provision of goods, facilities and services">Introduction</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/supplying-satisfactory-and-safe-goods-and-services.php">Supplying satisfactory and safe goods and services</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/how-equality-law-applies-to-the-provision-of-goods-facilities-and-services.php">How equality law applies to the provision of goods, facilities and services</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/customers-and-data-protection.php">Customers and data protection</a></li>
+			<li>Getting paid on time</li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/rules-for-businesses-that-dont-sell-to-customers-face-to-face.php">Rules for businesses that don't sell to customers face to face</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-to-customers-in-their-home-or-workplace.php">Special rules for businesses that sell to customers in their home or workplace</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-electronic-and-electrical-goods.php">Special rules for businesses that sell electronic and electrical goods</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/the-proof-of-age-standards-scheme.php">The Proof of Age Standards Scheme</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.bankofengland.co.uk/" target="_blank" rel="noopener noreferrer" title="Find out the Bank of England base rate">Current base rate on the Bank of England website</a></li>
+			<li><a href="../../../_files/berr_a-users-guide-to-late-payment-legislation_apr-2002.pdf" target="_blank" rel="noopener noreferrer" title="A User's guide to the late payment legislation">Download late payment legislation guidance from the BIS website (PDF, 390K)</a></li>
+			<li><a href="/finance-and-grants/debt-recovery/recover-your-debts.php">Use our interactive tool to find out how you can recover unpaid debts</a></li>
+			<li><a href="/finance-and-grants/debt-recovery/calculate-the-interest-due-on-an-unpaid-debt.php">Use our interactive tool to calculate statutory or contractual interest you may be able to charge on an unpaid debt</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/finance-and-grants/debt-recovery/ensuring-customers-pay-you-on-time.php">Ensuring customers pay you on time</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/invoicing-and-payment-terms.php">Invoicing and payment terms</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">Cashflow management: the basics</a></li>
+		</ul>
+
+		<p>
+			Developed with:<br />
+			<a href="http://webarchive.nationalarchives.gov.uk/20130403014324/http://oft.gov.uk/" target="_blank" rel="noopener noreferrer" title="Office of Fair Trading - Opens in a new window">
+				<img src="../../../_img/OFT_unagreed.gif" width="130" height="32"  alt="" /> Office of Fair Trading
+			</a>
+			<br />
+			<a href="https://www.businesscompanion.info/" target="_blank" rel="noopener noreferrer" title="Trading Standards - Opens in a new window">
+				<img src="../../../_img/TS_unagreed.gif" width="24" height="32"  alt="" /> Trading Standards
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
