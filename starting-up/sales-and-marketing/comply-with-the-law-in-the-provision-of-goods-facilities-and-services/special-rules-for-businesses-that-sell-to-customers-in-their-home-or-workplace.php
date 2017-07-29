52384fb40c47d18e7b96859ed7226a5349ba9d98
Caroline Hagan <info@blueocto.co.uk>
2017-07-29 12:52:44 +0100

    Completed sales-and-marketing > selling-and-the-law > comply-with-the-law-in-the-provision-of-goods-facilities-and-services > "Special rules for businesses that sell to customers in their home or workplace" page. Including global link updates.

---
+++ starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-to-customers-in-their-home-or-workplace.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Special rules for businesses that sell to customers in their home or workplace | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Comply with the law in the provision of goods, facilities and services</h1>
+		
+		<h2>Special rules for businesses that sell to customers in their home or workplace</h2>
+		<p>Since 1 October 2008, consumers have had the same rights to a cooling-off period for sales resulting from a solicited call by a trader as they do for sales resulting from an unsolicited visit by a trader to a consumer's home, workplace or at a location arranged by the trader away from their business premises. This means that consumers who agree to purchase goods or services with a total payment value of more than &pound;35 can cancel any agreement within a seven day cooling-off period. Businesses that have not yet done so should review their contracts and sales practices and make necessary adjustments.</p>
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20090609003649/http://webarchive.nationalarchives.gov.uk/20090609003649/http://www.berr.gov.uk/whatwedo/consumers/buying-selling/Doorstep-selling/index.html" title="Explains the main requirements on businesses including providing a notice of the right to cancel" target="_blank" rel="noopener noreferrer">Check your responsibilities to consumers when they buy products or services from you in their home or workplace on the Department for Business, Innovation &amp; Skills (BIS) website</a>.</p>
+		<p>If you sell goods <strong>to consumers</strong> online - via the internet, email, digital TV or mobile-phone text messages - you must comply with the distance selling regulations. See the page in this guide on <a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/rules-for-businesses-that-dont-sell-to-customers-face-to-face.php">rules for businesses that don't sell to consumers face to face</a>.</p>
+		<p>Similar requirements apply if you sell goods online <strong>to businesses</strong>. However, business customers do not have an automatic right to cancel.</p>
+		<p>Whether you are selling to consumers or businesses, you must provide customers with additional information, including:</p>
+		<ul>
+			<li>your VAT number (if applicable)</li>
+			<li>details of membership of any professional organisations</li>
+			<li>an opportunity to correct orders before they are completed</li>
+		</ul>
+
+		<p>Information on how to place an order, and how to correct errors made while placing it, must be provided before the order is made. You must also acknowledge orders promptly.</p>
+		<p>
+			<p>Most other information can be made available on your website. <a href="http://webarchive.nationalarchives.gov.uk/20090609024734/http://www.berr.gov.uk/whatwedo/sectors/ecommerce/ecommsdirectivefaqs/page10148.html" title="Frequently asked questions on the electronic commerce regulations on the BIS website" target="_blank" rel="noopener noreferrer">Find information on the electronic commerce regulations on the BIS website</a>. </p>
+		</p>
+		<p>See our guide on <a href="/sales-and-marketing/online-marketing/distance-selling-and-online-trading.php">distance selling and online trading</a>.</p>
+		<p>There are also special rules concerning marketing by email that you need to be aware of. You can find out more about these in our guide on <a href="/sales-and-marketing/online-marketing/email-marketing.php">email marketing</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/rules-for-businesses-that-dont-sell-to-customers-face-to-face.php" title="Rules for businesses that don't sell to customers face to face">Prev</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-electronic-and-electrical-goods.php" title="Special rules for businesses that sell electronic and electrical goods">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/selling-and-the-law/comply-with-the-law-in-the-provision-of-goods-facilities-and-services.php" title="Comply with the law in the provision of goods, facilities and services">Introduction</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/supplying-satisfactory-and-safe-goods-and-services.php">Supplying satisfactory and safe goods and services</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/how-equality-law-applies-to-the-provision-of-goods-facilities-and-services.php">How equality law applies to the provision of goods, facilities and services</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/customers-and-data-protection.php">Customers and data protection</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/getting-paid-on-time.php">Getting paid on time</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/rules-for-businesses-that-dont-sell-to-customers-face-to-face.php">Rules for businesses that don't sell to customers face to face</a></li>
+			<li>Special rules for businesses that sell to customers in their home or workplace</li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-electronic-and-electrical-goods.php">Special rules for businesses that sell electronic and electrical goods</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/the-proof-of-age-standards-scheme.php">The Proof of Age Standards Scheme</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20090609024734/http://www.berr.gov.uk/whatwedo/sectors/ecommerce/ecommsdirectivefaqs/page10148.html" title="Frequently asked questions on the electronic commerce regulations on the BIS website" target="_blank" rel="noopener noreferrer">Electronic commerce rules on the BIS website</a></li>
+			<li><a href="https://www.gov.uk/licence-finder" title="Use our licence tool to find out which licences and permits could apply to your business" target="_blank" rel="noopener noreferrer">Use our licence and permit checklist to find out which licences and permits could apply to your business</a></li>
+		</ul>
+				
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/online-marketing/distance-selling-and-online-trading.php">Distance selling and online trading</a></li>
+			<li><a href="/sales-and-marketing/selling-and-the-law/approved-codes-and-self-regulation.php">Approved codes and self regulation</a></li>
+			<li><a href="/sales-and-marketing/pricing/competing-fairly.php">Competing fairly</a></li>
+		</ul>
+		
+		<p>
+			Developed with:<br />
+			<a href="http://webarchive.nationalarchives.gov.uk/20130403014324/http://oft.gov.uk/" target="_blank" rel="noopener noreferrer" title="Office of Fair Trading - Opens in a new window">
+				<img src="../../../_img/OFT_unagreed.gif" width="130" height="32"  alt="" /> Office of Fair Trading
+			</a>
+			<br />
+			<a href="https://www.businesscompanion.info/" target="_blank" rel="noopener noreferrer" title="Trading Standards - Opens in a new window">
+				<img src="../../../_img/TS_unagreed.gif" width="24" height="32"  alt="" /> Trading Standards
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+		
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
