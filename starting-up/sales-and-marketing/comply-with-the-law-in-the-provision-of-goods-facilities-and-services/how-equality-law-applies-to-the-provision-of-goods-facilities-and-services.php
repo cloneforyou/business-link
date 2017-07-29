ffc70ed3a92bf83d3e946a53a3a5cf9c5a3445c5
Caroline Hagan <info@blueocto.co.uk>
2017-07-29 12:20:16 +0100

    Completed sales-and-marketing > selling-and-the-law > comply-with-the-law-in-the-provision-of-goods-facilities-and-services > "How equality law applies to the provision of goods, facilities and services" page.

---
+++ starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/how-equality-law-applies-to-the-provision-of-goods-facilities-and-services.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>How equality law applies to the provision of goods, facilities and services | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Comply with the law in the provision of goods, facilities and services</h1>
+		
+		<h2>How equality law applies to the provision of goods, facilities and services</h2>
+		<p>Under the Equality Act 2010 people who access your goods, facilities and services are protected from discrimination on the basis of a 'protected characteristic'.</p>
+		<p>The relevant protected characteristics are:</p>
+		<ul>
+			<li>sex</li>
+			<li>disability</li>
+			<li>religion or belief</li>
+			<li>sexual orientation</li>
+			<li>gender reassignment</li>
+			<li>pregnancy and maternity</li>
+			<li>race, which includes ethnic or national origins, colour and nationality</li>
+		</ul>
+
+		<p>Age is not currently covered.</p>
+		<h3>Direct discrimination</h3>
+		<p>This occurs when a person is treated less favourably than another person because of a protected characteristic. For example, if you refuse to serve a customer because of their sexual orientation.</p>
+		<p>Direct discrimination can also take place if you discriminate against someone because:</p>
+		<ul>
+			<li>they associate with someone who has a particular protected characteristic</li>
+			<li>you (wrongly) perceive that person to have - or deliberately treat them as if they have - a particular protected characteristic</li>
+		</ul>
+		
+		<h3>Indirect discrimination</h3>
+		<p>This occurs when there is a rule, policy or practice that applies generally but which particularly disadvantages people who share a particular protected characteristic.</p>
+		<p>For example, a 'no dogs' policy in a hotel would indirectly discriminate against those who need a guide dog to help them access premises.</p>
+		<p>However, such a policy or practice could be justified if it can be shown that its application is a proportionate way of achieving a legitimate aim.</p>
+		
+		<h3>Discrimination arising from disability</h3>
+		<p>This occurs when a disabled person is treated unfavourably because of something connected with their disability and this unfavourable treatment cannot be justified. For example, you exclude a person with a mobility impairment from your premises because they use a wheelchair.</p>
+		<p>You can justify this unfavourable treatment if you can show that it is intended to meet a legitimate objective in a fair, balanced and reasonable way.</p>
+		<p>This type of discrimination cannot occur if you do not know, or could not reasonably be expected to know, that the person is disabled.</p>
+		
+		<h3>Harassment</h3>
+		<p>This can occur when a service provider engages in unwanted conduct which is related to a relevant protected characteristic and which has the purpose or the effect of:</p>
+		<ul>
+			<li>violating the customer's dignity</li>
+			<li>creating an intimidating, hostile, degrading, humiliating or offensive environment</li>
+		</ul>
+		
+		<p>Harassment because of a person's pregnancy and maternity, religion and belief or sexual orientation are not protected directly under the harassment provisions but pregnancy and maternity harassment would amount to harassment related to sex. Also, where unwanted conduct related to sexual orientation or religion or belief results in a person suffering a detriment, that person could bring a claim of direct discrimination.</p>
+		
+		<h3>Victimisation</h3>
+		<p>This occurs when a service provider treats a person badly because they have made or supported a complaint about discrimination or harassment, or because the service provider thinks that a person is doing or may do these things.</p>
+		<p>A person is not protected from victimisation if they have maliciously made or supported an untrue complaint.</p>
+		
+		<h3>Reasonable adjustments</h3>
+		<p>You are required to make changes, where needed, to improve service for disabled customers or potential customers. Reasonable changes are required wherever disabled customers or potential customers would otherwise be at a substantial disadvantage compared with non-disabled people. Service providers cannot charge disabled customers for reasonable adjustments.</p>
+		
+		<h3>Breastfeeding mothers</h3>
+		<p>The law makes it clear that you must not discriminate against women outside work just because they are breastfeeding, regardless of the age of the child.</p>
+		<ul>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/supplying-satisfactory-and-safe-goods-and-services.php" title="Supplying satisfactory and safe goods and services">Prev</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/customers-and-data-protection.php" title="Customers and data protection">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/selling-and-the-law/comply-with-the-law-in-the-provision-of-goods-facilities-and-services.php" title="Comply with the law in the provision of goods, facilities and services">Introduction</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/supplying-satisfactory-and-safe-goods-and-services.php">Supplying satisfactory and safe goods and services</a></li>
+			<li>How equality law applies to the provision of goods, facilities and services</li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/customers-and-data-protection.php">Customers and data protection</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/getting-paid-on-time.php">Getting paid on time</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/rules-for-businesses-that-dont-sell-to-customers-face-to-face.php">Rules for businesses that don't sell to customers face to face</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-to-customers-in-their-home-or-workplace.php">Special rules for businesses that sell to customers in their home or workplace</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-electronic-and-electrical-goods.php">Special rules for businesses that sell electronic and electrical goods</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/the-proof-of-age-standards-scheme.php">The Proof of Age Standards Scheme</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>EHRC Helpline - England</h3>
+		<p>0845 604 6610</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="../../../_files/odi_growing-your-customer-base-to-include-disabled-people_aug-2012.pdf" target="_blank" rel="noopener noreferrer">Download a guide to growing your customer base to include disabled people from the ODI website (PDF, 1.04M)</a></li>
+			<li><a href="https://www.gov.uk/government/policies/equality" target="_blank" rel="noopener noreferrer">Equality Act guidance for businesses that provide goods and services on the Government Equalities Office website</a></li>
+			<li><a href="https://www.equalityhumanrights.com/en/advice-and-guidance/guidance-service-providers" target="_blank" rel="noopener noreferrer">Equality Act guidance for service providers on the Equality and Human Rights Commission website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/premises-and-property/access-and-facilities/access-and-facilities-for-disabled-people.php">Access and facilities for disabled people</a></li>
+		</ul>
+
+		<p>
+			Developed with:<br />
+			<a href="http://webarchive.nationalarchives.gov.uk/20130403014324/http://oft.gov.uk/" target="_blank" rel="noopener noreferrer" title="Office of Fair Trading - Opens in a new window">
+				<img src="../../../_img/OFT_unagreed.gif" width="130" height="32"  alt="" /> Office of Fair Trading
+			</a>
+			<br />
+			<a href="https://www.businesscompanion.info/" target="_blank" rel="noopener noreferrer" title="Trading Standards - Opens in a new window">
+				<img src="../../../_img/TS_unagreed.gif" width="24" height="32"  alt="" /> Trading Standards
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+	
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
