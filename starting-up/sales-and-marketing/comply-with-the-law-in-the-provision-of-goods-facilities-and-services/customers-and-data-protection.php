a3f8830b2e28cbe94dee016ce67eaf9c2157c404
Caroline Hagan <info@blueocto.co.uk>
2017-07-29 12:30:04 +0100

    Completed sales-and-marketing > selling-and-the-law > comply-with-the-law-in-the-provision-of-goods-facilities-and-services > "Customers and data protection" page.

---
+++ starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/customers-and-data-protection.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Customers and data protection | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Comply with the law in the provision of goods, facilities and services</h1>
+		
+		<h2>Customers and data protection</h2>
+		<p>All businesses that hold the details of customers, potential customers, suppliers, staff or any other business contacts are required to comply with the Data Protection Act. The Act applies to any records held electronically - for example on computer - or manually such as in a paper file.</p>
+		<p>You may be required to register with the Information Commissioner. <a href="/it-and-ecommerce/data-protection-and-legal-issues/notifying-the-information-commissioner.php">Use our interactive tool to find out whether you need to notify the Information Commissioner about the data your business holds</a>.</p>
+		<p>You can <a href="https://ico.org.uk/for-organisations/register/" title="A step-by-step guide on how to notify the ICO about personal information" target="_blank" rel="noopener noreferrer">register with the Information Commissioner on the Information Commissioner's Office (ICO) website</a> or by calling the ICO Helpline on Tel 0303 123 1113.</p>
+		<p>The Data Protection Act requires businesses to comply with a number of principles. These include:</p>
+		<ul>
+			<li>information must only be used for specified and lawful purposes</li>
+			<li>businesses should only hold on to information they actually need</li>
+			<li>information that is no longer required should be deleted or destroyed as soon as possible</li>
+			<li>the information must be accurate and up to date</li>
+			<li>the information must be held securely</li>
+			<li>businesses must observe the subject's rights</li>
+		</ul>
+
+		<p>You can <a href="https://ico.org.uk/for-organisations/guide-to-data-protection/data-protection-principles/" target="_blank" rel="noopener noreferrer">read information on the principles of the Data Protection Act on the ICO website</a>.</p>
+		<p>For further information on the implications of the Data Protection Act, read our guide on <a href="/sales-and-marketing/selling-and-the-law/privacy-and-data-protection-in-direct-marketing.php" title="What steps you must take to protect your customers' privacy and data">privacy and data protection in marketing</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/how-equality-law-applies-to-the-provision-of-goods-facilities-and-services.php" title="How equality law applies to the provision of goods, facilities and services">Prev</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/getting-paid-on-time.php" title="Getting paid on time">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/selling-and-the-law/comply-with-the-law-in-the-provision-of-goods-facilities-and-services.php" title="Comply with the law in the provision of goods, facilities and services">Introduction</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/supplying-satisfactory-and-safe-goods-and-services.php">Supplying satisfactory and safe goods and services</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/how-equality-law-applies-to-the-provision-of-goods-facilities-and-services.php">How equality law applies to the provision of goods, facilities and services</a></li>
+			<li>Customers and data protection</li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/getting-paid-on-time.php">Getting paid on time</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/rules-for-businesses-that-dont-sell-to-customers-face-to-face.php">Rules for businesses that don't sell to customers face to face</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-to-customers-in-their-home-or-workplace.php">Special rules for businesses that sell to customers in their home or workplace</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/special-rules-for-businesses-that-sell-electronic-and-electrical-goods.php">Special rules for businesses that sell electronic and electrical goods</a></li>
+			<li><a href="/starting-up/sales-and-marketing/comply-with-the-law-in-the-provision-of-goods-facilities-and-services/the-proof-of-age-standards-scheme.php">The Proof of Age Standards Scheme</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>ICO Helpline</h3>
+		<p>0303 123 1113</p>
+		
+		<img src="../../../_img/hr_140px_global.gif" alt="" />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/data-protection-and-legal-issues/notifying-the-information-commissioner.php" title="This interactive tool will help you understand the legal obligations associated with personal information">Use our interactive tool to find out whether you need to notify the Information Commissioner about the data your business holds</a></li>
+			<li><a href="https://ico.org.uk/for-organisations/register/" target="_blank" rel="noopener noreferrer" title="A step-by-step guide on how to notify the ICO about personal information">Registration advice on the ICO website</a></li>
+			<li><a href="https://ico.org.uk/for-organisations/guide-to-data-protection/data-protection-principles/" target="_blank" rel="noopener noreferrer">Data protection principles on the ICO website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/selling-and-the-law/privacy-and-data-protection-in-direct-marketing.php">Privacy and data protection in direct marketing</a></li>
+		</ul>
+
+		<p>
+			Developed with:<br />
+			<a href="http://webarchive.nationalarchives.gov.uk/20130403014324/http://oft.gov.uk/" target="_blank" rel="noopener noreferrer" title="Office of Fair Trading - Opens in a new window">
+				<img src="../../../_img/OFT_unagreed.gif" width="130" height="32"  alt="" /> Office of Fair Trading
+			</a>
+			<br />
+			<a href="https://www.businesscompanion.info/" target="_blank" rel="noopener noreferrer" title="Trading Standards - Opens in a new window">
+				<img src="../../../_img/TS_unagreed.gif" width="24" height="32"  alt="" /> Trading Standards
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
