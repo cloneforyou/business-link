844c77e10928f1d3f008e8423c635f65340110c6
Caroline Hagan <info@blueocto.co.uk>
2017-08-07 20:45:14 +0100

    Completed starting-up > it-and-ecommerce-for-startups > choose-and-manage-your-it-supplier > “Negotiating the contract for your IT system” page.

---
+++ starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/negotiating-the-contract-for-your-it-system.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Negotiating the contract for your IT system | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+		<h1>Choose and manage your IT supplier</h1>
+		<h2>Negotiating the contract for your IT system</h2>
+		<p>Make sure you're completely clear about exactly what the contract with your supplier includes. Hidden extras can be expensive.</p>
+		<p>Check whether you'll receive:</p>
+		<ul>
+			<li><strong>on-site service calls</strong> - a certain number may be included or charged at a reduced rate</li>
+			<li><strong>preventative maintenance</strong>, eg the use and regular updating of anti-virus software and firewalls to protect your system</li>
+			<li><strong>remote monitoring</strong> and diagnosis of system problems - if included, check whether you will have to purchase any extra equipment or communication links</li>
+			<li><strong>telephone support</strong> - there may be a limit on the number or length of telephone calls</li>
+			<li><strong>warranties</strong> - check what warranties, if any, are included</li>
+			<li><strong>software upgrades</strong> - check if upgrades are free, or whether you have to purchase new versions as they are released or pay an annual software subscription</li>
+			<li><strong>software support</strong> - the ability for you to report problems and receive help, patches and bug fixes</li>
+			<li><strong>user training</strong> - some suppliers include a limited amount of training in the system's purchase price, but others will charge extra</li>
+			<li><strong>manuals</strong> - you may need them if you end the relationship with your supplier in future</li>
+		</ul>
+
+		<p>When purchasing IT services, you should draw up a <strong>service-level agreement (SLA)</strong> with the supplier, which specifies precisely what you expect from the supplier. A typical SLA should cover:</p>
+		<ul>
+			<li><strong>scope</strong> - details of the hardware and software that is to be covered</li>
+			<li><strong>range of services</strong> - the services that the supplier will be required to provide</li>
+			<li><strong>service availability</strong> - when the service should be available</li>
+			<li><strong>response times</strong> - an important aspect of the SLA - usually the faster the response times, the greater the cost</li>
+			<li><strong>escalation procedures</strong> - an agreed series of actions used to ensure problems are dealt with quickly: responses are intensified until the problem is resolved - eg moving from off-site diagnosis through to on-site support</li>
+			<li><strong>record keeping</strong> - problems and solutions must be properly documented, and records maintained for dispute resolution</li>
+			<li><strong>performance review</strong> - to maintain acceptable levels of service over time</li>
+			<li><strong>supplier obligations</strong> - such as the provision of spare parts, the qualifications of support staff and the need to meet response times</li>
+			<li><strong>customer obligations</strong> - your responsibilities, eg providing information about changes that affect the scope of the contract, and the cooperation of staff with the supplier</li>
+			<li><strong>termination of agreement</strong> - a formal process which defines the specific terms and conditions under which the contract may be terminated</li>
+		</ul>
+
+		<ul>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-cost-evaluation.php" title="IT system cost evaluation">Prev</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/benefits-of-an-effective-it-supplier-relationship.php" title="Benefits of an effective IT supplier relationship">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/suppliers/choose-and-manage-your-it-supplier.php" title="Choose and manage your IT supplier">Introduction</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/the-different-types-of-it-suppliers.php">The different types of IT suppliers</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/identifying-and-contacting-potential-it-suppliers.php">Identifying and contacting potential IT suppliers</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/selecting-the-right-it-supplier.php">Selecting the right IT supplier</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-maintenance.php">IT system maintenance</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-support-services.php">IT support services</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-cost-evaluation.php">IT system cost evaluation</a></li>
+			<li>Negotiating the contract for your IT system</li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/benefits-of-an-effective-it-supplier-relationship.php">Benefits of an effective IT supplier relationship</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/checklist-choosing-an-it-supplier.php">Checklist: choosing an IT supplier</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.sla-zone.co.uk/" target="_blank" rel="noopener noreferrer">SLA information on the Service Level Agreement Zone website</a></li>
+			<li><a href="https://web.archive.org/web/20090220143122/http://ictknowledgebase.org.uk:80/whattoexpectfromsupport" target="_blank" rel="noopener noreferrer">IT support contract advice on the lasa knowledgebase website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/employees-and-it/it-support-for-your-staff.php" title="How to ensure that your staff receive the correct level of training and support to use IT systems effectively">IT support for your staff</a></li>
+			<li><a href="/it-and-ecommerce/introduction-to-it/computer-software-the-basics.php" title="The different types of software available and getting the right programs for your business needs">Computer software: the basics</a></li>
+			<li><a href="/it-and-ecommerce/software-and-business-applications/open-source-software.php" title="Introduction to what open source software is and why you might consider using it in your business">Open source software</a></li>
+			<li><a href="/it-and-ecommerce/software-and-business-applications/cloud-computing.php" title="How businesses can use cloud computing to cut IT costs and innovate more quickly">Cloud computing</a></li>
+			<li><a href="/it-and-ecommerce/introduction-to-it/computer-hardware-the-basics.php" title="How to assess your IT needs and get the right equipment to meet your business requirements">Computer hardware: the basics</a></li>
+			<li><a href="/grow-your-business/growth-through-strategic-sourcing/manage-your-suppliers.php" title="How to get the most from your suppliers">Manage your suppliers</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163006/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101787&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Choose the right IT for your business</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163008/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101793&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Create your first website</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163009/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097102020&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Get started with e-commerce</a></li>
+		</ul>
+
+		<p>
+			Developed with:<br />
+			<a href="https://www.nb2bc.co.uk/" title="The National B2B Centre - Opens in a new window" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/logo_b2b.gif" width="60" height="35"  alt="" /> The National B2B Centre
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
