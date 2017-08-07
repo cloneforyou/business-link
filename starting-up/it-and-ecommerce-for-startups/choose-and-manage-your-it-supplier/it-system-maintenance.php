3dd31079c484c07f6793cb9d2d67082b41929c23
Caroline Hagan <info@blueocto.co.uk>
2017-08-07 20:11:51 +0100

    Completed starting-up > it-and-ecommerce-for-startups > choose-and-manage-your-it-supplier > “IT system maintenance” page.

---
+++ starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-maintenance.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>IT system maintenance | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+		<h1>Choose and manage your IT supplier</h1>
+		<h2>IT system maintenance</h2>
+		<p>Your IT supplier will probably offer you a range of <strong>hardware-maintenance contracts</strong> - perhaps as part of an overall support contract.</p>
+		<p>Hardware failure is just one of many problems that could occur and you need to consider a range of potential risks carefully before making a decision. These include:</p>
+		<ul>
+			<li>theft of hardware</li>
+			<li>physical damage</li>
+			<li>intellectual property loss from hacking or human error</li>
+			<li>system failure</li>
+		</ul>
+
+		<p>You can also <a href="/it-and-ecommerce/it-security-and-risks/assess-and-minimise-your-it-risks.php">assess and minimise the IT risks facing your business with our IT risk assessment tool</a>.</p>
+		<p><strong>System maintenance and management</strong> are essential parts of managing these risks. Many IT suppliers can monitor problems remotely and take action to prevent a system failure.</p>
+		<p>Many different types of maintenance contract are available. Most are on-site contracts where an engineer comes to your premises to fix the problem. However, some contracts still operate on a return-to-depot basis where you are required to return the equipment for repair. Costs vary considerably depending on the minimum response time and working hours you need.</p>
+		<p>Contracts may seem expensive, particularly for standard PCs and peripherals with extended on-site warranties. After assessing the risks, you may decide not to get a maintenance contract and simply hold spares or purchase new parts when necessary.</p>
+		<p>However, the value of a contract is that someone else takes the responsibility for system maintenance but some suppliers may be reluctant to offer helpdesk, software support and other services unless they're also maintaining the hardware.</p>
+		
+		<h3>Software maintenance contracts</h3>
+		<p>You should ensure any new software purchases are compatible with your existing equipment and operating system.</p>
+		<p>Many IT software suppliers offer maintenance contracts when you purchase their products. These can include recurring licensing fees, product upgrades, new versions, and patches and bug fixes. Some vendors charge for these, while others will include them for free with your purchase. You should also look for a supplier who gives you the original disks and licences if your software is coming pre-installed.</p>
+		<p>If you don't have in-house experience, then look for a supplier that offers maintenance. Costs will vary depending on the type of software you're using, but the general rule of thumb is to have costs below 15 per cent of the purchase price. If using <strong>cloud computing</strong>, maintenance and other upgrades will be managed 'off site' by the service provider. For more information see our guide on <a href="/it-and-ecommerce/software-and-business-applications/cloud-computing.php">cloud computing</a>.</p>
+		
+		<ul>
+			<li><a href=/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/selecting-the-right-it-supplier.php" title="Selecting the right IT supplier">Prev</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-support-services.php" title="IT support services">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/suppliers/choose-and-manage-your-it-supplier.php" title="Choose and manage your IT supplier">Introduction</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/the-different-types-of-it-suppliers.php">The different types of IT suppliers</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/identifying-and-contacting-potential-it-suppliers.php">Identifying and contacting potential IT suppliers</a></li>
+			<li><a href=/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/selecting-the-right-it-supplier.php">Selecting the right IT supplier</a></li>
+			<li>IT system maintenance</li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-support-services.php">IT support services</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-cost-evaluation.php">IT system cost evaluation</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/negotiating-the-contract-for-your-it-system.php">Negotiating the contract for your IT system</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/benefits-of-an-effective-it-supplier-relationship.php">Benefits of an effective IT supplier relationship</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/checklist-choosing-an-it-supplier.php">Checklist: choosing an IT supplier</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/it-security-and-risks/assess-and-minimise-your-it-risks.php" title="This interactive tool helps you to deal with the risks facing your IT systems">Assess and minimise the IT risks facing your business with our IT risk assessment tool </a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163006/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101787&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Choose the right IT for your business</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163008/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101793&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Create your first website</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163009/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097102020&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Get started with e-commerce</a></li>
+		</ul>
+
+		<p>
+			Developed with:<br />
+			<a href="https://www.nb2bc.co.uk/" title="The National B2B Centre - Opens in a new window" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/logo_b2b.gif" width="60" height="35"  alt="" /> The National B2B Centre
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
