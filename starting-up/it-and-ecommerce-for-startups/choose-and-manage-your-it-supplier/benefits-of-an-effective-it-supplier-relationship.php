8f09a4e908da9f0636ead8418fe583f865f8d113
Caroline Hagan <info@blueocto.co.uk>
2017-08-07 20:46:52 +0100

    Completed starting-up > it-and-ecommerce-for-startups > choose-and-manage-your-it-supplier > “Benefits of an effective IT supplier relationship” page.

---
+++ starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/benefits-of-an-effective-it-supplier-relationship.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Benefits of an effective IT supplier relationship | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+		<h1>Choose and manage your IT supplier</h1>
+		<h2>Benefits of an effective IT supplier relationship</h2>
+		<p>Building a <strong>successful relationship</strong> with your IT supplier is essential to your system's success.</p>
+		<p>Trying to purchase IT systems and services at the lowest possible cost - with no regard to your business goals for the project or the overall cost of ownership - is likely to lead to a strained relationship with your supplier and cause both system and business problems.</p>
+		<p>Ideally, your supplier should have a clear view of your business goals and the role of IT in achieving these. This will help you build a relationship that can deliver a <strong>number of benefits</strong> including:</p>
+		<ul>
+			<li>ensuring service-level agreements are met and that system availability is high</li>
+			<li>speedy resolution of problems or queries</li>
+			<li>valuable guidance from the supplier on your business' future IT direction through their knowledge of your industry sector and the latest IT developments</li>
+			<li>delivery of the anticipated business benefits</li>
+		</ul>
+
+		<p>To build a successful relationship with any IT supplier you should be clear from the outset about what you both <strong>expect from the arrangement</strong>. Make sure:</p>
+		<ul>
+			<li>you both have clear expectations of exactly what you'll get in terms of hardware, software and services</li>
+			<li>the business relationship is mutually beneficial - your supplier will have no interest in whether you achieve your business goals if you just want goods as cheaply as possible</li>
+			<li>there are specific targets to be reached at set dates representing what your business gets out of its IT systems - not the delivery of boxes and cables</li>
+			<li>the supplier provides regular reports of progress towards the agreed business objectives for the system</li>
+			<li>you hold regular reviews with your supplier to see how things are going</li>
+			<li>you have enough flexibility to accommodate change - whether this is through new technologies, the business environment or refined business goals</li>
+			<li>you have a signed contract which you both agree to, that includes details of each party's obligations and what should happen in the event of a disagreement or dispute</li>
+		</ul>
+
+		<ul>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/negotiating-the-contract-for-your-it-system.php" title="Negotiating the contract for your IT system">Prev</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/checklist-choosing-an-it-supplier.php" title="Checklist: choosing an IT supplier">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/suppliers/choose-and-manage-your-it-supplier.php" title="Choose and manage your IT supplier">Introduction</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/the-different-types-of-it-suppliers.php">The different types of IT suppliers</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/identifying-and-contacting-potential-it-suppliers.php">Identifying and contacting potential IT suppliers</a></li>
+			<li><a href=/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/selecting-the-right-it-supplier.php">Selecting the right IT supplier</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-maintenance.php">IT system maintenance</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-support-services.php">IT support services</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-cost-evaluation.php">IT system cost evaluation</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/negotiating-the-contract-for-your-it-system.php">Negotiating the contract for your IT system</a></li>
+			<li>Benefits of an effective IT supplier relationship</li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/checklist-choosing-an-it-supplier.php">Checklist: choosing an IT supplier</a></li>
+		</ul>
+
+		
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/introduction-to-it/plan-your-it.php" title="How your business can ensure that it gets real benefits from the introduction and development of IT">Plan your IT</a></li>
+			<li><a href="/grow-your-business/growth-through-strategic-sourcing/manage-your-suppliers.php" title="How to get the most from your suppliers">Manage your suppliers</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163006/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101787&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Choose the right IT for your business</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163008/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101793&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Create your first website</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163009/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097102020&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Get started with e-commerce</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+		<p>
+			Developed with:<br />
+			<a href="https://www.nb2bc.co.uk/" title="The National B2B Centre - Opens in a new window" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/logo_b2b.gif" width="60" height="35"  alt="" /> The National B2B Centre
+			</a>
+		</p>
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
