5808fefb50b9fddf18459efd29a67b8ba8c39ba1
Caroline Hagan <info@blueocto.co.uk>
2017-08-07 20:19:29 +0100

    Completed starting-up > it-and-ecommerce-for-startups > choose-and-manage-your-it-supplier > “IT support services” page.

---
+++ starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-support-services.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>IT support services | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+		<h1>Choose and manage your IT supplier</h1>
+		<h2>IT support services</h2>
+		<p>Consider carefully exactly how much IT support you need. This depends on how much IT expertise you have <strong>in-house</strong>, how important it is to keep systems running smoothly, and how much you can sensibly afford for a support contract.</p>
+		<p>Services to consider include:</p>
+		<ul>
+			<li><strong>Installation and configuration</strong> - will you need the supplier to install and configure hardware and software so that it's fully functioning and ready to use?</li>
+			<li><strong>Training</strong> - will your staff require training?</li>
+			<li><strong>Telephone support</strong> - will your staff require telephone support, and during what hours? Does the supplier provide a helpdesk? Do they have a guaranteed response time? Will you be paying per call, or a fixed sum? Are you restricted to a single caller, or will they accept calls from different members of staff? What percentage of problems does the supplier resolve on the first call - a first-time fix? Is this support provided from the UK or overseas? If it is overseas-based - does this impact upon when it is available because of time differences?</li>
+			<li><strong>Email support</strong> - will email support be adequate for some or all of your needs? What's the supplier's guaranteed response time for email messages?</li>
+			<li><strong>Online support</strong> - does the supplier offer real-time online chat or other support technology? Is there a peer support forum where other users can suggest fixes?</li>
+			<li><strong>Site visits</strong> - does the supplier provide on-site support? If so, how much does it cost?</li>
+		</ul>
+
+		<ul>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-maintenance.php" title="IT system maintenance">Prev</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-cost-evaluation.php" title="IT system cost evaluation">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/suppliers/choose-and-manage-your-it-supplier.php" title="Choose and manage your IT supplier">Introduction</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/the-different-types-of-it-suppliers.php">The different types of IT suppliers</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/identifying-and-contacting-potential-it-suppliers.php">Identifying and contacting potential IT suppliers</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/selecting-the-right-it-supplier.php">Selecting the right IT supplier</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-maintenance.php">IT system maintenance</a></li>
+			<li>IT support services</li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-cost-evaluation.php">IT system cost evaluation</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/negotiating-the-contract-for-your-it-system.php">Negotiating the contract for your IT system</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/benefits-of-an-effective-it-supplier-relationship.php">Benefits of an effective IT supplier relationship</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/checklist-choosing-an-it-supplier.php">Checklist: choosing an IT supplier</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/introduction-to-it/computer-software-the-basics.php">Computer software: the basics</a></li>
+			<li><a href="/it-and-ecommerce/introduction-to-it/computer-hardware-the-basics.php">Computer hardware: the basics</a></li>
+			<li><a href="/it-and-ecommerce/software-and-business-applications/open-source-software.php">Open source software</a></li>
+			<li><a href="/it-and-ecommerce/software-and-business-applications/cloud-computing.php">Cloud computing</a></li>
+			<li><a href="/it-and-ecommerce/employees-and-it/basic-it-skills.php">Basic IT skills</a></li>
+			<li><a href="/it-and-ecommerce/employees-and-it/it-support-for-your-staff.php" title="How to ensure that your staff receive the correct level of training and support to use IT systems effectively">IT support for your staff</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163006/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101787&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Choose the right IT for your business</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163008/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101793&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Create your first website</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163009/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097102020&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Get started with e-commerce</a></li>
+		</ul>
+
+		<p>
+			Developed with:<br />
+			<a href="https://www.nb2bc.co.uk/" title="The National B2B Centre - Opens in a new window" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/logo_b2b.gif" width="60" height="35"  alt="" /> The National B2B Centre
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
