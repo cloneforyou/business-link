084babf3bfa0118238d5fb7ab900bacb11c0792e
Caroline Hagan <info@blueocto.co.uk>
2017-08-07 19:49:03 +0100

    Completed starting-up > it-and-ecommerce-for-startups > choose-and-manage-your-it-supplier > “The different types of IT suppliers” page.

---
+++ starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/the-different-types-of-it-suppliers.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>The different types of IT suppliers | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Choose and manage your IT supplier</h1>
+		<h2>The different types of IT suppliers</h2>
+		<p>Though most IT suppliers can provide a comprehensive range of hardware, software and services, there are some differences between them. For example:</p>
+		<ul>
+			<li><strong>resellers</strong> act as agents for hardware manufacturers, but can also offer software and a wide range of IT services and support</li>
+			<li><strong>system integrators</strong> select the appropriate hardware and software for your specific needs and deliver an integrated, working system</li>
+			<li><strong>specialist suppliers</strong> have expertise in one specific area, such as customer-relationship management systems</li>
+			<li><strong>consultants</strong> may just provide consultancy, with no hardware, software or maintenance services</li>
+			<li><strong>retail, mail order</strong> and <strong>online suppliers</strong> may be appropriate if you're confident you don't need advice and simply need someone to supply the IT system</li>
+		</ul>
+
+		<h3>Size of supplier</h3>
+		<p>IT suppliers range from small local outfits to global organisations. Even the largest suppliers can provide systems, services and consultancy to small businesses.</p>
+		<p>You may rely on your supplier for a system that's central to your business success. You need complete confidence in them. A local business, or a larger company with a local office, can be a reassuring presence.</p>
+		
+		<h3>Single source or multiple suppliers</h3>
+		<p>Unless you have extensive IT expertise in-house, it's generally best to use a single supplier for all hardware, software, services and support.</p>
+		<p>If you buy from multiple suppliers, you'll have to decide which one is responsible when there's a problem - unless you have a service provider prepared to support your whole system.</p>
+		<ul>
+			<li><a href="/it-and-ecommerce/suppliers/choose-and-manage-your-it-supplier.php" title="Choose and manage your IT supplier">Prev</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/identifying-and-contacting-potential-it-suppliers.php" title="Identifying and contacting potential IT suppliers">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/suppliers/choose-and-manage-your-it-supplier.php" title="Choose and manage your IT supplier">Introduction</a></li>
+			<li>The different types of IT suppliers</li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/identifying-and-contacting-potential-it-suppliers.php">Identifying and contacting potential IT suppliers</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/selecting-the-right-it-supplier.php">Selecting the right IT supplier</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-maintenance.php">IT system maintenance</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-support-services.php">IT support services</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-cost-evaluation.php">IT system cost evaluation</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/negotiating-the-contract-for-your-it-system.php">Negotiating the contract for your IT system</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/benefits-of-an-effective-it-supplier-relationship.php">Benefits of an effective IT supplier relationship</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/checklist-choosing-an-it-supplier.php">Checklist: choosing an IT supplier</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/introduction-to-it/setting-up-a-basic-it-system.php">Use our interactive tool to find out which computer equipment you should buy for your business</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/introduction-to-it/plan-your-it.php">Plan your IT</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163006/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101787&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Choose the right IT for your business</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163008/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101793&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Create your first website</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163009/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097102020&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Get started with e-commerce </a></li>
+		</ul>
+
+		<p>
+			Developed with:<br />
+			<a href="https://www.nb2bc.co.uk/" title="The National B2B Centre - Opens in a new window" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/logo_b2b.gif" width="60" height="35"  alt="" /> The National B2B Centre
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
