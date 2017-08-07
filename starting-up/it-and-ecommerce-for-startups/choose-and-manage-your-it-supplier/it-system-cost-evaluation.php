63d0ad3e3a519aa984f0c487fb40170952aced23
Caroline Hagan <info@blueocto.co.uk>
2017-08-07 20:32:34 +0100

    Completed starting-up > it-and-ecommerce-for-startups > choose-and-manage-your-it-supplier > “IT system cost evaluation” page.

---
+++ starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-cost-evaluation.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>IT system cost evaluation | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+		<h1>Choose and manage your IT supplier</h1>
+		<h2>IT system cost evaluation</h2>
+		<p><strong>System management</strong> is usually the biggest cost over the lifetime of an IT system. It includes installation, upgrades, expansion, maintenance, support, training and the time your staff spend dealing with system problems.</p>
+		<p>So it's essential to consider full system lifecycle costs. Saving on purchase costs could result in significant additional costs on an ongoing basis, while cutting back on services could waste a fortune in staff time or lost business.</p>
+		<p><strong>Initial costs</strong> could include:</p>
+		<ul>
+			<li>consultancy</li>
+			<li>hardware costs</li>
+			<li>software licence fees</li>
+			<li>software configuration and customisation work</li>
+			<li>installation</li>
+			<li>staff training</li>
+			<li>providing necessary infrastructure such as network cabling or an electricity supply</li>
+			<li>compliance with health and safety law relating to computer use</li>
+		</ul>
+
+		<p><strong>Ongoing IT management costs</strong> could include:</p>
+		<ul>
+			<li>software support contracts - fees for licence renewals and upgrades</li>
+			<li>hardware upgrades</li>
+			<li>expansion of memory, disk size, communications capacity</li>
+			<li>support - telephone, email, online, on site</li>
+			<li>maintenance and replacements</li>
+			<li>compliance with health and safety legislation relating to computer use</li>
+			<li>staff training</li>
+			<li>costs of consumables, such as printer cartridges, ink and paper</li>
+			<li>communications charges - telephone costs, internet access and subscription charges</li>
+		</ul>
+
+		<p>Agreeing a budget and contract with a supplier can be a lengthy process. You need to specify every aspect of the project in as much detail as possible. This will help to control costs and highlight any problems you encounter during the project.</p>
+		<p>When you negotiate a contract for IT systems or services with your supplier, they will usually expect payment upon delivery of the system. However, it is worthwhile seeking to extend the payment period until the system is actually installed and working to your satisfaction.</p>
+		
+		<ul>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-support-services.php" title="IT support services">Prev</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/negotiating-the-contract-for-your-it-system.php" title="Negotiating the contract for your IT system">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/suppliers/choose-and-manage-your-it-supplier.php" title="Choose and manage your IT supplier">Introduction</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/the-different-types-of-it-suppliers.php">The different types of IT suppliers</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/identifying-and-contacting-potential-it-suppliers.php">Identifying and contacting potential IT suppliers</a></li>
+			<li><a href=/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/selecting-the-right-it-supplier.php">Selecting the right IT supplier</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-maintenance.php">IT system maintenance</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-support-services.php">IT support services</a></li>
+			<li>IT system cost evaluation</li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/negotiating-the-contract-for-your-it-system.php">Negotiating the contract for your IT system</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/benefits-of-an-effective-it-supplier-relationship.php">Benefits of an effective IT supplier relationship</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/checklist-choosing-an-it-supplier.php">Checklist: choosing an IT supplier</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.learndirect.com/store/office-computing" target="_blank" rel="noopener noreferrer">Online IT training courses on the learndirect website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/introduction-to-it/computer-software-the-basics.php" title="The different types of software available and getting the right programs for your business needs">Computer software: the basics</a></li>
+			<li><a href="/it-and-ecommerce/software-and-business-applications/open-source-software.php" title="Introduction to what open source software is and why you might consider using it in your business">Open source software</a></li>
+			<li><a href="/it-and-ecommerce/software-and-business-applications/cloud-computing.php" title="How businesses can use cloud computing to cut IT costs and innovate more quickly">Cloud computing</a></li>
+			<li><a href="/it-and-ecommerce/introduction-to-it/computer-hardware-the-basics.php" title="How to assess your IT needs and get the right equipment to meet your business requirements">Computer hardware: the basics</a></li>
+			<li><a href="/it-and-ecommerce/employees-and-it/basic-it-skills.php" title="How to equip your staff with the necessary skills to use IT effectively">Basic IT skills</a></li>
+			<li><a href="/it-and-ecommerce/employees-and-it/it-support-for-your-staff.php" title="How to ensure that your staff receive the correct level of training and support to use IT systems effectively">IT support for your staff</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163006/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101787&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Choose the right IT for your business</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163008/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101793&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Create your first website</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163009/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097102020&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Get started with e-commerce</a></li>
+		</ul>
+
+		<p>
+			Developed with:<br />
+			<a href="https://www.nb2bc.co.uk/" title="The National B2B Centre - Opens in a new window" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/logo_b2b.gif" width="60" height="35"  alt="" /> The National B2B Centre
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
