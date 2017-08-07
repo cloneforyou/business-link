6817bea5f88fca5d5233bf630f00030f19f32dcb
Caroline Hagan <info@blueocto.co.uk>
2017-08-07 20:04:51 +0100

    Completed starting-up > it-and-ecommerce-for-startups > choose-and-manage-your-it-supplier > “Identifying and contacting potential IT suppliers” page.

---
+++ starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/identifying-and-contacting-potential-it-suppliers.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Identifying and contacting potential IT suppliers | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+		<h1>Choose and manage your IT supplier</h1>
+		<h2>Identifying and contacting potential IT suppliers</h2>
+		<p>At the outset of any IT system procurement exercise you should <strong>establish and document your requirements</strong>. You can provide potential suppliers with these details. It will also provide a framework to compare the different quotations you may receive.</p>
+		<p>The next stage is to <strong>research the potential solutions and suppliers</strong>. Try to identify three to five suppliers who are able to propose a solution for the budget you have set. Sources of information include:</p>
+		<ul>
+			<li>the internet, including the suppliers' own websites and sites offering independent guidance</li>
+			<li>recommendations from trusted colleagues</li>
+			<li>recommendations from trading and business partners</li>
+			<li>IT seminars and conferences</li>
+			<li>computer trade magazines</li>
+			<li>advice from relevant trade associations and professional bodies</li>
+			<li>IT exhibitions</li>
+		</ul>
+
+		<p>This research may highlight further options which should be reflected in your statement of requirements.</p>
+		<p>Having identified the potential suppliers, you can contact them. Two commonly used approaches are a request for proposal (RFP) or a request for quotation (RFQ).</p>
+		<p>The RFP asks for suppliers to submit formal proposals. This is usually followed by a shortlisting of candidates who are required to provide a series of presentations and/or demonstrations. The RFP should include:</p>
+		<ul>
+			<li>the agreed statement of requirements</li>
+			<li>an indication of what format the proposal should follow</li>
+			<li>an indication of the level of detail required in the proposal</li>
+			<li>the timescales within which the proposal should be submitted</li>
+			<li>the procedure for obtaining any further information from you should the suppliers have any queries</li>
+		</ul>
+
+		<p>The RFQ asks for potential suppliers to competitively provide a cost for the product or service in question. A customer sends solicitations to possible suppliers containing a list of requirements. These can include:</p>
+		<ul>
+			<li>part descriptions, specifications or numbers</li>
+			<li>quantities or volumes</li>
+			<li>a description or drawings of the product needed</li>
+			<li>personnel skills or competencies for the product or service</li>
+			<li>terms and conditions</li>
+			<li>term of the contract</li>
+			<li>any other value-added requirements or terms</li>
+			<li>delivery requirements</li>
+			<li>a draft contract</li>
+		</ul>
+
+		<p><a href="https://www.negotiations.com/articles/procurement-terms/" target="_blank" rel="noopener noreferrer" title="Advice to buyers and sellers on how to get the most from procurement and purchasing processes">Read information about requests for procurement processes on the Negotiations website</a>.</p>
+
+		<ul>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/the-different-types-of-it-suppliers.php" title="The different types of IT suppliers">Prev</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/selecting-the-right-it-supplier.php" title="Selecting the right IT supplier">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/suppliers/choose-and-manage-your-it-supplier.php" title="Choose and manage your IT supplier">Introduction</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/the-different-types-of-it-suppliers.php">The different types of IT suppliers</a></li>
+			<li>Identifying and contacting potential IT suppliers</li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/selecting-the-right-it-supplier.php">Selecting the right IT supplier</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-maintenance.php">IT system maintenance</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-support-services.php">IT support services</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-cost-evaluation.php">IT system cost evaluation</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/negotiating-the-contract-for-your-it-system.php">Negotiating the contract for your IT system</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/benefits-of-an-effective-it-supplier-relationship.php">Benefits of an effective IT supplier relationship</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/checklist-choosing-an-it-supplier.php">Checklist: choosing an IT supplier</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="#" title="A good practice guide for small businesses on purchasing ICT">Download ICT purchasing guidance from the Accredit UK website (PDF, 1.24MB)</a></li>
+			<li><a href="http://www.negotiations.com/articles/procurement-terms" target="_blank" rel="noopener noreferrer" title="Advice to buyers and sellers on how to get the most from procurement and purchasing processes">Requests for procurement processes on the Negotiations website</a></li>
+			<li><a href="/it-and-ecommerce/introduction-to-it/setting-up-a-basic-it-system.php">Use our interactive tool to find out which computer equipment you should buy for your business</a></li>
+		</ul>
+				
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/introduction-to-it/plan-your-it.php" title="How your business can ensure that it gets real benefits from the introduction and development of IT">Plan your IT</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163006/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101787&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Choose the right IT for your business</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163008/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101793&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Create your first website</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163009/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097102020&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Get started with e-commerce</a></li>
+		</ul>
+
+		<p>
+			Developed with:<br />
+			<a href="https://www.nb2bc.co.uk/" title="The National B2B Centre - Opens in a new window" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/logo_b2b.gif" width="60" height="35"  alt="" /> The National B2B Centre
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
