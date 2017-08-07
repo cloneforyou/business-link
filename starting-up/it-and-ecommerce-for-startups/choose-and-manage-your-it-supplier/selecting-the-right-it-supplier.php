63550fab7b06cbc690bfb454132c57caab8e18fa
Caroline Hagan <info@blueocto.co.uk>
2017-08-07 20:07:32 +0100

    Completed starting-up > it-and-ecommerce-for-startups > choose-and-manage-your-it-supplier > “Selecting the right IT supplier” page.

---
+++ starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/selecting-the-right-it-supplier.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Selecting the right IT supplier | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+		<h1>Choose and manage your IT supplier</h1>
+		<h2>Selecting the right IT supplier</h2>
+		<p>The actual process of selecting a supplier will normally follow on from a request for a number of suppliers to submit proposals.</p>
+		<p><strong>Review the proposals</strong> you have received in detail and come up with a shortlist.</p>
+		<p>Compare proposals on a like-for-like basis. Take into account:</p>
+		<ul>
+			<li>financial viability</li>
+			<li>implementation track record</li>
+			<li>quality and relevance of reference sites</li>
+			<li>project management capability</li>
+			<li>ability to understand your business at both the strategic and process levels</li>
+			<li>technical knowledge and expertise</li>
+			<li>confidence that the supplier will be able to provide the solution into the foreseeable future</li>
+			<li>the cost of the proposed solution</li>
+		</ul>
+
+		<p>Throughout the evaluation process, <strong>rank the suppliers</strong> and eliminate those that do not meet your technical requirements or fall outside your agreed budget.</p>
+		<p>This process should usually include a <strong>demonstration</strong> of their proposed offering by each of the suppliers. This gives you the opportunity to evaluate the system being proposed and the supplier.</p>
+		<p>Invite the potential suppliers to <strong>visit</strong> your company. Do they understand your business needs and have experience of your industry? Do you feel comfortable dealing with them?</p>
+		<p>Ensure that the shortlisted suppliers provide <strong>references</strong> and contact them as part of the evaluation process. Try to obtain references from businesses similar to your own as this is a useful way to assess the system and the supplier.</p>
+		<p>You should then be in a position to come up with a single preferred supplier. Consider carrying out further <strong>testing of the proposed solution</strong>, particularly if the system is going to be critical to your business.</p>
+		<p>If you are happy with the proposed solution of the preferred supplier, you can move to the contract stage. You will need to formally agree on factors such as the levels of system maintenance and support, as well as the overall cost of the proposed solution.</p>
+
+		<ul>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/identifying-and-contacting-potential-it-suppliers.php" title="Identifying and contacting potential IT suppliers">Prev</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-maintenance.php" title="IT system maintenance">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/suppliers/choose-and-manage-your-it-supplier.php" title="Choose and manage your IT supplier">Introduction</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/the-different-types-of-it-suppliers.php">The different types of IT suppliers</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/identifying-and-contacting-potential-it-suppliers.php">Identifying and contacting potential IT suppliers</a></li>
+			<li>Selecting the right IT supplier</li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-maintenance.php">IT system maintenance</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-support-services.php">IT support services</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/it-system-cost-evaluation.php">IT system cost evaluation</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/negotiating-the-contract-for-your-it-system.php">Negotiating the contract for your IT system</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/benefits-of-an-effective-it-supplier-relationship.php">Benefits of an effective IT supplier relationship</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/choose-and-manage-your-it-supplier/checklist-choosing-an-it-supplier.php">Checklist: choosing an IT supplier</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="#" title="A good practice guide for small businesses on purchasing ICT">Download ICT purchasing guidance from the Accredit UK website (PDF, 1.24MB)</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/introduction-to-it/plan-your-it.php">Plan your IT</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163006/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101787&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Choose the right IT for your business</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163008/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101793&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Create your first website</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163009/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097102020&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Get started with e-commerce </a></li>
+		</ul>
+
+		<p>
+			Developed with:<br />
+			<a href="https://www.nb2bc.co.uk/" title="The National B2B Centre - Opens in a new window" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/logo_b2b.gif" width="60" height="35"  alt="" /> The National B2B Centre
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
