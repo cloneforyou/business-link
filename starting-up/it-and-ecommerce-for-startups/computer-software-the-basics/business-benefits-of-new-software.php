cab4b9a735d03d2a0e4887a8cbb22f4273fc7630
Caroline Hagan <info@blueocto.co.uk>
2017-08-08 16:48:48 +0100

    Completed starting-up > it-and-ecommerce-for-startups > computer-software-the-basics > “Business benefits of new software” page.

---
+++ starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/business-benefits-of-new-software.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Business benefits of new software | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Computer software: the basics</h1>
+		<h2>Business benefits of new software</h2>
+		<p>Before investing in new software, you should think about what you want it to do for you.</p>
+		<p>For example, you might want to:</p>
+		<ul>
+			<li>cut costs by automating routine tasks</li>
+			<li>improve customer service - eg with an interactive online contact system for after-sales support or to check order status</li>
+			<li>enable your employees to work more efficiently</li>
+			<li>communicate and collaborate electronically with suppliers or partners</li>
+		</ul>
+
+		<p>You should take time to talk to your employees, suppliers and customers who may have ideas for improving your business processes using IT.</p>
+		<p>It will be helpful to write down the objectives and potential benefits of new software, prioritising the list to work out the best returns on investment. See the page in this guide on <a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/choosing-and-buying-software.php">choosing and buying software</a>.</p>
+		<p>Choose software that will run on your current hardware, as long as this doesn't reduce potential benefits. You should include any hardware upgrade costs in your budgets - see our guide to <a href="/it-and-ecommerce/introduction-to-it/computer-hardware-the-basics.php">computer hardware: the basics</a>.</p>
+		<p>Alternatively, look at outsourcing your software requirements - eg cloud computing - which could help reduce both your software and hardware costs. See the page in this guide on <a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/cloud-computing-software-as-a-service.php">cloud computing - software as a service</a> and our guide on <a href="/it-and-ecommerce/software-and-business-applications/cloud-computing.php">cloud computing</a>.</p>
+		
+		<h3>Planning for the future</h3>
+		<p>Ideally you should develop a long-term strategy, covering your future IT needs. This should take into account potential changes in customers, staffing levels and/or your products and services.</p>
+		
+		<ul>
+			<li><a href="/it-and-ecommerce/introduction-to-it/computer-software-the-basics.php" title="Computer software: the basics">Prev</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/types-of-software.php" title="Types of software">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/introduction-to-it/computer-software-the-basics.php" title="Computer software: the basics">Introduction</a></li>
+			<li>Business benefits of new software</li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/types-of-software.php">Types of software</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/open-source-and-proprietary-software.php">Open source and proprietary software</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/licences-for-proprietary-software.php">Licences for proprietary software</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/cloud-computing-software-as-a-service.php">Cloud computing - software as a service </a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/choosing-and-buying-software.php">Choosing and buying software</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/software-installation-activation-and-upgrades.php">Software installation, activation and upgrades</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/software-support.php">Software support</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/it-and-ecommerce/introduction-to-it/computer-hardware-the-basics.php">Computer hardware: the basics</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php">Prepare a business plan</a></li>
+			<li><a href="/it-and-ecommerce/software-and-business-applications/cloud-computing.php">Cloud computing</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163006/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101787&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Choose the right IT for your business</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163008/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101793&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Create your first website</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163009/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097102020&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Get started with e-commerce</a></li>
+		</ul>
+
+		<p>
+			Developed with:<br />
+			<a href="https://www.nb2bc.co.uk/" title="The National B2B Centre - Opens in a new window" target="_blank" rel="noopener noreferrer">
+				<img src="../../../_img/logo_b2b.gif" width="60" height="35"  alt="" /> The National B2B Centre
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
