44c964a74662ba7030ec1c2a3d0925ca0d44b172
Caroline Hagan <info@blueocto.co.uk>
2017-08-26 21:37:16 +0100

    Completed “starting-up > it-and-ecommerce-for-startups > computer-software-the-basics > Software support” page.

--- starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/software-support.php
+++ starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/software-support.php
@@ -22,11 +22,11 @@
 		<p>How much the support <strong>costs</strong> is important. If software is faulty, do you still have to pay to have any problems fixed? You might have to, particularly if support is on a premium-rate phone line.</p>
 		<p>You may get a period of free technical support while you install your software. However, software support is usually charged for after a certain period of time or after a certain number of queries. Check the cost carefully.</p>
 		<p>More importantly, suppliers may stop offering support for older versions of software. This is a very important issue when updates are needed to keep software secure.</p>
-		<p>If your business depends on your software, fixing problems quickly may be vital. If this is the case, you will need a maintenance contract, but always read the small print. See our guide on how to <a href="layer1a6f.html?topicId=1074003275" title="The importance of choosing the right IT supplier and developing an effective working relationship with them">choose and manage your IT supplier</a>.</p>
+		<p>If your business depends on your software, fixing problems quickly may be vital. If this is the case, you will need a maintenance contract, but always read the small print. See our guide on how to <a href="/it-and-ecommerce/suppliers/choose-and-manage-your-it-supplier.php">choose and manage your IT supplier</a>.</p>
 		<p>Look around for the best deal, but make sure that the supplier you choose can deliver on its promises - a cheap supplier may not be very responsive and end up costing you more money, while a more expensive supplier may be cheaper in the long run if they can fix problems within hours rather than days.</p>
 		
 		<ul>
-			<li><a href="detail8d4c.html?itemId=1073791280&amp;type=RESOURCES" title="Software installation, activation and upgrades">Prev</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/software-installation-activation-and-upgrades.php" title="Software installation, activation and upgrades">Prev</a></li>
 		</ul>
 		
 		<h3>Subjects covered in this guide</h3>
@@ -34,36 +34,35 @@
 			<li><a href="/it-and-ecommerce/introduction-to-it/computer-software-the-basics.php" title="Computer software: the basics">Introduction</a></li>
 			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/business-benefits-of-new-software.php">Business benefits of new software</a></li>
 			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/types-of-software.php">Types of software</a></li>
-			<li><a href="detail2575.html?itemId=1073791277&amp;type=RESOURCES">Open source and proprietary software</a></li>
-			<li><a href="detail12c1.html?itemId=1073791278&amp;type=RESOURCES">Licences for proprietary software</a></li>
-			<li><a href="detailccf0.html?itemId=1084673576&amp;type=RESOURCES" title="Cloud computing - software as a service ">Cloud computing - software as a service </a></li>
-			<li><a href="detailbeeb.html?itemId=1073791279&amp;type=RESOURCES">Choosing and buying software</a></li>
-			<li><a href="detail8d4c.html?itemId=1073791280&amp;type=RESOURCES">Software installation, activation and upgrades</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/open-source-and-proprietary-software.php">Open source and proprietary software</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/licences-for-proprietary-software.php">Licences for proprietary software</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/cloud-computing-software-as-a-service.php">Cloud computing - software as a service</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/choosing-and-buying-software.php">Choosing and buying software</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/software-installation-activation-and-upgrades.php">Software installation, activation and upgrades</a></li>
 			<li>Software support</li>
 		</ul>
 
-		<p><img src="../../../_img/hr_325px_global.gif" alt="" /></p>
-		
 		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
 		
 		<h3>Also on this site</h3>
 		<ul>
-			<li><a href="layer1a6f.html?topicId=1074003275" title="The importance of choosing the right IT supplier and developing an effective working relationship with them">Choose and manage your IT supplier</a></li>
-			<li><a href="layera8be.html?topicId=1074458463" title="Planning ahead to minimise the impact of crises on your business - from flood and fire to illness and IT failure">Crisis management and business continuity planning</a></li>
-			<li><a href="layer5a02.html?topicId=1075689112" title="How to ensure that your staff receive the correct level of training and support to use IT systems effectively">IT support for your staff</a></li>
+			<li><a href="/it-and-ecommerce/suppliers/choose-and-manage-your-it-supplier.php">Choose and manage your IT supplier</a></li>
+			<li><a href="/grow-your-business/managing-risk/crisis-management-and-business-continuity-planning.php">Crisis management and business continuity planning</a></li>
+			<li><a href="/it-and-ecommerce/employees-and-it/it-support-for-your-staff.php">IT support for your staff</a></li>
 			<li><a href="http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101787&amp;type=HUBCHASSIS" title="How to choose the right IT for your business">Choose the right IT for your business</a></li>
 			<li><a href="http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101793&amp;type=HUBCHASSIS" title="Find information on how to create your first website">Create your first website</a></li>
 			<li><a href="http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097102020&amp;type=HUBCHASSIS" title="Learn how to create an online shop and trade successfully on the internet">Get started with e-commerce </a></li>
 		</ul>
 
-		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
-		
 		<p>
 			Developed with:<br />
 			<a href="https://www.nb2bc.co.uk/" title="The National B2B Centre - Opens in a new window" target="_blank" rel="noopener noreferrer">
-				<img src="../../_img/logo_b2b.gif" width="60" height="35"  alt="" /> The National B2B Centre
+				<img src="../../../_img/logo_b2b.gif" width="60" height="35" alt="" /> The National B2B Centre
 			</a>
 		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
 
 	</div><!-- // .small-12 large-8 large-push-4 columns -->
 
