1699532dd2911ade2ff0503ce660bd98dca9b6d8
Caroline Hagan <info@blueocto.co.uk>
2017-08-26 20:22:18 +0100

    Completed “starting-up > it-and-ecommerce-for-startups > computer-software-the-basics > Cloud computing - software as a service” page.

--- starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/cloud-computing-software-as-a-service.php
+++ starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/cloud-computing-software-as-a-service.php
@@ -15,11 +15,13 @@
 	<div class="small-12 large-8 large-push-4 columns">
 
 		<h1>Computer software: the basics</h1>
-		<h2>Cloud computing - software as a service </h2>
+		<h2>Cloud computing - software as a service</h2>
+
 		<p>Cloud computing lets you file and access documents, emails, customer information, business applications and other assets are all stored online - 'in the cloud'. This makes them accessible from any computer or mobile device with an internet connection and a web browser.</p>
-		<p>Cloud computing is very flexible. It can help you make the most of a limited IT budget, while giving you access to the latest technology and professional support. However, there are also some potential disadvantages it is important to consider before making a final decision. For more information see our guide on <a href="/it-and-ecommerce/software-and-business-applications/cloud-computing.php" title="How businesses can use cloud computing to cut IT costs and innovate more quickly">cloud computing</a>.</p>
+		<p>Cloud computing is very flexible. It can help you make the most of a limited IT budget, while giving you access to the latest technology and professional support. However, there are also some potential disadvantages it is important to consider before making a final decision. For more information see our guide on <a href="/it-and-ecommerce/software-and-business-applications/cloud-computing.php">cloud computing</a>.</p>
+		
 		<ul>
-			<li><a href="detail12c1.html?itemId=1073791278&amp;type=RESOURCES" title="Licences for proprietary software">Prev</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/licences-for-proprietary-software.php" title="Licences for proprietary software">Prev</a></li>
 			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/choosing-and-buying-software.php" title="Choosing and buying software">Next</a></li>
 		</ul>
 
@@ -28,42 +30,40 @@
 			<li><a href="/it-and-ecommerce/introduction-to-it/computer-software-the-basics.php" title="Computer software: the basics">Introduction</a></li>
 			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/business-benefits-of-new-software.php">Business benefits of new software</a></li>
 			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/types-of-software.php">Types of software</a></li>
-			<li><a href="detail2575.html?itemId=1073791277&amp;type=RESOURCES">Open source and proprietary software</a></li>
-			<li><a href="detail12c1.html?itemId=1073791278&amp;type=RESOURCES">Licences for proprietary software</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/open-source-and-proprietary-software.php">Open source and proprietary software</a></li>
+			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/licences-for-proprietary-software.php">Licences for proprietary software</a></li>
 			<li>Cloud computing - software as a service </li>
 			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/choosing-and-buying-software.php">Choosing and buying software</a></li>
 			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/software-installation-activation-and-upgrades.php">Software installation, activation and upgrades</a></li>
 			<li><a href="/starting-up/it-and-ecommerce-for-startups/computer-software-the-basics/software-support.php">Software support</a></li>
 		</ul>
 
-		<p><img src="../../../_img/hr_325px_global.gif" alt="" /></p>
-		
 		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
 
 		<h3>Actions</h3>
 		<ul>
-			<li><a href="http://www.allthingscrm.com/crm-basics/why-you-should-know-about-cloud-computing.html" target="bgExternalonlinebusinesslinkgovuk" title="Hardware and software needed for cloud computing">Cloud computing principles explained on the AllThingsCRM website</a></li>
+			<li><a href="https://www.salesforce.com/cloudcomputing/" target="_blank" rel="noopener noreferrer">What is cloud computing?</a></li>
+			<!-- <li><a href="http://www.allthingscrm.com/crm-basics/why-you-should-know-about-cloud-computing.html" target="bgExternalonlinebusinesslinkgovuk" title="Hardware and software needed for cloud computing">Cloud computing principles explained on the AllThingsCRM website</a></li> -->
 		</ul>
-		
-		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
-		
+				
 		<h3>Also on this site</h3>
 		<ul>
-			<li><a href="/it-and-ecommerce/software-and-business-applications/cloud-computing.php" title="How businesses can use cloud computing to cut IT costs and innovate more quickly">Cloud computing</a></li>
-			<li><a href="layerea71.html?topicId=1074412952" title="The correct use of personal information under the Data Protection Act 1998">Comply with data protection legislation</a></li>
-			<li><a href="http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101787&amp;type=HUBCHASSIS" title="How to choose the right IT for your business">Choose the right IT for your business</a></li>
-			<li><a href="http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101793&amp;type=HUBCHASSIS" title="Find information on how to create your first website">Create your first website</a></li>
-			<li><a href="http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097102020&amp;type=HUBCHASSIS" title="Learn how to create an online shop and trade successfully on the internet">Get started with e-commerce </a></li>
+			<li><a href="/it-and-ecommerce/software-and-business-applications/cloud-computing.php">Cloud computing</a></li>
+			<li><a href="/it-and-ecommerce/data-protection-and-legal-issues/comply-with-data-protection-legislation.php">Comply with data protection legislation</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163006/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101787&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Choose the right IT for your business</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163008/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101793&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Create your first website</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20111219163009/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097102020&r.i=1073792238&r.l1=1073861197&r.l2=1073858975&r.l3=1074003275&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Get started with e-commerce</a></li>
 		</ul>
 
-		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
-		
 		<p>
 			Developed with:<br />
 			<a href="https://www.nb2bc.co.uk/" title="The National B2B Centre - Opens in a new window" target="_blank" rel="noopener noreferrer">
-				<img src="../../_img/logo_b2b.gif" width="60" height="35"  alt="" /> The National B2B Centre
+				<img src="../../../_img/logo_b2b.gif" width="60" height="35" alt="" /> The National B2B Centre
 			</a>
 		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
 
 	</div><!-- // .small-12 large-8 large-push-4 columns -->
 
