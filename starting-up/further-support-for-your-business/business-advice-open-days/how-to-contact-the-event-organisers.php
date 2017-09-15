c39134820bb1d87b97e4bdfc2ce1114fef1a13c7
Caroline Hagan <info@blueocto.co.uk>
2017-09-15 21:29:52 +0100

    Completed "starting-up > further-support-for-your-business > business-advice-open-days > How to contact the event organisers" page

---
+++ starting-up/further-support-for-your-business/business-advice-open-days/how-to-contact-the-event-organisers.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>How to contact the event organisers | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+
+			<h1>Business Advice Open Days</h1>
+			<h2>How to contact the event organisers</h2>
+			<p>If you have a query concerning any event, you can email the Business Liaison Team at <a href="#">open.days@hmrc.gsi.gov.uk</a>. You can also contact the HMRC Business Advice Open Day Booking Line on Tel 0845 603 2691 to discuss your booking.</p>
+			<p>However, please be aware that the team cannot deal directly with any queries concerning tax (eg VAT, Corporation Tax, National Insurance) or benefits (eg tax credits, child benefit) enquiries.</p>
+			<p>If your queries are about any of these subjects, you can contact specific HM Revenue &amp; Customs (HMRC) helplines. <a href="https://www.gov.uk/government/organisations/hm-revenue-customs/contact" target="_blank" rel="noopener noreferrer">Find a list of HMRC contact telephone numbers on the HMRC website</a>.</p>
+			
+			<h3>How the Business Liaison Team acts on the feedback</h3>
+			<p>All feedback received on Business Advice Open Days is valued and acted upon by the organisers. You can either complete a feedback sheet at the venue at the end of your visit, send your comments by email to <a href="#">open.days@hmrc.gsi.gov.uk</a>.</p>
+			<p>There have been a number of changes made to Business Advice Open Days because of feedback. For example, there is now a dedicated help desk situated at the entrance to all events and seminar programmes have been improved to reflect the topics of greatest interest to visitors.</p>
+			
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/where-to-go-for-advice-if-you-cannot-attend-a-business-advice-open-day.php" title="Where to go for advice if you cannot attend a Business Advice Open Day">Prev</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days.php" title="Business Advice Open Days">Introduction</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/who-should-attend-a-business-advice-open-day-and-why.php">Who should attend a Business Advice Open Day and why</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/organisations-you-can-meet-at-business-advice-open-days.php">Organisations you can meet at Business Advice Open Days</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/booking-your-place-at-a-business-advice-open-day.php">Booking your place at a Business Advice Open Day</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/questions-about-attending-a-business-advice-open-day.php">Questions about attending a Business Advice Open Day</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/where-to-go-for-advice-if-you-cannot-attend-a-business-advice-open-day.php">Where to go for advice if you cannot attend a Business Advice Open Day</a></li>
+				<li>How to contact the event organisers</li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120418183332/http://www.events.businesslink.gov.uk/eventsearch?text=BAOD" target="_blank" rel="noopener noreferrer">How to book a Business Advice Open Day</a></li>
+				<li><a href="https://www.gov.uk/government/organisations/hm-revenue-customs/contact" target="_blank" rel="noopener noreferrer">HMRC contact details</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120418183332/http://www.events.businesslink.gov.uk/eventsearch?text=BAOD" target="_blank" rel="noopener noreferrer">Submit or view feedback on an HMRC Business Advice Open Day event</a></li>
+				<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/tax-reliefs-and-allowances/tax-advantages-for-those-starting-up-in-business.php">Tax advantages for those starting up in business</a></li>
+			</ul>
+
+			<p>Developed with:<br />
+				<a href="http://www.hmrc.gov.uk/" target="_blank" rel="noopener noreferrer" title="HM Revenue &amp; Customs - Opens in a new window">
+					<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" /> HM Revenue &amp; Customs
+				</a>
+			</p>
+
+			<img src="../../syntegra/images/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+	</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
