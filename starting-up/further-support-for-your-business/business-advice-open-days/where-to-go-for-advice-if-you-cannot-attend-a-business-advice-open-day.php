d73c505a5719ba2f85154732a68435f32b8860ce
Caroline Hagan <info@blueocto.co.uk>
2017-09-15 21:23:13 +0100

    Completed "starting-up > further-support-for-your-business > business-advice-open-days > Where to go for advice if you cannot attend a Business Advice Open Day" page

---
+++ starting-up/further-support-for-your-business/business-advice-open-days/where-to-go-for-advice-if-you-cannot-attend-a-business-advice-open-day.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Where to go for advice if you cannot attend a Business Advice Open Day | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+
+			<h1>Business Advice Open Days</h1>
+			<h2>Where to go for advice if you cannot attend a Business Advice Open Day</h2>
+			<p>If you are unable to attend a Business Advice Open Day event, you can contact the HM Revenue &amp; Customs (HMRC) helpline specific to your need. <a href="https://www.gov.uk/government/organisations/hm-revenue-customs/contact" target="_blank" rel="noopener noreferrer">Find a list of HMRC contact telephone numbers on the HMRC website</a>.</p>
+			<p>There are also details of HMRC workshops and events in your area under the relevant sections of the HMRC website.</p>
+			<p>Additionally, you can use the event finder to identify training, learning and networking opportunities of interest.</p>
+			<p><a href="http://webarchive.nationalarchives.gov.uk/20120418183332/http://www.events.businesslink.gov.uk/eventsearch?text=BAOD" target="_blank" rel="noopener noreferrer">Search our events directory of national and local events, workshops and conferences</a>. </p>
+			<p>Or find out <a href="http://webarchive.nationalarchives.gov.uk/20120418183332/http://www.events.businesslink.gov.uk/eventsearch?text=BAOD" target="_blank" rel="noopener noreferrer">How to book a Business Advice Open Day</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/questions-about-attending-a-business-advice-open-day.php" title="Questions about attending a Business Advice Open Day">Prev</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/how-to-contact-the-event-organisers.php" title="How to contact the event organisers">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days.php" title="Business Advice Open Days">Introduction</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/who-should-attend-a-business-advice-open-day-and-why.php">Who should attend a Business Advice Open Day and why</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/organisations-you-can-meet-at-business-advice-open-days.php">Organisations you can meet at Business Advice Open Days</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/booking-your-place-at-a-business-advice-open-day.php">Booking your place at a Business Advice Open Day</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/questions-about-attending-a-business-advice-open-day.php">Questions about attending a Business Advice Open Day</a></li>
+				<li>Where to go for advice if you cannot attend a Business Advice Open Day</li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/how-to-contact-the-event-organisers.php">How to contact the event organisers</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120418183332/http://www.events.businesslink.gov.uk/eventsearch?text=BAOD" target="_blank" rel="noopener noreferrer">How to book a Business Advice Open Day</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120418183332/http://www.events.businesslink.gov.uk/eventsearch?text=BAOD" target="_blank" rel="noopener noreferrer">Find dates and locations for business Advice Open Days</a></li>
+				<li><a href="https://www.gov.uk/government/organisations/hm-revenue-customs/contact" target="_blank" rel="noopener noreferrer">Contact details from HMRC</a></li>
+				<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/tax-reliefs-and-allowances/tax-advantages-for-those-starting-up-in-business.php">Tax advantages for those starting up in business</a></li>
+			</ul>
+
+			<p>Developed with:<br />
+				<a href="http://www.hmrc.gov.uk/" target="_blank" rel="noopener noreferrer" title="HM Revenue &amp; Customs - Opens in a new window">
+					<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" /> HM Revenue &amp; Customs
+				</a>
+			</p>
+
+			<img src="../../syntegra/images/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+	</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
