946705ca27519325f33d4d4fb166c49b69431be2
Caroline Hagan <info@blueocto.co.uk>
2017-09-15 21:19:14 +0100

    Completed "starting-up > further-support-for-your-business > business-advice-open-days > Questions about attending a Business Advice Open Day" page

---
+++ starting-up/further-support-for-your-business/business-advice-open-days/questions-about-attending-a-business-advice-open-day.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Questions about attending a Business Advice Open Day | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+
+			<h1>Business Advice Open Days</h1>
+			<h2>Questions about attending a Business Advice Open Day</h2>
+			<p>Before you attend a Business Advice Open Day, there may be some things you want to know about. Below are some common questions about the events.</p>
+			
+			<h3>Do I have to stay for the whole day?</h3>
+			<p>No - you can drop in for as long as you wish. Choose only the sessions that are relevant to your business and make the most of your time.</p>
+			
+			<h3>Is there free parking at all the events?</h3>
+			<p>Free parking cannot be guaranteed at every event but most venues offer some free parking on a first come, first served basis. If you are a Blue Badge holder please notify the Business Liaison Team and they will liaise with the venue on your behalf to identify the closest suitable facilities.</p>
+			<p>Find Business Liaison Team contact details on the page in this guide on <a href="/starting-up/further-support-for-your-business/business-advice-open-days/how-to-contact-the-event-organisers.php">how to contact the event organisers</a>.</p>
+			<p>The Business Liaison Team tries to identify other parking sites near to the venue if facilities at the venue are limited. You can also find details of these in the appropriate flyer.</p>
+			
+			<h3>Are events accessible to all? </h3>
+			<p>Wherever possible, the open days use a venue with disabled access. Let the event organisers know in advance of any physical or communication difficulties, so that they can arrange to help you on the day.</p>
+			
+			<h3>Getting directions for an event</h3>
+			<p>You can find directions to any Business Advice Open Day event online. Simply click on the event you are interested in, and click on 'directions'.</p>
+			
+			<h3>Is there catering at the event?</h3>
+			<p>Light refreshments are provided at most venues, where the facilities allow. These will be on a cash basis and are likely to be tea, coffee biscuits etc.</p>
+			
+			<h3>Do I need to book in advance?</h3>
+			<p>No - you can drop in on the day but it helps the organisers manage numbers if you can register in advance.</p>
+			
+			<h3>Need further help?</h3>
+			<p>If you have any other specific needs regarding an event you plan to attend, please let the Business Liaison Team know in advance. Find Business Liaison Team contact details on the page in this guide on <a href="/starting-up/further-support-for-your-business/business-advice-open-days/how-to-contact-the-event-organisers.php">how to contact the event organisers</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/booking-your-place-at-a-business-advice-open-day.php" title="Booking your place at a Business Advice Open Day">Prev</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/where-to-go-for-advice-if-you-cannot-attend-a-business-advice-open-day.php" title="Where to go for advice if you cannot attend a Business Advice Open Day">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days.php" title="Business Advice Open Days">Introduction</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/who-should-attend-a-business-advice-open-day-and-why.php">Who should attend a Business Advice Open Day and why</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/organisations-you-can-meet-at-business-advice-open-days.php">Organisations you can meet at Business Advice Open Days</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/booking-your-place-at-a-business-advice-open-day.php">Booking your place at a Business Advice Open Day</a></li>
+				<li>Questions about attending a Business Advice Open Day</li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/where-to-go-for-advice-if-you-cannot-attend-a-business-advice-open-day.php">Where to go for advice if you cannot attend a Business Advice Open Day</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/how-to-contact-the-event-organisers.php">How to contact the event organisers</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120418183332/http://www.events.businesslink.gov.uk/eventsearch?text=BAOD" target="_blank" rel="noopener noreferrer">How to book a Business Advice Open Day</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120418183332/http://www.events.businesslink.gov.uk/eventsearch?text=BAOD" target="_blank" rel="noopener noreferrer">Find dates and locations for Business Advice Open Days</a></li>
+				<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/tax-reliefs-and-allowances/tax-advantages-for-those-starting-up-in-business.php">Tax advantages for those starting up in business</a></li>
+			</ul>
+
+			<p>Developed with:<br />
+				<a href="http://www.hmrc.gov.uk/" target="_blank" rel="noopener noreferrer" title="HM Revenue &amp; Customs - Opens in a new window">
+					<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" /> HM Revenue &amp; Customs
+				</a>
+			</p>
+
+			<img src="../../syntegra/images/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+	</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
