300e14d61dd529dc4c6a6a446e5f0c4ea7a13353
Caroline Hagan <info@blueocto.co.uk>
2017-09-15 21:55:04 +0100

    Completed "starting-up > further-support-for-your-business > extra-help-and-support-available-for-your-start-up > Support for female entrepreneurs" page.

---
+++ starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-female-entrepreneurs.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Support for female entrepreneurs | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+
+			<h1>Extra help and support available for your start up</h1>
+			<h2>Support for female entrepreneurs</h2>
+			<p>There are lots of business organisations aimed specifically at women - at both local and national levels.</p>
+			<p>These include:</p>
+			<p>
+				<ul>
+					<li>the Women in Business Network</li>
+					<li>everywoman</li>
+					<li>Prowess 2.0</li>
+					<li>Prince's Trust - offers mentoring services for young women</li>
+				</ul>
+			</p>
+			<p>However, there are many more who may offer face-to-face networking events and the opportunity to network online.</p>
+			
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/mentoring-networking-finance-and-training.php" title="Mentoring, networking, finance and training">Prev</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-mature-entrepreneurs.php" title="Support for mature entrepreneurs">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up.php" title="Extra help and support available for your start up">Introduction</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/mentoring-networking-finance-and-training.php">Mentoring, networking, finance and training</a></li>
+				<li>Support for female entrepreneurs</li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-mature-entrepreneurs.php">Support for mature entrepreneurs</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-disabled-people.php">Support for disabled people</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-black-asian-and-minority-ethnic-start-ups.php">Support for black, Asian and minority ethnic start ups</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-entrepreneurs-on-low-incomes.php">Support for entrepreneurs on low incomes</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/sources-of-support-for-young-entrepreneurs.php">Sources of support for young entrepreneurs</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>HMRC Newly Self-Employed Helpline</h3>
+			<p>0845 915 4515</p>
+
+			<hr />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://www.wibn.co.uk/" target="_blank" rel="noopener noreferrer">Business support for women on the Women in Business Network website</a></li>
+				<li><a href="https://www.everywoman.com/" target="_blank" rel="noopener noreferrer">Business support for women on the everywoman website</a></li>
+				<li><a href="http://www.prowess.org.uk/" target="_blank" rel="noopener noreferrer">Business support for women on the Prowess website</a></li>
+				<li><a href="http://www.horsesmouth.co.uk/publisha.content/Work/topics.publisha?topic=Starting+a+business" target="_blank" rel="noopener noreferrer">Online business mentoring explained on the horsesmouth website</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120823132835/https://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101805&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Find out about business mentoring</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120823132830/https://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101974&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Get tips on how to network</a></li>
+				<li><a href="https://www.gov.uk/business-finance-support" target="_blank" rel="noopener noreferrer">Find the right support for your new business</a></li>
+			</ul>
+			
+			<p>Developed with:<br />
+				<a href="http://www.berr.gov.uk/" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/logo_primary_bis.gif" width="160" height="32" alt="" /> Department for Business, Innovation &amp; Skills
+				</a>
+			</p>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+			
+
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+	</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
