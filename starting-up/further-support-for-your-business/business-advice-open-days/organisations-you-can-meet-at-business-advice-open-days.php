b9a655c3ecb46cb70fed2e33e3a147647fe0bf57
Caroline Hagan <info@blueocto.co.uk>
2017-09-15 21:07:00 +0100

    Completed "starting-up > further-support-for-your-business > business-advice-open-days > Organisations you can meet at Business Advice Open Days" page

---
+++ starting-up/further-support-for-your-business/business-advice-open-days/organisations-you-can-meet-at-business-advice-open-days.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Organisations you can meet at Business Advice Open Days | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+
+			<h1>Business Advice Open Days</h1>
+			<h2>Organisations you can meet at Business Advice Open Days</h2>
+			<p>At a Business Advice Open Day, you can meet people who have expert knowledge on a variety of business issues - from turning your idea into a successful business, to finding sources of help when you need them.</p>
+			<p>You can also make sure you are complying with the law in key areas such as health and safety, employment law and taxation.</p>
+			<p>The line-up of each event depends on the size of the venue. However, some of the key national partners that you can speak to at many of the open days include:</p>
+			<ul>
+				<li>Business Link</li>
+				<li><a href="http://www.hse.gov.uk/" target="_blank" rel="noopener noreferrer">the Health &amp; Safety Executive</a></li>
+				<li>Acas</li>
+				<li><a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">HM Revenue &amp; Customs (HMRC)</a></li>
+				<li><a href="http://gov.wales/" target="_blank" rel="noopener noreferrer">Welsh Government</a></li>
+				<li><a href="https://www.investni.com/" target="_blank" rel="noopener noreferrer">Invest NI</a></li>
+				<li><a href="http://www.gov.scot/" target="_blank" rel="noopener noreferrer">Scottish Government</a></li>
+				<li><a href="https://web.archive.org/web/20130222115907/http://www.prime.org.uk/" target="_blank" rel="noopener noreferrer">PRIME, the business start-up charity for people over 50</a></li>
+			</ul>
+			<p>As the events are organised by HMRC, there will also be individuals offering advice about all business tax issues.</p>
+			<p>These organisations may not attend all Business Advice Open Days and additional organisations may attend other programmes. You should <a href="http://webarchive.nationalarchives.gov.uk/20120418183332/http://www.events.businesslink.gov.uk/eventsearch?text=BAOD" target="_blank" rel="noopener noreferrer">check the information on the Events Finder website</a> for specific events to see who will be at the Business Advice Open Day you wish to attend.</p>
+			<p>Find out <a href="http://webarchive.nationalarchives.gov.uk/20120418183332/http://www.events.businesslink.gov.uk/eventsearch?text=BAOD" target="_blank" rel="noopener noreferrer">how to book a Business Advice Open Day</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/who-should-attend-a-business-advice-open-day-and-why.php" title="Who should attend a Business Advice Open Day and why">Prev</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/booking-your-place-at-a-business-advice-open-day.php" title="Booking your place at a Business Advice Open Day">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days.php" title="Business Advice Open Days">Introduction</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/who-should-attend-a-business-advice-open-day-and-why.php">Who should attend a Business Advice Open Day and why</a></li>
+				<li>Organisations you can meet at Business Advice Open Days</li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/booking-your-place-at-a-business-advice-open-day.php">Booking your place at a Business Advice Open Day</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/questions-about-attending-a-business-advice-open-day.php">Questions about attending a Business Advice Open Day</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/where-to-go-for-advice-if-you-cannot-attend-a-business-advice-open-day.php">Where to go for advice if you cannot attend a Business Advice Open Day</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/how-to-contact-the-event-organisers.php">How to contact the event organisers</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120418183332/http://www.events.businesslink.gov.uk/eventsearch?text=BAOD" target="_blank" rel="noopener noreferrer">How to book a Business Advice Open Day</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120418183332/http://www.events.businesslink.gov.uk/eventsearch?text=BAOD" target="_blank" rel="noopener noreferrer">Find dates and locations for Business Advice Open Days</a></li>
+			<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/tax-reliefs-and-allowances/tax-advantages-for-those-starting-up-in-business.php">Tax advantages for those starting up in business</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="http://www.hmrc.gov.uk/" target="_blank" rel="noopener noreferrer" title="HM Revenue &amp; Customs - Opens in a new window">
+				<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" /> HM Revenue &amp; Customs
+			</a>
+		</p>
+
+		<img src="../../syntegra/images/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+	</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
