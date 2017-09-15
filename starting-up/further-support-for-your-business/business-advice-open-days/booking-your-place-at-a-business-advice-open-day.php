40ee2245a38b86f1e187a45701a8d645f992de1f
Caroline Hagan <info@blueocto.co.uk>
2017-09-15 21:15:57 +0100

    Completed "starting-up > further-support-for-your-business > business-advice-open-days > Booking your place at a Business Advice Open Day" page

---
+++ starting-up/further-support-for-your-business/business-advice-open-days/booking-your-place-at-a-business-advice-open-day.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Booking your place at a Business Advice Open Day | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+
+			<h1>Business Advice Open Days</h1>
+			<h2>Booking your place at a Business Advice Open Day</h2>
+			<p>If you want to attend a Business Advice Open Day, please register your interest with HMRC in advance. It is possible to drop in on the day, but you may not be able to attend a seminar if all the places have been reserved.</p>
+			<p>You can notify HMRC in two ways. You can <a href="http://webarchive.nationalarchives.gov.uk/20120418183332/http://www.events.businesslink.gov.uk/eventsearch?text=BAOD" target="_blank" rel="noopener noreferrer">book an event on the Events Finder website and you will be sent an automatic email reminder</a>.</p>
+			
+			<h3>How to book</h3>
+			<p>You can book your place in one of three ways:</p>
+			<ul>
+				<li>book online and you will receive an automatic reminder ahead of the event to confirm your attendance</li>
+				<li>if you have received a flyer through the post because you live in the local area, you can fax your booking to Fax 01642 755214</li>
+				<li>if you prefer, you can call the HMRC Business Advice Open Day Booking Line on Tel 0845 603 2691.</li>
+			</ul>
+
+			<p>You should also use this number if you would like to tell the organisers about any special access, communication or personal needs you may have before the day.</p>
+			<p>Find out more about <a href="http://webarchive.nationalarchives.gov.uk/20120418183332/http://www.events.businesslink.gov.uk/eventsearch?text=BAOD" target="_blank" rel="noopener noreferrer">how to book a Business Advice Open Day</a>.</p>
+			<p><a href="http://webarchive.nationalarchives.gov.uk/20120418183332/http://www.events.businesslink.gov.uk/eventsearch?text=BAOD" target="_blank" rel="noopener noreferrer">Find dates, locations and booking information for Business Advice Open Days</a>.</p>
+			
+			<h3>Amending a booking</h3>
+			<p>To amend a booking for a Business Advice Open Day, you will need your booking reference and your email address you used to book.</p>
+			<p><a href="http://webarchive.nationalarchives.gov.uk/20120419021343/https://www.events.businesslink.gov.uk/bookingaccess" target="_blank" rel="noopener noreferrer">Amend your Business Advice Open Day booking</a>.</p>
+			
+			<h3>Automatic booking confirmation</h3>
+			<p>If you book online, you should receive an automatic confirmation. This confirmation should have all the details of your booking plus a unique reference number.</p>
+			<p>If you have booked by fax or post, you should expect to receive a fax confirmation two to three weeks before the event.</p>
+			
+			<h3>Do I have to pay to attend an event?</h3>
+			<p>The events are free to attend as the costs are met jointly by HMRC's main partners. There is no charge to attend any of the programmes or seminars. However, you will usually be required to pay for refreshments and not all venues can offer free parking.</p>
+			<p>It is not necessary to book to attend a roadshow but booking is required for seminars, as spaces are limited. </p>
+			
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/organisations-you-can-meet-at-business-advice-open-days.php" title="Organisations you can meet at Business Advice Open Days">Prev</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/questions-about-attending-a-business-advice-open-day.php" title="Questions about attending a Business Advice Open Day">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days.php" title="Business Advice Open Days">Introduction</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/who-should-attend-a-business-advice-open-day-and-why.php">Who should attend a Business Advice Open Day and why</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/organisations-you-can-meet-at-business-advice-open-days.php">Organisations you can meet at Business Advice Open Days</a></li>
+				<li>Booking your place at a Business Advice Open Day</li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/questions-about-attending-a-business-advice-open-day.php">Questions about attending a Business Advice Open Day</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/where-to-go-for-advice-if-you-cannot-attend-a-business-advice-open-day.php">Where to go for advice if you cannot attend a Business Advice Open Day</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/business-advice-open-days/how-to-contact-the-event-organisers.php">How to contact the event organisers</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120418183332/http://www.events.businesslink.gov.uk/eventsearch?text=BAOD" target="_blank" rel="noopener noreferrer">How to book a Business Advice Open Day</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120418183332/http://www.events.businesslink.gov.uk/eventsearch?text=BAOD" target="_blank" rel="noopener noreferrer">Amend your Business Advice Open Day booking</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120418183332/http://www.events.businesslink.gov.uk/eventsearch?text=BAOD" target="_blank" rel="noopener noreferrer">Find dates and locations for Business Advice Open Days</a></li>
+				<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/tax-reliefs-and-allowances/tax-advantages-for-those-starting-up-in-business.php">Tax advantages for those starting up in business</a></li>
+			</ul>
+
+			<p>Developed with:<br />
+				<a href="http://www.hmrc.gov.uk/" target="_blank" rel="noopener noreferrer" title="HM Revenue &amp; Customs - Opens in a new window">
+					<img src="../../../_img/HMRC_logo.gif" width="134" height="35" alt="" /> HM Revenue &amp; Customs
+				</a>
+			</p>
+
+			<img src="../../syntegra/images/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+	</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
