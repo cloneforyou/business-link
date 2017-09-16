fcbfa70178dcf204b8e634b47e1f9ff38fec2381
Caroline Hagan <info@blueocto.co.uk>
2017-09-16 17:06:32 +0100

    Completed "starting-up > further-support-for-your-business > extra-help-and-support-available-for-your-start-up > Support for disabled people" page.

---
+++ starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-disabled-people.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Support for disabled people | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+
+			<h1>Extra help and support available for your start up</h1>
+			<h2>Support for disabled people</h2>
+			<p>If you are disabled and wanting to set up a new business, there could be extra support available for you.</p>
+			<p><strong>Action for Blind People</strong> (ABP) self-employment advisers offer support to blind and partially sighted entrepreneurs who are looking to establish new businesses. They can also provide support for developing businesses that may be facing specific challenges. <a href="http://www.rnib.org.uk/who-we-are/action-for-blind-people" target="_blank" rel="noopener noreferrer">Find out about the work and services of ABP on the ABP website</a>.</p>
+			<p>
+				<p><strong>Jobcentre Plus</strong> provides help and assistance with training and finding work, such as self-employment. Their advisers can help you with support specifically for disabled people. <a href="https://www.gov.uk/looking-for-work-if-disabled" target="_blank" rel="noopener noreferrer">Find out how Jobcentre Plus can help you on the Directgov website</a>.</p>
+				<p>The <strong>Disabled Entrepreneurs Network</strong> (DEN) provides information and networking opportunities for self-employed disabled people. You can <a href="https://www.gov.uk/looking-for-work-if-disabled" target="_blank" rel="noopener noreferrer">find out about support available for disabled people on the Directgov website</a>.</p>
+				<p>The DEN has also produced a booklet called 'Setting up in Business: A Resource guide for disabled people and their advisers'. This includes information about tax, the Access to Work scheme, business planning, courses, grants, finance and working from home. To get a copy, you can apply by email to <a href="mailto:info@disabled-entrepreneurs.net">info@disabled-entrepreneurs.net</a>.</p>
+			</p>
+			<p>Disability charity Leonard Cheshire and easyGroup chairman Sir Stelios Haji-Ioannou run an annual award of &pound;50,000 for exceptional disabled entrepreneurs - the <strong>Stelios Disabled Entrepreneurs Award</strong>. <a href="https://www.leonardcheshire.org/support-and-information/life-and-work-skills-development/employment-programmes/stelios-award-for" target="_blank" rel="noopener noreferrer">Find out about the Disabled Entrepreneur Award on the Leonard Cheshire Disability website</a>.</p>
+			<p>If you have a disability and usually work 16 hours or more a week, you may also be able to get <strong>extra tax credits</strong>. The disability must be one that makes it hard for you to get a job and you must be receiving, or have recently received, a qualifying sickness or disability-related benefit. <a href="https://www.gov.uk/browse/benefits/tax-credits" target="_blank" rel="noopener noreferrer">Find out how to get extra tax credits on the Directgov website</a>.</p>
+			<p>The <strong>Shaw Trust</strong> is a national charity that provides training and work opportunities for people who are disadvantaged in the labour market due to disability, ill health or other social circumstances. <a href="https://www.shaw-trust.org.uk/" target="_blank" rel="noopener noreferrer">Find out how the Shaw Trust can help you on the Shaw Trust website</a>.</p>
+			<p>The <strong>Association of Disabled Professionals</strong> (ADP) provides a forum for disabled people to share problems and experiences. It also offers employment advice and information and support for disabled people, including advice on issues relating to benefits, networking opportunities and schemes such as Access to Work. You can <a href="http://www.adp.org.uk/" target="_blank" rel="noopener noreferrer">find disabled employment advice on the ADP website</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-mature-entrepreneurs.php" title="Support for mature entrepreneurs">Prev</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-black-asian-and-minority-ethnic-start-ups.php" title="Support for black, Asian and minority ethnic start ups">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up.php" title="Extra help and support available for your start up">Introduction</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/mentoring-networking-finance-and-training.php">Mentoring, networking, finance and training</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-mature-entrepreneurs.php">Support for female entrepreneurs</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-mature-entrepreneurs.php">Support for mature entrepreneurs</a></li>
+				<li>Support for disabled people</li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-black-asian-and-minority-ethnic-start-ups.php">Support for black, Asian and minority ethnic start ups</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-entrepreneurs-on-low-incomes.php">Support for entrepreneurs on low incomes</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/sources-of-support-for-young-entrepreneurs.php">Sources of support for young entrepreneurs</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/looking-for-work-if-disabled" target="_blank" rel="noopener noreferrer">Government support for disabled people explained on the Directgov website</a></li>
+				<li><a href="http://www.ucandoit.org.uk/" target="_blank" rel="noopener noreferrer">Computer training courses for disabled people on the UCanDoIt website</a></li>
+				<li><a href="https://www.abilitynet.org.uk/" target="_blank" rel="noopener noreferrer">Business technology solutions on the AbilityNet website</a></li>
+				<li><a href="https://www.gov.uk/transport-disabled" target="_blank" rel="noopener noreferrer">Transport advice and support for disabled people on the Directgov website </a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120823132835/https://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101805&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Find out about business mentoring</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120823132830/https://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101974&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Get tips on how to network</a></li>
+				<li><a href="https://www.gov.uk/business-finance-support" target="_blank" rel="noopener noreferrer">Find the right support for your new business</a></li>
+			</ul>
+			
+			<p>Developed with:<br />
+				<a href="https://www.gov.uk/government/organisations/department-for-business-energy-and-industrial-strategy" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/logo_primary_bis.gif" width="160" height="32" alt="" /> Department for
+Business, Energy &amp; Industrial Strategy <del datetime="2016-07-01">Department for Business, Innovation &amp; Skills</del>
+				</a>
+			</p>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+	</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
