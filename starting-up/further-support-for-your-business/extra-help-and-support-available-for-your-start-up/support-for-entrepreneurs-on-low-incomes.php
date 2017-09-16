969391144c33337a9169801d1cf501bca1a1228a
Caroline Hagan <info@blueocto.co.uk>
2017-09-16 17:20:14 +0100

    Completed "starting-up > further-support-for-your-business > extra-help-and-support-available-for-your-start-up > Support for entrepreneurs on low incomes" page.

---
+++ starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-entrepreneurs-on-low-incomes.php
@@ -0,0 +1,74 @@
+.php<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Support for entrepreneurs on low incomes | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+
+			<h1>Extra help and support available for your start up</h1>
+			<h2>Support for entrepreneurs on low incomes</h2>
+			<p>If you receive benefits and want to set up a new business, it is worth discussing your business plans with a Jobcentre Plus adviser. They will be able to tell you how starting a business will affect your benefits and point you in the direction of practical support. You can also ask your adviser about the self-employed support package provided by Jobcentre Plus. This package is aimed at people who have been unemployed and claiming Jobseeker's Allowance for 13 weeks or longer.</p>
+			<p>You may also be entitled to Working Tax Credits. These are payments to top up the earnings of working people on low incomes, including the self-employed. You can find out about Working Tax Credits by calling the HMRC Tax Credits Helpline on Tel 0845 300 3900.</p>
+			<p>You can also get business support from many charitable organisations, including the Fredericks Foundation. <a href="http://www.fredericksfoundation.org/" target="_blank" rel="noopener noreferrer">Find information on loans available for new start-ups on the Fredericks Foundation website</a>.</p>
+			<p>You can also <a href="https://www.gov.uk/moving-from-benefits-to-work/starting-your-own-business" target="_blank" rel="noopener noreferrer">find out about the Jobcentre Plus self-employed support package on the Directgov website</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-black-asian-and-minority-ethnic-start-ups.php" title="Support for black, Asian and minority ethnic start ups">Prev</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/sources-of-support-for-young-entrepreneurs.php" title="Sources of support for young entrepreneurs">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up.php" title="Extra help and support available for your start up">Introduction</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/mentoring-networking-finance-and-training.php">Mentoring, networking, finance and training</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-mature-entrepreneurs.php">Support for female entrepreneurs</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-mature-entrepreneurs.php">Support for mature entrepreneurs</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-disabled-people.php">Support for disabled people</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-black-asian-and-minority-ethnic-start-ups.php">Support for black, Asian and minority ethnic start ups</a></li>
+				<li>Support for entrepreneurs on low incomes</li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/sources-of-support-for-young-entrepreneurs.php">Sources of support for young entrepreneurs</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/moving-from-benefits-to-work/starting-your-own-business" target="_blank" rel="noopener noreferrer">Jobcentre Plus self-employed support package on the Directgov website</a></li>
+				<li><a href="https://www.gov.uk/claim-tax-credits" target="_blank" rel="noopener noreferrer">Tax credit information on the Directgov website</a></li>
+				<li><a href="http://www.fredericksfoundation.org/" target="_blank" rel="noopener noreferrer">Loans for new start-ups explained on the Fredericks Foundation website</a></li>
+				<li><a href="http://www.horsesmouth.co.uk/publisha.content/Work/topics.publisha?topic=Starting+a+business" target="_blank" rel="noopener noreferrer">Online business mentoring explained on the horsesmouth website</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120823132835/https://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101805&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Find out about business mentoring</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120823132830/https://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101974&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Get tips on how to network</a></li>
+				<li><a href="https://www.gov.uk/business-finance-support" target="_blank" rel="noopener noreferrer">Find the right support for your new business</a></li>
+			</ul>
+			
+			<p>Developed with:<br />
+				<a href="https://www.gov.uk/government/organisations/department-for-business-energy-and-industrial-strategy" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/logo_primary_bis.gif" width="160" height="32" alt="" /> Department for
+Business, Energy &amp; Industrial Strategy <del datetime="2016-07-01">Department for Business, Innovation &amp; Skills</del>
+				</a>
+			</p>
+			
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+ 
+
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+	</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
