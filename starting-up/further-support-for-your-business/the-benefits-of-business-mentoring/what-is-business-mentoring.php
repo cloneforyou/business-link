f9d8ad33af1953799fc545c6d3b1dc66ff84a8af
Caroline Hagan <info@blueocto.co.uk>
2017-09-16 17:39:34 +0100

    Completed "starting-up > further-support-for-your-business > the-benefits-of-business-mentoring > What is business mentoring?" page.

---
+++ starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/what-is-business-mentoring.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>What is business mentoring? | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+
+			<h1>The benefits of business mentoring</h1>
+			<h2>What is business mentoring?</h2>
+			<p>Business mentoring is a relationship between you - as an entrepreneur and someone with business experience who is willing to act as a guide. The business mentor offers advice, guidance and support to help you run and improve your business.</p>
+			<p>It can involve face-to-face meetings or online discussions - or a combination of both - depending on which arrangement is best for both parties.</p>
+			
+			<h3>Formal and informal mentoring</h3>
+			<p>It's likely that your mentor will stay in regular contact with you through meetings, telephone calls and emails. The nature of the relationship may vary from being fairly casual - eg a monthly telephone call or informal visit - to being more formal and structured - eg setting meeting agendas and business objectives. Some mentoring services are free and others are paid for or available by subscription to an organisation.</p>
+			<p>Not all mentors have to be formally established as such. Friends, family and business contacts can all become involved in unofficial mentoring.</p>
+			
+			<h3>How to find a mentor</h3>
+			<p>There are several organisations that make mentors available to businesses. Many of these are represented on the national business mentoring network, Mentorsme.co.uk, which provides access to around 10,000 mentors - offering both free and paid for mentoring. This government-backed service is funded by the British Bankers' Association and supported by the Better Business Finance Roundtable, which includes the key business groups in the UK.</p>
+			<p><a href="http://www.mentorsme.co.uk/" target="_blank" rel="noopener noreferrer">Find business mentors on the Mentorsme.co.uk website</a>.</p>
+			<p>There are many other sources of business mentors, including some geared specifically towards supporting local businesses, young entrepreneurs or specific business sectors. See the page in this guide on <a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/further-information-for-finding-or-becoming-a-business-mentor.php">further information for finding or becoming a business mentor</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring.php" title="The benefits of business mentoring">Prev</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/why-business-mentoring-is-important.php" title="Why business mentoring is important">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring.php" title="The benefits of business mentoring">Introduction</a></li>
+				<li>What is business mentoring?</li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/why-business-mentoring-is-important.php">Why business mentoring is important</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/face-to-face-business-mentoring.php">Face-to-face business mentoring</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/online-business-mentoring.php">Online business mentoring</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/business-mentoring-and-other-types-of-advice.php">Business mentoring and other types of advice</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/becoming-a-business-mentor.php">Becoming a business mentor</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/further-information-for-finding-or-becoming-a-business-mentor.php">Further information for finding or becoming a business mentor</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://startups.co.uk/mentoring/" target="_blank" rel="noopener noreferrer">Mentoring explained on the Startups website</a></li>
+				<li><a href="http://www.mentorsme.co.uk/" target="_blank" rel="noopener noreferrer">Business mentors on the Mentorsme.co.uk website</a></li>
+				<li><a href="http://www.horsesmouth.co.uk/publisha.content/Work/topics.publisha?topic=Starting+a+business" target="_blank" rel="noopener noreferrer">Online business mentoring information on the horsesmouth website</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough.php">Starting a business when economic conditions are tough</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php">Starting a business from home </a></li>
+				<li><a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">Research and develop your business ideas, new products and services</a></li>
+			</ul>
+			
+			<p>Developed with:<br />
+				<a href="https://www.gov.uk/government/organisations/department-for-business-energy-and-industrial-strategy" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/logo_primary_bis.gif" width="134" height="35"  alt="" /> Department for
+Business, Energy &amp; Industrial Strategy <del datetime="2016-07-01">Department for Business, Innovation &amp; Skills</del>
+				</a>
+			</p>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+	</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
