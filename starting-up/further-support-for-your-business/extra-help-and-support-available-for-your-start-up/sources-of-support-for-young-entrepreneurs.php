5618af484de15a1cac5b78f27999ca332c6eba4d
Caroline Hagan <info@blueocto.co.uk>
2017-09-16 17:28:05 +0100

    Completed "starting-up > further-support-for-your-business > extra-help-and-support-available-for-your-start-up > Sources of support for young entrepreneurs" page.

---
+++ starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/sources-of-support-for-young-entrepreneurs.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Sources of support for young entrepreneurs | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+
+			<h1>Extra help and support available for your start up</h1>
+			<h2>Sources of support for young entrepreneurs</h2>
+			<p>There are a number of schemes that offer advice and support if you are a young entrepreneur. Many of them can also help if you are finding it difficult to raise finance.</p>
+			
+			<h3>The Prince's Trust</h3>
+			<p>The Prince's Trust can help you test your idea, write a business plan, access mentoring and may even offer you financial support if you are struggling to raise money for your business.</p>
+			<p>The Prince's Trust offers:</p>
+			<ul>
+				<li>employment advice</li>
+				<li>business skills training</li>
+				<li>business planning support</li>
+				<li>start up funding</li>
+				<li>ongoing support from a mentor</li>
+			</ul>
+			
+			<p>You can <a href="https://www.princes-trust.org.uk/help-for-young-people/get-in-touch" target="_blank" rel="noopener noreferrer">register your interest on the Prince's Trust website</a>.</p>
+			
+			<h3>Shell LiveWIRE</h3>
+			<p>The Shell LiveWIRE website offers discussion forums where young entrepreneurs can share their experiences, and offer solutions to business problems.</p>
+			<p>Shell LiveWIRE is aimed specifically at young entrepreneurs aged between 16 and 30. It allows you to utilise a pool of experts with specialised knowledge. <a href="https://www.shell-livewire.org/" target="_blank" rel="noopener noreferrer">Find help and advice from the Shell LiveWIRE community on the Shell LiveWIRE website (registration required)</a>.</p>
+			<p>Shell LiveWIRE Grand Ideas Awards offer up to five &pound;1,000 prizes a month to the most innovative and unusual ideas submitted by young entrepreneurs aged 16-30 with new businesses in their first 12 months of trading.</p>
+			<p><a href="http://www.shell-livewire.org/listing/awards" target="_blank" rel="noopener noreferrer">Find out about criteria and how to apply for the Grand Idea Awards on the Shell LiveWIRE website</a>.</p>
+			
+			<h3>Young Enterprise</h3>
+			<p>Young Enterprise has several programmes aimed at giving young people an understanding of how business works through running their own businesses. <a href="https://www.young-enterprise.org.uk/what-we-do/our-programmes-new/" target="_blank" rel="noopener noreferrer">Find details of Young Enterprise programmes on the Young Enterprise website</a>.</p>
+			
+			<h3>Working tax credits</h3>
+			<p>If you are not earning very much in the first months of your business, you may also be entitled to Working Tax Credits. These are payments to top up the earnings of working people on low incomes, including the self-employed. For more information, see the page in this guide on <a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-entrepreneurs-on-low-incomes.php">support for entrepreneurs on low incomes</a>.</p>
+			<p><a href="https://www.gov.uk/tax-credits-calculator" target="_blank" rel="noopener noreferrer">Find out about Tax Credits on the HM Revenue &amp; Customs (HMRC) website</a>.</p>
+			<p>You can also find out about Working Tax Credits by calling the HMRC Tax Credits Helpline on Tel 0845 300 3900.</p>
+			
+			<h3>Graduate entrepreneurs</h3>
+			<p>If you're a graduate or student, you can <a href="https://web.archive.org/web/20120910070139/http://www.ncee.org.uk/students_and_graduates" target="_blank" rel="noopener noreferrer">access advice and support on the National Council for Entrepreneurship in Education (NCEE) website</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-entrepreneurs-on-low-incomes.php" title="Support for entrepreneurs on low incomes">Prev</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up.php" title="Extra help and support available for your start up">Introduction</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/mentoring-networking-finance-and-training.php">Mentoring, networking, finance and training</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-mature-entrepreneurs.php">Support for female entrepreneurs</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-mature-entrepreneurs.php">Support for mature entrepreneurs</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-disabled-people.php">Support for disabled people</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-black-asian-and-minority-ethnic-start-ups.php">Support for black, Asian and minority ethnic start ups</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-entrepreneurs-on-low-incomes.php">Support for entrepreneurs on low incomes</a></li>
+				<li>Sources of support for young entrepreneurs</li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Shell LiveWIRE</h3>
+			<p>0845 757 3252</p>
+
+			<hr />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.shell-livewire.org/" target="_blank" rel="noopener noreferrer">Start-up support for young people on the Shell LiveWIRE website</a></li>
+				<li><a href="https://www.princes-trust.org.uk/help-for-young-people/support-starting-business" target="_blank" rel="noopener noreferrer">Young entrepreneur support on the Prince's Trust website</a></li>
+				<li><a href="https://www.young-enterprise.org.uk/what-we-do/our-programmes-new/" target="_blank" rel="noopener noreferrer">Young enterprise programmes on the Young Enterprise website</a></li>
+				<li><a href="https://www.gov.uk/tax-credits-calculator" target="_blank" rel="noopener noreferrer">Tax Credits explained on the HMRC website</a></li>
+				<li><a href="https://web.archive.org/web/20120910070139/http://www.ncee.org.uk/students_and_graduates" target="_blank" rel="noopener noreferrer">Business advice and support on the NCEE website</a></li>
+				<li><a href="http://www.horsesmouth.co.uk/publisha.content/Work/topics.publisha?topic=Starting+a+business" target="_blank" rel="noopener noreferrer">Online business mentoring explained on the horsesmouth website</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120823132835/https://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101805&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Find out about business mentoring</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120823132830/https://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101974&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Get tips on how to network</a></li>
+				<li><a href="https://www.gov.uk/business-finance-support" target="_blank" rel="noopener noreferrer">Find the right support for your new business</a></li>
+			</ul>
+			
+			<p>Developed with:<br />
+				<a href="https://www.gov.uk/government/organisations/department-for-business-energy-and-industrial-strategy" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/logo_primary_bis.gif" width="160" height="32" alt="" /> Department for
+Business, Energy &amp; Industrial Strategy <del datetime="2016-07-01">Department for Business, Innovation &amp; Skills</del>
+				</a>
+			</p>
+			
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+ 
+
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+	</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
