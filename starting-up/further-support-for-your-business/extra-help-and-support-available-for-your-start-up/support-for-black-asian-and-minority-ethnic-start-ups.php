632b45b581dcf6003c1629b71996c72488a51be8
Caroline Hagan <info@blueocto.co.uk>
2017-09-16 17:16:29 +0100

    Completed "starting-up > further-support-for-your-business > extra-help-and-support-available-for-your-start-up > Support for black, Asian and minority ethnic start ups" page.

---
+++ starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-black-asian-and-minority-ethnic-start-ups.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Support for black, Asian and minority ethnic start ups | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+
+			<h1>Extra help and support available for your start up</h1>
+			<h2>Support for black, Asian and minority ethnic start ups</h2>
+			<p>There are several established business support networks specifically for black, Asian and minority ethnic businesses that provide information about starting a business, how to get finance and advice and other help once the business is up and running. This support includes:</p>
+			<ul>
+				<li>The <strong>Asian Business Development Network</strong> (ABDN) can help you develop your business by sharing best practice and improving opportunities - <a href="https://web.archive.org/web/20120710012027/http://www.abdn.org.uk" target="_blank" rel="noopener noreferrer">find information on the work and services of the ABDN on the ABDN website</a></li>
+				<li>The <strong>African Caribbean Business Network</strong> (ACBN) provides a business network helping African and Caribbean owned businesses in the UK - <a href="#">read about the work and services of the ACBN on the ACBN website</a></li>
+				<li>The <strong>National Black Women's Network</strong> (NBWN) offers consultation and support for black women in business - <a href="http://nbwn.org/" target="_blank" rel="noopener noreferrer">find out about business networking events near you on the NBWN website</a></li>
+			</ul>
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-disabled-people.php" title="Support for disabled people">Prev</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-entrepreneurs-on-low-incomes.php" title="Support for entrepreneurs on low incomes">Next</a></li>
+			</ul>
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up.php" title="Extra help and support available for your start up">Introduction</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/mentoring-networking-finance-and-training.php">Mentoring, networking, finance and training</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-mature-entrepreneurs.php">Support for female entrepreneurs</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-mature-entrepreneurs.php">Support for mature entrepreneurs</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-disabled-people.php">Support for disabled people</a></li>
+				<li>Support for black, Asian and minority ethnic start ups</li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/support-for-entrepreneurs-on-low-incomes.php">Support for entrepreneurs on low incomes</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/extra-help-and-support-available-for-your-start-up/sources-of-support-for-young-entrepreneurs.php">Sources of support for young entrepreneurs</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://web.archive.org/web/20120710012027/http://www.abdn.org.uk" target="_blank" rel="noopener noreferrer">Asian business networking information on the ABDN website</a></li>
+				<li><a href="#">ACBN services information on the ACBN website</a></li>
+				<li><a href="http://nbwn.org/" target="_blank" rel="noopener noreferrer">NBWN networking events on the NBWN website</a></li>
+				<li><a href="http://www.horsesmouth.co.uk/publisha.content/Work/topics.publisha?topic=Starting+a+business" target="_blank" rel="noopener noreferrer">Online business mentoring explained on the horsesmouth website</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120823132835/https://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101805&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Find out about business mentoring</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120823132830/https://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101974&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Get tips on how to network</a></li>
+				<li><a href="https://www.gov.uk/business-finance-support" target="_blank" rel="noopener noreferrer">Find the right support for your new business</a></li>
+			</ul>
+			
+			<p>Developed with:<br />
+				<a href="https://www.gov.uk/government/organisations/department-for-business-energy-and-industrial-strategy" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/logo_primary_bis.gif" width="160" height="32" alt="" /> Department for
+Business, Energy &amp; Industrial Strategy <del datetime="2016-07-01">Department for Business, Innovation &amp; Skills</del>
+				</a>
+			</p>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+	</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
