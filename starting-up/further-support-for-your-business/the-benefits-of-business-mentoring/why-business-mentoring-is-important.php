e07ae1eae2640d857fe5a0c760282a0cefb0e539
Caroline Hagan <info@blueocto.co.uk>
2017-09-21 16:23:29 +0100

    Completed "starting-up > further-support-for-your-business > the-benefits-of-business-mentoring > Why business mentoring is important" page.

---
+++ starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/why-business-mentoring-is-important.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Why business mentoring is important | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+
+			<h1>The benefits of business mentoring</h1>
+			<h2>Why business mentoring is important</h2>
+			<p>If you have a gap in your knowledge or experience, then mentoring could work very well for you. For example, you may have a great idea for a business but need a bit of guidance to turn it into a successful venture.</p>
+			<p>A mentor can provide you with a number of benefits, such as:</p>
+			<ul>
+				<li>guidance on developing and improving your business</li>
+				<li>help with difficult decisions</li>
+				<li>ideas for new products or services, or ways of working</li>
+				<li>tips for your business that are gained from practical experience</li>
+				<li>access to a network of contacts with other people</li>
+			</ul>
+
+			<p>A business mentor can also help you to:</p>
+			<ul>
+				<li>develop key business skills</li>
+				<li>improve your problem solving abilities</li>
+				<li>build confidence</li>
+				<li>work on your personal development</li>
+			</ul>
+
+			<p>However, a business mentor won't sort out all your problems for you, tell you what to do or offer business advice. They don't act as consultants or take the place of any existing professional advisers.</p>
+			
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/what-is-business-mentoring.php" title="What is business mentoring?">Prev</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/face-to-face-business-mentoring.php" title="Face-to-face business mentoring">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring.php" title="The benefits of business mentoring">Introduction</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/what-is-business-mentoring.php">What is business mentoring?</a></li>
+				<li>Why business mentoring is important</li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/face-to-face-business-mentoring.php">Face-to-face business mentoring</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/online-business-mentoring.php">Online business mentoring</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/business-mentoring-and-other-types-of-advice.php">Business mentoring and other types of advice</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/becoming-a-business-mentor.php">Becoming a business mentor</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/further-information-for-finding-or-becoming-a-business-mentor.php">Further information for finding or becoming a business mentor</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://www.horsesmouth.co.uk/publisha.content/Work/topics.publisha?topic=Starting+a+business" target="_blank" rel="noopener noreferrer">Online business mentoring information on the horsesmouth website</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough.php">Starting a business when economic conditions are tough</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php">Starting a business from home </a></li>
+				<li><a href="/starting-up/considering-starting-up/research-and-develop-your-business-ideas-new-products-and-services.php">Research and develop your business ideas, new products and services</a></li>
+			</ul>
+
+			<p>Developed with:<br />
+				<a href="https://www.gov.uk/government/organisations/department-for-business-energy-and-industrial-strategy" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/logo_primary_bis.gif" width="134" height="35"  alt="" /> Department for
+	Business, Energy &amp; Industrial Strategy <del datetime="2016-07-01">Department for Business, Innovation &amp; Skills</del>
+				</a>
+			</p>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+						
+
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+	</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
