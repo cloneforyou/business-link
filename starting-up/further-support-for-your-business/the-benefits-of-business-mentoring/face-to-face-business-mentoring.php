6f0efde0e80529c75c7e1d3135785c2e768830ef
Caroline Hagan <info@blueocto.co.uk>
2017-09-21 16:25:34 +0100

    Completed "starting-up > further-support-for-your-business > the-benefits-of-business-mentoring > Face-to-face business mentoring" page.

---
+++ starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/face-to-face-business-mentoring.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Face-to-face business mentoring | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+
+			<h1>The benefits of business mentoring</h1>
+			<h2>Face-to-face business mentoring</h2>
+			<p>If you choose to get involved in face-to-face business mentoring, mentor and mentee need to agree on:</p>
+			<ul>
+				<li>the degree of involvement you want</li>
+				<li>the level of structure you want</li>
+			</ul>
+
+			<h3>Degrees of involvement</h3>
+			<p>You will need to agree on the degree of involvement that suits you both. Some mentors and mentees work extremely closely with each other, speaking or meeting most weeks or even most days. In most cases, however, a mentor will not have day-to-day involvement with the business. Instead, they'll provide help every few weeks or months which could take the form of phone calls, emails, meetings, etc.</p>
+			
+			<h3>Level of structure</h3>
+			<p>You should also agree on a level of structure to suit you both. Mentoring can be a very formal and structured process with regular meetings which follow a specific agenda and set of goals. It can also be quite a casual arrangement, where the mentee calls on the mentor as and when problems or questions arise.</p>
+			
+			<h3>Finding a face-to-face business mentor</h3>
+			<p>Mentorsme.co.uk - the national mentoring network - can help you find mentors offering face-to-face support. This new government-backed service is funded by the British Bankers' Association and supported by the Better Business Finance Roundtable, which includes the key business groups in the UK.</p>
+			<p><a href="http://www.mentorsme.co.uk/" target="_blank" rel="noopener noreferrer">Find business mentors on the Mentorsme.co.uk website</a>.</p>
+			<p>For more about these and other mentoring services, see the page in this guide on <a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/further-information-for-finding-or-becoming-a-business-mentor.php">further information for finding or becoming a business mentor</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/why-business-mentoring-is-important.php" title="Why business mentoring is important">Prev</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/online-business-mentoring.php" title="Online business mentoring">Next</a></li>
+			</ul>
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring.php" title="The benefits of business mentoring">Introduction</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/what-is-business-mentoring.php">What is business mentoring?</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/why-business-mentoring-is-important.php">Why business mentoring is important</a></li>
+				<li>Face-to-face business mentoring</li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/online-business-mentoring.php">Online business mentoring</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/business-mentoring-and-other-types-of-advice.php">Business mentoring and other types of advice</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/becoming-a-business-mentor.php">Becoming a business mentor</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/further-information-for-finding-or-becoming-a-business-mentor.php">Further information for finding or becoming a business mentor</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://www.mentorsme.co.uk/" target="_blank" rel="noopener noreferrer">Business mentors on the Mentorsme.co.uk website</a></li>
+				<li><a href="http://www.horsesmouth.co.uk/publisha.content/Work/topics.publisha?topic=Starting+a+business" target="_blank" rel="noopener noreferrer">Online business mentoring information on the horsesmouth website</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough.php" title="Assess the opportunities for starting a business during an economic downturn">Starting a business when economic conditions are tough</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php" title="Decide whether starting a business from home is right for you">Starting a business from home </a></li>
+			</ul>
+
+			<p>Developed with:<br />
+				<a href="https://www.gov.uk/government/organisations/department-for-business-energy-and-industrial-strategy" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/logo_primary_bis.gif" width="134" height="35"  alt="" /> Department for
+	Business, Energy &amp; Industrial Strategy <del datetime="2016-07-01">Department for Business, Innovation &amp; Skills</del>
+				</a>
+			</p>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+			
+
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+	</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
