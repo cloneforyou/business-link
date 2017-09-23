13d961fb0b6f839ae68107cba14f1baddc80afc8
Caroline Hagan <info@blueocto.co.uk>
2017-09-23 17:01:57 +0100

    Completed "starting up > further support for your business > the benefits of business mentoring > Further information for finding or becoming a business mentor" page.

---
+++ starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/further-information-for-finding-or-becoming-a-business-mentor.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Further information for finding or becoming a business mentor | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+
+			<h1>The benefits of business mentoring</h1>
+			<h2>Further information for finding or becoming a business mentor</h2>
+			<p>There are several organisations that make mentors available to businesses. Many of these are represented on the national business mentoring network, Mentorsme.co.uk.</p>
+			<p>Mentorsme.co.uk lets you:</p>
+			<ul>
+				<li>search for business mentors, according to their location and/or area of expertise</li>
+				<li>offer your services as a business mentor, through an existing mentoring organisation  </li>
+			</ul>
+
+			<p><a href="http://www.mentorsme.co.uk/" target="_blank" rel="noopener noreferrer">Business mentors on the Mentorsme.co.uk website</a>.</p>
+			<p>There are more than 10,000 mentors available on the network. Both face-to-face and online support is offered, and many mentors have particular expertise in areas such as finance, leadership and innovation.</p>
+			<p>Mentorsme.co.uk is funded by the British Bankers' Association and supported by the Better Business Finance Roundtable, which includes the key business groups in the UK. It is backed by the Department for Business, Innovation &amp; Skills.</p>
+			
+			<h3>Other main providers of business mentoring services</h3>
+			<p>You can also find business mentors through the networks listed below.</p>
+			<p><a href="https://www.crunch.co.uk/blog/freelancer-advice/" target="_blank" rel="noopener noreferrer">Read information about online mentoring on the Freelance Advisor website</a>.</p>
+			<p><a href="http://www.horsesmouth.co.uk/publisha.content/Work/topics.publisha?topic=Starting+a+business" target="_blank" rel="noopener noreferrer">Read information about online business mentoring on the horsesmouth website</a>.</p>
+			<p><a href="https://www.enterprisenation.com/" target="_blank" rel="noopener noreferrer">Read information about online mentoring on the Enterprise Nation website</a>.</p>
+			<p><a href="https://www.shell-livewire.org/" target="_blank" rel="noopener noreferrer">Read information about e-mentoring for young entrepreneurs on the Shell LiveWIRE website</a>.</p>
+			
+			<h3>Further sources of advice</h3>
+			<p>The British Chambers of Commerce (BCC) network can point you towards business mentors with the skills and experience you need.</p>
+			<p><a href="http://www.britishchambers.org.uk/contact-the-bcc/" target="_blank" rel="noopener noreferrer">Find contact information for the BCC on the BCC website</a>.</p>
+			<p>You can call the British Chambers of Commerce on Tel 024 7669 4484.</p>
+			<p>If your business operates in the creative or technology sectors, the National Endowment for Science Technology and the Arts (NESTA) may be able to recommend suitable mentors from its network.</p>
+			<p></p>
+			<p><a href="http://www.nesta.org.uk/contact_us" target="_blank" rel="noopener noreferrer">Find contact information for the creative business mentoring network on the NESTA website</a>.</p>
+			<p>You can call the NESTA Helpline on Tel 020 7438 2500.</p>
+			<p>Businesses looking for mentoring support with leadership and management issues can contact the Management and Leadership Network (MLN).</p>
+			<p><a href="https://web.archive.org/web/20120121072633/http://www.mln.org.uk/online_mentoring.asp" target="_blank" rel="noopener noreferrer">Find contact information for the MLN e-mentoring scheme on the MLN website</a>.</p>
+			<p>You can call the Management and Leadership Network InfoPoint on Tel 0845 602 6634.</p>
+			
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/becoming-a-business-mentor.php" title="Becoming a business mentor">Prev</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring.php" title="The benefits of business mentoring">Introduction</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/what-is-business-mentoring.php">What is business mentoring?</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/why-business-mentoring-is-important.php">Why business mentoring is important</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/face-to-face-business-mentoring.php">Face-to-face business mentoring</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/online-business-mentoring.php">Online business mentoring</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/business-mentoring-and-other-types-of-advice.php">Business mentoring and other types of advice</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/becoming-a-business-mentor.php">Becoming a business mentor</a></li>
+				<li>Further information for finding or becoming a business mentor</li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/sales-and-marketing/online-marketing/online-business-networking-and-social-networking.php">Online business networking and social networking</a></li>
+				<li><a href="/starting-up/considering-starting-up/starting-a-business-when-economic-conditions-are-tough.php">Starting a business when economic conditions are tough</a></li>
+				<li><a href="/starting-up/choose-and-set-up-your-workplace/starting-a-business-from-home.php">Starting a business from home </a></li>
+			</ul>
+
+			<p>Developed with:<br />
+				<a href="https://www.gov.uk/government/organisations/department-for-business-energy-and-industrial-strategy" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/logo_primary_bis.gif" width="134" height="35"  alt="" /> Department for
+	Business, Energy &amp; Industrial Strategy <del datetime="2016-07-01">Department for Business, Innovation &amp; Skills</del>
+				</a>
+			</p>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+	</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
