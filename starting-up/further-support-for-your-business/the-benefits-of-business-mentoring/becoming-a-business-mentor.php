29b0f7297ab93e4b5536cb18e86a9921fa948084
Caroline Hagan <info@blueocto.co.uk>
2017-09-23 16:55:25 +0100

    Completed "starting up > further support for your business > the benefits of business mentoring > Becoming a business mentor" page.

---
+++ starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/becoming-a-business-mentor.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Becoming a business mentor | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+
+			<h1>The benefits of business mentoring</h1>
+			<h2>Becoming a business mentor</h2>
+			<p>If you already have plenty of business experience, you could consider offering your services as a mentor to other businesses. This can be a very rewarding way of passing on the benefits of your experience.</p>
+			<p>As a mentor, your role is to support, develop, stimulate and challenge your mentee, whether they're just starting up in business or are already established and seeking guidance.</p>
+			<p>You can mentor in a variety of ways - including online, face to face, by telephone, or as part of a discussion group. You can charge a fee for your services or offer free support.</p>
+			
+			<h3>The Get Mentoring initiative</h3>
+			<p>Get Mentoring aims to increase the number of volunteer business mentors in the UK by recruiting and training 15,000 volunteer mentors from the small, medium and micro business community. The initiative is led by the Small Firms Enterprise Development Initiative.</p>
+			<p>A free training course - Introduction to Enterprise Mentoring - will equip you with the skills and knowledge you need to become an effective mentor.</p>
+			<p><a href="http://getmentoring.org/" target="_blank" rel="noopener noreferrer">Register for free business mentor training on the Get Mentoring website</a>. </p>
+			
+			<h3>Mentorsme.co.uk</h3>
+			<p>Mentorsme.co.uk is the national web portal that allows businesses to access mentoring services. Once you're registered as a mentor, mentees will be able to find your details on the Mentorsme.co.uk website.</p>
+			<p><a href="http://www.mentorsme.co.uk/" target="_blank" rel="noopener noreferrer">Find out about becoming a business mentor on the Mentorsme.co.uk website</a>.</p>
+			
+			<h3>What you'll need to become a business mentor</h3>
+			<p>To mentor other business people effectively you need the right skills and experience. You should have:</p>
+			<ul>
+				<li>the ability to listen and empathise</li>
+				<li>respect for other people's views</li>
+				<li>experience of the problems faced by small businesses</li>
+				<li>the ability to offer encouragement and sympathy</li>
+				<li>a supportive, hands-off approach that allows others to make their own decisions</li>
+				<li>a willingness to volunteer your time and skills</li>
+			</ul>
+			
+			<p>You should also have a good, broad understanding of business. Many small businesses look for mentors who have expertise in particular areas or functions, such as:</p>
+			<ul>
+				<li>finance</li>
+				<li>sales and marketing</li>
+				<li>public relations</li>
+				<li>strategy and planning</li>
+				<li>negotiation</li>
+				<li>operations</li>
+			</ul>
+			
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/business-mentoring-and-other-types-of-advice.php" title="Business mentoring and other types of advice">Prev</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/further-information-for-finding-or-becoming-a-business-mentor.php" title="Further information for finding or becoming a business mentor">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring.php" title="The benefits of business mentoring">Introduction</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/what-is-business-mentoring.php">What is business mentoring?</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/why-business-mentoring-is-important.php">Why business mentoring is important</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/face-to-face-business-mentoring.php">Face-to-face business mentoring</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/online-business-mentoring.php">Online business mentoring</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/business-mentoring-and-other-types-of-advice.php">Business mentoring and other types of advice</a></li>
+				<li>Becoming a business mentor</li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/further-information-for-finding-or-becoming-a-business-mentor.php">Further information for finding or becoming a business mentor</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://www.mentorsme.co.uk/" target="_blank" rel="noopener noreferrer">Business mentoring on the Mentorsme.co.uk website</a></li>
+				<li><a href="http://startups.co.uk/mentoring/" target="_blank" rel="noopener noreferrer">Mentoring explained on the Startups website</a></li>
+				<li><a href="http://getmentoring.org/" target="_blank" rel="noopener noreferrer">Becoming a business mentor information on the Get Mentoring website</a></li>
+				<li><a href="http://www.horsesmouth.co.uk/publisha.content/Work/topics.publisha?topic=Starting+a+business" target="_blank" rel="noopener noreferrer">Online business mentoring information on the horsesmouth website</a></li>
+			</ul>
+
+			<p>Developed with:<br />
+				<a href="https://www.gov.uk/government/organisations/department-for-business-energy-and-industrial-strategy" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/logo_primary_bis.gif" width="134" height="35"  alt="" /> Department for
+Business, Energy &amp; Industrial Strategy <del datetime="2016-07-01">Department for Business, Innovation &amp; Skills</del>
+				</a>
+			</p>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+ 
+
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+	</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
