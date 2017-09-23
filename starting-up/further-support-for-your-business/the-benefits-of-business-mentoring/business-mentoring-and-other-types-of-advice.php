52baf31888111ad53dec8267e5d423b53e3b151c
Caroline Hagan <info@blueocto.co.uk>
2017-09-23 16:52:33 +0100

    Completed "starting up > further support for your business > the benefits of business mentoring > Business mentoring and other types of advice" page.

---
+++ starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/business-mentoring-and-other-types-of-advice.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Business mentoring and other types of advice | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+
+			<h1>The benefits of business mentoring</h1>
+			<h2>Business mentoring and other types of advice</h2>
+			<p>Using a business mentor is one way of helping you make the right decisions for your business, but there are other forms of support you could consider. These include:</p>
+			<ul>
+				<li>coaching</li>
+				<li>peer mentoring and peer learning alliances</li>
+				<li>networking</li>
+				<li>professional advice</li>
+			</ul>
+
+			<h3>Coaching</h3>
+			<p>Specific short-term business issues can be addressed through coaching which has similar elements as mentoring. There are many organisations that offer coaching services.</p>
+			
+			<h3>Peer mentoring and peer learning alliances</h3>
+			<p>You can also get help or advice from other people in a similar situation, such as people of a similar age or background. When setting up a business, this is likely to involve contacting someone who is also trying to set up a business and faces similar issues. Like business mentoring, this can be as formal or informal as you like.</p>
+			
+			<h3>Networking</h3>
+			<p>The wider world of networking allows you to meet a range of business people. You can do this at a local level - by talking to other business people in your area - or at a much broader level, eg through social networking websites and professional networks such as LinkedIn, Plaxo and Ecademy.</p>
+			<p>Sources of information about local networking groups or meetings are available from your local Chamber of Commerce, business advisers or trade associations. There are also Business Collaboration Networks - part of a range of support offered through the government's Solutions for Business portfolio. Solutions for Business is a package of products and services designed to help businesses start, grow and succeed.</p>
+			<p>For links to Solutions for Business advice and support, see our <a href="/solutions-for-business-portfolio.php">Solutions for Business portfolio</a>.</p>
+			<p><a href="http://webarchive.nationalarchives.gov.uk/20120418175516/http://improve.businesslink.gov.uk/resources/business-support-finder" target="_blank" rel="noopener noreferrer">Search our business support finder for grants, loans, expertise and advice for which your business may be eligible</a>.</p>
+			
+			<h3>Professional advice</h3>
+			<p>For tax and legal issues, it is important to seek professional advice. See our guides on how to <a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">choose and work with an accountant</a> and <a href="/finance-and-grants/expert-financial-advice/choose-and-work-with-a-solicitor.php">choose and work with a solicitor</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/online-business-mentoring.php" title="Online business mentoring">Prev</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/becoming-a-business-mentor.php" title="Becoming a business mentor">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring.php" title="The benefits of business mentoring">Introduction</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/what-is-business-mentoring.php">What is business mentoring?</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/why-business-mentoring-is-important.php">Why business mentoring is important</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/face-to-face-business-mentoring.php">Face-to-face business mentoring</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/online-business-mentoring.php">Online business mentoring</a></li>
+				<li>Business mentoring and other types of advice</li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/becoming-a-business-mentor.php">Becoming a business mentor</a></li>
+				<li><a href="/starting-up/further-support-for-your-business/the-benefits-of-business-mentoring/further-information-for-finding-or-becoming-a-business-mentor.php">Further information for finding or becoming a business mentor</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>British Chambers of Commerce</h3>
+			<p>024 7669 4484</p>
+			
+			<hr />
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120418175516/http://improve.businesslink.gov.uk/resources/business-support-finder" target="_blank" rel="noopener noreferrer">Search our business support finder for grants, loans, expertise and advice for which your business may be eligible</a></li>
+				<li><a href="http://www.britishchambers.org.uk/find-your-chamber/" target="_blank" rel="noopener noreferrer">Chambers of Commerce contact details on the British Chambers of Commerce website</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/employment-and-skills/skills-and-development/learning-through-networking-with-others.php">Learning through networking with others</a></li>
+				<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+				<li><a href="/finance-and-grants/expert-financial-advice/choose-and-work-with-a-solicitor.php">Choose and work with a solicitor</a></li>
+			</ul>
+
+			<p>Developed with:<br />
+				<a href="https://www.gov.uk/government/organisations/department-for-business-energy-and-industrial-strategy" target="_blank" rel="noopener noreferrer">
+					<img src="../../../_img/logo_primary_bis.gif" width="134" height="35"  alt="" /> Department for
+Business, Energy &amp; Industrial Strategy <del datetime="2016-07-01">Department for Business, Innovation &amp; Skills</del>
+				</a>
+			</p>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+	</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
