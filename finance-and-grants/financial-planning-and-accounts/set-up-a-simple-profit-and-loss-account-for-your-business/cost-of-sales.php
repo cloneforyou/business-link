096f6d66019be766185e11331027ae562e294e78
Caroline Hagan <info@blueocto.co.uk>
2017-09-24 16:10:39 +0100

    Completed "finance-and-grants > financial-planning-and-accounts > set-up-a-simple-profit-and-loss-account-for-your-business > Cost of sales" page.

---
+++ finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/cost-of-sales.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cost of sales | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Set up a simple profit and loss account for your business</h1>
+		<h2>Cost of sales</h2>
+		<p>The cost of sales is the cost of obtaining or creating your product.</p>
+		<p>This might include:</p>
+		<ul>
+			<li>the cost of stock you buy for resale</li>
+			<li>components/raw materials to make your product</li>
+			<li>labour to produce the product</li>
+			<li>machine hire</li>
+			<li>small tools</li>
+			<li>other production costs</li>
+		</ul>
+
+		<p>When you create your profit and loss account, you deduct your cost of sales from your overall sales, or turnover, to arrive at your 'gross profit'. This is your profit before deduction of expenses.</p>
+		<p>Cost of sales does not usually apply if you supply a service only.</p>
+		
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/recording-business-expenditure.php" title="Recording business expenditure">Prev</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-expenses.php" title="Business expenses">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business.php" title="Set up a simple profit and loss account for your business">Introduction</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/do-all-businesses-have-to-produce-formal-profit-and-loss-accounts.php">Do all businesses have to produce formal profit and loss accounts?</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/profit-reporting-how-when-and-where.php">Profit reporting: how, when and where?</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/records-required-for-producing-a-profit-and-loss-account-or-completing-a-tax-return.php">Records required for producing a profit and loss account or completing a tax return</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-income-sales.php">Business income: sales</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-income-other.php">Business income: other</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/recording-business-expenditure.php">Recording business expenditure</a></li>
+			<li>Cost of sales</li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-expenses.php">Business expenses</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/cost-of-equipment.php">Cost of equipment</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/profit-and-loss-accounting-periods-and-tax.php">Profit and loss accounting periods and tax</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://web.archive.org/web/20070819175954/http://www.lloydstsbbusiness.com/support/businessguides/profit_and_loss.asp" target="_blank" rel="noopener noreferrer" title="Practical advice on creating and understanding business accounts">Profit, loss and balance sheet advice from LloydsTSB Business</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/set-up-a-basic-record-keeping-system.php">Set up a basic record-keeping system</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php">Financial and management accounts: the basics</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/balance-sheets-the-basics.php">Balance sheets: the basics</a></li>
+			<li><a href="/it-and-ecommerce/software-and-business-applications/accounting-software.php">Accounting software</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/HMRC_logo.gif" width="134" height="35" alt="" /> HM Revenue &amp; Customs
+			</a>
+		</p>
+		<p>
+			<a href="http://www.icaew.com/" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/icaew_solus_logo_19x35px.JPG" width="19" height="35" alt="" /> Institute of Chartered Accountants in England and Wales
+			</a>
+		</p>
+
+		<img src="../../../_img/px_finance.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
