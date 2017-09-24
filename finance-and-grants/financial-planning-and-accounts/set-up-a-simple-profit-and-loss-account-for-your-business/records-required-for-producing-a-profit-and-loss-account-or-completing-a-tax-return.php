6bc912c894f50e8544583cfe7238dd00da9371cc
Caroline Hagan <info@blueocto.co.uk>
2017-09-24 15:58:27 +0100

    Completed "finance-and-grants > financial-planning-and-accounts > set-up-a-simple-profit-and-loss-account-for-your-business > Records required for producing a profit and loss account or completing a tax return" page.

---
+++ finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/records-required-for-producing-a-profit-and-loss-account-or-completing-a-tax-return.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Records required for producing a profit and loss account or completing a tax return | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Set up a simple profit and loss account for your business</h1>
+		<h2>Records required for producing a profit and loss account or completing a tax return</h2>
+		<p>Whatever your business type, you must keep accurate records of your income and expenditure. You need to keep self-employment records for <strong>five</strong> years after the 31 January deadline - and you may need to keep them for longer if you file your return late or if HM Revenue &amp; Customs starts a check. You need to keep limited company or partnership records for <strong>six</strong> years after the latest date your tax return is due.</p>
+		<p>Accurate record keeping has important benefits. It:</p>
+		<ul>
+			<li>gives you the information you need to manage and grow your business</li>
+			<li>enables you to report on your profit or loss easily and quickly when required</li>
+			<li>will improve your chances of getting a loan or mortgage</li>
+			<li>makes filling in your tax return easier and quicker</li>
+			<li>makes completing VAT Returns straightforward - if you are registered for VAT</li>
+			<li>helps you or your business pay the right tax</li>
+			<li>provides back-up for claims for certain allowances</li>
+			<li>helps you plan and budget for tax payments</li>
+			<li>reduces the risk of interest on late payments or late-filing penalties</li>
+			<li>helps reduce fees if you use an accountant - your annual accounts will be far easier to produce</li>
+		</ul>
+
+		<p>The <strong>basic records</strong> you will need to keep are:</p>
+		<ul>
+			<li>a record of all your sales and takings</li>
+			<li>a record of all your purchases and expenses</li>
+		</ul>
+
+		<p>You may also need to keep:</p>
+		<ul>
+			<li>a separate list for petty cash expenditure if relevant</li>
+			<li>a record of goods taken for personal use and payments to the business for these</li>
+			<li>a record of money taken out for personal use or paid in from personal funds - this applies to limited companies</li>
+			<li>back-up documents for all of the above</li>
+		</ul>
+
+		<p>You will need the information above to create your profit and loss account and to complete your tax returns.</p>
+		<p>For detailed information on the records you must keep, see our guides on:</p>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/self-assessment/your-tax-return-the-basics/record-keeping-individuals-and-directors.php">record keeping (individuals and directors)</a> </li>
+			<li><a href="/tax-payroll-and-company-information/self-assessment/tax-returns-if-you-are-self-employed-or-in-a-partnership/record-keeping-self-employed.php">record keeping (self-employed)</a> </li>
+			<li><a href="/tax-payroll-and-company-information/self-assessment/tax-returns-if-you-are-self-employed-or-in-a-partnership/record-keeping-partners-and-partnerships.php">record keeping (partners and partnerships)</a> </li>
+			<li><a href="/tax-payroll-and-company-information/corporation-tax/records-for-corporation-tax-what-you-need-to-keep.php">records for Corporation Tax: what you need to keep</a> </li>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-returns-and-accounts/accounts-and-records-for-your-vat.php">accounts and records for your VAT</a> </li>
+		</ul>
+
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/profit-reporting-how-when-and-where.php" title="Profit reporting: how, when and where?">Prev</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-income-sales.php" title="Business income: sales">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business.php" title="Set up a simple profit and loss account for your business">Introduction</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/do-all-businesses-have-to-produce-formal-profit-and-loss-accounts.php">Do all businesses have to produce formal profit and loss accounts?</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/profit-reporting-how-when-and-where.php">Profit reporting: how, when and where?</a></li>
+			<li>Records required for producing a profit and loss account or completing a tax return</li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-income-sales.php">Business income: sales</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-income-other.php">Business income: other</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/recording-business-expenditure.php">Recording business expenditure</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/cost-of-sales.php">Cost of sales</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-expenses.php">Business expenses</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/cost-of-equipment.php">Cost of equipment</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/profit-and-loss-accounting-periods-and-tax.php">Profit and loss accounting periods and tax</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/set-up-a-basic-record-keeping-system.php">Set up a basic record-keeping system</a></li>
+			<li><a href="/tax-payroll-and-company-information/self-assessment/your-tax-return-the-basics/record-keeping-individuals-and-directors.php">Record keeping (individuals and directors)</a></li>
+			<li><a href="/tax-payroll-and-company-information/self-assessment/tax-returns-if-you-are-self-employed-or-in-a-partnership/record-keeping-self-employed.php">Record keeping (self-employed)</a></li>
+			<li><a href="/tax-payroll-and-company-information/self-assessment/tax-returns-if-you-are-self-employed-or-in-a-partnership/record-keeping-partners-and-partnerships.php">Record keeping (partners and partnerships)</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php">Financial and management accounts: the basics</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/balance-sheets-the-basics.php">Balance sheets: the basics</a></li>
+			<li><a href="/it-and-ecommerce/software-and-business-applications/accounting-software.php">Accounting software</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/HMRC_logo.gif" width="134" height="35" alt="" /> HM Revenue &amp; Customs
+			</a>
+		</p>
+		<p>
+			<a href="http://www.icaew.com/" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/icaew_solus_logo_19x35px.JPG" width="19" height="35" alt="" /> Institute of Chartered Accountants in England and Wales
+			</a>
+		</p>
+
+		<img src="../../../_img/px_finance.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
