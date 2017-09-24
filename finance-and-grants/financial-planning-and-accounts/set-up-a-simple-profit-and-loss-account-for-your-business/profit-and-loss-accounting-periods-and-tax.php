9670d4238b1f431bbab91112a00bd1fa08a5f5ac
Caroline Hagan <info@blueocto.co.uk>
2017-09-24 16:23:34 +0100

    Completed "finance-and-grants > financial-planning-and-accounts > set-up-a-simple-profit-and-loss-account-for-your-business > Profit and loss accounting periods and tax" page.

---
+++ finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/profit-and-loss-accounting-periods-and-tax.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profit and loss accounting periods and tax | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Set up a simple profit and loss account for your business</h1>
+		<h2>Profit and loss accounting periods and tax</h2>
+		<p>Businesses normally work out their profit and loss for a twelve month period. This makes it easy to see how well your business is doing each year, and to compare one year with the next.</p>
+		<p>The way your accounts are taxed depends on what type of business you have.</p>
+		
+		<h3>Accounts for self-employed and partnerships</h3>
+		<p>Self-employed sole traders and business partnerships are normally taxed on the profits for the 12 month accounting period ending during the tax year. The tax year runs from 6 April to 5 April the following year.</p>
+		<p>For example, if you make your annual accounts up to 31 December each year, your profits to 31 December 2010 are used in your 2010-11 tax return (for the year to 5 April 2011).</p>
+		<p>The simplest approach can be to make your annual accounts up to 31 March or 5 April, so that they match the tax year.</p>
+		<p>Special rules apply when you start or close a business, or if you decide to change your accounting period, to ensure that all your profits are fairly taxed.</p>
+		<p>To find out more about accounting periods for the self-employed and partnerships, <a href="https://www.gov.uk/government/publications/how-to-calculate-your-taxable-profits-hs222-self-assessment-helpsheet" target="_blank" rel="noopener noreferrer">download a helpsheet on how to calculate your taxable profits from the HM Revenue &amp; Customs (HMRC) website (PDF, 94K)</a>.</p>
+		
+		<h3>Accounting periods for limited companies</h3>
+		<p>Limited companies are required to submit annual accounts to Companies House, including a profit and loss account.</p>
+		<p>When you start a new company, the financial year automatically runs to the end of the month a year after the company is incorporated. For example, if a company is incorporated on 10 June, the first financial year runs from 10 June to 30 June the following year. But you can choose a different financial year end if you wish.</p>
+		<p>Your profit subject to Corporation Tax is normally based on an accounting period that matches this financial year. There are special rules if your accounting period is longer than twelve months (for example, if your new company makes up its accounts to a date more than a year away).</p>
+		<p>For more information, see our guide: <a href="/tax-payroll-and-company-information/corporation-tax/introduction-to-corporation-tax.php">introduction to Corporation Tax</a>.</p>
+		
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/cost-of-equipment.php" title="Cost of equipment">Prev</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business.php" title="Set up a simple profit and loss account for your business">Introduction</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/do-all-businesses-have-to-produce-formal-profit-and-loss-accounts.php">Do all businesses have to produce formal profit and loss accounts?</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/profit-reporting-how-when-and-where.php">Profit reporting: how, when and where?</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/records-required-for-producing-a-profit-and-loss-account-or-completing-a-tax-return.php">Records required for producing a profit and loss account or completing a tax return</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-income-sales.php">Business income: sales</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-income-other.php">Business income: other</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/recording-business-expenditure.php">Recording business expenditure</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/cost-of-sales.php">Cost of sales</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-expenses.php">Business expenses</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/cost-of-equipment.php">Cost of equipment</a></li>
+			<li>Profit and loss accounting periods and tax</li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/publications/how-to-calculate-your-taxable-profits-hs222-self-assessment-helpsheet" target="_blank" rel="noopener noreferrer">Download help sheet HS222 on how to calculate your taxable profits from HMRC (PDF, 94K)</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/corporation-tax.php">Corporation Tax</a></li>
+			<li><a href="/tax-payroll-and-company-information/companies-house-annual-returns-and-accounts/filing-accounts-with-companies-house-dates-and-checklist.php">Filing accounts with Companies House: dates and checklist</a></li>
+			<li><a href="/tax-payroll-and-company-information/company-administration-an-introduction/reporting-changes-to-companies-house/change-your-accounting-date.php">Change your accounting date</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php">Financial and management accounts: the basics</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/balance-sheets-the-basics.php">Balance sheets: the basics</a></li>
+			<li><a href="/it-and-ecommerce/software-and-business-applications/accounting-software.php">Accounting software</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/HMRC_logo.gif" width="134" height="35" alt="" /> HM Revenue &amp; Customs
+			</a>
+		</p>
+		<p>
+			<a href="http://www.icaew.com/" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/icaew_solus_logo_19x35px.JPG" width="19" height="35" alt="" /> Institute of Chartered Accountants in England and Wales
+			</a>
+		</p>
+
+		<img src="../../../_img/px_finance.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
