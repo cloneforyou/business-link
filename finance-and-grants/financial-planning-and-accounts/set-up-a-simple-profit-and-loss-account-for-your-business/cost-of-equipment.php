d4a97a66f62dc90a690d30fb3f37fb522d319d70
Caroline Hagan <info@blueocto.co.uk>
2017-09-24 16:18:35 +0100

    Completed "finance-and-grants > financial-planning-and-accounts > set-up-a-simple-profit-and-loss-account-for-your-business > Cost of equipment" page.

---
+++ finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/cost-of-equipment.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cost of equipment | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Set up a simple profit and loss account for your business</h1>
+		<h2>Cost of equipment</h2>
+		<p>Any items of equipment you have bought or leased for long-term use are called 'capital items' or 'fixed assets'. These might include:</p>
+		<ul>
+			<li>furniture</li>
+			<li>computer equipment</li>
+			<li>cars or vans necessary for the business</li>
+			<li>machinery</li>
+			<li>premises</li>
+		</ul>
+
+		<p>Depending on the size of your business, you can record the cost of equipment you buy in a separate register of equipment, the 'fixed asset register', or you can include it in your general expenditure records and show it as a 'capital item'.</p>
+		
+		<h3>Depreciation and allowances</h3>
+		<p>The cost of capital items is not deducted from your profits in the same way that ordinary business expenses are. Instead, you make a charge for depreciation each year, reflecting the fall in the value of the asset over time. This spreads the cost of the asset over several years.</p>
+		<p>For your own profit and loss, you typically choose a depreciation charge that provides a realistic reflection of how long the asset will be useful. For example, you might expect a computer to be obsolete after three years, so charge a third of its cost against profit each year.</p>
+		<p>Read about depreciation in our guide on how to <a href="/finance-and-grants/manage-your-cashflow/decide-whether-to-lease-or-buy-assets.php">decide whether to lease or buy assets</a>.</p>
+		<p>For tax purposes, depreciation is not an allowable expense. Instead, there are set allowances that you can claim. For small businesses, the costs of most asset purchases can be fully claimed against tax using the annual investment allowance (up to an annual limit of &pound;100,000).</p>
+		<p>See our guide on <a href="/tax-payroll-and-company-information/tax-reliefs-and-allowances/capital-allowances/capital-allowances-the-basics.php">capital allowances: the basics</a>.</p>
+		
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-expenses.php" title="Business expenses">Prev</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/profit-and-loss-accounting-periods-and-tax.php" title="Profit and loss accounting periods and tax">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business.php" title="Set up a simple profit and loss account for your business">Introduction</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/do-all-businesses-have-to-produce-formal-profit-and-loss-accounts.php">Do all businesses have to produce formal profit and loss accounts?</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/profit-reporting-how-when-and-where.php">Profit reporting: how, when and where?</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/records-required-for-producing-a-profit-and-loss-account-or-completing-a-tax-return.php">Records required for producing a profit and loss account or completing a tax return</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-income-sales.php">Business income: sales</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-income-other.php">Business income: other</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/recording-business-expenditure.php">Recording business expenditure</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/cost-of-sales.php">Cost of sales</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-expenses.php">Business expenses</a></li>
+			<li>Cost of equipment</li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/profit-and-loss-accounting-periods-and-tax.php">Profit and loss accounting periods and tax</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://web.archive.org/web/20070819175954/http://www.lloydstsbbusiness.com/support/businessguides/profit_and_loss.asp" target="_blank" rel="noopener noreferrer" title="Practical advice on creating and understanding business accounts">Profit, loss and balance sheet advice from LloydsTSB Business</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/set-up-a-basic-record-keeping-system.php">Set up a basic record-keeping system</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php">Financial and management accounts: the basics</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/balance-sheets-the-basics.php">Balance sheets: the basics</a></li>
+			<li><a href="/it-and-ecommerce/software-and-business-applications/accounting-software.php">Accounting software</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/HMRC_logo.gif" width="134" height="35" alt="" /> HM Revenue &amp; Customs
+			</a>
+		</p>
+		<p>
+			<a href="http://www.icaew.com/" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/icaew_solus_logo_19x35px.JPG" width="19" height="35" alt="" /> Institute of Chartered Accountants in England and Wales
+			</a>
+		</p>
+
+		<img src="../../../_img/px_finance.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
