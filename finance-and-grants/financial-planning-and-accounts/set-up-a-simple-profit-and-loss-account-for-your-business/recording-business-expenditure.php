fffb87d1cef6bf9c726b061d6a6c14b9d9ad2944
Caroline Hagan <info@blueocto.co.uk>
2017-09-24 16:09:05 +0100

    Completed "finance-and-grants > financial-planning-and-accounts > set-up-a-simple-profit-and-loss-account-for-your-business > Recording business expenditure" page.

---
+++ finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/recording-business-expenditure.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Recording business expenditure | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Set up a simple profit and loss account for your business</h1>
+		<h2>Recording business expenditure</h2>
+		<p>Business expenditure falls into three key areas for the purpose of reporting your profit or loss. You can save yourself, or your accountant, time by grouping your costs accordingly in your purchase list or 'ledger'. The three key areas are:</p>
+		<ul>
+			<li>cost of sales - the base cost of obtaining or creating your product</li>
+			<li>business expenses</li>
+			<li>cost of equipment you have bought or leased for long-term use</li>
+		</ul>
+
+		<p>If your business is VAT-registered, you will need to record details of any VAT included in your expenditure. See our guide on <a href="/tax-payroll-and-company-information/vat/vat-returns-and-accounts/accounts-and-records-for-your-vat.php">accounts and records for your VAT</a>.</p>
+		
+		<h3>Business expenditure back-up</h3>
+		<p>The back-up records for your business expenditure fall into two categories. As with sales records, they will vary according to your business type.</p>
+		<p><strong>1. Purchase/expenditure documentation</strong></p>
+		<ul>
+			<li>copies of supplier invoices/receipts issued to you</li>
+			<li>till receipts for items bought over the counter</li>
+			<li>payroll and National Insurance records if you have employees</li>
+		</ul>
+
+		<p><strong>2. Proof of expenditure relating to the above</strong></p>
+		<ul>
+			<li>cheque book stubs</li>
+			<li>bank statements</li>
+			<li>credit card statements and receipts</li>
+		</ul>
+
+		<p>It is important for you to be able to cross-reference your records to your expenditure figures if asked. If you mislay a receipt for a small item, make sure you enter it in your purchase or petty cash book ledger and make a note that you have lost the receipt.</p>
+		
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-income-other.php" title="Business income: other">Prev</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/cost-of-sales.php" title="Cost of sales">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business.php" title="Set up a simple profit and loss account for your business">Introduction</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/do-all-businesses-have-to-produce-formal-profit-and-loss-accounts.php">Do all businesses have to produce formal profit and loss accounts?</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/profit-reporting-how-when-and-where.php">Profit reporting: how, when and where?</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/records-required-for-producing-a-profit-and-loss-account-or-completing-a-tax-return.php">Records required for producing a profit and loss account or completing a tax return</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-income-sales.php">Business income: sales</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-income-other.php">Business income: other</a></li>
+			<li>Recording business expenditure</li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/cost-of-sales.php">Cost of sales</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-expenses.php">Business expenses</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/cost-of-equipment.php">Cost of equipment</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/profit-and-loss-accounting-periods-and-tax.php">Profit and loss accounting periods and tax</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://web.archive.org/web/20070819175954/http://www.lloydstsbbusiness.com/support/businessguides/profit_and_loss.asp" target="_blank" rel="noopener noreferrer" title="Practical advice on creating and understanding business accounts">Profit, loss and balance sheet advice from LloydsTSB Business</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/set-up-a-basic-record-keeping-system.php">Set up a basic record-keeping system</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php">Financial and management accounts: the basics</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/balance-sheets-the-basics.php"">Balance sheets: the basics</a></li>
+			<li><a href="/it-and-ecommerce/software-and-business-applications/accounting-software.php">Accounting software</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/HMRC_logo.gif" width="134" height="35" alt="" /> HM Revenue &amp; Customs
+			</a>
+		</p>
+		<p>
+			<a href="http://www.icaew.com/" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/icaew_solus_logo_19x35px.JPG" width="19" height="35" alt="" /> Institute of Chartered Accountants in England and Wales
+			</a>
+		</p>
+
+		<img src="../../../_img/px_finance.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
