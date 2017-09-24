70a6c1460ad591a763dcfefd27fecd87c5b11c3d
Caroline Hagan <info@blueocto.co.uk>
2017-09-24 18:21:39 +0100

    Completed "tax-payroll-and-company-information > vat > how-and-when-to-register-for-vat > when-to-register-for-uk-vat > You're doing business in the UK, or intend to start" page.

---
+++ tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-are-doing-business-in-the-uk-or-intend-to-start.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>You're doing business in the UK, or intend to start | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>When to register for UK VAT </h1>
+		<h2>You're doing business in the UK, or intend to start</h2>
+		
+		<h3>If your place of business is in the UK or you live here</h3>
+		<p>You may need to register for VAT, or you may be able to choose to register voluntarily, if you are doing any of the following kinds of business in the UK:</p>
+		<ul>
+			<li><strong>Supplying goods or services within the UK</strong>. If your turnover of VAT taxable goods and services supplied within the UK for the previous 12 months is more than the current registration threshold of &pound;77,000, or you expect it to go over that figure in the next 30 days alone, you must register for VAT. However, if your turnover has gone over the registration threshold temporarily then you may be able to apply for exception from registration. For more information, see the page in this guide on <a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/calculating-your-vat-taxable-turnover.php">calculating your VAT taxable turnover</a>.</li>
+			<li><strong>Taking over a VAT-registered business from someone else</strong>. You have to add your own VAT taxable turnover over the last 12 months (if any) to that of the business you're taking over. If the total goes over the registration threshold on the day of the takeover (currently &pound;77,000), you'll have to register. However, if your turnover has gone over the registration threshold temporarily then you may be able to apply for exception from registration</li>
+			<li><strong>Receiving goods from other countries in the European Union (EU)</strong>. If you have received goods from other EU countries in the UK (these are known as acquisitions) with a total value greater than &pound;77,000 in the current year since 1 January, or you expect to acquire more than that value in the next 30 days alone, you must register for VAT.</li>
+			<li><strong>Supplying goods or services from the UK to other countries</strong>. See the page in this guide on how <a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-supply-goods-or-services-from-the-uk-to-other-countries.php">you supply goods or services from the UK to other countries</a>.</li>
+			<li><strong>Supplying goods or services to the UK from other countries</strong>. See the page in this guide on when <a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-supply-goods-to-the-uk-from-abroad-or-intend-to-start.php">you supply goods to the UK from abroad, or intend to start</a>.</li>
+		</ul>
+
+		<h3>If your trading is below the threshold for registration</h3>
+		<p>If you're carrying out one or more of the business activities described above but you have not crossed the registration threshold, you can still apply to register for VAT voluntarily - it might be of benefit to you. You can find more information in the page in this guide on <a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/voluntary-registration.php">voluntary registration</a>. In any case, you should regularly check your turnover to see if you need to register.</p>
+		
+		<h3>If you have gone over the threshold for registration temporarily</h3>
+		<p>You can apply for exception from registration if:</p>
+		<ul>
+			<li>you have to register for VAT because the value of your taxable supplies in the previous 12 months has exceeded the registration threshold of &pound;77,000 (including the value of supplies made by a VAT-registered business that you have taken over)</li>
+			<li>you can demonstrate to HM Revenue &amp; Customs (HMRC) that in the longer term you will only be trading below the de-registration threshold of &pound;75,000.</li>
+		</ul>
+
+		<p>You can ask HMRC if they can make an exception, and allow you not to register for VAT, by filling in a VAT registration form, stating why you are applying for an exception.</p>
+		<ul>
+			<li>If HMRC agrees to make an exception and allow you not to register this time, you must let them know of any relevant change in circumstances - for example, if your turnover goes over the threshold again.</li>
+			<li>If HMRC does not agree to make an exception, you will become registered for VAT from the day you should have been registered. You will need to account for VAT from that date.</li>
+		</ul>
+		
+		<h3>If you haven't started to trade yet</h3>
+		<p>If you intend to start carrying out one of the business activities described above, you can choose to register for VAT voluntarily. See the page in this guide on <a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/voluntary-registration.php">voluntary registration</a>.</p>
+		
+		<h3>If you do business in the Isle of Man</h3>
+		<p>Although the Isle of Man is not part of the UK, for VAT purposes you should add together the value (or estimated value) of VAT taxable supplies made in both the UK and the Isle of Man to work out if you need to register.</p>
+		<p>For information on other supplies you may need to consider see the page in this guide on <a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/calculating-your-vat-taxable-turnover.php">calculating your VAT taxable turnover</a>.</p>
+		
+		<h3>If you don't live in the UK or your place of business isn't in the UK</h3>
+		<p>From 1 December 2012 the UK VAT registration threshold will no longer apply to you.</p>
+		<p>Your registration date will be the earliest date that either:</p>
+		<ul>
+			<li>you make any taxable supplies here</li>
+			<li>you expect to make any taxable supplies here within the next 30 days</li>
+		</ul>
+		
+		<p>You must register as a Non-Established Taxable Person (NETP). You may also want to appoint a tax representative or agent to keep VAT records and accounts on your behalf - that's up to you.</p>
+		<p>Find out how to register for VAT if you're an NETP in the page on <a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/how-to-register-for-vat/how-to-register-for-vat-if-you-do-business-internationally.php">how to register for VAT if you do business internationally</a> in our guide on <a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/how-to-register-for-vat.php">how to register for VAT</a>.</p>
+		<p>For more about taking over a business and VAT registration, historical VAT registration thresholds, how to appoint someone to deal with your VAT affairs, what happens if you register late and exemption from registration see the guides below.</p>
+		<p><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/taking-over-a-business-and-vat-registration.php">Taking over a business and VAT registration</a></p>
+		<p><a href="https://www.gov.uk/government/publications/vat-notice-7001-and-70011-supplement#P25_1337" target="_blank" rel="noopener noreferrer">Find out about historical VAT-registration thresholds in VAT Notice 700/1-11 on the HMRC website</a></p>
+		<p><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/appointing-someone-to-deal-with-your-vat-affairs.php">Appointing someone to deal with your VAT affairs</a></p>
+		<p><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/how-to-register-for-vat.php">How to register for VAT</a></p>
+		<p><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/late-vat-registrations.php" title="If you should have registered for VAT but haven't, you must let HM Revenue & Customs know as soon as possible">Late VAT registrations</a></p>
+		<p><a href="https://www.gov.uk/government/publications/vat-notice-7001-should-i-be-registered-for-vat#P224_22474" target="_blank" rel="noopener noreferrer">Find out about exemption from VAT registration in VAT Notice 700/1 on the HMRC website</a></p>
+		
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/who-can-and-cannot-register-for-vat.php" title="Who can and cannot register for VAT">Prev</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-supply-goods-to-the-uk-from-abroad-or-intend-to-start.php" title="You supply goods to the UK from abroad, or intend to start">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat.php" title="When to register for UK VAT ">Introduction</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/who-can-and-cannot-register-for-vat.php">Who can and cannot register for VAT</a></li>
+			<li>You're doing business in the UK, or intend to start</li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-supply-goods-to-the-uk-from-abroad-or-intend-to-start.php">You supply goods to the UK from abroad, or intend to start</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-supply-goods-or-services-from-the-uk-to-other-countries.php">You supply goods or services from the UK to other countries</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/voluntary-registration.php">Voluntary registration</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/calculating-your-vat-taxable-turnover.php">Calculating your VAT taxable turnover</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/do-not-avoid-registering-for-vat-by-artificially-separating-business-activities.php">Do not avoid registering for VAT by artificially separating business activities</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/vat-registration" target="_blank" rel="noopener noreferrer">Online demonstrator from HMRC</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/how-to-register-for-vat.php">How to register for VAT</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/taking-over-a-business-and-vat-registration.php">Taking over a business and VAT registration</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/appointing-someone-to-deal-with-your-vat-affairs.php">Appointing someone to deal with your VAT affairs</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/late-vat-registrations.php">Late VAT registrations</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/HMRC_logo.gif" width="134" height="35" alt="" /> HM Revenue &amp; Customs
+			</a>
+		</p>
+
+		<img src="/_img/px_taxandreporting.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
