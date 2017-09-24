61c647a3807cfe8bfdcfa5bcdfbc908f9a462b0c
Caroline Hagan <info@blueocto.co.uk>
2017-09-24 17:40:34 +0100

    Completed "finance-and-grants > financial-planning-and-accounts > choose-and-work-with-an-accountant > Checklist: ten things to ask your prospective accountant" page.

---
+++ finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant/checklist-ten-things-to-ask-your-prospective-accountant.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Checklist: ten things to ask your prospective accountant | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+		<h1>Choose and work with an accountant</h1>
+		<h2>Checklist: ten things to ask your prospective accountant</h2>
+		<p>Different accountants can help your business in different ways, so you should always think about what you are looking for before choosing an accountant. For example, you may only need basic bookkeeping, or you may want someone who can give you specialist business advice.</p>
+		<p>Once you have drawn up a shortlist of potential accountants who would be suitable, you should arrange to meet them to make sure they are the ideal choice for your business.</p>
+		<p>You should take your business plan and other useful information about your business to the meeting. A good accountant should want to know as much about you as you do about them.</p>
+		<p>They should also be happy to pass on names of clients for you to take up references.</p>
+		<p>To make sure you get the best value for money, there are things you should find out about any prospective accountant. You should:</p>
+		<ul>
+			<li>ask about their qualifications</li>
+			<li>find out how many partners there are in the practice, and who will look after your business on a day-to-day basis</li>
+			<li>make sure they are experienced in dealing with businesses of a similar size and at a similar stage of growth to yours, and how they could help you develop your business</li>
+			<li>assess whether they understand your business sector and its needs</li>
+			<li>ask about their estimated response times</li>
+			<li>check if their service will be proactive - eg whether they will remind you when you need to submit accounts or send you updates on changes in tax law</li>
+			<li>find out if they offer any additional services - eg inheritance planning or advice on information systems</li>
+			<li>ask whether they offer any specialist services - eg in start-ups or stock-market listing</li>
+			<li>what services they will charge for and how and when you will pay for them - you may be able to pay a fixed fee for the first 12 months</li>
+			<li>the level of access you will be given to the data held about your business - you might need this data to update your business plan or for tender documents and you will also want easy access to it if you ever decide to change accountants</li>
+		</ul>
+
+		<p>For more information, see the page in this guide on <a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant/choosing-an-accountant.php">choosing an accountant</a>.</p>
+		
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant/choosing-an-accountant.php" title="Choosing an accountant">Prev</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant/managing-the-relationship-with-your-accountant.php" title="Managing the relationship with your accountant">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php" title="Choose and work with an accountant">Introduction</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant/how-an-accountant-can-help-your-business.php">How an accountant can help your business</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant/choosing-an-accountant.php">Choosing an accountant</a></li>
+			<li>Checklist: ten things to ask your prospective accountant</li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant/managing-the-relationship-with-your-accountant.php">Managing the relationship with your accountant</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.accaglobal.com/gb/en/member/find-an-accountant.html" target="_blank" rel="noopener noreferrer">Chartered accountant search on the Association of Chartered Certified Accountants website</a></li>
+			<li><a href="http://www.icaew.com/en/about-icaew/find-a-chartered-accountant" target="_blank" rel="noopener noreferrer">Accountant selection advice on the Institute of Chartered Accountants in England and Wales website</a></li>
+			<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the business records you must keep</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php">Financial and management accounts: the basics</a></li>
+			<li><a href="/grow-your-business/growth-through-strategic-sourcing/outsourcing.php">Outsourcing</a></li>
+			<li><a href="/employment-and-skills/staff-documents-and-employment-policies/the-employment-contract.php">The employment contract</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="http://www.icaew.com/" target="_blank" rel="noopener noreferrer">
+				<img src="../../_img/icaew_solus_logo_19x35px.JPG" width="19" height="35" alt="" /> 
+				Institute of Chartered Accountants in England and Wales
+			</a>
+		</p>
+
+		<img src="../../../_img/px_finance.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
