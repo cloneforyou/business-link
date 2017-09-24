c910e8ceba74c5bf0c987fcc2b474db61200ccbf
Caroline Hagan <info@blueocto.co.uk>
2017-09-24 15:14:16 +0100

    Another file 80% - picked up by Search Console.

--- buy-or-sell-a-business/floating-on-the-stock-market.php
+++ buy-or-sell-a-business/floating-on-the-stock-market.php
@@ -15,43 +15,44 @@
 
 		<h1>Floating on the stock market</h1>
 		<h2>Introduction</h2>
-		<span><img src="../_img/SC1010.gif" width="94" height="62" border="0" alt="" /></span>
+		<img src="../_img/SC1010.gif" width="94" height="62" border="0" alt="" />
 		<p>Floating your business on a stock market involves selling a percentage of your business in the form of shares, which are subsequently traded. There is a choice of stock markets in the UK, but the two largest are the London Stock Exchange and PLUS Markets Group.</p>
 		<p>Joining a stock market turns your business into a 'public company'. This presents a range of benefits, including access to capital, and the opportunity to increase your businesses profile and motivate staff through the introduction of share option schemes. There are also additional responsibilities and costs which come with being a publicly quoted company and these require careful consideration. You should note that floating on a stock market is not always the best option for obtaining growth capital and you should make sure to consider all your options before making a final decision.</p>
 		<p>This guide explains some of the benefits and matters for consideration, when thinking of floating your business on a stock exchange.</p>
+		
 		<ul>
-			<li><a href="floating-on-the-stock-market/is-your-business-suitable-to-be-a-public-company.php" title="Is your business suitable to be a public company?">Next</a></li>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market/is-your-business-suitable-to-be-a-public-company.php" title="Is your business suitable to be a public company?">Next</a></li>
 		</ul>
+		
 		<h3>Subjects covered in this guide</h3>
 		<ul>
 			<li>Introduction</li>
-			<li><a href="floating-on-the-stock-market/is-your-business-suitable-to-be-a-public-company.php">Is your business suitable to be a public company?</a></li>
-			<li><a href="floating-on-the-stock-market/advantages-and-disadvantages-of-stock-market-flotation.php">Advantages and disadvantages of stock market flotation</a></li>
-			<li><a href="floating-on-the-stock-market/the-process-of-floating-on-a-stock-market.php">The process of floating on a stock market</a></li>
-			<li><a href="floating-on-the-stock-market/appoint-your-advisers.html">Appoint your advisers</a></li>
-			<li><a href="../finance-and-grants/finance-options/shares-and-equity-finance/here-is-how-we-prepared-our-business-for-flotation.php">Here's how we prepared our business for flotation</a></li>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market/is-your-business-suitable-to-be-a-public-company.php">Is your business suitable to be a public company?</a></li>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market/advantages-and-disadvantages-of-stock-market-flotation.php">Advantages and disadvantages of stock market flotation</a></li>
+			<li><a href="/buy-or-sell-a-business/floating-on-the-stock-market/the-process-of-floating-on-a-stock-market.php">The process of floating on a stock market</a></li>
+			<li><a href="/finance-and-grants/finance-options/shares-and-equity-finance/floating-on-the-stock-market/appoint-your-advisers.php">Appoint your advisers</a></li>
+			<li><a href="/finance-and-grants/finance-options/shares-and-equity-finance/floating-on-the-stock-market/here-is-how-we-prepared-our-business-for-flotation.php">Here's how we prepared our business for flotation</a></li>
 		</ul>
-		<p><img src="../_img/hr_325px_global.gif" alt="" /></p>
-		<ul>
-			<li><a href="#">Add guide to my print portfolio (print later as PDF)</a></li>
-			<li><a href="#">Print page now</a></li>
-			<li><a href="#">Tell a friend about this page</a></li>
-			<li><a href="#">Send me an email alert when this page changes</a></li>
-		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
 		<h3>London Stock Exchange Enquiry Line</h3>
-		<p>020 7797 3429<br/>
-		</p>
-		<p><img src="../_img/hr_140px_global.gif" alt="" /></p>
+		<p>020 7797 3429</p>
+
+		<hr />
+
 		<h3>Actions</h3>
 		<ul>
-			<li><a href="http://www.londonstockexchange.com/companies-and-advisors/listing/float/float.htm" target="_blank">Flotation advantages and disadvantages explained on the London Stock Exchange website<span> - Opens in a new window</span></a></li>
-			<li><a href="#">PLUS market guidance on the PLUS website<span> - Opens in a new window</span></a></li>
-			<li><a href="http://www.londonstockexchange.com/companies-and-advisors/main-market/main-market/home.htm" target="_blank">Main Market introduction on the London Stock Exchange website<span> - Opens in a new window</span></a></li>
-			<li><a href="http://www.londonstockexchange.com/companies-and-advisors/aim/aim/aim.htm" target="_blank">AIM introduction on the London Stock Exchange website<span> - Opens in a new window</span></a></li>
-			<li><a href="http://www.londonstockexchange.com/companies-and-advisors/listing/markets/guide-to-capital-markets.pdf" target="_blank">Download Capital Markets information from the London Stock Exchange website (PDF, 1.68MB)<span> - Opens in a new window</span></a></li>
-			<li><a href="../finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php" title="This interactive tool provides you with a personalised list of the most suitable finance options for your business">Use our interactive tool to identify the right finance options for your business</a></li>
+			<li><a href="http://www.londonstockexchange.com/companies-and-advisors/listing/float/float.htm" target="_blank" rel="noopener noreferrer">Flotation advantages and disadvantages explained on the London Stock Exchange website</a></li>
+			<li><a href="#">PLUS market guidance on the PLUS website</a></li>
+			<li><a href="http://www.londonstockexchange.com/companies-and-advisors/main-market/main-market/home.htm" target="_blank" rel="noopener noreferrer">Main Market introduction on the London Stock Exchange website</a></li>
+			<li><a href="http://www.londonstockexchange.com/companies-and-advisors/aim/aim/aim.htm" target="_blank" rel="noopener noreferrer">AIM introduction on the London Stock Exchange website</a></li>
+			<li><a href="http://www.londonstockexchange.com/companies-and-advisors/listing/markets/guide-to-capital-markets.pdf" target="_blank" rel="noopener noreferrer">Download Capital Markets information from the London Stock Exchange website (PDF, 1.68MB)</a></li>
+			<li><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Use our interactive tool to identify the right finance options for your business</a></li>
 		</ul>
+
 		<img src="../_img/px_sellingabusiness.gif" alt="" width="181" height="5" />
+
 
 	</div><!-- // .small-12 large-8 large-push-4 columns -->
 
