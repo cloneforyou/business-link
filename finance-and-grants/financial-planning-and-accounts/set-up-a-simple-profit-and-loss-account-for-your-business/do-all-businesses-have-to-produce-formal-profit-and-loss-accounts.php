6d50ff71e3643d7970d3b8b3da79b478c348d93e
Caroline Hagan <info@blueocto.co.uk>
2017-09-24 15:47:11 +0100

    whoops just finish that file off.

--- finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/do-all-businesses-have-to-produce-formal-profit-and-loss-accounts.php
+++ finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/do-all-businesses-have-to-produce-formal-profit-and-loss-accounts.php
@@ -47,8 +47,6 @@
 			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
 		</ul>
 
-		<img src="../../../_img/px_finance.gif" alt="" width="181" height="5" />
-
 		<p>Developed with:<br />
 			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
 				<img src="/_img/HMRC_logo.gif" width="134" height="35" alt="" /> HM Revenue &amp; Customs
@@ -59,119 +57,9 @@
 				<img src="/_img/icaew_solus_logo_19x35px.JPG" width="19" height="35" alt="" /> Institute of Chartered Accountants in England and Wales
 			</a>
 		</p>
-		
 
-		<table cellpadding="0" cellspacing="0" width="100%">
-				<tr>
-					<td rowspan="16"><img src="../../../_img/px_white.gif" alt="" width="5" height="6" /></td>
-					<td colspan="2">
-						<p>
-							<a href="layere2de.html?topicId=1073858790" title="Find and manage money for your business">Finance and grants</a>
-						</p>
-						<p><img src="../../../_img/leftnav_sub_hr.gif" alt="" /></p>
-					</td>
-				</tr>
-				<tr>
-					<td colspan="2">
-						<p><a href="layer69e1.html?topicId=1073858944">Financial planning and accounts</a>
-						</p>
-						<p><img src="../../../_img/leftnav_sub_hr.gif" alt="" /></p>
-					</td>
-				</tr>
-				<tr>
-					<td> </td>
-					<td>
-						<p><strong>Set up a simple profit and loss account for your business</strong></p>
-					</td>
-				</tr>
-				<tr>
-					<td width="10"> </td>
-					<td>
-						<p><img src="../../../_img/leftnav_sub_sub_hr.gif" alt="" /></p>
-					</td>
-				</tr>
-				<tr>
-					<td width="10"> </td>
-					<td>
-						<p><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business.php" title="The financial records you need to keep in order to report your profit or loss">Introduction</a>
-						</p>
-					</td>
-				</tr>
-				<tr>
-					<td><img src="../../../_img/px_finance.gif" alt="Current section" width="5" height="5" /></td>
-					<td>
-						<p><strong>Do all businesses have to produce formal profit and loss accounts?</strong></p>
-					</td>
-				</tr>
-				<tr>
-					<td> </td>
-					<td>
-						<p><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/profit-reporting-how-when-and-where.php">Profit reporting: how, when and where?</a>
-						</p>
-					</td>
-				</tr>
-				<tr>
-					<td> </td>
-					<td>
-						<p><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/records-required-for-producing-a-profit-and-loss-account-or-completing-a-tax-return.php">Records required for producing a profit and loss account or completing a tax return</a>
-						</p>
-					</td>
-				</tr>
-				<tr>
-					<td> </td>
-					<td>
-						<p><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-income-sales.php">Business income: sales</a>
-						</p>
-					</td>
-				</tr>
-				<tr>
-					<td> </td>
-					<td>
-						<p><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-income-other.php">Business income: other</a>
-						</p>
-					</td>
-				</tr>
-				<tr>
-					<td> </td>
-					<td>
-						<p><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/recording-business-expenditure.php">Recording business expenditure</a>
-						</p>
-					</td>
-				</tr>
-				<tr>
-					<td> </td>
-					<td>
-						<p><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/cost-of-sales.php">Cost of sales</a>
-						</p>
-					</td>
-				</tr>
-				<tr>
-					<td> </td>
-					<td>
-						<p><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/business-expenses.php">Business expenses</a>
-						</p>
-					</td>
-				</tr>
-				<tr>
-					<td> </td>
-					<td>
-						<p><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/cost-of-equipment.php">Cost of equipment</a>
-						</p>
-					</td>
-				</tr>
-				<tr>
-					<td> </td>
-					<td>
-						<p><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business/profit-and-loss-accounting-periods-and-tax.php">Profit and loss accounting periods and tax</a>
-						</p>
-					</td>
-				</tr>
-				<tr>
-					<td colspan="2">
-						<p> </p>
-					</td>
-				</tr>
-			</table>
+		<img src="../../../_img/px_finance.gif" alt="" width="181" height="5" />
+		
 
 	</div><!-- // .small-12 large-8 large-push-4 columns -->
 
