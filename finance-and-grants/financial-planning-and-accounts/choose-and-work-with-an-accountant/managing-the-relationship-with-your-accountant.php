f85620b43b2916fdd59380c46c61e525a51e45f6
Caroline Hagan <info@blueocto.co.uk>
2017-09-24 17:43:03 +0100

    Completed "finance-and-grants > financial-planning-and-accounts > choose-and-work-with-an-accountant > Managing the relationship with your accountant" page.

---
+++ finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant/managing-the-relationship-with-your-accountant.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Managing the relationship with your accountant | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+		<h1>Choose and work with an accountant</h1>
+		<h2>Managing the relationship with your accountant</h2>
+		<p>To get the best out of your accountant you should arrange to have regular contact with them. You should always allow yourself plenty of time to discuss tax implications before the preparation of end-of-year accounts. Your accountant can also help you to manage and develop your business.</p>
+		<p>It is important to maintain a good relationship with your accountant. You should make sure that you always:</p>
+		<ul>
+			<li>Keep agreed records or deadlines.</li>
+			<li>Inform your accountant of any changes to your business.</li>
+			<li>Discuss major issues with your accountant - such as tax implications of amounts you take out as drawings, salary or dividend.</li>
+			<li>Pay your accountant on time. If you don't, your accountant has the right to withhold your figures and other business data until payment has been made, except for limited company accounts that must be filed with Companies House.</li>
+		</ul>
+
+		<p>However, just like with any business relationship, there could be problems with your accountant. For example:</p>
+		<ul>
+			<li>they may be difficult to contact</li>
+			<li>they could be failing to provide you with necessary information</li>
+			<li>you could be receiving unexpected fee invoices</li>
+			<li>you might need an accountant with different specialist skills and experience - eg if your business is expanding</li>
+			<li>the service offered may not be as proactive as your business requires</li>
+		</ul>
+
+		<p>Your accountant should be able to adapt as the needs of your business change. However, every three to five years, it is a good idea to review the relationship with your accountant. You should ask yourself:</p>
+		<ul>
+			<li>Am I still getting value for money?</li>
+			<li>Is my accountant informative and easy to contact?</li>
+			<li>Does my accountant still suit the needs of my business?</li>
+		</ul>
+
+		<p>You may find that as your business changes, so do your requirements for an accountant.</p>
+		
+		<h3>Changing your accountant</h3>
+		<p>If you choose to change your accountant, you should manage the process carefully. You should bear in mind that you must terminate the agreement with your existing accountant before you sign any agreement with a new one.</p>
+		<p>Before you do anything else check the terms and conditions that you have agreed with your accountant. You may find there is a notice period you must honour. The terms of the relationship with your accountant are set out in the letter of engagement issued to you.</p>
+		<p>You should then check that you have access to all the data relating to your business. You should make sure that these are transferred to your new accountant prior to the handover.</p>
+		
+		<h3>If you need to complain about your accountant</h3>
+		<p>If for any reason you suspect your accountant of misconduct then you could raise this with them directly. If you are still not satisfied you can make a complaint to the professional body they are accredited with. However, the professional bodies will not help you get compensation for any funds lost as a result of the misconduct - to do this you will need to seek legal advice.</p>
+		
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant/checklist-ten-things-to-ask-your-prospective-accountant.php" title="Checklist: ten things to ask your prospective accountant">Prev</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php" title="Choose and work with an accountant">Introduction</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant/how-an-accountant-can-help-your-business.php">How an accountant can help your business</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant/choosing-an-accountant.php">Choosing an accountant</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant/checklist-ten-things-to-ask-your-prospective-accountant.php">Checklist: ten things to ask your prospective accountant</a></li>
+			<li>Managing the relationship with your accountant</li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Companies House Contact Centre</h3>
+		<p>0303 1234 500</p>
+		
+		<hr />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.icaew.com/en/technical/ethics/icaew-code-of-ethics" target="_blank" rel="noopener noreferrer">Professional ethics guide on the Institute of Chartered Accountants in England and Wales website</a></li>
+			<li><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Solicitor search tool on the Law Society website</a></li>
+			<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the business records you must keep</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php">Financial and management accounts: the basics</a></li>
+			<li><a href="/grow-your-business/growth-through-strategic-sourcing/outsourcing.php">Outsourcing</a></li>
+			<li><a href="/finance-and-grants/expert-financial-advice/choose-and-work-with-a-solicitor.php">Choose and work with a solicitor</a></li>
+			<li><a href="/employment-and-skills/staff-documents-and-employment-policies/the-employment-contract.php">The employment contract</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="http://www.icaew.com/" target="_blank" rel="noopener noreferrer">
+				<img src="../../_img/icaew_solus_logo_19x35px.JPG" width="19" height="35" alt="" /> 
+				Institute of Chartered Accountants in England and Wales
+			</a>
+		</p>
+
+		<img src="../../../_img/px_finance.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
