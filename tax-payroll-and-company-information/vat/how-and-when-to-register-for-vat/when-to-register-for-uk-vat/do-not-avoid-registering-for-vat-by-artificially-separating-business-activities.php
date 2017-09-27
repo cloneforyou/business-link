6ef038571e0d26d0088d4f260865c9f9dcc406ad
Caroline Hagan <info@blueocto.co.uk>
2017-09-27 15:05:22 +0100

    Completed "tax-payroll-and-company-information > vat > how-and-when-to-register-for-vat > when-to-register-for-uk-vat > Do not avoid registering for VAT by artificially separating business activities" page.

---
+++ tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/do-not-avoid-registering-for-vat-by-artificially-separating-business-activities.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Do not avoid registering for VAT by artificially separating business activities | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>When to register for UK VAT </h1>
+		<h2>Do not avoid registering for VAT by artificially separating business activities</h2>
+		<p>If you run more than one business, the sales in all those businesses must normally be added together to determine whether or not you must register for VAT.</p>
+		<p>However, if you are involved in the running of several separate legal entities, you may not need to combine the sales of those businesses to find whether you need to be VAT registered.</p>
+		<p>If HM Revenue &amp; Customs (HMRC) decides that you have artificially separated one business into smaller parts to avoid registering for VAT, it can decide that the entire business is a single taxable person and therefore must be registered for VAT. For more information, see the page in this guide on <a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/who-can-and-cannot-register-for-vat.php">who can and cannot register for VAT</a>.</p>
+		<p>Situations that HMRC may consider a single taxable person for VAT purposes include:</p>
+		<ul>
+			<li><strong>Separate entities selling to registered and unregistered customers</strong>. The VAT-registered entity sells only to VAT-registered customers and the entity not registered for VAT sells to customers who are not registered for VAT.</li>
+			<li><strong>The same equipment or premises being used by different entities on a regular basis.</strong> The premises and/or equipment are owned by one of the parties, who charges rent to the others. This situation may occur in businesses such as launderettes and takeaway food operations.</li>
+			<li><strong>Splitting up what is usually a single sale.</strong> This is common in industries such as the bed and breakfast trade, where one business supplies the bed and another the breakfast.</li>
+		</ul>
+
+		<p>If you deliberately avoid registering for VAT, you may be liable for a penalty. For serious offences, the matter will be investigated and you may be prosecuted. You can read about how to register for VAT, what happens if you do not register for VAT on time and more about artificial separation of business activities in the guides below.</p>
+		<p><a href="/tax-payroll-and-company-information/vat/vat-problems-mistakes-adjustments-visits-and-questions/penalties-for-mistakes-and-delays-with-your-vat.php">Penalties for mistakes and delays with your VAT</a></p>
+		<p><a href="https://www.gov.uk/government/publications/vat-notice-7001-should-i-be-registered-for-vat/vat-notice-7001-should-i-be-registered-for-vat" target="_blank" rel="noopener noreferrer">Find out about artificial separation of business activities in section 13 of VAT Notice 700/1 on the HMRC website</a>&nbsp;</p>
+		<p><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/how-to-register-for-vat.php">How to register for VAT</a></p>
+		<p>If you have any questions about whether you should register for VAT, you can contact the HMRC VAT Helpline.</p>
+		<p><a href="https://www.gov.uk/government/organisations/hm-revenue-customs/contact" target="_blank" rel="noopener noreferrer">Find contact details for the VAT Helpline on the HMRC website</a></p>
+		
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/calculating-your-vat-taxable-turnover.php" title="Calculating your VAT taxable turnover">Prev</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat.php" title="When to register for UK VAT ">Introduction</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/who-can-and-cannot-register-for-vat.php">Who can and cannot register for VAT</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-are-doing-business-in-the-uk-or-intend-to-start.php">You're doing business in the UK, or intend to start</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-supply-goods-to-the-uk-from-abroad-or-intend-to-start.php">You supply goods to the UK from abroad, or intend to start</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-supply-goods-or-services-from-the-uk-to-other-countries.php">You supply goods or services from the UK to other countries</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/voluntary-registration.php">Voluntary registration</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/calculating-your-vat-taxable-turnover.php">Calculating your VAT taxable turnover</a></li>
+			<li>Do not avoid registering for VAT by artificially separating business activities</li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/vat-registration" target="_blank" rel="noopener noreferrer">Online demonstrator from HMRC</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/how-to-register-for-vat.php">How to register for VAT</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-problems-mistakes-adjustments-visits-and-questions/penalties-for-mistakes-and-delays-with-your-vat.php">Penalties for mistakes and delays with your VAT</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/HMRC_logo.gif" width="134" height="35" alt="" /> HM Revenue &amp; Customs
+			</a>
+		</p>
+
+		<img src="/_img/px_taxandreporting.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
