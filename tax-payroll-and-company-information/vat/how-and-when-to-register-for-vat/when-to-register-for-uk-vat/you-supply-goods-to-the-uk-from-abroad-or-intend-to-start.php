1ee3dc0d524d78291e4fb3cdce28b47e36e0d260
Caroline Hagan <info@blueocto.co.uk>
2017-09-27 14:35:45 +0100

    Completed "tax-payroll-and-company-information > vat > how-and-when-to-register-for-vat > when-to-register-for-uk-vat > You supply goods to the UK from abroad, or intend to start" page.

---
+++ tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-supply-goods-to-the-uk-from-abroad-or-intend-to-start.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>You supply goods to the UK from abroad, or intend to start | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>When to register for UK VAT </h1>
+		<h2>You supply goods to the UK from abroad, or intend to start</h2>
+		<p>You may need to register for VAT, or you may be able to choose to register voluntarily, if you are doing any of the following kinds of business in the UK:</p>
+		<ul>
+			<li><strong>Supplying and delivering goods from another European Union (EU) country to non VAT-registered customers in the UK</strong>. If you sell and deliver goods (or arrange for their delivery) from another EU country to customers in the UK who aren't registered for VAT and don't have to be registered, this is known as distance selling. Common examples are mail order and internet sales. If the value of your distance sales in the calendar year from 1 January goes over the distance selling threshold (currently &pound;70,000) you must register for UK VAT. You can also register voluntarily, if you are trading below the threshold or you intend to start trading.</li>
+			<li><strong>Supplying goods on which an 8th or 13th Directive claim has been made or is going to be made</strong>. In certain circumstances you have to register if you sell, supply or otherwise dispose of an asset on which someone has claimed - or intends to claim - an 8th or 13th Directive refund.</li>
+			<li><strong>Supplying and delivering excise goods from another country</strong>. If you sell and deliver goods from another country that are liable to UK Excise Duty (such as tobacco or alcohol) to UK customers, then you must register for VAT in the UK. There's no threshold - everyone must register.</li>
+		</ul>
+
+		<p><a href="/tax-payroll-and-company-information/vat/international-trade-international-visits-and-vat/distance-selling-into-the-uk-from-other-eu-countries-and-vat.php">Distance selling into the UK from other EU countries and VAT</a></p>
+		<p><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/how-to-register-for-vat.php">How to register for VAT</a></p>
+		
+		<h3>If you don't have a place of business in the UK or live here</h3>
+		<p>If you have to register for one of the reasons described above, but you don't live in the UK or have a place of business here, then you must register as a Non-Established Taxable Person (NETP). You may also want to appoint a tax agent or representative to keep VAT records and accounts on your behalf - that's up to you.</p>
+		<p>See the page on <a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/how-to-register-for-vat/how-to-register-for-vat-if-you-do-business-internationally.php">how to register for VAT if you do business internationally</a> in our guide on <a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/how-to-register-for-vat.php">how to register for VAT</a>.</p>
+		
+		<h3>Disposing of assets on which 8th or 13th Directive refunds have been claimed</h3>
+		<p>The 8th Directive allows someone registered for VAT in one EU country to reclaim VAT they have paid in another EU country. The 13th Directive allows businesses established outside the EU to reclaim VAT paid within the EU.</p>
+		<p>You will have to register for VAT if you claimed, or intend to claim, a repayment under either of these Directives on goods, and both the following apply:</p>
+		<ul>
+			<li>the goods are business assets - or are incorporated into business assets</li>
+			<li>you make a VAT taxable supply of those goods, or intend to make one in the next 30 days</li>
+		</ul>
+
+		<p>You'll also have to register if the repayment was claimed by someone who then transferred the assets to you under the Transfer of a Going Concern provisions. This applies even if assets were transferred more than once - you may have to check back several transfers.</p>
+		<p>There's no registration threshold. For any disposal of these assets in the circumstances outlined above, you must register.</p>
+		
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-are-doing-business-in-the-uk-or-intend-to-start.php" title="You're doing business in the UK, or intend to start">Prev</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-supply-goods-or-services-from-the-uk-to-other-countries.php" title="You supply goods or services from the UK to other countries">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat.php" title="When to register for UK VAT ">Introduction</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/who-can-and-cannot-register-for-vat.php">Who can and cannot register for VAT</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-are-doing-business-in-the-uk-or-intend-to-start.php">You're doing business in the UK, or intend to start</a></li>
+			<li>You supply goods to the UK from abroad, or intend to start</li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-supply-goods-or-services-from-the-uk-to-other-countries.php">You supply goods or services from the UK to other countries</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/voluntary-registration.php">Voluntary registration</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/calculating-your-vat-taxable-turnover.php">Calculating your VAT taxable turnover</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/do-not-avoid-registering-for-vat-by-artificially-separating-business-activities.php">Do not avoid registering for VAT by artificially separating business activities</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/send-vat-return" target="_blank" rel="noopener noreferrer">Log in to ESL online service</a></li>
+			<li><a href="https://www.gov.uk/vat-registration" target="_blank" rel="noopener noreferrer">Online demonstrator from HMRC</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/how-to-register-for-vat.php" title="Apply for VAT registration online and download VAT registration forms">How to register for VAT</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/HMRC_logo.gif" width="134" height="35" alt="" /> HM Revenue &amp; Customs
+			</a>
+		</p>
+
+		<img src="/_img/px_taxandreporting.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
