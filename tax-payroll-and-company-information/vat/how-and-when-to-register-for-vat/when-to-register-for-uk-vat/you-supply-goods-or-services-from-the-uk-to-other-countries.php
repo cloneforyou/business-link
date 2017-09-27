698c28404279efe27f83d9248eb8d26fee7d02c9
Caroline Hagan <info@blueocto.co.uk>
2017-09-27 14:46:08 +0100

    Completed "tax-payroll-and-company-information > vat > how-and-when-to-register-for-vat > when-to-register-for-uk-vat > You supply goods or services from the UK to other countries" page.

---
+++ tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-supply-goods-or-services-from-the-uk-to-other-countries.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>You supply goods or services from the UK to other countries | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>When to register for UK VAT </h1>
+		<h2>You supply goods or services from the UK to other countries</h2>
+		<p>If you supply goods and services both inside and outside the UK, then you may need to register for UK VAT if the value of your UK supplies alone exceeds the registration threshold.</p>
+		<p>You don't need to include supplies you make in other countries when calculating your VAT taxable turnover for registration purposes - so leave out of your calculation any goods and services you supply where the place of supply is another country rather than the UK.</p>
+		<p><a href="/tax-payroll-and-company-information/vat/international-trade-international-visits-and-vat/exports-supplies-of-goods-and-services-overseas-charging-vat/how-to-work-out-your-place-of-supply-of-services-for-vat.php">How to work out your place of supply of services for VAT</a></p>
+		
+		<h3>Distance selling</h3>
+		<p>If you supply and deliver goods, or have them delivered - for example, by mail or another delivery service - to customers in other European Union (EU) countries who are not registered for VAT and don't have to be registered, this is known as distance selling. If you are required to register and account for VAT on distance sales in another EU country then you don't include the value of those distance sales when working out whether you need to register for UK VAT. You can find out whether you need to register for VAT in other EU countries in our guide below.</p>
+		<p><a href="/tax-payroll-and-company-information/vat/international-trade-international-visits-and-vat/exports-supplies-of-goods-and-services-overseas-charging-vat/exports-dispatches-supplying-goods-abroad-charging-vat.php">Exports, despatches, supplying goods abroad: charging VAT</a></p>
+		
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-supply-goods-to-the-uk-from-abroad-or-intend-to-start.php" title="You supply goods to the UK from abroad, or intend to start">Prev</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/voluntary-registration.php" title="Voluntary registration">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat.php" title="When to register for UK VAT ">Introduction</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/who-can-and-cannot-register-for-vat.php">Who can and cannot register for VAT</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-are-doing-business-in-the-uk-or-intend-to-start.php">You're doing business in the UK, or intend to start</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-supply-goods-to-the-uk-from-abroad-or-intend-to-start.php">You supply goods to the UK from abroad, or intend to start</a></li>
+			<li>You supply goods or services from the UK to other countries</li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/voluntary-registration.php">Voluntary registration</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/calculating-your-vat-taxable-turnover.php">Calculating your VAT taxable turnover</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/do-not-avoid-registering-for-vat-by-artificially-separating-business-activities.php">Do not avoid registering for VAT by artificially separating business activities</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/send-vat-return" target="_blank" rel="noopener noreferrer">Log in to ESL online service</a></li>
+			<li><a href="https://www.gov.uk/vat-registration" target="_blank" rel="noopener noreferrer">Online demonstrator from HMRC</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/international-trade-international-visits-and-vat/exports-supplies-of-goods-and-services-overseas-charging-vat/how-to-work-out-your-place-of-supply-of-services-for-vat.php">How to work out your place of supply of services for VAT</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/international-trade-international-visits-and-vat/exports-supplies-of-goods-and-services-overseas-charging-vat/exports-dispatches-supplying-goods-abroad-charging-vat.php">Exports, dispatches, supplying goods abroad: charging VAT</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/HMRC_logo.gif" width="134" height="35" alt="" /> HM Revenue &amp; Customs
+			</a>
+		</p>
+
+		<img src="/_img/px_taxandreporting.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
