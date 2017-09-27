113922cbe9fa6ee9cfe037e503b40bc06ee3494f
Caroline Hagan <info@blueocto.co.uk>
2017-09-27 14:58:32 +0100

    Completed "tax-payroll-and-company-information > vat > how-and-when-to-register-for-vat > when-to-register-for-uk-vat > Calculating your VAT taxable turnover" page.

---
+++ tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/calculating-your-vat-taxable-turnover.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Calculating your VAT taxable turnover | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>When to register for UK VAT </h1>
+		<h2>Calculating your VAT taxable turnover</h2>
+		<p>Your VAT taxable turnover includes the value of any goods or services you supply within the UK, unless they are exempt from VAT. This means you must also include any supplies you make that would be zero-rated for VAT.</p>
+		<p>When calculating your VAT taxable turnover you will of course include your sales, but for VAT purposes, you should also include the value of certain other types of supply, including:</p>
+		<ul>
+			<li>Goods or services that you exchange or barter.</li>
+			<li>Supplies of certain services that you receive from suppliers in other countries that you have to 'reverse charge'. You can find more information about reverse charge in VAT Notice 741. <a href="https://www.gov.uk/government/publications/vat-notice-741-place-of-supply-of-services-before-1-january-2010" target="_blank" rel="noopener noreferrer">Download VAT Notice 741 from the HM Revenue &amp; Customs (HMRC) website (PDF, 558K)</a>.</li>
+			<li>Where you use your own labour to construct certain building or civil engineering works for your own business use with an open market value of &pound;100,000 or more.</li>
+		</ul>
+
+		<p>You must not attempt to avoid registration by artificially separating business activities to reduce your turnover. You can find information about artificially separating business in the page in this guide: <a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/do-not-avoid-registering-for-vat-by-artificially-separating-business-activities.php">Do not avoid registering for VAT by artificially separating business activities</a>.</p>
+		<p><a href="https://www.gov.uk/government/publications/vat-notice-741a-place-of-supply-of-services#P1027_129359" target="_blank" rel="noopener noreferrer">Find out about reverse charge services in section 18 of Notice 741A on the HMRC website</a>&nbsp;</p>
+		<p><a href="/tax-payroll-and-company-information/vat/part-exchanges-returned-goods-deposits-etc-and-vat/part-exchanges-barters-contras-and-vat.php">Part-exchanges, barters, contras and VAT</a></p>
+		<p><a href="https://www.gov.uk/government/publications/vat-notice-708-buildings-and-construction" target="_blank" rel="noopener noreferrer">VAT Notice 708 Buildings and construction on the HMRC website</a></p>
+		
+		<h3>If your VAT taxable turnover is below the threshold</h3>
+		<p>You might be charged a penalty if you register for VAT late, so you should check both of these from time to time:</p>
+		<ul>
+			<li>the current registration threshold - this changes regularly, and is usually announced in the Budget</li>
+			<li>your turnover over the previous 12 months to see if it is above the threshold, or if you can expect it to go above the threshold soon</li>
+		</ul>
+
+		<p>If you took over another VAT registered business within the last 12 months, then when you regularly check your turnover for the previous 12 months against the threshold, you should take into account the turnover of the business before you bought it as well as your own turnover.</p>
+		
+		<h3>If most or all of the goods and services you supply are zero-rated</h3>
+		<p>If most of your supplies are zero-rated, you may be able to apply for exemption for registration.</p>
+		<p>You can apply for an exemption from VAT registration by filling in a VAT registration form and stating why you are applying for an exemption. If some of the supplies you make are VAT taxable you must also show that the VAT you would reclaim on your purchases would normally be more than the VAT you would charge on your sales.</p>
+		<p>If HMRC agrees you can be exempt from VAT registration, you must inform them of any relevant change in circumstances - for example, if you no longer sell mainly zero-rated goods or services.</p>
+		<p>If HMRC doesn't give you an exemption, you will become registered for VAT from the day you should have been registered. You will need to account for VAT from that date.</p>
+		<p><a href="/tax-payroll-and-company-information/vat/vat-rates-thresholds-fuel-scales-and-exchange-rates/rates-of-vat-on-different-goods-and-services.php">Rates of VAT on different goods and services</a>&nbsp;</p>
+		<p><a href="/tax-payroll-and-company-information/vat/reclaiming-vat-on-your-purchases/exempt-and-partially-exempt-businesses-for-vat-purposes.php">Exempt and partially-exempt businesses for VAT purposes</a></p>
+		<p><a href="https://www.gov.uk/government/publications/vat-notice-7001-should-i-be-registered-for-vat#P224_22474" target="_blank" rel="noopener noreferrer">Find out about exemption from VAT registration in VAT Notice 700/1 on the HMRC website</a></p>
+		<p><a href="https://www.gov.uk/government/publications/vat-notice-708-buildings-and-construction#P1997_186024" target="_blank" rel="noopener noreferrer">Find out what construction services are considered self-supply for VAT purposes in VAT Notice 708 on the HMRC website</a></p>
+		<p><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/late-vat-registrations.php">Late VAT registrations</a></p>
+		
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/voluntary-registration.php" title="Voluntary registration">Prev</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/do-not-avoid-registering-for-vat-by-artificially-separating-business-activities.php" title="Do not avoid registering for VAT by artificially separating business activities">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat.php" title="When to register for UK VAT ">Introduction</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/who-can-and-cannot-register-for-vat.php">Who can and cannot register for VAT</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-are-doing-business-in-the-uk-or-intend-to-start.php">You're doing business in the UK, or intend to start</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-supply-goods-to-the-uk-from-abroad-or-intend-to-start.php">You supply goods to the UK from abroad, or intend to start</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-supply-goods-or-services-from-the-uk-to-other-countries.php">You supply goods or services from the UK to other countries</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/voluntary-registration.php">Voluntary registration</a></li>
+			<li>Calculating your VAT taxable turnover</li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/do-not-avoid-registering-for-vat-by-artificially-separating-business-activities.php">Do not avoid registering for VAT by artificially separating business activities</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/vat-registration" target="_blank" rel="noopener noreferrer">Online demonstrator from HMRC</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/how-to-register-for-vat.php">How to register for VAT</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/reclaiming-vat-on-your-purchases/exempt-and-partially-exempt-businesses-for-vat-purposes.php">Exempt and partially-exempt businesses for VAT purposes</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/late-vat-registrations.php">Late VAT registrations</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/HMRC_logo.gif" width="134" height="35" alt="" /> HM Revenue &amp; Customs
+			</a>
+		</p>
+
+		<img src="/_img/px_taxandreporting.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
