a27a7c9bc18ccff49ae5ce2ca71a3180f2b03eef
Caroline Hagan <info@blueocto.co.uk>
2017-09-27 14:51:22 +0100

    Completed "tax-payroll-and-company-information > vat > how-and-when-to-register-for-vat > when-to-register-for-uk-vat > Voluntary registration" page.

---
+++ tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/voluntary-registration.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Voluntary registration | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>When to register for UK VAT </h1>
+		<h2>Voluntary registration</h2>
+		
+		<h3>Benefits of voluntary registration</h3>
+		<p>There are potential cashflow advantages of being able to charge VAT on your sales and claim back VAT on your purchases, which you may benefit from depending on your circumstances. For example:</p>
+		<ul>
+			<li>if you sell zero-rated items and buy standard-rated items you would receive a VAT refund from HM Revenue &amp; Customs (HMRC)</li>
+			<li>if you have not yet sold anything or don't sell anything during a VAT accounting period, you may still be able to claim VAT back on your purchases</li>
+		</ul>
+
+		<p>If you're thinking about registering voluntarily, you might want to check the rules for reclaiming VAT on purchases made before registration, since it is often possible to reclaim some of the VAT you are charged on goods or services that you use to set up your business.</p>
+		<p>You can apply to backdate your voluntary VAT registration by up to four years.</p>
+		<p>You will have to account for VAT on any VAT taxable supplies you have made after your chosen date, and you won't be able to reclaim any VAT on your purchases unless you have the right evidence, and meet the other conditions for reclaiming VAT.</p>
+		<p><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/purchases-made-before-vat-registration-reclaiming-the-vat.php">Purchases made before VAT registration: reclaiming the VAT</a></p>
+		
+		<h3>Responsibilities of voluntary registration</h3>
+		<p>If you decide to voluntarily register for VAT, you have exactly the same responsibilities as someone who must register. You must keep all required VAT records and issue VAT invoices. You also have to complete and submit a VAT Return at regular intervals, along with your payment if one is due.</p>
+		<p><a href="/tax-payroll-and-company-information/vat/vat-returns-and-accounts/accounts-and-records-for-your-vat.php">Accounts and records for your VAT</a></p>
+		
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-supply-goods-or-services-from-the-uk-to-other-countries.php" title="You supply goods or services from the UK to other countries">Prev</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/calculating-your-vat-taxable-turnover.php" title="Calculating your VAT taxable turnover">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat.php" title="When to register for UK VAT ">Introduction</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/who-can-and-cannot-register-for-vat.php">Who can and cannot register for VAT</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-are-doing-business-in-the-uk-or-intend-to-start.php">You're doing business in the UK, or intend to start</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-supply-goods-to-the-uk-from-abroad-or-intend-to-start.php">You supply goods to the UK from abroad, or intend to start</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/you-supply-goods-or-services-from-the-uk-to-other-countries.php">You supply goods or services from the UK to other countries</a></li>
+			<li>Voluntary registration</li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/calculating-your-vat-taxable-turnover.php">Calculating your VAT taxable turnover</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/when-to-register-for-uk-vat/do-not-avoid-registering-for-vat-by-artificially-separating-business-activities.php">Do not avoid registering for VAT by artificially separating business activities</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/vat-registration" target="_blank" rel="noopener noreferrer">Online demonstrator from HMRC</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/vat/vat-returns-and-accounts/accounts-and-records-for-your-vat.php">Accounts and records for your VAT</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/purchases-made-before-vat-registration-reclaiming-the-vat.php">Purchases made before VAT registration: reclaiming the VAT</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/how-to-register-for-vat.php">How to register for VAT</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/HMRC_logo.gif" width="134" height="35" alt="" /> HM Revenue &amp; Customs
+			</a>
+		</p>
+
+		<img src="/_img/px_taxandreporting.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
