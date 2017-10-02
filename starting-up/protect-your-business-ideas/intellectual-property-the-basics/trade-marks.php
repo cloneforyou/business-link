8263671f269f8c4d5ebd557298dfe2794086bc6b
Caroline Hagan <info@blueocto.co.uk>
2017-10-02 21:47:44 +0100

    Completed "starting-up > protect-your-business-ideas > intellectual-property-the-basics > Trade marks" page.

---
+++ starting-up/protect-your-business-ideas/intellectual-property-the-basics/trade-marks.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Trade marks | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Intellectual property: the basics</h1>
+			<h2>Trade marks</h2>
+			<p>Trade marks set apart your products and services as different from other suppliers. They can be signs, logos, words and names, and there are rules on what can and cannot be registered as a trade mark. Trade marks are highly valuable and can be a major influence on your marketing and your recognition in the market place.</p>
+			<p>It's important to protect your own trade marks and also to avoid infringing those of other users. For more information, see our guide on how to <a href="/create-innovate-and-protect/protecting-your-intellectual-property/trade-marks-protecting-your-brand/use-trade-marks-in-your-business.php">use trade marks in your business</a>.</p>
+
+			<ul>
+				<li><a href="detail3b15.html?itemId=1087060628&amp;type=RESOURCES" title="Copyright">Prev</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/designs.php" title="Designs">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/patent-protection-and-your-business/intellectual-property-the-basics.php" title="Intellectual property: the basics">Introduction</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/what-is-intellectual-property.php">What is intellectual property?</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/how-you-can-protect-your-intellectual-property.php">How you can protect your intellectual property</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/patents.php">Patents</a></li>
+				<li><a href="detail3b15.html?itemId=1087060628&amp;type=RESOURCES">Copyright</a></li>
+				<li>Trade marks</li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/designs.php">Designs</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/a-start-up-intellectual-property-checklist.php">A start-up intellectual property checklist</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/organisations/intellectual-property-office" target="_blank" rel="noopener noreferrer">Intellectual property information on the Intellectual Property Office website</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/trade-marks-protecting-your-brand/use-trade-marks-in-your-business.php">Use trade marks in your business</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120729181630/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101996&r.i=1087060130&r.l1=1073858805&r.l2=1073859173&r.l3=1087059738&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Protect your business and premises</a></li>
+			</ul>
+
+			<p>Developed with:<br />
+				<a href="https://www.gov.uk/government/organisations/intellectual-property-office" target="_blank" rel="noopener noreferrer">
+					<img src="/_img/IPO_LOGO.gif" width="150" height="26"  alt="" /> Intellectual Property Office
+				</a>
+			</p>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+			
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
