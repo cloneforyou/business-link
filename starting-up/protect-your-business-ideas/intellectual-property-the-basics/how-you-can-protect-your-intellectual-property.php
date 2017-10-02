29a63d3b74087813193cefb5300a3dc9a00a779f
Caroline Hagan <info@blueocto.co.uk>
2017-10-02 21:40:51 +0100

    Completed "starting-up > protect-your-business-ideas > intellectual-property-the-basics > How you can protect your intellectual property" page.

---
+++ starting-up/protect-your-business-ideas/intellectual-property-the-basics/how-you-can-protect-your-intellectual-property.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>How you can protect your intellectual property | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Intellectual property: the basics</h1>
+			<h2>How you can protect your intellectual property</h2>
+			<p>The way you can protect your intellectual property depends on what exactly it is:</p>
+			<ul>
+				<li><strong>Copyright</strong> is granted to the person or organisation that creates published artistic work, which includes writing, film music and computer software. Unlike most other forms of intellectual property, copyright is granted automatically when the work is first published. For more information, see our guide on <a href="/create-innovate-and-protect/protecting-your-intellectual-property/copyright-design-and-protecting-your-ideas/copyright-for-your-business.php">copyright for your business</a>.</li>
+				<li><strong>Patents</strong> protect inventions, including the features and processes that make things work. For more information, see our guide on how to <a href="/create-innovate-and-protect/protecting-your-intellectual-property/patent-protection-and-your-business/get-patent-protection-for-your-business.php">get patent protection for your business</a>.</li>
+				<li><strong>Trade marks</strong> are symbols that differentiate between goods and services and can be logos or brand names. For more information, see our guide on how to <a href="/create-innovate-and-protect/protecting-your-intellectual-property/trade-marks-protecting-your-brand/use-trade-marks-in-your-business.php">use trade marks in your business</a>.</li>
+				<li><strong>Designs</strong> can be protected by registering them, and through design right. For more information, see our guide on <a href="/create-innovate-and-protect/protecting-your-intellectual-property/copyright-design-and-protecting-your-ideas/design-right-and-registration.php">design right and registration</a>.</li>
+				<li><strong>Digital intellectual property</strong> can be protected in other ways than using the law, such as encryption and using digital signatures. For more information, see our guide to <a href="/create-innovate-and-protect/protecting-your-intellectual-property/find-value-you-didnt-know-existed-in-your-business/digital-intellectual-property-and-your-business.php">digital intellectual property and your business</a>.</li>
+			</ul>
+
+			<h3>Did you know?</h3>
+			<p>There are several Government or affiliated organisations who offer a range of products and services that can help your business with design, innovation, intellectual property, measurement and standards needs.</p>
+			<p><a href="/_files/NI6partnersproductsand_services.pdf" target="_blank" rel="noopener noreferrer">Download a full list of services provided by these organisations for more information (PDF)</a>.</p>
+			<p>You can also use our interactive tool to <a href="/create-innovate-and-protect/protecting-your-intellectual-property/find-value-you-didnt-know-existed-in-your-business/find-out-how-to-protect-your-ideas.php">find our how to protect your ideas</a> if you are unsure what the best solution is for you.</p>
+			
+			<ul>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/what-is-intellectual-property.php" title="What is intellectual property?">Prev</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/patents.php" title="Patents">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/patent-protection-and-your-business/intellectual-property-the-basics.php" title="Intellectual property: the basics">Introduction</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/what-is-intellectual-property.php">What is intellectual property?</a></li>
+				<li>How you can protect your intellectual property</li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/patents.php">Patents</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/copyright.php">Copyright</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/trade-marks.php">Trade marks</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/designs.php">Designs</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/a-start-up-intellectual-property-checklist.php">A start-up intellectual property checklist</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/organisations/intellectual-property-office" target="_blank" rel="noopener noreferrer">Intellectual property information on the Intellectual Property Office website</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/copyright-design-and-protecting-your-ideas/copyright-for-your-business.php">Copyright for your business</a></li>
+				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/patent-protection-and-your-business/get-patent-protection-for-your-business.php">Get patent protection for your business</a></li>
+				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/trade-marks-protecting-your-brand/use-trade-marks-in-your-business.php">Use trade marks in your business</a></li>
+				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/copyright-design-and-protecting-your-ideas/design-right-and-registration.php">Design right and registration</a></li>
+				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/find-value-you-didnt-know-existed-in-your-business/digital-intellectual-property-and-your-business.php">Digital intellectual property and your business</a></li>
+				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/find-value-you-didnt-know-existed-in-your-business/find-out-how-to-protect-your-ideas.php">Find out how to protect your ideas</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120729181630/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101996&r.i=1087060130&r.l1=1073858805&r.l2=1073859173&r.l3=1087059738&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Protect your business and premises</a></li>
+			</ul>
+
+			<p>Developed with:<br />
+				<a href="https://www.gov.uk/government/organisations/intellectual-property-office" target="_blank" rel="noopener noreferrer">
+					<img src="/_img/IPO_LOGO.gif" width="150" height="26"  alt="" /> Intellectual Property Office
+				</a>
+			</p>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
