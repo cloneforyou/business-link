ba442d69f7c7283fdf006d4fd82a493082fd56d7
Caroline Hagan <info@blueocto.co.uk>
2017-10-02 21:44:28 +0100

    Completed "starting-up > protect-your-business-ideas > intellectual-property-the-basics > Copyright" page.

---
+++ starting-up/protect-your-business-ideas/intellectual-property-the-basics/copyright.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Copyright | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Intellectual property: the basics</h1>
+			<h2>Copyright</h2>
+			<p>When you create a literary, dramatic, musical or artistic piece of work, you will be granted copyright automatically. If someone you employ creates this for you during their normal course of employment, then you will own the copyright unless an agreement has been signed stipulating otherwise.</p>
+			<p>Copyright covers works such as company documentation, software, songs, original paintings, drawings or photographs. Copyright also protects sounds recordings and films.</p>
+			<p>If you own a copyright, you can decide whether to:</p>
+			<ul>
+				<li>allow other businesses or people to use the copyrighted work</li>
+				<li>allow work to be copied, adapted, published, performed or broadcast</li>
+				<li>allow other businesses to use work for a royalty or licence fee</li>
+				<li>sell the copyright</li>
+			</ul>
+
+			<p>For information on copyright protection and what to do if you think there has been a copyright infringement, see our guide on <a href="/create-innovate-and-protect/protecting-your-intellectual-property/copyright-design-and-protecting-your-ideas/copyright-for-your-business.php">copyright for your business</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/patents.php" title="Patents">Prev</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/trade-marks.php" title="Trade marks">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/patent-protection-and-your-business/intellectual-property-the-basics.php" title="Intellectual property: the basics">Introduction</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/what-is-intellectual-property.php">What is intellectual property?</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/how-you-can-protect-your-intellectual-property.php">How you can protect your intellectual property</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/patents.php">Patents</a></li>
+				<li>Copyright</li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/trade-marks.php">Trade marks</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/designs.php">Designs</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/a-start-up-intellectual-property-checklist.php">A start-up intellectual property checklist</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/organisations/intellectual-property-office" target="_blank" rel="noopener noreferrer">Intellectual property information on the Intellectual Property Office website</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/copyright-design-and-protecting-your-ideas/copyright-for-your-business.php">Copyright for your business</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120729181630/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101996&r.i=1087060130&r.l1=1073858805&r.l2=1073859173&r.l3=1087059738&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Protect your business and premises</a></li>
+			</ul>
+
+			<p>Developed with:<br />
+				<a href="https://www.gov.uk/government/organisations/intellectual-property-office" target="_blank" rel="noopener noreferrer">
+					<img src="/_img/IPO_LOGO.gif" width="150" height="26"  alt="" /> Intellectual Property Office
+				</a>
+			</p>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
