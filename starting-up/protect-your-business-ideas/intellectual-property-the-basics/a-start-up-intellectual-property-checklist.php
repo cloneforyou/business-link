0fd270ccdf22d6fa8c09524dbdff9510715b1a14
Caroline Hagan <info@blueocto.co.uk>
2017-10-02 21:57:21 +0100

    Completed "starting-up > protect-your-business-ideas > intellectual-property-the-basics > A start-up intellectual property checklist" page.

---
+++ starting-up/protect-your-business-ideas/intellectual-property-the-basics/a-start-up-intellectual-property-checklist.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>A start-up intellectual property checklist | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Intellectual property: the basics</h1>
+			<h2>A start-up intellectual property checklist</h2>
+			<p>Even if your business is a start-up, you can have intellectual property, and it is important to make sure it is protected. Issues to consider are:</p>
+			<ul>
+				<li>Have you registered your business, either with the tax authorities or with Companies House? The name of your business is an important asset and it is important to make sure it is not already in use by another business. Read our guide on <a href="/starting-up/business-structures/legal-structures-the-basics.php">legal structures: the basics</a>.</li>
+				<li>Do you have a logo which you can register as a trade mark? See the page in this guide on <a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/trade-marks.php">trade marks</a>.</li>
+				<li>Do you need to register a patent? See the page in this guide on <a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/patents.php">patents</a>.</li>
+				<li>Do you need to register a design? See the page in this guide on <a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/designs.php">designs</a>.</li>
+				<li>Do you need international protection for your intellectual property? See our guide on <a href="/sales-and-marketing/selling-overseas.php" title="How to protect your ideas and innovative inventions overseas">intellectual property protection overseas</a>.</li>
+				<li>Have you done an intellectual property audit? See our guide on <a href="/create-innovate-and-protect/protecting-your-intellectual-property/find-value-you-didnt-know-existed-in-your-business/protecting-intellectual-property.php">protecting intellectual property</a>.</li>
+			</ul>
+			
+			<ul>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/designs.php" title="Designs">Prev</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/patent-protection-and-your-business/intellectual-property-the-basics.php" title="Intellectual property: the basics">Introduction</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/what-is-intellectual-property.php">What is intellectual property?</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/how-you-can-protect-your-intellectual-property.php">How you can protect your intellectual property</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/patents.php">Patents</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/copyright.php">Copyright</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/trade-marks.php">Trade marks</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/designs.php">Designs</a></li>
+				<li>A start-up intellectual property checklist</li>
+			</ul>
+
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/organisations/intellectual-property-office" target="_blank" rel="noopener noreferrer">Intellectual property information on the Intellectual Property Office website</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/legal-structures-the-basics.php" title="Company, partnership or sole trader - find the right structure for your new business">Legal structures: the basics</a></li>
+				<li><a href="/sales-and-marketing/selling-overseas.php" title="How to protect your ideas and innovative inventions overseas">Intellectual property protection overseas</a></li>
+				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/find-value-you-didnt-know-existed-in-your-business/protecting-intellectual-property.php" title="How to conduct an audit of your business' intellectual property rights and take steps to protect and secure IP">Protecting intellectual property</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120729181630/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101996&r.i=1087060130&r.l1=1073858805&r.l2=1073859173&r.l3=1087059738&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Protect your business and premises</a></li>
+			</ul>
+
+			<p>Developed with:<br />
+				<a href="https://www.gov.uk/government/organisations/intellectual-property-office" target="_blank" rel="noopener noreferrer">
+					<img src="/_img/IPO_LOGO.gif" width="150" height="26"  alt="" /> Intellectual Property Office
+				</a>
+			</p>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
