1dba368529838e3132390b49168466a8a7e5f727
Caroline Hagan <info@blueocto.co.uk>
2017-10-02 20:48:30 +0100

    Completed "tax-payroll-and-company-information > vat > changing-or-cancelling-your-vat-registration > closing-or-selling-your-business-and-VAT-registration > Selling your business" page.

---
+++ tax-payroll-and-company-information/vat/changing-or-cancelling-your-vat-registration/closing-or-selling-your-business-and-VAT-registration/selling-your-business.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Selling your business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+
+			<h1>Closing or selling your business and VAT registration</h1>
+			<h2>Selling your business</h2>
+			<p>For VAT purposes, HM Revenue &amp; Customs (HMRC) considers that you have transferred your business as a going concern if:</p>
+			<ul>
+				<li>you retire and another person takes over the assets of your business</li>
+				<li>you sell your business and its assets to someone else</li>
+				<li>you sell part of your business that is capable of being operated separately to somebody else</li>
+			</ul>
+
+			<p>When you transfer your business as a going concern, you normally have to cancel your VAT registration.</p>
+			<p>However in some situations, the new owner of the business can apply to keep your VAT registration number. If you agree and allow the new owner to keep the VAT registration number, then you don't need to cancel your registration. Both you and the new owner of the business should complete form VAT 68. You need to do this within 30 calendar days of selling your business to avoid becoming liable for a financial penalty.</p>
+			<p>If you decide to allow the transfer of your VAT registration number to the new owner of the business you should cancel any Direct Debit that you have set up to pay your online VAT Returns. The new owner of the business can set up a new Direct Debit through the VAT online service.</p>
+			<p>Send your completed VAT 68 to:</p>
+			<p>
+				HM Revenue &amp; Customs<br />
+				VAT Registration Service<br />
+				Imperial House<br />
+				77 Victoria Street<br />
+				Grimsby<br />
+				DN31 1DB
+			</p>
+			<p><a href="https://www.gov.uk/government/publications/vat-request-for-transfer-of-a-registration-number-vat68" target="_blank" rel="noopener noreferrer">Find form VAT 68 to request the transfer of a registration number on the HMRC website</a></p>
+			
+			<h3>Treatment of the sale for VAT purposes</h3>
+			<p>Normally when you sell something, the sale is treated as a supply for VAT purposes. However when you sell your business, in certain circumstances special rules apply and the sale will not be treated as a supply, for VAT purposes, so no VAT should be charged. In other circumstances, you'll have to charge VAT in the normal way.</p>
+			<p>These special rules help businesses by improving their cashflow. They also avoid the need to separately value assets that may be liable at different rates or are exempt but have been sold as a whole package.</p>
+			<p><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/taking-over-a-business-and-vat-registration.php">Taking over a business and VAT registration</a></p>
+			<p><a href="https://www.gov.uk/government/publications/vat-notice-7009-transfer-of-business-as-a-going-concern" target="_blank" rel="noopener noreferrer">Read about the special rules when selling a business in VAT Notice 700/9 on the HMRC website</a></p>
+			<p><a href="https://www.gov.uk/government/publications/vat-notice-742-land-and-property" target="_blank" rel="noopener noreferrer">Read about how land and property are treated in VAT Notice 742 on the HMRC website</a></p>
+			<p><a href="https://www.gov.uk/government/publications/vat-notice-742a-opting-to-tax-land-and-buildings" target="_blank" rel="noopener noreferrer">Read about opting to tax land and buildings in VAT Notice 742A on the HMRC website</a></p>
+
+			<h3>Keeping records</h3>
+			<p>After you sell your business, you must retain your VAT records.</p>
+			<p>If the buyer keeps your VAT number, you must pass your records to them. Permission can be sought from HMRC for you to keep your records, but you will still have to make the information available to the buyer so that they can comply with their own VAT responsibilities.</p>
+			
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/vat/changing-or-cancelling-your-vat-registration/closing-or-selling-your-business-and-VAT-registration/closing-your-business.php" title="Closing your business">Prev</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/vat/changing-or-cancelling-your-vat-registration/closing-or-selling-your-business-and-VAT-registration.php" title="Closing or selling your business and VAT registration">Introduction</a></li>
+				<li><a href="/tax-payroll-and-company-information/vat/changing-or-cancelling-your-vat-registration/closing-or-selling-your-business-and-VAT-registration/closing-your-business.php">Closing your business</a></li>
+				<li>Selling your business</li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<dl>
+				<dt>Developed with:</dt>
+				<dd><a href="https://www.gov.uk/government/organisations/hm-revenue-customs" target="_blank" rel="noopener noreferrer"><img src="/_img/HMRC_logo.gif" width="134" height="35"  alt="" /> HM Revenue &amp; Customs</a> </dd>
+			</dl>
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/publications/vat-request-for-transfer-of-a-registration-number-vat68" target="_blank" rel="noopener noreferrer">Form VAT 68 from HMRC</a></li>
+				<li><a href="https://www.gov.uk/government/publications/vat-notice-7009-transfer-of-business-as-a-going-concern" target="_blank" rel="noopener noreferrer">The special rules when selling a business - VAT Notice 700/9 from HMRC</a></li>
+				<li><a href="https://www.gov.uk/government/publications/vat-notice-742-land-and-property" target="_blank" rel="noopener noreferrer">How land and property are treated - VAT Notice 742 from HMRC</a></li>
+				<li><a href="https://www.gov.uk/government/publications/vat-notice-742a-opting-to-tax-land-and-buildings" target="_blank" rel="noopener noreferrer">Opting to tax land and buildings - VAT Notice 742A from HMRC</a></li>
+				<li><a href="https://www.gov.uk/government/collections/vat-forms" target="_blank" rel="noopener noreferrer">VAT forms - from HMRC</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/tax-payroll-and-company-information/vat/how-and-when-to-register-for-vat/taking-over-a-business-and-vat-registration.php">Taking over a business and VAT registration</a></li>
+			</ul>
+
+			<img src="/_img/px_taxandreporting.gif" alt="" width="181" height="5" />
+
+
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
