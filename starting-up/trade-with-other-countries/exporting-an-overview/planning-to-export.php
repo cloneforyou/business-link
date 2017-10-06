ff725b9cc2b29c3b8c06dfb6109650c5a4cdd3c9
Caroline Hagan <info@blueocto.co.uk>
2017-10-06 22:01:56 +0100

    Completed "starting-up > trade-with-other-countries > exporting-an-overview > Planning to export?" page.

---
+++ starting-up/trade-with-other-countries/exporting-an-overview/planning-to-export.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Planning to export? | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Exporting - an overview</h1>
+		<h2>Planning to export?</h2>
+		<p>Before you commit to exporting you need to honestly assess your <b>export potential</b> - both in terms of the readiness of your business and of your product or service.</p>
+		<p>It's essential to carry out detailed <b>market research</b> to identify and evaluate the target market. Examine:</p>
+		<ul>
+			<li>the industry structure</li>
+			<li>the predicted demand for your product or service</li>
+			<li>competition and how you plan to fit into that marketplace</li>
+			<li>any modifications required to make your product or service saleable</li>
+		</ul>
+
+		<p>The UK Trade &amp; Investment Accessing International Markets programme can provide support and help in planning your entry into new overseas markets, including market research. <a href="http://webarchive.nationalarchives.gov.uk/20120418184810/http://www.ukti.gov.uk/export/howwehelp.html" target="_blank" rel="noopener noreferrer">Find out about growing your business internationally on the UK Trade &amp; investment website</a>.</p>
+		<p>After completing your market research, you can draw up an <b>export plan</b> defining how you will enter the new market.</p>
+		<p>Consider whether you have:</p>
+		<ul>
+			<li>a marketing strategy that incorporates international trade development</li>
+			<li>the necessary financial resources</li>
+			<li>the right people to develop the new export markets</li>
+			<li>adequate knowledge of the requirements of your chosen market - eg modifying packaging to meet local regulations and standards</li>
+			<li>an understanding of export payment mechanisms and export finance</li>
+		</ul>
+
+		<p>Then assess whether your product is <b>suitable</b> for export. Consider:</p>
+		<ul>
+			<li>product standards and regulations in the overseas market - the British Standards Institution offers help for exporters</li>
+			<li>the costs of adapting your product or service</li>
+		</ul>
+
+		<p>UK Trade &amp; Investment offers practical help with your ability to export. See the page in this guide on <a href="/starting-up/trade-with-other-countries/exporting-an-overview/helping-your-business-grow-internationally.php">Developing Your International Trade Potential</a>.</p>
+		<p>You can also:</p>
+		<ul>
+			<li><a href="#">read about the advice and support available to businesses based in Wales on the International Business Wales website</a></li>
+			<li><a href="https://www.sdi.co.uk/export-from-scotland" target="_blank" rel="noopener noreferrer">get information and advice on exporting for businesses based in Scotland on the Scottish Development International website</a></li>
+			<li><a href="https://www.investni.com/support-for-business/develop-an-export-strategy.html" target="_blank" rel="noopener noreferrer">read about the help available to businesses based in Northern Ireland on the Invest Northern Ireland website</a></li>
+		</ul>
+
+		<p><a href="/international-trade/doing-business-in-the-eu/getting-ready-to-export-to-individual-countries.php">Use our interactive tool to identify what you need to know to export to your target country</a>.</p>
+		<p><a href="#">Watch a video on exporting goods or services for the first time on the Growth and Improvement Service</a>.</p>
+		<!-- http://www.improve.businesslink.gov.uk/content/exporting-%E2%80%93-are-you-ready | https://web.archive.org/web/20120522054448/http://www.improve.businesslink.gov.uk:80/content/exporting-%E2%80%93-are-you-ready -->
+		
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/starting-out-in-international-trade/exporting-an-overview.php" title="Exporting - an overview">Prev</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/selling-and-distribution-in-overseas-markets.php" title="Selling and distribution in overseas markets">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/starting-out-in-international-trade/exporting-an-overview.php" title="Exporting - an overview">Introduction</a></li>
+			<li>Planning to export?</li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/selling-and-distribution-in-overseas-markets.php">Selling and distribution in overseas markets</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/marketing-your-product-or-service-overseas.php">Marketing your product or service overseas</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/your-legal-responsibilities-as-an-exporter.php">Your legal responsibilities as an exporter</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/transport-considerations.php">Transport considerations</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/financial-considerations.php">Financial considerations</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/working-effectively-with-different-cultures.php">Working effectively with different cultures</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/helping-your-business-grow-internationally.php">Helping your business grow internationally</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/ten-key-steps-to-successful-exporting.php">Ten key steps to successful exporting</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/here-is-how-I-developed-a-strong-export-market.php">Here's how I developed a strong export market</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/here-is-how-I-kept-my-business-profitable-by-exporting-my-products-abroad_flash-video.php">Here's how I kept my business profitable by exporting my products abroad (Flash video)</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>UK Trade &amp; Investment Enquiry Line</h3>
+		<p>020 7215 8000</p>
+
+		<h3>BSI Helpline</h3>
+		<p>020 8996 9001</p>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120418184810/http://www.ukti.gov.uk/export/howwehelp.html" target="_blank" rel="noopener noreferrer">Guidance on growing your business internationally on the UK Trade &amp; Investment website</a></li>
+			<li><a href="http://www.designcouncil.org.uk/news-opinion/export-week-how-design-can-open-new-markets-uk-businesses" target="_blank" rel="noopener noreferrer">Design advice for exports on the Design Council website</a></li>
+			<li><a href="/starting-up/business-start-up-organiser.php">Manage your personal list of starting-up tasks with our Business start-up organiser </a></li>
+			<li><a href="/international-trade/doing-business-in-the-eu/getting-ready-to-export-to-individual-countries.php">Use our interactive tool to identify what you need to know to export to your target country</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120418175729/http://www.improve.businesslink.gov.uk/resources/tools/exporting-growth" target="_blank" rel="noopener noreferrer">Use the tool Exporting for growth on the Growth and Improvement Service</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/starting-out-in-international-trade/preparing-to-export.php">Preparing to export</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/overseas-supplier-relationships/entering-overseas-markets.php">Entering overseas markets</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/managing-risks-of-importing-and-exporting/manage-the-risks-of-exporting.php">Manage the risks of exporting</a></li>
+			<li><a href="/international-trade/do-you-need-an-export-or-import-licence.php" title="Import and export licensing for controlled goods">Do you need an export or import licence?</a></li>
+			<li><a href="/international-trade/doing-business-in-the-eu/getting-ready-to-export-to-individual-countries.php">Getting ready to export to individual countries</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/department-for-international-trade" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/UKTI.gif" width="104" height="32" alt="" /> Department for International Trade
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
