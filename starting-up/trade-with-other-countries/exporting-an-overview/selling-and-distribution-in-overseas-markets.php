6a4b89c473199c9af024068cf2e19e462b1e7ed6
Caroline Hagan <info@blueocto.co.uk>
2017-10-06 22:08:47 +0100

    Completed "starting-up > trade-with-other-countries > exporting-an-overview > Selling and distribution in overseas markets" page.

---
+++ starting-up/trade-with-other-countries/exporting-an-overview/selling-and-distribution-in-overseas-markets.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Selling and distribution in overseas markets | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Exporting - an overview</h1>
+		<h2>Selling and distribution in overseas markets</h2>
+		<p>There are a number of elements you need to consider to sell successfully overseas. How you organise your <b>sales presence</b> in export markets is one of the key decisions.</p>
+		<p>Depending on your product, you may be able to sell directly. For example, you might be able to sell over the internet or by exhibiting at local trade shows.</p>
+		<p>Many businesses look for a <b>partner</b> who already understands the local market. For example:</p>
+		<ul>
+			<li>You can sell to a <b>distributor</b> who then sells your products locally.</li>
+			<li>You can use a <b>sales agent</b> who sells products on your behalf, or puts you into contact with potential customers on a commission basis.</li>
+			<li>You can enter into a <b>joint venture</b> with a local business. This gives you a share of the management and profits of the joint venture, but is a more complicated and expensive option.</li>
+			<li>If you want complete control over sales, you can set up your own <b>local office</b>. This is the most expensive option.</li>
+		</ul>
+
+		<p>The choice you make can have important financial and legal consequences. See our guide on selling and promotion overseas.</p>
+		<p>You can <a href="https://www.contactus.trade.gov.uk/office-finder/" target="_blank" rel="noopener noreferrer">find your local International Trade Team on the UK Trade &amp; Investment website</a>.</p>
+		<p>The UK Trade &amp; Investment Accessing International Markets programme can provide support and help in planning your entry into new overseas markets. <a href="http://webarchive.nationalarchives.gov.uk/20120418184810/http://www.ukti.gov.uk/export/howwehelp.html" target="_blank" rel="noopener noreferrer">Find out about growing your business internationally on the UK Trade &amp; investment website</a>.</p>
+		<p>When arranging a sales contract with an agent or distributor, you need to ensure that responsibility for delivery and payment is clearly defined.</p>
+		<p>It's also important to remember that intellectual property (IP) protection becomes more complicated if you sell goods overseas. Patents and trade marks are only recognised and protected in the country of origin, so you will need to secure IP protection in each country you intend to sell into. For more information, read our guide to <a href="/sales-and-marketing/selling-overseas.php">intellectual property protection overseas</a>.</p>
+		<p><a href="/international-trade/doing-business-in-the-eu/getting-ready-to-export-to-individual-countries.php">Use our interactive tool to identify what you need to know to export to your target country</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/planning-to-export.php" title="Planning to export?">Prev</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/marketing-your-product-or-service-overseas.php" title="Marketing your product or service overseas">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/starting-out-in-international-trade/exporting-an-overview.php" title="Exporting - an overview">Introduction</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/planning-to-export.php">Planning to export?</a></li>
+			<li>Selling and distribution in overseas markets</li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/marketing-your-product-or-service-overseas.php">Marketing your product or service overseas</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/your-legal-responsibilities-as-an-exporter.php">Your legal responsibilities as an exporter</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/transport-considerations.php">Transport considerations</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/financial-considerations.php">Financial considerations</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/working-effectively-with-different-cultures.php">Working effectively with different cultures</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/helping-your-business-grow-internationally.php">Helping your business grow internationally</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/ten-key-steps-to-successful-exporting.php">Ten key steps to successful exporting</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/here-is-how-I-developed-a-strong-export-market.php">Here's how I developed a strong export market</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/here-is-how-I-kept-my-business-profitable-by-exporting-my-products-abroad_flash-video.php">Here's how I kept my business profitable by exporting my products abroad (Flash video)</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>UK Trade &amp; Investment Enquiry Line</h3>
+		<p>020 7215 8000</p>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.contactus.trade.gov.uk/office-finder/" target="_blank" rel="noopener noreferrer">International trade team search on the UK Trade &amp; Investment website</a></li>
+			<li><a href="https://www.gov.uk/government/organisations/intellectual-property-office" target="_blank" rel="noopener noreferrer">Intellectual property protection guidance on the Intellectual Property Office website</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120418175729/http://www.improve.businesslink.gov.uk/resources/tools/exporting-growth" target="_blank" rel="noopener noreferrer">Use the tool Exporting for growth on the Growth and Improvement Service</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/overseas-supplier-relationships/entering-overseas-markets.php">Entering overseas markets</a></li>
+			<li><a href="/sales-and-marketing/selling-overseas.php">Intellectual property protection overseas</a></li>
+			<li><a href="/international-trade/do-you-need-an-export-or-import-licence.php">Do you need an export or import licence?</a></li>
+			<li><a href="/environment-and-efficiency/your-supply-chain/use-transit-and-collation-packaging-effectively.php">Use transit and collation packaging effectively</a></li>
+			<li><a href="/environment-and-efficiency/transport-and-fuel/reducing-your-vehicle-emissions.php">Reducing your vehicle emissions</a></li>
+			<li><a href="/environment-and-efficiency/your-supply-chain/reduce-the-environmental-impact-of-your-supply-chain.php">Reduce the environmental impact of your supply chain</a></li>
+			<li><a href="/international-trade/doing-business-in-the-eu/getting-ready-to-export-to-individual-countries.php">Getting ready to export to individual countries</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/department-for-international-trade" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/UKTI.gif" width="104" height="32" alt="" /> Department for International Trade
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
