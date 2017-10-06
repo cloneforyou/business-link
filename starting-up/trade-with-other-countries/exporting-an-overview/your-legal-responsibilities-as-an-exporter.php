dfb4be1b9f06daed6b3155979e950875f0f135eb
Caroline Hagan <info@blueocto.co.uk>
2017-10-06 22:30:46 +0100

    Completed "starting-up > trade-with-other-countries > exporting-an-overview > Your legal responsibilities as an exporter" page.

---
+++ starting-up/trade-with-other-countries/exporting-an-overview/your-legal-responsibilities-as-an-exporter.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Your legal responsibilities as an exporter | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Exporting - an overview</h1>
+		<h2>Your legal responsibilities as an exporter</h2>
+		<p>You'll need to familiarise yourself with the VAT rules administered by HM Revenue &amp; Customs (HMRC).</p>
+		<p>In most cases, exports will be zero-rated for VAT although there are exceptions. Check with the HMRC VAT Helpline on Tel 0845 010 9000 to find out what applies to you.</p>
+		<p>Details of any exports you make must be entered on your VAT return. If you have a high level of exports to European Union (EU) member states, you may have to submit more detailed declarations in an Intrastat return. Intrastat thresholds are reviewed annually. The 2011 thresholds are &pound;600,000 for Arrivals and &pound;250,000 for Dispatches.</p>
+		<p>Learn more about the Intrastat return in our guide on an <a href="/international-trade/import-and-export-procedures/customs-declarations/intrastat-returns/introduction-to-intrastat.php">introduction to Intrastat</a>.</p>
+		<p>For exports outside the EU you must report sales to HMRC electronically using the National Export System (NES) or using the single administrative document (SAD).</p>
+		<p>For information on VAT rules, see our section on <a href="/tax-payroll-and-company-information/vat/international-trade-international-visits-and-vat.php">international trade, international visits and VAT</a>.</p>
+		<p>HMRC has introduced a new status for businesses - Authorised Economic Operator. While the scheme is not compulsory, companies that meet the requirements can take advantage of simplified customs procedures. See our guide on <a href="/international-trade/authorised-economic-operators-aeos/authorised-economic-operators.php">Authorised Economic Operators</a>.</p>
+		<p>New security laws mean that you must declare your goods leaving the EU. If you supply normal export declarations such as the SAD, you will be covered, but if you're not declaring them in the usual way, you must complete an Exit Summary Declaration.</p>
+		<p><a href="/_files/eu_guidelines-on-export-and-exit_jan-2011.pdf" target="_blank" rel="noopener noreferrer">Download guidelines on security declarations on export and exit provisions from the Europa website (PDF, 272K)</a>.</p>
+		
+		<h3>Export controls</h3>
+		<p>Certain goods may only be exported following the issue of an export licence. Examples of goods subject to licensing control include fine art, firearms and chemicals. See our <a href="/international-trade/export-control-organisation/beginners-guide-to-export-controls.php">beginners' guide to export controls</a>.</p>
+		<p><a href="http://webarchive.nationalarchives.gov.uk/20120729133119/http://webarchive.nationalarchives.gov.uk/20120729133119/http://www.bis.gov.uk/policies/export-control-organisation/eco-training-skills-academy/eco-films" target="_blank" rel="noopener noreferrer">Watch a video about your export control responsibilities and how you can ensure you have the correct licences on the Department for Business, Innovation &amp; Skills (BIS) website</a>.</p>
+		
+		<h3>Understanding the law</h3>
+		<p>As soon as your goods enter another country they become subject to that country's laws.</p>
+		<p>You can <a href="http://webarchive.nationalarchives.gov.uk/20120729124841/http://www.ukti.gov.uk/es_es/export/exporting.html" target="_blank" rel="noopener noreferrer">get reports and data on specific countries from the UK Trade &amp; Investment website</a>.</p>
+		<p>You can also <a href="https://www.contactus.trade.gov.uk/office-finder/" target="_blank" rel="noopener noreferrer">find your local international trade team on the UK Trade &amp; Investment website</a>.</p>
+		<p><a href="/international-trade/doing-business-in-the-eu/getting-ready-to-export-to-individual-countries.php">Use our interactive tool to identify what you need to know to export to your target country</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/marketing-your-product-or-service-overseas.php" title="Marketing your product or service overseas">Prev</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/transport-considerations.php" title="Transport considerations">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/starting-out-in-international-trade/exporting-an-overview.php" title="Exporting - an overview">Introduction</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/planning-to-export.php">Planning to export?</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/selling-and-distribution-in-overseas-markets.php">Selling and distribution in overseas markets</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/marketing-your-product-or-service-overseas.php">Marketing your product or service overseas</a></li>
+			<li>Your legal responsibilities as an exporter</li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/transport-considerations.php">Transport considerations</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/financial-considerations.php">Financial considerations</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/working-effectively-with-different-cultures.php">Working effectively with different cultures</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/helping-your-business-grow-internationally.php">Helping your business grow internationally</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/ten-key-steps-to-successful-exporting.php">Ten key steps to successful exporting</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/here-is-how-I-developed-a-strong-export-market.php">Here's how I developed a strong export market</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/here-is-how-I-kept-my-business-profitable-by-exporting-my-products-abroad_flash-video.php">Here's how I kept my business profitable by exporting my products abroad (Flash video)</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>HMRC Tariff Classification Service Enquiry Line</h3>
+		<p>01702 366 077</p>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/international-trade/tax-obligations/understand-your-tax-obligations-when-importing-or-exporting.php">Use our interactive tool to understand your tax obligations when importing and exporting</a></li>
+			<li><a href="https://www.contactus.trade.gov.uk/office-finder/" target="_blank" rel="noopener noreferrer">International trade team search on the UK Trade &amp; Investment website</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120418175729/http://www.improve.businesslink.gov.uk/resources/tools/exporting-growth" target="_blank" rel="noopener noreferrer">Use the tool Exporting for growth on the Growth and Improvement Service</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/international-trade/export-control-organisation/beginners-guide-to-export-controls.php">Beginners' guide to export controls</a></li>
+			<li><a href="/international-trade/do-you-need-an-export-or-import-licence.php">Do you need an export or import licence?</a></li>
+			<li><a href="/international-trade/import-and-export-procedures/customs-declarations/intrastat-returns/introduction-to-intrastat.php">Introduction to Intrastat</a></li>
+			<li><a href="/international-trade/export-control-organisation/types-of-export-licence/licences-export-trade-control-and-transhipment.php">Licences - Export, Trade Control and Transhipment</a></li>
+			<li><a href="/international-trade/export-control-organisation/current-export-restrictions-by-country/current-arms-embargoes-and-other-restrictions/export-licensing-application-statistics.php">Export licensing application statistics</a></li>
+			<li><a href="/tax-payroll-and-company-information/vat/international-trade-international-visits-and-vat.php">International trade, international visits and VAT</a></li>
+			<li><a href="/international-trade/doing-business-in-the-eu/getting-ready-to-export-to-individual-countries.php">Getting ready to export to individual countries</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/department-for-international-trade" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/UKTI.gif" width="104" height="32" alt="" /> Department for International Trade
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+	
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
