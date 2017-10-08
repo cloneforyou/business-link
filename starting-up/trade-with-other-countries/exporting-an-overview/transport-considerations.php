f20c120153730ec8d7994ab3835349c0cb93d9f2
Caroline Hagan <info@blueocto.co.uk>
2017-10-08 20:23:06 +0100

    Completed "starting-up > trade-with-other-countries > exporting-an-overview > Transport considerations" page.

---
+++ starting-up/trade-with-other-countries/exporting-an-overview/transport-considerations.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Transport considerations | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Exporting - an overview</h1>
+		<h2>Transport considerations</h2>
+		<p>Your <b>responsibility</b> for transport depends on your agreement with your customer or supplier. For example, you might be responsible for delivering the goods to a warehouse in the customer's country. Your obligations should be clearly set out in a written contract using Incoterms. Incoterms are standard trade terms that set out who is responsible for transporting goods, insuring the goods during transportation, paying duties and customs clearance. Read our guide on <a href="/international-trade/importing-and-exporting-basics/getting-paid-when-trading-internationally/international-commercial-contracts-incoterms.php">International Commercial Contracts - Incoterms</a>.</p>
+		<p>The best <b>mode of transport</b> for your goods will depend on the type of goods and how quickly they need to be delivered. You may need more than one mode, for example, sending goods by lorry to a port in the UK and then by ship overseas. The goods will need suitable packaging and labelling for transportation. It may be possible for your goods to be sent via post. See our guides on <a href="/international-trade/preparing-your-goods-for-transport/international-transport-and-distribution.php">international transport and distribution</a> and <a href="/international-trade/importing-and-exporting-basics/starting-out-in-international-trade/importing-and-exporting-by-post.php">importing and exporting by post</a>.</p>
+		<p>Depending on the contract, you may need to arrange insurance. Marine insurance can cover transport by air, road or rail as well as by sea. <a href="https://www.biba.org.uk/find-insurance/" target="_blank" rel="noopener noreferrer">Find a member who can help you arrange insurance on the British Insurance Brokers Association website</a>.</p>
+		<p>Normally, you are responsible for UK customs procedures and your customer looks after customs in their country. In any case, you must ensure that you have the right paperwork.</p>
+		<p>Most companies use a specialist <b>freight forwarder</b> to handle transport. Confirm exactly what they will do and whether they can handle all documentation and other procedures. See our guide on <a href="/international-trade/freight-forwarding/using-brokers-and-forwarders.php">Using brokers and forwarders</a>.</p>
+		<p>Look for a forwarder who exports regularly to that destination. They can 'consolidate' your goods with other consignments in a single container to reduce costs. Reputable freight forwarders are usually members of the British International Freight Association (BIFA). You can <a href="http://www.bifa.org/home" target="_blank" rel="noopener noreferrer">find a forwarder on the BIFA website</a>.</p>
+		<p>Getting international transport right can be complicated. You can get advice from a member of UK Trade &amp; Investment international trade team. <a href="https://www.contactus.trade.gov.uk/office-finder/" target="_blank" rel="noopener noreferrer">Find your local international trade team on the UK Trade &amp; Investment website</a>.</p>
+		<p><a href="/international-trade/doing-business-in-the-eu/getting-ready-to-export-to-individual-countries.php">Use our interactive tool to identify what you need to know to export to your target country</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/your-legal-responsibilities-as-an-exporter.php" title="Your legal responsibilities as an exporter">Prev</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/financial-considerations.php" title="Financial considerations">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/starting-out-in-international-trade/exporting-an-overview.php" title="Exporting - an overview">Introduction</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/planning-to-export.php">Planning to export?</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/selling-and-distribution-in-overseas-markets.php">Selling and distribution in overseas markets</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/marketing-your-product-or-service-overseas.php">Marketing your product or service overseas</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/your-legal-responsibilities-as-an-exporter.php">Your legal responsibilities as an exporter</a></li>
+			<li>Transport considerations</li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/financial-considerations.php">Financial considerations</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/working-effectively-with-different-cultures.php">Working effectively with different cultures</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/helping-your-business-grow-internationally.php">Helping your business grow internationally</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/ten-key-steps-to-successful-exporting.php">Ten key steps to successful exporting</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/here-is-how-I-developed-a-strong-export-market.php">Here's how I developed a strong export market</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/here-is-how-I-kept-my-business-profitable-by-exporting-my-products-abroad_flash-video.php">Here's how I kept my business profitable by exporting my products abroad (Flash video)</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>BIFA Enquiry Line</h3>
+		<p>020 8844 2266</p>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://iccwbo.org/resources-for-business/incoterms-rules/" target="_blank" rel="noopener noreferrer">Incoterms introduction on the International Chamber of Commerce website</a></li>
+			<li><a href="https://www.biba.org.uk/find-insurance/" target="_blank" rel="noopener noreferrer">Member directory on the British Insurance Brokers' Association website</a></li>
+			<li><a href="https://www.contactus.trade.gov.uk/office-finder/" target="_blank" rel="noopener noreferrer">International trade team search on the UK Trade &amp; Investment website</a></li>
+			<li><a href="http://www.bifa.org/home" target="_blank" rel="noopener noreferrer">Member directory on the BIFA website</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120418175729/http://www.improve.businesslink.gov.uk/resources/tools/exporting-growth" target="_blank" rel="noopener noreferrer">Use the tool Exporting for growth on the Growth and Improvement Service</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/getting-paid-when-trading-internationally/international-commercial-contracts-incoterms.php">International Commercial Contracts - Incoterms</a></li>
+			<li><a href="/international-trade/preparing-your-goods-for-transport/international-transport-and-distribution.php">International transport and distribution</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/licensing-and-paperwork/international-trade-paperwork-the-basics.php">International trade paperwork: the basics</a></li>
+			<li><a href="/sales-and-marketing/selling-and-the-law/product-labelling-and-packaging.php">Product labelling and packaging</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/managing-risks-of-importing-and-exporting/insurance-for-international-trade.php" title="Potential risks of trading abroad, and how to insure against them">Insurance for international trade</a></li>
+			<li><a href="/environment-and-efficiency/your-supply-chain/use-transit-and-collation-packaging-effectively.php">Use transit and collation packaging effectively</a></li>
+			<li><a href="/environment-and-efficiency/transport-and-fuel/reducing-your-vehicle-emissions.php">Reducing your vehicle emissions</a></li>
+			<li><a href="/environment-and-efficiency/your-supply-chain/reduce-the-environmental-impact-of-your-supply-chain.php">Reduce the environmental impact of your supply chain</a></li>
+			<li><a href="/international-trade/doing-business-in-the-eu/getting-ready-to-export-to-individual-countries.php">Getting ready to export to individual countries</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/department-for-international-trade" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/UKTI.gif" width="104" height="32" alt="" /> Department for International Trade
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
