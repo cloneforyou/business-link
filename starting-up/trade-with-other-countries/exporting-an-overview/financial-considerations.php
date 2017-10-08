7040fea4722906c0a44a5724e3dc4fc111e61f11
Caroline Hagan <info@blueocto.co.uk>
2017-10-08 20:44:39 +0100

    Completed "starting-up > trade-with-other-countries > exporting-an-overview > Financial considerations" page.

---
+++ starting-up/trade-with-other-countries/exporting-an-overview/financial-considerations.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Financial considerations | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Exporting - an overview</h1>
+		<h2>Financial considerations</h2>
+		<p>The delay between the shipping of goods and your receiving payment for them will affect your cashflow.</p>
+		<p>It's worth discussing your cash position with your accountant and bank manager before committing to exporting.</p>
+		<p>It's also important to insure your business against not being paid in case one of your overseas customers goes out of business. UK Export Finance, run by the government, may be able to offer insurance and advice, depending on the type of export. <a href="https://www.gov.uk/government/organisations/uk-export-finance" target="_blank" rel="noopener noreferrer">Find out about export credits on the UK Export Finance website</a>.</p>
+		<p>Other commercial providers can also give information on credit insurance and offer cover. You can <a href="https://www.biba.org.uk/find-insurance/" target="_blank" rel="noopener noreferrer">find a listing through the British Insurance Brokers Association (BIBA) website</a>.</p>
+		<p>There are also currency issues you need to consider. Some of your customers could face problems obtaining foreign currency to pay for your exports. In this case, it's worth insisting on a (confirmed) irrevocable letter of credit that secures payments according to the terms of the credit and often at an agreed rate. See our guide on <a href="/international-trade/importing-and-exporting-basics/getting-paid-when-trading-internationally/letters-of-credit.php">letters of credit</a>.</p>
+		<p>Businesses that sell on credit to foreign customers can use factoring or invoice discounting to free up cashflow. Export factors specialise in the collection of money from overseas. The factoring company pays you a percentage of the invoice value up-front and the balance (less their percentage) once they have collected payment.</p>
+		<p>See our guide on <a href="/international-trade/importing-and-exporting-basics/getting-paid-when-trading-internationally/getting-paid-when-selling-overseas.php">getting paid when selling overseas</a>.</p>
+		
+		<h3>Trading abroad</h3>
+		<p>When arranging your sales contract you should ensure that delivery responsibilities are clearly defined using the Incoterms 2000 terminology and contracts. Read our guide on <a href="/international-trade/importing-and-exporting-basics/getting-paid-when-trading-internationally/international-commercial-contracts-incoterms.php">International Commercial Contracts - Incoterms</a>.</p>
+		<p>UK Trade &amp; Investment offers several services to new and experienced exporters which are part of the government's <a href="/solutions-for-business.php">Solutions for Business portfolio</a>.</p>
+		<p>
+			<p>You can get advice from a member of UK Trade &amp; Investment international trade team. <a href="https://www.contactus.trade.gov.uk/office-finder/" target="_blank" rel="noopener noreferrer">Find your local international trade team on the UK Trade &amp; Investment website</a>.</p>
+		</p>
+		<p><a href="/international-trade/doing-business-in-the-eu/getting-ready-to-export-to-individual-countries.php">Use our interactive tool to identify what you need to know to export to your target country</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/transport-considerations.php" title="Transport considerations">Prev</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/working-effectively-with-different-cultures.php" title="Working effectively with different cultures">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/starting-out-in-international-trade/exporting-an-overview.php" title="Exporting - an overview">Introduction</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/planning-to-export.php">Planning to export?</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/selling-and-distribution-in-overseas-markets.php">Selling and distribution in overseas markets</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/marketing-your-product-or-service-overseas.php">Marketing your product or service overseas</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/your-legal-responsibilities-as-an-exporter.php">Your legal responsibilities as an exporter</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/transport-considerations.php">Transport considerations</a></li>
+			<li>Financial considerations</li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/working-effectively-with-different-cultures.php">Working effectively with different cultures</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/helping-your-business-grow-internationally.php">Helping your business grow internationally</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/ten-key-steps-to-successful-exporting.php">Ten key steps to successful exporting</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/here-is-how-I-developed-a-strong-export-market.php">Here's how I developed a strong export market</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/here-is-how-I-kept-my-business-profitable-by-exporting-my-products-abroad_flash-video.php">Here's how I kept my business profitable by exporting my products abroad (Flash video)</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.contactus.trade.gov.uk/office-finder/" target="_blank" rel="noopener noreferrer">International trade team search on the UK Trade &amp; Investment website</a></li>
+			<li><a href="https://www.gov.uk/government/organisations/uk-export-finance" target="_blank" rel="noopener noreferrer">Export credits information on the UK Export Finance website</a></li>
+			<li><a href="https://www.biba.org.uk/find-insurance/" target="_blank" rel="noopener noreferrer">Member directory on the BIBA website</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120418175729/http://www.improve.businesslink.gov.uk/resources/tools/exporting-growth" target="_blank" rel="noopener noreferrer">Use the tool Exporting for growth on the Growth and Improvement Service</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/getting-paid-when-trading-internationally/getting-paid-when-selling-overseas.php">Getting paid when selling overseas</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/getting-paid-when-trading-internationally/letters-of-credit.php">Letters of credit</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">Cashflow management: the basics</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/managing-risks-of-importing-and-exporting/insurance-for-international-trade.php">Insurance for international trade</a></li>
+			<li><a href="/finance-and-grants/business-banking/foreign-currency-and-exchange-risks.php">Foreign currency and exchange risks</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/factoring-and-invoice-discounting-the-basics.php">Factoring and invoice discounting: the basics</a></li>
+			<li><a href="/international-trade/doing-business-in-the-eu/getting-ready-to-export-to-individual-countries.php">Getting ready to export to individual countries</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/department-for-international-trade" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/UKTI.gif" width="104" height="32" alt="" /> Department for International Trade
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
