b521480935d308c8ee4a78bd782073d47d716c99
Caroline Hagan <info@blueocto.co.uk>
2017-10-08 21:09:03 +0100

    Completed "starting-up > trade-with-other-countries > exporting-an-overview > Helping your business grow internationally" page.

---
+++ starting-up/trade-with-other-countries/exporting-an-overview/helping-your-business-grow-internationally.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Helping your business grow internationally | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Exporting - an overview</h1>
+		<h2>Helping your business grow internationally</h2>
+		<p>UK Trade &amp; Investment staff are experts in helping your business grow internationally, providing expert trade advice and practical support to UK-based companies wishing to grow their business overseas. With 2,400 staff and a presence in 96 countries, UK Trade &amp; Investment can assist on every step of the exporting journey, giving you the support that you need to expand and prosper.</p>
+		<p>Through a range of unique services, including participation at selected trade fairs, outward missions and providing bespoke market intelligence, UK Trade &amp; Investment can help you crack foreign markets and quickly get to grips with overseas regulations and business practice.</p>
+		
+		<h3>Speak to your local expert</h3>
+		<p>UK Trade &amp; Investment's International Trade Teams are located in local offices all over the country. Every UK region has dedicated sector specialists, helping new and established exporters every day. UK Trade &amp; Investment's local teams can provide support tailored to your company's particular needs and growth stage. You can <a href="https://www.contactus.trade.gov.uk/enquiry/topic" target="_blank" rel="noopener noreferrer">contact UK Trade &amp; Investment's regional and international teams on the UK Trade &amp; Investment's website</a>.</p>
+		
+		<h3>Find out about Passport to Export</h3>
+		<p>UK Trade &amp; Investment's Passport to Export programme offers new and inexperienced exporters free capability assessments, support in visiting markets, mentoring from a local professional, action plans, customised and subsidised training, and ongoing support once you've started to export. <a href="https://www.gov.uk/government/organisations/uk-trade-investment/about-our-services" target="_blank" rel="noopener noreferrer">Find out about the Passport to Export programme on the UK Trade &amp; Investment website</a>.</p>
+		
+		<h3>Access UK Trade &amp; Investment's market expertise</h3>
+		<p>Each business and each market is unique. UK Trade &amp; Investment's aim is to help you through research and advice, to make those initial approaches into new markets. It offers two key research and contact services, the Overseas Market Introduction Service (OMIS) and the Export Markets Research Scheme (EMRS). You can <a href="https://www.gov.uk/overseas-customers-export-opportunities" target="_blank" rel="noopener noreferrer">read information on OMIS on the UK Trade &amp; Investment website</a>. You can also <a href="https://www.gov.uk/government/organisations/uk-trade-investment/about-our-services" target="_blank" rel="noopener noreferrer">find out about EMRS on the UK Trade &amp; Investment website</a>.</p>
+		<p>Additionally, the UK Trade &amp; Investment website offers access to two market intelligence services for exporters. FCO country updates are assessments of business -relevant political and economic issues in key markets, and Overseas Security Information for Business (OSIB) provides declassified strategic information on business security related issues. You can <a href="https://www.gov.uk/government/collections/overseas-business-risk" target="_blank" rel="noopener noreferrer">access FCO country updates on the UK Trade &amp; Investment website</a>. You can also <a href="https://www.gov.uk/government/collections/overseas-business-risk" target="_blank" rel="noopener noreferrer">find out about OSIB on the UK Trade &amp; Investment website</a>.</p>
+		
+		<h3>Break down barriers</h3>
+		<p>If you need help with the linguistic and cultural aspects of doing business overseas, the Export Communications Review (ECR), jointly run by UK Trade &amp; Investment and the British Chambers of Commerce, offers companies a variety of options and advice, including cultural awareness reviews and communications planning. You can <a href="https://www.gov.uk/government/collections/overseas-business-risk" target="_blank" rel="noopener noreferrer">access the ECR on the UK Trade &amp; Investment website</a>.</p>
+		
+		<h3>Visit a market</h3>
+		<p>Taking part in overseas events, trade fairs or missions is an effective way to test markets, attract customers, appoint agents or distributors and make sales. UK Trade &amp; Investment helps arrange groups of UK companies to attend tradeshows and missions worldwide. <a href="https://www.gov.uk/guidance/tradeshow-access-programme" target="_blank" rel="noopener noreferrer">Read about the Tradeshow Access Programme on the UK Trade &amp; Investment website</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/working-effectively-with-different-cultures.php" title="Working effectively with different cultures">Prev</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/ten-key-steps-to-successful-exporting.php" title="Ten key steps to successful exporting">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/starting-out-in-international-trade/exporting-an-overview.php" title="Exporting - an overview">Introduction</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/planning-to-export.php">Planning to export?</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/selling-and-distribution-in-overseas-markets.php">Selling and distribution in overseas markets</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/marketing-your-product-or-service-overseas.php">Marketing your product or service overseas</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/your-legal-responsibilities-as-an-exporter.php">Your legal responsibilities as an exporter</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/transport-considerations.php">Transport considerations</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/financial-considerations.php">Financial considerations</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/working-effectively-with-different-cultures.php">Working effectively with different cultures</a></li>
+			<li>Helping your business grow internationally</li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/ten-key-steps-to-successful-exporting.php">Ten key steps to successful exporting</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/here-is-how-I-developed-a-strong-export-market.php">Here's how I developed a strong export market</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/here-is-how-I-kept-my-business-profitable-by-exporting-my-products-abroad_flash-video.php">Here's how I kept my business profitable by exporting my products abroad (Flash video)</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>UK Trade &amp; Investment Enquiry Line</h3>
+		<p>020 7215 8000</p>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120418184810/http://www.ukti.gov.uk/export/howwehelp.html" target="_blank" rel="noopener noreferrer">Guidance on growing your business internationally on the UK Trade &amp; Investment website</a></li>
+			<li><a href="https://www.contactus.trade.gov.uk/office-finder/" target="_blank" rel="noopener noreferrer">International trade team search on the UK Trade &amp; Investment website</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120418175729/http://www.improve.businesslink.gov.uk/resources/tools/exporting-growth" target="_blank" rel="noopener noreferrer">Use the tool Exporting for growth on the Growth and Improvement Service</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/overseas-supplier-relationships/entering-overseas-markets.php">Entering overseas markets</a></li>
+			<li><a href="/international-trade/doing-business-in-the-eu/getting-ready-to-export-to-individual-countries.php">Getting ready to export to individual countries</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/department-for-international-trade" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/UKTI.gif" width="104" height="32" alt="" /> Department for International Trade
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
