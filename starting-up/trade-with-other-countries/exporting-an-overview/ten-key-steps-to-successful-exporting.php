f241966c55759c4c3ea79edc5e1862a7bbbf6fd6
Caroline Hagan <info@blueocto.co.uk>
2017-10-08 21:13:40 +0100

    Completed "starting-up > trade-with-other-countries > exporting-an-overview > Ten key steps to successful exporting" page.

---
+++ starting-up/trade-with-other-countries/exporting-an-overview/ten-key-steps-to-successful-exporting.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Ten key steps to successful exporting | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Exporting - an overview</h1>
+		<h2>Ten key steps to successful exporting</h2>
+		<ol>
+			<li>
+				<strong>Research your market</strong> - does your prospective foreign customer need what you are selling at the price that will yield you a profit? What is the competition and how will they react?
+			</li>
+			<li>
+				<strong>Implement an export strategy</strong> and review your capabilities - ask yourself: what would my business gain from exporting?
+			</li>
+			<li>
+				<strong>Construct an export plan</strong> - define how you will enter the foreign market. Finalise human resources and marketing strategy and allocate an adequate budget to cover export start-up costs.
+			</li>
+			<li>
+				<strong>Choose your sales presence</strong> - establish whether you need a direct sales operation. Or is an agent or distributor more effective? How will you manage your overseas sales presence?
+			</li>
+			<li>
+				<strong>Promote your product</strong> - how are you going to market and sell your product? Customise marketing to the target country.
+			</li>
+			<li>
+				<strong>Get the Customs side right</strong> - contact HM Revenue &amp; Customs and the UK embassy of your destination country to clarify requirements. Make sure your reporting practices are watertight.
+			</li>
+			<li>
+				<strong>Get paid on time</strong> - ensure your cashflow will remain at a safe level. Guarantee sufficient credit for your future sales. Take out insurance cover if necessary.
+			</li>
+			<li>
+				<strong>Choose your distribution methods</strong> - consider the implications of selling over long distances and across national frontiers.
+			</li>
+			<li>
+				<strong>Transport goods effectively</strong> - assess and choose the most effective transport method and make sure the goods are insured by you or the importer.
+			</li>
+			<li>
+				<strong>After-sales policy</strong> - regularly liaise with customers, export agents and banks. Monitor political unrest or other adverse conditions in the country of destination. Manage regular servicing and warranty claims.
+			</li>
+		</ol>
+		<p><a href="/international-trade/doing-business-in-the-eu/getting-ready-to-export-to-individual-countries.php">Use our interactive tool to identify what you need to know to export to your target country</a>.</p>
+
+		<ul>
+			<li><a href="detail091f.html?itemId=1075078368&amp;type=RESOURCES" title="Helping your business grow internationally">Prev</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/here-is-how-I-developed-a-strong-export-market.php" title="Here's how I developed a strong export market">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/starting-out-in-international-trade/exporting-an-overview.php" title="Exporting - an overview">Introduction</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/planning-to-export.php">Planning to export?</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/selling-and-distribution-in-overseas-markets.php">Selling and distribution in overseas markets</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/marketing-your-product-or-service-overseas.php">Marketing your product or service overseas</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/your-legal-responsibilities-as-an-exporter.php">Your legal responsibilities as an exporter</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/transport-considerations.php">Transport considerations</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/financial-considerations.php">Financial considerations</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/working-effectively-with-different-cultures.php">Working effectively with different cultures</a></li>
+			<li><a href="detail091f.html?itemId=1075078368&amp;type=RESOURCES">Helping your business grow internationally</a></li>
+			<li>Ten key steps to successful exporting</li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/here-is-how-I-developed-a-strong-export-market.php">Here's how I developed a strong export market</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/exporting-an-overview/here-is-how-I-kept-my-business-profitable-by-exporting-my-products-abroad_flash-video.php">Here's how I kept my business profitable by exporting my products abroad (Flash video)</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>UK Trade &amp; Investment Enquiry Line</h3>
+		<p>020 7215 8000</p>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/international-trade/doing-business-in-the-eu/getting-ready-to-export-to-individual-countries.php">Use our interactive tool to identify what you need to know to export to your target country</a></li>
+			<li><a href="https://www.gov.uk/world" target="_blank" rel="noopener noreferrer">British embassy listings on the Foreign &amp; Commonwealth Office website</a></li>
+			<li><a href="/environment-and-efficiency/your-supply-chain/use-transit-and-collation-packaging-effectively.php">Use transit and collation packaging effectively</a></li>
+			<li><a href="/environment-and-efficiency/transport-and-fuel/reducing-your-vehicle-emissions.php">Reducing your vehicle emissions</a></li>
+			<li><a href="/environment-and-efficiency/your-supply-chain/reduce-the-environmental-impact-of-your-supply-chain.php">Reduce the environmental impact of your supply chain</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120418175729/http://www.improve.businesslink.gov.uk/resources/tools/exporting-growth" target="_blank" rel="noopener noreferrer">Use the tool Exporting for growth on the Growth and Improvement Service</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/overseas-supplier-relationships/entering-overseas-markets.php">Entering overseas markets</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/managing-risks-of-importing-and-exporting/manage-the-risks-of-exporting.php">Manage the risks of exporting</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/starting-out-in-international-trade/preparing-to-export.php">Preparing to export</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/getting-paid-when-trading-internationally/getting-paid-when-selling-overseas.php">Getting paid when selling overseas</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/managing-risks-of-importing-and-exporting/insurance-for-international-trade.php">Insurance for international trade</a></li>
+			<li><a href="/international-trade/doing-business-in-the-eu/getting-ready-to-export-to-individual-countries.php">Getting ready to export to individual countries</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/department-for-international-trade" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/UKTI.gif" width="104" height="32" alt="" /> Department for International Trade
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
