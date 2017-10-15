1fecdfdbcc2bdea12be03e02d4bb8cd5d583224a
Caroline Hagan <info@blueocto.co.uk>
2017-10-15 13:22:55 +0100

    Completed "starting-up > trade-with-other-countries > are-you-ready-to-import > Assess your skills and resources for importing" page.

---
+++ starting-up/trade-with-other-countries/are-you-ready-to-import/assess-your-skills-and-resources-for-importing.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Assess your skills and resources for importing | Business Link</title>
+
+	<!-- ref: xxx || /detail?itemId=1077722868&type=RESOURCES -->
+
+	<meta lang="en" content="Finances, personnel and other resources for importing" />
+	<meta lang="en" content="import readiness assessment;trade;trading;import;importing;paperwork;documentation;overseas;abroad;foreign;market research;international;strategy;plan;planning;supplier contract terms;supply chain management;logistics;customs;freight;shipping;agents;licenses;credit;incoterms;payment terms" />
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Are you ready to import?</h1>
+		<h2>Assess your skills and resources for importing</h2>
+		<p>Importing requires special skills and extra resources. Before you start importing, you should assess whether you have the right skills and resources and decide how to cope with any shortcomings.</p>
+		<p>You need to understand UK customs procedures and ensure you have the right paperwork when deliveries arrive in the UK. International trade often uses payment methods, such as letters of credit, that require an understanding of payment documentation. If you agree to pay in a foreign currency, you also need to be able to handle foreign exchange. You will probably need new <strong>administrative systems</strong> to track deliveries and payments and manage the paperwork. </p>
+		<p>If you do not have the skills in house, you may want to invest in <strong>training</strong>. Or you could use someone else - such as an import agent - to handle specialist areas. You may also need extra personnel to handle the additional workload.</p>
+		<p>Alternatively, you might decide to only deal with suppliers who handle most of the procedures. Many first time or small scale importers prefer to keep things simple. See the page in this guide on how to <a href="/starting-up/trade-with-other-countries/are-you-ready-to-import/decide-your-approach-to-importing.php">decide your approach to importing</a>.</p>
+		
+		<h3>Financial resources</h3>
+		<p>As well as skills, you need to think about what resources you can devote to importing. Researching overseas markets and managing overseas suppliers can be time-consuming and expensive, particularly if you need to visit them. See our guide on how to <a href="/international-trade/importing-and-exporting-basics/overseas-supplier-relationships/manage-overseas-suppliers.php">manage overseas suppliers</a>.</p>
+		<p>If your finances are limited, you may prefer to deal with suppliers who offer credit. On the other hand, agreeing to pay promptly could help you negotiate a competitive price.</p>
+		<p>Make sure you have planned how you are going to use the imports, and have the skills and resources you need in place. For example, you might need production capacity for processing imported components, or a marketing plan and sales resources for selling imports on to your customers.</p>
+		
+		<ul>
+			<li><a href="/starting-up/trade-with-other-countries/are-you-ready-to-import/understand-what-importing-involves.php" title="Understand what importing involves">Prev</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/are-you-ready-to-import/decide-your-approach-to-importing.php" title="Decide your approach to importing">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/starting-out-in-international-trade/are-you-ready-to-import.php" title="Are you ready to import?">Introduction</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/are-you-ready-to-import/plan-your-import-objectives.php">Plan your import objectives</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/are-you-ready-to-import/understand-what-importing-involves.php">Understand what importing involves</a></li>
+			<li>Assess your skills and resources for importing</li>
+			<li><a href="/starting-up/trade-with-other-countries/are-you-ready-to-import/decide-your-approach-to-importing.php">Decide your approach to importing</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/are-you-ready-to-import/negotiate-the-right-import-deal.php">Negotiate the right import deal</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/are-you-ready-to-import/review-your-import-readiness.php">Review your import readiness</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://web.archive.org/web/20120401235244/https://web.archive.org/web/20120401235244/http://www.knowledge.hsbc.co.uk/international+business/plan+your+imports/develop+an+import+strategy" target="_blank" rel="noopener noreferrer">Import strategy advice on the HSBC KnowledgeCentre website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/licensing-and-paperwork/international-trade-paperwork-the-basics.php">International trade paperwork: the basics</a></li>
+			<li><a href="/finance-and-grants/business-banking/foreign-currency-and-exchange-risks.php">Foreign currency and exchange risks</a></li>
+			<li><a href="/international-trade/preparing-your-goods-for-transport/international-transport-and-distribution.php">International transport and distribution</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/overseas-supplier-relationships/manage-overseas-suppliers.php">Manage overseas suppliers</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/managing-risks-of-importing-and-exporting/manage-the-risks-of-importing.php">Manage the risks of importing</a></li>
+		</ul>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
