4be8d2fa225babff5f21f82e92b9cc9689d1935a
Caroline Hagan <info@blueocto.co.uk>
2017-10-15 12:56:33 +0100

    Completed "starting-up > trade-with-other-countries > are-you-ready-to-import > Plan your import objectives" page.

---
+++ starting-up/trade-with-other-countries/are-you-ready-to-import/plan-your-import-objectives.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Plan your import objectives | Business Link </title>
+
+	<!-- ref: xxx || /detail?itemId=1077722868&type=RESOURCES -->
+
+	<meta lang="en" content="Setting goals and deciding how involved in importing you want to be" />
+	<meta lang="en" content="import readiness assessment;trade;trading;import;importing;paperwork;documentation;overseas;abroad;foreign;market research;international;strategy;plan;planning;supplier contract terms;supply chain management;logistics;customs;freight;shipping;agents;licenses;credit;incoterms;payment terms" />
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Are you ready to import?</h1>
+		<h2>Plan your import objectives</h2>
+		<p>Before you start importing, it's a good idea to be clear about what you are trying to achieve. You might want to find a cheaper source of supplies overseas, or to import products that aren't yet available in the UK to sell to your customers.</p>
+		<p>At the same time, importing should fit in with your overall <strong>business strategy</strong>. For example, importing low cost goods might be part of a cost-control strategy, but you want to be sure that this does not compromise your reputation for quality.</p>
+		<p>It's worth thinking about how much time, money and effort you want to devote to setting up and managing your importing activities. You are unlikely to want to invest heavily for a small, one-off import. But if importing is going to be an important part of your business, you might be prepared to spend more on finding the best suppliers, building relationships and so on. For more information, see the page in this guide on how to <a href="/starting-up/trade-with-other-countries/are-you-ready-to-import/understand-what-importing-involves.php">understand what importing involves</a>.</p>
+		<p>You also need to decide how much <strong>involvement</strong> you want to have in the import process. Importing can be fairly straightforward if you only deal with experienced suppliers who handle most of the paperwork and logistics, and get help from an import agent. On the other hand, dealing with less experienced suppliers and handling more of the process yourself could be more time consuming but also more profitable. See the page in this guide on how to <a href="/starting-up/trade-with-other-countries/are-you-ready-to-import/decide-your-approach-to-importing.php">decide your approach to importing</a>.</p>
+		<p>Whichever approach you choose, you should ensure that all the key people in your business agree and are committed to it.</p>
+
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/starting-out-in-international-trade/are-you-ready-to-import.php" title="Are you ready to import?">Prev</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/are-you-ready-to-import/understand-what-importing-involves.php" title="Understand what importing involves">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/starting-out-in-international-trade/are-you-ready-to-import.php" title="Are you ready to import?">Introduction</a></li>
+			<li>Plan your import objectives</li>
+			<li><a href="/starting-up/trade-with-other-countries/are-you-ready-to-import/understand-what-importing-involves.php">Understand what importing involves</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/are-you-ready-to-import/assess-your-skills-and-resources-for-importing.php">Assess your skills and resources for importing</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/are-you-ready-to-import/decide-your-approach-to-importing.php">Decide your approach to importing</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/are-you-ready-to-import/negotiate-the-right-import-deal.php">Negotiate the right import deal</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/are-you-ready-to-import/review-your-import-readiness.php">Review your import readiness</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://web.archive.org/web/20120401235244/https://web.archive.org/web/20120401235244/http://www.knowledge.hsbc.co.uk/international+business/plan+your+imports/develop+an+import+strategy" target="_blank" rel="noopener noreferrer">Import strategy advice on the HSBC KnowledgeCentre website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/overseas-supplier-relationships/manage-overseas-suppliers.php">Manage overseas suppliers</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/managing-risks-of-importing-and-exporting/manage-the-risks-of-importing.php">Manage the risks of importing</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/overseas-supplier-relationships/entering-overseas-markets.php">Entering overseas markets</a></li>
+		</ul>
+
+		<img src="/_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
