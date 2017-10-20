61933fb9e98cc52372653dc1757881abdea0c623
Caroline Hagan <info@blueocto.co.uk>
2017-10-20 14:21:46 +0100

    Completed "starting-up > protect-your-business-ideas > non-disclosure-agreements > Types of non-disclosure agreement" page.

--- starting-up/protect-your-business-ideas/non-disclosure-agreements/types-of-non-disclosure-agreement.php
+++ starting-up/protect-your-business-ideas/non-disclosure-agreements/types-of-non-disclosure-agreement.php
@@ -2,11 +2,11 @@
 <html class="no-js">
 
 <head>
-	<title>What is a non-disclosure agreement? | Business Link</title>
+	<title>Types of non-disclosure agreement | Business Link</title>
 	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
 
-	<!-- ref: detail32d3.html || xx -->
-	<meta name="description" content="Confidentiality agreements, trade secrets and information disclosure" />
+	<!-- ref: x_x.html || xx -->
+	<meta name="description" content="Use non-disclosure agreements to share intellectual property, commercial data or to formalise a relationship" />
 	<meta name="keywords" content="trade secrets; non-disclosure agreements;confidentiality agreements;non disclosure agreement;template;sample;NDA;secure information;confidential information;intellectual property;confidentiality clause;public domain;commercial relationship;contract;trade secrets;protecting;protection;Data security;intellectual property protection;patents;ideas;protecting ideas;collaboration;sharing ideas;secrets;information security;information breaches;security breaches;Data security;Freedom of Information Act; Data security" />
 </head>
 
@@ -17,34 +17,43 @@
 		<div class="small-12 large-8 large-push-4 columns">
 
 			<h1>Non-disclosure agreements</h1>
-			<h2>What is a non-disclosure agreement?</h2>
-			<p>A non-disclosure agreement (NDA), also known as a <strong>confidentiality</strong> <strong>agreement</strong>, is a low-cost way to protect your business' ideas.</p>
-			<p>An NDA is a legal contract between you and another party. Typically, you agree to disclose information to them for a specific purpose, while they agree not to disclose that information to anyone else. This allows you to share your trade secrets with business partners while preventing them from passing this information on.</p>
-			<p>For example, you may have come up with a product design and need to get an estimate from a manufacturer of how much it will cost to make. An NDA can ensure that your partner does not pass details of your idea to one of your competitors.</p>
-			<p>As well as using NDAs with your clients, suppliers, resellers and advisers, you can also use these agreements with your employees and with journalists to whom you want to give a 'sneak preview' of financial, technical or new product information.</p>
-			<p>NDAs can cover:</p>
+			<h2>Types of non-disclosure agreement</h2>
+			<p>A non-disclosure agreement (NDA) is a <strong>legal contract</strong> between you and another party not to disclose information you have shared for a specific purpose.</p>
+			<p>This allows businesses to approach potential partners, suppliers, consultants and customers with their idea, knowing that the other party is legally forbidden to pass on that information. These agreements can be <strong>tailored</strong> to your requirements.</p>
+			<p>They can be used to:</p>
 			<ul>
-				<li>trade secrets - eg a formula, programme or process</li>
-				<li>technical drawings and designs</li>
-				<li>mathematical and chemical formulae</li>
-				<li>business plans</li>
-				<li>customer and prospect lists</li>
+				<li>share intellectual property</li>
+				<li>share commercial or trading information</li>
+				<li>formalise a relationship, eg between an employer and employee</li>
 			</ul>
 
-			<p>Information qualifies as a <strong>trade secret</strong> or confidential when it is not known in the public domain and is valuable only as long as it remains secret or confidential.</p>
-			<p>NDAs between you and public bodies need to be carefully considered because any information that you might provide may be disclosed under the terms of the Freedom of Information Act 2000. This gives anyone the right of access to information held by public authorities. <a href="http://webarchive.nationalarchives.gov.uk/20150603171202/https://www.justice.gov.uk/information-access-rights/foi-guidance-for-practitioners" target="_blank" rel="noopener noreferrer">Read about the Freedom of Information Act on the Ministry of Justice (MoJ) website</a>.</p>
-			<p>If you need to approach potential business partners, have a clear idea beforehand of what you want to share with them - and stick to it. This minimises the risk of breaches of confidentiality.</p>
+			<h3>NDAs for employees</h3>
+			<p>Current and former employees are responsible for most breaches of confidentiality. Employees are under an <strong>implied duty</strong> not to use trade secrets in a manner that will harm your business, but it is best to get this in writing and to specify to the employee exactly what is confidential. Equally, you should ensure that the NDA makes provision for when the employee leaves your business.</p>
 			
+			<h3>Other types of NDAs</h3>
 			<ul>
-				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/patent-protection-and-your-business/non-disclosure-agreements.php" title="Non-disclosure agreements">Prev</a></li>
-				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/types-of-non-disclosure-agreement.php" title="Types of non-disclosure agreement">Next</a></li>
+				<li>
+					A <strong>precedent confidentiality agreement</strong> is used for mergers and acquisitions. If a company wants to buy your business, it will want to look at your books and assets. An NDA can provide an element of protection for your confidential information and trade secrets if the prospective buyer pulls out of the deal.
+				</li>
+				<li>If you don't know exactly what information you will need to disclose during a <strong>commercial relationship</strong>, you can still use an NDA. It is a good idea to classify as confidential any information that will be disclosed later, so that the NDA still applies.</li>
+			</ul>
+
+			<h3>One-way and two-way NDAs</h3>
+			<ul>
+				<li>A <strong>one-way</strong> NDA is used when only one business is sharing information and the other agrees to keep it confidential.</li>
+				<li>A <strong>two-way</strong> NDA is used when both businesses are sharing confidential information with each other and want to be sure that neither will disclose their trade secrets.</li>
 			</ul>
 			
+			<ul>
+				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/what-is-a-non-disclosure-agreement.php" title="What is a non-disclosure agreement?">Prev</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/ways-to-prepare-a-non-disclosure-agreement.php" title="Ways to prepare a non-disclosure agreement">Next</a></li>
+			</ul>
+
 			<h3>Subjects covered in this guide</h3>
 			<ul>
 				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/patent-protection-and-your-business/non-disclosure-agreements.php" title="Non-disclosure agreements">Introduction</a></li>
-				<li>What is a non-disclosure agreement?</li>
-				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/types-of-non-disclosure-agreement.php">Types of non-disclosure agreement</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/what-is-a-non-disclosure-agreement.php">What is a non-disclosure agreement?</a></li>
+				<li>Types of non-disclosure agreement</li>
 				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/ways-to-prepare-a-non-disclosure-agreement.php">Ways to prepare a non-disclosure agreement</a></li>
 				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/key-non-disclosure-agreement-terms.php">Key non-disclosure agreement terms</a></li>
 				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/creating-a-non-disclosure-agreement.php">Creating a non-disclosure agreement</a></li>
@@ -54,22 +63,21 @@
 			
 			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
 
-			<h3>Acas Helpline</h3>
-			<p>08457 47 47 47</p>
-
 			<h3>Lawyers for your Business</h3>
 			<p>020 7405 9075</p>
-			
+
 			<h3>Actions</h3>
 			<ul>
-				<li><a href="/_files/ipo_non-disclosure-agreements.pdf" target="_blank" rel="noopener noreferrer">Download confidential disclosure agreements information from the Intellectual Property Office website (PDF, 148K) </a></li>
-				<li><a href="http://webarchive.nationalarchives.gov.uk/20150603171202/https://www.justice.gov.uk/information-access-rights/foi-guidance-for-practitioners" target="_blank" rel="noopener noreferrer">Freedom of Information Act advice on the MoJ website</a></li>
+				<li><a href="/_files/ipo_non-disclosure-agreements.pdf" target="_blank" rel="noopener noreferrer">Download confidential disclosure agreements information from the Intellectual Property Office website (PDF, 148K)</a></li>
 				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/find-value-you-didnt-know-existed-in-your-business/find-out-how-to-protect-your-ideas.php">Use our interactive tool to find out the best ways to protect your intellectual property</a></li>
 			</ul>
 			
 			<h3>Also on this site</h3>
 			<ul>
+				<li><a href="/employment-and-skills/staff-documents-and-employment-policies/the-employment-contract.php">The employment contract</a></li>
 				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/find-value-you-didnt-know-existed-in-your-business/protecting-intellectual-property.php">Protecting intellectual property</a></li>
+				<li><a href="/buy-or-sell-a-business/considering-selling-a-business/preparing-to-sell-your-business.php">Preparing to sell your business</a></li>
+				<li><a href="/buy-or-sell-a-business/selling/complete-the-sale-of-your-business.php">Complete the sale of your business</a></li>
 				<li><a href="http://webarchive.nationalarchives.gov.uk/20120729181630/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101996&r.i=1087060130&r.l1=1073858805&r.l2=1073859173&r.l3=1087059738&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Protect your business and premises</a></li>
 			</ul>
 
@@ -80,7 +88,7 @@
 			</p>
 
 			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
-			
+
 
 		</div>
 		<!-- // .small-12 large-8 large-push-4 columns -->
