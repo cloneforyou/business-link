a98e86893ac32a8c7cfae6d3e31ceb0809e03a4d
Caroline Hagan <info@blueocto.co.uk>
2017-10-20 11:08:08 +0100

    Completed "starting-up > trade-with-other-countries > preparing-to-export > Understand the challenges of exporting" page.

---
+++ starting-up/trade-with-other-countries/preparing-to-export/understand-the-challenges-of-exporting.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Understand the challenges of exporting | Business Link</title>
+
+	<!-- ref: xxx || /detail?itemId=1073792488&type=RESOURCES -->
+
+	<meta name="description" content="Market research, export paperwork, financing and other resource requirements" />
+	<meta name="keywords" content="export readiness assessment; trade; trading; export; exporting; paperwork; documentation; overseas; abroad; foreign; market research; international; strategy; plan; planning; competition; export market; export opportunities; overseas trade planning; overseas markets; international market; international trade; foreign trade; Exports" />
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Preparing to export</h1>
+		<h2>Understand the challenges of exporting</h2>
+		<p>Exporting isn't simply an add-on to your existing business. It should be part of an overall strategy to develop the business.</p>
+		<p>Before you start exporting, it's worth making sure you have developed a complete export plan looking at all the costs and risks involved:</p>
+		<ul>
+			<li>Exporting presents all the normal challenges of <strong>marketing</strong> in the UK - it's up to you to find customers and convince them to buy from you. Understanding what customers want and how the market operates is vital. See our guide on <a href="/international-trade/importing-and-exporting-basics/overseas-supplier-relationships/entering-overseas-markets.php">researching and entering overseas markets</a>.</li>
+			<li>You need to cope with extra logistical problems,<strong> contractual issues and paperwork</strong>. You'll probably want a contract drawn up using internationally recognised terms and conditions and standard commercial practices to make it clear what your responsibilities are. There's also a range of paperwork for sorting out transport, customs clearance and payments. See our guide on <a href="/international-trade/importing-and-exporting-basics/licensing-and-paperwork/international-trade-paperwork-the-basics.php">international trade paperwork: the basics</a>.</li>
+			<li>If you are VAT registered, you must provide details of all your transactions with other European Union (EU) member states on your VAT return. If the value of goods you buy from or sell to other EU countries reaches the sales or purchases Intrastat threshold during the course of a calendar year you must submit additional monthly supplementary declarations to HM Revenue &amp; Customs. Intrastat thresholds are reviewed annually. The 2011 thresholds are &pound;600,000 for Arrivals and &pound;250,000 for Dispatches. See our guide: <a href="/international-trade/import-and-export-procedures/customs-declarations/intrastat-returns/introduction-to-intrastat.php">introduction to Intrastat</a>.</li>
+			<li>You need to <strong>comply with regulations</strong> in both the UK and overseas. For example, some goods that are allowed in the UK might not satisfy another country's standards or even be legal there.</li>
+			<li>Exporting demands additional <strong>resources</strong>, both in terms of financing and skilled personnel. See the page in this guide on how to <a href="/starting-up/trade-with-other-countries/preparing-to-export/assess-your-skills-and-resources-for-exporting.php">assess your skills and resources for exporting</a>. It can also present extra risks. See our guide on how to <a href="/international-trade/importing-and-exporting-basics/managing-risks-of-importing-and-exporting/manage-the-risks-of-exporting.php">manage the risks of exporting</a>.</li>
+		</ul>
+
+		<p>With the additional costs, such as international transport, you may find you simply can't compete with local suppliers. If the market only offers low margins, or you haven't got the resources you need, you may decide that exporting isn't for you.</p>
+		<p>But if you have got a good product to offer and a well-run business, the chances are there are opportunities out there. If the rewards you expect justify the investment and the risks, you should commit to your export plan and make it happen.</p>
+		
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/starting-out-in-international-trade/preparing-to-export.php" title="Preparing to export">Prev</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/preparing-to-export/assess-your-skills-and-resources-for-exporting.php" title="Assess your skills and resources for exporting">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/starting-out-in-international-trade/preparing-to-export.php" title="Preparing to export">Introduction</a></li>
+			<li>Understand the challenges of exporting</li>
+			<li><a href="/starting-up/trade-with-other-countries/preparing-to-export/assess-your-skills-and-resources-for-exporting.php">Assess your skills and resources for exporting</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/preparing-to-export/assess-export-market-opportunities.php">Assess export market opportunities</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/preparing-to-export/choose-which-export-markets-to-enter.php">Choose which export markets to enter</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/preparing-to-export/plan-your-export-market-entry-strategy.php">Plan your export market entry strategy</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/preparing-to-export/manage-export-contracts-and-logistics.php">Manage export contracts and logistics</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/preparing-to-export/use-the-right-export-support-services.php">Use the right export support services</a></li>
+			<li><a href="/starting-up/trade-with-other-countries/preparing-to-export/review-your-export-readiness.php">Review your export readiness</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>UK Trade &amp; Investment Enquiry Line</h3>
+		<p>020 7215 8000</p>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.contactus.trade.gov.uk/office-finder/" target="_blank" rel="noopener noreferrer">International trade team search on the UK Trade &amp; Investment website</a></li>
+			<li><a href="https://web.archive.org/web/20110225100557/http://www.google.co.uk/intl/en/exportadviser" target="_blank" rel="noopener noreferrer">Export advice on the Google Export Adviser website</a></li>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120729130354/http://www.improve.businesslink.gov.uk//resources//tools//exporting-growth" target="_blank" rel="noopener noreferrer"">Use the tool Exporting for growth on the Growth and Improvement Service</a></li>
+			<li><a href="/international-trade/doing-business-in-the-eu/getting-ready-to-export-to-individual-countries.php">Use our interactive tool to identify what you need to know to export to your target country</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/international-trade/importing-and-exporting-basics/licensing-and-paperwork/international-trade-paperwork-the-basics.php">International trade paperwork: the basics</a></li>
+			<li><a href="/international-trade/do-you-need-an-export-or-import-licence.php">Do you need an export or import licence?</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/managing-risks-of-importing-and-exporting/manage-the-risks-of-exporting.php"">Manage the risks of exporting</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/overseas-supplier-relationships/entering-overseas-markets.php">Entering overseas markets</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="http://webarchive.nationalarchives.gov.uk/20120418184810/http://www.ukti.gov.uk/export/howwehelp.html" target="_blank" rel="noopener noreferrer">
+				<img src="/_img/UKTI.gif" width="104" height="32"  alt="" /> UK Trade &amp; Investment
+			</a>
+		</p>
+
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
