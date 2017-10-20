b16e1cfb73e9410d67aa9159d45aa549f6e91bb9
Caroline Hagan <info@blueocto.co.uk>
2017-10-20 14:47:51 +0100

    Completed "starting-up > protect-your-business-ideas > non-disclosure-agreements > Issues when signing a non-disclosure agreement" page.

--- starting-up/protect-your-business-ideas/non-disclosure-agreements/issues-when-signing-a-non-disclosure-agreement.php
+++ starting-up/protect-your-business-ideas/non-disclosure-agreements/issues-when-signing-a-non-disclosure-agreement.php
@@ -2,11 +2,11 @@
 <html class="no-js">
 
 <head>
-	<title>Creating a non-disclosure agreement | Business Link</title>
+	<title>Issues when signing a non-disclosure agreement | Business Link</title>
 	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
-
-	<!-- ref: detail5cf4.html || xx -->
-	<meta name="description" content="Use non-disclosure agreements to protect ideas that you are not ready to patent and keep information secure" />
+	
+	<!-- ref: x_x.html || xx -->
+	<meta name="description" content="How to ensure your non-disclosure agreement protects you against conflict" />
 	<meta name="keywords" content="trade secrets; non-disclosure agreements;confidentiality agreements;non disclosure agreement;template;sample;NDA;secure information;confidential information;intellectual property;confidentiality clause;public domain;commercial relationship;contract;trade secrets;protecting;protection;Data security;intellectual property protection;patents;ideas;protecting ideas;collaboration;sharing ideas;secrets;information security;information breaches;security breaches;Data security;Freedom of Information Act; Data security" />
 </head>
 
@@ -17,59 +17,43 @@
 		<div class="small-12 large-8 large-push-4 columns">
 
 			<h1>Non-disclosure agreements</h1>
-			<h2>Creating a non-disclosure agreement</h2>
-			<p>A non-disclosure agreement (NDA) could suit you if you want to approach industry contacts such as a potential buyer, supplier, customer or partner.</p>
-			<p>In the agreement, it is a good idea to specify:</p>
-			<ul>
-				<li>how the information can be used</li>
-				<li>any restrictions on the use of the information you provide, eg preventing the recipient from making copies</li>
-				<li>that the owner retains the intellectual property rights, copyright or possible patents</li>
-			</ul>
-
-			<p>The key clauses will be those detailing:</p>
-			<ul>
-				<li>protection against the copying or retention of confidential information</li>
-				<li>protection against disclosure of information not already in the public domain</li>
-				<li>a remedy, eg compensation, for any breach of the agreement</li>
-			</ul>
-
-			<p>Before drawing up an NDA ensure that the information you are seeking to protect cannot be construed as being in the <strong>public domain</strong>. Once you demonstrate your product, tell someone about an idea or write about it, it can no longer be considered confidential.</p>
-			<p>Under the terms of the Freedom of Information Act 2000 public bodies will usually reject any confidentiality agreements that relate to the terms of the contract, its value and performance. It is a good idea to ask public bodies if they have a confidentiality agreement or NDA which meets the terms of the Act.</p>
-			<p>You can <a href="http://webarchive.nationalarchives.gov.uk/20120729155638/http://www.ipo.gov.uk/p-cda.htm" target="_blank" rel="noopener noreferrer">find out about confidentiality on the Intellectual Property Office website</a>.</p>
-			<p><strong>Physically secure</strong> your confidential information. In any legal action for breach of confidentiality, the courts would be reluctant to treat information as secret that you had not taken practical steps to protect.</p>
-			<p>Such measures could include:</p>
-			<ul>
-				<li>locking confidential information away</li>
-				<li>limiting the number of people who can access the information</li>
-				<li>ensuring you have up-to-date IT security systems in place</li>
-			</ul>
-
-			<p>It also helps if you have a company <strong>policy</strong> regarding the use and disclosure of confidential information.</p>
+			<h2>Issues when signing a non-disclosure agreement</h2>
+			<p>Once you sign a non-disclosure agreement (NDA), you have entered into a legally binding contract. This allows you to share ideas with business partners while preventing them from passing this information on.</p>
+			<p>Unfortunately, the contract is only as good as the person signing it. If your partner breaches the contract, you can take them to court for damages, but this might be expensive and time consuming. It can also be difficult to quantify the damages.</p>
+			<p>If you suspect that your business partner may be about to breach the NDA, you can get an <strong>injunction</strong>, ie a court order preventing them from breaching the agreement, to stop this happening.</p>
+			<p>To put you in a better position if you do have to go to court, make sure that all definitions and exceptions in the agreement meet your requirements and are appropriate to the type of trade secret you are sharing. If the contract is flawed, it might not provide enough protection.</p>
+			<p>It is a good idea to get to the patent-pending stage of a patent application before approaching investors, marketing or licensing companies. <a href="http://webarchive.nationalarchives.gov.uk/20120729155641/http://www.ipo.gov.uk/patent/p-applying/p-apply.htm" target="_blank" rel="noopener noreferrer">Find information about applying for a patent on the Intellectual Property Office website</a>.</p>
+			<p>The biggest difficulty is determining exactly what is and what isn't covered by the agreement. The person disclosing the information wants the contract to be interpreted in its broadest form, whereas the partner prefers to see the contract interpreted in a narrower way.</p>
+			<p>If any issues arise and worry either party, it is possible to change or add clauses to resolve the issue.</p>
+			<p>You can <a href="/_files/ipo_non-disclosure-agreements.pdf" target="_blank" rel="noopener noreferrer">download information about confidentiality and a sample agreement from the Intellectual Property Office website (PDF, 149K)</a>.</p>
 			
 			<ul>
-				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/creating-a-non-disclosure-agreement.php" title="Key non-disclosure agreement terms">Prev</a></li>
-				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/issues-when-signing-a-non-disclosure-agreement.php" title="Issues when signing a non-disclosure agreement">Next</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/creating-a-non-disclosure-agreement.php" title="Creating a non-disclosure agreement">Prev</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/here-is-how-I-use-ndas-in-my-business.php" title="Here's how I use NDAs in my business">Next</a></li>
 			</ul>
-			
+
 			<h3>Subjects covered in this guide</h3>
 			<ul>
 				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/patent-protection-and-your-business/non-disclosure-agreements.php" title="Non-disclosure agreements">Introduction</a></li>
 				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/what-is-a-non-disclosure-agreement.php">What is a non-disclosure agreement?</a></li>
 				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/types-of-non-disclosure-agreement.php">Types of non-disclosure agreement</a></li>
 				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/ways-to-prepare-a-non-disclosure-agreement.php">Ways to prepare a non-disclosure agreement</a></li>
-				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/creating-a-non-disclosure-agreement.php">Key non-disclosure agreement terms</a></li>
-				<li>Creating a non-disclosure agreement</li>
-				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/issues-when-signing-a-non-disclosure-agreement.php">Issues when signing a non-disclosure agreement</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/key-non-disclosure-agreement-terms.php">Key non-disclosure agreement terms</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/creating-a-non-disclosure-agreement.php">Creating a non-disclosure agreement</a></li>
+				<li>Issues when signing a non-disclosure agreement</li>
 				<li><a href="/starting-up/protect-your-business-ideas/non-disclosure-agreements/here-is-how-I-use-ndas-in-my-business.php">Here's how I use NDAs in my business</a></li>
 			</ul>
 			
 			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
 
+			<h3>Lawyers for your Business</h3>
+			<p>020 7405 9075</p>
+
 			<h3>Actions</h3>
 			<ul>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120729155642/http://www.ipo.gov.uk/patent.htm" target="_blank" rel="noopener noreferrer">Patent application advice on the Intellectual Property Office website</a></li>
 				<li><a href="/_files/ipo_non-disclosure-agreements.pdf" target="_blank" rel="noopener noreferrer">Download confidential disclosure agreements information from the Intellectual Property Office website (PDF, 149K)</a></li>
 				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/find-value-you-didnt-know-existed-in-your-business/find-out-how-to-protect-your-ideas.php">Use our interactive tool to find out the best ways to protect your intellectual property</a></li>
-				<li><a href="http://webarchive.nationalarchives.gov.uk/20120729155638/http://www.ipo.gov.uk/p-cda.htm" target="_blank" rel="noopener noreferrer">Confidentiality agreement guidance on the Intellectual Property Office website</a></li>
 			</ul>
 			
 			<h3>Also on this site</h3>
