580a508ad1f6fd0466f4bf941c5613ff220c23d2
Caroline Hagan <info@blueocto.co.uk>
2017-10-20 16:57:07 +0100

    Completed "Business Start Up Organiser" page.

---
+++ starting-up/business-start-up-organiser.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Business start-up organiser | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: x_x.html || xx -->
+</head>
+
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Business start-up organiser </h1>
+		<h2>Important information</h2>
+		<p>We have launched a new section of the website for people starting up a business. <strong>My New Business</strong> contains lots of new tools, videos and tutorials to help you start up.</p>
+		<p>It includes a tool that lets you build and manage your own list of tasks from a list of <strong>recommended start-up tasks</strong>. You can also add your own notes and target dates. <a href="http://webarchive.nationalarchives.gov.uk/20120729120317/https://online.businesslink.gov.uk/hub/action/render?pageId=tasklist">Create your task list</a>.</p>
+		<p>If you have previously registered to use the Business start-up organiser, we recommend you use the option on the Task summary page to download your tasks as a PDF and then begin using My New Business.</p>
+		
+		<img src="/_img/sujmPageThumb1.gif" alt="" width="149" height="80" />
+		
+		<p><strong>Questionnaire</strong></p>
+		<p>Answer a series of questions about your business</p>
+		
+		<img src="/_img/sujmPageThumb2.gif" alt="" width="149" height="80" />
+		
+		<p><strong>Task summary</strong></p>
+		<p>Receive a personalised list of step-by-step tasks to save and complete at your own pace</p>
+		
+		<img src="/_img/sujmPageThumb3.gif" alt="" width="149" height="80" />
+		
+		<p><strong>Work on tasks</strong></p>
+		<p>Access a library of resources to help you complete each task</p>
+		
+		<form method="GET" action=""><!-- action="http://www.businesslink.gov.uk/bdotg/action/startQuestionnaire" -->
+			<table cellpadding="0" cellspacing="0" width="100%" summary="">
+				<tr>
+					<td width="50%" valign="top">
+						<a href="#">Already created a Business start-up organiser? Log in</a>
+						<!-- http://online.businesslink.gov.uk/bdotg/action/startUpWB -->
+					</td>
+					<td width="50%" valign="top" align="right">
+						<input type="image" src="/_img/button_getstarted.gif" title="Get started" alt="" />
+					</td>
+				</tr>
+			</table>
+		</form>
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
