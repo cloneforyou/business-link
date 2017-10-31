92f4b7d28d326e14e61944fba14a8849f475f017
Caroline Hagan <info@blueocto.co.uk>
2017-10-31 14:37:07 +0000

    Completed "finance-and-grants > financial-planning-and-accounts > business-budgeting > Budgets and business planning" page.

---
+++ finance-and-grants/financial-planning-and-accounts/business-budgeting/budgets-and-business-planning.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Budgets and business planning | Business Link</title>
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: detail9df5.html || /detail?itemId=1074416965&type=RESOURCES -->
+	<meta name="description" content="Definition of a budget and the advantages of using one, including clear allocation of resources" />
+	<meta name="keywords" content="Business budgeting; business budget;business planning;financial planning;create budget;set budget;tax planning;tax budget;tax budgeting;compile budget;budget advice;benchmarking;benchmark;performance indicator;kpi;budget review;financial plan;cash flow;cashflow;money management;financial management;strategic planning;financial control; Business practice and regulation;Small businesses;Business development;Business management;Financial management;Performance management" />
+	
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Business budgeting</h1>
+		<h2>Budgets and business planning</h2>
+		<p>Your business plan is a roadmap for your future development. It describes your business, its objectives, financial forecasts and your market. It can help you secure external finance, measure success and grow the business. However, for internal management purposes, budgeting can be the most effective way to control your cashflow, allowing you to invest in new opportunities at the appropriate time.</p>
+		<p>See our guides on how to <a href="/starting-up/business-planning/prepare-a-business-plan.php">prepare a business plan</a> and how to <a href="/grow-your-business/setting-a-vision-for-growth/prepare-a-business-plan-for-growth.php" title="Planning after start-up - monitoring progress, reviewing plans, setting targets and allocating resources">prepare a business plan for growth</a>.</p>
+		<p>If your business is growing, you may not always be able to be hands-on with every part of it. You may have to split your budget up between different areas such as sales, production, marketing, etc. You'll find that money starts to move in many different directions through your organisation - having a budget is vital to ensuring that you stay in control of expenditure.</p>
+		<p>A budget is a plan to:</p>
+		<ul>
+			<li>control your finances</li>
+			<li>ensure you can continue to fund your current commitments</li>
+			<li>enable you to make confident financial decisions and meet your objectives</li>
+			<li>ensure you have enough money for your future projects</li>
+		</ul>
+		<p>It outlines what you will spend your money on and how that spending will be financed. However, it is not a forecast. A forecast is a prediction of the future whereas a budget is a <strong>planned outcome</strong> that your business wants to achieve.</p>
+		
+		<h3>Benefits of a business budget</h3>
+		<p>There are a number of benefits of drawing up a business budget, including being better able to:</p>
+		<ul>
+			<li>manage your money effectively</li>
+			<li>allocate appropriate resources to projects</li>
+			<li>monitor performance</li>
+			<li>meet your objectives</li>
+			<li>improve decision-making</li>
+			<li>identify problems before they occur - such as the need to raise finance or cashflow difficulties</li>
+			<li>plan for the future</li>
+			<li>increase staff motivation</li>
+		</ul>
+
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting.php" title="Business budgeting">Prev</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/creating-a-budget.php" title="Creating a budget">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting.php" title="Business budgeting">Introduction</a></li>
+			<li>Budgets and business planning</li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/creating-a-budget.php">Creating a budget</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/key-steps-in-drawing-up-a-budget.php">Key steps in drawing up a budget</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/what-your-budget-should-cover.php">What your budget should cover</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/using-your-budget-to-measure-performance.php">Using your budget to measure performance</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/reviewing-your-budget-regularly.php">Reviewing your budget regularly</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/here-is-how-budgeting-and-business-planning-helped-my-business-grow.php">Here's how budgeting and business planning helped my business grow </a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.learndirect.com/store/business-workplace/finance-accounts" target="_blank" rel="noopener noreferrer">Financial management online courses on the learndirect business website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php">Prepare a business plan</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/prepare-a-business-plan-for-growth.php">Prepare a business plan for growth</a></li>
+			<li><a href="/sales-and-marketing/selling/forecast-and-plan-your-sales.php">Forecast and plan your sales</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business.php">Set up a simple profit and loss account for your business</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php">Financial and management accounts: the basics</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">Cashflow management: the basics</a></li>
+			<li><a href="/finance-and-grants/managing-a-business-when-economic-conditions-are-tough.php" title="Run your business more effectively when economic conditions are tough or unpredictable">Managing a business when economic conditions are tough</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="http://www.icaew.com/" target="_blank" rel="noopener noreferrer" title="Institute of Chartered Accountants in England and Wales">
+				<img src="/_img/icaew_solus_logo_19x35px.JPG"  width="19" height="35" alt="" /> Institute of Chartered Accountants in England and Wales
+			</a>
+		</p>
+
+		<img src="/_img/px_finance.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
