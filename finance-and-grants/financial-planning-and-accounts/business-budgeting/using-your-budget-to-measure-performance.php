8d9969d851b5a9bdb5f5fae0726b8b3e4ee22741
Caroline Hagan <info@blueocto.co.uk>
2017-11-08 19:14:28 +0000

    Completed "finance-and-grants > financial-planning-and-accounts > business-budgeting > Using your budget to measure performance" page.

---
+++ finance-and-grants/financial-planning-and-accounts/business-budgeting/using-your-budget-to-measure-performance.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Using your budget to measure performance | Business Link</title>
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: detailbcc6.html || /detail?itemId=1074417241&type=RESOURCES -->
+	<meta name="description" content="Benchmark your business using your budget projections and key performance indicators" />
+	<meta name="keywords" content="Business budgeting; business budget;business planning;financial planning;create budget;set budget;tax planning;tax budget;tax budgeting;compile budget;budget advice;benchmarking;benchmark;performance indicator;kpi;budget review;financial plan;cash flow;cashflow;money management;financial management;strategic planning;financial control; Business practice and regulation;Small businesses;Business development;Business management;Financial management;Performance management" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+		<h1>Business budgeting</h1>
+		<h2>Using your budget to measure performance</h2>
+		<p>Your budget can be a financial action plan. This can be useful, particularly if you review your budgets regularly as part of your annual planning cycle - see our guide on how to <a href="/grow-your-business/setting-a-vision-for-growth/prepare-a-business-plan-for-growth.php">prepare a business plan for growth</a>.</p>
+		<p>Your budget can serve as:</p>
+		<ul>
+			<li>an indicator of the costs and revenues linked to each of your activities</li>
+			<li>
+				a way of providing information and supporting management decisions throughout the year
+			</li>
+			<li>a means of monitoring and controlling your business, particularly if you analyse the differences between your actual and budgeted income</li>
+		</ul>
+
+		<h3>Benchmarking performance</h3>
+		<p>Comparing your budget year on year can be an excellent way of benchmarking your business' performance - for example, you can compare your projected figures with previous years to measure your performance. See our guide on how to <a href="/sales-and-marketing/selling/forecast-and-plan-your-sales.php">forecast and plan your sales</a>.</p>
+		<p>You can also compare your figures for projected margins and growth with those of other businesses in the same sector, or across different parts of your business.</p>
+		
+		<h3>Key performance indicators (KPIs)</h3>
+		<p>To boost your business' performance you need to understand and monitor the key 'drivers' of your business - a driver is something that has a major impact on your business. There are many factors affecting every business' performance, so it is vital to focus on a handful of these and monitor them carefully.</p>
+		<p>The three key drivers for most businesses are:</p>
+		<ul>
+			<li>sales</li>
+			<li>costs</li>
+			<li>working capital</li>
+		</ul>
+
+		<p>Any trends towards cashflow problems or falling profitability will show up in these figures when measured against your budgets and forecasts. They can help you spot problems early on if they are calculated on a consistent basis.</p>
+		
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/what-your-budget-should-cover.php" title="What your budget should cover">Prev</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/reviewing-your-budget-regularly.php" title="Reviewing your budget regularly">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting.php" title="Business budgeting">Introduction</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/budgets-and-business-planning.php">Budgets and business planning</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/creating-a-budget.php">Creating a budget</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/key-steps-in-drawing-up-a-budget.php">Key steps in drawing up a budget</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/what-your-budget-should-cover.php">What your budget should cover</a></li>
+			<li>Using your budget to measure performance</li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/reviewing-your-budget-regularly.php">Reviewing your budget regularly</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/here-is-how-budgeting-and-business-planning-helped-my-business-grow.php" title="Here's how budgeting and business planning helped my business grow">Here's how budgeting and business planning helped my business grow</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="icaew_key-performance-indicators_oct-11.pdf" target="_blank" rel="noopener noreferrer">Download KPI guidance from the Institute of Chartered Accountants in England and Wales website (PDF, 191K)</a></li>
+			<li><a href="http://www.learndirect.com/store/business-workplace/finance-accounts" target="_blank" rel="noopener noreferrer">Financial management online courses on the learndirect business website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/selling/forecast-and-plan-your-sales.php">Forecast and plan your sales</a></li>
+			<li><a href="/grow-your-business/assessing-current-performance/measure-performance-and-set-targets.php">Measure performance and set targets</a></li>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php">Prepare a business plan</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/prepare-a-business-plan-for-growth.php">Prepare a business plan for growth</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/financial-and-management-accounts-the-basics.php">Financial and management accounts: the basics</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">Cashflow management: the basics</a></li>
+			<li><a href="/finance-and-grants/managing-a-business-when-economic-conditions-are-tough.php">Managing a business when economic conditions are tough</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="http://www.icaew.com/" target="_blank" rel="noopener noreferrer" title="Institute of Chartered Accountants in England and Wales">
+				<img src="/_img/icaew_solus_logo_19x35px.JPG"  width="19" height="35" alt="" /> Institute of Chartered Accountants in England and Wales
+			</a>
+		</p>
+		
+		<img src="/_img/px_finance.gif" alt="" width="181" height="5" />
+		
+		
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
