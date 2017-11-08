1f5d4977d40d925564a3cf536987ea5856b70de0
Caroline Hagan <info@blueocto.co.uk>
2017-11-08 19:21:27 +0000

    Completed "finance-and-grants > financial-planning-and-accounts > business-budgeting > Reviewing your budget regularly" page.

---
+++ finance-and-grants/financial-planning-and-accounts/business-budgeting/reviewing-your-budget-regularly.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Reviewing your budget regularly | Business Link</title>
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: detaile8d3.html || /detail?itemId=1074417319&type=RESOURCES -->
+	<meta name="description" content="When and why to review your budget and the main steps involved" />
+	<meta name="keywords" content="Business budgeting; business budget;business planning;financial planning;create budget;set budget;tax planning;tax budget;tax budgeting;compile budget;budget advice;benchmarking;benchmark;performance indicator;kpi;budget review;financial plan;cash flow;cashflow;money management;financial management;strategic planning;financial control; Business practice and regulation;Small businesses;Business development;Business management;Financial management;Performance management" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		<h1>Business budgeting</h1>
+		<h2>Reviewing your budget regularly</h2>
+		<p>To use your budgets effectively, you will need to review and revise them frequently. This is particularly true if your business is growing and you are planning to move into new areas.</p>
+		<p>Using up-to-date budgets will help you manage your cashflow effectively and identify what needs to be achieved in the next budgeting period.</p>
+		
+		<h3>Income and expenditure</h3>
+		<p>There are two main areas to consider when reviewing your budget - <strong>income</strong> and <strong>expenditure</strong>.</p>
+		<p><strong>Your actual income</strong> - each month, you should compare your actual income with your sales budget. To do this, you should:</p>
+		<ul>
+			<li>
+				analyse the reasons for any shortfall - for example, lower sales volumes, flat markets and underperforming products
+			</li>
+			<li>
+				consider the reasons for a particularly high turnover - for example, whether your targets were too low
+			</li>
+			<li>compare the timing of your income with your projections and check that they fit</li>
+		</ul>
+
+		<p>Analysing these variations will help you to set future budgets more accurately and also allow you to take action where needed.</p>
+		<p><strong>Your actual expenditure</strong> - regularly review your actual expenditure against your budget. This will help you to predict future costs with greater reliability. You should:</p>
+		<ul>
+			<li>look at how your fixed costs differed from your budget</li>
+			<li>check that your variable costs were in line with your budget - normally variable costs adjust in line with your sales volume</li>
+			<li>analyse any reasons for changes in the relationship between costs and turnover</li>
+			<li>analyse any differences in the timing of your expenditure - for example, by checking suppliers' payment terms</li>
+		</ul>
+
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/using-your-budget-to-measure-performance.php" title="Using your budget to measure performance">Prev</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/here-is-how-budgeting-and-business-planning-helped-my-business-grow.php" title="Here's how budgeting and business planning helped my business grow ">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting.php" title="Business budgeting">Introduction</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/budgets-and-business-planning.php">Budgets and business planning</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/creating-a-budget.php">Creating a budget</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/key-steps-in-drawing-up-a-budget.php">Key steps in drawing up a budget</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/what-your-budget-should-cover.php">What your budget should cover</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/using-your-budget-to-measure-performance.php">Using your budget to measure performance</a></li>
+			<li>Reviewing your budget regularly</li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/here-is-how-budgeting-and-business-planning-helped-my-business-grow.php" title="Here's how budgeting and business planning helped my business grow ">Here's how budgeting and business planning helped my business grow </a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.learndirect.com/store/business-workplace/finance-accounts" target="_blank" rel="noopener noreferrer">Financial management online courses on the learndirect business website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/starting-up/business-planning/prepare-a-business-plan.php">Prepare a business plan</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/prepare-a-business-plan-for-growth.php">Prepare a business plan for growth</a></li>
+			<li><a href="/sales-and-marketing/selling/forecast-and-plan-your-sales.php">Forecast and plan your sales</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/set-up-a-simple-profit-and-loss-account-for-your-business.php">Set up a simple profit and loss account for your business</a></li>
+			<li><a href="/starting-up/find-and-manage-the-money/stock-control-and-inventory.php">Stock control and inventory</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/invoicing-and-payment-terms.php">Invoicing and payment terms</a></li>
+			<li><a href="/finance-and-grants/managing-a-business-when-economic-conditions-are-tough.php">Managing a business when economic conditions are tough</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="http://www.icaew.com/" target="_blank" rel="noopener noreferrer" title="Institute of Chartered Accountants in England and Wales">
+				<img src="/_img/icaew_solus_logo_19x35px.JPG"  width="19" height="35" alt="" /> Institute of Chartered Accountants in England and Wales
+			</a>
+		</p>
+		
+		<img src="/_img/px_finance.gif" alt="" width="181" height="5" />
+		
+		
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
