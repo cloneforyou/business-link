2fafcff711e0be733d41a3ab055f30f73be612ce
Caroline Hagan <info@blueocto.co.uk>
2017-11-08 19:25:27 +0000

    Completed "finance-and-grants > financial-planning-and-accounts > business-budgeting > Here's how budgeting and business planning helped my business grow" page.

---
+++ finance-and-grants/financial-planning-and-accounts/business-budgeting/here-is-how-budgeting-and-business-planning-helped-my-business-grow.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Here's how budgeting and business planning helped my business grow | Business Link</title>
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: detail2744-2.html || /detail?itemId=1081936681&type=CASE STUDIES -->
+	<meta lname="description" content="In this case study Gideon Kasfiner explains how budgeting and business planning helped his business grow" />
+	<meta name="keywords" content="Business budgeting; business budget;business planning;financial planning;create budget;set budget;tax planning;tax budget;tax budgeting;compile budget;budget advice;benchmarking;benchmark;performance indicator;kpi;budget review;financial plan;cash flow;cashflow;money management;financial management;strategic planning;financial control; Business practice and regulation;Small businesses;Business development;Business management;Financial management;Performance management" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+		<h1>Here's how budgeting and business planning helped my business grow </h1>
+		<h2>Here's how budgeting and business planning helped my business grow </h2>
+		<p>Gideon Kasfiner launched First Luggage, a door-to-door luggage collection and delivery service, in 2004. The business now employs nine people and turns over &pound;2 million a year. Here Gideon explains how financial planning has helped him grow his business in a controlled way.</p>
+		
+		<h3>What I did</h3>
+		<h4>Worked to an annual and monthly timetable</h4>
+		<p>"I start doing my annual budgets three months before the year end. I look at the current year and see whether we are going to increase sales, and if we are how it's going to increase our costs, and so on. My accountant actually sets the budgets though, from the figures I give.</p>
+		<p>"Then, every month, I sit down with my management accounts. I look at my actual income and expenditure, compare them with my projections and look at the variance. I can see under every budgetary heading where things have gone up or down and I can make appropriate decisions.</p>
+		<p>"So, for example, we may have budgeted sales of &pound;200,000 for one month but only made &pound;170,000. I might, for example, look at the marketing budget and do some more advertising. I might take a service out where costs are high, or even put prices up. It's all about juggling."</p>
+		
+		<h4>Built a detailed picture of my business</h4>
+		<p>"I work with as many budget headings as I can. On the sales side, I break the figures down into the different products we deliver &ndash; suitcases, prams, golf bags, and so on. Then I can see exactly where revenue is coming from. I'll also look at the top 15 countries I'm delivering to.</p>
+		<p>"On the cost side there are all sorts of headings, from transportation to fuel surcharges, office rent, salaries, and PR and marketing.</p>
+		<p>"It's not easy doing things this way, but it gives me a better picture of my business and I have more areas to juggle with. Flexibility is very important."</p>
+		
+		<h4>Budgeted for loss of business</h4>
+		<p>"After doing the annual budgets, I do them again with a 20 per cent drop in sales to see what the impact would be on the cashflow if there are any changes in the market. I want to make sure I'm being as prudent as I can be with my cash.</p>
+		<p>"My mind is always looking at the downside as well as the upside. If I have a month that hasn't come up to where it should, my comfort zone is that I can do something about it."</p>
+		
+		<h3>What I'd do differently</h3>
+		<h4>Work with a colleague on monthly reviews from the start</h4>
+		<p>"Initially, I did the monthly reviews on my own and then I started doing them with a commercial colleague. Ideas are better when you have two heads looking at the same thing."</p>
+		
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/reviewing-your-budget-regularly.php" title="Reviewing your budget regularly">Prev</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting.php" title="Business budgeting">Introduction</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/budgets-and-business-planning.php">Budgets and business planning</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/creating-a-budget.php">Creating a budget</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/key-steps-in-drawing-up-a-budget.php">Key steps in drawing up a budget</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/what-your-budget-should-cover.php">What your budget should cover</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/using-your-budget-to-measure-performance.php">Using your budget to measure performance</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/business-budgeting/reviewing-your-budget-regularly.php">Reviewing your budget regularly</a></li>
+			<li>Here's how budgeting and business planning helped my business grow </li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<div class="media-object stack-for-small">
+			<div class="media-object-section">
+				<div class="thumbnail">
+					<img src="/_img/Gideon_Kasfiner.jpg" width="181" height="121" alt="Gideon Kasfiner - First Luggage" />
+				</div>
+			</div>
+			<div class="media-object-section">
+				<h3>
+					Gideon Kasfiner<br />
+					<a href="https://www.firstluggage.com/" target="_blank" rel="noopener noreferrer">First Luggage</a><br />
+					Gideon's top tips:
+				</h3>
+				<ul>
+					<li>"Understand your business intimately and exactly where your costs are going to come from."</li>
+					<li>"Be realistic about whether your costs can cover the sales you are forecasting."</li>
+					<li>"Calculate your cashflow when you review your budgets, so you can keep an eye on whether you are running short of cash."</li>
+				</ul>
+			</div>
+		</div>
+
+		<p><strong><a href="/find-a-case-study.php" title="">Find a case study</a></strong></p>
+		
+		<p>Developed with:<br />
+			<a href="http://www.icaew.com/" target="_blank" rel="noopener noreferrer" title="Institute of Chartered Accountants in England and Wales">
+				<img src="/_img/icaew_solus_logo_19x35px.JPG"  width="19" height="35" alt="" /> Institute of Chartered Accountants in England and Wales
+			</a>
+		</p>
+		
+		<img src="/_img/px_finance.gif" alt="" width="181" height="5" />
+		
+		
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
