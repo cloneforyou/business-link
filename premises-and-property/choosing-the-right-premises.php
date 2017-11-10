725e1bf984b98bb6a7d69be6cf2a32584bc5e216
Caroline Hagan <info@blueocto.co.uk>
2017-11-10 14:33:30 +0000

    Completed "> premises-and-property > Choosing the right premises" page.

---
+++ premises-and-property/choosing-the-right-premises.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Choosing the right premises | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: layer4e06.html || /layer?topicId=1073859248 -->
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Choosing the right premises</h1>
+		<h2>
+		<a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business.php">Choose the right premises for your business</a></h2>
+		<p>Finding premises that meet your business needs and using a specification to search effectively</p>
+		<h2>
+		<a href="/premises-and-property/choosing-the-right-premises/renting-business-premises.php">Renting business premises</a></h2>
+		<p>How to find the right premises to rent to fit your business needs</p>
+		<h2>
+		<a href="/premises-and-property/choosing-the-right-premises/commercial-property-responsibilities-of-business-tenants.php">Commercial property: responsibilities of business tenants</a></h2>
+		<p>How tenants are responsible for health and safety and maintenance duties in commercial properties</p>
+		<h2>
+		<a href="/premises-and-property/choosing-the-right-premises/renewing-and-ending-your-business-lease.php">Renewing and ending your business lease</a></h2>
+		<p>The rights of tenants and landlords to end or renew a business tenancy and the help and advice available</p>
+		<h2>
+		<a href="/premises-and-property/choosing-the-right-premises/use-your-home-as-a-workplace.php">Use your home as a workplace</a></h2>
+		<p>Tax implications, financial and legal issues, security and setting up your work area if you plan to work from home</p>
+		<h2>
+		<a href="/your-business-sector/property-management/commercial-property/buying-business-premises.php">Buying business premises</a></h2>
+		<p>What to consider when buying your own premises, financial and legal aspects, and when to use a solicitor or surveyor</p>
+		<h2>
+		<a href="/finance-and-grants/finance-options/borrowing/commercial-mortgages-and-lenders.php">Commercial mortgages and lenders</a></h2>
+		<p>How to find a mortgage or broker to suit your business situation</p>
+		
+		<img src="/_img/choosing_premises.jpg" width="146" height="146" alt="" />
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+		
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
