893e5885af556fb06f9a2f24bbed865b1f693b23
Caroline Hagan <info@blueocto.co.uk>
2017-11-14 20:33:30 +0000

    Completed "sales-and-marketing > keeping-your-customers > Know your customers' needs" page. - need to check when online

--- _inc/nav--sales-and-marketing.php
+++ _inc/nav--sales-and-marketing.php
@@ -10,7 +10,24 @@
 		<li><a href="/sales-and-marketing/selling-and-the-law.php">Selling and the law</a></li>
 		<li><a href="/sales-and-marketing/selling-overseas.php">Selling overseas</a></li>
 		<li><a href="/sales-and-marketing/pricing.php">Pricing</a></li>
-		<li><a href="/sales-and-marketing/keeping-your-customers.php">Keeping your customers</a></li>
+		<li>
+			<a href="/sales-and-marketing/keeping-your-customers.php">Keeping your customers</a>
+			<ul>
+				<li>
+					<a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs.php">Know your customers' needs</a>
+					<ul>
+						<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs.php" title="Know your customers' needs">Introduction</a></li>
+						<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs/why-do-your-customers-need-you.php">Why do your customers need you?</a></li>
+						<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs/what-do-you-know-about-your-customers.php">What do you know about your customers?</a></li>
+						<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs/the-customers-current-supplier.php">The customer's current supplier</a></li>
+						<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs/ten-things-you-need-to-know-about-your-customers.php">Ten things you need to know about your customers</a></li>
+						<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs/obtaining-information-on-your-customers.php">Obtaining information on your customers</a></li>
+						<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs/segment-your-customers.php">Segment your customers</a></li>
+						<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs/here-is-how-knowing-our-customers-needs-benefits-our-business_flash-video.php">Here's how knowing our customers' needs benefits our business (Flash video)</a></li>
+					</ul>
+				</li>
+			</ul>
+		</li>
 		<li><a href="/sales-and-marketing/suppliers-overview-of-tendering-for-contracts.php">Suppliers: overview of tendering for contracts</a></li>
 		<li><a href="/sales-and-marketing/buyers-publish-tender-documents.php">Buyers: publish tender documents</a></li>
 	</ul>
