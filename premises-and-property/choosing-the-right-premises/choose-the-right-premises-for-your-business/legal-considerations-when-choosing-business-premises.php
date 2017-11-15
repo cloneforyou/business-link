8a5ff12b248ccffef07e21ec0718fd21435b6822
Caroline Hagan <info@blueocto.co.uk>
2017-11-15 14:06:24 +0000

    Completed "premises-and-property > choosing-the-right-premises > choose-the-right-premises-for-your-business > Legal considerations when choosing business premises" page

---
+++ premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business/legal-considerations-when-choosing-business-premises.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Legal considerations when choosing business premises | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: x_x.html || /detail?itemId=1075839761&type=RESOURCES -->
+	<meta name="description" content="Key legal obligations and restrictions to consider when choosing business premises" />
+	<meta name="keywords" content="Premises for your business; find premises;choose premises;property budget;buying premises;relocation;buy a business property;business property to rent;business lease;buying or renting business premises;lease;commercial property;location;renting;premises specification;property spec;property specification;commercial agent;estate agent;licenced premises;commercial premises;business premises" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Choose the right premises for your business</h1>
+		<h2>Legal considerations when choosing business premises</h2>
+		<p>If you own or occupy business premises, you need to understand the legal obligations and restrictions that affect you. For example:</p>
+		<ul>
+			<li>The premises must have planning permission that allows them to be used for your type of business. For more information, see our guide: <a href="/premises-and-property/planning-permission-and-building-regulations/planning-permission-an-overview.php">an overview of the planning system</a>.</li>
+			<li>You must comply with building, fire, and health and safety regulations.</li>
+			<li>Stamp duty is payable on commercial leases and you are likely to be liable for business rates, though in rented premises these may be paid by the landlord. For more information, see our guides: <a href="/tax-payroll-and-company-information/business-rates/business-rates-an-overview.php">business rates - an overview</a> and <a href="/tax-payroll-and-company-information/stamp-taxes.php">stamp taxes</a>.</li>
+			<li>You are responsible for the health and safety of employees and visitors. See our guide on <a href="/workplace-health-and-safety/what-workplace-facilities-do-you-need-to-provide.php">what workplace facilities do you need to provide?</a></li>
+			<li>You also need to provide a suitable working environment.</li>
+			<li>If you provide goods or services to the public, you must take reasonable steps to make your premises accessible. See our guide on <a href="/premises-and-property/access-and-facilities/access-and-facilities-for-disabled-people.php">access and facilities for disabled people</a>.</li>
+			<li>You need to comply with the terms of any lease or licence agreement.</li>
+			<li>For some businesses, you may require a licence to operate or to sell certain products.</li>
+			<li>There may be restrictions on times when deliveries are allowed, noise and pollution levels, and how you or your customers dispose of waste.</li>
+		</ul>
+
+		<p>In licensed or leased premises, responsibilities are shared between the landlord and the tenant. For more information, see our guide on <a href="/premises-and-property/choosing-the-right-premises/commercial-property-responsibilities-of-business-tenants.php">commercial property: responsibilities of tenants</a>.</p>
+		<p>Whatever premises you choose, you need to ensure that you are properly insured. For more information, see our guide on how to <a href="/finance-and-grants/insurance/insure-your-business-and-assets-general-insurances.php">insure your business and assets - general insurances</a>.</p>
+		<p>If you are in any doubt about your legal obligations, you should take advice from your business adviser or solicitor.</p>
+		
+		<ul>
+			<li><a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business/choose-the-right-location-for-your-business-premises.php" title="Choose the right location for your business premises">Prev</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business/search-for-premises.php" title="Search for premises">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business.php" title="Choose the right premises for your business">Introduction</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business/specifying-your-premises-requirements.php">Specifying your premises requirements</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business/choose-the-right-location-for-your-business-premises.php">Choose the right location for your business premises</a></li>
+			<li>Legal considerations when choosing business premises</li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business/search-for-premises.php">Search for premises</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business/here-is-how-I-chose-the-right-premises-for-my-business_flash-video.php">Here's how I chose the right premises for my business (Flash video)</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>EHRC Helpline - England</h3>
+		<p>0845 604 6610</p>
+		
+		<hr />
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/_files/ootdpm_planning-permission-a-guide-for-business_jul-2005.pdf" target="_blank" rel="noopener noreferrer">Planning permission guidance on the Communities and Local Government website (PDF, 1.33MB)</a></li>
+			<li><a href="https://www.gov.uk/building-regulations-approval" target="_blank" rel="noopener noreferrer">Building regulation information on the Planning Portal website</a></li>
+			<li><a href="http://www.hse.gov.uk/simple-health-safety/index.htm" target="_blank" rel="noopener noreferrer">Health and safety law assistance on the Health and Safety Executive website</a></li>
+			<li><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Solicitor listings on the Law Society website</a></li>
+			<li><a href="/finance-and-grants/insurance/get-the-right-insurance-for-your-business.php">Use our interactive tool to work out which forms of insurance you should have for your business</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/business-rates/business-rates-an-overview.php">Business rates - an overview</a></li>
+			<li><a href="/tax-payroll-and-company-information/stamp-taxes.php">Stamp taxes</a></li>
+			<li><a href="/premises-and-property/access-and-facilities/access-and-facilities-for-disabled-people.php">Access and facilities for disabled people</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/commercial-property-responsibilities-of-business-tenants.php">Commercial property: responsibilities of business tenants</a></li>
+			<li><a href="/finance-and-grants/insurance/insure-your-business-and-assets-general-insurances.php">Insure your business and assets - general insurances</a></li>
+			<li><a href="/finance-and-grants/expert-financial-advice/choose-and-work-with-a-solicitor.php">Choose and work with a solicitor</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/renewing-and-ending-your-business-lease.php">Renewing and ending your business lease</a></li>
+			<li><a href="/workplace-health-and-safety/what-workplace-facilities-do-you-need-to-provide.php">What workplace facilities do you need to provide?</a></li>
+		</ul>
+
+		<img src="/_img/px_healthandenvironment.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
