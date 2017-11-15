01b6e2cda1a7352b5923007f2e1b667b90019a39
Caroline Hagan <info@blueocto.co.uk>
2017-11-15 12:56:48 +0000

    Completed "premises-and-property > choosing-the-right-premises > choose-the-right-premises-for-your-business > Choose the right location for your business premises" page

---
+++ premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business/choose-the-right-location-for-your-business-premises.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Choose the right location for your business premises | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: x_x.html || /detail?itemId=1075839688&type=RESOURCES -->
+	<meta name="description" content="Choosing the right business site for your employees, suppliers and customers" />
+	<meta name="keywords" content="Premises for your business; find premises;choose premises;property budget;buying premises;relocation;buy a business property;business property to rent;business lease;buying or renting business premises;lease;commercial property;location;renting;premises specification;property spec;property specification;commercial agent;estate agent;licenced premises;commercial premises;business premises" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Choose the right premises for your business</h1>
+		<h2>Choose the right location for your business premises</h2>
+		<p>A good location for your business is vital, but choosing the right one can be something of a balancing act. Ideally, the location should be convenient for your customers, employees and suppliers - without being too expensive.</p>
+		<p>Factors to consider include:</p>
+		<ul>
+			<li>the level of passing trade - depending on the nature of your business, the level of passing trade can have a huge impact on the success of your venture</li>
+			<li>the number of competitors - although some businesses, like estate agents, can benefit from being located in a cluster of similar businesses, for many others having too many close competitors can have a severe impact on sales and profitability</li>
+			<li>transport links and parking - good public transport links and local parking facilities make it easier for employees and customers who don't live within walking distance</li>
+			<li>delivery restrictions - these can cause problems for your suppliers, so you'll need to make sure that your premises are accessible if you expect to have regular deliveries</li>
+			<li>congestion charges</li>
+			<li>planning restrictions - make sure you check whether you're allowed to use the premises for the purpose you have in mind</li>
+			<li>local authority charges and business rates for services such as waste collection - these can add greatly to the ongoing costs of locating in a particular area, which may make the premises less desirable from your point of view</li>
+			<li>local amenities - employees generally prefer working in areas with good local facilities, and you may need to make regular trips to the bank or a postal depot</li>
+			<li>what sort of area it is - the image of your business may well be affected by the nature of your location</li>
+		</ul>
+
+		<p>Whatever option you go for, there are likely to be advantages and disadvantages. An office in a rural setting might be relaxing, but could be awkward for staff or suppliers making deliveries. Being right in the middle of the city could be very convenient, but might also be expensive.</p>
+		<p>Location has a major impact on cost. If you need premises in a prime location the extra costs may be justified.</p>
+		
+		<ul>
+			<li><a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business/specifying-your-premises-requirements.php" title="Specifying your premises requirements">Prev</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business/legal-considerations-when-choosing-business-premises.php" title="Legal considerations when choosing business premises">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business.php" title="Choose the right premises for your business">Introduction</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business/specifying-your-premises-requirements.php">Specifying your premises requirements</a></li>
+			<li>Choose the right location for your business premises</li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business/legal-considerations-when-choosing-business-premises.php">Legal considerations when choosing business premises</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business/search-for-premises.php">Search for premises</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business/here-is-how-I-chose-the-right-premises-for-my-business_flash-video.php">Here's how I chose the right premises for my business (Flash video)</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.rics.org/uk/knowledge/small-business-hub/proberty-in-business" target="_blank" rel="noopener noreferrer">Business premises advice on the Royal Institution of Chartered Surveyors website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/your-business-sector/property-management/commercial-property/buying-business-premises.php">Buying business premises</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/renting-business-premises.php">Renting business premises</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/use-your-home-as-a-workplace.php">Use your home as a workplace</a></li>
+		</ul>
+
+		<img src="/_img/px_healthandenvironment.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
