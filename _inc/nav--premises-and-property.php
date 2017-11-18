641285358880f511a296db915985d7fbef795a34
Caroline Hagan <info@blueocto.co.uk>
2017-11-18 19:58:32 +0000

    Completed "premises-and-property > choosing-the-right-premises > Renting business premises" page.

--- _inc/nav--premises-and-property.php
+++ _inc/nav--premises-and-property.php
@@ -22,6 +22,19 @@
 								<li><a href="/premises-and-property/choosing-the-right-premises/choose-the-right-premises-for-your-business/here-is-how-I-chose-the-right-premises-for-my-business_flash-video.php">Here's how I chose the right premises for my business (Flash video)</a></li>
 							</ul>
 						</li>
+						<li>
+							<a href="/premises-and-property/choosing-the-right-premises/renting-business-premises.php">Renting business premises</a>
+							<ul>
+								<li><a href="/premises-and-property/choosing-the-right-premises/renting-business-premises.php" title="Renting business premise">Introduction</a></li>
+								<li><a href="/your-business-sector/property-management/commercial-property/renting-business-premises/the-advantages-of-renting-premises.php">The advantages of renting premises</a></li>
+								<li><a href="/premises-and-property/choosing-the-right-premises/renting-business-premises/finding-the-right-premises-to-rent.php">Finding the right premises to rent</a></li>
+								<li><a href="/your-business-sector/property-management/commercial-property/renting-business-premises/types-of-property-rental-agreement.php">Types of property rental agreement</a></li>
+								<li><a href="/premises-and-property/choosing-the-right-premises/renting-business-premises/practicalities-of-renting-premises.php">Practicalities of renting premises</a></li>
+								<li><a href="/premises-and-property/choosing-the-right-premises/renting-business-premises/financial-considerations-when-renting-premises.php">Financial considerations when renting premises</a></li>
+								<li><a href="/premises-and-property/choosing-the-right-premises/renting-business-premises/alterations-to-the-property.php">Alterations to the property</a></li>
+								<li><a href="/premises-and-property/choosing-the-right-premises/renting-business-premises/here-is-how-we-decided-renting-premises-was-best-for-our-business.php">Here's how we decided renting premises was best for our business</a></li>
+							</ul>
+						</li>
 						<li><a href="/premises-and-property/choosing-the-right-premises/commercial-property-responsibilities-of-business-tenants.php">Commercial property: responsibilities of business tenants</a></li>
 					</ul>
 				</li>
---
+++ premises-and-property/choosing-the-right-premises/renting-business-premises.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Renting business premises | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: layer1677.html || /layer?topicId=1075411730 -->
+	<meta name="description" content="How to find the right premises to rent to fit your business needs" />
+	<meta name="keywords" content="Renting business premises; Business rates;rental agreement;utility rates;insurance;property value;capital gains tax;multiple occupancy;rent review;indexed rents;assignment;subletting;stamp duty;BIDs;capital allowances;planning permission;commercial agent;fixed term rent;periodic rent;break clauses;Landlord and Tenant Act;lease renewal;health and safety regulations;flexibility;guarantor;Business Improvement Districts;Business Premises Renovation Allowance" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+		<h1>Renting business premises</h1>
+		<h2>Introduction</h2>
+		<img src="/_img/business_premises.gif" width="78" height="118" alt="" />
+		<p>Finding the right premises for your business is crucial.</p>
+		<p>A key decision is whether to buy or rent property. Renting premises ties up less capital than buying which means you can invest it in the business instead. It can also give you flexibility to relocate easily should you need to.</p>
+		<p>The type of premises you rent and the location will be partly dictated by the type of business you are running. The amount you can afford - taking into account extra costs such as business and utility rates, and building insurance - will also be a deciding factor.</p>
+		<p>This guide outlines the things you should consider before deciding whether renting is a better option for you than buying. It highlights the practicalities you need to consider as a tenant and the type of expert help you can get to make the renting process as trouble free as possible.</p>
+		
+		<ul>
+			<li><a href="/your-business-sector/property-management/commercial-property/renting-business-premises/the-advantages-of-renting-premises.php" title="The advantages of renting premises">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li>Introduction</li>
+			<li><a href="/your-business-sector/property-management/commercial-property/renting-business-premises/the-advantages-of-renting-premises.php">The advantages of renting premises</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/renting-business-premises/finding-the-right-premises-to-rent.php">Finding the right premises to rent</a></li>
+			<li><a href="/your-business-sector/property-management/commercial-property/renting-business-premises/types-of-property-rental-agreement.php">Types of property rental agreement</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/renting-business-premises/practicalities-of-renting-premises.php">Practicalities of renting premises</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/renting-business-premises/financial-considerations-when-renting-premises.php">Financial considerations when renting premises</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/renting-business-premises/alterations-to-the-property.php">Alterations to the property</a></li>
+			<li><a href="/premises-and-property/choosing-the-right-premises/renting-business-premises/here-is-how-we-decided-renting-premises-was-best-for-our-business.php">Here's how we decided renting premises was best for our business</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/_files/ootdpm_thinking-of-taking-on-business-premises_jun-04.pdf" target="_blank" rel="noopener noreferrer">Download renting business premises guidance from the Communities and Local Government website (PDF, 242K)</a></li>
+			<li><a href="http://www.leasingbusinesspremises.co.uk/" target="_blank" rel="noopener noreferrer">Business premises leasing information on the Leasing Business Premises website </a></li>
+		</ul>
+		
+		<img src="/_img/px_healthandenvironment.gif" alt="" width="181" height="5" />
+
+		
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
