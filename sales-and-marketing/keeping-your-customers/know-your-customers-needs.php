1d5338f03e5fbc95bb5b899a738309de34d78acc
Caroline Hagan <info@blueocto.co.uk>
2017-11-18 21:45:19 +0000

    Completed "sales-and-marketing > keeping-your-customers > Know your customers' needs" page.

---
+++ sales-and-marketing/keeping-your-customers/know-your-customers-needs.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Know your customers' needs | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: layerd0da.html || /layer?topicId=1073897627 -->
+	<meta name="description" content="Why understanding your customers' needs is crucial to the success of your business" />
+	<meta name="keywords" content="Know customer's needs; customer research;target the right people;marketing and competitors;unique sales proposition;customers current supplier;customer needs;market information;usp;target market;market research;customer preferences;customers' buying habits;research;customer information;customer data;rival businesses;benefits;market reports;customer records;market trends;market statistics;competition;buying habits;products;best selling product;" />
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+
+			<h1>Know your customers' needs</h1>
+			<h2>Introduction</h2>
+			<img src="/_img/BS0020.gif" width="94" height="62" alt="" />
+			<p>However good your product or service is, the simple truth is that no-one will buy it if they don't want it or believe they don't need it. And you won't persuade anyone that they want or need to buy what you're offering unless you <b>clearly understand</b> what it is your customers really want.</p>
+			<p>Knowing and understanding <b>customer needs</b> is at the centre of every successful business, whether it sells directly to individuals or other businesses. Once you have this knowledge, you can use it to persuade potential and existing customers that buying from you is in their best interests.</p>
+			<p>This guide tells you what you <b>need to know</b> about your customers, how to use this information to sell more <strong>effectively</strong>, and how to win business from your competitors.</p>
+			
+			<ul>
+				<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs/why-do-your-customers-need-you.php" title="Why do your customers need you?">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li>Introduction</li>
+				<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs/why-do-your-customers-need-you.php">Why do your customers need you?</a></li>
+				<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs/what-do-you-know-about-your-customers.php">What do you know about your customers?</a></li>
+				<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs/the-customers-current-supplier.php">The customer's current supplier</a></li>
+				<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs/ten-things-you-need-to-know-about-your-customers.php">Ten things you need to know about your customers</a></li>
+				<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs/obtaining-information-on-your-customers.php">Obtaining information on your customers</a></li>
+				<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs/segment-your-customers.php">Segment your customers</a></li>
+				<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs/here-is-how-knowing-our-customers-needs-benefits-our-business_flash-video.php">Here's how knowing our customers' needs benefits our business (Flash video)</a></li>
+			</ul>
+		
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<p>Developed with:<br />
+				<a href="https://www.cim.co.uk/home/" title="The Chartered Institute of Marketing" target="_blank" rel="noopener noreferrer">
+					<img src="/_img/cim_logo.gif" width="140" height="32" alt="" /> The Chartered Institute of Marketing
+				</a>
+			</p>
+
+			<img src="/_img/px_salesandmarketing.gif" width="181" height="5" />
+
+			
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+	</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
