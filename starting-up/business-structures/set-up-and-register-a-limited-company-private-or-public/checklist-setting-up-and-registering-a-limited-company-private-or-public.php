ef67deb186572acdf80b99a3ad571f1c91e080f8
Caroline Hagan <info@blueocto.co.uk>
2017-12-01 00:39:02 +0000

    Indentation.

--- starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/checklist-setting-up-and-registering-a-limited-company-private-or-public.php
+++ starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/checklist-setting-up-and-registering-a-limited-company-private-or-public.php
@@ -10,83 +10,83 @@
 </head>
 
 <body>
-	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
-	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
-	<div class="row">
-		<div class="small-12 large-8 large-push-4 columns">
-			
-			<h1>Set up and register a limited company (private or public)</h1>
-			<h2>Checklist: setting up and registering a limited company (private or public)</h2>
-			<p>As well as registering your business as a company, there are several other things that you need to do to put it on a proper legal footing. Make sure you:</p>
-			<ul>
-				<li>
-					Display your company's name so it can be clearly seen by all visitors to its registered office or other places of business.
-				</li>
-				<li>
-					Display your company's name clearly on all its business stationery, including letters, invoices, receipts and cheques and your company website if you have one.
-				</li>
-				<li>
-					Show your company's place of registration, registered number and registered office address on all its business letters, order forms, emails, website and faxes.
-				</li>
-				<li>
-					If any company directors' names are included in letters (other than in the text or as the person signing it) you must include the names of <strong>all</strong> the directors.
-				</li>
-				<li>
-					Send all the necessary registration documents and forms to the Registrar of Companies, completed and signed. See the page in this guide on <a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/registration-documents-and-forms.php">registration documents and forms</a>.
-				</li>
-				<li>
-					Check you have received confirmation - certificate of incorporation - from the Registrar of Companies that your application for limited company status has been successful. The company comes into existence only when the Registrar of Companies issues a certificate of incorporation.
-				</li>
-				<li>
-					Contact HM Revenue &amp; Customs to inform them that your company exists.
-				</li>
-			</ul>
-			<p>It's worth remembering, though, that the above checklist only covers the basics. There are many other tax and reporting obligations that your company must meet from year to year. <a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a>.</p>
-			
-			<ul>
-				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/tax-matters-of-a-limited-company.php" title="Tax matters of a limited company">Prev</a></li>
-				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/right-to-manage-companies-and-commonhold-associations.php" title="Right to manage companies and commonhold associations">Next</a></li>
-			</ul>
-			
-			<h3>Subjects covered in this guide</h3>
-			<ul>
-				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public.php" title="Set up and register a limited company (private or public)">Introduction</a></li>
-				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/registration-documents-and-forms.php">Registration documents and forms</a></li>
-				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/the-companys-officers.php">The company's officers</a></li>
-				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/types-of-limited-company.php">Types of limited company</a></li>
-				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/requirements-for-public-limited-companies.php">Requirements for public limited companies</a></li>
-				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/where-to-register-your-company-and-get-help.php">Where to register your company and get help</a></li>
-				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/tax-matters-of-a-limited-company.php">Tax matters of a limited company</a></li>
-				<li>Checklist: setting up and registering a limited company (private or public)</li>
-				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/right-to-manage-companies-and-commonhold-associations.php">Right to manage companies and commonhold associations</a></li>
-			</ul>
-			
-			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+		<h1>Set up and register a limited company (private or public)</h1>
+		<h2>Checklist: setting up and registering a limited company (private or public)</h2>
+		<p>As well as registering your business as a company, there are several other things that you need to do to put it on a proper legal footing. Make sure you:</p>
+		<ul>
+			<li>
+				Display your company's name so it can be clearly seen by all visitors to its registered office or other places of business.
+			</li>
+			<li>
+				Display your company's name clearly on all its business stationery, including letters, invoices, receipts and cheques and your company website if you have one.
+			</li>
+			<li>
+				Show your company's place of registration, registered number and registered office address on all its business letters, order forms, emails, website and faxes.
+			</li>
+			<li>
+				If any company directors' names are included in letters (other than in the text or as the person signing it) you must include the names of <strong>all</strong> the directors.
+			</li>
+			<li>
+				Send all the necessary registration documents and forms to the Registrar of Companies, completed and signed. See the page in this guide on <a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/registration-documents-and-forms.php">registration documents and forms</a>.
+			</li>
+			<li>
+				Check you have received confirmation - certificate of incorporation - from the Registrar of Companies that your application for limited company status has been successful. The company comes into existence only when the Registrar of Companies issues a certificate of incorporation.
+			</li>
+			<li>
+				Contact HM Revenue &amp; Customs to inform them that your company exists.
+			</li>
+		</ul>
+		<p>It's worth remembering, though, that the above checklist only covers the basics. There are many other tax and reporting obligations that your company must meet from year to year. <a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a>.</p>
+		
+		<ul>
+			<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/tax-matters-of-a-limited-company.php" title="Tax matters of a limited company">Prev</a></li>
+			<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/right-to-manage-companies-and-commonhold-associations.php" title="Right to manage companies and commonhold associations">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public.php" title="Set up and register a limited company (private or public)">Introduction</a></li>
+			<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/registration-documents-and-forms.php">Registration documents and forms</a></li>
+			<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/the-companys-officers.php">The company's officers</a></li>
+			<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/types-of-limited-company.php">Types of limited company</a></li>
+			<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/requirements-for-public-limited-companies.php">Requirements for public limited companies</a></li>
+			<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/where-to-register-your-company-and-get-help.php">Where to register your company and get help</a></li>
+			<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/tax-matters-of-a-limited-company.php">Tax matters of a limited company</a></li>
+			<li>Checklist: setting up and registering a limited company (private or public)</li>
+			<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-company-private-or-public/right-to-manage-companies-and-commonhold-associations.php">Right to manage companies and commonhold associations</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
 
-			<h3>Companies House Contact Centre</h3>
-			<p>0303 1234 500</p>
+		<h3>Companies House Contact Centre</h3>
+		<p>0303 1234 500</p>
 
-			<hr />
-			
-			<h3>Actions</h3>
-			<ul>
-				<li><a href="https://www.gov.uk/government/publications/incorporation-and-names#ch9" target="_blank" rel="noopener noreferrer">Guidance on displaying the company name and other details from Companies House</a></li>
-				<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a></li>
-				<li><a href="/do-it-online/incorporate-your-company-online.php">Incorporate your limited company online with Companies House</a></li>
-			</ul>
-						
-			<h3>Also on this site</h3>
-			<ul>
-				<li><a href="/tax-payroll-and-company-information/company-administration-an-introduction/company-directors-responsibilities.php">Company directors' responsibilities</a></li>
-				<li><a href="/tax-payroll-and-company-information/company-administration-an-introduction/company-secretaries-responsibilities.php">Company secretaries' responsibilities</a></li>
-				<li><a href="/tax-payroll-and-company-information/company-administration-an-introduction/reporting-changes-to-companies-house/change-of-address-directors-and-more-what-to-report-to-companies-house.php">Change of address, directors and more - what to report to Companies House</a></li>
-				<li><a href="/for-professional-advisers/for-accountants-and-tax-advisers/helping-your-client-set-up-or-dissolve-a-limited-company/get-started-with-company-incorporation-for-agents.php">Get started with company incorporation for agents</a></li>
-				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
-				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
-				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
-			</ul>
+		<hr />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/government/publications/incorporation-and-names#ch9" target="_blank" rel="noopener noreferrer">Guidance on displaying the company name and other details from Companies House</a></li>
+			<li><a href="/tax-payroll-and-company-information/find-out-what-records-you-should-be-keeping.php">Use our interactive tool to find out the records you must keep as a business</a></li>
+			<li><a href="/do-it-online/incorporate-your-company-online.php">Incorporate your limited company online with Companies House</a></li>
+		</ul>
+					
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/company-administration-an-introduction/company-directors-responsibilities.php">Company directors' responsibilities</a></li>
+			<li><a href="/tax-payroll-and-company-information/company-administration-an-introduction/company-secretaries-responsibilities.php">Company secretaries' responsibilities</a></li>
+			<li><a href="/tax-payroll-and-company-information/company-administration-an-introduction/reporting-changes-to-companies-house/change-of-address-directors-and-more-what-to-report-to-companies-house.php">Change of address, directors and more - what to report to Companies House</a></li>
+			<li><a href="/for-professional-advisers/for-accountants-and-tax-advisers/helping-your-client-set-up-or-dissolve-a-limited-company/get-started-with-company-incorporation-for-agents.php">Get started with company incorporation for agents</a></li>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+			<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+		</ul>
 
-			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+		<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
 
 
 	</div><!-- // .small-12 large-8 large-push-4 columns -->
