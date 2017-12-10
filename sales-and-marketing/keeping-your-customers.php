35372ea76bf7a96d886b3d67910837d78cd7f007
Caroline Hagan <info@blueocto.co.uk>
2017-12-10 21:18:12 +0000

    Completed "sales-and-marketing > Keeping your customers" page.

---
+++ sales-and-marketing/keeping-your-customers.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Keeping your customers | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: layer70fc.html || /layer?topicId=1073858836 -->
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+		<h1>Keeping your customers</h1>
+
+		<img src="/_img/Profitable_customers.jpg" width="146" height="146" alt="" />
+		
+		<h2><a href="/sales-and-marketing/keeping-your-customers/discover-who-your-most-valuable-customers-are.php">Discover who your most valuable customers are</a></h2>
+		<p>This interactive tool will help you to analyse the behaviour of customers to identify which are the most valuable to your business</p>
+		
+		<h2><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs.php">Know your customers' needs</a></h2>
+		<p>Why understanding your customers' needs is crucial to the success of your business</p>
+		
+		<h2><a href="/sales-and-marketing/keeping-your-customers/manage-your-customer-care.php">Manage your customer care</a></h2>
+		<p>Use customer contact, feedback and loyalty schemes to retain customers and increase your sales</p>
+		
+		<h2><a href="/sales-and-marketing/keeping-your-customers/manage-your-customer-database.php">Manage your customer database</a></h2>
+		<p>How a customer database can enhance your business, what to include and how to maintain it</p>
+		
+		<h2><a href="/sales-and-marketing/keeping-your-customers/reach-your-customers-effectively.php">Reach your customers effectively</a></h2>
+		<p>How to decide which sales channels to use to reach your customers</p>
+		
+		<h2><a href="/sales-and-marketing/keeping-your-customers/retain-and-grow-your-customer-base.php">Retain and grow your customer base</a></h2>
+		<p>How to keep existing customers, grow your customer base and improve the effectiveness of your sales</p>
+
+			
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
