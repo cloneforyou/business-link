1285138e27277384b465bfbab3c78354dff156d6
Caroline Hagan <info@blueocto.co.uk>
2017-12-17 16:46:29 +0000

    Completed "finance-and-grants > Finance Options" page.

---
+++ finance-and-grants/finance-options.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Finance options | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+	<!-- ref: x_x.html || /layer?topicId=1084705429 -->
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+			
+		<h1>Finance options</h1>
+		<h2><a href="/finance-and-grants/finance-options/choose-the-right-finance-options.php">Choose the right finance options</a></h2>
+		<p>Find out about types of finance and decide which method of financing is the best fit for your needs</p>
+		
+		<h2><a href="/finance-and-grants/finance-options/borrowing.php">Borrowing</a></h2>
+		<p>Guidance on options available to businesses, including bank finance, commercial mortgages, finance from friends or family and tax efficient borrowing</p>
+		
+		<h2><a href="/finance-and-grants/finance-options/shares-and-equity-finance.php">Shares and equity finance</a></h2>
+		<p>Assess whether your business could benefit from equity finance, issuing shares or floating on the stock market</p>
+		
+		<h2><a href="/finance-and-grants/finance-options/grants-and-government-support.php">Grants and government support</a></h2>
+		<p>Find and secure grants and government support to help start up and develop your business</p>
+		
+		<h2><a href="/finance-and-grants/finance-options/capital-allowances.php">Capital allowances</a></h2>
+		<p>Assess what tax allowances your business can claim on certain purchases or investments</p>
+		
+		<h3>Spotlight</h3>
+		
+		<!-- <p><a href="http://www.youtube.com/BusinessLinkGov" target="bgExternalwwwbusinesslinkgovuk"   title="Video guidance from Business Link on the Business Link YouTube channel - Opens in a new window" >See finance advice videos on Business Link's You Tube channel</a></p> -->
+		
+		<!-- <p>Watch guidance on understanding, seeking and securing finance</p>
+		<a href="http://www.youtube.com/BusinessLinkGov" target="bgExternalwwwbusinesslinkgovuk" title="Video guidance from Business Link on the Business Link YouTube channel - Opens in a new window">
+			<img src="../../Horizontal_Services_images/Video3Pitch2.jpg" width="76" height="76"  alt="Video guidance from Business Link on the Business Link YouTube channel" />
+		</a> -->
+
+		<a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">
+			<img src="/_img/binocs.jpg" width="76" height="76" alt="This interactive tool provides you with a personalised list of the most suitable finance options for your business" />
+		</a>
+
+		<h2><a href="/finance-and-grants/finance-options/choose-the-right-finance-options/identify-the-right-finance-options-for-your-business.php">Find the right finance for your business</a></h2>
+		<p>Use our interactive tool to help you choose and secure the right finance options</p>
+
+		<h3>Case studies</h3>
+		<p>View case studies for finance and grants</p>
+		<ul>
+			<li><a href="/find-a-case-study.php">Find a case study</a></li>
+		</ul>
+
+		<img src="/_img/FGCasestudies.jpg" width="75" height="75" alt="" />
+		
+		<h3>What's popular</h3>
+		<ul>
+			<li><a href="/finance-and-grants/prepare-to-raise-finance/how-to-attract-investment/use-your-business-plan-to-get-funding.php">Use your business plan to get funding</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/cashflow-management-the-basics.php">Cashflow management: the basics</a></li>
+			<li><a href="/finance-and-grants/manage-your-cashflow/identify-potential-cashflow-problems.php">Identify potential cashflow problems</a></li>
+		</ul>
+
+		<h3>Useful links</h3>
+		<ul>
+			<li><a href="http://www.cicm.com/resources/cashflow-guides/" target="_blank" rel="noopener noreferrer">Credit Management Matters</a></li>
+		</ul>
+
+		<h3>Directories</h3>
+		<ul>
+			<li><a href="http://webarchive.nationalarchives.gov.uk/20120729143841/http://www.events.businesslink.gov.uk/eventsearch?ds_topic=108" title="National and local events, seminars and workshops in your area to help you learn or network" target="_blank" rel="noopener noreferrer">Finance and grants events near you</a></li>
+			<li><a href="/find-a-case-study/browse-by-theme-finance-and-grants.php">Finance and grants case studies</a></li>
+		</ul>
+		
+
+	</div>
+	<!-- // .small-12 large-8 large-push-4 columns -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+</div>
+<!-- // .row -->
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
