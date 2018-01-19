0e17f967c724fd1e3b38aec7914ea8aa83a58655
Caroline Hagan <info@blueocto.co.uk>
2018-01-19 14:35:01 +0000

    Completed "create-innovate-and-protect > research-and-development > support-networks-and-facilities-for-innovation-and-r-and-d > Knowledge Transfer Support" page.

---
+++ create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/knowledge-transfer-support.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Knowledge Transfer Support | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: x__x || /detail?itemId=1074501080&type=RESOURCES -->
+	<meta name="description" content="Networking for Innovation, Innovation Vouchers, Knowledge Transfer Partnerships  and where to find more information" />
+	<meta name="keywords" content="support networks and facilities for research and development;incubation;knowledge transfer;technology transfer;innovation;Inside UK enterprise;Faraday partnership;IUKE;innovation relay centre;IRC;business innovation centre;BIC;fit for the future;mentoring;R&amp;D;R and D;Development Agencies;RDAs;clusters;business incubators;regional initiatives;cluster concept;business development;science park;research and development;assisted area;grants; grants for small business; small business; business;; Research and development" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Support networks and facilities for innovation and R&amp;D</h1>
+		<h2>Knowledge Transfer Support</h2>
+		
+		<blockquote>
+			<h3>Did you know?</h3>
+			<p>There are several Government or affiliated organisations who offer a range of products and services that can help your business with design, innovation, intellectual property, measurement and standards needs.</p>
+			<!-- <p><a href="/_files/NI6partnersproductsand_services.pdf" target="bgExternalonlinebusinesslinkgovuk" title="Download a full list of services provided by the NI6 organisations for more information">Download a full list of services provided by these organisations for more information (PDF, 230K)</a>.</p>
+			http://webarchive.nationalarchives.gov.uk/20120823201633/http://www.businesslink.gov.uk/Horizontal_Services_files/NI6partnersproductsand_services.pdf -->
+		</blockquote>
+
+		<p>The government offers a range of Solutions for Business products to help business benefit from the knowledge and expertise of other businesses and academic institutions:</p>
+		<ul>
+			<li>Networking for Innovation.</li>
+			<li>Knowledge Transfer Partnerships (KTPs) - see the page on <a href="/create-innovate-and-protect/research-and-development/work-with-uk-universities-and-colleges/knowledge-transfer-partnerships.php">Knowledge Transfer Partnerships</a> in our guide on how to <a href="/create-innovate-and-protect/research-and-development/work-with-uk-universities-and-colleges.php">work with UK universities and colleges</a>. Also, see the page on <a href="/create-innovate-and-protect/research-and-development/technology-strategy-board/how-the-technology-strategy-board-can-help-your-business.php">how the Technology Strategy Board can help your business</a> in our guide on the <a href="/create-innovate-and-protect/research-and-development/technology-strategy-board.php">Technology Strategy Board</a>.</li>
+		</ul>
+
+		<p>Solutions for Business is a Government Business Support Scheme that is only available in England. Support for businesses is different in Scotland, Wales and Northern Ireland.</p>
+		<p>Read more about Solutions for Business in our guide on <a href="/finance-and-grants/finance-options/grants-and-government-support/government-support-for-businesses.php">government support for businesses</a>.</p>
+		
+		<h3>KTPs</h3>
+		<p>KTPs enable businesses to benefit from the expertise of organisations like further education colleges, universities or research institutes.</p>
+		<p>Recently qualified people at NVQ level 4 or above are recruited to work in the business as a Knowledge Transfer Partnership Associate. Projects last between ten weeks and three years, and address product design, manufacturing, product or management processes, computing or management information.</p>
+		<p>You can <a href="https://www.gov.uk/guidance/knowledge-transfer-partnerships-what-they-are-and-how-to-apply" target="_blank" rel="noopener noreferrer">find information on KTPs on the Technology Strategy Board website</a>.</p>
+		<p>Find out about the range of support for business included in the government's <a href="/solutions-for-business-portfolio.php">Solutions for Business portfolio</a>.</p>
+		
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d.php" title="Support networks and facilities for innovation and R&amp;D">Prev</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/virtual-support-networks.php" title="Virtual support networks">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d.php" title="Support networks and facilities for innovation and R&amp;D">Introduction</a></li>
+			<li>Knowledge Transfer Support</li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/virtual-support-networks.php">Virtual support networks</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/the-enterprise-europe-network.php">The Enterprise Europe Network</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/business-innovation-centres.php">Business innovation centres</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/science-parks.php">Science parks</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/chambers-of-commerce.php">Chambers of Commerce</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/assisted-areas.php">Assisted areas</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/business-clusters.php">Business clusters</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/the-small-business-research-initiative.php">The Small Business Research Initiative</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.gov.uk/guidance/knowledge-transfer-partnerships-what-they-are-and-how-to-apply" target="_blank" rel="noopener noreferrer">KTP information on the KTP website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/finance-and-grants/finance-options/grants-and-government-support/government-support-for-businesses.php">Government support for businesses</a></li>
+			<li><a href="/create-innovate-and-protect/use-innovation-in-your-business/find-support-for-inventors.php">Find support for inventors</a></li>
+			<li><a href="/finance-and-grants/finance-options/grants-and-government-support/innovation-research-and-development-grants.php">Innovation, research and development grants</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/technology-strategy-board.php">Technology Strategy Board</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/work-with-uk-universities-and-colleges.php">Work with UK universities and colleges</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/department-for-business-energy-and-industrial-strategy" target="_blank" rel="noopener noreferrer" title="Department for Business, Energy &amp; Industrial Strategy">
+				<img src="/_img/logo_primary_bis.gif" width="160" height="32" alt="" /> Department for Business, Energy &amp; Industrial Strategy
+			</a>
+		</p>
+
+		<img src="/_img/px_growthandinnovation.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
