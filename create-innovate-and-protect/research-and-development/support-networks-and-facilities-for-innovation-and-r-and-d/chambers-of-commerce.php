4368777e1e220d506383dcc22aeb0120ce9077a0
Caroline Hagan <info@blueocto.co.uk>
2018-02-03 16:14:04 +0000

    Completed "create-innovate-and-protect > research-and-development > support-networks-and-facilities-for-innovation-and-r-and-d > Chambers of commerce" page.

---
+++ create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/chambers-of-commerce.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Chambers of Commerce | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: x__x || /detail?itemId=1074419547&type=RESOURCES -->
+	<meta name="description" content="Find and access support services offered by local Chambers of Commerce" />
+	<meta name="keywords" content="support networks and facilities for research and development;incubation;knowledge transfer;technology transfer;innovation;Inside UK enterprise;Faraday partnership;IUKE;innovation relay centre;IRC;business innovation centre;BIC;fit for the future;mentoring;R&amp;D;R and D;Development Agencies;RDAs;clusters;business incubators;regional initiatives;cluster concept;business development;science park;research and development;assisted area;grants; grants for small business; small business; business;; Research and development" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Support networks and facilities for innovation and R&amp;D</h1>
+		<h2>Chambers of Commerce</h2>
+		<p>The British Chambers of Commerce form a network of 100 quality-accredited chambers throughout the UK.</p>
+		<p>Becoming a member of a chamber gives a business access to services such as:</p>
+		<ul>
+			<li>training</li>
+			<li>information</li>
+			<li>resources</li>
+			<li>networking</li>
+			<li>savings on essential overheads</li>
+		</ul>
+
+		<p>Some services are available online. Larger chambers may offer schemes to encourage innovation and foster best practice. Many areas also have more informal networks, some supporting particular industries.</p>
+		<p>You can <a href="http://www.britishchambers.org.uk/find-your-chamber/" target="_blank" rel="noopener noreferrer">find Chambers of Commerce in your area on the British Chambers of Commerce website</a>.</p>
+		<p>You can also <a href="https://web.archive.org/web/20120605130329/https://web.archive.org/web/20120605130329/http://www.bfora.net/default.asp" target="_blank" rel="noopener noreferrer">find business networks on the bFORA website</a>.</p>
+		
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/science-parks.php" title="Science parks">Prev</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/assisted-areas.php" title="Assisted areas">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d.php" title="Support networks and facilities for innovation and R&amp;D">Introduction</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/knowledge-transfer-support.php">Knowledge Transfer Support</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/virtual-support-networks.php">Virtual support networks</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/the-enterprise-europe-network.php">The Enterprise Europe Network</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/business-innovation-centres.php">Business innovation centres</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/science-parks.php">Science parks</a></li>
+			<li>Chambers of Commerce</li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/assisted-areas.php">Assisted areas</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/business-clusters.php">Business clusters</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/the-small-business-research-initiative.php">The Small Business Research Initiative</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>British Chambers of Commerce</h3>
+		<p>020 7654 5800</p>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://www.britishchambers.org.uk/find-your-chamber/" target="_blank" rel="noopener noreferrer">Local Chamber of Commerce contact details on the British Chambers of Commerce website</a></li>
+			<li><a href="https://web.archive.org/web/20120605130329/https://web.archive.org/web/20120605130329/http://www.bfora.net/default.asp" target="_blank" rel="noopener noreferrer">Business networks on the bFORA website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/finance-and-grants/finance-options/grants-and-government-support/government-support-for-businesses.php">Government support for businesses</a></li>
+			<li><a href="/create-innovate-and-protect/use-innovation-in-your-business/find-support-for-inventors.php">Find support for inventors</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/work-with-uk-universities-and-colleges.php">Work with UK universities and colleges</a></li>
+			<li><a href="/finance-and-grants/finance-options/grants-and-government-support/innovation-research-and-development-grants.php">Innovation, research and development grants</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/department-for-business-energy-and-industrial-strategy" target="_blank" rel="noopener noreferrer" title="Department for Business, Energy &amp; Industrial Strategy">
+				<img src="/_img/logo_primary_bis.gif" width="160" height="32" alt="" /> Department for Business, Energy &amp; Industrial Strategy
+			</a>
+		</p>
+
+		<img src="/_img/px_growthandinnovation.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
