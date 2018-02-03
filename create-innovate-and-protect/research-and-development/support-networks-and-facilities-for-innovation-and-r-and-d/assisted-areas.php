7d8c4ca1cd55ea6b13661361e7481e2fc268e156
Caroline Hagan <info@blueocto.co.uk>
2018-02-03 16:26:01 +0000

    Completed "create-innovate-and-protect > research-and-development > support-networks-and-facilities-for-innovation-and-r-and-d > Assisted areas" page.

---
+++ create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/assisted-areas.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Assisted areas | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: x__x || /detail?itemId=1084674043&type=RESOURCES -->
+	<meta name="description" content="Support and benefits of locating your business in an assisted area" />
+	<meta name="keywords" content="support networks and facilities for research and development;incubation;knowledge transfer;technology transfer;innovation;Inside UK enterprise;Faraday partnership;IUKE;innovation relay centre;IRC;business innovation centre;BIC;fit for the future;mentoring;R&amp;D;R and D;Development Agencies;RDAs;clusters;business incubators;regional initiatives;cluster concept;business development;science park;research and development;assisted area;grants; grants for small business; small business; business;; Research and development" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Support networks and facilities for innovation and R&amp;D</h1>
+		<h2>Assisted areas</h2>
+		<p>One of the ways in which the government's development agencies help particular areas to develop is by creating <strong>assisted areas</strong>.</p>
+		<p>Assisted areas are those locations where regional aid may be granted under European Union legislation. Businesses based in regions classified as assisted areas may benefit from a grant if they stimulate regional development, urban regeneration or an improvement in employment prospects.</p>
+		<p>There are two main assisted area grant schemes in Britain:</p>
+		<ul>
+			<!-- <li>The Grant for Business Investment (GBI) helps fund new investment projects that lead to long-term improvements in productivity, skills and employment. The scheme is now closed for new applications, except for large exceptional projects and offshore wind equipment. <a href="http://www.berr.gov.uk/whatwedo/regional/investment/page29183.html" target="bgExternalonlinebusinesslinkgovuk" title="Postcode checker and assisted areas map for the RSA scheme">Read about the GBI on the Department for Business, Innovation &amp; Skills (BIS) website</a>.</li> -->
+			<li>Regional Selective Assistance (RSA) Scotland is aimed at encouraging new investment projects, strengthening existing employment and new job creation. <a href="https://www.scottish-enterprise.com/knowledge-hub/articles/insight/changes-to-rsa-funding" target="_blank" rel="noopener noreferrer">Find out about RSA Scotland on the Scottish Enterprise website</a>.</li>
+		</ul>
+
+		<h3>Current Assisted Areas Map</h3>
+		<p>The current Assisted Areas Map came into effect on 13 February 2007, and remains in force until 31 December 2013. You can <a href="/_img/file38642.png" target="_blank" rel="noopener noreferrer">view the Current Assisted Areas map</a>.</p>
+		
+		<h3>Regional Growth Fund</h3>
+		<p>The Regional Growth Fund (RGF) operates across England from 2011 to 2014. It supports projects that create economic growth and long-term employment. RGF aims particularly to help areas that are currently dependent on the public sector.</p>
+		<p><a href="https://www.gov.uk/government/policies/regional-growth-fund" target="_blank" rel="noopener noreferrer">Read about the RGF on the BIS website</a>.</p>
+		
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/chambers-of-commerce.php" title="Chambers of Commerce">Prev</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/business-clusters.php" title="Business clusters">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d.php" title="Support networks and facilities for innovation and R&amp;D">Introduction</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/knowledge-transfer-support.php">Knowledge Transfer Support</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/virtual-support-networks.php">Virtual support networks</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/the-enterprise-europe-network.php">The Enterprise Europe Network</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/business-innovation-centres.php">Business innovation centres</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/science-parks.php">Science parks</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/chambers-of-commerce.php">Chambers of Commerce</a></li>
+			<li>Assisted areas</li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/business-clusters.php">Business clusters</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/support-networks-and-facilities-for-innovation-and-r-and-d/the-small-business-research-initiative.php">The Small Business Research Initiative</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/_img/file38642.png" target="_blank" rel="noopener noreferrer">Current Assisted Areas map</a></li>
+			<!-- <li><a href="http://www.berr.gov.uk/whatwedo/regional/investment/page29183.html" target="bgExternalonlinebusinesslinkgovuk" title="Postcode checker and assisted areas map for the RSA scheme">GBI information on the BIS website</a></li> -->
+			<li><a href="https://www.gov.uk/government/policies/regional-growth-fund" target="_blank" rel="noopener noreferrer">RGF guidance on the BIS website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/create-innovate-and-protect/use-innovation-in-your-business/find-support-for-inventors.php">Find support for inventors</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/work-with-uk-universities-and-colleges.php">Work with UK universities and colleges</a></li>
+			<li><a href="/create-innovate-and-protect/use-innovation-in-your-business/manufacturing-innovation.php" title="How new practices can improve business, what new technologies and thinking offer and where to go for further help">Manufacturing innovation</a></li>
+			<li><a href="/finance-and-grants/finance-options/grants-and-government-support/innovation-research-and-development-grants.php">Innovation, research and development grants</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.gov.uk/government/organisations/department-for-business-energy-and-industrial-strategy" target="_blank" rel="noopener noreferrer" title="Department for Business, Energy &amp; Industrial Strategy">
+				<img src="/_img/logo_primary_bis.gif" width="160" height="32" alt="" /> Department for Business, Energy &amp; Industrial Strategy
+			</a>
+		</p>
+
+		<img src="/_img/px_growthandinnovation.gif" alt="" width="181" height="5" />
+		
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
