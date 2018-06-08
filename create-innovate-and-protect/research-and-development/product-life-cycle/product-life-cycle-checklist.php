66e1c76be651f4a42265cd529b2673a724766590
Caroline Hagan <cmurphydesign@gmail.com>
2018-06-08 22:21:44 +0100

    Completed "create-innovate-and-protect > research-and-development > product-life-cycle > Product life cycle checklist" page.

---
+++ create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycle-checklist.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Product life cycle checklist | Business Link</title>
+	<meta lang="en" content="0.GI1400.06" />
+	<meta lang="en" content="Step-by-step guide on how to make your product as profitable as possible by understanding its product life cycle" />
+	<meta lang="en" content="Product life cycle;Product life cycle;development;introduction;growth;maturity;decline;profitability;development stage;product;pricing;distribution;promotion;introduction stage;Product growth stage;Product maturity stage;saturation;branding;trademarks;decline stage;product harvesting; skimming" />
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Product life cycle</h1>
+		<h2>Product life cycle checklist</h2>
+		<p>By understanding the life cycle of your product or service, you can react to situations that arise within your business. Being organised and prepared for each stage gives you the opportunity to create maximum profitability. This checklist outlines the key points to consider throughout your product or service's life cycle.</p>
+		
+		<h3>Pre-development</h3>
+		<ul>
+			<li>Have you got an initial idea for a product or service that you believe will be profitable?</li>
+			<li>Who could you work with to generate new ideas and embed a research and development process?</li>
+			<li>Consider how each stage of the product life cycle will practically affect the business and its profitability.</li>
+			<li>What services of specialist professionals - eg designers, materials experts, retailers, customers and recycling experts - do you need to employ at different stages of your product development?</li>
+			<li>Are you setting out to create a sustainable product or service with a closed loop life cycle?</li>
+		</ul>
+
+		<h3>Development stage</h3>
+		<ul>
+			<li>Set key performance indicators and plan potential outcomes before developing your product or service and analyse any barriers to success and the risks involved in the whole product development and marketing process.</li>
+			<li>Improve your development processes by getting help or feedback from team members, suppliers and business associates, or customers.</li>
+			<li>Produce prototypes of products and work with designers, suppliers and manufacturers to ensure you have the correct pricing for the materials used.</li>
+			<li>Sort out potential intellectual property issues, such as registering any relevant patents or copyrights.</li>
+		</ul>
+
+		<h3>Introducing your product</h3>
+		<ul>
+			<li>Do you intend to launch with an intense marketing effort? Focusing on establishing a clear brand identity and connecting with the right partners can help you promote your product.</li>
+			<li>Consider setting up consumer tests, or providing samples or trials to key target markets. Customer testimonials can increase your product or service's popularity.</li>
+			<li>Price the product or service as high as you believe you can feasibly sell it, and to reflect the quality level you are providing.</li>
+			<li>Try to limit the product or service to a specific type of consumer - being selective can boost demand.</li>
+		</ul>
+
+		<h3>Product growth - increasing profits</h3>
+		<ul>
+			<li>Maintain quality and consider including additional features or support services for the product or service to grow your market share.</li>
+			<li>Keep your pricing as high as is reasonable to keep demand and profits high.</li>
+			<li>Increase distribution channels to cope with additional demand.</li>
+			<li>Increase your promotion and marketing campaigns and aim them at a wider audience.</li>
+			<li>Consider skimming product prices if your profits are too low.</li>
+		</ul>
+
+		<h3>Maturity stage</h3>
+		<ul>
+			<li>Market saturation means you have to embrace some changes to your product, service or prices.</li>
+			<li>Update or improve the original version of your product or service to lure back existing customers and entice new ones.</li>
+			<li>Consider lowering your prices to attract consumers from rival businesses.</li>
+			<li>Use the difference between your product and others as a means of promotion.</li>
+		</ul>
+
+		<h3>Decline - end of product life</h3>
+		<ul>
+			<li>Maintain the product or service and wait for competitors to withdraw from the market first.</li>
+			<li>Reduce costs and find another use for the product or service.</li>
+			<li>Harvest the product or service before discontinuing it.</li>
+			<li>Discontinue the product or service when your profit disappears, or when you unveil a successor.</li>
+		</ul>
+
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycles-decline-stage.php" title="Product life cycles - decline stage">Prev</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/here-is-how-an-understanding-of-the-product-life-cycle-has-helped-my-business_flash-video.php" title="Here's how an understanding of the product life cycle has helped my business (Flash video)">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle.php" title="Product life cycle">Introduction</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/what-is-a-product-life-cycle.php">What is a product life cycle?</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/development-stage-of-a-product-life-cycle.php">Development stage of a product life cycle</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/introduction-stage-of-a-product-life-cycle.php">Introduction stage of a product life cycle</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycles-growing-and-maturing-a-product.php">Product life cycles - growing and maturing a product</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycles-decline-stage.php">Product life cycles - decline stage</a></li>
+			<li>Product life cycle checklist</li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/here-is-how-an-understanding-of-the-product-life-cycle-has-helped-my-business_flash-video.php">Here's how an understanding of the product life cycle has helped my business (Flash video)</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/create-innovate-and-protect/design-for-business-success/ecodesign-for-goods-and-services.php">Ecodesign for goods and services</a></li>
+		</ul>
+
+		<img src="/_img/px_growthandinnovation.gif" alt="" width="181" height="5" />
+		
+		<p>Developed with:<br />
+			<a href="https://www.designcouncil.org.uk/" target="_blank" rel="noopener noreferrer" title="Design Council">
+				<img src="/_img/Designcouncil_unagreed.gif" width="36" height="36" alt="" /> Design Council
+			</a>
+		</p>
+		
+
+	</div>
+	<!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div>
+<!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
