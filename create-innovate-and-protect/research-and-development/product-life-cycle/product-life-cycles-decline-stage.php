84a62e262be165a33f2147d1d87a54552f80b7c1
Caroline Hagan <cmurphydesign@gmail.com>
2018-06-08 22:20:32 +0100

    Completed "create-innovate-and-protect > research-and-development > product-life-cycle > Product life cycles - decline stage" page.

---
+++ create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycles-decline-stage.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Product life cycles - decline stage | Business Link</title>
+	<meta lang="en" content="0.GI1400.05" />
+	<meta lang="en" content="Techniques to prolong the life of your product and developing an updated or entirely new product or service" />
+	<meta lang="en" content="Product life cycle;Product life cycle;development;introduction;growth;maturity;decline;profitability;development stage;product;pricing;distribution;promotion;introduction stage;Product growth stage;Product maturity stage;saturation;branding;trademarks;decline stage;product harvesting; skimming" />
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Product life cycle</h1>
+		<h2>Product life cycles - decline stage</h2>
+		<p>Due to factors such as technological advances, fashion, innovation or changing consumer tastes, the sales of most products will decline at some stage. You will know when your product reaches the decline stage of its life cycle because you will notice a significant downturn in the revenue it generates.</p>
+		<p>At this stage, you should consider the following options:</p>
+		<ul>
+			<li>maintaining the product in the hope that your other competitors will withdraw their versions before you, which may create an increase in demand again</li>
+			<li>reducing your costs and finding another use for the product - entering into another niche area could increase profits</li>
+			<li>reducing marketing support, 'harvesting' the product, coasting along until profits dry up and then discontinuing the product</li>
+			<li>discontinuing the product when your profit disappears, or when you unveil a successor product</li>
+		</ul>
+
+		<p>Some of these methods can form an 'extension strategy' that prolongs the life of your current product or service. This may give you enough time to create a new version, or an entirely new product. However, this strategy will only temporarily delay a product or service's decline.</p>
+		<p>By understanding the product life cycle of all of your products and services, you can ensure that at least one of your ventures is at growth or maturity stage, while another is in decline. By doing this you can guarantee a regular source of profit for all of your products.</p>
+		
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycles-growing-and-maturing-a-product.php" title="Product life cycles - growing and maturing a product">Prev</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycle-checklist.php" title="Product life cycle checklist">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle.php" title="Product life cycle">Introduction</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/what-is-a-product-life-cycle.php">What is a product life cycle?</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/development-stage-of-a-product-life-cycle.php">Development stage of a product life cycle</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/introduction-stage-of-a-product-life-cycle.php">Introduction stage of a product life cycle</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycles-growing-and-maturing-a-product.php">Product life cycles - growing and maturing a product</a></li>
+			<li>Product life cycles - decline stage</li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycle-checklist.php">Product life cycle checklist</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/here-is-how-an-understanding-of-the-product-life-cycle-has-helped-my-business_flash-video.php">Here's how an understanding of the product life cycle has helped my business (Flash video)</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/create-innovate-and-protect/design-for-business-success/ecodesign-for-goods-and-services.php">Ecodesign for goods and services</a></li>
+		</ul>
+
+		<img src="/_img/px_growthandinnovation.gif" alt="" width="181" height="5" />
+		
+		<p>Developed with:<br />
+			<a href="https://www.designcouncil.org.uk/" target="_blank" rel="noopener noreferrer" title="Design Council">
+				<img src="/_img/Designcouncil_unagreed.gif" width="36" height="36" alt="" /> Design Council
+			</a>
+		</p>
+
+	</div>
+	<!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div>
+<!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
