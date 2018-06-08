6b1f65c75eeb56d2fedbad73aa5da8ba325c66f4
Caroline Hagan <cmurphydesign@gmail.com>
2018-06-08 22:07:53 +0100

    Completed "create-innovate-and-protect > research-and-development > product-life-cycle > What is a product life cycle?" page.

---
+++ create-innovate-and-protect/research-and-development/product-life-cycle/what-is-a-product-life-cycle.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>What is a product life cycle? | Business Link</title>
+	<meta lang="en" content="0.GI1400.01" />
+	<meta lang="en" content="Development of a product or service, and how to increase profits from it, and reduce its environmental impact" />
+	<meta lang="en" content="Product life cycle;Product life cycle;development;introduction;growth;maturity;decline;profitability;development stage;product;pricing;distribution;promotion;introduction stage;Product growth stage;Product maturity stage;saturation;branding;trademarks;decline stage;product harvesting; skimming" />
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Product life cycle</h1>
+		<h2>What is a product life cycle?</h2>
+		<p>All products or services created or provided by your business have a life cycle. This is the period that runs from the initial idea and development of a product to its withdrawal from the market and beyond. Read about how to understand the impact of goods and services across their life cycle in our guide on <a href="/create-innovate-and-protect/design-for-business-success/ecodesign-for-goods-and-services.php">ecodesign for goods and services</a>.</p>
+		
+		<h2>Did you know?</h2>
+		<p>There are several Government or affiliated organisations who offer a range of products and services that can help your business with design, innovation, intellectual property, measurement and standards needs.</p>
+		<p><a href="/_files/NI6-partners-products-and-services_2011.pdf" target="_blank" rel="noopener noreferrer">Download a full list of services provided by these organisations for more information (PDF, 229K)<span> - Opens in a new window</span></a>.</p>
+		<p>Depending on the type of product or service you launch, the length of a product's life cycle will vary substantially. For example, a fashionable item may have a life cycle of only a few months, whereas an everyday item may have one lasting years.</p>
+		<p>A product's marketplace life cycle typically goes through the following main stages:</p>
+		<ul>
+			<li><strong>development</strong> - the product or service is designed and physically created</li>
+			<li><strong>introduction</strong> - the product or service is launched and marketed to a small group of customers</li>
+			<li><strong>growth and maturity</strong> - the product or service is marketed to a wider audience and further iterations are produced</li>
+			<li><strong>decline</strong> - the product or service either comes to its natural end or is re-developed</li>
+		</ul>
+
+		<p>To understand more about the levels of revenue generated at different stages of a product's life cycle, see our example product life cycle curve below.</p>
+		<p>
+			<!-- <a href="http://online.businesslink.gov.uk/bdotg/action/openpopup?itemId=1087241030&amp;r.i=1087240996&amp;r.t=RESOURCES&amp;type=ONEOFFPAGE" title="How your product's life cycle develops, the stages it follows and how your total sales should mirror the stages - Opens in a new window" target="bgPopup"> -->
+				<img alt="Example product life cycle" src="/_img/product_lifecycle.JPG">
+			<!-- </a> -->
+		</p>
+		<p>When developing a new product or service you should assess the following key points:</p>
+		<ul>
+			<li><strong>design</strong> - how efficient the product is, how it can be produced and how it can be easily manufactured</li>
+			<li><strong>materials</strong> - how you can use the best or most cost effective materials to increase profit margins, improve the environmental impact of your product, or reduce financial risk</li>
+			<li><strong>manufacture</strong> - how to make the manufacturing process more efficient, or reduce costs</li>
+			<li><strong>retail</strong> - how to optimise costs incurred through packaging or transportation</li>
+			<li><strong>use</strong> - how you can make the product or service last longer in order to increase customer satisfaction</li>
+			<li><strong>end of life</strong> - how materials or ideas from your service or product can be recycled</li>
+		</ul>
+
+		<p>You need to think about how each of these will affect the various areas of your profitability.</p>
+		
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle.php" title="Product life cycle">Prev</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/development-stage-of-a-product-life-cycle.php" title="Development stage of a product life cycle">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle.php" title="Product life cycle">Introduction</a></li>
+			<li>What is a product life cycle?</li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/development-stage-of-a-product-life-cycle.php">Development stage of a product life cycle</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/introduction-stage-of-a-product-life-cycle.php">Introduction stage of a product life cycle</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycles-growing-and-maturing-a-product.php">Product life cycles - growing and maturing a product</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycles-decline-stage.php">Product life cycles - decline stage</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycle-checklist.php">Product life cycle checklist</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/here-is-how-an-understanding-of-the-product-life-cycle-has-helped-my-business_flash-video.php">Here's how an understanding of the product life cycle has helped my business (Flash video)</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/manage-your-research-design-and-development.php">Manage your research, design and development</a></li>
+			<li><a href="/create-innovate-and-protect/design-for-business-success/ecodesign-for-goods-and-services.php">Ecodesign for goods and services</a></li>
+		</ul>
+
+		<img src="/_img/px_growthandinnovation.gif" alt="" width="181" height="5" />
+		
+		<p>Developed with:<br />
+			<a href="https://www.designcouncil.org.uk/" target="_blank" rel="noopener noreferrer" title="Design Council">
+				<img src="/_img/Designcouncil_unagreed.gif" width="36" height="36" alt="" /> Design Council
+			</a>
+		</p>
+	
+
+	</div>
+	<!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div>
+<!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
