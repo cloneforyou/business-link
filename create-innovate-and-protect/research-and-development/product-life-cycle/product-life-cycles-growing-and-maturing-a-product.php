f7149aa0d0af6f5e14cc7b682e67a1d5407a12c0
Caroline Hagan <cmurphydesign@gmail.com>
2018-06-08 22:18:36 +0100

    Completed "create-innovate-and-protect > research-and-development > product-life-cycle > Product life cycles - growing and maturing a product" page.

---
+++ create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycles-growing-and-maturing-a-product.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Product life cycles - growing and maturing a product | Business Link</title>
+	<meta lang="en" content="0.GI1400.04" />
+	<meta lang="en" content="Increasing profit margins and maturing your product or service's popularity and profitability" />
+	<meta lang="en" content="Product life cycle;Product life cycle;development;introduction;growth;maturity;decline;profitability;development stage;product;pricing;distribution;promotion;introduction stage;Product growth stage;Product maturity stage;saturation;branding;trademarks;decline stage;product harvesting; skimming" />
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		<h1>Product life cycle</h1>
+		<h2>Product life cycles - growing and maturing a product</h2>
+		<p>At this point in your product's life cycle, you should be putting your efforts into increasing your product's market share and creating a brand preference for your customers.</p>
+		<p>This should be a period of rapid growth in both sales and profits for your product or service. Your profits should rise through an increase in output and more competitive pricing.</p>
+		<p>You should also consider the following:</p>
+		<ul>
+			<li>maintaining product quality and including additional features or support services for the product</li>
+			<li>maintaining pricing to increase demand for the product</li>
+			<li>increasing distribution channels to cope with additional demand</li>
+			<li>aiming promotion at a wider audience</li>
+		</ul>
+
+		<p>If your profits are still low, you could consider reducing the price of the product or service to increase the volume of sales.</p>
+		
+		<h3>Product maturity stage</h3>
+		<p>If your product or service makes it to the maturity stage, this should be the longest part of its product life cycle.</p>
+		<p>At this point, the market has often reached saturation as a result of competitors releasing their own version of your product. You will notice your product or service experiences a decreasing rate of sales, which should eventually stabilise.</p>
+		<p>You should try to differentiate your product or service from others that your competitors offer. You can do this by focusing and highlighting any branding, trademarks, or customer testimonials that may give you an advantage.</p>
+		<p>You will probably notice the following trends at the product maturity stage:</p>
+		<ul>
+			<li>product features may have to be enhanced to make your product more appealing than competitors'</li>
+			<li>pricing may need to be lowered due to increased competition</li>
+			<li>distribution becomes more intensive and incentives may need to be offered</li>
+			<li>promotion will focus on the difference between existing products</li>
+		</ul>
+
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/introduction-stage-of-a-product-life-cycle.php" title="Introduction stage of a product life cycle">Prev</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycles-decline-stage.php" title="Product life cycles - decline stage">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle.php" title="Product life cycle">Introduction</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/what-is-a-product-life-cycle.php">What is a product life cycle?</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/development-stage-of-a-product-life-cycle.php">Development stage of a product life cycle</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/introduction-stage-of-a-product-life-cycle.php">Introduction stage of a product life cycle</a></li>
+			<li>Product life cycles - growing and maturing a product</li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycles-decline-stage.php">Product life cycles - decline stage</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycle-checklist.php">Product life cycle checklist</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/here-is-how-an-understanding-of-the-product-life-cycle-has-helped-my-business_flash-video.php">Here's how an understanding of the product life cycle has helped my business (Flash video)</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/create-innovate-and-protect/design-for-business-success/ecodesign-for-goods-and-services.php">Ecodesign for goods and services</a></li>
+		</ul>
+
+		<img src="/_img/px_growthandinnovation.gif" alt="" width="181" height="5" />
+		
+		<p>Developed with:<br />
+			<a href="https://www.designcouncil.org.uk/" target="_blank" rel="noopener noreferrer" title="Design Council">
+				<img src="/_img/Designcouncil_unagreed.gif" width="36" height="36" alt="" /> Design Council
+			</a>
+		</p>
+
+
+	</div>
+	<!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div>
+<!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
