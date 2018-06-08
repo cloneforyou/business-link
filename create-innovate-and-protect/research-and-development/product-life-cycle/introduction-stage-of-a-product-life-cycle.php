b84ae2516a817f3824125790fa46ce013520fc4c
Caroline Hagan <cmurphydesign@gmail.com>
2018-06-08 22:16:40 +0100

    Completed "create-innovate-and-protect > research-and-development > product-life-cycle > Introduction stage of a product life cycle" page.

---
+++ create-innovate-and-protect/research-and-development/product-life-cycle/introduction-stage-of-a-product-life-cycle.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Introduction stage of a product life cycle | Business Link</title>
+	<meta lang="en" content="0.GI1400.03" />
+	<meta lang="en" content="How to introduce your product or service to potential customers, marketing your business, and promotions" />
+	<meta lang="en" content="Product life cycle;Product life cycle;development;introduction;growth;maturity;decline;profitability;development stage;product;pricing;distribution;promotion;introduction stage;Product growth stage;Product maturity stage;saturation;branding;trademarks;decline stage;product harvesting; skimming" />
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Product life cycle</h1>
+		<h2>Introduction stage of a product life cycle</h2>
+		<p>The introduction stage of a product's life cycle is when you can build an awareness of your product or service in certain markets and develop a specific market.</p>
+		<p>Once your product or service is introduced to the market, you should focus on an intense marketing effort to establish a clear identity and promote your product.</p>
+		<p>You should concentrate on building a base for the product at the introduction stage, rather than making an immediate profit.</p>
+		<p>At the introduction stage, you should focus on the following marketing factors:</p>
+		<ul>
+			<li>pricing</li>
+			<li>distribution</li>
+			<li>promotion</li>
+		</ul>
+
+		<h3>Pricing</h3>
+		<p>You should initially start pricing at the highest point you believe it is possible to achieve.</p>
+		<p>You can also consider a skimming price strategy: charging a relatively high price for a short time when a new, innovative, or much-improved product is launched onto a market. The objective with skimming is to skim off customers who are willing to pay more to be one of the first to have a new product. Prices can be lowered later when demand from the early adopters falls.</p>
+		<p>A penetration pricing strategy may work best for businesses entering a new market or building on a relatively small market share. It involves the setting of lower, rather than higher prices in order to achieve a large, if not dominant market share.</p>
+		
+		<h3>Distribution</h3>
+		<p>Your distribution should be selective and limited to a specific type of consumer, until your product is accepted. Also, different distribution models should be considered during different periods of the product life cycle, eg new products for different seasons in a clothes shop.</p>
+		
+		<h3>Promotion</h3>
+		<p>You should try to build brand awareness at an early stage. It is worth working with a brand design or communications agency as you develop a product to establish a strong brand.</p>
+		<p>You can use samples or trial incentives to capture early adopters of the product or service. Introductory promotions can also help convince potential resellers to carry your lines.</p>
+		<p>It is likely that at the introduction stage, your sales will be low until customers become aware of your product or your service's benefits. Due to the high cost of advertising and low sales, you may find that the product is producing negative profits at this stage. However, you should make up for this with increasing revenue generated at the growth and maturity stage of a product life cycle. For more information, see the page in this guide on <a href="/create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycles-growing-and-maturing-a-product.php">product life cycles - growing and maturing a product</a>.</p>
+		
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/development-stage-of-a-product-life-cycle.php" title="Development stage of a product life cycle">Prev</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycles-growing-and-maturing-a-product.php" title="Product life cycles - growing and maturing a product">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle.php" title="Product life cycle">Introduction</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/what-is-a-product-life-cycle.php">What is a product life cycle?</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/development-stage-of-a-product-life-cycle.php">Development stage of a product life cycle</a></li>
+			<li>Introduction stage of a product life cycle</li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycles-growing-and-maturing-a-product.php">Product life cycles - growing and maturing a product</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycles-decline-stage.php">Product life cycles - decline stage</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/product-life-cycle-checklist.php">Product life cycle checklist</a></li>
+			<li><a href="/create-innovate-and-protect/research-and-development/product-life-cycle/here-is-how-an-understanding-of-the-product-life-cycle-has-helped-my-business_flash-video.php">Here's how an understanding of the product life cycle has helped my business (Flash video)</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/advertising-branding-and-design/branding-the-basics.php">Branding: the basics</a></li>
+			<li><a href="/create-innovate-and-protect/design-for-business-success/designing-a-successful-brand.php" title="How to create a successful brand using design and how it can help your business">Designing a successful brand</a></li>
+			<li><a href="/create-innovate-and-protect/design-for-business-success/ecodesign-for-goods-and-services.php">Ecodesign for goods and services</a></li>
+		</ul>
+
+		<img src="/_img/px_growthandinnovation.gif" alt="" width="181" height="5" />
+		
+		<p>Developed with:<br />
+			<a href="https://www.designcouncil.org.uk/" target="_blank" rel="noopener noreferrer" title="Design Council">
+				<img src="/_img/Designcouncil_unagreed.gif" width="36" height="36" alt="" /> Design Council
+			</a>
+		</p>
+
+	</div>
+	<!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div>
+<!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
