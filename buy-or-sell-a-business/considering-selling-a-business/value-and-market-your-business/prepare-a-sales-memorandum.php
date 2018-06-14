82cda8c15f17a83dfa909eba0d0801954afbdf57
Caroline Hagan <cmurphydesign@gmail.com>
2018-06-14 22:39:31 +0100

    Completed "buy-or-sell-a-business > considering-selling-a-business > value-and-market-your-business > Prepare a sales memorandum" page.

---
+++ buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/prepare-a-sales-memorandum.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Prepare a sales memorandum | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+	
+	<!-- ref: detail9822.html || /detail?itemId=1074411273&type=RESOURCES -->
+	<meta name="description" content="A good sales memorandum encourages potential purchasers to find out more" />
+	<meta name="keywords" content="business valuation and marketing a business for sale;Sell business,find buyer,value business,valuation factors, capitalisation future cashflow,multiple of earnings,asset valuation,entry cost,sales memorandum,identify purchasers,approach buyers,shortlist buyers,confidentiality agreement,price negotiation,non disclosure agreement,future earnings" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+<div class="small-12 large-8 large-push-4 columns">
+	
+	<h1>Value and market your business</h1>
+	<h2>Prepare a sales memorandum</h2>
+	<p>The <strong>sales memorandum</strong> is the initial marketing document you use to spark interest in your business. Your corporate finance adviser or business broker, if you have one, helps produce this as a key task in selling your business.</p>
+	<p>The sales memorandum provides potential purchasers with basic information about the company and what the sale might include. It should contain details of:</p>
+	<ul>
+		<li>the preferred sale structure, eg assets and goodwill or share transfer</li>
+		<li>which industry your business is involved in and how long you have been trading</li>
+		<li>key financial figures such as profit, cashflow, value of assets and total debts</li>
+		<li>similar financial figures for previous years and how they have changed</li>
+		<li>number of employees, with job titles, and location of premises</li>
+	</ul>
+
+	<p>If there are any special features of your business, they need to be highlighted. For example: "Our unique patented product is the UK market leader."</p>
+	<p>The sales memorandum should of course be truthful while presenting the business as positively as possible. You should highlight any opportunities for growth or profit improvement. The aim is to interest potential purchasers, so that they want to know more.</p>
+	<p>Pay attention to presentation. Use clear, concise language and present financial information in a way which is visually appealing, ie through charts and tables.</p>
+	<p>The document shouldn't include confidential information such as names of customers or your pricing structure. You can reveal more detailed information later on in the process of marketing your business, when you have had a chance to gauge how serious prospective purchasers are. See the page in this guide on how to <a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/approach-potential-buyers.php">approach potential buyers</a>.</p>
+	
+	<ul>
+		<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/common-methods-of-valuing-a-business.php" title="Common methods of valuing a business">Prev</a></li>
+		<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/identify-likely-buyers.php" title="Identify likely buyers">Next</a></li>
+	</ul>
+
+	<h3>Subjects covered in this guide</h3>
+	<ul>
+		<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business.php" title="Value and market your business">Introduction</a></li>
+		<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/key-factors-affecting-the-value-of-your-business.php">Key factors affecting the value of your business</a></li>
+		<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/common-methods-of-valuing-a-business.php">Common methods of valuing a business</a></li>
+		<li>Prepare a sales memorandum</li>
+		<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/identify-likely-buyers.php">Identify likely buyers</a></li>
+		<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/research-sources-of-potential-buyers.php">Research sources of potential buyers</a></li>
+		<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/approach-potential-buyers.php">Approach potential buyers</a></li>
+		<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/here-is-how-I-marketed-my-business-for-sale.php">Here's how I marketed my business for sale</a></li>
+	</ul>
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+	
+	<h3>Actions</h3>
+	<ul>
+		<li><a href="/_files/cobalt-corporate-finance_selling-a-business_10-08.pdf" title="How to groom your business for sale and negotiate the best deal" target="_blank" rel="noopener noreferrer">Download guidance on selling your business from the Institute of Directors website (PDF, 163K)</a></li>
+	</ul>
+	
+	<h3>Also on this site</h3>
+	<ul>
+		<li><a href="/buy-or-sell-a-business/considering-selling-a-business/preparing-to-sell-your-business.php">Preparing to sell your business</a></li>
+		<li><a href="/buy-or-sell-a-business/selling/complete-the-sale-of-your-business.php">Complete the sale of your business</a></li>
+		<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+		<li><a href="/finance-and-grants/expert-financial-advice/choose-and-work-with-a-solicitor.php">Choose and work with a solicitor</a></li>
+		<li><a href="/buy-or-sell-a-business/considering-selling-a-business/consider-your-exit-strategy-when-starting-up.php">Consider your exit strategy when starting up</a></li>
+	</ul>
+
+	<img src="/_img/px_sellingabusiness.gif" alt="" width="181" height="5" />
+
+	</div>
+	<!-- // .small-12 large-8 large-push-4 columns -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div>
+<!-- // .row -->
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
