68b5bcd2e970499dbc3ea3b3946435d2c726d7a0
Caroline Hagan <cmurphydesign@gmail.com>
2018-06-14 21:58:08 +0100

    Completed "buy-or-sell-a-business > franchising > turn-your-business-into-a-franchise > Marketing your franchise opportunity" page.

---
+++ buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/marketing-your-franchise-opportunity.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Marketing your franchise opportunity | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: detail45f0.html || /detail?itemId=1077061297&type=RESOURCES -->
+	<meta name="description" content="Deciding who you want to franchise with and how to advertise and promote your business to attract them" />
+	<meta name="keywords" content="Turn your business into a franchise;Franchising;franchisee;business format franchise;distribution agreement;selling a licence;franchise fee;royalties;business growth;BFA;british franchise association;franchise your business;franchising advisor;business format;brand;licence agreement;distributor;agent;intellectual property" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+		<h1>Turn your business into a franchise</h1>
+		<h2>Marketing your franchise opportunity</h2>
+		<p>You need to be realistic about how quickly you can grow. Although new franchisees usually provide the capital for their operations, you will be involved helping them set up. For example, you will have to run initial training, or provide hands-on support when they first start trading.</p>
+		<p>This means that you may only be able to cope with one or two new franchisees at a time. You may also prefer new franchisees to be relatively nearby, making it easier to visit them. Many new franchising companies aim for a gradual roll out across the country in this way.</p>
+		<p>Once you have decided what to offer franchises, you need to prepare a prospectus. This tells potential franchisees what you are offering and what they can expect. It should include:</p>
+		<ul>
+			<li>
+				an explanation of your product or service
+			</li>
+			<li>
+				what franchise territories you are offering
+			</li>
+			<li>
+				what the franchise fees are
+			</li>
+			<li>
+				what financial returns the franchisee might expect
+			</li>
+			<li>
+				information about your experience, franchise package, training, timescales, next steps and application form
+			</li>
+		</ul>
+
+		<p>Bear in mind that you are generally competing with other businesses to attract potential franchisees. At the same time, think carefully about what kinds of people you want to apply. They should have the finances needed to invest in the business, and the right management skills and attitude.</p>
+		
+		<h3>Advertising and promotion</h3>
+		<p>To attract franchisees, you may want to advertise in the business opportunities section of a newspaper. Franchising exhibitions can also be a good way of recruiting potential franchisees. <a href="https://www.thebfa.org/events/" target="_blank" rel="noopener noreferrer">Find information about major franchising exhibitions on the British Franchise Association (BFA) website</a>. There are also several websites where you can advertise.</p>
+		<p><a href="https://www.daltonsbusiness.com/selling-a-business" target="_blank" rel="noopener noreferrer">Read about advertising your business on the Daltons Business website</a>.</p>
+		<p><a href="https://uk.businessesforsale.com/uk/sell-a-business" target="_blank" rel="noopener noreferrer">Find information on selling your business on the BusinessesForSale website</a>.</p>
+		<p>Joining the BFA can increase awareness of your franchise. <a href="https://www.thebfa.org/about-franchising-the-bfa/" target="_blank" rel="noopener noreferrer">Find out about the benefits of joining the BFA on the BFA website</a>.</p>
+		
+		<ul>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/franchise-fees-and-royalties.php" title="Franchise fees and royalties">Prev</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/managing-your-franchisees.php" title="Managing your franchisees">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise.php" title="Turn your business into a franchise">Introduction</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/franchising-options.php">Franchising options</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/the-benefits-of-franchising-your-business.php">The benefits of franchising your business</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/is-franchising-right-for-your-business.php">Is franchising right for your business?</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/develop-your-business-format.php">Develop your business format</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/the-franchise-agreement.php">The franchise agreement</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/franchise-fees-and-royalties.php">Franchise fees and royalties</a></li>
+			<li>Marketing your franchise opportunity</li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/managing-your-franchisees.php">Managing your franchisees</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/here-is-how-i-turned-my-existing-business-into-a-franchise.php">Here's how I turned my existing business into a franchise</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>BFA Enquiry Line</h3>
+		<p>01235 820 470</p>
+		
+		<hr />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.thebfa.org/events/" target="_blank" rel="noopener noreferrer">Franchising exhibition listings on the BFA website</a></li>
+			<li><a href="https://www.thebfa.org/affiliate-members/" target="_blank" rel="noopener noreferrer">Franchise consultant listings on the BFA website</a></li>
+			<li><a href="https://www.thebfa.org/about-franchising-the-bfa/" target="_blank" rel="noopener noreferrer">Membership benefits explained on the BFA website</a></li>
+			<li><a href="https://www.daltonsbusiness.com/selling-a-business" target="_blank" rel="noopener noreferrer">Advertise your business on the Daltons Business website</a></li>
+			<li><a href="https://uk.businessesforsale.com/uk/sell-a-business" target="_blank" rel="noopener noreferrer">Sell your business on the BusinessesForSale website</a></li>
+			<li><a href="https://www.thebfa.org/franchise-my-business/recruiting-franchisees/" target="_blank" rel="noopener noreferrer">Recruitment advice on the BFA website</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/create-an-action-plan-to-successfully-grow-your-business.php">Use our interactive tool to find your strengths and identify the areas you need to work on if you want your business to grow</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/advertising-branding-and-design/advertising-the-basics.php">Advertising: the basics</a></li>
+			<li><a href="/sales-and-marketing/marketing/trade-shows-and-exhibitions.php">Trade shows and exhibitions</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/how-to-grow-your-franchise.php">How to grow your franchise</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="http://www.whichfranchise.com/" target="_blank" rel="noopener noreferrer" title="">
+				<img src="/_img/whichfranchise_logo_sm.gif" width="105" height="35" alt="" /> whichfranchise.com
+			</a>
+		</p>
+
+		<img src="/_img/px_sellingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
