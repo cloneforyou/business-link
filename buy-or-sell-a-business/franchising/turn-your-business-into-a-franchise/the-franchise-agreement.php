83aa667ed57b9c47c6351983d101138f32b622f7
Caroline Hagan <cmurphydesign@gmail.com>
2018-06-14 21:44:17 +0100

    Completed "buy-or-sell-a-business > franchising > turn-your-business-into-a-franchise > The franchise agreement" page.

---
+++ buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/the-franchise-agreement.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The franchise agreement | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: detail8763.html || /detail?itemId=1077061251&type=RESOURCES -->
+	<meta name="description" content="A clear contract sets out what rights and obligations you and the franchisee have" />
+	<meta name="keywords" content="Turn your business into a franchise;Franchising;franchisee;business format franchise;distribution agreement;selling a licence;franchise fee;royalties;business growth;BFA;british franchise association;franchise your business;franchising advisor;business format;brand;licence agreement;distributor;agent;intellectual property" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+		<h1>Turn your business into a franchise</h1>
+		<h2>The franchise agreement</h2>
+		<p>When you franchise your business, you - the franchisor - enter into a legal agreement with the franchisee. A clear, written contract is essential.</p>
+		<p>This franchise agreement sets out what rights and obligations you each have. Key issues include:</p>
+		<ul>
+			<li>what territory the franchisee can operate in and whether they have exclusive rights in that territory</li>
+			<li>what rights the franchisee has to use your intellectual property - eg your trade marks</li>
+			<li>what restrictions there are on what the franchisee can do</li>
+			<li>what fees the franchisee will pay</li>
+			<li>how you will help the franchisee initially - eg training</li>
+			<li>what continuing support you will provide - eg national marketing campaigns or administrative support</li>
+			<li>how long the franchise lasts and what happens at the end of the term</li>
+			<li>what happens if either of you wants to bring the agreement to an end or sell your business</li>
+		</ul>
+
+		<p>Agreements tend to be in favour of the franchisor. However, if the agreement is too one-sided in the franchisor's favour, it will be difficult to attract potential franchisees. For more information on what franchisees look for in a franchise agreement, see our guide on how to <a href="/buy-or-sell-a-business/franchising/buy-a-franchise.php">buy a franchise</a>. </p>
+		<p>Franchise agreements involve complex legal issues.</p>
+		<p>You should take advice from a lawyer with franchising expertise. <a href="https://www.thebfa.org/affiliate-members/" target="_blank" rel="noopener noreferrer">Find lawyers with franchising expertise on the British Franchise Association (BFA) website</a>.</p>
+
+		<ul>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/develop-your-business-format.php" title="Develop your business format">Prev</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/franchise-fees-and-royalties.php" title="Franchise fees and royalties">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise.php" title="Turn your business into a franchise">Introduction</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/franchising-options.php">Franchising options</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/the-benefits-of-franchising-your-business.php">The benefits of franchising your business</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/is-franchising-right-for-your-business.php">Is franchising right for your business?</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/develop-your-business-format.php">Develop your business format</a></li>
+			<li>The franchise agreement</li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/franchise-fees-and-royalties.php">Franchise fees and royalties</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/marketing-your-franchise-opportunity.php">Marketing your franchise opportunity</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/managing-your-franchisees.php">Managing your franchisees</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/here-is-how-i-turned-my-existing-business-into-a-franchise.php">Here's how I turned my existing business into a franchise</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>BFA Enquiry Line</h3>
+		<p>01235 820 470</p>
+
+		<hr />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="http://solicitors.lawsociety.org.uk/" target="_blank" rel="noopener noreferrer">Solicitor listings on the Law Society website</a></li>
+			<li><a href="https://www.thebfa.org/affiliate-members/" target="_blank" rel="noopener noreferrer">Franchise lawyer listings on the BFA website</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/create-an-action-plan-to-successfully-grow-your-business.php">Use our interactive tool to find your strengths and identify the areas you need to work on if you want your business to grow</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/buy-or-sell-a-business/franchising/buy-a-franchise.php">Buy a franchise</a></li>
+			<li><a href="/finance-and-grants/expert-financial-advice/choose-and-work-with-a-solicitor.php">Choose and work with a solicitor</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/how-to-grow-your-franchise.php">How to grow your franchise</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="http://www.whichfranchise.com/" target="_blank" rel="noopener noreferrer" title="">
+				<img src="../../../_img/whichfranchise_logo_sm.gif" width="105" height="35" alt="" /> whichfranchise.com
+			</a>
+		</p>
+
+		<img src="/_img/px_sellingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
