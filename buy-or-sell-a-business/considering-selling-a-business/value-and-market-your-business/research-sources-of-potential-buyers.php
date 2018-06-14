fe3e9b182a7cc65a8296e39f2b8b1889f9743ad4
Caroline Hagan <cmurphydesign@gmail.com>
2018-06-14 22:44:43 +0100

    Completed "buy-or-sell-a-business > considering-selling-a-business > value-and-market-your-business > Research sources of potential buyers" page.

---
+++ buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/research-sources-of-potential-buyers.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Research sources of potential buyers | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+	<!-- ref: detailb1bd.html || /detail?itemId=1074411480&type=RESOURCES -->
+	<meta name="description" content="Your own trade knowledge, and your adviser, should help you produce a shortlist of potential buyers" />
+	<meta name="keywords" content="business valuation and marketing a business for sale;Sell business,find buyer,value business,valuation factors, capitalisation future cashflow,multiple of earnings,asset valuation,entry cost,sales memorandum,identify purchasers,approach buyers,shortlist buyers,confidentiality agreement,price negotiation,non disclosure agreement,future earnings" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+			
+		<h1>Value and market your business</h1>
+		<h2>Research sources of potential buyers</h2>
+		<p>Potential purchasers of your business are likely to come from a number of key sources:</p>
+		<ul>
+			<li>Your own knowledge of your sector should mean you know whether any of your competitors, customers or suppliers would be interested in buying your business.</li>
+			<li>Trade magazines, business directories and the financial press may provide you with ideas.</li>
+			<li>Your existing management team may be interested - but you'll need to be sure they can raise the necessary finance.</li>
+			<li>Your corporate finance adviser or business broker should be able to help you identify possible buyers, both in the UK and abroad. They will have access to databases of prospective purchasers as well as an extensive network of contacts. For example, they may be able to advise you on equity investors or management buy-in teams who might be interested. They will also be able to help you assess whether buyers are capable of funding a purchase.</li>
+			<li>For certain types of business it can be worth advertising for potential buyers. For example, to find buyers for a shop, hotel, restaurant or pub you can advertise in publications such as Daltons Weekly or use online business for sale listings. <a href="https://uk.businessesforsale.com/uk/sell-a-business" target="_blank" rel="noopener noreferrer">Find information on selling your business on the BusinessesForSale website</a>.</li>
+		</ul>
+
+		<p>Together with your adviser, you should draw up a shortlist of potential purchasers to approach. It can be a good idea to split this into two - a list of favoured prospective buyers you'll approach first and a back-up list if this doesn't produce results.</p>
+		<p>It's important that you don't focus all your efforts on a single prospect. If the buyer knows they are the sole interested party, they can call all the shots.</p>
+		<p>For information on the advisers you'll need when selling your business, see our guide on <a href="/buy-or-sell-a-business/considering-selling-a-business/preparing-to-sell-your-business.php">preparing to sell your business</a>.</p>
+		
+		<ul>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/identify-likely-buyers.php" title="Identify likely buyers">Prev</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/approach-potential-buyers.php" title="Approach potential buyers">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business.php" title="Value and market your business">Introduction</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/key-factors-affecting-the-value-of-your-business.php">Key factors affecting the value of your business</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/common-methods-of-valuing-a-business.php">Common methods of valuing a business</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/prepare-a-sales-memorandum.php">Prepare a sales memorandum</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/identify-likely-buyers.php">Identify likely buyers</a></li>
+			<li>Research sources of potential buyers</li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/approach-potential-buyers.php">Approach potential buyers</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/here-is-how-I-marketed-my-business-for-sale.php">Here's how I marketed my business for sale</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.daltonsbusiness.com/selling-a-business" target="_blank" rel="noopener noreferrer">Advertise your business on the Daltons Business website</a></li>
+			<li><a href="https://uk.businessesforsale.com/uk/sell-a-business" target="_blank" rel="noopener noreferrer">Sell your business on the BusinessesForSale website</a></li>
+			<li><a href="https://www.bvca.co.uk/Member-Directory" target="_blank" rel="noopener noreferrer">Corporate finance specialists listed on the British Private Equity &amp; Venture Capital Association website (registration required)</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/preparing-to-sell-your-business.php">Preparing to sell your business</a></li>
+			<li><a href="/buy-or-sell-a-business/selling/complete-the-sale-of-your-business.php">Complete the sale of your business</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/consider-your-exit-strategy-when-starting-up.php">Consider your exit strategy when starting up</a></li>
+		</ul>
+
+		<img src="/_img/px_sellingabusiness.gif" alt="" width="181" height="5" />
+
+	</div>
+	<!-- // .small-12 large-8 large-push-4 columns -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div>
+<!-- // .row -->
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
