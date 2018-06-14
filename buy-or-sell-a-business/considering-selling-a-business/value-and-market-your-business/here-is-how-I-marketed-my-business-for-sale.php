1171cf223f392d7c58ad71b05f4fe7edc17fa51e
Caroline Hagan <cmurphydesign@gmail.com>
2018-06-14 22:58:04 +0100

    Completed "buy-or-sell-a-business > considering-selling-a-business > value-and-market-your-business > Here's how I marketed my business for sale" page.

---
+++ buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/here-is-how-I-marketed-my-business-for-sale.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Here's how I marketed my business for sale | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+	
+	<!-- ref: detail2fb5.html || /detail?itemId=1075391757&type=CASE STUDIES -->
+	<meta name="descrption" content="In this case study, the founder of Premier Colour Limited explains how the business was marketed for sale" />
+	<meta name="keywords" content="business valuation and marketing a business for sale;Sell business,find buyer,value business,valuation factors, capitalisation future cashflow,multiple of earnings,asset valuation,entry cost,sales memorandum,identify purchasers,approach buyers,shortlist buyers,confidentiality agreement,price negotiation,non disclosure agreement,future earnings" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+		
+		<h1>Here's how I marketed my business for sale</h1>
+		<h2>Here's how I marketed my business for sale</h2>
+		<p>John Hunter set up print supply company Premier Colour Limited in 1995. Nine years later, having built the business to a point where it turned over &pound;800,000 a year and employed eight staff, he decided it was time to sell.</p>
+		<p><strong>What I did</strong></p>
+		
+		<h3>Draw up a marketing document</h3>
+		<p>"I met an agent through a local business club and he talked me through drawing up a marketing document. We did it in the back-to-front way of agreeing what we were trying to achieve and writing the summary first.</p>
+		<p>"Only then did we draw up the contents, including headings like history and company ethos, business strategy, awards and achievements, customer profile and management organisation. Appendices formed a large part of the document, with financial reports and information for the previous three years.</p>
+		<p>"We geared everything in it to the particular nature of Premier Colour. We stressed how valuable the business' database was and, importantly, included customer quotes. As a brokerage our customer list was vital to the sale and the customer comments added to our saleability."</p>
+		
+		<h3>Identify potential purchasers</h3>
+		<p>"It was always likely we'd sell to someone bigger who could find synergies and savings by bolting two businesses together. With my industry knowledge I drew up two lists of suitors.</p>
+		<p>"List number one was people in the industry that I knew - from reading the trade press or going to my trade association or because they supplied us - to be proactive players. They were all from a radius of about 50 miles as printing is pretty parochial.</p>
+		<p>"List number two was drawn from going through Yellow Pages and trade magazines. These were print-related groups I knew less about."</p>
+		
+		<h3>Approach potential purchasers</h3>
+		<p>"My agent wrote to those on the first list. He only identified my business by turnover though, describing the profitability without giving figures. To actually understand what made the company tick and what made it valuable they needed more detailed information.</p>
+		<p>"It was enough to elicit interest though and five people signed confidentiality agreements to be sent the marketing document.</p>
+		<p>"As it turned out, by coincidence, at the same time we were marketing to list number one a company actually approached us. Although that was who we ended up selling to, I remain convinced that going through the marketing process still made our sale competitive and increased the eventual price we got."</p>
+		<p><strong>What I'd do differently</strong></p>
+		<p>"As a brokerage, the value of my business was not from an asset register but from goodwill. That's difficult to put a figure on. At first, I went to four national sales agents and it was amazing the different figures I got. The difference between the top and bottom end was five times.</p>
+		<p>"I wouldn't go to the large agents again. The highest valuer I ruled out straight away as their figure wasn't realistic. The lowest valuer just didn't understand our business. I think it's best where possible to stick to local agents who understand your industry sector."</p>
+		
+		<ul>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/approach-potential-buyers.php" title="Approach potential buyers">Prev</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business.php" title="Value and market your business">Introduction</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/key-factors-affecting-the-value-of-your-business.php">Key factors affecting the value of your business</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/common-methods-of-valuing-a-business.php">Common methods of valuing a business</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/prepare-a-sales-memorandum.php">Prepare a sales memorandum</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/identify-likely-buyers.php">Identify likely buyers</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/research-sources-of-potential-buyers.php">Research sources of potential buyers</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/approach-potential-buyers.php">Approach potential buyers</a></li>
+			<li>Here's how I marketed my business for sale</li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<div class="media-object stack-for-small">
+			<div class="media-object-section">
+				<div class="thumbnail">
+					<img src="/_img/JohnHunter_format.jpg" width="181" height="121" alt="John Hunter, Premier Colour Limited" />
+				</div>
+			</div>
+			<div class="media-object-section">
+				<h3>
+					John Hunter<br />
+					Premier Colour Limited<br />
+					John's top tips:
+				</h3>
+				<ul>
+					<li>"Use an agent who understands your sector."</li>
+					<li>"Work with someone you can trust."</li>
+					<li>"Be honest in your marketing document. If you&rsquo;re not, you will be found out."</li>
+				</ul>
+			</div>
+		</div>
+
+		<h3>More case studies</h3>
+		<ul>
+			<li><a href="/tax-payroll-and-company-information/selling-or-closing-your-business/check-tax-and-legal-issues-when-selling-a-business.php">Here's how I handled the sale of my business</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/preparing-to-sell-your-business/here-is-how-i-prepared-my-business-for-sale.php">Here's how I prepared my business for sale</a></li>
+		</ul>
+		
+		<p><strong><a href="/find-a-case-study.php">Find a case study</a></strong></p>
+
+	</div>
+	<!-- // .small-12 large-8 large-push-4 columns -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div>
+<!-- // .row -->
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
