062354b015244e656068863ab5fad4ffdf401720
Caroline Hagan <cmurphydesign@gmail.com>
2018-06-14 21:48:07 +0100

    Completed "buy-or-sell-a-business > franchising > turn-your-business-into-a-franchise > Franchise fees and royalties" page.

---
+++ buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/franchise-fees-and-royalties.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Franchise fees and royalties | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: detail2057.html || /detail?itemId=1077061265&type=RESOURCES -->
+	<meta name="description" content="Initial fees help recover development and marketing costs while continuing royalties or mark-ups provide your profit" />
+	<meta name="keywords" content="Turn your business into a franchise;Franchising;franchisee;business format franchise;distribution agreement;selling a licence;franchise fee;royalties;business growth;BFA;british franchise association;franchise your business;franchising advisor;business format;brand;licence agreement;distributor;agent;intellectual property" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+		<h1>Turn your business into a franchise</h1>
+		<h2>Franchise fees and royalties</h2>
+		<p>When you franchise your business, you make money from fees the franchisee pays you.</p>
+		<p>You charge an initial fee for the purchase of the franchise. This fee contributes towards the costs you have in developing and marketing the franchise concept.</p>
+		<p>Most companies charge an initial fee. Fees vary but usually they are at least &pound;15,000. The fee is based on the cost of setting the franchisee up in business. This initial fee enables the franchisee to invest a larger percentage of their capital in setting up and developing the business.</p>
+		
+		<h3>Continuing fees</h3>
+		<p>Your profits come from the continuing fees that franchisees pay you. Typically, they pay a management service fee based on turnover. If you are supplying them with products or other supplies, you can also profit from the mark-up on the prices at which you sell to them. Both these methods give you a common interest: the more the franchisee sells, the more profits you both make.</p>
+		<p>Setting the right level of continuing fees requires careful judgement. If the fees are too high, the franchise will not be attractive to new franchisees and existing franchisees might struggle to keep going. Fees should be based on the services provided by the franchisor and the costs incurred in providing them.</p>
+		<p>Advertising and marketing fees are also usually charged as a fixed percentage of the sales achieved by franchisees. These can range from 1 per cent to as much as 5 per cent of sales. These fees are used to fund the regional and national marketing, advertising and brand awareness initiatives that you carry out on behalf of your franchisees.</p>
+		<p>Your bank may have specialist franchising advisers who can work with you on your franchising business plan and the fees you plan to charge. You may also want to take advice from your accountant or a specialist consultant.</p>
+		
+		<ul>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/the-franchise-agreement.php" title="The franchise agreement">Prev</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/marketing-your-franchise-opportunity.php" title="Marketing your franchise opportunity">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise.php" title="Turn your business into a franchise">Introduction</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/franchising-options.php">Franchising options</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/the-benefits-of-franchising-your-business.php">The benefits of franchising your business</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/is-franchising-right-for-your-business.php">Is franchising right for your business?</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/develop-your-business-format.php">Develop your business format</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/the-franchise-agreement.php">The franchise agreement</a></li>
+			<li>Franchise fees and royalties</li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/marketing-your-franchise-opportunity.php">Marketing your franchise opportunity</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/managing-your-franchisees.php">Managing your franchisees</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/here-is-how-i-turned-my-existing-business-into-a-franchise.php">Here's how I turned my existing business into a franchise</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>BFA Enquiry Line</h3>
+		<p>01235 820 470</p>
+		
+		<hr />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.thebfa.org/members/" target="_blank" rel="noopener noreferrer">Franchising adviser listings on the British Franchise Association website</a></li>
+			<li><a href="http://www.accaglobal.com/uk/en/member.html" target="_blank" rel="noopener noreferrer">Chartered accountant search on the Association of Certified Chartered Accountants website</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/create-an-action-plan-to-successfully-grow-your-business.php">Use our interactive tool to find your strengths and identify the areas you need to work on if you want your business to grow</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/how-to-grow-your-franchise.php">How to grow your franchise</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="http://www.whichfranchise.com/" target="_blank" rel="noopener noreferrer" title="">
+				<img src="../../../_img/whichfranchise_logo_sm.gif" width="105" height="35" alt="" /> whichfranchise.com
+			</a>
+		</p>
+
+		<img src="/_img/px_sellingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
