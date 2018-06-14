1bc5911a23d02a8cc99a2412e44eddfd05af48d3
Caroline Hagan <cmurphydesign@gmail.com>
2018-06-14 22:42:02 +0100

    Completed "buy-or-sell-a-business > considering-selling-a-business > value-and-market-your-business > Identify likely buyers" page.

---
+++ buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/identify-likely-buyers.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Identify likely buyers | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+	<!-- ref: detail819e.html || /detail?itemId=1074411356&type=RESOURCES -->
+	<meta name="descrption" content="Key points to look out for when trying to identify potential buyers for your business" />
+	<meta name="keywords" content="business valuation and marketing a business for sale;Sell business,find buyer,value business,valuation factors, capitalisation future cashflow,multiple of earnings,asset valuation,entry cost,sales memorandum,identify purchasers,approach buyers,shortlist buyers,confidentiality agreement,price negotiation,non disclosure agreement,future earnings" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+			
+		<h1>Value and market your business</h1>
+		<h2>Identify likely buyers</h2>
+		<p>Target those who you think would be prepared to pay a good price for your business because:</p>
+		<ul>
+			<li>you're the market leader in a particular segment - a competitor might want <strong>access to your customers</strong> so they can cross-sell their products</li>
+			<li>you may have a product that fills a gap in their <strong>product range</strong></li>
+			<li>they might be able to use your <strong>distribution</strong> channels to sell their product range</li>
+			<li>they might benefit from <strong>economies of scale</strong> in areas such as purchasing, production and sales</li>
+			<li>they might want access to your <strong>people and skills</strong></li>
+			<li>you can show an excellent <strong>potential for growth</strong></li>
+			<li>an <strong>overseas company</strong> might want to expand into the UK market</li>
+		</ul>
+		
+		<p>Make sure you identify every likely candidate for your business - including private individuals and investment groups. At the same time, you want to be sure that any potential purchaser is serious. Crucially, you should satisfy yourself that they would be able to afford to buy your business. Your advisers should be able to help you establish this.</p>
+		<p>For more information on finding buyers, see the page in this guide on <a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/research-sources-of-potential-buyers.php">research sources of potential buyers</a>.</p>
+		<p><a href="/_files/cobalt-corporate-finance_valuing-a-business_10-08.pdf" title="This briefing is aimed at anyone who needs to know how businesses are valued" target="_blank" rel="noopener noreferrer">Download a factsheet about valuing your business from the Institute of Directors (IoD) website (PDF, 156K)</a>.</p>
+		<p>You can also <a href="/_files/cobalt-corporate-finance_selling-a-business_10-08.pdf" title="How to groom your business for sale and negotiate the best deal" target="_blank" rel="noopener noreferrer">download a factsheet about selling your business from the IoD website (PDF, 163K)</a>.</p>
+		
+		<ul>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/prepare-a-sales-memorandum.php" title="Prepare a sales memorandum">Prev</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/research-sources-of-potential-buyers.php" title="Research sources of potential buyers">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business.php" title="Value and market your business">Introduction</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/key-factors-affecting-the-value-of-your-business.php">Key factors affecting the value of your business</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/common-methods-of-valuing-a-business.php">Common methods of valuing a business</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/prepare-a-sales-memorandum.php">Prepare a sales memorandum</a></li>
+			<li>Identify likely buyers</li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/research-sources-of-potential-buyers.php">Research sources of potential buyers</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/approach-potential-buyers.php">Approach potential buyers</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/here-is-how-I-marketed-my-business-for-sale.php">Here's how I marketed my business for sale</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="/_files/cobalt-corporate-finance_valuing-a-business_10-08.pdf" title="This briefing is aimed at anyone who needs to know how businesses are valued" target="_blank" rel="noopener noreferrer">Download a business valuation factsheet from the IoD website (PDF, 156K)</a></li>
+			<li><a href="/_files/cobalt-corporate-finance_selling-a-business_10-08.pdf" title="How to groom your business for sale and negotiate the best deal" target="_blank" rel="noopener noreferrer">Download the selling your business factsheet from the IoD website (PDF, 163K)</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/preparing-to-sell-your-business.php">Preparing to sell your business</a></li>
+			<li><a href="/buy-or-sell-a-business/selling/complete-the-sale-of-your-business.php">Complete the sale of your business</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/consider-your-exit-strategy-when-starting-up.php">Consider your exit strategy when starting up</a></li>
+		</ul>
+
+		<img src="/_img/px_sellingabusiness.gif" alt="" width="181" height="5" />
+
+	</div>
+	<!-- // .small-12 large-8 large-push-4 columns -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div>
+<!-- // .row -->
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
