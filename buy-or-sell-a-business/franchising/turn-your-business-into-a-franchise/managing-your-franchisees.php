5100110f4d8f7d5d78347b8aeb8da9e6de0c0998
Caroline Hagan <cmurphydesign@gmail.com>
2018-06-14 22:02:31 +0100

    Completed "buy-or-sell-a-business > franchising > turn-your-business-into-a-franchise > Managing your franchisees" page.

---
+++ buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/managing-your-franchisees.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Managing your franchisees | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: detail07b5.html || /detail?itemId=1077061355&type=RESOURCES -->
+	<meta name="description" content="Supporting and motivating your franchisees to grow sales and increase franchising fees" />
+	<meta name="keywords" content="Turn your business into a franchise;Franchising;franchisee;business format franchise;distribution agreement;selling a licence;franchise fee;royalties;business growth;BFA;british franchise association;franchise your business;franchising advisor;business format;brand;licence agreement;distributor;agent;intellectual property" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+	
+		<h1>Turn your business into a franchise</h1>
+		<h2>Managing your franchisees</h2>
+		<p>Franchising your business isn't about selling franchises and then forgetting about them. You have a continuing relationship with your franchisees.</p>
+		<p>As part of this, you must provide the support detailed in the franchise agreement. This can include:</p>
+		<ul>
+			<li>
+				helping them with initial set up of their franchise
+			</li>
+			<li>
+				providing training in how to run the business
+			</li>
+			<li>
+				running national promotional campaigns to increase sales
+			</li>
+			<li>
+				helping them manage their business effectively
+			</li>
+			<li>
+				innovating to keep your product or service ahead of the competition
+			</li>
+			<li>
+				monitoring their performance and processes against the standards contained in the operations manual
+			</li>
+		</ul>
+
+		<p>Doing all this isn't just a matter of fulfilling your contractual obligations. By supporting franchisees, you help them succeed - and increase the fees you receive.</p>
+		<p>Although it isn't included in the agreement, you also have a role to play in <strong>motivating franchisees</strong>. New franchisees can find the early months difficult and may become discouraged. Successful franchisees may reach their own comfort level and stop trying so hard to increase sales. Again, by motivating them you help yourself.</p>
+		<p>At the same time, you need to ensure that they are running their businesses the way they should. One of the keys to building a successful brand is <strong>consistency</strong>. If different franchisees run their businesses differently, your brand can suffer. Your operations manual should give clear information on the correct procedures. For more information, see the page in this guide on how to <a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/develop-your-business-format.php">develop your business format</a>.</p>
+		<p>Last but not least, you need efficient administration to ensure that you collect the right franchise fees.</p>
+		
+		<ul>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/marketing-your-franchise-opportunity.php" title="Marketing your franchise opportunity">Prev</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/here-is-how-i-turned-my-existing-business-into-a-franchise.php" title="Here's how I turned my existing business into a franchise">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise.php" title="Turn your business into a franchise">Introduction</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/franchising-options.php">Franchising options</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/the-benefits-of-franchising-your-business.php">The benefits of franchising your business</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/is-franchising-right-for-your-business.php">Is franchising right for your business?</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/develop-your-business-format.php">Develop your business format</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/the-franchise-agreement.php">The franchise agreement</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/franchise-fees-and-royalties.php">Franchise fees and royalties</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/marketing-your-franchise-opportunity.php">Marketing your franchise opportunity</a></li>
+			<li>Managing your franchisees</li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/here-is-how-i-turned-my-existing-business-into-a-franchise.php">Here's how I turned my existing business into a franchise</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>BFA Enquiry Line</h3>
+		<p>01235 820 470</p>
+		
+		<hr />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.thebfa.org/" target="_blank" rel="noopener noreferrer">Franchising advice on the British Franchise Association website</a></li>
+			<li><a href="http://www.learndirect.com/ilm-level-3-diploma-leadership-management" target="_blank" rel="noopener noreferrer">Leadership and management courses on the learndirect business website</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/create-an-action-plan-to-successfully-grow-your-business.php">Use our interactive tool to find your strengths and identify the areas you need to work on if you want your business to grow</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/how-to-grow-your-franchise.php">How to grow your franchise</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="http://www.whichfranchise.com/" target="_blank" rel="noopener noreferrer" title="">
+				<img src="/_img/whichfranchise_logo_sm.gif" width="105" height="35" alt="" /> whichfranchise.com
+			</a>
+		</p>
+
+		<img src="/_img/px_sellingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
