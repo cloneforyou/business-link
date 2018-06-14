049a66ce75dce0b5fceceb82b174c1f6726849f9
Caroline Hagan <cmurphydesign@gmail.com>
2018-06-14 21:37:15 +0100

    Completed "buy-or-sell-a-business > franchising > turn-your-business-into-a-franchise > Is franchising right for your business?" page.

---
+++ buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/is-franchising-right-for-your-business.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Is franchising right for your business? | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: detail4f6f.html || /detail?itemId=1077060959&type=RESOURCES -->
+	<meta name="description" content="Offering franchisees a successful business model, and having the right resources and skills" />
+	<meta name="keywords" content="Turn your business into a franchise;Franchising;franchisee;business format franchise;distribution agreement;selling a licence;franchise fee;royalties;business growth;BFA;british franchise association;franchise your business;franchising advisor;business format;brand;licence agreement;distributor;agent;intellectual property" />
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+	
+		<h1>Turn your business into a franchise</h1>
+		<h2>Is franchising right for your business?</h2>
+		<p>Many businesses have used franchising successfully, including well-known names like McDonalds, Prontaprint, Domino's, Molly Maid, Thorntons, Snap-on, Rosemary Conley and Select Appointments. However, franchising doesn't suit every business.</p>
+		<p>To start with, your business needs to be successful. Nobody will want to buy the right to franchise a business that doesn't make money. A franchised business needs to be profitable enough to make money for both the franchisee and you.</p>
+		<p>More broadly, your business needs to be one that can be replicated in different locations by your franchisees. Businesses that need high skills levels or professional qualifications can be more difficult to franchise but all the major optician chains have succeeded in doing so.</p>
+		<p>At the same time, you need to offer your franchisees something that makes it worth their while paying you, instead of simply setting up their business independently. For example, you might have a recognised brand name, provide equipment or supplies they need, or help with training and marketing support. For more information, see the page in this guide on how to <a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/develop-your-business-format.php">develop your business format</a>.</p>
+		<p>Having a well-organised and well-run franchise helps you recruit franchisees and is a strong incentive for franchisees to remain part of the franchise at the end of the initial franchise period.</p>
+		<p>For more information on how franchisees assess opportunities, see our guide on how to <a href="/buy-or-sell-a-business/franchising/buy-a-franchise.php">buy a franchise</a>.</p>
+		
+		<h3>Your resources</h3>
+		<p>You also need to think about the demands franchising places on you. You need to invest in developing and marketing the format. If you have limited financial resources, or are already working flat out running your business, you may not be able to do this.</p>
+		<p>Finally, you need to have the right skills and attitude to make franchising a success. You need to be able to sell your concept to potential franchisees, and to work with and control them. Rather than dealing directly with customers yourself, you profit by helping your franchisees to be successful.</p>
+		<p>For more information, <a href="https://www.thebfa.org/" target="_blank" rel="noopener noreferrer">read about businesses that suit franchising on the British Franchise Association (BFA) website</a>.</p>
+		
+		<ul>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/the-benefits-of-franchising-your-business.php" title="The benefits of franchising your business">Prev</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/develop-your-business-format.php" title="Develop your business format">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise.php" title="Turn your business into a franchise">Introduction</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/franchising-options.php">Franchising options</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/the-benefits-of-franchising-your-business.php">The benefits of franchising your business</a></li>
+			<li>Is franchising right for your business?</li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/develop-your-business-format.php">Develop your business format</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/the-franchise-agreement.php">The franchise agreement</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/franchise-fees-and-royalties.php">Franchise fees and royalties</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/marketing-your-franchise-opportunity.php">Marketing your franchise opportunity</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/managing-your-franchisees.php">Managing your franchisees</a></li>
+			<li><a href="/buy-or-sell-a-business/franchising/turn-your-business-into-a-franchise/here-is-how-i-turned-my-existing-business-into-a-franchise.php">Here's how I turned my existing business into a franchise</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>BFA Enquiry Line</h3>
+		<p>01235 820 470</p>
+		
+		<hr />
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.thebfa.org/" target="_blank" rel="noopener noreferrer">Suitable franchise business characteristics on the BFA website</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/create-an-action-plan-to-successfully-grow-your-business.php">Use our interactive tool to find your strengths and identify the areas you need to work on if you want your business to grow</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/buy-or-sell-a-business/franchising/buy-a-franchise.php">Buy a franchise</a></li>
+			<li><a href="/grow-your-business/setting-a-vision-for-growth/how-to-grow-your-franchise.php">How to grow your franchise</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="http://www.whichfranchise.com/" target="_blank" rel="noopener noreferrer" title="">
+				<img src="../../../_img/whichfranchise_logo_sm.gif" width="105" height="35" alt="" /> whichfranchise.com
+			</a>
+		</p>
+
+		<img src="/_img/px_sellingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
