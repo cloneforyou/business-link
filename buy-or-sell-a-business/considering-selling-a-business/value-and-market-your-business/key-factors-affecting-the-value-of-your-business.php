b15774234f3bb45c30c7f5def8711287fe3eaeda
Caroline Hagan <cmurphydesign@gmail.com>
2018-06-14 22:28:00 +0100

    Completed "buy-or-sell-a-business > considering-selling-a-business > value-and-market-your-business > Key factors affecting the value of your business" page.

---
+++ buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/key-factors-affecting-the-value-of-your-business.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Key factors affecting the value of your business | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+
+	<!-- ref: detail556d.html || /detail?itemId=1074411173&type=RESOURCES -->
+	<meta name="description" content="Planning ahead lets you influence some of the many factors affecting how much your business is worth" />
+	<meta name="keywords" content="business valuation and marketing a business for sale;Sell business,find buyer,value business,valuation factors, capitalisation future cashflow,multiple of earnings,asset valuation,entry cost,sales memorandum,identify purchasers,approach buyers,shortlist buyers,confidentiality agreement,price negotiation,non disclosure agreement,future earnings" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+			
+		<h1>Value and market your business</h1>
+		<h2>Key factors affecting the value of your business</h2>
+		<p>There's a range of key factors that can affect the value of your business.</p>
+		
+		<h3>Finance</h3>
+		<ul>
+			<li>
+				Historical, current and projected profits and cashflow.
+			</li>
+			<li>How well you control costs.</li>
+			<li>Need for capital expenditure in the near future. </li>
+		</ul>
+		
+		<h3>External factors</h3>
+		<ul>
+			<li>State of the economy in general including interest rate levels and the level of demand in your market in particular.</li>
+			<li>How similar businesses are being valued.</li>
+			<li>How many potential purchasers are interested in the business.</li>
+			<li>How many similar businesses in your sector are on the market.</li>
+		</ul>
+		
+		<h3>Intangibles</h3>
+		<ul>
+			<li>
+				Goodwill and intellectual property such as patents.
+			</li>
+			<li>Strength of customer relationships - and how profitable they are.</li>
+			<li>Your business' growth potential.</li>
+			<li>Economies of scale a new owner could leverage.</li>
+		</ul>
+		
+		<h3>Assets and liabilities</h3>
+		<ul>
+			<li>Value of assets such as property, equipment, debtors and stock-in-hand.</li>
+			<li>How full your order book is.</li>
+			<li>Level of debt and other existing liabilities.</li>
+		</ul>
+		
+		<h3>People</h3>
+		<ul>
+			<li>
+				The management's record of success.
+			</li>
+			<li>How dependent the business is on your own skills - and the likely extent of your future involvement.</li>
+			<li>Experience and commitment of key staff.</li>
+		</ul>
+
+		<p>While some of these factors are outside your control and may affect the timing of your sale, you can take steps to make your business as valuable as possible. You need to start planning well in advance. Consider inserting an exit strategy into your original business plan. For more information, see our guide on <a href="/buy-or-sell-a-business/considering-selling-a-business/preparing-to-sell-your-business.php">preparing to sell your business</a>.</p>
+		<p>Remember that any valuation you and your advisers come up with is likely to be subjective. Business owners often place too high a value on their business. In the end, the value of your business is only as much as a purchaser is prepared to offer.</p>
+			
+		<ul>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business.php" title="Value and market your business">Prev</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/common-methods-of-valuing-a-business.php" title="Common methods of valuing a business">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business.php" title="Value and market your business">Introduction</a></li>
+			<li>Key factors affecting the value of your business</li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/common-methods-of-valuing-a-business.php">Common methods of valuing a business</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/prepare-a-sales-memorandum.php">Prepare a sales memorandum</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/identify-likely-buyers.php">Identify likely buyers</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/research-sources-of-potential-buyers.php">Research sources of potential buyers</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/approach-potential-buyers.php">Approach potential buyers</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/value-and-market-your-business/here-is-how-I-marketed-my-business-for-sale.php">Here's how I marketed my business for sale</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>Actions</h3>
+		<ul>
+			<li><a href="https://www.lawdonut.co.uk/business/exit-strategies/selling-your-business/valuing-a-business" target="_blank" rel="noopener noreferrer">Business valuation advice on the Law Donut website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/grow-your-business/assessing-current-performance/identifying-protecting-and-transferring-your-assets.php">Identifying, protecting and transferring your assets</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/consider-your-exit-strategy-when-starting-up.php">Consider your exit strategy when starting up</a></li>
+			<li><a href="/buy-or-sell-a-business/considering-selling-a-business/preparing-to-sell-your-business.php">Preparing to sell your business</a></li>
+			<li><a href="/buy-or-sell-a-business/selling/complete-the-sale-of-your-business.php">Complete the sale of your business</a></li>
+			<li><a href="/finance-and-grants/financial-planning-and-accounts/choose-and-work-with-an-accountant.php">Choose and work with an accountant</a></li>
+		</ul>
+
+		<img src="/_img/px_sellingabusiness.gif" alt="" width="181" height="5" />
+
+	</div>
+	<!-- // .small-12 large-8 large-push-4 columns -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div>
+<!-- // .row -->
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
