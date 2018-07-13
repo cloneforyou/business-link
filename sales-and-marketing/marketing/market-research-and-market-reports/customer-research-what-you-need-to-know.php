8afd060f907fa11c9509dd135f6eac48b1c99fbd
Caroline Hagan <cmurphydesign@gmail.com>
2018-07-13 15:05:22 +0100

    Completed "sales-and-marketing/marketing/market-research-and-market-reports > Customer research: what you need to know" page.

---
+++ sales-and-marketing/marketing/market-research-and-market-reports/customer-research-what-you-need-to-know.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Customer research: what you need to know | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+	<!-- ref: .html || /detail?itemId=1073790738&type=RESOURCES -->
+	<meta name="keywords" content="The key things to find out about your customers and the factors that influence their buying decisions" />
+	<meta name="description" content="Market research; customers;potential customers;market trends;buying habits;market research;customer survey;competitors;competitor intelligence;market reports;Office for National Statistics;ONS;Chambers of Commerce;UKTI;UK Trade & Investment;local trade association;internet;search engine;qualitative research;quantitative research;field research;sales plan;marketing strategy" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Market research and market reports</h1>
+		<h2>Customer research: what you need to know</h2>
+		<p>Carrying out customer research on loyalty, satisfaction and service can make a big difference to your business. If you know how your existing and potential customers make their buying decisions, you can adapt your sales methods to fit their needs.</p>
+		<p>Knowing such things as your customers' gender, age, occupation, income, lifestyle and attitudes can help with your marketing and sales plans. But knowing what they think of your products and service and how they rate you against your competitors will enable you improve your business' offering.</p>
+		<p>To target your customers successfully, you should know as much about their buying habits as possible. To find out what questions you should be asking, see the page on <a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs/ten-things-you-need-to-know-about-your-customers.php">ten things you need to know about your customers</a> in our guide: <a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs.php">know your customers' needs</a>.</p>
+		
+		<ul>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports.php" title="Market research and market reports">Prev</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/information-on-market-trends-and-competitor-intelligence.php" title="Information on market trends and competitor intelligence">Next</a></li>
+		</ul>
+
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports.php" title="Market research and market reports">Introduction</a></li>
+			<li>Customer research: what you need to know</li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/information-on-market-trends-and-competitor-intelligence.php">Information on market trends and competitor intelligence</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/using-market-reports-and-other-data.php">Using market reports and other data</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/interpreting-market-information.php">Interpreting market information</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/the-basics-of-quantitative-and-qualitative-field-research.php">The basics of quantitative and qualitative field research</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/planning-field-research.php">Planning field research</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/tips-for-successful-field-research.php">Tips for successful field research</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/should-i-use-a-market-research-agency.php">Should I use a market research agency?</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/here-is-how-i-made-the-most-of-market-research.php">Here's how I made the most of market research</a></li>
+		</ul>
+		
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+		<h3>Actions</h3>
+		<ul>
+			<!-- No longer exists, not in Archive 
+				<li><a href="https://www.microsoft.com/business/en-us/resources/marketing/market-research/researching-your-customers-6-tips-on-what-to-find-out.aspx" target="bgExternalonlinebusinesslinkgovuk" title="Find out what your customers want and why they buy">Researching your customers guidance on the Microsoft Small Business Centre website</a></li> -->
+			<li><a href="https://www.marketingdonut.co.uk/market-research/questionnaires-surveys-and-focus-groups" target="_blank" rel="noopener noreferrer">Questionnaires, surveys and focus groups on the Marketing Donut website</a></li>
+			<li><a href="https://www.learndirectpd.com/courses/retail-knowledge-rqf" target="_blank" rel="noopener noreferrer">Marketing e-course on the learndirect business website</a></li>
+		</ul>
+		
+		<h3>Also on this site</h3>
+		<ul>
+			<li><a href="/sales-and-marketing/marketing/create-your-marketing-strategy.php">Create your marketing strategy</a></li>
+			<li><a href="/sales-and-marketing/marketing/write-a-marketing-plan.php">Write a marketing plan</a></li>
+			<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs.php">Know your customers' needs</a></li>
+			<li><a href="/sales-and-marketing/selling/the-sales-process.php">The sales process</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/overseas-supplier-relationships/entering-overseas-markets.php">Entering overseas markets</a></li>
+		</ul>
+
+		<p>Developed with:<br />
+			<a href="https://www.cim.co.uk/home/" target="_blank" rel="noopener noreferrer" title="The Chartered Institute of Marketing">
+				<img src="/_img/cim_logo.gif" width="140" height="32" alt="" /> The Chartered Institute of Marketing
+			</a>
+		</p>
+
+		<img src="/_img/px_salesandmarketing.gif" alt="" width="181" height="5" />
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
