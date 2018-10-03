86bce78bacf5623581b9452ebd2f0049d0d64e53
Caroline Hagan <hello@blueocto.co.uk>
2018-10-03 11:23:56 +0100

    Completed "sales-and-marketing/marketing/market-research-and-market-reports > Planning field research" page.

--- sales-and-marketing/marketing/market-research-and-market-reports/planning-field-research.php
+++ sales-and-marketing/marketing/market-research-and-market-reports/planning-field-research.php
@@ -17,12 +17,12 @@
 
 		<h1>Market research and market reports</h1>
 		<h2>Planning field research</h2>
-		<p>Good planning is essential if you want&nbsp;to get the right results from field research.</p>
+		<p>Good planning is essential if you want to get the right results from field research.</p>
 		<p>First you need to decide how to collect the information you want. Popular methods include:</p>
 		<ul>
-			<li>A&nbsp;<b>survey</b>, using a fixed set of questions.&nbsp;The most effective way of carrying out a survey is typically with face-to-face interviews, but phone interviews, online questionnaires&nbsp;and postal surveys are also possibilities.</li>
-			<li>A&nbsp;<b>discussion -</b>&nbsp;often held in small focus groups. Discussions are good for qualitative research as they allow you to explore people's attitudes in more detail.</li>
-			<li><strong>Observation,</strong> to&nbsp;investigate what people do rather than what they say. For example, look at how shoppers react when they pass a particular point-of-sale display.</li>
+			<li>A <b>survey</b>, using a fixed set of questions. The most effective way of carrying out a survey is typically with face-to-face interviews, but phone interviews, online questionnaires and postal surveys are also possibilities.</li>
+			<li>A <b>discussion -</b> often held in small focus groups. Discussions are good for qualitative research as they allow you to explore people's attitudes in more detail.</li>
+			<li><strong>Observation,</strong> to investigate what people do rather than what they say. For example, look at how shoppers react when they pass a particular point-of-sale display.</li>
 			<li>An <strong>experiment -</strong> you might, for instance, run a blind taste test of your soft drink against your competitors' products. Alternatively, you could lend your new product to a customer and ask for feedback.</li>
 		</ul>
 
@@ -30,10 +30,10 @@
 		<p>You'll need to <b>design</b> your research. For example, drawing up a questionnaire or deciding how you'll run a focus group.</p>
 		<p>Then there are the <b>logistics</b>. If you want to carry out street interviews, make sure your researchers have the required local authority licence and identity card. If you want to run a focus group or conduct face-to-face interviews or product tests, where will you hold them? Where will you find the participants? And who'll run the session?</p>
 		<p>Consider carefully whether you have got the <b>skills</b> in-house to do this. If not, it's probably a good idea to get a market research agency to do your research for you.</p>
-		<p>See the page in&nbsp;this guide on&nbsp;<a href="/sales-and-marketing/marketing/market-research-and-market-reports/tips-for-successful-field-research.php" title="Key points to remember when designing your field research and interpreting your results">tips for successful field research</a>.</p>
+		<p>See the page in this guide on <a href="/sales-and-marketing/marketing/market-research-and-market-reports/tips-for-successful-field-research.php">tips for successful field research</a>.</p>
 		
 		<ul>
-			<li><a href="detaila6f3.html?itemId=1073790739&amp;type=RESOURCES" title="The basics of quantitative and qualitative field research">Prev</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/the-basics-of-quantitative-and-qualitative-field-research.php" title="The basics of quantitative and qualitative field research">Prev</a></li>
 			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/tips-for-successful-field-research.php" title="Tips for successful field research">Next</a></li>
 		</ul>
 
@@ -41,10 +41,10 @@
 		<ul>
 			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports.php" title="Market research and market reports">Introduction</a></li>
 			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/customer-research-what-you-need-to-know.php">Customer research: what you need to know</a></li>
-			<li><a href="detail2aa4.html?itemId=1073790736&amp;type=RESOURCES">Information on market trends and competitor intelligence</a></li>
-			<li><a href="detail07ca.html?itemId=1073790734&amp;type=RESOURCES">Using market reports and other data</a></li>
-			<li><a href="detailc221.html?itemId=1073790737&amp;type=RESOURCES">Interpreting market information</a></li>
-			<li><a href="detaila6f3.html?itemId=1073790739&amp;type=RESOURCES">The basics of quantitative and qualitative field research</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports.php">Information on market trends and competitor intelligence</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/using-market-reports-and-other-data.php">Using market reports and other data</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/interpreting-market-information.php">Interpreting market information</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/the-basics-of-quantitative-and-qualitative-field-research.php">The basics of quantitative and qualitative field research</a></li>
 			<li>Planning field research</li>
 			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/tips-for-successful-field-research.php">Tips for successful field research</a></li>
 			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/should-i-use-a-market-research-agency.php">Should I use a market research agency?</a></li>
@@ -55,17 +55,17 @@
 
 		<h3>Actions</h3>
 		<ul>
-			<li><a href="http://www.mrs.org.uk/mrindustry/downloads/newcomers.pdf" target="bgExternalonlinebusinesslinkgovuk" title="A guide for newcomers which outlines different research methods, the information they can obtain, and how they will be able to help you">Download a research methods guide from the Market Research Society website (PDF, 36K)</a></li>
-			<li><a href="http://www.learndirect.com/marketing" target="bgExternalonlinebusinesslinkgovuk" title="This course introduces the key principles of marketing and shows how you can apply them within your business">Marketing e-course on the learndirect business website</a></li>
+			<!-- /* not online */ <li><a href="http://www.mrs.org.uk/mrindustry/downloads/newcomers.pdf" target="bgExternalonlinebusinesslinkgovuk" title="A guide for newcomers which outlines different research methods, the information they can obtain, and how they will be able to help you">Download a research methods guide from the Market Research Society website (PDF, 36K)</a></li> -->
+			<li><a href="https://www.learndirect.com/course/business-marketing" target="_blank" rel="noopener noreferrer">Marketing e-course on the learndirect business website</a></li>
 		</ul>
 		
 		<h3>Also on this site</h3>
 		<ul>
-			<li><a href="/sales-and-marketing/marketing/create-your-marketing-strategy.php" title="Decide how to market your product or service to potential customers">Create your marketing strategy</a></li>
-			<li><a href="/sales-and-marketing/marketing/write-a-marketing-plan.php" title="How to identify your objectives and write a plan that will help your marketing generate sales">Write a marketing plan</a></li>
-			<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs.php" title="Why understanding your customers' needs is crucial to the success of your business">Know your customers' needs</a></li>
-			<li><a href="layer48a3.html?topicId=1074002278" title="What steps you must take to protect your customers' privacy and data">Privacy and data protection in direct marketing</a></li>
-			<li><a href="/international-trade/importing-and-exporting-basics/overseas-supplier-relationships/entering-overseas-markets.php" title="Deciding between selling directly and using an intermediary is a key choice when entering an overseas market">Entering overseas markets</a></li>
+			<li><a href="/sales-and-marketing/marketing/create-your-marketing-strategy.php">Create your marketing strategy</a></li>
+			<li><a href="/sales-and-marketing/marketing/write-a-marketing-plan.php">Write a marketing plan</a></li>
+			<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs.php">Know your customers' needs</a></li>
+			<li><a href="/sales-and-marketing/selling-and-the-law/privacy-and-data-protection-in-direct-marketing.php">Privacy and data protection in direct marketing</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/overseas-supplier-relationships/entering-overseas-markets.php">Entering overseas markets</a></li>
 		</ul>
 
 		<p>Developed with:<br />
