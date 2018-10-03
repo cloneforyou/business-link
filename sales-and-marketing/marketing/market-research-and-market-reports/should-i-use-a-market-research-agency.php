2db8340dd95f8e07784cde8d2bd68815b47591de
Caroline Hagan <hello@blueocto.co.uk>
2018-10-03 11:29:46 +0100

    Completed "sales-and-marketing/marketing/market-research-and-market-reports > Should I use a market research agency?" page.

--- sales-and-marketing/marketing/market-research-and-market-reports/should-i-use-a-market-research-agency.php
+++ sales-and-marketing/marketing/market-research-and-market-reports/should-i-use-a-market-research-agency.php
@@ -17,7 +17,7 @@
 
 		<h1>Market research and market reports</h1>
 		<h2>Should I use a market research agency?</h2>
-		<p>Though you may be able to do your own field research, it may be better and more cost-effective to outsource the job to professionals and use the services of a market research agency - see our guide on <a href="layere30b.html?topicId=1073921035" title="How to decide whether to outsource any of your business functions or activities to other companies">outsourcing</a>.</p>
+		<p>Though you may be able to do your own field research, it may be better and more cost-effective to outsource the job to professionals and use the services of a market research agency - see our guide on <a href="/grow-your-business/growth-through-strategic-sourcing/outsourcing.php">outsourcing</a>.</p>
 		<p>The benefits to your business are:</p>
 		<ul>
 			<li>Market research professionals are likely to get <b>better results</b>. They have experience in designing survey questionnaires, running focus groups and asking the right questions.</li>
@@ -26,14 +26,14 @@
 			<li>You may find it difficult to be <b>impartial</b>, particularly if people criticise your business.</li>
 		</ul>
 
-		<p>For small-scale field research, your&nbsp;best option may be a <b>freelance researcher</b>. A market research agency won't usually take on projects with a budget&nbsp;below &pound;3,000.</p>
-		<p>As well as recommendations from business contacts, you can <a href="http://www.rbg.org.uk/" target="bgExternalonlinebusinesslinkgovuk">search for a market research agency on the Research Buyers Guide website (registration required)</a>.</p>
-		<p>Before taking on a market research&nbsp;agency or a freelance researcher, investigate their <b>reputation</b>. Ask for a list of previous clients and contact them for feedback. Check the agency or researcher has relevant experience and consider how comfortable you'd feel working with them. And get a clear idea of <b>fees</b> for the services you want.</p>
+		<p>For small-scale field research, your best option may be a <b>freelance researcher</b>. A market research agency won't usually take on projects with a budget below &pound;3,000.</p>
+		<p>As well as recommendations from business contacts, you can <a href="https://www.mrs.org.uk/researchbuyersguide" target="_blank" rel="noopener noreferrer">search for a market research agency on the Research Buyers Guide website (registration required)</a>.</p>
+		<p>Before taking on a market research agency or a freelance researcher, investigate their <b>reputation</b>. Ask for a list of previous clients and contact them for feedback. Check the agency or researcher has relevant experience and consider how comfortable you'd feel working with them. And get a clear idea of <b>fees</b> for the services you want.</p>
 		<p>Check the agency's researchers or freelance researchers fit the image of your business. If they'll be carrying out <b>street interviews</b>, confirm they'll have the required local authority licence and identity card.</p>
-		<p>Make sure you provide a thorough and clear&nbsp;<b>brief</b>. This needs to cover areas such as the business objectives behind the project, the information the research should uncover and how you intend to use the results.</p>
+		<p>Make sure you provide a thorough and clear <b>brief</b>. This needs to cover areas such as the business objectives behind the project, the information the research should uncover and how you intend to use the results.</p>
 		
 		<ul>
-			<li><a href="detail3698.html?itemId=1073790741&amp;type=RESOURCES" title="Tips for successful field research">Prev</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/tips-for-successful-field-research.php" title="Tips for successful field research">Prev</a></li>
 			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/here-is-how-i-made-the-most-of-market-research.php" title="Here's how I made the most of market research">Next</a></li>
 		</ul>
 
@@ -41,12 +41,12 @@
 		<ul>
 			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports.php" title="Market research and market reports">Introduction</a></li>
 			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/customer-research-what-you-need-to-know.php">Customer research: what you need to know</a></li>
-			<li><a href="detail2aa4.html?itemId=1073790736&amp;type=RESOURCES">Information on market trends and competitor intelligence</a></li>
-			<li><a href="detail07ca.html?itemId=1073790734&amp;type=RESOURCES">Using market reports and other data</a></li>
-			<li><a href="detailc221.html?itemId=1073790737&amp;type=RESOURCES">Interpreting market information</a></li>
-			<li><a href="detaila6f3.html?itemId=1073790739&amp;type=RESOURCES">The basics of quantitative and qualitative field research</a></li>
-			<li><a href="detailec03.html?itemId=1073790740&amp;type=RESOURCES">Planning field research</a></li>
-			<li><a href="detail3698.html?itemId=1073790741&amp;type=RESOURCES">Tips for successful field research</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports.php">Information on market trends and competitor intelligence</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/using-market-reports-and-other-data.php">Using market reports and other data</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/interpreting-market-information.php">Interpreting market information</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/the-basics-of-quantitative-and-qualitative-field-research.php">The basics of quantitative and qualitative field research</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/planning-field-research.php">Planning field research</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/tips-for-successful-field-research.php">Tips for successful field research</a></li>
 			<li>Should I use a market research agency?</li>
 			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/here-is-how-i-made-the-most-of-market-research.php">Here's how I made the most of market research</a></li>
 		</ul>
@@ -55,18 +55,19 @@
 
 		<h3>Actions</h3>
 		<ul>
-			<li><a href="http://www.rbg.org.uk/" target="bgExternalonlinebusinesslinkgovuk">Market research agency search on the Research Buyer's Guide website (registration required)</a></li>
-			<li><a href="http://www.learndirect.com/marketing" target="bgExternalonlinebusinesslinkgovuk" title="This course introduces the key principles of marketing and shows how you can apply them within your business">Marketing e-course on the learndirect business website</a></li>
+			<li><a href="https://www.mrs.org.uk/researchbuyersguide" target="_blank" rel="noopener noreferrer">Market research agency search on the Research Buyer's Guide website (registration required)</a></li>
+			<li><a href="https://www.learndirect.com/course/business-marketing" target="_blank" rel="noopener noreferrer">Marketing e-course on the learndirect business website</a></li>
 		</ul>
 		
 		<h3>Also on this site</h3>
 		<ul>
-			<li><a href="/sales-and-marketing/marketing/create-your-marketing-strategy.php" title="Decide how to market your product or service to potential customers">Create your marketing strategy</a></li>
-			<li><a href="/sales-and-marketing/marketing/write-a-marketing-plan.php" title="How to identify your objectives and write a plan that will help your marketing generate sales">Write a marketing plan</a></li>
-			<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs.php" title="Why understanding your customers' needs is crucial to the success of your business">Know your customers' needs</a></li>
-			<li><a href="layere30b.html?topicId=1073921035" title="How to decide whether to outsource any of your business functions or activities to other companies">Outsourcing</a></li>
-			<li><a href="/international-trade/importing-and-exporting-basics/overseas-supplier-relationships/entering-overseas-markets.php" title="Deciding between selling directly and using an intermediary is a key choice when entering an overseas market">Entering overseas markets</a></li>
+			<li><a href="/sales-and-marketing/marketing/create-your-marketing-strategy.php">Create your marketing strategy</a></li>
+			<li><a href="/sales-and-marketing/marketing/write-a-marketing-plan.php">Write a marketing plan</a></li>
+			<li><a href="/sales-and-marketing/keeping-your-customers/know-your-customers-needs.php">Know your customers' needs</a></li>
+			<li><a href="/grow-your-business/growth-through-strategic-sourcing/outsourcing.php">Outsourcing</a></li>
+			<li><a href="/international-trade/importing-and-exporting-basics/overseas-supplier-relationships/entering-overseas-markets.php">Entering overseas markets</a></li>
 		</ul>
+		
 		<p>Developed with:<br />
 			<a href="https://www.cim.co.uk/home/" target="_blank" rel="noopener noreferrer" title="The Chartered Institute of Marketing">
 				<img src="/_img/cim_logo.gif" width="140" height="32" alt="" /> The Chartered Institute of Marketing
