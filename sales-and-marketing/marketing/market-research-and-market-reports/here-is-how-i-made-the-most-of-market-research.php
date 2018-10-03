72621005c3348e5882128cafd67dd21b103135b0
Caroline Hagan <hello@blueocto.co.uk>
2018-10-03 11:40:08 +0100

    Completed "sales-and-marketing/marketing/market-research-and-market-reports > Here's how I made the most of market research" page.

--- sales-and-marketing/marketing/market-research-and-market-reports/here-is-how-i-made-the-most-of-market-research.php
+++ sales-and-marketing/marketing/market-research-and-market-reports/here-is-how-i-made-the-most-of-market-research.php
@@ -21,12 +21,12 @@
 		<p><strong>What I did</strong></p>
 		
 		<h3>Spend time not money</h3>
-		<p>"Like most companies starting out, we needed to research our target market but didn't have limitless cash to pay someone to do it for us. Doing it yourself is cheap in money terms&nbsp;- but you have to invest your time if you expect to get anything useful out of it.</p>
+		<p>"Like most companies starting out, we needed to research our target market but didn't have limitless cash to pay someone to do it for us. Doing it yourself is cheap in money terms - but you have to invest your time if you expect to get anything useful out of it.</p>
 		<p>"Over a period of three months, I went on a virtual fact-finding mission using the internet. If you persevere, you can find an incredible amount of quality information for free, including market reports and expert analysis. Business consultants' websites, industry bodies and sector-leading companies are a good place to start."</p>
 		
 		<h3>Apply the research</h3>
 		<p>"Throughout my research, I was careful to avoid <strong>analysis paralysis</strong>. There's no point hiding behind piles of market data and thinking you have done something constructive. You have to use the information to develop your business. Having clear objectives helps. I wanted to know the size of the market, to learn from competitors' successes and mistakes and to understand what potential clients want.</p>
-		<p>"I focused on data from reputable sources and used it to help formulate a business plan. At a later stage, I also used research facts in Bladonmore's marketing material&nbsp;- but always bearing in mind that I didn't want to give away too much to competitors."</p>
+		<p>"I focused on data from reputable sources and used it to help formulate a business plan. At a later stage, I also used research facts in Bladonmore's marketing material - but always bearing in mind that I didn't want to give away too much to competitors."</p>
 		
 		<h3>Keep up to date</h3>
 		<p>"Research isn't just about reading the occasional market report. It should be an ongoing process that keeps you up to date with your market, your rivals and your clients. I find newspapers one of the best research sources. There's something relevant to our business in the press almost every day. For example an article about increased activity in venture capital (VC) markets is useful, because companies looking for VC finance often need financial presentation training."</p>
@@ -39,71 +39,48 @@
 		<p>"I now make it part of my daily routine to clip useful research out of newspapers. I even carry around a small pair of scissors for the purpose. It's yielded several business development ideas and I wish I'd got into the habit sooner."</p>
 		
 		<ul>
-			<li><a href="detail9005.html?itemId=1073790742&amp;type=RESOURCES" title="Should I use a market research agency?">Prev</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/should-i-use-a-market-research-agency.php" title="Should I use a market research agency?">Prev</a></li>
 		</ul>
 		
 		<h3>Subjects covered in this guide</h3>
 		<ul>
 			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports.php" title="Market research and market reports">Introduction</a></li>
 			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/customer-research-what-you-need-to-know.php">Customer research: what you need to know</a></li>
-			<li><a href="detail2aa4.html?itemId=1073790736&amp;type=RESOURCES">Information on market trends and competitor intelligence</a></li>
-			<li><a href="detail07ca.html?itemId=1073790734&amp;type=RESOURCES">Using market reports and other data</a></li>
-			<li><a href="detailc221.html?itemId=1073790737&amp;type=RESOURCES">Interpreting market information</a></li>
-			<li><a href="detaila6f3.html?itemId=1073790739&amp;type=RESOURCES">The basics of quantitative and qualitative field research</a></li>
-			<li><a href="detailec03.html?itemId=1073790740&amp;type=RESOURCES">Planning field research</a></li>
-			<li><a href="detail3698.html?itemId=1073790741&amp;type=RESOURCES">Tips for successful field research</a></li>
-			<li><a href="detail9005.html?itemId=1073790742&amp;type=RESOURCES">Should I use a market research agency?</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports.php">Information on market trends and competitor intelligence</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/using-market-reports-and-other-data.php">Using market reports and other data</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/interpreting-market-information.php">Interpreting market information</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/the-basics-of-quantitative-and-qualitative-field-research.php">The basics of quantitative and qualitative field research</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/planning-field-research.php">Planning field research</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/tips-for-successful-field-research.php">Tips for successful field research</a></li>
+			<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports/should-i-use-a-market-research-agency.php">Should I use a market research agency?</a></li>
 			<li>Here's how I made the most of market research</li>
 		</ul>
 		
 		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
 
-		<table cellpadding="0" cellspacing="0">
-			<tr>
-				<td><img src="/_img/px_white.gif" width="181" height="5" alt="" /><img src="../../_img/CS0021.jpg" width="181" height="121" alt="Richard Rivlin, Blandonmore" /><img src="/_img/px_salesandmarketing.gif" width="181" height="5" alt="" /></td>
-			</tr>
-			<tr>
-				<td class=highlighttext>&nbsp;</td>
-			</tr>
-			<tr>
-				<td>
-					<h3>Richard Rivlin<br />
-	<a href="http://www.bladonmore.com/" target="bgExternalonlinebusinesslinkgovuk"  title="The website for Blandonmore, subject of one of our case studies" >Bladonmore</a></h3>
-					<p>&nbsp;</p>
-					<h3>Richard's top tips:</h3>
-					<ul>
-						<li>"Invest time - it's your most important research commodity."</li>
-						<li>"Split research into three areas - market trends, competitors and potential customers."</li>
-						<li>"Focus customer research by writing a wish list of your three dream clients, then thinking specifically about their needs."</li>
-					</ul>
-					<!-- Actions section begin -->
-				</td>
-			</tr>
-		</table>
+		<div class="media-object stack-for-small">
+			<div class="media-object-section">
+				<div class="thumbnail">
+					<img src="/_img/CS0021.jpg" width="181" height="121" border="0" alt="Richard Rivlin, Blandonmore" />
+				</div>
+			</div>
+			<div class="media-object-section">
+				<h3>
+					Richard Rivlin<br />
+					<a href="https://www.bladonmore.com/" target="_blank" rel="noopener noreferrer">Bladonmore</a><br />
+					Richard's top tips:
+				</h3>
+				<ul>
+					<li>"Invest time - it's your most important research commodity."</li>
+					<li>"Split research into three areas - market trends, competitors and potential customers."</li>
+					<li>"Focus customer research by writing a wish list of your three dream clients, then thinking specifically about their needs."</li>
+				</ul>
+			</div>
+		</div>
 		
-		<table cellpadding="0" cellspacing="0" width="100%">
-			<tr>
-				<td>
-					<h4>More case studies</h4></td>
-			</tr>
-			<tr>
-				<td>
-					<ul>
-						<li><a href="detaile18f.html?itemId=1075068373&amp;type=CASE STUDIES" title="Brown and Forrest used their website to open up a new sales channel - see how they did it in our case study">Here's how I marketed my products and services on the web</a></li>
-					</ul>
-				</td>
-			</tr>
-			<!-- Find a case study link -->
-			<tr>
-			</tr>
-			<tr>
-				<td>
-					<ul>
-						<li><a href="/find-a-case-study.php" title=""><b>Find a case study</b></a></li>
-					</ul>
-				</td>
-			</tr>
-		</table>
+		<h4>More case studies</h4>
+		<p><a href="/sales-and-marketing/online-marketing/generate-business-from-your-e-marketing-plan/here-is-how-I-marketed-my-products-and-services-on-the-web.php">Here's how I marketed my products and services on the web</a></p>
+		<p><a href="/find-a-case-study.php" title=""><b>Find a case study</b></a></p>
 		
 		<p>Developed with:<br />
 			<a href="https://www.cim.co.uk/home/" target="_blank" rel="noopener noreferrer" title="The Chartered Institute of Marketing">
