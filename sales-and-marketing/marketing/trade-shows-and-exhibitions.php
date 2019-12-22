488b27d5531381d7630746fd7db20a060cfcea2c
Caroline Hagan <caroline@blueocto.co.uk>
2019-12-22 19:55:31 +0000

    Completed "/sales-and-marketing/marketing > Trade shows and exhibitions" page.

---
+++ sales-and-marketing/marketing/trade-shows-and-exhibitions.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<title>Trade shows and exhibitions | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+	
+	<!-- ref: layer9295.html || /layer?topicId=1074022023 -->
+
+	<meta name="description" lang="en" content="Getting the most out of trade shows - using specialist event companies, attending trade events abroad" />
+	<meta name="keywords" lang="en" content="Trade shows;exhibitions;trade development;foreign trade;expanding market;trade development;exhibition grants;exhibition funding;funding for exhibitions;trade fair;trade shows;exhibitor;display goods;display stand;expo;trade expo;exhibitor;trade exhibition;trade promotion;trade events;marketing opportunity;exhibition stand;" />
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+
+<div class="row">
+	<div class="small-12 large-8 large-push-4 columns">
+
+		<h1>Trade shows and exhibitions</h1>
+		<h2>Introduction</h2>
+		<img src="/_img/EP1070.gif" width="78" height="118" alt="" />
+		<p>Trade shows and exhibitions are specialist market places that allow exhibitors to promote their products and services, reach new customers and generate new sales. They also give visitors the opportunity to find out about the products and services on display.</p>
+		<p>Trade shows and exhibitions can also provide business people with the opportunity to network and make useful contacts with other people working in their industry.</p>
+		<p>This guide aims to help you get the best from exhibiting at one of these events (for simplicity, we'll call them all <b>trade fairs</b>) or attending as a visitor.</p>
+		
+		<ul>
+			<li><a href="/sales-and-marketing/marketing/trade-shows-and-exhibitions/using-a-trade-fair-to-promote-your-product-or-service.php" title="Using a trade fair to promote your product or service">Next</a></li>
+		</ul>
+		
+		<h3>Subjects covered in this guide</h3>
+		<ul>
+			<li>Introduction</li>
+			<li><a href="/sales-and-marketing/marketing/trade-shows-and-exhibitions/using-a-trade-fair-to-promote-your-product-or-service.php">Using a trade fair to promote your product or service</a></li>
+			<li><a href="/sales-and-marketing/marketing/trade-shows-and-exhibitions/going-to-a-trade-fair-as-a-customer.php">Going to a trade fair as a customer</a></li>
+			<li><a href="/sales-and-marketing/marketing/trade-shows-and-exhibitions/choosing-the-right-trade-fair.php">Choosing the right trade fair</a></li>
+			<li><a href="/sales-and-marketing/marketing/trade-shows-and-exhibitions/exhibiting-using-specialist-event-companies.php">Exhibiting - using specialist event companies</a></li>
+			<li><a href="/sales-and-marketing/marketing/trade-shows-and-exhibitions/exhibiting-doing-it-yourself.php">Exhibiting - doing it yourself</a></li>
+			<li><a href="/sales-and-marketing/marketing/trade-shows-and-exhibitions/access-and-legal-issues.php">Access and legal issues</a></li>
+			<li><a href="/sales-and-marketing/marketing/trade-shows-and-exhibitions/checklist-exhibiting-successfully.php">Checklist: exhibiting successfully</a></li>
+			<li><a href="/sales-and-marketing/marketing/trade-shows-and-exhibitions/international-trade-fairs.php">International trade fairs</a></li>
+			<li><a href="/sales-and-marketing/marketing/trade-shows-and-exhibitions/following-up-from-a-trade-fair.php">Following up from a trade fair</a></li>
+			<li><a href="/sales-and-marketing/marketing/trade-shows-and-exhibitions/here-is-how-i-generated-new-business-by-attending-trade-shows-and-exhibitions.php">Here's how I generated new business by attending trade shows and exhibitions</a></li>
+		</ul>
+
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+		
+		<h3>AEO Enquiry Line</h3>
+		<p>01442 285 810</p>
+
+		<img src="/_img/px_salesandmarketing.gif" width="181" height="5" alt="" />
+	
+		<p>Developed with:<br />
+			<a href="https://www.cim.co.uk/" target="_blank" rel="noopener noreferrer" title="The Chartered Institute of Marketing - Opens in a new window">
+				<img src="/_img/cim_logo.gif" width="140" height="32" alt="" /> The Chartered Institute of Marketing
+			</a>
+		</p>
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
