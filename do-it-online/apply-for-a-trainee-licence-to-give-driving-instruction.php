99e29a65e4139e8305a913843eed745a22b53c84
Caroline Hagan <caroline@blueocto.co.uk>
2020-01-05 18:35:13 +0000

    Completed /do-it-online/ "Apply for a trainee licence to give driving instruction" page.

---
+++ do-it-online/apply-for-a-trainee-licence-to-give-driving-instruction.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Apply for a trainee licence to give driving instruction | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+	<!-- ref: detaila956.html || /detail?itemId=1096908366&type=PIP -->
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Apply for a trainee licence to give driving instruction</h1>
+			<p>Apply to the Driving Standards Agency for a trainee licence to give driving instruction once you have passed the first two approved driving instructor (ADI) qualification tests.</p>
+			
+			<h3>Before you start</h3>
+			<p>You can use this service to apply for a trainee licence to give driving instruction.</p>
+			<p>You can use it between 6.00 am and midnight, Monday to Sunday.</p>
+			
+			<h3>What you will need</h3>
+			<p>This service is available to registered users only.</p>
+			<p>You will need:</p>
+			<ul>
+				<li>your online services user ID</li>
+				<li>your password</li>
+				<li>a valid credit or debit card to pay the fee</li>
+				<li>your personal reference number (PRN)</li>
+				<li>a printer</li>
+			</ul>
+			
+			<h3>Payment</h3>
+			<p>Payment will be taken from your account as soon as your application has been received. If your application is refused, the Driving Standards Agency (DSA) will give you a full refund.</p>
+			
+			<h3>Sending your application</h3>
+			<p>When you have completed the screens, you will need to print a copy of the final page. You can do this using your browser print option.</p>
+			<p>You should send the documents listed to the DSA address below:</p>
+			<ul>
+				<li>the printed final page</li>
+				<li>a passport sized photograph (with your PRN written on the back)</li>
+				<li>your 21T form</li>
+				<li>ADI 3L (MD) form (if applying for a first trainee licence)</li>
+			</ul>
+			
+			<p>Driving Standards Agency
+				<br /> The Axis Building
+				<br /> 112 Upper Parliament Street
+				<br /> Nottingham
+				<br /> NG1 6LP</p>
+			<p>You can get help by phoning the DSA Helpline on Tel 0300 200 1122 or emailing them at <a href="mailto:adireg@dsa.gsi.gov.uk">adireg@dsa.gsi.gov.uk</a>.</p>
+			<p>The DSA aims to process your application within ten working days.</p>
+			
+			<p><a href="https://dsa.dft.gov.uk/DSAWeb/" target="_blank" rel="noopener noreferrer">Apply for a trainee licence online</a></p>
+			
+			<p>In partnership with:<br />
+				<a href="https://www.gov.uk/government/organisations/driving-standards-agency" target="_blank" rel="noopener noreferrer" title="Driving Standards Agency">
+					<img src="../../_img/dsa_logo_35px.gif" width="72" height="35" alt="" /> Driving Standards Agency
+				</a>
+			</p>
+			
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div><!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
