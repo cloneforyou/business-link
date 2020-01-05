137b7833689bec976ddae29d8d095548115e53dd
Caroline Hagan <caroline@blueocto.co.uk>
2020-01-05 17:54:16 +0000

    Completed /create-innovate-and-protect/create-and-improve-products-and-services/importance-of-knowledge-management-to-a-growing-business/ "What is knowledge in a business?" page.

---
+++ create-innovate-and-protect/create-and-improve-products-and-services/importance-of-knowledge-management-to-a-growing-business/what-is-knowledge-in-a-business.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>What is knowledge in a business? | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+	<!-- ref: .html || / -->
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Importance of knowledge management to a growing business</h1>
+			<h2>What is knowledge in a business?</h2>
+			<p>Using knowledge in your business isn't necessarily about thinking up clever new products and services, or devising ingenious new ways of selling them. It's much more straightforward.</p>
+			<p>Useful and important knowledge already exists in your business. It can be found in:</p>
+			<ul>
+				<li> the <strong>experience</strong> of your workforce</li>
+				<li> the <strong>designs and processes</strong> for your goods and services</li>
+				<li> your files or <strong>documents</strong> (whether held digitally, on paper or both)</li>
+				<li> your <strong>plans</strong> for future activities, such as ideas for new products or services</li>
+			</ul>
+			
+			<p>The challenge is to harness these intangible assets in a coherent and productive way.</p>
+			<p>You can find out more about what intangible assets are in our guide on <a href="/grow-your-business/assessing-current-performance/identifying-protecting-and-transferring-your-assets.php">identifying, protecting and transferring your assets</a>.</p>
+			<p>How a business creates, shares and applies knowledge in order to help it achieve its goals is known as <strong>knowledge management</strong>.</p>
+			<p>An effective knowledge management strategy will make it possible for knowledge to be shared at a variety of levels including:</p>
+			<ul>
+				<li>between individuals</li>
+				<li>among a team</li>
+				<li>among an entire organisation</li>
+				<li>between different organisations</li>
+			</ul>
+			
+			<h3>Existing forms of knowledge</h3>
+			<p>You have probably done <b>market research</b> into the need for your business to exist in the first place. If nobody wanted what you're selling, you wouldn't be trading. You can tailor this market knowledge to target particular customers with specific types of product or service. See our guide on <a href="/sales-and-marketing/marketing/market-research-and-market-reports.php">market research and market reports</a> to find out how market research can improve your business.</p>
+			<p>Your files or <strong>documents</strong> from and about customers and suppliers hold a wealth of information which can be invaluable both in developing new products or services and improving existing ones.</p>
+			<p>Your <b>workers</b> are likely to have <strong>skills and experience</strong> that you can use as an asset. Having staff who are knowledgeable can be invaluable in setting you apart from competitors.</p>
+			<p>You should make sure that your employees' knowledge and skills are passed on to their colleagues and successors wherever possible, eg by</p>
+			<ul>
+				<li>holding brainstorming sessions</li>
+				<li>organising training courses</li>
+				<li>maintaining up-to-date documentation about processes and procedures</li>
+			</ul>
+			
+			<p>See the page in this guide on how to <a href="/create-innovate-and-protect/create-and-improve-products-and-services/importance-of-knowledge-management-to-a-growing-business/create-a-knowledge-strategy-for-your-business.php">create a knowledge strategy for your business</a>.</p>
+			<p>Your understanding of what customers want, combined with your workers' know-how, can be regarded as your <strong>knowledge base</strong>.</p>
+			<p>Using this knowledge in the right way can help you run your business more efficiently, decrease business risks and exploit opportunities to the full. This is known as the <strong>knowledge advantage</strong>.</p>
+			
+			<ul>
+				<li><a href="/create-innovate-and-protect/create-and-improve-products-and-services/importance-of-knowledge-management-to-a-growing-business.php" title="Importance of knowledge management to a growing business">Prev</a></li>
+				<li><a href="/create-innovate-and-protect/create-and-improve-products-and-services/importance-of-knowledge-management-to-a-growing-business/basic-sources-of-knowledge.php" title="Basic sources of knowledge">Next</a></li>
+			</ul>
+
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/create-innovate-and-protect/create-and-improve-products-and-services/importance-of-knowledge-management-to-a-growing-business.php" title="Importance of knowledge management to a growing business">Introduction</a></li>
+				<li>What is knowledge in a business?</li>
+				<li><a href="/create-innovate-and-protect/create-and-improve-products-and-services/importance-of-knowledge-management-to-a-growing-business/basic-sources-of-knowledge.php">Basic sources of knowledge</a></li>
+				<li><a href="/create-innovate-and-protect/create-and-improve-products-and-services/importance-of-knowledge-management-to-a-growing-business/exploiting-your-knowledge.php">Exploiting your knowledge</a></li>
+				<li><a href="/create-innovate-and-protect/create-and-improve-products-and-services/importance-of-knowledge-management-to-a-growing-business/make-knowledge-central-to-your-business.php">Make knowledge central to your business</a></li>
+				<li><a href="/create-innovate-and-protect/create-and-improve-products-and-services/importance-of-knowledge-management-to-a-growing-business/sharing-knowledge-across-your-business.php">Sharing knowledge across your business</a></li>
+				<li><a href="/create-innovate-and-protect/create-and-improve-products-and-services/importance-of-knowledge-management-to-a-growing-business/create-a-knowledge-strategy-for-your-business.php">Create a knowledge strategy for your business</a></li>
+				<li><a href="/create-innovate-and-protect/create-and-improve-products-and-services/importance-of-knowledge-management-to-a-growing-business/using-information-technology-to-gain-and-manage-knowledge.php">Using information technology to gain and manage knowledge</a></li>
+				<li><a href="/create-innovate-and-protect/create-and-improve-products-and-services/importance-of-knowledge-management-to-a-growing-business/here-is-how-I-used-knowledge-management-to-make-my-business-better_flash-video.php">Here's how I used knowledge management to make my business better (Flash video)</a></li>
+			</ul>
+
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<!-- 
+			<h3>Actions</h3>
+			<ul>
+				Website no longer exists 
+					<li><a href="http://www.knowledgeboard.com/" target="bgExternalonlinebusinesslinkgovuk" title="Share knowledge and information on the EU-funded KnowledgeBoard website">Knowledge management discussions and events listed on the KnowledgeBoard website</a></li>
+				<li><a href="http://www.knowledgebusiness.com/knowledgebusiness/Templates/Home.aspx?siteId=1&amp;menuItemId=25" target="bgExternalonlinebusinesslinkgovuk" title="Networking, benchmarking and best practice resources to help your business share knowledge">Business knowledge news and resources on the KNOW website</a></li>
+			</ul>
+			 -->
+						
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/grow-your-business/assessing-current-performance/identifying-protecting-and-transferring-your-assets.php">Identifying, protecting and transferring your assets</a></li>
+				<li><a href="/create-innovate-and-protect/use-innovation-in-your-business/use-innovation-to-start-or-grow-your-business.php">Use innovation to start or grow your business</a></li>
+				<li><a href="/grow-your-business/setting-a-vision-for-growth/make-best-use-of-standards.php">Make best use of standards</a></li>
+				<li><a href="/grow-your-business/growth-through-strategic-sourcing/manage-your-suppliers.php">Manage your suppliers</a></li>
+				<li><a href="/sales-and-marketing/keeping-your-customers/manage-your-customer-database.php">Manage your customer database</a></li>
+				<li><a href="/it-and-ecommerce/introduction-to-it/plan-your-it.php">Plan your IT</a></li>
+				<li><a href="/sales-and-marketing/marketing/market-research-and-market-reports.php">Market research and market reports</a></li>
+				<li><a href="/create-innovate-and-protect/research-and-development/work-with-uk-universities-and-colleges.php">Work with UK universities and colleges</a></li>
+			</ul>
+			
+			<img src="/_img/px_growthandinnovation.gif" alt="" width="181" height="5" />
+			
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div><!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
