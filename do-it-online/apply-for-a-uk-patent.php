2968cfeab26adf2ba733bbdb07d170caa309f643
Caroline Hagan <caroline@blueocto.co.uk>
2020-01-05 18:40:40 +0000

    Completed /do-it-online/ "Apply for a UK patent" page.

---
+++ do-it-online/apply-for-a-uk-patent.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Apply for a UK patent | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+	<!-- ref: .html || / -->
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Apply for a UK patent</h1>
+			<p>Before you apply for a patent you should check that it is a new invention and doesn't already exist. You can <a href="/do-it-online/look-up-patents.php">use the Intellectual Property Office online patents register</a> to check.</p>
+			<p>You can also find further information that will help you decide if you need to apply for a patent in our guide on how to <a href="/create-innovate-and-protect/protecting-your-intellectual-property/patent-protection-and-your-business/get-patent-protection-for-your-business.php">get patent protection for your business</a>.</p>
+			<p>To apply for a patent online you will need:</p>
+			<ul>
+				<li>a valid email address where the Intellectual Property Office can send the web filing receipt</li>
+				<li>a valid credit/debit card or details of a customer/deposit account that you hold with the Intellectual Property Office (if payment is required when filing)</li>
+				<li>the name and address details of the applicant(s) and, if needed, the inventor(s) and/or representative or patent agent</li>
+				<li>filing details of any prior application being declared, divided from or relied upon instead of a description</li>
+				<li>PDF documents - for example, description, drawings, claims, abstract, covering letter</li>
+			</ul>
+
+			<p>Please use the following links to apply for a UK patent or file forms and documents you may need for a pending application.</p>
+			
+			<ul>
+				<li><a href="https://www.gov.uk/apply-for-a-patent" target="_blank" rel="noopener noreferrer"><strong>Apply for a patent online with the Intellectual Property Office</strong></a></li>
+			</ul>
+
+			<ul>
+				<li><a href="https://www.gov.uk/file-documents-pending-patent" target="_blank" rel="noopener noreferrer"><strong>File other patent forms/documents for a pending patent application with the Intellectual Property Office</strong></a></li>
+			</ul>
+
+			<p>For further information, you can contact the Intellectual Property Office Information Centre on Tel 0300 300 2000 between 09.00 and 17.00 Monday to Friday, excluding bank holidays, or by email at <a href="mailto:information@ipo.gov.uk">information@ipo.gov.uk</a>.</p>
+			<p>Please note these services may be closed at certain times for maintenance and that between 01.00 and 03.00 (UK time) there are scheduled housekeeping tasks which may affect the availability of these forms.</p>
+			
+			<p>In partnership with:<br />
+				<a href="https://www.gov.uk/government/organisations/intellectual-property-office" target="_blank" rel="noopener noreferrer" title="Intellectual Property Office">
+					<img src="/_img/IPO_LOGO.gif" width="150" height="26" alt="" /> Intellectual Property Office
+				</a>
+			</p>
+
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div><!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
