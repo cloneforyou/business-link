c8d088b0be080fde826c99838d9033fa84e1da0b
Caroline Hagan <caroline@blueocto.co.uk>
2020-09-05 23:45:44 +0100

    Completed page "Assess your options for growth" for section /grow-your-business/setting-a-vision-for-growth/

---
+++ grow-your-business/setting-a-vision-for-growth/assess-your-options-for-growth.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Assess your options for growth | Business Link</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+	<!-- ref: .html || / -->
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Assess your options for growth</h1>
+			<h2>Introduction</h2>
+			<img src="/_img/Assess_your_options_for_growth.jpg" width="94" height="62" alt="" />
+			<p>It is important to evaluate whether you want to consolidate your business' position or find ways to grow.</p>
+			<p>If you decide that your priority is growth, you need to plan carefully in order to succeed. Growth has its risks, but the right strategy can deliver stability, security and long-term profits. Once you have assessed your current position and how well equipped you are to handle growth, you can start building your strategy for growth.</p>
+			<p>This guide shows you how to evaluate the right strategy for your business, when to launch it and the finance options available. It looks at the pros and cons of diversifying and what other considerations you must think of to ensure development is smooth, on time and on target.</p>
+			<ul>
+				<li><a href="/grow-your-business/setting-a-vision-for-growth/assess-your-options-for-growth/the-importance-of-business-growth.php" title="The importance of business growth">Next</a></li>
+			</ul>
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li>Introduction</li>
+				<li><a href="/grow-your-business/setting-a-vision-for-growth/assess-your-options-for-growth/the-importance-of-business-growth.php">The importance of business growth</a></li>
+				<li><a href="/grow-your-business/setting-a-vision-for-growth/assess-your-options-for-growth/consolidate-your-existing-performance.php">Consolidate your existing performance</a></li>
+				<li><a href="/grow-your-business/setting-a-vision-for-growth/assess-your-options-for-growth/options-for-growth-increasing-market-share.php">Options for growth: increasing market share</a></li>
+				<li><a href="/grow-your-business/setting-a-vision-for-growth/assess-your-options-for-growth/options-for-growth-diversification.php">Options for growth: diversification</a></li>
+				<li><a href="/grow-your-business/setting-a-vision-for-growth/assess-your-options-for-growth/options-for-growth-partnerships-joint-ventures-mergers-and-acquisitions.php">Options for growth: partnerships, joint ventures, mergers and acquisitions</a></li>
+				<li><a href="/grow-your-business/setting-a-vision-for-growth/assess-your-options-for-growth/assess-which-growth-option-best-suits-your-business.php">Assess which growth option best suits your business</a></li>
+				<li><a href="/grow-your-business/setting-a-vision-for-growth/assess-your-options-for-growth/the-practicalities-of-growth.php">The practicalities of growth</a></li>
+				<li><a href="/grow-your-business/setting-a-vision-for-growth/assess-your-options-for-growth/getting-a-return-on-your-investment-for-growth.php">Getting a return on your investment for growth</a></li>
+				<li><a href="/grow-your-business/setting-a-vision-for-growth/assess-your-options-for-growth/financing-your-growth-strategy.php">Financing your growth strategy</a></li>
+				<li><a href="/grow-your-business/setting-a-vision-for-growth/assess-your-options-for-growth/here-is-how-i-diversified-into-new-areas.php">Here's how I diversified into new areas</a></li>
+			</ul>
+
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+			
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="/_files/cim_approaches-to-business-growth_2001.pdf" target="_blank">Download business review and growth guidance from the Chartered Institute of Marketing website (PDF, 149K)</a></li>
+				<li><a href="https://www.learndirect.com/course/business-decision-analysis" target="_blank" rel="nofollow noreferrer">Business Decision Analysis course on the learndirect business website</a></li>
+			</ul>
+			
+			<img src="/_img/px_buyingandselling.gif" alt="" width="181" height="5" />
+			
+			<p>Developed with:<br />
+				<a href="https://www.cranfield.ac.uk/som" target="_blank" rel="noopener noreferrer">
+					<img src="/_img/Cranfield_agreed.gif" width="93" height="36" alt="" /> Cranfield School of Management
+				</a>
+			</p>
+
+			<ul>
+				<li><a href="/about-business-link.php" title="About us">About us</a></li>
+				<li><a href="/my-business.php">My Business</a></li>
+				<li><a href="/whats-new.php">What's new?</a></li>
+				<li><a href="/do-it-online.php">Do it online</a></li>
+				<li><a href="/directories.php">Directories</a></li>
+			</ul>
+
+		</div><!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div><!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
